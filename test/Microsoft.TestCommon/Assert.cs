771da624001398da8f785e6af5767c977eda22d8
N. Taylor Mullen <nimullen@microsoft.com>
2014-01-18 23:43:51 -0800

    Removed a lot of the Assert extensibility types

--- test/Microsoft.TestCommon/Assert.cs
+++ test/Microsoft.TestCommon/Assert.cs
@@ -9,22 +9,5 @@ namespace Microsoft.TestCommon
     // See files named XxxAssertions for root extensions to Assert.
     public partial class Assert : Xunit.Assert
     {
-        public static readonly ReflectionAssert Reflection = new ReflectionAssert();
-
-        public static readonly TypeAssert Type = new TypeAssert();
-
-        public static readonly HttpAssert Http = new HttpAssert();
-
-        public static readonly MediaTypeAssert MediaType = new MediaTypeAssert();
-
-        public static readonly GenericTypeAssert GenericType = new GenericTypeAssert();
-
-        public static readonly SerializerAssert Serializer = new SerializerAssert();
-
-        public static readonly StreamAssert Stream = new StreamAssert();
-
-        public static readonly TaskAssert Task = new TaskAssert();
-
-        public static readonly XmlAssert Xml = new XmlAssert();
     }
 }
