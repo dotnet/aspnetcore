fbc894b0b9a613f5dc88a52f938384dc463b705b
David Fowler <davidfowl@gmail.com>
2014-01-23 22:31:19 -0800

    Updated build file to makefile.shade

--- build.cmd
+++ build.cmd
@@ -9,4 +9,4 @@ mkdir .nuget
 :part2
 set EnableNuGetPackageRestore=true
 .nuget\NuGet.exe install Sake -version 0.2 -o packages
-packages\Sake.0.2\tools\Sake.exe -I build -f Sakefile.shade %*
+packages\Sake.0.2\tools\Sake.exe -I build -f makefile.shade %*
