a8b4c71284f3ad4f22a7cd9ae95c479a8ba5cc69
David Fowler <davidfowl@gmail.com>
2014-01-25 16:29:15 -0800

    Added temporary owin glue for HttpAbstractions based on Microsoft.Owin.

---
+++ src/Microsoft.AspNet.Abstractions.Owin/OwinExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Abstractions.Owin;
+
+namespace Owin
+{
+    public static class OwinExtensions
+    {
+        public static void RunHttpContext(this IAppBuilder app, Func<OwinHttpContext, Task> handler)
+        {
+            app.Run(context =>
+            {
+                var httpContext = new OwinHttpContext(context);
+
+                return handler(httpContext);
+            });
+        }
+    }
+}
