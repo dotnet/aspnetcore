62bcfc0a95d60358037d0a50361f309ef1ec7994
David Fowler <davidfowl@gmail.com>
2014-01-25 16:36:02 -0800

    Fixed up the func.

--- src/Microsoft.AspNet.Abstractions.Owin/OwinExtensions.cs
+++ src/Microsoft.AspNet.Abstractions.Owin/OwinExtensions.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Abstractions;
 using Microsoft.AspNet.Abstractions.Owin;
 
 namespace Owin
 {
     public static class OwinExtensions
     {
-        public static void RunHttpContext(this IAppBuilder app, Func<OwinHttpContext, Task> handler)
+        public static void RunHttpContext(this IAppBuilder app, Func<HttpContext, Task> handler)
         {
             app.Run(context =>
             {
