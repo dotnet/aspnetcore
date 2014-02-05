856c09ae1086de70c00ad91f2f5f840781e4fbc3
Ryan Nowak <nowakra@gmail.com>
2014-02-04 16:24:17 -0800

    Removing OwinRouteEndpoint

--- samples/RoutingSample/Startup.cs
+++ samples/RoutingSample/Startup.cs
@@ -2,17 +2,9 @@
 
 #if NET45
 
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Abstractions;
 using Microsoft.AspNet.Routing.Owin;
 using Owin;
-using Microsoft.AspNet.PipelineCore.Owin;
-using Microsoft.AspNet.Routing;
-using System;
-using Microsoft.AspNet.Abstractions;
 
 namespace RoutingSample
 {
@@ -29,21 +21,13 @@ namespace RoutingSample
         {            
             var routes = builder.UseRouter();
 
-            var endpoint1 = new OwinRouteEndpoint(async (context) => await WriteToBodyAsync(context, "match1"));
+            var endpoint1 = new HttpContextRouteEndpoint(async (context) => await context.Response.WriteAsync("match1"));
             var endpoint2 = new HttpContextRouteEndpoint(async (context) => await context.Response.WriteAsync("Hello, World!"));
 
             routes.Add(new PrefixRoute(endpoint1, "api/store"));
             routes.Add(new PrefixRoute(endpoint1, "api/checkout"));
             routes.Add(new PrefixRoute(endpoint2, "hello/world"));
             routes.Add(new PrefixRoute(endpoint1, ""));
-        }
-
-        private static async Task WriteToBodyAsync(IDictionary<string, object> context, string text)
-        {
-            var stream = (Stream)context["owin.ResponseBody"];
-
-            byte[] bytes = Encoding.UTF8.GetBytes(text);
-            await stream.WriteAsync(bytes, 0, bytes.Length);
         }
     }
 }
