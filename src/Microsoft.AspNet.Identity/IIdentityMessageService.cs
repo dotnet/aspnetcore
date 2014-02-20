27027b917ce718950bc7c35c553206ca84ef73a0
Hao Kung <haok@microsoft.com>
2014-02-19 16:27:47 -0800

    Split IdentityMessage into its own file

--- src/Microsoft.AspNet.Identity/IIdentityMessageService.cs
+++ src/Microsoft.AspNet.Identity/IIdentityMessageService.cs
@@ -14,25 +14,4 @@ namespace Microsoft.AspNet.Identity
         /// <returns></returns>
         Task Send(IdentityMessage message);
     }
-
-    /// <summary>
-    ///     Represents a message
-    /// </summary>
-    public class IdentityMessage
-    {
-        /// <summary>
-        ///     Destination, i.e. To email, or SMS phone number
-        /// </summary>
-        public virtual string Destination { get; set; }
-
-        /// <summary>
-        ///     Subject
-        /// </summary>
-        public virtual string Subject { get; set; }
-
-        /// <summary>
-        ///     Message contents
-        /// </summary>
-        public virtual string Body { get; set; }
-    }
 }
---
+++ src/Microsoft.AspNet.Identity/IdentityMessage.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.Identity
+{
+    /// <summary>
+    ///     Represents a message
+    /// </summary>
+    public class IdentityMessage
+    {
+        /// <summary>
+        ///     Destination, i.e. To email, or SMS phone number
+        /// </summary>
+        public virtual string Destination { get; set; }
+
+        /// <summary>
+        ///     Subject
+        /// </summary>
+        public virtual string Subject { get; set; }
+
+        /// <summary>
+        ///     Message contents
+        /// </summary>
+        public virtual string Body { get; set; }
+    }
+}
