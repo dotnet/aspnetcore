aspnet/Mvc

Introduce FormValueProviderFactory to expose form data as a value provider

src/Microsoft.AspNet.Mvc.ModelBinding/ValueProviders/IValueProviderFactory.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Threading.Tasks;
+
 namespace Microsoft.AspNet.Mvc.ModelBinding
 {
     public interface IValueProviderFactory
@@ -8,6 +9,6 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
         /// </summary>
         /// <param name="requestContext">RequestContext that value provider will populate from</param>
         /// <returns>a value provider instance or null</returns>
-        IValueProvider GetValueProvider(RequestContext requestContext);
+        Task<IValueProvider> GetValueProvider(RequestContext requestContext);
     }
 }

src/Microsoft.AspNet.Mvc.ModelBinding/ValueProviders/QueryStringValueProviderFactory.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Globalization;
+﻿using System.Globalization;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc.ModelBinding.Internal;
 
 namespace Microsoft.AspNet.Mvc.ModelBinding
@@ -8,24 +8,28 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
     {
         private static readonly object _cacheKey = new object();
 
-        public IValueProvider GetValueProvider(RequestContext requestContext)
+        public Task<IValueProvider> GetValueProvider(RequestContext requestContext)
         {
             if (requestContext == null)
             {
                 throw Error.ArgumentNull("requestContext");
             }
 
-            // Process the query string once-per request. 
-            IDictionary<object, object> storage = requestContext.HttpContext.Items;
+            // Process the query collection once-per request. 
+            var storage = requestContext.HttpContext.Items;
             object value;
+            IValueProvider provider;
             if (!storage.TryGetValue(_cacheKey, out value))
             {
-                var provider = new QueryStringValueProvider(requestContext.HttpContext, CultureInfo.InvariantCulture);
+                var queryCollection = requestContext.HttpContext.Request.Query;
+                provider = new ReadableStringCollectionValueProvider(queryCollection, CultureInfo.InvariantCulture);
                 storage[_cacheKey] = provider;
-                return provider;
             }
-
-            return (QueryStringValueProvider)value;
+            else
+            {
+                provider = (ReadableStringCollectionValueProvider)value;
+            }
+            return Task.FromResult(provider);
         }
     }
 }

src/Microsoft.AspNet.Mvc.ModelBinding/ValueProviders/RouteValueValueProviderFactory.cs
@@ -1,11 +1,14 @@
 ﻿
+using System.Threading.Tasks;
+
 namespace Microsoft.AspNet.Mvc.ModelBinding
 {
     public class RouteValueValueProviderFactory : IValueProviderFactory
     {
-        public IValueProvider GetValueProvider(RequestContext requestContext)
+        public Task<IValueProvider> GetValueProvider(RequestContext requestContext)
         {
-            return new DictionaryBasedValueProvider(requestContext.RouteValues);
+            var valueProvider = new DictionaryBasedValueProvider(requestContext.RouteValues);
+            return Task.FromResult<IValueProvider>(valueProvider);
         }
     }
 }

src/Microsoft.AspNet.Mvc/DefaultActionSelector.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc.ModelBinding;
 
 namespace Microsoft.AspNet.Mvc
@@ -16,7 +17,7 @@ namespace Microsoft.AspNet.Mvc
             _valueProviderFactory = valueProviderFactories;
         }
 
-        public ActionDescriptor Select(RequestContext context)
+        public async Task<ActionDescriptor> Select(RequestContext context)
         {
             if (context == null)
             {
@@ -36,7 +37,7 @@ namespace Microsoft.AspNet.Mvc
             }
             else
             {
-                return SelectBestCandidate(context, matching);
+                return await SelectBestCandidate(context, matching);
             }
         }
 
@@ -52,9 +53,10 @@ namespace Microsoft.AspNet.Mvc
                    (descriptor.DynamicConstraints == null || descriptor.DynamicConstraints.All(c => c.Accept(context)));
         }
 
-        protected virtual ActionDescriptor SelectBestCandidate(RequestContext context, List<ActionDescriptor> candidates)
+        protected virtual async Task<ActionDescriptor> SelectBestCandidate(RequestContext context, List<ActionDescriptor> candidates)
         {
-            var valueProviders = _valueProviderFactory.Select(vpf => vpf.GetValueProvider(context)).ToArray();
+            var valueProviders = await Task.WhenAll(_valueProviderFactory.Select(vpf => vpf.GetValueProvider(context)));
+            valueProviders = valueProviders.Where(vp => vp != null).ToArray();
 
             var applicableCandiates = new List<ActionDescriptorCandidate>();
             foreach (var action in candidates)

src/Microsoft.AspNet.Mvc/IActionSelector.cs
@@ -1,8 +1,10 @@
-﻿namespace Microsoft.AspNet.Mvc
+﻿using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.Mvc
 {
     public interface IActionSelector
     {
-        ActionDescriptor Select(RequestContext context);
+        Task<ActionDescriptor> Select(RequestContext context);
 
         bool Match(ActionDescriptor descriptor, RequestContext context);
     }

src/Microsoft.AspNet.Mvc/Routing/RouteEndpoint.cs
@@ -51,7 +51,7 @@ namespace Microsoft.AspNet.Mvc.Routing
             var routeValues = context.GetFeature<IRouteValues>();
             var requestContext = new RequestContext(context, routeValues.Values);
 
-            var actionDescriptor = ActionSelector.Select(requestContext);
+            var actionDescriptor = await ActionSelector.Select(requestContext);
 
             if (actionDescriptor == null)
             {

dotnet/aspnetcore

src/Microsoft.AspNet.Mvc.ModelBinding/ValueProviders/IValueProviderFactory.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Threading.Tasks;
+
 namespace Microsoft.AspNet.Mvc.ModelBinding
 {
     public interface IValueProviderFactory
@@ -8,6 +9,6 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
         /// </summary>
         /// <param name="requestContext">RequestContext that value provider will populate from</param>
         /// <returns>a value provider instance or null</returns>
-        IValueProvider GetValueProvider(RequestContext requestContext);
+        Task<IValueProvider> GetValueProviderAsync(RequestContext requestContext);
     }
 }

src/Microsoft.AspNet.Mvc.ModelBinding/ValueProviders/QueryStringValueProviderFactory.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Globalization;
+﻿using System.Globalization;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc.ModelBinding.Internal;
 
 namespace Microsoft.AspNet.Mvc.ModelBinding
@@ -8,24 +8,28 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
     {
         private static readonly object _cacheKey = new object();
 
-        public IValueProvider GetValueProvider(RequestContext requestContext)
+        public Task<IValueProvider> GetValueProviderAsync(RequestContext requestContext)
         {
             if (requestContext == null)
             {
                 throw Error.ArgumentNull("requestContext");
             }
 
-            // Process the query string once-per request. 
-            IDictionary<object, object> storage = requestContext.HttpContext.Items;
+            // Process the query collection once-per request. 
+            var storage = requestContext.HttpContext.Items;
             object value;
+            IValueProvider provider;
             if (!storage.TryGetValue(_cacheKey, out value))
             {
-                var provider = new QueryStringValueProvider(requestContext.HttpContext, CultureInfo.InvariantCulture);
+                var queryCollection = requestContext.HttpContext.Request.Query;
+                provider = new ReadableStringCollectionValueProvider(queryCollection, CultureInfo.InvariantCulture);
                 storage[_cacheKey] = provider;
-                return provider;
             }
-
-            return (QueryStringValueProvider)value;
+            else
+            {
+                provider = (ReadableStringCollectionValueProvider)value;
+            }
+            return Task.FromResult(provider);
         }
     }
 }

src/Microsoft.AspNet.Mvc.ModelBinding/ValueProviders/RouteValueValueProviderFactory.cs
@@ -1,11 +1,14 @@
 ﻿
+using System.Threading.Tasks;
+
 namespace Microsoft.AspNet.Mvc.ModelBinding
 {
     public class RouteValueValueProviderFactory : IValueProviderFactory
     {
-        public IValueProvider GetValueProvider(RequestContext requestContext)
+        public Task<IValueProvider> GetValueProviderAsync(RequestContext requestContext)
         {
-            return new DictionaryBasedValueProvider(requestContext.RouteValues);
+            var valueProvider = new DictionaryBasedValueProvider(requestContext.RouteValues);
+            return Task.FromResult<IValueProvider>(valueProvider);
         }
     }
 }

src/Microsoft.AspNet.Mvc/DefaultActionSelector.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc.ModelBinding;
 
 namespace Microsoft.AspNet.Mvc
@@ -16,7 +17,7 @@ namespace Microsoft.AspNet.Mvc
             _valueProviderFactory = valueProviderFactories;
         }
 
-        public ActionDescriptor Select(RequestContext context)
+        public async Task<ActionDescriptor> SelectAsync(RequestContext context)
         {
             if (context == null)
             {
@@ -36,7 +37,7 @@ namespace Microsoft.AspNet.Mvc
             }
             else
             {
-                return SelectBestCandidate(context, matching);
+                return await SelectBestCandidate(context, matching);
             }
         }
 
@@ -52,9 +53,10 @@ namespace Microsoft.AspNet.Mvc
                    (descriptor.DynamicConstraints == null || descriptor.DynamicConstraints.All(c => c.Accept(context)));
         }
 
-        protected virtual ActionDescriptor SelectBestCandidate(RequestContext context, List<ActionDescriptor> candidates)
+        protected virtual async Task<ActionDescriptor> SelectBestCandidate(RequestContext context, List<ActionDescriptor> candidates)
         {
-            var valueProviders = _valueProviderFactory.Select(vpf => vpf.GetValueProvider(context)).ToArray();
+            var valueProviders = await Task.WhenAll(_valueProviderFactory.Select(vpf => vpf.GetValueProviderAsync(context)));
+            valueProviders = valueProviders.Where(vp => vp != null).ToArray();
 
             var applicableCandiates = new List<ActionDescriptorCandidate>();
             foreach (var action in candidates)

src/Microsoft.AspNet.Mvc/IActionSelector.cs
@@ -1,8 +1,10 @@
-﻿namespace Microsoft.AspNet.Mvc
+﻿using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.Mvc
 {
     public interface IActionSelector
     {
-        ActionDescriptor Select(RequestContext context);
+        Task<ActionDescriptor> SelectAsync(RequestContext context);
 
         bool Match(ActionDescriptor descriptor, RequestContext context);
     }

src/Microsoft.AspNet.Mvc/Routing/RouteEndpoint.cs
@@ -51,7 +51,7 @@ namespace Microsoft.AspNet.Mvc.Routing
             var routeValues = context.GetFeature<IRouteValues>();
             var requestContext = new RequestContext(context, routeValues.Values);
 
-            var actionDescriptor = ActionSelector.Select(requestContext);
+            var actionDescriptor = await ActionSelector.SelectAsync(requestContext);
 
             if (actionDescriptor == null)
             {