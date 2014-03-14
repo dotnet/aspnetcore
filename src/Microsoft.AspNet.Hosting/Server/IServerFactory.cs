4d511b3aef12874d100d7cdf5289c330b4c362df
Chris Ross <Tratcher@Outlook.com>
2014-03-14 15:13:00 -0700

    Make IServerFactory not assembly neutral temporarily.

--- src/Microsoft.AspNet.Hosting/Server/IServerFactory.cs
+++ src/Microsoft.AspNet.Hosting/Server/IServerFactory.cs
@@ -5,7 +5,7 @@ using Microsoft.Net.Runtime;
 
 namespace Microsoft.AspNet.Hosting.Server
 {
-    [AssemblyNeutral]
+    // TODO: [AssemblyNeutral]
     public interface IServerFactory
     {
         IDisposable Start(Func<object, Task> application);
