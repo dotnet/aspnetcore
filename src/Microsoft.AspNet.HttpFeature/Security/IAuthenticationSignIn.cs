fc8832f69fd53c220a12e4d6ef675ec12fe8cb33
Chris Ross <Tratcher@Outlook.com>
2014-03-18 10:23:25 -0700

    Add Claims dependency.

--- src/Microsoft.AspNet.HttpFeature/Security/IAuthenticationSignIn.cs
+++ src/Microsoft.AspNet.HttpFeature/Security/IAuthenticationSignIn.cs
@@ -1,15 +1,11 @@
 using System.Collections.Generic;
-#if NET45
 using System.Security.Claims;
-#endif
 
 namespace Microsoft.AspNet.HttpFeature.Security
 {
     public interface IAuthenticationSignIn
     {
-#if NET45
         ClaimsPrincipal User { get; }
-#endif
         IDictionary<string, string> Properties { get; }
     }
 }
