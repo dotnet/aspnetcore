34afda9dfce5b09252bc159a4f644af9e52eb14d
Pranav K <prkrishn@hotmail.com>
2014-03-17 22:19:27 -0700

    Adding tooling for mirroring project k packages

---
+++ tools/ProjectKClone/MyServiceInstaller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetClone
+{
+    [RunInstaller(true)]
+    public class MyServiceInstaller : Installer
+    {
+        internal const string ServiceName = "ProjectKClone";
+
+        public MyServiceInstaller()
+        {
+            var processInstaller = new ServiceProcessInstaller();
+            var serviceInstaller = new ServiceInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalSystem;
+            processInstaller.Username = null;
+            processInstaller.Password = null;
+
+            serviceInstaller.ServiceName = ServiceName;
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            serviceInstaller.ServiceName = ServiceName;
+
+            this.Installers.Add(processInstaller);
+            this.Installers.Add(serviceInstaller);
+
+            this.Committed += new InstallEventHandler(ServiceInstaller_Committed);
+        }
+
+        void ServiceInstaller_Committed(object sender, InstallEventArgs e)
+        {
+            // Auto Start the Service Once Installation is Finished.
+            var controller = new ServiceController(ServiceName);
+            controller.Start();
+        }
+    }
+}
---
+++ tools/ProjectKClone/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetClone
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new MyService() 
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
