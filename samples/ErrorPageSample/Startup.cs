a18e0375784a2aa3b3cbe5c5505c885119482a04
David Fowler <davidfowl@gmail.com>
2014-03-27 22:42:39 -0700

    Updated samples to use new hosting APIs

--- samples/ErrorPageSample/Startup.cs
+++ samples/ErrorPageSample/Startup.cs
@@ -1,27 +1,18 @@
-﻿#if NET45
-using System;
+﻿using System;
 using Microsoft.AspNet;
 using Microsoft.AspNet.Abstractions;
-using Owin;
 
 namespace ErrorPageSample
 {
     public class Startup
     {
-        public void Configuration(IAppBuilder app)
+        public void Configuration(IBuilder app)
         {
-            // Temporary bridge from katana to Owin
-            app.UseBuilder(ConfigurePK);
-        }
-
-        private void ConfigurePK(IBuilder builder)
-        {
-            builder.UseErrorPage();
-            builder.Run(context =>
+            app.UseErrorPage();
+            app.Run(context =>
             {
                 throw new Exception("Demonstration exception");
             });
         }
     }
 }
-#endif
--- samples/WelcomePageSample/Startup.cs
+++ samples/WelcomePageSample/Startup.cs
@@ -1,22 +1,13 @@
-﻿#if NET45
-using Microsoft.AspNet;
+﻿using Microsoft.AspNet;
 using Microsoft.AspNet.Abstractions;
-using Owin;
 
 namespace WelcomePageSample
 {
     public class Startup
     {
-        public void Configuration(IAppBuilder app)
+        public void Configuration(IBuilder app)
         {
-            // Temporary bridge from katana to Owin
-            app.UseBuilder(ConfigurePK);
-        }
-
-        private void ConfigurePK(IBuilder builder)
-        {
-            builder.UseWelcomePage();
+            app.UseWelcomePage();
         }
     }
 }
-#endif
