ff5d919e0d93d8ceca6181cd8650606c871bd8da
Yishai Galatzer <yigalatz@microsoft.com>
2014-03-28 18:16:01 -0700

    Remove unused usings

--- test/Microsoft.AspNet.Routing.Tests/ConstraintsBuilderTests.cs
+++ test/Microsoft.AspNet.Routing.Tests/ConstraintsBuilderTests.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Abstractions;
 using Microsoft.AspNet.Testing;
 using Moq;
 using Xunit;
