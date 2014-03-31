cb982f19bb6822b0cf394b84fe76517823f0dc8f
Chris Ross <Tratcher@Outlook.com>
2014-03-31 13:05:03 -0700

    Update samples.

---
+++ samples/SelfHostServer/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Abstractions;
+using Microsoft.AspNet.Server.WebListener;
+
+namespace SelfHostServer
+{
+    public class Startup
+    {
+        public void Configuration(IBuilder app)
+        {
+            var info = (ServerInformation)app.Server;
+            info.Listener.AuthenticationManager.AuthenticationTypes = AuthenticationType.None;
+
+            app.Run(async context =>
+            {
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync("Hello world");
+            });
+        }
+    }
+}
