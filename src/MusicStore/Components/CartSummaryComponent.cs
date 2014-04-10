b51f54e98a41d970cfbe03ee2842215c33b86541
Praburaj <praburaj.t@microsoft.com>
2014-04-10 13:46:58 -0700

    Adding a view component for cart summary
    1. Added a View component named CartSummary
    2. Moved the CartSummary.cshtml as default.cshtml to shared\Components

---
+++ src/MusicStore/Components/CartSummaryComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using MusicStore.Models;
+
+namespace MusicStore.Components
+{
+    [ViewComponent(Name = "CartSummary")]
+    public class CartSummaryComponent : ViewComponent
+    {
+        private MusicStoreContext db = new MusicStoreContext();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var cart = ShoppingCart.GetCart(db, this.Context);
+
+            var cartItems = cart.GetCartItems()
+                .Select(a => a.Album.Title)
+                .OrderBy(x => x);
+
+            //Bug: Uncommenting results in compilation error. 
+            //ViewBag.CartCount = cartItems.Count();
+            //ViewBag.CartSummary = string.Join("\n", cartItems.Distinct());
+
+            return View();
+        }
+    }
+}
