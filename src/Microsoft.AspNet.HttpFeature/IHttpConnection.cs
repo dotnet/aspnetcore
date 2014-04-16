28fbacc7f4a64fdfe765fce6b43f06b7262cfd9c
Chris Ross <Tratcher@Outlook.com>
2014-04-16 09:17:37 -0700

    Remove #if NET45s for IPAddress, X509Certificate.

--- src/Microsoft.AspNet.HttpFeature/IHttpConnection.cs
+++ src/Microsoft.AspNet.HttpFeature/IHttpConnection.cs
@@ -1,15 +1,11 @@
-#if NET45
 using System.Net;
-#endif
 
 namespace Microsoft.AspNet.HttpFeature
 {
     public interface IHttpConnection
     {
-#if NET45
         IPAddress RemoteIpAddress { get; set; }
         IPAddress LocalIpAddress { get; set; }
-#endif
         int RemotePort { get; set; }
         int LocalPort { get; set; }
         bool IsLocal { get; set; }
--- src/Microsoft.AspNet.HttpFeature/IHttpTransportLayerSecurity.cs
+++ src/Microsoft.AspNet.HttpFeature/IHttpTransportLayerSecurity.cs
@@ -1,15 +1,12 @@
-#if NET45
+
 using System.Security.Cryptography.X509Certificates;
-#endif
 using System.Threading.Tasks;
 
 namespace Microsoft.AspNet.HttpFeature
 {
     public interface IHttpTransportLayerSecurity
     {
-#if NET45
         X509Certificate ClientCertificate { get; set; }
-#endif
         Task LoadAsync();
     }
 }
