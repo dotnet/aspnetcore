98f4212915fb5c6ac615b0b21fca43dba6f5127c
Chris Ross <Tratcher@Outlook.com>
2014-04-30 10:53:33 -0700

    #20 - Add Use extension for inline middleware.

---
+++ src/Microsoft.AspNet.Abstractions/Extensions/UseExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Abstractions;
+
+namespace Microsoft.AspNet
+{
+    public static class UseExtensions
+    {
+        /// <summary>
+        /// Use middleware defined in-line.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="middleware">A function that handles the request or calls the given next function.</param>
+        /// <returns></returns>
+        public static IBuilder Use(this IBuilder app, Func<HttpContext, Func<Task>, Task> middleware)
+        {
+            return app.Use(next =>
+            {
+                return context =>
+                {
+                    Func<Task> simpleNext = () => next(context);
+                    return middleware(context, simpleNext);
+                };
+            });
+        }
+    }
+}
