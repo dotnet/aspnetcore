8208cd6b2f53d555e3c8389cbfced72ca0e4f5fe
anpete <anpete@microsoft.com>
2014-05-01 17:42:33 -0700

    Update file headers

--- src/Microsoft.AspNet.Mvc.Core/RouteKeyHandling.cs
+++ src/Microsoft.AspNet.Mvc.Core/RouteKeyHandling.cs
@@ -1,4 +1,21 @@
-﻿namespace Microsoft.AspNet.Mvc
+// Copyright (c) Microsoft Open Technologies, Inc.
+// All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
+// WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF
+// TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR
+// NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing
+// permissions and limitations under the License.
+
+namespace Microsoft.AspNet.Mvc
 {
     // This needs more thought, the intent is that we would be able to cache over this constraint without running the accept method.
     public enum RouteKeyHandling
--- test/Microsoft.AspNet.Mvc.Razor.Test/RazorViewTest.cs
+++ test/Microsoft.AspNet.Mvc.Razor.Test/RazorViewTest.cs
@@ -1,4 +1,21 @@
-﻿using System;
+// Copyright (c) Microsoft Open Technologies, Inc.
+// All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
+// WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF
+// TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR
+// NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing
+// permissions and limitations under the License.
+
+using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Abstractions;
