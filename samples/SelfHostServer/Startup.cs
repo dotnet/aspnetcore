21616bcde53e4aa64c38151240c073a7c0e3cb16
David Fowler <davidfowl@gmail.com>
2014-05-04 11:28:12 -0700

    Fixed up xunit references and fixed sample.

--- samples/SelfHostServer/Startup.cs
+++ samples/SelfHostServer/Startup.cs
@@ -15,6 +15,7 @@
 // See the Apache 2 License for the specific language governing
 // permissions and limitations under the License.
 
+using Microsoft.AspNet;
 using Microsoft.AspNet.Abstractions;
 using Microsoft.AspNet.Server.WebListener;
 using Microsoft.Net.Server;
