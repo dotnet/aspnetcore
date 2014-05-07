078ba4547701d9366145e3207fcfa34d69f33637
ajcvickers <ajcvickers@hotmail.com>
2014-05-07 10:27:51 -0700

    Updating for SqlServerConnectionString => UseSqlServer change

--- src/Microsoft.AspNet.Identity.Entity/IdentityContext.cs
+++ src/Microsoft.AspNet.Identity.Entity/IdentityContext.cs
@@ -52,7 +52,7 @@ namespace Microsoft.AspNet.Identity.Entity
         protected override void OnConfiguring(DbContextOptions builder)
         {
 //#if NET45
-//            builder.SqlServerConnectionString(@"Server=(localdb)\v11.0;Database=IdentityDb3;Trusted_Connection=True;");
+//            builder.UseSqlServer(@"Server=(localdb)\v11.0;Database=IdentityDb3;Trusted_Connection=True;");
 //#else
             builder.UseInMemoryStore();
 //#endif
