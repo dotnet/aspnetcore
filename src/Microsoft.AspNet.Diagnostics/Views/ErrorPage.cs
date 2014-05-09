54c9794c2a7042aba042c2cc2739dfd99a70dc25
Andrew Peters <anpete@microsoft.com>
2014-05-08 23:00:53 -0700

    Updating copyright headers

--- src/Microsoft.AspNet.Diagnostics/Views/ErrorPage.cs
+++ src/Microsoft.AspNet.Diagnostics/Views/ErrorPage.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 namespace Microsoft.AspNet.Diagnostics.Views
 {
 #line 1 "ErrorPage.cshtml"
