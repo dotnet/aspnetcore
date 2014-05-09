d02f76a2fd11508dc3eccdc99c4610fefb17e66d
Andrew Peters <anpete@microsoft.com>
2014-05-08 23:01:33 -0700

    Updating copyright headers

--- src/Microsoft.AspNet.Security/Infrastructure/HttpContextExtensions.cs
+++ src/Microsoft.AspNet.Security/Infrastructure/HttpContextExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNet.Http;
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Http;
 using Microsoft.AspNet.HttpFeature.Security;
 using Microsoft.AspNet.PipelineCore.Security;
 
--- src/Microsoft.AspNet.Security/Infrastructure/SignInIdentityContext.cs
+++ src/Microsoft.AspNet.Security/Infrastructure/SignInIdentityContext.cs
@@ -1,4 +1,7 @@
-﻿using System.Security.Claims;
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Security.Claims;
 using Microsoft.AspNet.Http.Security;
 
 namespace Microsoft.AspNet.Security.Infrastructure
