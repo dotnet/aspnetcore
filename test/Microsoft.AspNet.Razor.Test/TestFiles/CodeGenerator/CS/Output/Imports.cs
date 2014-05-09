20c91146f1c011d1fd2cb096b08790c55fe43adb
N. Taylor Mullen <nimullen@microsoft.com>
2014-05-09 11:08:22 -0700

    Remove copyright header from generated test files.

--- test/Microsoft.AspNet.Razor.Test/TestFiles/CodeGenerator/CS/Output/Imports.cs
+++ test/Microsoft.AspNet.Razor.Test/TestFiles/CodeGenerator/CS/Output/Imports.cs
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
 namespace TestOutput
 {
 #line 1 "Imports.cshtml"
