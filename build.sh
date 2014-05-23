92e0c16499d9703ac2b1f89efbc5d29ee96d436f
David Fowler <davidfowl@gmail.com>
2014-05-23 03:15:48 -0700

    Made universe mono ready
    - Added build.sh to universe
    - Updated makefile to run build.sh instead of build.cmd
    - Updated build.sh script

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ if test `uname` = Darwin; then
     cachedir=~/Library/Caches/KBuild
 else
     if x$XDG_DATA_HOME = x; then
-    cachedir=$HOME/.local/share
+	cachedir=$HOME/.local/share
     else
-    cachedir=$XDG_DATA_HOME;
+	cachedir=$XDG_DATA_HOME;
     fi
 fi
 mkdir -p $cachedir
