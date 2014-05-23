d8ef441454d5910051e7eff051cd0fb9b00889d8
David Fowler <davidfowl@gmail.com>
2014-05-23 04:16:20 -0700

    Updated build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ if test `uname` = Darwin; then
     cachedir=~/Library/Caches/KBuild
 else
     if x$XDG_DATA_HOME = x; then
-	cachedir=$HOME/.local/share
+    cachedir=$HOME/.local/share
     else
-	cachedir=$XDG_DATA_HOME;
+    cachedir=$XDG_DATA_HOME;
     fi
 fi
 mkdir -p $cachedir
@@ -26,5 +26,15 @@ if test ! -d packages/KoreBuild; then
     mono .nuget/nuget.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
     mono .nuget/nuget.exe install Sake -version 0.2 -o packages -ExcludeVersion
 fi
+
+KRE_VERSION=$(mono .nuget/nuget.exe install KRE-mono45-x86 -pre -o ~/.kre/packages | head -1 | sed "s/.*KRE-mono45-x86 \([^']*\).*/\1/")
+KRE_BIN=~/.kre/packages/KRE-mono45-x86.$KRE_VERSION/bin
+
+chmod +x $KRE_BIN/k
+chmod +x $KRE_BIN/klr
+chmod +x $KRE_BIN/kpm
+chmod +x $KRE_BIN/k-build
+
+export PATH=$KRE_BIN:$PATH
 
 mono packages/Sake/tools/Sake.exe -I packages/KoreBuild/build -f makefile.shade "$@"
