5616ce0bfafc19f34a7a13cb7ef0628c48285043
Ryan Nowak <nowakra@gmail.com>
2014-05-30 15:57:40 -0700

    Fix for #36 - IBuilder extension methods should take IRouter

--- src/Microsoft.AspNet.Routing/BuilderExtensions.cs
+++ src/Microsoft.AspNet.Routing/BuilderExtensions.cs
@@ -1,17 +1,15 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.AspNet;
-using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Routing;
 
 namespace Microsoft.AspNet.Builder
 {
     public static class BuilderExtensions
     {
-        public static IBuilder UseRouter(this IBuilder builder, IRouteCollection routes)
+        public static IBuilder UseRouter([NotNull] this IBuilder builder, [NotNull] IRouter router)
         {
-            builder.Use((next) => new RouterMiddleware(next, routes).Invoke);
+            builder.Use((next) => new RouterMiddleware(next, router).Invoke);
             return builder;
         }
     }
