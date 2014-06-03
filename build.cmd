032ad4a444d978cd1c7e95b78a263c4c51d88aeb
Pranav K <prkrishn@hotmail.com>
2014-06-03 11:10:02 -0700

    Updating makefile to include Options and Logging repos

--- build.cmd
+++ build.cmd
@@ -18,10 +18,11 @@ copy %CACHED_NUGET% .nuget\nuget.exe > nul
 IF EXIST packages\KoreBuild goto run
 .nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
 .nuget\NuGet.exe install Sake -version 0.2 -o packages -ExcludeVersion
+
+IF "%SKIP_KRE_INSTALL%"=="1" goto run
 CALL packages\KoreBuild\build\kvm upgrade -svr50 -x86
 CALL packages\KoreBuild\build\kvm install default -svrc50 -x86
 
 :run
 CALL packages\KoreBuild\build\kvm use default -svr50 -x86
 packages\Sake\tools\Sake.exe -I packages\KoreBuild\build -f makefile.shade %*
-
