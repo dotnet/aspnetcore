95aa6ad60707a7bb8bb0b92f133321f107c0fa49
Yishai Galatzer <yigalatz@microsoft.com>
2014-06-09 14:56:40 -0700

    Add AutoFac to the MVC sample

--- samples/MvcSample.Web/Startup.cs
+++ samples/MvcSample.Web/Startup.cs
@@ -1,10 +1,16 @@
-using Microsoft.AspNet;
+using System;
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Routing;
 using Microsoft.Framework.DependencyInjection;
 using MvcSample.Web.Filters;
 using MvcSample.Web.Services;
+
+#if NET45 
+using Autofac;
+using Microsoft.Framework.DependencyInjection.Autofac;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.OptionsModel;
+#endif
 
 namespace MvcSample.Web
 {
@@ -12,13 +18,48 @@ namespace MvcSample.Web
     {
         public void Configure(IBuilder app)
         {
-            app.UseServices(services =>
+#if NET45
+            var configuration = new Configuration()
+                                    .AddJsonFile(@"App_Data\config.json")
+                                    .AddEnvironmentVariables();
+
+            string diSystem;
+
+            if (configuration.TryGet("DependencyInjection", out diSystem) && 
+                diSystem.Equals("AutoFac", StringComparison.OrdinalIgnoreCase))
             {
+                var services = new ServiceCollection();
+
                 services.AddMvc();
                 services.AddSingleton<PassThroughAttribute>();
                 services.AddSingleton<UserNameService>();
-                services.AddTransient<ITestService, TestService>();
-            });
+                services.AddTransient<ITestService, TestService>();                
+                services.Add(OptionsServices.GetDefaultServices());
+
+                // Create the autofac container 
+                ContainerBuilder builder = new ContainerBuilder();
+
+                // Create the container and use the default application services as a fallback 
+                AutofacRegistration.Populate(
+                    builder,
+                    services,
+                    fallbackServiceProvider: app.ApplicationServices);
+
+                IContainer container = builder.Build();
+
+                app.UseServices(container.Resolve<IServiceProvider>());
+            }
+            else
+#endif
+            {
+                app.UseServices(services =>
+                {
+                    services.AddMvc();
+                    services.AddSingleton<PassThroughAttribute>();
+                    services.AddSingleton<UserNameService>();
+                    services.AddTransient<ITestService, TestService>();
+                });
+            }
 
             app.UseMvc(routes =>
             {
