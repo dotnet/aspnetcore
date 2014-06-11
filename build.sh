ae6004dd11a6df89fc8c1681dd307ade4f30ec3e
Pranav K <prkrishn@hotmail.com>
2014-06-10 17:22:44 -0700

    Updating build.sh based on KRuntime changes

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 if test `uname` = Darwin; then
     cachedir=~/Library/Caches/KBuild
 else
-    if x$XDG_DATA_HOME = x; then
-    cachedir=$HOME/.local/share
+    if [ -z $XDG_DATA_HOME ]; then
+        cachedir=$HOME/.local/share
     else
-    cachedir=$XDG_DATA_HOME;
+        cachedir=$XDG_DATA_HOME;
     fi
 fi
 mkdir -p $cachedir
@@ -14,12 +14,12 @@ mkdir -p $cachedir
 url=https://www.nuget.org/nuget.exe
 
 if test ! -f $cachedir/nuget.exe; then
-    wget -o $cachedir/nuget.exe $url 2>/dev/null || curl -o $cachedir/nuget.exe --location $url /dev/null
+    wget -O $cachedir/nuget.exe $url 2>/dev/null || curl -o $cachedir/nuget.exe --location $url /dev/null
 fi
 
 if test ! -e .nuget; then
     mkdir .nuget
-    cp $cachedir/nuget.exe .nuget
+    cp $cachedir/nuget.exe .nuget/nuget.exe
 fi
 
 if test ! -d packages/KoreBuild; then
@@ -27,14 +27,12 @@ if test ! -d packages/KoreBuild; then
     mono .nuget/nuget.exe install Sake -version 0.2 -o packages -ExcludeVersion
 fi
 
-KRE_VERSION=$(mono .nuget/nuget.exe install KRE-mono45-x86 -pre -o ~/.kre/packages | head -1 | sed "s/.*KRE-mono45-x86 \([^']*\).*/\1/")
-KRE_BIN=~/.kre/packages/KRE-mono45-x86.$KRE_VERSION/bin
+if ! type k > /dev/null 2>&1; then
+    source setup/kvm.sh
+fi
 
-chmod +x $KRE_BIN/k
-chmod +x $KRE_BIN/klr
-chmod +x $KRE_BIN/kpm
-chmod +x $KRE_BIN/k-build
-
-export PATH=$KRE_BIN:$PATH
+if ! type k > /dev/null 2>&1; then
+    kvm upgrade
+fi
 
 mono packages/Sake/tools/Sake.exe -I packages/KoreBuild/build -f makefile.shade "$@"
