aspnet/Mvc

Adding support for ActivateAttribute via IControllerActivator
This mechanism replaces code in Inject that activated properties
by looking them up by names.

src/Microsoft.AspNet.Mvc/MvcServices.cs
@@ -26,6 +26,7 @@ namespace Microsoft.AspNet.Mvc
             var describe = new ServiceDescriber(configuration);
 
             yield return describe.Transient<IControllerFactory, DefaultControllerFactory>();
+            yield return describe.Transient<IControllerActivator, DefaultControllerActivator>();
             yield return describe.Transient<IControllerDescriptorFactory, DefaultControllerDescriptorFactory>();
             yield return describe.Transient<IActionSelector, DefaultActionSelector>();
             yield return describe.Transient<IActionInvokerFactory, ActionInvokerFactory>();

dotnet/aspnetcore

src/Microsoft.AspNet.Mvc/MvcServices.cs
@@ -26,6 +26,7 @@ namespace Microsoft.AspNet.Mvc
             var describe = new ServiceDescriber(configuration);
 
             yield return describe.Transient<IControllerFactory, DefaultControllerFactory>();
+            yield return describe.Singleton<IControllerActivator, DefaultControllerActivator>();
             yield return describe.Scoped<IActionSelector, DefaultActionSelector>();
             yield return describe.Transient<IActionInvokerFactory, ActionInvokerFactory>();
             yield return describe.Transient<IControllerAssemblyProvider, DefaultControllerAssemblyProvider>();