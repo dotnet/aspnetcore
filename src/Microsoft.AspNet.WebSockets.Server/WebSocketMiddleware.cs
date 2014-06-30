b24d808b584f4e9b55c6588f83349b198fcd0877
Chris Ross <Tratcher@Outlook.com>
2014-06-30 12:22:16 -0700

    #10 - Rename Middleware package to Server.

--- src/Microsoft.AspNet.WebSockets.Server/WebSocketMiddleware.cs
+++ src/Microsoft.AspNet.WebSockets.Server/WebSocketMiddleware.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.WebSockets;
@@ -8,7 +11,7 @@ using Microsoft.AspNet.Http;
 using Microsoft.AspNet.HttpFeature;
 using Microsoft.AspNet.WebSockets.Protocol;
 
-namespace Microsoft.AspNet.WebSockets.Middleware
+namespace Microsoft.AspNet.WebSockets.Server
 {
     public class WebSocketMiddleware
     {
--- src/Microsoft.AspNet.WebSockets.Server/WebSocketMiddlewareExtensions.cs
+++ src/Microsoft.AspNet.WebSockets.Server/WebSocketMiddlewareExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNet.WebSockets.Middleware;
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.WebSockets.Server;
 
 namespace Microsoft.AspNet.Builder
 {
