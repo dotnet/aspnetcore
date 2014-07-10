221a8f4f50394ff344493604872e70022bae15b0
David Fowler <davidfowl@gmail.com>
2014-07-10 10:06:16 -0700

    Changed defines from DEBUG to DIAGNOSTICS_PAGE_MIDDLEWARE

--- src/Microsoft.AspNet.Diagnostics/DiagnosticsPageExtensions.cs
+++ src/Microsoft.AspNet.Diagnostics/DiagnosticsPageExtensions.cs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 
-#if DEBUG
+#if DIAGNOSTICS_PAGE_MIDDLEWARE
 using System;
 using Microsoft.AspNet.Abstractions;
 using Microsoft.AspNet.Diagnostics;
