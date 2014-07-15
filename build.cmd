6b2099df28bb97d7495e47e07c257d487b211bec
Pranav K <prkrishn@hotmail.com>
2014-07-14 22:06:25 -0700

    Consume SKIP_KRE_INSTALL in build.cmd to skip installing kvm

--- build.cmd
+++ build.cmd
@@ -18,6 +18,8 @@ copy %CACHED_NUGET% .nuget\nuget.exe > nul
 IF EXIST packages\KoreBuild goto run
 .nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
 .nuget\NuGet.exe install Sake -version 0.2 -o packages -ExcludeVersion
+
+IF "%SKIP_KRE_INSTALL%"=="1" goto run
 CALL packages\KoreBuild\build\kvm upgrade -svr50 -x86
 CALL packages\KoreBuild\build\kvm install default -svrc50 -x86
 
