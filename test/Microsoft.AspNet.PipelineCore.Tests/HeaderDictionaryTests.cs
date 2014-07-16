de1017e0108484278f57cf44f702d1d2f81d5d61
Chris Ross <Tratcher@Outlook.com>
2014-07-16 12:20:57 -0700

    Fix test...

--- test/Microsoft.AspNet.PipelineCore.Tests/HeaderDictionaryTests.cs
+++ test/Microsoft.AspNet.PipelineCore.Tests/HeaderDictionaryTests.cs
@@ -20,7 +20,7 @@ namespace Microsoft.AspNet.PipelineCore.Tests
                 });
 
             Assert.Equal(1, headers.Count);
-            Assert.Equal(new[] { "Header1" }, headers.Keys);
+            Assert.Equal<string>(new[] { "Header1" }, headers.Keys);
             Assert.True(headers.ContainsKey("header1"));
             Assert.False(headers.ContainsKey("header2"));
             Assert.Equal("Value1", headers["header1"]);
