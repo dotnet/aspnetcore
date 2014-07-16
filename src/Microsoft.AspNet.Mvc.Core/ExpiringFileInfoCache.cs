aspnet/Mvc

Cache file info access in viewengine
Move compilation and VirtualPathViewFactory to be singletons
And cache access to files.

The cache time is controlled by MVC options.

The cache is implemented in the ExpiringFileInfoCache.cs

src/Microsoft.AspNet.Mvc.Core/ExpiringFileInfoCache.cs
@@ -1,0 +1,82 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Concurrent;
+using Microsoft.AspNet.FileSystems;
+using Microsoft.Framework.OptionsModel;
+using Microsoft.Framework.Runtime;
+
+namespace Microsoft.AspNet.Mvc.Core
+{
+    /// <summary>
+    /// A default implementation for the <see cref="IExpiringFileInfoCache" interface./>
+    /// </summary>
+    public class ExpiringFileInfoCache : IExpiringFileInfoCache
+    {
+        private readonly ConcurrentDictionary<string, ExpiringFileInfo> _fileInfoCache =
+            new ConcurrentDictionary<string, ExpiringFileInfo>(StringComparer.Ordinal);
+
+        private readonly PhysicalFileSystem _fileSystem;
+        private readonly TimeSpan _offset;
+
+        protected virtual IFileSystem FileSystem
+        {
+            get
+            {
+                return _fileSystem;
+            }
+        }
+
+        protected virtual DateTime UtcNow
+        {
+            get
+            {
+                return DateTime.UtcNow;
+            }
+        }
+
+        public ExpiringFileInfoCache(IApplicationEnvironment env,
+                                     IOptionsAccessor<MvcOptions> optionsAccessor)
+        {
+            // TODO: Inject the IFileSystem but only when we get it from the host
+            _fileSystem = new PhysicalFileSystem(env.ApplicationBasePath);
+            _offset = optionsAccessor.Options.ViewEngineOptions.ExpirationBeforeCheckingFilesOnDisk;
+        }
+
+        /// <inheritdoc />
+        public IFileInfo GetFileInfo([NotNull] string virtualPath)
+        {
+            IFileInfo fileInfo;
+            ExpiringFileInfo expiringFileInfo;
+
+            var utcNow = UtcNow;
+
+            if (_fileInfoCache.TryGetValue(virtualPath, out expiringFileInfo)
+                && expiringFileInfo.ValidUntil > utcNow)
+            {
+                fileInfo = expiringFileInfo.FileInfo;
+            }
+            else
+            {
+                FileSystem.TryGetFileInfo(virtualPath, out fileInfo);
+
+                expiringFileInfo = new ExpiringFileInfo()
+                {
+                    FileInfo = fileInfo,
+                    ValidUntil = utcNow.Add(_offset),
+                };
+
+                _fileInfoCache.AddOrUpdate(virtualPath, expiringFileInfo, (a, b) => expiringFileInfo);
+            }
+
+            return fileInfo;
+        }
+
+        private class ExpiringFileInfo
+        {
+            public IFileInfo FileInfo { get; set; }
+            public DateTime ValidUntil { get; set; }
+        }
+    }
+}

src/Microsoft.AspNet.Mvc.Razor/ViewEngine/VirtualPathViewFactory.cs
@@ -2,36 +2,35 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using Microsoft.AspNet.FileSystems;
+using Microsoft.AspNet.Mvc.Core;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.Framework.DependencyInjection;
-using Microsoft.Framework.Runtime;
 
 namespace Microsoft.AspNet.Mvc.Razor
 {
     public class VirtualPathViewFactory : IVirtualPathViewFactory
     {
-        private readonly PhysicalFileSystem _fileSystem;
         private readonly IRazorCompilationService _compilationService;
         private readonly ITypeActivator _activator;
         private readonly IServiceProvider _serviceProvider;
+        private readonly IExpiringFileInfoCache _fileInfoCache;
 
-        public VirtualPathViewFactory(IApplicationEnvironment env,
-                                      IRazorCompilationService compilationService,
+        public VirtualPathViewFactory(IRazorCompilationService compilationService,
                                       ITypeActivator typeActivator,
-                                      IServiceProvider serviceProvider)
+                                      IServiceProvider serviceProvider,
+                                      IExpiringFileInfoCache fileInfoCache)
         {
-            // TODO: Continue to inject the IFileSystem but only when we get it from the host
-            _fileSystem = new PhysicalFileSystem(env.ApplicationBasePath);
             _compilationService = compilationService;
             _activator = typeActivator;
             _serviceProvider = serviceProvider;
+            _fileInfoCache = fileInfoCache;
         }
 
         public IView CreateInstance([NotNull] string virtualPath)
         {
-            IFileInfo fileInfo;
-            if (_fileSystem.TryGetFileInfo(virtualPath, out fileInfo))
+            var fileInfo = _fileInfoCache.GetFileInfo(virtualPath);
+
+            if (fileInfo != null)
             {
                 var result = _compilationService.Compile(fileInfo);
                 return (IView)_activator.CreateInstance(_serviceProvider, result.CompiledType);

src/Microsoft.AspNet.Mvc/MvcServices.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using Microsoft.AspNet.Mvc.Core;
 using Microsoft.AspNet.Mvc.Filters;
 using Microsoft.AspNet.Mvc.ModelBinding;
 using Microsoft.AspNet.Mvc.Razor;
@@ -41,9 +42,12 @@ namespace Microsoft.AspNet.Mvc
 
             yield return describe.Singleton<IViewEngineProvider, DefaultViewEngineProvider>();
             yield return describe.Scoped<ICompositeViewEngine, CompositeViewEngine>();
-            yield return describe.Transient<IRazorCompilationService, RazorCompilationService>();
-            yield return describe.Transient<IVirtualPathViewFactory, VirtualPathViewFactory>();
+            yield return describe.Singleton<IRazorCompilationService, RazorCompilationService>();
+
             yield return describe.Singleton<IRazorViewActivator, RazorViewActivator>();
+            // Virtual path view factory needs to stay scoped so views can get get scoped services.
+            yield return describe.Scoped<IVirtualPathViewFactory, VirtualPathViewFactory>();
+            yield return describe.Singleton<IExpiringFileInfoCache, ExpiringFileInfoCache>();
 
             yield return describe.Transient<INestedProvider<ActionDescriptorProviderContext>,
                                             ReflectedActionDescriptorProvider>();

dotnet/aspnetcore

src/Microsoft.AspNet.Mvc.Core/ExpiringFileInfoCache.cs
@@ -1,0 +1,82 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Concurrent;
+using Microsoft.AspNet.FileSystems;
+using Microsoft.Framework.OptionsModel;
+using Microsoft.Framework.Runtime;
+
+namespace Microsoft.AspNet.Mvc.Core
+{
+    /// <summary>
+    /// A default implementation for the <see cref="IFileInfoCache" interface./>
+    /// </summary>
+    public class ExpiringFileInfoCache : IFileInfoCache
+    {
+        private readonly ConcurrentDictionary<string, ExpiringFileInfo> _fileInfoCache =
+            new ConcurrentDictionary<string, ExpiringFileInfo>(StringComparer.Ordinal);
+
+        private readonly PhysicalFileSystem _fileSystem;
+        private readonly TimeSpan _offset;
+
+        protected virtual IFileSystem FileSystem
+        {
+            get
+            {
+                return _fileSystem;
+            }
+        }
+
+        protected virtual DateTime UtcNow
+        {
+            get
+            {
+                return DateTime.UtcNow;
+            }
+        }
+
+        public ExpiringFileInfoCache(IApplicationEnvironment env,
+                                     IOptionsAccessor<MvcOptions> optionsAccessor)
+        {
+            // TODO: Inject the IFileSystem but only when we get it from the host
+            _fileSystem = new PhysicalFileSystem(env.ApplicationBasePath);
+            _offset = optionsAccessor.Options.ViewEngineOptions.ExpirationBeforeCheckingFilesOnDisk;
+        }
+
+        /// <inheritdoc />
+        public IFileInfo GetFileInfo([NotNull] string virtualPath)
+        {
+            IFileInfo fileInfo;
+            ExpiringFileInfo expiringFileInfo;
+
+            var utcNow = UtcNow;
+
+            if (_fileInfoCache.TryGetValue(virtualPath, out expiringFileInfo)
+                && expiringFileInfo.ValidUntil > utcNow)
+            {
+                fileInfo = expiringFileInfo.FileInfo;
+            }
+            else
+            {
+                FileSystem.TryGetFileInfo(virtualPath, out fileInfo);
+
+                expiringFileInfo = new ExpiringFileInfo()
+                {
+                    FileInfo = fileInfo,
+                    ValidUntil = _offset == TimeSpan.MaxValue ? DateTime.MaxValue : utcNow.Add(_offset),
+                };
+
+                _fileInfoCache.AddOrUpdate(virtualPath, expiringFileInfo, (a, b) => expiringFileInfo);
+            }
+
+            return fileInfo;
+        }
+
+        private class ExpiringFileInfo
+        {
+            public IFileInfo FileInfo { get; set; }
+            public DateTime ValidUntil { get; set; }
+        }
+    }
+}

src/Microsoft.AspNet.Mvc.Razor/ViewEngine/VirtualPathViewFactory.cs
@@ -2,36 +2,35 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using Microsoft.AspNet.FileSystems;
+using Microsoft.AspNet.Mvc.Core;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.Framework.DependencyInjection;
-using Microsoft.Framework.Runtime;
 
 namespace Microsoft.AspNet.Mvc.Razor
 {
     public class VirtualPathViewFactory : IVirtualPathViewFactory
     {
-        private readonly PhysicalFileSystem _fileSystem;
         private readonly IRazorCompilationService _compilationService;
         private readonly ITypeActivator _activator;
         private readonly IServiceProvider _serviceProvider;
+        private readonly IFileInfoCache _fileInfoCache;
 
-        public VirtualPathViewFactory(IApplicationEnvironment env,
-                                      IRazorCompilationService compilationService,
+        public VirtualPathViewFactory(IRazorCompilationService compilationService,
                                       ITypeActivator typeActivator,
-                                      IServiceProvider serviceProvider)
+                                      IServiceProvider serviceProvider,
+                                      IFileInfoCache fileInfoCache)
         {
-            // TODO: Continue to inject the IFileSystem but only when we get it from the host
-            _fileSystem = new PhysicalFileSystem(env.ApplicationBasePath);
             _compilationService = compilationService;
             _activator = typeActivator;
             _serviceProvider = serviceProvider;
+            _fileInfoCache = fileInfoCache;
         }
 
         public IView CreateInstance([NotNull] string virtualPath)
         {
-            IFileInfo fileInfo;
-            if (_fileSystem.TryGetFileInfo(virtualPath, out fileInfo))
+            var fileInfo = _fileInfoCache.GetFileInfo(virtualPath);
+
+            if (fileInfo != null)
             {
                 var result = _compilationService.Compile(fileInfo);
                 return (IView)_activator.CreateInstance(_serviceProvider, result.CompiledType);

src/Microsoft.AspNet.Mvc/MvcServices.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using Microsoft.AspNet.Mvc.Core;
 using Microsoft.AspNet.Mvc.Filters;
 using Microsoft.AspNet.Mvc.ModelBinding;
 using Microsoft.AspNet.Mvc.Razor;
@@ -41,9 +42,12 @@ namespace Microsoft.AspNet.Mvc
 
             yield return describe.Singleton<IViewEngineProvider, DefaultViewEngineProvider>();
             yield return describe.Scoped<ICompositeViewEngine, CompositeViewEngine>();
-            yield return describe.Transient<IRazorCompilationService, RazorCompilationService>();
-            yield return describe.Transient<IVirtualPathViewFactory, VirtualPathViewFactory>();
+            yield return describe.Singleton<IRazorCompilationService, RazorCompilationService>();
+
             yield return describe.Singleton<IRazorViewActivator, RazorViewActivator>();
+            // Virtual path view factory needs to stay scoped so views can get get scoped services.
+            yield return describe.Scoped<IVirtualPathViewFactory, VirtualPathViewFactory>();
+            yield return describe.Singleton<IFileInfoCache, ExpiringFileInfoCache>();
 
             yield return describe.Transient<INestedProvider<ActionDescriptorProviderContext>,
                                             ReflectedActionDescriptorProvider>();