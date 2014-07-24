aspnet/Mvc

Modify FindView and FindPartialView to accept ActionContext
Fixes #787

test/Microsoft.AspNet.Mvc.Razor.Test/RazorViewEngineTest.cs
@@ -3,7 +3,9 @@
 
 using System;
 using System.Collections.Generic;
+using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Routing;
 using Moq;
 using Xunit;
 
@@ -39,10 +41,11 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
         {
             // Arrange
             var viewEngine = CreateSearchLocationViewEngineTester();
+            var context = GetActionContext(_controllerTestContext);
 
             // Act & Assert
             Assert.Throws<InvalidOperationException>(() =>
-                viewEngine.FindView(_controllerTestContext, viewName));
+                viewEngine.FindView(context, viewName));
         }
 
         [Theory]
@@ -53,10 +56,11 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
             var viewEngine = CreateSearchLocationViewEngineTester();
             // Append .cshtml so the viewname is no longer invalid
             viewName += ".cshtml";
+            var context = GetActionContext(_controllerTestContext);
 
             // Act & Assert
             // If this throws then our test case fails
-            var result = viewEngine.FindPartialView(_controllerTestContext, viewName);
+            var result = viewEngine.FindPartialView(context, viewName);
 
             Assert.False(result.Success);
         }
@@ -67,10 +71,11 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
         {
             // Arrange
             var viewEngine = CreateSearchLocationViewEngineTester();
+            var context = GetActionContext(_controllerTestContext);
 
             // Act & Assert
             Assert.Throws<InvalidOperationException>(() =>
-                viewEngine.FindPartialView(_controllerTestContext, partialViewName));
+                viewEngine.FindPartialView(context, partialViewName));
         }
 
         [Theory]
@@ -81,10 +86,11 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
             var viewEngine = CreateSearchLocationViewEngineTester();
             // Append .cshtml so the viewname is no longer invalid
             partialViewName += ".cshtml";
+            var context = GetActionContext(_controllerTestContext);
 
             // Act & Assert
             // If this throws then our test case fails
-            var result = viewEngine.FindPartialView(_controllerTestContext, partialViewName);
+            var result = viewEngine.FindPartialView(context, partialViewName);
 
             Assert.False(result.Success);
         }
@@ -95,15 +101,16 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
             // Arrange
             var searchedLocations = new List<string>();
             var viewEngine = CreateSearchLocationViewEngineTester();
+            var context = GetActionContext(_areaTestContext);
 
             // Act
-            var result = viewEngine.FindPartialView(_areaTestContext, "partial");
+            var result = viewEngine.FindPartialView(context, "partial");
 
             // Assert
             Assert.False(result.Success);
-            Assert.Equal(new[] { 
-                "/Areas/foo/Views/bar/partial.cshtml", 
-                "/Areas/foo/Views/Shared/partial.cshtml", 
+            Assert.Equal(new[] {
+                "/Areas/foo/Views/bar/partial.cshtml",
+                "/Areas/foo/Views/Shared/partial.cshtml",
                 "/Views/Shared/partial.cshtml",
             }, result.SearchedLocations);
         }
@@ -113,14 +120,15 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
         {
             // Arrange
             var viewEngine = CreateSearchLocationViewEngineTester();
+            var context = GetActionContext(_controllerTestContext);
 
             // Act
-            var result = viewEngine.FindPartialView(_controllerTestContext, "partialNoArea");
+            var result = viewEngine.FindPartialView(context, "partialNoArea");
 
             // Assert
             Assert.False(result.Success);
-            Assert.Equal(new[] { 
-                "/Views/bar/partialNoArea.cshtml", 
+            Assert.Equal(new[] {
+                "/Views/bar/partialNoArea.cshtml",
                 "/Views/Shared/partialNoArea.cshtml",
             }, result.SearchedLocations);
         }
@@ -130,15 +138,16 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
         {
             // Arrange
             var viewEngine = CreateSearchLocationViewEngineTester();
+            var context = GetActionContext(_areaTestContext);
 
             // Act
-            var result = viewEngine.FindView(_areaTestContext, "full");
+            var result = viewEngine.FindView(context, "full");
 
             // Assert
             Assert.False(result.Success);
-            Assert.Equal(new[] { 
-                "/Areas/foo/Views/bar/full.cshtml", 
-                "/Areas/foo/Views/Shared/full.cshtml", 
+            Assert.Equal(new[] {
+                "/Areas/foo/Views/bar/full.cshtml",
+                "/Areas/foo/Views/Shared/full.cshtml",
                 "/Views/Shared/full.cshtml",
             }, result.SearchedLocations);
         }
@@ -148,14 +157,15 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
         {
             // Arrange
             var viewEngine = CreateSearchLocationViewEngineTester();
+            var context = GetActionContext(_controllerTestContext);
 
             // Act
-            var result = viewEngine.FindView(_controllerTestContext, "fullNoArea");
+            var result = viewEngine.FindView(context, "fullNoArea");
 
             // Assert
             Assert.False(result.Success);
-            Assert.Equal(new[] { 
-                "/Views/bar/fullNoArea.cshtml", 
+            Assert.Equal(new[] {
+                "/Views/bar/fullNoArea.cshtml",
                 "/Views/Shared/fullNoArea.cshtml",
             }, result.SearchedLocations);
         }
@@ -168,6 +178,13 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
             var viewEngine = new RazorViewEngine(virtualPathFactory.Object);
 
             return viewEngine;
+        }
+
+        private static ActionContext GetActionContext(IDictionary<string, object> routeValues)
+        {
+            var httpContext = Mock.Of<HttpContext>();
+            var routeContext = new RouteData { Values = routeValues };
+            return new ActionContext(httpContext, routeContext, new ActionDescriptor());
         }
     }
 }

dotnet/aspnetcore

test/Microsoft.AspNet.Mvc.Razor.Test/RazorViewEngineTest.cs
@@ -3,7 +3,9 @@
 
 using System;
 using System.Collections.Generic;
+using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Routing;
 using Moq;
 using Xunit;
 
@@ -39,10 +41,11 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
         {
             // Arrange
             var viewEngine = CreateSearchLocationViewEngineTester();
+            var context = GetActionContext(_controllerTestContext);
 
             // Act & Assert
             Assert.Throws<InvalidOperationException>(() =>
-                viewEngine.FindView(_controllerTestContext, viewName));
+                viewEngine.FindView(context, viewName));
         }
 
         [Theory]
@@ -53,10 +56,11 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
             var viewEngine = CreateSearchLocationViewEngineTester();
             // Append .cshtml so the viewname is no longer invalid
             viewName += ".cshtml";
+            var context = GetActionContext(_controllerTestContext);
 
             // Act & Assert
             // If this throws then our test case fails
-            var result = viewEngine.FindPartialView(_controllerTestContext, viewName);
+            var result = viewEngine.FindPartialView(context, viewName);
 
             Assert.False(result.Success);
         }
@@ -67,10 +71,11 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
         {
             // Arrange
             var viewEngine = CreateSearchLocationViewEngineTester();
+            var context = GetActionContext(_controllerTestContext);
 
             // Act & Assert
             Assert.Throws<InvalidOperationException>(() =>
-                viewEngine.FindPartialView(_controllerTestContext, partialViewName));
+                viewEngine.FindPartialView(context, partialViewName));
         }
 
         [Theory]
@@ -81,10 +86,11 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
             var viewEngine = CreateSearchLocationViewEngineTester();
             // Append .cshtml so the viewname is no longer invalid
             partialViewName += ".cshtml";
+            var context = GetActionContext(_controllerTestContext);
 
             // Act & Assert
             // If this throws then our test case fails
-            var result = viewEngine.FindPartialView(_controllerTestContext, partialViewName);
+            var result = viewEngine.FindPartialView(context, partialViewName);
 
             Assert.False(result.Success);
         }
@@ -95,15 +101,16 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
             // Arrange
             var searchedLocations = new List<string>();
             var viewEngine = CreateSearchLocationViewEngineTester();
+            var context = GetActionContext(_areaTestContext);
 
             // Act
-            var result = viewEngine.FindPartialView(_areaTestContext, "partial");
+            var result = viewEngine.FindPartialView(context, "partial");
 
             // Assert
             Assert.False(result.Success);
-            Assert.Equal(new[] { 
-                "/Areas/foo/Views/bar/partial.cshtml", 
-                "/Areas/foo/Views/Shared/partial.cshtml", 
+            Assert.Equal(new[] {
+                "/Areas/foo/Views/bar/partial.cshtml",
+                "/Areas/foo/Views/Shared/partial.cshtml",
                 "/Views/Shared/partial.cshtml",
             }, result.SearchedLocations);
         }
@@ -113,14 +120,15 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
         {
             // Arrange
             var viewEngine = CreateSearchLocationViewEngineTester();
+            var context = GetActionContext(_controllerTestContext);
 
             // Act
-            var result = viewEngine.FindPartialView(_controllerTestContext, "partialNoArea");
+            var result = viewEngine.FindPartialView(context, "partialNoArea");
 
             // Assert
             Assert.False(result.Success);
-            Assert.Equal(new[] { 
-                "/Views/bar/partialNoArea.cshtml", 
+            Assert.Equal(new[] {
+                "/Views/bar/partialNoArea.cshtml",
                 "/Views/Shared/partialNoArea.cshtml",
             }, result.SearchedLocations);
         }
@@ -130,15 +138,16 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
         {
             // Arrange
             var viewEngine = CreateSearchLocationViewEngineTester();
+            var context = GetActionContext(_areaTestContext);
 
             // Act
-            var result = viewEngine.FindView(_areaTestContext, "full");
+            var result = viewEngine.FindView(context, "full");
 
             // Assert
             Assert.False(result.Success);
-            Assert.Equal(new[] { 
-                "/Areas/foo/Views/bar/full.cshtml", 
-                "/Areas/foo/Views/Shared/full.cshtml", 
+            Assert.Equal(new[] {
+                "/Areas/foo/Views/bar/full.cshtml",
+                "/Areas/foo/Views/Shared/full.cshtml",
                 "/Views/Shared/full.cshtml",
             }, result.SearchedLocations);
         }
@@ -148,14 +157,15 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
         {
             // Arrange
             var viewEngine = CreateSearchLocationViewEngineTester();
+            var context = GetActionContext(_controllerTestContext);
 
             // Act
-            var result = viewEngine.FindView(_controllerTestContext, "fullNoArea");
+            var result = viewEngine.FindView(context, "fullNoArea");
 
             // Assert
             Assert.False(result.Success);
-            Assert.Equal(new[] { 
-                "/Views/bar/fullNoArea.cshtml", 
+            Assert.Equal(new[] {
+                "/Views/bar/fullNoArea.cshtml",
                 "/Views/Shared/fullNoArea.cshtml",
             }, result.SearchedLocations);
         }
@@ -170,9 +180,10 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
             var viewEngine = new RazorViewEngine(pageFactory.Object,
                                                  Mock.Of<IRazorPageActivator>(),
                                                  Mock.Of<IViewStartProvider>());
+            var context = GetActionContext(_controllerTestContext);
 
             // Act
-            var result = viewEngine.FindView(_controllerTestContext, "test-view");
+            var result = viewEngine.FindView(context, "test-view");
 
             // Assert
             Assert.True(result.Success);
@@ -191,6 +202,13 @@ namespace Microsoft.AspNet.Mvc.Razor.Test
                                                  Mock.Of<IViewStartProvider>());
 
             return viewEngine;
+        }
+
+        private static ActionContext GetActionContext(IDictionary<string, object> routeValues)
+        {
+            var httpContext = Mock.Of<HttpContext>();
+            var routeData = new RouteData { Values = routeValues };
+            return new ActionContext(httpContext, routeData, new ActionDescriptor());
         }
     }
 }