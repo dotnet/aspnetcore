aspnet/Mvc

Commonizing code in option provider
* Adding ValueProviderFactoryProvider and CompositeValueProviderFactory to
  maintain parity in pattern with other option types.

Fixes #818

src/Microsoft.AspNet.Mvc.Core/Descriptors/DefaultModelBinderProvider.cs
@@ -1,0 +1,39 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.Mvc.ModelBinding;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.OptionsModel;
+
+namespace Microsoft.AspNet.Mvc.Descriptors
+{
+    /// <inheritdoc />
+    public class DefaultModelBindersProvider : OptionProviderBase<IModelBinder>, IModelBinderProvider
+    {
+        /// <summary>
+        /// Initializes a new instance of the DefaultModelBindersProvider class.
+        /// </summary>
+        /// <param name="options">An accessor to the <see cref="MvcOptions"/> configured for this application.</param>
+        /// <param name="typeActivator">An <see cref="ITypeActivator"/> instance used to instantiate types.</param>
+        /// <param name="serviceProvider">A <see cref="IServiceProvider"/> instance that retrieves services from the 
+        /// service collection.</param>
+        public DefaultModelBindersProvider(
+                IOptionsAccessor<MvcOptions> optionsAccessor,
+                ITypeActivator typeActivator,
+                IServiceProvider serviceProvider)
+            : base(optionsAccessor.Options.ModelBinders, typeActivator, serviceProvider)
+        {
+        }
+
+        /// <inheritdoc />
+        public IReadOnlyList<IModelBinder> ModelBinders
+        {
+            get
+            {
+                return Options;
+            }
+        }
+    }
+}

src/Microsoft.AspNet.Mvc.Core/Descriptors/ModelBinderDescriptorExtensions.cs
@@ -3,8 +3,10 @@
 
 using System;
 using System.Collections.Generic;
+using Microsoft.AspNet.Mvc.Descriptors;
+using Microsoft.AspNet.Mvc.ModelBinding;
 
-namespace Microsoft.AspNet.Mvc.ModelBinding
+namespace Microsoft.AspNet.Mvc
 {
     /// <summary>
     /// Extension methods for adding model binders to a collection.

src/Microsoft.AspNet.Mvc.Core/Descriptors/ValueProviderFactoryDescriptor.cs
@@ -1,0 +1,33 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNet.Mvc.ModelBinding;
+
+namespace Microsoft.AspNet.Mvc.Descriptors
+{
+    /// <summary>
+    /// Encapsulates information that describes an <see cref="IValueProviderFactory"/>.
+    /// </summary>
+    public class ValueProviderFactoryDescriptor : OptionDescriptor<IValueProviderFactory>
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ValueProviderFactoryDescriptor"/>.
+        /// </summary>
+        /// <param name="type">The <see cref="IValueProviderFactory/> type that the descriptor represents.</param>
+        public ValueProviderFactoryDescriptor([NotNull] Type type)
+            : base(type)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="ValueProviderFactoryDescriptor"/> using the specified type.
+        /// </summary>
+        /// <param name="valueProviderFactory">An instance of <see cref="IValueProviderFactory"/> 
+        /// that the descriptor represents.</param>
+        public ValueProviderFactoryDescriptor([NotNull] IValueProviderFactory valueProviderFactory)
+            : base(valueProviderFactory)
+        {
+        }
+    }
+}

src/Microsoft.AspNet.Mvc/MvcServices.cs
@@ -3,6 +3,7 @@
 
 using System.Collections.Generic;
 using Microsoft.AspNet.Mvc.Core;
+using Microsoft.AspNet.Mvc.Descriptors;
 using Microsoft.AspNet.Mvc.Filters;
 using Microsoft.AspNet.Mvc.ModelBinding;
 using Microsoft.AspNet.Mvc.Razor;
@@ -63,8 +64,10 @@ namespace Microsoft.AspNet.Mvc
             yield return describe.Transient<IInputFormatter, JsonInputFormatter>();
             yield return describe.Transient<IInputFormatterProvider, TempInputFormatterProvider>();
 
-            yield return describe.Transient<IModelBindersProvider, DefaultModelBindersProvider>();
-            yield return describe.Transient<ICompositeModelBinder, CompositeModelBinder>();
+            yield return describe.Transient<IModelBinderProvider, DefaultModelBindersProvider>();
+            yield return describe.Scoped<ICompositeModelBinder, CompositeModelBinder>();
+            yield return describe.Transient<IValueProviderFactoryProvider, DefaultValueProviderFactoryProvider>();
+            yield return describe.Scoped<ICompositeValueProviderFactory, CompositeValueProviderFactory>();
 
             yield return describe.Transient<INestedProvider<FilterProviderContext>, DefaultFilterProvider>();
 

test/Microsoft.AspNet.Mvc.Core.Test/Descriptors/DefaultValueProviderFactoryProviderTest.cs
@@ -1,62 +1,58 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-#if NET45
 using System;
-using System.Collections.Generic;
+using Microsoft.AspNet.Mvc.ModelBinding;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.OptionsModel;
 using Moq;
 using Xunit;
 
-namespace Microsoft.AspNet.Mvc.Rendering
+namespace Microsoft.AspNet.Mvc.Descriptors
 {
-    public class DefaultViewEngineProviderTest
+    public class DefaultValueProviderFactoryProviderTest
     {
         [Fact]
         public void ViewEngine_ReturnsInstantiatedListOfViewEngines()
         {
             // Arrange
             var service = Mock.Of<ITestService>();
-            var viewEngine = Mock.Of<IViewEngine>();
-            var type = typeof(TestViewEngine);
+            var valueProviderFactory = Mock.Of<IValueProviderFactory>();
+            var type = typeof(TestValueProviderFactory);
             var typeActivator = new TypeActivator();
             var serviceProvider = new Mock<IServiceProvider>();
             serviceProvider.Setup(p => p.GetService(typeof(ITestService)))
                            .Returns(service);
             var options = new MvcOptions();
-            options.ViewEngines.Add(viewEngine);
-            options.ViewEngines.Add(type);
+            options.ValueProviderFactories.Add(valueProviderFactory);
+            options.ValueProviderFactories.Add(type);
             var accessor = new Mock<IOptionsAccessor<MvcOptions>>();
             accessor.SetupGet(a => a.Options)
                     .Returns(options);
-            var provider = new DefaultViewEngineProvider(typeActivator, serviceProvider.Object, accessor.Object);
+            var provider = new DefaultValueProviderFactoryProvider(accessor.Object,
+                                                                   typeActivator,
+                                                                   serviceProvider.Object);
 
             // Act
-            var result = provider.ViewEngines;
+            var result = provider.ValueProviderFactories;
 
             // Assert
             Assert.Equal(2, result.Count);
-            Assert.Same(viewEngine, result[0]);
-            var testViewEngine = Assert.IsType<TestViewEngine>(result[1]);
-            Assert.Same(service, testViewEngine.Service);
+            Assert.Same(valueProviderFactory, result[0]);
+            var testValueProviderFactory = Assert.IsType<TestValueProviderFactory>(result[1]);
+            Assert.Same(service, testValueProviderFactory.Service);
         }
 
-        private class TestViewEngine : IViewEngine
+        private class TestValueProviderFactory : IValueProviderFactory
         {
-            public TestViewEngine(ITestService service)
+            public TestValueProviderFactory(ITestService service)
             {
                 Service = service;
             }
 
             public ITestService Service { get; private set; }
 
-            public ViewEngineResult FindPartialView(ActionContext context, string partialViewName)
-            {
-                throw new NotImplementedException();
-            }
-
-            public ViewEngineResult FindView(ActionContext context, string viewName)
+            public IValueProvider GetValueProvider(ValueProviderFactoryContext context)
             {
                 throw new NotImplementedException();
             }
@@ -67,4 +63,3 @@ namespace Microsoft.AspNet.Mvc.Rendering
         }
     }
 }
-#endif

dotnet/aspnetcore

src/Microsoft.AspNet.Mvc.Core/OptionDescriptors/DefaultModelBinderProvider.cs
@@ -1,0 +1,39 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.Mvc.ModelBinding;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.OptionsModel;
+
+namespace Microsoft.AspNet.Mvc.OptionDescriptors
+{
+    /// <inheritdoc />
+    public class DefaultModelBindersProvider : OptionDescriptorBasedProvider<IModelBinder>, IModelBinderProvider
+    {
+        /// <summary>
+        /// Initializes a new instance of the DefaultModelBindersProvider class.
+        /// </summary>
+        /// <param name="options">An accessor to the <see cref="MvcOptions"/> configured for this application.</param>
+        /// <param name="typeActivator">An <see cref="ITypeActivator"/> instance used to instantiate types.</param>
+        /// <param name="serviceProvider">A <see cref="IServiceProvider"/> instance that retrieves services from the 
+        /// service collection.</param>
+        public DefaultModelBindersProvider(
+                IOptionsAccessor<MvcOptions> optionsAccessor,
+                ITypeActivator typeActivator,
+                IServiceProvider serviceProvider)
+            : base(optionsAccessor.Options.ModelBinders, typeActivator, serviceProvider)
+        {
+        }
+
+        /// <inheritdoc />
+        public IReadOnlyList<IModelBinder> ModelBinders
+        {
+            get
+            {
+                return Options;
+            }
+        }
+    }
+}

src/Microsoft.AspNet.Mvc.Core/OptionDescriptors/ModelBinderDescriptorExtensions.cs
@@ -3,8 +3,10 @@
 
 using System;
 using System.Collections.Generic;
+using Microsoft.AspNet.Mvc.ModelBinding;
+using Microsoft.AspNet.Mvc.OptionDescriptors;
 
-namespace Microsoft.AspNet.Mvc.ModelBinding
+namespace Microsoft.AspNet.Mvc
 {
     /// <summary>
     /// Extension methods for adding model binders to a collection.

src/Microsoft.AspNet.Mvc.Core/OptionDescriptors/ValueProviderFactoryDescriptor.cs
@@ -1,0 +1,33 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNet.Mvc.ModelBinding;
+
+namespace Microsoft.AspNet.Mvc.OptionDescriptors
+{
+    /// <summary>
+    /// Encapsulates information that describes an <see cref="IValueProviderFactory"/>.
+    /// </summary>
+    public class ValueProviderFactoryDescriptor : OptionDescriptor<IValueProviderFactory>
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ValueProviderFactoryDescriptor"/>.
+        /// </summary>
+        /// <param name="type">The <see cref="IValueProviderFactory/> type that the descriptor represents.</param>
+        public ValueProviderFactoryDescriptor([NotNull] Type type)
+            : base(type)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="ValueProviderFactoryDescriptor"/> using the specified type.
+        /// </summary>
+        /// <param name="valueProviderFactory">An instance of <see cref="IValueProviderFactory"/> 
+        /// that the descriptor represents.</param>
+        public ValueProviderFactoryDescriptor([NotNull] IValueProviderFactory valueProviderFactory)
+            : base(valueProviderFactory)
+        {
+        }
+    }
+}

src/Microsoft.AspNet.Mvc/MvcServices.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using Microsoft.AspNet.Mvc.Core;
 using Microsoft.AspNet.Mvc.Filters;
 using Microsoft.AspNet.Mvc.ModelBinding;
+using Microsoft.AspNet.Mvc.OptionDescriptors;
 using Microsoft.AspNet.Mvc.Razor;
 using Microsoft.AspNet.Mvc.Razor.Compilation;
 using Microsoft.AspNet.Mvc.Rendering;
@@ -63,8 +64,10 @@ namespace Microsoft.AspNet.Mvc
             yield return describe.Transient<IInputFormatter, JsonInputFormatter>();
             yield return describe.Transient<IInputFormatterProvider, TempInputFormatterProvider>();
 
-            yield return describe.Transient<IModelBindersProvider, DefaultModelBindersProvider>();
-            yield return describe.Transient<ICompositeModelBinder, CompositeModelBinder>();
+            yield return describe.Transient<IModelBinderProvider, DefaultModelBindersProvider>();
+            yield return describe.Scoped<ICompositeModelBinder, CompositeModelBinder>();
+            yield return describe.Transient<IValueProviderFactoryProvider, DefaultValueProviderFactoryProvider>();
+            yield return describe.Scoped<ICompositeValueProviderFactory, CompositeValueProviderFactory>();
 
             yield return describe.Transient<INestedProvider<FilterProviderContext>, DefaultFilterProvider>();
 

test/Microsoft.AspNet.Mvc.Core.Test/OptionDescriptors/DefaultValueProviderFactoryProviderTest.cs
@@ -1,62 +1,58 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-#if NET45
 using System;
-using System.Collections.Generic;
+using Microsoft.AspNet.Mvc.ModelBinding;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.OptionsModel;
 using Moq;
 using Xunit;
 
-namespace Microsoft.AspNet.Mvc.Rendering
+namespace Microsoft.AspNet.Mvc.OptionDescriptors
 {
-    public class DefaultViewEngineProviderTest
+    public class DefaultValueProviderFactoryProviderTest
     {
         [Fact]
         public void ViewEngine_ReturnsInstantiatedListOfViewEngines()
         {
             // Arrange
             var service = Mock.Of<ITestService>();
-            var viewEngine = Mock.Of<IViewEngine>();
-            var type = typeof(TestViewEngine);
+            var valueProviderFactory = Mock.Of<IValueProviderFactory>();
+            var type = typeof(TestValueProviderFactory);
             var typeActivator = new TypeActivator();
             var serviceProvider = new Mock<IServiceProvider>();
             serviceProvider.Setup(p => p.GetService(typeof(ITestService)))
                            .Returns(service);
             var options = new MvcOptions();
-            options.ViewEngines.Add(viewEngine);
-            options.ViewEngines.Add(type);
+            options.ValueProviderFactories.Add(valueProviderFactory);
+            options.ValueProviderFactories.Add(type);
             var accessor = new Mock<IOptionsAccessor<MvcOptions>>();
             accessor.SetupGet(a => a.Options)
                     .Returns(options);
-            var provider = new DefaultViewEngineProvider(typeActivator, serviceProvider.Object, accessor.Object);
+            var provider = new DefaultValueProviderFactoryProvider(accessor.Object,
+                                                                   typeActivator,
+                                                                   serviceProvider.Object);
 
             // Act
-            var result = provider.ViewEngines;
+            var result = provider.ValueProviderFactories;
 
             // Assert
             Assert.Equal(2, result.Count);
-            Assert.Same(viewEngine, result[0]);
-            var testViewEngine = Assert.IsType<TestViewEngine>(result[1]);
-            Assert.Same(service, testViewEngine.Service);
+            Assert.Same(valueProviderFactory, result[0]);
+            var testValueProviderFactory = Assert.IsType<TestValueProviderFactory>(result[1]);
+            Assert.Same(service, testValueProviderFactory.Service);
         }
 
-        private class TestViewEngine : IViewEngine
+        private class TestValueProviderFactory : IValueProviderFactory
         {
-            public TestViewEngine(ITestService service)
+            public TestValueProviderFactory(ITestService service)
             {
                 Service = service;
             }
 
             public ITestService Service { get; private set; }
 
-            public ViewEngineResult FindPartialView(ActionContext context, string partialViewName)
-            {
-                throw new NotImplementedException();
-            }
-
-            public ViewEngineResult FindView(ActionContext context, string viewName)
+            public IValueProvider GetValueProvider(ValueProviderFactoryContext context)
             {
                 throw new NotImplementedException();
             }
@@ -67,4 +63,3 @@ namespace Microsoft.AspNet.Mvc.Rendering
         }
     }
 }
-#endif