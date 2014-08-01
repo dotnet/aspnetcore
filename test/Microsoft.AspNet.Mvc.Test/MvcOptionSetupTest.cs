aspnet/Mvc

Adding TextPlainFormatter to always handle returning strings as text\plain format.

test/Microsoft.AspNet.Mvc.Test/MvcOptionSetupTest.cs
@@ -72,8 +72,9 @@ namespace Microsoft.AspNet.Mvc
             setup.Setup(mvcOptions);
 
             // Assert
-            Assert.Equal(1, mvcOptions.OutputFormatters.Count);
-            Assert.IsType<JsonOutputFormatter>(mvcOptions.OutputFormatters[0].OutputFormatter);
+            Assert.Equal(2, mvcOptions.OutputFormatters.Count);
+            Assert.IsType<TextPlainFormatter>(mvcOptions.OutputFormatters[0].OutputFormatter);
+            Assert.IsType<JsonOutputFormatter>(mvcOptions.OutputFormatters[1].OutputFormatter);
         }
     }
 }

dotnet/aspnetcore

Conflicts:
	src/Microsoft.AspNet.Mvc.Common/Encodings.cs
	src/Microsoft.AspNet.Mvc.Core/Formatters/JsonOutputFormatter.cs
	src/Microsoft.AspNet.Mvc/MvcOptionsSetup.cs
	test/Microsoft.AspNet.Mvc.Core.Test/ActionResults/ObjectContentResultTests.cs
	test/Microsoft.AspNet.Mvc.Test/MvcOptionSetupTest.cs

test/Microsoft.AspNet.Mvc.Test/MvcOptionSetupTest.cs
@@ -72,8 +72,9 @@ namespace Microsoft.AspNet.Mvc
             setup.Setup(mvcOptions);
 
             // Assert
-            Assert.Equal(1, mvcOptions.OutputFormatters.Count);
-            Assert.IsType<JsonOutputFormatter>(mvcOptions.OutputFormatters[0].Instance);
+            Assert.Equal(2, mvcOptions.OutputFormatters.Count);
+            Assert.IsType<TextPlainFormatter>(mvcOptions.OutputFormatters[0].Instance);
+            Assert.IsType<JsonOutputFormatter>(mvcOptions.OutputFormatters[1].Instance);
         }
     }
 }