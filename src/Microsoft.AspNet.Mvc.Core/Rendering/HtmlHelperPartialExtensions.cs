7ab93d34e4ccc153101358e54ba7efc45f9d9394
dougbu <dougbu@microsoft.com>
2014-08-05 11:05:25 -0700

    Remove incorrect references to `HtmlHelper` class - extension methods extend `IHtmlHelper`

--- src/Microsoft.AspNet.Mvc.Core/Rendering/HtmlHelperPartialExtensions.cs
+++ src/Microsoft.AspNet.Mvc.Core/Rendering/HtmlHelperPartialExtensions.cs
@@ -10,7 +10,7 @@ namespace Microsoft.AspNet.Mvc.Rendering
         /// <summary>
         /// Renders the partial view with the parent's view data and model to a string.
         /// </summary>
-        /// <param name="htmlHelper">The <see cref="HtmlHelper"/> instance that this method extends.</param>
+        /// <param name="htmlHelper">The <see cref="IHtmlHelper"/> instance that this method extends.</param>
         /// <param name="partialViewName">The name of the partial view to render.</param>
         /// <returns>
         /// A <see cref="Task{T}"/> that represents when rendering to the <see cref="HtmlString"/> has completed.
@@ -25,7 +25,7 @@ namespace Microsoft.AspNet.Mvc.Rendering
         /// <summary>
         /// Renders the partial view with the given view data and, implicitly, the given view data's model to a string.
         /// </summary>
-        /// <param name="htmlHelper">The <see cref="HtmlHelper"/> instance that this method extends.</param>
+        /// <param name="htmlHelper">The <see cref="IHtmlHelper"/> instance that this method extends.</param>
         /// <param name="partialViewName">The name of the partial view to render.</param>
         /// <param name="viewData">
         /// The <see cref="ViewDataDictionary"/> that is provided to the partial view that will be rendered.
@@ -44,7 +44,7 @@ namespace Microsoft.AspNet.Mvc.Rendering
         /// <summary>
         /// Renders the partial view with an empty view data and the given model to a string.
         /// </summary>
-        /// <param name="htmlHelper">The <see cref="HtmlHelper"/> instance that this method extends.</param>
+        /// <param name="htmlHelper">The <see cref="IHtmlHelper"/> instance that this method extends.</param>
         /// <param name="partialViewName">The name of the partial view to render.</param>
         /// <param name="model">The model to provide to the partial view that will be rendered.</param>
         /// <returns>
@@ -61,7 +61,7 @@ namespace Microsoft.AspNet.Mvc.Rendering
         /// <summary>
         /// Renders the partial view with the parent's view data and model.
         /// </summary>
-        /// <param name="htmlHelper">The <see cref="HtmlHelper"/> instance that this method extends.</param>
+        /// <param name="htmlHelper">The <see cref="IHtmlHelper"/> instance that this method extends.</param>
         /// <param name="partialViewName">The name of the partial view to render.</param>
         /// <returns>A <see cref="Task"/> that represents when rendering has completed.</returns>
         public static Task RenderPartialAsync(
@@ -75,7 +75,7 @@ namespace Microsoft.AspNet.Mvc.Rendering
         /// <summary>
         /// Renders the partial view with the given view data and, implicitly, the given view data's model.
         /// </summary>
-        /// <param name="htmlHelper">The <see cref="HtmlHelper"/> instance that this method extends.</param>
+        /// <param name="htmlHelper">The <see cref="IHtmlHelper"/> instance that this method extends.</param>
         /// <param name="partialViewName">The name of the partial view to render.</param>
         /// <param name="viewData">
         /// The <see cref="ViewDataDictionary"/> that is provided to the partial view that will be rendered.
@@ -92,7 +92,7 @@ namespace Microsoft.AspNet.Mvc.Rendering
         /// <summary>
         /// Renders the partial view with an empty view data and the given model.
         /// </summary>
-        /// <param name="htmlHelper">The <see cref="HtmlHelper"/> instance that this method extends.</param>
+        /// <param name="htmlHelper">The <see cref="IHtmlHelper"/> instance that this method extends.</param>
         /// <param name="partialViewName">The name of the partial view to render.</param>
         /// <param name="model">The model to provide to the partial view that will be rendered.</param>
         /// <returns>A <see cref="Task"/> that represents when rendering has completed.</returns>
