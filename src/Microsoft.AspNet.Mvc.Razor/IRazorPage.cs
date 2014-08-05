aspnet/Mvc

Introducing RazorTextWriter
RazorTextWriter represents the result of rendering a page as a sequence of
strings rather than a concatenated string. This avoids building up large
strings in memory.

src/Microsoft.AspNet.Mvc.Razor/IRazorPage.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Microsoft.AspNet.Mvc.Razor
@@ -16,7 +18,11 @@ namespace Microsoft.AspNet.Mvc.Razor
         /// </summary>
         ViewContext ViewContext { get; set; }
 
-        string BodyContent { get; set; }
+        /// <summary>
+        /// Gets or sets the action invoked to render the body.
+        /// </summary>
+        // TODO: https://github.com/aspnet/Mvc/issues/845 tracks making this async
+        Action<TextWriter> BodyAction { get; set; }
 
         /// <summary>
         /// Gets or sets the path of a layout page.

test/Microsoft.AspNet.Mvc.Razor.Test/RazorPageTest.cs
@@ -122,7 +122,7 @@ namespace Microsoft.AspNet.Mvc.Razor
             {
                 { "baz", new HelperResult(writer => { }) }
             };
-            page.BodyContent = "body-content";
+            page.BodyAction = CreateBodyAction("body-content");
 
             // Act
             await page.ExecuteAsync();
@@ -146,7 +146,7 @@ namespace Microsoft.AspNet.Mvc.Razor
             {
                 { "baz", new HelperResult(writer => { }) }
             };
-            page.BodyContent = "body-content";
+            page.BodyAction = CreateBodyAction("body-content");
 
             // Act
             await page.ExecuteAsync();
@@ -211,7 +211,7 @@ namespace Microsoft.AspNet.Mvc.Razor
             var page = CreatePage(v =>
             {
             });
-            page.BodyContent = "some content";
+            page.BodyAction = CreateBodyAction("some content");
 
             // Act
             await page.ExecuteAsync();
@@ -240,7 +240,7 @@ Layout end
                 v.WriteLiteral("Layout end" + Environment.NewLine);
 
             });
-            page.BodyContent = "body content" + Environment.NewLine;
+            page.BodyAction = CreateBodyAction("body content" + Environment.NewLine);
             page.PreviousSectionWriters = new Dictionary<string, HelperResult>
             {
                 {
@@ -289,9 +289,14 @@ Layout end
                 new StringWriter());
         }
 
+        private static Action<TextWriter> CreateBodyAction(string value)
+        {
+            return writer => writer.Write(value);
+        }
+
         public abstract class TestableRazorPage : RazorPage
         {
-            public HtmlString RenderBodyPublic()
+            public HelperResult RenderBodyPublic()
             {
                 return base.RenderBody();
             }

dotnet/aspnetcore

src/Microsoft.AspNet.Mvc.Razor/IRazorPage.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Microsoft.AspNet.Mvc.Razor
@@ -17,11 +19,15 @@ namespace Microsoft.AspNet.Mvc.Razor
         ViewContext ViewContext { get; set; }
 
         /// <summary>
+        /// Gets or sets the action invoked to render the body.
+        /// </summary>
+        // TODO: https://github.com/aspnet/Mvc/issues/845 tracks making this async
+        Action<TextWriter> RenderBodyDelegate { get; set; }
+
+        /// <summary>
         /// Gets the path to the page.
         /// </summary>
         string Path { get; set; }
-
-        string BodyContent { get; set; }
 
         /// <summary>
         /// Gets or sets the path of a layout page.

test/Microsoft.AspNet.Mvc.Razor.Test/RazorPageTest.cs
@@ -122,7 +122,7 @@ namespace Microsoft.AspNet.Mvc.Razor
             {
                 { "baz", new HelperResult(writer => { }) }
             };
-            page.BodyContent = "body-content";
+            page.RenderBodyDelegate = CreateBodyAction("body-content");
 
             // Act
             await page.ExecuteAsync();
@@ -146,7 +146,7 @@ namespace Microsoft.AspNet.Mvc.Razor
             {
                 { "baz", new HelperResult(writer => { }) }
             };
-            page.BodyContent = "body-content";
+            page.RenderBodyDelegate = CreateBodyAction("body-content");
 
             // Act
             await page.ExecuteAsync();
@@ -211,7 +211,7 @@ namespace Microsoft.AspNet.Mvc.Razor
             var page = CreatePage(v =>
             {
             });
-            page.BodyContent = "some content";
+            page.RenderBodyDelegate = CreateBodyAction("some content");
 
             // Act
             await page.ExecuteAsync();
@@ -240,7 +240,7 @@ Layout end
                 v.WriteLiteral("Layout end" + Environment.NewLine);
 
             });
-            page.BodyContent = "body content" + Environment.NewLine;
+            page.RenderBodyDelegate = CreateBodyAction("body content" + Environment.NewLine);
             page.PreviousSectionWriters = new Dictionary<string, HelperResult>
             {
                 {
@@ -289,9 +289,14 @@ Layout end
                 new StringWriter());
         }
 
+        private static Action<TextWriter> CreateBodyAction(string value)
+        {
+            return writer => writer.Write(value);
+        }
+
         public abstract class TestableRazorPage : RazorPage
         {
-            public HtmlString RenderBodyPublic()
+            public HelperResult RenderBodyPublic()
             {
                 return base.RenderBody();
             }