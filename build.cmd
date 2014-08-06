c3daa5c8c0bdcebabd088476414bc0d8de5717ce
Pinpoint <kevinchalet@gmail.com>
2014-08-06 18:09:26 +0200

    Handle paths containing spaces in build.cmd
    #99

--- build.cmd
+++ build.cmd
@@ -2,11 +2,11 @@
 cd %~dp0
 
 SETLOCAL
-SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
+SET CACHED_NUGET="%LocalAppData%\NuGet\NuGet.exe"
 
 IF EXIST %CACHED_NUGET% goto copynuget
 echo Downloading latest version of NuGet.exe...
-IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
+IF NOT EXIST "%LocalAppData%\NuGet" md "%LocalAppData%\NuGet"
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
 
 :copynuget
