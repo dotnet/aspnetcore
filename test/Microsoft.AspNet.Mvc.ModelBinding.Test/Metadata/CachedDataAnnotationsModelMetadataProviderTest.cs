0595e9ebc9e4ceb66093c550ae1e45f01fdb566a
dougbu <dougbu@microsoft.com>
2014-08-16 16:14:13 -0700

    Move two recently-added `[HiddenInput]` tests into `CachedDataAnnotationsModelMetadataProviderTest`

--- test/Microsoft.AspNet.Mvc.ModelBinding.Test/Metadata/CachedDataAnnotationsModelMetadataProviderTest.cs
+++ test/Microsoft.AspNet.Mvc.ModelBinding.Test/Metadata/CachedDataAnnotationsModelMetadataProviderTest.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using Xunit;
 
 namespace Microsoft.AspNet.Mvc.ModelBinding
@@ -34,6 +35,36 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
             Assert.False(provider.GetMetadataForProperty(null, type, "ScaffoldColumnFalse").ShowForEdit);
         }
 
+        [Fact]
+        public void HiddenInputWorksOnProperty()
+        {
+            // Arrange
+            var provider = new DataAnnotationsModelMetadataProvider();
+            var metadata = provider.GetMetadataForType(modelAccessor: null, modelType: typeof(ClassWithHiddenProperties));
+            var property = metadata.Properties.First(m => string.Equals("DirectlyHidden", m.PropertyName));
+
+            // Act
+            var result = property.HideSurroundingHtml;
+
+            // Assert
+            Assert.True(result);
+        }
+
+        // TODO #1000; enable test once we detect attributes on the property's type
+        public void HiddenInputWorksOnPropertyType()
+        {
+            // Arrange
+            var provider = new DataAnnotationsModelMetadataProvider();
+            var metadata = provider.GetMetadataForType(modelAccessor: null, modelType: typeof(ClassWithHiddenProperties));
+            var property = metadata.Properties.First(m => string.Equals("OfHiddenType", m.PropertyName));
+
+            // Act
+            var result = property.HideSurroundingHtml;
+
+            // Assert
+            Assert.True(result);
+        }
+
         private class ScaffoldColumnModel
         {
             public int NoAttribute { get; set; }
@@ -43,6 +74,20 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
 
             [ScaffoldColumn(scaffold: false)]
             public int ScaffoldColumnFalse { get; set; }
+        }
+
+        [HiddenInput(DisplayValue = false)]
+        private class HiddenClass
+        {
+            public string Property { get; set; }
+        }
+
+        private class ClassWithHiddenProperties
+        {
+            [HiddenInput(DisplayValue = false)]
+            public string DirectlyHidden { get; set; }
+
+            public HiddenClass OfHiddenType { get; set; }
         }
     }
 }
--- test/Microsoft.AspNet.Mvc.ModelBinding.Test/Metadata/CachedDataAnnotationsModelMetadataTest.cs
+++ test/Microsoft.AspNet.Mvc.ModelBinding.Test/Metadata/CachedDataAnnotationsModelMetadataTest.cs
@@ -164,53 +164,9 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
             Assert.Equal(expectedResult, result);
         }
 
-        [Fact]
-        public void HiddenInputWorksOnProperty()
-        {
-            // Arrange
-            var provider = new DataAnnotationsModelMetadataProvider();
-            var metadata = provider.GetMetadataForType(modelAccessor: null, modelType: typeof(ClassWithHiddenProperties));
-            var property = metadata.Properties.First(m => string.Equals("DirectlyHidden", m.PropertyName));
-
-            // Act
-            var result = property.HideSurroundingHtml;
-
-            // Assert
-            Assert.True(result);
-        }
-
-        // TODO #1000; enable test once we detect attributes on the property's type
-        public void HiddenInputWorksOnPropertyType()
-        {
-            // Arrange
-            var provider = new DataAnnotationsModelMetadataProvider();
-            var metadata = provider.GetMetadataForType(modelAccessor: null, modelType: typeof(ClassWithHiddenProperties));
-            var property = metadata.Properties.First(m => string.Equals("OfHiddenType", m.PropertyName));
-
-            // Act
-            var result = property.HideSurroundingHtml;
-
-            // Assert
-            Assert.True(result);
-        }
-
         private class ClassWithDisplayableColumn
         {
             public string Property { get; set; }
-        }
-
-        [HiddenInput(DisplayValue = false)]
-        private class HiddenClass
-        {
-            public string Property { get; set; }
-        }
-
-        private class ClassWithHiddenProperties
-        {
-            [HiddenInput(DisplayValue = false)]
-            public string DirectlyHidden { get; set; }
-
-            public HiddenClass OfHiddenType { get; set; }
         }
     }
 }
