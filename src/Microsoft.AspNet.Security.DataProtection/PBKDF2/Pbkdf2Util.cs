4588b1c898ae3fc9892a4dbaf85b5dd533a430c6
Levi B <levib@yahoo.com>
2014-08-20 22:47:47 -0700

    Add preliminary PBKDF2 routines to unblock identity work. Win7 and Win8 optimizations will be committed as part of the larger DataProtection overhaul.

---
+++ src/Microsoft.AspNet.Security.DataProtection/PBKDF2/Pbkdf2Util.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Text;
+
+namespace Microsoft.AspNet.Security.DataProtection.Cng.PBKDF2
+{
+    /// <summary>
+    /// Internal base class used for abstracting away the PBKDF2 implementation since the implementation is OS-specific.
+    /// </summary>
+    internal static class Pbkdf2Util
+    {
+        public static readonly IPbkdf2Provider Pbkdf2Provider = GetPbkdf2Provider();
+        public static readonly UTF8Encoding SecureUtf8Encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: false);
+
+        private static IPbkdf2Provider GetPbkdf2Provider()
+        {
+            // In priority order, our three implementations are Win8, Win7, and "other".
+
+            // TODO: Provide Win7 & Win8 implementations when the new DataProtection stack is fully copied over.
+            return new ManagedPbkdf2Provider();
+        }
+    }
+}
