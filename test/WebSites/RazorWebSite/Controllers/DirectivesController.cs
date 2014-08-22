Shazwazza/Mvc

Adding support for inheriting chunks from _ViewStarts as part of host parsing.
Fixes #881

test/WebSites/RazorWebSite/Controllers/DirectivesController.cs
@@ -1,0 +1,21 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc;
+
+namespace RazorWebSite
+{
+    public class DirectivesController : Controller
+    {
+        public ViewResult ViewInheritsInjectAndUsingsFromViewStarts()
+        {
+            return View(new Person { Name = "Person1" });
+        }
+
+        public ViewResult ViewInheritsModelFromViewStarts()
+        {
+            return View("/views/directives/scoped/ViewInheritsModelFromViewStarts.cshtml", 
+                        new Person { Name = "Person2" });
+        }
+    }
+}

dotnet/aspnetcore

test/WebSites/RazorWebSite/Controllers/DirectivesController.cs
@@ -1,0 +1,22 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Diagnostics;
+using Microsoft.AspNet.Mvc;
+
+namespace RazorWebSite
+{
+    public class DirectivesController : Controller
+    {
+        public ViewResult ViewInheritsInjectAndUsingsFromViewStarts()
+        {
+            return View(new Person { Name = "Person1" });
+        }
+
+        public ViewResult ViewInheritsBasePageFromViewStarts()
+        {
+            return View("/views/directives/scoped/ViewInheritsBasePageFromViewStarts.cshtml", 
+                        new Person { Name = "Person2" });
+        }
+    }
+}