wulg/Mvc

Limit the maximum number of Model errors to a reasonable value.
Fixes #490

test/Microsoft.AspNet.Mvc.FunctionalTests/ModelBindingTests.cs
@@ -2,9 +2,12 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.TestHost;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace Microsoft.AspNet.Mvc.FunctionalTests
@@ -47,6 +50,49 @@ namespace Microsoft.AspNet.Mvc.FunctionalTests
             //Assert
             Assert.Equal(200, response.StatusCode);
             Assert.Equal("\0", await response.ReadBodyAsStringAsync());
+        }
+
+        [Fact]
+        public async Task ModelBinding_LimitsErrosToMaxErrorCount()
+        {
+            // Arrange
+            var server = TestServer.Create(_services, _app);
+            var client = server.CreateClient();
+            var queryString = string.Join("=&", Enumerable.Range(0, 10).Select(i => "field" + i));
+
+            // Act
+            var response = await client.GetStringAsync("http://localhost/Home/ModelWithTooManyValidationErrors?" + queryString);
+
+            //Assert
+            var json = JsonConvert.DeserializeObject<Dictionary<string, string>>(response);
+            // 8 is the value of MaxModelValidationErrors for the application being tested.
+            Assert.Equal(8, json.Count);
+            Assert.Equal("The Field1 field is required.", json["Field1.Field1"]);
+            Assert.Equal("The Field2 field is required.", json["Field1.Field2"]);
+            Assert.Equal("The Field3 field is required.", json["Field1.Field3"]);
+            Assert.Equal("The Field1 field is required.", json["Field2.Field1"]);
+            Assert.Equal("The Field2 field is required.", json["Field2.Field2"]);
+            Assert.Equal("The Field3 field is required.", json["Field2.Field3"]);
+            Assert.Equal("The Field1 field is required.", json["Field3.Field1"]);
+            Assert.Equal("The Field2 field is required.", json["Field3.Field2"]);
+        }
+
+        [Fact]
+        public async Task ModelBinding_ValidatesAllPropertiesInModel()
+        {
+            // Arrange
+            var server = TestServer.Create(_services, _app);
+            var client = server.CreateClient();
+
+            // Act
+            var response = await client.GetStringAsync("http://localhost/Home/ModelWithFewValidationErrors?model=");
+
+            //Assert
+            var json = JsonConvert.DeserializeObject<Dictionary<string, string>>(response);
+            Assert.Equal(3, json.Count);
+            Assert.Equal("The Field1 field is required.", json["model.Field1"]);
+            Assert.Equal("The Field2 field is required.", json["model.Field2"]);
+            Assert.Equal("The Field3 field is required.", json["model.Field3"]);
         }
     }
 }

dotnet/aspnetcore

test/Microsoft.AspNet.Mvc.FunctionalTests/ModelBindingTests.cs
@@ -2,10 +2,13 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.TestHost;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace Microsoft.AspNet.Mvc.FunctionalTests
@@ -43,6 +46,49 @@ namespace Microsoft.AspNet.Mvc.FunctionalTests
             //Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             Assert.Equal("\0", await response.Content.ReadAsStringAsync());
+        }
+
+        [Fact]
+        public async Task ModelBinding_LimitsErrorsToMaxErrorCount()
+        {
+            // Arrange
+            var server = TestServer.Create(_services, _app);
+            var client = server.CreateClient();
+            var queryString = string.Join("=&", Enumerable.Range(0, 10).Select(i => "field" + i));
+
+            // Act
+            var response = await client.GetStringAsync("http://localhost/Home/ModelWithTooManyValidationErrors?" + queryString);
+
+            //Assert
+            var json = JsonConvert.DeserializeObject<Dictionary<string, string>>(response);
+            // 8 is the value of MaxModelValidationErrors for the application being tested.
+            Assert.Equal(8, json.Count);
+            Assert.Equal("The Field1 field is required.", json["Field1.Field1"]);
+            Assert.Equal("The Field2 field is required.", json["Field1.Field2"]);
+            Assert.Equal("The Field3 field is required.", json["Field1.Field3"]);
+            Assert.Equal("The Field1 field is required.", json["Field2.Field1"]);
+            Assert.Equal("The Field2 field is required.", json["Field2.Field2"]);
+            Assert.Equal("The Field3 field is required.", json["Field2.Field3"]);
+            Assert.Equal("The Field1 field is required.", json["Field3.Field1"]);
+            Assert.Equal("The maximum number of allowed model errors has been reached.", json[""]);
+        }
+
+        [Fact]
+        public async Task ModelBinding_ValidatesAllPropertiesInModel()
+        {
+            // Arrange
+            var server = TestServer.Create(_services, _app);
+            var client = server.CreateClient();
+
+            // Act
+            var response = await client.GetStringAsync("http://localhost/Home/ModelWithFewValidationErrors?model=");
+
+            //Assert
+            var json = JsonConvert.DeserializeObject<Dictionary<string, string>>(response);
+            Assert.Equal(3, json.Count);
+            Assert.Equal("The Field1 field is required.", json["model.Field1"]);
+            Assert.Equal("The Field2 field is required.", json["model.Field2"]);
+            Assert.Equal("The Field3 field is required.", json["model.Field3"]);
         }
     }
 }