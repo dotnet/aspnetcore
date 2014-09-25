aspnet/Mvc

Create a pre compilation module and apis to allow meta programming to precompile razor pages.
This is limited to sites where the .cshtml are still deployed. It's
current purpose is to speed up startup. Deploying without the razor
files is a separate feature.

src/Microsoft.AspNet.Mvc.Razor/Compilation/CompilerCache.cs
@@ -3,34 +3,102 @@
 
 using System;
 using System.Collections.Concurrent;
-using Microsoft.AspNet.FileSystems;
+using System.Linq;
+using System.Reflection;
 
 namespace Microsoft.AspNet.Mvc.Razor
 {
     public class CompilerCache
     {
-        private readonly ConcurrentDictionary<string, Type> _cache;
+        public static bool DebugBreak { get; set; }
+        private readonly ConcurrentDictionary<string, CompilerCacheEntry> _cache;
+        private static readonly Type[] EmptyType = new Type[0];
 
-        public CompilerCache()
+        internal CompilerCache()
         {
-            _cache = new ConcurrentDictionary<string, Type>(StringComparer.OrdinalIgnoreCase);
+            _cache = new ConcurrentDictionary<string, CompilerCacheEntry>(StringComparer.OrdinalIgnoreCase);
         }
 
-        public CompilationResult GetOrAdd(IFileInfo file, Func<CompilationResult> compile)
+        public CompilerCache([NotNull] IControllerAssemblyProvider _controllerAssemblyProvider)
+            : this()
         {
-            // Generate a content id
-            var contentId = file.PhysicalPath + '|' + file.LastModified.Ticks;
+            var assemblies = _controllerAssemblyProvider.CandidateAssemblies;
+            var types = assemblies.SelectMany(a => a.ExportedTypes);
+            var preCompiledCollections =
+                types
+                .Where(Match);
 
-            Type compiledType;
-            if (!_cache.TryGetValue(contentId, out compiledType))
+            foreach (var collectionType in preCompiledCollections)
             {
-                var result = compile();
-                _cache.TryAdd(contentId, result.CompiledType);
+                var preCompiledCollection = Activator.CreateInstance(collectionType)
+                                                    as ViewDescriptorCollection;
 
-                return result;
+                foreach (var fileInfo in preCompiledCollection.FileInfos)
+                {
+                    var containingAssembly = collectionType.GetTypeInfo().Assembly;
+                    var viewType = containingAssembly.GetType(fileInfo.FullTypeName);
+                    var cacheEntry = new CompilerCacheEntry(fileInfo, viewType);
+
+                    _cache.AddOrUpdate(fileInfo.RelativePath, cacheEntry, (a, b) => cacheEntry);
+                }
             }
+        }
 
-            return CompilationResult.Successful(compiledType);
+        private static bool Match(Type t)
+        {
+            var b = t.GetConstructor(EmptyType) != null;
+
+            var inAssemblyType = typeof(ViewDescriptorCollection);
+            var c = inAssemblyType.IsAssignableFrom(t);
+
+            return b && c
+                && !t.GetTypeInfo().IsAbstract
+                && !t.GetTypeInfo().ContainsGenericParameters;
+        }
+
+        public CompilationResult GetOrAdd(RelativeFileInfo fileInfo, Func<CompilationResult> compile)
+        {
+            CompilerCacheEntry cacheEntry;
+
+            if (!_cache.TryGetValue(fileInfo.RelativePath, out cacheEntry))
+            {
+                return OnCacheMiss(fileInfo, compile);
+            }
+            else
+            {
+                if (cacheEntry.Length != fileInfo.FileInfo.Length)
+                {
+                    // it's not a match, recompile
+                    return OnCacheMiss(fileInfo, compile);
+                }
+
+                if (cacheEntry.RuntimeTimeStamp == fileInfo.FileInfo.LastModified)
+                {
+                    // Match, not update needed
+                    return CompilationResult.Successful(cacheEntry.ViewType);
+                }
+
+                if (cacheEntry.CompiledTimeStamp == fileInfo.FileInfo.LastModified ||
+                    // Date doesn't match but it might be because of deployment, compare the hash
+                    cacheEntry.Hash == RazorFileHash.GetHash(fileInfo.FileInfo))
+                {
+                    // Cache hit, but we need to update the entry
+                    return OnCacheMiss(fileInfo, () => CompilationResult.Successful(cacheEntry.ViewType));
+                }
+
+                // it's not a match, recompile
+                return OnCacheMiss(fileInfo, compile);
+            }
+        }
+
+        private CompilationResult OnCacheMiss(RelativeFileInfo file, Func<CompilationResult> compile)
+        {
+            var result = compile();
+
+            var cacheEntry = new CompilerCacheEntry(file, result.CompiledType, null);
+            _cache.AddOrUpdate(file.RelativePath, cacheEntry, (a, b) => cacheEntry);
+
+            return result;
         }
     }
 }

dotnet/aspnetcore

src/Microsoft.AspNet.Mvc.Razor/Compilation/CompilerCache.cs
@@ -3,34 +3,110 @@
 
 using System;
 using System.Collections.Concurrent;
-using Microsoft.AspNet.FileSystems;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Microsoft.AspNet.Mvc.Razor
 {
     public class CompilerCache
     {
-        private readonly ConcurrentDictionary<string, Type> _cache;
+        private readonly ConcurrentDictionary<string, CompilerCacheEntry> _cache;
+        private static readonly Type[] EmptyType = new Type[0];
 
-        public CompilerCache()
+        public CompilerCache([NotNull] IEnumerable<Assembly> assemblies)
+            : this(GetFileInfos(assemblies))
         {
-            _cache = new ConcurrentDictionary<string, Type>(StringComparer.OrdinalIgnoreCase);
         }
 
-        public CompilationResult GetOrAdd(IFileInfo file, Func<CompilationResult> compile)
+        internal CompilerCache(IEnumerable<RazorFileInfoCollection> viewCollections) : this()
         {
-            // Generate a content id
-            var contentId = file.PhysicalPath + '|' + file.LastModified.Ticks;
-
-            Type compiledType;
-            if (!_cache.TryGetValue(contentId, out compiledType))
+            foreach (var viewCollection in viewCollections)
             {
-                var result = compile();
-                _cache.TryAdd(contentId, result.CompiledType);
+                foreach (var fileInfo in viewCollection.FileInfos)
+                {
+                    var containingAssembly = viewCollection.GetType().GetTypeInfo().Assembly;
+                    var viewType = containingAssembly.GetType(fileInfo.FullTypeName);
+                    var cacheEntry = new CompilerCacheEntry(fileInfo, viewType);
 
-                return result;
+                    // There shouldn't be any duplicates and if there are any the first will win.
+                    // If the result doesn't match the one on disk its going to recompile anyways.
+                    _cache.TryAdd(fileInfo.RelativePath, cacheEntry);
+                }
+            }
+        }
+
+        internal CompilerCache()
+        {
+            _cache = new ConcurrentDictionary<string, CompilerCacheEntry>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        internal static IEnumerable<RazorFileInfoCollection>
+                            GetFileInfos(IEnumerable<Assembly> assemblies)
+        {
+            return assemblies.SelectMany(a => a.ExportedTypes)
+                    .Where(Match)
+                    .Select(c => (RazorFileInfoCollection)Activator.CreateInstance(c));
+        }
+
+        private static bool Match(Type t)
+        {
+            var inAssemblyType = typeof(RazorFileInfoCollection);
+            if (inAssemblyType.IsAssignableFrom(t))
+            {
+                var hasParameterlessConstructor = t.GetConstructor(EmptyType) != null;
+
+                return hasParameterlessConstructor
+                    && !t.GetTypeInfo().IsAbstract
+                    && !t.GetTypeInfo().ContainsGenericParameters;
             }
 
-            return CompilationResult.Successful(compiledType);
+            return false;
+        }
+
+        public CompilationResult GetOrAdd(RelativeFileInfo fileInfo, Func<CompilationResult> compile)
+        {
+            if (!_cache.TryGetValue(fileInfo.RelativePath, out var cacheEntry))
+            {
+                return OnCacheMiss(fileInfo, compile);
+            }
+            else
+            {
+                if (cacheEntry.Length != fileInfo.FileInfo.Length)
+                {
+                    // it's not a match, recompile
+                    return OnCacheMiss(fileInfo, compile);
+                }
+
+                if (cacheEntry.LastModified == fileInfo.FileInfo.LastModified)
+                {
+                    // Match, not update needed
+                    return CompilationResult.Successful(cacheEntry.ViewType);
+                }
+
+                var hash = RazorFileHash.GetHash(fileInfo.FileInfo);
+
+                // Timestamp doesn't match but it might be because of deployment, compare the hash.
+                if (cacheEntry.IsPreCompiled &&
+                    string.Equals(cacheEntry.Hash, hash, StringComparison.Ordinal))
+                {
+                    // Cache hit, but we need to update the entry
+                    return OnCacheMiss(fileInfo, () => CompilationResult.Successful(cacheEntry.ViewType));
+                }
+
+                // it's not a match, recompile
+                return OnCacheMiss(fileInfo, compile);
+            }
+        }
+
+        private CompilationResult OnCacheMiss(RelativeFileInfo file, Func<CompilationResult> compile)
+        {
+            var result = compile();
+
+            var cacheEntry = new CompilerCacheEntry(file, result.CompiledType);
+            _cache.AddOrUpdate(file.RelativePath, cacheEntry, (a, b) => cacheEntry);
+
+            return result;
         }
     }
 }