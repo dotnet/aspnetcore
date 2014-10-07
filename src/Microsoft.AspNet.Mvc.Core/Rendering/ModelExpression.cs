3492be3c6cde3772788e9cf848ad9844ebe8b1d9
Doug Bunting <dougbu@microsoft.com>
2014-10-07 10:00:24 -0700

    Add copyright notices to new files

--- src/Microsoft.AspNet.Mvc.Core/Rendering/ModelExpression.cs
+++ src/Microsoft.AspNet.Mvc.Core/Rendering/ModelExpression.cs
@@ -1,4 +1,6 @@
-﻿
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System;
 using Microsoft.AspNet.Mvc.Core;
 using Microsoft.AspNet.Mvc.ModelBinding;
