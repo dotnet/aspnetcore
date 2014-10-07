aspnet/Mvc

Issue #1176: Renaming ActionDescriptor, ActionDescriptorProvider, ActionInvoker, ActionInvokerProvider, ActionExecutor, ModelBuilder, Tests. Either dropping "Reflected" or changing it to "Controller" depending on the context.

test/WebSites/ModelBuilderWebSite/Controllers/ActionModelController.cs
@@ -3,23 +3,23 @@
 
 using System;
 using Microsoft.AspNet.Mvc;
-using Microsoft.AspNet.Mvc.ReflectedModelBuilder;
+using Microsoft.AspNet.Mvc.ApplicationModel;
 
-namespace ReflectedModelWebSite
+namespace ModelBuilderWebSite
 {
     // This controller uses an reflected model attribute to change an action name, and thus
     // the URL.
-    public class ReflectedActionModelController : Controller
+    public class ActionModelController : Controller
     {
         [ActionName2("ActionName")]
         public string GetActionName()
         {
-            var actionDescriptor = (ReflectedActionDescriptor)ActionContext.ActionDescriptor;
+            var actionDescriptor = (ControllerActionDescriptor)ActionContext.ActionDescriptor;
 
             return actionDescriptor.Name;
         }
 
-        private class ActionName2Attribute : Attribute, IReflectedActionModelConvention
+        private class ActionName2Attribute : Attribute, IActionModelConvention
         {
             private readonly string _actionName;
 
@@ -28,7 +28,7 @@ namespace ReflectedModelWebSite
                 _actionName = actionName;
             }
 
-            public void Apply(ReflectedActionModel model)
+            public void Apply(ActionModel model)
             {
                 model.ActionName = _actionName;
             }

test/WebSites/ModelBuilderWebSite/Controllers/ControllerModelController.cs
@@ -3,23 +3,23 @@
 
 using System;
 using Microsoft.AspNet.Mvc;
-using Microsoft.AspNet.Mvc.ReflectedModelBuilder;
+using Microsoft.AspNet.Mvc.ApplicationModel;
 
-namespace ReflectedModelWebSite
+namespace ModelBuilderWebSite
 {
     // This controller uses an reflected model attribute to change the controller name, and thus
     // the URL.
     [ControllerName("CoolController")]
-    public class ReflectedControllerModelController : Controller
+    public class ControllerModelController : Controller
     {
         public string GetControllerName()
         {
-            var actionDescriptor = (ReflectedActionDescriptor)ActionContext.ActionDescriptor;
+            var actionDescriptor = (ControllerActionDescriptor)ActionContext.ActionDescriptor;
 
             return actionDescriptor.ControllerName;
         }
 
-        private class ControllerNameAttribute : Attribute, IReflectedControllerModelConvention
+        private class ControllerNameAttribute : Attribute, IControllerModelConvention
         {
             private readonly string _controllerName;
 
@@ -28,7 +28,7 @@ namespace ReflectedModelWebSite
                 _controllerName = controllerName;
             }
 
-            public void Apply(ReflectedControllerModel model)
+            public void Apply(ControllerModel model)
             {
                 model.ControllerName = _controllerName;
             }

test/WebSites/ModelBuilderWebSite/Controllers/ParameterModelController.cs
@@ -3,23 +3,23 @@
 
 using System;
 using Microsoft.AspNet.Mvc;
-using Microsoft.AspNet.Mvc.ReflectedModelBuilder;
+using Microsoft.AspNet.Mvc.ApplicationModel;
 
-namespace ReflectedModelWebSite
+namespace ModelBuilderWebSite
 {
     // This controller uses an reflected model attribute to change a parameter to optional.
-    public class ReflectedParameterModelController : Controller
+    public class ParameterModelController : Controller
     {
         public string GetParameterIsOptional([Optional] int? id)
         {
-            var actionDescriptor = (ReflectedActionDescriptor)ActionContext.ActionDescriptor;
+            var actionDescriptor = (ControllerActionDescriptor)ActionContext.ActionDescriptor;
 
             return actionDescriptor.Parameters[0].IsOptional.ToString();
         }
 
-        private class OptionalAttribute : Attribute, IReflectedParameterModelConvention
+        private class OptionalAttribute : Attribute, IParameterModelConvention
         {
-            public void Apply(ReflectedParameterModel model)
+            public void Apply(ParameterModel model)
             {
                 model.IsOptional = true;
             }

test/WebSites/ModelBuilderWebSite/Startup.cs
@@ -4,7 +4,7 @@
 using Microsoft.AspNet.Builder;
 using Microsoft.Framework.DependencyInjection;
 
-namespace ReflectedModelWebSite
+namespace ModelBuilderWebSite
 {
     public class Startup
     {

dotnet/aspnetcore

test/WebSites/ApplicationModelWebSite/Controllers/ActionModelController.cs
@@ -3,23 +3,23 @@
 
 using System;
 using Microsoft.AspNet.Mvc;
-using Microsoft.AspNet.Mvc.ReflectedModelBuilder;
+using Microsoft.AspNet.Mvc.ApplicationModel;
 
-namespace ReflectedModelWebSite
+namespace ApplicationModelWebSite
 {
     // This controller uses an reflected model attribute to change an action name, and thus
     // the URL.
-    public class ReflectedActionModelController : Controller
+    public class ActionModelController : Controller
     {
         [ActionName2("ActionName")]
         public string GetActionName()
         {
-            var actionDescriptor = (ReflectedActionDescriptor)ActionContext.ActionDescriptor;
+            var actionDescriptor = (ControllerActionDescriptor)ActionContext.ActionDescriptor;
 
             return actionDescriptor.Name;
         }
 
-        private class ActionName2Attribute : Attribute, IReflectedActionModelConvention
+        private class ActionName2Attribute : Attribute, IActionModelConvention
         {
             private readonly string _actionName;
 
@@ -28,7 +28,7 @@ namespace ReflectedModelWebSite
                 _actionName = actionName;
             }
 
-            public void Apply(ReflectedActionModel model)
+            public void Apply(ActionModel model)
             {
                 model.ActionName = _actionName;
             }

test/WebSites/ApplicationModelWebSite/Controllers/ControllerModelController.cs
@@ -3,23 +3,23 @@
 
 using System;
 using Microsoft.AspNet.Mvc;
-using Microsoft.AspNet.Mvc.ReflectedModelBuilder;
+using Microsoft.AspNet.Mvc.ApplicationModel;
 
-namespace ReflectedModelWebSite
+namespace ApplicationModelWebSite
 {
     // This controller uses an reflected model attribute to change the controller name, and thus
     // the URL.
     [ControllerName("CoolController")]
-    public class ReflectedControllerModelController : Controller
+    public class ControllerModelController : Controller
     {
         public string GetControllerName()
         {
-            var actionDescriptor = (ReflectedActionDescriptor)ActionContext.ActionDescriptor;
+            var actionDescriptor = (ControllerActionDescriptor)ActionContext.ActionDescriptor;
 
             return actionDescriptor.ControllerName;
         }
 
-        private class ControllerNameAttribute : Attribute, IReflectedControllerModelConvention
+        private class ControllerNameAttribute : Attribute, IControllerModelConvention
         {
             private readonly string _controllerName;
 
@@ -28,7 +28,7 @@ namespace ReflectedModelWebSite
                 _controllerName = controllerName;
             }
 
-            public void Apply(ReflectedControllerModel model)
+            public void Apply(ControllerModel model)
             {
                 model.ControllerName = _controllerName;
             }

test/WebSites/ApplicationModelWebSite/Controllers/ParameterModelController.cs
@@ -3,23 +3,23 @@
 
 using System;
 using Microsoft.AspNet.Mvc;
-using Microsoft.AspNet.Mvc.ReflectedModelBuilder;
+using Microsoft.AspNet.Mvc.ApplicationModel;
 
-namespace ReflectedModelWebSite
+namespace ApplicationModelWebSite
 {
     // This controller uses an reflected model attribute to change a parameter to optional.
-    public class ReflectedParameterModelController : Controller
+    public class ParameterModelController : Controller
     {
         public string GetParameterIsOptional([Optional] int? id)
         {
-            var actionDescriptor = (ReflectedActionDescriptor)ActionContext.ActionDescriptor;
+            var actionDescriptor = (ControllerActionDescriptor)ActionContext.ActionDescriptor;
 
             return actionDescriptor.Parameters[0].IsOptional.ToString();
         }
 
-        private class OptionalAttribute : Attribute, IReflectedParameterModelConvention
+        private class OptionalAttribute : Attribute, IParameterModelConvention
         {
-            public void Apply(ReflectedParameterModel model)
+            public void Apply(ParameterModel model)
             {
                 model.IsOptional = true;
             }

test/WebSites/ApplicationModelWebSite/Startup.cs
@@ -4,7 +4,7 @@
 using Microsoft.AspNet.Builder;
 using Microsoft.Framework.DependencyInjection;
 
-namespace ReflectedModelWebSite
+namespace ApplicationModelWebSite
 {
     public class Startup
     {