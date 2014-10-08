f66345263d9135f0aa80bf2f1766a22d39230bce
Ryan Nowak <nowakra@gmail.com>
2014-10-08 09:04:49 -0700

    Add WebApiCompatShim project structure

---
+++ test/WebSites/WebApiCompatShimWebSite/Startup.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+
+namespace WebApiCompatShimWebSite
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            var configuration = app.GetTestConfiguration();
+
+            app.UsePerRequestServices(services =>
+            {
+                services.AddMvc(configuration);
+            });
+
+            app.UseMvc();
+        }
+    }
+}
