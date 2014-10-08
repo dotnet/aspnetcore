684aaed9bdade5c44964ab5ef89427c28463f39a
Hao Kung <haok@microsoft.com>
2014-10-08 13:23:12 -0700

    Cleanup headers

--- test/Microsoft.AspNet.Hosting.Tests/UseServicesFacts.cs
+++ test/Microsoft.AspNet.Hosting.Tests/UseServicesFacts.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using Microsoft.AspNet.Builder;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.DependencyInjection.Fallback;
