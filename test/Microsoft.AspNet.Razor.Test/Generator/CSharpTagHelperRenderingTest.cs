aspnet/Razor

Add tests to validate @addtaghelper directive.
- Fixed existing tests to work with new RazorParser.
- Validated directive syntax tree creation, errors and code generation.

#111

test/Microsoft.AspNet.Razor.Test/Generator/CSharpTagHelperRenderingTest.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using System.Reflection;
+using Microsoft.AspNet.Razor.Generator.Compiler;
 using Microsoft.AspNet.Razor.TagHelpers;
 using Moq;
 using Xunit;
@@ -10,6 +12,29 @@ namespace Microsoft.AspNet.Razor.Test.Generator
 {
     public class CSharpTagHelperRenderingTest : TagHelperTestBase
     {
+        [Fact]
+        public void CSharpCodeGeneratorCorrectlyGeneratesMappingsForAddTagHelperDirective()
+        {
+            RunTest("AddTagHelperDirective",
+                    designTimeMode: true,
+                    tabTest: TabTest.NoTabs,
+                    expectedDesignTimePragmas: new List<LineMapping>()
+                    {
+                        BuildLineMapping(14, 0, 440, 14, 14, 11)
+                    });
+        }
+
+        [Fact]
+        public void TagHelpers_DirectivesGenerateDesignTimeMappings()
+        {
+            // Act & Assert
+            RunTagHelperTest(testName: "AddTagHelperDirective",
+                             designTimeMode: true,
+                             tagHelperDescriptors: new[] {
+                                new TagHelperDescriptor("p", "pTagHelper", ContentBehavior.None)
+                             });
+        }
+
         [Theory]
         [InlineData("SingleTagHelper")]
         [InlineData("BasicTagHelpers")]
@@ -26,50 +51,48 @@ namespace Microsoft.AspNet.Razor.Test.Generator
             checkedPropertyInfo.Setup(ipi => ipi.PropertyType).Returns(typeof(bool));
             checkedPropertyInfo.Setup(ipi => ipi.Name).Returns("Checked");
             // Arrange
-            var tagHelperProvider = new TagHelperDescriptorProvider(
-                new TagHelperDescriptor[]
-                {
-                    new TagHelperDescriptor("p",
-                                            "PTagHelper", 
-                                            ContentBehavior.None,
-                                            new [] {
-                                                new TagHelperAttributeDescriptor("foo", pFooPropertyInfo.Object)
-                                            }),
-                    new TagHelperDescriptor("input",
-                                            "InputTagHelper",
-                                            ContentBehavior.None,
-                                            new TagHelperAttributeDescriptor[] {
-                                                new TagHelperAttributeDescriptor("type", inputTypePropertyInfo.Object)
-                                            }),
-                    new TagHelperDescriptor("input",
-                                            "InputTagHelper2",
-                                            ContentBehavior.None,
-                                            new TagHelperAttributeDescriptor[] {
-                                                new TagHelperAttributeDescriptor("type", inputTypePropertyInfo.Object),
-                                                new TagHelperAttributeDescriptor("checked", checkedPropertyInfo.Object)
-                                            }),
-                });
+            var tagHelperDescriptors = new TagHelperDescriptor[]
+            {
+                new TagHelperDescriptor("p",
+                                        "PTagHelper", 
+                                        ContentBehavior.None,
+                                        new [] {
+                                            new TagHelperAttributeDescriptor("foo", pFooPropertyInfo.Object)
+                                        }),
+                new TagHelperDescriptor("input",
+                                        "InputTagHelper",
+                                        ContentBehavior.None,
+                                        new TagHelperAttributeDescriptor[] {
+                                            new TagHelperAttributeDescriptor("type", inputTypePropertyInfo.Object)
+                                        }),
+                new TagHelperDescriptor("input",
+                                        "InputTagHelper2",
+                                        ContentBehavior.None,
+                                        new TagHelperAttributeDescriptor[] {
+                                            new TagHelperAttributeDescriptor("type", inputTypePropertyInfo.Object),
+                                            new TagHelperAttributeDescriptor("checked", checkedPropertyInfo.Object)
+                                        })
+            };
 
             // Act & Assert
-            RunTagHelperTest(testType, tagHelperProvider);
+            RunTagHelperTest(testType, tagHelperDescriptors);
         }
 
         [Fact]
         public void TagHelpers_ContentBehavior()
         {
             // Arrange
-            var tagHelperProvider = new TagHelperDescriptorProvider(
-                new TagHelperDescriptor[]
-                {
+            var tagHelperDescriptors = new TagHelperDescriptor[]
+            {
                     new TagHelperDescriptor("modify", "ModifyTagHelper", ContentBehavior.Modify),
                     new TagHelperDescriptor("none", "NoneTagHelper", ContentBehavior.None),
                     new TagHelperDescriptor("append", "AppendTagHelper", ContentBehavior.Append),
                     new TagHelperDescriptor("prepend", "PrependTagHelper", ContentBehavior.Prepend),
                     new TagHelperDescriptor("replace", "ReplaceTagHelper", ContentBehavior.Replace),
-                });
+            };
 
             // Act & Assert
-            RunTagHelperTest("ContentBehaviorTagHelpers", tagHelperProvider);
+            RunTagHelperTest("ContentBehaviorTagHelpers", tagHelperDescriptors);
         }
     }
 }

dotnet/aspnetcore

test/Microsoft.AspNet.Razor.Test/Generator/CSharpTagHelperRenderingTest.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using System.Reflection;
+using Microsoft.AspNet.Razor.Generator.Compiler;
 using Microsoft.AspNet.Razor.TagHelpers;
 using Moq;
 using Xunit;
@@ -10,6 +12,34 @@ namespace Microsoft.AspNet.Razor.Test.Generator
 {
     public class CSharpTagHelperRenderingTest : TagHelperTestBase
     {
+        [Fact]
+        public void CSharpCodeGenerator_CorrectlyGeneratesMappings_ForAddTagHelperDirective()
+        {
+            // Act & Assert
+            RunTagHelperTest("AddTagHelperDirective",
+                             designTimeMode: true,
+                             expectedDesignTimePragmas: new List<LineMapping>()
+                             {
+                                    BuildLineMapping(documentAbsoluteIndex: 14,
+                                                     documentLineIndex: 0,
+                                                     generatedAbsoluteIndex: 433,
+                                                     generatedLineIndex: 14,
+                                                     characterOffsetIndex: 14,
+                                                     contentLength: 11)
+                             });
+        }
+
+        [Fact]
+        public void TagHelpers_Directive_GenerateDesignTimeMappings()
+        {
+            // Act & Assert
+            RunTagHelperTest("AddTagHelperDirective",
+                             designTimeMode: true,
+                             tagHelperDescriptors: new[] {
+                                new TagHelperDescriptor("p", "pTagHelper", ContentBehavior.None)
+                             });
+        }
+
         [Theory]
         [InlineData("TagHelpersInSection")]
         [InlineData("TagHelpersInHelper")]
@@ -19,21 +49,20 @@ namespace Microsoft.AspNet.Razor.Test.Generator
             var propertyInfoMock = new Mock<PropertyInfo>();
             propertyInfoMock.Setup(propertyInfo => propertyInfo.PropertyType).Returns(typeof(string));
             propertyInfoMock.Setup(propertyInfo => propertyInfo.Name).Returns("BoundProperty");
-            var tagHelperDescriptorProvider = new TagHelperDescriptorProvider(
-                new TagHelperDescriptor[]
-                {
-                    new TagHelperDescriptor("MyTagHelper",
-                                            "MyTagHelper",
-                                            ContentBehavior.None,
-                                            new [] {
-                                                new TagHelperAttributeDescriptor("BoundProperty", 
-                                                                                 propertyInfoMock.Object)
-                                            }),
-                    new TagHelperDescriptor("NestedTagHelper", "NestedTagHelper", ContentBehavior.Modify)
-                });
+            var tagHelperDescriptors = new TagHelperDescriptor[]
+            {
+                new TagHelperDescriptor("MyTagHelper",
+                                        "MyTagHelper",
+                                        ContentBehavior.None,
+                                        new [] {
+                                            new TagHelperAttributeDescriptor("BoundProperty",
+                                                                             propertyInfoMock.Object)
+                                        }),
+                new TagHelperDescriptor("NestedTagHelper", "NestedTagHelper", ContentBehavior.Modify)
+            };
 
             // Act & Assert
-            RunTagHelperTest(testType, tagHelperDescriptorProvider: tagHelperDescriptorProvider);
+            RunTagHelperTest(testType, tagHelperDescriptors: tagHelperDescriptors);
         }
 
         [Theory]
@@ -52,50 +81,48 @@ namespace Microsoft.AspNet.Razor.Test.Generator
             var checkedPropertyInfo = new Mock<PropertyInfo>();
             checkedPropertyInfo.Setup(propertyInfo => propertyInfo.PropertyType).Returns(typeof(bool));
             checkedPropertyInfo.Setup(propertyInfo => propertyInfo.Name).Returns("Checked");
-            var tagHelperDescriptorProvider = new TagHelperDescriptorProvider(
-                new TagHelperDescriptor[]
-                {
-                    new TagHelperDescriptor("p",
-                                            "PTagHelper", 
-                                            ContentBehavior.None,
-                                            new [] {
-                                                new TagHelperAttributeDescriptor("foo", pFooPropertyInfo.Object)
-                                            }),
-                    new TagHelperDescriptor("input",
-                                            "InputTagHelper",
-                                            ContentBehavior.None,
-                                            new TagHelperAttributeDescriptor[] {
-                                                new TagHelperAttributeDescriptor("type", inputTypePropertyInfo.Object)
-                                            }),
-                    new TagHelperDescriptor("input",
-                                            "InputTagHelper2",
-                                            ContentBehavior.None,
-                                            new TagHelperAttributeDescriptor[] {
-                                                new TagHelperAttributeDescriptor("type", inputTypePropertyInfo.Object),
-                                                new TagHelperAttributeDescriptor("checked", checkedPropertyInfo.Object)
-                                            }),
-                });
+            var tagHelperDescriptors = new TagHelperDescriptor[]
+            {
+                new TagHelperDescriptor("p",
+                                        "PTagHelper",
+                                        ContentBehavior.None,
+                                        new [] {
+                                            new TagHelperAttributeDescriptor("foo", pFooPropertyInfo.Object)
+                                        }),
+                new TagHelperDescriptor("input",
+                                        "InputTagHelper",
+                                        ContentBehavior.None,
+                                        new TagHelperAttributeDescriptor[] {
+                                            new TagHelperAttributeDescriptor("type", inputTypePropertyInfo.Object)
+                                        }),
+                new TagHelperDescriptor("input",
+                                        "InputTagHelper2",
+                                        ContentBehavior.None,
+                                        new TagHelperAttributeDescriptor[] {
+                                            new TagHelperAttributeDescriptor("type", inputTypePropertyInfo.Object),
+                                            new TagHelperAttributeDescriptor("checked", checkedPropertyInfo.Object)
+                                        })
+            };
 
             // Act & Assert
-            RunTagHelperTest(testType, tagHelperDescriptorProvider: tagHelperDescriptorProvider);
+            RunTagHelperTest(testType, tagHelperDescriptors: tagHelperDescriptors);
         }
 
         [Fact]
         public void TagHelpers_WithContentBehaviors_GenerateExpectedOutput()
         {
             // Arrange
-            var tagHelperDescriptorProvider = new TagHelperDescriptorProvider(
-                new TagHelperDescriptor[]
-                {
+            var tagHelperDescriptors = new TagHelperDescriptor[]
+            {
                     new TagHelperDescriptor("modify", "ModifyTagHelper", ContentBehavior.Modify),
                     new TagHelperDescriptor("none", "NoneTagHelper", ContentBehavior.None),
                     new TagHelperDescriptor("append", "AppendTagHelper", ContentBehavior.Append),
                     new TagHelperDescriptor("prepend", "PrependTagHelper", ContentBehavior.Prepend),
                     new TagHelperDescriptor("replace", "ReplaceTagHelper", ContentBehavior.Replace),
-                });
+            };
 
             // Act & Assert
-            RunTagHelperTest("ContentBehaviorTagHelpers", tagHelperDescriptorProvider: tagHelperDescriptorProvider);
+            RunTagHelperTest("ContentBehaviorTagHelpers", tagHelperDescriptors: tagHelperDescriptors);
         }
     }
 }