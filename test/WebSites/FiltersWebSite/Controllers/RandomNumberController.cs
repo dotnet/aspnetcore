aspnet/Mvc

Issue #937: Adding functional tests for Filters.

test/WebSites/FiltersWebSite/Controllers/RandomNumberController.cs
@@ -1,0 +1,34 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc;
+
+namespace FiltersWebSite
+{
+    public class RandomNumberController : Controller
+    {
+        [ServiceFilter(typeof(RandomNumberFilter))]
+        public int GetRandomNumber()
+        {
+            return 2;
+        }
+
+        [ServiceFilter(typeof(FakeUserAttribute))]
+        public int GetFakeRandomNumber()
+        {
+            return 2;
+        }
+
+        [TypeFilter(typeof(RandomNumberProvider))]
+        public int GetModifiedRandomNumber(int randomNumber)
+        {
+            return randomNumber / 2;
+        }
+
+        [TypeFilter(typeof(FakeRandomNumberProvider))]
+        public int GetFakeModifiedRandomNumber(int randomNumber)
+        {
+            return randomNumber / 2;
+        }
+    }
+}

test/WebSites/FiltersWebSite/Filters/RandomNumberProvider.cs
@@ -1,0 +1,26 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc;
+
+namespace FiltersWebSite
+{
+    public class RandomNumberProvider : IActionFilter
+    {
+        private RandomNumberService _random;
+
+        public RandomNumberProvider(RandomNumberService random)
+        {
+            _random = random;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            context.ActionArguments["randomNumber"] = _random.GetRand();
+        }
+    }
+}

dotnet/aspnetcore

test/WebSites/FiltersWebSite/Controllers/RandomNumberController.cs
@@ -1,0 +1,34 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc;
+
+namespace FiltersWebSite
+{
+    public class RandomNumberController : Controller
+    {
+        [ServiceFilter(typeof(RandomNumberFilter))]
+        public int GetRandomNumber()
+        {
+            return 2;
+        }
+
+        [ServiceFilter(typeof(AuthorizeUserAttribute))]
+        public int GetAuthorizedRandomNumber()
+        {
+            return 2;
+        }
+
+        [TypeFilter(typeof(RandomNumberProvider))]
+        public int GetModifiedRandomNumber(int randomNumber)
+        {
+            return randomNumber / 2;
+        }
+
+        [TypeFilter(typeof(ModifiedRandomNumberProvider))]
+        public int GetHalfOfModifiedRandomNumber(int randomNumber)
+        {
+            return randomNumber / 2;
+        }
+    }
+}

test/WebSites/FiltersWebSite/Filters/RandomNumberProvider.cs
@@ -1,0 +1,26 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc;
+
+namespace FiltersWebSite
+{
+    public class RandomNumberProvider : IActionFilter
+    {
+        private RandomNumberService _random;
+
+        public RandomNumberProvider(RandomNumberService random)
+        {
+            _random = random;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            context.ActionArguments["randomNumber"] = _random.GetRandamNumber();
+        }
+    }
+}