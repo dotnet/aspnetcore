aspnet/Mvc

[Fixes #1216] Provide a property on JsonOutputFormatter to set serializer settings [Fixes #1221] Rename OutputFormatter's WriteResponseContentHeaders to WriteResponseHeaders [Fixes #932] Setting Json Serializer Settings

test/Microsoft.AspNet.Mvc.Core.Test/Formatters/JsonOutputFormatterTests.cs
@@ -1,0 +1,128 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc.HeaderValueAbstractions;
+using Microsoft.AspNet.Routing;
+using Moq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Xunit;
+
+namespace Microsoft.AspNet.Mvc.Core.Test.Formatters
+{
+    public class JsonOutputFormatterTests
+    {
+        [Fact]
+        public void Creates_SerializerSettings_ByDefault()
+        {
+            // Arrange
+            // Act
+            var jsonFrmtr = new JsonOutputFormatter();
+
+            // Assert
+            Assert.NotNull(jsonFrmtr.SerializerSettings);
+        }
+
+
+        [Fact]
+        public async Task ChangesTo_DefaultSerializerSettings_TakesEffect()
+        {
+            // Arrange
+            var person = new User() { Name = "John", Age = 35 };
+            string expectedOutput = JsonConvert.SerializeObject(person, new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Formatting.Indented
+            });
+
+            var jsonFrmtr = new JsonOutputFormatter();
+            jsonFrmtr.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            jsonFrmtr.SerializerSettings.Formatting = Formatting.Indented;
+            var outputFormatterContext = GetOutputFormatterContext(person, typeof(User));
+
+            // Act
+            await jsonFrmtr.WriteResponseBodyAsync(outputFormatterContext);
+
+            // Assert
+            Assert.NotNull(outputFormatterContext.ActionContext.HttpContext.Response.Body);
+            outputFormatterContext.ActionContext.HttpContext.Response.Body.Position = 0;
+            Assert.Equal(expectedOutput,
+                new StreamReader(outputFormatterContext.ActionContext.HttpContext.Response.Body, Encoding.UTF8)
+                        .ReadToEnd());
+        }
+
+        [Fact]
+        public async Task CustomSerializerSettingsObect_TakesEffect()
+        {
+            // Arrange
+            var person = new User() { Name = "John", Age = 35 };
+            string expectedOutput = JsonConvert.SerializeObject(person, new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Formatting.Indented
+            });
+
+            var jsonFrmtr = new JsonOutputFormatter();
+            jsonFrmtr.SerializerSettings = new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Formatting.Indented
+            };
+
+            var outputFormatterContext = GetOutputFormatterContext(person, typeof(User));
+
+            // Act
+            await jsonFrmtr.WriteResponseBodyAsync(outputFormatterContext);
+
+            // Assert
+            Assert.NotNull(outputFormatterContext.ActionContext.HttpContext.Response.Body);
+            outputFormatterContext.ActionContext.HttpContext.Response.Body.Position = 0;
+            Assert.Equal(expectedOutput,
+                new StreamReader(outputFormatterContext.ActionContext.HttpContext.Response.Body, Encoding.UTF8)
+                        .ReadToEnd());
+        }
+
+        private OutputFormatterContext GetOutputFormatterContext(object outputValue, Type outputType,
+                                            string contentType = "application/xml; charset=utf-8")
+        {
+            var mediaTypeHeaderValue = MediaTypeHeaderValue.Parse(contentType);
+
+            return new OutputFormatterContext
+            {
+                Object = outputValue,
+                DeclaredType = outputType,
+                ActionContext = GetActionContext(mediaTypeHeaderValue),
+                SelectedEncoding = Encoding.GetEncoding(mediaTypeHeaderValue.Charset)
+            };
+        }
+
+        private static ActionContext GetActionContext(MediaTypeHeaderValue contentType)
+        {
+            var request = new Mock<HttpRequest>();
+            var headers = new Mock<IHeaderDictionary>();
+            request.Setup(r => r.ContentType).Returns(contentType.RawValue);
+            request.SetupGet(r => r.Headers).Returns(headers.Object);
+            request.SetupGet(f => f.AcceptCharset).Returns(contentType.Charset);
+            var response = new Mock<HttpResponse>();
+            response.SetupGet(f => f.Body).Returns(new MemoryStream());
+            var httpContext = new Mock<HttpContext>();
+            httpContext.SetupGet(c => c.Request).Returns(request.Object);
+            httpContext.SetupGet(c => c.Response).Returns(response.Object);
+            return new ActionContext(httpContext.Object, routeData: null, actionDescriptor: null);
+        }
+
+        private sealed class User
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+        }
+    }
+}

test/Microsoft.AspNet.Mvc.FunctionalTests/JsonOutputFormatterTests.cs
@@ -1,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.TestHost;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Microsoft.AspNet.Mvc.FunctionalTests
+{
+    public class JsonOutputFormatterTests
+    {
+        private readonly IServiceProvider _provider = TestHelper.CreateServices("FormatterWebSite");
+        private readonly Action<IApplicationBuilder> _app = new FormatterWebSite.Startup().Configure;
+
+        [Fact]
+        public async Task JsonOutputFormatter_ReturnsIndentedJson()
+        {
+            // Arrange
+            var user = new FormatterWebSite.User()
+            {
+                Id = 1,
+                Alias = "john",
+                description = "Administrator",
+                Designation = "Administrator",
+                Name = "John Williams"
+            };
+
+            var serializerSettings = new JsonSerializerSettings();
+            serializerSettings.Formatting = Formatting.Indented;
+            string expectedBody = JsonConvert.SerializeObject(user, serializerSettings);
+
+            var server = TestServer.Create(_provider, _app);
+            var client = server.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("http://localhost/JsonFormattter/ReturnsIndentedJson");
+
+            // Assert
+            var actualBody = await response.Content.ReadAsStringAsync();
+            Assert.Equal(expectedBody, actualBody);
+        }
+    }
+}

test/WebSites/ConnegWebSite/Controllers/NormalController.cs
@@ -3,6 +3,8 @@
 
 using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Mvc.HeaderValueAbstractions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace ConnegWebsite
 {
@@ -15,8 +17,10 @@ namespace ConnegWebsite
             {
                 result.Formatters.Add(new PlainTextFormatter());
                 result.Formatters.Add(new CustomFormatter("application/custom"));
-                result.Formatters.Add(new JsonOutputFormatter(JsonOutputFormatter.CreateDefaultSettings(),
-                                                              indent: true));
+
+                var jsonFrmtr = new JsonOutputFormatter();
+                jsonFrmtr.SerializerSettings.Formatting = Formatting.Indented;
+                result.Formatters.Add(jsonFrmtr);
             }
 
             base.OnActionExecuted(context);

test/WebSites/FormatterWebSite/Controllers/JsonFormattterController.cs
@@ -1,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json;
+
+namespace FormatterWebSite.Controllers
+{
+    public class JsonFormattterController : Controller
+    {
+        public IActionResult ReturnsIndentedJson()
+        {
+            var user = new User()
+            {
+                Id = 1,
+                Alias = "john",
+                description = "Administrator",
+                Designation = "Administrator",
+                Name = "John Williams"
+            };
+
+            var jsonFrmtr = new JsonOutputFormatter();
+            jsonFrmtr.SerializerSettings.Formatting = Formatting.Indented;
+
+            var objResult = new ObjectResult(user);
+            objResult.Formatters.Add(jsonFrmtr);
+
+            return objResult;
+        }
+    }
+}

dotnet/aspnetcore

test/Microsoft.AspNet.Mvc.Core.Test/Formatters/JsonOutputFormatterTests.cs
@@ -1,0 +1,128 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc.HeaderValueAbstractions;
+using Microsoft.AspNet.Routing;
+using Moq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Xunit;
+
+namespace Microsoft.AspNet.Mvc.Core.Test.Formatters
+{
+    public class JsonOutputFormatterTests
+    {
+        [Fact]
+        public void Creates_SerializerSettings_ByDefault()
+        {
+            // Arrange
+            // Act
+            var jsonFormatter = new JsonOutputFormatter();
+
+            // Assert
+            Assert.NotNull(jsonFormatter.SerializerSettings);
+        }
+
+
+        [Fact]
+        public async Task ChangesTo_DefaultSerializerSettings_TakesEffect()
+        {
+            // Arrange
+            var person = new User() { Name = "John", Age = 35 };
+            var expectedOutput = JsonConvert.SerializeObject(person, new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Formatting.Indented
+            });
+
+            var jsonFormatter = new JsonOutputFormatter();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            jsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+            var outputFormatterContext = GetOutputFormatterContext(person, typeof(User));
+
+            // Act
+            await jsonFormatter.WriteResponseBodyAsync(outputFormatterContext);
+
+            // Assert
+            Assert.NotNull(outputFormatterContext.ActionContext.HttpContext.Response.Body);
+            outputFormatterContext.ActionContext.HttpContext.Response.Body.Position = 0;
+            Assert.Equal(expectedOutput,
+                new StreamReader(outputFormatterContext.ActionContext.HttpContext.Response.Body, Encoding.UTF8)
+                        .ReadToEnd());
+        }
+
+        [Fact]
+        public async Task CustomSerializerSettingsObject_TakesEffect()
+        {
+            // Arrange
+            var person = new User() { Name = "John", Age = 35 };
+            var expectedOutput = JsonConvert.SerializeObject(person, new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Formatting.Indented
+            });
+
+            var jsonFormatter = new JsonOutputFormatter();
+            jsonFormatter.SerializerSettings = new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Formatting.Indented
+            };
+
+            var outputFormatterContext = GetOutputFormatterContext(person, typeof(User));
+
+            // Act
+            await jsonFormatter.WriteResponseBodyAsync(outputFormatterContext);
+
+            // Assert
+            Assert.NotNull(outputFormatterContext.ActionContext.HttpContext.Response.Body);
+            outputFormatterContext.ActionContext.HttpContext.Response.Body.Position = 0;
+
+            var streamReader = new StreamReader(outputFormatterContext.ActionContext.HttpContext.Response.Body, Encoding.UTF8);
+            Assert.Equal(expectedOutput, streamReader.ReadToEnd());
+        }
+
+        private OutputFormatterContext GetOutputFormatterContext(object outputValue, Type outputType,
+                                            string contentType = "application/xml; charset=utf-8")
+        {
+            var mediaTypeHeaderValue = MediaTypeHeaderValue.Parse(contentType);
+
+            return new OutputFormatterContext
+            {
+                Object = outputValue,
+                DeclaredType = outputType,
+                ActionContext = GetActionContext(mediaTypeHeaderValue),
+                SelectedEncoding = Encoding.GetEncoding(mediaTypeHeaderValue.Charset)
+            };
+        }
+
+        private static ActionContext GetActionContext(MediaTypeHeaderValue contentType)
+        {
+            var request = new Mock<HttpRequest>();
+            var headers = new Mock<IHeaderDictionary>();
+            request.Setup(r => r.ContentType).Returns(contentType.RawValue);
+            request.SetupGet(r => r.Headers).Returns(headers.Object);
+            request.SetupGet(f => f.AcceptCharset).Returns(contentType.Charset);
+            var response = new Mock<HttpResponse>();
+            response.SetupGet(f => f.Body).Returns(new MemoryStream());
+            var httpContext = new Mock<HttpContext>();
+            httpContext.SetupGet(c => c.Request).Returns(request.Object);
+            httpContext.SetupGet(c => c.Response).Returns(response.Object);
+            return new ActionContext(httpContext.Object, routeData: null, actionDescriptor: null);
+        }
+
+        private sealed class User
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+        }
+    }
+}

test/Microsoft.AspNet.Mvc.FunctionalTests/JsonOutputFormatterTests.cs
@@ -1,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.TestHost;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Microsoft.AspNet.Mvc.FunctionalTests
+{
+    public class JsonOutputFormatterTests
+    {
+        private readonly IServiceProvider _provider = TestHelper.CreateServices("FormatterWebSite");
+        private readonly Action<IApplicationBuilder> _app = new FormatterWebSite.Startup().Configure;
+
+        [Fact]
+        public async Task JsonOutputFormatter_ReturnsIndentedJson()
+        {
+            // Arrange
+            var user = new FormatterWebSite.User()
+            {
+                Id = 1,
+                Alias = "john",
+                description = "Administrator",
+                Designation = "Administrator",
+                Name = "John Williams"
+            };
+
+            var serializerSettings = new JsonSerializerSettings();
+            serializerSettings.Formatting = Formatting.Indented;
+            var expectedBody = JsonConvert.SerializeObject(user, serializerSettings);
+
+            var server = TestServer.Create(_provider, _app);
+            var client = server.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("http://localhost/JsonFormatter/ReturnsIndentedJson");
+
+            // Assert
+            var actualBody = await response.Content.ReadAsStringAsync();
+            Assert.Equal(expectedBody, actualBody);
+        }
+    }
+}

test/WebSites/ConnegWebSite/Controllers/NormalController.cs
@@ -3,6 +3,8 @@
 
 using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Mvc.HeaderValueAbstractions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace ConnegWebsite
 {
@@ -15,8 +17,10 @@ namespace ConnegWebsite
             {
                 result.Formatters.Add(new PlainTextFormatter());
                 result.Formatters.Add(new CustomFormatter("application/custom"));
-                result.Formatters.Add(new JsonOutputFormatter(JsonOutputFormatter.CreateDefaultSettings(),
-                                                              indent: true));
+
+                var jsonFormatter = new JsonOutputFormatter();
+                jsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+                result.Formatters.Add(jsonFormatter);
             }
 
             base.OnActionExecuted(context);

test/WebSites/FormatterWebSite/Controllers/JsonFormattterController.cs
@@ -1,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json;
+
+namespace FormatterWebSite.Controllers
+{
+    public class JsonFormatterController : Controller
+    {
+        public IActionResult ReturnsIndentedJson()
+        {
+            var user = new User()
+            {
+                Id = 1,
+                Alias = "john",
+                description = "Administrator",
+                Designation = "Administrator",
+                Name = "John Williams"
+            };
+
+            var jsonFormatter = new JsonOutputFormatter();
+            jsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+
+            var objectResult = new ObjectResult(user);
+            objectResult.Formatters.Add(jsonFormatter);
+
+            return objectResult;
+        }
+    }
+}