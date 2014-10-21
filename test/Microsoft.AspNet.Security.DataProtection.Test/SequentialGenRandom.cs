6e557dc19321618548e6fa56965981fa66ab65d4
Levi B <levib@yahoo.com>
2014-10-21 15:30:35 -0700

    Fix ManagedAuthenticatedEncryptor not round-tripping payloads correctly.

---
+++ test/Microsoft.AspNet.Security.DataProtection.Test/SequentialGenRandom.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNet.Security.DataProtection.Cng;
+using Microsoft.AspNet.Security.DataProtection.Managed;
+
+namespace Microsoft.AspNet.Security.DataProtection.Test
+{
+    internal unsafe class SequentialGenRandom : IBCryptGenRandom, IManagedGenRandom
+    {
+        private byte _value;
+
+        public byte[] GenRandom(int numBytes)
+        {
+            byte[] bytes = new byte[numBytes];
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = _value++;
+            }
+            return bytes;
+        }
+
+        public void GenRandom(byte* pbBuffer, uint cbBuffer)
+        {
+            for (uint i = 0; i < cbBuffer; i++)
+            {
+                pbBuffer[i] = _value++;
+            }
+        }
+    }
+}
