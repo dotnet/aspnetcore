30884d8d26fc0c7918f4da6fcb3bd22f3f984e10
Ryan Nowak <nowakra@gmail.com>
2014-10-31 18:54:18 -0700

    Removing types marked as obsolete

--- src/Microsoft.AspNet.Mvc.WebApiCompatShim/ActionResults/IHttpActionResult.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
-namespace System.Web.Http
-{
-    [Obsolete("This interface is obsolete. Use Microsoft.AspNet.Mvc.IActionResult as a replacement.", error: true)]
-    public interface IHttpActionResult { }
-}
--- src/Microsoft.AspNet.Mvc.WebApiCompatShim/ActionResults/JsonResult.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
-namespace System.Web.Http
-{
-    [Obsolete("This class is obsolete. Use Microsoft.AspNet.Mvc.JsonResult as a replacement.", error: true)]
-    public class JsonResult<T> { }
-}
