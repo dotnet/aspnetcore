ardalis/Mvc

Fix to special case Dispose method to be treated as non-action
Addressing CR fedback for #1294

src/Microsoft.AspNet.Mvc.Core/ApplicationModels/DefaultControllerModelBuilder.cs
@@ -37,7 +37,7 @@ namespace Microsoft.AspNet.Mvc.ApplicationModels
 
             foreach (var methodInfo in typeInfo.AsType().GetMethods())
             {
-                var actionModels = _actionModelBuilder.BuildActionModels(methodInfo);
+                var actionModels = _actionModelBuilder.BuildActionModels(methodInfo, typeInfo);
                 if (actionModels != null)
                 {
                     foreach (var actionModel in actionModels)

src/Microsoft.AspNet.Mvc.Core/ApplicationModels/IActionModelBuilder.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 
@@ -16,11 +17,12 @@ namespace Microsoft.AspNet.Mvc.ApplicationModels
         /// <paramref name="methodInfo"/> is not an action method.
         /// </summary>
         /// <param name="methodInfo">The <see cref="MethodInfo"/>.</param>
+        /// <param name="typeInfo">The <see cref="TypeInfo"/>.</param>
         /// <returns>A set of <see cref="ActionModel"/> or null.</returns>
         /// <remarks>
         /// Instances of <see cref="ActionModel"/> returned from this interface should have their
         /// <see cref="ActionModel.Parameters"/> initialized. 
         /// </remarks>
-        IEnumerable<ActionModel> BuildActionModels([NotNull] MethodInfo methodInfo);
+        IEnumerable<ActionModel> BuildActionModels([NotNull] MethodInfo methodInfo, [NotNull] TypeInfo typeInfo);
     }
 }

dotnet/aspnetcore

src/Microsoft.AspNet.Mvc.Core/ApplicationModels/DefaultControllerModelBuilder.cs
@@ -37,7 +37,7 @@ namespace Microsoft.AspNet.Mvc.ApplicationModels
 
             foreach (var methodInfo in typeInfo.AsType().GetMethods())
             {
-                var actionModels = _actionModelBuilder.BuildActionModels(methodInfo);
+                var actionModels = _actionModelBuilder.BuildActionModels(typeInfo, methodInfo);
                 if (actionModels != null)
                 {
                     foreach (var actionModel in actionModels)

src/Microsoft.AspNet.Mvc.Core/ApplicationModels/IActionModelBuilder.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 
@@ -16,11 +17,12 @@ namespace Microsoft.AspNet.Mvc.ApplicationModels
         /// <paramref name="methodInfo"/> is not an action method.
         /// </summary>
         /// <param name="methodInfo">The <see cref="MethodInfo"/>.</param>
+        /// <param name="typeInfo">The <see cref="TypeInfo"/>.</param>
         /// <returns>A set of <see cref="ActionModel"/> or null.</returns>
         /// <remarks>
         /// Instances of <see cref="ActionModel"/> returned from this interface should have their
         /// <see cref="ActionModel.Parameters"/> initialized. 
         /// </remarks>
-        IEnumerable<ActionModel> BuildActionModels([NotNull] MethodInfo methodInfo);
+        IEnumerable<ActionModel> BuildActionModels([NotNull] TypeInfo typeInfo, [NotNull] MethodInfo methodInfo);
     }
 }