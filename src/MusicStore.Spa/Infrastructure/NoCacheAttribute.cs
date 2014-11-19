62e84ae620cca24fd9d83c494ac43c1ae578200b
DamianEdwards <damian@damianedwards.com>
2014-11-18 17:19:21 -0800

    Make MusicStore.Spa..NoCacheAttribute try *really* hard to prevent caching

--- src/MusicStore.Spa/Infrastructure/NoCacheAttribute.cs
+++ src/MusicStore.Spa/Infrastructure/NoCacheAttribute.cs
@@ -8,7 +8,9 @@ namespace MusicStore.Spa.Infrastructure
     {
         public override void OnResultExecuting(ResultExecutingContext context)
         {
-            context.HttpContext.Response.Headers["Cache-Control"] = "private, max-age=0";
+            context.HttpContext.Response.Headers["Cache-Control"] = "no-cache, no-store, max-age=0";
+            context.HttpContext.Response.Headers["Pragma"] = "no-cache";
+            context.HttpContext.Response.Headers["Expires"] = "-1";
 
             base.OnResultExecuting(context);
         }
