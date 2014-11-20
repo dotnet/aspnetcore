jorik041/Razor

Refactored error handling mechanisms for parsing.
- This is needed to make passing error handling devices into more pieces of the parser easy.

#210

src/Microsoft.AspNet.Razor/Parser/TagHelpers/TagHelperParseTreeRewriter.cs
@@ -141,7 +141,7 @@ namespace Microsoft.AspNet.Razor.Parser.TagHelpers.Internal
 
                 // We only create a single error because we can't reasonably determine other invalid tag helpers in the
                 // document; having one malformed tag helper puts the document into an invalid state.
-                context.OnError(
+                context.ErrorHandler.OnError(
                     malformedTagHelper.Start,
                     RazorResources.FormatTagHelpersParseTreeRewriter_FoundMalformedTagHelper(
                         malformedTagHelper.TagName));

test/Microsoft.AspNet.Razor.Test/Parser/Html/HtmlAttributeTest.cs
@@ -185,7 +185,7 @@ namespace Microsoft.AspNet.Razor.Test.Parser.Html
         {
             // Act
             var results = ParseDocument("<a href='~/Foo/Bar' />");
-            var rewritingContext = new RewritingContext(results.Document);
+            var rewritingContext = new RewritingContext(results.Document, new ParserErrorHandler());
             new ConditionalAttributeCollapser(new HtmlMarkupParser().BuildSpan).Rewrite(rewritingContext);
             new MarkupCollapser(new HtmlMarkupParser().BuildSpan).Rewrite(rewritingContext);
             var rewritten = rewritingContext.SyntaxTree;
@@ -272,7 +272,7 @@ namespace Microsoft.AspNet.Razor.Test.Parser.Html
 
             // Act
             var results = ParseDocument(code);
-            var rewritingContext = new RewritingContext(results.Document);
+            var rewritingContext = new RewritingContext(results.Document, new ParserErrorHandler());
             new ConditionalAttributeCollapser(new HtmlMarkupParser().BuildSpan).Rewrite(rewritingContext);
             new MarkupCollapser(new HtmlMarkupParser().BuildSpan).Rewrite(rewritingContext);
             var rewritten = rewritingContext.SyntaxTree;

test/Microsoft.AspNet.Razor.Test/Parser/WhitespaceRewriterTest.cs
@@ -32,7 +32,7 @@ namespace Microsoft.AspNet.Razor.Test.Parser
                 factory.Markup("test")
                 );
             var rewriter = new WhiteSpaceRewriter(new HtmlMarkupParser().BuildSpan);
-            var rewritingContext = new RewritingContext(start);
+            var rewritingContext = new RewritingContext(start, new ParserErrorHandler());
 
             // Act
             rewriter.Rewrite(rewritingContext);

dotnet/aspnetcore

src/Microsoft.AspNet.Razor/Parser/TagHelpers/TagHelperParseTreeRewriter.cs
@@ -141,7 +141,7 @@ namespace Microsoft.AspNet.Razor.Parser.TagHelpers.Internal
 
                 // We only create a single error because we can't reasonably determine other invalid tag helpers in the
                 // document; having one malformed tag helper puts the document into an invalid state.
-                context.OnError(
+                context.ErrorSink.OnError(
                     malformedTagHelper.Start,
                     RazorResources.FormatTagHelpersParseTreeRewriter_FoundMalformedTagHelper(
                         malformedTagHelper.TagName));

test/Microsoft.AspNet.Razor.Test/Parser/Html/HtmlAttributeTest.cs
@@ -185,7 +185,7 @@ namespace Microsoft.AspNet.Razor.Test.Parser.Html
         {
             // Act
             var results = ParseDocument("<a href='~/Foo/Bar' />");
-            var rewritingContext = new RewritingContext(results.Document);
+            var rewritingContext = new RewritingContext(results.Document, new ParserErrorSink());
             new ConditionalAttributeCollapser(new HtmlMarkupParser().BuildSpan).Rewrite(rewritingContext);
             new MarkupCollapser(new HtmlMarkupParser().BuildSpan).Rewrite(rewritingContext);
             var rewritten = rewritingContext.SyntaxTree;
@@ -272,7 +272,7 @@ namespace Microsoft.AspNet.Razor.Test.Parser.Html
 
             // Act
             var results = ParseDocument(code);
-            var rewritingContext = new RewritingContext(results.Document);
+            var rewritingContext = new RewritingContext(results.Document, new ParserErrorSink());
             new ConditionalAttributeCollapser(new HtmlMarkupParser().BuildSpan).Rewrite(rewritingContext);
             new MarkupCollapser(new HtmlMarkupParser().BuildSpan).Rewrite(rewritingContext);
             var rewritten = rewritingContext.SyntaxTree;

test/Microsoft.AspNet.Razor.Test/Parser/WhitespaceRewriterTest.cs
@@ -32,7 +32,7 @@ namespace Microsoft.AspNet.Razor.Test.Parser
                 factory.Markup("test")
                 );
             var rewriter = new WhiteSpaceRewriter(new HtmlMarkupParser().BuildSpan);
-            var rewritingContext = new RewritingContext(start);
+            var rewritingContext = new RewritingContext(start, new ParserErrorSink());
 
             // Act
             rewriter.Rewrite(rewritingContext);