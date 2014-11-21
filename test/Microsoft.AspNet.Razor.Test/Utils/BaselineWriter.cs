a477bd5cb15853acb2b168456aa32ec0b898f818
Doug Bunting <dougbu@microsoft.com>
2014-11-20 23:17:48 -0800

    Add missing license headers - #EngineeringDay - skip generated files such as Resources.Designer.cs and files under   test\Microsoft.AspNet.Razor.Test\TestFiles\CodeGenerator\CS\Output

--- test/Microsoft.AspNet.Razor.Test/Utils/BaselineWriter.cs
+++ test/Microsoft.AspNet.Razor.Test/Utils/BaselineWriter.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics;
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Diagnostics;
 using System.IO;
 
 namespace Microsoft.AspNet.Razor.Test.Utils
