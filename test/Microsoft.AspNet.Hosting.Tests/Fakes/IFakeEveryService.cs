aebfecdf87023dd1d39fe74b9bc9e3e6755884d1
Pranav K <prkrishn@hotmail.com>
2014-11-24 10:10:51 -0800

    Reacting to DI changes

--- test/Microsoft.AspNet.Hosting.Tests/Fakes/IFakeEveryService.cs
+++ test/Microsoft.AspNet.Hosting.Tests/Fakes/IFakeEveryService.cs
@@ -4,7 +4,7 @@
 namespace Microsoft.AspNet.Hosting.Fakes
 {
     interface IFakeEveryService :
-            IFakeService,
+            IFakeScopedService,
             IFakeServiceInstance,
             IFakeSingletonService
     {
