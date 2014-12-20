bangoi/Razor

Add line mappings to project TagHelper attribute values.
- We now create LineMappings for instances where a TagHelper's attribute value is not of a string type.
- This will enable the Razor editor to create projections from .cshtml => .cs for TagHelper attributes.
- Modified the TagHelperCodeGenerator and TagHelperBlockBuiler to accurately track the attribute values start locations so it could flow into code generation.
- Modified existing tests to account for the new line mappings.

#207

src/Microsoft.AspNet.Razor/Generator/Compiler/CodeBuilder/CSharp/CSharpTagHelperCodeRenderer.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using Microsoft.AspNet.Razor.TagHelpers;
+using Microsoft.AspNet.Razor.Text;
 
 namespace Microsoft.AspNet.Razor.Generator.Compiler.CSharp
 {
@@ -249,7 +250,7 @@ namespace Microsoft.AspNet.Razor.Generator.Compiler.CSharp
 
                             // We aren't a bufferable attribute which means we have no Razor code in our value.
                             // Therefore we can just use the "textValue" as the attribute value.
-                            RenderRawAttributeValue(textValue, attributeDescriptor);
+                            RenderRawAttributeValue(textValue, attributeValueChunk.Start, attributeDescriptor);
                         }
 
                         // End the assignment to the attribute.
@@ -405,13 +406,26 @@ namespace Microsoft.AspNet.Razor.Generator.Compiler.CSharp
                 });
         }
 
-        private void RenderRawAttributeValue(string value, TagHelperAttributeDescriptor attributeDescriptor)
+        private void RenderRawAttributeValue(string value,
+                                             SourceLocation documentLocation,
+                                             TagHelperAttributeDescriptor attributeDescriptor)
         {
             RenderAttributeValue(
                 attributeDescriptor,
                 valueRenderer: (writer) =>
                 {
-                    writer.Write(value);
+                    // We only want to do the work of generating a line mapping if we're in design time mode.
+                    if (_context.Host.DesignTimeMode)
+                    {
+                        using (new CSharpLineMappingWriter(_writer, documentLocation, value.Length))
+                        {
+                            writer.Write(value);
+                        }
+                    }
+                    else
+                    {
+                        writer.Write(value);
+                    }
                 });
         }
 

dotnet/aspnetcore

src/Microsoft.AspNet.Razor/Generator/Compiler/CodeBuilder/CSharp/CSharpTagHelperCodeRenderer.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using Microsoft.AspNet.Razor.TagHelpers;
+using Microsoft.AspNet.Razor.Text;
 
 namespace Microsoft.AspNet.Razor.Generator.Compiler.CSharp
 {
@@ -265,7 +266,7 @@ namespace Microsoft.AspNet.Razor.Generator.Compiler.CSharp
 
                             // We aren't a bufferable attribute which means we have no Razor code in our value.
                             // Therefore we can just use the "textValue" as the attribute value.
-                            RenderRawAttributeValue(textValue, attributeDescriptor);
+                            RenderRawAttributeValue(textValue, attributeValueChunk.Start, attributeDescriptor);
                         }
 
                         // End the assignment to the attribute.
@@ -421,13 +422,25 @@ namespace Microsoft.AspNet.Razor.Generator.Compiler.CSharp
                 });
         }
 
-        private void RenderRawAttributeValue(string value, TagHelperAttributeDescriptor attributeDescriptor)
+        private void RenderRawAttributeValue(string value,
+                                             SourceLocation documentLocation,
+                                             TagHelperAttributeDescriptor attributeDescriptor)
         {
             RenderAttributeValue(
                 attributeDescriptor,
                 valueRenderer: (writer) =>
                 {
-                    writer.Write(value);
+                    if (_context.Host.DesignTimeMode)
+                    {
+                        using (new CSharpLineMappingWriter(writer, documentLocation, value.Length))
+                        {
+                            writer.Write(value);
+                        }
+                    }
+                    else
+                    {
+                        writer.Write(value);
+                    }
                 });
         }
 