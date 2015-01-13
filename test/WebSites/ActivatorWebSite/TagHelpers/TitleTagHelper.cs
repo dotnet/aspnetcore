62b6ab87216be0f942bb9b1ac208cc4ad23edcd7
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2015-01-12 16:48:35 -0800

    Added missed license header and removed unused file

--- test/WebSites/ActivatorWebSite/TagHelpers/TitleTagHelper.cs
+++ test/WebSites/ActivatorWebSite/TagHelpers/TitleTagHelper.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.AspNet.Razor.Runtime.TagHelpers;
 using Microsoft.AspNet.Razor.TagHelpers;
