provenstyle/Mvc

[Fixes #1791]Remove XML formatter from defaults

test/Microsoft.AspNet.Mvc.Core.Test/ActionResults/CreatedAtRouteResultTests.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Http;
 using Microsoft.AspNet.PipelineCore.Collections;
 using Microsoft.AspNet.Routing;
 using Microsoft.AspNet.Testing;
+using Microsoft.Framework.OptionsModel;
 using Moq;
 using Xunit;
 
@@ -103,10 +104,16 @@ namespace Microsoft.AspNet.Mvc
         {
             var httpContext = new Mock<HttpContext>();
 
+            var mockOptions = new Mock<IOptions<MvcOptions>>();
+            mockOptions.SetupGet(o => o.Options)
+                       .Returns(new MvcOptions());
+            
             httpContext.Setup(o => o.Response)
                        .Returns(response);
             httpContext.Setup(o => o.RequestServices.GetService(typeof(IOutputFormattersProvider)))
                        .Returns(new TestOutputFormatterProvider());
+            httpContext.Setup(o => o.RequestServices.GetService(typeof(IOptions<MvcOptions>)))
+                       .Returns(mockOptions.Object);
             httpContext.Setup(o => o.Request.PathBase)
                        .Returns(new PathString(""));
 

test/Microsoft.AspNet.Mvc.Core.Test/JsonResultTest.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Mvc.HeaderValueAbstractions;
 using Microsoft.AspNet.PipelineCore;
 using Microsoft.AspNet.Routing;
+using Microsoft.Framework.OptionsModel;
 using Moq;
 using Xunit;
 
@@ -191,6 +192,12 @@ namespace Microsoft.AspNet.Mvc
             services
                 .Setup(s => s.GetService(typeof(IOutputFormattersProvider)))
                 .Returns(mockFormattersProvider.Object);
+
+            var mockOptions = new Mock<IOptions<MvcOptions>>();
+            mockOptions.SetupGet(o => o.Options)
+                       .Returns(new MvcOptions());
+            services.Setup(s => s.GetService(typeof(IOptions<MvcOptions>)))
+                       .Returns(mockOptions.Object);
 
             // This is the ultimate fallback, it will be used if none of the formatters from options
             // work.

test/Microsoft.AspNet.Mvc.Test/MvcOptionSetupTest.cs
@@ -82,11 +82,10 @@ namespace Microsoft.AspNet.Mvc
             setup.Configure(mvcOptions);
 
             // Assert
-            Assert.Equal(4, mvcOptions.OutputFormatters.Count);
+            Assert.Equal(3, mvcOptions.OutputFormatters.Count);
             Assert.IsType<HttpNoContentOutputFormatter>(mvcOptions.OutputFormatters[0].Instance);
             Assert.IsType<TextPlainFormatter>(mvcOptions.OutputFormatters[1].Instance);
             Assert.IsType<JsonOutputFormatter>(mvcOptions.OutputFormatters[2].Instance);
-            Assert.IsType<XmlDataContractSerializerOutputFormatter>(mvcOptions.OutputFormatters[3].Instance);
         }
 
         [Fact]
@@ -100,9 +99,8 @@ namespace Microsoft.AspNet.Mvc
             setup.Configure(mvcOptions);
 
             // Assert
-            Assert.Equal(2, mvcOptions.InputFormatters.Count);
+            Assert.Equal(1, mvcOptions.InputFormatters.Count);
             Assert.IsType<JsonInputFormatter>(mvcOptions.InputFormatters[0].Instance);
-            Assert.IsType<XmlDataContractSerializerInputFormatter>(mvcOptions.InputFormatters[1].Instance);
         }
 
         [Fact]
@@ -119,6 +117,20 @@ namespace Microsoft.AspNet.Mvc
             Assert.Equal(2, mvcOptions.ModelValidatorProviders.Count);
             Assert.IsType<DataAnnotationsModelValidatorProvider>(mvcOptions.ModelValidatorProviders[0].Instance);
             Assert.IsType<DataMemberModelValidatorProvider>(mvcOptions.ModelValidatorProviders[1].Instance);
+        }
+
+        [Fact]
+        public void Setup_IgnoresAcceptHeaderHavingWildCardMediaAndSubMediaTypes()
+        {
+            // Arrange
+            var mvcOptions = new MvcOptions();
+            var setup = new MvcOptionsSetup();
+
+            // Act
+            setup.Configure(mvcOptions);
+
+            // Assert
+            Assert.True(mvcOptions.IgnoreBrowserAcceptHeader);
         }
 
         [Fact]

dotnet/aspnetcore

test/Microsoft.AspNet.Mvc.Core.Test/ActionResults/CreatedAtRouteResultTests.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Http.Core;
 using Microsoft.AspNet.Routing;
 using Microsoft.AspNet.Testing;
+using Microsoft.Framework.OptionsModel;
 using Moq;
 using Xunit;
 
@@ -97,6 +98,11 @@ namespace Microsoft.AspNet.Mvc
 
             httpContext.Setup(o => o.Request)
                        .Returns(request);
+            var options = new Mock<IOptions<MvcOptions>>();
+            options.SetupGet(o => o.Options)
+                       .Returns(new MvcOptions());
+            httpContext.Setup(o => o.RequestServices.GetService(typeof(IOptions<MvcOptions>)))
+                       .Returns(options.Object);
             httpContext.Setup(o => o.Response)
                        .Returns(response);
             httpContext.Setup(o => o.RequestServices.GetService(typeof(IOutputFormattersProvider)))

test/Microsoft.AspNet.Mvc.Core.Test/JsonResultTest.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Http.Core;
 using Microsoft.AspNet.Routing;
+using Microsoft.Framework.OptionsModel;
 using Microsoft.Net.Http.Headers;
 using Moq;
 using Xunit;
@@ -191,6 +192,12 @@ namespace Microsoft.AspNet.Mvc
             services
                 .Setup(s => s.GetService(typeof(IOutputFormattersProvider)))
                 .Returns(mockFormattersProvider.Object);
+
+            var options = new Mock<IOptions<MvcOptions>>();
+            options.SetupGet(o => o.Options)
+                       .Returns(new MvcOptions());
+            services.Setup(s => s.GetService(typeof(IOptions<MvcOptions>)))
+                       .Returns(options.Object);
 
             // This is the ultimate fallback, it will be used if none of the formatters from options
             // work.

test/Microsoft.AspNet.Mvc.Test/MvcOptionSetupTest.cs
@@ -83,11 +83,10 @@ namespace Microsoft.AspNet.Mvc
             setup.Configure(mvcOptions);
 
             // Assert
-            Assert.Equal(4, mvcOptions.OutputFormatters.Count);
+            Assert.Equal(3, mvcOptions.OutputFormatters.Count);
             Assert.IsType<HttpNoContentOutputFormatter>(mvcOptions.OutputFormatters[0].Instance);
             Assert.IsType<TextPlainFormatter>(mvcOptions.OutputFormatters[1].Instance);
             Assert.IsType<JsonOutputFormatter>(mvcOptions.OutputFormatters[2].Instance);
-            Assert.IsType<XmlDataContractSerializerOutputFormatter>(mvcOptions.OutputFormatters[3].Instance);
         }
 
         [Fact]
@@ -101,9 +100,8 @@ namespace Microsoft.AspNet.Mvc
             setup.Configure(mvcOptions);
 
             // Assert
-            Assert.Equal(2, mvcOptions.InputFormatters.Count);
+            Assert.Equal(1, mvcOptions.InputFormatters.Count);
             Assert.IsType<JsonInputFormatter>(mvcOptions.InputFormatters[0].Instance);
-            Assert.IsType<XmlDataContractSerializerInputFormatter>(mvcOptions.InputFormatters[1].Instance);
         }
 
         [Fact]
@@ -120,6 +118,20 @@ namespace Microsoft.AspNet.Mvc
             Assert.Equal(2, mvcOptions.ModelValidatorProviders.Count);
             Assert.IsType<DataAnnotationsModelValidatorProvider>(mvcOptions.ModelValidatorProviders[0].Instance);
             Assert.IsType<DataMemberModelValidatorProvider>(mvcOptions.ModelValidatorProviders[1].Instance);
+        }
+
+        [Fact]
+        public void Setup_IgnoresAcceptHeaderHavingWildCardMediaAndSubMediaTypes()
+        {
+            // Arrange
+            var mvcOptions = new MvcOptions();
+            var setup = new MvcOptionsSetup();
+
+            // Act
+            setup.Configure(mvcOptions);
+
+            // Assert
+            Assert.False(mvcOptions.RespectBrowserAcceptHeader);
         }
 
         [Fact]