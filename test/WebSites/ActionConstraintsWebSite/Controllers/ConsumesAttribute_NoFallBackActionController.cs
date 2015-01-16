shawnwildermuth/Mvc

Adding Support for consumes. Consumes has overriding behavior and the one closest to action wins.

test/WebSites/ActionConstraintsWebSite/Controllers/ConsumesAttribute_NoFallBackActionController.cs
@@ -1,0 +1,23 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc;
+
+namespace ActionConstraintsWebSite
+{
+    [Route("ConsumesAttribute_AmbiguousActions/[action]")]
+    public class ConsumesAttribute_NoFallBackActionController : Controller
+    {
+        [Consumes("application/json", "text/json")]
+        public Dummy CreateDummy([FromBody] DummyClass_Json jsonInput)
+        {
+            return jsonInput;
+        }
+
+        [Consumes("application/xml")]
+        public Dummy CreateDummy([FromBody] DummyClass_Xml xmlInput)
+        {
+            return xmlInput;
+        }
+    }
+}

test/WebSites/ActionConstraintsWebSite/Controllers/ConsumesAttribute_OveridesController.cs
@@ -1,0 +1,18 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc;
+
+namespace ActionConstraintsWebSite
+{
+    [Consumes("application/xml")]
+    public class ConsumesAttribute_OverridesController : ConsumesAttribute_OverridesBaseController
+    {
+        public override IActionResult CreateDummy([FromBody] Dummy dummy)
+        {
+            // should be picked if request content type is text/json.
+            dummy.SampleString = "ConsumesAttribute_OverridesController_application/xml";
+            return new JsonResult(dummy);
+        }
+    }
+}

test/WebSites/ActionConstraintsWebSite/Controllers/ConsumesAttribute_WithFallbackActionController.cs
@@ -1,0 +1,29 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNet.Mvc;
+
+namespace ActionConstraintsWebSite
+{
+    [Route("ConsumesAttribute_Company/[action]")]
+    public class ConsumesAttribute_WithFallbackActionController : Controller
+    {
+        [Consumes("application/json")]
+        public Dummy CreateDummy([FromBody] DummyClass_Json jsonInput)
+        {
+            return jsonInput;
+        }
+
+        [Consumes("application/xml")]
+        public Dummy CreateDummy([FromBody] DummyClass_Xml xmlInput)
+        {
+            return xmlInput;
+        }
+
+        public Dummy CreateDummy([FromBody] DummyClass_Text defaultInput)
+        {
+            return defaultInput;
+        }
+    }
+}

dotnet/aspnetcore

test/WebSites/ActionConstraintsWebSite/Controllers/ConsumesAttribute_NoFallBackActionController.cs
@@ -1,0 +1,23 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc;
+
+namespace ActionConstraintsWebSite
+{
+    [Route("ConsumesAttribute_AmbiguousActions/[action]")]
+    public class ConsumesAttribute_NoFallBackActionController : Controller
+    {
+        [Consumes("application/json", "text/json")]
+        public Product CreateProduct([FromBody] Product_Json jsonInput)
+        {
+            return jsonInput;
+        }
+
+        [Consumes("application/xml")]
+        public Product CreateProduct([FromBody] Product_Xml xmlInput)
+        {
+            return xmlInput;
+        }
+    }
+}

test/WebSites/ActionConstraintsWebSite/Controllers/ConsumesAttribute_OveridesController.cs
@@ -1,0 +1,18 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc;
+
+namespace ActionConstraintsWebSite
+{
+    [Consumes("application/xml")]
+    public class ConsumesAttribute_OverridesController : ConsumesAttribute_OverridesBaseController
+    {
+        public override IActionResult CreateProduct([FromBody] Product product)
+        {
+            // should be picked if request content type is text/json.
+            product.SampleString = "ConsumesAttribute_OverridesController_application/xml";
+            return new JsonResult(product);
+        }
+    }
+}

test/WebSites/ActionConstraintsWebSite/Controllers/ConsumesAttribute_WithFallbackActionController.cs
@@ -1,0 +1,29 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNet.Mvc;
+
+namespace ActionConstraintsWebSite
+{
+    [Route("ConsumesAttribute_Company/[action]")]
+    public class ConsumesAttribute_WithFallbackActionController : Controller
+    {
+        [Consumes("application/json")]
+        public Product CreateProduct([FromBody] Product_Json jsonInput)
+        {
+            return jsonInput;
+        }
+
+        [Consumes("application/xml")]
+        public Product CreateProduct([FromBody] Product_Xml xmlInput)
+        {
+            return xmlInput;
+        }
+
+        public Product CreateProduct([FromBody] Product_Text defaultInput)
+        {
+            return defaultInput;
+        }
+    }
+}