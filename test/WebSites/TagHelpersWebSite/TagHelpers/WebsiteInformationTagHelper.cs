bbqchickenrobot/Mvc

Modify TagHelper tests to abide by new content mode design.
- React to aspnet/Razor#221
- Modified existing TagHelper tests to no longer rely on ContentBehavior.
- Updated signatures of TagHelperExecutionContext and TagHelperContext pieces.

test/WebSites/TagHelpersWebSite/TagHelpers/WebsiteInformationTagHelper.cs
@@ -8,7 +8,6 @@ using TagHelpersWebSite.Models;
 
 namespace TagHelpersWebSite.TagHelpers
 {
-    [ContentBehavior(ContentBehavior.Append)]
     public class WebsiteInformationTagHelper : TagHelper
     {
         public WebsiteContext Info { get; set; }
@@ -16,7 +15,7 @@ namespace TagHelpersWebSite.TagHelpers
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "section";
-            output.Content = string.Format(
+            output.PreContent = string.Format(
                 "<p><strong>Version:</strong> {0}</p>" + Environment.NewLine +
                 "<p><strong>Copyright Year:</strong> {1}</p>" + Environment.NewLine +
                 "<p><strong>Approved:</strong> {2}</p>" + Environment.NewLine +

dotnet/aspnetcore

test/WebSites/TagHelpersWebSite/TagHelpers/WebsiteInformationTagHelper.cs
@@ -3,12 +3,10 @@
 
 using System;
 using Microsoft.AspNet.Razor.Runtime.TagHelpers;
-using Microsoft.AspNet.Razor.TagHelpers;
 using TagHelpersWebSite.Models;
 
 namespace TagHelpersWebSite.TagHelpers
 {
-    [ContentBehavior(ContentBehavior.Append)]
     public class WebsiteInformationTagHelper : TagHelper
     {
         public WebsiteContext Info { get; set; }
@@ -16,7 +14,7 @@ namespace TagHelpersWebSite.TagHelpers
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "section";
-            output.Content = string.Format(
+            output.PostContent = string.Format(
                 "<p><strong>Version:</strong> {0}</p>" + Environment.NewLine +
                 "<p><strong>Copyright Year:</strong> {1}</p>" + Environment.NewLine +
                 "<p><strong>Approved:</strong> {2}</p>" + Environment.NewLine +