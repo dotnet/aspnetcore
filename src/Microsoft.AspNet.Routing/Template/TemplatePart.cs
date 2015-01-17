aspnet/Routing

Basic URL Extension functionality working. 1. Template parser now allows a parameter to be an optional parameter in a complex segment if    it is the last and only optional parameter and it is followed by a period. 2. Template matcher modified to take into consideration the optional parameter in the complex    segment. Also the period shouldn't be present if the optional parameter is not present

src/Microsoft.AspNet.Routing/Template/TemplatePart.cs
@@ -40,6 +40,7 @@ namespace Microsoft.AspNet.Routing.Template
         public bool IsLiteral { get; private set; }
         public bool IsParameter { get; private set; }
         public bool IsOptional { get; private set; }
+        public bool IsFollowingAPeriod { get; set; }
         public string Name { get; private set; }
         public string Text { get; private set; }
         public object DefaultValue { get; private set; }

dotnet/aspnetcore

src/Microsoft.AspNet.Routing/Template/TemplatePart.cs
@@ -40,6 +40,7 @@ namespace Microsoft.AspNet.Routing.Template
         public bool IsLiteral { get; private set; }
         public bool IsParameter { get; private set; }
         public bool IsOptional { get; private set; }
+        public bool IsOptionalSeperator { get; set; }
         public string Name { get; private set; }
         public string Text { get; private set; }
         public object DefaultValue { get; private set; }