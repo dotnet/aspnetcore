93c5b0f2c8fe7f5a14eab7475b916813271f453f
Wei Wang <chengtian.wang@gmail.com>
2015-01-20 18:19:02 -0800

    Rename SKIP_KRE_INSTALL to SKIP_DOTNET_INSTALL

--- build.cmd
+++ build.cmd
@@ -19,7 +19,7 @@ IF EXIST packages\KoreBuild goto run
 .nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
 .nuget\NuGet.exe install Sake -version 0.2 -o packages -ExcludeVersion
 
-IF "%SKIP_KRE_INSTALL%"=="1" goto run
+IF "%SKIP_DOTNET_INSTALL%"=="1" goto run
 CALL packages\KoreBuild\build\dotnetsdk upgrade -runtime CLR -x86
 CALL packages\KoreBuild\build\dotnetsdk install default -runtime CoreCLR -x86
 
