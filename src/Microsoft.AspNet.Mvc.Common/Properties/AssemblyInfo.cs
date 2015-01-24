074eae6059e44b5382ca49adcfdd9a491b82341b
N. Taylor Mullen <nimullen@microsoft.com>
2015-01-23 16:29:31 -0800

    Fixed Microsoft.AspNet.Mvc.Common unneeded assemblyinfo.

--- src/Microsoft.AspNet.Mvc.Common/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Microsoft.AspNet.Mvc.Common.Test")]
-[assembly: InternalsVisibleTo("Microsoft.AspNet.Mvc.Razor.Host")]
