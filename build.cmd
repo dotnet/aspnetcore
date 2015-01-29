9800bc82bd2b366d4120c8064ac381b9ac24116e
Wei Wang <chengtian.wang@gmail.com>
2015-01-28 18:21:41 -0800

    Change SKIP_DOTNET_INSTALL to SKIP_KRE_INSTALL

--- build.cmd
+++ build.cmd
@@ -19,7 +19,7 @@ IF EXIST packages\KoreBuild goto run
 .nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
 .nuget\NuGet.exe install Sake -version 0.2 -o packages -ExcludeVersion
 
-IF "%SKIP_DOTNET_INSTALL%"=="1" goto run
+IF "%SKIP_KRE_INSTALL%"=="1" goto run
 CALL packages\KoreBuild\build\kvm upgrade -x64
 CALL packages\KoreBuild\build\kvm install default -runtime CoreCLR -x64
 CALL packages\KoreBuild\build\kvm install default -x86
