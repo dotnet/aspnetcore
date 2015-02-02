e22b86a28e721e4de6e46597ec41d125ba7d012c
anurse <andrew@andrewnurse.net>
2015-02-02 14:27:33 -0800

    added message with source url

--- kvminstall.ps1
+++ kvminstall.ps1
@@ -8,11 +8,13 @@ $kvmCmdPath = Join-Path $tempPath "kvm.cmd"
 Write-Host "Using temporary directory: $tempPath"
 if (!(Test-Path $tempPath)) { md $tempPath | Out-Null }
 
+$BaseUrl = "https://raw.githubusercontent.com/$Repository/$Branch"
+Write-Host "Downloading from $BaseUrl"
 
 $webClient = New-Object System.Net.WebClient
 Write-Host "Downloading KVM.ps1 to $kvmPs1Path"
-$webClient.DownloadFile("https://raw.githubusercontent.com/$Repository/$Branch/kvm.ps1", $kvmPs1Path)
+$webClient.DownloadFile("$BaseUrl/kvm.ps1", $kvmPs1Path)
 Write-Host "Downloading KVM.cmd to $kvmCmdPath"
-$webClient.DownloadFile("https://raw.githubusercontent.com/$Repository/$Branch/kvm.cmd", $kvmCmdPath)
+$webClient.DownloadFile("$BaseUrl/kvm.cmd", $kvmCmdPath)
 Write-Host "Installing KVM"
 & $kvmCmdPath setup
