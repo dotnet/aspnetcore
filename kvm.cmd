bec5f95a22ca199da7d3d5226d4b126d0f24d4ae
anurse <andrew@andrewnurse.net>
2015-02-03 14:56:29 -0800

    :arrow_up: kvm.sh/ps1/cmd

--- kvm.cmd
+++ kvm.cmd
@@ -2,7 +2,7 @@
 
 PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0kvm.ps1' %*"
 
-IF EXIST "%USERPROFILE%\.kre\run-once.cmd" (
-  CALL "%USERPROFILE%\.kre\run-once.cmd"
-  DEL "%USERPROFILE%\.kre\run-once.cmd"
+IF EXIST "%USERPROFILE%\.k\temp-set-envvars.cmd" (
+  CALL "%USERPROFILE%\.k\temp-set-envvars.cmd"
+  DEL "%USERPROFILE%\.k\temp-set-envvars.cmd"
 )
