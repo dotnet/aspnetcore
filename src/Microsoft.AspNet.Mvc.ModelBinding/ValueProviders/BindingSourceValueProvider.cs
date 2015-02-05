liuxiqin/Mvc

Make BindingSource extensible
This is a major refactor of how IBinderMetadata interacts with model
binders and value providers. We're doing this to support better
extensibility for metadata in ApiExplorer.

You'll notice a bunch of deleted code in DefaultApiDescriptionProvider
that maps metadata marker interfaces to a fixed list of Api sources. This
is replaced now with IBindingSourceMetadata - which also replaces the
hierarchy of marker interfaces. Now user code can create an arbitrary
binding source and have a consistent API for model-binders,
value-providers and full-visibility in ApiExplorer as
well.

Additonally, there's some error checking in place that better enforces the
constraints we already have in the system. IE you can't create a 'greedy'
model binder that uses value-provider data.

Two additional enhancements are planned for followup PRs:
1. Add a BindingSource property to model-metadata. This will remove some
duplication, but I want to delay it because it would touch another 10 or
so files.

2. Add an extensibility interface for our 'special' model binders like the
file binder so these can show up in ApiExplorer as well.

src/Microsoft.AspNet.Mvc.ModelBinding/ValueProviders/BindingSourceValueProvider.cs
@@ -1,0 +1,77 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.Mvc.ModelBinding
+{
+
+    /// <summary>
+    /// A value provider which provides data from a specific <see cref="BindingSource"/>.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// A <see cref="BindingSourceValueProvider"/> is an <see cref="IValueProvider"/> base-implementation which
+    /// can provide data for all parameters and model properties which specify the corresponding
+    /// <see cref="ModelBinding.BindingSource"/>.
+    /// </para>
+    /// <para>
+    /// <see cref="BindingSourceValueProvider"/> implements <see cref="IBindingSourceValueProvider"/> and will
+    /// include or exclude itself from the set of value providers based on the model's associated
+    /// <see cref="ModelBinding.BindingSource"/>. Value providers are by-default included; if a model does not
+    /// specify a <see cref="ModelBinding.BindingSource"/> then all value providers are valid.
+    /// </para>
+    /// </remarks>
+    public abstract class BindingSourceValueProvider : IBindingSourceValueProvider
+    {
+        /// <summary>
+        /// Creates a new <see cref="BindingSourceValueProvider"/>.
+        /// </summary>
+        /// <param name="bindingSource">
+        /// The <see cref="ModelBinding.BindingSource"/>. Must be a single-source (non-composite) with
+        /// <see cref="ModelBinding.BindingSource.IsValueProvider"/> equal to <c>true</c>.
+        /// </param>
+        public BindingSourceValueProvider([NotNull] BindingSource bindingSource)
+        {
+            if (!bindingSource.IsValueProvider)
+            {
+                var message = Resources.FormatBindingSource_MustBeValueProvider(
+                    bindingSource.DisplayName,
+                    nameof(BindingSourceValueProvider));
+                throw new ArgumentException(message, nameof(bindingSource));
+            }
+
+            if (bindingSource is CompositeBindingSource)
+            {
+                var message = Resources.FormatBindingSource_CannotBeComposite(
+                    bindingSource.DisplayName,
+                    nameof(BindingSourceValueProvider));
+                throw new ArgumentException(message, nameof(bindingSource));
+            }
+
+            BindingSource = bindingSource;
+        }
+
+        protected BindingSource BindingSource { get; }
+
+        /// <inheritdoc />
+        public abstract Task<bool> ContainsPrefixAsync(string prefix);
+
+        /// <inheritdoc />
+        public abstract Task<ValueProviderResult> GetValueAsync(string key);
+
+        /// <inheritdoc />
+        public virtual IValueProvider Filter(BindingSource bindingSource)
+        {
+            if (bindingSource.CanAcceptDataFrom(BindingSource))
+            {
+                return this;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}

test/Microsoft.AspNet.Mvc.ModelBinding.Test/ValueProviders/CompositeValueProviderTests.cs
@@ -13,21 +13,12 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
 {
     public class CompositeValueProviderTests
     {
-        public static IEnumerable<object[]> RegisteredAsMetadataClasses
-        {
-            get
-            {
-                yield return new object[] { new TestValueProviderMetadata() };
-                yield return new object[] { new DerivedValueBinderMetadata() };
-            }
-        }
-
         [Fact]
         public async Task GetKeysFromPrefixAsync_ReturnsResultFromFirstValueProviderThatReturnsValues()
         {
             // Arrange
             var provider1 = Mock.Of<IValueProvider>();
-            var dictionary =  new Dictionary<string, string>(StringComparer.Ordinal)
+            var dictionary = new Dictionary<string, string>(StringComparer.Ordinal)
             {
                 { "prefix-test", "some-value" },
             };
@@ -62,19 +53,29 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
             Assert.Empty(values);
         }
 
+        public static IEnumerable<object[]> BinderMetadata
+        {
+            get
+            {
+                yield return new object[] { new TestValueProviderMetadata() };
+                yield return new object[] { new DerivedValueProviderMetadata() };
+            }
+        }
+
         [Theory]
-        [MemberData(nameof(RegisteredAsMetadataClasses))]
-        public void FilterReturnsItself_ForAnyClassRegisteredAsGenericParam(IValueProviderMetadata metadata)
+        [MemberData(nameof(BinderMetadata))]
+        public void FilterReturnsItself_ForAnyClassRegisteredAsGenericParam(IBindingSourceMetadata metadata)
         {
             // Arrange
             var values = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
-            var unrelatedMetadata = new UnrelatedValueBinderMetadata();
-            var valueProvider1 = GetMockValueProvider(metadata);
-            var valueProvider2 = GetMockValueProvider(unrelatedMetadata);
+
+            var valueProvider1 = GetMockValueProvider("Test");
+            var valueProvider2 = GetMockValueProvider("Unrelated");
+
             var provider = new CompositeValueProvider(new List<IValueProvider>() { valueProvider1.Object, valueProvider2.Object });
 
             // Act
-            var result = provider.Filter(metadata);
+            var result = provider.Filter(metadata.BindingSource);
 
             // Assert
             var valueProvider = Assert.IsType<CompositeValueProvider>(result);
@@ -84,23 +85,45 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
             Assert.Same(valueProvider1.Object, filteredProvider);
         }
 
-        private Mock<IMetadataAwareValueProvider> GetMockValueProvider(IValueProviderMetadata metadata)
+        private Mock<IBindingSourceValueProvider> GetMockValueProvider(string bindingSourceId)
         {
-            var valueProvider = new Mock<IMetadataAwareValueProvider>();
-            valueProvider.Setup(o => o.Filter(metadata))
-                         .Returns(valueProvider.Object);
+            var valueProvider = new Mock<IBindingSourceValueProvider>(MockBehavior.Strict);
+
+            valueProvider
+                .Setup(o => o.Filter(It.Is<BindingSource>(s => s.Id == bindingSourceId)))
+                .Returns(valueProvider.Object);
+
+            valueProvider
+                .Setup(o => o.Filter(It.Is<BindingSource>(s => s.Id != bindingSourceId)))
+                .Returns((IBindingSourceValueProvider)null);
+
             return valueProvider;
         }
-        private class TestValueProviderMetadata : IValueProviderMetadata
+
+        private class TestValueProviderMetadata : IBindingSourceMetadata
+        {
+            public BindingSource BindingSource
+            {
+                get
+                {
+                    return new BindingSource("Test", displayName: null, isValueProvider: true, isUserInput: true);
+                }
+            }
+        }
+
+        private class DerivedValueProviderMetadata : TestValueProviderMetadata
         {
         }
 
-        private class DerivedValueBinderMetadata : TestValueProviderMetadata
+        private class UnrelatedValueBinderMetadata : IBindingSourceMetadata
         {
-        }
-
-        private class UnrelatedValueBinderMetadata : IValueProviderMetadata
-        {
+            public BindingSource BindingSource
+            {
+                get
+                {
+                    return new BindingSource("Unrelated", displayName: null, isValueProvider: true, isUserInput: true);
+                }
+            }
         }
     }
 }

dotnet/aspnetcore

src/Microsoft.AspNet.Mvc.ModelBinding/ValueProviders/BindingSourceValueProvider.cs
@@ -1,0 +1,79 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.Mvc.ModelBinding
+{
+    /// <summary>
+    /// A value provider which provides data from a specific <see cref="BindingSource"/>.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// A <see cref="BindingSourceValueProvider"/> is an <see cref="IValueProvider"/> base-implementation which
+    /// can provide data for all parameters and model properties which specify the corresponding
+    /// <see cref="ModelBinding.BindingSource"/>.
+    /// </para>
+    /// <para>
+    /// <see cref="BindingSourceValueProvider"/> implements <see cref="IBindingSourceValueProvider"/> and will
+    /// include or exclude itself from the set of value providers based on the model's associated
+    /// <see cref="ModelBinding.BindingSource"/>. Value providers are by-default included; if a model does not
+    /// specify a <see cref="ModelBinding.BindingSource"/> then all value providers are valid.
+    /// </para>
+    /// </remarks>
+    public abstract class BindingSourceValueProvider : IBindingSourceValueProvider
+    {
+        /// <summary>
+        /// Creates a new <see cref="BindingSourceValueProvider"/>.
+        /// </summary>
+        /// <param name="bindingSource">
+        /// The <see cref="ModelBinding.BindingSource"/>. Must be a single-source (non-composite) with
+        /// <see cref="ModelBinding.BindingSource.IsGreedy"/> equal to <c>false</c>.
+        /// </param>
+        public BindingSourceValueProvider([NotNull] BindingSource bindingSource)
+        {
+            if (bindingSource.IsGreedy)
+            {
+                var message = Resources.FormatBindingSource_CannotBeGreedy(
+                    bindingSource.DisplayName,
+                    nameof(BindingSourceValueProvider));
+                throw new ArgumentException(message, nameof(bindingSource));
+            }
+
+            if (bindingSource is CompositeBindingSource)
+            {
+                var message = Resources.FormatBindingSource_CannotBeComposite(
+                    bindingSource.DisplayName,
+                    nameof(BindingSourceValueProvider));
+                throw new ArgumentException(message, nameof(bindingSource));
+            }
+
+            BindingSource = bindingSource;
+        }
+
+        /// <summary>
+        /// Gets the corresponding <see cref="ModelBinding.BindingSource"/>.
+        /// </summary>
+        protected BindingSource BindingSource { get; }
+
+        /// <inheritdoc />
+        public abstract Task<bool> ContainsPrefixAsync(string prefix);
+
+        /// <inheritdoc />
+        public abstract Task<ValueProviderResult> GetValueAsync(string key);
+
+        /// <inheritdoc />
+        public virtual IValueProvider Filter(BindingSource bindingSource)
+        {
+            if (bindingSource.CanAcceptDataFrom(BindingSource))
+            {
+                return this;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}

test/Microsoft.AspNet.Mvc.ModelBinding.Test/ValueProviders/CompositeValueProviderTests.cs
@@ -13,21 +13,12 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
 {
     public class CompositeValueProviderTests
     {
-        public static IEnumerable<object[]> RegisteredAsMetadataClasses
-        {
-            get
-            {
-                yield return new object[] { new TestValueProviderMetadata() };
-                yield return new object[] { new DerivedValueBinderMetadata() };
-            }
-        }
-
         [Fact]
         public async Task GetKeysFromPrefixAsync_ReturnsResultFromFirstValueProviderThatReturnsValues()
         {
             // Arrange
             var provider1 = Mock.Of<IValueProvider>();
-            var dictionary =  new Dictionary<string, string>(StringComparer.Ordinal)
+            var dictionary = new Dictionary<string, string>(StringComparer.Ordinal)
             {
                 { "prefix-test", "some-value" },
             };
@@ -62,19 +53,29 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
             Assert.Empty(values);
         }
 
+        public static IEnumerable<object[]> BinderMetadata
+        {
+            get
+            {
+                yield return new object[] { new TestValueProviderMetadata() };
+                yield return new object[] { new DerivedValueProviderMetadata() };
+            }
+        }
+
         [Theory]
-        [MemberData(nameof(RegisteredAsMetadataClasses))]
-        public void FilterReturnsItself_ForAnyClassRegisteredAsGenericParam(IValueProviderMetadata metadata)
+        [MemberData(nameof(BinderMetadata))]
+        public void FilterReturnsItself_ForAnyClassRegisteredAsGenericParam(IBindingSourceMetadata metadata)
         {
             // Arrange
             var values = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
-            var unrelatedMetadata = new UnrelatedValueBinderMetadata();
-            var valueProvider1 = GetMockValueProvider(metadata);
-            var valueProvider2 = GetMockValueProvider(unrelatedMetadata);
+
+            var valueProvider1 = GetMockValueProvider("Test");
+            var valueProvider2 = GetMockValueProvider("Unrelated");
+
             var provider = new CompositeValueProvider(new List<IValueProvider>() { valueProvider1.Object, valueProvider2.Object });
 
             // Act
-            var result = provider.Filter(metadata);
+            var result = provider.Filter(metadata.BindingSource);
 
             // Assert
             var valueProvider = Assert.IsType<CompositeValueProvider>(result);
@@ -84,23 +85,45 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
             Assert.Same(valueProvider1.Object, filteredProvider);
         }
 
-        private Mock<IMetadataAwareValueProvider> GetMockValueProvider(IValueProviderMetadata metadata)
+        private Mock<IBindingSourceValueProvider> GetMockValueProvider(string bindingSourceId)
         {
-            var valueProvider = new Mock<IMetadataAwareValueProvider>();
-            valueProvider.Setup(o => o.Filter(metadata))
-                         .Returns(valueProvider.Object);
+            var valueProvider = new Mock<IBindingSourceValueProvider>(MockBehavior.Strict);
+
+            valueProvider
+                .Setup(o => o.Filter(It.Is<BindingSource>(s => s.Id == bindingSourceId)))
+                .Returns(valueProvider.Object);
+
+            valueProvider
+                .Setup(o => o.Filter(It.Is<BindingSource>(s => s.Id != bindingSourceId)))
+                .Returns((IBindingSourceValueProvider)null);
+
             return valueProvider;
         }
-        private class TestValueProviderMetadata : IValueProviderMetadata
+
+        private class TestValueProviderMetadata : IBindingSourceMetadata
+        {
+            public BindingSource BindingSource
+            {
+                get
+                {
+                    return new BindingSource("Test", displayName: null, isGreedy: true, isFromRequest: true);
+                }
+            }
+        }
+
+        private class DerivedValueProviderMetadata : TestValueProviderMetadata
         {
         }
 
-        private class DerivedValueBinderMetadata : TestValueProviderMetadata
+        private class UnrelatedValueBinderMetadata : IBindingSourceMetadata
         {
-        }
-
-        private class UnrelatedValueBinderMetadata : IValueProviderMetadata
-        {
+            public BindingSource BindingSource
+            {
+                get
+                {
+                    return new BindingSource("Unrelated", displayName: null, isGreedy: true, isFromRequest: true);
+                }
+            }
         }
     }
 }