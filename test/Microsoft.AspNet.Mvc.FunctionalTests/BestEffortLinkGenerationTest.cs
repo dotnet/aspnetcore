d2aa55893dccaa3a3973991c7ef58ee67f0a3676
Ryan Nowak <nowakra@gmail.com>
2015-02-09 20:11:56 -0800

    Functional tests for #302
    This feature allows routing to generate a link when the action that is
    being linked-to does not yet exist. See the PR in routing for the actual
    implementation changes. This PR just has a simple functional test for the
    scenario.

---
+++ test/Microsoft.AspNet.Mvc.FunctionalTests/BestEffortLinkGenerationTest.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.TestHost;
+using Xunit;
+
+namespace Microsoft.AspNet.Mvc.FunctionalTests
+{
+
+    public class BestEffortLinkGenerationTest
+    {
+        private readonly IServiceProvider _services = TestHelper.CreateServices(nameof(BestEffortLinkGenerationWebSite));
+        private readonly Action<IApplicationBuilder> _app = new BestEffortLinkGenerationWebSite.Startup().Configure;
+
+        private const string ExpectedOutput = @"<html>
+<body>
+<a href=""/Home/About"">About Us</a>
+</body>
+</html>";
+
+        [Fact]
+        public async Task GenerateLink_NonExistentAction()
+        {
+            // Arrange
+            var server = TestServer.Create(_services, _app);
+            var client = server.CreateClient();
+
+            var url = "http://localhost/Home/Index";
+            // Act
+            var response = await client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(ExpectedOutput, content);
+        }
+
+    }
+}
---
+++ test/WebSites/BestEffortLinkGenerationWebSite/Startup.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Routing;
+using Microsoft.Framework.DependencyInjection;
+
+namespace BestEffortLinkGenerationWebSite
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            var configuration = app.GetTestConfiguration();
+
+            app.UseServices(services =>
+            {
+                services.AddMvc(configuration);
+                services.Configure<RouteOptions>((options) =>
+                {
+                    options.UseBestEffortLinkGeneration = true;
+                });
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
