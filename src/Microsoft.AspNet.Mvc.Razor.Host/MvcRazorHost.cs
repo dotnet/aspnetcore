OXB0319/Mvc

Results of parsing _ViewStart files should be cached
Fixes #1016

src/Microsoft.AspNet.Mvc.Razor.Host/MvcRazorHost.cs
@@ -31,11 +31,10 @@ namespace Microsoft.AspNet.Mvc.Razor
             new InjectChunk("Microsoft.AspNet.Mvc.IUrlHelper", "Url"),
         };
 
-        private readonly IFileProvider _fileProvider;
-
         // CodeGenerationContext.DefaultBaseClass is set to MyBaseType<dynamic>.
         // This field holds the type name without the generic decoration (MyBaseType)
         private readonly string _baseType;
+        private readonly ICodeTreeCache _codeTreeCache;
         private ChunkInheritanceUtility _chunkInheritanceUtility;
 
 #if NET45
@@ -44,8 +43,12 @@ namespace Microsoft.AspNet.Mvc.Razor
         /// <param name="root"/>.
         /// </summary>
         /// <param name="root">The path to the application base.</param>
+        // Note: This constructor is used by tooling and is created once for each
+        // Razor page that is loaded. Consequently, each loaded page has its own copy of
+        // the CodeTreeCache, but this ok - having a shared CodeTreeCache per application in tooling
+        // is problematic to manage.
         public MvcRazorHost(string root) :
-            this(new PhysicalFileProvider(root))
+            this(new CodeTreeCache(new PhysicalFileProvider(root)))
         {
         }
 #endif
@@ -53,11 +56,11 @@ namespace Microsoft.AspNet.Mvc.Razor
         /// Initializes a new instance of <see cref="MvcRazorHost"/> using the specified <paramref name="fileProvider"/>.
         /// </summary>
         /// <param name="fileProvider">A <see cref="IFileProvider"/> rooted at the application base path.</param>
-        public MvcRazorHost(IFileProvider fileProvider)
+        public MvcRazorHost(ICodeTreeCache codeTreeCache)
             : base(new CSharpRazorCodeLanguage())
         {
-            _fileProvider = fileProvider;
             _baseType = BaseType;
+            _codeTreeCache = codeTreeCache;
 
             TagHelperDescriptorResolver = new TagHelperDescriptorResolver();
             DefaultBaseClass = BaseType + "<" + DefaultModel + ">";
@@ -164,7 +167,7 @@ namespace Microsoft.AspNet.Mvc.Razor
                 if (_chunkInheritanceUtility == null)
                 {
                     // This needs to be lazily evaluated to support DefaultInheritedChunks being virtual.
-                    _chunkInheritanceUtility = new ChunkInheritanceUtility(this, _fileProvider, DefaultInheritedChunks);
+                    _chunkInheritanceUtility = new ChunkInheritanceUtility(this, _codeTreeCache, DefaultInheritedChunks);
                 }
 
                 return _chunkInheritanceUtility;

src/Microsoft.AspNet.Mvc/MvcServices.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Mvc.Internal;
 using Microsoft.AspNet.Mvc.ModelBinding;
 using Microsoft.AspNet.Mvc.OptionDescriptors;
 using Microsoft.AspNet.Mvc.Razor;
+using Microsoft.AspNet.Mvc.Razor.Directives;
 using Microsoft.AspNet.Mvc.Razor.OptionDescriptors;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.AspNet.Mvc.Routing;
@@ -106,13 +107,14 @@ namespace Microsoft.AspNet.Mvc
             yield return describe.Transient<IViewLocationExpanderProvider, DefaultViewLocationExpanderProvider>();
             // Caches view locations that are valid for the lifetime of the application.
             yield return describe.Singleton<IViewLocationCache, DefaultViewLocationCache>();
-
-            // The host is designed to be discarded after consumption and is very inexpensive to initialize.
-            yield return describe.Transient<IMvcRazorHost>(serviceProvider =>
+            yield return describe.Singleton<ICodeTreeCache>(serviceProvider =>
             {
                 var cachedFileProvider = serviceProvider.GetRequiredService<IOptions<RazorViewEngineOptions>>();
-                return new MvcRazorHost(cachedFileProvider.Options.FileProvider);
+                return new CodeTreeCache(cachedFileProvider.Options.FileProvider);
             });
+
+            // The host is designed to be discarded after consumption and is very inexpensive to initialize.
+            yield return describe.Transient<IMvcRazorHost, MvcRazorHost>();
 
             // Caches compilation artifacts across the lifetime of the application.
             yield return describe.Singleton<ICompilerCache, CompilerCache>();

test/Microsoft.AspNet.Mvc.Razor.Host.Test/Directives/ChunkInheritanceUtilityTest.cs
@@ -30,8 +30,9 @@ namespace Microsoft.AspNet.Mvc.Razor.Directives
                 new InjectChunk("MyTestHtmlHelper", "Html"),
                 new UsingChunk { Namespace = "AppNamespace.Model" },
             };
-            var host = new MvcRazorHost(fileProvider);
-            var utility = new ChunkInheritanceUtility(host, fileProvider, defaultChunks);
+            var cache = new CodeTreeCache(fileProvider);
+            var host = new MvcRazorHost(cache);
+            var utility = new ChunkInheritanceUtility(host, cache, defaultChunks);
 
             // Act
             var codeTrees = utility.GetInheritedCodeTrees(@"Views\home\Index.cshtml");
@@ -70,13 +71,14 @@ namespace Microsoft.AspNet.Mvc.Razor.Directives
             fileProvider.AddFile(@"_ViewStart.cs", string.Empty);
             fileProvider.AddFile(@"Views\_Layout.cshtml", string.Empty);
             fileProvider.AddFile(@"Views\home\_not-viewstart.cshtml", string.Empty);
-            var host = new MvcRazorHost(fileProvider);
+            var cache = new CodeTreeCache(fileProvider);
+            var host = new MvcRazorHost(cache);
             var defaultChunks = new Chunk[]
             {
                 new InjectChunk("MyTestHtmlHelper", "Html"),
                 new UsingChunk { Namespace = "AppNamespace.Model" },
             };
-            var utility = new ChunkInheritanceUtility(host, fileProvider, defaultChunks);
+            var utility = new ChunkInheritanceUtility(host, cache, defaultChunks);
 
             // Act
             var codeTrees = utility.GetInheritedCodeTrees(@"Views\home\Index.cshtml");
@@ -92,7 +94,8 @@ namespace Microsoft.AspNet.Mvc.Razor.Directives
             var fileProvider = new TestFileProvider();
             fileProvider.AddFile(@"Views\_ViewStart.cshtml",
                                "@inject DifferentHelper<TModel> Html");
-            var host = new MvcRazorHost(fileProvider);
+            var cache = new CodeTreeCache(fileProvider);
+            var host = new MvcRazorHost(cache);
             var defaultChunks = new Chunk[]
             {
                 new InjectChunk("MyTestHtmlHelper", "Html"),
@@ -117,7 +120,7 @@ namespace Microsoft.AspNet.Mvc.Razor.Directives
                 }
             };
 
-            var utility = new ChunkInheritanceUtility(host, fileProvider, defaultChunks);
+            var utility = new ChunkInheritanceUtility(host, cache, defaultChunks);
             var codeTree = new CodeTree();
 
             // Act

test/Microsoft.AspNet.Mvc.Razor.Host.Test/InjectChunkVisitorTest.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.AspNet.Mvc.Razor.Directives;
 using Microsoft.AspNet.Razor.Generator;
 using Microsoft.AspNet.Razor.Generator.Compiler;
 using Microsoft.AspNet.Razor.Generator.Compiler.CSharp;
@@ -145,8 +146,9 @@ MyType1
 
         private static CodeBuilderContext CreateContext()
         {
+            var codeTreeCache = new CodeTreeCache(new TestFileProvider());
             return new CodeBuilderContext(
-                new CodeGeneratorContext(new MvcRazorHost(new TestFileProvider()),
+                new CodeGeneratorContext(new MvcRazorHost(codeTreeCache),
                                          "MyClass",
                                          "MyNamespace",
                                          string.Empty,

test/Microsoft.AspNet.Mvc.Razor.Host.Test/ModelChunkVisitorTest.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.AspNet.Mvc.Razor.Directives;
 using Microsoft.AspNet.Razor.Generator;
 using Microsoft.AspNet.Razor.Generator.Compiler;
 using Microsoft.AspNet.Razor.Generator.Compiler.CSharp;
@@ -101,8 +102,9 @@ Environment.NewLine +
 
         private static CodeBuilderContext CreateContext()
         {
+            var fileProvider = new TestFileProvider();
             return new CodeBuilderContext(
-                new CodeGeneratorContext(new MvcRazorHost(new TestFileProvider()),
+                new CodeGeneratorContext(new MvcRazorHost(new CodeTreeCache(fileProvider)),
                                          "MyClass",
                                          "MyNamespace",
                                          string.Empty,

test/Microsoft.AspNet.Mvc.Razor.Host.Test/MvcRazorHostTest.cs
@@ -3,7 +3,7 @@
 
 using System.Collections.Generic;
 using System.IO;
-using Microsoft.AspNet.FileProviders;
+using Microsoft.AspNet.Mvc.Razor.Directives;
 using Microsoft.AspNet.Razor;
 using Microsoft.AspNet.Razor.Generator;
 using Microsoft.AspNet.Razor.Generator.Compiler;
@@ -19,7 +19,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         public void MvcRazorHost_EnablesInstrumentationByDefault()
         {
             // Arrange
-            var host = new MvcRazorHost(new TestFileProvider());
+            var fileProvider = new TestFileProvider();
+            var host = new MvcRazorHost(new CodeTreeCache(fileProvider));
 
             // Act
             var instrumented = host.EnableInstrumentation;
@@ -32,7 +33,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         public void MvcRazorHost_GeneratesTagHelperModelExpressionCode_DesignTime()
         {
             // Arrange
-            var host = new MvcRazorHost(new TestFileProvider())
+            var fileProvider = new TestFileProvider();
+            var host = new MvcRazorHost(new CodeTreeCache(fileProvider))
             {
                 DesignTimeMode = true
             };
@@ -85,7 +87,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         public void MvcRazorHost_ParsesAndGeneratesCodeForBasicScenarios(string scenarioName)
         {
             // Arrange
-            var host = new TestMvcRazorHost(new TestFileProvider());
+            var fileProvider = new TestFileProvider();
+            var host = new TestMvcRazorHost(new CodeTreeCache(fileProvider));
 
             // Act and Assert
             RunRuntimeTest(host, scenarioName);
@@ -95,7 +98,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         public void InjectVisitor_GeneratesCorrectLineMappings()
         {
             // Arrange
-            var host = new MvcRazorHost(new TestFileProvider())
+            var fileProvider = new TestFileProvider();
+            var host = new MvcRazorHost(new CodeTreeCache(fileProvider))
             {
                 DesignTimeMode = true
             };
@@ -114,7 +118,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         public void InjectVisitorWithModel_GeneratesCorrectLineMappings()
         {
             // Arrange
-            var host = new MvcRazorHost(new TestFileProvider())
+            var fileProvider = new TestFileProvider();
+            var host = new MvcRazorHost(new CodeTreeCache(fileProvider))
             {
                 DesignTimeMode = true
             };
@@ -134,7 +139,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         public void InjectVisitorWithSemicolon_GeneratesCorrectLineMappings()
         {
             // Arrange
-            var host = new MvcRazorHost(new TestFileProvider())
+            var fileProvider = new TestFileProvider();
+            var host = new MvcRazorHost(new CodeTreeCache(fileProvider))
             {
                 DesignTimeMode = true
             };
@@ -156,7 +162,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         public void ModelVisitor_GeneratesCorrectLineMappings()
         {
             // Arrange
-            var host = new MvcRazorHost(new TestFileProvider())
+            var fileProvider = new TestFileProvider();
+            var host = new MvcRazorHost(new CodeTreeCache(fileProvider))
             {
                 DesignTimeMode = true
             };
@@ -252,8 +259,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         /// </summary>
         private class TestMvcRazorHost : MvcRazorHost
         {
-            public TestMvcRazorHost(IFileProvider fileProvider)
-                : base(fileProvider)
+            public TestMvcRazorHost(ICodeTreeCache codeTreeCache)
+                : base(codeTreeCache)
             { }
 
             public override CodeBuilder DecorateCodeBuilder(CodeBuilder incomingBuilder, CodeBuilderContext context)

dotnet/aspnetcore

src/Microsoft.AspNet.Mvc.Razor.Host/MvcRazorHost.cs
@@ -31,11 +31,10 @@ namespace Microsoft.AspNet.Mvc.Razor
             new InjectChunk("Microsoft.AspNet.Mvc.IUrlHelper", "Url"),
         };
 
-        private readonly IFileProvider _fileProvider;
-
         // CodeGenerationContext.DefaultBaseClass is set to MyBaseType<dynamic>.
         // This field holds the type name without the generic decoration (MyBaseType)
         private readonly string _baseType;
+        private readonly ICodeTreeCache _codeTreeCache;
         private ChunkInheritanceUtility _chunkInheritanceUtility;
 
 #if NET45
@@ -44,8 +43,12 @@ namespace Microsoft.AspNet.Mvc.Razor
         /// <param name="root"/>.
         /// </summary>
         /// <param name="root">The path to the application base.</param>
+        // Note: This constructor is used by tooling and is created once for each
+        // Razor page that is loaded. Consequently, each loaded page has its own copy of
+        // the CodeTreeCache, but this ok - having a shared CodeTreeCache per application in tooling
+        // is problematic to manage.
         public MvcRazorHost(string root) :
-            this(new PhysicalFileProvider(root))
+            this(new DefaultCodeTreeCache(new PhysicalFileProvider(root)))
         {
         }
 #endif
@@ -53,11 +56,11 @@ namespace Microsoft.AspNet.Mvc.Razor
         /// Initializes a new instance of <see cref="MvcRazorHost"/> using the specified <paramref name="fileProvider"/>.
         /// </summary>
         /// <param name="fileProvider">A <see cref="IFileProvider"/> rooted at the application base path.</param>
-        public MvcRazorHost(IFileProvider fileProvider)
+        public MvcRazorHost(ICodeTreeCache codeTreeCache)
             : base(new CSharpRazorCodeLanguage())
         {
-            _fileProvider = fileProvider;
             _baseType = BaseType;
+            _codeTreeCache = codeTreeCache;
 
             TagHelperDescriptorResolver = new TagHelperDescriptorResolver();
             DefaultBaseClass = BaseType + "<" + DefaultModel + ">";
@@ -164,7 +167,7 @@ namespace Microsoft.AspNet.Mvc.Razor
                 if (_chunkInheritanceUtility == null)
                 {
                     // This needs to be lazily evaluated to support DefaultInheritedChunks being virtual.
-                    _chunkInheritanceUtility = new ChunkInheritanceUtility(this, _fileProvider, DefaultInheritedChunks);
+                    _chunkInheritanceUtility = new ChunkInheritanceUtility(this, _codeTreeCache, DefaultInheritedChunks);
                 }
 
                 return _chunkInheritanceUtility;

src/Microsoft.AspNet.Mvc/MvcServices.cs
@@ -9,10 +9,10 @@ using Microsoft.AspNet.Mvc.Internal;
 using Microsoft.AspNet.Mvc.ModelBinding;
 using Microsoft.AspNet.Mvc.OptionDescriptors;
 using Microsoft.AspNet.Mvc.Razor;
+using Microsoft.AspNet.Mvc.Razor.Directives;
 using Microsoft.AspNet.Mvc.Razor.OptionDescriptors;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.AspNet.Mvc.Routing;
-using Microsoft.AspNet.Security;
 using Microsoft.Framework.Cache.Memory;
 using Microsoft.Framework.ConfigurationModel;
 using Microsoft.Framework.DependencyInjection;
@@ -107,13 +107,14 @@ namespace Microsoft.AspNet.Mvc
             yield return describe.Transient<IViewLocationExpanderProvider, DefaultViewLocationExpanderProvider>();
             // Caches view locations that are valid for the lifetime of the application.
             yield return describe.Singleton<IViewLocationCache, DefaultViewLocationCache>();
-
-            // The host is designed to be discarded after consumption and is very inexpensive to initialize.
-            yield return describe.Transient<IMvcRazorHost>(serviceProvider =>
+            yield return describe.Singleton<ICodeTreeCache>(serviceProvider =>
             {
                 var cachedFileProvider = serviceProvider.GetRequiredService<IOptions<RazorViewEngineOptions>>();
-                return new MvcRazorHost(cachedFileProvider.Options.FileProvider);
+                return new DefaultCodeTreeCache(cachedFileProvider.Options.FileProvider);
             });
+
+            // The host is designed to be discarded after consumption and is very inexpensive to initialize.
+            yield return describe.Transient<IMvcRazorHost, MvcRazorHost>();
 
             // Caches compilation artifacts across the lifetime of the application.
             yield return describe.Singleton<ICompilerCache, CompilerCache>();

test/Microsoft.AspNet.Mvc.Razor.Host.Test/Directives/ChunkInheritanceUtilityTest.cs
@@ -30,8 +30,9 @@ namespace Microsoft.AspNet.Mvc.Razor.Directives
                 new InjectChunk("MyTestHtmlHelper", "Html"),
                 new UsingChunk { Namespace = "AppNamespace.Model" },
             };
-            var host = new MvcRazorHost(fileProvider);
-            var utility = new ChunkInheritanceUtility(host, fileProvider, defaultChunks);
+            var cache = new DefaultCodeTreeCache(fileProvider);
+            var host = new MvcRazorHost(cache);
+            var utility = new ChunkInheritanceUtility(host, cache, defaultChunks);
 
             // Act
             var codeTrees = utility.GetInheritedCodeTrees(@"Views\home\Index.cshtml");
@@ -70,13 +71,14 @@ namespace Microsoft.AspNet.Mvc.Razor.Directives
             fileProvider.AddFile(@"_ViewStart.cs", string.Empty);
             fileProvider.AddFile(@"Views\_Layout.cshtml", string.Empty);
             fileProvider.AddFile(@"Views\home\_not-viewstart.cshtml", string.Empty);
-            var host = new MvcRazorHost(fileProvider);
+            var cache = new DefaultCodeTreeCache(fileProvider);
+            var host = new MvcRazorHost(cache);
             var defaultChunks = new Chunk[]
             {
                 new InjectChunk("MyTestHtmlHelper", "Html"),
                 new UsingChunk { Namespace = "AppNamespace.Model" },
             };
-            var utility = new ChunkInheritanceUtility(host, fileProvider, defaultChunks);
+            var utility = new ChunkInheritanceUtility(host, cache, defaultChunks);
 
             // Act
             var codeTrees = utility.GetInheritedCodeTrees(@"Views\home\Index.cshtml");
@@ -92,7 +94,8 @@ namespace Microsoft.AspNet.Mvc.Razor.Directives
             var fileProvider = new TestFileProvider();
             fileProvider.AddFile(@"Views\_ViewStart.cshtml",
                                "@inject DifferentHelper<TModel> Html");
-            var host = new MvcRazorHost(fileProvider);
+            var cache = new DefaultCodeTreeCache(fileProvider);
+            var host = new MvcRazorHost(cache);
             var defaultChunks = new Chunk[]
             {
                 new InjectChunk("MyTestHtmlHelper", "Html"),
@@ -117,7 +120,7 @@ namespace Microsoft.AspNet.Mvc.Razor.Directives
                 }
             };
 
-            var utility = new ChunkInheritanceUtility(host, fileProvider, defaultChunks);
+            var utility = new ChunkInheritanceUtility(host, cache, defaultChunks);
             var codeTree = new CodeTree();
 
             // Act

test/Microsoft.AspNet.Mvc.Razor.Host.Test/InjectChunkVisitorTest.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.AspNet.Mvc.Razor.Directives;
 using Microsoft.AspNet.Razor.Generator;
 using Microsoft.AspNet.Razor.Generator.Compiler;
 using Microsoft.AspNet.Razor.Generator.Compiler.CSharp;
@@ -145,8 +146,9 @@ MyType1
 
         private static CodeBuilderContext CreateContext()
         {
+            var codeTreeCache = new DefaultCodeTreeCache(new TestFileProvider());
             return new CodeBuilderContext(
-                new CodeGeneratorContext(new MvcRazorHost(new TestFileProvider()),
+                new CodeGeneratorContext(new MvcRazorHost(codeTreeCache),
                                          "MyClass",
                                          "MyNamespace",
                                          string.Empty,

test/Microsoft.AspNet.Mvc.Razor.Host.Test/ModelChunkVisitorTest.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.AspNet.Mvc.Razor.Directives;
 using Microsoft.AspNet.Razor.Generator;
 using Microsoft.AspNet.Razor.Generator.Compiler;
 using Microsoft.AspNet.Razor.Generator.Compiler.CSharp;
@@ -101,8 +102,9 @@ Environment.NewLine +
 
         private static CodeBuilderContext CreateContext()
         {
+            var fileProvider = new TestFileProvider();
             return new CodeBuilderContext(
-                new CodeGeneratorContext(new MvcRazorHost(new TestFileProvider()),
+                new CodeGeneratorContext(new MvcRazorHost(new DefaultCodeTreeCache(fileProvider)),
                                          "MyClass",
                                          "MyNamespace",
                                          string.Empty,

test/Microsoft.AspNet.Mvc.Razor.Host.Test/MvcRazorHostTest.cs
@@ -3,7 +3,7 @@
 
 using System.Collections.Generic;
 using System.IO;
-using Microsoft.AspNet.FileProviders;
+using Microsoft.AspNet.Mvc.Razor.Directives;
 using Microsoft.AspNet.Razor;
 using Microsoft.AspNet.Razor.Generator;
 using Microsoft.AspNet.Razor.Generator.Compiler;
@@ -19,7 +19,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         public void MvcRazorHost_EnablesInstrumentationByDefault()
         {
             // Arrange
-            var host = new MvcRazorHost(new TestFileProvider());
+            var fileProvider = new TestFileProvider();
+            var host = new MvcRazorHost(new DefaultCodeTreeCache(fileProvider));
 
             // Act
             var instrumented = host.EnableInstrumentation;
@@ -32,7 +33,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         public void MvcRazorHost_GeneratesTagHelperModelExpressionCode_DesignTime()
         {
             // Arrange
-            var host = new MvcRazorHost(new TestFileProvider())
+            var fileProvider = new TestFileProvider();
+            var host = new MvcRazorHost(new DefaultCodeTreeCache(fileProvider))
             {
                 DesignTimeMode = true
             };
@@ -85,7 +87,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         public void MvcRazorHost_ParsesAndGeneratesCodeForBasicScenarios(string scenarioName)
         {
             // Arrange
-            var host = new TestMvcRazorHost(new TestFileProvider());
+            var fileProvider = new TestFileProvider();
+            var host = new TestMvcRazorHost(new DefaultCodeTreeCache(fileProvider));
 
             // Act and Assert
             RunRuntimeTest(host, scenarioName);
@@ -95,7 +98,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         public void InjectVisitor_GeneratesCorrectLineMappings()
         {
             // Arrange
-            var host = new MvcRazorHost(new TestFileProvider())
+            var fileProvider = new TestFileProvider();
+            var host = new MvcRazorHost(new DefaultCodeTreeCache(fileProvider))
             {
                 DesignTimeMode = true
             };
@@ -114,7 +118,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         public void InjectVisitorWithModel_GeneratesCorrectLineMappings()
         {
             // Arrange
-            var host = new MvcRazorHost(new TestFileProvider())
+            var fileProvider = new TestFileProvider();
+            var host = new MvcRazorHost(new DefaultCodeTreeCache(fileProvider))
             {
                 DesignTimeMode = true
             };
@@ -134,7 +139,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         public void InjectVisitorWithSemicolon_GeneratesCorrectLineMappings()
         {
             // Arrange
-            var host = new MvcRazorHost(new TestFileProvider())
+            var fileProvider = new TestFileProvider();
+            var host = new MvcRazorHost(new DefaultCodeTreeCache(fileProvider))
             {
                 DesignTimeMode = true
             };
@@ -156,7 +162,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         public void ModelVisitor_GeneratesCorrectLineMappings()
         {
             // Arrange
-            var host = new MvcRazorHost(new TestFileProvider())
+            var fileProvider = new TestFileProvider();
+            var host = new MvcRazorHost(new DefaultCodeTreeCache(fileProvider))
             {
                 DesignTimeMode = true
             };
@@ -252,8 +259,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         /// </summary>
         private class TestMvcRazorHost : MvcRazorHost
         {
-            public TestMvcRazorHost(IFileProvider fileProvider)
-                : base(fileProvider)
+            public TestMvcRazorHost(ICodeTreeCache codeTreeCache)
+                : base(codeTreeCache)
             { }
 
             public override CodeBuilder DecorateCodeBuilder(CodeBuilder incomingBuilder, CodeBuilderContext context)