vaualbus/Mvc

ControllerActivator should be able to use controllers registered as services
* Added WithControllersFromServiceProvider that replaces the default
  controller activator with a service based one.
* Move activation to DefaultControllerFactory
* Modify [Activate] behavior so that it no longer activates services. Use
  [FromService] attribute to hydrate services

Fixes #1707

test/WebSites/ControllersFromServicesWebSite/ControllerWithConstructorInjection.cs
@@ -1,0 +1,36 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+
+namespace ControllersFromServicesWebSite
+{
+    public class ConstructorInjectionController
+    {
+        public ConstructorInjectionController(IUrlHelper urlHelper,
+                                              QueryValueService queryService)
+        {
+            UrlHelper = urlHelper;
+            QueryService = queryService;
+        }
+
+        private IUrlHelper UrlHelper { get; }
+
+        private QueryValueService QueryService { get; }
+
+        [Activate]
+        public HttpRequest Request { get; set; }
+
+        [HttpGet("/constructorinjection")]
+        public IActionResult Index()
+        {
+            var content = string.Join(" ", 
+                                      UrlHelper.Action(), 
+                                      QueryService.GetValue(), 
+                                      Request.Headers["Test-Header"]);
+
+            return new ContentResult { Content = content };
+        }
+    }
+}

dotnet/aspnetcore

test/WebSites/ControllersFromServicesClassLibrary/ControllerWithConstructorInjection.cs
@@ -1,0 +1,36 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+
+namespace ControllersFromServicesClassLibrary
+{
+    public class ConstructorInjectionController
+    {
+        public ConstructorInjectionController(IUrlHelper urlHelper,
+                                              QueryValueService queryService)
+        {
+            UrlHelper = urlHelper;
+            QueryService = queryService;
+        }
+
+        private IUrlHelper UrlHelper { get; }
+
+        private QueryValueService QueryService { get; }
+
+        [Activate]
+        public HttpRequest Request { get; set; }
+
+        [HttpGet("/constructorinjection")]
+        public IActionResult Index()
+        {
+            var content = string.Join(" ", 
+                                      UrlHelper.Action(), 
+                                      QueryService.GetValue(), 
+                                      Request.Headers["Test-Header"]);
+
+            return new ContentResult { Content = content };
+        }
+    }
+}