5a5975e348d0b17541ca8fcdb424ddb8285b058f
dtkujawski <david@kujawski.ws>
2015-02-13 11:47:42 -0600

    Removed Ctor, Cleaned up namespace and removed extra vertical spacing as per comments.

--- test/Microsoft.AspNet.Hosting.Tests/Fakes/StartupBase.cs
+++ test/Microsoft.AspNet.Hosting.Tests/Fakes/StartupBase.cs
@@ -1,20 +1,12 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.AspNet.Builder;
 using Microsoft.Framework.DependencyInjection;
-using Microsoft.Framework.DependencyInjection.Fallback;
-using Microsoft.Framework.OptionsModel;
-using System;
 
 namespace Microsoft.AspNet.Hosting.Fakes
 {
     public class StartupBase
     {
-        public StartupBase()
-        {
-        }
-
         public void ConfigureBaseClassServices(IServiceCollection services)
         {
             services.AddOptions();
@@ -24,6 +16,5 @@ namespace Microsoft.AspNet.Hosting.Fakes
                 o.Environment = "BaseClass";
             });
         }
-
     }
 }
