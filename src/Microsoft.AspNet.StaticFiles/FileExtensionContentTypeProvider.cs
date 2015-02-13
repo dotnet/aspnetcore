d16a73cc05d842390768f78134c1eeb794215fae
Praburaj <praburaj.t@microsoft.com>
2015-02-13 15:03:16 -0800

    Adding woff2 MIME type
    Fixes : https://github.com/aspnet/StaticFiles/issues/28

--- src/Microsoft.AspNet.StaticFiles/FileExtensionContentTypeProvider.cs
+++ src/Microsoft.AspNet.StaticFiles/FileExtensionContentTypeProvider.cs
@@ -348,6 +348,7 @@ namespace Microsoft.AspNet.StaticFiles
                 { ".wmx", "video/x-ms-wmx" },
                 { ".wmz", "application/x-ms-wmz" },
                 { ".woff", "application/font-woff" },
+                { ".woff2", "application/font-woff2" },
                 { ".wps", "application/vnd.ms-works" },
                 { ".wri", "application/x-mswrite" },
                 { ".wrl", "x-world/x-vrml" },
