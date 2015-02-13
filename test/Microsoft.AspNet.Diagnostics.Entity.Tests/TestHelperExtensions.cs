e78dadcf0a57b1b8b625a71ca9d2a200dcbd98b7
Pranav K <prkrishn@hotmail.com>
2015-02-12 17:47:27 -0800

    Reacting to EF renames

--- test/Microsoft.AspNet.Diagnostics.Entity.Tests/TestHelperExtensions.cs
+++ test/Microsoft.AspNet.Diagnostics.Entity.Tests/TestHelperExtensions.cs
@@ -1,13 +1,14 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.Data.Entity.Infrastructure;
 using Microsoft.Framework.DependencyInjection;
 
 namespace Microsoft.Data.Entity.Tests
 {
     public static class TestHelperExtensions
     {
-        public static EntityServicesBuilder AddProviderServices(this EntityServicesBuilder entityServicesBuilder)
+        public static EntityFrameworkServicesBuilder AddProviderServices(this EntityFrameworkServicesBuilder entityServicesBuilder)
         {
             return entityServicesBuilder.AddInMemoryStore();
         }
