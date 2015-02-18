42da0144b9114ce3c765bbcd4cce1c0de7731aa3
Peter Hsu <shhsu@microsoft.com>
2015-02-18 13:37:18 -0800

    Ensure the latest AspNet.Loader.dll is retrieved

--- src/MusicStore/CopyAspNetLoader.cmd
+++ src/MusicStore/CopyAspNetLoader.cmd
@@ -3,6 +3,6 @@ md wwwroot\bin
 
 SET ASPNETLOADER_PACKAGE_BASEPATH=%USERPROFILE%\.k\packages\Microsoft.AspNet.Loader.IIS.Interop
 REM figure out the path of AspNet.Loader.dll
-FOR /F %%j IN ('dir /b /o:-d %ASPNETLOADER_PACKAGE_BASEPATH%\*') do (SET AspNetLoaderPath=%ASPNETLOADER_PACKAGE_BASEPATH%\%%j\tools\AspNet.Loader.dll)
+FOR /F %%j IN ('dir /b /o:D %ASPNETLOADER_PACKAGE_BASEPATH%\*') do (SET AspNetLoaderPath=%ASPNETLOADER_PACKAGE_BASEPATH%\%%j\tools\AspNet.Loader.dll)
 echo Found AspNetLoader.dll at %AspNetLoaderPath%. Copying to bin\
 copy %AspNetLoaderPath% wwwroot\bin\
