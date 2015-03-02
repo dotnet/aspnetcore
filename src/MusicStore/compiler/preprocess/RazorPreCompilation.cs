273ad22337872fb62b7bdc4e28673a85814e1e8e
Peter Hsu <shhsu@microsoft.com>
2015-03-02 12:27:44 -0800

    Add precompiler to allow kre precompile views

---
+++ src/MusicStore/compiler/preprocess/RazorPreCompilation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.Runtime;
+
+namespace MusicStore
+{
+    public class RazorPreCompilation : RazorPreCompileModule
+    {
+        public RazorPreCompilation(IServiceProvider provider,
+                                   IApplicationEnvironment applicationEnvironment)
+            : base(provider)
+        {
+            GenerateSymbols = string.Equals(applicationEnvironment.Configuration,
+                                            "debug",
+                                            StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
