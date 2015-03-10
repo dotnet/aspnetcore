078171f8ff86df3ece954cd88a6454f8825d6391
damianedwards <damian@damianedwards.com>
2015-03-09 21:05:40 -0700

    Fix stuff

--- Channels.ps1
+++ Channels.ps1
@@ -15,8 +15,10 @@ function Use-Dev
 
 function Use-Feed($desiredFeed)
 {
-    # TODO: Make this stick
-    $env:DNX_FEED = "https://www.myget.org/F/aspnet$desiredFeed/api/v2"
+    $feedUrl = "https://www.myget.org/F/aspnet$desiredFeed/api/v2"
+
+    Write-Host "Setting environment variable DNX_FEED to $feedUrl"
+    [Environment]::SetEnvironmentVariable("DNX_FEED", $feedUrl, [System.EnvironmentVariableTarget]::User)
     
     $nugetConfigPath = [IO.Path]::Combine($env:AppData, "NuGet", "NuGet.config")
     [xml]$configXml = Get-Content $nugetConfigPath
@@ -27,11 +29,14 @@ function Use-Feed($desiredFeed)
 
     $packageSources = $configuration.packageSources
     
+    Write-Host "Updating $nugetConfigPath"
+
     # Create the disabledPackageSources element if it doesn't exist
     if ($configXml.configuration.disabledPackageSources -eq $null)
     {
+        Write-Host "  Creating disabledPackageSources element"
         $disabledPackageSources = $configXml.CreateElement("disabledPackageSources")
-        $configXml.configuration.AppendChild($disabledPackageSources)
+        $configXml.configuration.AppendChild($disabledPackageSources) | Out-Null
     }
 
     ForEach ($feed in $aspNet5FeedNames)
@@ -43,35 +48,41 @@ function Use-Feed($desiredFeed)
         {
             # Add package source
             $newFeed = $configXml.CreateElement("add")
-            $newFeed.Attributes.Append($configXml.CreateAttribute("key"))
+            $newFeed.Attributes.Append($configXml.CreateAttribute("key")) | Out-Null
             $newFeed.key = "$keyPrefix ($feed)"
-            $newFeed.Attributes.Append($configXml.CreateAttribute("value"))
+            $newFeed.Attributes.Append($configXml.CreateAttribute("value")) | Out-Null
             $newFeed.value = "https://www.myget.org/F/aspnet$feed/api/v2"
-            $configXml.configuration.packageSources.AppendChild($newFeed)
+
+            Write-Host "  Adding '$fullFeedName' to packageSources"
+            $configXml.configuration.packageSources.AppendChild($newFeed) | out-null
         }
 
         $currentFeed = $configXml.configuration.disabledPackageSources.add | Where-Object key -eq $fullFeedName
 
-        if ($currentFeed -eq $null -and (-Not ($feed -eq $desiredFeed)))
+        if (($currentFeed -eq $null) -and (-Not ($feed -eq $desiredFeed)))
         {
             # Add disabled packaeg source
             $newFeed = $configXml.CreateElement("add")
-            $newFeed.Attributes.Append($configXml.CreateAttribute("key"))
+            $newFeed.Attributes.Append($configXml.CreateAttribute("key")) | Out-Null
             $newFeed.key = "$keyPrefix ($feed)"
-            $newFeed.Attributes.Append($configXml.CreateAttribute("value"))
+            $newFeed.Attributes.Append($configXml.CreateAttribute("value")) | Out-Null
             $newFeed.value = "true"
-            $configXml.configuration.disabledPackageSources.AppendChild($newFeed)
+
+            Write-Host "  Adding '$fullFeedName' to disabledPackageSources"
+            $configXml.configuration.disabledPackageSources.AppendChild($newFeed) | Out-Null
         }
-        elseif ((-Not ($currentFeed -eq $null)) -and $feed -eq $desiredFeed)
+        elseif ((-Not ($currentFeed -eq $null)) -and ($feed -eq $desiredFeed))
         {
             # Remove desired feed from disabledPackageSources list
-            $configXml.configuration.disabledPackageSources.RemoveChild($currentFeed)
+            Write-Host "  Removing '$fullFeedName' from disabledPackageSources"
+            $configXml.configuration.disabledPackageSources.RemoveChild($currentFeed) | Out-Null
         }
     }
 
-    $configXml.Save($nugetConfigPath)
+    Write-Host "  Saving $nugetConfigPath"
+    $configXml.Save($nugetConfigPath) | out-null
 
-    dnvm upgrade
-    dnvm install default -r coreclr
-    dnvm use default -r clr -p
+    dnvm.cmd upgrade
+    dnvm.cmd install default -r coreclr
+    dnvm.cmd use default -r clr -p
 }
