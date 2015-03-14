0966e37d949d4acb24d6bec7bc46065a47d7389e
Levi B <levib@yahoo.com>
2015-03-14 15:43:39 -0700

    Doc comment cleanup on GetApplicationUniqueIdentifier

--- src/Microsoft.AspNet.DataProtection.Interfaces/DataProtectionExtensions.cs
+++ src/Microsoft.AspNet.DataProtection.Interfaces/DataProtectionExtensions.cs
@@ -88,10 +88,17 @@ namespace Microsoft.AspNet.DataProtection
         /// <returns>A unique application identifier, or null if <paramref name="services"/> is null
         /// or cannot provide a unique application identifier.</returns>
         /// <remarks>
+        /// <para>
         /// The returned identifier should be stable for repeated runs of this same application on
         /// this machine. Additionally, the identifier is only unique within the scope of a single
         /// machine, e.g., two different applications on two different machines may return the same
         /// value.
+        /// </para>
+        /// <para>
+        /// This identifier may contain security-sensitive information such as physical file paths,
+        /// configuration settings, or other machine-specific information. Callers should take
+        /// special care not to disclose this information to untrusted entities.
+        /// </para>
         /// </remarks>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public static string GetApplicationUniqueIdentifier(this IServiceProvider services)
