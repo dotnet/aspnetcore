6086bb0a622312f49a09d27bf256b8807364e623
Praburaj <praburaj.t@microsoft.com>
2015-03-18 15:04:58 -0700

    Fixing the OpenIdConnect sample

--- samples/OpenIdConnectSample/Startup.cs
+++ samples/OpenIdConnectSample/Startup.cs
@@ -24,6 +24,7 @@ namespace OpenIdConnectSample
 
             app.UseCookieAuthentication(options =>
             {
+                options.AutomaticAuthentication = true;
             });
 
             app.UseOpenIdConnectAuthentication(options =>
---
+++ samples/OpenIdConnectSample/wwwroot/placeholder.html
@@ -0,0 +1,10 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+</head>
+<body>
+
+</body>
+</html>
