whyou126/Mvc

Adding support for model binding specifically marked controller properties.

src/Microsoft.AspNet.Mvc.Core/ControllerActionDescriptorBuilder.cs
@@ -42,6 +42,7 @@ namespace Microsoft.AspNet.Mvc
 
             foreach (var controller in application.Controllers)
             {
+                var controllerPropertyDescriptors = controller.ControllerProperties.Select(CreateParameterDescriptor);
                 foreach (var action in controller.Actions)
                 {
                     // Controllers with multiple [Route] attributes (or user defined implementation of
@@ -59,6 +60,11 @@ namespace Microsoft.AspNet.Mvc
                         AddApiExplorerInfo(actionDescriptor, application, controller, action);
                         AddRouteConstraints(removalConstraints, actionDescriptor, controller, action);
                         AddProperties(actionDescriptor, action, controller, application);
+
+                        // Only add properties which are explictly marked to bind.
+                        actionDescriptor.CommonParameters = controllerPropertyDescriptors
+                            .Where(p => p.BinderMetadata != null)
+                            .ToList();
 
                         if (IsAttributeRoutedAction(actionDescriptor))
                         {
@@ -279,6 +285,18 @@ namespace Microsoft.AspNet.Mvc
                 BinderMetadata = parameter.BinderMetadata,
                 Name = parameter.ParameterName,
                 ParameterType = parameter.ParameterInfo.ParameterType,
+            };
+
+            return parameterDescriptor;
+        }
+
+        private static ParameterDescriptor CreateParameterDescriptor(PropertyModel property)
+        {
+            var parameterDescriptor = new ParameterDescriptor()
+            {
+                BinderMetadata = property.BinderMetadata,
+                Name = property.PropertyName,
+                ParameterType = property.PropertyInfo.PropertyType,
             };
 
             return parameterDescriptor;

src/Microsoft.AspNet.Mvc.Core/DefaultControllerActionArgumentBinder.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc.Core;
 using Microsoft.AspNet.Mvc.ModelBinding;
@@ -32,7 +33,8 @@ namespace Microsoft.AspNet.Mvc
 
         public async Task<IDictionary<string, object>> GetActionArgumentsAsync(
             ActionContext actionContext,
-            ActionBindingContext actionBindingContext)
+            ActionBindingContext actionBindingContext, 
+            object controller)
         {
             var actionDescriptor = actionContext.ActionDescriptor as ControllerActionDescriptor;
             if (actionDescriptor == null)
@@ -57,61 +59,38 @@ namespace Microsoft.AspNet.Mvc
                 }
             }
 
-            var actionArguments = new Dictionary<string, object>(StringComparer.Ordinal);
-            await PopulateArgumentAsync(actionContext, actionBindingContext, actionArguments, parameterMetadata);
-            return actionArguments;
-        }
-
-        private void UpdateParameterMetadata(ModelMetadata metadata, IBinderMetadata binderMetadata)
-        {
-            if (binderMetadata != null)
+            var controllerType = actionDescriptor.ControllerTypeInfo.AsType();
+            var propertyMetadata = new List<ModelMetadata>();
+            foreach (var property in actionDescriptor.CommonParameters)
             {
-                metadata.BinderMetadata = binderMetadata;
-            }
+                var metadata = _modelMetadataProvider.GetMetadataForProperty(
+                    controllerType,
+                    propertyName: property.Name);
 
-            var nameProvider = binderMetadata as IModelNameProvider;
-            if (nameProvider != null && nameProvider.Name != null)
-            {
-                metadata.BinderModelName = nameProvider.Name;
-            }
-        }
-
-        private async Task PopulateArgumentAsync(
-            ActionContext actionContext,
-            ActionBindingContext bindingContext,
-            IDictionary<string, object> arguments,
-            IEnumerable<ModelMetadata> parameterMetadata)
-        {
-            var operationBindingContext = new OperationBindingContext
-            {
-                ModelBinder = bindingContext.ModelBinder,
-                ValidatorProvider = bindingContext.ValidatorProvider,
-                MetadataProvider = _modelMetadataProvider,
-                HttpContext = actionContext.HttpContext,
-                ValueProvider = bindingContext.ValueProvider,
-            };
-
-            var modelState = actionContext.ModelState;
-            modelState.MaxAllowedErrors = _options.MaxModelValidationErrors;
-            foreach (var parameter in parameterMetadata)
-            {
-                var parameterType = parameter.ModelType;
-
-                var modelBindingContext = GetModelBindingContext(parameter, modelState, operationBindingContext);
-                var modelBindingResult = await bindingContext.ModelBinder.BindModelAsync(modelBindingContext);
-                if (modelBindingResult != null && modelBindingResult.IsModelSet)
+                if (metadata != null)
                 {
-                    var modelExplorer = new ModelExplorer(_modelMetadataProvider, parameter, modelBindingResult.Model);
-
-                    arguments[parameter.PropertyName] = modelBindingResult.Model;
-                    var validationContext = new ModelValidationContext(
-                        modelBindingResult.Key,
-                        bindingContext.ValidatorProvider,
-                        actionContext.ModelState,
-                        modelExplorer);
-                    _validator.Validate(validationContext);
+                    UpdateParameterMetadata(metadata, property.BinderMetadata);
+                    propertyMetadata.Add(metadata);
                 }
             }
+
+            var operationBindingContext = GetOperationBindingContext(actionContext, actionBindingContext);
+           
+            var controllerProperties = new Dictionary<string, object>(StringComparer.Ordinal);
+            await PopulateArgumentAsync(
+                operationBindingContext,
+                actionContext.ModelState,
+                controllerProperties,
+                propertyMetadata);
+            ActivateProperties(controller, controllerType, controllerProperties);
+
+            var actionArguments = new Dictionary<string, object>(StringComparer.Ordinal);
+            await PopulateArgumentAsync(
+                operationBindingContext,
+                actionContext.ModelState,
+                actionArguments,
+                parameterMetadata);
+            return actionArguments;
         }
 
         // Internal for tests
@@ -133,6 +112,83 @@ namespace Microsoft.AspNet.Mvc
             };
 
             return modelBindingContext;
+        }
+
+        private void ActivateProperties(object controller, Type containerType, Dictionary<string, object> properties)
+        {
+            foreach (var property in properties)
+            {
+                SetProperty(property.Key, containerType, controller, property.Value);
+            }
+        }
+
+        private void SetProperty(string propertyName, Type containerType, object container, object value)
+        {
+            var bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.IgnoreCase;
+            var property = containerType.GetProperty(propertyName, bindingFlags);
+            if (property == null || !property.CanWrite)
+            {
+                // nothing to do
+                return;
+            }
+
+            property.SetValue(container, value);
+        }
+
+        private void UpdateParameterMetadata(ModelMetadata metadata, IBinderMetadata binderMetadata)
+        {
+            if (binderMetadata != null)
+            {
+                metadata.BinderMetadata = binderMetadata;
+            }
+
+            var nameProvider = binderMetadata as IModelNameProvider;
+            if (nameProvider != null && nameProvider.Name != null)
+            {
+                metadata.BinderModelName = nameProvider.Name;
+            }
+        }
+
+        private async Task PopulateArgumentAsync(
+            OperationBindingContext operationContext,
+            ModelStateDictionary modelState,
+            IDictionary<string, object> arguments,
+            IEnumerable<ModelMetadata> parameterMetadata)
+        {
+            modelState.MaxAllowedErrors = _options.MaxModelValidationErrors;
+            foreach (var parameter in parameterMetadata)
+            {
+                var parameterType = parameter.ModelType;
+
+                var modelBindingContext = GetModelBindingContext(parameter, modelState, operationContext);
+                var modelBindingResult = await operationContext.ModelBinder.BindModelAsync(modelBindingContext);
+                if (modelBindingResult != null && modelBindingResult.IsModelSet)
+                {
+                    var modelExplorer = new ModelExplorer(_modelMetadataProvider, parameter, modelBindingResult.Model);
+
+                    arguments[parameter.PropertyName] = modelBindingResult.Model;
+                    var validationContext = new ModelValidationContext(
+                        modelBindingResult.Key,
+                        operationContext.ValidatorProvider,
+                        modelState,
+                        modelExplorer);
+                    _validator.Validate(validationContext);
+                }
+            }
+        }
+
+        private OperationBindingContext GetOperationBindingContext(
+            ActionContext actionContext,
+            ActionBindingContext bindingContext)
+        {
+            return new OperationBindingContext
+            {
+                ModelBinder = bindingContext.ModelBinder,
+                ValidatorProvider = bindingContext.ValidatorProvider,
+                MetadataProvider = _modelMetadataProvider,
+                HttpContext = actionContext.HttpContext,
+                ValueProvider = bindingContext.ValueProvider,
+            };
         }
     }
 }

src/Microsoft.AspNet.Mvc.Core/Logging/ActionDescriptorValues.cs
@@ -21,6 +21,7 @@ namespace Microsoft.AspNet.Mvc.Logging
             Name = inner.Name;
             DisplayName = inner.DisplayName;
             Parameters = inner.Parameters.Select(p => new ParameterDescriptorValues(p)).ToList();
+            CommonParameters = inner.CommonParameters.Select(p => new ParameterDescriptorValues(p)).ToList();
             FilterDescriptors = inner.FilterDescriptors.Select(f => new FilterDescriptorValues(f)).ToList();
             RouteConstraints = inner.RouteConstraints.Select(r => new RouteDataActionConstraintValues(r)).ToList();
             AttributeRouteInfo = new AttributeRouteInfoValues(inner.AttributeRouteInfo);
@@ -53,6 +54,12 @@ namespace Microsoft.AspNet.Mvc.Logging
         /// See <see cref="ActionDescriptor.Parameters"/>.
         /// </summary>
         public IList<ParameterDescriptorValues> Parameters { get; }
+
+        /// <summary>
+        /// The parameters of the action as <see cref="ParameterDescriptorValues"/>.
+        /// See <see cref="ActionDescriptor.CommonParameters"/>.
+        /// </summary>
+        public IList<ParameterDescriptorValues> CommonParameters { get; }
 
         /// <summary>
         /// The filters of the action as <see cref="FilterDescriptorValues"/>.

test/Microsoft.AspNet.Mvc.Core.Test/ControllerActionInvokerTest.cs
@@ -2023,6 +2023,8 @@ namespace Microsoft.AspNet.Mvc
             // Arrange
             var actionDescriptor = new ControllerActionDescriptor
             {
+                ControllerTypeInfo = typeof(TestController).GetTypeInfo(),
+                CommonParameters = new List<ParameterDescriptor>(),
                 MethodInfo = typeof(TestController).GetTypeInfo()
                     .DeclaredMethods
                     .First(m => m.Name.Equals("ActionMethodWithDefaultValues", StringComparison.Ordinal)),

dotnet/aspnetcore

src/Microsoft.AspNet.Mvc.Core/ControllerActionDescriptorBuilder.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Mvc.ApplicationModels;
 using Microsoft.AspNet.Mvc.Core;
 using Microsoft.AspNet.Mvc.Description;
 using Microsoft.AspNet.Mvc.Routing;
+using Microsoft.AspNet.Mvc.ModelBinding;
 
 namespace Microsoft.AspNet.Mvc
 {
@@ -42,6 +43,12 @@ namespace Microsoft.AspNet.Mvc
 
             foreach (var controller in application.Controllers)
             {
+                // Only add properties which are explictly marked to bind.
+                // The attribute check is required for ModelBinder attribute.
+                var controllerPropertyDescriptors = controller.ControllerProperties
+                    .Where(p => p.BindingInfo != null)
+                    .Select(CreateParameterDescriptor)
+                    .ToList();
                 foreach (var action in controller.Actions)
                 {
                     // Controllers with multiple [Route] attributes (or user defined implementation of
@@ -60,6 +67,7 @@ namespace Microsoft.AspNet.Mvc
                         AddRouteConstraints(removalConstraints, actionDescriptor, controller, action);
                         AddProperties(actionDescriptor, action, controller, application);
 
+                        actionDescriptor.BoundProperties = controllerPropertyDescriptors;
                         if (IsAttributeRoutedAction(actionDescriptor))
                         {
                             hasAttributeRoutes = true;
@@ -272,13 +280,25 @@ namespace Microsoft.AspNet.Mvc
             return actionDescriptor;
         }
 
-        private static ParameterDescriptor CreateParameterDescriptor(ParameterModel parameter)
+        private static ParameterDescriptor CreateParameterDescriptor(ParameterModel parameterModel)
         {
             var parameterDescriptor = new ParameterDescriptor()
             {
-                Name = parameter.ParameterName,
-                ParameterType = parameter.ParameterInfo.ParameterType,
-                BindingInfo = parameter.BindingInfo
+                Name = parameterModel.ParameterName,
+                ParameterType = parameterModel.ParameterInfo.ParameterType,
+                BindingInfo = parameterModel.BindingInfo
+            };
+
+            return parameterDescriptor;
+        }
+
+        private static ParameterDescriptor CreateParameterDescriptor(PropertyModel propertyModel)
+        {
+            var parameterDescriptor = new ParameterDescriptor()
+            {
+                BindingInfo = propertyModel.BindingInfo,
+                Name = propertyModel.PropertyName,
+                ParameterType = propertyModel.PropertyInfo.PropertyType,
             };
 
             return parameterDescriptor;

src/Microsoft.AspNet.Mvc.Core/DefaultControllerActionArgumentBinder.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNet.Mvc.Core;
 using Microsoft.AspNet.Mvc.ModelBinding;
 using Microsoft.AspNet.Mvc.ModelBinding.Metadata;
 using Microsoft.AspNet.Mvc.ModelBinding.Validation;
+using Microsoft.Framework.Internal;
 using Microsoft.Framework.OptionsModel;
 
 namespace Microsoft.AspNet.Mvc
@@ -34,9 +35,10 @@ namespace Microsoft.AspNet.Mvc
             _validator = validator;
         }
 
-        public async Task<IDictionary<string, object>> GetActionArgumentsAsync(
+        public async Task<IDictionary<string, object>> BindActionArgumentsAsync(
             ActionContext actionContext,
-            ActionBindingContext actionBindingContext)
+            ActionBindingContext actionBindingContext, 
+            object controller)
         {
             var actionDescriptor = actionContext.ActionDescriptor as ControllerActionDescriptor;
             if (actionDescriptor == null)
@@ -47,31 +49,48 @@ namespace Microsoft.AspNet.Mvc
                         nameof(actionContext));
             }
 
+            var operationBindingContext = GetOperationBindingContext(actionContext, actionBindingContext);
+            var controllerProperties = new Dictionary<string, object>(StringComparer.Ordinal);
+            await PopulateArgumentsAsync(
+                operationBindingContext,
+                actionContext.ModelState,
+                controllerProperties,
+                actionDescriptor.BoundProperties);
+            var controllerType = actionDescriptor.ControllerTypeInfo.AsType();
+            ActivateProperties(controller, controllerType, controllerProperties);
+
             var actionArguments = new Dictionary<string, object>(StringComparer.Ordinal);
             await PopulateArgumentsAsync(
-                actionContext,
-                actionBindingContext,
+                operationBindingContext,
+                actionContext.ModelState,
                 actionArguments,
                 actionDescriptor.Parameters);
             return actionArguments;
         }   
 
+        private void ActivateProperties(object controller, Type containerType, Dictionary<string, object> properties)
+        {
+            var propertyHelpers = PropertyHelper.GetProperties(controller);
+            foreach (var property in properties)
+            {
+                var propertyHelper = propertyHelpers.First(helper => helper.Name == property.Key);
+                if (propertyHelper.Property == null || !propertyHelper.Property.CanWrite)
+                {
+                    // nothing to do
+                    return;
+                }
+
+                var setter = PropertyHelper.MakeFastPropertySetter(propertyHelper.Property);
+                setter(controller, property.Value);
+            }
+        }
+
         private async Task PopulateArgumentsAsync(
-            ActionContext actionContext,
-            ActionBindingContext bindingContext,
+            OperationBindingContext operationContext,
+            ModelStateDictionary modelState,
             IDictionary<string, object> arguments,
             IEnumerable<ParameterDescriptor> parameterMetadata)
         {
-            var operationBindingContext = new OperationBindingContext
-            {
-                ModelBinder = bindingContext.ModelBinder,
-                ValidatorProvider = bindingContext.ValidatorProvider,
-                MetadataProvider = _modelMetadataProvider,
-                HttpContext = actionContext.HttpContext,
-                ValueProvider = bindingContext.ValueProvider,
-            };
-
-            var modelState = actionContext.ModelState;
             modelState.MaxAllowedErrors = _options.MaxModelValidationErrors;
             foreach (var parameter in parameterMetadata)
             {
@@ -82,9 +101,9 @@ namespace Microsoft.AspNet.Mvc
                     metadata,
                     parameter.BindingInfo,
                     modelState,
-                    operationBindingContext);
+                    operationContext);
 
-                var modelBindingResult = await bindingContext.ModelBinder.BindModelAsync(modelBindingContext);
+                var modelBindingResult = await operationContext.ModelBinder.BindModelAsync(modelBindingContext);
                 if (modelBindingResult != null && modelBindingResult.IsModelSet)
                 {
                     var modelExplorer = new ModelExplorer(
@@ -95,8 +114,9 @@ namespace Microsoft.AspNet.Mvc
                     arguments[parameter.Name] = modelBindingResult.Model;
                     var validationContext = new ModelValidationContext(
                         modelBindingResult.Key,
-                        bindingContext.ValidatorProvider,
-                        actionContext.ModelState,
+                        modelBindingContext.BindingSource,
+                        operationContext.ValidatorProvider,
+                        modelState,
                         modelExplorer);
                     _validator.Validate(validationContext);
                 }
@@ -120,6 +140,20 @@ namespace Microsoft.AspNet.Mvc
             modelBindingContext.OperationBindingContext = operationBindingContext;
 
             return modelBindingContext;
+        }
+
+        private OperationBindingContext GetOperationBindingContext(
+            ActionContext actionContext,
+            ActionBindingContext bindingContext)
+        {
+            return new OperationBindingContext
+            {
+                ModelBinder = bindingContext.ModelBinder,
+                ValidatorProvider = bindingContext.ValidatorProvider,
+                MetadataProvider = _modelMetadataProvider,
+                HttpContext = actionContext.HttpContext,
+                ValueProvider = bindingContext.ValueProvider,
+            };
         }
     }
 }

src/Microsoft.AspNet.Mvc.Core/Logging/ActionDescriptorValues.cs
@@ -21,6 +21,7 @@ namespace Microsoft.AspNet.Mvc.Logging
             Name = inner.Name;
             DisplayName = inner.DisplayName;
             Parameters = inner.Parameters.Select(p => new ParameterDescriptorValues(p)).ToList();
+            BoundProperties = inner.BoundProperties.Select(p => new ParameterDescriptorValues(p)).ToList();
             FilterDescriptors = inner.FilterDescriptors.Select(f => new FilterDescriptorValues(f)).ToList();
             RouteConstraints = inner.RouteConstraints.Select(r => new RouteDataActionConstraintValues(r)).ToList();
             AttributeRouteInfo = new AttributeRouteInfoValues(inner.AttributeRouteInfo);
@@ -53,6 +54,12 @@ namespace Microsoft.AspNet.Mvc.Logging
         /// See <see cref="ActionDescriptor.Parameters"/>.
         /// </summary>
         public IList<ParameterDescriptorValues> Parameters { get; }
+
+        /// <summary>
+        /// The parameters of the action as <see cref="ParameterDescriptorValues"/>.
+        /// See <see cref="ActionDescriptor.BoundProperties"/>.
+        /// </summary>
+        public IList<ParameterDescriptorValues> BoundProperties { get; }
 
         /// <summary>
         /// The filters of the action as <see cref="FilterDescriptorValues"/>.

test/Microsoft.AspNet.Mvc.Core.Test/ControllerActionInvokerTest.cs
@@ -2063,6 +2063,8 @@ namespace Microsoft.AspNet.Mvc
             // Arrange
             var actionDescriptor = new ControllerActionDescriptor
             {
+                ControllerTypeInfo = typeof(TestController).GetTypeInfo(),
+                BoundProperties = new List<ParameterDescriptor>(),
                 MethodInfo = typeof(TestController).GetTypeInfo()
                     .DeclaredMethods
                     .First(m => m.Name.Equals("ActionMethodWithDefaultValues", StringComparison.Ordinal)),