e0694a21d86c06524d516d1c0e4204c13ba64070
N. Taylor Mullen <nimullen@microsoft.com>
2015-04-07 14:50:16 -0700

    Add serviceable attribute to projects.
    aspnet/DNX#1600

---
+++ src/Microsoft.AspNet.Authentication.Google/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Reflection;
+
+[assembly: AssemblyMetadata("Serviceable", "True")]
