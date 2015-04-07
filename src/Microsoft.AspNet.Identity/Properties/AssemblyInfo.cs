b652ca3332cb0c8f3a368061433851bb9f104960
N. Taylor Mullen <nimullen@microsoft.com>
2015-04-07 14:46:20 -0700

    Add serviceable attribute to projects.
    aspnet/DNX#1600

--- src/Microsoft.AspNet.Identity/Properties/AssemblyInfo.cs
+++ src/Microsoft.AspNet.Identity/Properties/AssemblyInfo.cs
@@ -1,9 +1,11 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Microsoft.AspNet.Identity.Test")]
 [assembly: InternalsVisibleTo("Microsoft.AspNet.Identity.EntityFramework.Test")]
 [assembly: InternalsVisibleTo("Microsoft.AspNet.Identity.EntityFramework.InMemory.Test")]
 [assembly: InternalsVisibleTo("Microsoft.AspNet.Identity.InMemory.Test")]
+[assembly: AssemblyMetadata("Serviceable", "True")]
