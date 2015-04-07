84afe105ec984f2092751669addc4c9882e376a2
N. Taylor Mullen <nimullen@microsoft.com>
2015-04-07 14:48:25 -0700

    Add serviceable attribute to projects.
    aspnet/DNX#1600

--- src/Microsoft.AspNet.Razor/Properties/AssemblyInfo.cs
+++ src/Microsoft.AspNet.Razor/Properties/AssemblyInfo.cs
@@ -1,7 +1,9 @@
-﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Microsoft.AspNet.Razor.Test")]
 [assembly: InternalsVisibleTo("Microsoft.AspNet.Razor.Runtime.Test")]
+[assembly: AssemblyMetadata("Serviceable", "True")]
