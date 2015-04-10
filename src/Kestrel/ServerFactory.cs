a23b665e8da5f291cbf9b200e339a373e9731233
Chris Ross <Tratcher@Outlook.com>
2015-04-10 10:47:31 -0700

    Update IServerInformation namespace.

--- src/Kestrel/ServerFactory.cs
+++ src/Kestrel/ServerFactory.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.FeatureModel;
 using Microsoft.AspNet.Hosting.Server;
 using Microsoft.AspNet.Server.Kestrel;
--- src/Kestrel/ServerInformation.cs
+++ src/Kestrel/ServerInformation.cs
@@ -1,8 +1,11 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System;
-using Microsoft.AspNet.Builder;
-using Microsoft.Framework.ConfigurationModel;
-using System.Globalization;
 using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.AspNet.Hosting.Server;
+using Microsoft.Framework.ConfigurationModel;
 
 namespace Kestrel
 {
