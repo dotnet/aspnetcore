jeremyiverson/Mvc

Part 1 of #1712 - Remove reflection in MutableObjectModelBinder
This change introduces a new property to ModelMetadata called
IsBindingRequired, which specifies whether or not a model value must be
present on the wire during model binding.

[DataMember(IsRequired = true)] is currently the only thing that will set
this property.

Updated tests and documentation for clarity on the difference in meaning
between MM.IsRequired and MM.IsBindingRequired. Moved setting for
IsRequired to ValidationMetadata which is a better fit.

Also added functional tests for [BindingBehavior] and [DataMember] in
model binding because they were totally missing.

test/Microsoft.AspNet.Mvc.FunctionalTests/ModelBindingBindingBehaviorTest.cs
@@ -1,0 +1,128 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+using ModelBindingWebSite;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Microsoft.AspNet.Mvc.FunctionalTests
+{
+    public class ModelBindingBindingBehaviorTest
+    {
+        private const string SiteName = nameof(ModelBindingWebSite);
+        private readonly Action<IApplicationBuilder> _app = new Startup().Configure;
+        private readonly Action<IServiceCollection> _configureServices = new Startup().ConfigureServices;
+
+        [Fact]
+        public async Task BindingBehavior_MissingRequiredProperties_ValidationErrors()
+        {
+            // Arrange
+            var server = TestHelper.CreateServer(_app, SiteName, _configureServices);
+            var client = server.CreateClient();
+
+            var url = "http://localhost/BindingBehavior/EchoModelValues";
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            var formData = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("model.Property2", "Hi"),
+            };
+
+            request.Content = new FormUrlEncodedContent(formData);
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+
+            var body = await response.Content.ReadAsStringAsync();
+            var errors = JsonConvert.DeserializeObject<SerializableError>(body);
+
+            Assert.Equal(2, errors.Count);
+
+            var error = Assert.Single(errors, kvp => kvp.Key == "model.Property3" );
+            Assert.Equal("The 'Property3' property is required.", ((JArray)error.Value)[0].Value<string>());
+
+            error = Assert.Single(errors, kvp => kvp.Key == "model.Property4");
+            Assert.Equal("The 'Property4' property is required.", ((JArray)error.Value)[0].Value<string>());
+        }
+
+        [Fact]
+        public async Task BindingBehavior_OptionalIsOptional()
+        {
+            // Arrange
+            var server = TestHelper.CreateServer(_app, SiteName, _configureServices);
+            var client = server.CreateClient();
+
+            var url = "http://localhost/BindingBehavior/EchoModelValues";
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            var formData = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("model.Property3", "Hello"),
+                new KeyValuePair<string, string>("model.Property4", "World!"),
+            };
+
+            request.Content = new FormUrlEncodedContent(formData);
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var body = await response.Content.ReadAsStringAsync();
+            var model = JsonConvert.DeserializeObject<BindingBehaviorModel>(body);
+
+            Assert.Null(model.Property1);
+            Assert.Null(model.Property2);
+            Assert.Equal("Hello", model.Property3);
+            Assert.Equal("World!", model.Property4);
+            Assert.Null(model.Property5);
+        }
+
+        [Fact]
+        public async Task BindingBehavior_Never_IsNotBound()
+        {
+            // Arrange
+            var server = TestHelper.CreateServer(_app, SiteName, _configureServices);
+            var client = server.CreateClient();
+
+            var url = "http://localhost/BindingBehavior/EchoModelValues";
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            var formData = new List<KeyValuePair<string, string>>
+            {
+
+                new KeyValuePair<string, string>("model.Property1", "Ignored"),
+                new KeyValuePair<string, string>("model.Property2", "Optional"),
+                new KeyValuePair<string, string>("model.Property3", "Hello"),
+                new KeyValuePair<string, string>("model.Property4", "World!"),
+                new KeyValuePair<string, string>("model.Property5", "Ignored"),
+            };
+
+            request.Content = new FormUrlEncodedContent(formData);
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var body = await response.Content.ReadAsStringAsync();
+            var model = JsonConvert.DeserializeObject<BindingBehaviorModel>(body);
+
+            Assert.Null(model.Property1);
+            Assert.Equal("Optional", model.Property2);
+            Assert.Equal("Hello", model.Property3);
+            Assert.Equal("World!", model.Property4);
+            Assert.Null(model.Property5);
+        }
+    }
+}

test/Microsoft.AspNet.Mvc.FunctionalTests/ModelBindingDataMemberRequiredTest.cs
@@ -1,0 +1,87 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+using ModelBindingWebSite;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Microsoft.AspNet.Mvc.FunctionalTests
+{
+    public class ModelBindingDataMemberRequiredTest
+    {
+        private const string SiteName = nameof(ModelBindingWebSite);
+        private readonly Action<IApplicationBuilder> _app = new Startup().Configure;
+        private readonly Action<IServiceCollection> _configureServices = new Startup().ConfigureServices;
+
+        [Fact]
+        public async Task DataMember_MissingRequiredProperty_ValidationError()
+        {
+            // Arrange
+            var server = TestHelper.CreateServer(_app, SiteName, _configureServices);
+            var client = server.CreateClient();
+
+            var url = "http://localhost/DataMemberRequired/EchoModelValues";
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            var formData = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("model.Property2", "Hi"),
+            };
+
+            request.Content = new FormUrlEncodedContent(formData);
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+
+            var body = await response.Content.ReadAsStringAsync();
+            var errors = JsonConvert.DeserializeObject<SerializableError>(body);
+
+            Assert.Equal(1, errors.Count);
+
+            var error = Assert.Single(errors, kvp => kvp.Key == "model.Property3");
+            Assert.Equal("The 'Property3' property is required.", ((JArray)error.Value)[0].Value<string>());
+        }
+
+        [Fact]
+        public async Task DataMember_RequiredPropertyProvided_Success()
+        {
+            // Arrange
+            var server = TestHelper.CreateServer(_app, SiteName, _configureServices);
+            var client = server.CreateClient();
+
+            var url = "http://localhost/DataMemberRequired/EchoModelValues";
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            var formData = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("model.Property1", "Hello"),
+                new KeyValuePair<string, string>("model.Property2", "World!"),
+                new KeyValuePair<string, string>("model.Property3", "Required!"),
+            };
+
+            request.Content = new FormUrlEncodedContent(formData);
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var body = await response.Content.ReadAsStringAsync();
+            var model = JsonConvert.DeserializeObject<DataMemberRequiredModel>(body);
+
+            Assert.Equal("Hello", model.Property1);
+            Assert.Equal("World!", model.Property2);
+            Assert.Equal("Required!", model.Property3);
+        }
+    }
+}

test/WebSites/ModelBindingWebSite/Models/BindingBehaviorModel.cs
@@ -1,0 +1,25 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc.ModelBinding;
+
+namespace ModelBindingWebSite
+{
+    public class BindingBehaviorModel
+    {
+        [BindingBehavior(BindingBehavior.Never)]
+        public string Property1 { get; set; }
+
+        [BindingBehavior(BindingBehavior.Optional)]
+        public string Property2 { get; set; }
+
+        [BindingBehavior(BindingBehavior.Required)]
+        public string Property3 { get; set; }
+
+        [BindRequired]
+        public string Property4 { get; set; }
+
+        [BindNever]
+        public string Property5 { get; set; }
+    }
+}

test/WebSites/ModelBindingWebSite/Models/DataMemberRequiredModel.cs
@@ -1,0 +1,20 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Runtime.Serialization;
+
+namespace ModelBindingWebSite
+{
+    [DataContract]
+    public class DataMemberRequiredModel
+    {
+        [DataMember]
+        public string Property1 { get; set; }
+
+        [DataMember(IsRequired = false)]
+        public string Property2 { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public string Property3 { get; set; }
+    }
+}

dotnet/aspnetcore

test/Microsoft.AspNet.Mvc.FunctionalTests/ModelBindingBindingBehaviorTest.cs
@@ -1,0 +1,128 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+using ModelBindingWebSite;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Microsoft.AspNet.Mvc.FunctionalTests
+{
+    public class ModelBindingBindingBehaviorTest
+    {
+        private const string SiteName = nameof(ModelBindingWebSite);
+        private readonly Action<IApplicationBuilder> _app = new Startup().Configure;
+        private readonly Action<IServiceCollection> _configureServices = new Startup().ConfigureServices;
+
+        [Fact]
+        public async Task BindingBehavior_MissingRequiredProperties_ValidationErrors()
+        {
+            // Arrange
+            var server = TestHelper.CreateServer(_app, SiteName, _configureServices);
+            var client = server.CreateClient();
+
+            var url = "http://localhost/BindingBehavior/EchoModelValues";
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            var formData = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("model.BehaviourOptionalProperty", "Hi"),
+            };
+
+            request.Content = new FormUrlEncodedContent(formData);
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+
+            var body = await response.Content.ReadAsStringAsync();
+            var errors = JsonConvert.DeserializeObject<SerializableError>(body);
+
+            Assert.Equal(2, errors.Count);
+
+            var error = Assert.Single(errors, kvp => kvp.Key == "model.BehaviourRequiredProperty");
+            Assert.Equal("The 'BehaviourRequiredProperty' property is required.", ((JArray)error.Value)[0].Value<string>());
+
+            error = Assert.Single(errors, kvp => kvp.Key == "model.BindRequiredProperty");
+            Assert.Equal("The 'BindRequiredProperty' property is required.", ((JArray)error.Value)[0].Value<string>());
+        }
+
+        [Fact]
+        public async Task BindingBehavior_OptionalIsOptional()
+        {
+            // Arrange
+            var server = TestHelper.CreateServer(_app, SiteName, _configureServices);
+            var client = server.CreateClient();
+
+            var url = "http://localhost/BindingBehavior/EchoModelValues";
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            var formData = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("model.BehaviourRequiredProperty", "Hello"),
+                new KeyValuePair<string, string>("model.BindRequiredProperty", "World!"),
+            };
+
+            request.Content = new FormUrlEncodedContent(formData);
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var body = await response.Content.ReadAsStringAsync();
+            var model = JsonConvert.DeserializeObject<BindingBehaviorModel>(body);
+
+            Assert.Null(model.BehaviourNeverProperty);
+            Assert.Null(model.BehaviourOptionalProperty);
+            Assert.Equal("Hello", model.BehaviourRequiredProperty);
+            Assert.Equal("World!", model.BindRequiredProperty);
+            Assert.Null(model.BindNeverProperty);
+        }
+
+        [Fact]
+        public async Task BindingBehavior_Never_IsNotBound()
+        {
+            // Arrange
+            var server = TestHelper.CreateServer(_app, SiteName, _configureServices);
+            var client = server.CreateClient();
+
+            var url = "http://localhost/BindingBehavior/EchoModelValues";
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            var formData = new List<KeyValuePair<string, string>>
+            {
+
+                new KeyValuePair<string, string>("model.BehaviourNeverProperty", "Ignored"),
+                new KeyValuePair<string, string>("model.BehaviourOptionalProperty", "Optional"),
+                new KeyValuePair<string, string>("model.BehaviourRequiredProperty", "Hello"),
+                new KeyValuePair<string, string>("model.BindRequiredProperty", "World!"),
+                new KeyValuePair<string, string>("model.BindNeverProperty", "Ignored"),
+            };
+
+            request.Content = new FormUrlEncodedContent(formData);
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var body = await response.Content.ReadAsStringAsync();
+            var model = JsonConvert.DeserializeObject<BindingBehaviorModel>(body);
+
+            Assert.Null(model.BehaviourNeverProperty);
+            Assert.Equal("Optional", model.BehaviourOptionalProperty);
+            Assert.Equal("Hello", model.BehaviourRequiredProperty);
+            Assert.Equal("World!", model.BindRequiredProperty);
+            Assert.Null(model.BindNeverProperty);
+        }
+    }
+}

test/Microsoft.AspNet.Mvc.FunctionalTests/ModelBindingDataMemberRequiredTest.cs
@@ -1,0 +1,87 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+using ModelBindingWebSite;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Microsoft.AspNet.Mvc.FunctionalTests
+{
+    public class ModelBindingDataMemberRequiredTest
+    {
+        private const string SiteName = nameof(ModelBindingWebSite);
+        private readonly Action<IApplicationBuilder> _app = new Startup().Configure;
+        private readonly Action<IServiceCollection> _configureServices = new Startup().ConfigureServices;
+
+        [Fact]
+        public async Task DataMember_MissingRequiredProperty_ValidationError()
+        {
+            // Arrange
+            var server = TestHelper.CreateServer(_app, SiteName, _configureServices);
+            var client = server.CreateClient();
+
+            var url = "http://localhost/DataMemberRequired/EchoModelValues";
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            var formData = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("model.ExplicitlyOptionalProperty", "Hi"),
+            };
+
+            request.Content = new FormUrlEncodedContent(formData);
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+
+            var body = await response.Content.ReadAsStringAsync();
+            var errors = JsonConvert.DeserializeObject<SerializableError>(body);
+
+            Assert.Equal(1, errors.Count);
+
+            var error = Assert.Single(errors, kvp => kvp.Key == "model.RequiredProperty");
+            Assert.Equal("The 'RequiredProperty' property is required.", ((JArray)error.Value)[0].Value<string>());
+        }
+
+        [Fact]
+        public async Task DataMember_RequiredPropertyProvided_Success()
+        {
+            // Arrange
+            var server = TestHelper.CreateServer(_app, SiteName, _configureServices);
+            var client = server.CreateClient();
+
+            var url = "http://localhost/DataMemberRequired/EchoModelValues";
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            var formData = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("model.ImplicitlyOptionalProperty", "Hello"),
+                new KeyValuePair<string, string>("model.ExplicitlyOptionalProperty", "World!"),
+                new KeyValuePair<string, string>("model.RequiredProperty", "Required!"),
+            };
+
+            request.Content = new FormUrlEncodedContent(formData);
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var body = await response.Content.ReadAsStringAsync();
+            var model = JsonConvert.DeserializeObject<DataMemberRequiredModel>(body);
+
+            Assert.Equal("Hello", model.ImplicitlyOptionalProperty);
+            Assert.Equal("World!", model.ExplicitlyOptionalProperty);
+            Assert.Equal("Required!", model.RequiredProperty);
+        }
+    }
+}

test/WebSites/ModelBindingWebSite/Models/BindingBehaviorModel.cs
@@ -1,0 +1,25 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc.ModelBinding;
+
+namespace ModelBindingWebSite
+{
+    public class BindingBehaviorModel
+    {
+        [BindingBehavior(BindingBehavior.Never)]
+        public string BehaviourNeverProperty { get; set; }
+
+        [BindingBehavior(BindingBehavior.Optional)]
+        public string BehaviourOptionalProperty { get; set; }
+
+        [BindingBehavior(BindingBehavior.Required)]
+        public string BehaviourRequiredProperty { get; set; }
+
+        [BindRequired]
+        public string BindRequiredProperty { get; set; }
+
+        [BindNever]
+        public string BindNeverProperty { get; set; }
+    }
+}

test/WebSites/ModelBindingWebSite/Models/DataMemberRequiredModel.cs
@@ -1,0 +1,20 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Runtime.Serialization;
+
+namespace ModelBindingWebSite
+{
+    [DataContract]
+    public class DataMemberRequiredModel
+    {
+        [DataMember]
+        public string ImplicitlyOptionalProperty { get; set; }
+
+        [DataMember(IsRequired = false)]
+        public string ExplicitlyOptionalProperty { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public string RequiredProperty { get; set; }
+    }
+}