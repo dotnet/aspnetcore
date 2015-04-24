59e40904ad6d6cbd68f4c1c3eecda73a02973c03
Chris Ross <Tratcher@Outlook.com>
2015-04-24 09:50:36 -0700

    Remove redundant websocket sample code.

--- samples/SampleApp/Startup.cs
+++ samples/SampleApp/Startup.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNet.Builder;
+﻿using System;
+using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
-using System;
-using System.Net.WebSockets;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace SampleApp
 {
@@ -19,44 +16,10 @@ namespace SampleApp
                     context.Request.Path,
                     context.Request.QueryString);
 
-                if (context.IsWebSocketRequest)
-                {
-                    var webSocket = await context.AcceptWebSocketAsync();
-                    await EchoAsync(webSocket);
-                }
-                else
-                {
-                    context.Response.ContentLength = 11;
-                    context.Response.ContentType = "text/plain";
-                    await context.Response.WriteAsync("Hello world");
-                }
+                context.Response.ContentLength = 11;
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync("Hello world");
             });
-        }
-
-        public async Task EchoAsync(WebSocket webSocket)
-        {
-            var buffer = new ArraySegment<byte>(new byte[8192]);
-            for (; ;)
-            {
-                var result = await webSocket.ReceiveAsync(
-                    buffer,
-                    CancellationToken.None);
-
-                if (result.MessageType == WebSocketMessageType.Close)
-                {
-                    return;
-                }
-                else if (result.MessageType == WebSocketMessageType.Text)
-                {
-                    Console.WriteLine("{0}", System.Text.Encoding.UTF8.GetString(buffer.Array, 0, result.Count));
-                }
-
-                await webSocket.SendAsync(
-                    new ArraySegment<byte>(buffer.Array, 0, result.Count),
-                    result.MessageType,
-                    result.EndOfMessage,
-                    CancellationToken.None);
-            }
         }
     }
 }
