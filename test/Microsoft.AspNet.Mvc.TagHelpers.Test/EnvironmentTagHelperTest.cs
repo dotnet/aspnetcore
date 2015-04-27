LordZoltan/Mvc

React to aspnet/Razor#279 changes.
- Updated all tests and TagHelpers to utilize the new TagHelperOutput.Attributes and TagHelperContext.AllAttribute types.

aspnet/Razor#279

test/Microsoft.AspNet.Mvc.TagHelpers.Test/EnvironmentTagHelperTest.cs
@@ -80,7 +80,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers.Test
             // Arrange
             var content = "content";
             var context = MakeTagHelperContext(
-                attributes: new Dictionary<string, object> { { "names", namesAttribute } },
+                attributes: new TagHelperAttributes { { "names", namesAttribute } },
                 content: content);
             var output = MakeTagHelperOutput("environment");
             var hostingEnvironment = new Mock<IHostingEnvironment>();
@@ -108,7 +108,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers.Test
             // Arrange
             var content = "content";
             var context = MakeTagHelperContext(
-                attributes: new Dictionary<string, object> { { "names", namesAttribute } },
+                attributes: new TagHelperAttributes { { "names", namesAttribute } },
                 content: content);
             var output = MakeTagHelperOutput("environment");
             var hostingEnvironment = new Mock<IHostingEnvironment>();
@@ -129,10 +129,10 @@ namespace Microsoft.AspNet.Mvc.TagHelpers.Test
         }
 
         private TagHelperContext MakeTagHelperContext(
-            IDictionary<string, object> attributes = null,
+            TagHelperAttributes attributes = null,
             string content = null)
         {
-            attributes = attributes ?? new Dictionary<string, object>();
+            attributes = attributes ?? new TagHelperAttributes();
 
             return new TagHelperContext(
                 attributes,
@@ -146,9 +146,9 @@ namespace Microsoft.AspNet.Mvc.TagHelpers.Test
                 });
         }
 
-        private TagHelperOutput MakeTagHelperOutput(string tagName, IDictionary<string, object> attributes = null)
+        private TagHelperOutput MakeTagHelperOutput(string tagName, TagHelperAttributes attributes = null)
         {
-            attributes = attributes ?? new Dictionary<string, object>();
+            attributes = attributes ?? new TagHelperAttributes();
 
             return new TagHelperOutput(tagName, attributes);
         }

test/Microsoft.AspNet.Mvc.TagHelpers.Test/LabelTagHelperTest.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc.ModelBinding;
 using Microsoft.AspNet.Mvc.Rendering;
@@ -164,7 +165,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
         {
             // Arrange
             var expectedTagName = "not-label";
-            var expectedAttributes = new Dictionary<string, object>
+            var expectedAttributes = new TagHelperAttributes
             {
                 { "class", "form-control" },
                 { "for", tagHelperOutputContent.ExpectedId }
@@ -186,7 +187,8 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var expectedPostContent = "original post-content";
 
             var tagHelperContext = new TagHelperContext(
-                allAttributes: new Dictionary<string, object>(),
+                allAttributes: new ReadOnlyTagHelperAttributes<IReadOnlyTagHelperAttribute>(
+                    Enumerable.Empty<IReadOnlyTagHelperAttribute>()),
                 items: new Dictionary<object, object>(),
                 uniqueId: "test",
                 getChildContentAsync: () =>
@@ -195,7 +197,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
                     tagHelperContent.SetContent(tagHelperOutputContent.OriginalChildContent);
                     return Task.FromResult<TagHelperContent>(tagHelperContent);
                 });
-            var htmlAttributes = new Dictionary<string, object>
+            var htmlAttributes = new TagHelperAttributes
             {
                 { "class", "form-control" },
             };

test/Microsoft.AspNet.Mvc.TagHelpers.Test/SelectTagHelperTest.cs
@@ -171,13 +171,13 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             string ignored)
         {
             // Arrange
-            var originalAttributes = new Dictionary<string, object>
+            var originalAttributes = new TagHelperAttributes
             {
                 { "class", "form-control" },
             };
             var originalPostContent = "original content";
 
-            var expectedAttributes = new Dictionary<string, object>(originalAttributes)
+            var expectedAttributes = new TagHelperAttributes(originalAttributes)
             {
                 { "id", nameAndId.Id },
                 { "name", nameAndId.Name },
@@ -198,7 +198,8 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var modelExpression = new ModelExpression(nameAndId.Name, modelExplorer);
 
             var tagHelperContext = new TagHelperContext(
-                allAttributes: new Dictionary<string, object>(),
+                allAttributes: new ReadOnlyTagHelperAttributes<IReadOnlyTagHelperAttribute>(
+                    Enumerable.Empty<IReadOnlyTagHelperAttribute>()),
                 items: new Dictionary<object, object>(),
                 uniqueId: "test",
                 getChildContentAsync: () =>
@@ -257,13 +258,13 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             string expectedOptions)
         {
             // Arrange
-            var originalAttributes = new Dictionary<string, object>
+            var originalAttributes = new TagHelperAttributes
             {
                 { "class", "form-control" },
             };
             var originalPostContent = "original content";
 
-            var expectedAttributes = new Dictionary<string, object>(originalAttributes)
+            var expectedAttributes = new TagHelperAttributes(originalAttributes)
             {
                 { "id", nameAndId.Id },
                 { "name", nameAndId.Name },
@@ -285,7 +286,8 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var modelExpression = new ModelExpression(nameAndId.Name, modelExplorer);
 
             var tagHelperContext = new TagHelperContext(
-                allAttributes: new Dictionary<string, object>(),
+                allAttributes: new ReadOnlyTagHelperAttributes<IReadOnlyTagHelperAttribute>(
+                    Enumerable.Empty<IReadOnlyTagHelperAttribute>()),
                 items: new Dictionary<object, object>(),
                 uniqueId: "test",
                 getChildContentAsync: () =>
@@ -358,13 +360,13 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             string expectedOptions)
         {
             // Arrange
-            var originalAttributes = new Dictionary<string, object>
+            var originalAttributes = new TagHelperAttributes
             {
                 { "class", "form-control" },
             };
             var originalPostContent = "original content";
 
-            var expectedAttributes = new Dictionary<string, object>(originalAttributes)
+            var expectedAttributes = new TagHelperAttributes(originalAttributes)
             {
                 { "id", nameAndId.Id },
                 { "name", nameAndId.Name },
@@ -386,7 +388,8 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var modelExpression = new ModelExpression(name: string.Empty, modelExplorer: modelExplorer);
 
             var tagHelperContext = new TagHelperContext(
-                allAttributes: new Dictionary<string, object>(),
+                allAttributes: new ReadOnlyTagHelperAttributes<IReadOnlyTagHelperAttribute>(
+                    Enumerable.Empty<IReadOnlyTagHelperAttribute>()),
                 items: new Dictionary<object, object>(),
                 uniqueId: "test",
                 getChildContentAsync: () =>
@@ -459,12 +462,12 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             IEnumerable<SelectListItem> expectedItems)
         {
             // Arrange
-            var contextAttributes = new Dictionary<string, object>
+            var contextAttributes = new TagHelperAttributes
             {
                 // Provided for completeness. Select tag helper does not confirm AllAttributes set is consistent.
                 { attributeName, attributeValue },
             };
-            var originalAttributes = new Dictionary<string, object>
+            var originalAttributes = new TagHelperAttributes
             {
                 { attributeName, attributeValue },
             };
@@ -545,8 +548,9 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             bool allowMultiple)
         {
             // Arrange
-            var contextAttributes = new Dictionary<string, object>();
-            var originalAttributes = new Dictionary<string, object>();
+            var contextAttributes = new ReadOnlyTagHelperAttributes<IReadOnlyTagHelperAttribute>(
+                    Enumerable.Empty<IReadOnlyTagHelperAttribute>());
+            var originalAttributes = new TagHelperAttributes();
             var propertyName = "Property1";
             var tagName = "select";
 

test/Microsoft.AspNet.Mvc.TagHelpers.Test/ValidationSummaryTagHelperTest.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Mvc.ModelBinding;
@@ -44,7 +45,8 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var expectedPreContent = "original pre-content";
             var expectedContent = "original content";
             var tagHelperContext = new TagHelperContext(
-                allAttributes: new Dictionary<string, object>(),
+                allAttributes: new ReadOnlyTagHelperAttributes<IReadOnlyTagHelperAttribute>(
+                    Enumerable.Empty<IReadOnlyTagHelperAttribute>()),
                 items: new Dictionary<object, object>(),
                 uniqueId: "test",
                 getChildContentAsync: () =>
@@ -55,7 +57,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
                 });
             var output = new TagHelperOutput(
                 expectedTagName,
-                attributes: new Dictionary<string, object>
+                attributes: new TagHelperAttributes
                 {
                     { "class", "form-control" }
                 });
@@ -74,9 +76,9 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
 
             // Assert
             Assert.Equal(2, output.Attributes.Count);
-            var attribute = Assert.Single(output.Attributes, kvp => kvp.Key.Equals("class"));
+            var attribute = Assert.Single(output.Attributes, attr => attr.Name.Equals("class"));
             Assert.Equal("form-control validation-summary-valid", attribute.Value);
-            attribute = Assert.Single(output.Attributes, kvp => kvp.Key.Equals("data-valmsg-summary"));
+            attribute = Assert.Single(output.Attributes, attr => attr.Name.Equals("data-valmsg-summary"));
             Assert.Equal("true", attribute.Value);
             Assert.Equal(expectedPreContent, output.PreContent.GetContent());
             Assert.Equal(expectedContent, output.Content.GetContent());
@@ -102,7 +104,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var expectedPostContent = "original post-content";
             var output = new TagHelperOutput(
                 "div",
-                attributes: new Dictionary<string, object>());
+                attributes: new TagHelperAttributes());
             output.PreContent.SetContent(expectedPreContent);
             output.Content.SetContent(expectedContent);
             output.PostContent.SetContent(expectedPostContent);
@@ -144,7 +146,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var expectedContent = "original content";
             var output = new TagHelperOutput(
                 "div",
-                attributes: new Dictionary<string, object>());
+                attributes: new TagHelperAttributes());
             output.PreContent.SetContent(expectedPreContent);
             output.Content.SetContent(expectedContent);
             output.PostContent.SetContent("Content of validation summary");
@@ -177,11 +179,11 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             // Assert
             Assert.Equal("div", output.TagName);
             Assert.Equal(3, output.Attributes.Count);
-            var attribute = Assert.Single(output.Attributes, kvp => kvp.Key.Equals("data-foo"));
+            var attribute = Assert.Single(output.Attributes, attr => attr.Name.Equals("data-foo"));
             Assert.Equal("bar", attribute.Value);
-            attribute = Assert.Single(output.Attributes, kvp => kvp.Key.Equals("data-hello"));
+            attribute = Assert.Single(output.Attributes, attr => attr.Name.Equals("data-hello"));
             Assert.Equal("world", attribute.Value);
-            attribute = Assert.Single(output.Attributes, kvp => kvp.Key.Equals("anything"));
+            attribute = Assert.Single(output.Attributes, attr => attr.Name.Equals("anything"));
             Assert.Equal("something", attribute.Value);
             Assert.Equal(expectedPreContent, output.PreContent.GetContent());
             Assert.Equal(expectedContent, output.Content.GetContent());
@@ -201,7 +203,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var expectedPostContent = "original post-content";
             var output = new TagHelperOutput(
                 "div",
-                attributes: new Dictionary<string, object>());
+                attributes: new TagHelperAttributes());
             output.PreContent.SetContent(expectedPreContent);
             output.Content.SetContent(expectedContent);
             output.PostContent.SetContent(expectedPostContent);
@@ -236,7 +238,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var expectedContent = "original content";
             var output = new TagHelperOutput(
                 "div",
-                attributes: new Dictionary<string, object>());
+                attributes: new TagHelperAttributes());
             output.PreContent.SetContent(expectedPreContent);
             output.Content.SetContent(expectedContent);
             output.PostContent.SetContent("Content of validation message");

dotnet/aspnetcore

test/Microsoft.AspNet.Mvc.TagHelpers.Test/EnvironmentTagHelperTest.cs
@@ -80,7 +80,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers.Test
             // Arrange
             var content = "content";
             var context = MakeTagHelperContext(
-                attributes: new Dictionary<string, object> { { "names", namesAttribute } },
+                attributes: new TagHelperAttributeList { { "names", namesAttribute } },
                 content: content);
             var output = MakeTagHelperOutput("environment");
             var hostingEnvironment = new Mock<IHostingEnvironment>();
@@ -108,7 +108,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers.Test
             // Arrange
             var content = "content";
             var context = MakeTagHelperContext(
-                attributes: new Dictionary<string, object> { { "names", namesAttribute } },
+                attributes: new TagHelperAttributeList { { "names", namesAttribute } },
                 content: content);
             var output = MakeTagHelperOutput("environment");
             var hostingEnvironment = new Mock<IHostingEnvironment>();
@@ -129,10 +129,10 @@ namespace Microsoft.AspNet.Mvc.TagHelpers.Test
         }
 
         private TagHelperContext MakeTagHelperContext(
-            IDictionary<string, object> attributes = null,
+            TagHelperAttributeList attributes = null,
             string content = null)
         {
-            attributes = attributes ?? new Dictionary<string, object>();
+            attributes = attributes ?? new TagHelperAttributeList();
 
             return new TagHelperContext(
                 attributes,
@@ -146,9 +146,9 @@ namespace Microsoft.AspNet.Mvc.TagHelpers.Test
                 });
         }
 
-        private TagHelperOutput MakeTagHelperOutput(string tagName, IDictionary<string, object> attributes = null)
+        private TagHelperOutput MakeTagHelperOutput(string tagName, TagHelperAttributeList attributes = null)
         {
-            attributes = attributes ?? new Dictionary<string, object>();
+            attributes = attributes ?? new TagHelperAttributeList();
 
             return new TagHelperOutput(tagName, attributes);
         }

test/Microsoft.AspNet.Mvc.TagHelpers.Test/LabelTagHelperTest.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc.ModelBinding;
 using Microsoft.AspNet.Mvc.Rendering;
@@ -164,7 +165,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
         {
             // Arrange
             var expectedTagName = "not-label";
-            var expectedAttributes = new Dictionary<string, object>
+            var expectedAttributes = new TagHelperAttributeList
             {
                 { "class", "form-control" },
                 { "for", tagHelperOutputContent.ExpectedId }
@@ -186,7 +187,8 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var expectedPostContent = "original post-content";
 
             var tagHelperContext = new TagHelperContext(
-                allAttributes: new Dictionary<string, object>(),
+                allAttributes: new ReadOnlyTagHelperAttributeList<IReadOnlyTagHelperAttribute>(
+                    Enumerable.Empty<IReadOnlyTagHelperAttribute>()),
                 items: new Dictionary<object, object>(),
                 uniqueId: "test",
                 getChildContentAsync: () =>
@@ -195,7 +197,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
                     tagHelperContent.SetContent(tagHelperOutputContent.OriginalChildContent);
                     return Task.FromResult<TagHelperContent>(tagHelperContent);
                 });
-            var htmlAttributes = new Dictionary<string, object>
+            var htmlAttributes = new TagHelperAttributeList
             {
                 { "class", "form-control" },
             };

test/Microsoft.AspNet.Mvc.TagHelpers.Test/SelectTagHelperTest.cs
@@ -171,13 +171,13 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             string ignored)
         {
             // Arrange
-            var originalAttributes = new Dictionary<string, object>
+            var originalAttributes = new TagHelperAttributeList
             {
                 { "class", "form-control" },
             };
             var originalPostContent = "original content";
 
-            var expectedAttributes = new Dictionary<string, object>(originalAttributes)
+            var expectedAttributes = new TagHelperAttributeList(originalAttributes)
             {
                 { "id", nameAndId.Id },
                 { "name", nameAndId.Name },
@@ -198,7 +198,8 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var modelExpression = new ModelExpression(nameAndId.Name, modelExplorer);
 
             var tagHelperContext = new TagHelperContext(
-                allAttributes: new Dictionary<string, object>(),
+                allAttributes: new ReadOnlyTagHelperAttributeList<IReadOnlyTagHelperAttribute>(
+                    Enumerable.Empty<IReadOnlyTagHelperAttribute>()),
                 items: new Dictionary<object, object>(),
                 uniqueId: "test",
                 getChildContentAsync: () =>
@@ -257,13 +258,13 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             string expectedOptions)
         {
             // Arrange
-            var originalAttributes = new Dictionary<string, object>
+            var originalAttributes = new TagHelperAttributeList
             {
                 { "class", "form-control" },
             };
             var originalPostContent = "original content";
 
-            var expectedAttributes = new Dictionary<string, object>(originalAttributes)
+            var expectedAttributes = new TagHelperAttributeList(originalAttributes)
             {
                 { "id", nameAndId.Id },
                 { "name", nameAndId.Name },
@@ -285,7 +286,8 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var modelExpression = new ModelExpression(nameAndId.Name, modelExplorer);
 
             var tagHelperContext = new TagHelperContext(
-                allAttributes: new Dictionary<string, object>(),
+                allAttributes: new ReadOnlyTagHelperAttributeList<IReadOnlyTagHelperAttribute>(
+                    Enumerable.Empty<IReadOnlyTagHelperAttribute>()),
                 items: new Dictionary<object, object>(),
                 uniqueId: "test",
                 getChildContentAsync: () =>
@@ -358,13 +360,13 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             string expectedOptions)
         {
             // Arrange
-            var originalAttributes = new Dictionary<string, object>
+            var originalAttributes = new TagHelperAttributeList
             {
                 { "class", "form-control" },
             };
             var originalPostContent = "original content";
 
-            var expectedAttributes = new Dictionary<string, object>(originalAttributes)
+            var expectedAttributes = new TagHelperAttributeList(originalAttributes)
             {
                 { "id", nameAndId.Id },
                 { "name", nameAndId.Name },
@@ -386,7 +388,8 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var modelExpression = new ModelExpression(name: string.Empty, modelExplorer: modelExplorer);
 
             var tagHelperContext = new TagHelperContext(
-                allAttributes: new Dictionary<string, object>(),
+                allAttributes: new ReadOnlyTagHelperAttributeList<IReadOnlyTagHelperAttribute>(
+                    Enumerable.Empty<IReadOnlyTagHelperAttribute>()),
                 items: new Dictionary<object, object>(),
                 uniqueId: "test",
                 getChildContentAsync: () =>
@@ -459,12 +462,12 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             IEnumerable<SelectListItem> expectedItems)
         {
             // Arrange
-            var contextAttributes = new Dictionary<string, object>
+            var contextAttributes = new TagHelperAttributeList
             {
                 // Provided for completeness. Select tag helper does not confirm AllAttributes set is consistent.
                 { attributeName, attributeValue },
             };
-            var originalAttributes = new Dictionary<string, object>
+            var originalAttributes = new TagHelperAttributeList
             {
                 { attributeName, attributeValue },
             };
@@ -545,8 +548,9 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             bool allowMultiple)
         {
             // Arrange
-            var contextAttributes = new Dictionary<string, object>();
-            var originalAttributes = new Dictionary<string, object>();
+            var contextAttributes = new ReadOnlyTagHelperAttributeList<IReadOnlyTagHelperAttribute>(
+                    Enumerable.Empty<IReadOnlyTagHelperAttribute>());
+            var originalAttributes = new TagHelperAttributeList();
             var propertyName = "Property1";
             var tagName = "select";
 

test/Microsoft.AspNet.Mvc.TagHelpers.Test/ValidationSummaryTagHelperTest.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Mvc.ModelBinding;
@@ -44,7 +45,8 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var expectedPreContent = "original pre-content";
             var expectedContent = "original content";
             var tagHelperContext = new TagHelperContext(
-                allAttributes: new Dictionary<string, object>(),
+                allAttributes: new ReadOnlyTagHelperAttributeList<IReadOnlyTagHelperAttribute>(
+                    Enumerable.Empty<IReadOnlyTagHelperAttribute>()),
                 items: new Dictionary<object, object>(),
                 uniqueId: "test",
                 getChildContentAsync: () =>
@@ -55,7 +57,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
                 });
             var output = new TagHelperOutput(
                 expectedTagName,
-                attributes: new Dictionary<string, object>
+                attributes: new TagHelperAttributeList
                 {
                     { "class", "form-control" }
                 });
@@ -74,9 +76,9 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
 
             // Assert
             Assert.Equal(2, output.Attributes.Count);
-            var attribute = Assert.Single(output.Attributes, kvp => kvp.Key.Equals("class"));
+            var attribute = Assert.Single(output.Attributes, attr => attr.Name.Equals("class"));
             Assert.Equal("form-control validation-summary-valid", attribute.Value);
-            attribute = Assert.Single(output.Attributes, kvp => kvp.Key.Equals("data-valmsg-summary"));
+            attribute = Assert.Single(output.Attributes, attr => attr.Name.Equals("data-valmsg-summary"));
             Assert.Equal("true", attribute.Value);
             Assert.Equal(expectedPreContent, output.PreContent.GetContent());
             Assert.Equal(expectedContent, output.Content.GetContent());
@@ -102,7 +104,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var expectedPostContent = "original post-content";
             var output = new TagHelperOutput(
                 "div",
-                attributes: new Dictionary<string, object>());
+                attributes: new TagHelperAttributeList());
             output.PreContent.SetContent(expectedPreContent);
             output.Content.SetContent(expectedContent);
             output.PostContent.SetContent(expectedPostContent);
@@ -144,7 +146,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var expectedContent = "original content";
             var output = new TagHelperOutput(
                 "div",
-                attributes: new Dictionary<string, object>());
+                attributes: new TagHelperAttributeList());
             output.PreContent.SetContent(expectedPreContent);
             output.Content.SetContent(expectedContent);
             output.PostContent.SetContent("Content of validation summary");
@@ -177,11 +179,11 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             // Assert
             Assert.Equal("div", output.TagName);
             Assert.Equal(3, output.Attributes.Count);
-            var attribute = Assert.Single(output.Attributes, kvp => kvp.Key.Equals("data-foo"));
+            var attribute = Assert.Single(output.Attributes, attr => attr.Name.Equals("data-foo"));
             Assert.Equal("bar", attribute.Value);
-            attribute = Assert.Single(output.Attributes, kvp => kvp.Key.Equals("data-hello"));
+            attribute = Assert.Single(output.Attributes, attr => attr.Name.Equals("data-hello"));
             Assert.Equal("world", attribute.Value);
-            attribute = Assert.Single(output.Attributes, kvp => kvp.Key.Equals("anything"));
+            attribute = Assert.Single(output.Attributes, attr => attr.Name.Equals("anything"));
             Assert.Equal("something", attribute.Value);
             Assert.Equal(expectedPreContent, output.PreContent.GetContent());
             Assert.Equal(expectedContent, output.Content.GetContent());
@@ -201,7 +203,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var expectedPostContent = "original post-content";
             var output = new TagHelperOutput(
                 "div",
-                attributes: new Dictionary<string, object>());
+                attributes: new TagHelperAttributeList());
             output.PreContent.SetContent(expectedPreContent);
             output.Content.SetContent(expectedContent);
             output.PostContent.SetContent(expectedPostContent);
@@ -236,7 +238,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
             var expectedContent = "original content";
             var output = new TagHelperOutput(
                 "div",
-                attributes: new Dictionary<string, object>());
+                attributes: new TagHelperAttributeList());
             output.PreContent.SetContent(expectedPreContent);
             output.Content.SetContent(expectedContent);
             output.PostContent.SetContent("Content of validation message");