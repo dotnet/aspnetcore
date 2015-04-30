602f638a8c08c8473eeb299380036d4cd74615d3
Kiran Challa <kichalla@microsoft.com>
2015-04-30 13:21:14 -0700

    Remove EncoderServices and add default services directly in the service collection extension

--- src/Microsoft.Framework.WebEncoders/EncoderServiceCollectionExtensions.cs
+++ src/Microsoft.Framework.WebEncoders/EncoderServiceCollectionExtensions.cs
@@ -3,6 +3,7 @@
 
 using System;
 using Microsoft.Framework.Internal;
+using Microsoft.Framework.OptionsModel;
 using Microsoft.Framework.WebEncoders;
 
 namespace Microsoft.Framework.DependencyInjection
@@ -17,12 +18,35 @@ namespace Microsoft.Framework.DependencyInjection
         public static IServiceCollection AddWebEncoders([NotNull] this IServiceCollection services, Action<WebEncoderOptions> configureOptions)
         {
             services.AddOptions();
-            services.TryAdd(EncoderServices.GetDefaultServices());
+
+            // Register the default encoders
+            // We want to call the 'Default' property getters lazily since they perform static caching
+            services.TryAdd(ServiceDescriptor.Singleton<IHtmlEncoder>(
+                CreateFactory(() => HtmlEncoder.Default, filter => new HtmlEncoder(filter))));
+            services.TryAdd(ServiceDescriptor.Singleton<IJavaScriptStringEncoder>(
+                CreateFactory(() => JavaScriptStringEncoder.Default, filter => new JavaScriptStringEncoder(filter))));
+            services.TryAdd(ServiceDescriptor.Singleton<IUrlEncoder>(
+                CreateFactory(() => UrlEncoder.Default, filter => new UrlEncoder(filter))));
+
             if (configureOptions != null)
             {
                 services.Configure(configureOptions);
             }
+
             return services;
+        }
+
+        private static Func<IServiceProvider, T> CreateFactory<T>(
+            Func<T> defaultFactory,
+            Func<ICodePointFilter, T> customFilterFactory)
+        {
+            return serviceProvider =>
+            {
+                var codePointFilter = serviceProvider?.GetService<IOptions<WebEncoderOptions>>()?
+                                                      .Options?
+                                                      .CodePointFilter;
+                return (codePointFilter != null) ? customFilterFactory(codePointFilter) : defaultFactory();
+            };
         }
     }
 }
