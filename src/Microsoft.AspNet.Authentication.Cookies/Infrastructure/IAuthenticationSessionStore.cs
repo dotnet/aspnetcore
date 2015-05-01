5cf0564484cf5bb2a7a16e6485816d19287538e6
N. Taylor Mullen <nimullen@microsoft.com>
2015-05-01 14:00:05 -0700

    Update LICENSE.txt and license header on files.

--- src/Microsoft.AspNet.Authentication.Cookies/Infrastructure/IAuthenticationSessionStore.cs
+++ src/Microsoft.AspNet.Authentication.Cookies/Infrastructure/IAuthenticationSessionStore.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+// Copyright (c) .NET Foundation. All rights reserved. See License.txt in the project root for license information.
 
 using System.Threading.Tasks;
 
