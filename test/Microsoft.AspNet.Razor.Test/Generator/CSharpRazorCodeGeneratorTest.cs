zhangx3000/Razor

[Fixes #50] Removed unnecessary whitespaces before text tag

test/Microsoft.AspNet.Razor.Test/Generator/CSharpRazorCodeGeneratorTest.cs
@@ -78,6 +78,7 @@ namespace Microsoft.AspNet.Razor.Test.Generator
         [InlineData("ConditionalAttributes")]
         [InlineData("ResolveUrl")]
         [InlineData("Await")]
+        [InlineData("SimpleCodeBlock")]
         public void CSharpCodeGeneratorCorrectlyGeneratesRunTimeCode(string testType)
         {
             RunTest(testType);

dotnet/aspnetcore

test/Microsoft.AspNet.Razor.Test/Generator/CSharpRazorCodeGeneratorTest.cs
@@ -66,6 +66,7 @@ namespace Microsoft.AspNet.Razor.Test.Generator
         [InlineData("ConditionalAttributes")]
         [InlineData("ResolveUrl")]
         [InlineData("Await")]
+        [InlineData("CodeBlockWithTextElement")]
         public void CSharpCodeGeneratorCorrectlyGeneratesRunTimeCode(string testType)
         {
             RunTest(testType);