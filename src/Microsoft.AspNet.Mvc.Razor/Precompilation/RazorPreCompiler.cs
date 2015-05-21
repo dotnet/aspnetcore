Inner89/Mvc

React to Caching api review changes

src/Microsoft.AspNet.Mvc.Razor/Precompilation/RazorPreCompiler.cs
@@ -91,9 +91,14 @@ namespace Microsoft.AspNet.Mvc.Razor.Precompilation
             Parallel.For(0, filesToProcess.Count, parallelOptions, index =>
             {
                 var file = filesToProcess[index];
-                var cacheEntry = PreCompilationCache.GetOrSet(file.RelativePath,
-                                                              file,
-                                                              OnCacheMiss);
+
+                PrecompilationCacheEntry cacheEntry;
+                if(!PreCompilationCache.TryGetValue(file.RelativePath, out cacheEntry))
+                {
+                    cacheEntry = GetCacheEntry(file);
+                    PreCompilationCache.Set(file.RelativePath, cacheEntry, GetCacheEntryOptions(file, cacheEntry));
+                }
+
                 if (cacheEntry != null)
                 {
                     if (cacheEntry.Success)
@@ -190,21 +195,17 @@ namespace Microsoft.AspNet.Mvc.Razor.Precompilation
             };
         }
 
-        private PrecompilationCacheEntry OnCacheMiss(ICacheSetContext cacheSetContext)
+        private CacheEntryOptions GetCacheEntryOptions(
+            RelativeFileInfo fileInfo,
+            PrecompilationCacheEntry cacheEntry)
         {
-            var fileInfo = (RelativeFileInfo)cacheSetContext.State;
-            var entry = GetCacheEntry(fileInfo);
-
-            if (entry != null)
+            var options = new CacheEntryOptions();
+            options.AddExpirationTrigger(FileProvider.Watch(fileInfo.RelativePath));
+            foreach (var path in ViewHierarchyUtility.GetGlobalImportLocations(fileInfo.RelativePath))
             {
-                cacheSetContext.AddExpirationTrigger(FileProvider.Watch(fileInfo.RelativePath));
-                foreach (var path in ViewHierarchyUtility.GetGlobalImportLocations(fileInfo.RelativePath))
-                {
-                    cacheSetContext.AddExpirationTrigger(FileProvider.Watch(path));
-                }
+                options.AddExpirationTrigger(FileProvider.Watch(path));
             }
-
-            return entry;
+            return options;
         }
 
         private void GetFileInfosRecursive(string root, List<RelativeFileInfo> razorFiles)

src/Microsoft.AspNet.Mvc.TagHelpers/Internal/GlobbingUrlBuilder.cs
@@ -101,16 +101,21 @@ namespace Microsoft.AspNet.Mvc.TagHelpers.Internal
             if (Cache != null)
             {
                 var cacheKey = $"{nameof(GlobbingUrlBuilder)}-inc:{include}-exc:{exclude}";
-                return Cache.GetOrSet(cacheKey, cacheSetContext =>
+                IEnumerable<string> files;
+                if (!Cache.TryGetValue(cacheKey, out files))
                 {
+                    var options = new CacheEntryOptions();
                     foreach (var pattern in includePatterns)
                     {
                         var trigger = FileProvider.Watch(pattern);
-                        cacheSetContext.AddExpirationTrigger(trigger);
+                        options.AddExpirationTrigger(trigger);
                     }
 
-                    return FindFiles(includePatterns, excludePatterns);
-                });
+                    files = FindFiles(includePatterns, excludePatterns);
+
+                    Cache.Set(cacheKey, files, options);
+                }
+                return files;
             }
 
             return FindFiles(includePatterns, excludePatterns);
@@ -175,7 +180,7 @@ namespace Microsoft.AspNet.Mvc.TagHelpers.Internal
                     // Only extension differs so just compare the extension
                     var xExt = xExtIndex >= 0 ? x.Substring(xExtIndex) : string.Empty;
                     var yExt = yExtIndex >= 0 ? y.Substring(yExtIndex) : string.Empty;
-                    
+
                     return string.Compare(xExt, yExt, StringComparison.Ordinal);
                 }
 

test/Microsoft.AspNet.Mvc.TagHelpers.Test/ScriptTagHelperTest.cs
@@ -842,29 +842,18 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
         {
             var cache = new Mock<IMemoryCache>();
             cache.CallBase = true;
-            cache.Setup(c => c.TryGetValue(It.IsAny<string>(), It.IsAny<IEntryLink>(), out result))
+            cache.Setup(c => c.TryGetValue(It.IsAny<string>(), out result))
                 .Returns(result != null);
 
-            var cacheSetContext = new Mock<ICacheSetContext>();
-            cacheSetContext.Setup(c => c.AddExpirationTrigger(It.IsAny<IExpirationTrigger>()));
+            var cacheEntryOptions = new CacheEntryOptions();
+            cacheEntryOptions.AddExpirationTrigger(new Mock<IExpirationTrigger>().Object);
             cache
                 .Setup(
                     c => c.Set(
                         /*key*/ It.IsAny<string>(),
-                        /*link*/ It.IsAny<IEntryLink>(),
-                        /*state*/ It.IsAny<object>(),
-                        /*create*/ It.IsAny<Func<ICacheSetContext, object>>()))
-                .Returns((
-                    string input,
-                    IEntryLink entryLink,
-                    object state,
-                    Func<ICacheSetContext, object> create) =>
-                {
-                    {
-                        cacheSetContext.Setup(c => c.State).Returns(state);
-                        return create(cacheSetContext.Object);
-                    }
-                });
+                        /*value*/ It.IsAny<object>(),
+                        /*options*/ cacheEntryOptions))
+                .Returns(result);
             return cache.Object;
         }
     }

dotnet/aspnetcore

src/Microsoft.AspNet.Mvc.Razor/Precompilation/RazorPreCompiler.cs
@@ -91,9 +91,17 @@ namespace Microsoft.AspNet.Mvc.Razor.Precompilation
             Parallel.For(0, filesToProcess.Count, parallelOptions, index =>
             {
                 var file = filesToProcess[index];
-                var cacheEntry = PreCompilationCache.GetOrSet(file.RelativePath,
-                                                              file,
-                                                              OnCacheMiss);
+
+                PrecompilationCacheEntry cacheEntry;
+                if(!PreCompilationCache.TryGetValue(file.RelativePath, out cacheEntry))
+                {
+                    cacheEntry = GetCacheEntry(file);
+                    PreCompilationCache.Set(
+                        file.RelativePath,
+                        cacheEntry,
+                        GetMemoryCacheEntryOptions(file, cacheEntry));
+                }
+
                 if (cacheEntry != null)
                 {
                     if (cacheEntry.Success)
@@ -190,21 +198,17 @@ namespace Microsoft.AspNet.Mvc.Razor.Precompilation
             };
         }
 
-        private PrecompilationCacheEntry OnCacheMiss(ICacheSetContext cacheSetContext)
+        private MemoryCacheEntryOptions GetMemoryCacheEntryOptions(
+            RelativeFileInfo fileInfo,
+            PrecompilationCacheEntry cacheEntry)
         {
-            var fileInfo = (RelativeFileInfo)cacheSetContext.State;
-            var entry = GetCacheEntry(fileInfo);
-
-            if (entry != null)
+            var options = new MemoryCacheEntryOptions();
+            options.AddExpirationTrigger(FileProvider.Watch(fileInfo.RelativePath));
+            foreach (var path in ViewHierarchyUtility.GetViewImportsLocations(fileInfo.RelativePath))
             {
-                cacheSetContext.AddExpirationTrigger(FileProvider.Watch(fileInfo.RelativePath));
-                foreach (var path in ViewHierarchyUtility.GetViewImportsLocations(fileInfo.RelativePath))
-                {
-                    cacheSetContext.AddExpirationTrigger(FileProvider.Watch(path));
-                }
+                options.AddExpirationTrigger(FileProvider.Watch(path));
             }
-
-            return entry;
+            return options;
         }
 
         private void GetFileInfosRecursive(string root, List<RelativeFileInfo> razorFiles)

src/Microsoft.AspNet.Mvc.TagHelpers/Internal/GlobbingUrlBuilder.cs
@@ -101,16 +101,21 @@ namespace Microsoft.AspNet.Mvc.TagHelpers.Internal
             if (Cache != null)
             {
                 var cacheKey = $"{nameof(GlobbingUrlBuilder)}-inc:{include}-exc:{exclude}";
-                return Cache.GetOrSet(cacheKey, cacheSetContext =>
+                IEnumerable<string> files;
+                if (!Cache.TryGetValue(cacheKey, out files))
                 {
+                    var options = new MemoryCacheEntryOptions();
                     foreach (var pattern in includePatterns)
                     {
                         var trigger = FileProvider.Watch(pattern);
-                        cacheSetContext.AddExpirationTrigger(trigger);
+                        options.AddExpirationTrigger(trigger);
                     }
 
-                    return FindFiles(includePatterns, excludePatterns);
-                });
+                    files = FindFiles(includePatterns, excludePatterns);
+
+                    Cache.Set(cacheKey, files, options);
+                }
+                return files;
             }
 
             return FindFiles(includePatterns, excludePatterns);
@@ -175,7 +180,6 @@ namespace Microsoft.AspNet.Mvc.TagHelpers.Internal
                     // Only extension differs so just compare the extension
                     var xExt = xExtIndex >= 0 ? x.Substring(xExtIndex) : string.Empty;
                     var yExt = yExtIndex >= 0 ? y.Substring(yExtIndex) : string.Empty;
-                    
                     return string.Compare(xExt, yExt, StringComparison.Ordinal);
                 }
 

test/Microsoft.AspNet.Mvc.TagHelpers.Test/ScriptTagHelperTest.cs
@@ -842,29 +842,18 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
         {
             var cache = new Mock<IMemoryCache>();
             cache.CallBase = true;
-            cache.Setup(c => c.TryGetValue(It.IsAny<string>(), It.IsAny<IEntryLink>(), out result))
+            cache.Setup(c => c.TryGetValue(It.IsAny<string>(), out result))
                 .Returns(result != null);
 
-            var cacheSetContext = new Mock<ICacheSetContext>();
-            cacheSetContext.Setup(c => c.AddExpirationTrigger(It.IsAny<IExpirationTrigger>()));
+            var cacheEntryOptions = new MemoryCacheEntryOptions();
+            cacheEntryOptions.AddExpirationTrigger(new Mock<IExpirationTrigger>().Object);
             cache
                 .Setup(
                     c => c.Set(
                         /*key*/ It.IsAny<string>(),
-                        /*link*/ It.IsAny<IEntryLink>(),
-                        /*state*/ It.IsAny<object>(),
-                        /*create*/ It.IsAny<Func<ICacheSetContext, object>>()))
-                .Returns((
-                    string input,
-                    IEntryLink entryLink,
-                    object state,
-                    Func<ICacheSetContext, object> create) =>
-                {
-                    {
-                        cacheSetContext.Setup(c => c.State).Returns(state);
-                        return create(cacheSetContext.Object);
-                    }
-                });
+                        /*value*/ It.IsAny<object>(),
+                        /*options*/ cacheEntryOptions))
+                .Returns(result);
             return cache.Object;
         }
     }