355a2b0a783f7bd4539340792785ec5d2b3ca837
Ryan Nowak <nowakra@gmail.com>
2015-06-10 12:52:20 -0700

    Add an IServiceCollection extensions

---
+++ src/Microsoft.AspNet.Antiforgery/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNet.Antiforgery;
+using Microsoft.Framework.Internal;
+
+namespace Microsoft.Framework.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAntiforgery([NotNull] this IServiceCollection services)
+        {
+            services.AddDataProtection();
+
+            services.TryAdd(ServiceDescriptor.Singleton<IClaimUidExtractor, DefaultClaimUidExtractor>());
+            services.TryAdd(ServiceDescriptor.Singleton<Antiforgery, Antiforgery>());
+            services.TryAdd(
+                ServiceDescriptor.Singleton<IAntiforgeryAdditionalDataProvider, DefaultAntiforgeryAdditionalDataProvider>());
+            return services;
+        }
+
+        public static IServiceCollection ConfigureAntiforgery(
+            [NotNull] this IServiceCollection services,
+            [NotNull] Action<AntiforgeryOptions> setupAction)
+        {
+            services.Configure(setupAction);
+            return services;
+        }
+    }
+}
