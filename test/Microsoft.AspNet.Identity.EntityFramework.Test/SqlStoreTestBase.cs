6fa89f5da18f685321c0d54569820ccac0c048d8
Arthur Vickers <ajcvickers@hotmail.com>
2015-06-19 11:20:41 -0700

    Add missing using directive.

--- test/Microsoft.AspNet.Identity.EntityFramework.Test/SqlStoreTestBase.cs
+++ test/Microsoft.AspNet.Identity.EntityFramework.Test/SqlStoreTestBase.cs
@@ -8,6 +8,7 @@ using System.Linq.Expressions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity.Test;
+using Microsoft.Data.Entity;
 using Microsoft.Data.Entity.Relational;
 using Microsoft.Framework.DependencyInjection;
 using Xunit;
