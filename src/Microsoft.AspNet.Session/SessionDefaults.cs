32ecf485b9cd6bf1f24b98eb15e3b959f3d80ca1
Hisham Abdullah Bin Ateya <hishamco_2007@hotmail.com>
2015-06-25 20:21:58 +0300

    Add 'SessionDefaults' docs & react to PR #36 changes
    Grammatical fixes

--- src/Microsoft.AspNet.Session/SessionDefaults.cs
+++ src/Microsoft.AspNet.Session/SessionDefaults.cs
@@ -5,9 +5,19 @@ using System;
 
 namespace Microsoft.AspNet.Session
 {
+    /// <summary>
+    /// Represents defaults for the Session.
+    /// </summary>
     public static class SessionDefaults
     {
+        /// <summary>
+        /// Represent the default cookie name, which is ".AspNet.Session".
+        /// </summary>
         public static readonly string CookieName = ".AspNet.Session";
+        
+        /// <summary>
+        /// Represents the default path used to create the cookie, which is "/".
+        /// </summary>
         public static readonly string CookiePath = "/";
     }
 }
