52af452a08b0bdbeb361b337a088d2831c505f10
sornaks <sornaks@microsoft.com>
2015-07-10 16:47:04 -0700

    Fixing XML docs.

--- src/Microsoft.AspNet.Mvc.TagHelpers/TagHelperContentExtensions.cs
+++ src/Microsoft.AspNet.Mvc.TagHelpers/TagHelperContentExtensions.cs
@@ -23,9 +23,10 @@ namespace Microsoft.AspNet.Mvc.TagHelpers
         /// <param name="value">The <see cref="object"/> to write.</param>
         /// <returns><paramref name="content"/> after the write operation has completed.</returns>
         /// <remarks>
-        /// <paramref name="value"/>s of type <see cref="IHtmlContent"/> are written using 
-        /// <see cref="IHtmlContent.WriteTo(TextWriter, IHtmlEncoder)"/>.For all other types,
-        /// the encoded result of <see cref="object.ToString"/> is written to the <paramref name="content"/>.
+        /// <paramref name="value"/>s of type <see cref="Html.Abstractions.IHtmlContent"/> are written using 
+        /// <see cref="Html.Abstractions.IHtmlContent.WriteTo(System.IO.TextWriter, IHtmlEncoder)"/>.
+        /// For all other types, the encoded result of <see cref="object.ToString"/>
+        /// is written to the <paramref name="content"/>.
         /// </remarks>
         public static TagHelperContent Append(
             [NotNull] this TagHelperContent content,
