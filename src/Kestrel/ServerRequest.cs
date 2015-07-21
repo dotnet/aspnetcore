4a284e5f81d469c2740ac730b544fbe43c471fde
Stephen Halter <halter73@gmail.com>
2015-07-21 14:17:11 -0700

    Revert "React to FeatureModel package change."
    This should not have been in the release branch yet!
    
    This reverts commit 3cc0dd7a257207e27c4985b6d4c0617b00ca275e.

--- src/Kestrel/ServerRequest.cs
+++ src/Kestrel/ServerRequest.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNet.FeatureModel;
 using Microsoft.AspNet.Http.Features;
 using Microsoft.AspNet.Server.Kestrel.Http;
 
