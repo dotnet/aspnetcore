aspnet/Mvc

Fix #2338 - Add ViewComponentResult
Makes it easier to render a view component from inside a controller. This
makes it possible to implement behavior where an ajax request refreshes
part of a page.

test/Microsoft.AspNet.Mvc.FunctionalTests/ViewComponentTests.cs
@@ -64,6 +64,19 @@ ViewWithSyncComponents Invoke: hello from viewdatacomponent"
             Assert.Equal("10", body.Trim());
         }
 
+        [Fact]
+        public async Task ViewComponents_InvokeWithViewComponentResult()
+        {
+            var server = TestHelper.CreateServer(_app, SiteName, _configureServices);
+            var client = server.CreateClient();
+
+            // Act
+            var body = await client.GetStringAsync("http://localhost/ViewComponentResultController/Invoke?number=31");
+
+            // Assert
+            Assert.Equal("31", body.Trim());
+        }
+
         [Theory]
         [InlineData("http://localhost/Home/ViewComponentWithEnumerableModelUsingWhere", "Where")]
         [InlineData("http://localhost/Home/ViewComponentWithEnumerableModelUsingSelect", "Select")]

test/Microsoft.AspNet.Mvc.ViewFeatures.Test/PartialViewResultTest.cs
@@ -126,6 +126,33 @@ namespace Microsoft.AspNet.Mvc
         }
 
         [Fact]
+        public async Task ViewResult_SetsStatusCode()
+        {
+            // Arrange
+            var viewName = "myview";
+            var httpContext = GetHttpContext();
+            var context = new ActionContext(httpContext, new RouteData(), new ActionDescriptor());
+            var viewEngine = new Mock<IViewEngine>();
+            var view = Mock.Of<IView>();
+
+            viewEngine.Setup(e => e.FindPartialView(context, "myview"))
+                      .Returns(ViewEngineResult.Found("myview", view));
+
+            var viewResult = new PartialViewResult
+            {
+                ViewName = viewName,
+                ViewEngine = viewEngine.Object,
+                StatusCode = 404,
+            };
+
+            // Act
+            await viewResult.ExecuteResultAsync(context);
+
+            // Assert
+            Assert.Equal(404, httpContext.Response.StatusCode);
+        }
+
+        [Fact]
         public async Task ExecuteResultAsync_UsesActionDescriptorName_IfViewNameIsNull()
         {
             // Arrange

dotnet/aspnetcore

test/Microsoft.AspNet.Mvc.FunctionalTests/ViewComponentTests.cs
@@ -64,6 +64,19 @@ ViewWithSyncComponents Invoke: hello from viewdatacomponent"
             Assert.Equal("10", body.Trim());
         }
 
+        [Fact]
+        public async Task ViewComponents_InvokeWithViewComponentResult()
+        {
+            var server = TestHelper.CreateServer(_app, SiteName, _configureServices);
+            var client = server.CreateClient();
+
+            // Act
+            var body = await client.GetStringAsync("http://localhost/ViewComponentResult/Invoke?number=31");
+
+            // Assert
+            Assert.Equal("31", body.Trim());
+        }
+
         [Theory]
         [InlineData("http://localhost/Home/ViewComponentWithEnumerableModelUsingWhere", "Where")]
         [InlineData("http://localhost/Home/ViewComponentWithEnumerableModelUsingSelect", "Select")]

test/Microsoft.AspNet.Mvc.ViewFeatures.Test/PartialViewResultTest.cs
@@ -48,7 +48,7 @@ namespace Microsoft.AspNet.Mvc
         }
 
         [Fact]
-        public async Task ViewResult_UsesFindPartialViewOnSpecifiedViewEngineToLocateViews()
+        public async Task PartialViewResult_UsesFindPartialViewOnSpecifiedViewEngineToLocateViews()
         {
             // Arrange
             var viewName = "myview";
@@ -123,6 +123,33 @@ namespace Microsoft.AspNet.Mvc
 
             // Assert
             Assert.Equal(expectedContentTypeHeaderValue, httpContext.Response.ContentType);
+        }
+
+        [Fact]
+        public async Task PartialViewResult_SetsStatusCode()
+        {
+            // Arrange
+            var viewName = "myview";
+            var httpContext = GetHttpContext();
+            var context = new ActionContext(httpContext, new RouteData(), new ActionDescriptor());
+            var viewEngine = new Mock<IViewEngine>();
+            var view = Mock.Of<IView>();
+
+            viewEngine.Setup(e => e.FindPartialView(context, "myview"))
+                      .Returns(ViewEngineResult.Found("myview", view));
+
+            var viewResult = new PartialViewResult
+            {
+                ViewName = viewName,
+                ViewEngine = viewEngine.Object,
+                StatusCode = 404,
+            };
+
+            // Act
+            await viewResult.ExecuteResultAsync(context);
+
+            // Assert
+            Assert.Equal(404, httpContext.Response.StatusCode);
         }
 
         [Fact]