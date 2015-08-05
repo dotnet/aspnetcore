51693304c1c57a586a42f939af50f0d10e60b2b3
Stephen Halter <halter73@gmail.com>
2015-08-05 10:32:53 -0700

    Update dependencies after rename to Microsoft.Dnx.Compilation.CSharp

--- src/Microsoft.AspNet.Server.Kestrel.GeneratedCode/KnownHeaders.cs
+++ src/Microsoft.AspNet.Server.Kestrel.GeneratedCode/KnownHeaders.cs
@@ -1,8 +1,7 @@
-﻿using Microsoft.Framework.Runtime.Roslyn;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Dnx.Compilation.CSharp;
 
 namespace Microsoft.AspNet.Server.Kestrel.GeneratedCode
 {
