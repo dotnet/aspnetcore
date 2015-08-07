aspnet/Mvc

Make saving TempData operate via a filter
This change moves the responsibility for saving TempData into a filter,
which is registered with other View features. This moves TempData into the
ViewFeatures package.

ActionResults which require TempData to be kept use a new marker interface
which is handled by the filter.

src/Microsoft.AspNet.Mvc.Core/RedirectResult.cs
@@ -8,7 +8,7 @@ using Microsoft.Framework.Internal;
 
 namespace Microsoft.AspNet.Mvc
 {
-    public class RedirectResult : ActionResult
+    public class RedirectResult : ActionResult, IRedirectResult
     {
         private string _url;
 
@@ -60,8 +60,6 @@ namespace Microsoft.AspNet.Mvc
                 destinationUrl = urlHelper.Content(Url);
             }
 
-            var tempData = context.HttpContext.RequestServices.GetRequiredService<ITempDataDictionary>();
-            tempData.Keep();
             context.HttpContext.Response.Redirect(destinationUrl, Permanent);
         }
 

src/Microsoft.AspNet.Mvc.Core/RedirectToActionResult.cs
@@ -9,7 +9,7 @@ using Microsoft.Framework.Internal;
 
 namespace Microsoft.AspNet.Mvc
 {
-    public class RedirectToActionResult : ActionResult
+    public class RedirectToActionResult : ActionResult, IRedirectResult
     {
         public RedirectToActionResult(
             string actionName,
@@ -51,8 +51,6 @@ namespace Microsoft.AspNet.Mvc
                 throw new InvalidOperationException(Resources.NoRoutesMatched);
             }
 
-            var tempData = context.HttpContext.RequestServices.GetRequiredService<ITempDataDictionary>();
-            tempData.Keep();
             context.HttpContext.Response.Redirect(destinationUrl, Permanent);
         }
 

src/Microsoft.AspNet.Mvc.Core/RedirectToRouteResult.cs
@@ -9,7 +9,7 @@ using Microsoft.Framework.Internal;
 
 namespace Microsoft.AspNet.Mvc
 {
-    public class RedirectToRouteResult : ActionResult
+    public class RedirectToRouteResult : ActionResult, IRedirectResult
     {
         public RedirectToRouteResult(object routeValues)
             : this(routeName: null, routeValues: routeValues)
@@ -51,8 +51,6 @@ namespace Microsoft.AspNet.Mvc
                 throw new InvalidOperationException(Resources.NoRoutesMatched);
             }
 
-            var tempData = context.HttpContext.RequestServices.GetRequiredService<ITempDataDictionary>();
-            tempData.Keep();
             context.HttpContext.Response.Redirect(destinationUrl, Permanent);
         }
 

src/Microsoft.AspNet.Mvc.ViewFeatures/TempData/SaveTempDataFilter.cs
@@ -1,0 +1,49 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.AspNet.Mvc
+{
+    /// <summary>
+    /// An filter which saves temp data.
+    /// </summary>
+    public class SaveTempDataFilter : IResourceFilter, IResultFilter
+    {
+        private readonly ITempDataDictionary _tempData;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="SaveTempDataFilter"/>.
+        /// </summary>
+        /// <param name="tempData">The <see cref="ITempDataDictionary"/> for the current request.</param>
+        public SaveTempDataFilter(ITempDataDictionary tempData)
+        {
+            _tempData = tempData;
+        }
+
+        /// <inheritdoc />
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+        }
+
+        /// <inheritdoc />
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            _tempData.Save();
+        }
+
+        /// <inheritdoc />
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+        }
+
+        /// <inheritdoc />
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            if (context.Result is IRedirectResult)
+            {
+                _tempData.Keep();
+            }
+        }
+    }
+}

dotnet/aspnetcore

src/Microsoft.AspNet.Mvc.Core/RedirectResult.cs
@@ -8,7 +8,7 @@ using Microsoft.Framework.Internal;
 
 namespace Microsoft.AspNet.Mvc
 {
-    public class RedirectResult : ActionResult
+    public class RedirectResult : ActionResult, IKeepTempDataResult
     {
         private string _url;
 
@@ -60,8 +60,6 @@ namespace Microsoft.AspNet.Mvc
                 destinationUrl = urlHelper.Content(Url);
             }
 
-            var tempData = context.HttpContext.RequestServices.GetRequiredService<ITempDataDictionary>();
-            tempData.Keep();
             context.HttpContext.Response.Redirect(destinationUrl, Permanent);
         }
 

src/Microsoft.AspNet.Mvc.Core/RedirectToActionResult.cs
@@ -9,7 +9,7 @@ using Microsoft.Framework.Internal;
 
 namespace Microsoft.AspNet.Mvc
 {
-    public class RedirectToActionResult : ActionResult
+    public class RedirectToActionResult : ActionResult, IKeepTempDataResult
     {
         public RedirectToActionResult(
             string actionName,
@@ -51,8 +51,6 @@ namespace Microsoft.AspNet.Mvc
                 throw new InvalidOperationException(Resources.NoRoutesMatched);
             }
 
-            var tempData = context.HttpContext.RequestServices.GetRequiredService<ITempDataDictionary>();
-            tempData.Keep();
             context.HttpContext.Response.Redirect(destinationUrl, Permanent);
         }
 

src/Microsoft.AspNet.Mvc.Core/RedirectToRouteResult.cs
@@ -9,7 +9,7 @@ using Microsoft.Framework.Internal;
 
 namespace Microsoft.AspNet.Mvc
 {
-    public class RedirectToRouteResult : ActionResult
+    public class RedirectToRouteResult : ActionResult, IKeepTempDataResult
     {
         public RedirectToRouteResult(object routeValues)
             : this(routeName: null, routeValues: routeValues)
@@ -51,8 +51,6 @@ namespace Microsoft.AspNet.Mvc
                 throw new InvalidOperationException(Resources.NoRoutesMatched);
             }
 
-            var tempData = context.HttpContext.RequestServices.GetRequiredService<ITempDataDictionary>();
-            tempData.Keep();
             context.HttpContext.Response.Redirect(destinationUrl, Permanent);
         }
 

src/Microsoft.AspNet.Mvc.ViewFeatures/TempData/SaveTempDataFilter.cs
@@ -1,0 +1,47 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.AspNet.Mvc
+{
+    /// <summary>
+    /// A filter which saves temp data.
+    /// </summary>
+    public class SaveTempDataFilter : IResourceFilter, IResultFilter
+    {
+        private readonly ITempDataDictionary _tempData;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="SaveTempDataFilter"/>.
+        /// </summary>
+        /// <param name="tempData">The <see cref="ITempDataDictionary"/> for the current request.</param>
+        public SaveTempDataFilter(ITempDataDictionary tempData)
+        {
+            _tempData = tempData;
+        }
+
+        /// <inheritdoc />
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+        }
+
+        /// <inheritdoc />
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            _tempData.Save();
+        }
+
+        /// <inheritdoc />
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+        }
+
+        /// <inheritdoc />
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            if (context.Result is IKeepTempDataResult)
+            {
+                _tempData.Keep();
+            }
+        }
+    }
+}