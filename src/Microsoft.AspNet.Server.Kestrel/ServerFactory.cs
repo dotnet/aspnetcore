1584d70e1f72c9c143495f95444681c8ce58a589
Stephen Halter <halter73@gmail.com>
2015-08-25 12:41:11 -0700

    Clean up if an exception is thrown in the middle of ServerFactory.Start

--- src/Microsoft.AspNet.Server.Kestrel/ServerFactory.cs
+++ src/Microsoft.AspNet.Server.Kestrel/ServerFactory.cs
@@ -36,30 +36,44 @@ namespace Microsoft.AspNet.Server.Kestrel
 
         public IDisposable Start(IFeatureCollection serverFeatures, Func<IFeatureCollection, Task> application)
         {
-            var disposables = new List<IDisposable>();
-            var information = (KestrelServerInformation)serverFeatures.Get<IKestrelServerInformation>();
-            var engine = new KestrelEngine(_libraryManager, _appShutdownService);
-            engine.Start(information.ThreadCount == 0 ? 1 : information.ThreadCount);
-            foreach (var address in information.Addresses)
-            {
-                disposables.Add(engine.CreateServer(
-                    address.Scheme,
-                    address.Host,
-                    address.Port,
-                    async frame =>
-                    {
-                        var request = new ServerRequest(frame);
-                        await application.Invoke(request.Features).ConfigureAwait(false);
-                    }));
-            }
-            disposables.Add(engine);
-            return new Disposable(() =>
+            var disposables = new Stack<IDisposable>();
+            var disposer = new Disposable(() =>
             {
                 foreach (var disposable in disposables)
                 {
                     disposable.Dispose();
                 }
             });
+
+            try
+            {
+                var information = (KestrelServerInformation)serverFeatures.Get<IKestrelServerInformation>();
+                var engine = new KestrelEngine(_libraryManager, _appShutdownService);
+
+                disposables.Push(engine);
+
+                engine.Start(information.ThreadCount == 0 ? 1 : information.ThreadCount);
+
+                foreach (var address in information.Addresses)
+                {
+                    disposables.Push(engine.CreateServer(
+                        address.Scheme,
+                        address.Host,
+                        address.Port,
+                        async frame =>
+                        {
+                            var request = new ServerRequest(frame);
+                            await application.Invoke(request.Features).ConfigureAwait(false);
+                        }));
+                }
+
+                return disposer;
+            }
+            catch
+            {
+                disposer.Dispose();
+                throw;
+            }
         }
     }
 }
