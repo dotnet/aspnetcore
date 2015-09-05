dotnet/aspnetcore

commit b25d2d97721aca7e6137de3619cdc875e37eac77

Continued updates for #184
* Reordering members
- Fields
- Constructors
- Properties
- EverythingElse
- NestedTypes

* Removing commented code

src/Microsoft.AspNet.Server.Kestrel/Http/FrameResponseStream.cs
@@ -17,9 +17,25 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
             _context = context;
         }
 
+        public override bool CanRead => false;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => false;
+
+        public override long Length
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override long Position { get; set; }
+
         public override void Flush()
         {
-            //_write(default(ArraySegment<byte>), null);
+            FlushAsync(CancellationToken.None).Wait();
         }
 
         public override Task FlushAsync(CancellationToken cancellationToken)
@@ -83,39 +99,5 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
                 tcs);
             return tcs.Task;
         }
-
-        public override bool CanRead
-        {
-            get
-            {
-                return false;
-            }
-        }
-
-        public override bool CanSeek
-        {
-            get
-            {
-                return false;
-            }
-        }
-
-        public override bool CanWrite
-        {
-            get
-            {
-                return true;
-            }
-        }
-
-        public override long Length
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public override long Position { get; set; }
     }
 }

src/Microsoft.AspNet.Server.Kestrel/Http/Connection.cs
@@ -15,16 +15,6 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
         private static readonly Action<UvStreamHandle, int, Exception, object> _readCallback = ReadCallback;
         private static readonly Func<UvStreamHandle, int, object, Libuv.uv_buf_t> _allocCallback = AllocCallback;
 
-        private static Libuv.uv_buf_t AllocCallback(UvStreamHandle handle, int suggestedSize, object state)
-        {
-            return ((Connection)state).OnAlloc(handle, suggestedSize);
-        }
-
-        private static void ReadCallback(UvStreamHandle handle, int nread, Exception error, object state)
-        {
-            ((Connection)state).OnRead(handle, nread, error);
-        }
-
         private readonly UvStreamHandle _socket;
         private Frame _frame;
         private long _connectionId = 0;
@@ -48,11 +38,21 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
             _socket.ReadStart(_allocCallback, _readCallback, this);
         }
 
+        private static Libuv.uv_buf_t AllocCallback(UvStreamHandle handle, int suggestedSize, object state)
+        {
+            return ((Connection)state).OnAlloc(handle, suggestedSize);
+        }
+
         private Libuv.uv_buf_t OnAlloc(UvStreamHandle handle, int suggestedSize)
         {
             return handle.Libuv.buf_init(
                 SocketInput.Pin(2048),
                 2048);
+        }
+
+        private static void ReadCallback(UvStreamHandle handle, int nread, Exception error, object state)
+        {
+            ((Connection)state).OnRead(handle, nread, error);
         }
 
         private void OnRead(UvStreamHandle handle, int status, Exception error)
@@ -117,13 +117,13 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
 
                         KestrelTrace.Log.ConnectionWriteFin(_connectionId, 0);
                         Thread.Post(
-                            x =>
+                            state =>
                             {
                                 KestrelTrace.Log.ConnectionWriteFin(_connectionId, 1);
-                                var self = (Connection)x;
+                                var self = (Connection)state;
                                 var shutdown = new UvShutdownReq();
                                 shutdown.Init(self.Thread.Loop);
-                                shutdown.Shutdown(self._socket, (req, status, state) =>
+                                shutdown.Shutdown(self._socket, (req, status, _) =>
                                 {
                                     KestrelTrace.Log.ConnectionWriteFin(_connectionId, 1);
                                     req.Dispose();
@@ -140,7 +140,7 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
                         KestrelTrace.Log.ConnectionKeepAlive(_connectionId);
                         _frame = new Frame(this);
                         Thread.Post(
-                            x => ((Frame)x).Consume(),
+                            state => ((Frame)state).Consume(),
                             _frame);
                         break;
                     case ProduceEndType.SocketDisconnect:
@@ -152,10 +152,10 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
 
                         KestrelTrace.Log.ConnectionDisconnect(_connectionId);
                         Thread.Post(
-                            x =>
+                            state =>
                             {
                                 KestrelTrace.Log.ConnectionStop(_connectionId);
-                                ((UvHandle)x).Dispose();
+                                ((UvHandle)state).Dispose();
                             },
                             _socket);
                         break;

src/Microsoft.AspNet.Server.Kestrel/Http/Frame.cs
@@ -17,18 +17,16 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
     public class Frame : FrameContext, IFrameControl
     {
         private static Encoding _ascii = Encoding.ASCII;
+        private static readonly ArraySegment<byte> _endChunkBytes = CreateAsciiByteArraySegment("\r\n");
+        private static readonly ArraySegment<byte> _endChunkedResponseBytes = CreateAsciiByteArraySegment("0\r\n\r\n");
+        private static readonly ArraySegment<byte> _continueBytes = CreateAsciiByteArraySegment("HTTP/1.1 100 Continue\r\n\r\n");
+
         private Mode _mode;
         private bool _responseStarted;
         private bool _keepAlive;
         private bool _autoChunk;
         private readonly FrameRequestHeaders _requestHeaders = new FrameRequestHeaders();
         private readonly FrameResponseHeaders _responseHeaders = new FrameResponseHeaders();
-
-        /*
-        //IDictionary<string, object> _environment;
-
-        CancellationTokenSource _cts = new CancellationTokenSource();
-        */
 
         private List<KeyValuePair<Func<object, Task>, object>> _onStarting;
         private List<KeyValuePair<Func<object, Task>, object>> _onCompleted;
@@ -64,18 +62,6 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
             get { return _responseStarted; }
         }
 
-
-        /*
-        public bool LocalIntakeFin
-        {
-            get
-            {
-                return _mode == Mode.MessageBody
-                    ? _messageBody.LocalIntakeFin
-                    : _mode == Mode.Terminated;
-            }
-        }
-        */
         public void Consume()
         {
             var input = SocketInput;
@@ -131,7 +117,6 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
                             break;
                         }
 
-                        //var resumeBody = HandleExpectContinue(callback);
                         _mode = Mode.MessageBody;
                         Execute();
                         break;
@@ -298,9 +283,6 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
                     immediate: false);
         }
 
-        private static readonly ArraySegment<byte> _endChunkBytes = CreateAsciiByteArraySegment("\r\n");
-        private static readonly ArraySegment<byte> _endChunkedResponseBytes = CreateAsciiByteArraySegment("0\r\n\r\n");
-
         private void WriteChunkSuffix()
         {
             SocketOutput.Write(_endChunkBytes,
@@ -333,12 +315,7 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
         {
             _keepAlive = false;
             ProduceStart();
-
-            // NOTE: needs changes
-            //_upgradeTask = callback(_callContext);
         }
-
-        private static readonly ArraySegment<byte> _continueBytes = CreateAsciiByteArraySegment("HTTP/1.1 100 Continue\r\n\r\n");
 
         private static ArraySegment<byte> CreateAsciiByteArraySegment(string text)
         {
@@ -380,13 +357,13 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
             var responseHeader = CreateResponseHeader(status, appCompleted, ResponseHeaders);
             SocketOutput.Write(
                 responseHeader.Item1,
-                (error, x) =>
+                (error, state) =>
                 {
                     if (error != null)
                     {
                         Trace.WriteLine("ProduceStart " + error.ToString());
                     }
-                    ((IDisposable)x).Dispose();
+                    ((IDisposable)state).Dispose();
                 },
                 responseHeader.Item2,
                 immediate: immediate);

src/Microsoft.AspNet.Server.Kestrel/Http/FrameDuplexStream.cs
@@ -21,6 +21,82 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
             _responseStream = responseStream;
         }
 
+        public override bool CanRead
+        {
+            get
+            {
+                return _requestStream.CanRead;
+            }
+        }
+
+        public override bool CanSeek
+        {
+            get
+            {
+                return _requestStream.CanSeek;
+            }
+        }
+
+        public override bool CanTimeout
+        {
+            get
+            {
+                return _responseStream.CanTimeout || _requestStream.CanTimeout;
+            }
+        }
+
+        public override bool CanWrite
+        {
+            get
+            {
+                return _responseStream.CanWrite;
+            }
+        }
+
+        public override long Length
+        {
+            get
+            {
+                return _requestStream.Length;
+            }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                return _requestStream.Position;
+            }
+            set
+            {
+                _requestStream.Position = value;
+            }
+        }
+
+        public override int ReadTimeout
+        {
+            get
+            {
+                return _requestStream.ReadTimeout;
+            }
+            set
+            {
+                _requestStream.ReadTimeout = value;
+            }
+        }
+
+        public override int WriteTimeout
+        {
+            get
+            {
+                return _responseStream.WriteTimeout;
+            }
+            set
+            {
+                _responseStream.WriteTimeout = value;
+            }
+        }
+
 #if DNX451
         public override void Close()
         {
@@ -115,82 +191,6 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
         public override void WriteByte(byte value)
         {
             _responseStream.WriteByte(value);
-        }
-
-        public override bool CanRead
-        {
-            get
-            {
-                return _requestStream.CanRead;
-            }
-        }
-
-        public override bool CanSeek
-        {
-            get
-            {
-                return _requestStream.CanSeek;
-            }
-        }
-
-        public override bool CanTimeout
-        {
-            get
-            {
-                return _responseStream.CanTimeout || _requestStream.CanTimeout;
-            }
-        }
-
-        public override bool CanWrite
-        {
-            get
-            {
-                return _responseStream.CanWrite;
-            }
-        }
-
-        public override long Length
-        {
-            get
-            {
-                return _requestStream.Length;
-            }
-        }
-
-        public override long Position
-        {
-            get
-            {
-                return _requestStream.Position;
-            }
-            set
-            {
-                _requestStream.Position = value;
-            }
-        }
-
-        public override int ReadTimeout
-        {
-            get
-            {
-                return _requestStream.ReadTimeout;
-            }
-            set
-            {
-                _requestStream.ReadTimeout = value;
-            }
-        }
-
-        public override int WriteTimeout
-        {
-            get
-            {
-                return _responseStream.WriteTimeout;
-            }
-            set
-            {
-                _responseStream.WriteTimeout = value;
-            }
         }
     }
 }

src/Microsoft.AspNet.Server.Kestrel/Http/FrameHeaders.cs
@@ -15,6 +15,27 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
 
         protected Dictionary<string, StringValues> Unknown => MaybeUnknown ?? (MaybeUnknown = new Dictionary<string, StringValues>(StringComparer.OrdinalIgnoreCase));
 
+        StringValues IDictionary<string, StringValues>.this[string key]
+        {
+            get
+            {
+                return GetValueFast(key);
+            }
+
+            set
+            {
+                SetValueFast(key, value);
+            }
+        }
+
+        int ICollection<KeyValuePair<string, StringValues>>.Count => GetCountFast();
+
+        bool ICollection<KeyValuePair<string, StringValues>>.IsReadOnly => false;
+
+        ICollection<string> IDictionary<string, StringValues>.Keys => ((IDictionary<string, StringValues>)this).Select(pair => pair.Key).ToList();
+
+        ICollection<StringValues> IDictionary<string, StringValues>.Values => ((IDictionary<string, StringValues>)this).Select(pair => pair.Value).ToList();
+
         protected static StringValues AppendValue(StringValues existing, string append)
         {
             return StringValues.Concat(existing, append);
@@ -46,28 +67,6 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
 
         protected virtual IEnumerator<KeyValuePair<string, StringValues>> GetEnumeratorFast()
         { throw new NotImplementedException(); }
-
-
-        StringValues IDictionary<string, StringValues>.this[string key]
-        {
-            get
-            {
-                return GetValueFast(key);
-            }
-
-            set
-            {
-                SetValueFast(key, value);
-            }
-        }
-
-        int ICollection<KeyValuePair<string, StringValues>>.Count => GetCountFast();
-
-        bool ICollection<KeyValuePair<string, StringValues>>.IsReadOnly => false;
-
-        ICollection<string> IDictionary<string, StringValues>.Keys => ((IDictionary<string, StringValues>)this).Select(x => x.Key).ToList();
-
-        ICollection<StringValues> IDictionary<string, StringValues>.Values => ((IDictionary<string, StringValues>)this).Select(x => x.Value).ToList();
 
         void ICollection<KeyValuePair<string, StringValues>>.Add(KeyValuePair<string, StringValues> item)
         {

src/Microsoft.AspNet.Server.Kestrel/Http/FrameRequestStream.cs
@@ -12,14 +12,26 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
     {
         private readonly MessageBody _body;
 
-        //int _readLength;
-        //bool _readFin;
-        //Exception _readError;
-
         public FrameRequestStream(MessageBody body)
         {
             _body = body;
         }
+
+        public override bool CanRead { get { return true; } }
+
+        public override bool CanSeek { get { return false; } }
+
+        public override bool CanWrite { get { return false; } }
+
+        public override long Length
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override long Position { get; set; }
 
         public override void Flush()
         {
@@ -67,20 +79,20 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
         {
             var tcs = new TaskCompletionSource<int>(state);
             var task = _body.ReadAsync(new ArraySegment<byte>(buffer, offset, count));
-            task.ContinueWith((t, x) =>
+            task.ContinueWith((task2, state2) =>
             {
-                var tcs2 = (TaskCompletionSource<int>)x;
-                if (t.IsCanceled)
+                var tcs2 = (TaskCompletionSource<int>)state2;
+                if (task2.IsCanceled)
                 {
                     tcs2.SetCanceled();
                 }
-                else if (t.IsFaulted)
+                else if (task2.IsFaulted)
                 {
-                    tcs2.SetException(t.Exception);
+                    tcs2.SetException(task2.Exception);
                 }
                 else
                 {
-                    tcs2.SetResult(t.Result);
+                    tcs2.SetResult(task2.Result);
                 }
             }, tcs);
             return tcs.Task;
@@ -90,21 +102,5 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
         {
             throw new NotImplementedException();
         }
-
-        public override bool CanRead { get { return true; } }
-
-        public override bool CanSeek { get { return false; } }
-
-        public override bool CanWrite { get { return false; } }
-
-        public override long Length
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public override long Position { get; set; }
     }
 }

src/Microsoft.AspNet.Server.Kestrel/Http/Listener.cs
@@ -13,24 +13,12 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
     /// </summary>
     public abstract class Listener : ListenerContext, IDisposable
     {
-        protected UvStreamHandle ListenSocket { get; private set; }
-
-        protected static void ConnectionCallback(UvStreamHandle stream, int status, Exception error, object state)
-        {
-            if (error != null)
-            {
-                Trace.WriteLine("Listener.ConnectionCallback " + error.ToString());
-            }
-            else
-            {
-                ((Listener)state).OnConnection(stream, status);
-            }
-        }
-
         protected Listener(IMemoryPool memory)
         {
             Memory = memory;
         }
+
+        protected UvStreamHandle ListenSocket { get; private set; }
 
         public Task StartAsync(
             string scheme,
@@ -62,6 +50,18 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
         /// Creates the socket used to listen for incoming connections
         /// </summary>
         protected abstract UvStreamHandle CreateListenSocket(string host, int port);
+
+        protected static void ConnectionCallback(UvStreamHandle stream, int status, Exception error, object state)
+        {
+            if (error != null)
+            {
+                Trace.WriteLine("Listener.ConnectionCallback " + error.ToString());
+            }
+            else
+            {
+                ((Listener)state).OnConnection(stream, status);
+            }
+        }
 
         /// <summary>
         /// Handles an incoming connection

src/Microsoft.AspNet.Server.Kestrel/Http/ListenerPrimary.cs
@@ -15,8 +15,6 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
     /// </summary>
     abstract public class ListenerPrimary : Listener
     {
-        UvPipeHandle ListenPipe { get; set; }
-
         private List<UvPipeHandle> _dispatchPipes = new List<UvPipeHandle>();
         private int _dispatchIndex;
         private ArraySegment<ArraySegment<byte>> _1234 = new ArraySegment<ArraySegment<byte>>(new[] { new ArraySegment<byte>(new byte[] { 1, 2, 3, 4 }) });
@@ -24,6 +22,8 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
         protected ListenerPrimary(IMemoryPool memory) : base(memory)
         {
         }
+
+        UvPipeHandle ListenPipe { get; set; }
 
         public async Task StartAsync(
             string pipeName,

commit 5ee80e0155b028e7f58c181ea90ac5fac833c939

Fix bug #191 introduced by b25d2d9

src/Microsoft.AspNet.Server.Kestrel/Http/FrameResponseStream.cs
@@ -21,7 +21,7 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
 
         public override bool CanSeek => false;
 
-        public override bool CanWrite => false;
+        public override bool CanWrite => true;
 
         public override long Length
         {