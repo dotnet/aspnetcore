8dc4ddf195afecd9a67f63ba0a947fa5bfa04087
Pranav K <prkrishn@hotmail.com>
2015-09-10 16:45:07 -0700

    Adding NeutralResourcesLanguageAttribute

--- src/Microsoft.AspNet.Diagnostics/Properties/AssemblyInfo.cs
+++ src/Microsoft.AspNet.Diagnostics/Properties/AssemblyInfo.cs
@@ -2,8 +2,10 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Microsoft.AspNet.Diagnostics.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
+[assembly: NeutralResourcesLanguage("en-US")]
