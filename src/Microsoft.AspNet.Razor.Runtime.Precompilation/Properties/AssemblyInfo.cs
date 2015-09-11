9ab9a330ed56455ed1ecc85768e2f2b9b6ccbef5
Pranav K <prkrishn@hotmail.com>
2015-09-10 18:26:46 -0700

    Adding NeutralResourcesLanguageAttribute

--- src/Microsoft.AspNet.Razor.Runtime.Precompilation/Properties/AssemblyInfo.cs
+++ src/Microsoft.AspNet.Razor.Runtime.Precompilation/Properties/AssemblyInfo.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Microsoft.AspNet.Razor.Runtime.Precompilation.Test")]
+[assembly: NeutralResourcesLanguage("en-us")]
+[assembly: AssemblyMetadata("Serviceable", "True")]
