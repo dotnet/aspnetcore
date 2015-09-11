98af679b0bfc0c1862f6102a270976cf5d778a1a
Pranav K <prkrishn@hotmail.com>
2015-09-10 18:45:23 -0700

    Adding NeutralResourcesLanguageAttribute

--- src/Microsoft.AspNet.WebSockets.Protocol/Properties/AssemblyInfo.cs
+++ src/Microsoft.AspNet.WebSockets.Protocol/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -35,3 +36,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
+[assembly: NeutralResourcesLanguage("en-us")]
