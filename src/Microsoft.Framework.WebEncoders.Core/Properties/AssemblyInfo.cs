b1a2db0a7cb31c3effafcabbf1871a872ff6ce50
Pranav K <prkrishn@hotmail.com>
2015-09-10 17:47:52 -0700

    Adding NeutralResourcesLanguageAttribute

--- src/Microsoft.Framework.WebEncoders.Core/Properties/AssemblyInfo.cs
+++ src/Microsoft.Framework.WebEncoders.Core/Properties/AssemblyInfo.cs
@@ -1,9 +1,10 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Microsoft.Framework.WebEncoders.Tests")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
+[assembly: NeutralResourcesLanguage("en-us")]
