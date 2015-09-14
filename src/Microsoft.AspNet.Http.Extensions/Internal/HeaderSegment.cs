7a24045953df16e0ee69173a05078c51764ba720
David Fowler <davidfowl@gmail.com>
2015-09-13 22:07:17 -0700

    Split ParsingHelpers classes into their own files
    #397

---
+++ src/Microsoft.AspNet.Http.Extensions/Internal/HeaderSegment.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Microsoft.AspNet.Http.Internal
+{
+    internal struct HeaderSegment : IEquatable<HeaderSegment>
+    {
+        private readonly StringSegment _formatting;
+        private readonly StringSegment _data;
+
+        // <summary>
+        // Initializes a new instance of the <see cref="T:System.Object"/> class.
+        // </summary>
+        public HeaderSegment(StringSegment formatting, StringSegment data)
+        {
+            _formatting = formatting;
+            _data = data;
+        }
+
+        public StringSegment Formatting
+        {
+            get { return _formatting; }
+        }
+
+        public StringSegment Data
+        {
+            get { return _data; }
+        }
+
+        public bool Equals(HeaderSegment other)
+        {
+            return _formatting.Equals(other._formatting) && _data.Equals(other._data);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            return obj is HeaderSegment && Equals((HeaderSegment)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (_formatting.GetHashCode() * 397) ^ _data.GetHashCode();
+            }
+        }
+
+        public static bool operator ==(HeaderSegment left, HeaderSegment right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(HeaderSegment left, HeaderSegment right)
+        {
+            return !left.Equals(right);
+        }
+    }
+}
---
+++ src/Microsoft.AspNet.Http.Extensions/Internal/ParsingHelpers.cs
@@ -0,0 +1,137 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Framework.Internal;
+using Microsoft.Framework.Primitives;
+
+namespace Microsoft.AspNet.Http.Internal
+{
+    internal static class ParsingHelpers
+    {
+        public static StringValues GetHeader(IDictionary<string, StringValues> headers, string key)
+        {
+            StringValues value;
+            return headers.TryGetValue(key, out value) ? value : StringValues.Empty;
+        }
+
+        public static StringValues GetHeaderSplit(IDictionary<string, StringValues> headers, string key)
+        {
+            var values = GetHeaderUnmodified(headers, key);
+            return new StringValues(GetHeaderSplitImplementation(values).ToArray());
+        }
+
+        private static IEnumerable<string> GetHeaderSplitImplementation(StringValues values)
+        {
+            foreach (var segment in new HeaderSegmentCollection(values))
+            {
+                if (segment.Data.HasValue)
+                {
+                    yield return DeQuote(segment.Data.Value);
+                }
+            }
+        }
+
+        public static StringValues GetHeaderUnmodified([NotNull] IDictionary<string, StringValues> headers, string key)
+        {
+            StringValues values;
+            return headers.TryGetValue(key, out values) ? values : StringValues.Empty;
+        }
+
+        public static void SetHeaderJoined([NotNull] IDictionary<string, StringValues> headers, [NotNull] string key, StringValues value)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            if (StringValues.IsNullOrEmpty(value))
+            {
+                headers.Remove(key);
+            }
+            else
+            {
+                headers[key] = string.Join(",", value.Select(QuoteIfNeeded));
+            }
+        }
+
+        // Quote items that contain comas and are not already quoted.
+        private static string QuoteIfNeeded(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                // Ignore
+            }
+            else if (value.Contains(','))
+            {
+                if (value[0] != '"' || value[value.Length - 1] != '"')
+                {
+                    value = '"' + value + '"';
+                }
+            }
+
+            return value;
+        }
+
+        private static string DeQuote(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                // Ignore
+            }
+            else if (value.Length > 1 && value[0] == '"' && value[value.Length - 1] == '"')
+            {
+                value = value.Substring(1, value.Length - 2);
+            }
+
+            return value;
+        }
+
+        public static void SetHeaderUnmodified([NotNull] IDictionary<string, StringValues> headers, [NotNull] string key, StringValues? values)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            if (!values.HasValue || StringValues.IsNullOrEmpty(values.Value))
+            {
+                headers.Remove(key);
+            }
+            else
+            {
+                headers[key] = values.Value;
+            }
+        }
+
+        public static void AppendHeaderJoined([NotNull] IDictionary<string, StringValues> headers, [NotNull] string key, params string[] values)
+        {
+            if (values == null || values.Length == 0)
+            {
+                return;
+            }
+
+            string existing = GetHeader(headers, key);
+            if (existing == null)
+            {
+                SetHeaderJoined(headers, key, values);
+            }
+            else
+            {
+                headers[key] = existing + "," + string.Join(",", values.Select(value => QuoteIfNeeded(value)));
+            }
+        }
+
+        public static void AppendHeaderUnmodified([NotNull] IDictionary<string, StringValues> headers, [NotNull] string key, StringValues values)
+        {
+            if (values.Count == 0)
+            {
+                return;
+            }
+
+            var existing = GetHeaderUnmodified(headers, key);
+            SetHeaderUnmodified(headers, key, StringValues.Concat(existing, values));
+        }
+    }
+}
