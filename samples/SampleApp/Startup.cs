13f46bb66b7586b8a6451d734866f6d7843aaeae
Louis DeJardin <lodejard@microsoft.com>
2015-09-18 17:22:10 -0700

    Using multiple threads in SampleApp

--- samples/SampleApp/Startup.cs
+++ samples/SampleApp/Startup.cs
@@ -5,6 +5,7 @@ using System;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.Logging;
+using Microsoft.AspNet.Server.Kestrel;
 
 namespace SampleApp
 {
@@ -12,6 +13,9 @@ namespace SampleApp
     {
         public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
+            var ksi = app.ServerFeatures[typeof(IKestrelServerInformation)] as IKestrelServerInformation;
+            ksi.ThreadCount = 4;
+
             loggerFactory.MinimumLevel = LogLevel.Debug;
 
             loggerFactory.AddConsole(LogLevel.Debug);
