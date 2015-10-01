b22474afb3a613a7104c54e70c81b1800d886313
Hisham Bin Ateya <hishamco_2007@hotmail.com>
2015-10-01 18:03:50 +0300

    Add missing Asserts

--- test/Microsoft.AspNet.Localization.Tests/AccessLanguageHeaderRequestCultureProviderTest.cs
+++ test/Microsoft.AspNet.Localization.Tests/AccessLanguageHeaderRequestCultureProviderTest.cs
@@ -104,6 +104,7 @@ namespace Microsoft.Extensions.Localization.Tests
                 client.DefaultRequestHeaders.AcceptLanguage.ParseAdd("en-GB,ar-MA,en-US");
                 var count = client.DefaultRequestHeaders.AcceptLanguage.Count;
                 var response = await client.GetAsync(string.Empty);
+                Assert.Equal(3, count);
             }
         }
         
@@ -136,7 +137,9 @@ namespace Microsoft.Extensions.Localization.Tests
             {
                 var client = server.CreateClient();
                 client.DefaultRequestHeaders.AcceptLanguage.ParseAdd("en-GB,ar-YE,en-US");
+                var count = client.DefaultRequestHeaders.AcceptLanguage.Count;
                 var response = await client.GetAsync(string.Empty);
+                Assert.Equal(3, count);
             }
         }
     }
