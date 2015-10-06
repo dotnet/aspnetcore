618a73b399b363a5c202c1d093280b1941bc6457
Doug Bunting <dougbu@microsoft.com>
2015-10-05 23:02:50 -0700

    Support building with Core CLR (on Linux) - hits all of the checkboxes in #272
    Specify test runtime explicitly and support testing w/ DNX Core on Linux
    - turn on DNX Core on Linux testing with `--test-dnxcore` target
      - or `KOREBUILD_TEST_DNXCORE` environment variable
    
    Work around aspnet/dnx#2566
    - builds fail with DNX Core on Linux otherwise
    
    Do not attempt to install nodejs if not on Windows
    
    Use user's `default` DNVM when `%SKIP_DNX_INSTALL%` defined on Windows
    - enables builds with DNX Core
    - should be the default for new repos; older repos can opt in
    
    Use consistent case for all `--quiet` environment variables
      - also name these variables consistently; start w/ `KOREBUILD_`
    - environment variables are case-sensitive on Linux and mixed case was annoying
    - also get rid of leading space in these environment variable values
    
    Check `IsLinux` and not `IsMono`
    - `IsMono` is always `true` on Linux since Sake always runs in Mono there
      - but `IsLinux` is the right question
    
    nits:
    - remove tabs from a few files
    - remove compilation warnings in local `makefile.shade`

--- build-template/build.cmd
+++ build-template/build.cmd
@@ -19,20 +19,20 @@ copy %CACHED_NUGET% .nuget\nuget.exe > nul
 :restore
 IF EXIST packages\KoreBuild goto run
 IF %BUILDCMD_KOREBUILD_VERSION%=="" (
-	.nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
+    .nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
 ) ELSE (
-	.nuget\NuGet.exe install KoreBuild -version %BUILDCMD_KOREBUILD_VERSION% -ExcludeVersion -o packages -nocache -pre
+    .nuget\NuGet.exe install KoreBuild -version %BUILDCMD_KOREBUILD_VERSION% -ExcludeVersion -o packages -nocache -pre
 )
 .nuget\NuGet.exe install Sake -ExcludeVersion -Source https://www.nuget.org/api/v2/ -Out packages
 
 IF "%SKIP_DNX_INSTALL%"=="1" goto run
 IF %BUILDCMD_DNX_VERSION%=="" (
-	CALL packages\KoreBuild\build\dnvm upgrade -runtime CLR -arch x86
+    CALL packages\KoreBuild\build\dnvm upgrade -runtime CLR -arch x86
 ) ELSE (
-	CALL packages\KoreBuild\build\dnvm install %BUILDCMD_DNX_VERSION% -runtime CLR -arch x86 -alias default
+    CALL packages\KoreBuild\build\dnvm install %BUILDCMD_DNX_VERSION% -runtime CLR -arch x86 -alias default
 )
 CALL packages\KoreBuild\build\dnvm install default -runtime CoreCLR -arch x86
 
 :run
-CALL packages\KoreBuild\build\dnvm use default -runtime CLR -arch x86
+CALL packages\KoreBuild\build\dnvm use default
 packages\Sake\tools\Sake.exe -I packages\KoreBuild\build -f makefile.shade %*
