3eec43a0c3279d8ba758ee3712b2716538ce7d5f
Stephen Halter <halter73@gmail.com>
2015-10-09 17:27:33 -0700

    Move static BitCount method to base class
    - Updated comment to point to a specific commit in corefx
    - This is more consistent with AppendValue since BitCount doesn't need
      to be generated

--- src/Microsoft.AspNet.Server.Kestrel/Http/FrameHeaders.Generated.cs
+++ src/Microsoft.AspNet.Server.Kestrel/Http/FrameHeaders.Generated.cs
@@ -586,22 +586,6 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
             }
         }
         
-        private static int BitCount(long value)
-        {
-            // see https://github.com/dotnet/corefx/blob/master/src/System.Reflection.Metadata/src/System/Reflection/Internal/Utilities/BitArithmetic.cs
-
-            const ulong Mask01010101 = 0x5555555555555555UL;
-            const ulong Mask00110011 = 0x3333333333333333UL;
-            const ulong Mask00001111 = 0x0F0F0F0F0F0F0F0FUL;
-            const ulong Mask00000001 = 0x0101010101010101UL;
-
-            var v = (ulong)value;
-
-            v = v - ((v >> 1) & Mask01010101);
-            v = (v & Mask00110011) + ((v >> 2) & Mask00110011);
-            return (int)(unchecked(((v + (v >> 4)) & Mask00001111) * Mask00000001) >> 56);
-        }
-
         protected override int GetCountFast()
         {
             return BitCount(_bits) + (MaybeUnknown?.Count ?? 0);
@@ -5428,22 +5412,6 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
             }
         }
         
-        private static int BitCount(long value)
-        {
-            // see https://github.com/dotnet/corefx/blob/master/src/System.Reflection.Metadata/src/System/Reflection/Internal/Utilities/BitArithmetic.cs
-
-            const ulong Mask01010101 = 0x5555555555555555UL;
-            const ulong Mask00110011 = 0x3333333333333333UL;
-            const ulong Mask00001111 = 0x0F0F0F0F0F0F0F0FUL;
-            const ulong Mask00000001 = 0x0101010101010101UL;
-
-            var v = (ulong)value;
-
-            v = v - ((v >> 1) & Mask01010101);
-            v = (v & Mask00110011) + ((v >> 2) & Mask00110011);
-            return (int)(unchecked(((v + (v >> 4)) & Mask00001111) * Mask00000001) >> 56);
-        }
-
         protected override int GetCountFast()
         {
             return BitCount(_bits) + (MaybeUnknown?.Count ?? 0);
