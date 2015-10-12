291997e9954537e2f3463a0d9d1666097b009b35
Chris R <Tratcher@outlook.com>
2015-10-12 11:08:53 -0700

    React to IHttpResponseFeature changes.

--- test/Microsoft.AspNet.Authentication.Test/AuthenticationHandlerFacts.cs
+++ test/Microsoft.AspNet.Authentication.Test/AuthenticationHandlerFacts.cs
@@ -2,14 +2,13 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Http.Features;
 using Microsoft.AspNet.Http.Features.Authentication;
 using Microsoft.AspNet.Http.Internal;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Primitives;
 using Xunit;
 
 namespace Microsoft.AspNet.Authentication
@@ -193,7 +192,7 @@ namespace Microsoft.AspNet.Authentication
                 }
             }
 
-            public IDictionary<string, StringValues> Headers
+            public IHeaderDictionary Headers
             {
                 get
                 {
