5279786748163e60666a2ead5b1006faef778b46
DamianEdwards <damian@damianedwards.com>
2015-10-19 10:51:08 -0700

    Added doc-comments to IHostingEnvironment - Coherence-Signed#75

--- src/Microsoft.AspNet.Hosting.Abstractions/IHostingEnvironment.cs
+++ src/Microsoft.AspNet.Hosting.Abstractions/IHostingEnvironment.cs
@@ -5,14 +5,27 @@ using Microsoft.AspNet.FileProviders;
 
 namespace Microsoft.AspNet.Hosting
 {
+    /// <summary>
+    /// Provides information about the web hosting environment an application is running in.
+    /// </summary>
     public interface IHostingEnvironment
     {
+        /// <summary>
+        /// Gets or sets the name of the environment. This property is automatically set by the host to the value
+        /// of the <c>ASPNET_ENV</c> environment variable.
+        /// </summary>
         // This must be settable!
         string EnvironmentName { get; set;  }
 
+        /// <summary>
+        /// Gets or sets the absolute path to the directory that contains the web-servable application content files.
+        /// </summary>
         // This must be settable!
         string WebRootPath { get; set; }
 
+        /// <summary>
+        /// Gets or sets an <see cref="IFileProvider"/> pointing at <see cref="WebRootPath"/>.
+        /// </summary>
         // This must be settable!
         IFileProvider WebRootFileProvider { get; set; }
     }
