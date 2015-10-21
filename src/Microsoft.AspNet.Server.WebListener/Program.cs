f36faf2a72fce71fcf0a45a741ea551df21477ed
Pavel Krymets <pavel@krymets.com>
2015-10-21 10:20:10 -0700

    React to hosting changes

--- src/Microsoft.AspNet.Server.WebListener/Program.cs
+++ src/Microsoft.AspNet.Server.WebListener/Program.cs
@@ -28,18 +28,10 @@ namespace Microsoft.AspNet.Server.WebListener
 {
     public class Program
     {
-        private readonly IServiceProvider _serviceProvider;
-
-        public Program(IServiceProvider serviceProvider)
+        public static void Main(string[] args)
         {
-            _serviceProvider = serviceProvider;
-        }
-
-        public void Main(string[] args)
-        {
-            var program = new Hosting.Program(_serviceProvider);
             var mergedArgs = new[] { "--server", "Microsoft.AspNet.Server.WebListener" }.Concat(args).ToArray();
-            program.Main(mergedArgs);
+            Hosting.Program.Main(mergedArgs);
         }
     }
 }
