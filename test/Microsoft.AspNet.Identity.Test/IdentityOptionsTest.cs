cd8252bd55d9b1db0684704096fe8c1ddc930d40
Kirthi Krishnamraju <kirthik@microsoft.com>
2015-10-22 14:34:18 -0700

    React to configuration change aspnet/Configuration #299

--- test/Microsoft.AspNet.Identity.Test/IdentityOptionsTest.cs
+++ test/Microsoft.AspNet.Identity.Test/IdentityOptionsTest.cs
@@ -60,7 +60,8 @@ namespace Microsoft.AspNet.Identity.Test
                 {"identity:lockout:AllowedForNewUsers", "FALSe"},
                 {"identity:lockout:MaxFailedAccessAttempts", "1000"}
             };
-            var builder = new ConfigurationBuilder(new MemoryConfigurationProvider(dic));
+            var builder = new ConfigurationBuilder();
+            builder.AddInMemoryCollection(dic);
             var config = builder.Build();
             Assert.Equal(roleClaimType, config["identity:claimsidentity:roleclaimtype"]);
 
@@ -93,7 +94,8 @@ namespace Microsoft.AspNet.Identity.Test
                 {"identity:user:requireUniqueEmail", "true"},
                 {"identity:lockout:MaxFailedAccessAttempts", "1000"}
             };
-            var builder = new ConfigurationBuilder(new MemoryConfigurationProvider(dic));
+            var builder = new ConfigurationBuilder();
+            builder.AddInMemoryCollection(dic);
             var config = builder.Build();
             var services = new ServiceCollection();
             services.Configure<IdentityOptions>(config.GetSection("identity"));
