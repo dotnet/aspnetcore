fa72b35883dccc39d6d3e9d66e738185f7e3f2f0
David Fowler <davidfowl@gmail.com>
2015-10-23 02:59:50 -0700

    More granualr null checks for platform services

--- src/Microsoft.AspNet.Hosting/WebHostBuilder.cs
+++ src/Microsoft.AspNet.Hosting/WebHostBuilder.cs
@@ -92,12 +92,28 @@ namespace Microsoft.AspNet.Hosting
                 _configureServices(services);
             }
 
-            if (PlatformServices.Default != null)
+            if (PlatformServices.Default?.Application != null)
             {
                 services.TryAdd(ServiceDescriptor.Instance(PlatformServices.Default.Application));
+            }
+            
+            if (PlatformServices.Default?.Runtime != null)
+            {
                 services.TryAdd(ServiceDescriptor.Instance(PlatformServices.Default.Runtime));
+            }
+            
+            if (PlatformServices.Default?.AssemblyLoadContextAccessor != null)
+            {
                 services.TryAdd(ServiceDescriptor.Instance(PlatformServices.Default.AssemblyLoadContextAccessor));
+            }
+            
+            if (PlatformServices.Default?.AssemblyLoaderContainer != null)
+            {
                 services.TryAdd(ServiceDescriptor.Instance(PlatformServices.Default.AssemblyLoaderContainer));
+            }
+            
+            if (PlatformServices.Default?.LibraryManager != null)
+            {
                 services.TryAdd(ServiceDescriptor.Instance(PlatformServices.Default.LibraryManager));
             }
 
