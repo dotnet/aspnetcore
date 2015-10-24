327589d41e315bdbace11eeee25c034bbef0a429
David Fowler <davidfowl@gmail.com>
2015-10-24 01:07:01 -0700

    Fixed ifdefs

--- src/Microsoft.AspNet.IISPlatformHandler/NativeMethods.cs
+++ src/Microsoft.AspNet.IISPlatformHandler/NativeMethods.cs
@@ -8,13 +8,13 @@ namespace Microsoft.AspNet.IISPlatformHandler
 {
     internal class NativeMethods
     {
-#if DNXCORE50
+#if DOTNET5_4
         private const string api_ms_win_core_handle_LIB = "api-ms-win-core-handle-l1-1-0.dll";
 #else
         private const string KERNEL32 = "kernel32.dll";
 #endif
 
-#if DNXCORE50
+#if DOTNET5_4
         [DllImport(api_ms_win_core_handle_LIB, ExactSpelling = true, SetLastError = true)]
 #else
         [DllImport(KERNEL32, ExactSpelling = true, SetLastError = true)]
