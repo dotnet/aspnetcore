e4d1259da5d2aa09ae9cbd37d395cf7021d804ef
Pavel Krymets <pavel@krymets.com>
2015-10-29 12:06:12 -0700

    Remove usage of CallContextLocator

--- test/Microsoft.AspNet.Razor.Runtime.Precompilation.Test/CompilationUtility.cs
+++ test/Microsoft.AspNet.Razor.Runtime.Precompilation.Test/CompilationUtility.cs
@@ -37,10 +37,7 @@ namespace Microsoft.AspNet.Razor.Runtime.Precompilation
                 syntaxTrees.Add(CSharpSyntaxTree.ParseText(resourceContent));
             }
 
-            var libraryExporter = (ILibraryExporter)CallContextServiceLocator
-                .Locator
-                .ServiceProvider
-                .GetService(typeof(ILibraryExporter));
+            var libraryExporter = CompilationServices.Default.LibraryExporter;
             var applicationName = ExecutingAssembly.GetName().Name;
             var libraryExport = libraryExporter.GetExport(applicationName);
 
