037196d5c7eff127d6dc7ef29e6ba029b8c129e6
Doug Bunting <dougbu@microsoft.com>
2015-10-30 19:09:08 -0700

    Restore `null` and `string.Empty` handling from `EncoderExtensions` - prior test encoders were never invoked for `null` or empty `string`s e.g. ``` c#     public static void HtmlEncode(this IHtmlEncoder htmlEncoder, string value, TextWriter output)     { ...         if (!String.IsNullOrEmpty(value))         {             htmlEncoder.HtmlEncode(value, 0, value.Length, output);         }     } ``` - add missing `null` checks and handle `string.Empty` in `TextWriter output, string value, ...` overloads   - better match for the underlying `TextEncoder` behaviour   - `EncoderExtensions` provided an API like `TextEncoder.Encode(TextWriter output, string value)`     - that method calls `Encode(TextWriter output, string value, int startIndex, int characterCount)`

--- src/Microsoft.Extensions.WebEncoders/Testing/JavaScriptTestEncoder.cs
+++ src/Microsoft.Extensions.WebEncoders/Testing/JavaScriptTestEncoder.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.IO;
 using System.Text.Encodings.Web;
 
@@ -18,11 +19,36 @@ namespace Microsoft.Extensions.WebEncoders.Testing
 
         public override string Encode(string value)
         {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (value.Length == 0)
+            {
+                return string.Empty;
+            }
+
             return $"JavaScriptEncode[[{value}]]";
         }
 
         public override void Encode(TextWriter output, char[] value, int startIndex, int characterCount)
         {
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (characterCount == 0)
+            {
+                return;
+            }
+
             output.Write("JavaScriptEncode[[");
             output.Write(value, startIndex, characterCount);
             output.Write("]]");
@@ -30,6 +56,21 @@ namespace Microsoft.Extensions.WebEncoders.Testing
 
         public override void Encode(TextWriter output, string value, int startIndex, int characterCount)
         {
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (characterCount == 0)
+            {
+                return;
+            }
+
             output.Write("JavaScriptEncode[[");
             output.Write(value.Substring(startIndex, characterCount));
             output.Write("]]");
@@ -45,8 +86,17 @@ namespace Microsoft.Extensions.WebEncoders.Testing
             return -1;
         }
 
-        public override unsafe bool TryEncodeUnicodeScalar(int unicodeScalar, char* buffer, int bufferLength, out int numberOfCharactersWritten)
+        public override unsafe bool TryEncodeUnicodeScalar(
+            int unicodeScalar,
+            char* buffer,
+            int bufferLength,
+            out int numberOfCharactersWritten)
         {
+            if (buffer == null)
+            {
+                throw new ArgumentNullException(nameof(buffer));
+            }
+
             numberOfCharactersWritten = 0;
             return false;
         }
