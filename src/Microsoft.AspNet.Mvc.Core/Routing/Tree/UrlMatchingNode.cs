gmarica01/Mvc

Replace InnerAttributeRoute with TreeRouter

src/Microsoft.AspNet.Mvc.Core/Routing/Tree/UrlMatchingNode.cs
@@ -1,0 +1,34 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.AspNet.Mvc.Routing
+{
+    public class UrlMatchingNode
+    {
+        public UrlMatchingNode(int length)
+        {
+            Length = length;
+
+            Matches = new List<UrlMatchingEntry>();
+            Literals = new Dictionary<string, UrlMatchingNode>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        public int Length { get; }
+
+        // These entries need to be sorted by precedence then template
+        public List<UrlMatchingEntry> Matches { get; }
+
+        public Dictionary<string, UrlMatchingNode> Literals { get; }
+
+        public UrlMatchingNode ConstrainedParameters { get; set; }
+
+        public UrlMatchingNode Parameters { get; set; }
+
+        public UrlMatchingNode ConstrainedCatchAlls { get; set; }
+
+        public UrlMatchingNode CatchAlls { get; set; }
+    }
+}

dotnet/aspnetcore

src/Microsoft.AspNet.Mvc.Core/Routing/Tree/UrlMatchingNode.cs
@@ -1,0 +1,34 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.AspNet.Mvc.Routing
+{
+    public class UrlMatchingNode
+    {
+        public UrlMatchingNode(int length)
+        {
+            Length = length;
+
+            Matches = new List<TreeRouteMatchingEntry>();
+            Literals = new Dictionary<string, UrlMatchingNode>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        public int Length { get; }
+
+        // These entries are sorted by precedence then template
+        public List<TreeRouteMatchingEntry> Matches { get; }
+
+        public Dictionary<string, UrlMatchingNode> Literals { get; }
+
+        public UrlMatchingNode ConstrainedParameters { get; set; }
+
+        public UrlMatchingNode Parameters { get; set; }
+
+        public UrlMatchingNode ConstrainedCatchAlls { get; set; }
+
+        public UrlMatchingNode CatchAlls { get; set; }
+    }
+}