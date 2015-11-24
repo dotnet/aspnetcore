4ba80c32100c2fea86054bb8c4c729ed6eea133e
SteveSandersonMS <stevesa@microsoft.com>
2015-11-24 14:20:14 +0000

    Fix styling in React example

--- samples/react/ReactGrid/ReactApp/boot-client.jsx
+++ samples/react/ReactGrid/ReactApp/boot-client.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import ReactApp from './components/ReactApp.jsx';
+import 'bootstrap/dist/css/bootstrap.css';
 
 // In the browser, we render into a DOM node and hook up to the browser's history APIs
 var history = createBrowserHistory();
