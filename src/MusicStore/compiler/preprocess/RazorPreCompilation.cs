705769bda6943bec6c1585afdfe207f0c23de5f6
Pavel Krymets <pavel@krymets.com>
2015-11-24 15:40:45 -0800

    Fix build

--- src/MusicStore/compiler/preprocess/RazorPreCompilation.cs
+++ src/MusicStore/compiler/preprocess/RazorPreCompilation.cs
@@ -6,11 +6,5 @@ namespace MusicStore
 {
     public class RazorPreCompilation : RazorPreCompileModule
     {
-        public RazorPreCompilation(IApplicationEnvironment applicationEnvironment)
-        {
-            GenerateSymbols = string.Equals(applicationEnvironment.Configuration,
-                                            "debug",
-                                            StringComparison.OrdinalIgnoreCase);
-        }
     }
 }
