aspnet/Mvc

Remove IActionContextAccessor from UrlHelper
This change removes the IActionContextAccessor as a dependency of
UrlHelper, and shifts UrlHelper to use a factory pattern. Consumers of
IUrlHelper should create an instance using the factory when needed.

This is the last part of MVC that has a dependency on IActionContext
accessor. As part of this change we no longer register it by default, and
treat it as an optional component.

src/Microsoft.AspNet.Mvc.Core/CreatedAtActionResult.cs
@@ -5,6 +5,7 @@ using System;
 using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Mvc.Core;
 using Microsoft.AspNet.Routing;
+using Microsoft.AspNet.Mvc.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Net.Http.Headers;
 
@@ -67,7 +68,13 @@ namespace Microsoft.AspNet.Mvc
             base.OnFormatting(context);
 
             var request = context.HttpContext.Request;
-            var urlHelper = UrlHelper ?? context.HttpContext.RequestServices.GetRequiredService<IUrlHelper>();
+
+            var urlHelper = UrlHelper;
+            if (urlHelper == null)
+            {
+                var services = context.HttpContext.RequestServices;
+                urlHelper = services.GetRequiredService<IUrlHelperFactory>().Create(context);
+            }
 
             var url = urlHelper.Action(
                 ActionName,

src/Microsoft.AspNet.Mvc.Core/RedirectResult.cs
@@ -4,6 +4,7 @@
 using System;
 using Microsoft.AspNet.Mvc.Core;
 using Microsoft.AspNet.Mvc.Logging;
+using Microsoft.AspNet.Mvc.Routing;
 using Microsoft.AspNet.Mvc.ViewFeatures;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -85,7 +86,14 @@ namespace Microsoft.AspNet.Mvc
 
         private IUrlHelper GetUrlHelper(ActionContext context)
         {
-            return UrlHelper ?? context.HttpContext.RequestServices.GetRequiredService<IUrlHelper>();
+            var urlHelper = UrlHelper;
+            if (urlHelper == null)
+            {
+                var services = context.HttpContext.RequestServices;
+                urlHelper = services.GetRequiredService<IUrlHelperFactory>().Create(context);
+            }
+
+            return urlHelper;
         }
     }
 }

src/Microsoft.AspNet.Mvc.Razor/RazorPage.cs
@@ -15,6 +15,7 @@ using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Mvc.Infrastructure;
 using Microsoft.AspNet.Mvc.Razor.Internal;
 using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Mvc.Routing;
 using Microsoft.AspNet.Mvc.ViewFeatures;
 using Microsoft.AspNet.Razor.Runtime.TagHelpers;
 using Microsoft.AspNet.Razor.TagHelpers;
@@ -629,7 +630,9 @@ namespace Microsoft.AspNet.Mvc.Razor
 
             if (_urlHelper == null)
             {
-                _urlHelper = Context.RequestServices.GetRequiredService<IUrlHelper>();
+                var services = Context.RequestServices;
+                var factory = services.GetRequiredService<IUrlHelperFactory>();
+                _urlHelper = factory.Create(ViewContext);
             }
 
             return _urlHelper.Content(contentPath);

test/Microsoft.AspNet.Mvc.ViewFeatures.Test/Rendering/DefaultTemplatesUtilities.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNet.Http.Internal;
 using Microsoft.AspNet.Mvc.Abstractions;
 using Microsoft.AspNet.Mvc.ModelBinding;
 using Microsoft.AspNet.Mvc.ModelBinding.Validation;
+using Microsoft.AspNet.Mvc.Routing;
 using Microsoft.AspNet.Mvc.ViewEngines;
 using Microsoft.AspNet.Mvc.ViewFeatures;
 using Microsoft.AspNet.Routing;
@@ -232,13 +233,18 @@ namespace Microsoft.AspNet.Mvc.Rendering
                 .SetupGet(o => o.Value)
                 .Returns(options);
 
+            var urlHelperFactory = new Mock<IUrlHelperFactory>();
+            urlHelperFactory
+                .Setup(f => f.Create(It.IsAny<ActionContext>()))
+                .Returns(urlHelper);
+
             var serviceProvider = new Mock<IServiceProvider>();
             serviceProvider
                 .Setup(s => s.GetService(typeof(ICompositeViewEngine)))
                 .Returns(viewEngine);
             serviceProvider
-                .Setup(s => s.GetService(typeof(IUrlHelper)))
-                .Returns(urlHelper);
+                .Setup(s => s.GetService(typeof(IUrlHelperFactory)))
+                .Returns(urlHelperFactory.Object);
             serviceProvider
                 .Setup(s => s.GetService(typeof(IViewComponentHelper)))
                 .Returns(new Mock<IViewComponentHelper>().Object);
@@ -253,7 +259,7 @@ namespace Microsoft.AspNet.Mvc.Rendering
                     Mock.Of<IAntiforgery>(),
                     optionsAccessor.Object,
                     provider,
-                    urlHelper,
+                    urlHelperFactory.Object,
                     new HtmlTestEncoder());
             }
 

test/WebSites/RoutingWebSite/TestResponseGenerator.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Mvc.Controllers;
 using Microsoft.AspNet.Mvc.Infrastructure;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNet.Mvc.Routing;
 
 namespace RoutingWebSite
 {
@@ -15,9 +15,12 @@ namespace RoutingWebSite
     public class TestResponseGenerator
     {
         private readonly ActionContext _actionContext;
+        private readonly IUrlHelperFactory _urlHelperFactory;
 
-        public TestResponseGenerator(IActionContextAccessor contextAccessor)
+        public TestResponseGenerator(IActionContextAccessor contextAccessor, IUrlHelperFactory urlHelperFactory)
         {
+            _urlHelperFactory = urlHelperFactory;
+
             _actionContext = contextAccessor.ActionContext;
             if (_actionContext == null)
             {
@@ -35,7 +38,7 @@ namespace RoutingWebSite
                     .Where(kvp => kvp.Key != "link" && kvp.Key != "link_action" && kvp.Key != "link_controller")
                     .ToDictionary(kvp => kvp.Key.Substring("link_".Length), kvp => (object)kvp.Value[0]);
 
-                var urlHelper = _actionContext.HttpContext.RequestServices.GetRequiredService<IUrlHelper>();
+                var urlHelper = _urlHelperFactory.Create(_actionContext);
                 link = urlHelper.Action(query["link_action"], query["link_controller"], values);
             }
 

test/WebSites/TagHelpersWebSite/TagHelpers/ATagHelper.cs
@@ -3,20 +3,26 @@
 
 using System.Linq;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.AspNet.Mvc.Routing;
+using Microsoft.AspNet.Mvc.ViewFeatures;
 using Microsoft.AspNet.Razor.TagHelpers;
 
 namespace TagHelpersWebSite.TagHelpers
 {
     public class ATagHelper : TagHelper
     {
-        public ATagHelper(IUrlHelper urlHelper)
+        public ATagHelper(IUrlHelperFactory urlHelperFactory)
         {
-            UrlHelper = urlHelper;
+            UrlHelperFactory = urlHelperFactory;
         }
 
         [HtmlAttributeNotBound]
-        public IUrlHelper UrlHelper { get; }
+        public IUrlHelperFactory UrlHelperFactory { get; }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
 
         public string Controller { get; set; }
 
@@ -33,7 +39,8 @@ namespace TagHelpersWebSite.TagHelpers
                 // be parameters to our final href value.
                 output.Attributes.Clear();
 
-                output.Attributes["href"] = UrlHelper.Action(Action, Controller, methodParameters);
+                var urlHelper = UrlHelperFactory.Create(ViewContext);
+                output.Attributes["href"] = urlHelper.Action(Action, Controller, methodParameters);
 
                 output.PreContent.SetContent("My ");
             }

dotnet/aspnetcore

src/Microsoft.AspNet.Mvc.Core/CreatedAtActionResult.cs
@@ -5,6 +5,7 @@ using System;
 using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Mvc.Core;
 using Microsoft.AspNet.Routing;
+using Microsoft.AspNet.Mvc.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Net.Http.Headers;
 
@@ -67,7 +68,13 @@ namespace Microsoft.AspNet.Mvc
             base.OnFormatting(context);
 
             var request = context.HttpContext.Request;
-            var urlHelper = UrlHelper ?? context.HttpContext.RequestServices.GetRequiredService<IUrlHelper>();
+
+            var urlHelper = UrlHelper;
+            if (urlHelper == null)
+            {
+                var services = context.HttpContext.RequestServices;
+                urlHelper = services.GetRequiredService<IUrlHelperFactory>().GetUrlHelper(context);
+            }
 
             var url = urlHelper.Action(
                 ActionName,

src/Microsoft.AspNet.Mvc.Core/RedirectResult.cs
@@ -4,6 +4,7 @@
 using System;
 using Microsoft.AspNet.Mvc.Core;
 using Microsoft.AspNet.Mvc.Logging;
+using Microsoft.AspNet.Mvc.Routing;
 using Microsoft.AspNet.Mvc.ViewFeatures;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -85,7 +86,14 @@ namespace Microsoft.AspNet.Mvc
 
         private IUrlHelper GetUrlHelper(ActionContext context)
         {
-            return UrlHelper ?? context.HttpContext.RequestServices.GetRequiredService<IUrlHelper>();
+            var urlHelper = UrlHelper;
+            if (urlHelper == null)
+            {
+                var services = context.HttpContext.RequestServices;
+                urlHelper = services.GetRequiredService<IUrlHelperFactory>().GetUrlHelper(context);
+            }
+
+            return urlHelper;
         }
     }
 }

src/Microsoft.AspNet.Mvc.Razor/RazorPage.cs
@@ -15,6 +15,7 @@ using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Mvc.Infrastructure;
 using Microsoft.AspNet.Mvc.Razor.Internal;
 using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Mvc.Routing;
 using Microsoft.AspNet.Mvc.ViewFeatures;
 using Microsoft.AspNet.Razor.Runtime.TagHelpers;
 using Microsoft.AspNet.Razor.TagHelpers;
@@ -629,7 +630,9 @@ namespace Microsoft.AspNet.Mvc.Razor
 
             if (_urlHelper == null)
             {
-                _urlHelper = Context.RequestServices.GetRequiredService<IUrlHelper>();
+                var services = Context.RequestServices;
+                var factory = services.GetRequiredService<IUrlHelperFactory>();
+                _urlHelper = factory.GetUrlHelper(ViewContext);
             }
 
             return _urlHelper.Content(contentPath);

test/Microsoft.AspNet.Mvc.ViewFeatures.Test/Rendering/DefaultTemplatesUtilities.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNet.Http.Internal;
 using Microsoft.AspNet.Mvc.Abstractions;
 using Microsoft.AspNet.Mvc.ModelBinding;
 using Microsoft.AspNet.Mvc.ModelBinding.Validation;
+using Microsoft.AspNet.Mvc.Routing;
 using Microsoft.AspNet.Mvc.ViewEngines;
 using Microsoft.AspNet.Mvc.ViewFeatures;
 using Microsoft.AspNet.Routing;
@@ -232,13 +233,18 @@ namespace Microsoft.AspNet.Mvc.Rendering
                 .SetupGet(o => o.Value)
                 .Returns(options);
 
+            var urlHelperFactory = new Mock<IUrlHelperFactory>();
+            urlHelperFactory
+                .Setup(f => f.GetUrlHelper(It.IsAny<ActionContext>()))
+                .Returns(urlHelper);
+
             var serviceProvider = new Mock<IServiceProvider>();
             serviceProvider
                 .Setup(s => s.GetService(typeof(ICompositeViewEngine)))
                 .Returns(viewEngine);
             serviceProvider
-                .Setup(s => s.GetService(typeof(IUrlHelper)))
-                .Returns(urlHelper);
+                .Setup(s => s.GetService(typeof(IUrlHelperFactory)))
+                .Returns(urlHelperFactory.Object);
             serviceProvider
                 .Setup(s => s.GetService(typeof(IViewComponentHelper)))
                 .Returns(new Mock<IViewComponentHelper>().Object);
@@ -253,7 +259,7 @@ namespace Microsoft.AspNet.Mvc.Rendering
                     Mock.Of<IAntiforgery>(),
                     optionsAccessor.Object,
                     provider,
-                    urlHelper,
+                    urlHelperFactory.Object,
                     new HtmlTestEncoder());
             }
 

test/WebSites/RoutingWebSite/TestResponseGenerator.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Mvc.Controllers;
 using Microsoft.AspNet.Mvc.Infrastructure;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNet.Mvc.Routing;
 
 namespace RoutingWebSite
 {
@@ -15,9 +15,12 @@ namespace RoutingWebSite
     public class TestResponseGenerator
     {
         private readonly ActionContext _actionContext;
+        private readonly IUrlHelperFactory _urlHelperFactory;
 
-        public TestResponseGenerator(IActionContextAccessor contextAccessor)
+        public TestResponseGenerator(IActionContextAccessor contextAccessor, IUrlHelperFactory urlHelperFactory)
         {
+            _urlHelperFactory = urlHelperFactory;
+
             _actionContext = contextAccessor.ActionContext;
             if (_actionContext == null)
             {
@@ -35,7 +38,7 @@ namespace RoutingWebSite
                     .Where(kvp => kvp.Key != "link" && kvp.Key != "link_action" && kvp.Key != "link_controller")
                     .ToDictionary(kvp => kvp.Key.Substring("link_".Length), kvp => (object)kvp.Value[0]);
 
-                var urlHelper = _actionContext.HttpContext.RequestServices.GetRequiredService<IUrlHelper>();
+                var urlHelper = _urlHelperFactory.GetUrlHelper(_actionContext);
                 link = urlHelper.Action(query["link_action"], query["link_controller"], values);
             }
 

test/WebSites/TagHelpersWebSite/TagHelpers/ATagHelper.cs
@@ -3,20 +3,26 @@
 
 using System.Linq;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.AspNet.Mvc.Routing;
+using Microsoft.AspNet.Mvc.ViewFeatures;
 using Microsoft.AspNet.Razor.TagHelpers;
 
 namespace TagHelpersWebSite.TagHelpers
 {
     public class ATagHelper : TagHelper
     {
-        public ATagHelper(IUrlHelper urlHelper)
+        public ATagHelper(IUrlHelperFactory urlHelperFactory)
         {
-            UrlHelper = urlHelper;
+            UrlHelperFactory = urlHelperFactory;
         }
 
         [HtmlAttributeNotBound]
-        public IUrlHelper UrlHelper { get; }
+        public IUrlHelperFactory UrlHelperFactory { get; }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
 
         public string Controller { get; set; }
 
@@ -33,7 +39,8 @@ namespace TagHelpersWebSite.TagHelpers
                 // be parameters to our final href value.
                 output.Attributes.Clear();
 
-                output.Attributes["href"] = UrlHelper.Action(Action, Controller, methodParameters);
+                var urlHelper = UrlHelperFactory.GetUrlHelper(ViewContext);
+                output.Attributes["href"] = urlHelper.Action(Action, Controller, methodParameters);
 
                 output.PreContent.SetContent("My ");
             }