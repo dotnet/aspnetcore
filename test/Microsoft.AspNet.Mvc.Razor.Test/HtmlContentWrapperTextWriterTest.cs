aspnet/Mvc

Implement a backing-buffer for Razor using pooled memory
Fixes #3532

test/Microsoft.AspNet.Mvc.Razor.Test/HtmlContentWrapperTextWriterTest.cs
@@ -1,0 +1,159 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc.Razor.Buffer;
+using Microsoft.AspNet.Mvc.Rendering;
+using Xunit;
+
+namespace Microsoft.AspNet.Mvc.Razor
+{
+    public class HtmlContentWrapperTextWriterTest
+    {
+        [Fact]
+        public async Task Write_WritesCharBuffer()
+        {
+            // Arrange
+            var input1 = new ArraySegment<char>(new char[] { 'a', 'b', 'c', 'd' }, 1, 3);
+            var input2 = new ArraySegment<char>(new char[] { 'e', 'f' }, 0, 2);
+            var input3 = new ArraySegment<char>(new char[] { 'g', 'h', 'i', 'j' }, 3, 1);
+            var buffer = new RazorBuffer(new TestRazorBufferSource(), "some-name");
+            var writer = new HtmlContentWrapperTextWriter(buffer, Encoding.UTF8);
+
+            // Act
+            writer.Write(input1.Array, input1.Offset, input1.Count);
+            await writer.WriteAsync(input2.Array, input2.Offset, input2.Count);
+            await writer.WriteLineAsync(input3.Array, input3.Offset, input3.Count);
+
+            // Assert
+            var bufferValues = GetValues(buffer);
+            Assert.Equal(4, bufferValues.Length);
+            Assert.Equal("bcd", bufferValues[0]);
+            Assert.Equal("ef", bufferValues[1]);
+            Assert.Equal("j", bufferValues[2]);
+            Assert.Equal(Environment.NewLine, bufferValues[3]);
+        }
+
+        [Fact]
+        public void Write_SplitsCharBuffer_Into1kbStrings()
+        {
+            // Arrange
+            var charArray = Enumerable.Range(0, 2050).Select(_ => 'a').ToArray();
+            var buffer = new RazorBuffer(new TestRazorBufferSource(), "some-name");
+            var writer = new HtmlContentWrapperTextWriter(buffer, Encoding.UTF8);
+
+            // Act
+            writer.Write(charArray);
+
+            // Assert
+            Assert.Collection(GetValues(buffer),
+                value => Assert.Equal(new string('a', 1024), value),
+                value => Assert.Equal(new string('a', 1024), value),
+                value => Assert.Equal("aa", value));
+        }
+
+        [Fact]
+        public void Write_HtmlContent_AddsToEntries()
+        {
+            // Arrange
+            var buffer = new RazorBuffer(new TestRazorBufferSource(), "some-name");
+            var writer = new HtmlContentWrapperTextWriter(buffer, Encoding.UTF8);
+            var content = new HtmlString("Hello, world!");
+
+            // Act
+            writer.Write(content);
+
+            // Assert
+            Assert.Collection(
+                GetValues(buffer),
+                item => Assert.Same(content, item));
+        }
+
+        [Fact]
+        public void Write_Object_HtmlContent_AddsToEntries()
+        {
+            // Arrange
+            var buffer = new RazorBuffer(new TestRazorBufferSource(), "some-name");
+            var writer = new HtmlContentWrapperTextWriter(buffer, Encoding.UTF8);
+            var content = new HtmlString("Hello, world!");
+
+            // Act
+            writer.Write((object)content);
+
+            // Assert
+            Assert.Collection(
+                GetValues(buffer),
+                item => Assert.Same(content, item));
+        }
+
+        [Fact]
+        public void WriteLine_Object_HtmlContent_AddsToEntries()
+        {
+            // Arrange
+            var buffer = new RazorBuffer(new TestRazorBufferSource(), "some-name");
+            var writer = new HtmlContentWrapperTextWriter(buffer, Encoding.UTF8);
+            var content = new HtmlString("Hello, world!");
+
+            // Act
+            writer.WriteLine(content);
+
+            // Assert
+            Assert.Collection(
+                GetValues(buffer),
+                item => Assert.Same(content, item),
+                item => Assert.Equal(Environment.NewLine, item));
+        }
+
+        [Fact]
+        public async Task Write_WritesStringBuffer()
+        {
+            // Arrange
+            var newLine = Environment.NewLine;
+            var input1 = "Hello";
+            var input2 = "from";
+            var input3 = "ASP";
+            var input4 = ".Net";
+            var buffer = new RazorBuffer(new TestRazorBufferSource(), "some-name");
+            var writer = new HtmlContentWrapperTextWriter(buffer, Encoding.UTF8);
+
+            // Act
+            writer.Write(input1);
+            writer.WriteLine(input2);
+            await writer.WriteAsync(input3);
+            await writer.WriteLineAsync(input4);
+
+            // Assert
+            var actual = GetValues(buffer);
+            Assert.Equal(new object[] { input1, input2, newLine, input3, input4, newLine }, actual);
+        }
+
+        [Fact]
+        public void Write_HtmlContent_WritesToBuffer()
+        {
+            // Arrange
+            var buffer = new RazorBuffer(new TestRazorBufferSource(), "some-name");
+            var writer = new HtmlContentWrapperTextWriter(buffer, Encoding.UTF8);
+            var content = new HtmlString("Hello, world!");
+
+            // Act
+            writer.Write(content);
+
+            // Assert
+            Assert.Collection(
+                GetValues(buffer),
+                item => Assert.Same(content, item));
+        }
+
+        private static object[] GetValues(RazorBuffer buffer)
+        {
+            return buffer.BufferChunks
+                .SelectMany(c => c.Data)
+                .Select(d => d.Value)
+                .TakeWhile(d => d != null)
+                .ToArray();
+        }
+    }
+}

test/Microsoft.AspNet.Mvc.Razor.Test/RazorPageTest.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Html;
 using Microsoft.AspNet.Http.Internal;
 using Microsoft.AspNet.Mvc.Abstractions;
 using Microsoft.AspNet.Mvc.ModelBinding;
+using Microsoft.AspNet.Mvc.Razor.Buffer;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.AspNet.Mvc.TestCommon;
 using Microsoft.AspNet.Mvc.ViewEngines;
@@ -183,7 +184,8 @@ namespace Microsoft.AspNet.Mvc.Razor
             var page = CreatePage(v =>
             {
                 v.HtmlEncoder = new HtmlTestEncoder();
-                v.StartTagHelperWritingScope(new RazorTextWriter(TextWriter.Null, Encoding.UTF8, v.HtmlEncoder));
+                var buffer = new RazorBuffer(new TestRazorBufferSource(), v.Path);
+                v.StartTagHelperWritingScope(new RazorTextWriter(TextWriter.Null, buffer, v.HtmlEncoder));
                 v.Write("Hello ");
                 v.Write("World!");
                 var returnValue = v.EndTagHelperWritingScope();
@@ -1117,7 +1119,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         public async Task Write_WithHtmlString_WritesValueWithoutEncoding()
         {
             // Arrange
-            var writer = new RazorTextWriter(TextWriter.Null, Encoding.UTF8, new HtmlTestEncoder());
+            var buffer = new RazorBuffer(new TestRazorBufferSource(), string.Empty);
+            var writer = new RazorTextWriter(TextWriter.Null, buffer, new HtmlTestEncoder());
 
             var page = CreatePage(p =>
             {
@@ -1129,9 +1132,7 @@ namespace Microsoft.AspNet.Mvc.Razor
             await page.ExecuteAsync();
 
             // Assert
-            var buffer = writer.BufferedWriter.Entries;
-            Assert.Equal(1, buffer.Count);
-            Assert.Equal("Hello world", HtmlContentUtilities.HtmlContentToString(((IHtmlContent)buffer[0])));
+            Assert.Equal("Hello world", HtmlContentUtilities.HtmlContentToString(writer.Buffer));
         }
 
         private static TestableRazorPage CreatePage(

test/Microsoft.AspNet.Mvc.Razor.Test/RazorViewTest.cs
@@ -10,9 +10,11 @@ using Microsoft.AspNet.Http.Features;
 using Microsoft.AspNet.Http.Internal;
 using Microsoft.AspNet.Mvc.Abstractions;
 using Microsoft.AspNet.Mvc.ModelBinding;
+using Microsoft.AspNet.Mvc.Razor.Buffer;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.AspNet.Mvc.ViewFeatures;
 using Microsoft.AspNet.Routing;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Primitives;
 using Microsoft.Extensions.WebEncoders.Testing;
 using Moq;
@@ -1563,6 +1565,10 @@ namespace Microsoft.AspNet.Mvc.Razor
         private static ViewContext CreateViewContext(RazorView view)
         {
             var httpContext = new DefaultHttpContext();
+            var serviceProvider = new ServiceCollection()
+                .AddScoped<IRazorBufferScope, TestRazorBufferSource>()
+                .BuildServiceProvider();
+            httpContext.RequestServices = serviceProvider;
             var actionContext = new ActionContext(httpContext, new RouteData(), new ActionDescriptor());
             return new ViewContext(
                 actionContext,

dotnet/aspnetcore

test/Microsoft.AspNet.Mvc.Razor.Test/HtmlContentWrapperTextWriterTest.cs
@@ -1,0 +1,159 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc.Razor.Buffer;
+using Microsoft.AspNet.Mvc.Rendering;
+using Xunit;
+
+namespace Microsoft.AspNet.Mvc.Razor
+{
+    public class HtmlContentWrapperTextWriterTest
+    {
+        [Fact]
+        public async Task Write_WritesCharBuffer()
+        {
+            // Arrange
+            var input1 = new ArraySegment<char>(new char[] { 'a', 'b', 'c', 'd' }, 1, 3);
+            var input2 = new ArraySegment<char>(new char[] { 'e', 'f' }, 0, 2);
+            var input3 = new ArraySegment<char>(new char[] { 'g', 'h', 'i', 'j' }, 3, 1);
+            var buffer = new RazorBuffer(new TestRazorBufferScope(), "some-name");
+            var writer = new HtmlContentWrapperTextWriter(buffer, Encoding.UTF8);
+
+            // Act
+            writer.Write(input1.Array, input1.Offset, input1.Count);
+            await writer.WriteAsync(input2.Array, input2.Offset, input2.Count);
+            await writer.WriteLineAsync(input3.Array, input3.Offset, input3.Count);
+
+            // Assert
+            var bufferValues = GetValues(buffer);
+            Assert.Equal(4, bufferValues.Length);
+            Assert.Equal("bcd", bufferValues[0]);
+            Assert.Equal("ef", bufferValues[1]);
+            Assert.Equal("j", bufferValues[2]);
+            Assert.Equal(Environment.NewLine, bufferValues[3]);
+        }
+
+        [Fact]
+        public void Write_SplitsCharBuffer_Into1kbStrings()
+        {
+            // Arrange
+            var charArray = Enumerable.Range(0, 2050).Select(_ => 'a').ToArray();
+            var buffer = new RazorBuffer(new TestRazorBufferScope(), "some-name");
+            var writer = new HtmlContentWrapperTextWriter(buffer, Encoding.UTF8);
+
+            // Act
+            writer.Write(charArray);
+
+            // Assert
+            Assert.Collection(GetValues(buffer),
+                value => Assert.Equal(new string('a', 1024), value),
+                value => Assert.Equal(new string('a', 1024), value),
+                value => Assert.Equal("aa", value));
+        }
+
+        [Fact]
+        public void Write_HtmlContent_AddsToEntries()
+        {
+            // Arrange
+            var buffer = new RazorBuffer(new TestRazorBufferScope(), "some-name");
+            var writer = new HtmlContentWrapperTextWriter(buffer, Encoding.UTF8);
+            var content = new HtmlString("Hello, world!");
+
+            // Act
+            writer.Write(content);
+
+            // Assert
+            Assert.Collection(
+                GetValues(buffer),
+                item => Assert.Same(content, item));
+        }
+
+        [Fact]
+        public void Write_Object_HtmlContent_AddsToEntries()
+        {
+            // Arrange
+            var buffer = new RazorBuffer(new TestRazorBufferScope(), "some-name");
+            var writer = new HtmlContentWrapperTextWriter(buffer, Encoding.UTF8);
+            var content = new HtmlString("Hello, world!");
+
+            // Act
+            writer.Write((object)content);
+
+            // Assert
+            Assert.Collection(
+                GetValues(buffer),
+                item => Assert.Same(content, item));
+        }
+
+        [Fact]
+        public void WriteLine_Object_HtmlContent_AddsToEntries()
+        {
+            // Arrange
+            var buffer = new RazorBuffer(new TestRazorBufferScope(), "some-name");
+            var writer = new HtmlContentWrapperTextWriter(buffer, Encoding.UTF8);
+            var content = new HtmlString("Hello, world!");
+
+            // Act
+            writer.WriteLine(content);
+
+            // Assert
+            Assert.Collection(
+                GetValues(buffer),
+                item => Assert.Same(content, item),
+                item => Assert.Equal(Environment.NewLine, item));
+        }
+
+        [Fact]
+        public async Task Write_WritesStringBuffer()
+        {
+            // Arrange
+            var newLine = Environment.NewLine;
+            var input1 = "Hello";
+            var input2 = "from";
+            var input3 = "ASP";
+            var input4 = ".Net";
+            var buffer = new RazorBuffer(new TestRazorBufferScope(), "some-name");
+            var writer = new HtmlContentWrapperTextWriter(buffer, Encoding.UTF8);
+
+            // Act
+            writer.Write(input1);
+            writer.WriteLine(input2);
+            await writer.WriteAsync(input3);
+            await writer.WriteLineAsync(input4);
+
+            // Assert
+            var actual = GetValues(buffer);
+            Assert.Equal(new object[] { input1, input2, newLine, input3, input4, newLine }, actual);
+        }
+
+        [Fact]
+        public void Write_HtmlContent_WritesToBuffer()
+        {
+            // Arrange
+            var buffer = new RazorBuffer(new TestRazorBufferScope(), "some-name");
+            var writer = new HtmlContentWrapperTextWriter(buffer, Encoding.UTF8);
+            var content = new HtmlString("Hello, world!");
+
+            // Act
+            writer.Write(content);
+
+            // Assert
+            Assert.Collection(
+                GetValues(buffer),
+                item => Assert.Same(content, item));
+        }
+
+        private static object[] GetValues(RazorBuffer buffer)
+        {
+            return buffer.BufferSegments
+                .SelectMany(c => c.Data)
+                .Select(d => d.Value)
+                .TakeWhile(d => d != null)
+                .ToArray();
+        }
+    }
+}

test/Microsoft.AspNet.Mvc.Razor.Test/RazorPageTest.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Html;
 using Microsoft.AspNet.Http.Internal;
 using Microsoft.AspNet.Mvc.Abstractions;
 using Microsoft.AspNet.Mvc.ModelBinding;
+using Microsoft.AspNet.Mvc.Razor.Buffer;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.AspNet.Mvc.Routing;
 using Microsoft.AspNet.Mvc.TestCommon;
@@ -184,7 +185,8 @@ namespace Microsoft.AspNet.Mvc.Razor
             var page = CreatePage(v =>
             {
                 v.HtmlEncoder = new HtmlTestEncoder();
-                v.StartTagHelperWritingScope(new RazorTextWriter(TextWriter.Null, Encoding.UTF8, v.HtmlEncoder));
+                var buffer = new RazorBuffer(new TestRazorBufferScope(), v.Path);
+                v.StartTagHelperWritingScope(new RazorTextWriter(TextWriter.Null, buffer, v.HtmlEncoder));
                 v.Write("Hello ");
                 v.Write("World!");
                 var returnValue = v.EndTagHelperWritingScope();
@@ -1125,7 +1127,8 @@ namespace Microsoft.AspNet.Mvc.Razor
         public async Task Write_WithHtmlString_WritesValueWithoutEncoding()
         {
             // Arrange
-            var writer = new RazorTextWriter(TextWriter.Null, Encoding.UTF8, new HtmlTestEncoder());
+            var buffer = new RazorBuffer(new TestRazorBufferScope(), string.Empty);
+            var writer = new RazorTextWriter(TextWriter.Null, buffer, new HtmlTestEncoder());
 
             var page = CreatePage(p =>
             {
@@ -1137,9 +1140,7 @@ namespace Microsoft.AspNet.Mvc.Razor
             await page.ExecuteAsync();
 
             // Assert
-            var buffer = writer.BufferedWriter.Entries;
-            Assert.Equal(1, buffer.Count);
-            Assert.Equal("Hello world", HtmlContentUtilities.HtmlContentToString(((IHtmlContent)buffer[0])));
+            Assert.Equal("Hello world", HtmlContentUtilities.HtmlContentToString(writer.Buffer));
         }
 
         private static TestableRazorPage CreatePage(

test/Microsoft.AspNet.Mvc.Razor.Test/RazorViewTest.cs
@@ -10,9 +10,11 @@ using Microsoft.AspNet.Http.Features;
 using Microsoft.AspNet.Http.Internal;
 using Microsoft.AspNet.Mvc.Abstractions;
 using Microsoft.AspNet.Mvc.ModelBinding;
+using Microsoft.AspNet.Mvc.Razor.Buffer;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.AspNet.Mvc.ViewFeatures;
 using Microsoft.AspNet.Routing;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Primitives;
 using Microsoft.Extensions.WebEncoders.Testing;
 using Moq;
@@ -1563,6 +1565,10 @@ namespace Microsoft.AspNet.Mvc.Razor
         private static ViewContext CreateViewContext(RazorView view)
         {
             var httpContext = new DefaultHttpContext();
+            var serviceProvider = new ServiceCollection()
+                .AddScoped<IRazorBufferScope, TestRazorBufferScope>()
+                .BuildServiceProvider();
+            httpContext.RequestServices = serviceProvider;
             var actionContext = new ActionContext(httpContext, new RouteData(), new ActionDescriptor());
             return new ViewContext(
                 actionContext,