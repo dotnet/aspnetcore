29a4f302d893cc87b79bba7c5426cc00438e009f
David Fowler <davidfowl@gmail.com>
2015-12-06 01:51:38 -0800

    Remove non-essential platform services
    - Remove services that can be registered by the application itself.
    - These services use to come from the DNX but now they are stand alone
    implementations that can be registered by applications if they choose.
    
    #501

--- src/Microsoft.AspNet.Hosting/WebHostBuilder.cs
+++ src/Microsoft.AspNet.Hosting/WebHostBuilder.cs
@@ -98,21 +98,6 @@ namespace Microsoft.AspNet.Hosting
                 services.TryAdd(ServiceDescriptor.Instance(PlatformServices.Default.Runtime));
             }
 
-            if (PlatformServices.Default?.AssemblyLoadContextAccessor != null)
-            {
-                services.TryAdd(ServiceDescriptor.Instance(PlatformServices.Default.AssemblyLoadContextAccessor));
-            }
-
-            if (PlatformServices.Default?.AssemblyLoaderContainer != null)
-            {
-                services.TryAdd(ServiceDescriptor.Instance(PlatformServices.Default.AssemblyLoaderContainer));
-            }
-
-            if (PlatformServices.Default?.LibraryManager != null)
-            {
-                services.TryAdd(ServiceDescriptor.Instance(PlatformServices.Default.LibraryManager));
-            }
-
             return services;
         }
 
