8a0cbe789ec2ce6cb95231182f06a24bab19f070
SteveSandersonMS <stevesa@microsoft.com>
2015-12-10 20:30:02 +0000

    Use cache priming in Music Store sample

--- samples/angular/MusicStore/wwwroot/ng-app/components/app/bootstrap.ts
+++ samples/angular/MusicStore/wwwroot/ng-app/components/app/bootstrap.ts
@@ -1,6 +1,7 @@
 import * as ng from 'angular2/angular2';
 import * as router from 'angular2/router';
-import { Http, HTTP_BINDINGS } from 'angular2/http';
+import { Http, HTTP_PROVIDERS } from 'angular2/http';
+import { CACHE_PRIMED_HTTP_PROVIDERS } from 'angular2-aspnet';
 import { App } from './app';
 
-ng.bootstrap(App, [router.ROUTER_BINDINGS, HTTP_BINDINGS, ng.FormBuilder]);
+ng.bootstrap(App, [router.ROUTER_BINDINGS, HTTP_PROVIDERS, CACHE_PRIMED_HTTP_PROVIDERS, ng.FormBuilder]);
