f49c218bdff8085387d1cb89b4301ae655f0dcbe
John Luo <jt.luo@mail.utoronto.ca>
2015-12-17 21:04:04 -0800

    Reacting to new Hosting API

--- samples/AntiforgerySample/Startup.cs
+++ samples/AntiforgerySample/Startup.cs
@@ -5,6 +5,7 @@ using System;
 using System.IO;
 using Microsoft.AspNet.Antiforgery;
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Routing;
 using Microsoft.Extensions.DependencyInjection;
@@ -68,6 +69,16 @@ namespace AntiforgerySample
             });
 
             app.UseRouter(routes.Build());
+        }
+
+        public static void Main(string[] args)
+        {
+            var application = new WebApplicationBuilder()
+                .UseConfiguration(WebApplicationConfiguration.GetDefault(args))
+                .UseStartup<Startup>()
+                .Build();
+
+            application.Run();
         }
     }
 }
