c31c6cd18677821d0f99c78de9aec50300c1c048
Chris R <Tratcher@outlook.com>
2015-12-21 10:54:47 -0800

    React to Hosting changes.

---
+++ test/LocalizationWebsite/Program.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Hosting;
+
+namespace LocalizationWebsite
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var application = new WebApplicationBuilder()
+                .UseConfiguration(WebApplicationConfiguration.GetDefault(args))
+                .UseStartup("LocalizationWebsite")
+                .Build();
+
+            application.Run();
+        }
+    }
+}
