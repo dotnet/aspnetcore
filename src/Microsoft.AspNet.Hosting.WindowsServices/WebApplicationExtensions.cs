83c8816eb79ca42158fe8fc7684f5013c9d529c8
Erez Testiler <erez@taskmatics.com>
2015-12-22 09:39:50 -0500

    Add support for hosting within a Windows service.

---
+++ src/Microsoft.AspNet.Hosting.WindowsServices/WebApplicationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.ServiceProcess;
+
+namespace Microsoft.AspNet.Hosting.WindowsServices
+{
+    /// <summary>
+    ///     Extensions to <see cref="IWebApplication" for hosting inside a Windows service. />
+    /// </summary>
+    public static class WebApplicationExtensions
+    {
+        /// <summary>
+        ///     Runs the specified web application inside a Windows service and blocks until the service is stopped.
+        /// </summary>
+        /// <param name="application">An instance of the <see cref="IWebApplication"/> to host in the Windows service.</param>
+        /// <example>
+        ///     This example shows how to use <see cref="WebApplicationService.Run"/>.
+        ///     <code>
+        ///         public class Program
+        ///         {
+        ///             public static void Main(string[] args)
+        ///             {
+        ///                 var config = WebApplicationConfiguration.GetDefault(args);
+        ///                 
+        ///                 var application = new WebApplicationBuilder()
+        ///                     .UseConfiguration(config)
+        ///                     .Build();
+        ///          
+        ///                 // This call will block until the service is stopped.
+        ///                 application.RunAsService();
+        ///             }
+        ///         }
+        ///     </code>
+        /// </example>
+        public static void RunAsService(this IWebApplication application)
+        {
+            var webApplicationService = new WebApplicationService(application);
+            ServiceBase.Run(webApplicationService);
+        }
+    }
+}
---
+++ src/Microsoft.AspNet.Hosting.WindowsServices/WebApplicationService.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNet.Hosting.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.ServiceProcess;
+
+namespace Microsoft.AspNet.Hosting.WindowsServices
+{
+    /// <summary>
+    ///     Provides an implementation of a Windows service that hosts ASP.NET.
+    /// </summary>
+    public class WebApplicationService : ServiceBase
+    {
+        private const string HostingJsonFile = "hosting.json";
+        private const string EnvironmentVariablesPrefix = "ASPNET_";
+        private const string ConfigFileKey = "config";
+        private IWebApplication _application;
+        private IDisposable _applicationShutdown;
+        private bool _stopRequestedByWindows;
+
+        /// <summary>
+        /// Creates an instance of <c>WebApplicationService</c> which hosts the specified web application.
+        /// </summary>
+        /// <param name="application">The web application to host in the Windows service.</param>
+        public WebApplicationService(IWebApplication application)
+        {
+            _application = application;
+        }
+
+        protected sealed override void OnStart(string[] args)
+        {
+            OnStarting(args);
+
+            _application
+                .Services
+                .GetRequiredService<IApplicationLifetime>()
+                .ApplicationStopped
+                .Register(() =>
+                {
+                    if (!_stopRequestedByWindows)
+                    {
+                        Stop();
+                    }
+                });
+
+            _applicationShutdown = _application.Start();
+
+            OnStarted();
+        }
+
+        protected sealed override void OnStop()
+        {
+            _stopRequestedByWindows = true;
+            OnStopping();
+            _applicationShutdown?.Dispose();
+            OnStopped();
+        }
+
+        /// <summary>
+        /// Executes before ASP.NET starts.
+        /// </summary>
+        /// <param name="args">The command line arguments passed to the service.</param>
+        protected virtual void OnStarting(string[] args) { }
+
+        /// <summary>
+        /// Executes after ASP.NET starts.
+        /// </summary>
+        protected virtual void OnStarted() { }
+
+        /// <summary>
+        /// Executes before ASP.NET shuts down.
+        /// </summary>
+        protected virtual void OnStopping() { }
+
+        /// <summary>
+        /// Executes after ASP.NET shuts down.
+        /// </summary>
+        protected virtual void OnStopped() { }
+    }
+}
