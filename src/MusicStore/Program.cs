26850f54c2ccc7435bb4dd3ef4f68facc75b68c2
John Luo <jt.luo@mail.utoronto.ca>
2015-12-22 11:22:45 -0800

    Fix build, relocating entrypoint to Program

---
+++ src/MusicStore/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Hosting;
+
+namespace MusicStore
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var application = new WebApplicationBuilder()
+                .UseConfiguration(WebApplicationConfiguration.GetDefault(args))
+                .UseStartup("MusicStore")
+                .Build();
+
+            application.Run();
+        }
+    }
+}
--- src/MusicStore/Startup.cs
+++ src/MusicStore/Startup.cs
@@ -220,15 +220,5 @@ namespace MusicStore
             //Populates the MusicStore sample data
             SampleData.InitializeMusicStoreDatabaseAsync(app.ApplicationServices).Wait();
         }
-
-        public static void Main(string[] args)
-        {
-            var application = new WebApplicationBuilder()
-                .UseConfiguration(WebApplicationConfiguration.GetDefault(args))
-                .UseStartup<Startup>()
-                .Build();
-
-            application.Run();
-        }
     }
 }
