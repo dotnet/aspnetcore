c1589ef9bcd293d5c3c44669d5bb31b6e66fb60a
John Luo <jt.luo@mail.utoronto.ca>
2015-12-23 15:09:59 -0800

    Adding back middleware initialization with options instance.

--- src/Microsoft.AspNet.Localization/ApplicationBuilderExtensions.cs
+++ src/Microsoft.AspNet.Localization/ApplicationBuilderExtensions.cs
@@ -37,5 +37,28 @@ namespace Microsoft.AspNet.Builder
 
             return app.UseMiddleware<RequestLocalizationMiddleware>(options);
         }
+
+        /// <summary>
+        /// Adds the <see cref="RequestLocalizationMiddleware"/> to automatically set culture information for
+        /// requests based on information provided by the client.
+        /// </summary>
+        /// <param name="app">The <see cref="IApplicationBuilder"/>.</param>
+        /// <param name="options">The <see cref="RequestLocalizationOptions"/> to configure the middleware with.</param>
+        /// <returns>The <see cref="IApplicationBuilder"/>.</returns>
+        public static IApplicationBuilder UseRequestLocalization(
+            this IApplicationBuilder app,
+            RequestLocalizationOptions options)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            return app.UseMiddleware<RequestLocalizationMiddleware>(options);
+        }
     }
 }
