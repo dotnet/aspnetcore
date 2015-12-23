6d202b0fdfdc001ff8b2e8d0e782266991643556
John Luo <jt.luo@mail.utoronto.ca>
2015-12-22 20:42:09 -0800

    Standardizing middleware to use configureOptions lambda

--- src/Microsoft.AspNet.IISPlatformHandler/IISPlatformHandlerMiddlewareExtensions.cs
+++ src/Microsoft.AspNet.IISPlatformHandler/IISPlatformHandlerMiddlewareExtensions.cs
@@ -12,7 +12,7 @@ namespace Microsoft.AspNet.Builder
         /// Adds middleware for interacting with the IIS HttpPlatformHandler reverse proxy module.
         /// This will handle forwarded Windows Authentication, request scheme, remote IPs, etc..
         /// </summary>
-        /// <param name="builder"></param>
+        /// <param name="app"></param>
         /// <returns></returns>
         public static IApplicationBuilder UseIISPlatformHandler(this IApplicationBuilder app)
         {
@@ -21,40 +21,25 @@ namespace Microsoft.AspNet.Builder
                 throw new ArgumentNullException(nameof(app));
             }
 
-            return app.UseMiddleware<IISPlatformHandlerMiddleware>(new IISPlatformHandlerOptions());
+            return app.UseIISPlatformHandler(options => { });
         }
 
         /// <summary>
         /// Adds middleware for interacting with the IIS HttpPlatformHandler reverse proxy module.
         /// This will handle forwarded Windows Authentication, request scheme, remote IPs, etc..
         /// </summary>
-        /// <param name="builder"></param>
-        /// <returns></returns>
-        public static IApplicationBuilder UseIISPlatformHandler(this IApplicationBuilder app, IISPlatformHandlerOptions options)
-        {
-            if (app == null)
-            {
-                throw new ArgumentNullException(nameof(app));
-            }
-            if (options == null)
-            {
-                throw new ArgumentNullException(nameof(options));
-            }
-
-            return app.UseMiddleware<IISPlatformHandlerMiddleware>(options);
-        }
-
-        /// <summary>
-        /// Adds middleware for interacting with the IIS HttpPlatformHandler reverse proxy module.
-        /// This will handle forwarded Windows Authentication, request scheme, remote IPs, etc..
-        /// </summary>
-        /// <param name="builder"></param>
+        /// <param name="app"></param>
+        /// <param name="configureOptions"></param>
         /// <returns></returns>
         public static IApplicationBuilder UseIISPlatformHandler(this IApplicationBuilder app, Action<IISPlatformHandlerOptions> configureOptions)
         {
             if (app == null)
             {
                 throw new ArgumentNullException(nameof(app));
+            }
+            if (configureOptions == null)
+            {
+                throw new ArgumentNullException(nameof(configureOptions));
             }
 
             var options = new IISPlatformHandlerOptions();
@@ -63,7 +48,7 @@ namespace Microsoft.AspNet.Builder
                 configureOptions(options);
             }
 
-            return app.UseIISPlatformHandler(options);
+            return app.UseMiddleware<IISPlatformHandlerMiddleware>(options);
         }
     }
 }
