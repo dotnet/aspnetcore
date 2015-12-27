a9f4969cfcacb6b8d861527d2d96760468998ac4
Ben Adams <thundercat@illyriad.co.uk>
2015-12-27 08:24:37 +0000

    Use ArrayPool.Shared for StreamCopyOperation

--- src/Microsoft.AspNet.StaticFiles/StreamCopyOperation.cs
+++ src/Microsoft.AspNet.StaticFiles/StreamCopyOperation.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Buffers;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
@@ -12,49 +13,56 @@ namespace Microsoft.AspNet.StaticFiles
     // FYI: In most cases the source will be a FileStream and the destination will be to the network.
     internal static class StreamCopyOperation
     {
-        private const int DefaultBufferSize = 1024 * 16;
+        private const int DefaultBufferSize = 4096;
 
         internal static async Task CopyToAsync(Stream source, Stream destination, long? length, CancellationToken cancel)
         {
             long? bytesRemaining = length;
-            byte[] buffer = new byte[DefaultBufferSize];
 
-            Debug.Assert(source != null);
-            Debug.Assert(destination != null);
-            Debug.Assert(!bytesRemaining.HasValue || bytesRemaining.Value >= 0);
-            Debug.Assert(buffer != null);
-
-            while (true)
+            var buffer = ArrayPool<byte>.Shared.Rent(DefaultBufferSize);
+            try
             {
-                // The natural end of the range.
-                if (bytesRemaining.HasValue && bytesRemaining.Value <= 0)
+                Debug.Assert(source != null);
+                Debug.Assert(destination != null);
+                Debug.Assert(!bytesRemaining.HasValue || bytesRemaining.Value >= 0);
+                Debug.Assert(buffer != null);
+
+                while (true)
                 {
-                    return;
+                    // The natural end of the range.
+                    if (bytesRemaining.HasValue && bytesRemaining.Value <= 0)
+                    {
+                        return;
+                    }
+
+                    cancel.ThrowIfCancellationRequested();
+
+                    int readLength = buffer.Length;
+                    if (bytesRemaining.HasValue)
+                    {
+                        readLength = (int)Math.Min(bytesRemaining.Value, (long)readLength);
+                    }
+                    int count = await source.ReadAsync(buffer, 0, readLength, cancel);
+
+                    if (bytesRemaining.HasValue)
+                    {
+                        bytesRemaining -= count;
+                    }
+
+                    // End of the source stream.
+                    if (count == 0)
+                    {
+                        return;
+                    }
+
+                    cancel.ThrowIfCancellationRequested();
+
+                    await destination.WriteAsync(buffer, 0, count, cancel);
                 }
-
-                cancel.ThrowIfCancellationRequested();
-
-                int readLength = buffer.Length;
-                if (bytesRemaining.HasValue)
-                {
-                    readLength = (int)Math.Min(bytesRemaining.Value, (long)readLength);
-                }
-                int count = await source.ReadAsync(buffer, 0, readLength, cancel);
-
-                if (bytesRemaining.HasValue)
-                {
-                    bytesRemaining -= count;
-                }
-
-                // End of the source stream.
-                if (count == 0)
-                {
-                    return;
-                }
-
-                cancel.ThrowIfCancellationRequested();
-
-                await destination.WriteAsync(buffer, 0, count, cancel);
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(buffer);
             }
         }
     }
