90601ef85390ffee0acf61ebd472fb3bad37235a
N. Taylor Mullen <nimullen@microsoft.com>
2015-12-28 16:48:37 -0800

    Cleanup `Microsoft.AspNet.Server.IIS` usage.

--- src/MusicStore/CopyAspNetLoader.cmd
+++
@@ -1,8 +0,0 @@
-REM copy the AspNet.Loader.dll to bin folder
-md wwwroot\bin
-
-SET ASPNETLOADER_PACKAGE_BASEPATH=%USERPROFILE%\\.dnx\\packages\Microsoft.AspNet.Loader.IIS.Interop
-REM figure out the path of AspNet.Loader.dll
-FOR /F %%j IN ('dir /b /o:D %ASPNETLOADER_PACKAGE_BASEPATH%\*') do (SET AspNetLoaderPath=%ASPNETLOADER_PACKAGE_BASEPATH%\%%j\tools\AspNet.Loader.dll)
-echo Found AspNetLoader.dll at %AspNetLoaderPath%. Copying to bin\
-copy %AspNetLoaderPath% wwwroot\bin\
--- src/MusicStore/Helios.cmd
+++
@@ -1,6 +0,0 @@
-SET DNX_HOME=%USERPROFILE%\.dnx\
-
-REM copy the AspNet.Loader.dll to the bin folder
-call CopyAspNetLoader.cmd
-
-"%ProgramFiles(x86)%\iis Express\iisexpress.exe" /port:5001 /path:"%cd%\wwwroot"
