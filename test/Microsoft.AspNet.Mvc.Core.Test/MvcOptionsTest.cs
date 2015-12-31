51564d04c02665b7db3abad47d04fe1079a77905
Ryan Nowak <nowakra@gmail.com>
2015-12-30 16:55:51 -0800

    Remove a test that tests Dictionary
    This test is coupled to Dictionary<>'s error message. We don't need this.

--- test/Microsoft.AspNet.Mvc.Core.Test/MvcOptionsTest.cs
+++ test/Microsoft.AspNet.Mvc.Core.Test/MvcOptionsTest.cs
@@ -18,18 +18,5 @@ namespace Microsoft.AspNet.Mvc
             var ex = Assert.Throws<ArgumentOutOfRangeException>(() => options.MaxModelValidationErrors = -1);
             Assert.Equal("value", ex.ParamName);
         }
-
-        [Fact]
-        public void ThrowsWhenMultipleCacheProfilesWithSameNameAreAdded()
-        {
-            // Arrange
-            var options = new MvcOptions();
-            options.CacheProfiles.Add("HelloWorld", new CacheProfile { Duration = 10 });
-
-            // Act & Assert
-            var ex = Assert.Throws<ArgumentException>(
-                () => options.CacheProfiles.Add("HelloWorld", new CacheProfile { Duration = 5 }));
-            Assert.Equal("An item with the same key has already been added.", ex.Message);
-        }
     }
 }
