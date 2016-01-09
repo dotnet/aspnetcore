38d50bab1f96255420c7099e6a99ec5189420935
John Luo <jt.luo@mail.utoronto.ca>
2016-01-08 17:45:28 -0800

    Fixing xml documentation

--- src/Microsoft.AspNet.Diagnostics.Entity/DatabaseErrorPageOptions.cs
+++ src/Microsoft.AspNet.Diagnostics.Entity/DatabaseErrorPageOptions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.AspNet.Diagnostics.Entity;
 using Microsoft.AspNet.Http;
 
 namespace Microsoft.AspNet.Builder
@@ -12,8 +13,7 @@ namespace Microsoft.AspNet.Builder
     {
         /// <summary>
         /// Gets or sets the path that <see cref="MigrationsEndPointMiddleware"/> will listen
-        /// for requests to execute migrations commands. The middleware is only registered if
-        /// <see cref="EnableMigrationCommands"/> is set to true.
+        /// for requests to execute migrations commands.
         /// </summary>
         public virtual PathString MigrationsEndPointPath { get; set; } = MigrationsEndPointOptions.DefaultPath;
     }
--- src/Microsoft.AspNet.Diagnostics.Entity/MigrationsEndPointOptions.cs
+++ src/Microsoft.AspNet.Diagnostics.Entity/MigrationsEndPointOptions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.AspNet.Diagnostics.Entity;
 using Microsoft.AspNet.Http;
 
 namespace Microsoft.AspNet.Builder
