4b64d187c2d9ba5f57cbb7935a24091edf0d0014
Kristian Hellang <kristian@hellang.com>
2016-01-13 12:34:00 +0100

    Promote IFormFile extension methods to IFormFile

--- src/Microsoft.AspNet.Http.Abstractions/IFormFile.cs
+++ src/Microsoft.AspNet.Http.Abstractions/IFormFile.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Microsoft.AspNet.Http
 {
@@ -10,18 +12,52 @@ namespace Microsoft.AspNet.Http
     /// </summary>
     public interface IFormFile
     {
+        /// <summary>
+        /// Gets the raw Content-Type header of the uploaded file.
+        /// </summary>
         string ContentType { get; }
 
+        /// <summary>
+        /// Gets the raw Content-Disposition header of the uploaded file.
+        /// </summary>
         string ContentDisposition { get; }
 
+        /// <summary>
+        /// Gets the header dictionary of the uploaded file.
+        /// </summary>
         IHeaderDictionary Headers { get; }
 
+        /// <summary>
+        /// Gets the file length in bytes.
+        /// </summary>
         long Length { get; }
 
+        /// <summary>
+        /// Gets the name from the Content-Disposition header.
+        /// </summary>
         string Name { get; }
 
+        /// <summary>
+        /// Gets the file name from the Content-Disposition header.
+        /// </summary>
         string FileName { get; }
 
+        /// <summary>
+        /// Opens the request stream for reading the uploaded file.
+        /// </summary>
         Stream OpenReadStream();
+
+        /// <summary>
+        /// Saves the contents of the uploaded file.
+        /// </summary>
+        /// <param name="path">The path of the file to create.</param>
+        void SaveAs(string path);
+
+        /// <summary>
+        /// Asynchronously saves the contents of the uploaded file.
+        /// </summary>
+        /// <param name="path">The path of the file to create.</param>
+        /// <param name="cancellationToken"></param>
+        Task SaveAsAsync(string path, CancellationToken cancellationToken = default(CancellationToken));
     }
 }
