b74e26883e90280e4ed0e9b163bb085578878d3a
Brennan <brecon@microsoft.com>
2016-01-21 09:50:28 -0800

    React to Logging API changes

--- test/Shared/TestLogger.cs
+++ test/Shared/TestLogger.cs
@@ -27,7 +27,7 @@ namespace Microsoft.AspNetCore.Identity.Test
             return true;
         }
 
-        public void Log(LogLevel logLevel, int eventId, object state, Exception exception, Func<object, Exception, string> formatter)
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
             if (formatter == null)
             {
