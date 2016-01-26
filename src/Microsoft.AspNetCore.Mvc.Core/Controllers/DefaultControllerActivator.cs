corefan/Mvc

[Fixes #3752] Cleanup Controller invocation pipeline
* Added a Release method to IControllerActivator
* Changed Create in IControllerActivator to take in a ControllerActionContext
* Move the check to determine if a controller can be instantiated into the controller activator.
* Move logic for disposing controllers into the controller activator and make release on the
  controller factory delegate into the activator.
* Changed release methods to take in a controller context.

src/Microsoft.AspNet.Mvc.Core/Controllers/DefaultControllerActivator.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Reflection;
+using Microsoft.AspNet.Mvc.Core;
 using Microsoft.AspNet.Mvc.Infrastructure;
 
 namespace Microsoft.AspNet.Mvc.Controllers
@@ -19,23 +21,63 @@ namespace Microsoft.AspNet.Mvc.Controllers
         /// <param name="typeActivatorCache">The <see cref="ITypeActivatorCache"/>.</param>
         public DefaultControllerActivator(ITypeActivatorCache typeActivatorCache)
         {
+            if (typeActivatorCache == null)
+            {
+                throw new ArgumentNullException(nameof(typeActivatorCache));
+            }
+
             _typeActivatorCache = typeActivatorCache;
         }
+
         /// <inheritdoc />
-        public virtual object Create(ActionContext actionContext, Type controllerType)
+        public virtual object Create(ControllerContext actionContext)
         {
             if (actionContext == null)
             {
                 throw new ArgumentNullException(nameof(actionContext));
             }
 
-            if (controllerType == null)
+            if (actionContext.ActionDescriptor == null)
             {
-                throw new ArgumentNullException(nameof(controllerType));
+                throw new ArgumentException(Resources.FormatPropertyOfTypeCannotBeNull(
+                    nameof(ControllerContext.ActionDescriptor),
+                    nameof(ControllerContext)));
+            }
+
+            var controllerTypeInfo = actionContext.ActionDescriptor.ControllerTypeInfo;
+            if (controllerTypeInfo.IsValueType ||
+                controllerTypeInfo.IsInterface ||
+                controllerTypeInfo.IsAbstract ||
+                (controllerTypeInfo.IsGenericType && controllerTypeInfo.IsGenericTypeDefinition))
+            {
+                var message = Resources.FormatValueInterfaceAbstractOrOpenGenericTypesCannotBeActivated(
+                    controllerTypeInfo.FullName,
+                    GetType().FullName);
+
+                throw new InvalidOperationException(message);
             }
 
             var serviceProvider = actionContext.HttpContext.RequestServices;
-            return _typeActivatorCache.CreateInstance<object>(serviceProvider, controllerType);
+            return _typeActivatorCache.CreateInstance<object>(serviceProvider, controllerTypeInfo.AsType());
+        }
+
+        public virtual void Release(ControllerContext context, object controller)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (controller == null)
+            {
+                throw new ArgumentNullException(nameof(controller));
+            }
+
+            var disposable = controller as IDisposable;
+            if (disposable != null)
+            {
+                disposable.Dispose();
+            }
         }
     }
 }

test/Microsoft.AspNet.Mvc.Core.Test/Controllers/DefaultControllerActivatorTest.cs
@@ -2,9 +2,12 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Reflection;
 using Microsoft.AspNet.Http.Internal;
 using Microsoft.AspNet.Mvc.Abstractions;
 using Microsoft.AspNet.Mvc.Infrastructure;
+using Microsoft.AspNet.Mvc.ModelBinding;
+using Microsoft.AspNet.Mvc.ModelBinding.Validation;
 using Microsoft.AspNet.Routing;
 using Moq;
 using Xunit;
@@ -26,14 +29,63 @@ namespace Microsoft.AspNet.Mvc.Controllers
             {
                 RequestServices = serviceProvider.Object
             };
-            var actionContext = new ActionContext(httpContext,
-                                                  new RouteData(),
-                                                  new ActionDescriptor());
+
+            var actionContext = new ControllerContext(
+                new ActionContext(
+                    httpContext,
+                    new RouteData(),
+                    new ControllerActionDescriptor
+                    {
+                        ControllerTypeInfo = type.GetTypeInfo()
+                    }));
+
             // Act
-            var instance = activator.Create(actionContext, type);
+            var instance = activator.Create(actionContext);
 
             // Assert
             Assert.IsType(type, instance);
+        }
+
+        [Theory]
+        [InlineData(typeof(int))]
+        [InlineData(typeof(OpenGenericType<>))]
+        [InlineData(typeof(AbstractType))]
+        [InlineData(typeof(InterfaceType))]
+        public void CreateController_ThrowsIfControllerCannotBeActivated(Type type)
+        {
+            // Arrange
+            var actionDescriptor = new ControllerActionDescriptor
+            {
+                ControllerTypeInfo = type.GetTypeInfo()
+            };
+
+            var context = new ControllerContext()
+            {
+                ActionDescriptor = actionDescriptor,
+                HttpContext = new DefaultHttpContext()
+                {
+                    RequestServices = GetServices(),
+                },
+            };
+            var factory = new DefaultControllerActivator(new DefaultTypeActivatorCache());
+
+            // Act and Assert
+            var exception = Assert.Throws<InvalidOperationException>(() => factory.Create(context));
+            Assert.Equal(
+                $"The type '{type.FullName}' cannot be activated by '{typeof(DefaultControllerActivator).FullName}' " +
+                "because it is either a value type, an interface, an abstract class or an open generic type.",
+                exception.Message);
+        }
+
+        [Fact]
+        public void DefaultControllerActivator_ReleasesNonIDisposableController()
+        {
+            // Arrange
+            var activator = new DefaultControllerActivator(Mock.Of<ITypeActivatorCache>());
+            var controller = new object();
+
+            // Act + Assert (does not throw)
+            activator.Release(Mock.Of<ControllerContext>(), controller);
         }
 
         [Fact]
@@ -46,16 +98,23 @@ namespace Microsoft.AspNet.Mvc.Controllers
             serviceProvider.Setup(s => s.GetService(typeof(TestService)))
                            .Returns(testService)
                            .Verifiable();
-                           
+
             var httpContext = new DefaultHttpContext
             {
                 RequestServices = serviceProvider.Object
             };
-            var actionContext = new ActionContext(httpContext,
-                                                  new RouteData(),
-                                                  new ActionDescriptor());
+
+            var actionContext = new ControllerContext(
+                new ActionContext(
+                    httpContext,
+                    new RouteData(),
+                    new ControllerActionDescriptor
+                    {
+                        ControllerTypeInfo = typeof(TypeDerivingFromControllerWithServices).GetTypeInfo()
+                    }));
+
             // Act
-            var instance = activator.Create(actionContext, typeof(TypeDerivingFromControllerWithServices));
+            var instance = activator.Create(actionContext);
 
             // Assert
             var controller = Assert.IsType<TypeDerivingFromControllerWithServices>(instance);
@@ -81,11 +140,36 @@ namespace Microsoft.AspNet.Mvc.Controllers
             public TestService TestService { get; }
         }
 
+        private IServiceProvider GetServices()
+        {
+            var metadataProvider = new EmptyModelMetadataProvider();
+            var services = new Mock<IServiceProvider>();
+            services.Setup(s => s.GetService(typeof(IUrlHelper)))
+                    .Returns(Mock.Of<IUrlHelper>());
+            services.Setup(s => s.GetService(typeof(IModelMetadataProvider)))
+                    .Returns(metadataProvider);
+            services.Setup(s => s.GetService(typeof(IObjectModelValidator)))
+                    .Returns(new DefaultObjectValidator(metadataProvider));
+            return services.Object;
+        }
+
         private class PocoType
         {
         }
 
         private class TestService
+        {
+        }
+
+        private class OpenGenericType<T> : Controller
+        {
+        }
+
+        private abstract class AbstractType : Controller
+        {
+        }
+
+        private interface InterfaceType
         {
         }
     }

dotnet/aspnetcore

src/Microsoft.AspNetCore.Mvc.Core/Controllers/DefaultControllerActivator.cs
@@ -2,6 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.Core;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Internal;
 
 namespace Microsoft.AspNetCore.Mvc.Controllers
@@ -19,23 +22,64 @@ namespace Microsoft.AspNetCore.Mvc.Controllers
         /// <param name="typeActivatorCache">The <see cref="ITypeActivatorCache"/>.</param>
         public DefaultControllerActivator(ITypeActivatorCache typeActivatorCache)
         {
+            if (typeActivatorCache == null)
+            {
+                throw new ArgumentNullException(nameof(typeActivatorCache));
+            }
+
             _typeActivatorCache = typeActivatorCache;
         }
+
         /// <inheritdoc />
-        public virtual object Create(ActionContext actionContext, Type controllerType)
+        public virtual object Create(ControllerContext controllerContext)
         {
-            if (actionContext == null)
+            if (controllerContext == null)
             {
-                throw new ArgumentNullException(nameof(actionContext));
+                throw new ArgumentNullException(nameof(controllerContext));
             }
 
-            if (controllerType == null)
+            if (controllerContext.ActionDescriptor == null)
             {
-                throw new ArgumentNullException(nameof(controllerType));
+                throw new ArgumentException(Resources.FormatPropertyOfTypeCannotBeNull(
+                    nameof(ControllerContext.ActionDescriptor),
+                    nameof(ControllerContext)));
             }
 
-            var serviceProvider = actionContext.HttpContext.RequestServices;
-            return _typeActivatorCache.CreateInstance<object>(serviceProvider, controllerType);
+            var controllerTypeInfo = controllerContext.ActionDescriptor.ControllerTypeInfo;
+            if (controllerTypeInfo.IsValueType ||
+                controllerTypeInfo.IsInterface ||
+                controllerTypeInfo.IsAbstract ||
+                (controllerTypeInfo.IsGenericType && controllerTypeInfo.IsGenericTypeDefinition))
+            {
+                var message = Resources.FormatValueInterfaceAbstractOrOpenGenericTypesCannotBeActivated(
+                    controllerTypeInfo.FullName,
+                    GetType().FullName);
+
+                throw new InvalidOperationException(message);
+            }
+
+            var serviceProvider = controllerContext.HttpContext.RequestServices;
+            return _typeActivatorCache.CreateInstance<object>(serviceProvider, controllerTypeInfo.AsType());
+        }
+
+        /// <inheritdoc />
+        public virtual void Release(ControllerContext context, object controller)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (controller == null)
+            {
+                throw new ArgumentNullException(nameof(controller));
+            }
+
+            var disposable = controller as IDisposable;
+            if (disposable != null)
+            {
+                disposable.Dispose();
+            }
         }
     }
 }

test/Microsoft.AspNetCore.Mvc.Core.Test/Controllers/DefaultControllerActivatorTest.cs
@@ -2,9 +2,13 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Reflection;
 using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Routing;
 using Moq;
 using Xunit;
@@ -26,14 +30,77 @@ namespace Microsoft.AspNetCore.Mvc.Controllers
             {
                 RequestServices = serviceProvider.Object
             };
-            var actionContext = new ActionContext(httpContext,
-                                                  new RouteData(),
-                                                  new ActionDescriptor());
+
+            var context = new ControllerContext(
+                new ActionContext(
+                    httpContext,
+                    new RouteData(),
+                    new ControllerActionDescriptor
+                    {
+                        ControllerTypeInfo = type.GetTypeInfo()
+                    }));
+
             // Act
-            var instance = activator.Create(actionContext, type);
+            var instance = activator.Create(context);
 
             // Assert
             Assert.IsType(type, instance);
+        }
+
+        [Fact]
+        public void Release_DisposesController_IfDisposable()
+        {
+            // Arrange
+            var controller = new MyController();
+            var activator = new DefaultControllerActivator(Mock.Of<ITypeActivatorCache>());
+
+            // Act
+            activator.Release(new ControllerContext(), controller);
+
+            // Assert
+            Assert.Equal(true, controller.Disposed);
+        }
+
+        [Theory]
+        [InlineData(typeof(int))]
+        [InlineData(typeof(OpenGenericType<>))]
+        [InlineData(typeof(AbstractType))]
+        [InlineData(typeof(InterfaceType))]
+        public void CreateController_ThrowsIfControllerCannotBeActivated(Type type)
+        {
+            // Arrange
+            var actionDescriptor = new ControllerActionDescriptor
+            {
+                ControllerTypeInfo = type.GetTypeInfo()
+            };
+
+            var context = new ControllerContext()
+            {
+                ActionDescriptor = actionDescriptor,
+                HttpContext = new DefaultHttpContext()
+                {
+                    RequestServices = GetServices(),
+                },
+            };
+            var factory = new DefaultControllerActivator(new TypeActivatorCache());
+
+            // Act and Assert
+            var exception = Assert.Throws<InvalidOperationException>(() => factory.Create(context));
+            Assert.Equal(
+                $"The type '{type.FullName}' cannot be activated by '{typeof(DefaultControllerActivator).FullName}' " +
+                "because it is either a value type, an interface, an abstract class or an open generic type.",
+                exception.Message);
+        }
+
+        [Fact]
+        public void DefaultControllerActivator_ReleasesNonIDisposableController()
+        {
+            // Arrange
+            var activator = new DefaultControllerActivator(Mock.Of<ITypeActivatorCache>());
+            var controller = new object();
+
+            // Act + Assert (does not throw)
+            activator.Release(Mock.Of<ControllerContext>(), controller);
         }
 
         [Fact]
@@ -46,16 +113,23 @@ namespace Microsoft.AspNetCore.Mvc.Controllers
             serviceProvider.Setup(s => s.GetService(typeof(TestService)))
                            .Returns(testService)
                            .Verifiable();
-                           
+
             var httpContext = new DefaultHttpContext
             {
                 RequestServices = serviceProvider.Object
             };
-            var actionContext = new ActionContext(httpContext,
-                                                  new RouteData(),
-                                                  new ActionDescriptor());
+
+            var context = new ControllerContext(
+                new ActionContext(
+                    httpContext,
+                    new RouteData(),
+                    new ControllerActionDescriptor
+                    {
+                        ControllerTypeInfo = typeof(TypeDerivingFromControllerWithServices).GetTypeInfo()
+                    }));
+
             // Act
-            var instance = activator.Create(actionContext, typeof(TypeDerivingFromControllerWithServices));
+            var instance = activator.Create(context);
 
             // Assert
             var controller = Assert.IsType<TypeDerivingFromControllerWithServices>(instance);
@@ -81,12 +155,47 @@ namespace Microsoft.AspNetCore.Mvc.Controllers
             public TestService TestService { get; }
         }
 
+        private IServiceProvider GetServices()
+        {
+            var metadataProvider = new EmptyModelMetadataProvider();
+            var services = new Mock<IServiceProvider>();
+            services.Setup(s => s.GetService(typeof(IUrlHelper)))
+                .Returns(Mock.Of<IUrlHelper>());
+            services.Setup(s => s.GetService(typeof(IModelMetadataProvider)))
+                .Returns(metadataProvider);
+            services.Setup(s => s.GetService(typeof(IObjectModelValidator)))
+                .Returns(new DefaultObjectValidator(metadataProvider));
+            return services.Object;
+        }
+
         private class PocoType
         {
         }
 
         private class TestService
         {
+        }
+
+        private class OpenGenericType<T> : Controller
+        {
+        }
+
+        private abstract class AbstractType : Controller
+        {
+        }
+
+        private interface InterfaceType
+        {
+        }
+
+        private class MyController : IDisposable
+        {
+            public bool Disposed { get; set; }
+
+            public void Dispose()
+            {
+                Disposed = true;
+            }
         }
     }
 }