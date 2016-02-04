b1df299ec396f4e0164cb8ca61e15fc5730d5f33
Hisham Bin Ateya <hishamco_2007@hotmail.com>
2016-02-04 13:37:04 +0300

    Add 'UseServer'

--- samples/AntiforgerySample/Startup.cs
+++ samples/AntiforgerySample/Startup.cs
@@ -70,6 +70,7 @@ namespace AntiforgerySample
         {
             var host = new WebHostBuilder()
                 .UseDefaultConfiguration(args)
+                .UseServer("Microsoft.AspNetCore.Server.Kestrel")
                 .UseIISPlatformHandlerUrl()
                 .UseStartup<Startup>()
                 .Build();
