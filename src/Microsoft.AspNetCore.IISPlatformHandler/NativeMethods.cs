078604f3a862db1dbf58ae4f6ee7ce42471f5977
N. Taylor Mullen <nimullen@microsoft.com>
2016-03-01 13:36:12 -0800

    Transition to netstandard.
    - dotnet5.X => netstandard1.y (where y = x-1).
    - DNXCore50 => netstandardapp1.5.
    - Applied the same changes to ifdefs.

--- src/Microsoft.AspNetCore.IISPlatformHandler/NativeMethods.cs
+++ src/Microsoft.AspNetCore.IISPlatformHandler/NativeMethods.cs
@@ -8,13 +8,13 @@ namespace Microsoft.AspNetCore.IISPlatformHandler
 {
     internal class NativeMethods
     {
-#if DOTNET5_4
+#if NETSTANDARD1_3
         private const string api_ms_win_core_handle_LIB = "api-ms-win-core-handle-l1-1-0.dll";
 #else
         private const string KERNEL32 = "kernel32.dll";
 #endif
 
-#if DOTNET5_4
+#if NETSTANDARD1_3
         [DllImport(api_ms_win_core_handle_LIB, ExactSpelling = true, SetLastError = true)]
 #else
         [DllImport(KERNEL32, ExactSpelling = true, SetLastError = true)]
