2c3f29df11ced33e55e1f9d1b079fa5dfdfaf9ec
SteveSandersonMS <stevesa@microsoft.com>
2016-03-01 13:23:51 +0000

    Avoid bogus build warning about unused variable

--- templates/ReactSpa/ClientApp/boot.tsx
+++ templates/ReactSpa/ClientApp/boot.tsx
@@ -4,7 +4,7 @@ import './css/site.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { browserHistory, Router } from 'react-router';
-import { routes } from './routes';
+import routes from './routes';
 
 // This code starts up the React app when it runs in a browser. It sets up the routing configuration
 // and injects the app into a DOM element.
--- templates/ReactSpa/ClientApp/routes.tsx
+++ templates/ReactSpa/ClientApp/routes.tsx
@@ -5,7 +5,7 @@ import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
-export const routes = <Route component={ Layout }>
+export default <Route component={ Layout }>
     <Route path='/' components={{ body: Home }} />
     <Route path='/counter' components={{ body: Counter }} />
     <Route path='/fetchdata' components={{ body: FetchData }} />
