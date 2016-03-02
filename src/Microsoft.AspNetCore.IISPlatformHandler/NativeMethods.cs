911506dc6be419db58cccd5d4089cfeadfc23715
N. Taylor Mullen <nimullen@microsoft.com>
2016-03-02 10:06:46 -0800

    Revert "Transition to netstandard."
    This reverts commit 078604f3a862db1dbf58ae4f6ee7ce42471f5977.

--- src/Microsoft.AspNetCore.IISPlatformHandler/NativeMethods.cs
+++ src/Microsoft.AspNetCore.IISPlatformHandler/NativeMethods.cs
@@ -8,13 +8,13 @@ namespace Microsoft.AspNetCore.IISPlatformHandler
 {
     internal class NativeMethods
     {
-#if NETSTANDARD1_3
+#if DOTNET5_4
         private const string api_ms_win_core_handle_LIB = "api-ms-win-core-handle-l1-1-0.dll";
 #else
         private const string KERNEL32 = "kernel32.dll";
 #endif
 
-#if NETSTANDARD1_3
+#if DOTNET5_4
         [DllImport(api_ms_win_core_handle_LIB, ExactSpelling = true, SetLastError = true)]
 #else
         [DllImport(KERNEL32, ExactSpelling = true, SetLastError = true)]
