6b933b5e8a40c07c4aeff84636afcc6e3c9abb2c
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2016-03-03 18:10:20 -0800

    Removed duplicate attribute

--- src/Microsoft.AspNetCore.TestHost/Properties/AssemblyInfo.cs
+++ src/Microsoft.AspNetCore.TestHost/Properties/AssemblyInfo.cs
@@ -26,7 +26,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Microsoft.AspNetCore.TestHost")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Microsoft.AspNetCore.TestHost")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
