73a9ed7a64a81f8c471dabd722ae990201b84d58
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2016-03-03 17:33:17 -0800

    Added Company, Copyright and Product attributes to AssemblyInfo

--- src/Microsoft.AspNetCore.MiddlewareAnalysis/Properties/AssemblyInfo.cs
+++ src/Microsoft.AspNetCore.MiddlewareAnalysis/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Reflection;
@@ -6,3 +6,6 @@ using System.Resources;
 
 [assembly: AssemblyMetadata("Serviceable", "True")]
 [assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyCompany("Microsoft Corporation.")]
+[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
+[assembly: AssemblyProduct("Microsoft ASP.NET Core")]
