dd7198f410c7a039cf67732f9c82ef352e1bb6a3
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2016-03-03 17:32:08 -0800

    Added Company, Copyright and Product attributes to AssemblyInfo

--- src/Microsoft.Net.Http.Headers/Properties/AssemblyInfo.cs
+++ src/Microsoft.Net.Http.Headers/Properties/AssemblyInfo.cs
@@ -6,3 +6,5 @@ using System.Resources;
 
 [assembly: AssemblyMetadata("Serviceable", "True")]
 [assembly: NeutralResourcesLanguage("en-us")]
+[assembly: AssemblyCompany("Microsoft Corporation.")]
+[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
