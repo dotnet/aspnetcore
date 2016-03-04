94e0088eea883c3dc9a268f1c3939dfe958f8870
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2016-03-03 17:33:22 -0800

    Added Company, Copyright and Product attributes to AssemblyInfo

--- src/Microsoft.Owin.Security.Interop/Properties/AssemblyInfo.cs
+++ src/Microsoft.Owin.Security.Interop/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Reflection;
@@ -10,3 +10,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a7922dd8-09f1-43e4-938b-cc523ea08898")]
+[assembly: AssemblyCompany("Microsoft Corporation.")]
+[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
