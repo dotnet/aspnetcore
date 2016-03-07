f3740b2196ea5ca035971d40ad11df5ae4402e96
Pranav K <prkrishn@hotmail.com>
2016-03-07 15:49:11 -0800

    Reacting to KoreBuild xunit test changes

--- test/Microsoft.AspNetCore.Mvc.FunctionalTests/MvcSampleFixture.cs
+++ test/Microsoft.AspNetCore.Mvc.FunctionalTests/MvcSampleFixture.cs
@@ -1,16 +1,14 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.IO;
+
 namespace Microsoft.AspNetCore.Mvc.FunctionalTests
 {
     public class MvcSampleFixture<TStartup> : MvcTestFixture<TStartup>
     {
         public MvcSampleFixture()
-#if NETSTANDARDAPP1_5
-            :base("../../../../../../samples/")
-#else
-            : base("../../../../samples/")
-#endif
+            : base(Path.Combine("..", "..", "..", "..", "..", "..", "samples"))
         {
         }
     }
--- test/Microsoft.AspNetCore.Mvc.FunctionalTests/MvcTestFixture.cs
+++ test/Microsoft.AspNetCore.Mvc.FunctionalTests/MvcTestFixture.cs
@@ -24,11 +24,7 @@ namespace Microsoft.AspNetCore.Mvc.FunctionalTests
         private readonly TestServer _server;
 
         public MvcTestFixture()
-#if NETSTANDARDAPP1_5
             : this(Path.Combine("..", "..", "..", "..", "..", "WebSites"))
-#else
-            : this(Path.Combine("..", "..", "..", "WebSites"))
-#endif
         {
         }
 
