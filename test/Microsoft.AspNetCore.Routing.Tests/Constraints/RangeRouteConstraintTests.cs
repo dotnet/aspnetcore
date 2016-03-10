b7fc5f06cd6d74f97c1b45b19f3bda20c9198bc1
John Luo <jt.luo@mail.utoronto.ca>
2016-03-10 15:15:56 -0800

    Fix test with mismatched literal type

--- test/Microsoft.AspNetCore.Routing.Tests/Constraints/RangeRouteConstraintTests.cs
+++ test/Microsoft.AspNetCore.Routing.Tests/Constraints/RangeRouteConstraintTests.cs
@@ -42,7 +42,7 @@ namespace Microsoft.AspNetCore.Routing.Tests
                 () => new RangeRouteConstraint(3, 2),
                 "min",
                 expectedMessage,
-                3);
+                3L);
         }
     }
 }
