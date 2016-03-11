5c50ba0898e2141daa2ac2b1ec484e9576acd32d
moozzyk <pawelka@microsoft.com>
2016-03-11 10:19:24 -0800

    Creating Microsoft.AspNetCore.Server.IISIntegration.Tools package
    See #95

---
+++ src/Microsoft.AspNetCore.Server.IISIntegration.Tools/WebConfigTransform.cs
@@ -0,0 +1,104 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Microsoft.AspNetCore.Server.IISIntegration.Tools
+{
+    public static class WebConfigTransform
+    {
+        public static XDocument Transform(XDocument webConfig, string appName, bool configureForAzure)
+        {
+            const string HandlersElementName = "handlers";
+            const string httpPlatformElementName = "httpPlatform";
+
+            webConfig = webConfig == null || webConfig.Root.Name.LocalName != "configuration"
+                ? XDocument.Parse("<configuration />")
+                : webConfig;
+
+            var webServerSection = GetOrCreateChild(webConfig.Root, "system.webServer");
+
+            TransformHandlers(GetOrCreateChild(webServerSection, HandlersElementName));
+            TransformHttpPlatform(GetOrCreateChild(webServerSection, httpPlatformElementName), appName, configureForAzure);
+
+            // make sure that the httpPlatform element is after handlers element
+            var httpPlatformElement = webServerSection.Element(HandlersElementName)
+                .ElementsBeforeSelf(httpPlatformElementName).SingleOrDefault();
+            if (httpPlatformElement != null)
+            {
+                httpPlatformElement.Remove();
+                webServerSection.Element(HandlersElementName).AddAfterSelf(httpPlatformElement);
+            }
+
+            return webConfig;
+        }
+
+        private static void TransformHandlers(XElement handlersElement)
+        {
+            var platformHandlerElement =
+                handlersElement.Elements("add")
+                    .FirstOrDefault(e => string.Equals((string)e.Attribute("name"), "httpplatformhandler", StringComparison.OrdinalIgnoreCase));
+
+            if (platformHandlerElement == null)
+            {
+                platformHandlerElement = new XElement("add");
+                handlersElement.Add(platformHandlerElement);
+            }
+
+            platformHandlerElement.SetAttributeValue("name", "httpPlatformHandler");
+            SetAttributeValueIfEmpty(platformHandlerElement, "path", "*");
+            SetAttributeValueIfEmpty(platformHandlerElement, "verb", "*");
+            SetAttributeValueIfEmpty(platformHandlerElement, "modules", "httpPlatformHandler");
+            SetAttributeValueIfEmpty(platformHandlerElement, "resourceType", "Unspecified");
+        }
+
+        private static void TransformHttpPlatform(XElement httpPlatformElement, string appName, bool configureForAzure)
+        {
+            var appPath = Path.Combine(configureForAzure ? @"%home%\site" : "..", appName);
+            var logPath = Path.Combine(configureForAzure ? @"\\?\%home%\LogFiles" : @"..\logs", "stdout.log");
+
+            httpPlatformElement.SetAttributeValue("processPath", appPath);
+            SetAttributeValueIfEmpty(httpPlatformElement, "stdoutLogEnabled", "false");
+            SetAttributeValueIfEmpty(httpPlatformElement, "stdoutLogFile", logPath);
+            SetAttributeValueIfEmpty(httpPlatformElement, "startupTimeLimit", "3600");
+
+            AddApplicationBase(httpPlatformElement);
+        }
+
+        private static void AddApplicationBase(XElement httpPlatformElement)
+        {
+            const string appBaseKeyName = "ASPNET_APPLICATIONBASE";
+
+            var envVariables = GetOrCreateChild(httpPlatformElement, "environmentVariables");
+            var appBaseElement = envVariables.Elements("environmentVariable").SingleOrDefault(e =>
+                string.Equals((string)e.Attribute("name"), appBaseKeyName, StringComparison.CurrentCultureIgnoreCase));
+
+            if (appBaseElement == null)
+            {
+                appBaseElement = new XElement("environmentVariable", new XAttribute("name", appBaseKeyName));
+                envVariables.AddFirst(appBaseElement);
+            }
+
+            appBaseElement.SetAttributeValue("value", ".");
+        }
+
+        private static XElement GetOrCreateChild(XElement parent, string childName)
+        {
+            var childElement = parent.Element(childName);
+            if (childElement == null)
+            {
+                childElement = new XElement(childName);
+                parent.Add(childElement);
+            }
+            return childElement;
+        }
+
+        private static void SetAttributeValueIfEmpty(XElement element, string attributeName, string value)
+        {
+            element.SetAttributeValue(attributeName, (string)element.Attribute(attributeName) ?? value);
+        }
+    }
+}
---
+++ test/Microsoft.AspNetCore.Server.IISIntegration.Tools.Tests/WebConfigTransformFacts.cs
@@ -0,0 +1,202 @@
+using Xunit;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Microsoft.AspNetCore.Server.IISIntegration.Tools.Tests
+{
+    public class WebConfigTransformFacts
+    {
+        private XDocument WebConfigTemplate => XDocument.Parse(
+@"<configuration>
+  <system.webServer>
+    <handlers>
+      <add name=""httpPlatformHandler"" path=""*"" verb=""*"" modules=""httpPlatformHandler"" resourceType=""Unspecified""/>
+    </handlers>
+    <httpPlatform processPath=""..\test.exe"" stdoutLogEnabled=""false"" stdoutLogFile=""..\logs\stdout.log"" startupTimeLimit=""3600"">
+      <environmentVariables>
+        <environmentVariable name=""ASPNET_APPLICATIONBASE"" value=""."" />
+      </environmentVariables>
+    </httpPlatform>
+  </system.webServer>
+</configuration>");
+
+        [Fact]
+        public void WebConfigTransform_creates_new_config_if_one_does_not_exist()
+        {
+            Assert.True(XNode.DeepEquals(WebConfigTemplate,
+                    WebConfigTransform.Transform(null, "test.exe", configureForAzure: false)));
+        }
+
+        [Fact]
+        public void WebConfigTransform_creates_new_config_if_one_has_unexpected_format()
+        {
+            Assert.True(XNode.DeepEquals(WebConfigTemplate,
+                WebConfigTransform.Transform(XDocument.Parse("<unexpected />"), "test.exe", configureForAzure: false)));
+        }
+
+        [Theory]
+        [InlineData(new object[] { new[] { "system.webServer" } })]
+        [InlineData(new object[] { new[] { "add" } })]
+        [InlineData(new object[] { new[] { "handlers" } })]
+        [InlineData(new object[] { new[] { "httpPlatform" } })]
+        [InlineData(new object[] { new[] { "environmentVariables" } })]
+        [InlineData(new object[] { new[] { "environmentVariable" } })]
+        [InlineData(new object[] { new[] { "handlers", "httpPlatform", "environmentVariables" } })]
+        public void WebConfigTransform_adds_missing_elements(string[] elementNames)
+        {
+            var input = WebConfigTemplate;
+            foreach (var elementName in elementNames)
+            {
+                input.Descendants(elementName).Remove();
+            }
+
+            Assert.True(XNode.DeepEquals(WebConfigTemplate,
+                WebConfigTransform.Transform(input, "test.exe", configureForAzure: false)));
+        }
+
+        [Theory]
+        [InlineData("add", "path", "test")]
+        [InlineData("add", "verb", "test")]
+        [InlineData("add", "modules", "mods")]
+        [InlineData("add", "resourceType", "Either")]
+        [InlineData("httpPlatform", "stdoutLogEnabled", "true")]
+        [InlineData("httpPlatform", "startupTimeLimit", "1200")]
+        [InlineData("httpPlatform", "arguments", "arg1")]
+        [InlineData("httpPlatform", "stdoutLogFile", "logfile.log")]
+        public void WebConfigTransform_wont_override_custom_values(string elementName, string attributeName, string attributeValue)
+        {
+            var input = WebConfigTemplate;
+            input.Descendants(elementName).Single().SetAttributeValue(attributeName, attributeValue);
+
+            var output = WebConfigTransform.Transform(input, "test.exe", configureForAzure: false);
+            Assert.Equal(attributeValue, (string)output.Descendants(elementName).Single().Attribute(attributeName));
+        }
+
+        [Fact]
+        public void WebConfigTransform_overwrites_processPath()
+        {
+            var newProcessPath =
+                (string)WebConfigTransform.Transform(WebConfigTemplate, "app.exe", configureForAzure: false)
+                    .Descendants("httpPlatform").Single().Attribute("processPath");
+
+            Assert.Equal(@"..\app.exe", newProcessPath);
+        }
+
+        [Fact]
+        public void WebConfigTransform_fixes_httpPlatformHandler_casing()
+        {
+            var input = WebConfigTemplate;
+            input.Descendants("add").Single().SetAttributeValue("name", "httpplatformhandler");
+
+            Assert.True(XNode.DeepEquals(WebConfigTemplate,
+                WebConfigTransform.Transform(input, "test.exe", configureForAzure: false)));
+        }
+
+        [Fact]
+        public void WebConfigTransform_does_not_remove_children_of_httpPlatform_element()
+        {
+            var envVarElement =
+                new XElement("environmentVariable", new XAttribute("name", "ENVVAR"), new XAttribute("value", "123"));
+
+            var input = WebConfigTemplate;
+            input.Descendants("environmentVariable").Single().Add(envVarElement);
+
+            Assert.True(XNode.DeepEquals(envVarElement,
+                WebConfigTransform.Transform(input, "app.exe", configureForAzure: false)
+                    .Descendants("environmentVariable").SingleOrDefault(e => (string)e.Attribute("name") == "ENVVAR")));
+        }
+
+        [Fact]
+        public void WebConfigTransform_adds_stdoutLogEnabled_if_attribute_is_missing()
+        {
+            var input = WebConfigTemplate;
+            input.Descendants("httpPlatform").Attributes("stdoutLogEnabled").Remove();
+
+            Assert.Equal(
+                "false",
+                (string)WebConfigTransform.Transform(input, "test.exe", configureForAzure: false)
+                    .Descendants().Attributes("stdoutLogEnabled").Single());
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("false")]
+        [InlineData("true")]
+        public void WebConfigTransform_adds_stdoutLogFile_if_attribute_is_missing(string stdoutLogFile)
+        {
+            var input = WebConfigTemplate;
+
+            var httpPlatformElement = input.Descendants("httpPlatform").Single();
+            httpPlatformElement.Attribute("stdoutLogEnabled").Remove();
+            if (stdoutLogFile != null)
+            {
+                httpPlatformElement.SetAttributeValue("stdoutLogEnabled", stdoutLogFile);
+            }
+
+            Assert.Equal(
+                @"..\logs\stdout.log",
+                (string)WebConfigTransform.Transform(input, "test.exe", configureForAzure: false)
+                    .Descendants().Attributes("stdoutLogFile").Single());
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("true")]
+        [InlineData("false")]
+        public void WebConfigTransform_does_not_change_existing_stdoutLogEnabled(string stdoutLogEnabledValue)
+        {
+            var input = WebConfigTemplate;
+            var httpPlatformElement = input.Descendants("httpPlatform").Single();
+
+            httpPlatformElement.SetAttributeValue("stdoutLogFile", "mylog.txt");
+            httpPlatformElement.Attributes("stdoutLogEnabled").Remove();
+            if (stdoutLogEnabledValue != null)
+            {
+                input.Descendants("httpPlatform").Single().SetAttributeValue("stdoutLogEnabled", stdoutLogEnabledValue);
+            }
+
+            Assert.Equal(
+                "mylog.txt",
+                (string)WebConfigTransform.Transform(input, "test.exe", configureForAzure: false)
+                    .Descendants().Attributes("stdoutLogFile").Single());
+        }
+
+        [Fact]
+        public void WebConfigTransform_correctly_configures_for_Azure()
+        {
+            var input = WebConfigTemplate;
+            input.Descendants("httpPlatform").Attributes().Remove();
+
+            var httPlatformElement = WebConfigTransform.Transform(input, "test.exe", configureForAzure: true)
+                .Descendants("httpPlatform").Single();
+            httPlatformElement.Elements().Remove();
+
+            Assert.True(XNode.DeepEquals(
+                XDocument.Parse(@"<httpPlatform processPath=""%home%\site\test.exe"" stdoutLogEnabled=""false""
+                    stdoutLogFile=""\\?\%home%\LogFiles\stdout.log"" startupTimeLimit=""3600""/>").Root,
+                httPlatformElement));
+        }
+
+        [Fact]
+        public void WebConfigTransform_overrites_value_for_ASPNET_APPLICATIONBASE()
+        {
+            var input = WebConfigTemplate;
+            input.Descendants("environmentVariable").Single().SetAttributeValue("value", "abc");
+
+            Assert.True(XNode.DeepEquals(WebConfigTemplate,
+                WebConfigTransform.Transform(input, "test.exe", configureForAzure: false)));
+        }
+
+        private bool VerifyMissingElementCreated(params string[] elementNames)
+        {
+            var input = WebConfigTemplate;
+            foreach (var elementName in elementNames)
+            {
+                input.Descendants(elementName).Remove();
+            }
+
+            return XNode.DeepEquals(WebConfigTemplate,
+                WebConfigTransform.Transform(input, "test.exe", configureForAzure: false));
+        }
+    }
+}
