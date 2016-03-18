bb3809d3dc34279a948f81ed0d3160cb2819e92c
Doug Bunting <dougbu@microsoft.com>
2016-03-17 22:05:12 -0700

    React to HttpAbstractions change: No features in `.Internal` namespace - see issue aspnet/HttpAbstractions#561 and pull aspnet/HttpAbstractions#589

--- src/Microsoft.AspNetCore.Hosting/Internal/RequestServicesFeature.cs
+++ src/Microsoft.AspNetCore.Hosting/Internal/RequestServicesFeature.cs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using Microsoft.AspNetCore.Http.Features.Internal;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.AspNetCore.Hosting.Internal
--- test/Microsoft.AspNetCore.TestHost.Tests/TestServerTests.cs
+++ test/Microsoft.AspNetCore.TestHost.Tests/TestServerTests.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features.Internal;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.AspNetCore.Testing.xunit;
 using Microsoft.Extensions.DependencyInjection;
