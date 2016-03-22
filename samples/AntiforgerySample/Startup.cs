4d64c1849ddfa7a4ae08bb7d1c758311949504a1
John Luo <jt.luo@mail.utoronto.ca>
2016-03-22 11:55:44 -0700

    Reacting to Hosting changes

--- samples/AntiforgerySample/Startup.cs
+++ samples/AntiforgerySample/Startup.cs
@@ -69,7 +69,7 @@ namespace AntiforgerySample
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
-                .UseDefaultConfiguration(args)
+                .UseDefaultHostingConfiguration(args)
                 .UseServer("Microsoft.AspNetCore.Server.Kestrel")
                 .UseIISPlatformHandlerUrl()
                 .UseStartup<Startup>()
