6a2a3d953275afccd7181f095252c5219303f8fc
SteveSandersonMS <stevesa@microsoft.com>
2016-04-08 11:34:53 +0100

    Fix JSON formatting in Angular2Spa template

--- templates/Angular2Spa/Startup.cs
+++ templates/Angular2Spa/Startup.cs
@@ -18,7 +18,10 @@ namespace WebApplicationBasic
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
