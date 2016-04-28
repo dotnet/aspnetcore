fc705e8bf8fb6c4a93b07033bf3ce3bc4e3ceb09
SteveSandersonMS <stevesa@microsoft.com>
2016-04-28 14:34:47 +0100

    Simplify by removing diff feature. Now just writes each template as an independent copy.

--- templates/yeoman/typings/tsd.d.ts
+++ templates/yeoman/typings/tsd.d.ts
@@ -3,6 +3,5 @@
 /// <reference path="glob/glob.d.ts" />
 /// <reference path="minimatch/minimatch.d.ts" />
 /// <reference path="lodash/lodash.d.ts" />
-/// <reference path="diff/diff.d.ts" />
 /// <reference path="mkdirp/mkdirp.d.ts" />
 /// <reference path="rimraf/rimraf.d.ts" />
