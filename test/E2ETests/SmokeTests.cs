333b4feda5851dcc6fc8336b1ddd0a8b635e48cb
Cesar Blum Silveira <cesars@microsoft.com>
2016-05-23 11:37:39 -0700

    Disable one more test.

--- test/E2ETests/SmokeTests.cs
+++ test/E2ETests/SmokeTests.cs
@@ -88,7 +88,7 @@ namespace E2ETests
         [InlineData(ServerType.Kestrel, RuntimeFlavor.CoreClr, RuntimeArchitecture.x64, ApplicationType.Standalone, "http://localhost:5016/")]
         //[InlineData(ServerType.IISExpress, RuntimeFlavor.Clr, RuntimeArchitecture.x64, ApplicationType.Portable, "http://localhost:5017/")] // https://github.com/aspnet/MusicStore/issues/673
         //[InlineData(ServerType.IISExpress, RuntimeFlavor.CoreClr, RuntimeArchitecture.x64, ApplicationType.Portable, "http://localhost:5018/")] // https://github.com/aspnet/MusicStore/issues/673
-        [InlineData(ServerType.IISExpress, RuntimeFlavor.CoreClr, RuntimeArchitecture.x64, ApplicationType.Standalone, "http://localhost:5019/")]
+        //[InlineData(ServerType.IISExpress, RuntimeFlavor.CoreClr, RuntimeArchitecture.x64, ApplicationType.Standalone, "http://localhost:5019/")]
         public async Task WindowsOS(
             ServerType serverType,
             RuntimeFlavor runtimeFlavor,
