ce8d23d3e5a21c6f2d54d743e5b7ff95a6199d70
Chris R <Tratcher@outlook.com>
2016-05-27 09:49:54 -0700

    Update sample

--- samples/StaticFileSample/Startup.cs
+++ samples/StaticFileSample/Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,6 +30,7 @@ namespace StaticFilesSample
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseKestrel()
                 .UseIISIntegration()
                 .UseStartup<Startup>()
