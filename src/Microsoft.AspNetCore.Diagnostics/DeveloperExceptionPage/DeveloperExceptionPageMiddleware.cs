aspnet/DiagnosticsPages

Use Microsoft.Extensions.StackTrace.Sources

src/Microsoft.AspNetCore.Diagnostics/DeveloperExceptionPage/DeveloperExceptionPageMiddleware.cs
@@ -4,8 +4,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +14,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using StackFrame = Microsoft.AspNetCore.Diagnostics.Views.StackFrame;
+using Microsoft.Extensions.StackTrace.Sources;
 
 namespace Microsoft.AspNetCore.Diagnostics
 {
@@ -27,10 +25,9 @@ namespace Microsoft.AspNetCore.Diagnostics
     {
         private readonly RequestDelegate _next;
         private readonly DeveloperExceptionPageOptions _options;
-        private static readonly bool IsMono = Type.GetType("Mono.Runtime") != null;
         private readonly ILogger _logger;
         private readonly IFileProvider _fileProvider;
-        private readonly DiagnosticSource _diagnosticSource;
+        private readonly System.Diagnostics.DiagnosticSource _diagnosticSource;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="DeveloperExceptionPageMiddleware"/> class
@@ -45,7 +42,7 @@ namespace Microsoft.AspNetCore.Diagnostics
             IOptions<DeveloperExceptionPageOptions> options,
             ILoggerFactory loggerFactory,
             IHostingEnvironment hostingEnvironment,
-            DiagnosticSource diagnosticSource)
+            System.Diagnostics.DiagnosticSource diagnosticSource)
         {
             if (next == null)
             {
@@ -122,7 +119,7 @@ namespace Microsoft.AspNetCore.Diagnostics
 
         private Task DisplayCompilationException(
             HttpContext context,
-                                                 ICompilationException compilationException)
+            ICompilationException compilationException)
         {
             var model = new CompilationErrorPageModel
             {
@@ -131,7 +128,7 @@ namespace Microsoft.AspNetCore.Diagnostics
 
             foreach (var compilationFailure in compilationException.CompilationFailures)
             {
-                var stackFrames = new List<StackFrame>();
+                var stackFrames = new List<StackFrameInfo>();
                 var errorDetails = new ErrorDetails
                 {
                     StackFrames = stackFrames
@@ -142,7 +139,7 @@ namespace Microsoft.AspNetCore.Diagnostics
 
                 foreach (var item in compilationFailure.Messages)
                 {
-                    var frame = new StackFrame
+                    var frame = new StackFrameInfo
                     {
                         File = compilationFailure.SourceFilePath,
                         Line = item.StartLine,
@@ -187,75 +184,40 @@ namespace Microsoft.AspNetCore.Diagnostics
         {
             for (var scan = ex; scan != null; scan = scan.InnerException)
             {
+                var stackTrace = ex.StackTrace;
                 yield return new ErrorDetails
                 {
                     Error = scan,
-                    StackFrames = StackFrames(scan)
+                    StackFrames = StackTraceHelper.GetFrames(ex)
+                        .Select(frame => GetStackFrameInfo(frame.Method, frame.FilePath, frame.LineNumber))
                 };
-            }
-        }
-
-        private IEnumerable<StackFrame> StackFrames(Exception ex)
-        {
-            var stackTrace = ex.StackTrace;
-            if (!string.IsNullOrEmpty(stackTrace))
-            {
-                var heap = new Chunk { Text = stackTrace + Environment.NewLine, End = stackTrace.Length + Environment.NewLine.Length };
-                for (var line = heap.Advance(Environment.NewLine); line.HasValue; line = heap.Advance(Environment.NewLine))
-                {
-                    yield return StackFrame(line);
-                }
-            }
-        }
-
-        private StackFrame StackFrame(Chunk line)
-        {
-            line.Advance("  at ");
-            string function = line.Advance(" in ").ToString();
-
-            //exception message line format differences in .net and mono
-            //On .net : at ConsoleApplication.Program.Main(String[] args) in D:\Program.cs:line 16
-            //On Mono : at ConsoleApplication.Program.Main(String[] args) in d:\Program.cs:16
-            string file = !IsMono ?
-                line.Advance(":line ").ToString() :
-                line.Advance(":").ToString();
-
-            int lineNumber = line.ToInt32();
-
-            if (string.IsNullOrEmpty(file))
-            {
-                return GetStackFrame(
-                    // Handle stack trace lines like
-                    // "--- End of stack trace from previous location where exception from thrown ---"
-                    string.IsNullOrEmpty(function) ? line.ToString() : function,
-                    file: string.Empty,
-                    lineNumber: 0);
-            }
-            else
-            {
-                return GetStackFrame(function, file, lineNumber);
-            }
+            };
         }
 
         // make it internal to enable unit testing
-        internal StackFrame GetStackFrame(string function, string file, int lineNumber)
+        internal StackFrameInfo GetStackFrameInfo(string method, string filePath, int lineNumber)
         {
-            var frame = new StackFrame { Function = function, File = file, Line = lineNumber };
-
-            if (string.IsNullOrEmpty(file))
+            var stackFrame = new StackFrameInfo
             {
-                return frame;
+                Function = method,
+                File = filePath,
+                Line = lineNumber
+            };
+
+            if (string.IsNullOrEmpty(stackFrame.File))
+            {
+                return stackFrame;
             }
 
             IEnumerable<string> lines = null;
-            if (File.Exists(file))
+            if (File.Exists(stackFrame.File))
             {
-                lines = File.ReadLines(file);
+                lines = File.ReadLines(stackFrame.File);
             }
             else
             {
                 // Handle relative paths and embedded files
-                var fileInfo = _fileProvider.GetFileInfo(file);
+                var fileInfo = _fileProvider.GetFileInfo(stackFrame.File);
                 if (fileInfo.Exists)
                 {
                     // ReadLines doesn't accept a stream. Use ReadLines as its more efficient
@@ -273,15 +235,15 @@ namespace Microsoft.AspNetCore.Diagnostics
 
             if (lines != null)
             {
-                ReadFrameContent(frame, lines, lineNumber, lineNumber);
+                ReadFrameContent(stackFrame, lines, stackFrame.Line, stackFrame.Line);
             }
 
-            return frame;
+            return stackFrame;
         }
 
         // make it internal to enable unit testing
         internal void ReadFrameContent(
-            StackFrame frame,
+            StackFrameInfo frame,
             IEnumerable<string> allLines,
             int errorStartLineNumberInFile,
             int errorEndLineNumberInFile)
@@ -317,43 +279,6 @@ namespace Microsoft.AspNetCore.Diagnostics
                 {
                     yield return line;
                 }
-            }
-        }
-
-        internal class Chunk
-        {
-            public string Text { get; set; }
-            public int Start { get; set; }
-            public int End { get; set; }
-
-            public bool HasValue => Text != null;
-
-            public Chunk Advance(string delimiter)
-            {
-                int indexOf = HasValue ? Text.IndexOf(delimiter, Start, End - Start, StringComparison.Ordinal) : -1;
-                if (indexOf < 0)
-                {
-                    return new Chunk();
-                }
-
-                var chunk = new Chunk { Text = Text, Start = Start, End = indexOf };
-                Start = indexOf + delimiter.Length;
-                return chunk;
-            }
-
-            public override string ToString()
-            {
-                return HasValue ? Text.Substring(Start, End - Start) : string.Empty;
-            }
-
-            public int ToInt32()
-            {
-                int value;
-                return HasValue && int.TryParse(
-                    Text.Substring(Start, End - Start),
-                    NumberStyles.Integer,
-                    CultureInfo.InvariantCulture,
-                    out value) ? value : 0;
             }
         }
     }

test/Microsoft.AspNetCore.Diagnostics.Tests/DeveloperExceptionPageMiddlewareTest.cs
@@ -8,7 +8,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Runtime.Versioning;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,14 +15,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.AspNetCore.Testing;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
-using Xunit;
-using StackFrame = Microsoft.AspNetCore.Diagnostics.Views.StackFrame;
 using Moq;
+using Xunit;
 
 namespace Microsoft.AspNetCore.Diagnostics
 {
@@ -53,7 +51,7 @@ namespace Microsoft.AspNetCore.Diagnostics
         {
             // Arrange & Act
             var middleware = GetErrorPageMiddleware(fileProvider: null);
-            var stackFrame = middleware.GetStackFrame("func1", relativePath, lineNumber: 10);
+            var stackFrame = middleware.GetStackFrameInfo("func1", relativePath, lineNumber: 10);
 
             // Assert
             // Lines 4-16 (inclusive) is the code block
@@ -94,7 +92,7 @@ namespace Microsoft.AspNetCore.Diagnostics
             {
                 // Act
                 var middleware = GetErrorPageMiddleware(provider);
-                var stackFrame = middleware.GetStackFrame("func1", absoluteFilePath, lineNumber: 10);
+                var stackFrame = middleware.GetStackFrameInfo("func1", absoluteFilePath, lineNumber: 10);
 
                 // Assert
                 // Lines 4-16 (inclusive) is the code block
@@ -115,7 +113,7 @@ namespace Microsoft.AspNetCore.Diagnostics
             {
                 // Act
                 var middleware = GetErrorPageMiddleware(provider);
-                var stackFrame = middleware.GetStackFrame("func1", relativePath, lineNumber: 10);
+                var stackFrame = middleware.GetStackFrameInfo("func1", relativePath, lineNumber: 10);
 
                 // Assert
                 // Lines 4-16 (inclusive) is the code block
@@ -138,7 +136,7 @@ namespace Microsoft.AspNetCore.Diagnostics
 
             // Act
             var middleware = GetErrorPageMiddleware(provider);
-            var stackFrame = middleware.GetStackFrame("func1", relativePath, lineNumber: 10);
+            var stackFrame = middleware.GetStackFrameInfo("func1", relativePath, lineNumber: 10);
 
             // Assert
             // Lines 4-16 (inclusive) is the code block
@@ -273,7 +271,7 @@ namespace Microsoft.AspNetCore.Diagnostics
         {
             // Arrange
             var middleware = GetErrorPageMiddleware();
-            var stackFrame = new StackFrame();
+            var stackFrame = new Views.StackFrameInfo();
 
             // Act
             middleware.ReadFrameContent(

dotnet/aspnetcore

src/Microsoft.AspNetCore.Diagnostics/DeveloperExceptionPage/DeveloperExceptionPageMiddleware.cs
@@ -4,8 +4,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +14,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using StackFrame = Microsoft.AspNetCore.Diagnostics.Views.StackFrame;
+using Microsoft.Extensions.StackTrace.Sources;
 
 namespace Microsoft.AspNetCore.Diagnostics
 {
@@ -27,10 +25,9 @@ namespace Microsoft.AspNetCore.Diagnostics
     {
         private readonly RequestDelegate _next;
         private readonly DeveloperExceptionPageOptions _options;
-        private static readonly bool IsMono = Type.GetType("Mono.Runtime") != null;
         private readonly ILogger _logger;
         private readonly IFileProvider _fileProvider;
-        private readonly DiagnosticSource _diagnosticSource;
+        private readonly System.Diagnostics.DiagnosticSource _diagnosticSource;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="DeveloperExceptionPageMiddleware"/> class
@@ -45,7 +42,7 @@ namespace Microsoft.AspNetCore.Diagnostics
             IOptions<DeveloperExceptionPageOptions> options,
             ILoggerFactory loggerFactory,
             IHostingEnvironment hostingEnvironment,
-            DiagnosticSource diagnosticSource)
+            System.Diagnostics.DiagnosticSource diagnosticSource)
         {
             if (next == null)
             {
@@ -122,7 +119,7 @@ namespace Microsoft.AspNetCore.Diagnostics
 
         private Task DisplayCompilationException(
             HttpContext context,
-                                                 ICompilationException compilationException)
+            ICompilationException compilationException)
         {
             var model = new CompilationErrorPageModel
             {
@@ -187,75 +184,40 @@ namespace Microsoft.AspNetCore.Diagnostics
         {
             for (var scan = ex; scan != null; scan = scan.InnerException)
             {
+                var stackTrace = ex.StackTrace;
                 yield return new ErrorDetails
                 {
                     Error = scan,
-                    StackFrames = StackFrames(scan)
+                    StackFrames = StackTraceHelper.GetFrames(ex)
+                        .Select(frame => GetStackFrame(frame.Method, frame.FilePath, frame.LineNumber))
                 };
-            }
-        }
-
-        private IEnumerable<StackFrame> StackFrames(Exception ex)
-        {
-            var stackTrace = ex.StackTrace;
-            if (!string.IsNullOrEmpty(stackTrace))
-            {
-                var heap = new Chunk { Text = stackTrace + Environment.NewLine, End = stackTrace.Length + Environment.NewLine.Length };
-                for (var line = heap.Advance(Environment.NewLine); line.HasValue; line = heap.Advance(Environment.NewLine))
-                {
-                    yield return StackFrame(line);
-                }
-            }
-        }
-
-        private StackFrame StackFrame(Chunk line)
-        {
-            line.Advance("  at ");
-            string function = line.Advance(" in ").ToString();
-
-            //exception message line format differences in .net and mono
-            //On .net : at ConsoleApplication.Program.Main(String[] args) in D:\Program.cs:line 16
-            //On Mono : at ConsoleApplication.Program.Main(String[] args) in d:\Program.cs:16
-            string file = !IsMono ?
-                line.Advance(":line ").ToString() :
-                line.Advance(":").ToString();
-
-            int lineNumber = line.ToInt32();
-
-            if (string.IsNullOrEmpty(file))
-            {
-                return GetStackFrame(
-                    // Handle stack trace lines like
-                    // "--- End of stack trace from previous location where exception from thrown ---"
-                    string.IsNullOrEmpty(function) ? line.ToString() : function,
-                    file: string.Empty,
-                    lineNumber: 0);
-            }
-            else
-            {
-                return GetStackFrame(function, file, lineNumber);
-            }
+            };
         }
 
         // make it internal to enable unit testing
-        internal StackFrame GetStackFrame(string function, string file, int lineNumber)
+        internal StackFrame GetStackFrame(string method, string filePath, int lineNumber)
         {
-            var frame = new StackFrame { Function = function, File = file, Line = lineNumber };
-
-            if (string.IsNullOrEmpty(file))
+            var stackFrame = new StackFrame
             {
-                return frame;
+                Function = method,
+                File = filePath,
+                Line = lineNumber
+            };
+
+            if (string.IsNullOrEmpty(stackFrame.File))
+            {
+                return stackFrame;
             }
 
             IEnumerable<string> lines = null;
-            if (File.Exists(file))
+            if (File.Exists(stackFrame.File))
             {
-                lines = File.ReadLines(file);
+                lines = File.ReadLines(stackFrame.File);
             }
             else
             {
                 // Handle relative paths and embedded files
-                var fileInfo = _fileProvider.GetFileInfo(file);
+                var fileInfo = _fileProvider.GetFileInfo(stackFrame.File);
                 if (fileInfo.Exists)
                 {
                     // ReadLines doesn't accept a stream. Use ReadLines as its more efficient
@@ -273,10 +235,10 @@ namespace Microsoft.AspNetCore.Diagnostics
 
             if (lines != null)
             {
-                ReadFrameContent(frame, lines, lineNumber, lineNumber);
+                ReadFrameContent(stackFrame, lines, stackFrame.Line, stackFrame.Line);
             }
 
-            return frame;
+            return stackFrame;
         }
 
         // make it internal to enable unit testing
@@ -317,43 +279,6 @@ namespace Microsoft.AspNetCore.Diagnostics
                 {
                     yield return line;
                 }
-            }
-        }
-
-        internal class Chunk
-        {
-            public string Text { get; set; }
-            public int Start { get; set; }
-            public int End { get; set; }
-
-            public bool HasValue => Text != null;
-
-            public Chunk Advance(string delimiter)
-            {
-                int indexOf = HasValue ? Text.IndexOf(delimiter, Start, End - Start, StringComparison.Ordinal) : -1;
-                if (indexOf < 0)
-                {
-                    return new Chunk();
-                }
-
-                var chunk = new Chunk { Text = Text, Start = Start, End = indexOf };
-                Start = indexOf + delimiter.Length;
-                return chunk;
-            }
-
-            public override string ToString()
-            {
-                return HasValue ? Text.Substring(Start, End - Start) : string.Empty;
-            }
-
-            public int ToInt32()
-            {
-                int value;
-                return HasValue && int.TryParse(
-                    Text.Substring(Start, End - Start),
-                    NumberStyles.Integer,
-                    CultureInfo.InvariantCulture,
-                    out value) ? value : 0;
             }
         }
     }

test/Microsoft.AspNetCore.Diagnostics.Tests/DeveloperExceptionPageMiddlewareTest.cs
@@ -8,7 +8,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Runtime.Versioning;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,14 +15,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.AspNetCore.Testing;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
-using Xunit;
-using StackFrame = Microsoft.AspNetCore.Diagnostics.Views.StackFrame;
 using Moq;
+using Xunit;
 
 namespace Microsoft.AspNetCore.Diagnostics
 {
@@ -273,7 +271,7 @@ namespace Microsoft.AspNetCore.Diagnostics
         {
             // Arrange
             var middleware = GetErrorPageMiddleware();
-            var stackFrame = new StackFrame();
+            var stackFrame = new Views.StackFrame();
 
             // Act
             middleware.ReadFrameContent(