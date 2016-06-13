2a1e6649b0d3d9bbf9fb8e73b5e7b27ee457dfb7
Alex Forbes-Reed <0xdeafcafe@users.noreply.github.com>
2016-06-13 12:04:09 +0100

    Added RFC 4918 support

--- src/Microsoft.AspNetCore.Http.Abstractions/StatusCodes.cs
+++ src/Microsoft.AspNetCore.Http.Abstractions/StatusCodes.cs
@@ -12,6 +12,7 @@ namespace Microsoft.AspNetCore.Http
         public const int Status204NoContent = 204;
         public const int Status205ResetContent = 205;
         public const int Status206PartialContent = 206;
+        public const int Status207MultiStatus = 207;
 
         public const int Status300MultipleChoices = 300;
         public const int Status301MovedPermanently = 301;
@@ -42,6 +43,9 @@ namespace Microsoft.AspNetCore.Http
         public const int Status417ExpectationFailed = 417;
         public const int Status418ImATeapot = 418;
         public const int Status419AuthenticationTimeout = 419;
+        public const int Status422UnprocessableEntity = 422;
+        public const int Status423Locked = 423;
+        public const int Status424FailedDependency = 424;
 
         public const int Status500InternalServerError = 500;
         public const int Status501NotImplemented = 501;
@@ -50,5 +54,6 @@ namespace Microsoft.AspNetCore.Http
         public const int Status504GatewayTimeout = 504;
         public const int Status505HttpVersionNotsupported = 505;
         public const int Status506VariantAlsoNegotiates = 506;
+        public const int Status507InsufficientStorage = 507;
     }
 }
