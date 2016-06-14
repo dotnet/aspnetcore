288810c95ce6f6d2589f4cbbd016e12d48a8e4c3
Pranav K <prkrishn@hotmail.com>
2016-06-14 16:21:56 -0700

    Updating to release.

--- build.ps1
+++ build.ps1
@@ -33,7 +33,7 @@ cd $PSScriptRoot
 $repoFolder = $PSScriptRoot
 $env:REPO_FOLDER = $repoFolder
 
-$koreBuildZip="https://github.com/aspnet/KoreBuild/archive/dev.zip"
+$koreBuildZip="https://github.com/aspnet/KoreBuild/archive/release.zip"
 if ($env:KOREBUILD_ZIP)
 {
     $koreBuildZip=$env:KOREBUILD_ZIP
