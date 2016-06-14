a881ca5b96e1c2d8253bb1c4cd18c42e8b0a1d8e
SteveSandersonMS <stevesa@microsoft.com>
2016-06-14 10:43:03 +0100

    Correctly set NODE_PATH when starting OutOfProcessNodeInstance

--- src/Microsoft.AspNetCore.NodeServices/HostingModels/OutOfProcessNodeInstance.cs
+++ src/Microsoft.AspNetCore.NodeServices/HostingModels/OutOfProcessNodeInstance.cs
@@ -91,9 +91,9 @@ namespace Microsoft.AspNetCore.NodeServices
 
                     var nodePathValue = existingNodePath + Path.Combine(_projectPath, "node_modules");
 #if NET451
-                    startInfo.EnvironmentVariables.Add("NODE_PATH", nodePathValue);
+                    startInfo.EnvironmentVariables["NODE_PATH"] = nodePathValue;
 #else
-                    startInfo.Environment.Add("NODE_PATH", nodePathValue);
+                    startInfo.Environment["NODE_PATH"] = nodePathValue;
 #endif
 
                     _nodeProcess = Process.Start(startInfo);
