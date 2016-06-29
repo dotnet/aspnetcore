92c658216f76f8700ec2fef5e3012c747d0bb36f
Pranav K <prkrishn@hotmail.com>
2016-06-29 16:43:38 -0700

    Updating to RTM builds of xunit

--- test/Microsoft.DotNet.Watcher.Tools.FunctionalTests/AppWithDepsTests.cs
+++ test/Microsoft.DotNet.Watcher.Tools.FunctionalTests/AppWithDepsTests.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Diagnostics;
 using System.IO;
 using Xunit;
 
@@ -31,7 +30,6 @@ namespace Microsoft.DotNet.Watcher.Tools.FunctionalTests
                 }
             }
         }
-
 
         private class AppWithDepsScenario : DotNetWatchScenario
         {
