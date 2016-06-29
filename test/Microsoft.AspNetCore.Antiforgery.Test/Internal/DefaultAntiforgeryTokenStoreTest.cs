d529378a4601d4f5a2da7399d953dd904a619850
Pranav K <prkrishn@hotmail.com>
2016-06-29 12:32:06 -0700

    Updating to RTM builds of dotnet-test-xunit and Moq

--- test/Microsoft.AspNetCore.Antiforgery.Test/Internal/DefaultAntiforgeryTokenStoreTest.cs
+++ test/Microsoft.AspNetCore.Antiforgery.Test/Internal/DefaultAntiforgeryTokenStoreTest.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Internal;
