c1a1bdf373da625cfff56c54f2b276bc257962f4
SteveSandersonMS <stevesa@microsoft.com>
2016-07-07 15:50:37 +0100

    Update React template homepage as per #158

--- templates/ReactReduxSpa/ClientApp/components/Home.tsx
+++ templates/ReactReduxSpa/ClientApp/components/Home.tsx
@@ -15,7 +15,7 @@ export default class Home extends React.Component<any, void> {
             <ul>
                 <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
                 <li><strong>Webpack dev middleware</strong>. In development mode, there's no need to run the <code>webpack</code> build tool. Your client-side resources are dynamically built on demand. Updates are available as soon as you modify any file.</li>
-                <li><strong>Hot module replacement</strong>. In development mode, you don't even need to reload the page after making most changes. Within seconds of saving changes to files, rebuilt CSS and React components will be injected directly into your running application, preserving its live state.</li>
+                <li><strong>Hot module replacement</strong>. In development mode, you don't even need to reload the page after making most changes. Within seconds of saving changes to files, rebuilt React components will be injected directly into your running application, preserving its live state.</li>
                 <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and the <code>webpack</code> build tool produces minified static CSS and JavaScript files.</li>
                 <li><strong>Server-side prerendering</strong>. To optimize startup time, your React application is first rendered on the server. The initial HTML and state is then transferred to the browser, where client-side code picks up where the server left off.</li>
             </ul>
