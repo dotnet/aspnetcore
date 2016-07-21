96228711f26cd356bdab816fadfa235383b43b5f
Mark Pieszak <mpieszak84@gmail.com>
2016-07-21 11:41:43 -0400

    chore(package): Update to rc4, beta router & universal 104.5
    104.5 includes some bug fixes.
    Router beta update required pathMatch on home.
    
    Tested w/ JS on/off everything passes & works.

--- templates/Angular2Spa/ClientApp/routes.ts
+++ templates/Angular2Spa/ClientApp/routes.ts
@@ -4,7 +4,7 @@ import { FetchData } from './components/fetch-data/fetch-data';
 import { Counter } from './components/counter/counter';
 
 export const routes: RouterConfig = [
-    { path: '', redirectTo: 'home' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: Home },
     { path: 'counter', component: Counter },
     { path: 'fetch-data', component: FetchData },
