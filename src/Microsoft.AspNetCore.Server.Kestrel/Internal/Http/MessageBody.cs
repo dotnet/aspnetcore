aspnet/KestrelHttpServer

Use HttpVersionType enum rather than string compare

src/Microsoft.AspNetCore.Server.Kestrel/Internal/Http/MessageBody.cs
@@ -97,13 +97,13 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Internal.Http
         public abstract ValueTask<int> ReadAsyncImplementation(ArraySegment<byte> buffer, CancellationToken cancellationToken);
 
         public static MessageBody For(
-            string httpVersion,
+            HttpVersionType httpVersion,
             FrameRequestHeaders headers,
             Frame context)
         {
             // see also http://tools.ietf.org/html/rfc2616#section-4.4
 
-            var keepAlive = !ReferenceEquals(httpVersion, KnownStrings.Http10Version);
+            var keepAlive = httpVersion != HttpVersionType.Http10;
 
             var connection = headers.HeaderConnection.ToString();
             if (connection.Length > 0)

test/Microsoft.AspNetCore.Server.KestrelTests/FrameTests.cs
@@ -502,7 +502,7 @@ namespace Microsoft.AspNetCore.Server.KestrelTests
             var frame = new Frame<object>(application: null, context: connectionContext);
             frame.InitializeHeaders();
 
-            var messageBody = MessageBody.For(KnownStrings.Http11Version, (FrameRequestHeaders)frame.RequestHeaders, frame);
+            var messageBody = MessageBody.For(HttpVersionType.Http11, (FrameRequestHeaders)frame.RequestHeaders, frame);
             frame.InitializeStreams(messageBody);
 
             var originalRequestBody = frame.RequestBody;

test/Microsoft.AspNetCore.Server.KestrelTests/MessageBodyTests.cs
@@ -19,7 +19,7 @@ namespace Microsoft.AspNetCore.Server.KestrelTests
         {
             using (var input = new TestInput())
             {
-                var body = MessageBody.For(KnownStrings.Http10Version, new FrameRequestHeaders(), input.FrameContext);
+                var body = MessageBody.For(HttpVersionType.Http10, new FrameRequestHeaders(), input.FrameContext);
                 var stream = new FrameRequestStream();
                 stream.StartAcceptingReads(body);
 
@@ -40,7 +40,7 @@ namespace Microsoft.AspNetCore.Server.KestrelTests
         {
             using (var input = new TestInput())
             {
-                var body = MessageBody.For(KnownStrings.Http10Version, new FrameRequestHeaders(), input.FrameContext);
+                var body = MessageBody.For(HttpVersionType.Http10, new FrameRequestHeaders(), input.FrameContext);
                 var stream = new FrameRequestStream();
                 stream.StartAcceptingReads(body);
 
@@ -61,7 +61,7 @@ namespace Microsoft.AspNetCore.Server.KestrelTests
         {
             using (var input = new TestInput())
             {
-                var body = MessageBody.For(KnownStrings.Http10Version, new FrameRequestHeaders(), input.FrameContext);
+                var body = MessageBody.For(HttpVersionType.Http10, new FrameRequestHeaders(), input.FrameContext);
                 var stream = new FrameRequestStream();
                 stream.StartAcceptingReads(body);
 

dotnet/aspnetcore

src/Microsoft.AspNetCore.Server.Kestrel/Internal/Http/MessageBody.cs
@@ -97,13 +97,12 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Internal.Http
         public abstract ValueTask<int> ReadAsyncImplementation(ArraySegment<byte> buffer, CancellationToken cancellationToken);
 
         public static MessageBody For(
-            string httpVersion,
+            HttpVersion httpVersion,
             FrameRequestHeaders headers,
             Frame context)
         {
             // see also http://tools.ietf.org/html/rfc2616#section-4.4
-
-            var keepAlive = httpVersion != "HTTP/1.0";
+            var keepAlive = httpVersion != HttpVersion.Http10;
 
             var connection = headers.HeaderConnection.ToString();
             if (connection.Length > 0)

test/Microsoft.AspNetCore.Server.KestrelTests/FrameTests.cs
@@ -502,7 +502,7 @@ namespace Microsoft.AspNetCore.Server.KestrelTests
             var frame = new Frame<object>(application: null, context: connectionContext);
             frame.InitializeHeaders();
 
-            var messageBody = MessageBody.For("HTTP/1.1", (FrameRequestHeaders)frame.RequestHeaders, frame);
+            var messageBody = MessageBody.For(HttpVersion.Http11, (FrameRequestHeaders)frame.RequestHeaders, frame);
             frame.InitializeStreams(messageBody);
 
             var originalRequestBody = frame.RequestBody;

test/Microsoft.AspNetCore.Server.KestrelTests/MessageBodyTests.cs
@@ -19,7 +19,7 @@ namespace Microsoft.AspNetCore.Server.KestrelTests
         {
             using (var input = new TestInput())
             {
-                var body = MessageBody.For("HTTP/1.0", new FrameRequestHeaders(), input.FrameContext);
+                var body = MessageBody.For(HttpVersion.Http10, new FrameRequestHeaders(), input.FrameContext);
                 var stream = new FrameRequestStream();
                 stream.StartAcceptingReads(body);
 
@@ -40,7 +40,7 @@ namespace Microsoft.AspNetCore.Server.KestrelTests
         {
             using (var input = new TestInput())
             {
-                var body = MessageBody.For("HTTP/1.0", new FrameRequestHeaders(), input.FrameContext);
+                var body = MessageBody.For(HttpVersion.Http10, new FrameRequestHeaders(), input.FrameContext);
                 var stream = new FrameRequestStream();
                 stream.StartAcceptingReads(body);
 
@@ -61,7 +61,7 @@ namespace Microsoft.AspNetCore.Server.KestrelTests
         {
             using (var input = new TestInput())
             {
-                var body = MessageBody.For("HTTP/1.0", new FrameRequestHeaders(), input.FrameContext);
+                var body = MessageBody.For(HttpVersion.Http10, new FrameRequestHeaders(), input.FrameContext);
                 var stream = new FrameRequestStream();
                 stream.StartAcceptingReads(body);
 