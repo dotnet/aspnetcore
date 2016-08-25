e927dd6924cbbff48a5db149e5d4aab905c67785
BrennanConroy <brecon@microsoft.com>
2016-08-25 11:45:19 -0700

    More old code

--- src/Microsoft.AspNetCore.Server.IntegrationTesting/Deployers/ApplicationDeployer.cs
+++ src/Microsoft.AspNetCore.Server.IntegrationTesting/Deployers/ApplicationDeployer.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Threading;
 using Microsoft.Extensions.Internal;
 using Microsoft.Extensions.Logging;
