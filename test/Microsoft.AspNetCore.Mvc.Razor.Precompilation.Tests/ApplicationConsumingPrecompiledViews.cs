021ceb71be2a7e2e121dcb29d8bb4cdcbdff2fd9
Pranav K <prkrishn@hotmail.com>
2016-08-26 09:58:33 -0700

    Don't run desktop tests on xplat

--- test/Microsoft.AspNetCore.Mvc.Razor.Precompilation.Tests/ApplicationConsumingPrecompiledViews.cs
+++ test/Microsoft.AspNetCore.Mvc.Razor.Precompilation.Tests/ApplicationConsumingPrecompiledViews.cs
@@ -2,7 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Server.IntegrationTesting;
@@ -21,9 +23,16 @@ namespace Microsoft.AspNetCore.Mvc.Razor.Precompilation
 
         public ApplicationTestFixture Fixture { get; }
 
+        public static IEnumerable<object[]> SupportedFlavorsTheoryData
+        {
+            get
+            {
+                return RuntimeFlavors.SupportedFlavors.Select(f => new object[] { f });
+            }
+        }
+
         [Theory]
-        [InlineData(RuntimeFlavor.Clr)]
-        [InlineData(RuntimeFlavor.CoreClr)]
+        [MemberData(nameof(SupportedFlavorsTheoryData))]
         public async Task ConsumingClassLibrariesWithPrecompiledViewsWork(RuntimeFlavor flavor)
         {
             // Arrange
