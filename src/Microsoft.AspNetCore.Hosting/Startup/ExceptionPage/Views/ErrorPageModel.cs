9c44d493ca8878dcce543b1fc8033242337201ea
Kiran Challa <kichalla@microsoft.com>
2016-08-29 12:40:13 -0700

    Revert "Revert "Remove existing implementation of StartupExceptionPage and use the one in Common""
    This reverts commit 3f6b558cf34aa2f57d0e8e6749f4a2c1a4c68743.

---
+++ src/Microsoft.AspNetCore.Hosting/Startup/ExceptionPage/Views/ErrorPageModel.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.Extensions.StackTrace.Sources;
+
+namespace Microsoft.AspNetCore.Hosting.Views
+{
+    /// <summary>
+    /// Holds data to be displayed on the error page.
+    /// </summary>
+    internal class ErrorPageModel
+    {
+        /// <summary>
+        /// Detailed information about each exception in the stack.
+        /// </summary>
+        public IEnumerable<ExceptionDetails> ErrorDetails { get; set; }
+
+        public string RuntimeDisplayName { get; set; }
+
+        public string RuntimeArchitecture { get; set; }
+
+        public string ClrVersion { get; set; }
+
+        public string CurrentAssemblyVesion { get; set; }
+
+        public string OperatingSystemDescription { get; set; }
+    }
+}
