alipasakaratas/ResponseCaching

API review renames and updates continued
- Consolidate base key to be singular

src/Microsoft.AspNetCore.ResponseCaching/Extensions/ResponseCacheServiceCollectionExtensions.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
-    public static class ResponseCachingServiceCollectionExtensions
+    public static class ResponseCacheServiceCollectionExtensions
     {
         public static IServiceCollection AddMemoryResponseCache(this IServiceCollection services)
         {
@@ -18,8 +18,8 @@ namespace Microsoft.Extensions.DependencyInjection
             }
 
             services.AddMemoryCache();
-            services.AddResponseCachingServices();
-            services.TryAdd(ServiceDescriptor.Singleton<IResponseCache, MemoryResponseCache>());
+            services.AddResponseCacheServices();
+            services.TryAdd(ServiceDescriptor.Singleton<IResponseCacheStore, MemoryResponseCache>());
 
             return services;
         }
@@ -32,16 +32,16 @@ namespace Microsoft.Extensions.DependencyInjection
             }
 
             services.AddDistributedMemoryCache();
-            services.AddResponseCachingServices();
-            services.TryAdd(ServiceDescriptor.Singleton<IResponseCache, DistributedResponseCache>());
+            services.AddResponseCacheServices();
+            services.TryAdd(ServiceDescriptor.Singleton<IResponseCacheStore, DistributedResponseCache>());
 
             return services;
         }
 
-        private static IServiceCollection AddResponseCachingServices(this IServiceCollection services)
+        private static IServiceCollection AddResponseCacheServices(this IServiceCollection services)
         {
-            services.TryAdd(ServiceDescriptor.Singleton<ICacheKeyProvider, CacheKeyProvider>());
-            services.TryAdd(ServiceDescriptor.Singleton<ICacheabilityValidator, CacheabilityValidator>());
+            services.TryAdd(ServiceDescriptor.Singleton<IResponseCacheKeyProvider, ResponseCacheKeyProvider>());
+            services.TryAdd(ServiceDescriptor.Singleton<IResponseCachePolicyProvider, ResponseCachePolicyProvider>());
 
             return services;
         }

src/Microsoft.AspNetCore.ResponseCaching/ResponseCacheFeature.cs
@@ -1,16 +1,13 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
 
 namespace Microsoft.AspNetCore.ResponseCaching
 {
     // TODO: Temporary interface for endpoints to specify options for response caching
-    public static class ResponseCachingHttpContextExtensions
+    public class ResponseCacheFeature
     {
-        public static ResponseCachingFeature GetResponseCachingFeature(this HttpContext httpContext)
-        {
-            return httpContext.Features.Get<ResponseCachingFeature>();
-        }
+        public StringValues VaryParams { get; set; }
     }
 }

dotnet/aspnetcore

src/Microsoft.AspNetCore.ResponseCaching/Extensions/ResponseCacheServiceCollectionExtensions.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
-    public static class ResponseCachingServiceCollectionExtensions
+    public static class ResponseCacheServiceCollectionExtensions
     {
         public static IServiceCollection AddMemoryResponseCache(this IServiceCollection services)
         {
@@ -18,8 +18,8 @@ namespace Microsoft.Extensions.DependencyInjection
             }
 
             services.AddMemoryCache();
-            services.AddResponseCachingServices();
-            services.TryAdd(ServiceDescriptor.Singleton<IResponseCache, MemoryResponseCache>());
+            services.AddResponseCacheServices();
+            services.TryAdd(ServiceDescriptor.Singleton<IResponseCacheStore, MemoryResponseCacheStore>());
 
             return services;
         }
@@ -32,16 +32,16 @@ namespace Microsoft.Extensions.DependencyInjection
             }
 
             services.AddDistributedMemoryCache();
-            services.AddResponseCachingServices();
-            services.TryAdd(ServiceDescriptor.Singleton<IResponseCache, DistributedResponseCache>());
+            services.AddResponseCacheServices();
+            services.TryAdd(ServiceDescriptor.Singleton<IResponseCacheStore, DistributedResponseCacheStore>());
 
             return services;
         }
 
-        private static IServiceCollection AddResponseCachingServices(this IServiceCollection services)
+        private static IServiceCollection AddResponseCacheServices(this IServiceCollection services)
         {
-            services.TryAdd(ServiceDescriptor.Singleton<ICacheKeyProvider, CacheKeyProvider>());
-            services.TryAdd(ServiceDescriptor.Singleton<ICacheabilityValidator, CacheabilityValidator>());
+            services.TryAdd(ServiceDescriptor.Singleton<IResponseCacheKeyProvider, ResponseCacheKeyProvider>());
+            services.TryAdd(ServiceDescriptor.Singleton<IResponseCachePolicyProvider, ResponseCachePolicyProvider>());
 
             return services;
         }

src/Microsoft.AspNetCore.ResponseCaching/ResponseCacheFeature.cs
@@ -1,16 +1,13 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
 
 namespace Microsoft.AspNetCore.ResponseCaching
 {
     // TODO: Temporary interface for endpoints to specify options for response caching
-    public static class ResponseCachingHttpContextExtensions
+    public class ResponseCacheFeature
     {
-        public static ResponseCachingFeature GetResponseCachingFeature(this HttpContext httpContext)
-        {
-            return httpContext.Features.Get<ResponseCachingFeature>();
-        }
+        public StringValues VaryByParams { get; set; }
     }
 }