1e08548aa0f9654ac3698962116282b268ae9b98
SteveSandersonMS <stevesa@microsoft.com>
2016-09-19 10:33:02 +0100

    Remove now-redundant 'typings' dir and custom-typings.d.ts

--- templates/Angular2Spa/typings/custom-typings.d.ts
+++
@@ -1,7 +0,0 @@
-// This file is a workaround for angular2-universal-preview version 0.84.2 relying on the declaration of
-// Node's 'url' module. Ideally it would not declare dependencies on Node APIs except where it also supplies
-// the definitions itself.
-
-declare module 'url' {
-    export interface Url {}
-}
