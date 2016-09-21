3e62285de5bf16269faea65d621b4b6d8d0f711f
Pranav K <prkrishn@hotmail.com>
2016-09-20 17:30:41 -0700

    Making build use Sake

--- build.cmd
+++ build.cmd
@@ -1,1 +1,2 @@
-msbuild "%~dp0\Build\build.msbuild" /v:minimal /maxcpucount /nodeReuse:false %*
+@ECHO OFF
+PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0build.ps1' %*; exit $LASTEXITCODE"
