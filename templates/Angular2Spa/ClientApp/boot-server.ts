5214a553a7d98bc532b956af7ad14b8905302878
SteveSandersonMS <stevesa@microsoft.com>
2016-09-21 09:29:13 +0100

    In Angular2Spa, server-side rendering now reports all errors

--- templates/Angular2Spa/ClientApp/boot-server.ts
+++ templates/Angular2Spa/ClientApp/boot-server.ts
@@ -8,21 +8,27 @@ enableProdMode();
 const platform = platformNodeDynamic();
 
 export default function (params: any) : Promise<{ html: string, globals?: any }> {
-    const requestZone = Zone.current.fork({
-        name: 'angular-universal request',
-        properties: {
-            baseUrl: '/',
-            requestUrl: params.url,
-            originUrl: params.origin,
-            preboot: false,
-            // TODO: Render just the <app> component instead of wrapping it inside an extra HTML document
-            // Waiting on https://github.com/angular/universal/issues/347
-            document: '<!DOCTYPE html><html><head></head><body><app></app></body></html>'
-        }
-    });
-
-    return requestZone.run<Promise<string>>(() => platform.serializeModule(AppModule))
-        .then(html => {
-            return { html: html };
+    return new Promise((resolve, reject) => {
+        const requestZone = Zone.current.fork({
+            name: 'angular-universal request',
+            properties: {
+                baseUrl: '/',
+                requestUrl: params.url,
+                originUrl: params.origin,
+                preboot: false,
+                // TODO: Render just the <app> component instead of wrapping it inside an extra HTML document
+                // Waiting on https://github.com/angular/universal/issues/347
+                document: '<!DOCTYPE html><html><head></head><body><app></app></body></html>'
+            },
+            onHandleError: (parentZone, currentZone, targetZone, error) => {
+                // If any error occurs while rendering the module, reject the whole operation
+                reject(error);
+                return true;
+            }
         });
+
+        return requestZone.run<Promise<string>>(() => platform.serializeModule(AppModule)).then(html => {
+            resolve({ html: html });
+        }, reject);
+    });
 }
