554eb73223dc87d71f6ccbe8b9e67ef349f96c8c
Pavel Krymets <pavel@krymets.com>
2016-09-30 14:48:00 -0700

    Add Microsoft.AspNetCore.Hosting.Azure.AppServices package

---
+++ src/Microsoft.AspNetCore.AzureAppServicesIntegration/AppServicesWebHostBuilderExtensions.cs
@@ -0,0 +1,29 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.AspNetCore.Hosting
+{
+    public static class AppServicesWebHostBuilderExtensions
+    {
+        /// <summary>
+        /// Configures application to use Azure AppServices integration.
+        /// </summary>
+        /// <param name="hostBuilder"></param>
+        /// <returns></returns>
+        public static IWebHostBuilder UseAzureAppServices(this IWebHostBuilder hostBuilder)
+        {
+            if (hostBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(hostBuilder));
+            }
+
+            hostBuilder.UseIISIntegration();
+            hostBuilder.ConfigureLogging(loggerFactory => loggerFactory.AddAzureWebAppDiagnostics());
+
+            return hostBuilder;
+        }
+    }
+}
