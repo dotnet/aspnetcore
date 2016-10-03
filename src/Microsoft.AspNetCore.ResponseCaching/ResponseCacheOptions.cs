4e10d191756c505d48a56bcaa499954c17f7f0f1
BrennanConroy <brecon@microsoft.com>
2016-10-03 08:25:31 -0700

    Update default Max Body Size

--- src/Microsoft.AspNetCore.ResponseCaching/ResponseCacheOptions.cs
+++ src/Microsoft.AspNetCore.ResponseCaching/ResponseCacheOptions.cs
@@ -9,9 +9,9 @@ namespace Microsoft.AspNetCore.Builder
     public class ResponseCacheOptions
     {
         /// <summary>
-        /// The largest cacheable size for the response body in bytes. The default is set to 1 MB.
+        /// The largest cacheable size for the response body in bytes. The default is set to 64 MB.
         /// </summary>
-        public long MaximumBodySize { get; set; } = 1024 * 1024;
+        public long MaximumBodySize { get; set; } = 64 * 1024 * 1024;
 
         /// <summary>
         /// <c>true</c> if request paths are case-sensitive; otherwise <c>false</c>. The default is to treat paths as case-insensitive.
