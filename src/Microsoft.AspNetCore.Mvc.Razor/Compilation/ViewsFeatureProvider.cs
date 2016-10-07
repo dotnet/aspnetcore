a623b4edd188f5d568001edcc53b370aab385695
Pranav K <prkrishn@hotmail.com>
2016-10-07 10:36:39 -0700

    Use Assembly's containing directory to locate precompiled views assembly

--- src/Microsoft.AspNetCore.Mvc.Razor/Compilation/ViewsFeatureProvider.cs
+++ src/Microsoft.AspNetCore.Mvc.Razor/Compilation/ViewsFeatureProvider.cs
@@ -64,7 +64,7 @@ namespace Microsoft.AspNetCore.Mvc.Razor.Compilation
                     + PrecompiledViewsAssemblySuffix
                     + ".dll";
                 var precompiledAssemblyFilePath = Path.Combine(
-                    assemblyPart.Assembly.Location,
+                    Path.GetDirectoryName(assemblyPart.Assembly.Location),
                     precompiledAssemblyFileName);
 
                 if (File.Exists(precompiledAssemblyFilePath))
