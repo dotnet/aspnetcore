d5b0126f618826d9227654ce6fdd0dd255a723ea
BrennanConroy <brecon@microsoft.com>
2016-10-10 14:18:36 -0700

    Add AspNetCore meta package

---
+++ build.cmd
@@ -0,0 +1,2 @@
+@ECHO OFF
+PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0build.ps1' %*; exit $LASTEXITCODE"
