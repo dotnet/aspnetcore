24773c8aa471c3c5484e80b9c9e91a944c60c1df
SteveSandersonMS <stevesa@microsoft.com>
2016-10-11 15:54:14 +0100

    In aspnet-webpack, provide a SourceMapDevToolPlugin wrapper that strips out "charset=utf-8;" from inline source map URLs to enable VS debugger compatibility

--- src/Microsoft.AspNetCore.SpaServices/npm/aspnet-webpack/src/index.ts
+++ src/Microsoft.AspNetCore.SpaServices/npm/aspnet-webpack/src/index.ts
@@ -1,2 +1,3 @@
 export { createWebpackDevServer } from './WebpackDevMiddleware';
 export { loadViaWebpack } from './LoadViaWebpack';
+export { SourceMapDevToolPlugin } from './SourceMapDevToolPlugin';
