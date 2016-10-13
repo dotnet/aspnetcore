a52d692de0f89765d2ff4128b5cc9bd53741febf
Pranav K <prkrishn@hotmail.com>
2016-10-13 11:21:09 -0700

    Updating to netcoreapp1.1

--- test/Microsoft.AspNetCore.Mvc.WebApiCompatShimTest/Mocks/MockContentNegotiator.cs
+++ test/Microsoft.AspNetCore.Mvc.WebApiCompatShimTest/Mocks/MockContentNegotiator.cs
@@ -34,7 +34,7 @@ namespace System.Net.Http.Formatting.Mocks
             return base.SelectResponseCharacterEncoding(request, formatter);
         }
 
-#if !NETCOREAPP1_0
+#if !NETCOREAPP1_1
 
         public new MediaTypeFormatterMatch MatchMediaTypeMapping(HttpRequestMessage request, MediaTypeFormatter formatter)
         {
