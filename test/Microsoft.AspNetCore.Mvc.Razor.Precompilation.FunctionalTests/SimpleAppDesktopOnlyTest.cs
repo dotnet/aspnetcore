3d1caa474ee0cd9f8e2bc72782f3096393b5cd78
Pranav K <prkrishn@hotmail.com>
2016-10-21 12:00:57 -0700

    Adding a functional test for desktop only project

---
+++ test/Microsoft.AspNetCore.Mvc.Razor.Precompilation.FunctionalTests/SimpleAppDesktopOnlyTest.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Server.IntegrationTesting;
+using Microsoft.AspNetCore.Testing.xunit;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.Razor.Precompilation
+{
+    public class SimpleAppDesktopOnlyTest : IClassFixture<SimpleAppDesktopOnlyTest.SimpleAppDesktopOnlyTestFixture>
+    {
+        public SimpleAppDesktopOnlyTest(SimpleAppDesktopOnlyTestFixture fixture)
+        {
+            Fixture = fixture;
+        }
+
+        public ApplicationTestFixture Fixture { get; }
+
+        [Fact]
+        [OSSkipConditionAttribute(OperatingSystems.Linux)]
+        [OSSkipConditionAttribute(OperatingSystems.Windows)]
+        public async Task Precompilation_WorksForSimpleApps()
+        {
+            // Arrange
+            using (var deployer = Fixture.CreateDeployment(RuntimeFlavor.Clr))
+            {
+                var deploymentResult = deployer.Deploy();
+
+                // Act
+                var response = await Fixture.HttpClient.GetStringWithRetryAsync(
+                    deploymentResult.ApplicationBaseUri,
+                    Fixture.Logger);
+
+                // Assert
+                TestEmbeddedResource.AssertContent("SimpleAppDesktopOnly.Home.Index.txt", response);
+            }
+        }
+
+        public class SimpleAppDesktopOnlyTestFixture : ApplicationTestFixture
+        {
+            public SimpleAppDesktopOnlyTestFixture()
+                : base("SimpleAppDesktopOnly")
+            {
+            }
+        }
+    }
+}
---
+++ testapps/SimpleAppDesktopOnly/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
