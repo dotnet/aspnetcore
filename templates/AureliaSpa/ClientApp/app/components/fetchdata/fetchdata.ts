6bceb234d877e521b949adec6953e0decf0c769e
SteveSandersonMS <stevesa@microsoft.com>
2016-10-28 15:11:25 +0100

    In AureliaSpa, use whatwg-fetch to provide type info

--- templates/AureliaSpa/ClientApp/app/components/fetchdata/fetchdata.ts
+++ templates/AureliaSpa/ClientApp/app/components/fetchdata/fetchdata.ts
@@ -1,5 +1,7 @@
-/// <reference path="../../../../node_modules/aurelia-fetch-client/doc/whatwg-fetch.d.ts" />
+// The following line is a workaround for aurelia-fetch-client requiring the type UrlSearchParams
+// to exist in global scope, but that type not being declared in any public @types/* package.
 /// <reference path="../../../../node_modules/aurelia-fetch-client/doc/url.d.ts" />
+
 import { HttpClient } from 'aurelia-fetch-client';
 import { inject } from 'aurelia-framework';
 
