01b237dda9f7320313cbbc881d881d0a26ec432f
Ryan Brandenburg <rybrande@microsoft.com>
2016-10-28 16:21:50 -0700

    Antiforgery goes at the end of filters

---
+++ test/Microsoft.AspNetCore.Mvc.FunctionalTests/AntiforgeryAuthTests.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SecurityWebSite;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.FunctionalTests
+{
+    public class AntiforgeryAuthTests : IClassFixture<MvcTestFixture<Startup>>
+    {
+        public AntiforgeryAuthTests(MvcTestFixture<Startup> fixture)
+        {
+            Client = fixture.Client;
+        }
+
+        public HttpClient Client { get; }
+
+        [Fact]
+        public async Task AutomaticAuthenticationBeforeAntiforgery()
+        {
+            // Arrange & Act
+            var response = await Client.PostAsync("http://localhost/Home/AutoAntiforgery", null);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+            Assert.Equal("/Home/Login", response.Headers.Location.AbsolutePath, StringComparer.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public async Task AuthBeforeAntiforgery()
+        {
+            // Arrange & Act
+            var response = await Client.GetAsync("http://localhost/Home/Antiforgery");
+
+            // Assert
+            // Redirected to login page, Antiforgery didn't fail yet
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+            Assert.Equal("/Home/Login", response.Headers.Location.AbsolutePath, StringComparer.OrdinalIgnoreCase);
+        }
+    }
+}
