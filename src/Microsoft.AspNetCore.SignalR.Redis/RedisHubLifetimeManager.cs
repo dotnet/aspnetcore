dbd738726ad0dabd1da0286931dbfbecdff02ec9
BrennanConroy <brecon@microsoft.com>
2016-11-03 15:00:13 -0700

    Remove unused usings

--- src/Microsoft.AspNetCore.SignalR.Redis/RedisHubLifetimeManager.cs
+++ src/Microsoft.AspNetCore.SignalR.Redis/RedisHubLifetimeManager.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using Channels;
 using Microsoft.AspNetCore.Sockets;
--- src/Microsoft.AspNetCore.SignalR.Redis/RedisOptions.cs
+++ src/Microsoft.AspNetCore.SignalR.Redis/RedisOptions.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using StackExchange.Redis;
 
 namespace Microsoft.AspNetCore.SignalR.Redis
