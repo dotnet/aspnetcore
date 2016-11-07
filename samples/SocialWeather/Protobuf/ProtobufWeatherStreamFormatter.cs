f3dfb62af96daeac238eb2f16998cae29a51426c
moozzyk <pawelka@microsoft.com>
2016-11-07 13:06:16 -0800

    Adding protobuf formatter

---
+++ samples/SocialWeather/Protobuf/ProtobufWeatherStreamFormatter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Google.Protobuf;
+
+namespace SocialWeather.Protobuf
+{
+    public class ProtobufWeatherStreamFormatter : IStreamFormatter<SocialWeather.WeatherReport>
+    {
+        public Task<SocialWeather.WeatherReport> ReadAsync(Stream stream)
+        {
+            var inputStream = new CodedInputStream(stream, leaveOpen: true);
+            var protoWeatherReport = new Protobuf.WeatherReport();
+            inputStream.ReadMessage(protoWeatherReport);
+            return Task.FromResult(new SocialWeather.WeatherReport
+            {
+                Temperature = protoWeatherReport.Temperature,
+                ReportTime = protoWeatherReport.ReportTime,
+                Weather = (Weather)(int)protoWeatherReport.Weather,
+                ZipCode = protoWeatherReport.ZipCode
+            });
+        }
+
+        public async Task WriteAsync(SocialWeather.WeatherReport weatherReport, Stream stream)
+        {
+            var outputStream = new CodedOutputStream(stream, leaveOpen: true);
+            var protoWeatherReport = new Protobuf.WeatherReport
+            {
+                Temperature = weatherReport.Temperature,
+                ReportTime = weatherReport.ReportTime,
+                Weather = (Protobuf.WeatherReport.Types.WeatherKind)(int)weatherReport.Weather,
+                ZipCode = weatherReport.ZipCode
+            };
+
+            outputStream.WriteMessage(protoWeatherReport);
+            outputStream.Flush();
+            await stream.FlushAsync();
+        }
+    }
+}
--- samples/SocialWeather/Startup.cs
+++ samples/SocialWeather/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SocialWeather.Protobuf;
 
 namespace SocialWeather
 {
