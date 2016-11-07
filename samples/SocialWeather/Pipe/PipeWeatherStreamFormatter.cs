81ddada76b0aaba37250e3311cf8e93478ead843
moozzyk <pawelka@microsoft.com>
2016-11-07 13:16:23 -0800

    Moving Pipe formatter to a subfolder

--- samples/SocialWeather/Pipe/PipeWeatherStreamFormatter.cs
+++ samples/SocialWeather/Pipe/PipeWeatherStreamFormatter.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SocialWeather
+namespace SocialWeather.Pipe
 {
     public class PipeWeatherStreamFormatter : IStreamFormatter<WeatherReport>
     {
--- samples/SocialWeather/Startup.cs
+++ samples/SocialWeather/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SocialWeather.Pipe;
 using SocialWeather.Protobuf;
 
 namespace SocialWeather
