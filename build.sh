a0e07744c086b503ac49825d03c9bea46956c3d9
Nate McMaster <nate.mcmaster@microsoft.com>
2016-11-17 14:13:21 -0800

    Pin versions

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 repoFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $repoFolder
 
-koreBuildZip="https://github.com/aspnet/KoreBuild/archive/feature/msbuild.zip"
+koreBuildZip="https://github.com/aspnet/KoreBuild/archive/rel/1.0.0-msbuild2.zip"
 if [ ! -z $KOREBUILD_ZIP ]; then
     koreBuildZip=$KOREBUILD_ZIP
 fi
