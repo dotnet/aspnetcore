e316898c9fe115c8bba0bc80d3bc0843d9b44b99
Kristian Hellang <kristian@hellang.com>
2016-11-22 13:08:34 +0100

    Added missing copyright header

--- tools/Microsoft.AspNetCore.Server.Kestrel.GeneratedCode/FrameFeatureCollection.cs
+++ tools/Microsoft.AspNetCore.Server.Kestrel.GeneratedCode/FrameFeatureCollection.cs
@@ -62,7 +62,9 @@ namespace Microsoft.AspNetCore.Server.Kestrel.GeneratedCode
                 typeof(IHttpConnectionFeature)
             };
 
-            return $@"
+            return $@"// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System;
 using System.Collections.Generic;
 
