9f6b0b0573ea24d1d8fb46f91c93844593e8b7b2
SteveSandersonMS <stevesa@microsoft.com>
2016-11-25 15:06:23 +0000

    Build SpaServices 'Node/Content/*.js' files from TypeScript sources

---
+++ src/Microsoft.AspNetCore.SpaServices/TypeScript/Prerenderer.ts
@@ -0,0 +1,20 @@
+// Pass through the invocation to the 'aspnet-prerendering' package, verifying that it can be loaded
+export function renderToString(callback) {
+    let aspNetPrerendering;
+    try {
+        aspNetPrerendering = require('aspnet-prerendering');
+    } catch (ex) {
+        // Developers sometimes have trouble with badly-configured Node installations, where it's unable
+        // to find node_modules. Or they accidentally fail to deploy node_modules, or even to run 'npm install'.
+        // Make sure such errors are reported back to the .NET part of the app.
+        callback(
+            'Prerendering failed because of an error while loading \'aspnet-prerendering\'. Error was: '
+            + ex.stack
+            + '\nCurrent directory is: '
+            + process.cwd()
+        );
+        return;
+    }
+
+    return aspNetPrerendering.renderToString.apply(this, arguments);
+}
