a3cba50e886189b5049770e8858cd3f212adcc6f
SteveSandersonMS <stevesa@microsoft.com>
2016-11-28 17:28:08 +0000

    Make ReactReduxSpa able to do prerendering without node_modules at runtime

--- templates/ReactReduxSpa/webpack.config.vendor.js
+++ templates/ReactReduxSpa/webpack.config.vendor.js
@@ -2,39 +2,84 @@ var isDevBuild = process.argv.indexOf('--env.prod') < 0;
 var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var merge = require('webpack-merge');
 var extractCSS = new ExtractTextPlugin('vendor.css');
 
-module.exports = {
-    resolve: {
-        extensions: [ '', '.js' ]
-    },
+var sharedConfig = {
+    resolve: { extensions: [ '', '.js' ] },
     module: {
         loaders: [
-            { test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/, loader: 'url-loader?limit=100000' },
-            { test: /\.css(\?|$)/, loader: extractCSS.extract(['css-loader']) }
+            { test: /\.json$/, loader: require.resolve('json-loader') },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/, loader: 'url-loader?limit=100000' }
         ]
     },
     entry: {
-        vendor: ['bootstrap', 'bootstrap/dist/css/bootstrap.css', 'domain-task', 'event-source-polyfill', 'react', 'react-dom', 'react-router', 'redux', 'redux-thunk', 'react-router-redux', 'style-loader', 'jquery'],
+        vendor: [
+            'bootstrap',
+            'bootstrap/dist/css/bootstrap.css',
+            'domain-task',
+            'event-source-polyfill',
+            'react',
+            'react-dom',
+            'react-router',
+            'react-redux',
+            'redux',
+            'redux-thunk',
+            'react-router-redux',
+            'style-loader',
+            'jquery'
+        ],
     },
     output: {
-        path: path.join(__dirname, 'wwwroot', 'dist'),
         publicPath: '/dist/',
         filename: '[name].js',
         library: '[name]_[hash]',
     },
     plugins: [
-        extractCSS,
         new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }), // Maps these identifiers to the jQuery package (because Bootstrap expects it to be a global variable)
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.DllPlugin({
-            path: path.join(__dirname, 'wwwroot', 'dist', '[name]-manifest.json'),
-            name: '[name]_[hash]'
-        }),
+        new webpack.NormalModuleReplacementPlugin(/\/iconv-loader$/, require.resolve('node-noop')), // Workaround for https://github.com/andris9/encoding/issues/16
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': isDevBuild ? '"development"' : '"production"'
         })
+    ]
+};
+
+var clientBundleConfig = merge(sharedConfig, {
+    output: { path: path.join(__dirname, 'wwwroot', 'dist') },
+    module: {
+        loaders: [
+            { test: /\.css(\?|$)/, loader: extractCSS.extract(['css-loader']) }
+        ]
+    },
+    plugins: [
+        extractCSS,
+        new webpack.DllPlugin({
+            path: path.join(__dirname, 'wwwroot', 'dist', '[name]-manifest.json'),
+            name: '[name]_[hash]'
+        })
     ].concat(isDevBuild ? [] : [
+        new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } })
     ])
-};
+});
+
+var serverBundleConfig = merge(sharedConfig, {
+    target: 'node',
+    resolve: { packageMains: ['main'] },
+    output: {
+        path: path.join(__dirname, 'ClientApp', 'dist'),
+        libraryTarget: 'commonjs2',
+    },
+    module: {
+        loaders: [ { test: /\.css(\?|$)/, loader: 'css-loader' } ]
+    },
+    entry: { vendor: ['aspnet-prerendering', 'react-dom/server'] },
+    plugins: [
+        new webpack.DllPlugin({
+            path: path.join(__dirname, 'ClientApp', 'dist', '[name]-manifest.json'),
+            name: '[name]_[hash]'
+        })
+    ]
+});
+
+module.exports = [clientBundleConfig, serverBundleConfig];
