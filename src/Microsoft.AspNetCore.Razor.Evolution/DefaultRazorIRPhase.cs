2f54c12b820800c9afdfd1190a29d5e6ecd6f2bc
Ryan Nowak <nowakra@gmail.com>
2016-12-06 10:25:53 -0800

    Add missing interface to IR phase
    Addressing PR feedback

--- src/Microsoft.AspNetCore.Razor.Evolution/DefaultRazorIRPhase.cs
+++ src/Microsoft.AspNetCore.Razor.Evolution/DefaultRazorIRPhase.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace Microsoft.AspNetCore.Razor.Evolution
 {
-    internal class DefaultRazorIRPhase : RazorEnginePhaseBase
+    internal class DefaultRazorIRPhase : RazorEnginePhaseBase, IRazorIRPhase
     {
         public IRazorIRPass[] Passes { get; private set; }
 
