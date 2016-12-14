9e59e79a94d995dcc2edbcdfab43889c26a7156e
Jonathan Underwood <jonathan@jyunderwood.com>
2016-12-14 14:47:33 -0600

    Sample: use FindByEmailAsync in ResetPassword flow

--- samples/IdentitySample.Mvc/Controllers/AccountController.cs
+++ samples/IdentitySample.Mvc/Controllers/AccountController.cs
@@ -270,7 +270,7 @@ namespace IdentitySample.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = await _userManager.FindByNameAsync(model.Email);
+                var user = await _userManager.FindByEmailAsync(model.Email);
                 if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
                 {
                     // Don't reveal that the user does not exist or is not confirmed
@@ -319,7 +319,7 @@ namespace IdentitySample.Controllers
             {
                 return View(model);
             }
-            var user = await _userManager.FindByNameAsync(model.Email);
+            var user = await _userManager.FindByEmailAsync(model.Email);
             if (user == null)
             {
                 // Don't reveal that the user does not exist
