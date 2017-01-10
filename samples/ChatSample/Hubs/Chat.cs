b18606ec8d8b15b0d2aad60b58c682b71c06b358
moozzyk <pawelka@microsoft.com>
2017-01-10 11:41:47 -0800

    Fixing chat sample - it was still using fully qualified hub method name

--- samples/ChatSample/Hubs/Chat.cs
+++ samples/ChatSample/Hubs/Chat.cs
@@ -2,11 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 
 namespace ChatSample.Hubs
 {
