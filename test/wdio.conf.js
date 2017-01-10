f8f42a53b20312b41b00f30b917c2685395c751a
SteveSandersonMS <stevesa@microsoft.com>
2017-01-10 10:37:00 +0000

    Increase Mocha timeout. Trying to fix intermittent test failures on AppVeyor.

--- test/wdio.conf.js
+++ test/wdio.conf.js
@@ -129,7 +129,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 30000
+        timeout: 60000
     },
     //
     // =====
