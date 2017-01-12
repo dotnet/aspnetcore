c273496f6ccd73a49a0d94271fda4c805f55ddb5
Nate McMaster <nate.mcmaster@microsoft.com>
2017-01-12 14:22:19 -0800

    Add some helper output to reserve-package script

--- tools/Reserve-Package.ps1
+++ tools/Reserve-Package.ps1
@@ -5,7 +5,7 @@ Reserve a NuGet package name on nuget.org
 
 .DESCRIPTION
 
-Generate an empty NuGet package and temporarily publish to nuget.org in order to reserve a pacakge name.
+Generate an empty NuGet package and temporarily publish to nuget.org in order to reserve a package name.
 
 .EXAMPLE
 
@@ -17,7 +17,7 @@ A NuGet ApiKey is required to publish a NuGet package. If the package is authori
 
 .PARAMETER packageName
 
-The name of pacakge to be reserved on nuget.org 
+The name of package to be reserved on nuget.org
 
 .PARAMETER publish
 
@@ -45,5 +45,7 @@ pushd nuspec
 if ($publish) {
     ..\nuget.exe push "$packageName.0.0.1-alpha.nupkg" -Source https://www.nuget.org -ApiKey $apikey -Non
     ..\nuget.exe delete $packageName 0.0.1-alpha -Source https://www.nuget.org -ApiKey $apikey -Non
+} else {
+    Write-Host "Skipping publish. Re-run with -publish to push to nuget.org"
 }
 popd
