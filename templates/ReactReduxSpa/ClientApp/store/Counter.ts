867d070263e287b55842b5c70229952ea5552c95
SteveSandersonMS <stevesa@microsoft.com>
2017-01-18 19:14:17 +0000

    Remove unused references to ThunkAction. Fixes a build error since @types/redux-thunk 2.2.0 removes the ThunkAction export.

--- templates/ReactReduxSpa/ClientApp/store/Counter.ts
+++ templates/ReactReduxSpa/ClientApp/store/Counter.ts
@@ -1,4 +1,4 @@
-import { Action, Reducer, ThunkAction } from 'redux';
+import { Action, Reducer } from 'redux';
 
 // -----------------
 // STATE - This defines the type of data maintained in the Redux store.
--- templates/ReactReduxSpa/ClientApp/store/WeatherForecasts.ts
+++ templates/ReactReduxSpa/ClientApp/store/WeatherForecasts.ts
@@ -1,5 +1,5 @@
 import { fetch, addTask } from 'domain-task';
-import { Action, Reducer, ThunkAction, ActionCreator } from 'redux';
+import { Action, Reducer, ActionCreator } from 'redux';
 import { AppThunkAction } from './';
 
 // -----------------
