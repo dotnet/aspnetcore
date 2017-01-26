CarlosLanderas/SignalR

Converting pipelines to channels

src/Microsoft.AspNetCore.Sockets.Client/Connection.cs
@@ -2,56 +2,45 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.IO.Pipelines;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Channels;
+using Microsoft.AspNetCore.Sockets.Internal;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.AspNetCore.Sockets.Client
 {
-    public class Connection : IPipelineConnection
+    public class Connection : IChannelConnection<Message>
     {
-        private IPipelineConnection _consumerPipe;
+        private IChannelConnection<Message> _toFromTransport;
         private ITransport _transport;
         private readonly ILogger _logger;
 
         public Uri Url { get; }
 
         // TODO: Review. This is really only designed to be used from ConnectAsync
-        private Connection(Uri url, ITransport transport, IPipelineConnection consumerPipe, ILogger logger)
+        private Connection(Uri url, ITransport transport, IChannelConnection<Message> toFromTransport, ILogger logger)
         {
             Url = url;
 
             _logger = logger;
             _transport = transport;
-            _consumerPipe = consumerPipe;
-
-            _consumerPipe.Output.Writing.ContinueWith(t =>
-            {
-                if (t.IsFaulted)
-                {
-                    _consumerPipe.Input.Complete(t.Exception);
-                }
-
-                return t;
-            });
+            _toFromTransport = toFromTransport;
         }
 
-        public IPipelineReader Input => _consumerPipe.Input;
-        public IPipelineWriter Output => _consumerPipe.Output;
+        public ReadableChannel<Message> Input => _toFromTransport.Input;
+        public WritableChannel<Message> Output => _toFromTransport.Output;
 
         public void Dispose()
         {
-            _consumerPipe.Dispose();
             _transport.Dispose();
         }
 
-        // TODO: More overloads. PipelineFactory should be optional but someone needs to dispose the pool, if we're OK with it being the GC, then this is easy.
-        public static Task<Connection> ConnectAsync(Uri url, ITransport transport, PipelineFactory pipelineFactory) => ConnectAsync(url, transport, new HttpClient(), pipelineFactory, NullLoggerFactory.Instance);
-        public static Task<Connection> ConnectAsync(Uri url, ITransport transport, PipelineFactory pipelineFactory, ILoggerFactory loggerFactory) => ConnectAsync(url, transport, new HttpClient(), pipelineFactory, loggerFactory);
-        public static Task<Connection> ConnectAsync(Uri url, ITransport transport, HttpClient httpClient, PipelineFactory pipelineFactory) => ConnectAsync(url, transport, httpClient, pipelineFactory, NullLoggerFactory.Instance);
+        public static Task<Connection> ConnectAsync(Uri url, ITransport transport) => ConnectAsync(url, transport, new HttpClient(), NullLoggerFactory.Instance);
+        public static Task<Connection> ConnectAsync(Uri url, ITransport transport, ILoggerFactory loggerFactory) => ConnectAsync(url, transport, new HttpClient(), loggerFactory);
+        public static Task<Connection> ConnectAsync(Uri url, ITransport transport, HttpClient httpClient) => ConnectAsync(url, transport, httpClient, NullLoggerFactory.Instance);
 
-        public static async Task<Connection> ConnectAsync(Uri url, ITransport transport, HttpClient httpClient, PipelineFactory pipelineFactory, ILoggerFactory loggerFactory)
+        public static async Task<Connection> ConnectAsync(Uri url, ITransport transport, HttpClient httpClient, ILoggerFactory loggerFactory)
         {
             if (url == null)
             {
@@ -66,11 +55,6 @@ namespace Microsoft.AspNetCore.Sockets.Client
             if (httpClient == null)
             {
                 throw new ArgumentNullException(nameof(httpClient));
-            }
-
-            if (pipelineFactory == null)
-            {
-                throw new ArgumentNullException(nameof(pipelineFactory));
             }
 
             if (loggerFactory == null)
@@ -97,12 +81,16 @@ namespace Microsoft.AspNetCore.Sockets.Client
 
             var connectedUrl = Utils.AppendQueryString(url, "id=" + connectionId);
 
-            var pair = pipelineFactory.CreatePipelinePair();
+            var connectionToTransport = Channel.CreateUnbounded<Message>();
+            var transportToConnection = Channel.CreateUnbounded<Message>();
+            var toFromConnection = new ChannelConnection<Message>(transportToConnection, connectionToTransport);
+            var toFromTransport = new ChannelConnection<Message>(connectionToTransport, transportToConnection);
+
 
             // Start the transport, giving it one end of the pipeline
             try
             {
-                await transport.StartAsync(connectedUrl, pair.Item1);
+                await transport.StartAsync(connectedUrl, toFromConnection);
             }
             catch (Exception ex)
             {
@@ -111,7 +99,7 @@ namespace Microsoft.AspNetCore.Sockets.Client
             }
 
             // Create the connection, giving it the other end of the pipeline
-            return new Connection(url, transport, pair.Item2, logger);
+            return new Connection(url, transport, toFromTransport, logger);
         }
     }
 }

src/Microsoft.AspNetCore.Sockets.Client/ITransport.cs
@@ -2,13 +2,12 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.IO.Pipelines;
 using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.Sockets.Client
 {
     public interface ITransport : IDisposable
     {
-        Task StartAsync(Uri url, IPipelineConnection pipeline);
+        Task StartAsync(Uri url, IChannelConnection<Message> toFromConnection);
     }
 }

src/Microsoft.AspNetCore.Sockets.Client/LongPollingTransport.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.IO;
 using System.IO.Pipelines;
 using System.Net;
 using System.Net.Http;
@@ -23,7 +24,8 @@ namespace Microsoft.AspNetCore.Sockets.Client
         private readonly CancellationTokenSource _senderCts = new CancellationTokenSource();
         private readonly CancellationTokenSource _pollCts = new CancellationTokenSource();
 
-        private IPipelineConnection _pipeline;
+        private IChannelConnection<Message> _toFromConnection;
+
         private Task _sender;
         private Task _poller;
 
@@ -39,104 +41,98 @@ namespace Microsoft.AspNetCore.Sockets.Client
         {
             _senderCts.Cancel();
             _pollCts.Cancel();
-            _pipeline?.Dispose();
         }
 
-        public Task StartAsync(Uri url, IPipelineConnection pipeline)
+        public Task StartAsync(Uri url, IChannelConnection<Message> toFromConnection)
         {
-            _pipeline = pipeline;
-
-            // Schedule shutdown of the poller when the output is closed
-            pipeline.Output.Writing.ContinueWith(_ =>
-            {
-                _pollCts.Cancel();
-                return TaskCache.CompletedTask;
-            });
+            _toFromConnection = toFromConnection;
 
             // Start sending and polling
-            _poller = Poll(Utils.AppendPath(url, "poll"), _pollCts.Token);
-            _sender = SendMessages(Utils.AppendPath(url, "send"), _senderCts.Token);
+            _poller = Poll(Utils.AppendPath(url, "poll"), _pollCts.Token).ContinueWith(_ => _senderCts.Cancel());
+            _sender = SendMessages(Utils.AppendPath(url, "send"), _senderCts.Token).ContinueWith(_ => _pollCts.Cancel());
             Running = Task.WhenAll(_sender, _poller);
 
             return TaskCache.CompletedTask;
         }
 
-        private async Task Poll(Uri pollUrl, CancellationToken cancellationToken)
+        private async Task Poll(Uri pollUrl, CancellationToken pollCancellationToken)
         {
             try
             {
-                while (!cancellationToken.IsCancellationRequested)
+                while (!pollCancellationToken.IsCancellationRequested)
                 {
                     var request = new HttpRequestMessage(HttpMethod.Get, pollUrl);
                     request.Headers.UserAgent.Add(DefaultUserAgentHeader);
 
-                    var response = await _httpClient.SendAsync(request, cancellationToken);
+                    var response = await _httpClient.SendAsync(request, pollCancellationToken);
                     response.EnsureSuccessStatusCode();
 
-                    if (response.StatusCode == HttpStatusCode.NoContent || cancellationToken.IsCancellationRequested)
+                    if (response.StatusCode == HttpStatusCode.NoContent || pollCancellationToken.IsCancellationRequested)
                     {
                         // Transport closed or polling stopped, we're done
                         break;
                     }
                     else
                     {
-                        // Write the data to the output
-                        var buffer = _pipeline.Output.Alloc();
-                        var stream = new WriteableBufferStream(buffer);
-                        await response.Content.CopyToAsync(stream);
-                        await buffer.FlushAsync();
+                        var ms = new MemoryStream();
+                        await response.Content.CopyToAsync(ms);
+                        var message = new Message(ReadableBuffer.Create(ms.ToArray()).Preserve(), Format.Text);
+
+                        while (await _toFromConnection.Output.WaitToWriteAsync(pollCancellationToken))
+                        {
+                            if (_toFromConnection.Output.TryWrite(message))
+                            {
+                                break;
+                            }
+                        }
                     }
                 }
 
-                // Polling complete
-                _pipeline.Output.Complete();
+                _toFromConnection.Output.TryComplete();
+            }
+            catch (OperationCanceledException)
+            {
+                // transport is being closed
+                _toFromConnection.Output.TryComplete();
             }
             catch (Exception ex)
             {
-                // Shut down the output pipeline and log
                 _logger.LogError("Error while polling '{0}': {1}", pollUrl, ex);
-                _pipeline.Output.Complete(ex);
-                _pipeline.Input.Complete(ex);
+                _toFromConnection.Output.TryComplete(ex);
             }
         }
 
-        private async Task SendMessages(Uri sendUrl, CancellationToken cancellationToken)
+        private async Task SendMessages(Uri sendUrl, CancellationToken sendCancellationToken)
         {
-            using (cancellationToken.Register(() => _pipeline.Input.Complete()))
+            try
             {
-                try
+                while (await _toFromConnection.Input.WaitToReadAsync(sendCancellationToken))
                 {
-                    while (!cancellationToken.IsCancellationRequested)
+                    Message message;
+                    if (!_toFromConnection.Input.TryRead(out message))
                     {
-                        var result = await _pipeline.Input.ReadAsync();
-                        var buffer = result.Buffer;
-                        if (buffer.IsEmpty || result.IsCompleted)
-                        {
-                            // No more data to send
-                            break;
-                        }
-
-                        // Create a message to send
-                        var message = new HttpRequestMessage(HttpMethod.Post, sendUrl);
-                        message.Headers.UserAgent.Add(DefaultUserAgentHeader);
-                        message.Content = new ReadableBufferContent(buffer);
-
-                        // Send it
-                        var response = await _httpClient.SendAsync(message);
-                        response.EnsureSuccessStatusCode();
-
-                        _pipeline.Input.Advance(buffer.End);
+                        continue;
                     }
 
-                    // Sending complete
-                    _pipeline.Input.Complete();
+                    var request = new HttpRequestMessage(HttpMethod.Post, sendUrl);
+                    request.Headers.UserAgent.Add(DefaultUserAgentHeader);
+                    request.Content = new ReadableBufferContent(message.Payload.Buffer);
+
+                    var response = await _httpClient.SendAsync(request);
+                    response.EnsureSuccessStatusCode();
                 }
-                catch (Exception ex)
-                {
-                    // Shut down the input pipeline and log
-                    _logger.LogError("Error while sending to '{0}': {1}", sendUrl, ex);
-                    _pipeline.Input.Complete(ex);
-                }
+
+                _toFromConnection.Output.TryComplete();
+            }
+            catch (OperationCanceledException)
+            {
+                // transport is being closed
+                _toFromConnection.Output.TryComplete();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error while sending to '{0}': {1}", sendUrl, ex);
+                _toFromConnection.Output.TryComplete(ex);
             }
         }
     }

dotnet/aspnetcore

src/Microsoft.AspNetCore.Sockets.Client/Connection.cs
@@ -2,56 +2,45 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.IO.Pipelines;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Channels;
+using Microsoft.AspNetCore.Sockets.Internal;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.AspNetCore.Sockets.Client
 {
-    public class Connection : IPipelineConnection
+    public class Connection : IChannelConnection<Message>
     {
-        private IPipelineConnection _consumerPipe;
+        private IChannelConnection<Message> _transportChannel;
         private ITransport _transport;
         private readonly ILogger _logger;
 
         public Uri Url { get; }
 
         // TODO: Review. This is really only designed to be used from ConnectAsync
-        private Connection(Uri url, ITransport transport, IPipelineConnection consumerPipe, ILogger logger)
+        private Connection(Uri url, ITransport transport, IChannelConnection<Message> transportChannel, ILogger logger)
         {
             Url = url;
 
             _logger = logger;
             _transport = transport;
-            _consumerPipe = consumerPipe;
-
-            _consumerPipe.Output.Writing.ContinueWith(t =>
-            {
-                if (t.IsFaulted)
-                {
-                    _consumerPipe.Input.Complete(t.Exception);
-                }
-
-                return t;
-            });
+            _transportChannel = transportChannel;
         }
 
-        public IPipelineReader Input => _consumerPipe.Input;
-        public IPipelineWriter Output => _consumerPipe.Output;
+        public ReadableChannel<Message> Input => _transportChannel.Input;
+        public WritableChannel<Message> Output => _transportChannel.Output;
 
         public void Dispose()
         {
-            _consumerPipe.Dispose();
             _transport.Dispose();
         }
 
-        // TODO: More overloads. PipelineFactory should be optional but someone needs to dispose the pool, if we're OK with it being the GC, then this is easy.
-        public static Task<Connection> ConnectAsync(Uri url, ITransport transport, PipelineFactory pipelineFactory) => ConnectAsync(url, transport, new HttpClient(), pipelineFactory, NullLoggerFactory.Instance);
-        public static Task<Connection> ConnectAsync(Uri url, ITransport transport, PipelineFactory pipelineFactory, ILoggerFactory loggerFactory) => ConnectAsync(url, transport, new HttpClient(), pipelineFactory, loggerFactory);
-        public static Task<Connection> ConnectAsync(Uri url, ITransport transport, HttpClient httpClient, PipelineFactory pipelineFactory) => ConnectAsync(url, transport, httpClient, pipelineFactory, NullLoggerFactory.Instance);
+        public static Task<Connection> ConnectAsync(Uri url, ITransport transport) => ConnectAsync(url, transport, new HttpClient(), NullLoggerFactory.Instance);
+        public static Task<Connection> ConnectAsync(Uri url, ITransport transport, ILoggerFactory loggerFactory) => ConnectAsync(url, transport, new HttpClient(), loggerFactory);
+        public static Task<Connection> ConnectAsync(Uri url, ITransport transport, HttpClient httpClient) => ConnectAsync(url, transport, httpClient, NullLoggerFactory.Instance);
 
-        public static async Task<Connection> ConnectAsync(Uri url, ITransport transport, HttpClient httpClient, PipelineFactory pipelineFactory, ILoggerFactory loggerFactory)
+        public static async Task<Connection> ConnectAsync(Uri url, ITransport transport, HttpClient httpClient, ILoggerFactory loggerFactory)
         {
             if (url == null)
             {
@@ -66,11 +55,6 @@ namespace Microsoft.AspNetCore.Sockets.Client
             if (httpClient == null)
             {
                 throw new ArgumentNullException(nameof(httpClient));
-            }
-
-            if (pipelineFactory == null)
-            {
-                throw new ArgumentNullException(nameof(pipelineFactory));
             }
 
             if (loggerFactory == null)
@@ -97,12 +81,16 @@ namespace Microsoft.AspNetCore.Sockets.Client
 
             var connectedUrl = Utils.AppendQueryString(url, "id=" + connectionId);
 
-            var pair = pipelineFactory.CreatePipelinePair();
+            var applicationToTransport = Channel.CreateUnbounded<Message>();
+            var transportToApplication = Channel.CreateUnbounded<Message>();
+            var applicationSide = new ChannelConnection<Message>(transportToApplication, applicationToTransport);
+            var transportSide = new ChannelConnection<Message>(applicationToTransport, transportToApplication);
+
 
             // Start the transport, giving it one end of the pipeline
             try
             {
-                await transport.StartAsync(connectedUrl, pair.Item1);
+                await transport.StartAsync(connectedUrl, applicationSide);
             }
             catch (Exception ex)
             {
@@ -111,7 +99,7 @@ namespace Microsoft.AspNetCore.Sockets.Client
             }
 
             // Create the connection, giving it the other end of the pipeline
-            return new Connection(url, transport, pair.Item2, logger);
+            return new Connection(url, transport, transportSide, logger);
         }
     }
 }

src/Microsoft.AspNetCore.Sockets.Client/ITransport.cs
@@ -2,13 +2,12 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.IO.Pipelines;
 using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.Sockets.Client
 {
     public interface ITransport : IDisposable
     {
-        Task StartAsync(Uri url, IPipelineConnection pipeline);
+        Task StartAsync(Uri url, IChannelConnection<Message> application);
     }
 }

src/Microsoft.AspNetCore.Sockets.Client/LongPollingTransport.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.IO;
 using System.IO.Pipelines;
 using System.Net;
 using System.Net.Http;
@@ -20,12 +21,10 @@ namespace Microsoft.AspNetCore.Sockets.Client
 
         private readonly HttpClient _httpClient;
         private readonly ILogger _logger;
-        private readonly CancellationTokenSource _senderCts = new CancellationTokenSource();
-        private readonly CancellationTokenSource _pollCts = new CancellationTokenSource();
-
-        private IPipelineConnection _pipeline;
+        private IChannelConnection<Message> _application;
         private Task _sender;
         private Task _poller;
+        private readonly CancellationTokenSource _transportCts = new CancellationTokenSource();
 
         public Task Running { get; private set; }
 
@@ -37,26 +36,21 @@ namespace Microsoft.AspNetCore.Sockets.Client
 
         public void Dispose()
         {
-            _senderCts.Cancel();
-            _pollCts.Cancel();
-            _pipeline?.Dispose();
+            _transportCts.Cancel();
         }
 
-        public Task StartAsync(Uri url, IPipelineConnection pipeline)
+        public Task StartAsync(Uri url, IChannelConnection<Message> application)
         {
-            _pipeline = pipeline;
-
-            // Schedule shutdown of the poller when the output is closed
-            pipeline.Output.Writing.ContinueWith(_ =>
-            {
-                _pollCts.Cancel();
-                return TaskCache.CompletedTask;
-            });
+            _application = application;
 
             // Start sending and polling
-            _poller = Poll(Utils.AppendPath(url, "poll"), _pollCts.Token);
-            _sender = SendMessages(Utils.AppendPath(url, "send"), _senderCts.Token);
-            Running = Task.WhenAll(_sender, _poller);
+            _poller = Poll(Utils.AppendPath(url, "poll"), _transportCts.Token);
+            _sender = SendMessages(Utils.AppendPath(url, "send"), _transportCts.Token);
+
+            Running = Task.WhenAll(_sender, _poller).ContinueWith(t => {
+                _application.Output.TryComplete(t.IsFaulted ? t.Exception.InnerException : null);
+                return t;
+            }).Unwrap();
 
             return TaskCache.CompletedTask;
         }
@@ -80,63 +74,70 @@ namespace Microsoft.AspNetCore.Sockets.Client
                     }
                     else
                     {
-                        // Write the data to the output
-                        var buffer = _pipeline.Output.Alloc();
-                        var stream = new WriteableBufferStream(buffer);
-                        await response.Content.CopyToAsync(stream);
-                        await buffer.FlushAsync();
+                        var ms = new MemoryStream();
+                        await response.Content.CopyToAsync(ms);
+                        var message = new Message(ReadableBuffer.Create(ms.ToArray()).Preserve(), Format.Text);
+
+                        while (await _application.Output.WaitToWriteAsync(cancellationToken))
+                        {
+                            if (_application.Output.TryWrite(message))
+                            {
+                                break;
+                            }
+                        }
                     }
                 }
-
-                // Polling complete
-                _pipeline.Output.Complete();
+            }
+            catch (OperationCanceledException)
+            {
+                // transport is being closed
             }
             catch (Exception ex)
             {
-                // Shut down the output pipeline and log
                 _logger.LogError("Error while polling '{0}': {1}", pollUrl, ex);
-                _pipeline.Output.Complete(ex);
-                _pipeline.Input.Complete(ex);
+                throw;
+            }
+            finally
+            {
+                // Make sure the send loop is terminated
+                _transportCts.Cancel();
             }
         }
 
         private async Task SendMessages(Uri sendUrl, CancellationToken cancellationToken)
         {
-            using (cancellationToken.Register(() => _pipeline.Input.Complete()))
+            try
             {
-                try
+                while (await _application.Input.WaitToReadAsync(cancellationToken))
                 {
-                    while (!cancellationToken.IsCancellationRequested)
+                    Message message;
+                    while (!cancellationToken.IsCancellationRequested && _application.Input.TryRead(out message))
                     {
-                        var result = await _pipeline.Input.ReadAsync();
-                        var buffer = result.Buffer;
-                        if (buffer.IsEmpty || result.IsCompleted)
+                        using (message)
                         {
-                            // No more data to send
-                            break;
+                            var request = new HttpRequestMessage(HttpMethod.Post, sendUrl);
+                            request.Headers.UserAgent.Add(DefaultUserAgentHeader);
+                            request.Content = new ReadableBufferContent(message.Payload.Buffer);
+
+                            var response = await _httpClient.SendAsync(request);
+                            response.EnsureSuccessStatusCode();
                         }
-
-                        // Create a message to send
-                        var message = new HttpRequestMessage(HttpMethod.Post, sendUrl);
-                        message.Headers.UserAgent.Add(DefaultUserAgentHeader);
-                        message.Content = new ReadableBufferContent(buffer);
-
-                        // Send it
-                        var response = await _httpClient.SendAsync(message);
-                        response.EnsureSuccessStatusCode();
-
-                        _pipeline.Input.Advance(buffer.End);
                     }
-
-                    // Sending complete
-                    _pipeline.Input.Complete();
                 }
-                catch (Exception ex)
-                {
-                    // Shut down the input pipeline and log
-                    _logger.LogError("Error while sending to '{0}': {1}", sendUrl, ex);
-                    _pipeline.Input.Complete(ex);
-                }
+            }
+            catch (OperationCanceledException)
+            {
+                // transport is being closed
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error while sending to '{0}': {1}", sendUrl, ex);
+                throw;
+            }
+            finally
+            {
+                // Make sure the poll loop is terminated
+                _transportCts.Cancel();
             }
         }
     }