aspnet/Mvc

Introducing IPageModelActivatorProvider
Fixes #5480

src/Microsoft.AspNetCore.Mvc.RazorPages/Internal/DefaultPageModelFactoryProvider.cs
@@ -1,0 +1,70 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Reflection;
+using Microsoft.Extensions.Internal;
+
+namespace Microsoft.AspNetCore.Mvc.RazorPages.Internal
+{
+    public class DefaultPageModelFactoryProvider : IPageModelFactoryProvider
+    {
+        private static readonly Func<PropertyInfo, PropertyActivator<PageContext>> _createActivateInfo =
+            CreateActivateInfo;
+        private readonly IPageModelActivatorProvider _modelActivator;
+
+        public DefaultPageModelFactoryProvider(IPageModelActivatorProvider modelActivator)
+        {
+            _modelActivator = modelActivator;
+        }
+
+        public virtual Func<PageContext, object> CreateModelFactory(CompiledPageActionDescriptor descriptor)
+        {
+            if (descriptor == null)
+            {
+                throw new ArgumentNullException(nameof(descriptor));
+            }
+
+            if (descriptor.ModelTypeInfo == null)
+            {
+                return null;
+            }
+
+            var modelActivator = _modelActivator.CreateActivator(descriptor);
+            var propertyActivator = PropertyActivator<PageContext>.GetPropertiesToActivate(
+                    descriptor.ModelTypeInfo.AsType(),
+                    typeof(PageContextAttribute),
+                    _createActivateInfo,
+                    includeNonPublic: false);
+
+            return pageContext =>
+            {
+                var model = modelActivator(pageContext);
+                for (var i = 0; i < propertyActivator.Length; i++)
+                {
+                    propertyActivator[i].Activate(model, pageContext);
+                }
+
+                return model;
+            };
+        }
+
+        public virtual Action<PageContext, object> CreateModelDisposer(CompiledPageActionDescriptor descriptor)
+        {
+            if (descriptor == null)
+            {
+                throw new ArgumentNullException(nameof(descriptor));
+            }
+
+            if (descriptor.ModelTypeInfo == null)
+            {
+                return null;
+            }
+
+            return _modelActivator.CreateReleaser(descriptor);
+        }
+
+        private static PropertyActivator<PageContext> CreateActivateInfo(PropertyInfo property) =>
+            new PropertyActivator<PageContext>(property, pageContext => pageContext);
+    }
+}

test/Microsoft.AspNetCore.Mvc.RazorPages.Test/Internal/DefaultPageModelActivatorProviderTest.cs
@@ -1,0 +1,155 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.RazorPages.Internal
+{
+    public class DefaultPageModelActivatorProviderTest
+    {
+        [Fact]
+        public void CreateActivator_ThrowsIfModelTypeInfoOnActionDescriptorIsNull()
+        {
+            // Arrange
+            var activatorProvider = new DefaultPageModelActivatorProvider();
+            var actionDescriptor = new CompiledPageActionDescriptor();
+
+            // Act & Assert
+            ExceptionAssert.ThrowsArgument(
+                () => activatorProvider.CreateActivator(actionDescriptor),
+                "actionDescriptor",
+                "The 'ModelTypeInfo' property of 'actionDescriptor' must not be null.");
+        }
+
+        [Fact]
+        public void CreateActivator_CreatesModelInstance()
+        {
+            // Arrange
+            var activatorProvider = new DefaultPageModelActivatorProvider();
+            var actionDescriptor = new CompiledPageActionDescriptor
+            {
+                ModelTypeInfo = typeof(SimpleModel).GetTypeInfo(),
+            };
+            var serviceCollection = new ServiceCollection();
+            var generator = Mock.Of<IHtmlGenerator>();
+            serviceCollection.AddSingleton(generator);
+            var httpContext = new DefaultHttpContext
+            {
+                RequestServices = serviceCollection.BuildServiceProvider(),
+            };
+            var pageContext = new PageContext
+            {
+                HttpContext = httpContext
+            };
+
+            // Act
+            var activator = activatorProvider.CreateActivator(actionDescriptor);
+            var model = activator(pageContext);
+
+            // Assert
+            var simpleModel = Assert.IsType<SimpleModel>(model);
+            Assert.NotNull(simpleModel);
+        }
+
+        [Fact]
+        public void CreateActivator_TypeActivatesModelType()
+        {
+            // Arrange
+            var activatorProvider = new DefaultPageModelActivatorProvider();
+            var actionDescriptor = new CompiledPageActionDescriptor
+            {
+                ModelTypeInfo = typeof(ModelWithServices).GetTypeInfo(),
+            };
+            var serviceCollection = new ServiceCollection();
+            var generator = Mock.Of<IHtmlGenerator>();
+            serviceCollection.AddSingleton(generator);
+            var httpContext = new DefaultHttpContext
+            {
+                RequestServices = serviceCollection.BuildServiceProvider(),
+            };
+            var pageContext = new PageContext
+            {
+                HttpContext = httpContext
+            };
+
+            // Act
+            var activator = activatorProvider.CreateActivator(actionDescriptor);
+            var model = activator(pageContext);
+
+            // Assert
+            var modelWithServices = Assert.IsType<ModelWithServices>(model);
+            Assert.Same(generator, modelWithServices.Generator);
+        }
+
+        [Theory]
+        [InlineData(typeof(SimpleModel))]
+        [InlineData(typeof(object))]
+        public void CreateReleaser_ReturnsNullForModelsThatDoNotImplementDisposable(Type pageType)
+        {
+            // Arrange
+            var context = new PageContext();
+            var activator = new DefaultPageActivator();
+            var actionDescriptor = new CompiledPageActionDescriptor
+            {
+                PageTypeInfo = pageType.GetTypeInfo(),
+            };
+
+            // Act
+            var releaser = activator.CreateReleaser(actionDescriptor);
+
+            // Assert
+            Assert.Null(releaser);
+        }
+
+        [Fact]
+        public void CreateReleaser_CreatesDelegateThatDisposesDisposableTypes()
+        {
+            // Arrange
+            var context = new PageContext();
+            var activator = new DefaultPageActivator();
+            var actionDescriptor = new CompiledPageActionDescriptor
+            {
+                PageTypeInfo = typeof(DisposableModel).GetTypeInfo(),
+            };
+            var model = new DisposableModel();
+
+            // Act & Assert
+            var releaser = activator.CreateReleaser(actionDescriptor);
+            releaser(context, model);
+
+            // Assert
+            Assert.True(model.Disposed);
+        }
+
+        private class SimpleModel
+        {
+        }
+
+        private class ModelWithServices
+        {
+            public ModelWithServices(IHtmlGenerator generator)
+            {
+                Generator = generator;
+            }
+
+            public IHtmlGenerator Generator { get; }
+        }
+
+        private class DisposableModel : IDisposable
+        {
+            public bool Disposed { get; private set; }
+
+            public void Dispose()
+            {
+                Disposed = true;
+            }
+        }
+    }
+}

test/Microsoft.AspNetCore.Mvc.RazorPages.Test/Internal/DefaultPageModelFactoryProviderTest.cs
@@ -1,0 +1,136 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Reflection;
+using Moq;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.RazorPages.Internal
+{
+    public class DefaultPageModelFactoryProviderTest
+    {
+        [Fact]
+        public void CreateModelFactory_ReturnsNullFactoryIfModelTypeIsNull()
+        {
+            // Arrange
+            var descriptor = new CompiledPageActionDescriptor();
+            var pageContext = new PageContext();
+            var factoryProvider = CreateModelFactoryProvider();
+
+            // Act
+            var factory = factoryProvider.CreateModelFactory(descriptor);
+
+            // Assert
+            Assert.Null(factory);
+        }
+
+        [Fact]
+        public void CreateModelDisposer_ReturnsNullFactoryIfModelTypeIsNull()
+        {
+            // Arrange
+            var descriptor = new CompiledPageActionDescriptor();
+            var pageContext = new PageContext();
+            var factoryProvider = CreateModelFactoryProvider();
+
+            // Act
+            var disposer = factoryProvider.CreateModelDisposer(descriptor);
+
+            // Assert
+            Assert.Null(disposer);
+        }
+
+        [Fact]
+        public void ModelFactory_InitializesModelInstances()
+        {
+            // Arrange
+            var descriptor = new CompiledPageActionDescriptor
+            {
+                ModelTypeInfo = typeof(SimpleModel).GetTypeInfo(),
+            };
+            var pageContext = new PageContext();
+            var factoryProvider = CreateModelFactoryProvider();
+
+            // Act
+            var factory = factoryProvider.CreateModelFactory(descriptor);
+            var instance = factory(pageContext);
+
+            // Assert
+            var model = Assert.IsType<SimpleModel>(instance);
+            Assert.NotNull(model);
+        }
+
+        [Fact]
+        public void ModelFactory_InjectsPropertiesWithPageContextAttribute()
+        {
+            // Arrange
+            var descriptor = new CompiledPageActionDescriptor
+            {
+                ModelTypeInfo = typeof(ModelWithPageContext).GetTypeInfo(),
+            };
+            var pageContext = new PageContext();
+            var factoryProvider = CreateModelFactoryProvider();
+
+            // Act
+            var factory = factoryProvider.CreateModelFactory(descriptor);
+            var instance = factory(pageContext);
+
+            // Assert
+            var testModel = Assert.IsType<ModelWithPageContext>(instance);
+            Assert.Same(pageContext, testModel.ContextWithAttribute);
+            Assert.Null(testModel.ContextWithoutAttribute);
+        }
+
+        [Fact]
+        public void CreateModelDisposer_ReturnsDisposerFromModelActivatorProvider()
+        {
+            // Arrange
+            var descriptor = new CompiledPageActionDescriptor
+            {
+                ModelTypeInfo = typeof(SimpleModel).GetTypeInfo()
+            };
+            var pageContext = new PageContext();
+            var modelActivatorProvider = new Mock<IPageModelActivatorProvider>();
+            Action<PageContext, object> disposer = (_, __) => { };
+            modelActivatorProvider.Setup(p => p.CreateReleaser(descriptor))
+                .Returns(disposer);
+            var factoryProvider = CreateModelFactoryProvider(modelActivatorProvider.Object);
+
+            // Act
+            var actual = factoryProvider.CreateModelDisposer(descriptor);
+
+            // Assert
+            Assert.Same(disposer, actual);
+        }
+
+        private static DefaultPageModelFactoryProvider CreateModelFactoryProvider(
+            IPageModelActivatorProvider modelActivator = null)
+        {
+            if (modelActivator == null)
+            {
+                var mockActivator = new Mock<IPageModelActivatorProvider>();
+                mockActivator.Setup(a => a.CreateActivator(It.IsAny<CompiledPageActionDescriptor>()))
+                    .Returns((CompiledPageActionDescriptor descriptor) =>
+                    {
+                        return (context) => Activator.CreateInstance(descriptor.ModelTypeInfo.AsType());
+                    });
+
+                modelActivator = mockActivator.Object;
+            }
+
+            return new DefaultPageModelFactoryProvider(modelActivator);
+        }
+
+        private class SimpleModel
+        {
+        }
+
+        private class ModelWithPageContext
+        {
+            [PageContext]
+            public PageContext ContextWithAttribute { get; set; }
+
+            public PageContext ContextWithoutAttribute { get; set; }
+        }
+    }
+}

dotnet/aspnetcore

src/Microsoft.AspNetCore.Mvc.RazorPages/Infrastructure/DefaultPageModelFactoryProvider.cs
@@ -1,0 +1,70 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Reflection;
+using Microsoft.Extensions.Internal;
+
+namespace Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure
+{
+    public class DefaultPageModelFactoryProvider : IPageModelFactoryProvider
+    {
+        private static readonly Func<PropertyInfo, PropertyActivator<PageContext>> _createActivateInfo =
+            CreateActivateInfo;
+        private readonly IPageModelActivatorProvider _modelActivator;
+
+        public DefaultPageModelFactoryProvider(IPageModelActivatorProvider modelActivator)
+        {
+            _modelActivator = modelActivator;
+        }
+
+        public virtual Func<PageContext, object> CreateModelFactory(CompiledPageActionDescriptor descriptor)
+        {
+            if (descriptor == null)
+            {
+                throw new ArgumentNullException(nameof(descriptor));
+            }
+
+            if (descriptor.ModelTypeInfo == null)
+            {
+                return null;
+            }
+
+            var modelActivator = _modelActivator.CreateActivator(descriptor);
+            var propertyActivator = PropertyActivator<PageContext>.GetPropertiesToActivate(
+                    descriptor.ModelTypeInfo.AsType(),
+                    typeof(PageContextAttribute),
+                    _createActivateInfo,
+                    includeNonPublic: false);
+
+            return pageContext =>
+            {
+                var model = modelActivator(pageContext);
+                for (var i = 0; i < propertyActivator.Length; i++)
+                {
+                    propertyActivator[i].Activate(model, pageContext);
+                }
+
+                return model;
+            };
+        }
+
+        public virtual Action<PageContext, object> CreateModelDisposer(CompiledPageActionDescriptor descriptor)
+        {
+            if (descriptor == null)
+            {
+                throw new ArgumentNullException(nameof(descriptor));
+            }
+
+            if (descriptor.ModelTypeInfo == null)
+            {
+                return null;
+            }
+
+            return _modelActivator.CreateReleaser(descriptor);
+        }
+
+        private static PropertyActivator<PageContext> CreateActivateInfo(PropertyInfo property) =>
+            new PropertyActivator<PageContext>(property, pageContext => pageContext);
+    }
+}

test/Microsoft.AspNetCore.Mvc.RazorPages.Test/Infrastructure/DefaultPageModelActivatorProviderTest.cs
@@ -1,0 +1,155 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure
+{
+    public class DefaultPageModelActivatorProviderTest
+    {
+        [Fact]
+        public void CreateActivator_ThrowsIfModelTypeInfoOnActionDescriptorIsNull()
+        {
+            // Arrange
+            var activatorProvider = new DefaultPageModelActivatorProvider();
+            var actionDescriptor = new CompiledPageActionDescriptor();
+
+            // Act & Assert
+            ExceptionAssert.ThrowsArgument(
+                () => activatorProvider.CreateActivator(actionDescriptor),
+                "actionDescriptor",
+                "The 'ModelTypeInfo' property of 'actionDescriptor' must not be null.");
+        }
+
+        [Fact]
+        public void CreateActivator_CreatesModelInstance()
+        {
+            // Arrange
+            var activatorProvider = new DefaultPageModelActivatorProvider();
+            var actionDescriptor = new CompiledPageActionDescriptor
+            {
+                ModelTypeInfo = typeof(SimpleModel).GetTypeInfo(),
+            };
+            var serviceCollection = new ServiceCollection();
+            var generator = Mock.Of<IHtmlGenerator>();
+            serviceCollection.AddSingleton(generator);
+            var httpContext = new DefaultHttpContext
+            {
+                RequestServices = serviceCollection.BuildServiceProvider(),
+            };
+            var pageContext = new PageContext
+            {
+                HttpContext = httpContext
+            };
+
+            // Act
+            var activator = activatorProvider.CreateActivator(actionDescriptor);
+            var model = activator(pageContext);
+
+            // Assert
+            var simpleModel = Assert.IsType<SimpleModel>(model);
+            Assert.NotNull(simpleModel);
+        }
+
+        [Fact]
+        public void CreateActivator_TypeActivatesModelType()
+        {
+            // Arrange
+            var activatorProvider = new DefaultPageModelActivatorProvider();
+            var actionDescriptor = new CompiledPageActionDescriptor
+            {
+                ModelTypeInfo = typeof(ModelWithServices).GetTypeInfo(),
+            };
+            var serviceCollection = new ServiceCollection();
+            var generator = Mock.Of<IHtmlGenerator>();
+            serviceCollection.AddSingleton(generator);
+            var httpContext = new DefaultHttpContext
+            {
+                RequestServices = serviceCollection.BuildServiceProvider(),
+            };
+            var pageContext = new PageContext
+            {
+                HttpContext = httpContext
+            };
+
+            // Act
+            var activator = activatorProvider.CreateActivator(actionDescriptor);
+            var model = activator(pageContext);
+
+            // Assert
+            var modelWithServices = Assert.IsType<ModelWithServices>(model);
+            Assert.Same(generator, modelWithServices.Generator);
+        }
+
+        [Theory]
+        [InlineData(typeof(SimpleModel))]
+        [InlineData(typeof(object))]
+        public void CreateReleaser_ReturnsNullForModelsThatDoNotImplementDisposable(Type pageType)
+        {
+            // Arrange
+            var context = new PageContext();
+            var activator = new DefaultPageActivator();
+            var actionDescriptor = new CompiledPageActionDescriptor
+            {
+                PageTypeInfo = pageType.GetTypeInfo(),
+            };
+
+            // Act
+            var releaser = activator.CreateReleaser(actionDescriptor);
+
+            // Assert
+            Assert.Null(releaser);
+        }
+
+        [Fact]
+        public void CreateReleaser_CreatesDelegateThatDisposesDisposableTypes()
+        {
+            // Arrange
+            var context = new PageContext();
+            var activator = new DefaultPageActivator();
+            var actionDescriptor = new CompiledPageActionDescriptor
+            {
+                PageTypeInfo = typeof(DisposableModel).GetTypeInfo(),
+            };
+            var model = new DisposableModel();
+
+            // Act & Assert
+            var releaser = activator.CreateReleaser(actionDescriptor);
+            releaser(context, model);
+
+            // Assert
+            Assert.True(model.Disposed);
+        }
+
+        private class SimpleModel
+        {
+        }
+
+        private class ModelWithServices
+        {
+            public ModelWithServices(IHtmlGenerator generator)
+            {
+                Generator = generator;
+            }
+
+            public IHtmlGenerator Generator { get; }
+        }
+
+        private class DisposableModel : IDisposable
+        {
+            public bool Disposed { get; private set; }
+
+            public void Dispose()
+            {
+                Disposed = true;
+            }
+        }
+    }
+}

test/Microsoft.AspNetCore.Mvc.RazorPages.Test/Infrastructure/DefaultPageModelFactoryProviderTest.cs
@@ -1,0 +1,136 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Reflection;
+using Moq;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure
+{
+    public class DefaultPageModelFactoryProviderTest
+    {
+        [Fact]
+        public void CreateModelFactory_ReturnsNullFactoryIfModelTypeIsNull()
+        {
+            // Arrange
+            var descriptor = new CompiledPageActionDescriptor();
+            var pageContext = new PageContext();
+            var factoryProvider = CreateModelFactoryProvider();
+
+            // Act
+            var factory = factoryProvider.CreateModelFactory(descriptor);
+
+            // Assert
+            Assert.Null(factory);
+        }
+
+        [Fact]
+        public void CreateModelDisposer_ReturnsNullFactoryIfModelTypeIsNull()
+        {
+            // Arrange
+            var descriptor = new CompiledPageActionDescriptor();
+            var pageContext = new PageContext();
+            var factoryProvider = CreateModelFactoryProvider();
+
+            // Act
+            var disposer = factoryProvider.CreateModelDisposer(descriptor);
+
+            // Assert
+            Assert.Null(disposer);
+        }
+
+        [Fact]
+        public void ModelFactory_InitializesModelInstances()
+        {
+            // Arrange
+            var descriptor = new CompiledPageActionDescriptor
+            {
+                ModelTypeInfo = typeof(SimpleModel).GetTypeInfo(),
+            };
+            var pageContext = new PageContext();
+            var factoryProvider = CreateModelFactoryProvider();
+
+            // Act
+            var factory = factoryProvider.CreateModelFactory(descriptor);
+            var instance = factory(pageContext);
+
+            // Assert
+            var model = Assert.IsType<SimpleModel>(instance);
+            Assert.NotNull(model);
+        }
+
+        [Fact]
+        public void ModelFactory_InjectsPropertiesWithPageContextAttribute()
+        {
+            // Arrange
+            var descriptor = new CompiledPageActionDescriptor
+            {
+                ModelTypeInfo = typeof(ModelWithPageContext).GetTypeInfo(),
+            };
+            var pageContext = new PageContext();
+            var factoryProvider = CreateModelFactoryProvider();
+
+            // Act
+            var factory = factoryProvider.CreateModelFactory(descriptor);
+            var instance = factory(pageContext);
+
+            // Assert
+            var testModel = Assert.IsType<ModelWithPageContext>(instance);
+            Assert.Same(pageContext, testModel.ContextWithAttribute);
+            Assert.Null(testModel.ContextWithoutAttribute);
+        }
+
+        [Fact]
+        public void CreateModelDisposer_ReturnsDisposerFromModelActivatorProvider()
+        {
+            // Arrange
+            var descriptor = new CompiledPageActionDescriptor
+            {
+                ModelTypeInfo = typeof(SimpleModel).GetTypeInfo()
+            };
+            var pageContext = new PageContext();
+            var modelActivatorProvider = new Mock<IPageModelActivatorProvider>();
+            Action<PageContext, object> disposer = (_, __) => { };
+            modelActivatorProvider.Setup(p => p.CreateReleaser(descriptor))
+                .Returns(disposer);
+            var factoryProvider = CreateModelFactoryProvider(modelActivatorProvider.Object);
+
+            // Act
+            var actual = factoryProvider.CreateModelDisposer(descriptor);
+
+            // Assert
+            Assert.Same(disposer, actual);
+        }
+
+        private static DefaultPageModelFactoryProvider CreateModelFactoryProvider(
+            IPageModelActivatorProvider modelActivator = null)
+        {
+            if (modelActivator == null)
+            {
+                var mockActivator = new Mock<IPageModelActivatorProvider>();
+                mockActivator.Setup(a => a.CreateActivator(It.IsAny<CompiledPageActionDescriptor>()))
+                    .Returns((CompiledPageActionDescriptor descriptor) =>
+                    {
+                        return (context) => Activator.CreateInstance(descriptor.ModelTypeInfo.AsType());
+                    });
+
+                modelActivator = mockActivator.Object;
+            }
+
+            return new DefaultPageModelFactoryProvider(modelActivator);
+        }
+
+        private class SimpleModel
+        {
+        }
+
+        private class ModelWithPageContext
+        {
+            [PageContext]
+            public PageContext ContextWithAttribute { get; set; }
+
+            public PageContext ContextWithoutAttribute { get; set; }
+        }
+    }
+}