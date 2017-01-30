e7785dff01c19ed6bfdbef3981229dc664b5b265
SteveSandersonMS <stevesa@microsoft.com>
2017-01-30 13:01:00 +0000

    Update templates to match VS2017RC build 26127 project file format, plus latest ASP.NET Core 'Program.cs' convention. Fixes #620.

--- templates/KnockoutSpa/Program.cs
+++ templates/KnockoutSpa/Program.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 
 namespace WebApplicationBasic
 {
@@ -12,13 +11,7 @@ namespace WebApplicationBasic
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-                .Build();
-
             var host = new WebHostBuilder()
-                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
