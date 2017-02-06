fea5d5cfdc8fc9413ba7816cc108f4d893ac16e1
Ben Adams <thundercat@illyriad.co.uk>
2017-02-06 22:36:42 +0000

    Truncate SystemClock to Seconds Precision (#1110)

--- src/Microsoft.AspNetCore.Authentication/SystemClock.cs
+++ src/Microsoft.AspNetCore.Authentication/SystemClock.cs
@@ -7,7 +7,7 @@ using System;
 namespace Microsoft.AspNetCore.Authentication
 {
     /// <summary>
-    /// Provides access to the normal system clock.
+    /// Provides access to the normal system clock with precision in seconds.
     /// </summary>
     public class SystemClock : ISystemClock
     {
@@ -20,8 +20,8 @@ namespace Microsoft.AspNetCore.Authentication
             {
                 // the clock measures whole seconds only, to have integral expires_in results, and
                 // because milliseconds do not round-trip serialization formats
-                DateTimeOffset utcNow = DateTimeOffset.UtcNow;
-                return utcNow.AddMilliseconds(-utcNow.Millisecond);
+                var utcNowPrecisionSeconds = new DateTime((DateTime.UtcNow.Ticks / TimeSpan.TicksPerSecond) * TimeSpan.TicksPerSecond, DateTimeKind.Utc);
+                return new DateTimeOffset(utcNowPrecisionSeconds);
             }
         }
     }
