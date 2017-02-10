3c14472bf20ced77138e528ae6a9e2666379b559
Roderic Bos <git@rooc.nl>
2017-02-10 10:24:37 +0100

    Updated common.targets to use AssemblyName instead of MSBuildProjectName so renaming output assembly from csproj works
    Fixes https://github.com/aspnet/Mvc/issues/5780

---
+++ test/Microsoft.AspNetCore.Mvc.Razor.ViewCompilation.FunctionalTests/SimpleAppWithAssemblyRenameTest.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Server.IntegrationTesting;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.Razor.ViewCompilation
+{
+    public class SimpleAppWithAssemblyRenameTest : IClassFixture<SimpleAppWithAssemblyRenameTest.SimpleAppWithAssemblyRenameTestFixture>
+    {
+        public SimpleAppWithAssemblyRenameTest(SimpleAppWithAssemblyRenameTestFixture fixture)
+        {
+            Fixture = fixture;
+        }
+
+        public ApplicationTestFixture Fixture { get; }
+
+        public static TheoryData SupportedFlavorsTheoryData => RuntimeFlavors.SupportedFlavorsTheoryData;
+
+        [Theory]
+        [MemberData(nameof(SupportedFlavorsTheoryData))]
+        public async Task Precompilation_WorksForSimpleApps(RuntimeFlavor flavor)
+        {
+            // Arrange
+            using (var deployer = Fixture.CreateDeployment(flavor))
+            {
+                var deploymentResult = deployer.Deploy();
+
+                // Act
+                var response = await Fixture.HttpClient.GetStringWithRetryAsync(
+                    deploymentResult.ApplicationBaseUri,
+                    Fixture.Logger);
+
+                // Assert
+                TestEmbeddedResource.AssertContent("SimpleAppWithAssemblyRenameTest.Home.Index.txt", response);
+            }
+        }
+
+        public class SimpleAppWithAssemblyRenameTestFixture : ApplicationTestFixture
+        {
+            public SimpleAppWithAssemblyRenameTestFixture()
+                : base("SimpleAppWithAssemblyRename")
+            {
+            }
+
+            public override DeploymentParameters GetDeploymentParameters(RuntimeFlavor flavor)
+            {
+                var parameters = base.GetDeploymentParameters(flavor);
+                parameters.ApplicationName = "NewAssemblyName";
+                return parameters;
+            }
+        }
+    }
+}
---
+++ testapps/SimpleAppWithAssemblyRename/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleAppWithAssemblyRename.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
