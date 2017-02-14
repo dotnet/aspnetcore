0527786686a63afd193d9a896041161bcec388fe
Doug Bunting <dougbu@microsoft.com>
2017-02-14 13:19:30 -0800

    Bump test projects up to .NET 4.5.2 - aspnet/Testing#248 - xUnit no longer supports .NET 4.5.1 - build tests for desktop .NET only on Windows

--- test/ServerComparison.FunctionalTests/NtlmAuthenticationTest.cs
+++ test/ServerComparison.FunctionalTests/NtlmAuthenticationTest.cs
@@ -1,6 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-#if NET451
+#if NET452
 
 using System;
 using System.Net;
