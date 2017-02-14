76bd114a2f0776a157cce83f4f2d9af45e28c64b
moozzyk <pawelka@microsoft.com>
2017-02-14 15:15:31 -0800

    Preventing from building ts client multiple times

--- test/Microsoft.AspNetCore.SignalR.Test.Server/gulpfile.js
+++
@@ -1,6 +0,0 @@
-const gulp = require('gulp');
-
-gulp.task('copy-jasmine', () => {
-  gulp.src(['../../node_modules/jasmine-core/lib/jasmine-core/*.js', '../../node_modules/jasmine-core/lib/jasmine-core/*.css'])
-    .pipe(gulp.dest('./wwwroot/lib/jasmine/'));
-});
