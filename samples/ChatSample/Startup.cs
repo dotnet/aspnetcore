b1dafc2f34d267306997a62ab91d5018f871de3a
Pawel Kadluczka <moozzyk@users.noreply.github.com>
2017-02-15 16:32:49 -0800

    Fixing build warning (#208)
    13:40:19.336     1>Startup.cs(30,17): warning CS0618: 'ConfigurationExtensions.AddUserSecrets(IConfigurationBuilder)' is obsolete: 'This method is obsolete and will be removed in a future version. The recommended alternative is .AddUserSecrets(string userSecretsId) or .AddUserSecrets<TStartup>().' [C:\Source\SignalR-Core\samples\ChatSample\ChatSample.csproj] [C:\Source\SignalR-Core\.build\makefile.proj]

--- samples/ChatSample/Startup.cs
+++ samples/ChatSample/Startup.cs
@@ -27,7 +27,7 @@ namespace ChatSample
             if (env.IsDevelopment())
             {
                 // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
-                builder.AddUserSecrets();
+                builder.AddUserSecrets<Startup>();
             }
 
             builder.AddEnvironmentVariables();
