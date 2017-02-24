af5e7da4786a41d3c0179f1d0be137e60b349b15
Nate McMaster <nate.mcmaster@microsoft.com>
2017-02-23 16:07:46 -0800

    Remove workarounds and unify dependency versions

--- test/Microsoft.AspNetCore.SignalR.Tests/ServerFixture.cs
+++ test/Microsoft.AspNetCore.SignalR.Tests/ServerFixture.cs
@@ -2,14 +2,13 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Builder;
-using Xunit;
-using System.Diagnostics;
 
 namespace Microsoft.AspNetCore.SignalR.Tests
 {
