3772c7c343168ce62f9860534259495a8814d756
Pranav K <prkrishn@hotmail.com>
2017-02-27 11:59:51 -0800

    Downgrade versions of new packages to 1.0.0

--- tooling/Microsoft.VisualStudio.RazorExtension/Properties/BindingRedirectAttributes.cs
+++ tooling/Microsoft.VisualStudio.RazorExtension/Properties/BindingRedirectAttributes.cs
@@ -16,29 +16,29 @@ using Microsoft.VisualStudio.Shell;
     GenerateCodeBase = true,
     PublicKeyToken = "adb9793829ddae60",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "1.2.0.0",
-    NewVersion = "1.2.0.0")]
+    OldVersionUpperBound = "1.0.0.0",
+    NewVersion = "1.0.0.0")]
 
 [assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.CodeAnalysis.Razor.Workspaces",
     GenerateCodeBase = true,
     PublicKeyToken = "adb9793829ddae60",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "1.2.0.0",
-    NewVersion = "1.2.0.0")]
+    OldVersionUpperBound = "1.0.0.0",
+    NewVersion = "1.0.0.0")]
 
 [assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.CodeAnalysis.Remote.Razor",
     GenerateCodeBase = true,
     PublicKeyToken = "adb9793829ddae60",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "1.2.0.0",
-    NewVersion = "1.2.0.0")]
+    OldVersionUpperBound = "1.0.0.0",
+    NewVersion = "1.0.0.0")]
 
 [assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.VisualStudio.LanguageServices.Razor",
     GenerateCodeBase = true,
     PublicKeyToken = "adb9793829ddae60",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "1.2.0.0",
-    NewVersion = "1.2.0.0")]
+    OldVersionUpperBound = "1.0.0.0",
+    NewVersion = "1.0.0.0")]
