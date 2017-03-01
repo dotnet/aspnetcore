0162c19ccf682f270b274a77b87f2a9f54c164d3
Pawel Kadluczka <moozzyk@users.noreply.github.com>
2017-03-01 15:13:22 -0800

    MSBuildifying gulpified node (#237)
    - Creating an MSBuild project for the TS client
    - Adding project references to the TS client project from projects that need the client - (ensures the correct targets dependency graph and prevents building the client multiple times and related races)
    - Removing gulp tasks from individual projects (allows containing npm only in the TS client source and node tests)
    - Using incremental compilation to build the TS client only when inputs change (prevents building the client multiple times or when not needed at all)
    - Removing `npm install` from all the projects (takes up to 10 seconds even if there is nothing to restore) - npm packages will still be installed when running full build (if needed) or need to be installed manually

--- src/Microsoft.AspNetCore.SignalR.Client.TS/gulpfile.js
+++ src/Microsoft.AspNetCore.SignalR.Client.TS/gulpfile.js
@@ -3,7 +3,6 @@ const browserify = require('browserify');
 const ts = require('gulp-typescript');
 const source = require('vinyl-source-stream');
 const del = require('del');
-const argv = require('yargs').argv;
 
 const tsProject = ts.createProject('./tsconfig.json');
 const clientOutDir = tsProject.options.outDir;
@@ -26,15 +25,5 @@ gulp.task('browserify-client', ['compile-ts-client'], () => {
 });
 
 gulp.task('build-ts-client', ['clean', 'compile-ts-client', 'browserify-client']);
-
-gulp.task('bundle-client', ['build-ts-client'], () => {
-    if (!argv.bundleOutDir) {
-        console.log('Use \'--bundleOutDir\' option to specify the target file for the bundled client.');
-    }
-    else {
-        return gulp.src(clientOutDir + '/../browser/signalr-client.js')
-            .pipe(gulp.dest(argv.bundleOutDir));
-    }
-});
 
 gulp.task('default', ['build-ts-client']);
