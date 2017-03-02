0404bcc58c6de8740b16e393de325bf982eefaa4
Cesar Blum Silveira <cesars@microsoft.com>
2017-03-01 17:29:13 -0800

    Add more microbenchmarks.

---
+++ test/Microsoft.AspNetCore.Server.Kestrel.Performance/KestrelHttpParser.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO.Pipelines;
+using BenchmarkDotNet.Attributes;
+using Microsoft.AspNetCore.Server.Kestrel.Internal.Http;
+
+namespace Microsoft.AspNetCore.Server.Kestrel.Performance
+{
+    [Config(typeof(CoreConfig))]
+
+    public class KestrelHttpParser : IHttpRequestLineHandler, IHttpHeadersHandler
+    {
+        private readonly Internal.Http.KestrelHttpParser _parser = new Internal.Http.KestrelHttpParser(log: null);
+
+        private ReadableBuffer _buffer;
+
+        [Benchmark(Baseline = true, OperationsPerInvoke = RequestParsingData.InnerLoopCount)]
+        public void PlaintextTechEmpower()
+        {
+            for (var i = 0; i < RequestParsingData.InnerLoopCount; i++)
+            {
+                InsertData(RequestParsingData.PlaintextTechEmpowerRequest);
+                ParseData();
+            }
+        }
+
+        [Benchmark(OperationsPerInvoke = RequestParsingData.InnerLoopCount)]
+        public void LiveAspNet()
+        {
+            for (var i = 0; i < RequestParsingData.InnerLoopCount; i++)
+            {
+                InsertData(RequestParsingData.LiveaspnetRequest);
+                ParseData();
+            }
+        }
+
+        [Benchmark(OperationsPerInvoke = RequestParsingData.InnerLoopCount)]
+        public void Unicode()
+        {
+            for (var i = 0; i < RequestParsingData.InnerLoopCount; i++)
+            {
+                InsertData(RequestParsingData.UnicodeRequest);
+                ParseData();
+            }
+        }
+
+        private void InsertData(byte[] data)
+        {
+            _buffer = ReadableBuffer.Create(data);
+        }
+
+        private void ParseData()
+        {
+            if (!_parser.ParseRequestLine(this, _buffer, out var consumed, out var examined))
+            {
+                RequestParsing.ThrowInvalidRequestHeaders();
+            }
+
+            _buffer = _buffer.Slice(consumed, _buffer.End);
+
+            if (!_parser.ParseHeaders(this, _buffer, out consumed, out examined, out var consumedBytes))
+            {
+                RequestParsing.ThrowInvalidRequestHeaders();
+            }
+        }
+
+        public void OnStartLine(HttpMethod method, HttpVersion version, Span<byte> target, Span<byte> path, Span<byte> query, Span<byte> customMethod)
+        {
+        }
+
+        public void OnHeader(Span<byte> name, Span<byte> value)
+        {
+        }
+    }
+}
--- test/Microsoft.AspNetCore.Server.Kestrel.Performance/RequestParsing.cs
+++ test/Microsoft.AspNetCore.Server.Kestrel.Performance/RequestParsing.cs
@@ -3,8 +3,6 @@
 
 using System;
 using System.IO.Pipelines;
-using System.Linq;
-using System.Text;
 using BenchmarkDotNet.Attributes;
 using Microsoft.AspNetCore.Server.Kestrel.Internal.Http;
 using Microsoft.AspNetCore.Testing;
@@ -14,107 +12,82 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Performance
     [Config(typeof(CoreConfig))]
     public class RequestParsing
     {
-        private const int InnerLoopCount = 512;
-        private const int Pipelining = 16;
-
-        private const string plaintextTechEmpower = "GET /plaintext HTTP/1.1\r\n" +
-            "Host: localhost\r\n" +
-            "Accept: text/plain,text/html;q=0.9,application/xhtml+xml;q=0.9,application/xml;q=0.8,*/*;q=0.7\r\n" +
-            "Connection: keep-alive\r\n\r\n";
-
-        private const string liveaspnetRequest = "GET https://live.asp.net/ HTTP/1.1\r\n" +
-            "Host: live.asp.net\r\n" +
-            "Connection: keep-alive\r\n" +
-            "Upgrade-Insecure-Requests: 1\r\n" +
-            "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36\r\n" +
-            "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n" +
-            "DNT: 1\r\n" +
-            "Accept-Encoding: gzip, deflate, sdch, br\r\n" +
-            "Accept-Language: en-US,en;q=0.8\r\n" +
-            "Cookie: __unam=7a67379-1s65dc575c4-6d778abe-1; omniID=9519gfde_3347_4762_8762_df51458c8ec2\r\n\r\n";
-
-        private const string unicodeRequest =
-            "GET http://stackoverflow.com/questions/40148683/why-is-%e0%a5%a7%e0%a5%a8%e0%a5%a9-numeric HTTP/1.1\r\n" +
-            "Accept: text/html, application/xhtml+xml, image/jxr, */*\r\n" +
-            "Accept-Language: en-US,en-GB;q=0.7,en;q=0.3\r\n" +
-            "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.14965\r\n" +
-            "Accept-Encoding: gzip, deflate\r\n" +
-            "Host: stackoverflow.com\r\n" +
-            "Connection: Keep-Alive\r\n" +
-            "Cache-Control: max-age=0\r\n" +
-            "Upgrade-Insecure-Requests: 1\r\n" +
-            "DNT: 1\r\n" +
-            "Referer: http://stackoverflow.com/?tab=month\r\n" +
-            "Pragma: no-cache\r\n" +
-            "Cookie: prov=20629ccd-8b0f-e8ef-2935-cd26609fc0bc; __qca=P0-1591065732-1479167353442; _ga=GA1.2.1298898376.1479167354; _gat=1; sgt=id=9519gfde_3347_4762_8762_df51458c8ec2; acct=t=why-is-%e0%a5%a7%e0%a5%a8%e0%a5%a9-numeric&s=why-is-%e0%a5%a7%e0%a5%a8%e0%a5%a9-numeric\r\n\r\n";
-
-        private static readonly byte[] _plaintextTechEmpowerPipelinedRequests = Encoding.ASCII.GetBytes(string.Concat(Enumerable.Repeat(plaintextTechEmpower, Pipelining)));
-        private static readonly byte[] _plaintextTechEmpower = Encoding.ASCII.GetBytes(plaintextTechEmpower);
-        
-        private static readonly byte[] _liveaspnentPipelinedRequests = Encoding.ASCII.GetBytes(string.Concat(Enumerable.Repeat(liveaspnetRequest, Pipelining)));
-        private static readonly byte[] _liveaspnentRequest = Encoding.ASCII.GetBytes(liveaspnetRequest);
-
-        private static readonly byte[] _unicodePipelinedRequests = Encoding.ASCII.GetBytes(string.Concat(Enumerable.Repeat(unicodeRequest, Pipelining)));
-        private static readonly byte[] _unicodeRequest = Encoding.ASCII.GetBytes(unicodeRequest);
-
-        [Params(typeof(KestrelHttpParser))]
+        [Params(typeof(Internal.Http.KestrelHttpParser))]
         public Type ParserType { get; set; }
 
-        [Benchmark(Baseline = true, OperationsPerInvoke = InnerLoopCount)]
-        public void ParsePlaintextTechEmpower()
+        public IPipe Pipe { get; set; }
+
+        public Frame<object> Frame { get; set; }
+
+        public PipeFactory PipelineFactory { get; set; }
+
+        [Setup]
+        public void Setup()
         {
-            for (var i = 0; i < InnerLoopCount; i++) {
-                InsertData(_plaintextTechEmpower);
+            var connectionContext = new MockConnection(new KestrelServerOptions());
+            connectionContext.ListenerContext.ServiceContext.HttpParserFactory = frame => (IHttpParser)Activator.CreateInstance(ParserType, frame.ConnectionContext.ListenerContext.ServiceContext.Log);
+
+            Frame = new Frame<object>(application: null, context: connectionContext);
+            PipelineFactory = new PipeFactory();
+            Pipe = PipelineFactory.Create();
+        }
+
+        [Benchmark(Baseline = true, OperationsPerInvoke = RequestParsingData.InnerLoopCount)]
+        public void PlaintextTechEmpower()
+        {
+            for (var i = 0; i < RequestParsingData.InnerLoopCount; i++)
+            {
+                InsertData(RequestParsingData.PlaintextTechEmpowerRequest);
                 ParseData();
             }
         }
 
-        [Benchmark(OperationsPerInvoke = InnerLoopCount * Pipelining)]
-        public void ParsePipelinedPlaintextTechEmpower()
+        [Benchmark(OperationsPerInvoke = RequestParsingData.InnerLoopCount * RequestParsingData.Pipelining)]
+        public void PipelinedPlaintextTechEmpower()
         {
-            for (var i = 0; i < InnerLoopCount; i++)
+            for (var i = 0; i < RequestParsingData.InnerLoopCount; i++)
             {
-                InsertData(_plaintextTechEmpowerPipelinedRequests);
+                InsertData(RequestParsingData.PlaintextTechEmpowerPipelinedRequests);
                 ParseData();
             }
         }
 
-        [Benchmark(OperationsPerInvoke = InnerLoopCount)]
-        public void ParseLiveAspNet()
+        [Benchmark(OperationsPerInvoke = RequestParsingData.InnerLoopCount)]
+        public void LiveAspNet()
         {
-            for (var i = 0; i < InnerLoopCount; i++)
+            for (var i = 0; i < RequestParsingData.InnerLoopCount; i++)
             {
-                InsertData(_liveaspnentRequest);
+                InsertData(RequestParsingData.LiveaspnetRequest);
                 ParseData();
             }
         }
 
-        [Benchmark(OperationsPerInvoke = InnerLoopCount * Pipelining)]
-        public void ParsePipelinedLiveAspNet()
+        [Benchmark(OperationsPerInvoke = RequestParsingData.InnerLoopCount * RequestParsingData.Pipelining)]
+        public void PipelinedLiveAspNet()
         {
-            for (var i = 0; i < InnerLoopCount; i++)
+            for (var i = 0; i < RequestParsingData.InnerLoopCount; i++)
             {
-                InsertData(_liveaspnentPipelinedRequests);
+                InsertData(RequestParsingData.LiveaspnetPipelinedRequests);
                 ParseData();
             }
         }
 
-        [Benchmark(OperationsPerInvoke = InnerLoopCount)]
-        public void ParseUnicode()
+        [Benchmark(OperationsPerInvoke = RequestParsingData.InnerLoopCount)]
+        public void Unicode()
         {
-            for (var i = 0; i < InnerLoopCount; i++)
+            for (var i = 0; i < RequestParsingData.InnerLoopCount; i++)
             {
-                InsertData(_unicodeRequest);
+                InsertData(RequestParsingData.UnicodeRequest);
                 ParseData();
             }
         }
 
-        [Benchmark(OperationsPerInvoke = InnerLoopCount * Pipelining)]
-        public void ParseUnicodePipelined()
+        [Benchmark(OperationsPerInvoke = RequestParsingData.InnerLoopCount * RequestParsingData.Pipelining)]
+        public void UnicodePipelined()
         {
-            for (var i = 0; i < InnerLoopCount; i++)
+            for (var i = 0; i < RequestParsingData.InnerLoopCount; i++)
             {
-                InsertData(_unicodePipelinedRequests);
+                InsertData(RequestParsingData.UnicodePipelinedRequests);
                 ParseData();
             }
         }
@@ -145,7 +118,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Performance
 
                 if (!Frame.TakeStartLine(readableBuffer, out var consumed, out var examined))
                 {
-                    ThrowInvalidStartLine();
+                    ThrowInvalidRequestLine();
                 }
                 Pipe.Reader.Advance(consumed, examined);
 
@@ -156,38 +129,21 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Performance
 
                 if (!Frame.TakeMessageHeaders(readableBuffer, out consumed, out examined))
                 {
-                    ThrowInvalidMessageHeaders();
+                    ThrowInvalidRequestHeaders();
                 }
                 Pipe.Reader.Advance(consumed, examined);
             }
             while (true);
         }
 
-        private void ThrowInvalidStartLine()
+        public static void ThrowInvalidRequestLine()
         {
-            throw new InvalidOperationException("Invalid StartLine");
+            throw new InvalidOperationException("Invalid request line");
         }
 
-        private void ThrowInvalidMessageHeaders()
+        public static void ThrowInvalidRequestHeaders()
         {
-            throw new InvalidOperationException("Invalid MessageHeaders");
+            throw new InvalidOperationException("Invalid request headers");
         }
-
-        [Setup]
-        public void Setup()
-        {
-            var connectionContext = new MockConnection(new KestrelServerOptions());
-            connectionContext.ListenerContext.ServiceContext.HttpParserFactory = frame => (IHttpParser)Activator.CreateInstance(ParserType, frame.ConnectionContext.ListenerContext.ServiceContext.Log);
-
-            Frame = new Frame<object>(application: null, context: connectionContext);
-            PipelineFactory = new PipeFactory();
-            Pipe = PipelineFactory.Create();
-        }
-
-        public IPipe Pipe { get; set; }
-
-        public Frame<object> Frame { get; set; }
-
-        public PipeFactory PipelineFactory { get; set; }
     }
 }
