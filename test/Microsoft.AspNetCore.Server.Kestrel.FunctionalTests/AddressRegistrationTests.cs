47f1db20e063c2da75d9d89653fad4eafe24446c
Cesar Blum Silveira <cesarbs@gmail.com>
2017-03-24 16:46:40 -0700

    Fix races on port acquisition in AddressRegistrationTests (#1520).

--- test/Microsoft.AspNetCore.Server.Kestrel.FunctionalTests/AddressRegistrationTests.cs
+++ test/Microsoft.AspNetCore.Server.Kestrel.FunctionalTests/AddressRegistrationTests.cs
@@ -505,32 +505,17 @@ namespace Microsoft.AspNetCore.Server.Kestrel.FunctionalTests
             });
         }
 
-        private static int _nextPort = 8001;
-        private static object _portLock = new object();
         private static int GetNextPort()
         {
-            lock (_portLock)
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
             {
-                using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
-                {
-                    while (true)
-                    {
-                        try
-                        {
-                            var port = _nextPort++;
-                            socket.Bind(new IPEndPoint(IPAddress.Loopback, port));
-                            return port;
-                        }
-                        catch (SocketException)
-                        {
-                            // Retry unless exhausted
-                            if (_nextPort == 65536)
-                            {
-                                throw;
-                            }
-                        }
-                    }
-                }
+                // Let the OS assign the next available port. Unless we cycle through all ports
+                // on a test run, the OS will always increment the port number when making these calls.
+                // This prevents races in parallel test runs where a test is already bound to
+                // a given port, and a new test is able to bind to the same port due to port
+                // reuse being enabled by default by the OS.
+                socket.Bind(new IPEndPoint(IPAddress.Loopback, 0));
+                return ((IPEndPoint)socket.LocalEndPoint).Port;
             }
         }
 
