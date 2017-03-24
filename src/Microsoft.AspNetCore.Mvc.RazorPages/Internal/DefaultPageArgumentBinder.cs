maschram/Mvc

Support model binding to Razor Pages properties on page and PageModel classes
Fixes #5952

src/Microsoft.AspNetCore.Mvc.RazorPages/Internal/DefaultPageArgumentBinder.cs
@@ -3,29 +3,41 @@
 
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
 
 namespace Microsoft.AspNetCore.Mvc.RazorPages.Internal
 {
     public class DefaultPageArgumentBinder : PageArgumentBinder
     {
-        private readonly IModelMetadataProvider _modelMetadataProvider;
-        private readonly IModelBinderFactory _modelBinderFactory;
-        private readonly IObjectModelValidator _validator;
+        private readonly ParameterBinder _parameterBinder;
 
-        public DefaultPageArgumentBinder(
-            IModelMetadataProvider modelMetadataProvider,
-            IModelBinderFactory modelBinderFactory,
-            IObjectModelValidator validator)
+        public DefaultPageArgumentBinder(ParameterBinder binder)
         {
-            _modelMetadataProvider = modelMetadataProvider;
-            _modelBinderFactory = modelBinderFactory;
-            _validator = validator;
+            _parameterBinder = binder;
         }
 
         protected override async Task<ModelBindingResult> BindAsync(PageContext pageContext, object value, string name, Type type)
+        {
+            var valueProvider = await GetCompositeValueProvider(pageContext);
+            var parameterDescriptor = new ParameterDescriptor
+            {
+                BindingInfo = null,
+                Name = name,
+                ParameterType = type,
+            };
+
+            return await _parameterBinder.BindModelAsync(parameterDescriptor, pageContext, valueProvider, value);
+        }
+
+        public override async Task<ModelBindingResult> BindAsync(PageContext pageContext, ParameterDescriptor parameterDescriptor)
+        {
+            var valueProvider = await GetCompositeValueProvider(pageContext);
+            return await _parameterBinder.BindModelAsync(parameterDescriptor, pageContext, valueProvider);
+        }
+
+        private static async Task<CompositeValueProvider> GetCompositeValueProvider(PageContext pageContext)
         {
             var factories = pageContext.ValueProviderFactories;
             var valueProviderFactoryContext = new ValueProviderFactoryContext(pageContext);
@@ -35,48 +47,7 @@ namespace Microsoft.AspNetCore.Mvc.RazorPages.Internal
                 await factory.CreateValueProviderAsync(valueProviderFactoryContext);
             }
 
-            var valueProvider = new CompositeValueProvider(valueProviderFactoryContext.ValueProviders);
-
-            var metadata = _modelMetadataProvider.GetMetadataForType(type);
-            var binder = _modelBinderFactory.CreateBinder(new ModelBinderFactoryContext()
-            {
-                BindingInfo = null,
-                Metadata = metadata,
-                CacheToken = null,
-            });
-
-            var modelBindingContext = DefaultModelBindingContext.CreateBindingContext(
-                pageContext,
-                valueProvider,
-                metadata,
-                null,
-                name);
-            modelBindingContext.Model = value;
-
-            if (modelBindingContext.ValueProvider.ContainsPrefix(name))
-            {
-                // We have a match for the parameter name, use that as that prefix.
-                modelBindingContext.ModelName = name;
-            }
-            else
-            {
-                // No match, fallback to empty string as the prefix.
-                modelBindingContext.ModelName = string.Empty;
-            }
-
-            await binder.BindModelAsync(modelBindingContext);
-
-            var result = modelBindingContext.Result;
-            if (result.IsModelSet)
-            {
-                _validator.Validate(
-                    pageContext,
-                    modelBindingContext.ValidationState,
-                    modelBindingContext.ModelName,
-                    result.Model);
-            }
-
-            return result;
+            return new CompositeValueProvider(valueProviderFactoryContext.ValueProviders);
         }
     }
 }

dotnet/aspnetcore

src/Microsoft.AspNetCore.Mvc.RazorPages/Internal/DefaultPageArgumentBinder.cs
@@ -3,29 +3,35 @@
 
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
 
 namespace Microsoft.AspNetCore.Mvc.RazorPages.Internal
 {
     public class DefaultPageArgumentBinder : PageArgumentBinder
     {
-        private readonly IModelMetadataProvider _modelMetadataProvider;
-        private readonly IModelBinderFactory _modelBinderFactory;
-        private readonly IObjectModelValidator _validator;
+        private readonly ParameterBinder _parameterBinder;
 
-        public DefaultPageArgumentBinder(
-            IModelMetadataProvider modelMetadataProvider,
-            IModelBinderFactory modelBinderFactory,
-            IObjectModelValidator validator)
+        public DefaultPageArgumentBinder(ParameterBinder binder)
         {
-            _modelMetadataProvider = modelMetadataProvider;
-            _modelBinderFactory = modelBinderFactory;
-            _validator = validator;
+            _parameterBinder = binder;
         }
 
         protected override async Task<ModelBindingResult> BindAsync(PageContext pageContext, object value, string name, Type type)
+        {
+            var valueProvider = await GetCompositeValueProvider(pageContext);
+            var parameterDescriptor = new ParameterDescriptor
+            {
+                BindingInfo = null,
+                Name = name,
+                ParameterType = type,
+            };
+
+            return await _parameterBinder.BindModelAsync(pageContext, valueProvider, parameterDescriptor, value);
+        }
+
+        private static async Task<CompositeValueProvider> GetCompositeValueProvider(PageContext pageContext)
         {
             var factories = pageContext.ValueProviderFactories;
             var valueProviderFactoryContext = new ValueProviderFactoryContext(pageContext);
@@ -35,48 +41,7 @@ namespace Microsoft.AspNetCore.Mvc.RazorPages.Internal
                 await factory.CreateValueProviderAsync(valueProviderFactoryContext);
             }
 
-            var valueProvider = new CompositeValueProvider(valueProviderFactoryContext.ValueProviders);
-
-            var metadata = _modelMetadataProvider.GetMetadataForType(type);
-            var binder = _modelBinderFactory.CreateBinder(new ModelBinderFactoryContext()
-            {
-                BindingInfo = null,
-                Metadata = metadata,
-                CacheToken = null,
-            });
-
-            var modelBindingContext = DefaultModelBindingContext.CreateBindingContext(
-                pageContext,
-                valueProvider,
-                metadata,
-                null,
-                name);
-            modelBindingContext.Model = value;
-
-            if (modelBindingContext.ValueProvider.ContainsPrefix(name))
-            {
-                // We have a match for the parameter name, use that as that prefix.
-                modelBindingContext.ModelName = name;
-            }
-            else
-            {
-                // No match, fallback to empty string as the prefix.
-                modelBindingContext.ModelName = string.Empty;
-            }
-
-            await binder.BindModelAsync(modelBindingContext);
-
-            var result = modelBindingContext.Result;
-            if (result.IsModelSet)
-            {
-                _validator.Validate(
-                    pageContext,
-                    modelBindingContext.ValidationState,
-                    modelBindingContext.ModelName,
-                    result.Model);
-            }
-
-            return result;
+            return new CompositeValueProvider(valueProviderFactoryContext.ValueProviders);
         }
     }
 }