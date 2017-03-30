68866f87160e0d10aa3b9fd9468bf7e3b59a1e5f
Kiran Challa <kichalla@microsoft.com>
2017-03-30 10:28:04 -0700

    Added functional tests for JsonPatch

---
+++ test/WebSites/FormatterWebSite/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace FormatterWebSite
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public List<Review> Reviews { get; set; } = new List<Review>();
+    }
+}
