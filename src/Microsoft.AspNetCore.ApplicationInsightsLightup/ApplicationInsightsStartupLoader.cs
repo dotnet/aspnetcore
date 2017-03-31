975cfcb8ce2bf9efde1bce795c38595e615603c7
Chris R <Tratcher@outlook.com>
2017-03-31 12:01:45 -0700

    #16 Create a lightup package for Application Insights

---
+++ src/Microsoft.AspNetCore.ApplicationInsightsLightup/ApplicationInsightsStartupLoader.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(Microsoft.AspNetCore.ApplicationInsightsLightup.ApplicationInsightsStartupLoader))]
+
+namespace Microsoft.AspNetCore.ApplicationInsightsLightup
+{
+    /// <summary>
+    /// A dynamic Application Insights lightup experiance
+    /// </summary>
+    public class ApplicationInsightsStartupLoader : IHostingStartup
+    {
+        /// <summary>
+        /// Calls UseApplicationInsights
+        /// </summary>
+        /// <param name="builder"></param>
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.UseApplicationInsights();
+        }
+    }
+}
