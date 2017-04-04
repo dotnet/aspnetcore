19cb15be540c5c9ce6733ce143375d42a893fb48
Kiran Challa <kichalla@microsoft.com>
2017-04-03 17:13:49 -0700

    Revert "Skipping model validation tests temporarily to unblock mirror"
    This reverts commit e25bfa8919bbe4c1b3e90c04a36a3ad78e43857d.

--- test/Microsoft.AspNetCore.Mvc.IntegrationTests/TryValidateModelIntegrationTest.cs
+++ test/Microsoft.AspNetCore.Mvc.IntegrationTests/TryValidateModelIntegrationTest.cs
@@ -75,7 +75,7 @@ namespace Microsoft.AspNetCore.Mvc.IntegrationTests
             Assert.Empty(modelStateErrors);
         }
 
-        [Fact(Skip = "https://github.com/aspnet/Mvc/issues/6058")]
+        [Fact]
         [ReplaceCulture]
         public void TryValidateModel_CollectionsModel_ReturnsErrorsForInvalidProperties()
         {
