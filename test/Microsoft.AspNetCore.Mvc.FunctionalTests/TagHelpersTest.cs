3424e20c67ed8944e0572e606823625781e32a6a
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2017-04-04 19:25:50 -0700

    Re-enable skipped test

--- test/Microsoft.AspNetCore.Mvc.FunctionalTests/TagHelpersTest.cs
+++ test/Microsoft.AspNetCore.Mvc.FunctionalTests/TagHelpersTest.cs
@@ -34,7 +34,7 @@ namespace Microsoft.AspNetCore.Mvc.FunctionalTests
         public HttpClient EncodedClient { get; }
 
         [Theory]
-        //[InlineData("Index")] Skipping temporarily to unblock build
+        [InlineData("Index")]
         [InlineData("About")]
         [InlineData("Help")]
         [InlineData("UnboundDynamicAttributes")]
