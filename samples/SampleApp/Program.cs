55d50b2d4181445c49a326b86191ebd3d506d0fc
damianedwards <damian@damianedwards.com>
2017-04-06 18:09:58 -0700

    Adds new WebHost API for creating IWebHost & IWebHostBuilder with defaults: - #24

---
+++ samples/SampleApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace SampleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //HelloWorld();
+
+            //CustomUrl();
+
+            Router();
+
+            //StartupClass(args);
+        }
+
+        private static void HelloWorld()
+        {
+            var host = WebHost.Start(context => context.Response.WriteAsync("Hello, World!"));
+            //host.WaitForShutdown(); // TODO: This method needs to be added to Hosting
+            Console.WriteLine("Press any key to shutdown...");
+            Console.ReadKey();
+        }
+
+        private static void CustomUrl()
+        {
+            // Changing the listening URL
+            var host = WebHost.Start("http://localhost:8080", context => context.Response.WriteAsync("Hello, World!"));
+            //host.WaitForShutdown(); // TODO: This method needs to be added to Hosting
+            Console.WriteLine("Press any key to shutdown...");
+            Console.ReadKey();
+        }
+
+        private static void Router()
+        {
+            // Using a router
+            var host = WebHost.Start(router => router
+                .MapGet("hello/{name}", (req, res, data) => res.WriteAsync($"Hello, {data.Values["name"]}"))
+                .MapGet("goodbye/{name}", (req, res, data) => res.WriteAsync($"Goodbye, {data.Values["name"]}"))
+                .MapGet("throw/{message?}", (req, res, data) => throw new Exception((string)data.Values["message"] ?? "Uh oh!"))
+                .MapGet("{greeting}/{name}", (req, res, data) => res.WriteAsync($"{data.Values["greeting"]}, {data.Values["name"]}"))
+                .MapGet("", (req, res, data) => res.WriteAsync($"Hello, World!"))
+            );
+            //host.WaitForShutdown(); // TODO: This method needs to be added to Hosting
+            Console.WriteLine("Press any key to shutdown...");
+            Console.ReadKey();
+        }
+
+        private static void StartupClass(string[] args)
+        {
+            // Using defaults with a Startup class
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();
+        }
+    }
+}
---
+++ samples/SampleApp/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SampleApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"Hello from {nameof(Startup)}!");
+            });
+        }
+    }
+}
