801a7c76527a571c77dad8d3bd247e7039f73173
Cesar Blum Silveira <cesarbs@gmail.com>
2017-04-17 20:47:29 -0700

    Add copyright header to TransportSocketOptions.cs.

--- src/Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets/SocketTransportOptions.cs
+++ src/Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets/SocketTransportOptions.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,6 +10,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets
     // TODO: Come up with some options
     public class SocketTransportOptions
     {
-        
+
     }
 }
