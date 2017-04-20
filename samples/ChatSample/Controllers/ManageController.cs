62abf9431f1aff302c62d1e1bc55111bdd14d352
Hao Kung <haok@microsoft.com>
2017-04-19 19:46:25 -0700

    Add using to sample

--- samples/ChatSample/Controllers/ManageController.cs
+++ samples/ChatSample/Controllers/ManageController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
