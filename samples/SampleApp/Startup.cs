d9661ed70dc4f6d8347d956f787484d8d59ef3e4
John Luo <jt.luo@mail.utoronto.ca>
2017-04-21 12:20:44 -0700

    Un-revert changes for static WebHost API
    Keeping the same netstandard1.3 TFM so as to not break apps targeting net46.

---
+++ samples/SampleApp/Startup.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SampleApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"Hello from {nameof(Startup)}!");
+            });
+        }
+    }
+}
