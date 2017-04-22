5472763fc27d3a84dc44eeef36c9ae1bf9bbdec6
David Fowler <davidfowl@gmail.com>
2017-04-22 02:52:40 -0700

    Added feature to control http body behavior (#821)
    * Added feature to control http body behavior
    - Added a flag to allow synchronous IO

---
+++ src/Microsoft.AspNetCore.Http.Features/IHttpBodyControlFeature.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.AspNetCore.Http.Features
+{
+    /// <summary>
+    /// Controls the IO behavior for the <see cref="IHttpRequestFeature.Body"/> and <see cref="IHttpResponseFeature.Body"/> 
+    /// </summary>
+    public interface IHttpBodyControlFeature
+    {
+        /// <summary>
+        /// Gets or sets a value that controls whether synchronous IO is allowed for the <see cref="IHttpRequestFeature.Body"/> and <see cref="IHttpResponseFeature.Body"/> 
+        /// </summary>
+        bool AllowSynchronousIO { get; set; }
+    }
+}
