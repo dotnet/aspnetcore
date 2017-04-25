6527001b3f7929806d5e4cd7f2d46448312c9fcd
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2017-04-25 15:18:58 -0700

    remove test logging (#1040)

--- test/Microsoft.AspNetCore.Hosting.FunctionalTests/ShutdownTests.cs
+++ test/Microsoft.AspNetCore.Hosting.FunctionalTests/ShutdownTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Diagnostics;
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Server.IntegrationTesting;
 using Microsoft.AspNetCore.Server.IntegrationTesting.xunit;
 using Microsoft.AspNetCore.Testing.xunit;
+using Microsoft.Extensions.Logging.Testing;
 using Xunit;
 using Xunit.Abstractions;
 
