0edd62339ed779a208895f9a032329b0b2ed539c
Chris R <Tratcher@outlook.com>
2017-04-26 14:37:58 -0700

    React to IHostedService namespace change

--- src/Microsoft.AspNetCore.Sockets/SocketsDependencyInjectionExtensions.cs
+++ src/Microsoft.AspNetCore.Sockets/SocketsDependencyInjectionExtensions.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Sockets;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
