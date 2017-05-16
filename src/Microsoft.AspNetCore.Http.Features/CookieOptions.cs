dfd938e4f7c2138877b83925dc84bb74f48a00ce
user1336 <h2354067@mvrht.com>
2017-05-16 21:42:12 +0200

    Fixed � in summary (#838)

--- src/Microsoft.AspNetCore.Http.Features/CookieOptions.cs
+++ src/Microsoft.AspNetCore.Http.Features/CookieOptions.cs
@@ -37,7 +37,7 @@ namespace Microsoft.AspNetCore.Http
         public DateTimeOffset? Expires { get; set; }
 
         /// <summary>
-        /// Gets or sets a value that indicates whether to transmit the cookie using Secure Sockets Layer (SSL)�that is, over HTTPS only.
+        /// Gets or sets a value that indicates whether to transmit the cookie using Secure Sockets Layer (SSL)--that is, over HTTPS only.
         /// </summary>
         /// <returns>true to transmit the cookie only over an SSL connection (HTTPS); otherwise, false.</returns>
         public bool Secure { get; set; }
