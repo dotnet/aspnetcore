ece5e748adea86a4ec49816e15eb507198652c03
Mardoxx <mardoxx@gmail.com>
2017-05-18 13:01:43 +0100

    Minor documentation fix for SerializableError
    ModelStateEntry -> ModelStateDictionary

--- src/Microsoft.AspNetCore.Mvc.Core/SerializableError.cs
+++ src/Microsoft.AspNetCore.Mvc.Core/SerializableError.cs
@@ -26,7 +26,7 @@ namespace Microsoft.AspNetCore.Mvc
         /// <summary>
         /// Creates a new instance of <see cref="SerializableError"/>.
         /// </summary>
-        /// <param name="modelState"><see cref="ModelStateEntry"/> containing the validation errors.</param>
+        /// <param name="modelState"><see cref="ModelStateDictionary"/> containing the validation errors.</param>
         public SerializableError(ModelStateDictionary modelState)
             : this()
         {
