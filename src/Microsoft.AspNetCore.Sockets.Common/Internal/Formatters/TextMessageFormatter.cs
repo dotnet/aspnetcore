8f18ff44234e038c6728f9798faf970df3244fad
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2017-05-19 15:06:46 -0700

    react to corefxlabs changes (#464) (#465)

--- src/Microsoft.AspNetCore.Sockets.Common/Internal/Formatters/TextMessageFormatter.cs
+++ src/Microsoft.AspNetCore.Sockets.Common/Internal/Formatters/TextMessageFormatter.cs
@@ -1,8 +1,9 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
 using System.Binary;
+using System.Binary.Base64;
 using System.Buffers;
 using System.Text;
 using System.Text.Formatting;
