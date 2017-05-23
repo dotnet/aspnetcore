a31c6931788d5c92552eb04904e8a3c545f387f0
Hao Kung <haok@microsoft.com>
2017-05-23 14:11:26 -0700

    React to identity

--- samples/ChatSample/Models/ApplicationUser.cs
+++ samples/ChatSample/Models/ApplicationUser.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace ChatSample.Models
 {
