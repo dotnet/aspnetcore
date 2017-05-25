06028e81c77759357d8b16fa37795431feb1bb85
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2017-05-24 18:20:46 -0700

    fix build by adding OutputType exe to test project (#179)

---
+++ test/Microsoft.AspNetCore.WebSockets.Test/Program.cs
@@ -0,0 +1,20 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+#if NET461
+using System;
+
+namespace Microsoft.AspNetCore.WebSockets.Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This Program.Main is only here to work around https://github.com/dotnet/sdk/issues/909");
+        }
+    }
+}
+#elif NETCOREAPP2_0
+#else
+#error Target frameworks need to be updated
+#endif
