landocalris/Identity

Add support for keyvault

src/Microsoft.AspNetCore.Identity.Service.KeyVault/IdentityServiceBuilderExtensions.cs
@@ -1,0 +1,51 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.AspNetCore.Identity.Service.KeyVault
+{
+    public static class IdentityServiceBuilderExtensions
+    {
+        public static IIdentityServiceBuilder AddKeyVault(this IIdentityServiceBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            var services = builder.Services;
+            services.TryAddEnumerable(ServiceDescriptor.Singleton<IConfigureOptions<KeyVaultSigningCredentialsSourceOptions>, DefaultSetup>());
+            services.TryAddSingleton<ISigningCredentialsSource, KeyVaultSigningCredentialSource>();
+            return builder;
+        }
+
+        public static IIdentityServiceBuilder AddKeyVault(this IIdentityServiceBuilder builder, Action<KeyVaultSigningCredentialsSourceOptions> configure)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (configure == null)
+            {
+                throw new ArgumentNullException(nameof(configure));
+            }
+
+            builder.Services.Configure(configure);
+            builder.Services.TryAddSingleton<ISigningCredentialsSource, KeyVaultSigningCredentialSource>();
+
+            return builder;
+        }
+
+        private class DefaultSetup : ConfigureOptions<KeyVaultSigningCredentialsSourceOptions>
+        {
+            public DefaultSetup(IConfiguration configuration)
+                : base(options => configuration.GetSection("Identity:KeyVault").Bind(options)) { }
+        }
+    }
+}

src/Microsoft.AspNetCore.Identity.Service.KeyVault/KeyVaultSigningCredentialSource.cs
@@ -1,0 +1,61 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Microsoft.AspNetCore.Identity.Service.KeyVault
+{
+    public class KeyVaultSigningCredentialSource : ISigningCredentialsSource
+    {
+        private readonly IOptions<KeyVaultSigningCredentialsSourceOptions> _options;
+
+        public KeyVaultSigningCredentialSource(IOptions<KeyVaultSigningCredentialsSourceOptions> options)
+        {
+            _options = options;
+        }
+
+        public async Task<IEnumerable<SigningCredentialsDescriptor>> GetCredentials()
+        {
+            var options = _options.Value;
+            var client = new KeyVaultClient(KeyVaultCallBack, options.ClientHandler);
+
+            var certificateBundle = await client.GetCertificateAsync(options.VaultUri, options.CertificateName);
+            var secret = await client.GetSecretAsync(certificateBundle.SecretIdentifier.Identifier);
+            var certificate = new X509Certificate2(Base64UrlEncoder.DecodeBytes(secret.Value), string.Empty);
+            var signingCredentials = new SigningCredentials(new X509SecurityKey(certificate), CryptographyHelpers.FindAlgorithm(certificate));
+            var descriptor = new SigningCredentialsDescriptor(
+                signingCredentials,
+                CryptographyHelpers.GetAlgorithm(signingCredentials),
+                certificateBundle.Attributes.NotBefore.Value.ToUniversalTime(),
+                certificateBundle.Attributes.Expires.Value.ToUniversalTime(),
+                GetMetadata(signingCredentials));
+
+            return new List<SigningCredentialsDescriptor>() { descriptor };
+
+            IDictionary<string, string> GetMetadata(SigningCredentials credentials)
+            {
+                var rsaParameters = CryptographyHelpers.GetRSAParameters(credentials);
+                return new Dictionary<string, string>
+                {
+                    [JsonWebKeyParameterNames.E] = Base64UrlEncoder.Encode(rsaParameters.Exponent),
+                    [JsonWebKeyParameterNames.N] = Base64UrlEncoder.Encode(rsaParameters.Modulus),
+                };
+            }
+
+            async Task<string> KeyVaultCallBack(string authority, string resource, string scope)
+            {
+
+                var adCredential = new ClientCredential(options.ClientId, options.ClientSecret);
+                var authenticationContext = new AuthenticationContext(authority, null);
+                var tokenResponse = await authenticationContext.AcquireTokenAsync(resource, adCredential);
+                return tokenResponse.AccessToken;
+            }
+        }
+    }
+}

src/Microsoft.AspNetCore.Identity.Service.KeyVault/KeyVaultSigningCredentialsSourceOptions.cs
@@ -1,0 +1,17 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Net.Http;
+
+namespace Microsoft.AspNetCore.Identity.Service.KeyVault
+{
+    public class KeyVaultSigningCredentialsSourceOptions
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string CertificateThumbprint { get; set; }
+        public string VaultUri { get; set; }
+        public string CertificateName { get; set; }
+        public HttpClient ClientHandler { get; set; } = new HttpClient();
+    }
+}

dotnet/aspnetcore

src/Microsoft.AspNetCore.Identity.Service.AzureKeyVault/IdentityServiceBuilderExtensions.cs
@@ -1,0 +1,51 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.AspNetCore.Identity.Service.AzureKeyVault
+{
+    public static class IdentityServiceBuilderExtensions
+    {
+        public static IIdentityServiceBuilder AddKeyVault(this IIdentityServiceBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            var services = builder.Services;
+            services.TryAddEnumerable(ServiceDescriptor.Singleton<IConfigureOptions<KeyVaultSigningCredentialsSourceOptions>, DefaultSetup>());
+            services.TryAddSingleton<ISigningCredentialsSource, KeyVaultSigningCredentialSource>();
+            return builder;
+        }
+
+        public static IIdentityServiceBuilder AddKeyVault(this IIdentityServiceBuilder builder, Action<KeyVaultSigningCredentialsSourceOptions> configure)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (configure == null)
+            {
+                throw new ArgumentNullException(nameof(configure));
+            }
+
+            builder.Services.Configure(configure);
+            builder.Services.TryAddSingleton<ISigningCredentialsSource, KeyVaultSigningCredentialSource>();
+
+            return builder;
+        }
+
+        private class DefaultSetup : ConfigureOptions<KeyVaultSigningCredentialsSourceOptions>
+        {
+            public DefaultSetup(IConfiguration configuration)
+                : base(options => configuration.GetSection("Identity:KeyVault").Bind(options)) { }
+        }
+    }
+}

src/Microsoft.AspNetCore.Identity.Service.AzureKeyVault/KeyVaultSigningCredentialSource.cs
@@ -1,0 +1,61 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Microsoft.AspNetCore.Identity.Service.AzureKeyVault
+{
+    public class KeyVaultSigningCredentialSource : ISigningCredentialsSource
+    {
+        private readonly IOptions<KeyVaultSigningCredentialsSourceOptions> _options;
+
+        public KeyVaultSigningCredentialSource(IOptions<KeyVaultSigningCredentialsSourceOptions> options)
+        {
+            _options = options;
+        }
+
+        public async Task<IEnumerable<SigningCredentialsDescriptor>> GetCredentials()
+        {
+            var options = _options.Value;
+            var client = new KeyVaultClient(KeyVaultCallBack, options.ClientHandler);
+
+            var certificateBundle = await client.GetCertificateAsync(options.VaultUri, options.CertificateName);
+            var secret = await client.GetSecretAsync(certificateBundle.SecretIdentifier.Identifier);
+            var certificate = new X509Certificate2(Base64UrlEncoder.DecodeBytes(secret.Value), string.Empty);
+            var signingCredentials = new SigningCredentials(new X509SecurityKey(certificate), CryptographyHelpers.FindAlgorithm(certificate));
+            var descriptor = new SigningCredentialsDescriptor(
+                signingCredentials,
+                CryptographyHelpers.GetAlgorithm(signingCredentials),
+                certificateBundle.Attributes.NotBefore.Value.ToUniversalTime(),
+                certificateBundle.Attributes.Expires.Value.ToUniversalTime(),
+                GetMetadata(signingCredentials));
+
+            return new List<SigningCredentialsDescriptor>() { descriptor };
+
+            IDictionary<string, string> GetMetadata(SigningCredentials credentials)
+            {
+                var rsaParameters = CryptographyHelpers.GetRSAParameters(credentials);
+                return new Dictionary<string, string>
+                {
+                    [JsonWebKeyParameterNames.E] = Base64UrlEncoder.Encode(rsaParameters.Exponent),
+                    [JsonWebKeyParameterNames.N] = Base64UrlEncoder.Encode(rsaParameters.Modulus),
+                };
+            }
+
+            async Task<string> KeyVaultCallBack(string authority, string resource, string scope)
+            {
+
+                var adCredential = new ClientCredential(options.ClientId, options.ClientSecret);
+                var authenticationContext = new AuthenticationContext(authority, null);
+                var tokenResponse = await authenticationContext.AcquireTokenAsync(resource, adCredential);
+                return tokenResponse.AccessToken;
+            }
+        }
+    }
+}

src/Microsoft.AspNetCore.Identity.Service.AzureKeyVault/KeyVaultSigningCredentialsSourceOptions.cs
@@ -1,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Net.Http;
+
+namespace Microsoft.AspNetCore.Identity.Service.AzureKeyVault
+{
+    public class KeyVaultSigningCredentialsSourceOptions
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string VaultUri { get; set; }
+        public string CertificateName { get; set; }
+        public HttpClient ClientHandler { get; set; } = new HttpClient();
+    }
+}