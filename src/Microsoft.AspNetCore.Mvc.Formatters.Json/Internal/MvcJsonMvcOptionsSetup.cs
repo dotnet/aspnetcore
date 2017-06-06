mickey2009/Mvc

Enabled a way to buffer request body in formatters

src/Microsoft.AspNetCore.Mvc.Formatters.Json/Internal/MvcJsonMvcOptionsSetup.cs
@@ -67,14 +67,16 @@ namespace Microsoft.AspNetCore.Mvc.Formatters.Json.Internal
                 jsonInputPatchLogger,
                 _jsonSerializerSettings,
                 _charPool,
-                _objectPoolProvider));
+                _objectPoolProvider,
+                options.BufferrRequestBodyInFormatters));
 
             var jsonInputLogger = _loggerFactory.CreateLogger<JsonInputFormatter>();
             options.InputFormatters.Add(new JsonInputFormatter(
                 jsonInputLogger,
                 _jsonSerializerSettings,
                 _charPool,
-                _objectPoolProvider));
+                _objectPoolProvider,
+                options.BufferrRequestBodyInFormatters));
 
             options.FormatterMappings.SetMediaTypeMappingForFormat("json", MediaTypeHeaderValue.Parse("application/json"));
 

src/Microsoft.AspNetCore.Mvc.Formatters.Xml/Internal/MvcXmlDataContractSerializerMvcOptionsSetup.cs
@@ -22,7 +22,7 @@ namespace Microsoft.AspNetCore.Mvc.Formatters.Xml.Internal
             options.ModelMetadataDetailsProviders.Add(new DataMemberRequiredBindingMetadataProvider());
 
             options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
-            options.InputFormatters.Add(new XmlDataContractSerializerInputFormatter());
+            options.InputFormatters.Add(new XmlDataContractSerializerInputFormatter(options.BufferrRequestBodyInFormatters));
 
             options.ModelMetadataDetailsProviders.Add(new SuppressChildValidationMetadataProvider("System.Xml.Linq.XObject"));
             options.ModelMetadataDetailsProviders.Add(new SuppressChildValidationMetadataProvider("System.Xml.XmlNode"));

src/Microsoft.AspNetCore.Mvc.Formatters.Xml/Internal/MvcXmlSerializerMvcOptionsSetup.cs
@@ -18,7 +18,7 @@ namespace Microsoft.AspNetCore.Mvc.Formatters.Xml.Internal
         public void Configure(MvcOptions options)
         {
             options.OutputFormatters.Add(new XmlSerializerOutputFormatter());
-            options.InputFormatters.Add(new XmlSerializerInputFormatter());
+            options.InputFormatters.Add(new XmlSerializerInputFormatter(options.BufferrRequestBodyInFormatters));
         }
     }
 }

dotnet/aspnetcore

src/Microsoft.AspNetCore.Mvc.Formatters.Json/Internal/MvcJsonMvcOptionsSetup.cs
@@ -67,14 +67,16 @@ namespace Microsoft.AspNetCore.Mvc.Formatters.Json.Internal
                 jsonInputPatchLogger,
                 _jsonSerializerSettings,
                 _charPool,
-                _objectPoolProvider));
+                _objectPoolProvider,
+                options.SuppressInputFormatterBuffering));
 
             var jsonInputLogger = _loggerFactory.CreateLogger<JsonInputFormatter>();
             options.InputFormatters.Add(new JsonInputFormatter(
                 jsonInputLogger,
                 _jsonSerializerSettings,
                 _charPool,
-                _objectPoolProvider));
+                _objectPoolProvider,
+                options.SuppressInputFormatterBuffering));
 
             options.FormatterMappings.SetMediaTypeMappingForFormat("json", MediaTypeHeaderValue.Parse("application/json"));
 

src/Microsoft.AspNetCore.Mvc.Formatters.Xml/Internal/MvcXmlDataContractSerializerMvcOptionsSetup.cs
@@ -22,7 +22,7 @@ namespace Microsoft.AspNetCore.Mvc.Formatters.Xml.Internal
             options.ModelMetadataDetailsProviders.Add(new DataMemberRequiredBindingMetadataProvider());
 
             options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
-            options.InputFormatters.Add(new XmlDataContractSerializerInputFormatter());
+            options.InputFormatters.Add(new XmlDataContractSerializerInputFormatter(options.SuppressInputFormatterBuffering));
 
             options.ModelMetadataDetailsProviders.Add(new SuppressChildValidationMetadataProvider("System.Xml.Linq.XObject"));
             options.ModelMetadataDetailsProviders.Add(new SuppressChildValidationMetadataProvider("System.Xml.XmlNode"));

src/Microsoft.AspNetCore.Mvc.Formatters.Xml/Internal/MvcXmlSerializerMvcOptionsSetup.cs
@@ -18,7 +18,7 @@ namespace Microsoft.AspNetCore.Mvc.Formatters.Xml.Internal
         public void Configure(MvcOptions options)
         {
             options.OutputFormatters.Add(new XmlSerializerOutputFormatter());
-            options.InputFormatters.Add(new XmlSerializerInputFormatter());
+            options.InputFormatters.Add(new XmlSerializerInputFormatter(options.SuppressInputFormatterBuffering));
         }
     }
 }