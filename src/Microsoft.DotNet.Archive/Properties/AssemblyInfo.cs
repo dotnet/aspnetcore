0e4db81898b72b6a0f193b1b9027288dbd18c92f
John Luo <jt.luo@mail.utoronto.ca>
2017-06-22 12:47:06 -0700

    Remove dotnet-archive and Microsoft.Dotnet.Archive

--- src/Microsoft.DotNet.Archive/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) .NET Foundation and contributors. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-using System.Reflection;
-
-[assembly: AssemblyMetadataAttribute("Serviceable", "True")]
