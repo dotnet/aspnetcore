5f2c00bc80849f5b61b6c76398d0d8df2eb745d9
N. Taylor Mullen <nimullen@microsoft.com>
2017-06-26 17:08:13 -0700

    Add doc comments for `DirectiveUsage`.

--- src/Microsoft.AspNetCore.Razor.Language/DirectiveUsage.cs
+++ src/Microsoft.AspNetCore.Razor.Language/DirectiveUsage.cs
@@ -3,9 +3,20 @@
 
 namespace Microsoft.AspNetCore.Razor.Language
 {
+    /// <summary>
+    /// The way a directive can be used. The usage determines how many, and where directives can exist per file.
+    /// </summary>
     public enum DirectiveUsage
     {
+        /// <summary>
+        /// Directive can exist anywhere in the file.
+        /// </summary>
         Unrestricted,
+
+        /// <summary>
+        /// Directive must exist prior to any HTML or code and have no duplicates. When importing the directive, if it is 
+        /// <see cref="DirectiveKind.SingleLine"/>, the last occurrence of the directive is imported.
+        /// </summary>
         FileScopedSinglyOccurring,
     }
 }
