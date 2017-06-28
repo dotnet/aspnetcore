e70ce98213dcc10f7339e851be6b4c7281152a4e
Ryan Nowak <nowakra@gmail.com>
2017-06-28 09:05:04 -0700

    Add docs about limitation of this option
    This is all the work we're planning to do for #1361 for 2.0.0. Will
    revisit this functionality in 2.1.0.

--- src/Microsoft.AspNetCore.Razor.Language/RazorParserOptions.cs
+++ src/Microsoft.AspNetCore.Razor.Language/RazorParserOptions.cs
@@ -38,6 +38,14 @@ namespace Microsoft.AspNetCore.Razor.Language
 
         public abstract IReadOnlyCollection<DirectiveDescriptor> Directives { get; }
 
+        /// <summary>
+        /// Gets a value which indicates whether the parser will parse only the leading directives. If <c>true</c>
+        /// the parser will halt at the first HTML content or C# code block. If <c>false</c> the whole document is parsed.
+        /// </summary>
+        /// <remarks>
+        /// Currently setting this option to <c>true</c> will result in only the first line of directives being parsed.
+        /// In a future release this may be updated to include all leading directive content.
+        /// </remarks>
         public abstract bool ParseOnlyLeadingDirectives { get; }
     }
 }
