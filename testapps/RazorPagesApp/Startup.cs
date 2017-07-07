55d7cd2286e959a3db5b483e00ae6890ecbec2ca
Hao Kung <haok@microsoft.com>
2017-07-07 14:00:32 -0700

    React to defaults

--- testapps/RazorPagesApp/Startup.cs
+++ testapps/RazorPagesApp/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -10,7 +11,7 @@ namespace RazorPagesApp
         public void ConfigureServices(IServiceCollection services)
         {
             var builder = services.AddMvc();
-            services.AddAuthentication().AddCookie(options => options.LoginPath = "/Login");
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options => options.LoginPath = "/Login");
             ConfigureMvc(builder);
         }
 
