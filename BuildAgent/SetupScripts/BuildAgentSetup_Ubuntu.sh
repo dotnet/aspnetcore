95ef7fab411be7308eb4e9a044bd33aedc42c3c2
Mike Harder <mharder@microsoft.com>
2017-08-04 11:45:46 -0700

    Remove Mono (#12)
    Mono is no longer required for 2.0 builds.  It is required for building 1.0 and 1.1 patches, but we can manually install Mono on an Ubuntu 14.04 agent for patch builds.

--- BuildAgent/SetupScripts/BuildAgentSetup_Ubuntu.sh
+++ BuildAgent/SetupScripts/BuildAgentSetup_Ubuntu.sh
@@ -32,12 +32,6 @@ apt-get install -y git
 # Sometimes git pull stalls, so this could fix it
 git config --global http.postBuffer 2M
 
-echo "Installing Mono 4.4.0..."
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/debian wheezy/snapshots/4.4.0/. main" | tee /etc/apt/sources.list.d/mono-xamarin.list
-apt-get update
-apt-get install -y mono-complete
-
 echo "Installing libunwind8..."
 apt-get install -y libunwind8
 
