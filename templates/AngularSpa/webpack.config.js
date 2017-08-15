fc398d602a9e908b62028c26fb7ecde827a519f7
frederikprijck <frederik.prijck@gmail.com>
2017-08-15 23:45:31 +0200

    Allow lazy loading with AngularSpa in dev build
    Previously, the AngularSpa didn't include `angular2-router-loader`.
    This commit ensures it does.
    
    Closes #1194

--- templates/AngularSpa/webpack.config.js
+++ templates/AngularSpa/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = (env) => {
         },
         module: {
             rules: [
-                { test: /\.ts$/, use: isDevBuild ? ['awesome-typescript-loader?silent=true', 'angular2-template-loader'] : '@ngtools/webpack' },
+                { test: /\.ts$/, use: isDevBuild ? ['awesome-typescript-loader?silent=true', 'angular2-template-loader', 'angular2-router-loader'] : '@ngtools/webpack' },
                 { test: /\.html$/, use: 'html-loader?minimize=false' },
                 { test: /\.css$/, use: [ 'to-string-loader', isDevBuild ? 'css-loader' : 'css-loader?minimize' ] },
                 { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
