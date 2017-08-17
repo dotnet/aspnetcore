ec09268698bdedb298d50ab73eb57eca239a8eae
Mikael Mengistu <mikaelm12@users.noreply.github.com>
2017-08-17 09:49:23 -0700

    Add using for presence in chat sample (#727)

--- samples/ChatSample/Startup.cs
+++ samples/ChatSample/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Redis;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
