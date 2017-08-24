7ca8255d6f92470493cb24537967786d326acc78
Ryan Nowak <nowakra@gmail.com>
2017-08-24 11:29:26 -0700

    Add a default VS foregrounddispatcher

---
+++ src/Microsoft.VisualStudio.LanguageServices.Razor/VisualStudioForegroundDispatcher.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Shell;
+
+namespace Microsoft.VisualStudio.LanguageServices.Razor
+{
+    [Export(typeof(ForegroundDispatcher))]
+    internal class VisualStudioForegroundDispatcher : ForegroundDispatcher
+    {
+        public override bool IsForegroundThread => ThreadHelper.CheckAccess();
+    }
+}
