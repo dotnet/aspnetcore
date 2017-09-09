20ec50d5d26f2052afcfcf640c69c6049208e799
Tugberk Ugurlu <tugberk@outlook.com>
2017-09-09 22:00:49 +0100

    use WebHost.CreateDefaultBuilder in IdentitySample.Mvc
    so that it picks up the logging config from the `appsettings.json`

--- samples/IdentitySample.Mvc/Program.cs
+++ samples/IdentitySample.Mvc/Program.cs
@@ -1,20 +1,15 @@
-using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace IdentitySample
 {
     public static class Program
     {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+        public static void Main(string[] args) => BuildWebHost(args).Run();
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-
-            host.Run();
-        }
     }
 }
