b38f0dbc7af904171a8583d3733d1761d2a9b1f5
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2017-09-11 18:32:54 +0100

    Change tests to use 2.0 templates until SDK is updated

--- test/Templates.Test/Helpers/TemplatePackageInstaller.cs
+++ test/Templates.Test/Helpers/TemplatePackageInstaller.cs
@@ -15,7 +15,6 @@ namespace Templates.Test.Helpers
             "Microsoft.DotNet.Web.Client.ItemTemplates",
             "Microsoft.DotNet.Web.ItemTemplates",
             "Microsoft.DotNet.Web.ProjectTemplates.2.0",
-            "Microsoft.DotNet.Web.ProjectTemplates.2.1",
             "Microsoft.DotNet.Web.Spa.ProjectTemplates",
             "Microsoft.AspNetCore.SpaTemplates",
         };
