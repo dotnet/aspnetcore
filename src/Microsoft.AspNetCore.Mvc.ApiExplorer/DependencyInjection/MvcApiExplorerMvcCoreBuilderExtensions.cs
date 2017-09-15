Lasie989/Mvc

Introduce opinionated API defaults.
* Introduce ProblemDescriptionAttribute to enhance some 4xx messages and produce better API description.
* Introduce IErrorDescriptionProvider to modify the shape of error response.

Fixes #6785, Fixes #6786

src/Microsoft.AspNetCore.Mvc.ApiExplorer/DependencyInjection/MvcApiExplorerMvcCoreBuilderExtensions.cs
@@ -26,6 +26,8 @@ namespace Microsoft.Extensions.DependencyInjection
             services.TryAddSingleton<IApiDescriptionGroupCollectionProvider, ApiDescriptionGroupCollectionProvider>();
             services.TryAddEnumerable(
                 ServiceDescriptor.Transient<IApiDescriptionProvider, DefaultApiDescriptionProvider>());
+            services.TryAddEnumerable(
+                ServiceDescriptor.Transient<IApiDescriptionProvider, ProblemAttributeApiDescriptionProvider>());
         }
     }
 }

src/Microsoft.AspNetCore.Mvc.Core/ErrorDescription/ErrorDescriptionContext.cs
@@ -1,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc.Abstractions;
+
+namespace Microsoft.AspNetCore.Mvc.ErrorDescription
+{
+    public class ErrorDescriptionContext
+    {
+        public ErrorDescriptionContext(ActionDescriptor actionDescriptor)
+        {
+            ActionDescriptor = actionDescriptor;
+        }
+
+        public ActionDescriptor ActionDescriptor { get; }
+
+        public object Result { get; set; }
+    }
+}

src/Microsoft.AspNetCore.Mvc.Core/ErrorDescription/IErrorDescriptionFactory.cs
@@ -1,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc.Abstractions;
+
+namespace Microsoft.AspNetCore.Mvc.ErrorDescription
+{
+    /// <summary>
+    /// Defines a contract for creating or modifying an error response.
+    /// </summary>
+    public interface IErrorDescriptionFactory
+    {
+        object CreateErrorDescription(ActionDescriptor actionDescriptor, object result);
+    }
+}

dotnet/aspnetcore

src/Microsoft.AspNetCore.Mvc.ApiExplorer/DependencyInjection/MvcApiExplorerMvcCoreBuilderExtensions.cs
@@ -26,6 +26,8 @@ namespace Microsoft.Extensions.DependencyInjection
             services.TryAddSingleton<IApiDescriptionGroupCollectionProvider, ApiDescriptionGroupCollectionProvider>();
             services.TryAddEnumerable(
                 ServiceDescriptor.Transient<IApiDescriptionProvider, DefaultApiDescriptionProvider>());
+            services.TryAddEnumerable(
+                ServiceDescriptor.Transient<IApiDescriptionProvider, ProblemDetailsApiDescriptionProvider>());
         }
     }
 }

src/Microsoft.AspNetCore.Mvc.Core/Infrastructure/ErrorDescriptionContext.cs
@@ -1,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc.Abstractions;
+
+namespace Microsoft.AspNetCore.Mvc.Infrastructure
+{
+    public class ErrorDescriptionContext
+    {
+        public ErrorDescriptionContext(ActionDescriptor actionDescriptor)
+        {
+            ActionDescriptor = actionDescriptor;
+        }
+
+        public ActionDescriptor ActionDescriptor { get; }
+
+        public object Result { get; set; }
+    }
+}

src/Microsoft.AspNetCore.Mvc.Core/Infrastructure/IErrorDescriptionFactory.cs
@@ -1,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc.Abstractions;
+
+namespace Microsoft.AspNetCore.Mvc.Infrastructure
+{
+    /// <summary>
+    /// Defines a contract for creating or modifying an error response.
+    /// </summary>
+    public interface IErrorDescriptionFactory
+    {
+        object CreateErrorDescription(ActionDescriptor actionDescriptor, object result);
+    }
+}