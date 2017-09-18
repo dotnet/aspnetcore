aspnet/Mvc

Allow "page" route parameter to be used in Mvc controllers
Fixes #6660

test/Microsoft.AspNetCore.Mvc.FunctionalTests/BasicTests.cs
@@ -376,5 +376,31 @@ namespace Microsoft.AspNetCore.Mvc.FunctionalTests
             // Assert
             Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         }
+
+        [Fact]
+        public async Task UsingPageRouteParameterInConventionRouteWorks()
+        {
+            // Arrange
+            var expected = "ConventionalRoute - Hello from mypage";
+
+            // Act
+            var response = await Client.GetStringAsync("/PageRoute/ConventionalRoute/mypage");
+
+            // Assert
+            Assert.Equal(expected, response.Trim());
+        }
+
+        [Fact]
+        public async Task UsingPageRouteParameterInAttributeRouteWorks()
+        {
+            // Arrange
+            var expected = "AttributeRoute - Hello from test-page";
+
+            // Act
+            var response = await Client.GetStringAsync("/PageRoute/Attribute/test-page");
+
+            // Assert
+            Assert.Equal(expected, response.Trim());
+        }
     }
 }

dotnet/aspnetcore

test/Microsoft.AspNetCore.Mvc.FunctionalTests/BasicTests.cs
@@ -376,5 +376,31 @@ namespace Microsoft.AspNetCore.Mvc.FunctionalTests
             // Assert
             Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         }
+
+        [Fact]
+        public async Task UsingPageRouteParameterInConventionalRouteWorks()
+        {
+            // Arrange
+            var expected = "ConventionalRoute - Hello from mypage";
+
+            // Act
+            var response = await Client.GetStringAsync("/PageRoute/ConventionalRoute/mypage");
+
+            // Assert
+            Assert.Equal(expected, response.Trim());
+        }
+
+        [Fact]
+        public async Task UsingPageRouteParameterInAttributeRouteWorks()
+        {
+            // Arrange
+            var expected = "AttributeRoute - Hello from test-page";
+
+            // Act
+            var response = await Client.GetStringAsync("/PageRoute/Attribute/test-page");
+
+            // Assert
+            Assert.Equal(expected, response.Trim());
+        }
     }
 }