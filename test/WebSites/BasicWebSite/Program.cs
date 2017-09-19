davidrevoledo/Mvc

Improvements to the testing package.
* Detect a ConfigureDefaultBuilder static method on Program.cs of the test site
and use it to initialize the IWebHostBuilder instance that we configure for testing.
* Make use of the new service configuration extensibility points in Hosting to override
services for testing.
* Get rid of MvcWebApplicationBuilder and move MVC specific methods into extension methods.

test/WebSites/BasicWebSite/Program.cs
@@ -3,21 +3,27 @@
 
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BasicWebSite
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
+        public static void Main(string[] args) => CreateDefaultBuilder(args).Build().Run();
+
+        // Do not change. This is the pattern our test infrastructure uses to initialize a IWebHostBuilder from
+        // a users app.
+        public static IWebHostBuilder CreateDefaultBuilder(string[] args) =>
+            new WebHostBuilder()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
+                .ConfigureServices(s => s.AddSingleton(new TestService { Message = "true" }))
                 .UseKestrel()
-                .UseIISIntegration()
-                .Build();
+                .UseIISIntegration();
+    }
 
-            host.Run();
-        }
+    public class TestService
+    {
+        public string Message { get; set; }
     }
 }

dotnet/aspnetcore

test/WebSites/BasicWebSite/Program.cs
@@ -3,21 +3,27 @@
 
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BasicWebSite
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
+        public static void Main(string[] args) => CreateWebHostBuilder(args).Build().Run();
+
+        // Do not change. This is the pattern our test infrastructure uses to initialize a IWebHostBuilder from
+        // a users app.
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            new WebHostBuilder()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
+                .ConfigureServices(s => s.AddSingleton(new TestService { Message = "true" }))
                 .UseKestrel()
-                .UseIISIntegration()
-                .Build();
+                .UseIISIntegration();
+    }
 
-            host.Run();
-        }
+    public class TestService
+    {
+        public string Message { get; set; }
     }
 }