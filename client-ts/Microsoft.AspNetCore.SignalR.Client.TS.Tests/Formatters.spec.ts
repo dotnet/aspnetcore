4f4fb174ea03f3a16821406b613f5d5383342fe1
Pawel Kadluczka <pawelka@microsoft.com>
2017-09-22 17:51:25 -0700

    Replacing 8-byte-long length prefix with varint

--- client-ts/Microsoft.AspNetCore.SignalR.Client.TS.Tests/Formatters.spec.ts
+++ client-ts/Microsoft.AspNetCore.SignalR.Client.TS.Tests/Formatters.spec.ts
@@ -29,10 +29,11 @@ describe("Text Message Formatter", () => {
 
 describe("Binary Message Formatter", () => {
     ([
-        [[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], <Uint8Array[]>[ new Uint8Array([])]],
-        [[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0xff], <Uint8Array[]>[ new Uint8Array([0xff])]],
-        [[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0xff,
-          0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x7f], <Uint8Array[]>[ new Uint8Array([0xff]), new Uint8Array([0x7f])]],
+        [[], <Uint8Array[]>[]],
+        [[0x00], <Uint8Array[]>[ new Uint8Array([])]],
+        [[0x01, 0xff], <Uint8Array[]>[ new Uint8Array([0xff])]],
+        [[0x01, 0xff,
+          0x01, 0x7f], <Uint8Array[]>[ new Uint8Array([0xff]), new Uint8Array([0x7f])]],
     ] as [[number[], Uint8Array[]]]).forEach(([payload, expected_messages]) => {
         it(`should parse '${payload}' correctly`, () => {
             let messages = BinaryMessageFormat.parse(new Uint8Array(payload).buffer);
@@ -41,14 +42,15 @@ describe("Binary Message Formatter", () => {
     });
 
     ([
-        [[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], new Error("Cannot read message size")],
-        [[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x01, 0x80, 0x00], new Error("Cannot read message size")],
-        [[0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], new Error("Messages bigger than 2147483647 bytes are not supported")],
-        [[0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], new Error("Messages bigger than 2147483647 bytes are not supported")],
-        [[0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00], new Error("Messages bigger than 2147483647 bytes are not supported")],
-        [[0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00], new Error("Messages bigger than 2147483647 bytes are not supported")],
-        [[0x00, 0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00], new Error("Messages bigger than 2147483647 bytes are not supported")],
-        [[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00], new Error("Incomplete message")],
+        [[0x80], new Error("Cannot read message size.")],
+        [[0x02, 0x01, 0x80, 0x80], new Error("Cannot read message size.")],
+        [[0x07, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x80], new Error("Cannot read message size.")], // the size of the second message is cut
+        [[0x07, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01], new Error("Incomplete message.")], // second message has only size
+        [[0xff, 0xff, 0xff, 0xff, 0xff], new Error("Messages bigger than 2GB are not supported.")],
+        [[0x80, 0x80, 0x80, 0x80, 0x08], new Error("Messages bigger than 2GB are not supported.")],
+        [[0x80, 0x80, 0x80, 0x80, 0x80], new Error("Messages bigger than 2GB are not supported.")],
+        [[0x02, 0x00], new Error("Incomplete message.")],
+        [[0xff, 0xff, 0xff, 0xff, 0x07], new Error("Incomplete message.")]
     ] as [[number[], Error]]).forEach(([payload, expected_error]) => {
         it(`should fail to parse '${payload}'`, () => {
             expect(() => BinaryMessageFormat.parse(new Uint8Array(payload).buffer)).toThrow(expected_error);
@@ -56,8 +58,8 @@ describe("Binary Message Formatter", () => {
     });
 
     ([
-        [[], [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]],
-        [[0x20], [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x20]],
+        [[], [0x00]],
+        [[0x20], [0x01, 0x20]],
     ] as [[number[], number[]]]).forEach(([input, expected_payload]) => {
         it(`should write '${input}'`, () => {
             let actual = new Uint8Array(BinaryMessageFormat.write(new Uint8Array(input)));
@@ -65,4 +67,18 @@ describe("Binary Message Formatter", () => {
             expect(actual).toEqual(expected);
         })
     });
+
+    ([0x0000, 0x0001, 0x007f, 0x0080, 0x3fff, 0x4000, 0xc0de] as number[]).forEach(size => {
+        it(`messages should be roundtrippable (message size: '${size}')`, () => {
+            const message = [];
+            for (let i = 0; i < size; i++) {
+                message.push(i & 0xff);
+            }
+
+            var payload = new Uint8Array(message);
+            expect(payload).toEqual(BinaryMessageFormat.parse(BinaryMessageFormat.write(payload))[0]);
+        })
+    });
+
+
 });
--- src/Microsoft.AspNetCore.SignalR.Common/Internal/Formatters/BinaryMessageParser.cs
+++ src/Microsoft.AspNetCore.SignalR.Common/Internal/Formatters/BinaryMessageParser.cs
@@ -2,44 +2,70 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Binary;
 
 namespace Microsoft.AspNetCore.SignalR.Internal.Formatters
 {
     public static class BinaryMessageParser
     {
+        private static int[] _numBitsToShift = new[] { 0, 7, 14, 21, 28 };
+        private const int MaxLengthPrefixSize = 5;
+
         public static bool TryParseMessage(ref ReadOnlyBuffer<byte> buffer, out ReadOnlyBuffer<byte> payload)
         {
-            long length = 0;
-            payload = default(ReadOnlyBuffer<byte>);
+            payload = default;
 
-            if (buffer.Length < sizeof(long))
+            if (buffer.IsEmpty)
             {
                 return false;
             }
 
-            // Read the length
-            length = buffer.Span.Slice(0, sizeof(long)).ReadBigEndian<long>();
+            // The payload starts with a length prefix encoded as a VarInt. VarInts use the most significant bit
+            // as a marker whether the byte is the last byte of the VarInt or if it spans to the next byte. Bytes
+            // appear in the reverse order - i.e. the first byte contains the least significant bits of the value
+            // Examples:
+            // VarInt: 0x35 - %00110101 - the most significant bit is 0 so the value is %x0110101 i.e. 0x35 (53)
+            // VarInt: 0x80 0x25 - %10000000 %00101001 - the most significant bit of the first byte is 1 so the
+            // remaining bits (%x0000000) are the lowest bits of the value. The most significant bit of the second
+            // byte is 0 meaning this is last byte of the VarInt. The actual value bits (%x0101001) need to be
+            // prepended to the bits we already read so the values is %01010010000000 i.e. 0x1480 (5248)
+            // We support paylads up to 2GB so the biggest number we support is 7fffffff which when encoded as
+            // VarInt is 0xFF 0xFF 0xFF 0xFF 0x7F - hence the maximum length prefix is 5 bytes.
 
-            if (length > Int32.MaxValue)
+            var length = 0U;
+            var numBytes = 0;
+
+            var lengthPrefixBuffer = buffer.Span.Slice(0, Math.Min(MaxLengthPrefixSize, buffer.Length));
+            byte byteRead;
+            do
             {
-                throw new FormatException("Messages over 2GB in size are not supported");
+                byteRead = lengthPrefixBuffer[numBytes];
+                length = length | (((uint)(byteRead & 0x7f)) << _numBitsToShift[numBytes]);
+                numBytes++;
+            }
+            while (numBytes < lengthPrefixBuffer.Length && ((byteRead & 0x80) != 0));
+
+            // size bytes are missing
+            if ((byteRead & 0x80) != 0 && (numBytes < MaxLengthPrefixSize))
+            {
+                return false;
             }
 
-            // Skip over the length
-            var remaining = buffer.Slice(sizeof(long));
+            if ((byteRead & 0x80) != 0 || (numBytes == MaxLengthPrefixSize && byteRead > 7))
+            {
+                throw new FormatException("Messages over 2GB in size are not supported.");
+            }
 
             // We don't have enough data
-            while (remaining.Length < (int)length)
+            if (buffer.Length < length + numBytes)
             {
                 return false;
             }
 
             // Get the payload
-            payload = remaining.Slice(0, (int)length);
+            payload = buffer.Slice(numBytes, (int)length);
 
             // Skip the payload
-            buffer = remaining.Slice((int)length);
+            buffer = buffer.Slice(numBytes + (int)length);
             return true;
         }
     }
