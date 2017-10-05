3c5d283689879ed50f5a249146c6c3b559cef53a
Kai Ruhnau <Kai.Ruhnau@live.com>
2017-10-05 10:48:18 +0200

    Fix the VarInt example for the 2GiB
    `0x7fffffff` has 31 binary ones. The first four bytes of the VarInt account for 4*7=28, so the fifth byte only needs the remaining three

--- src/Microsoft.AspNetCore.SignalR.Common/Internal/Formatters/BinaryMessageParser.cs
+++ src/Microsoft.AspNetCore.SignalR.Common/Internal/Formatters/BinaryMessageParser.cs
@@ -29,7 +29,7 @@ namespace Microsoft.AspNetCore.SignalR.Internal.Formatters
             // byte is 0 meaning this is last byte of the VarInt. The actual value bits (%x0101001) need to be
             // prepended to the bits we already read so the values is %01010010000000 i.e. 0x1480 (5248)
             // We support paylads up to 2GB so the biggest number we support is 7fffffff which when encoded as
-            // VarInt is 0xFF 0xFF 0xFF 0xFF 0x7F - hence the maximum length prefix is 5 bytes.
+            // VarInt is 0xFF 0xFF 0xFF 0xFF 0x07 - hence the maximum length prefix is 5 bytes.
 
             var length = 0U;
             var numBytes = 0;
