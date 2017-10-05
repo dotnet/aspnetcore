5e752169d6e15b3a6d10e1fddd33479c66c7b13f
Murat Girgin <murat@girg.in>
2017-10-04 20:36:45 -0700

    Vue.js template: Use async/await

--- src/Microsoft.AspNetCore.SpaTemplates/content/Vue-CSharp/ClientApp/components/fetchdata/fetchdata.ts
+++ src/Microsoft.AspNetCore.SpaTemplates/content/Vue-CSharp/ClientApp/components/fetchdata/fetchdata.ts
@@ -12,11 +12,14 @@ interface WeatherForecast {
 export default class FetchDataComponent extends Vue {
     forecasts: WeatherForecast[] = [];
 
-    mounted() {
-        fetch('api/SampleData/WeatherForecasts')
-            .then(response => response.json() as Promise<WeatherForecast[]>)
-            .then(data => {
-                this.forecasts = data;
-            });
+    async mounted() {
+        try {
+            let response = await fetch('api/SampleData/WeatherForecasts'); 
+            let data = await response.json();
+            this.forecasts = data;
+         }
+        catch (err) {
+            console.log(err);
+        }
     }
 }
