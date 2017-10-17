4bb84cf6d0d9e2761f4a5094a8f254a8579fba65
Nate McMaster <nate.mcmaster@microsoft.com>
2017-10-17 15:46:53 -0700

    Ensure the deb/rpm installers use the same dotnet-core version
     - Remove unnecessary tasks and scripts
     - Ensure the KOREBUILD_DOTNET_* environment variables are preserved in the docker build context
     - Other MSBuild cleanup of the targets

--- build/tools/scripts/GetSharedFrameworkVersion.ps1
+++
@@ -1,6 +0,0 @@
-$infoOutput = dotnet --info
-$versions = $infoOutput | Select-String -Pattern "version"
-$FXVersionRaw = $versions | Select-Object -Last 1
-$FXVersionString = $FXVersionRaw.ToString()
-$FXVersion = $FXVersionString.SubString($FXVersionString.IndexOf(':') + 1).Trim()
-Write-Host $FXVersion
