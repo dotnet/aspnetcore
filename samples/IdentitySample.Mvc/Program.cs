f147951d92ff5b4413d7ffdd55dcc91dfcfdfdca
Hao Kung <haok@microsoft.com>
2017-10-24 14:54:41 -0700

    Revert "use WebHost.CreateDefaultBuilder in IdentitySample.Mvc"
    This reverts commit 20ec50d5d26f2052afcfcf640c69c6049208e799.

--- samples/IdentitySample.Mvc/Program.cs
+++ samples/IdentitySample.Mvc/Program.cs
@@ -1,15 +1,20 @@
-using Microsoft.AspNetCore;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 
 namespace IdentitySample
 {
     public static class Program
     {
-        public static void Main(string[] args) => BuildWebHost(args).Run();
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
+
+            host.Run();
+        }
     }
 }
