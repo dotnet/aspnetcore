a01e23250f98fbce016cd2b3bdf69e43fc4505c8
Pranav K <prkrishn@hotmail.com>
2017-11-09 14:59:33 -0800

    Preparing to update to 2.2 SDK

--- test/Microsoft.AspNetCore.Mvc.FunctionalTests/HtmlGenerationTest.cs
+++ test/Microsoft.AspNetCore.Mvc.FunctionalTests/HtmlGenerationTest.cs
@@ -255,7 +255,7 @@ namespace Microsoft.AspNetCore.Mvc.FunctionalTests
 #endif
         }
 
-        [Fact]
+        [Fact(Skip = "https://github.com/aspnet/Mvc/issues/7042")]
         public async Task CacheTagHelper_CanCachePortionsOfViewsPartialViewsAndViewComponents()
         {
             // Arrange
@@ -440,7 +440,7 @@ namespace Microsoft.AspNetCore.Mvc.FunctionalTests
             Assert.Equal(expected2, response4.Trim());
         }
 
-        [Fact]
+        [Fact(Skip = "https://github.com/aspnet/Mvc/issues/7042")]
         public async Task CacheTagHelper_BubblesExpirationOfNestedTagHelpers()
         {
             // Arrange & Act - 1
