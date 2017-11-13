b6144142feb9509566c3251e2d1a7fa49c3d7817
Pranav K <prkrishn@hotmail.com>
2017-11-13 13:10:07 -0800

    Update samples and tests to target netcoreapp2.1

--- test/WebSites/RazorWebSite/Services/FrameworkSpecificHelper.cs
+++ test/WebSites/RazorWebSite/Services/FrameworkSpecificHelper.cs
@@ -10,7 +10,7 @@ namespace RazorWebSite
             return "This method is running from NETCOREAPP2_0";
         }
 
-#if NETCOREAPP2_0_CUSTOM_DEFINE
+#if NETCOREAPP2_0_CUSTOM_DEFINE || NETCOREAPP2_1_CUSTOM_DEFINE
         public string ExecuteNetCoreApp2_0Operation()
         {
             return "This method is only defined in NETCOREAPP2_0";
