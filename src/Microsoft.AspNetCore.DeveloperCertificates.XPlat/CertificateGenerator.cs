6c4427cde5f05c64d792062036a7312a0edae309
Javier Calvarro Nelson <jacalvar@microsoft.com>
2017-11-15 11:42:49 -0800

    Added a package for generating HTTPS Certificates on CLI first run scenarios

---
+++ src/Microsoft.AspNetCore.DeveloperCertificates.XPlat/CertificateGenerator.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.AspNetCore.Certificates.Generation;
+
+namespace Microsoft.AspNetCore.DeveloperCertificates.XPlat
+{
+    public static class CertificateGenerator
+    {
+        public static void GenerateAspNetHttpsCertificate()
+        {
+            var manager = new CertificateManager();
+            var now = DateTimeOffset.Now;
+            manager.EnsureAspNetCoreHttpsDevelopmentCertificate(now, now.AddYears(1));
+        }
+    }
+}
