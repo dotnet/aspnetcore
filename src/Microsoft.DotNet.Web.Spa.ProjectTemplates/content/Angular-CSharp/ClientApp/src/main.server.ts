505ebcf1e6a48f1a08c8e316eb95011dda043fe4
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2017-11-22 15:17:34 +0000

    Update to newer Angular SSR APIs to support AoT-enabled builds

--- src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/Angular-CSharp/ClientApp/src/main.server.ts
+++ src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/Angular-CSharp/ClientApp/src/main.server.ts
@@ -1,38 +1,29 @@
+import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
-import 'zone.js';
-import 'rxjs/add/operator/first';
+import { renderModule, renderModuleFactory } from '@angular/platform-server';
 import { APP_BASE_HREF } from '@angular/common';
-import { enableProdMode, ApplicationRef, NgModule, NgZone, ValueProvider } from '@angular/core';
-import { platformDynamicServer, PlatformState, INITIAL_CONFIG } from '@angular/platform-server';
-import { AppServerModule } from './app/app.server.module';
-import { createServerRenderer, RenderResult } from 'aspnet-prerendering';
+import { enableProdMode } from '@angular/core';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+import { createServerRenderer } from 'aspnet-prerendering';
+export { AppServerModule } from './app/app.server.module';
+const { AppServerModule, AppServerModuleNgFactory, LAZY_MODULE_MAP } = (module as any).exports;
 
 enableProdMode();
 
 export default createServerRenderer(params => {
-    const providers = [
-        { provide: INITIAL_CONFIG, useValue: { document: params.data.originalHtml, url: params.url } },
-        { provide: APP_BASE_HREF, useValue: params.baseUrl },
-        { provide: 'BASE_URL', useValue: params.origin + params.baseUrl },
-    ];
+  const options = {
+    document: params.data.originalHtml,
+    url: params.url,
+    extraProviders: [
+      provideModuleMap(LAZY_MODULE_MAP),
+      { provide: APP_BASE_HREF, useValue: params.baseUrl },
+      { provide: 'BASE_URL', useValue: params.origin + params.baseUrl }
+    ]
+  };
 
-    return platformDynamicServer(providers).bootstrapModule(AppServerModule).then(moduleRef => {
-        const appRef: ApplicationRef = moduleRef.injector.get(ApplicationRef);
-        const state = moduleRef.injector.get(PlatformState);
-        const zone: NgZone = moduleRef.injector.get(NgZone);
-
-        return new Promise<RenderResult>((resolve, reject) => {
-            zone.onError.subscribe((errorInfo: any) => reject(errorInfo));
-            appRef.isStable.first(isStable => isStable).subscribe(() => {
-                // Because 'onStable' fires before 'onError', we have to delay slightly before
-                // completing the request in case there's an error to report
-                setImmediate(() => {
-                    resolve({
-                        html: state.renderToString()
-                    });
-                    moduleRef.destroy();
-                });
-            });
-        });
-    });
+  const renderPromise = AppServerModuleNgFactory
+    ? /* AoT */ renderModuleFactory(AppServerModuleNgFactory, options)
+    : /* dev */ renderModule(AppServerModule, options);
+    
+  return renderPromise.then(html => ({ html }));
 });
