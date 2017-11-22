LeandroCannizzaro/Razor

Added TagHelper discovery to Razor project systen

src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/ProjectSnapshotManagerBase.cs
@@ -17,6 +17,8 @@ namespace Microsoft.CodeAnalysis.Razor.ProjectSystem
 
         public abstract void ProjectRemoved(Project underlyingProject);
 
+        public abstract void ProjectBuilt(Project underlyingProject);
+
         public abstract void ProjectsCleared();
 
         public abstract void ReportError(Exception exception);

dotnet/aspnetcore

src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/ProjectSnapshotManagerBase.cs
@@ -17,6 +17,8 @@ namespace Microsoft.CodeAnalysis.Razor.ProjectSystem
 
         public abstract void ProjectRemoved(Project underlyingProject);
 
+        public abstract void ProjectBuildComplete(Project underlyingProject);
+
         public abstract void ProjectsCleared();
 
         public abstract void ReportError(Exception exception);