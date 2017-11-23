40603f15661849cc31da51002b47bc2053e95aa8
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2017-11-23 12:22:58 +0000

    In Angular template, make nav button collapsing work (without importing all bootstrap.js scripts)

--- src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/Angular-CSharp/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/Angular-CSharp/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent {
+  isExpanded = false;
+
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
 }
