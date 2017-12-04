bc8b2c86fdda75380941cae18a57d1c54e736347
N. Taylor Mullen <nimullen@microsoft.com>
2017-12-04 10:42:57 -0800

    Add Mac implementation of VisualStudioForegroundDispatcher.
    #1789

---
+++ src/Microsoft.VisualStudio.Mac.LanguageServices.Razor/VisualStudioForegroundDispatcher.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Composition;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.Razor;
+
+namespace Microsoft.VisualStudio.Mac.LanguageServices.Razor
+{
+    [Shared]
+    [ExportWorkspaceService(typeof(ForegroundDispatcher), ServiceLayer.Host)]
+    internal class VisualStudioForegroundDispatcher : ForegroundDispatcher
+    {
+        public override TaskScheduler BackgroundScheduler { get; } = TaskScheduler.Default;
+
+        public override TaskScheduler ForegroundScheduler { get; } = MonoDevelop.Core.Runtime.MainTaskScheduler;
+
+        public override bool IsForegroundThread => MonoDevelop.Core.Runtime.IsMainThread;
+    }
+}
