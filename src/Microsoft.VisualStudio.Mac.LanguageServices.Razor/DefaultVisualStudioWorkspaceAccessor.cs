f67670927f6e13242d8b3797611a480eb0c336ad
N. Taylor Mullen <nimullen@microsoft.com>
2017-12-04 10:40:50 -0800

    Add Mac implementation of VisualStudioWorkspaceAccessor.
    #1789

---
+++ src/Microsoft.VisualStudio.Mac.LanguageServices.Razor/DefaultVisualStudioWorkspaceAccessor.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.ComponentModel.Composition;
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.Editor.Razor;
+using MonoDevelop.Ide.TypeSystem;
+
+namespace Microsoft.VisualStudio.Mac.LanguageServices.Razor
+{
+    [System.Composition.Shared]
+    [Export(typeof(VisualStudioWorkspaceAccessor))]
+    internal class DefaultVisualStudioWorkspaceAccessor : VisualStudioWorkspaceAccessor
+    {
+        public DefaultVisualStudioWorkspaceAccessor()
+        {
+            Workspace = TypeSystemService.Workspace;
+        }
+
+        public override Workspace Workspace { get; }
+    }
+}
