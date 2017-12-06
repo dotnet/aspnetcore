a63b695db86a42c663e820e86649bc973316ad96
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2017-12-06 23:17:23 +0000

    Begin on DevHost and sample of standalone app

---
+++ src/Microsoft.Blazor.DevHost/Server/Startup.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Blazor.DevHost.Server
+{
+    internal class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseBlazor();
+        }
+    }
+}
