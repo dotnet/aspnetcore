aspnet/Mvc

[Fixes #6533] Log when XML formatters fail to create a serializer

src/Microsoft.AspNetCore.Mvc.Formatters.Xml/Internal/MvcXmlSerializerMvcOptionsSetup.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Microsoft.AspNetCore.Mvc.Formatters.Xml.Internal
@@ -11,13 +13,31 @@ namespace Microsoft.AspNetCore.Mvc.Formatters.Xml.Internal
     /// </summary>
     public class MvcXmlSerializerMvcOptionsSetup : IConfigureOptions<MvcOptions>
     {
+        private readonly ILoggerFactory _loggerFactory;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="MvcXmlSerializerMvcOptionsSetup"/>.
+        /// </summary>
+        /// <param name="loggerFactory">The <see cref="ILoggerFactory"/>.</param>
+        public MvcXmlSerializerMvcOptionsSetup(ILoggerFactory loggerFactory)
+        {
+            if (loggerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(loggerFactory));
+            }
+
+            _loggerFactory = loggerFactory;
+        }
+
         /// <summary>
         /// Adds the XML serializer formatters to <see cref="MvcOptions"/>.
         /// </summary>
         /// <param name="options">The <see cref="MvcOptions"/>.</param>
         public void Configure(MvcOptions options)
         {
-            options.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+            var outputFormatterLogger = _loggerFactory.CreateLogger<XmlSerializerOutputFormatter>();
+
+            options.OutputFormatters.Add(new XmlSerializerOutputFormatter(outputFormatterLogger));
             options.InputFormatters.Add(new XmlSerializerInputFormatter(options.SuppressInputFormatterBuffering));
         }
     }

src/Microsoft.AspNetCore.Mvc.Formatters.Xml/XmlSerializerOutputFormatter.cs
@@ -11,6 +11,7 @@ using System.Xml;
 using System.Xml.Serialization;
 using Microsoft.AspNetCore.Mvc.Formatters.Xml;
 using Microsoft.AspNetCore.Mvc.Formatters.Xml.Internal;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.AspNetCore.Mvc.Formatters
 {
@@ -21,13 +22,24 @@ namespace Microsoft.AspNetCore.Mvc.Formatters
     public class XmlSerializerOutputFormatter : TextOutputFormatter
     {
         private readonly ConcurrentDictionary<Type, object> _serializerCache = new ConcurrentDictionary<Type, object>();
+        private readonly ILogger _logger;
 
         /// <summary>
         /// Initializes a new instance of <see cref="XmlSerializerOutputFormatter"/>
         /// with default XmlWriterSettings.
         /// </summary>
-        public XmlSerializerOutputFormatter() :
-            this(FormattingUtilities.GetDefaultXmlWriterSettings())
+        public XmlSerializerOutputFormatter()
+            : this(FormattingUtilities.GetDefaultXmlWriterSettings())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="XmlSerializerOutputFormatter"/>
+        /// with default XmlWriterSettings.
+        /// </summary>
+        /// <param name="logger">The <see cref="ILogger"/>.</param>
+        public XmlSerializerOutputFormatter(ILogger logger)
+            : this(FormattingUtilities.GetDefaultXmlWriterSettings(), logger)
         {
         }
 
@@ -36,6 +48,16 @@ namespace Microsoft.AspNetCore.Mvc.Formatters
         /// </summary>
         /// <param name="writerSettings">The settings to be used by the <see cref="XmlSerializer"/>.</param>
         public XmlSerializerOutputFormatter(XmlWriterSettings writerSettings)
+            : this(writerSettings, logger: null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="XmlSerializerOutputFormatter"/>
+        /// </summary>
+        /// <param name="writerSettings">The settings to be used by the <see cref="XmlSerializer"/>.</param>
+        /// <param name="logger">The <see cref="ILogger"/>.</param>
+        public XmlSerializerOutputFormatter(XmlWriterSettings writerSettings, ILogger logger)
         {
             if (writerSettings == null)
             {
@@ -54,6 +76,8 @@ namespace Microsoft.AspNetCore.Mvc.Formatters
             WrapperProviderFactories = new List<IWrapperProviderFactory>();
             WrapperProviderFactories.Add(new EnumerableWrapperProviderFactory(WrapperProviderFactories));
             WrapperProviderFactories.Add(new SerializableErrorWrapperProviderFactory());
+
+            _logger = logger;
         }
 
         /// <summary>
@@ -114,8 +138,10 @@ namespace Microsoft.AspNetCore.Mvc.Formatters
                 // If the serializer does not support this type it will throw an exception.
                 return new XmlSerializer(type);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                _logger?.FailedToCreateXmlSerializer(type.FullName, ex);
+
                 // We do not surface the caught exception because if CanWriteResult returns
                 // false, then this Formatter is not picked up at all.
                 return null;

dotnet/aspnetcore

src/Microsoft.AspNetCore.Mvc.Formatters.Xml/Internal/MvcXmlSerializerMvcOptionsSetup.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Microsoft.AspNetCore.Mvc.Formatters.Xml.Internal
@@ -11,13 +13,29 @@ namespace Microsoft.AspNetCore.Mvc.Formatters.Xml.Internal
     /// </summary>
     public class MvcXmlSerializerMvcOptionsSetup : IConfigureOptions<MvcOptions>
     {
+        private readonly ILoggerFactory _loggerFactory;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="MvcXmlSerializerMvcOptionsSetup"/>.
+        /// </summary>
+        /// <param name="loggerFactory">The <see cref="ILoggerFactory"/>.</param>
+        public MvcXmlSerializerMvcOptionsSetup(ILoggerFactory loggerFactory)
+        {
+            if (loggerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(loggerFactory));
+            }
+
+            _loggerFactory = loggerFactory;
+        }
+
         /// <summary>
         /// Adds the XML serializer formatters to <see cref="MvcOptions"/>.
         /// </summary>
         /// <param name="options">The <see cref="MvcOptions"/>.</param>
         public void Configure(MvcOptions options)
         {
-            options.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+            options.OutputFormatters.Add(new XmlSerializerOutputFormatter(_loggerFactory));
             options.InputFormatters.Add(new XmlSerializerInputFormatter(options));
         }
     }

src/Microsoft.AspNetCore.Mvc.Formatters.Xml/XmlSerializerOutputFormatter.cs
@@ -11,6 +11,7 @@ using System.Xml;
 using System.Xml.Serialization;
 using Microsoft.AspNetCore.Mvc.Formatters.Xml;
 using Microsoft.AspNetCore.Mvc.Formatters.Xml.Internal;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.AspNetCore.Mvc.Formatters
 {
@@ -21,13 +22,33 @@ namespace Microsoft.AspNetCore.Mvc.Formatters
     public class XmlSerializerOutputFormatter : TextOutputFormatter
     {
         private readonly ConcurrentDictionary<Type, object> _serializerCache = new ConcurrentDictionary<Type, object>();
+        private readonly ILogger _logger;
 
         /// <summary>
         /// Initializes a new instance of <see cref="XmlSerializerOutputFormatter"/>
-        /// with default XmlWriterSettings.
+        /// with default <see cref="XmlWriterSettings"/>.
         /// </summary>
-        public XmlSerializerOutputFormatter() :
-            this(FormattingUtilities.GetDefaultXmlWriterSettings())
+        public XmlSerializerOutputFormatter()
+            : this(FormattingUtilities.GetDefaultXmlWriterSettings())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="XmlSerializerOutputFormatter"/>
+        /// with default <see cref="XmlWriterSettings"/>.
+        /// </summary>
+        /// <param name="loggerFactory">The <see cref="ILoggerFactory"/>.</param>
+        public XmlSerializerOutputFormatter(ILoggerFactory loggerFactory)
+            : this(FormattingUtilities.GetDefaultXmlWriterSettings(), loggerFactory)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="XmlSerializerOutputFormatter"/>.
+        /// </summary>
+        /// <param name="writerSettings">The settings to be used by the <see cref="XmlSerializer"/>.</param>
+        public XmlSerializerOutputFormatter(XmlWriterSettings writerSettings)
+            : this(writerSettings, loggerFactory: null)
         {
         }
 
@@ -35,7 +56,8 @@ namespace Microsoft.AspNetCore.Mvc.Formatters
         /// Initializes a new instance of <see cref="XmlSerializerOutputFormatter"/>
         /// </summary>
         /// <param name="writerSettings">The settings to be used by the <see cref="XmlSerializer"/>.</param>
-        public XmlSerializerOutputFormatter(XmlWriterSettings writerSettings)
+        /// <param name="loggerFactory">The <see cref="ILoggerFactory"/>.</param>
+        public XmlSerializerOutputFormatter(XmlWriterSettings writerSettings, ILoggerFactory loggerFactory)
         {
             if (writerSettings == null)
             {
@@ -54,6 +76,8 @@ namespace Microsoft.AspNetCore.Mvc.Formatters
             WrapperProviderFactories = new List<IWrapperProviderFactory>();
             WrapperProviderFactories.Add(new EnumerableWrapperProviderFactory(WrapperProviderFactories));
             WrapperProviderFactories.Add(new SerializableErrorWrapperProviderFactory());
+
+            _logger = loggerFactory?.CreateLogger(GetType());
         }
 
         /// <summary>
@@ -114,8 +138,10 @@ namespace Microsoft.AspNetCore.Mvc.Formatters
                 // If the serializer does not support this type it will throw an exception.
                 return new XmlSerializer(type);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                _logger?.FailedToCreateXmlSerializer(type.FullName, ex);
+
                 // We do not surface the caught exception because if CanWriteResult returns
                 // false, then this Formatter is not picked up at all.
                 return null;