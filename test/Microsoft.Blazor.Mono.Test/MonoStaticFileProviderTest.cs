fb8a01a1381e72914d69fd6f0668567e3bd3e732
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2017-12-11 17:00:32 +0000

    Fix tests in preceding commit

--- test/Microsoft.Blazor.Mono.Test/MonoStaticFileProviderTest.cs
+++ test/Microsoft.Blazor.Mono.Test/MonoStaticFileProviderTest.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Linq;
 using Xunit;
 
 namespace Microsoft.Blazor.Mono.Test
@@ -12,16 +13,16 @@ namespace Microsoft.Blazor.Mono.Test
         {
             // The collection is small enough that we can assert the exact full list
 
-            Assert.Collection(MonoStaticFileProvider.JsFiles.GetDirectoryContents("/"),
+            Assert.Collection(MonoStaticFileProvider.JsFiles.GetDirectoryContents("/").OrderBy(i => i.Name),
                 item => Assert.Equal("/asmjs", item.PhysicalPath),
                 item => Assert.Equal("/wasm", item.PhysicalPath));
 
-            Assert.Collection(MonoStaticFileProvider.JsFiles.GetDirectoryContents("/asmjs"),
+            Assert.Collection(MonoStaticFileProvider.JsFiles.GetDirectoryContents("/asmjs").OrderBy(i => i.Name),
                 item => Assert.Equal("/asmjs/mono.asm.js", item.PhysicalPath),
                 item => Assert.Equal("/asmjs/mono.js", item.PhysicalPath),
                 item => Assert.Equal("/asmjs/mono.js.mem", item.PhysicalPath));
 
-            Assert.Collection(MonoStaticFileProvider.JsFiles.GetDirectoryContents("/wasm"),
+            Assert.Collection(MonoStaticFileProvider.JsFiles.GetDirectoryContents("/wasm").OrderBy(i => i.Name),
                 item => Assert.Equal("/wasm/mono.js", item.PhysicalPath),
                 item => Assert.Equal("/wasm/mono.wasm", item.PhysicalPath));
         }
@@ -30,18 +31,18 @@ namespace Microsoft.Blazor.Mono.Test
         public void SuppliesBclFiles()
         {
             Assert.Collection(MonoStaticFileProvider.BclFiles.GetDirectoryContents("/"),
-                item => Assert.Equal("/bin", item.PhysicalPath));
+                item => Assert.Equal("/bcl", item.PhysicalPath));
 
-            Assert.Collection(MonoStaticFileProvider.BclFiles.GetDirectoryContents("/bin"),
-                item => Assert.Equal("/bin/mscorlib.dll", item.PhysicalPath),
-                item => Assert.Equal("/bin/System.Core.dll", item.PhysicalPath),
-                item => Assert.Equal("/bin/System.dll", item.PhysicalPath),
-                item => Assert.Equal("/bin/Facades", item.PhysicalPath));
+            Assert.Collection(MonoStaticFileProvider.BclFiles.GetDirectoryContents("/bcl").OrderBy(i => i.Name),
+                item => Assert.Equal("/bcl/Facades", item.PhysicalPath),
+                item => Assert.Equal("/bcl/mscorlib.dll", item.PhysicalPath),
+                item => Assert.Equal("/bcl/System.Core.dll", item.PhysicalPath),
+                item => Assert.Equal("/bcl/System.dll", item.PhysicalPath));
 
             // Not an exhaustive list. The full list is long.
-            var actualFacades = MonoStaticFileProvider.BclFiles.GetDirectoryContents("/bin/Facades");
-            Assert.Contains(actualFacades, item => item.PhysicalPath == "/bin/Facades/netstandard.dll");
-            Assert.Contains(actualFacades, item => item.PhysicalPath == "/bin/Facades/System.Console.dll");
+            var actualFacades = MonoStaticFileProvider.BclFiles.GetDirectoryContents("/bcl/Facades");
+            Assert.Contains(actualFacades, item => item.PhysicalPath == "/bcl/Facades/netstandard.dll");
+            Assert.Contains(actualFacades, item => item.PhysicalPath == "/bcl/Facades/System.Console.dll");
         }
     }
 }
