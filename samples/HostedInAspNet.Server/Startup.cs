c5a104c56de6e0c5e98ae27a8080bc13cee9525b
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2017-12-12 19:54:27 +0000

    Remove redundant 'using'

--- samples/HostedInAspNet.Server/Startup.cs
+++ samples/HostedInAspNet.Server/Startup.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
