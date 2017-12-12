velyo/Razor

Add Mac implementation of FileChangeTracker.
- Added a LanguageServices Mac test project.
- Added a few tests for the new `DefaultFileChangeTracker`. There's currently an issue with the MonoDevelop.Core binaries we're compiling against which doesn't allow me to unit test other pieces of the class (they depend on instantiating mono develop objects).
- Added IVT from product projects to new mac testing project.

#1789

src/Microsoft.VisualStudio.Mac.LanguageServices.Razor/DefaultFileChangeTracker.cs
@@ -1,0 +1,130 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.CodeAnalysis.Razor;
+using Microsoft.VisualStudio.Editor.Razor;
+using MonoDevelop.Core;
+
+namespace Microsoft.VisualStudio.Mac.LanguageServices.Razor
+{
+    internal class DefaultFileChangeTracker : FileChangeTracker
+    {
+        private readonly ForegroundDispatcher _foregroundDispatcher;
+        private readonly string _normalizedFullPath;
+        private bool _listening;
+
+        public override event EventHandler<FileChangeEventArgs> Changed;
+
+        public DefaultFileChangeTracker(
+            string filePath,
+            ForegroundDispatcher foregroundDispatcher)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentException(Resources.ArgumentCannotBeNullOrEmpty, nameof(filePath));
+            }
+
+            if (foregroundDispatcher == null)
+            {
+                throw new ArgumentNullException(nameof(foregroundDispatcher));
+            }
+
+            FilePath = filePath;
+            _normalizedFullPath = NormalizePath(FilePath);
+            _foregroundDispatcher = foregroundDispatcher;
+        }
+
+        public override string FilePath { get; }
+
+        public override void StartListening()
+        {
+            _foregroundDispatcher.AssertForegroundThread();
+
+            if (_listening)
+            {
+                return;
+            }
+
+            AttachToFileServiceEvents();
+
+            _listening = true;
+        }
+
+        public override void StopListening()
+        {
+            _foregroundDispatcher.AssertForegroundThread();
+
+            if (!_listening)
+            {
+                return;
+            }
+
+            DetatchFromFileServiceEvents();
+
+            _listening = false;
+        }
+
+        // Virtual for testing
+        protected virtual void AttachToFileServiceEvents()
+        {
+            FileService.FileChanged += FileService_FileChanged;
+            FileService.FileCreated += FileService_FileCreated;
+            FileService.FileRemoved += FileService_FileRemoved;
+        }
+
+        // Virtual for testing
+        protected virtual void DetatchFromFileServiceEvents()
+        {
+            FileService.FileChanged -= FileService_FileChanged;
+            FileService.FileCreated -= FileService_FileCreated;
+            FileService.FileRemoved -= FileService_FileRemoved;
+        }
+
+        private void FileService_FileChanged(object sender, FileEventArgs args) => HandleFileChangeEvent(FileChangeKind.Changed, args);
+
+        private void FileService_FileCreated(object sender, FileEventArgs args) => HandleFileChangeEvent(FileChangeKind.Added, args);
+
+        private void FileService_FileRemoved(object sender, FileEventArgs args) => HandleFileChangeEvent(FileChangeKind.Removed, args);
+
+        private void HandleFileChangeEvent(FileChangeKind changeKind, FileEventArgs args)
+        {
+            _foregroundDispatcher.AssertForegroundThread();
+
+            if (Changed == null)
+            {
+                return;
+            }
+
+            foreach (var fileEvent in args)
+            {
+                if (fileEvent.IsDirectory)
+                {
+                    continue;
+                }
+
+                var normalizedEventPath = NormalizePath(fileEvent.FileName.FullPath);
+                if (string.Equals(_normalizedFullPath, normalizedEventPath, StringComparison.OrdinalIgnoreCase))
+                {
+                    OnChanged(changeKind);
+                    return;
+                }
+            }
+        }
+
+        private void OnChanged(FileChangeKind changeKind)
+        {
+            _foregroundDispatcher.AssertForegroundThread();
+
+            var args = new FileChangeEventArgs(FilePath, changeKind);
+            Changed?.Invoke(this, args);
+        }
+
+        private static string NormalizePath(string path)
+        {
+            path = path.Replace('\\', '/');
+
+            return path;
+        }
+    }
+}

test/Microsoft.VisualStudio.Mac.LanguageServices.Razor.Test/DefaultFileChangeTrackerTest.cs
@@ -1,0 +1,89 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.CodeAnalysis.Razor;
+using Microsoft.VisualStudio.Editor.Razor;
+using MonoDevelop.Core;
+using Moq;
+using Xunit;
+
+namespace Microsoft.VisualStudio.Mac.LanguageServices.Razor
+{
+    public class DefaultFileChangeTrackerTest : ForegroundDispatcherTestBase
+    {
+        [ForegroundFact]
+        public void StartListening_AdvisesForFileChange()
+        {
+            // Arrange
+            var tracker = new TestFileChangeTracker("C:/_ViewImports.cshtml", Dispatcher);
+
+            // Act
+            tracker.StartListening();
+
+            // Assert
+            Assert.Equal(1, tracker.AttachToFileServiceEventsCount);
+        }
+
+        [ForegroundFact]
+        public void StartListening_AlreadyListening_DoesNothing()
+        {
+            // Arrange
+            var tracker = new TestFileChangeTracker("C:/_ViewImports.cshtml", Dispatcher);
+            tracker.StartListening();
+
+            // Act
+            tracker.StartListening();
+
+            // Assert
+            Assert.Equal(1, tracker.AttachToFileServiceEventsCount);
+        }
+
+        [ForegroundFact]
+        public void StopListening_UnadvisesForFileChange()
+        {
+            // Arrange
+            var tracker = new TestFileChangeTracker("C:/_ViewImports.cshtml", Dispatcher);
+            tracker.StartListening(); // Start listening for changes.
+
+            // Act
+            tracker.StopListening();
+
+            // Assert
+            Assert.Equal(1, tracker.AttachToFileServiceEventsCount);
+            Assert.Equal(1, tracker.DetatchFromFileServiceEventsCount);
+        }
+
+        [ForegroundFact]
+        public void StopListening_NotListening_DoesNothing()
+        {
+            // Arrange
+            var tracker = new TestFileChangeTracker("C:/_ViewImports.cshtml", Dispatcher);
+
+            // Act
+            tracker.StopListening();
+
+            // Assert
+
+            Assert.Equal(0, tracker.AttachToFileServiceEventsCount);
+            Assert.Equal(0, tracker.DetatchFromFileServiceEventsCount);
+        }
+
+        private class TestFileChangeTracker : DefaultFileChangeTracker
+        {
+            public TestFileChangeTracker(
+                string filePath, 
+                ForegroundDispatcher foregroundDispatcher) : base(filePath, foregroundDispatcher)
+            {
+            }
+
+            public int AttachToFileServiceEventsCount { get; private set; }
+
+            public int DetatchFromFileServiceEventsCount { get; private set; }
+
+            protected override void AttachToFileServiceEvents() => AttachToFileServiceEventsCount++;
+
+            protected override void DetatchFromFileServiceEvents() => DetatchFromFileServiceEventsCount++;
+        }
+    }
+}

dotnet/aspnetcore

src/Microsoft.VisualStudio.Mac.LanguageServices.Razor/DefaultFileChangeTracker.cs
@@ -1,0 +1,130 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.CodeAnalysis.Razor;
+using Microsoft.VisualStudio.Editor.Razor;
+using MonoDevelop.Core;
+
+namespace Microsoft.VisualStudio.Mac.LanguageServices.Razor
+{
+    internal class DefaultFileChangeTracker : FileChangeTracker
+    {
+        private readonly ForegroundDispatcher _foregroundDispatcher;
+        private readonly string _normalizedFilePath;
+        private bool _listening;
+
+        public override event EventHandler<FileChangeEventArgs> Changed;
+
+        public DefaultFileChangeTracker(
+            string filePath,
+            ForegroundDispatcher foregroundDispatcher)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentException(Resources.ArgumentCannotBeNullOrEmpty, nameof(filePath));
+            }
+
+            if (foregroundDispatcher == null)
+            {
+                throw new ArgumentNullException(nameof(foregroundDispatcher));
+            }
+
+            FilePath = filePath;
+            _normalizedFilePath = NormalizePath(FilePath);
+            _foregroundDispatcher = foregroundDispatcher;
+        }
+
+        public override string FilePath { get; }
+
+        public override void StartListening()
+        {
+            _foregroundDispatcher.AssertForegroundThread();
+
+            if (_listening)
+            {
+                return;
+            }
+
+            AttachToFileServiceEvents();
+
+            _listening = true;
+        }
+
+        public override void StopListening()
+        {
+            _foregroundDispatcher.AssertForegroundThread();
+
+            if (!_listening)
+            {
+                return;
+            }
+
+            DetachFromFileServiceEvents();
+
+            _listening = false;
+        }
+
+        // Virtual for testing
+        protected virtual void AttachToFileServiceEvents()
+        {
+            FileService.FileChanged += FileService_FileChanged;
+            FileService.FileCreated += FileService_FileCreated;
+            FileService.FileRemoved += FileService_FileRemoved;
+        }
+
+        // Virtual for testing
+        protected virtual void DetachFromFileServiceEvents()
+        {
+            FileService.FileChanged -= FileService_FileChanged;
+            FileService.FileCreated -= FileService_FileCreated;
+            FileService.FileRemoved -= FileService_FileRemoved;
+        }
+
+        private void FileService_FileChanged(object sender, FileEventArgs args) => HandleFileChangeEvent(FileChangeKind.Changed, args);
+
+        private void FileService_FileCreated(object sender, FileEventArgs args) => HandleFileChangeEvent(FileChangeKind.Added, args);
+
+        private void FileService_FileRemoved(object sender, FileEventArgs args) => HandleFileChangeEvent(FileChangeKind.Removed, args);
+
+        private void HandleFileChangeEvent(FileChangeKind changeKind, FileEventArgs args)
+        {
+            _foregroundDispatcher.AssertForegroundThread();
+
+            if (Changed == null)
+            {
+                return;
+            }
+
+            foreach (var fileEvent in args)
+            {
+                if (fileEvent.IsDirectory)
+                {
+                    continue;
+                }
+
+                var normalizedEventPath = NormalizePath(fileEvent.FileName.FullPath);
+                if (string.Equals(_normalizedFilePath, normalizedEventPath, StringComparison.OrdinalIgnoreCase))
+                {
+                    OnChanged(changeKind);
+                    return;
+                }
+            }
+        }
+
+        private void OnChanged(FileChangeKind changeKind)
+        {
+            _foregroundDispatcher.AssertForegroundThread();
+
+            var args = new FileChangeEventArgs(FilePath, changeKind);
+            Changed?.Invoke(this, args);
+        }
+
+        private static string NormalizePath(string path)
+        {
+            path = path.Replace('\\', '/');
+
+            return path;
+        }
+    }
+}

test/Microsoft.VisualStudio.Mac.LanguageServices.Razor.Test/DefaultFileChangeTrackerTest.cs
@@ -1,0 +1,89 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.CodeAnalysis.Razor;
+using Microsoft.VisualStudio.Editor.Razor;
+using MonoDevelop.Core;
+using Moq;
+using Xunit;
+
+namespace Microsoft.VisualStudio.Mac.LanguageServices.Razor
+{
+    public class DefaultFileChangeTrackerTest : ForegroundDispatcherTestBase
+    {
+        [ForegroundFact]
+        public void StartListening_AdvisesForFileChange()
+        {
+            // Arrange
+            var tracker = new TestFileChangeTracker("C:/_ViewImports.cshtml", Dispatcher);
+
+            // Act
+            tracker.StartListening();
+
+            // Assert
+            Assert.Equal(1, tracker.AttachToFileServiceEventsCount);
+        }
+
+        [ForegroundFact]
+        public void StartListening_AlreadyListening_DoesNothing()
+        {
+            // Arrange
+            var tracker = new TestFileChangeTracker("C:/_ViewImports.cshtml", Dispatcher);
+            tracker.StartListening();
+
+            // Act
+            tracker.StartListening();
+
+            // Assert
+            Assert.Equal(1, tracker.AttachToFileServiceEventsCount);
+        }
+
+        [ForegroundFact]
+        public void StopListening_UnadvisesForFileChange()
+        {
+            // Arrange
+            var tracker = new TestFileChangeTracker("C:/_ViewImports.cshtml", Dispatcher);
+            tracker.StartListening(); // Start listening for changes.
+
+            // Act
+            tracker.StopListening();
+
+            // Assert
+            Assert.Equal(1, tracker.AttachToFileServiceEventsCount);
+            Assert.Equal(1, tracker.DetachFromFileServiceEventsCount);
+        }
+
+        [ForegroundFact]
+        public void StopListening_NotListening_DoesNothing()
+        {
+            // Arrange
+            var tracker = new TestFileChangeTracker("C:/_ViewImports.cshtml", Dispatcher);
+
+            // Act
+            tracker.StopListening();
+
+            // Assert
+
+            Assert.Equal(0, tracker.AttachToFileServiceEventsCount);
+            Assert.Equal(0, tracker.DetachFromFileServiceEventsCount);
+        }
+
+        private class TestFileChangeTracker : DefaultFileChangeTracker
+        {
+            public TestFileChangeTracker(
+                string filePath, 
+                ForegroundDispatcher foregroundDispatcher) : base(filePath, foregroundDispatcher)
+            {
+            }
+
+            public int AttachToFileServiceEventsCount { get; private set; }
+
+            public int DetachFromFileServiceEventsCount { get; private set; }
+
+            protected override void AttachToFileServiceEvents() => AttachToFileServiceEventsCount++;
+
+            protected override void DetachFromFileServiceEvents() => DetachFromFileServiceEventsCount++;
+        }
+    }
+}