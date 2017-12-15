7daec15a8eaf918730858a4502e3f29251a4d9d9
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2017-12-15 20:01:23 +0000

    Add TS part of RegisteredFunction API

---
+++ src/Microsoft.Blazor.Browser.JS/src/RegisteredFunction.ts
@@ -0,0 +1,12 @@
+﻿const registeredFunctions = {};
+
+// Code in Mono 'driver.c' looks for the registered functions here
+window['__blazorRegisteredFunctions'] = registeredFunctions;
+
+export function registerFunction(identifier: string, implementation: Function) {
+  if (registeredFunctions.hasOwnProperty(identifier)) {
+    throw new Error(`A function with the identifier '${identifier}' has already been registered.`);
+  }
+
+  registeredFunctions[identifier] = implementation;
+}
