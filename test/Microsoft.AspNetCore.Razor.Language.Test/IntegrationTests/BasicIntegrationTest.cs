aspnet/Razor

Moved CSharpCodeParser out of LegacyRazorDiagnostic

test/Microsoft.AspNetCore.Razor.Language.Test/IntegrationTests/BasicIntegrationTest.cs
@@ -80,10 +80,9 @@ namespace Microsoft.AspNetCore.Razor.Language.IntegrationTests
 
             var document = RazorCodeDocument.Create(TestRazorSourceDocument.Create("@!!!", fileName: "test.cshtml"));
 
-            var expected = RazorDiagnostic.Create(new RazorError(
-                LegacyResources.FormatParseError_Unexpected_Character_At_Start_Of_CodeBlock_CS("!"),
-                new SourceLocation("test.cshtml", 1, 0, 1),
-                length: 1));
+            var expected = RazorDiagnosticFactory.CreateParsing_UnexpectedCharacterAtStartOfCodeBlockCS(
+                                new SourceSpan(new SourceLocation("test.cshtml", 1, 0, 1), contentLength: 1),
+                                "!");
 
             // Act
             engine.Process(document);
@@ -102,10 +101,8 @@ namespace Microsoft.AspNetCore.Razor.Language.IntegrationTests
 
             var document = RazorCodeDocument.Create(TestRazorSourceDocument.Create("@{", fileName: "test.cshtml"));
 
-            var expected = RazorDiagnostic.Create(new RazorError(
-                LegacyResources.FormatParseError_Expected_EndOfBlock_Before_EOF(LegacyResources.BlockName_Code, "}", "{"),
-                new SourceLocation("test.cshtml", 1, 0, 1),
-                length: 1));
+            var expected = RazorDiagnosticFactory.CreateParsing_ExpectedEndOfBlockBeforeEOF(
+                new SourceSpan(new SourceLocation("test.cshtml", 1, 0, 1), contentLength: 1), LegacyResources.BlockName_Code, "}", "{");
 
             // Act
             engine.Process(document);

test/Microsoft.AspNetCore.Razor.Language.Test/Legacy/CSharpBlockTest.cs
@@ -499,10 +499,8 @@ while(true);", BlockKindInternal.Statement, SpanKindInternal.Code, acceptedChara
                 document,
                 BlockKindInternal.Statement,
                 SpanKindInternal.Code,
-                RazorDiagnostic.Create(new RazorError(
-                    LegacyResources.FormatParseError_Expected_EndOfBlock_Before_EOF("foreach", '}', '{'),
-                    SourceLocation.Zero,
-                    length: 1)));
+                RazorDiagnosticFactory.CreateParsing_ExpectedEndOfBlockBeforeEOF(
+                    new SourceSpan(SourceLocation.Zero, contentLength: 1), "foreach", "}", "{"));
         }
 
         [Fact]
@@ -516,10 +514,8 @@ while(true);", BlockKindInternal.Statement, SpanKindInternal.Code, acceptedChara
                 SpanKindInternal.Code,
                 RazorDiagnosticFactory.CreateParsing_BlockCommentNotTerminated(
                     new SourceSpan(new SourceLocation(24, 0, 24), contentLength: 1)),
-                RazorDiagnostic.Create(new RazorError(
-                    LegacyResources.FormatParseError_Expected_EndOfBlock_Before_EOF("foreach", '}', '{'),
-                    SourceLocation.Zero,
-                    length: 1)));
+                RazorDiagnosticFactory.CreateParsing_ExpectedEndOfBlockBeforeEOF(
+                    new SourceSpan(SourceLocation.Zero, contentLength: 1), "foreach", "}", "{"));
         }
 
         [Fact]
@@ -531,10 +527,8 @@ while(true);", BlockKindInternal.Statement, SpanKindInternal.Code, acceptedChara
                 document,
                 BlockKindInternal.Statement,
                 SpanKindInternal.Code,
-                RazorDiagnostic.Create(new RazorError(
-                    LegacyResources.FormatParseError_Expected_EndOfBlock_Before_EOF("foreach", '}', '{'),
-                    SourceLocation.Zero,
-                    length: 1)));
+                RazorDiagnosticFactory.CreateParsing_ExpectedEndOfBlockBeforeEOF(
+                    new SourceSpan(SourceLocation.Zero, contentLength: 1), "foreach", "}", "{"));
         }
 
         [Fact]
@@ -1155,10 +1149,8 @@ catch(bar) { baz(); }", BlockKindInternal.Statement, SpanKindInternal.Code);
                     @"End of file or an unexpected character was reached before the ""span"" tag could be parsed.  Elements inside markup blocks must be complete. They must either be self-closing (""<br />"") or have matching end tags (""<p>Hello</p>"").  If you intended to display a ""<"" character, use the ""&lt;"" HTML entity.",
                     new SourceLocation(2, 0, 2),
                     length: 4)),
-                RazorDiagnostic.Create(new RazorError(
-                    @"The code block is missing a closing ""}"" character.  Make sure you have a matching ""}"" character for all the ""{"" characters within this block, and that none of the ""}"" characters are being interpreted as markup.",
-                    SourceLocation.Zero,
-                    length: 1)),
+                RazorDiagnosticFactory.CreateParsing_ExpectedEndOfBlockBeforeEOF(
+                    new SourceSpan(SourceLocation.Zero, contentLength: 1), LegacyResources.BlockName_Code, "}", "{"),
             };
 
             // Act & Assert
@@ -1194,14 +1186,11 @@ catch(bar) { baz(); }", BlockKindInternal.Statement, SpanKindInternal.Code);
                 Factory.MetaCode("}").Accepts(AcceptedCharactersInternal.None));
             var expectedErrors = new RazorDiagnostic[]
             {
-                RazorDiagnostic.Create(new RazorError(
-                    @"A space or line break was encountered after the ""@"" character.  Only valid identifiers, keywords, comments, ""("" and ""{"" are valid at the start of a code block and they must occur immediately following ""@"" with no space in between.",
-                    new SourceLocation(13, 0, 13),
-                    length: 1)),
-                RazorDiagnostic.Create(new RazorError(
-                    @"""' />}"" is not valid at the start of a code block.  Only identifiers, keywords, comments, ""("" and ""{"" are valid.",
-                    new SourceLocation(15, 0, 15),
-                    length: 5)),
+                RazorDiagnosticFactory.CreateParsing_UnexpectedWhiteSpaceAtStartOfCodeBlockCS(
+                    new SourceSpan(new SourceLocation(13, 0, 13), contentLength: 1)),
+                RazorDiagnosticFactory.CreateParsing_UnexpectedCharacterAtStartOfCodeBlockCS(
+                    new SourceSpan(new SourceLocation(15, 0, 15), contentLength: 5),
+                    "' />}"),
             };
 
             // Act & Assert

test/Microsoft.AspNetCore.Razor.Language.Test/Legacy/CSharpImplicitExpressionTest.cs
@@ -135,10 +135,9 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
                                Factory.EmptyCSharp()
                                    .AsImplicitExpression(KeywordSet)
                                    .Accepts(AcceptedCharactersInternal.NonWhiteSpace)),
-                           RazorDiagnostic.Create(new RazorError(
-                               LegacyResources.FormatParseError_Unexpected_Character_At_Start_Of_CodeBlock_CS("/"),
-                               new SourceLocation(1, 0, 1),
-                               length: 1)));
+                           RazorDiagnosticFactory.CreateParsing_UnexpectedCharacterAtStartOfCodeBlockCS(
+                                new SourceSpan(new SourceLocation(1, 0, 1), contentLength: 1),
+                                "/"));
         }
 
         [Fact]
@@ -150,10 +149,8 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
                                Factory.EmptyCSharp()
                                    .AsImplicitExpression(KeywordSet)
                                    .Accepts(AcceptedCharactersInternal.NonWhiteSpace)),
-                           RazorDiagnostic.Create(new RazorError(
-                               LegacyResources.ParseError_Unexpected_EndOfFile_At_Start_Of_CodeBlock,
-                               new SourceLocation(1, 0, 1),
-                               length: 1)));
+                           RazorDiagnosticFactory.CreateParsing_UnexpectedEndOfFileAtStartOfCodeBlock(
+                                new SourceSpan(new SourceLocation(1, 0, 1), contentLength: 1)));
         }
 
         [Fact]

test/Microsoft.AspNetCore.Razor.Language.Test/Legacy/CSharpVerbatimBlockTest.cs
@@ -44,10 +44,9 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
                            designTime: true,
                            expectedErrors: new[]
                            {
-                               RazorDiagnostic.Create(new RazorError(
-                                   LegacyResources.FormatParseError_Unexpected_Character_At_Start_Of_CodeBlock_CS("}"),
-                                   new SourceLocation(2, 0, 2),
-                                   length: 1))
+                               RazorDiagnosticFactory.CreateParsing_UnexpectedCharacterAtStartOfCodeBlockCS(
+                                new SourceSpan(new SourceLocation(2, 0, 2), contentLength: 1),
+                                "}")
                            });
         }
 
@@ -69,10 +68,9 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
                            designTime: true,
                            expectedErrors: new[]
                            {
-                               RazorDiagnostic.Create(new RazorError(
-                                   LegacyResources.FormatParseError_Unexpected_Character_At_Start_Of_CodeBlock_CS("."),
-                                   new SourceLocation(2, 0, 2),
-                                   length: 1))
+                               RazorDiagnosticFactory.CreateParsing_UnexpectedCharacterAtStartOfCodeBlockCS(
+                                   new SourceSpan(new SourceLocation(2, 0, 2), contentLength: 1),
+                                   ".")
                            });
         }
 
@@ -94,10 +92,8 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
                                Factory.Code(Environment.NewLine).AsStatement(),
                                Factory.MetaCode("}").Accepts(AcceptedCharactersInternal.None)),
                 /* designTimeParser */ true,
-                           RazorDiagnostic.Create(new RazorError(
-                               LegacyResources.ParseError_Unexpected_WhiteSpace_At_Start_Of_CodeBlock_CS,
-                               new SourceLocation(6 + Environment.NewLine.Length, 1, 5),
-                               Environment.NewLine.Length)));
+                           RazorDiagnosticFactory.CreateParsing_UnexpectedWhiteSpaceAtStartOfCodeBlockCS(
+                                new SourceSpan(new SourceLocation(6 + Environment.NewLine.Length, 1, 5), Environment.NewLine.Length)));
         }
 
         [Fact]

test/Microsoft.AspNetCore.Razor.Language.Test/Legacy/HtmlDocumentTest.cs
@@ -64,10 +64,8 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
                                .AsImplicitExpression(CSharpCodeParser.DefaultKeywords)
                                .Accepts(AcceptedCharactersInternal.NonWhiteSpace)),
                     Factory.EmptyHtml()),
-                RazorDiagnostic.Create(new RazorError(
-                    LegacyResources.ParseError_Unexpected_EndOfFile_At_Start_Of_CodeBlock,
-                    new SourceLocation(1, 0, 1),
-                    length: 1)));
+                RazorDiagnosticFactory.CreateParsing_UnexpectedEndOfFileAtStartOfCodeBlock(
+                    new SourceSpan(new SourceLocation(1, 0, 1), contentLength: 1)));
         }
 
         [Fact]
@@ -159,10 +157,8 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
                                .AsImplicitExpression(CSharpCodeParser.DefaultKeywords)
                                .Accepts(AcceptedCharactersInternal.NonWhiteSpace)),
                     Factory.EmptyHtml()),
-                RazorDiagnostic.Create(new RazorError(
-                    LegacyResources.ParseError_Unexpected_EndOfFile_At_Start_Of_CodeBlock,
-                    new SourceLocation(5, 0, 5),
-                    length: 1)));
+                RazorDiagnosticFactory.CreateParsing_UnexpectedEndOfFileAtStartOfCodeBlock(
+                    new SourceSpan(new SourceLocation(5, 0, 5), contentLength: 1)));
         }
 
         [Fact]
@@ -788,14 +784,11 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
                     Factory.Markup(" />")));
             var expectedErrors = new RazorDiagnostic[]
             {
-                RazorDiagnostic.Create(new RazorError(
-                    @"A space or line break was encountered after the ""@"" character.  Only valid identifiers, keywords, comments, ""("" and ""{"" are valid at the start of a code block and they must occur immediately following ""@"" with no space in between.",
-                    new SourceLocation(12, 0, 12),
-                    length: 1)),
-                RazorDiagnostic.Create(new RazorError(
-                    @"""' />"" is not valid at the start of a code block.  Only identifiers, keywords, comments, ""("" and ""{"" are valid.",
-                    new SourceLocation(14, 0, 14),
-                    length: 4)),
+                RazorDiagnosticFactory.CreateParsing_UnexpectedWhiteSpaceAtStartOfCodeBlockCS(
+                    new SourceSpan(new SourceLocation(12, 0, 12), contentLength: 1)),
+                RazorDiagnosticFactory.CreateParsing_UnexpectedCharacterAtStartOfCodeBlockCS(
+                    new SourceSpan(new SourceLocation(14, 0, 14), contentLength: 4),
+                    "' />"),
             };
 
             // Act & Assert

dotnet/aspnetcore

test/Microsoft.AspNetCore.Razor.Language.Test/IntegrationTests/BasicIntegrationTest.cs
@@ -80,10 +80,9 @@ namespace Microsoft.AspNetCore.Razor.Language.IntegrationTests
 
             var document = RazorCodeDocument.Create(TestRazorSourceDocument.Create("@!!!", fileName: "test.cshtml"));
 
-            var expected = RazorDiagnostic.Create(new RazorError(
-                LegacyResources.FormatParseError_Unexpected_Character_At_Start_Of_CodeBlock_CS("!"),
-                new SourceLocation("test.cshtml", 1, 0, 1),
-                length: 1));
+            var expected = RazorDiagnosticFactory.CreateParsing_UnexpectedCharacterAtStartOfCodeBlock(
+                                new SourceSpan(new SourceLocation("test.cshtml", 1, 0, 1), contentLength: 1),
+                                "!");
 
             // Act
             engine.Process(document);
@@ -102,10 +101,8 @@ namespace Microsoft.AspNetCore.Razor.Language.IntegrationTests
 
             var document = RazorCodeDocument.Create(TestRazorSourceDocument.Create("@{", fileName: "test.cshtml"));
 
-            var expected = RazorDiagnostic.Create(new RazorError(
-                LegacyResources.FormatParseError_Expected_EndOfBlock_Before_EOF(LegacyResources.BlockName_Code, "}", "{"),
-                new SourceLocation("test.cshtml", 1, 0, 1),
-                length: 1));
+            var expected = RazorDiagnosticFactory.CreateParsing_ExpectedEndOfBlockBeforeEOF(
+                new SourceSpan(new SourceLocation("test.cshtml", 1, 0, 1), contentLength: 1), LegacyResources.BlockName_Code, "}", "{");
 
             // Act
             engine.Process(document);

test/Microsoft.AspNetCore.Razor.Language.Test/Legacy/CSharpBlockTest.cs
@@ -499,10 +499,8 @@ while(true);", BlockKindInternal.Statement, SpanKindInternal.Code, acceptedChara
                 document,
                 BlockKindInternal.Statement,
                 SpanKindInternal.Code,
-                RazorDiagnostic.Create(new RazorError(
-                    LegacyResources.FormatParseError_Expected_EndOfBlock_Before_EOF("foreach", '}', '{'),
-                    SourceLocation.Zero,
-                    length: 1)));
+                RazorDiagnosticFactory.CreateParsing_ExpectedEndOfBlockBeforeEOF(
+                    new SourceSpan(SourceLocation.Zero, contentLength: 1), "foreach", "}", "{"));
         }
 
         [Fact]
@@ -516,10 +514,8 @@ while(true);", BlockKindInternal.Statement, SpanKindInternal.Code, acceptedChara
                 SpanKindInternal.Code,
                 RazorDiagnosticFactory.CreateParsing_BlockCommentNotTerminated(
                     new SourceSpan(new SourceLocation(24, 0, 24), contentLength: 1)),
-                RazorDiagnostic.Create(new RazorError(
-                    LegacyResources.FormatParseError_Expected_EndOfBlock_Before_EOF("foreach", '}', '{'),
-                    SourceLocation.Zero,
-                    length: 1)));
+                RazorDiagnosticFactory.CreateParsing_ExpectedEndOfBlockBeforeEOF(
+                    new SourceSpan(SourceLocation.Zero, contentLength: 1), "foreach", "}", "{"));
         }
 
         [Fact]
@@ -531,10 +527,8 @@ while(true);", BlockKindInternal.Statement, SpanKindInternal.Code, acceptedChara
                 document,
                 BlockKindInternal.Statement,
                 SpanKindInternal.Code,
-                RazorDiagnostic.Create(new RazorError(
-                    LegacyResources.FormatParseError_Expected_EndOfBlock_Before_EOF("foreach", '}', '{'),
-                    SourceLocation.Zero,
-                    length: 1)));
+                RazorDiagnosticFactory.CreateParsing_ExpectedEndOfBlockBeforeEOF(
+                    new SourceSpan(SourceLocation.Zero, contentLength: 1), "foreach", "}", "{"));
         }
 
         [Fact]
@@ -1155,10 +1149,8 @@ catch(bar) { baz(); }", BlockKindInternal.Statement, SpanKindInternal.Code);
                     @"End of file or an unexpected character was reached before the ""span"" tag could be parsed.  Elements inside markup blocks must be complete. They must either be self-closing (""<br />"") or have matching end tags (""<p>Hello</p>"").  If you intended to display a ""<"" character, use the ""&lt;"" HTML entity.",
                     new SourceLocation(2, 0, 2),
                     length: 4)),
-                RazorDiagnostic.Create(new RazorError(
-                    @"The code block is missing a closing ""}"" character.  Make sure you have a matching ""}"" character for all the ""{"" characters within this block, and that none of the ""}"" characters are being interpreted as markup.",
-                    SourceLocation.Zero,
-                    length: 1)),
+                RazorDiagnosticFactory.CreateParsing_ExpectedEndOfBlockBeforeEOF(
+                    new SourceSpan(SourceLocation.Zero, contentLength: 1), LegacyResources.BlockName_Code, "}", "{"),
             };
 
             // Act & Assert
@@ -1194,14 +1186,11 @@ catch(bar) { baz(); }", BlockKindInternal.Statement, SpanKindInternal.Code);
                 Factory.MetaCode("}").Accepts(AcceptedCharactersInternal.None));
             var expectedErrors = new RazorDiagnostic[]
             {
-                RazorDiagnostic.Create(new RazorError(
-                    @"A space or line break was encountered after the ""@"" character.  Only valid identifiers, keywords, comments, ""("" and ""{"" are valid at the start of a code block and they must occur immediately following ""@"" with no space in between.",
-                    new SourceLocation(13, 0, 13),
-                    length: 1)),
-                RazorDiagnostic.Create(new RazorError(
-                    @"""' />}"" is not valid at the start of a code block.  Only identifiers, keywords, comments, ""("" and ""{"" are valid.",
-                    new SourceLocation(15, 0, 15),
-                    length: 5)),
+                RazorDiagnosticFactory.CreateParsing_UnexpectedWhiteSpaceAtStartOfCodeBlock(
+                    new SourceSpan(new SourceLocation(13, 0, 13), contentLength: 1)),
+                RazorDiagnosticFactory.CreateParsing_UnexpectedCharacterAtStartOfCodeBlock(
+                    new SourceSpan(new SourceLocation(15, 0, 15), contentLength: 5),
+                    "' />}"),
             };
 
             // Act & Assert

test/Microsoft.AspNetCore.Razor.Language.Test/Legacy/CSharpImplicitExpressionTest.cs
@@ -135,10 +135,9 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
                                Factory.EmptyCSharp()
                                    .AsImplicitExpression(KeywordSet)
                                    .Accepts(AcceptedCharactersInternal.NonWhiteSpace)),
-                           RazorDiagnostic.Create(new RazorError(
-                               LegacyResources.FormatParseError_Unexpected_Character_At_Start_Of_CodeBlock_CS("/"),
-                               new SourceLocation(1, 0, 1),
-                               length: 1)));
+                           RazorDiagnosticFactory.CreateParsing_UnexpectedCharacterAtStartOfCodeBlock(
+                                new SourceSpan(new SourceLocation(1, 0, 1), contentLength: 1),
+                                "/"));
         }
 
         [Fact]
@@ -150,10 +149,8 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
                                Factory.EmptyCSharp()
                                    .AsImplicitExpression(KeywordSet)
                                    .Accepts(AcceptedCharactersInternal.NonWhiteSpace)),
-                           RazorDiagnostic.Create(new RazorError(
-                               LegacyResources.ParseError_Unexpected_EndOfFile_At_Start_Of_CodeBlock,
-                               new SourceLocation(1, 0, 1),
-                               length: 1)));
+                           RazorDiagnosticFactory.CreateParsing_UnexpectedEndOfFileAtStartOfCodeBlock(
+                                new SourceSpan(new SourceLocation(1, 0, 1), contentLength: 1)));
         }
 
         [Fact]

test/Microsoft.AspNetCore.Razor.Language.Test/Legacy/CSharpVerbatimBlockTest.cs
@@ -44,10 +44,9 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
                            designTime: true,
                            expectedErrors: new[]
                            {
-                               RazorDiagnostic.Create(new RazorError(
-                                   LegacyResources.FormatParseError_Unexpected_Character_At_Start_Of_CodeBlock_CS("}"),
-                                   new SourceLocation(2, 0, 2),
-                                   length: 1))
+                               RazorDiagnosticFactory.CreateParsing_UnexpectedCharacterAtStartOfCodeBlock(
+                                new SourceSpan(new SourceLocation(2, 0, 2), contentLength: 1),
+                                "}")
                            });
         }
 
@@ -69,10 +68,9 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
                            designTime: true,
                            expectedErrors: new[]
                            {
-                               RazorDiagnostic.Create(new RazorError(
-                                   LegacyResources.FormatParseError_Unexpected_Character_At_Start_Of_CodeBlock_CS("."),
-                                   new SourceLocation(2, 0, 2),
-                                   length: 1))
+                               RazorDiagnosticFactory.CreateParsing_UnexpectedCharacterAtStartOfCodeBlock(
+                                   new SourceSpan(new SourceLocation(2, 0, 2), contentLength: 1),
+                                   ".")
                            });
         }
 
@@ -94,10 +92,8 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
                                Factory.Code(Environment.NewLine).AsStatement(),
                                Factory.MetaCode("}").Accepts(AcceptedCharactersInternal.None)),
                 /* designTimeParser */ true,
-                           RazorDiagnostic.Create(new RazorError(
-                               LegacyResources.ParseError_Unexpected_WhiteSpace_At_Start_Of_CodeBlock_CS,
-                               new SourceLocation(6 + Environment.NewLine.Length, 1, 5),
-                               Environment.NewLine.Length)));
+                           RazorDiagnosticFactory.CreateParsing_UnexpectedWhiteSpaceAtStartOfCodeBlock(
+                                new SourceSpan(new SourceLocation(6 + Environment.NewLine.Length, 1, 5), Environment.NewLine.Length)));
         }
 
         [Fact]

test/Microsoft.AspNetCore.Razor.Language.Test/Legacy/HtmlDocumentTest.cs
@@ -64,10 +64,8 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
                                .AsImplicitExpression(CSharpCodeParser.DefaultKeywords)
                                .Accepts(AcceptedCharactersInternal.NonWhiteSpace)),
                     Factory.EmptyHtml()),
-                RazorDiagnostic.Create(new RazorError(
-                    LegacyResources.ParseError_Unexpected_EndOfFile_At_Start_Of_CodeBlock,
-                    new SourceLocation(1, 0, 1),
-                    length: 1)));
+                RazorDiagnosticFactory.CreateParsing_UnexpectedEndOfFileAtStartOfCodeBlock(
+                    new SourceSpan(new SourceLocation(1, 0, 1), contentLength: 1)));
         }
 
         [Fact]
@@ -159,10 +157,8 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
                                .AsImplicitExpression(CSharpCodeParser.DefaultKeywords)
                                .Accepts(AcceptedCharactersInternal.NonWhiteSpace)),
                     Factory.EmptyHtml()),
-                RazorDiagnostic.Create(new RazorError(
-                    LegacyResources.ParseError_Unexpected_EndOfFile_At_Start_Of_CodeBlock,
-                    new SourceLocation(5, 0, 5),
-                    length: 1)));
+                RazorDiagnosticFactory.CreateParsing_UnexpectedEndOfFileAtStartOfCodeBlock(
+                    new SourceSpan(new SourceLocation(5, 0, 5), contentLength: 1)));
         }
 
         [Fact]
@@ -788,14 +784,11 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
                     Factory.Markup(" />")));
             var expectedErrors = new RazorDiagnostic[]
             {
-                RazorDiagnostic.Create(new RazorError(
-                    @"A space or line break was encountered after the ""@"" character.  Only valid identifiers, keywords, comments, ""("" and ""{"" are valid at the start of a code block and they must occur immediately following ""@"" with no space in between.",
-                    new SourceLocation(12, 0, 12),
-                    length: 1)),
-                RazorDiagnostic.Create(new RazorError(
-                    @"""' />"" is not valid at the start of a code block.  Only identifiers, keywords, comments, ""("" and ""{"" are valid.",
-                    new SourceLocation(14, 0, 14),
-                    length: 4)),
+                RazorDiagnosticFactory.CreateParsing_UnexpectedWhiteSpaceAtStartOfCodeBlock(
+                    new SourceSpan(new SourceLocation(12, 0, 12), contentLength: 1)),
+                RazorDiagnosticFactory.CreateParsing_UnexpectedCharacterAtStartOfCodeBlock(
+                    new SourceSpan(new SourceLocation(14, 0, 14), contentLength: 4),
+                    "' />"),
             };
 
             // Act & Assert