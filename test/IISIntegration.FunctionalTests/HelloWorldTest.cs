c0761df41140bcc684a03480b3d69b8084e905c0
Justin Kotalik <jukotali@microsoft.com>
2018-01-04 12:41:27 -0800

    Disables Functional tests due to status code 400 issues (#505)

--- test/IISIntegration.FunctionalTests/HelloWorldTest.cs
+++ test/IISIntegration.FunctionalTests/HelloWorldTest.cs
@@ -20,13 +20,13 @@ namespace Microsoft.AspNetCore.Server.IISIntegration.FunctionalTests
         {
         }
 
-        [Fact]
+        [Fact(Skip = "See https://github.com/aspnet/IISIntegration/issues/498")]
         public Task HelloWorld_IISExpress_Clr_X64_Portable()
         {
             return HelloWorld(RuntimeFlavor.Clr, ApplicationType.Portable);
         }
 
-        [Fact]
+        [Fact(Skip = "See https://github.com/aspnet/IISIntegration/issues/498")]
         public Task HelloWorld_IISExpress_CoreClr_X64_Portable()
         {
             return HelloWorld(RuntimeFlavor.CoreClr, ApplicationType.Portable);
