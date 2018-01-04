22da82ff8a27d4df61235a22c95e4cccb5b5d4b8
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-01-04 11:10:35 +0000

    Sync remaining Angular-CSharp changes from OOB release into dev branch

--- src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/Angular-CSharp/Startup.cs
+++ src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/Angular-CSharp/Startup.cs
@@ -53,20 +53,10 @@ namespace AngularSpa
 
             app.UseSpa(spa =>
             {
-                spa.Options.SourcePath = "ClientApp";
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                /*
-                // If you want to enable server-side rendering (SSR),
-                // [1] In AngularSpa.csproj, change the <BuildServerSideRenderer> property
-                //     value to 'true', so that the SSR bundle is built during publish
-                // [2] Uncomment this code block
-                spa.UseSpaPrerendering(options =>
-                {
-                    options.BootModulePath = $"{spa.Options.SourcePath}/dist-server/main.bundle.js";
-                    options.BootModuleBuilder = env.IsDevelopment() ? new AngularCliBuilder(npmScript: "build:ssr") : null;
-                    options.ExcludeUrls = new[] { "/sockjs-node" };
-                });
-                */
+                spa.Options.SourcePath = "ClientApp";
 
                 if (env.IsDevelopment())
                 {
