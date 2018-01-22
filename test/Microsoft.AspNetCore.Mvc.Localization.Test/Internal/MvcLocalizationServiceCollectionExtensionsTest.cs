ea149425258c441e09ca254843514bd74cabe1e5
Ryan Brandenburg <rybrande@microsoft.com>
2018-01-22 12:41:33 -0800

    React to Options change

--- test/Microsoft.AspNetCore.Mvc.Localization.Test/Internal/MvcLocalizationServiceCollectionExtensionsTest.cs
+++ test/Microsoft.AspNetCore.Mvc.Localization.Test/Internal/MvcLocalizationServiceCollectionExtensionsTest.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,30 +31,9 @@ namespace Microsoft.AspNetCore.Mvc.Localization.Internal
                 setupAction: null);
 
             // Assert
-            Assert.Collection(collection,
-                service =>
-                {
-                    Assert.Equal(typeof(IConfigureOptions<RazorViewEngineOptions>), service.ServiceType);
-                    Assert.Equal(ServiceLifetime.Singleton, service.Lifetime);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(IHtmlLocalizerFactory), service.ServiceType);
-                    Assert.Equal(typeof(HtmlLocalizerFactory), service.ImplementationType);
-                    Assert.Equal(ServiceLifetime.Singleton, service.Lifetime);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(IHtmlLocalizer<>), service.ServiceType);
-                    Assert.Equal(typeof(HtmlLocalizer<>), service.ImplementationType);
-                    Assert.Equal(ServiceLifetime.Transient, service.Lifetime);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(IViewLocalizer), service.ServiceType);
-                    Assert.Equal(typeof(ViewLocalizer), service.ImplementationType);
-                    Assert.Equal(ServiceLifetime.Transient, service.Lifetime);
-                });
+            AssertContainsSingle(collection, typeof(IHtmlLocalizerFactory), typeof(HtmlLocalizerFactory));
+            AssertContainsSingle(collection, typeof(IHtmlLocalizer<>), typeof(HtmlLocalizer<>));
+            AssertContainsSingle(collection, typeof(IViewLocalizer), typeof(ViewLocalizer));
         }
 
         [Fact]
@@ -61,264 +41,136 @@ namespace Microsoft.AspNetCore.Mvc.Localization.Internal
         {
             // Arrange
             var collection = new ServiceCollection();
-            var testEncoder = new HtmlTestEncoder();
 
             // Act
             collection.Add(ServiceDescriptor.Singleton(typeof(IHtmlLocalizerFactory), typeof(TestHtmlLocalizerFactory)));
             collection.Add(ServiceDescriptor.Transient(typeof(IHtmlLocalizer<>), typeof(TestHtmlLocalizer<>)));
             collection.Add(ServiceDescriptor.Transient(typeof(IViewLocalizer), typeof(TestViewLocalizer)));
-            collection.Add(ServiceDescriptor.Singleton(typeof(HtmlEncoder), testEncoder));
 
             MvcLocalizationServices.AddMvcViewLocalizationServices(
                 collection,
                 LanguageViewLocationExpanderFormat.Suffix,
                 setupAction: null);
 
-            // Assert
-            Assert.Collection(collection,
-                service =>
-                {
-                    Assert.Equal(typeof(IHtmlLocalizerFactory), service.ServiceType);
-                    Assert.Equal(typeof(TestHtmlLocalizerFactory), service.ImplementationType);
-                    Assert.Equal(ServiceLifetime.Singleton, service.Lifetime);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(IHtmlLocalizer<>), service.ServiceType);
-                    Assert.Equal(typeof(TestHtmlLocalizer<>), service.ImplementationType);
-                    Assert.Equal(ServiceLifetime.Transient, service.Lifetime);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(IViewLocalizer), service.ServiceType);
-                    Assert.Equal(typeof(TestViewLocalizer), service.ImplementationType);
-                    Assert.Equal(ServiceLifetime.Transient, service.Lifetime);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(HtmlEncoder), service.ServiceType);
-                    Assert.Same(testEncoder, service.ImplementationInstance);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(IConfigureOptions<RazorViewEngineOptions>), service.ServiceType);
-                    Assert.Equal(ServiceLifetime.Singleton, service.Lifetime);
-                });
+            AssertContainsSingle(collection, typeof(IHtmlLocalizerFactory), typeof(TestHtmlLocalizerFactory));
+            AssertContainsSingle(collection, typeof(IHtmlLocalizer<>), typeof(TestHtmlLocalizer<>));
+            AssertContainsSingle(collection, typeof(IViewLocalizer), typeof(TestViewLocalizer));
         }
 
-        [Fact]
-        public void AddCustomLocalizers_AfterAddLocalizationServices_AddsNeededServices()
+        private void AssertContainsSingle(
+            IServiceCollection services,
+            Type serviceType,
+            Type implementationType)
         {
-            // Arrange
-            var collection = new ServiceCollection();
-            var htmlEncoder = new HtmlTestEncoder();
+            var matches = services
+                .Where(sd =>
+                    sd.ServiceType == serviceType &&
+                    sd.ImplementationType == implementationType)
+                .ToArray();
 
-            collection.Configure<RazorViewEngineOptions>(options =>
+            if (matches.Length == 0)
             {
-                options.ViewLocationExpanders.Add(new CustomPartialDirectoryViewLocationExpander());
-            });
-
-            // Act
-            MvcLocalizationServices.AddMvcViewLocalizationServices(
-                collection,
-                LanguageViewLocationExpanderFormat.Suffix,
-                setupAction: null);
-
-
-            collection.Add(ServiceDescriptor.Transient(typeof(IHtmlLocalizer<>), typeof(TestHtmlLocalizer<>)));
-            collection.Add(ServiceDescriptor.Transient(typeof(IHtmlLocalizer), typeof(TestViewLocalizer)));
-            collection.Add(ServiceDescriptor.Singleton(typeof(HtmlEncoder), htmlEncoder));
-
-            // Assert
-            Assert.Collection(collection,
-                service =>
-                {
-                    Assert.Equal(typeof(IConfigureOptions<RazorViewEngineOptions>), service.ServiceType);
-                    Assert.Equal(ServiceLifetime.Singleton, service.Lifetime);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(IConfigureOptions<RazorViewEngineOptions>), service.ServiceType);
-                    Assert.Equal(ServiceLifetime.Singleton, service.Lifetime);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(IHtmlLocalizerFactory), service.ServiceType);
-                    Assert.Equal(typeof(HtmlLocalizerFactory), service.ImplementationType);
-                    Assert.Equal(ServiceLifetime.Singleton, service.Lifetime);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(IHtmlLocalizer<>), service.ServiceType);
-                    Assert.Equal(typeof(HtmlLocalizer<>), service.ImplementationType);
-                    Assert.Equal(ServiceLifetime.Transient, service.Lifetime);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(IViewLocalizer), service.ServiceType);
-                    Assert.Equal(typeof(ViewLocalizer), service.ImplementationType);
-                    Assert.Equal(ServiceLifetime.Transient, service.Lifetime);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(IHtmlLocalizer<>), service.ServiceType);
-                    Assert.Equal(typeof(TestHtmlLocalizer<>), service.ImplementationType);
-                    Assert.Equal(ServiceLifetime.Transient, service.Lifetime);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(IHtmlLocalizer), service.ServiceType);
-                    Assert.Equal(typeof(TestViewLocalizer), service.ImplementationType);
-                    Assert.Equal(ServiceLifetime.Transient, service.Lifetime);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(HtmlEncoder), service.ServiceType);
-                    Assert.Same(htmlEncoder, service.ImplementationInstance);
-                    Assert.Equal(ServiceLifetime.Singleton, service.Lifetime);
-                });
+                Assert.True(
+                    false,
+                    $"Could not find an instance of {implementationType} registered as {serviceType}");
+            }
+            else if (matches.Length > 1)
+            {
+                Assert.True(
+                    false,
+                    $"Found multiple instances of {implementationType} registered as {serviceType}");
+            }
         }
 
-        [Fact]
-        public void AddLocalizationServicesWithLocalizationOptions_AddsNeededServices()
+        public class TestViewLocalizer : IViewLocalizer
         {
-            // Arrange
-            var collection = new ServiceCollection();
+            public LocalizedHtmlString this[string name]
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+            }
 
-            // Act
-            MvcLocalizationServices.AddMvcViewLocalizationServices(
-                collection,
-                LanguageViewLocationExpanderFormat.Suffix,
-                options => options.ResourcesPath = "Resources");
+            public LocalizedHtmlString this[string name, params object[] arguments]
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+            }
 
-            // Assert
-            Assert.Collection(collection,
-                service =>
-                {
-                    Assert.Equal(typeof(IConfigureOptions<RazorViewEngineOptions>), service.ServiceType);
-                    Assert.Equal(ServiceLifetime.Singleton, service.Lifetime);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(IHtmlLocalizerFactory), service.ServiceType);
-                    Assert.Equal(typeof(HtmlLocalizerFactory), service.ImplementationType);
-                    Assert.Equal(ServiceLifetime.Singleton, service.Lifetime);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(IHtmlLocalizer<>), service.ServiceType);
-                    Assert.Equal(typeof(HtmlLocalizer<>), service.ImplementationType);
-                    Assert.Equal(ServiceLifetime.Transient, service.Lifetime);
-                },
-                service =>
-                {
-                    Assert.Equal(typeof(IViewLocalizer), service.ServiceType);
-                    Assert.Equal(typeof(ViewLocalizer), service.ImplementationType);
-                    Assert.Equal(ServiceLifetime.Transient, service.Lifetime);
-                });
-        }
-    }
+            public LocalizedString GetString(string name)
+            {
+                throw new NotImplementedException();
+            }
 
-    public class TestViewLocalizer : IViewLocalizer
-    {
-        public LocalizedHtmlString this[string name]
-        {
-            get
+            public LocalizedString GetString(string name, params object[] arguments)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IHtmlLocalizer WithCulture(CultureInfo culture)
             {
                 throw new NotImplementedException();
             }
         }
 
-        public LocalizedHtmlString this[string name, params object[] arguments]
+        public class TestHtmlLocalizer<HomeController> : IHtmlLocalizer<HomeController>
         {
-            get
+            public LocalizedHtmlString this[string name]
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public LocalizedHtmlString this[string name, params object[] arguments]
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public LocalizedString GetString(string name)
+            {
+                throw new NotImplementedException();
+            }
+
+            public LocalizedString GetString(string name, params object[] arguments)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IHtmlLocalizer WithCulture(CultureInfo culture)
             {
                 throw new NotImplementedException();
             }
         }
 
-        public LocalizedString GetString(string name)
+        public class TestHtmlLocalizerFactory : IHtmlLocalizerFactory
         {
-            throw new NotImplementedException();
-        }
-
-        public LocalizedString GetString(string name, params object[] arguments)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IHtmlLocalizer WithCulture(CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class TestHtmlLocalizer<HomeController> : IHtmlLocalizer<HomeController>
-    {
-        public LocalizedHtmlString this[string name]
-        {
-            get
+            public IHtmlLocalizer Create(Type resourceSource)
             {
                 throw new NotImplementedException();
             }
-        }
 
-        public LocalizedHtmlString this[string name, params object[] arguments]
-        {
-            get
+            public IHtmlLocalizer Create(string baseName, string location)
             {
                 throw new NotImplementedException();
             }
-        }
-
-        public LocalizedString GetString(string name)
-        {
-            throw new NotImplementedException();
-        }
-
-        public LocalizedString GetString(string name, params object[] arguments)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IHtmlLocalizer WithCulture(CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class TestHtmlLocalizerFactory : IHtmlLocalizerFactory
-    {
-        public IHtmlLocalizer Create(Type resourceSource)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IHtmlLocalizer Create(string baseName, string location)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class CustomPartialDirectoryViewLocationExpander : IViewLocationExpander
-    {
-        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void PopulateValues(ViewLocationExpanderContext context)
-        {
         }
     }
 }
