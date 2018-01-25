zanhaipeng/Razor

Flow RazorLanguageVersion to RazorEngine.
- Restructured RazorLanguageVersion to be a sealed concrete type to enable things like `RazorLanguageVersion.Latest`; it also allows us to make broader changes in the future. Also, in the future if we want to add support for overriding operators to enable greater than comparisons we can as well.
- Removed version validity checks because we restrict who can construct a `RazorLanguageVersion` now. This way we don't have to check for valid versions all throughout our code.
- Added a simple `ProjectExtensibilityConfiguration` => `RazorLanguageVersion` method in the `DefaultProjectExtensibilityConfigurationFactory` to temporarily enable letting the system operate on the `RazorLanguageVersion`. Eventually that entire class will change.

#1961

test/Microsoft.VisualStudio.Editor.Razor.Test/DefaultTemplateEngineFactoryServiceTest.cs
@@ -38,6 +38,7 @@ namespace Microsoft.VisualStudio.Editor.Razor
             projectManager.ProjectUpdated(new ProjectSnapshotUpdateContext(Project)
             {
                 Configuration = new MvcExtensibilityConfiguration(
+                    RazorLanguageVersion.Version2_0,
                     ProjectExtensibilityConfigurationKind.ApproximateMatch,
                     new ProjectExtensibilityAssembly(new AssemblyIdentity("Microsoft.AspNetCore.Mvc.Razor", new Version("2.0.0.0"))),
                     new ProjectExtensibilityAssembly(new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("2.0.0.0")))),
@@ -67,6 +68,7 @@ namespace Microsoft.VisualStudio.Editor.Razor
             projectManager.ProjectUpdated(new ProjectSnapshotUpdateContext(Project)
             {
                 Configuration = new MvcExtensibilityConfiguration(
+                    RazorLanguageVersion.Version1_1,
                     ProjectExtensibilityConfigurationKind.ApproximateMatch,
                     new ProjectExtensibilityAssembly(new AssemblyIdentity("Microsoft.AspNetCore.Mvc.Razor", new Version("1.1.3.0"))),
                     new ProjectExtensibilityAssembly(new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("1.1.3.0")))),
@@ -96,6 +98,7 @@ namespace Microsoft.VisualStudio.Editor.Razor
             projectManager.ProjectUpdated(new ProjectSnapshotUpdateContext(Project)
             {
                 Configuration = new MvcExtensibilityConfiguration(
+                    RazorLanguageVersion.Version1_0,
                     ProjectExtensibilityConfigurationKind.ApproximateMatch,
                     new ProjectExtensibilityAssembly(new AssemblyIdentity("Microsoft.AspNetCore.Mvc.Razor", new Version("1.0.0.0"))),
                     new ProjectExtensibilityAssembly(new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("1.0.0.0")))),
@@ -124,6 +127,7 @@ namespace Microsoft.VisualStudio.Editor.Razor
             projectManager.ProjectUpdated(new ProjectSnapshotUpdateContext(Project)
             {
                 Configuration = new MvcExtensibilityConfiguration(
+                    RazorLanguageVersion.Latest,
                     ProjectExtensibilityConfigurationKind.ApproximateMatch,
                     new ProjectExtensibilityAssembly(new AssemblyIdentity("Microsoft.AspNetCore.Mvc.Razor", new Version("3.0.0.0"))),
                     new ProjectExtensibilityAssembly(new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("3.0.0.0")))),

dotnet/aspnetcore

test/Microsoft.VisualStudio.Editor.Razor.Test/DefaultTemplateEngineFactoryServiceTest.cs
@@ -38,6 +38,7 @@ namespace Microsoft.VisualStudio.Editor.Razor
             projectManager.ProjectUpdated(new ProjectSnapshotUpdateContext(Project)
             {
                 Configuration = new MvcExtensibilityConfiguration(
+                    RazorLanguageVersion.Version_2_0,
                     ProjectExtensibilityConfigurationKind.ApproximateMatch,
                     new ProjectExtensibilityAssembly(new AssemblyIdentity("Microsoft.AspNetCore.Mvc.Razor", new Version("2.0.0.0"))),
                     new ProjectExtensibilityAssembly(new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("2.0.0.0")))),
@@ -67,6 +68,7 @@ namespace Microsoft.VisualStudio.Editor.Razor
             projectManager.ProjectUpdated(new ProjectSnapshotUpdateContext(Project)
             {
                 Configuration = new MvcExtensibilityConfiguration(
+                    RazorLanguageVersion.Version_1_1,
                     ProjectExtensibilityConfigurationKind.ApproximateMatch,
                     new ProjectExtensibilityAssembly(new AssemblyIdentity("Microsoft.AspNetCore.Mvc.Razor", new Version("1.1.3.0"))),
                     new ProjectExtensibilityAssembly(new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("1.1.3.0")))),
@@ -96,6 +98,7 @@ namespace Microsoft.VisualStudio.Editor.Razor
             projectManager.ProjectUpdated(new ProjectSnapshotUpdateContext(Project)
             {
                 Configuration = new MvcExtensibilityConfiguration(
+                    RazorLanguageVersion.Version_1_0,
                     ProjectExtensibilityConfigurationKind.ApproximateMatch,
                     new ProjectExtensibilityAssembly(new AssemblyIdentity("Microsoft.AspNetCore.Mvc.Razor", new Version("1.0.0.0"))),
                     new ProjectExtensibilityAssembly(new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("1.0.0.0")))),
@@ -124,6 +127,7 @@ namespace Microsoft.VisualStudio.Editor.Razor
             projectManager.ProjectUpdated(new ProjectSnapshotUpdateContext(Project)
             {
                 Configuration = new MvcExtensibilityConfiguration(
+                    RazorLanguageVersion.Latest,
                     ProjectExtensibilityConfigurationKind.ApproximateMatch,
                     new ProjectExtensibilityAssembly(new AssemblyIdentity("Microsoft.AspNetCore.Mvc.Razor", new Version("3.0.0.0"))),
                     new ProjectExtensibilityAssembly(new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("3.0.0.0")))),