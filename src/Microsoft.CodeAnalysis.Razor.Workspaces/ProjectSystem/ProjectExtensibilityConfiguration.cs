zanhaipeng/Razor

Flow RazorLanguageVersion to RazorEngine.
- Restructured RazorLanguageVersion to be a sealed concrete type to enable things like `RazorLanguageVersion.Latest`; it also allows us to make broader changes in the future. Also, in the future if we want to add support for overriding operators to enable greater than comparisons we can as well.
- Removed version validity checks because we restrict who can construct a `RazorLanguageVersion` now. This way we don't have to check for valid versions all throughout our code.
- Added a simple `ProjectExtensibilityConfiguration` => `RazorLanguageVersion` method in the `DefaultProjectExtensibilityConfigurationFactory` to temporarily enable letting the system operate on the `RazorLanguageVersion`. Eventually that entire class will change.

#1961

src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/ProjectExtensibilityConfiguration.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Razor.Language;
 
 namespace Microsoft.CodeAnalysis.Razor.ProjectSystem
 {
@@ -15,6 +16,8 @@ namespace Microsoft.CodeAnalysis.Razor.ProjectSystem
         public abstract ProjectExtensibilityConfigurationKind Kind { get; }
 
         public abstract ProjectExtensibilityAssembly RazorAssembly { get; }
+
+        public abstract RazorLanguageVersion RazorLanguageVersion { get; }
 
         public abstract bool Equals(ProjectExtensibilityConfiguration other);
 

test/Microsoft.CodeAnalysis.Razor.Workspaces.Test/ProjectSystem/DefaultProjectExtensibilityConfigurationFactoryTest.cs
@@ -2,12 +2,64 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.AspNetCore.Razor.Language;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.Razor.ProjectSystem
 {
     public class DefaultProjectExtensibilityConfigurationFactoryTest
     {
+        public static TheoryData LanguageVersionMappingData
+        {
+            get
+            {
+                return new TheoryData<AssemblyIdentity, RazorLanguageVersion>
+                {
+                    { new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("1.0.0.0")), RazorLanguageVersion.Version1_0 },
+                    { new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("1.1.0.0")), RazorLanguageVersion.Version1_1 },
+                    { new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("2.0.0.0")), RazorLanguageVersion.Version2_0 },
+                    { new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("2.1.0.0")), RazorLanguageVersion.Version2_1 },
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(LanguageVersionMappingData))]
+        public void GetLanguageVersion_MapsExactVersionsCorrectly(AssemblyIdentity assemblyIdentity, RazorLanguageVersion expectedVersion)
+        {
+            // Act
+            var languageVersion = DefaultProjectExtensibilityConfigurationFactory.GetLanguageVersion(assemblyIdentity);
+
+            // Assert
+            Assert.Same(expectedVersion, languageVersion);
+        }
+
+        [Fact]
+        public void GetLanguageVersion_MapsFuture_1_0_VersionsCorrectly()
+        {
+            // Arrange
+            var assemblyIdentity = new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("1.3.0.0"));
+
+            // Act
+            var languageVersion = DefaultProjectExtensibilityConfigurationFactory.GetLanguageVersion(assemblyIdentity);
+
+            // Assert
+            Assert.Same(RazorLanguageVersion.Version1_1, languageVersion);
+        }
+
+        [Fact]
+        public void GetLanguageVersion_MapsFuture_2_0_VersionsCorrectly()
+        {
+            // Arrange
+            var assemblyIdentity = new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("2.3.0.0"));
+
+            // Act
+            var languageVersion = DefaultProjectExtensibilityConfigurationFactory.GetLanguageVersion(assemblyIdentity);
+
+            // Assert
+            Assert.Same(RazorLanguageVersion.Latest, languageVersion);
+        }
+
         [Theory]
         [InlineData("1.0.0.0", "1.0.0.0")]
         [InlineData("1.1.0.0", "1.1.0.0")]

dotnet/aspnetcore

src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/ProjectExtensibilityConfiguration.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Razor.Language;
 
 namespace Microsoft.CodeAnalysis.Razor.ProjectSystem
 {
@@ -15,6 +16,8 @@ namespace Microsoft.CodeAnalysis.Razor.ProjectSystem
         public abstract ProjectExtensibilityConfigurationKind Kind { get; }
 
         public abstract ProjectExtensibilityAssembly RazorAssembly { get; }
+
+        public abstract RazorLanguageVersion LanguageVersion { get; }
 
         public abstract bool Equals(ProjectExtensibilityConfiguration other);
 

test/Microsoft.CodeAnalysis.Razor.Workspaces.Test/ProjectSystem/DefaultProjectExtensibilityConfigurationFactoryTest.cs
@@ -2,12 +2,64 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.AspNetCore.Razor.Language;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.Razor.ProjectSystem
 {
     public class DefaultProjectExtensibilityConfigurationFactoryTest
     {
+        public static TheoryData LanguageVersionMappingData
+        {
+            get
+            {
+                return new TheoryData<AssemblyIdentity, RazorLanguageVersion>
+                {
+                    { new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("1.0.0.0")), RazorLanguageVersion.Version_1_0 },
+                    { new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("1.1.0.0")), RazorLanguageVersion.Version_1_1 },
+                    { new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("2.0.0.0")), RazorLanguageVersion.Version_2_0 },
+                    { new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("2.1.0.0")), RazorLanguageVersion.Version_2_1 },
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(LanguageVersionMappingData))]
+        public void GetLanguageVersion_MapsExactVersionsCorrectly(AssemblyIdentity assemblyIdentity, RazorLanguageVersion expectedVersion)
+        {
+            // Act
+            var languageVersion = DefaultProjectExtensibilityConfigurationFactory.GetLanguageVersion(assemblyIdentity);
+
+            // Assert
+            Assert.Same(expectedVersion, languageVersion);
+        }
+
+        [Fact]
+        public void GetLanguageVersion_MapsFuture_1_0_VersionsCorrectly()
+        {
+            // Arrange
+            var assemblyIdentity = new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("1.3.0.0"));
+
+            // Act
+            var languageVersion = DefaultProjectExtensibilityConfigurationFactory.GetLanguageVersion(assemblyIdentity);
+
+            // Assert
+            Assert.Same(RazorLanguageVersion.Version_1_1, languageVersion);
+        }
+
+        [Fact]
+        public void GetLanguageVersion_MapsFuture_2_0_VersionsCorrectly()
+        {
+            // Arrange
+            var assemblyIdentity = new AssemblyIdentity("Microsoft.AspNetCore.Razor", new Version("2.3.0.0"));
+
+            // Act
+            var languageVersion = DefaultProjectExtensibilityConfigurationFactory.GetLanguageVersion(assemblyIdentity);
+
+            // Assert
+            Assert.Same(RazorLanguageVersion.Latest, languageVersion);
+        }
+
         [Theory]
         [InlineData("1.0.0.0", "1.0.0.0")]
         [InlineData("1.1.0.0", "1.1.0.0")]