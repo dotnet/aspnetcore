2408fce9755416fb88421ba718e9076336e11839
Mike Harder <mharder@microsoft.com>
2018-02-01 15:54:29 -0800

    Improvements to Ubuntu setup script
    - Append "sudo" to commands requiring it
    - Rename dir from "TeamCity" to "BuildAgent" to match TeamCity docs
    - Update startup script name and code to match TeamCity docs

--- BuildAgent/SetupScripts/BuildAgentSetup_Ubuntu.sh
+++ BuildAgent/SetupScripts/BuildAgentSetup_Ubuntu.sh
@@ -15,7 +15,7 @@ AGENTNAME=$1
 SERVERURL=${2%/} # trim the final '/' in the string
 ASPNETOSNAME=$3
 
-apt-get update
+sudo apt-get update
 
 echo "Updating dhclient.conf..."
 echo 'supersede domain-name "redmond.corp.microsoft.com";' >> /etc/dhcp/dhclient.conf
@@ -23,83 +23,87 @@ echo 'supersede domain-search "redmond.corp.microsoft.com";' >> /etc/dhcp/dhclie
 echo 'supersede search "redmond.corp.microsoft.com";' >> /etc/dhcp/dhclient.conf
 
 echo "Restarting dhclient..."
-dhclient -r
-dhclient
+sudo dhclient -r
+sudo dhclient
 
 echo "Installing Git..."
-apt-get install -y git
+sudo apt-get install -y git
 
 # Sometimes git pull stalls, so this could fix it
 git config --global http.postBuffer 2M
 
 echo "Installing libunwind8..."
-apt-get install -y libunwind8
+sudo apt-get install -y libunwind8
 
 echo "Installing Java..."
-apt-get install -y default-jre-headless unzip
+sudo apt-get install -y default-jre-headless unzip
 
 echo "Installing Node.js..."
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-apt-get install -y nodejs
+sudo apt-get install -y nodejs
 
 echo "Installing TypeScript globally..."
-npm install -g typescript
+sudo npm install -g typescript
 
 echo "Installing Nginx..."
-apt-get install -y nginx
-update-rc.d nginx defaults
+sudo apt-get install -y nginx
+sudo update-rc.d nginx defaults
 
 echo "Installing Docker..."
 export CHANNEL=stable
-curl -fsSL https://get.docker.com | sh
-usermod -aG docker $SUDO_USER
+curl -fsSL https://get.docker.com | sudo sh
+sudo usermod -aG docker $SUDO_USER
 
 echo "Downloading build agent from $SERVERURL and updating the properties..."
-mkdir ~/TeamCity
-cd ~/TeamCity
+mkdir ~/BuildAgent
+cd ~/BuildAgent
 wget $SERVERURL/update/buildAgent.zip
 unzip buildAgent.zip
 cd bin
 chmod +x agent.sh
-cd ~/TeamCity/conf
+cd ~/BuildAgent/conf
 cp buildAgent.dist.properties buildAgent.properties
 
 # Set the build agent name and CI server urls
 sed -i "s|^name=.*|name=$AGENTNAME|" buildAgent.properties
 sed -i "s|^serverUrl=.*|serverUrl=$SERVERURL|" buildAgent.properties
 
-# Use the local SSD (/mnt) for the work and temp dirs.
+# Use the local SSD (/mnt) for the work, temp, and system dirs.
 # * It should be as fast or faster than the OS disk (which is backed by blob storage).
 # * It reduces load and storage on the OS disk.
 # * The data on the local SSD may lost at any time (typically after a reboot), but this
-#   should be fine for the work and temp dirs.
+#   should be fine for the work, temp, and system dirs.
 sed -i "s|^workDir=.*|workDir=/mnt/work|" buildAgent.properties
 sed -i "s|^tempDir=.*|tempDir=/mnt/temp|" buildAgent.properties
+sed -i "s|^systemDir=.*|systemDir=/mnt/system|" buildAgent.properties
 
 echo >> buildAgent.properties # append a new line
 echo "system.aspnet.os.name=$ASPNETOSNAME" >> buildAgent.properties
 
-cd ~/TeamCity
+cd ~/BuildAgent
 
-cat <<EOF >> agentStartStop
-#!/usr/bin/env bash
+sudo cat <<EOF >> buildAgent
+#!/bin/sh
 ### BEGIN INIT INFO
-# Provides:          TeamCity build agent
+# Provides:          TeamCity Build Agent
 # Required-Start:    $remote_fs $syslog
 # Required-Stop:     $remote_fs $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: Start daemon at boot time
+# Short-Description: Start build agent daemon at boot time
 # Description:       Enable service provided by daemon.
 ### END INIT INFO
-case "\$1" in
+#Provide the correct user name:
+USER="aspnetagent"
+ 
+case "$1" in
 start)
  # Grant all users write access to /mnt, since TeamCity uses /mnt for temp storage
  chmod a+w /mnt
- sudo ~/TeamCity/bin/agent.sh start
+ su - $USER -c "cd BuildAgent/bin ; ./agent.sh start"
 ;;
 stop)
- sudo ~/TeamCity/bin/agent.sh stop
+ su - $USER -c "cd BuildAgent/bin ; ./agent.sh stop"
 ;;
 *)
   echo "usage start/stop"
@@ -111,9 +115,9 @@ esac
 exit 0
 EOF
 
-chmod +x agentStartStop
-cp agentStartStop /etc/init.d/
-update-rc.d agentStartStop defaults
+sudo chmod +x buildAgent
+sudo mv buildAgent /etc/init.d/
+sudo update-rc.d buildAgent defaults
 
 echo "Starting the build agent..."
-~/TeamCity/bin/agent.sh start
+sudo /etc/init.d/buildAgent start
