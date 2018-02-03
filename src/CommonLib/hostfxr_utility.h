5465216be950680c8b655f52eb662914b292b550
Justin Kotalik <jukotali@microsoft.com>
2018-02-02 16:07:51 -0800

    React to hostfxr api changes (#577)

--- src/CommonLib/hostfxr_utility.h
+++ src/CommonLib/hostfxr_utility.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-typedef INT(*hostfxr_get_native_search_directories_fn) (const int argc, const PCWSTR argv[], PCWSTR dest, size_t dest_size);
+typedef INT(*hostfxr_get_native_search_directories_fn) (CONST INT argc, CONST PCWSTR* argv, PWSTR buffer, DWORD buffer_size, DWORD* required_buffer_size);
 typedef INT(*hostfxr_main_fn) (CONST DWORD argc, CONST PCWSTR argv[]);
 
 #define READ_BUFFER_SIZE 4096
