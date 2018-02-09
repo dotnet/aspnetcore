67cf534bdab91f59d2d9d732242201c67990b23d
Javier Calvarro Nelson <jacalvar@microsoft.com>
2018-02-08 18:41:14 -0800

    Fix B2C templates

---
+++ src/Microsoft.DotNet.Web.ProjectTemplates/content/RazorPagesWeb-CSharp/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Company.WebApplication1.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
