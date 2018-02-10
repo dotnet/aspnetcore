342c4c44f6fa2053acec1c7271d796f9e0f98265
David Fowler <davidfowl@gmail.com>
2018-02-10 12:44:55 -0800

    Change namespace to avoid conflict

--- src/Microsoft.AspNetCore.Sockets.Abstractions/DuplexPipe.cs
+++ src/Microsoft.AspNetCore.Sockets.Abstractions/DuplexPipe.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-namespace System.IO.Pipelines
+using System.IO.Pipelines;
+
+namespace Microsoft.AspNetCore.Sockets
 {
     public class DuplexPipe : IDuplexPipe
     {
