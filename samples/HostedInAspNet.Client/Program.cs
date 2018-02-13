b834f3b35bc583b1c620bef175342094571d5e6a
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-02-13 19:58:39 +0000

    Use Razor component in HostedInAspNet.Client

--- samples/HostedInAspNet.Client/Program.cs
+++ samples/HostedInAspNet.Client/Program.cs
@@ -1,10 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.AspNetCore.Blazor.Browser;
 using Microsoft.AspNetCore.Blazor.Browser.Rendering;
-using Microsoft.AspNetCore.Blazor.Components;
-using Microsoft.AspNetCore.Blazor.RenderTree;
 
 namespace HostedInAspNet.Client
 {
@@ -12,20 +9,7 @@ namespace HostedInAspNet.Client
     {
         static void Main(string[] args)
         {
-            // Temporarily render this test component until there's a proper mechanism
-            // for testing this.
-            new BrowserRenderer().AddComponent<MyComponent>("app");
-        }
-    }
-
-    internal class MyComponent : IComponent
-    {
-        public void Init(RenderHandle renderHandle)
-        {
-        }
-
-        public void SetParameters(ParameterCollection parameters)
-        {
+            new BrowserRenderer().AddComponent<Home>("app");
         }
     }
 }
