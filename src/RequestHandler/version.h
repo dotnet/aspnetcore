4775706bbc861af92db9faaceb9fa49ab92d3006
Justin Kotalik <jukotali@microsoft.com>
2018-02-22 10:03:51 -0800

    Add version numbers on aspnetcore.dll and aspnetcorerh.dll, remove unnecessary files (#601)

--- src/RequestHandler/version.h
+++
@@ -1,8 +0,0 @@
-// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
-
-#define FileVersion 7,1,1987,0
-#define FileVersionStr "7.1.1987.0\0"
-#define ProductVersion 7,1,1987,0
-#define ProductVersionStr "7.1.1987.0\0"
-#define PlatformToolset "v141\0"
