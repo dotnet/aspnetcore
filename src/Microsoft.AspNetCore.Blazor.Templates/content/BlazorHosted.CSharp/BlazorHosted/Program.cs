89ebeda38cb036db1a06f0a8e71d29bcc051df29
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-02-27 16:02:56 +0000

    Add multi-project template "BlazorHosted"

---
+++ src/Microsoft.AspNetCore.Blazor.Templates/content/BlazorHosted.CSharp/BlazorHosted/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Blazor.Browser.Rendering;
+using Microsoft.AspNetCore.Blazor.Browser.Services;
+using System;
+
+namespace BlazorHosted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new BrowserServiceProvider(configure =>
+            {
+                // Add any custom services here
+            });
+
+            new BrowserRenderer(serviceProvider).AddComponent<App>("app");
+        }
+    }
+}
