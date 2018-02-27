39153955d637f3f6ecfd632646037f70c30568b0
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-02-27 14:31:42 +0000

    Add "dotnet new" template BlazorStandalone.CSharp

---
+++ src/Microsoft.AspNetCore.Blazor.Templates/content/BlazorStandalone.CSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Blazor.Browser.Rendering;
+using Microsoft.AspNetCore.Blazor.Browser.Services;
+using System;
+
+namespace BlazorStandalone.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new BrowserServiceProvider(configure =>
+            {
+                // Add any custom services here
+            });
+
+            new BrowserRenderer(serviceProvider).AddComponent<App>("app");
+        }
+    }
+}
