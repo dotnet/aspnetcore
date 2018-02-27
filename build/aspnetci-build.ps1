9a17a343cc308fde854088e46a0913d492440036
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-02-27 20:22:19 +0000

    Ensure Microsoft.AspNetCore.Blazor.Build is packed in a post-publish form so it's cross-plat compatible

--- build/aspnetci-build.ps1
+++ build/aspnetci-build.ps1
@@ -1,5 +1,4 @@
 Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1' -OutFile .\build\dotnet-install.ps1
 .\build\dotnet-install.ps1 -channel Current -version 2.1.3 -InstallDir dotnetsdk
-Get-Command dotnet | Format-List source
 dotnet --version
 dotnet pack Blazor.sln --configuration Release
