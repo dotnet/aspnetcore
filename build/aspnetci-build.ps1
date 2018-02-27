41fa69f665c661041361a767a02f0b50e646df0b
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-02-27 18:52:35 +0000

    Force CI to build with a specific version of .NET Core

---
+++ build/aspnetci-build.ps1
@@ -0,0 +1,3 @@
+Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1' -OutFile .\build\dotnet-install.ps1
+.\build\dotnet-install.ps1 -channel Current -version 2.1.3 -InstallDir dotnetsdk
+dotnet pack Blazor.sln
