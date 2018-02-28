3e0360a8915d457abd19534adb2fe8f6581f589e
Ryan Nowak <nowakra@gmail.com>
2018-02-28 13:21:04 -0800

    Fix #2041 - Add static constructor to DocumentWriter
    The instance Create method was always supposed to be static.
    
    (cherry picked from commit e05c2abd949d6a9acdfb82ae17594031582ca06f)

--- src/Microsoft.AspNetCore.Razor.Language/CodeGeneration/DocumentWriter.cs
+++ src/Microsoft.AspNetCore.Razor.Language/CodeGeneration/DocumentWriter.cs
@@ -2,12 +2,30 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.ComponentModel;
 using Microsoft.AspNetCore.Razor.Language.Intermediate;
 
 namespace Microsoft.AspNetCore.Razor.Language.CodeGeneration
 {
     public abstract class DocumentWriter
     {
+        public static DocumentWriter CreateDefault(CodeTarget codeTarget, RazorCodeGenerationOptions options)
+        {
+            if (codeTarget == null)
+            {
+                throw new ArgumentNullException(nameof(codeTarget));
+            }
+
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            return new DefaultDocumentWriter(codeTarget, options);
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        [Obsolete("This method was intended to be static, use CreateDefault instead.")]
         public DocumentWriter Create(CodeTarget codeTarget, RazorCodeGenerationOptions options)
         {
             if (codeTarget == null)
