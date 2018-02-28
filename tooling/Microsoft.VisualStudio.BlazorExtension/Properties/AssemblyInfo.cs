2978830a9f5354206f0a3a99fdf08e1ae42a50e4
Ryan Nowak <nowakra@gmail.com>
2018-02-28 08:19:28 -0800

    Fix version shenanigans

--- tooling/Microsoft.VisualStudio.BlazorExtension/Properties/AssemblyInfo.cs
+++ tooling/Microsoft.VisualStudio.BlazorExtension/Properties/AssemblyInfo.cs
@@ -5,10 +5,13 @@ using Microsoft.VisualStudio.Shell;
 
 // Add binding redirects for each assembly we ship in VS. This is required so that these assemblies show
 // up in the Load context, which means that we can use ServiceHub and other nice things.
+//
+// The versions here need to match what the build is producing. If you change the version numbers
+// for the Blazor assemblies, this needs to change as well.
 [assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.VisualStudio.LanguageServices.Blazor",
     GenerateCodeBase = true,
     PublicKeyToken = "",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "1.0.0.0",
-    NewVersion = "1.0.0.0")]
+    OldVersionUpperBound = "0.0.5.0",
+    NewVersion = "0.0.5.0")]
