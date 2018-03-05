ede0bdcdee6ac69dbd4123447255ba6e82ca58ac
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-03-05 14:07:24 -0800

    Temporarily hardcode msbuild path in build script

--- build/aspnetci-build.ps1
+++ build/aspnetci-build.ps1
@@ -2,5 +2,5 @@ Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1' -OutFile .\build
 .\build\dotnet-install.ps1 -channel Current -version 2.1.3 -InstallDir dotnetsdk
 dotnet --version
 dotnet pack Blazor.sln --configuration Release
-msbuild BlazorTooling.sln /t:Restore
-msbuild BlazorTooling.sln
+cmd /c "C:\Program Files (x86)\Microsoft Visual Studio\aspnetci\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" BlazorTooling.sln /t:Restore
+cmd /c "C:\Program Files (x86)\Microsoft Visual Studio\aspnetci\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" BlazorTooling.sln
