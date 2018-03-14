a3886057d5925e26fd6dcda3ce6df81db00ff148
BrennanConroy <brecon@microsoft.com>
2018-03-14 09:28:50 -0700

    Add benchmark application to our repo (#1579)

---
+++ benchmarks/BenchmarkServer/Hubs/EchoHub.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BenchmarkServer.Hubs
+{
+    public class EchoHub : Hub
+    {
+        public async Task Echo(int duration)
+        {
+            try
+            {
+                var t = new CancellationTokenSource();
+                t.CancelAfter(TimeSpan.FromSeconds(duration));
+                while (!t.IsCancellationRequested && !Context.Connection.ConnectionAbortedToken.IsCancellationRequested)
+                {
+                    await Clients.All.SendAsync("echo", DateTime.UtcNow);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("Echo exited");
+        }
+    }
+}
---
+++ benchmarks/BenchmarkServer/Program.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace BenchmarkServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .AddCommandLine(args)
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseConfiguration(config)
+                .ConfigureLogging(loggerFactory =>
+                {
+                    if (Enum.TryParse(config["LogLevel"], out LogLevel logLevel))
+                    {
+                        loggerFactory.AddConsole().SetMinimumLevel(logLevel);
+                    }
+                })
+                .UseKestrel()
+                .UseStartup<Startup>();
+
+            host.Build().Run();
+        }
+    }
+}
