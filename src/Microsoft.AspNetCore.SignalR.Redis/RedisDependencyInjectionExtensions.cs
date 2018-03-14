2b68db873aceb70b2911f755d543395e9ba14b85
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-03-14 16:24:28 -0700

    add AddRedis overload that takes connection string (#1588)

--- src/Microsoft.AspNetCore.SignalR.Redis/RedisDependencyInjectionExtensions.cs
+++ src/Microsoft.AspNetCore.SignalR.Redis/RedisDependencyInjectionExtensions.cs
@@ -4,6 +4,7 @@
 using System;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.SignalR.Redis;
+using StackExchange.Redis;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -12,6 +13,14 @@ namespace Microsoft.Extensions.DependencyInjection
         public static ISignalRBuilder AddRedis(this ISignalRBuilder builder)
         {
             return AddRedis(builder, o => { });
+        }
+
+        public static ISignalRBuilder AddRedis(this ISignalRBuilder builder, string redisConnectionString)
+        {
+            return AddRedis(builder, o =>
+            {
+                o.Options = ConfigurationOptions.Parse(redisConnectionString);
+            });
         }
 
         public static ISignalRBuilder AddRedis(this ISignalRBuilder builder, Action<RedisOptions> configure)
