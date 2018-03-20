dotnet/blazor

Support route constraints of the form ":type" (e.g, ":int", ":guid", etc.)

src/Microsoft.AspNetCore.Blazor/Routing/RouteConstraint.cs
@@ -1,0 +1,74 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Microsoft.AspNetCore.Blazor.Routing
+{
+    internal abstract class RouteConstraint
+    {
+        private static readonly IDictionary<string, RouteConstraint> _cachedConstraints
+            = new Dictionary<string, RouteConstraint>();
+
+        public abstract bool Match(string pathSegment, out object convertedValue);
+
+        public static RouteConstraint Parse(string template, string segment, string constraint)
+        {
+            if (string.IsNullOrEmpty(constraint))
+            {
+                throw new ArgumentException($"Malformed segment '{segment}' in route '{template}' contains an empty constraint.");
+            }
+
+            if (_cachedConstraints.TryGetValue(constraint, out var cachedInstance))
+            {
+                return cachedInstance;
+            }
+            else
+            {
+                var newInstance = CreateRouteConstraint(constraint);
+                if (newInstance != null)
+                {
+                    _cachedConstraints[constraint] = newInstance;
+                    return newInstance;
+                }
+                else
+                {
+                    throw new ArgumentException($"Unsupported constraint '{constraint}' in route '{template}'.");
+                }
+            }
+        }
+
+        private static RouteConstraint CreateRouteConstraint(string constraint)
+        {
+            switch (constraint)
+            {
+                case "bool":
+                    return new RouteConstraintType<bool>(bool.TryParse);
+                case "datetime":
+                    return new RouteConstraintType<DateTime>((string str, out DateTime result)
+                        => DateTime.TryParse(str, CultureInfo.InvariantCulture, DateTimeStyles.None, out result));
+                case "decimal":
+                    return new RouteConstraintType<decimal>((string str, out decimal result)
+                        => decimal.TryParse(str, NumberStyles.Number, CultureInfo.InvariantCulture, out result));
+                case "double":
+                    return new RouteConstraintType<double>((string str, out double result)
+                        => double.TryParse(str, NumberStyles.Number, CultureInfo.InvariantCulture, out result));
+                case "float":
+                    return new RouteConstraintType<float>((string str, out float result)
+                        => float.TryParse(str, NumberStyles.Number, CultureInfo.InvariantCulture, out result));
+                case "guid":
+                    return new RouteConstraintType<Guid>(Guid.TryParse);
+                case "int":
+                    return new RouteConstraintType<int>((string str, out int result)
+                        => int.TryParse(str, NumberStyles.None, CultureInfo.InvariantCulture, out result));
+                case "long":
+                    return new RouteConstraintType<long>((string str, out long result)
+                        => long.TryParse(str, NumberStyles.None, CultureInfo.InvariantCulture, out result));
+                default:
+                    return null;
+            }
+        }
+    }
+}

dotnet/aspnetcore

src/Microsoft.AspNetCore.Blazor/Routing/RouteConstraint.cs
@@ -1,0 +1,74 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Microsoft.AspNetCore.Blazor.Routing
+{
+    internal abstract class RouteConstraint
+    {
+        private static readonly IDictionary<string, RouteConstraint> _cachedConstraints
+            = new Dictionary<string, RouteConstraint>();
+
+        public abstract bool Match(string pathSegment, out object convertedValue);
+
+        public static RouteConstraint Parse(string template, string segment, string constraint)
+        {
+            if (string.IsNullOrEmpty(constraint))
+            {
+                throw new ArgumentException($"Malformed segment '{segment}' in route '{template}' contains an empty constraint.");
+            }
+
+            if (_cachedConstraints.TryGetValue(constraint, out var cachedInstance))
+            {
+                return cachedInstance;
+            }
+            else
+            {
+                var newInstance = CreateRouteConstraint(constraint);
+                if (newInstance != null)
+                {
+                    _cachedConstraints[constraint] = newInstance;
+                    return newInstance;
+                }
+                else
+                {
+                    throw new ArgumentException($"Unsupported constraint '{constraint}' in route '{template}'.");
+                }
+            }
+        }
+
+        private static RouteConstraint CreateRouteConstraint(string constraint)
+        {
+            switch (constraint)
+            {
+                case "bool":
+                    return new TypeRouteConstraint<bool>(bool.TryParse);
+                case "datetime":
+                    return new TypeRouteConstraint<DateTime>((string str, out DateTime result)
+                        => DateTime.TryParse(str, CultureInfo.InvariantCulture, DateTimeStyles.None, out result));
+                case "decimal":
+                    return new TypeRouteConstraint<decimal>((string str, out decimal result)
+                        => decimal.TryParse(str, NumberStyles.Number, CultureInfo.InvariantCulture, out result));
+                case "double":
+                    return new TypeRouteConstraint<double>((string str, out double result)
+                        => double.TryParse(str, NumberStyles.Number, CultureInfo.InvariantCulture, out result));
+                case "float":
+                    return new TypeRouteConstraint<float>((string str, out float result)
+                        => float.TryParse(str, NumberStyles.Number, CultureInfo.InvariantCulture, out result));
+                case "guid":
+                    return new TypeRouteConstraint<Guid>(Guid.TryParse);
+                case "int":
+                    return new TypeRouteConstraint<int>((string str, out int result)
+                        => int.TryParse(str, NumberStyles.None, CultureInfo.InvariantCulture, out result));
+                case "long":
+                    return new TypeRouteConstraint<long>((string str, out long result)
+                        => long.TryParse(str, NumberStyles.None, CultureInfo.InvariantCulture, out result));
+                default:
+                    return null;
+            }
+        }
+    }
+}