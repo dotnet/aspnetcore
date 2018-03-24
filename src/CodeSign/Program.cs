07b403b536547430047280708efc6105bb661467
Nate McMaster <nate.mcmaster@microsoft.com>
2018-03-23 18:35:32 -0700

    Reduce duplication in binaries sent for signing

--- src/CodeSign/Program.cs
+++ src/CodeSign/Program.cs
@@ -15,21 +15,25 @@ namespace CodeSign
             var localBuild = app.Option("--local-build", "Local (test) build", CommandOptionType.NoValue);
             var description = app.Option("--description", "Description", CommandOptionType.SingleValue);
             var displayName = app.Option("--display-name", "Display Name", CommandOptionType.SingleValue);
+            var jobName = app.Option("--job-name", "Job Name", CommandOptionType.SingleValue);
             var displayUrl = app.Option("--display-url", "Display Url", CommandOptionType.SingleValue);
+            var batchSize = app.Option("--batch-size", "Batch size", CommandOptionType.SingleValue);
+            var logDir = app.Option("--log-dir", "LogDir", CommandOptionType.SingleValue);
             var files = app.Option("-f", "File", CommandOptionType.MultipleValue);
 
             app.OnExecute(() =>
             {
                 var submitCodeSignJob = new SubmitCodeSignJob
                 {
-
                     Approvers = approvers.Values,
                     Certificates = certificates.Values,
                     LocalBuild = localBuild.HasValue(),
                     Description = description.Value(),
                     DisplayName = displayName.Value(),
                     DisplayUrl = displayUrl.Value(),
+                    JobName = jobName.Value(),
                     Files = files.Values,
+                    LogDir = logDir.Value() ?? AppDomain.CurrentDomain.BaseDirectory,
                 };
 
                 using (submitCodeSignJob)
--- src/CodeSign/SubmitCodeSignJob.cs
+++ src/CodeSign/SubmitCodeSignJob.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using CODESIGN.Submitter;
 
@@ -34,6 +37,10 @@ namespace CodeSign
 
         public string DisplayUrl { get; set; }
 
+        public string JobName { get; set; }
+
+        public string LogDir { get; set; }
+
         public List<string> Files { get; set; }
 
         public void Execute()
@@ -44,14 +51,31 @@ namespace CodeSign
 
             var fileMapping = StageFiles();
 
+            Console.WriteLine($"Job '{JobName}' contains {fileMapping.Count} unique file(s)");
+
+            var sb = new StringBuilder();
+            foreach (var file in fileMapping)
+            {
+                sb.Append(file.Value[0].FileName).Append(", hash=").AppendLine(file.Key);
+                foreach (var request in file.Value)
+                {
+                    sb.Append("      ").AppendLine(request.OriginalPath);
+                }
+                sb.AppendLine();
+            }
+
+            File.WriteAllText(Path.Combine(LogDir, $"Signjob-{JobName}-files.txt"), sb.ToString());
+
             string completionPath;
             if (!LocalBuild)
             {
                 var job = InitializeJob(fileMapping.Keys);
                 job.Send();
+                var timer = Stopwatch.StartNew();
                 Console.WriteLine($"Job #{job.JobNumber} was sent.");
                 WatchJob(job);
-                Console.WriteLine($"Job #{job.JobNumber} finished at '{job.JobCompletionPath}'.");
+                timer.Stop();
+                Console.WriteLine($"Job #{job.JobNumber} finished at '{job.JobCompletionPath}' in {timer.ElapsedMilliseconds / 1000} sec.");
                 completionPath = job.JobCompletionPath;
             }
             else
@@ -108,25 +132,48 @@ namespace CodeSign
             }
         }
 
-        private Dictionary<string, string> StageFiles()
+        private IReadOnlyDictionary<string, List<FileRequest>> StageFiles()
         {
             Directory.CreateDirectory(_stagingDirectory);
-            var fileMapping = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 
-            for (var i = 0; i < Files.Count; i++)
+            var fileMapping = new ConcurrentDictionary<string, List<FileRequest>>(StringComparer.OrdinalIgnoreCase);
+            Parallel.ForEach(Files, f =>
             {
-                var file = Files[i];
-                var mappedFileName = $"{i}_{Path.GetFileName(file)}";
-                var mappedFilePath = Path.Combine(_stagingDirectory, mappedFileName);
+                var hash = GetFileHash(f);
+                var request = new FileRequest { OriginalPath = f, FileName = Path.GetFileName(f) };
+                fileMapping.AddOrUpdate(hash,
+                    (_) => { return new List<FileRequest> { request }; },
+                    (_, list) => { list.Add(request); return list; });
+            });
 
-                fileMapping.Add(mappedFilePath, file);
-                File.Copy(file, mappedFilePath, overwrite: true);
+            foreach (var item in fileMapping)
+            {
+                var mappedFilePath = Path.Combine(_stagingDirectory, item.Key);
+                File.Copy(item.Value[0].OriginalPath, mappedFilePath, overwrite: true);
             }
 
             return fileMapping;
         }
 
-        private Job InitializeJob(ICollection<string> files)
+        private static string GetFileHash(string path)
+        {
+            byte[] hash;
+            using (var algorithm = new SHA256Managed())
+            using (var file = File.OpenRead(path))
+            {
+                hash = algorithm.ComputeHash(file);
+            }
+
+            var sb = new StringBuilder();
+            foreach (var b in hash)
+            {
+                sb.AppendFormat("{0:X2}", b);
+            }
+
+            return sb.ToString();
+        }
+
+        private Job InitializeJob(IEnumerable<string> files)
         {
             // Begin job submission
             var isSSL = true;
@@ -145,7 +192,7 @@ namespace CodeSign
             }
 
             // Add approvers and exclude the submitter from the list
-            foreach (var approver in Approvers.Except(new [] { Submitter }))
+            foreach (var approver in Approvers.Except(new[] { Submitter }))
             {
                 job.AddApprover(approver);
             }
@@ -195,18 +242,20 @@ namespace CodeSign
             }
         }
 
-        private void CopyStagedFiles(string completionPath, Dictionary<string, string> fileMappings)
+        private void CopyStagedFiles(string completionPath, IReadOnlyDictionary<string, List<FileRequest>> fileMappings)
         {
             var copiedFiles = new ConcurrentBag<string>();
             Parallel.ForEach(new DirectoryInfo(completionPath).EnumerateFiles(), file =>
             {
                 // Calculate the destination using the name of the drop share. e.g.
-                // \\drop-share\1.dll -> c:\temp\staging-dir\1.dll -> obj\extracted\Lib.dll
-                var mappedLocation = Path.Combine(_stagingDirectory, file.Name);
-                if (fileMappings.TryGetValue(mappedLocation, out var finalDestination))
+                // obj\extracted\Lib.dll -> c:\temp\staging-dir\HASHABC123 -> $completionPath\HASHABC123 -> obj\extracted\Lib.dll
+                if (fileMappings.TryGetValue(file.Name, out var requests))
                 {
-                    file.CopyTo(finalDestination, overwrite: true);
-                    copiedFiles.Add(mappedLocation);
+                    foreach (var dest in requests)
+                    {
+                        file.CopyTo(dest.OriginalPath, overwrite: true);
+                    }
+                    copiedFiles.Add(file.Name);
                 }
                 else
                 {
@@ -219,6 +268,13 @@ namespace CodeSign
                 var missingFiles = fileMappings.Keys.Except(copiedFiles, StringComparer.OrdinalIgnoreCase);
                 throw new Exception($"The following files were not found in the drop location:{Environment.NewLine}{missingFiles}");
             }
+        }
+
+        private struct FileRequest
+        {
+            public string OriginalPath;
+            public string FileName;
+            public string Hash;
         }
 
         public void Dispose()
