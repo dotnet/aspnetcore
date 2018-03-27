811482d9dab0874161d391ed3e2c69550fbece2d
Pranav K <prkrishn@hotmail.com>
2018-03-27 13:51:04 -0700

    Update versions to 0.2.0-preview1

--- tooling/Microsoft.VisualStudio.BlazorExtension/Properties/AssemblyInfo.cs
+++ tooling/Microsoft.VisualStudio.BlazorExtension/Properties/AssemblyInfo.cs
@@ -20,12 +20,12 @@ using Microsoft.VisualStudio.Shell;
     GenerateCodeBase = true,
     PublicKeyToken = "",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "0.1.0.0",
-    NewVersion = "0.1.0.0")]
+    OldVersionUpperBound = "0.2.0.0",
+    NewVersion = "0.2.0.0")]
 [assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.VisualStudio.LanguageServices.Blazor",
     GenerateCodeBase = true,
     PublicKeyToken = "",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "0.1.0.0",
-    NewVersion = "0.1.0.0")]
+    OldVersionUpperBound = "0.2.0.0",
+    NewVersion = "0.2.0.0")]
