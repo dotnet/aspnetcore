307ed01051803607f4f58ca50509f7fc9aa39a1d
BrennanConroy <brecon@microsoft.com>
2018-04-05 17:16:39 -0700

    ReEnable SSETransportStopsSendAndReceiveLoopsWhenTransportStopped (#1877)

--- test/Microsoft.AspNetCore.SignalR.Client.Tests/ServerSentEventsTransportTests.cs
+++ test/Microsoft.AspNetCore.SignalR.Client.Tests/ServerSentEventsTransportTests.cs
@@ -65,7 +65,7 @@ namespace Microsoft.AspNetCore.SignalR.Client.Tests
             }
         }
 
-        [Fact(Skip = "Flaky tests keep failing")]
+        [Fact]
         public async Task SSETransportStopsSendAndReceiveLoopsWhenTransportStopped()
         {
             var eventStreamCts = new CancellationTokenSource();
@@ -107,7 +107,7 @@ namespace Microsoft.AspNetCore.SignalR.Client.Tests
                     transportActiveTask = sseTransport.Running;
                     Assert.False(transportActiveTask.IsCompleted);
                     var message = await pair.Transport.Input.ReadSingleAsync().OrTimeout();
-                    Assert.Equal("3:abc", Encoding.ASCII.GetString(message));
+                    Assert.StartsWith("3:abc", Encoding.ASCII.GetString(message));
                 }
                 finally
                 {
