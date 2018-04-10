5c91dbe062f83960f0454ec385b75958f68ff9bc
Justin Kotalik <jukotali@microsoft.com>
2018-04-10 10:41:38 -0700

    Get tests working

---
+++ src/AspNetCoreModuleV1/AspNetCore/resource.h
@@ -0,0 +1,18 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by aspnetcoremodule.rc
+//
+#define ASPNETCORE_EVENT_MSG_BUFFER_SIZE 256
+#define IDS_INVALID_PROPERTY            1000
+#define IDS_SERVER_ERROR                1001
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
