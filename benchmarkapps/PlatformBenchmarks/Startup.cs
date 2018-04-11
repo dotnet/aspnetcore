e411cbf7e2910c848221b895cda19b77d7b9761d
Sébastien Ros <sebastienros@gmail.com>
2018-04-11 14:27:34 -0700

    Moving PlatformBenchmarks from aspnet/benchmarks (#2457)

---
+++ benchmarkapps/PlatformBenchmarks/Startup.cs
@@ -0,0 +1,19 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace PlatformBenchmarks
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            
+        }
+    }
+}
