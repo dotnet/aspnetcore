7ec405f87e44b59fe825ec330d2abaafcdacefa0
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-04-11 18:39:23 +0100

    Update binding redirects for 0.3.0 builds

--- tooling/Microsoft.VisualStudio.BlazorExtension/Properties/AssemblyInfo.cs
+++ tooling/Microsoft.VisualStudio.BlazorExtension/Properties/AssemblyInfo.cs
@@ -20,12 +20,12 @@ using Microsoft.VisualStudio.Shell;
     GenerateCodeBase = true,
     PublicKeyToken = "",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "0.2.0.0",
-    NewVersion = "0.2.0.0")]
+    OldVersionUpperBound = "0.3.0.0",
+    NewVersion = "0.3.0.0")]
 [assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.VisualStudio.LanguageServices.Blazor",
     GenerateCodeBase = true,
     PublicKeyToken = "",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "0.2.0.0",
-    NewVersion = "0.2.0.0")]
+    OldVersionUpperBound = "0.3.0.0",
+    NewVersion = "0.3.0.0")]
