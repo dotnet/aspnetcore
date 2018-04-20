31673e84f4925cee9edfebf832e67202255ef23e
Pranav K <prkrishn@hotmail.com>
2018-04-20 10:57:53 -0700

    Re-enable Windows dotnet-ci builds (#1978)
    * Re-enable Windows dotnet-ci builds

--- build/buildpipeline/windows.groovy
+++ build/buildpipeline/windows.groovy
@@ -1,2 +1,14 @@
 @Library('dotnet-ci') _
 
+// 'node' indicates to Jenkins that the enclosed block runs on a node that matches
+// the label 'windows-with-vs'
+simpleNode('Windows.10.Enterprise.RS3.ASPNET') {
+    stage ('Checking out source') {
+        checkout scm
+    }
+    stage ('Build') {
+        def logFolder = getLogFolder()
+        def environment = "set ASPNETCORE_TEST_LOG_DIR=${WORKSPACE}\\${logFolder}"
+        bat "${environment}&.\\run.cmd -CI default-build"
+    }
+}
