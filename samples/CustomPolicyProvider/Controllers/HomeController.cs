8d0f84ea464ed3c0f9ec0a28d9f5f7ecd02617c0
Mike Rousos <mikerou@microsoft.com>
2018-04-26 16:25:07 -0400

    Add IAuthorizationPolicyProvider sample (#31)
    * Add IAuthorizationPolicyProvider sample
    
    * Update IAuthorizationPolicyProvider sample to show falling back to a default provider
    
    * Make project, directory, and namespace names consistent for custom policy provider sample
    
    * Make policy provider sample an MVC app and replace Values controller with a home controller (with views)
    
    * Add cookie authentication and account controller/views
    
    * Update readme and comments to reflect new authorization policy provider scenario
    
    * Add some more information to the access denied page for debugging purposes
    
    * Add some additional logging
    
    * Minor updates based on PR feedback

---
+++ samples/CustomPolicyProvider/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomPolicyProvider.Controllers
+{
+    // Sample actions to demonstrate the use of the [MinimumAgeAuthorize] attribute
+    [Controller]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // View protected with custom parameterized authorization policy
+        [MinimumAgeAuthorize(10)]
+        public IActionResult MinimumAge10()
+        {
+            return View("MinimumAge", 10);
+        }
+
+        // View protected with custom parameterized authorization policy
+        [MinimumAgeAuthorize(50)]
+        public IActionResult MinimumAge50()
+        {
+            return View("MinimumAge", 50);
+        }
+    }
+}
