cc47426c77fd863e74e3d57f201b95392292f48b
Hao Kung <HaoK@users.noreply.github.com>
2018-05-01 13:04:56 -0700

    Add some initial functional tests for auth samples (#41)

--- test/AuthSamples.FunctionalTests/CustomPolicyProviderTests.cs
+++ test/AuthSamples.FunctionalTests/CustomPolicyProviderTests.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -13,7 +15,7 @@ namespace AuthSamples.FunctionalTests
     {
         public CustomPolicyProviderTests(WebApplicationFactory<CustomPolicyProvider.Startup> fixture)
         {
-            Client = fixture.CreateDefaultClient();
+            Client = fixture.CreateClient();
         }
 
         public HttpClient Client { get; }
@@ -37,8 +39,70 @@ namespace AuthSamples.FunctionalTests
             var content = await response.Content.ReadAsStringAsync();
 
             // Assert
-            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
-            Assert.Equal("http://localhost/account/signin?ReturnUrl=%2FHome%2FMinimumAge10", response.Headers.Location.ToString());
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("http://localhost/account/signin?ReturnUrl=%2FHome%2FMinimumAge10", response.RequestMessage.RequestUri.ToString());
+        }
+
+        [Fact]
+        public async Task MinimumAge10WorksIfOldEnough()
+        {
+            // Arrange & Act
+            var signIn = await SignIn(Client, "Dude", DateTime.Now.Subtract(TimeSpan.FromDays(365 * 20)).ToShortDateString());
+            Assert.Equal(HttpStatusCode.OK, signIn.StatusCode);
+
+            var response = await Client.GetAsync("/Home/MinimumAge10");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Contains("Welcome, Dude", content);
+            Assert.Contains("Welcome to a page restricted to users 10 or older", content);
+        }
+
+        [Fact]
+        public async Task MinimumAge10FailsIfNotOldEnough()
+        {
+            // Arrange & Act
+            var signIn = await SignIn(Client, "Dude", DateTime.Now.Subtract(TimeSpan.FromDays(365 * 5)).ToShortDateString());
+            Assert.Equal(HttpStatusCode.OK, signIn.StatusCode);
+
+            var response = await Client.GetAsync("/Home/MinimumAge10");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Contains("Access Denied: Dude is not authorized to view this page.", content);
+        }
+
+        [Fact]
+        public async Task MinimumAge50WorksIfOldEnough()
+        {
+            // Arrange & Act
+            var signIn = await SignIn(Client, "Dude", DateTime.Now.Subtract(TimeSpan.FromDays(365 * 55)).ToShortDateString());
+            Assert.Equal(HttpStatusCode.OK, signIn.StatusCode);
+
+            var response = await Client.GetAsync("/Home/MinimumAge50");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Contains("Welcome, Dude", content);
+            Assert.Contains("Welcome to a page restricted to users 50 or older", content);
+        }
+
+        [Fact]
+        public async Task MinimumAge50FailsIfNotOldEnough()
+        {
+            // Arrange & Act
+            var signIn = await SignIn(Client, "Dude", DateTime.Now.Subtract(TimeSpan.FromDays(365 * 20)).ToShortDateString());
+            Assert.Equal(HttpStatusCode.OK, signIn.StatusCode);
+
+            var response = await Client.GetAsync("/Home/MinimumAge50");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Contains("Access Denied: Dude is not authorized to view this page.", content);
         }
 
         [Fact]
@@ -49,8 +113,21 @@ namespace AuthSamples.FunctionalTests
             var content = await response.Content.ReadAsStringAsync();
 
             // Assert
-            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
-            Assert.Equal("http://localhost/account/signin?ReturnUrl=%2FHome%2FMinimumAge50", response.Headers.Location.ToString());
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("http://localhost/account/signin?ReturnUrl=%2FHome%2FMinimumAge50", response.RequestMessage.RequestUri.ToString());
+        }
+
+        internal static async Task<HttpResponseMessage> SignIn(HttpClient client, string userName, string dob)
+        {
+            var goToSignIn = await client.GetAsync("/account/signin");
+            var signIn = await TestAssert.IsHtmlDocumentAsync(goToSignIn);
+
+            var form = TestAssert.HasForm(signIn);
+            return await client.SendAsync(form, new Dictionary<string, string>()
+            {
+                ["UserName"] = userName,
+                ["DOB"] = dob,
+            });
         }
     }
 }
