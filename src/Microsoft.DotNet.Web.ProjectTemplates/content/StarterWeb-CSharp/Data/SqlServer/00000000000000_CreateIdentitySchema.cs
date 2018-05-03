89956df7eac902c55c365cfbf3800c9a5a8fdc1d
Hao Kung <HaoK@users.noreply.github.com>
2018-05-03 13:22:26 -0700

    [Review] Updated migrations and scripts (#484)
    * Update migrations
    
    * Add regex for schema name and product version
    
    * Fix ModelSnapshot product version
    
    * Change version to 2.1.0

--- src/Microsoft.DotNet.Web.ProjectTemplates/content/StarterWeb-CSharp/Data/SqlServer/00000000000000_CreateIdentitySchema.cs
+++ src/Microsoft.DotNet.Web.ProjectTemplates/content/StarterWeb-CSharp/Data/SqlServer/00000000000000_CreateIdentitySchema.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
