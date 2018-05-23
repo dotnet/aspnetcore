6cf45a4f8a05d68382601adef3bcf80a6c762705
Nate McMaster <nate.mcmaster@microsoft.com>
2018-05-23 15:38:16 -0700

    Prepare the 2.1.1 patch

---
+++ build/tools/NuspecBaselineGenerator/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using McMaster.Extensions.CommandLineUtils;
+using NuGet.Packaging;
+
+namespace NuspecBaselineGenerator
+{
+    class Program
+    {
+        static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        [Required]
+        [DirectoryExists]
+        [Argument(0)]
+        public string[] Directories { get; }
+
+        [Required]
+        [Option]
+        [FileExists]
+        public string Artifacts { get; }
+
+        private void OnExecute()
+        {
+            var doc = XDocument.Load(Artifacts);
+            var versions = new List<(string, string)>();
+            foreach (var dir in Directories)
+            {
+                foreach (var nupkg in Directory.EnumerateFiles(dir, "*.nupkg"))
+                {
+                    using (var reader = new PackageArchiveReader(nupkg))
+                    {
+                        var identity = reader.GetIdentity();
+                        versions.Add((identity.Id, identity.Version.ToNormalizedString()));
+                    }
+                }
+            }
+
+            void WriteAttribute(XElement element, string attr)
+            {
+                var attribute = element.Attribute(attr);
+                if (attribute != null)
+                {
+                    Console.Write($" {attr}=\"{attribute.Value}\"");
+                }
+            }
+
+            foreach (var item in versions.OrderBy(i => i.Item1))
+            {
+                var element = doc
+                    .Descendants("PackageArtifact")
+                    .SingleOrDefault(p => p.Attribute("Include")?.Value == item.Item1);
+
+
+                Console.Write($"<ExternalDependency Include=\"{item.Item1}\" Version=\"{item.Item2}\"");
+                if (element != null)
+                {
+                    WriteAttribute(element, "Analyzer");
+                    WriteAttribute(element, "AllMetapackage");
+                    WriteAttribute(element, "AppMetapackage");
+                    WriteAttribute(element, "LZMA");
+                    WriteAttribute(element, "PackageType");
+                    WriteAttribute(element, "Category");
+                }
+                Console.WriteLine(" />");
+            }
+        }
+    }
+}
---
+++ scripts/PatchVersionPrefix.ps1
@@ -0,0 +1,67 @@
+#!/usr/bin/env pwsh
+
+<#
+.SYNOPSIS
+    Updates the version.props file in repos to a newer patch version
+.PARAMETER Repos
+    A list of the repositories that should be patched
+#>
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true)]
+    [string[]]$Repos,
+    [switch]$NoCommit
+)
+
+$ErrorActionPreference = 'Stop'
+
+Import-Module -Scope Local -Force "$PSScriptRoot/common.psm1"
+
+function BumpPatch([System.Xml.XmlNode]$node) {
+    if (-not $node) {
+        return
+    }
+    [version] $version = $node.InnerText
+    $node.InnerText = "{0}.{1}.{2}" -f $version.Major, $version.Minor, ($version.Build + 1)
+    return "Bumping version from $version to $($node.InnerText)"
+}
+
+foreach ($repo in $Repos) {
+    $repoPath = "$PSScriptRoot/../modules/$repo"
+    Push-Location $repoPath
+    try
+    {
+        $path = "$repoPath/version.props"
+        Write-Host -ForegroundColor Magenta "Updating $repo"
+        if (-not (Test-Path $path)) {
+            Write-Warning "$path does not exist"
+            continue
+        }
+        $path = Resolve-Path $path
+        Write-Verbose "$path"
+        [xml] $xml = LoadXml $path
+
+        $suffix = $xml.SelectSingleNode('/Project/PropertyGroup/VersionSuffix')
+        if (-not $suffix) {
+            write-error "$path does not have VersionSuffix"
+        }
+
+        $versionPrefix = $xml.SelectSingleNode('/Project/PropertyGroup/VersionPrefix')
+        $epxVersionPrefix = $xml.SelectSingleNode('/Project/PropertyGroup/ExperimentalProjectVersionPrefix')
+        $exVersionPrefix = $xml.SelectSingleNode('/Project/PropertyGroup/ExperimentalVersionPrefix')
+        BumpPatch $epxVersionPrefix | write-host
+        BumpPatch $exVersionPrefix | write-host
+        $message = BumpPatch $versionPrefix
+        Write-Host $message
+        SaveXml $xml $path
+        if (-not $NoCommit) {
+            Invoke-Block { & git add $path }
+            Invoke-Block { & git commit -m $message }
+        }
+    }
+    finally
+    {
+        Pop-Location
+    }
+}
+
