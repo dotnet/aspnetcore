b8bb75d592a1a24ac9e23fe52abd56f9198a2a25
Pranav K <prkrishn@hotmail.com>
2018-05-24 15:51:12 -0700

    Fix typo

--- build/buildpipeline/pipeline.groovy
+++ build/buildpipeline/pipeline.groovy
@@ -1,7 +1,7 @@
 import org.dotnet.ci.pipelines.Pipeline
 
 def windowsPipeline = Pipeline.createPipeline(this, 'build/buildpipeline/windows.groovy')
-def windowsPipeline = Pipeline.createPipeline(this, 'build/buildpipeline/windows-es.groovy')
+def windowsESPipeline = Pipeline.createPipeline(this, 'build/buildpipeline/windows-es.groovy')
 def linuxPipeline = Pipeline.createPipeline(this, 'build/buildpipeline/linux.groovy')
 def osxPipeline = Pipeline.createPipeline(this, 'build/buildpipeline/osx.groovy')
 String configuration = 'Release'
@@ -12,8 +12,8 @@ def parameters = [
 windowsPipeline.triggerPipelineOnEveryGithubPR("Windows ${configuration} x64 Build", parameters)
 windowsPipeline.triggerPipelineOnGithubPush(parameters)
 
-windowsPipeline.triggerPipelineOnEveryGithubPR("Windows ${configuration} x64 Build (Spanish language image) ", parameters)
-windowsPipeline.triggerPipelineOnGithubPush(parameters)
+windowsESPipeline.triggerPipelineOnEveryGithubPR("Windows ${configuration} x64 Build (Spanish language image) ", parameters)
+windowsESPipeline.triggerPipelineOnGithubPush(parameters)
 
 linuxPipeline.triggerPipelineOnEveryGithubPR("Ubuntu 16.04 ${configuration} Build", parameters)
 linuxPipeline.triggerPipelineOnGithubPush(parameters)
