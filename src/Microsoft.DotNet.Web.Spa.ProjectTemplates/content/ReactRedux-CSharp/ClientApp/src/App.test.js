f7a9b6f6a3ea27af69632000217912c85c5c545b
Simon Grimshaw <simongrimshaw@gmail.com>
2018-05-27 21:30:52 +0100

    Fixing issue in the react and redux template where the test would fail as its not wrapped in a router.

--- src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/ClientApp/src/App.test.js
+++ src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/ClientApp/src/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
 import App from './App';
 
 it('renders without crashing', () => {
+  const mockStore = configureStore([])({});
+
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={mockStore}>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </Provider>, div);
 });
