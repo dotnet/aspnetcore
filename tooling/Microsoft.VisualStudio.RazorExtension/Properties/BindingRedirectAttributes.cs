cdbcfb7a05db42f33c27aff0dcffa76735db13e7
Pranav K <prkrishn@hotmail.com>
2018-05-29 14:31:42 -0700

    Fixup references to versions in vsix

--- tooling/Microsoft.VisualStudio.RazorExtension/Properties/BindingRedirectAttributes.cs
+++ tooling/Microsoft.VisualStudio.RazorExtension/Properties/BindingRedirectAttributes.cs
@@ -8,61 +8,61 @@ using Microsoft.VisualStudio.Shell;
     GenerateCodeBase = true,
     PublicKeyToken = "adb9793829ddae60",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "2.1.0.0",
-    NewVersion = "2.1.0.0")]
+    OldVersionUpperBound = "2.1.1.0",
+    NewVersion = "2.1.1.0")]
 
 [assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.AspNetCore.Mvc.Razor.Extensions.Version1_X",
     GenerateCodeBase = true,
     PublicKeyToken = "adb9793829ddae60",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "2.1.0.0",
-    NewVersion = "2.1.0.0")]
+    OldVersionUpperBound = "2.1.1.0",
+    NewVersion = "2.1.1.0")]
 
 [assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.AspNetCore.Razor.Language",
     GenerateCodeBase = true,
     PublicKeyToken = "adb9793829ddae60",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "2.1.0.0",
-    NewVersion = "2.1.0.0")]
+    OldVersionUpperBound = "2.1.1.0",
+    NewVersion = "2.1.1.0")]
 
 [assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.CodeAnalysis.Razor",
     GenerateCodeBase = true,
     PublicKeyToken = "adb9793829ddae60",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "2.1.0.0",
-    NewVersion = "2.1.0.0")]
+    OldVersionUpperBound = "2.1.1.0",
+    NewVersion = "2.1.1.0")]
 
 [assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.CodeAnalysis.Razor.Workspaces",
     GenerateCodeBase = true,
     PublicKeyToken = "adb9793829ddae60",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "2.1.0.0",
-    NewVersion = "2.1.0.0")]
+    OldVersionUpperBound = "2.1.1.0",
+    NewVersion = "2.1.1.0")]
 
 [assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.CodeAnalysis.Remote.Razor",
     GenerateCodeBase = true,
     PublicKeyToken = "adb9793829ddae60",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "2.1.0.0",
-    NewVersion = "2.1.0.0")]
+    OldVersionUpperBound = "2.1.1.0",
+    NewVersion = "2.1.1.0")]
 
 [assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.VisualStudio.Editor.Razor",
     GenerateCodeBase = true,
     PublicKeyToken = "adb9793829ddae60",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "2.1.0.0",
-    NewVersion = "2.1.0.0")]
+    OldVersionUpperBound = "2.1.1.0",
+    NewVersion = "2.1.1.0")]
 
 [assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.VisualStudio.LanguageServices.Razor",
     GenerateCodeBase = true,
     PublicKeyToken = "adb9793829ddae60",
     OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "2.1.0.0",
-    NewVersion = "2.1.0.0")]
+    OldVersionUpperBound = "2.1.1.0",
+    NewVersion = "2.1.1.0")]
