60f63cf9fb7e64b20899d22cd7e86d73a29a5133
Kiran Challa <kichalla@microsoft.com>
2018-05-30 17:01:28 -0700

    Added a sample app for benchmarks

---
+++ benchmarkapps/Benchmarks/Program.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            GetWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder GetWebHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .Build();
+
+            // Consoler logger has a major impact on perf results, so do not use
+            // default builder.
+
+            return new WebHostBuilder()
+                .UseConfiguration(config)
+                .UseKestrel()
+                .UseStartup<Startup>();
+        }
+    }
+}
