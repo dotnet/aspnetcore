e1b08b273911b439cf53d467f97b19c8e2e1270b
Simon Grimshaw <simongrimshaw@gmail.com>
2018-06-04 21:51:57 +0100

    Removing dependency on redux-mock-store.

--- src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/ClientApp/src/App.test.js
+++ src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/ClientApp/src/App.test.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
-import configureStore from 'redux-mock-store';
 import App from './App';
 
 it('renders without crashing', () => {
-  const mockStore = configureStore([])({});
+  const storeFake = (state) => ({
+    default: () => { },
+    subscribe: () => { },
+    dispatch: () => { },
+    getState: () => ({ ...state })
+  });
+  const store = storeFake({});
 
   const div = document.createElement('div');
   ReactDOM.render(
-    <Provider store={mockStore}>
+    <Provider store={store}>
       <MemoryRouter>
         <App />
       </MemoryRouter>
