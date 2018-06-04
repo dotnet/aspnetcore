ef071f605f9a73c95ad484e5bcf048b81e6315d4
Justin Kotalik <jukotali@microsoft.com>
2018-06-04 11:38:30 -0700

    Make gtest a submodule (#885)

--- build/buildpipeline/windows.groovy
+++ build/buildpipeline/windows.groovy
@@ -2,13 +2,14 @@
 
 // 'node' indicates to Jenkins that the enclosed block runs on a node that matches
 // the label 'windows-with-vs'
-simpleNode('Windows.10.Enterprise.RS3.ASPNET') {
+simpleNode('Windows.10.Amd64.EnterpriseRS3.ASPNET.Open') {
     stage ('Checking out source') {
         checkout scm
+        bat 'git submodule update --init --recursive'
     }
     stage ('Build') {
         def logFolder = getLogFolder()
         def environment = "\$env:ASPNETCORE_TEST_LOG_DIR='${WORKSPACE}\\${logFolder}'"
-        bat "powershell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command \"&.\\tools\\update_schema.ps1;${environment};&.\\run.ps1 -CI default-build /p:Configuration=${params.Configuration}\""
+        bat "${environment}&.\\run.cmd -CI default-build"
     }
 }
