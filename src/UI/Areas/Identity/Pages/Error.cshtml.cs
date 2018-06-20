6cdd622f23e9d0230682d319282a302bab60eb88
Pranav K <prkrishn@hotmail.com>
2018-06-20 14:08:58 -0700

    Apply attribute to page model rather than handler

--- src/UI/Areas/Identity/Pages/Error.cshtml.cs
+++ src/UI/Areas/Identity/Pages/Error.cshtml.cs
@@ -9,13 +9,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace Microsoft.AspNetCore.Identity.UI.Pages
 {
     [AllowAnonymous]
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
