fc17ee0749959fb005ab47560646d69d85fae1bb
N. Taylor Mullen <nimullen@microsoft.com>
2018-06-20 15:14:11 -0700

    Update Razor SDK packing logic to not include Pack at properties layer.
    - Added test to verify that no-build scenarios work as expected.
    
    #2378

--- test/Microsoft.AspNetCore.Razor.Design.Test/IntegrationTests/PackIntegrationTest.cs
+++ test/Microsoft.AspNetCore.Razor.Design.Test/IntegrationTests/PackIntegrationTest.cs
@@ -17,6 +17,46 @@ namespace Microsoft.AspNetCore.Razor.Design.IntegrationTests
 
         [Fact]
         [InitializeTestProject("ClassLibrary")]
+        public async Task Pack__NoBuild_Works_IncludesRazorAssembly()
+        {
+            var result = await DotnetMSBuild("Build");
+            Assert.BuildPassed(result);
+
+            result = await DotnetMSBuild("Pack", "/p:NoBuild=true");
+            Assert.BuildPassed(result);
+
+            Assert.FileExists(result, OutputPath, "ClassLibrary.dll");
+            Assert.FileExists(result, OutputPath, "ClassLibrary.Views.dll");
+
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                // Travis on OSX produces different full paths in C# and MSBuild
+                Assert.NuspecContains(
+                    result,
+                    Path.Combine("obj", Configuration, "ClassLibrary.1.0.0.nuspec"),
+                    $"<file src=\"{Path.Combine(Project.DirectoryPath, "bin", Configuration, "netstandard2.0", "ClassLibrary.Views.dll")}\" " +
+                    $"target=\"{Path.Combine("lib", "netstandard2.0", "ClassLibrary.Views.dll")}\" />");
+
+                Assert.NuspecDoesNotContain(
+                    result,
+                    Path.Combine("obj", Configuration, "ClassLibrary.1.0.0.nuspec"),
+                    $"<file src=\"{Path.Combine(Project.DirectoryPath, "bin", Configuration, "netstandard2.0", "ClassLibrary.Views.pdb")}\" " +
+                    $"target=\"{Path.Combine("lib", "netstandard2.0", "ClassLibrary.Views.pdb")}\" />");
+            }
+
+            Assert.NuspecDoesNotContain(
+                result,
+                Path.Combine("obj", Configuration, "ClassLibrary.1.0.0.nuspec"),
+                @"<files include=""any/netstandard2.0/Views/Shared/_Layout.cshtml"" buildAction=""Content"" />");
+
+            Assert.NupkgContains(
+                result,
+                Path.Combine("bin", Configuration, "ClassLibrary.1.0.0.nupkg"),
+                Path.Combine("lib", "netstandard2.0", "ClassLibrary.Views.dll"));
+        }
+
+        [Fact]
+        [InitializeTestProject("ClassLibrary")]
         public async Task Pack_Works_IncludesRazorAssembly()
         {
             var result = await DotnetMSBuild("Pack");
