ea96c50dcea2b58bf559bf16198a7196f27638fd
Ryan Nowak <nowakra@gmail.com>
2018-06-27 15:38:34 -0700

    Convert WhitespaceRewriterTest

--- test/Microsoft.AspNetCore.Razor.Language.Test/Legacy/WhiteSpaceRewriterTest.cs
+++ test/Microsoft.AspNetCore.Razor.Language.Test/Legacy/WhiteSpaceRewriterTest.cs
@@ -1,40 +1,34 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using Xunit;
 
 namespace Microsoft.AspNetCore.Razor.Language.Legacy
 {
-    public class WhiteSpaceRewriterTest
+    public class WhiteSpaceRewriterTest : CsHtmlMarkupParserTestBase
     {
+        public WhiteSpaceRewriterTest()
+        {
+            UseBaselineTests = true;
+        }
+
         [Fact]
         public void Rewrite_Moves_Whitespace_Preceeding_ExpressionBlock_To_Parent_Block()
         {
             // Arrange
-            var factory = new SpanFactory();
-            var start = new MarkupBlock(
-                factory.Markup("test"),
-                new ExpressionBlock(
-                    factory.Code("    ").AsExpression(),
-                    factory.CodeTransition(SyntaxConstants.TransitionString),
-                    factory.Code("foo").AsExpression()),
-                factory.Markup("test"));
+            var parsed = ParseDocument(
+                RazorLanguageVersion.Latest,
+                "test    @foo test",
+                Array.Empty<DirectiveDescriptor>());
+
             var rewriter = new WhiteSpaceRewriter();
 
             // Act
-            var rewritten = rewriter.Rewrite(start);
-            factory.Reset();
+            var rewritten = rewriter.Rewrite(parsed.Root);
 
             // Assert
-            ParserTestBase.EvaluateParseTree(
-                rewritten,
-                new MarkupBlock(
-                    factory.Markup("test"),
-                    factory.Markup("    "),
-                    new ExpressionBlock(
-                        factory.CodeTransition(SyntaxConstants.TransitionString),
-                        factory.Code("foo").AsExpression()),
-                    factory.Markup("test")));
+            BaselineTest(parsed);
         }
     }
 }
