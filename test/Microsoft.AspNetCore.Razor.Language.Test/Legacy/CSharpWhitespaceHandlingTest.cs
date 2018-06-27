67b5588185b96b81c4ec457085dc6b14ea592d2b
N. Taylor Mullen <nimullen@microsoft.com>
2018-06-27 14:42:00 -0700

    Convert CSharpWhitespaceHandlingTest to use baselines.
    #2263

--- test/Microsoft.AspNetCore.Razor.Language.Test/Legacy/CSharpWhitespaceHandlingTest.cs
+++ test/Microsoft.AspNetCore.Razor.Language.Test/Legacy/CSharpWhitespaceHandlingTest.cs
@@ -8,27 +8,15 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
 {
     public class CSharpWhitespaceHandlingTest : CsHtmlMarkupParserTestBase
     {
+        public CSharpWhitespaceHandlingTest()
+        {
+            UseBaselineTests = true;
+        }
+
         [Fact]
         public void StatementBlockDoesNotAcceptTrailingNewlineIfNewlinesAreSignificantToAncestor()
         {
-            ParseBlockTest("@: @if (true) { }" + Environment.NewLine
-                         + "}",
-                           new MarkupBlock(
-                               Factory.MarkupTransition()
-                                   .Accepts(AcceptedCharactersInternal.None),
-                               Factory.MetaMarkup(":", HtmlSymbolType.Colon),
-                               Factory.Markup(" ")
-                                   .With(new SpanEditHandler(
-                                       CSharpLanguageCharacteristics.Instance.TokenizeString,
-                                       AcceptedCharactersInternal.Any)),
-                               new StatementBlock(
-                                   Factory.CodeTransition()
-                                       .Accepts(AcceptedCharactersInternal.None),
-                                   Factory.Code("if (true) { }")
-                                       .AsStatement()
-                                   ),
-                               Factory.Markup(Environment.NewLine)
-                                   .Accepts(AcceptedCharactersInternal.None)));
+            ParseBlockTest("@: @if (true) { }" + Environment.NewLine + "}");
         }
     }
 }
