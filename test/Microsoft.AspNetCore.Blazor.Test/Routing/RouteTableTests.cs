844e38e641c50e5abaa6467fcad0c976e6f23d9c
Ryan Nowak <nowakra@gmail.com>
2018-06-29 13:41:31 -0700

    Update versions to 2.1
    Updates ASP.NET package versions to 2.1
    
    Tweaks TFMs
    - libraries = netstandard2.0
    - exes/apps/tools = netcoreapp2.1
    - unit tests = netcoreapp2.0
    - e2e tests = netcoreapp2.1 (dependency on apps)
    
    The exception to this is the Browser.JS project which depends on our
    tool for its build. however this project just builds the js code so its
    TFM doesn't really matter.

--- test/Microsoft.AspNetCore.Blazor.Test/Routing/RouteTableTests.cs
+++ test/Microsoft.AspNetCore.Blazor.Test/Routing/RouteTableTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -305,10 +305,21 @@ namespace Microsoft.AspNetCore.Blazor.Test.Routing
                 return this;
             }
 
-            public RouteTable Build() => new RouteTable(_routeTemplates
-                .Select(rt => new RouteEntry(TemplateParser.ParseTemplate(rt.Item1), rt.Item2))
-                .OrderBy(id => id, RouteTable.RoutePrecedence)
-                .ToArray());
+            public RouteTable Build()
+            {
+                try
+                {
+                    return new RouteTable(_routeTemplates
+                        .Select(rt => new RouteEntry(TemplateParser.ParseTemplate(rt.Item1), rt.Item2))
+                        .OrderBy(id => id, RouteTable.RoutePrecedence)
+                        .ToArray());
+                }
+                catch (InvalidOperationException ex) when (ex.InnerException is InvalidOperationException)
+                {
+                    // ToArray() will wrap our exception in its own.
+                    throw ex.InnerException;
+                }
+            }
         }
     }
 }
