b9d956df59b645746faa5b7b967493c96d7f1b0b
John Luo <johluo@microsoft.com>
2018-06-28 20:28:36 -0700

    Ensure no trailing slashes for LZMA generation

--- build/tasks/CreateLzma.cs
+++ build/tasks/CreateLzma.cs
@@ -25,8 +25,9 @@ namespace RepoTasks
                 {
                     if (Directory.Exists(source))
                     {
-                        Log.LogMessage(MessageImportance.High, $"Adding directory: {source}");
-                        archive.AddDirectory(source, progress);
+                        var trimmedSource = source.TrimEnd(new char[] { '\\', '/' });
+                        Log.LogMessage(MessageImportance.High, $"Adding directory: {trimmedSource}");
+                        archive.AddDirectory(trimmedSource, progress);
                     }
                     else
                     {
