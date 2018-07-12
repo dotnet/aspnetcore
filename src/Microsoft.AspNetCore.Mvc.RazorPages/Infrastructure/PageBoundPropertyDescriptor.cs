ggeurts/Mvc

Use ModelMetadata from actual types for validation
Fixes https://github.com/aspnet/Mvc/issues/7952

src/Microsoft.AspNetCore.Mvc.RazorPages/Infrastructure/PageBoundPropertyDescriptor.cs
@@ -3,11 +3,14 @@
 
 using System.Reflection;
 using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Internal;
 
 namespace Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure
 {
-    public class PageBoundPropertyDescriptor : ParameterDescriptor
+    public class PageBoundPropertyDescriptor : ParameterDescriptor, IPropertyInfoParameterDescriptor
     {
         public PropertyInfo Property { get; set; }
+
+        PropertyInfo IPropertyInfoParameterDescriptor.PropertyInfo => Property;
     }
 }

test/WebSites/BasicWebSite/Controllers/PolymorphicBindingController.cs
@@ -1,0 +1,32 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using BasicWebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicWebSite.Controllers
+{
+    public class PolymorphicBindingController : ControllerBase
+    {
+        public IActionResult ModelBound([ModelBinder(typeof(PolymorphicBinder))] Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult InputFormatted([FromBody] IPerson person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(person);
+        }
+    }
+}

test/WebSites/BasicWebSite/PolymorphicBinder.cs
@@ -1,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using BasicWebSite.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace BasicWebSite.Controllers
+{
+    public class PolymorphicBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var model = new DerivedPerson
+            {
+                DerivedProperty = bindingContext.ValueProvider.GetValue(nameof(DerivedPerson.DerivedProperty)).FirstValue,
+            };
+
+            bindingContext.Result = ModelBindingResult.Success(model);
+
+            return Task.CompletedTask;
+        }
+    }
+}

dotnet/aspnetcore

src/Microsoft.AspNetCore.Mvc.RazorPages/Infrastructure/PageBoundPropertyDescriptor.cs
@@ -3,11 +3,17 @@
 
 using System.Reflection;
 using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure
 {
-    public class PageBoundPropertyDescriptor : ParameterDescriptor
+    public class PageBoundPropertyDescriptor : ParameterDescriptor, IPropertyInfoParameterDescriptor
     {
+        /// <summary>
+        /// Gets or sets the <see cref="System.Reflection.PropertyInfo"/> for this property.
+        /// </summary>
         public PropertyInfo Property { get; set; }
+
+        PropertyInfo IPropertyInfoParameterDescriptor.PropertyInfo => Property;
     }
 }

test/WebSites/FormatterWebSite/Controllers/PolymorphicBindingController.cs
@@ -1,0 +1,32 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using FormatterWebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormatterWebSite.Controllers
+{
+    public class PolymorphicBindingController : ControllerBase
+    {
+        public IActionResult ModelBound([ModelBinder(typeof(PolymorphicBinder))] BaseModel person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult InputFormatted([FromBody] IModel person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(person);
+        }
+    }
+}

test/WebSites/FormatterWebSite/PolymorphicBinder.cs
@@ -1,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using FormatterWebSite.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace FormatterWebSite.Controllers
+{
+    public class PolymorphicBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var model = new DerivedModel
+            {
+                DerivedProperty = bindingContext.ValueProvider.GetValue(nameof(DerivedModel.DerivedProperty)).FirstValue,
+            };
+
+            bindingContext.Result = ModelBindingResult.Success(model);
+
+            return Task.CompletedTask;
+        }
+    }
+}