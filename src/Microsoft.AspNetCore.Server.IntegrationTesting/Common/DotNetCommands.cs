ec0b7ab2cd6c351199ba7956fbc3a93d9f70923f
Pavel Krymets <pavel@krymets.com>
2018-07-17 10:16:50 -0700

    Remove IISExpress deployer (#1493)

--- src/Microsoft.AspNetCore.Server.IntegrationTesting/Common/DotNetCommands.cs
+++ src/Microsoft.AspNetCore.Server.IntegrationTesting/Common/DotNetCommands.cs
@@ -7,14 +7,14 @@ using System.Runtime.InteropServices;
 
 namespace Microsoft.AspNetCore.Server.IntegrationTesting
 {
-    internal static class DotNetCommands
+    public static class DotNetCommands
     {
         private const string _dotnetFolderName = ".dotnet";
 
         internal static string DotNetHome { get; } = GetDotNetHome();
 
         // Compare to https://github.com/aspnet/BuildTools/blob/314c98e4533217a841ff9767bb38e144eb6c93e4/tools/KoreBuild.Console/Commands/CommandContext.cs#L76
-        private static string GetDotNetHome()
+        public static string GetDotNetHome()
         {
             var dotnetHome = Environment.GetEnvironmentVariable("DOTNET_HOME");
             var userProfile = Environment.GetEnvironmentVariable("USERPROFILE");
@@ -37,7 +37,7 @@ namespace Microsoft.AspNetCore.Server.IntegrationTesting
             return result;
         }
 
-        internal static string GetDotNetInstallDir(RuntimeArchitecture arch)
+        public static string GetDotNetInstallDir(RuntimeArchitecture arch)
         {
             var dotnetDir = DotNetHome;
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
@@ -48,7 +48,7 @@ namespace Microsoft.AspNetCore.Server.IntegrationTesting
             return dotnetDir;
         }
 
-        internal static string GetDotNetExecutable(RuntimeArchitecture arch)
+        public static string GetDotNetExecutable(RuntimeArchitecture arch)
         {
             var dotnetDir = GetDotNetInstallDir(arch);
 
