03a7ba4995f1e2d2c6a1b54ee60eafc661f302c4
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-08-01 13:21:26 -0700

    remove Jenkins configs already covered by VSTS (#2706)

--- build/buildpipeline/pipeline.groovy
+++ build/buildpipeline/pipeline.groovy
@@ -2,8 +2,6 @@ import org.dotnet.ci.pipelines.Pipeline
 
 def windowsPipeline = Pipeline.createPipeline(this, 'build/buildpipeline/windows.groovy')
 def windowsESPipeline = Pipeline.createPipeline(this, 'build/buildpipeline/windows-es.groovy')
-def linuxPipeline = Pipeline.createPipeline(this, 'build/buildpipeline/linux.groovy')
-def osxPipeline = Pipeline.createPipeline(this, 'build/buildpipeline/osx.groovy')
 String configuration = 'Release'
 def parameters = [
     'Configuration': configuration
@@ -14,9 +12,3 @@ windowsPipeline.triggerPipelineOnGithubPush(parameters)
 
 windowsESPipeline.triggerPipelineOnEveryGithubPR("Windows ${configuration} Spanish Language x64 Build", parameters)
 windowsESPipeline.triggerPipelineOnGithubPush(parameters)
-
-linuxPipeline.triggerPipelineOnEveryGithubPR("Ubuntu 16.04 ${configuration} Build", parameters)
-linuxPipeline.triggerPipelineOnGithubPush(parameters)
-
-osxPipeline.triggerPipelineOnEveryGithubPR("OSX 10.12 ${configuration} Build", parameters)
-osxPipeline.triggerPipelineOnGithubPush(parameters)
