ce8fc29728394ddf104bdfc1bf164a7895e5f7ae
Pranav K <prkrishn@hotmail.com>
2018-08-02 15:36:27 -0700

    ConsumesAttribute accepts requests without content type Fixes #8174

--- test/Microsoft.AspNetCore.Mvc.Core.Test/ConsumesAttributeTests.cs
+++ test/Microsoft.AspNetCore.Mvc.Core.Test/ConsumesAttributeTests.cs
@@ -326,7 +326,7 @@ namespace Microsoft.AspNetCore.Mvc
         [Theory]
         [InlineData("")]
         [InlineData(null)]
-        public void OnResourceExecuting_NullOrEmptyRequestContentType_IsNoOp(string contentType)
+        public void OnResourceExecuting_NullOrEmptyRequestContentType_SetsUnsupportedMediaTypeResult(string contentType)
         {
             // Arrange
             var httpContext = new DefaultHttpContext();
@@ -349,7 +349,8 @@ namespace Microsoft.AspNetCore.Mvc
             consumesFilter.OnResourceExecuting(resourceExecutingContext);
 
             // Assert
-            Assert.Null(resourceExecutingContext.Result);
+            Assert.NotNull(resourceExecutingContext.Result);
+            Assert.IsType<UnsupportedMediaTypeResult>(resourceExecutingContext.Result);
         }
 
         [Theory]
