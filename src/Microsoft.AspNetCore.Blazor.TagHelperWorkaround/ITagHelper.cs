3370113a0530d22a6d047128af1b2a7e5ca772f2
Ryan Nowak <nowakra@gmail.com>
2018-08-21 14:32:56 -0700

    Move ITagHelper to another assembly

--- src/Microsoft.AspNetCore.Blazor.TagHelperWorkaround/ITagHelper.cs
+++ src/Microsoft.AspNetCore.Blazor.TagHelperWorkaround/ITagHelper.cs
@@ -10,6 +10,10 @@ namespace Microsoft.AspNetCore.Razor.TagHelpers
     // This is an unfortunate workaround due to https://github.com/aspnet/Razor/issues/2482
     // The Razor tooling looks for a type with exactly this name and will prevent tag helper
     // discovery if it is not found.
+    //
+    // This has to be its own assembly because we need to reference it in Blazor component libraries
+    // in order for component discovery to work, but if we allow it as a reference for server-side
+    // projects it will break MVC's features.
     internal class ITagHelper
     {
     }
