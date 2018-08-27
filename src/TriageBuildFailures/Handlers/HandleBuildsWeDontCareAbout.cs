74cccfebe797d279d2aa7461cd8236f08432f8b4
Ryan Brandenburg <rybrande@microsoft.com>
2018-08-27 11:27:19 -0700

    Add ignored build

--- src/TriageBuildFailures/Handlers/HandleBuildsWeDontCareAbout.cs
+++ src/TriageBuildFailures/Handlers/HandleBuildsWeDontCareAbout.cs
@@ -16,7 +16,7 @@ namespace TriageBuildFailures.Handlers
     /// </remarks>
     public class HandleLowValueBuilds : HandleFailureBase
     {
-        private IEnumerable<string> LowValueBuilds = new string[] { "Lite_Infrastructure_AspNetCoreModuleSetup", "Benchmarks", "Setup_Ancm_IISIntegration", "Setup_Ancm_SignBinaries" };
+        private IEnumerable<string> LowValueBuilds = new string[] { "Lite_Infrastructure_AspNetCoreModuleSetup", "Releases_22xPublic_PbTestWindowsServer2012", "Benchmarks", "Setup_Ancm_IISIntegration", "Setup_Ancm_SignBinaries" };
 
         public override bool CanHandleFailure(TeamCityBuild build)
         {
