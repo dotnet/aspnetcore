ilmax/Mvc

Use ClientErrorData to configure ClientErrorResultFilter
Fixes #8289

src/Microsoft.AspNetCore.Mvc.Core/ClientErrorData.cs
@@ -1,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.AspNetCore.Mvc
+{
+    /// <summary>
+    /// Information for producing client errors. This type is used to configure client errors
+    /// produced by consumers of <see cref="ApiBehaviorOptions.ClientErrorMapping"/>.
+    /// </summary>
+    public class ClientErrorData
+    {
+        /// <summary>
+        /// Gets or sets a link (URI) that describes the client error.
+        /// </summary>
+        /// <remarks>
+        /// By default, this maps to <see cref="ProblemDetails.Type"/>.
+        /// </remarks>
+        public string Link { get; set; }
+
+        /// <summary>
+        /// Gets or sets the summary of the client error.
+        /// </summary>
+        /// <remarks>
+        /// By default, this maps to <see cref="ProblemDetails.Title"/> and should not change
+        /// between multiple occurences of the same error.
+        /// </remarks>
+        public string Summary { get; set; }
+    }
+}

src/Microsoft.AspNetCore.Mvc.Core/Infrastructure/ProblemDetailsClientErrorFactory.cs
@@ -1,0 +1,44 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.AspNetCore.Mvc.Infrastructure
+{
+    internal class ProblemDetailsClientErrorFactory : IClientErrorFactory
+    {
+        private readonly ApiBehaviorOptions _options;
+
+        public ProblemDetailsClientErrorFactory(IOptions<ApiBehaviorOptions> options)
+        {
+            _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        public IActionResult GetClientError(ActionContext actionContext, IClientErrorActionResult clientError)
+        {
+            var problemDetails = new ProblemDetails
+            {
+                Status = clientError.StatusCode,
+                Type = "about:blank",
+            };
+
+            if (clientError.StatusCode is int statusCode &&
+                _options.ClientErrorMapping.TryGetValue(statusCode, out var errorData))
+            {
+                problemDetails.Title = errorData.Summary;
+                problemDetails.Type = errorData.Link;
+            }
+
+            return new ObjectResult(problemDetails)
+            {
+                StatusCode = problemDetails.Status,
+                ContentTypes =
+                {
+                    "application/problem+json",
+                    "application/problem+xml",
+                },
+            };
+        }
+    }
+}

test/Microsoft.AspNetCore.Mvc.Core.Test/Infrastructure/ProblemDetalsClientErrorFactoryTest.cs
@@ -1,0 +1,65 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.Options;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.Infrastructure
+{
+    public class ProblemDetalsClientErrorFactoryTest
+    {
+        [Fact]
+        public void GetClientError_ReturnsProblemDetails_IfNoMappingWasFound()
+        {
+            // Arrange
+            var clientError = new UnsupportedMediaTypeResult();
+            var factory = new ProblemDetailsClientErrorFactory(Options.Create(new ApiBehaviorOptions
+            {
+                ClientErrorMapping =
+                {
+                    [405] = new ClientErrorData { Link = "Some link", Summary = "Summary" },
+                },
+            }));
+
+            // Act
+            var result = factory.GetClientError(new ActionContext(), clientError);
+
+            // Assert
+            var objectResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal(new[] { "application/problem+json", "application/problem+xml" }, objectResult.ContentTypes);
+            var problemDetails = Assert.IsType<ProblemDetails>(objectResult.Value);
+            Assert.Equal(415, problemDetails.Status);
+            Assert.Equal("about:blank", problemDetails.Type);
+            Assert.Null(problemDetails.Title);
+            Assert.Null(problemDetails.Detail);
+            Assert.Null(problemDetails.Instance);
+        }
+
+        [Fact]
+        public void GetClientError_ReturnsProblemDetails()
+        {
+            // Arrange
+            var clientError = new UnsupportedMediaTypeResult();
+            var factory = new ProblemDetailsClientErrorFactory(Options.Create(new ApiBehaviorOptions
+            {
+                ClientErrorMapping =
+                {
+                    [415] = new ClientErrorData { Link = "Some link", Summary = "Summary" },
+                },
+            }));
+
+            // Act
+            var result = factory.GetClientError(new ActionContext(), clientError);
+
+            // Assert
+            var objectResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal(new[] { "application/problem+json", "application/problem+xml" }, objectResult.ContentTypes);
+            var problemDetails = Assert.IsType<ProblemDetails>(objectResult.Value);
+            Assert.Equal(415, problemDetails.Status);
+            Assert.Equal("Some link", problemDetails.Type);
+            Assert.Equal("Summary", problemDetails.Title);
+            Assert.Null(problemDetails.Detail);
+            Assert.Null(problemDetails.Instance);
+        }
+    }
+}

dotnet/aspnetcore

src/Microsoft.AspNetCore.Mvc.Core/ClientErrorData.cs
@@ -1,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.AspNetCore.Mvc
+{
+    /// <summary>
+    /// Information for producing client errors. This type is used to configure client errors
+    /// produced by consumers of <see cref="ApiBehaviorOptions.ClientErrorMapping"/>.
+    /// </summary>
+    public class ClientErrorData
+    {
+        /// <summary>
+        /// Gets or sets a link (URI) that describes the client error.
+        /// </summary>
+        /// <remarks>
+        /// By default, this maps to <see cref="ProblemDetails.Type"/>.
+        /// </remarks>
+        public string Link { get; set; }
+
+        /// <summary>
+        /// Gets or sets the summary of the client error.
+        /// </summary>
+        /// <remarks>
+        /// By default, this maps to <see cref="ProblemDetails.Title"/> and should not change
+        /// between multiple occurences of the same error.
+        /// </remarks>
+        public string Title { get; set; }
+    }
+}

src/Microsoft.AspNetCore.Mvc.Core/Infrastructure/ProblemDetailsClientErrorFactory.cs
@@ -1,0 +1,44 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.AspNetCore.Mvc.Infrastructure
+{
+    internal class ProblemDetailsClientErrorFactory : IClientErrorFactory
+    {
+        private readonly ApiBehaviorOptions _options;
+
+        public ProblemDetailsClientErrorFactory(IOptions<ApiBehaviorOptions> options)
+        {
+            _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        public IActionResult GetClientError(ActionContext actionContext, IClientErrorActionResult clientError)
+        {
+            var problemDetails = new ProblemDetails
+            {
+                Status = clientError.StatusCode,
+                Type = "about:blank",
+            };
+
+            if (clientError.StatusCode is int statusCode &&
+                _options.ClientErrorMapping.TryGetValue(statusCode, out var errorData))
+            {
+                problemDetails.Title = errorData.Title;
+                problemDetails.Type = errorData.Link;
+            }
+
+            return new ObjectResult(problemDetails)
+            {
+                StatusCode = problemDetails.Status,
+                ContentTypes =
+                {
+                    "application/problem+json",
+                    "application/problem+xml",
+                },
+            };
+        }
+    }
+}

test/Microsoft.AspNetCore.Mvc.Core.Test/Infrastructure/ProblemDetalsClientErrorFactoryTest.cs
@@ -1,0 +1,65 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.Options;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.Infrastructure
+{
+    public class ProblemDetalsClientErrorFactoryTest
+    {
+        [Fact]
+        public void GetClientError_ReturnsProblemDetails_IfNoMappingWasFound()
+        {
+            // Arrange
+            var clientError = new UnsupportedMediaTypeResult();
+            var factory = new ProblemDetailsClientErrorFactory(Options.Create(new ApiBehaviorOptions
+            {
+                ClientErrorMapping =
+                {
+                    [405] = new ClientErrorData { Link = "Some link", Title = "Summary" },
+                },
+            }));
+
+            // Act
+            var result = factory.GetClientError(new ActionContext(), clientError);
+
+            // Assert
+            var objectResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal(new[] { "application/problem+json", "application/problem+xml" }, objectResult.ContentTypes);
+            var problemDetails = Assert.IsType<ProblemDetails>(objectResult.Value);
+            Assert.Equal(415, problemDetails.Status);
+            Assert.Equal("about:blank", problemDetails.Type);
+            Assert.Null(problemDetails.Title);
+            Assert.Null(problemDetails.Detail);
+            Assert.Null(problemDetails.Instance);
+        }
+
+        [Fact]
+        public void GetClientError_ReturnsProblemDetails()
+        {
+            // Arrange
+            var clientError = new UnsupportedMediaTypeResult();
+            var factory = new ProblemDetailsClientErrorFactory(Options.Create(new ApiBehaviorOptions
+            {
+                ClientErrorMapping =
+                {
+                    [415] = new ClientErrorData { Link = "Some link", Title = "Summary" },
+                },
+            }));
+
+            // Act
+            var result = factory.GetClientError(new ActionContext(), clientError);
+
+            // Assert
+            var objectResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal(new[] { "application/problem+json", "application/problem+xml" }, objectResult.ContentTypes);
+            var problemDetails = Assert.IsType<ProblemDetails>(objectResult.Value);
+            Assert.Equal(415, problemDetails.Status);
+            Assert.Equal("Some link", problemDetails.Type);
+            Assert.Equal("Summary", problemDetails.Title);
+            Assert.Null(problemDetails.Detail);
+            Assert.Null(problemDetails.Instance);
+        }
+    }
+}