03ff6ca02284201eaffd5bc24b180af7876c48d1
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-08-30 09:42:01 +0100

    Update jsinterop to version that supports Mono bfc35fc6e65

--- samples/MonoSanityClient/Examples.cs
+++ samples/MonoSanityClient/Examples.cs
@@ -1,7 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Mono.WebAssembly.Interop;
+using WebAssembly.JSInterop;
 using System;
 using System.Runtime.InteropServices;
 using System.Text;
