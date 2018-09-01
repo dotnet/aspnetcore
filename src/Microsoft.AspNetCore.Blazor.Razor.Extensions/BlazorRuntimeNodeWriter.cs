BigHam/Blazor

Add support for Razor templates
Adds support for Razor templates and RenderFragment<T>.

Razor templates are a little-known Razor feature that looks like:
```
@<tag>....<tag>
```

It's so little known that it's not even covered in our docs, but it's
been around for many many years. This features hasn't been implemented
until now for Blazor, and this feature brings it back as a build
building block for templated components (more to come).

In Blazor land a template like:
```
@{ RenderFragment<Person> template = @<div>@item.Name</div>; }
```

complies to code like:
```
RenderFragment<Person> template = (__builder, item) =>
{
    __builder.OpenElement(...);
    ...
    __builder.CloseElement(...);
}
```

Since the declaration always has a generic type parameter inside, it
needs to be in a context where the type is known.. ie: not with `var`.

This PR enables the use of templates as an explicit expression to a
component parameter.
```
<MyComponent Template="@(@<div>...</div>)" />
```

See tests for ways to consume templates.

NOTE: There are the following caveats for templates
- Colorization and completion of templates inside attributes is wonky.
We warned you.
- Templates inside attributes require an explicit expression @(...)
- Templates require a single root element.
- Templates don't work in the `@functions { }` block

These limitations are baked into the core of Razor and will take a while
for us to address (v3.0).

src/Microsoft.AspNetCore.Blazor.Razor.Extensions/BlazorRuntimeNodeWriter.cs
@@ -9,6 +9,7 @@ using System.Text;
 using Microsoft.AspNetCore.Blazor.Shared;
 using Microsoft.AspNetCore.Razor.Language;
 using Microsoft.AspNetCore.Razor.Language.CodeGeneration;
+using Microsoft.AspNetCore.Razor.Language.Extensions;
 using Microsoft.AspNetCore.Razor.Language.Intermediate;
 
 namespace Microsoft.AspNetCore.Blazor.Razor
@@ -114,7 +115,7 @@ namespace Microsoft.AspNetCore.Blazor.Razor
                 else
                 {
                     // There may be something else inside the expression like a Template or another extension node.
-                    throw new NotImplementedException("Unsupported: CSharpExpression with child node that isn't a CSharp node");
+                    context.RenderNode(node.Children[i]);
                 }
             }
 
@@ -202,7 +203,7 @@ namespace Microsoft.AspNetCore.Blazor.Razor
                 context.RenderNode(capture);
             }
 
-            _scopeStack.OpenScope(tagName: node.TagName, isComponent: false);
+            _scopeStack.OpenElementScope(node.TagName);
 
             // Render body of the tag inside the scope
             foreach (var child in node.Body)
@@ -321,7 +322,7 @@ namespace Microsoft.AspNetCore.Blazor.Razor
                 context.RenderNode(attribute);
             }
 
-            _scopeStack.OpenScope(node.TagName, isComponent: true);
+            _scopeStack.OpenComponentScope(node.TagName);
             foreach (var child in node.Body)
             {
                 context.RenderNode(child);
@@ -382,6 +383,29 @@ namespace Microsoft.AspNetCore.Blazor.Razor
                 var content = string.Join(string.Empty, GetHtmlTokens(htmlNode).Select(t => t.Content));
                 context.CodeWriter.WriteStringLiteral(content);
             }
+            else if (node.Children.Count == 1 && node.Children[0] is TemplateIntermediateNode template)
+            {
+                // Templates are represented as lambdas assignable for RenderFragment<T> (for some T).
+                // We don't have a type to write down unless its bound to a stronly typed attribute.
+                // That's OK because the compiler gives an error if we can't type check it.
+
+                // If we have a parameter type, then add a type check.
+                if (node.BoundAttribute != null && !node.BoundAttribute.IsWeaklyTyped())
+                {
+                    context.CodeWriter.Write(BlazorApi.RuntimeHelpers.TypeCheck);
+                    context.CodeWriter.Write("<");
+                    context.CodeWriter.Write(node.BoundAttribute.TypeName);
+                    context.CodeWriter.Write(">");
+                    context.CodeWriter.Write("(");
+                }
+
+                context.RenderNode(template);
+
+                if (node.BoundAttribute != null && !node.BoundAttribute.IsWeaklyTyped())
+                {
+                    context.CodeWriter.Write(")");
+                }
+            }
             else
             {
                 // See comments in BlazorDesignTimeNodeWriter for a description of the cases that are possible.
@@ -436,6 +460,27 @@ namespace Microsoft.AspNetCore.Blazor.Razor
                 // We generally expect all children to be HTML, this is here just in case.
                 return html.FindDescendantNodes<IntermediateToken>().Where(t => t.IsHtml).ToArray();
             }
+        }
+
+        public override void WriteTemplate(CodeRenderingContext context, TemplateIntermediateNode node)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (node == null)
+            {
+                throw new ArgumentNullException(nameof(node));
+            }
+
+            // Since the user doesn't write this name themselves, we have to use something hardcoded
+            // and predicatable.
+            const string VariableName = "item";
+
+            _scopeStack.OpenTemplateScope(context, VariableName);
+            context.RenderChildren(node);
+            _scopeStack.CloseScope(context);
         }
 
         public override void WriteReferenceCapture(CodeRenderingContext context, RefExtensionNode node)

test/Microsoft.AspNetCore.Blazor.Build.Test/TestFiles/DesignTimeCodeGenerationTest/RazorTemplate_InCodeBlock/TestComponent.codegen.cs
@@ -1,0 +1,56 @@
+// <auto-generated/>
+#pragma warning disable 1591
+namespace Test
+{
+    #line hidden
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Blazor;
+    using Microsoft.AspNetCore.Blazor.Components;
+    public class TestComponent : Microsoft.AspNetCore.Blazor.Components.BlazorComponent
+    {
+        #pragma warning disable 219
+        private void __RazorDirectiveTokenHelpers__() {
+        }
+        #pragma warning restore 219
+        #pragma warning disable 0414
+        private static System.Object __o = null;
+        #pragma warning restore 0414
+        #pragma warning disable 1998
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Blazor.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+#line 1 "x:\dir\subdir\Test\TestComponent.cshtml"
+  
+    RenderFragment<Person> p = 
+
+#line default
+#line hidden
+            (builder2, item) => {
+#line 2 "x:\dir\subdir\Test\TestComponent.cshtml"
+                                __o = item.Name;
+
+#line default
+#line hidden
+            }
+#line 2 "x:\dir\subdir\Test\TestComponent.cshtml"
+                                                     ;
+
+#line default
+#line hidden
+        }
+        #pragma warning restore 1998
+#line 4 "x:\dir\subdir\Test\TestComponent.cshtml"
+            
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+#line default
+#line hidden
+    }
+}
+#pragma warning restore 1591

test/Microsoft.AspNetCore.Blazor.Build.Test/TestFiles/DesignTimeCodeGenerationTest/RazorTemplate_InExplicitExpression/TestComponent.codegen.cs
@@ -1,0 +1,52 @@
+// <auto-generated/>
+#pragma warning disable 1591
+namespace Test
+{
+    #line hidden
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Blazor;
+    using Microsoft.AspNetCore.Blazor.Components;
+    public class TestComponent : Microsoft.AspNetCore.Blazor.Components.BlazorComponent
+    {
+        #pragma warning disable 219
+        private void __RazorDirectiveTokenHelpers__() {
+        }
+        #pragma warning restore 219
+        #pragma warning disable 0414
+        private static System.Object __o = null;
+        #pragma warning restore 0414
+        #pragma warning disable 1998
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Blazor.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+#line 1 "x:\dir\subdir\Test\TestComponent.cshtml"
+__o = RenderPerson((builder2, item) => {
+#line 1 "x:\dir\subdir\Test\TestComponent.cshtml"
+                __o = item.Name;
+
+#line default
+#line hidden
+}
+);
+
+#line default
+#line hidden
+        }
+        #pragma warning restore 1998
+#line 2 "x:\dir\subdir\Test\TestComponent.cshtml"
+            
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+    object RenderPerson(RenderFragment<Person> p) => null;
+
+#line default
+#line hidden
+    }
+}
+#pragma warning restore 1591

test/Microsoft.AspNetCore.Blazor.Build.Test/TestFiles/DesignTimeCodeGenerationTest/RazorTemplate_InImplicitExpression/TestComponent.codegen.cs
@@ -1,0 +1,52 @@
+// <auto-generated/>
+#pragma warning disable 1591
+namespace Test
+{
+    #line hidden
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Blazor;
+    using Microsoft.AspNetCore.Blazor.Components;
+    public class TestComponent : Microsoft.AspNetCore.Blazor.Components.BlazorComponent
+    {
+        #pragma warning disable 219
+        private void __RazorDirectiveTokenHelpers__() {
+        }
+        #pragma warning restore 219
+        #pragma warning disable 0414
+        private static System.Object __o = null;
+        #pragma warning restore 0414
+        #pragma warning disable 1998
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Blazor.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+#line 1 "x:\dir\subdir\Test\TestComponent.cshtml"
+__o = RenderPerson((builder2, item) => {
+#line 1 "x:\dir\subdir\Test\TestComponent.cshtml"
+               __o = item.Name;
+
+#line default
+#line hidden
+}
+);
+
+#line default
+#line hidden
+        }
+        #pragma warning restore 1998
+#line 2 "x:\dir\subdir\Test\TestComponent.cshtml"
+            
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+    object RenderPerson(RenderFragment<Person> p) => null;
+
+#line default
+#line hidden
+    }
+}
+#pragma warning restore 1591

dotnet/aspnetcore

src/Microsoft.AspNetCore.Blazor.Razor.Extensions/BlazorRuntimeNodeWriter.cs
@@ -9,6 +9,7 @@ using System.Text;
 using Microsoft.AspNetCore.Blazor.Shared;
 using Microsoft.AspNetCore.Razor.Language;
 using Microsoft.AspNetCore.Razor.Language.CodeGeneration;
+using Microsoft.AspNetCore.Razor.Language.Extensions;
 using Microsoft.AspNetCore.Razor.Language.Intermediate;
 
 namespace Microsoft.AspNetCore.Blazor.Razor
@@ -114,7 +115,7 @@ namespace Microsoft.AspNetCore.Blazor.Razor
                 else
                 {
                     // There may be something else inside the expression like a Template or another extension node.
-                    throw new NotImplementedException("Unsupported: CSharpExpression with child node that isn't a CSharp node");
+                    context.RenderNode(node.Children[i]);
                 }
             }
 
@@ -202,7 +203,7 @@ namespace Microsoft.AspNetCore.Blazor.Razor
                 context.RenderNode(capture);
             }
 
-            _scopeStack.OpenScope(tagName: node.TagName, isComponent: false);
+            _scopeStack.OpenElementScope(node.TagName);
 
             // Render body of the tag inside the scope
             foreach (var child in node.Body)
@@ -321,7 +322,7 @@ namespace Microsoft.AspNetCore.Blazor.Razor
                 context.RenderNode(attribute);
             }
 
-            _scopeStack.OpenScope(node.TagName, isComponent: true);
+            _scopeStack.OpenComponentScope(node.TagName);
             foreach (var child in node.Body)
             {
                 context.RenderNode(child);
@@ -382,6 +383,29 @@ namespace Microsoft.AspNetCore.Blazor.Razor
                 var content = string.Join(string.Empty, GetHtmlTokens(htmlNode).Select(t => t.Content));
                 context.CodeWriter.WriteStringLiteral(content);
             }
+            else if (node.Children.Count == 1 && node.Children[0] is TemplateIntermediateNode template)
+            {
+                // Templates are represented as lambdas assignable for RenderFragment<T> (for some T).
+                // We don't have a type to write down unless its bound to a stronly typed attribute.
+                // That's OK because the compiler gives an error if we can't type check it.
+
+                // If we have a parameter type, then add a type check.
+                if (node.BoundAttribute != null && !node.BoundAttribute.IsWeaklyTyped())
+                {
+                    context.CodeWriter.Write(BlazorApi.RuntimeHelpers.TypeCheck);
+                    context.CodeWriter.Write("<");
+                    context.CodeWriter.Write(node.BoundAttribute.TypeName);
+                    context.CodeWriter.Write(">");
+                    context.CodeWriter.Write("(");
+                }
+
+                context.RenderNode(template);
+
+                if (node.BoundAttribute != null && !node.BoundAttribute.IsWeaklyTyped())
+                {
+                    context.CodeWriter.Write(")");
+                }
+            }
             else
             {
                 // See comments in BlazorDesignTimeNodeWriter for a description of the cases that are possible.
@@ -436,6 +460,27 @@ namespace Microsoft.AspNetCore.Blazor.Razor
                 // We generally expect all children to be HTML, this is here just in case.
                 return html.FindDescendantNodes<IntermediateToken>().Where(t => t.IsHtml).ToArray();
             }
+        }
+
+        public override void WriteTemplate(CodeRenderingContext context, TemplateIntermediateNode node)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (node == null)
+            {
+                throw new ArgumentNullException(nameof(node));
+            }
+
+            // Since the user doesn't write this name themselves, we have to use something hardcoded
+            // and predicatable.
+            const string VariableName = "context";
+
+            _scopeStack.OpenTemplateScope(context, VariableName);
+            context.RenderChildren(node);
+            _scopeStack.CloseScope(context);
         }
 
         public override void WriteReferenceCapture(CodeRenderingContext context, RefExtensionNode node)

test/Microsoft.AspNetCore.Blazor.Build.Test/TestFiles/DesignTimeCodeGenerationTest/RazorTemplate_InCodeBlock/TestComponent.codegen.cs
@@ -1,0 +1,56 @@
+// <auto-generated/>
+#pragma warning disable 1591
+namespace Test
+{
+    #line hidden
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Blazor;
+    using Microsoft.AspNetCore.Blazor.Components;
+    public class TestComponent : Microsoft.AspNetCore.Blazor.Components.BlazorComponent
+    {
+        #pragma warning disable 219
+        private void __RazorDirectiveTokenHelpers__() {
+        }
+        #pragma warning restore 219
+        #pragma warning disable 0414
+        private static System.Object __o = null;
+        #pragma warning restore 0414
+        #pragma warning disable 1998
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Blazor.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+#line 1 "x:\dir\subdir\Test\TestComponent.cshtml"
+  
+    RenderFragment<Person> p = 
+
+#line default
+#line hidden
+            (builder2, context) => {
+#line 2 "x:\dir\subdir\Test\TestComponent.cshtml"
+                                __o = context.Name;
+
+#line default
+#line hidden
+            }
+#line 2 "x:\dir\subdir\Test\TestComponent.cshtml"
+                                                        ;
+
+#line default
+#line hidden
+        }
+        #pragma warning restore 1998
+#line 4 "x:\dir\subdir\Test\TestComponent.cshtml"
+            
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+#line default
+#line hidden
+    }
+}
+#pragma warning restore 1591

test/Microsoft.AspNetCore.Blazor.Build.Test/TestFiles/DesignTimeCodeGenerationTest/RazorTemplate_InExplicitExpression/TestComponent.codegen.cs
@@ -1,0 +1,52 @@
+// <auto-generated/>
+#pragma warning disable 1591
+namespace Test
+{
+    #line hidden
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Blazor;
+    using Microsoft.AspNetCore.Blazor.Components;
+    public class TestComponent : Microsoft.AspNetCore.Blazor.Components.BlazorComponent
+    {
+        #pragma warning disable 219
+        private void __RazorDirectiveTokenHelpers__() {
+        }
+        #pragma warning restore 219
+        #pragma warning disable 0414
+        private static System.Object __o = null;
+        #pragma warning restore 0414
+        #pragma warning disable 1998
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Blazor.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+#line 1 "x:\dir\subdir\Test\TestComponent.cshtml"
+__o = RenderPerson((builder2, context) => {
+#line 1 "x:\dir\subdir\Test\TestComponent.cshtml"
+                __o = context.Name;
+
+#line default
+#line hidden
+}
+);
+
+#line default
+#line hidden
+        }
+        #pragma warning restore 1998
+#line 2 "x:\dir\subdir\Test\TestComponent.cshtml"
+            
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+    object RenderPerson(RenderFragment<Person> p) => null;
+
+#line default
+#line hidden
+    }
+}
+#pragma warning restore 1591

test/Microsoft.AspNetCore.Blazor.Build.Test/TestFiles/DesignTimeCodeGenerationTest/RazorTemplate_InImplicitExpression/TestComponent.codegen.cs
@@ -1,0 +1,52 @@
+// <auto-generated/>
+#pragma warning disable 1591
+namespace Test
+{
+    #line hidden
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Blazor;
+    using Microsoft.AspNetCore.Blazor.Components;
+    public class TestComponent : Microsoft.AspNetCore.Blazor.Components.BlazorComponent
+    {
+        #pragma warning disable 219
+        private void __RazorDirectiveTokenHelpers__() {
+        }
+        #pragma warning restore 219
+        #pragma warning disable 0414
+        private static System.Object __o = null;
+        #pragma warning restore 0414
+        #pragma warning disable 1998
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Blazor.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+#line 1 "x:\dir\subdir\Test\TestComponent.cshtml"
+__o = RenderPerson((builder2, context) => {
+#line 1 "x:\dir\subdir\Test\TestComponent.cshtml"
+               __o = context.Name;
+
+#line default
+#line hidden
+}
+);
+
+#line default
+#line hidden
+        }
+        #pragma warning restore 1998
+#line 2 "x:\dir\subdir\Test\TestComponent.cshtml"
+            
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+    object RenderPerson(RenderFragment<Person> p) => null;
+
+#line default
+#line hidden
+    }
+}
+#pragma warning restore 1591