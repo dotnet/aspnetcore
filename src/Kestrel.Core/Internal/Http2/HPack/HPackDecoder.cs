aspnet/KestrelHttpServer

Add configurability for max header field size in HPACK

src/Kestrel.Core/Internal/Http2/HPack/HPackDecoder.cs
@@ -23,9 +23,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2.HPack
             DynamicTableSizeUpdate
         }
 
-        // TODO: add new configurable limit
-        public const int MaxStringOctets = 4096;
-
         // http://httpwg.org/specs/rfc7541.html#rfc.section.6.1
         //   0   1   2   3   4   5   6   7
         // +---+---+---+---+---+---+---+---+
@@ -83,9 +80,9 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2.HPack
         private readonly int _maxDynamicTableSize;
         private readonly DynamicTable _dynamicTable;
         private readonly IntegerDecoder _integerDecoder = new IntegerDecoder();
-        private readonly byte[] _stringOctets = new byte[MaxStringOctets];
-        private readonly byte[] _headerNameOctets = new byte[MaxStringOctets];
-        private readonly byte[] _headerValueOctets = new byte[MaxStringOctets];
+        private readonly byte[] _stringOctets;
+        private readonly byte[] _headerNameOctets;
+        private readonly byte[] _headerValueOctets;
 
         private State _state = State.Ready;
         private byte[] _headerName;
@@ -97,17 +94,18 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2.HPack
         private bool _huffman;
         private bool _headersObserved;
 
-        public HPackDecoder(int maxDynamicTableSize)
-            : this(maxDynamicTableSize, new DynamicTable(maxDynamicTableSize))
-        {
-            _maxDynamicTableSize = maxDynamicTableSize;
-        }
+        public HPackDecoder(int maxDynamicTableSize, int maxHeaderFieldSize)
+            : this(maxDynamicTableSize, maxHeaderFieldSize, new DynamicTable(maxDynamicTableSize)) { }
 
         // For testing.
-        internal HPackDecoder(int maxDynamicTableSize, DynamicTable dynamicTable)
+        internal HPackDecoder(int maxDynamicTableSize, int maxHeaderFieldSize, DynamicTable dynamicTable)
         {
             _maxDynamicTableSize = maxDynamicTableSize;
             _dynamicTable = dynamicTable;
+
+            _stringOctets = new byte[maxHeaderFieldSize];
+            _headerNameOctets = new byte[maxHeaderFieldSize];
+            _headerValueOctets = new byte[maxHeaderFieldSize];
         }
 
         public void Decode(ReadOnlySequence<byte> data, bool endHeaders, IHttpHeadersHandler handler)

src/Kestrel.Core/Internal/Http2/Http2Connection.cs
@@ -86,13 +86,16 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2
 
         public Http2Connection(Http2ConnectionContext context)
         {
+            var httpLimits = context.ServiceContext.ServerOptions.Limits;
+            var http2Limits = httpLimits.Http2;
+
             _context = context;
             _frameWriter = new Http2FrameWriter(context.Transport.Output, context.ConnectionContext, _outputFlowControl, this, context.ConnectionId, context.ServiceContext.Log);
-            _serverSettings.MaxConcurrentStreams = (uint)context.ServiceContext.ServerOptions.Limits.Http2.MaxStreamsPerConnection;
-            _serverSettings.MaxFrameSize = (uint)context.ServiceContext.ServerOptions.Limits.Http2.MaxFrameSize;
-            _serverSettings.HeaderTableSize = (uint)context.ServiceContext.ServerOptions.Limits.Http2.HeaderTableSize;
-            _hpackDecoder = new HPackDecoder((int)_serverSettings.HeaderTableSize);
-            _serverSettings.MaxHeaderListSize = (uint)context.ServiceContext.ServerOptions.Limits.MaxRequestHeadersTotalSize;
+            _serverSettings.MaxConcurrentStreams = (uint)http2Limits.MaxStreamsPerConnection;
+            _serverSettings.MaxFrameSize = (uint)http2Limits.MaxFrameSize;
+            _serverSettings.HeaderTableSize = (uint)http2Limits.HeaderTableSize;
+            _hpackDecoder = new HPackDecoder(http2Limits.HeaderTableSize, http2Limits.MaxHeaderFieldSize);
+            _serverSettings.MaxHeaderListSize = (uint)httpLimits.MaxRequestHeadersTotalSize;
         }
 
         public string ConnectionId => _context.ConnectionId;

dotnet/aspnetcore

src/Kestrel.Core/Internal/Http2/HPack/HPackDecoder.cs
@@ -23,9 +23,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2.HPack
             DynamicTableSizeUpdate
         }
 
-        // TODO: add new configurable limit
-        public const int MaxStringOctets = 4096;
-
         // http://httpwg.org/specs/rfc7541.html#rfc.section.6.1
         //   0   1   2   3   4   5   6   7
         // +---+---+---+---+---+---+---+---+
@@ -83,9 +80,9 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2.HPack
         private readonly int _maxDynamicTableSize;
         private readonly DynamicTable _dynamicTable;
         private readonly IntegerDecoder _integerDecoder = new IntegerDecoder();
-        private readonly byte[] _stringOctets = new byte[MaxStringOctets];
-        private readonly byte[] _headerNameOctets = new byte[MaxStringOctets];
-        private readonly byte[] _headerValueOctets = new byte[MaxStringOctets];
+        private readonly byte[] _stringOctets;
+        private readonly byte[] _headerNameOctets;
+        private readonly byte[] _headerValueOctets;
 
         private State _state = State.Ready;
         private byte[] _headerName;
@@ -97,17 +94,18 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2.HPack
         private bool _huffman;
         private bool _headersObserved;
 
-        public HPackDecoder(int maxDynamicTableSize)
-            : this(maxDynamicTableSize, new DynamicTable(maxDynamicTableSize))
-        {
-            _maxDynamicTableSize = maxDynamicTableSize;
-        }
+        public HPackDecoder(int maxDynamicTableSize, int maxRequestHeaderFieldSize)
+            : this(maxDynamicTableSize, maxRequestHeaderFieldSize, new DynamicTable(maxDynamicTableSize)) { }
 
         // For testing.
-        internal HPackDecoder(int maxDynamicTableSize, DynamicTable dynamicTable)
+        internal HPackDecoder(int maxDynamicTableSize, int maxRequestHeaderFieldSize, DynamicTable dynamicTable)
         {
             _maxDynamicTableSize = maxDynamicTableSize;
             _dynamicTable = dynamicTable;
+
+            _stringOctets = new byte[maxRequestHeaderFieldSize];
+            _headerNameOctets = new byte[maxRequestHeaderFieldSize];
+            _headerValueOctets = new byte[maxRequestHeaderFieldSize];
         }
 
         public void Decode(ReadOnlySequence<byte> data, bool endHeaders, IHttpHeadersHandler handler)

src/Kestrel.Core/Internal/Http2/Http2Connection.cs
@@ -86,13 +86,16 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2
 
         public Http2Connection(Http2ConnectionContext context)
         {
+            var httpLimits = context.ServiceContext.ServerOptions.Limits;
+            var http2Limits = httpLimits.Http2;
+
             _context = context;
             _frameWriter = new Http2FrameWriter(context.Transport.Output, context.ConnectionContext, _outputFlowControl, this, context.ConnectionId, context.ServiceContext.Log);
-            _serverSettings.MaxConcurrentStreams = (uint)context.ServiceContext.ServerOptions.Limits.Http2.MaxStreamsPerConnection;
-            _serverSettings.MaxFrameSize = (uint)context.ServiceContext.ServerOptions.Limits.Http2.MaxFrameSize;
-            _serverSettings.HeaderTableSize = (uint)context.ServiceContext.ServerOptions.Limits.Http2.HeaderTableSize;
-            _hpackDecoder = new HPackDecoder((int)_serverSettings.HeaderTableSize);
-            _serverSettings.MaxHeaderListSize = (uint)context.ServiceContext.ServerOptions.Limits.MaxRequestHeadersTotalSize;
+            _serverSettings.MaxConcurrentStreams = (uint)http2Limits.MaxStreamsPerConnection;
+            _serverSettings.MaxFrameSize = (uint)http2Limits.MaxFrameSize;
+            _serverSettings.HeaderTableSize = (uint)http2Limits.HeaderTableSize;
+            _hpackDecoder = new HPackDecoder(http2Limits.HeaderTableSize, http2Limits.MaxRequestHeaderFieldSize);
+            _serverSettings.MaxHeaderListSize = (uint)httpLimits.MaxRequestHeadersTotalSize;
         }
 
         public string ConnectionId => _context.ConnectionId;