1f31e0556d7cfb7fc99c6c8650173d0cc4ba0717
Ryan Nowak <nowakra@gmail.com>
2018-10-08 17:58:45 -0700

    Add IHealthCheckPublisher for push-based checks (#498)
    IHealthCheckPublisher allows you to configure and run health checks
    regularly inside an application, and push the notifications elsewhere.
    
    All publishers are part of a single queue with a configurable period and
    timeout.

--- src/Microsoft.Extensions.Diagnostics.HealthChecks/DefaultHealthCheckService.cs
+++ src/Microsoft.Extensions.Diagnostics.HealthChecks/DefaultHealthCheckService.cs
@@ -126,19 +126,19 @@ namespace Microsoft.Extensions.Diagnostics.HealthChecks
             }
         }
 
+        internal static class EventIds
+        {
+            public static readonly EventId HealthCheckProcessingBegin = new EventId(100, "HealthCheckProcessingBegin");
+            public static readonly EventId HealthCheckProcessingEnd = new EventId(101, "HealthCheckProcessingEnd");
+
+            public static readonly EventId HealthCheckBegin = new EventId(102, "HealthCheckBegin");
+            public static readonly EventId HealthCheckEnd = new EventId(103, "HealthCheckEnd");
+            public static readonly EventId HealthCheckError = new EventId(104, "HealthCheckError");
+            public static readonly EventId HealthCheckData = new EventId(105, "HealthCheckData");
+        }
+
         private static class Log
         {
-            public static class EventIds
-            {
-                public static readonly EventId HealthCheckProcessingBegin = new EventId(100, "HealthCheckProcessingBegin");
-                public static readonly EventId HealthCheckProcessingEnd = new EventId(101, "HealthCheckProcessingEnd");
-
-                public static readonly EventId HealthCheckBegin = new EventId(102, "HealthCheckBegin");
-                public static readonly EventId HealthCheckEnd = new EventId(103, "HealthCheckEnd");
-                public static readonly EventId HealthCheckError = new EventId(104, "HealthCheckError");
-                public static readonly EventId HealthCheckData = new EventId(105, "HealthCheckData");
-            }
-
             private static readonly Action<ILogger, Exception> _healthCheckProcessingBegin = LoggerMessage.Define(
                 LogLevel.Debug,
                 EventIds.HealthCheckProcessingBegin,
--- test/Microsoft.Extensions.Diagnostics.HealthChecks.Tests/DependencyInjection/ServiceCollectionExtensionsTest.cs
+++ test/Microsoft.Extensions.Diagnostics.HealthChecks.Tests/DependencyInjection/ServiceCollectionExtensionsTest.cs
@@ -1,7 +1,9 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Linq;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
 using Xunit;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -19,12 +21,20 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddHealthChecks();
 
             // Assert
-            Assert.Collection(services,
+            Assert.Collection(services.OrderBy(s => s.ServiceType.FullName),
                 actual =>
                 {
                     Assert.Equal(ServiceLifetime.Singleton, actual.Lifetime);
                     Assert.Equal(typeof(HealthCheckService), actual.ServiceType);
                     Assert.Equal(typeof(DefaultHealthCheckService), actual.ImplementationType);
+                    Assert.Null(actual.ImplementationInstance);
+                    Assert.Null(actual.ImplementationFactory);
+                },
+                actual =>
+                {
+                    Assert.Equal(ServiceLifetime.Singleton, actual.Lifetime);
+                    Assert.Equal(typeof(IHostedService), actual.ServiceType);
+                    Assert.Equal(typeof(HealthCheckPublisherHostedService), actual.ImplementationType);
                     Assert.Null(actual.ImplementationInstance);
                     Assert.Null(actual.ImplementationFactory);
                 });
