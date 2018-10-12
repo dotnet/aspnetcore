fb3f5d95c2dc5c395efe1248763221ac2b2b3de6
Nate McMaster <nate.mcmaster@microsoft.com>
2018-10-12 07:33:28 -0700

    Backport infrastructure changes and source reorganization
    * Add build definition for Azure DevOps
    * Put code for metapackages in a subfolder
    * Update targets to prepare for submodules merging into this repo
    * Add source code for windows installer
    * Add source code for Debian installers

--- scripts/common.psm1
+++ scripts/common.psm1
@@ -1,4 +1,6 @@
 $ErrorActionPreference = 'Stop'
+# Update the default TLS support to 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 function Assert-Git {
     if (!(Get-Command git -ErrorAction Ignore)) {
@@ -7,19 +9,31 @@ function Assert-Git {
     }
 }
 
-function Invoke-Block([scriptblock]$cmd) {
-    $cmd | Out-String | Write-Verbose
-    & $cmd
+function Invoke-Block([scriptblock]$cmd, [string]$WorkingDir = $null) {
+    if ($WorkingDir) {
+        Push-Location $WorkingDir
+    }
 
-    # Need to check both of these cases for errors as they represent different items
-    # - $?: did the powershell script block throw an error
-    # - $lastexitcode: did a windows command executed by the script block end in error
-    if ((-not $?) -or ($lastexitcode -ne 0)) {
-        if ($error -ne $null)
-        {
-            Write-Warning $error[0]
+    try {
+
+        $cmd | Out-String | Write-Verbose
+        & $cmd
+
+        # Need to check both of these cases for errors as they represent different items
+        # - $?: did the powershell script block throw an error
+        # - $lastexitcode: did a windows command executed by the script block end in error
+        if ((-not $?) -or ($lastexitcode -ne 0)) {
+            if ($error -ne $null)
+            {
+                Write-Warning $error[0]
+            }
+            throw "Command failed to execute: $cmd"
         }
-        throw "Command failed to execute: $cmd"
+    }
+    finally {
+        if ($WorkingDir) {
+            Pop-Location
+        }
     }
 }
 
@@ -229,4 +243,73 @@ function UpdateVersions([hashtable]$variables, [xml]$dependencies, [string]$deps
     }
 
     return $updatedVars
+}
+
+function Get-MSBuildPath {
+    param(
+        [switch]$Prerelease,
+        [string[]]$Requires
+    )
+
+    $vsInstallDir = $null
+    if ($env:VSINSTALLDIR -and (Test-Path $env:VSINSTALLDIR)) {
+        $vsInstallDir = $env:VSINSTALLDIR
+        Write-Verbose "Using VSINSTALLDIR=$vsInstallDir"
+    }
+    else {
+        $vswhere = "${env:ProgramFiles(x86)}/Microsoft Visual Studio/Installer/vswhere.exe"
+        Write-Verbose "Using vswhere.exe from $vswhere"
+
+        if (-not (Test-Path $vswhere)) {
+            Write-Error "Missing prerequisite: could not find vswhere"
+        }
+
+        [string[]] $vswhereArgs = @()
+
+        if ($Prerelease) {
+            $vswhereArgs += '-prerelease'
+        }
+
+        if ($Requires) {
+            foreach ($r in $Requires) {
+                $vswhereArgs += '-requires', $r
+            }
+        }
+
+        $installs = & $vswhere -format json -version '[15.0, 16.0)' -latest -products * @vswhereArgs | ConvertFrom-Json
+        if (!$installs) {
+            Write-Error "Missing prerequisite: could not find any installations of Visual Studio"
+        }
+
+        $vs = $installs | Select-Object -First 1
+        $vsInstallDir = $vs.installationPath
+        Write-Host "Using $($vs.displayName)"
+    }
+
+    $msbuild = Join-Path  $vsInstallDir 'MSBuild/15.0/bin/msbuild.exe'
+    if (!(Test-Path $msbuild)) {
+        Write-Error "Missing prerequisite: could not find msbuild.exe"
+    }
+    return $msbuild
+}
+
+function Get-RemoteFile([string]$RemotePath, [string]$LocalPath) {
+    if ($RemotePath -notlike 'http*') {
+        Copy-Item $RemotePath $LocalPath
+        return
+    }
+
+    $retries = 10
+    while ($retries -gt 0) {
+        $retries -= 1
+        try {
+            Invoke-WebRequest -UseBasicParsing -Uri $RemotePath -OutFile $LocalPath
+            return
+        }
+        catch {
+            Write-Verbose "Request failed. $retries retries remaining"
+        }
+    }
+
+    Write-Error "Download failed: '$RemotePath'."
 }
---
+++ src/Installers/Debian/test/test_assets/test_package_layout/samples/testsample.cs
@@ -0,0 +1,12 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+public class Program {
+
+    public static void Main(){
+        System.Console.WriteLine("Hello World");
+    }
+
+}
