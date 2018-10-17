97acb95715f894de638974d4cc32dcc7f30e1ebf
John Luo <johluo@microsoft.com>
2018-10-17 11:03:36 -0700

    Set default protocol to HTTP/1.x

--- test/Kestrel.Core.Tests/ListenOptionsTests.cs
+++ test/Kestrel.Core.Tests/ListenOptionsTests.cs
@@ -15,7 +15,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Tests
         public void ProtocolsDefault()
         {
             var listenOptions = new ListenOptions(new IPEndPoint(IPAddress.Loopback, 0));
-            Assert.Equal(HttpProtocols.Http1AndHttp2, listenOptions.Protocols);
+            Assert.Equal(HttpProtocols.Http1, listenOptions.Protocols);
         }
 
         [Fact]
