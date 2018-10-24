171c2073d1a824965d59c7e528be5645aba20d18
Justin Kotalik <jukotali@microsoft.com>
2018-10-24 16:20:23 -0700

    update

--- scripts/Run-Starterweb-Locally.ps1
+++ scripts/Run-Starterweb-Locally.ps1
@@ -9,4 +9,4 @@ $ErrorActionPreference = 'Stop'
 
 . $PSScriptRoot\Custom-Hive.ps1
 
-Test-Template "mvc" "mvc -au Individual" "Microsoft.DotNet.Web.ProjectTemplates.3.0.0-alpha1.nupkg" $false
+Test-Template "mvc" "mvc -au Individual" "Microsoft.DotNet.Web.ProjectTemplates.3.0.3.0.0-alpha1.nupkg" $false
