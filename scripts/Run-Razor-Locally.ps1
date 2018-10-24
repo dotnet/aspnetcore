ee78882b27274c4d8b9c04a24ebfa61f5bc01911
Justin Kotalik <jukotali@microsoft.com>
2018-10-24 12:07:51 -0700

    update master

--- scripts/Run-Razor-Locally.ps1
+++ scripts/Run-Razor-Locally.ps1
@@ -6,4 +6,4 @@ param()
 
 . $PSScriptRoot\Custom-Hive.ps1
 
-Test-Template "webapp" "webapp -au Individual" "Microsoft.DotNet.Web.ProjectTemplates.2.2.2.2.0-rtm-t000.nupkg" $false
+Test-Template "webapp" "webapp -au Individual" "Microsoft.DotNet.Web.ProjectTemplates.3.0.0-alpha1.nupkg" $false
