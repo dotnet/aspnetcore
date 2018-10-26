e1b602a7c5331e0c20efe4a087d73eb63441fd9c
Mikael Mengistu <mikaelm12@users.noreply.github.com>
2018-10-26 13:38:35 -0700

    Remove BiFunction Ref (#3213)

--- clients/java/signalr/src/main/java/com/microsoft/signalr/CallbackMap.java
+++ clients/java/signalr/src/main/java/com/microsoft/signalr/CallbackMap.java
@@ -3,29 +3,45 @@
 
 package com.microsoft.signalr;
 
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
 
 class CallbackMap {
-    private final Map<String, List<InvocationHandler>> handlers = new ConcurrentHashMap<>();
+    private final Map<String, List<InvocationHandler>> handlers = new HashMap<>();
+    private final ReentrantLock lock = new ReentrantLock();
 
     public InvocationHandler put(String target, ActionBase action, Class<?>... classes) {
-        InvocationHandler handler = new InvocationHandler(action, classes);
-        handlers.compute(target, (key, value) -> {
-            if (value == null) {
-                value = new ArrayList<>();
+        try {
+            lock.lock();
+            InvocationHandler handler = new InvocationHandler(action, classes);
+            if (!handlers.containsKey(target)) {
+                handlers.put(target, new ArrayList<>());
             }
-            value.add(handler);
-            return value;
-        });
-        return handler;
+            handlers.get(target).add(handler);
+            return handler;
+        } finally {
+            lock.unlock();
+        }
     }
 
     public List<InvocationHandler> get(String key) {
-        return handlers.get(key);
+        try {
+            lock.lock();
+            return handlers.get(key);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void remove(String key) {
-        handlers.remove(key);
+        try {
+            lock.lock();
+            handlers.remove(key);
+        } finally {
+            lock.unlock();
+        }
     }
 }
