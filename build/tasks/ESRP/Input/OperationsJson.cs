f57ef13ca42d6d36ad7a195283cfbf0fbac6a253
Nate McMaster <nate.mcmaster@microsoft.com>
2018-10-27 00:07:45 -0700

    Remove config for all SHA1 and SHA1 dual certificates and only use SHA2

--- build/tasks/ESRP/Input/OperationsJson.cs
+++ build/tasks/ESRP/Input/OperationsJson.cs
@@ -100,68 +100,6 @@ namespace Microsoft.Build.OOB.ESRP
                 }
             };
 
-            // Classic ID: 401
-            var _microsoftAuthentiCodeSha1Sha2 = new Operations[]
-            {
-                new Operations
-                {
-                    KeyCode = ESRP.KeyCode.CP_229803,
-                    OperationCode = OperationCode.SigntoolSign,
-                    Parameters = new JObject
-                    {
-                        { "OpusName", "Microsoft" },
-                        { "OpusInfo", "http://www.microsoft.com" },
-                        { "PageHash", "/NPH" },
-                        { "TimeStamp", @"/t ""http://ts4096.gtm.microsoft.com/TSS/AuthenticodeTS""" }
-                    },
-                },
-                new Operations
-                {
-                    KeyCode = ESRP.KeyCode.CP_230012,
-                    OperationCode = OperationCode.SigntoolSign,
-                    Parameters = new JObject
-                    {
-                        { "OpusName", "Microsoft" },
-                        { "OpusInfo", "http://www.microsoft.com" },
-                        { "Append", "/as" },
-                        { "FileDigest", @"/fd ""SHA256""" },
-                        { "PageHash", "/NPH" },
-                        { "TimeStamp", @"/tr ""http://rfc3161.gtm.corp.microsoft.com/TSS/HttpTspServer"" /td sha256" }
-                    }
-                },
-                new Operations
-                {
-                    KeyCode = ESRP.KeyCode.CP_230012,
-                    OperationCode = OperationCode.SigntoolVerify,
-                    Parameters = new JObject
-                    {
-                        { "VerifyAll", "/all" }
-                    }
-                }
-            };
-
-            // Classic ID: 402
-            var _microsoftAuthentiCodeSha2HashSha1 = new Operations[]
-            {
-                new Operations
-                {
-                    KeyCode = ESRP.KeyCode.CP_230012,
-                    OperationCode = OperationCode.SigntoolSign,
-                    Parameters = new JObject
-                    {
-                        { "OpusName", _opusName },
-                        { "OpusInfo", _opusInfo },
-                        { "PageHash", "/NPH" },
-                        { "TimeStamp", @"/t ""http://ts4096.gtm.microsoft.com/TSS/AuthenticodeTS""" }
-                    }
-                },
-                new Operations
-                {
-                    KeyCode = ESRP.KeyCode.CP_230012,
-                    OperationCode = OperationCode.SigntoolVerify
-                }
-            };
-
             // Classic ID: N/A
             var _microsoftNuGet = new Operations[]
             {
@@ -174,21 +112,6 @@ namespace Microsoft.Build.OOB.ESRP
                 {
                     KeyCode = ESRP.KeyCode.CP_401405,
                     OperationCode = OperationCode.NuGetVerify
-                }
-            };
-
-            // Classic ID: 160
-            var _microsoftOpc = new Operations[]
-            {
-                new Operations
-                {
-                    KeyCode = ESRP.KeyCode.CP_299834,
-                    OperationCode = OperationCode.OpcSign
-                },
-                new Operations
-                {
-                    KeyCode = ESRP.KeyCode.CP_299834,
-                    OperationCode = OperationCode.OpcVerify
                 }
             };
 
@@ -235,47 +158,6 @@ namespace Microsoft.Build.OOB.ESRP
                 }
             };
 
-            // Classic ID: 135020001
-            var _microsoft3rdPartyAppComponentDual = new Operations[]
-            {
-                new Operations
-                {
-                    KeyCode = ESRP.KeyCode.CP_231522,
-                    OperationCode = OperationCode.SigntoolSign,
-                    Parameters = new JObject
-                    {
-                        { "OpusName", _opusName },
-                        { "OpusInfo", _opusInfo },
-                        { "Append", "/as" },
-                        { "PageHash", "/NPH" },
-                        { "TimeStamp", @"/tr ""http://rfc3161.gtm.corp.microsoft.com/TSS/HttpTspServer""" }
-                    }
-                },
-                new Operations
-                {
-                    KeyCode = ESRP.KeyCode.CP_231514,
-                    OperationCode = OperationCode.SigntoolSign,
-                    Parameters = new JObject
-                    {
-                        { "OpusName", _opusName },
-                        { "OpusInfo", _opusInfo },
-                        { "Append", "/as" },
-                        { "FileDigest", @"/fd ""SHA256""" },
-                        { "PageHash", "/NPH" },
-                        { "TimeStamp", @"/tr ""http://rfc3161.gtm.corp.microsoft.com/TSS/HttpTspServer"" /td sha256" }
-                    }
-                },
-                new Operations
-                {
-                    KeyCode = ESRP.KeyCode.CP_231514,
-                    OperationCode = OperationCode.SigntoolVerify,
-                    Parameters = new JObject
-                    {
-                        { "VerifyAll", "/all" }
-                    }
-                }
-            };
-
             var _microsoftJava = new Operations[]
             {
                 new Operations
@@ -299,13 +181,9 @@ namespace Microsoft.Build.OOB.ESRP
                 { "MicrosoftSN", _microsoftSN },
                 { "MicrosoftSharedLibrariesSN", _microsoftSharedLibrariesSN },
                 { "MicrosoftAuthentiCodeSha2", _microsoftAuthentiCodeSha2 },
-                { "MicrosoftAuthentiCodeSha1Sha2", _microsoftAuthentiCodeSha1Sha2 },
-                { "MicrosoftAuthentiCodeSha2HashSha1", _microsoftAuthentiCodeSha2HashSha1 },
                 { "MicrosoftNuGet", _microsoftNuGet },
-                { "MicrosoftOpc", _microsoftOpc },
                 { "MicrosoftOpc2", _microsoftOpc2 },
                 { "Microsoft3rdPartyAppComponent", _microsoft3rdPartyAppComponent },
-                { "Microsoft3rdPartyAppComponentDual", _microsoft3rdPartyAppComponentDual },
                 { "MicrosoftJava", _microsoftJava },
             };
         }
