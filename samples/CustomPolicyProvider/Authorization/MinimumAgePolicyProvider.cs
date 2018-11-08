906f316d2bef72ae9142dff54e41e7970a623822
James Newton-King <james@newtonking.com>
2018-11-09 10:54:43 +1300

    React to AuthorizationMiddleware (#66)

--- samples/CustomPolicyProvider/Authorization/MinimumAgePolicyProvider.cs
+++ samples/CustomPolicyProvider/Authorization/MinimumAgePolicyProvider.cs
@@ -27,6 +27,8 @@ namespace CustomPolicyProvider
 
         public Task<AuthorizationPolicy> GetDefaultPolicyAsync() => FallbackPolicyProvider.GetDefaultPolicyAsync();
 
+        public Task<AuthorizationPolicy> GetRequiredPolicyAsync() => FallbackPolicyProvider.GetRequiredPolicyAsync();
+
         // Policies are looked up by string name, so expect 'parameters' (like age)
         // to be embedded in the policy names. This is abstracted away from developers
         // by the more strongly-typed attributes derived from AuthorizeAttribute
