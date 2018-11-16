6c5d291b10806decdd8d70d8648012471cda47ca
Pavel Krymets <pavel@krymets.com>
2018-11-16 08:33:57 -0800

    Remove internal types from Logging and Abstractions  (dotnet/extensions#513)
    \n\nCommit migrated from https://github.com/dotnet/extensions/commit/55518d79834d3319c91f40b449d028338b129ed6

--- src/Shared/BenchmarkRunner/DefaultCoreValidationConfig.cs
+++ src/Shared/BenchmarkRunner/DefaultCoreValidationConfig.cs
@@ -1,11 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Linq;
-using System.Reflection;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Loggers;
