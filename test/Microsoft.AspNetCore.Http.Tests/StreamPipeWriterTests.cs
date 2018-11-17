962ec07bdbb090ac2e9cd2a0e68e24c6c276a972
Justin Kotalik <jukotali@microsoft.com>
2018-11-16 19:18:47 -0800

    Adds PipeWriterAdapter (#1065)

---
+++ test/Microsoft.AspNetCore.Http.Tests/StreamPipeWriterTests.cs
@@ -0,0 +1,380 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Buffers;
+using System.IO;
+using System.IO.Pipelines;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Http.Tests
+{
+    public class StreamPipeWriterTests : PipeTest
+    {
+        [Fact]
+        public async Task CanWriteAsyncMultipleTimesIntoSameBlock()
+        {
+
+            await Writer.WriteAsync(new byte[] { 1 });
+            await Writer.WriteAsync(new byte[] { 2 });
+            await Writer.WriteAsync(new byte[] { 3 });
+
+            Assert.Equal(new byte[] { 1, 2, 3 }, Read());
+        }
+
+        [Theory]
+        [InlineData(100, 1000)]
+        [InlineData(100, 8000)]
+        [InlineData(100, 10000)]
+        [InlineData(8000, 100)]
+        [InlineData(8000, 8000)]
+        public async Task CanAdvanceWithPartialConsumptionOfFirstSegment(int firstWriteLength, int secondWriteLength)
+        {
+            await Writer.WriteAsync(Encoding.ASCII.GetBytes("a"));
+
+            var expectedLength = firstWriteLength + secondWriteLength + 1;
+
+            var memory = Writer.GetMemory(firstWriteLength);
+            Writer.Advance(firstWriteLength);
+
+            memory = Writer.GetMemory(secondWriteLength);
+            Writer.Advance(secondWriteLength);
+
+            await Writer.FlushAsync();
+
+            Assert.Equal(expectedLength, Read().Length);
+        }
+
+        [Fact]
+        public async Task ThrowsOnCompleteAndWrite()
+        {
+            Writer.Complete(new InvalidOperationException("Whoops"));
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(async () => await Writer.FlushAsync());
+
+            Assert.Equal("Whoops", exception.Message);
+        }
+
+        [Fact]
+        public async Task WriteCanBeCancelledViaProvidedCancellationToken()
+        {
+            var pipeWriter = new StreamPipeWriter(new HangingStream());
+            var cts = new CancellationTokenSource(1);
+            await Assert.ThrowsAsync<TaskCanceledException>(async () => await pipeWriter.WriteAsync(Encoding.ASCII.GetBytes("data"), cts.Token));
+        }
+
+        [Fact]
+        public async Task WriteCanBeCanceledViaCancelPendingFlushWhenFlushIsAsync()
+        {
+            var pipeWriter = new StreamPipeWriter(new HangingStream());
+            FlushResult flushResult = new FlushResult();
+
+            var tcs = new TaskCompletionSource<int>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            var task = Task.Run(async () =>
+            {
+                try
+                {
+                    var writingTask = pipeWriter.WriteAsync(Encoding.ASCII.GetBytes("data"));
+                    tcs.SetResult(0);
+                    flushResult = await writingTask;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    throw ex;
+                }
+            });
+
+            await tcs.Task;
+
+            pipeWriter.CancelPendingFlush();
+
+            await task;
+
+            Assert.True(flushResult.IsCanceled);
+        }
+
+        [Fact]
+        public void FlushAsyncCompletedAfterPreCancellation()
+        {
+            PipeWriter writableBuffer = Writer.WriteEmpty(1);
+
+            Writer.CancelPendingFlush();
+
+            ValueTask<FlushResult> flushAsync = writableBuffer.FlushAsync();
+
+            Assert.True(flushAsync.IsCompleted);
+
+            FlushResult flushResult = flushAsync.GetAwaiter().GetResult();
+
+            Assert.True(flushResult.IsCanceled);
+
+            flushAsync = writableBuffer.FlushAsync();
+
+            Assert.True(flushAsync.IsCompleted);
+        }
+
+        [Fact]
+        public void FlushAsyncReturnsCanceledIfCanceledBeforeFlush()
+        {
+            CheckCanceledFlush();
+        }
+
+        [Fact]
+        public void FlushAsyncReturnsCanceledIfCanceledBeforeFlushMultipleTimes()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                CheckCanceledFlush();
+            }
+        }
+
+        [Fact]
+        public async Task FlushAsyncReturnsCanceledInterleaved()
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                CheckCanceledFlush();
+                await CheckWriteIsNotCanceled();
+            }
+        }
+
+        [Fact]
+        public async Task CancelPendingFlushBetweenWritesAllDataIsPreserved()
+        {
+            MemoryStream = new SingleWriteStream();
+            Writer = new StreamPipeWriter(MemoryStream);
+            FlushResult flushResult = new FlushResult();
+
+            var tcs = new TaskCompletionSource<int>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            var task = Task.Run(async () =>
+            {
+                try
+                {
+                    await Writer.WriteAsync(Encoding.ASCII.GetBytes("data"));
+
+                    var writingTask = Writer.WriteAsync(Encoding.ASCII.GetBytes(" data"));
+                    tcs.SetResult(0);
+                    flushResult = await writingTask;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    throw ex;
+                }
+            });
+
+            await tcs.Task;
+
+            Writer.CancelPendingFlush();
+
+            await task;
+
+            Assert.True(flushResult.IsCanceled);
+
+            await Writer.WriteAsync(Encoding.ASCII.GetBytes(" more data"));
+            Assert.Equal(Encoding.ASCII.GetBytes("data data more data"), Read());
+        }
+
+        [Fact]
+        public async Task CancelPendingFlushAfterAllWritesAllDataIsPreserved()
+        {
+            MemoryStream = new CannotFlushStream();
+            Writer = new StreamPipeWriter(MemoryStream);
+            FlushResult flushResult = new FlushResult();
+
+            var tcs = new TaskCompletionSource<int>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            var task = Task.Run(async () =>
+            {
+                try
+                {
+                    // Create two Segments
+                    // First one will succeed to write, other one will hang.
+                    var writingTask = Writer.WriteAsync(Encoding.ASCII.GetBytes("data"));
+                    tcs.SetResult(0);
+                    flushResult = await writingTask;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    throw ex;
+                }
+            });
+
+            await tcs.Task;
+
+            Writer.CancelPendingFlush();
+
+            await task;
+
+            Assert.True(flushResult.IsCanceled);
+        }
+
+        [Fact]
+        public async Task CancelPendingFlushLostOfCancellationsNoDataLost()
+        {
+            var writeSize = 16;
+            var singleWriteStream = new SingleWriteStream();
+            MemoryStream = singleWriteStream;
+            Writer = new StreamPipeWriter(MemoryStream, minimumSegmentSize: writeSize);
+
+            for (var i = 0; i < 10; i++)
+            {
+                FlushResult flushResult = new FlushResult();
+                var expectedData = Encoding.ASCII.GetBytes(new string('a', writeSize));
+
+                var tcs = new TaskCompletionSource<int>(TaskCreationOptions.RunContinuationsAsynchronously);
+                // TaskCreationOptions.RunAsync
+
+                var task = Task.Run(async () =>
+                {
+                    try
+                    {
+                        // Create two Segments
+                        // First one will succeed to write, other one will hang.
+                        for (var j = 0; j < 2; j++)
+                        {
+                            Writer.Write(expectedData);
+                        }
+
+                        var flushTask = Writer.FlushAsync();
+                        tcs.SetResult(0);
+                        flushResult = await flushTask;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        throw ex;
+                    }
+                });
+
+                await tcs.Task;
+
+                Writer.CancelPendingFlush();
+
+                await task;
+
+                Assert.True(flushResult.IsCanceled);
+            }
+
+            // Only half of the data was written because every other flush failed.
+            Assert.Equal(16 * 10, ReadWithoutFlush().Length);
+
+            // Start allowing all writes to make read succeed.
+            singleWriteStream.AllowAllWrites = true;
+
+            Assert.Equal(16 * 10 * 2, Read().Length);
+        }
+
+        private async Task CheckWriteIsNotCanceled()
+        {
+            var flushResult = await Writer.WriteAsync(Encoding.ASCII.GetBytes("data"));
+            Assert.False(flushResult.IsCanceled);
+        }
+
+        private void CheckCanceledFlush()
+        {
+            PipeWriter writableBuffer = Writer.WriteEmpty(MaximumSizeHigh);
+
+            Writer.CancelPendingFlush();
+
+            ValueTask<FlushResult> flushAsync = writableBuffer.FlushAsync();
+
+            Assert.True(flushAsync.IsCompleted);
+            FlushResult flushResult = flushAsync.GetAwaiter().GetResult();
+            Assert.True(flushResult.IsCanceled);
+        }
+    }
+
+    internal class HangingStream : MemoryStream
+    {
+
+        public HangingStream()
+        {
+        }
+
+        public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            await Task.Delay(30000, cancellationToken);
+        }
+
+        public override async Task FlushAsync(CancellationToken cancellationToken)
+        {
+            await Task.Delay(30000, cancellationToken);
+        }
+
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            await Task.Delay(30000, cancellationToken);
+            return 0;
+        }
+    }
+
+    internal class SingleWriteStream : MemoryStream
+    {
+        private bool _shouldNextWriteFail;
+
+        public bool AllowAllWrites { get; set; }
+
+
+#if NETCOREAPP2_2
+        public override async ValueTask WriteAsync(ReadOnlyMemory<byte> source, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                if (_shouldNextWriteFail && !AllowAllWrites)
+                {
+                    await Task.Delay(30000, cancellationToken);
+                }
+                else
+                {
+                    await base.WriteAsync(source, cancellationToken);
+                }
+            }
+            finally
+            {
+                _shouldNextWriteFail = !_shouldNextWriteFail;
+            }
+        }
+#endif
+
+        public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            try
+            {
+                if (_shouldNextWriteFail && !AllowAllWrites)
+                {
+                    await Task.Delay(30000, cancellationToken);
+                }
+                await base.WriteAsync(buffer, offset, count, cancellationToken);
+            }
+            finally
+            {
+                _shouldNextWriteFail = !_shouldNextWriteFail;
+            }
+        }
+    }
+
+    internal class CannotFlushStream : MemoryStream
+    {
+        public override async Task FlushAsync(CancellationToken cancellationToken)
+        {
+            await Task.Delay(30000, cancellationToken);
+        }
+    }
+
+    internal static class TestWriterExtensions
+    {
+        public static PipeWriter WriteEmpty(this PipeWriter Writer, int count)
+        {
+            Writer.GetSpan(count).Slice(0, count).Fill(0);
+            Writer.Advance(count);
+            return Writer;
+        }
+    }
+}
---
+++ test/Microsoft.AspNetCore.Http.Tests/TestMemoryPool.cs
@@ -0,0 +1,139 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Buffers;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Microsoft.AspNetCore.Http.Tests
+{
+    public class TestMemoryPool : MemoryPool<byte>
+    {
+        private MemoryPool<byte> _pool = Shared;
+
+        private bool _disposed;
+
+        public override IMemoryOwner<byte> Rent(int minBufferSize = -1)
+        {
+            CheckDisposed();
+            return new PooledMemory(_pool.Rent(minBufferSize), this);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _disposed = true;
+        }
+
+        public override int MaxBufferSize => 4096;
+
+        internal void CheckDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(nameof(TestMemoryPool));
+            }
+        }
+
+        private class PooledMemory : MemoryManager<byte>
+        {
+            private IMemoryOwner<byte> _owner;
+
+            private readonly TestMemoryPool _pool;
+
+            private int _referenceCount;
+
+            private bool _returned;
+
+            private string _leaser;
+
+            public PooledMemory(IMemoryOwner<byte> owner, TestMemoryPool pool)
+            {
+                _owner = owner;
+                _pool = pool;
+                _leaser = Environment.StackTrace;
+                _referenceCount = 1;
+            }
+
+            ~PooledMemory()
+            {
+                Debug.Assert(_returned, "Block being garbage collected instead of returned to pool" + Environment.NewLine + _leaser);
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                _pool.CheckDisposed();
+            }
+
+            public override MemoryHandle Pin(int elementIndex = 0)
+            {
+                _pool.CheckDisposed();
+                Interlocked.Increment(ref _referenceCount);
+
+                if (!MemoryMarshal.TryGetArray(_owner.Memory, out ArraySegment<byte> segment))
+                {
+                    throw new InvalidOperationException();
+                }
+
+                unsafe
+                {
+                    try
+                    {
+                        if ((uint)elementIndex > (uint)segment.Count)
+                        {
+                            throw new ArgumentOutOfRangeException(nameof(elementIndex));
+                        }
+
+                        GCHandle handle = GCHandle.Alloc(segment.Array, GCHandleType.Pinned);
+
+                        return new MemoryHandle(Unsafe.Add<byte>(((void*)handle.AddrOfPinnedObject()), elementIndex + segment.Offset), handle, this);
+                    }
+                    catch
+                    {
+                        Unpin();
+                        throw;
+                    }
+                }
+            }
+
+            public override void Unpin()
+            {
+                _pool.CheckDisposed();
+
+                int newRefCount = Interlocked.Decrement(ref _referenceCount);
+
+                if (newRefCount < 0)
+                    throw new InvalidOperationException();
+
+                if (newRefCount == 0)
+                {
+                    _returned = true;
+                }
+            }
+
+            protected override bool TryGetArray(out ArraySegment<byte> segment)
+            {
+                _pool.CheckDisposed();
+                return MemoryMarshal.TryGetArray(_owner.Memory, out segment);
+            }
+
+            public override Memory<byte> Memory
+            {
+                get
+                {
+                    _pool.CheckDisposed();
+                    return _owner.Memory;
+                }
+            }
+
+            public override Span<byte> GetSpan()
+            {
+                _pool.CheckDisposed();
+                return _owner.Memory.Span;
+            }
+        }
+    }
+}
