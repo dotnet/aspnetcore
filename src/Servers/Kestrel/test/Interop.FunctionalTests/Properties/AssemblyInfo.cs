9612f493ee111486d940310e0740729b8c13c249
Stephen Halter <halter73@gmail.com>
2018-11-16 17:49:09 -0800

    Re-enable trace-level Kestrel test logging

--- src/Servers/Kestrel/test/Interop.FunctionalTests/Properties/AssemblyInfo.cs
+++ src/Servers/Kestrel/test/Interop.FunctionalTests/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Testing;
 
 [assembly: ShortClassName]
+[assembly: LogLevel(LogLevel.Trace)]
