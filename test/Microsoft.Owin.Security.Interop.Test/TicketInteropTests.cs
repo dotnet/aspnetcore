e0538784647990905f9c5fdc5bc2f0e5c7388909
Nate McMaster <natemcmaster@users.noreply.github.com>
2018-11-19 09:47:07 -0800

    Upgrade packages and projects to target netcoreapp3.0 (aspnet/Security#1916)
    Changes:
    
    * Upgrade dependencies and build tools
    * Change TFM on Microsoft.AspNetCore.* packages to netcoreapp3.0
    * Remove .NET Framework tests
    * Disable Owin interop tests. They need to be completely refactored

--- test/Microsoft.Owin.Security.Interop.Test/TicketInteropTests.cs
+++ test/Microsoft.Owin.Security.Interop.Test/TicketInteropTests.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+/*
+
 using System;
 using System.Linq;
 using System.Security.Claims;
@@ -87,5 +89,6 @@ namespace Microsoft.Owin.Security.Interop.Test
         }
     }
 }
+*/
 
 
