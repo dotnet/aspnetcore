2f44dc9e13613e9742d2ed66a1f2a18fcd692481
MikaelMengistu <ethmengistu7@gmail.com>
2018-11-27 15:37:16 -0800

    update helper method

--- src/Middleware/WebSockets/test/ConformanceTests/Helpers.cs
+++ src/Middleware/WebSockets/test/ConformanceTests/Helpers.cs
@@ -8,21 +8,8 @@ namespace Microsoft.AspNetCore.WebSockets.ConformanceTest
         public static string GetApplicationPath(string projectName)
         {
             var applicationBasePath = AppContext.BaseDirectory;
-
             var directoryInfo = new DirectoryInfo(applicationBasePath);
-            do
-            {
-                var solutionFileInfo = new FileInfo(Path.Combine(directoryInfo.FullName, "WebSockets.sln"));
-                if (solutionFileInfo.Exists)
-                {
-                    return Path.GetFullPath(Path.Combine(directoryInfo.FullName, "test", projectName));
-                }
-
-                directoryInfo = directoryInfo.Parent;
-            }
-            while (directoryInfo.Parent != null);
-
-            throw new Exception($"Solution root could not be found using {applicationBasePath}");
+            return Path.GetFullPath(Path.Combine(directoryInfo.FullName, "test", projectName));
         }
     }
 }
