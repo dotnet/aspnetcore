b7554cdee5cf65ed02dfa1b32469e2f4ab1ec787
Chandler Newman <chandler2newman@hotmail.co.uk>
2018-11-28 10:41:32 +0000

    Add service factory autofac test

--- src/Components/test/Microsoft.AspNetCore.Components.Browser.Test/Hosting/WebAssemblyHostBuilderTest.cs
+++ src/Components/test/Microsoft.AspNetCore.Components.Browser.Test/Hosting/WebAssemblyHostBuilderTest.cs
@@ -2,7 +2,10 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.Text;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
@@ -76,5 +79,44 @@ namespace Microsoft.AspNetCore.Components.Hosting
                 services.AddSingleton<string>("foo");
             }
         }
+
+        [Fact]
+        public void HostBuilder_ServiceFactory_Autofac()
+        {
+            // Arrange
+            var builder = new WebAssemblyHostBuilder();
+            builder.ConfigureServices((c, s) => s.AddSingleton<IServiceProviderFactory<IServiceCollection>>(new MyServiceProviderFactory()));
+
+            // Act
+            var host = builder.Build();
+
+            // Assert
+            Assert.IsType<AutofacServiceProvider>(host.Services);
+        }
+
+        private class MyServiceProviderFactory : IServiceProviderFactory<IServiceCollection>
+        {
+            public IServiceCollection CreateBuilder(IServiceCollection services)
+            {
+                return new MyServiceCollection(services);
+            }
+
+            public IServiceProvider CreateServiceProvider(IServiceCollection serviceCollection)
+            {
+                Assert.IsType<MyServiceCollection>(serviceCollection);
+                var containerBuilder = new ContainerBuilder();
+                containerBuilder.Populate(serviceCollection);
+                var container = containerBuilder.Build();
+                return new AutofacServiceProvider(container);
+            }
+        }
+
+        private class MyServiceCollection : List<ServiceDescriptor>, IServiceCollection
+        {
+            public MyServiceCollection(IEnumerable<ServiceDescriptor> collection) : base(collection)
+            {
+            }
+        }
+
     }
 }
