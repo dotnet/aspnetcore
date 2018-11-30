697955855ead9d76e83ba430ead8080e65d3586e
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-11-30 10:11:25 -0800

    add a script that parses out the browser logs

--- build/splat-browser-logs.ps1
+++
@@ -1,9 +0,0 @@
-# Takes an input browser log file and splits it into separate files for each browser
-param(
-    [Parameter(Mandatory=$true, Position=0)][string]$InputFile,
-    [Parameter(Mandatory=$false)][string]$OutputDirectory,
-)
-
-if(!$OutputDirectory) {
-    $OutputDirectory = Split-Path -Parent $InputFile
-}
