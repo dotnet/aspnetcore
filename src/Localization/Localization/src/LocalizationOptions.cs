e5fee282abf55ea72729256f7e532d0aa7b226ba
Nate McMaster <nate.mcmaster@microsoft.com>
2018-12-03 16:17:41 -0800

    Reorganize source code in preparation to move into aspnet/Extensions
    Prior to reorganization, this source code was found in https://github.com/aspnet/Localization/tree/dotnet/extensions@0bcac31dd705fb9db60723f5d7eaeffb728358f5
    \n\nCommit migrated from https://github.com/dotnet/extensions/commit/c8e0450d7a0b8310f671c0c39bc19638b7b04c47

---
+++ src/Localization/Localization/src/LocalizationOptions.cs
@@ -0,0 +1,16 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.Extensions.Localization
+{
+    /// <summary>
+    /// Provides programmatic configuration for localization.
+    /// </summary>
+    public class LocalizationOptions
+    {
+        /// <summary>
+        /// The relative path under application root where resource files are located.
+        /// </summary>
+        public string ResourcesPath { get; set; } = string.Empty;
+    }
+}
