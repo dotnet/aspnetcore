00cf0a4d7da0c8a20425e694574f252fde91b210
Nate McMaster <nate.mcmaster@microsoft.com>
2018-12-05 17:55:51 -0800

    Fix compiler errors

--- src/AuthSamples/samples/StaticFilesAuth/Startup.cs
+++ src/AuthSamples/samples/StaticFilesAuth/Startup.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Endpoints;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
