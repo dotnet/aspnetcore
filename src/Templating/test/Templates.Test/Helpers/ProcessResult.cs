51a16c284f2d3f3873068abf1ff86789d88352f0
Ryan Brandenburg <rybrande@microsoft.com>
2018-12-10 15:00:20 -0800

    Fix namespace

--- src/Templating/test/Templates.Test/Helpers/ProcessResult.cs
+++ src/Templating/test/Templates.Test/Helpers/ProcessResult.cs
@@ -1,9 +1,9 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Diagnostics;
 
-namespace FunctionalTests
+namespace Templates.Test
 {
     public readonly struct ProcessResult
     {
