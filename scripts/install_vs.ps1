e34a02e599180e62004244de40563e827b21818b
Justin Kotalik <jukotali@microsoft.com>
2018-12-12 11:18:59 -0800

    Fb

--- scripts/install_vs.ps1
+++ scripts/install_vs.ps1
@@ -19,11 +19,11 @@ $intermedateDir = "$PSScriptRoot\obj"
 mkdir $intermedateDir -ErrorAction Ignore | Out-Null
 
 $bootstrapper = "$intermedateDir\vs_enterprise1.exe"
-Invoke-WebRequest -Uri 'https://aka.ms/vs/15/pre/vs_enterprise.exe' -OutFile $bootstrapper
+Invoke-WebRequest -Uri 'https://aka.ms/vs/15/release/vs_enterprise.exe' -OutFile $bootstrapper
 
 $vsJson = "$PSScriptRoot\VsRequirements\vs.json"
 # no backslashes - this breaks the installer
-$vsInstallPath = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\aspnetci\Enterprise"
+$vsInstallPath = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\Enterprise"
 $arguments = @(
     '--installPath', "`"$vsInstallPath`"",
     '--in', $vsJson,
@@ -41,30 +41,26 @@ if ($Quiet) {
     $arguments += '--quiet'
 }
 
-try {
-    Write-Host "Running '$bootstrapper $arguments' on $(hostname)"
-    $process = Start-Process -FilePath $bootstrapper `
-        -ArgumentList $arguments `
-        -Verb runas `
-        -PassThru `
-        -ErrorAction Stop
-    Write-Host "pid = $($process.Id)"
-    Wait-Process -InputObject $process
-    Write-Host "exit code = $($process.ExitCode)"
+Write-Host "Running '$bootstrapper $arguments' on $(hostname)"
+$process = Start-Process -FilePath $bootstrapper `
+    -ArgumentList $arguments `
+    -Verb runas `
+    -PassThru `
+    -ErrorAction Stop
+Write-Host "pid = $($process.Id)"
+Wait-Process -InputObject $process
+Write-Host "exit code = $($process.ExitCode)"
 
-    # https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio#error-codes
-    if ($process.ExitCode -eq 3010) {
-        Write-Warning "Agent $(hostname) requires restart to finish the VS update"
-    }
-    elseif ($process.ExitCode -eq 5007) {
-        Write-Error "Operation was blocked - the computer does not meet the requirements"
-    }
-    elseif (($process.ExitCode -eq 5004) -or ($process.ExitCode -eq 1602)) {
-        Write-Error "Operation was canceled"
-    }
-    elseif ($process.ExitCode -ne 0) {
-        Write-Error "Installation failed on $(hostname) for unknown reason"
-    }
+# https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio#error-codes
+if ($process.ExitCode -eq 3010) {
+    Write-Warning "Agent $(hostname) requires restart to finish the VS update"
 }
-finally {
+elseif ($process.ExitCode -eq 5007) {
+    Write-Error "Operation was blocked - the computer does not meet the requirements"
+}
+elseif (($process.ExitCode -eq 5004) -or ($process.ExitCode -eq 1602)) {
+    Write-Error "Operation was canceled"
+}
+elseif ($process.ExitCode -ne 0) {
+    Write-Error "Installation failed on $(hostname) for unknown reason"
 }
