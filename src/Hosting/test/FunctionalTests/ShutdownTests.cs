49d5525352bae013e94399b54eb57ee4a9c771d4
Chris Ross <Tratcher@Outlook.com>
2018-12-12 13:34:57 -0800

    Change Hosting projects to target netcoreapp3.0 #3754 (#4611)

--- src/Hosting/test/FunctionalTests/ShutdownTests.cs
+++ src/Hosting/test/FunctionalTests/ShutdownTests.cs
@@ -57,7 +57,7 @@ namespace Microsoft.AspNetCore.Hosting.FunctionalTests
                     RuntimeArchitecture.x64)
                 {
                     EnvironmentName = "Shutdown",
-                    TargetFramework = Tfm.NetCoreApp22,
+                    TargetFramework = Tfm.NetCoreApp30,
                     ApplicationType = ApplicationType.Portable,
                     PublishApplicationBeforeDeployment = true,
                     StatusMessagesEnabled = false
