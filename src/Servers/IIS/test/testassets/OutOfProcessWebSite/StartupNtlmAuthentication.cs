ce566db62c3c28093a0678e57dfca8df687e8ba8
Justin Kotalik <jukotali@microsoft.com>
2018-12-12 11:49:53 -0800

    Feedback

--- src/Servers/IIS/test/testassets/OutOfProcessWebSite/StartupNtlmAuthentication.cs
+++ src/Servers/IIS/test/testassets/OutOfProcessWebSite/StartupNtlmAuthentication.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Server.IISIntegration;
 using Microsoft.Extensions.Logging;
-using Xunit;
 
 namespace TestSites
 {
@@ -47,7 +46,6 @@ namespace TestSites
                 {
                     if (context.User.Identity.IsAuthenticated)
                     {
-                        Assert.IsType<WindowsPrincipal>(context.User);
                         return context.Response.WriteAsync(context.User.Identity.AuthenticationType);
                     }
                     else
