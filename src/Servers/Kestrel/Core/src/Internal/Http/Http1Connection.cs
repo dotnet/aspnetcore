68ba96d69f8a81637a44ed22dd869c372c7498b0
Stephen Halter <halter73@gmail.com>
2018-12-12 17:24:29 -0800

    WIP Tick

--- src/Servers/Kestrel/Core/src/Internal/Http/Http1Connection.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http/Http1Connection.cs
@@ -528,6 +528,8 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
             }
         }
 
+        void IRequestProcessor.Tick(DateTimeOffset now) { }
+
         private Pipe CreateRequestBodyPipe()
             => new Pipe(new PipeOptions
             (
