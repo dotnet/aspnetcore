a868e418893ff649caae3d25c7ba38be252a3260
Nate McMaster <nate.mcmaster@microsoft.com>
2018-12-13 11:38:27 -0800

    Replace Microsoft.Extensions.StackTrace.Sources with a local copy of code

---
+++ src/Shared/StackTrace/ExceptionDetails/ExceptionDetails.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Extensions.StackTrace.Sources
+{
+    /// <summary>
+    /// Contains details for individual exception messages.
+    /// </summary>
+    internal class ExceptionDetails
+    {
+        /// <summary>
+        /// An individual exception
+        /// </summary>
+        public Exception Error { get; set; }
+
+        /// <summary>
+        /// The generated stack frames
+        /// </summary>
+        public IEnumerable<StackFrameSourceCodeInfo> StackFrames { get; set; }
+
+        /// <summary>
+        /// Gets or sets the summary message.
+        /// </summary>
+        public string ErrorMessage { get; set; }
+    }
+}
