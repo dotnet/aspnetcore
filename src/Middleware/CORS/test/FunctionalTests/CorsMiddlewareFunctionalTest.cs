42afc62acfa990ef193714390127ce60106ec8e4
Mikael Mengistu <mikaelm12@users.noreply.github.com>
2018-12-13 16:51:00 -0800

    Update src/Middleware/CORS/test/FunctionalTests/CorsMiddlewareFunctionalTest.cs
    Co-Authored-By: ajaybhargavb <ajaybhargavb@gmail.com>

--- src/Middleware/CORS/test/FunctionalTests/CorsMiddlewareFunctionalTest.cs
+++ src/Middleware/CORS/test/FunctionalTests/CorsMiddlewareFunctionalTest.cs
@@ -62,7 +62,6 @@ namespace FunctionalTests
         {
             var solutionPath = TestPathUtilities.GetSolutionRootDirectory("Middleware");
 
-
             var configuration =
 #if RELEASE
                 "Release";
