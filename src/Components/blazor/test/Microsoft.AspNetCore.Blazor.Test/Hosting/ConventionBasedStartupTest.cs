a9baa17d6344de0e5ce1f1536a6d2b8505003ec3
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-12-18 09:58:55 +0000

    Fix namespace

--- src/Components/blazor/test/Microsoft.AspNetCore.Blazor.Test/Hosting/ConventionBasedStartupTest.cs
+++ src/Components/blazor/test/Microsoft.AspNetCore.Blazor.Test/Hosting/ConventionBasedStartupTest.cs
@@ -3,7 +3,7 @@
 
 using System;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Blazor.Builder;
+using Microsoft.AspNetCore.Blazor.Hosting;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
