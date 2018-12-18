cbe7759322f4fe4019e2fbf75c44a24d74dbdc98
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-12-18 09:55:30 +0000

    Remove redundant ActivateMonoJSRuntime helper

--- src/Components/test/testapps/BasicTestApp/Startup.cs
+++ src/Components/test/testapps/BasicTestApp/Startup.cs
@@ -23,8 +23,6 @@ namespace BasicTestApp
                 // Needed because the test server runs on a different port than the client app,
                 // and we want to test sending/receiving cookies underling this config
                 WebAssemblyHttpMessageHandler.DefaultCredentials = FetchCredentialsOption.Include;
-
-                GC.KeepAlive(ActivateMonoJSRuntime.EnsureActivated());
             }
 
             app.AddComponent<Index>("root");
