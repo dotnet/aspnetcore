293ba0877fe61cbd29c26a096898688b014d044f
Justin Kotalik <jukotali@microsoft.com>
2018-12-19 10:37:05 -0800

    Update IIS directory layout

---
+++ src/Servers/IIS/AspNetCoreModuleV1/AspNetCore/version.h
@@ -0,0 +1,13 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+
+// This file is auto-generated
+
+
+#define FileVersion 12,1,18353,7
+#define FileVersionStr "12.1.18353.7\0"
+#define ProductVersion 12,1,18353,7
+#define ProductVersionStr "12.1.18353.7\0"
+#define PlatformToolset "v141\0"
+#define CommitHash "0cbc52ca157bf8b98741e6a9ecef69d071632894\0"
