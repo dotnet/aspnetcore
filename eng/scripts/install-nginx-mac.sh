e07f5b45cef61d5a96e801d7986c90b38dbcd66d
Pavel Krymets <pavel@krymets.com>
2018-12-21 11:29:35 -0800

    Remove scripts

--- eng/scripts/install-nginx-mac.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-brew update
-brew install openssl nginx
-ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
-ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
-export PATH="$PATH:$HOME/nginxinstall/sbin/"
