34fceccdf076e532c96791985e6d74c8f2383df0
Pranav K <prkrishn@hotmail.com>
2018-12-31 15:46:01 -0800

    Do not throw when a status code with a codefix appears multiple times in the method body
    Fixes https://github.com/aspnet/AspNetCore/issues/4480

---
+++ src/Mvc/test/Mvc.Api.Analyzers.Test/TestFiles/AddResponseTypeAttributeCodeFixProviderIntegrationTest/CodeFixWorksWhenMultipleIdenticalStatusCodesAreInError.Output.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.AspNetCore.Mvc.Api.Analyzers._OUTPUT_
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class CodeFixWorksWhenMultipleIdenticalStatusCodesAreInError : ControllerBase
+    {
+        public List<CodeFixWorksWhenMultipleIdenticalStatusCodesAreInErrorModel> Values { get; } = 
+            new List<CodeFixWorksWhenMultipleIdenticalStatusCodesAreInErrorModel>();
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public ActionResult<CodeFixWorksWhenMultipleIdenticalStatusCodesAreInErrorModel> GetItem(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            var model = Values.FirstOrDefault(m => m.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return model;
+        }
+    }
+
+    public class CodeFixWorksWhenMultipleIdenticalStatusCodesAreInErrorModel
+    {
+        public int Id { get; set; }
+    }
+}
