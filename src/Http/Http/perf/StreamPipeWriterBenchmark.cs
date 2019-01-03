f16cc2bb95afbfdcfa705226b31f26142306774b
Justin Kotalik <jukotali@microsoft.com>
2019-01-03 12:26:16 -0800

    unneeded changes

--- src/Http/Http/perf/StreamPipeWriterBenchmark.cs
+++ src/Http/Http/perf/StreamPipeWriterBenchmark.cs
@@ -1,8 +1,7 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.IO;
-using System.IO.Pipelines;
 using System.Text;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
