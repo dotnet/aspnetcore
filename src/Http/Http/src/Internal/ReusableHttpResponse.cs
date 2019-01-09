688ad19170df1b41ca6539805fd9348bf9eb5dac
David Fowler <davidfowl@gmail.com>
2019-01-08 16:54:46 -0800

    Reuse HttpContext object per HTTP/1 connection and HTTP/2 stream (#6424)
    - Today in Kestrel, we reuse the IFeatureCollection per connection and per Http2Stream. This PR aims to take advantage of that same technique and affinitize the HttpContext and friends so that they are only allocated per connection.
    - ReusableHttpContext and friends mimic the functionality of DefaultHttpContext but is sealed and has no overridable methods.
    - Introduce IHttpContextContainer which allows servers to cache the HttpContext and friends across requests.

--- src/Http/Http/src/Internal/ReusableHttpResponse.cs
+++ src/Http/Http/src/Internal/ReusableHttpResponse.cs
@@ -1,15 +1,14 @@
-// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Net.Http.Headers;
 
 namespace Microsoft.AspNetCore.Http.Internal
 {
-    public class DefaultHttpResponse : HttpResponse
+    public sealed class ReusableHttpResponse : HttpResponse
     {
         // Lambdas hoisted to static readonly fields to improve inlining https://github.com/dotnet/roslyn/issues/13624
         private readonly static Func<IFeatureCollection, IHttpResponseFeature> _nullResponseFeature = f => null;
@@ -18,21 +17,21 @@ namespace Microsoft.AspNetCore.Http.Internal
         private HttpContext _context;
         private FeatureReferences<FeatureInterfaces> _features;
 
-        public DefaultHttpResponse(HttpContext context)
+        public ReusableHttpResponse(HttpContext context)
         {
             Initialize(context);
         }
 
-        public virtual void Initialize(HttpContext context)
+        public void Initialize(HttpContext context)
         {
             _context = context;
             _features = new FeatureReferences<FeatureInterfaces>(context.Features);
         }
 
-        public virtual void Uninitialize()
+        public void Uninitialize()
         {
             _context = null;
-            _features = default(FeatureReferences<FeatureInterfaces>);
+            _features = default;
         }
 
         private IHttpResponseFeature HttpResponseFeature =>
