4e72798bce2bb9332e8dd56206ca76eb6be88c38
Ryan Nowak <nowakra@gmail.com>
2019-01-09 14:03:25 -0800

    Fix #6102 - Intense CPU utilization on page change
    The issue here was that every time a Razor Page changed, we would
    subscribe an additional time to the endpoint change notifications. This
    means that if you tweaked a page 30 times, we would update the address
    table 31 times when you save the file. If you were doing a lot of editing
    then this would grow to a really large amount of computation.
    
    The fix is to use DataSourceDependentCache, which is an existing utility
    type we developed for this purpose. I'm not sure why it wasn't being
    used for this already. We're already using DataSourceDependentCache in a
    bunch of other places, and it's well tested.
    
    I also tweaked the stucture of this code to be more similar to
    EndpointNameAddressScheme. This involved some test changes that all
    seemed like good cleanup. The way this was being tested was a little
    wonky.

--- src/Http/Routing/src/DataSourceDependentCache.cs
+++ src/Http/Routing/src/DataSourceDependentCache.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -24,6 +24,11 @@ namespace Microsoft.AspNetCore.Routing
             if (dataSource == null)
             {
                 throw new ArgumentNullException(nameof(dataSource));
+            }
+
+            if (initialize == null)
+            {
+                throw new ArgumentNullException(nameof(initialize));
             }
 
             _dataSource = dataSource;
--- src/Http/Routing/src/RouteValuesAddressScheme.cs
+++ src/Http/Routing/src/RouteValuesAddressScheme.cs
@@ -1,46 +1,44 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing.Internal;
 using Microsoft.AspNetCore.Routing.Template;
 using Microsoft.AspNetCore.Routing.Tree;
-using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
 
 namespace Microsoft.AspNetCore.Routing
 {
     internal class RouteValuesAddressScheme : IEndpointAddressScheme<RouteValuesAddress>
     {
-        private readonly EndpointDataSource _dataSource;
-        private LinkGenerationDecisionTree _allMatchesLinkGenerationTree;
-        private Dictionary<string, List<OutboundMatchResult>> _namedMatchResults;
-
+        private readonly DataSourceDependentCache<StateEntry> _cache;
+   
         public RouteValuesAddressScheme(EndpointDataSource dataSource)
         {
-            _dataSource = dataSource;
-
-            // Build initial matches
-            BuildOutboundMatches();
-
-            // Register for changes in endpoints
-            ChangeToken.OnChange(
-                _dataSource.GetChangeToken,
-                HandleChange);
+            _cache = new DataSourceDependentCache<StateEntry>(dataSource, Initialize);
         }
+
+        // Internal for tests
+        internal StateEntry State => _cache.EnsureInitialized();
 
         public IEnumerable<Endpoint> FindEndpoints(RouteValuesAddress address)
         {
+            if (address == null)
+            {
+                throw new ArgumentNullException(nameof(address));
+            }
+
+            var state = State;
+
             IList<OutboundMatchResult> matchResults = null;
             if (string.IsNullOrEmpty(address.RouteName))
             {
-                matchResults = _allMatchesLinkGenerationTree.GetMatches(
+                matchResults = state.AllMatchesLinkGenerationTree.GetMatches(
                     address.ExplicitValues,
                     address.AmbientValues);
             }
-            else if (_namedMatchResults.TryGetValue(address.RouteName, out var namedMatchResults))
+            else if (state.NamedMatches.TryGetValue(address.RouteName, out var namedMatchResults))
             {
                 matchResults = namedMatchResults;
             }
@@ -74,52 +72,31 @@ namespace Microsoft.AspNetCore.Routing
             }
         }
 
-        private void HandleChange()
-        {
-            // rebuild the matches
-            BuildOutboundMatches();
-
-            // re-register the callback as the change token is one time use only and a new change token
-            // is produced every time
-            ChangeToken.OnChange(
-                _dataSource.GetChangeToken,
-                HandleChange);
-        }
-
-        private void BuildOutboundMatches()
-        {
-            // Refresh the matches in the case where a datasource's endpoints changes. The following is OK to do
-            // as refresh of new endpoints happens within a lock and also these fields are not publicly accessible.
-            var (allMatches, namedMatchResults) = GetOutboundMatches();
-            _namedMatchResults = namedMatchResults;
-            _allMatchesLinkGenerationTree = new LinkGenerationDecisionTree(allMatches);
-        }
-
-        /// Decision tree is built using the 'required values' of actions.
-        /// - When generating a url using route values, decision tree checks the explicitly supplied route values +
-        ///   ambient values to see if they have a match for the required-values-based-tree.
-        /// - When generating a url using route name, route values for controller, action etc.might not be provided
-        ///   (this is expected because as a user I want to avoid writing all those and instead chose to use a
-        ///   routename which is quick). So since these values are not provided and might not be even in ambient
-        ///   values, decision tree would fail to find a match. So for this reason decision tree is not used for named
-        ///   matches. Instead all named matches are returned as is and the LinkGenerator uses a TemplateBinder to
-        ///   decide which of the matches can generate a url.
-        ///   For example, for a route defined like below with current ambient values like new { controller = "Home",
-        ///   action = "Index" }
-        ///     "api/orders/{id}",
-        ///     routeName: "OrdersApi",
-        ///     defaults: new { controller = "Orders", action = "GetById" },
-        ///     requiredValues: new { controller = "Orders", action = "GetById" },
-        ///   A call to GetLink("OrdersApi", new { id = "10" }) cannot generate url as neither the supplied values or
-        ///   current ambient values do not satisfy the decision tree that is built based on the required values.
-        protected virtual (List<OutboundMatch>, Dictionary<string, List<OutboundMatchResult>>) GetOutboundMatches()
+        private StateEntry Initialize(IReadOnlyList<Endpoint> endpoints)
         {
             var allOutboundMatches = new List<OutboundMatch>();
-            var namedOutboundMatchResults = new Dictionary<string, List<OutboundMatchResult>>(
-                StringComparer.OrdinalIgnoreCase);
+            var namedOutboundMatchResults = new Dictionary<string, List<OutboundMatchResult>>(StringComparer.OrdinalIgnoreCase);
 
-            foreach (var endpoint in _dataSource.Endpoints)
+            // Decision tree is built using the 'required values' of actions.
+            // - When generating a url using route values, decision tree checks the explicitly supplied route values +
+            //   ambient values to see if they have a match for the required-values-based-tree.
+            // - When generating a url using route name, route values for controller, action etc.might not be provided
+            //   (this is expected because as a user I want to avoid writing all those and instead chose to use a
+            //   routename which is quick). So since these values are not provided and might not be even in ambient
+            //   values, decision tree would fail to find a match. So for this reason decision tree is not used for named
+            //   matches. Instead all named matches are returned as is and the LinkGenerator uses a TemplateBinder to
+            //   decide which of the matches can generate a url.
+            //   For example, for a route defined like below with current ambient values like new { controller = "Home",
+            //   action = "Index" }
+            //     "api/orders/{id}",
+            //     routeName: "OrdersApi",
+            //     defaults: new { controller = "Orders", action = "GetById" },
+            //     requiredValues: new { controller = "Orders", action = "GetById" },
+            //   A call to GetLink("OrdersApi", new { id = "10" }) cannot generate url as neither the supplied values or
+            //   current ambient values do not satisfy the decision tree that is built based on the required values.
+            for (var i = 0; i < endpoints.Count; i++)
             {
+                var endpoint = endpoints[i];
                 if (!(endpoint is RouteEndpoint routeEndpoint))
                 {
                     continue;
@@ -157,7 +134,10 @@ namespace Microsoft.AspNetCore.Routing
                 matchResults.Add(new OutboundMatchResult(outboundMatch, isFallbackMatch: false));
             }
 
-            return (allOutboundMatches, namedOutboundMatchResults);
+            return new StateEntry(
+                allOutboundMatches,
+                new LinkGenerationDecisionTree(allOutboundMatches),
+                namedOutboundMatchResults);
         }
 
         private OutboundRouteEntry CreateOutboundRouteEntry(
@@ -177,6 +157,24 @@ namespace Microsoft.AspNetCore.Routing
             };
             entry.Defaults = new RouteValueDictionary(endpoint.RoutePattern.Defaults);
             return entry;
+        }
+
+        internal class StateEntry
+        {
+            // For testing
+            public readonly List<OutboundMatch> AllMatches;
+            public readonly LinkGenerationDecisionTree AllMatchesLinkGenerationTree;
+            public readonly Dictionary<string, List<OutboundMatchResult>> NamedMatches;
+
+            public StateEntry(
+                List<OutboundMatch> allMatches,
+                LinkGenerationDecisionTree allMatchesLinkGenerationTree,
+                Dictionary<string, List<OutboundMatchResult>> namedMatches)
+            {
+                AllMatches = allMatches;
+                AllMatchesLinkGenerationTree = allMatchesLinkGenerationTree;
+                NamedMatches = namedMatches;
+            }
         }
     }
 }
