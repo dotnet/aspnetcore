87af0c3f1f7f18e7f3d377a97344b8c69f6cb1f7
Chris Ross <Tratcher@Outlook.com>
2019-01-14 12:28:25 -0800

    Update Google Auth UserInfo endpoint (#6338)
    * Update Google Auth UserInfo endpoint #6069
    
    * Add Google to PatchConfig

--- src/Security/Authentication/Google/src/GoogleDefaults.cs
+++ src/Security/Authentication/Google/src/GoogleDefaults.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+
 namespace Microsoft.AspNetCore.Authentication.Google
 {
     /// <summary>
@@ -16,6 +18,22 @@ namespace Microsoft.AspNetCore.Authentication.Google
 
         public static readonly string TokenEndpoint = "https://www.googleapis.com/oauth2/v4/token";
 
-        public static readonly string UserInformationEndpoint = "https://www.googleapis.com/plus/v1/people/me";
+        public static readonly string UserInformationEndpoint;
+
+        private const string UseGooglePlusSwitch = "Switch.Microsoft.AspNetCore.Authentication.Google.UsePlus";
+
+        internal static readonly bool UseGooglePlus;
+
+        static GoogleDefaults()
+        {
+            if (AppContext.TryGetSwitch(UseGooglePlusSwitch, out UseGooglePlus) && UseGooglePlus)
+            {
+                UserInformationEndpoint = "https://www.googleapis.com/plus/v1/people/me";
+            }
+            else
+            {
+                UserInformationEndpoint = "https://www.googleapis.com/oauth2/v2/userinfo";
+            }
+        }
     }
 }
