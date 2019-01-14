80dabca328cbe09bee35761c3906649f3066eb11
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-01-14 12:24:34 -0800

    Update Razor Components template

--- src/Templating/src/Microsoft.DotNet.Web.ProjectTemplates/content/RazorComponentsWeb-CSharp/RazorComponentsWeb-CSharp.Server/Startup.cs
+++ src/Templating/src/Microsoft.DotNet.Web.ProjectTemplates/content/RazorComponentsWeb-CSharp/RazorComponentsWeb-CSharp.Server/Startup.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Mime;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace RazorComponentsWeb_CSharp.Server
@@ -18,17 +15,7 @@ namespace RazorComponentsWeb_CSharp.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            // Adds the Server-Side Blazor services, and those registered by the app project's startup.
-            services.AddServerSideBlazor<App.Startup>();
-
-            services.AddResponseCompression(options =>
-            {
-                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[]
-                {
-                    MediaTypeNames.Application.Octet,
-                    WasmMediaTypeNames.Application.Wasm,
-                });
-            });
+            services.AddRazorComponents<App.Startup>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,8 +26,8 @@ namespace RazorComponentsWeb_CSharp.Server
                 app.UseDeveloperExceptionPage();
             }
 
-            // Use component registrations and static files from the app project.
-            app.UseServerSideBlazor<App.Startup>();
+            app.UseStaticFiles();
+            app.UseRazorComponents<App.Startup>();
         }
     }
 }
