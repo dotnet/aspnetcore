6811762291426b95decdb01a1d2e44997b24e3d8
James Newton-King <james@newtonking.com>
2019-01-16 09:33:54 +1300

    Unit test route name with RouteUrl and ambient values

---
+++ src/Mvc/test/WebSites/RoutingWebSite/Controllers/ContactController.cs
@@ -0,0 +1,23 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingWebSite
+{
+    // This controller is reachable via traditional routing.
+    public class ContactController : Controller
+    {
+        private readonly TestResponseGenerator _generator;
+
+        public ContactController(TestResponseGenerator generator)
+        {
+            _generator = generator;
+        }
+
+        public IActionResult Index()
+        {
+            return Content(Url.RouteUrl("ActionAsMethod", null, Url.ActionContext.HttpContext.Request.Scheme));
+        }
+    }
+}
