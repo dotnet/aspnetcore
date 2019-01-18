36981a4537e3098ead5ae336c3213dcbbbccb896
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-01-18 09:03:41 -0800

    Clean up templates

--- src/Templating/src/Microsoft.DotNet.Web.ProjectTemplates/content/RazorComponentsWeb-CSharp/RazorComponentsWeb-CSharp.App/Services/WeatherForecast.cs
+++ src/Templating/src/Microsoft.DotNet.Web.ProjectTemplates/content/RazorComponentsWeb-CSharp/RazorComponentsWeb-CSharp.App/Services/WeatherForecast.cs
@@ -5,8 +5,11 @@ namespace RazorComponentsWeb_CSharp.App.Services
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
+
         public int TemperatureC { get; set; }
+
         public int TemperatureF { get; set; }
+
         public string Summary { get; set; }
     }
 }
