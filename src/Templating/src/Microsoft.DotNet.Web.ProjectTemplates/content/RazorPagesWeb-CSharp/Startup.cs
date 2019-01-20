371595962a3c10d870d503269ce1c38f7083e618
Pranav K <prkrishn@hotmail.com>
2019-01-19 21:18:26 -0800

    Use AddNewtonsoftJson in RazorPages template (#6877)

--- src/Templating/src/Microsoft.DotNet.Web.ProjectTemplates/content/RazorPagesWeb-CSharp/Startup.cs
+++ src/Templating/src/Microsoft.DotNet.Web.ProjectTemplates/content/RazorPagesWeb-CSharp/Startup.cs
@@ -125,9 +125,11 @@ namespace Company.WebApplication1
                     .RequireAuthenticatedUser()
                     .Build();
                 options.Filters.Add(new AuthorizeFilter(policy));
-            });
+            })
+            .AddNewtonsoftJson();
 #else
-            services.AddMvc();
+            services.AddMvc()
+                .AddNewtonsoftJson();
 #endif
         }
 
