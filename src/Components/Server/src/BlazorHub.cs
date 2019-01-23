fbadced6224c0845398c432b036cfd58ec769e6f
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-01-23 04:54:58 -0800

    Prerrendering interactive

--- src/Components/Server/src/BlazorHub.cs
+++ src/Components/Server/src/BlazorHub.cs
@@ -61,7 +61,7 @@ namespace Microsoft.AspNetCore.Components.Server
             var circuitHost = _circuitFactory.CreateCircuitHost(Context.GetHttpContext(), Clients.Caller);
             circuitHost.UnhandledException += CircuitHost_UnhandledException;
 
-            var uriHelper = (RemoteUriHelper)circuitHost.Services.GetRequiredService<IUriHelper>();
+            var uriHelper = circuitHost.Services.GetRequiredService<RemoteUriHelper>();
             uriHelper.Initialize(uriAbsolute, baseUriAbsolute);
 
             // If initialization fails, this will throw. The caller will explode if they
--- src/Components/test/testassets/ComponentsApp.Server/Startup.cs
+++ src/Components/test/testassets/ComponentsApp.Server/Startup.cs
@@ -1,8 +1,14 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components.Server.Circuits;
+using Microsoft.AspNetCore.Components.Services;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ComponentsApp.Server
@@ -13,6 +19,28 @@ namespace ComponentsApp.Server
         {
             services.AddRazorComponents<App.Startup>();
             services.AddSingleton<WeatherForecastService, DefaultWeatherForecastService>();
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Latest);
+
+            services.AddRouting(options => options.ConstraintMap.Add("clientRoute", typeof(ClientRouteConstraint)));
+
+            services.AddScoped<RemoteUriHelper>();
+            services.AddScoped<ContextBasedUriHelper>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IUriHelper>(sp =>
+            {
+                var contextBased = sp.GetRequiredService<ContextBasedUriHelper>();
+                var remoteBased = sp.GetRequiredService<RemoteUriHelper>();
+
+                if (contextBased.CanHandle())
+                {
+                    return contextBased;
+                }
+                else
+                {
+                    return remoteBased;
+                }
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -24,6 +52,77 @@ namespace ComponentsApp.Server
 
             app.UseStaticFiles();
             app.UseRazorComponents<App.Startup>();
+            app.UseMvc();
+        }
+    }
+
+    internal class ClientRouteConstraint : IRouteConstraint
+    {
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            if (!(httpContext.Request.Path.Equals("/counter", StringComparison.OrdinalIgnoreCase) ||
+                httpContext.Request.Path.Equals("/fetch-data", StringComparison.OrdinalIgnoreCase) ||
+                httpContext.Request.Path.Equals("/index", StringComparison.OrdinalIgnoreCase) ||
+                httpContext.Request.Path.Equals("/", StringComparison.OrdinalIgnoreCase)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    internal class ContextBasedUriHelper : UriHelperBase
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        private HttpContext _context = null;
+        public ContextBasedUriHelper(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        public HttpContext Context
+        {
+            get
+            {
+                if (_context == null)
+                {
+                    _context = _contextAccessor.HttpContext;
+                }
+                if (_context == null)
+                {
+                    throw new InvalidOperationException("Couldn't retrieve the HttpContext");
+                }
+
+                return _context;
+            }
+        }
+
+        protected override void InitializeState()
+        {
+            SetAbsoluteUri(GetAbsoluteUriFromContext());
+            SetAbsoluteBaseUri(GetBaseUriFromContext());
+        }
+
+        protected override void NavigateToCore(string uri, bool forceLoad)
+        {
+            SetAbsoluteUri(uri);
+        }
+
+        internal bool CanHandle() => (_context != null || _contextAccessor.HttpContext != null) &&
+            _contextAccessor.HttpContext?.WebSockets?.IsWebSocketRequest == false;
+
+        private string GetAbsoluteUriFromContext()
+        {
+            var request = Context.Request;
+            var absoluteUri = $"{request.Scheme}://{request.Host}{request.PathBase}{request.Path}{request.QueryString}";
+            return absoluteUri;
+        }
+
+        private string GetBaseUriFromContext()
+        {
+            var request = Context.Request;
+            return $"{request.Scheme}://{request.Host}{request.PathBase}/";
         }
     }
 }
