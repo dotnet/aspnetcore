5d554aeecd1aec8bc422052801a95e05c2393aed
Stephen Halter <halter73@gmail.com>
2019-01-24 11:23:58 -0800

    Revert "Wait to dispose RequestAborted CTS (#4447)"
    This reverts commit 0622513058eff9a4374a0edca726d2c62959ea1e.

--- src/Servers/Kestrel/Core/test/Http1ConnectionTests.cs
+++ src/Servers/Kestrel/Core/test/Http1ConnectionTests.cs
@@ -53,11 +53,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Tests
             var connectionFeatures = new FeatureCollection();
             connectionFeatures.Set(Mock.Of<IConnectionLifetimeFeature>());
 
-            _serviceContext = new TestServiceContext()
-            {
-                Scheduler = PipeScheduler.Inline
-            };
-
+            _serviceContext = new TestServiceContext();
             _timeoutControl = new Mock<ITimeoutControl>();
             _http1ConnectionContext = new HttpConnectionContext
             {
@@ -726,25 +722,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Tests
 
             Assert.False(original.IsCancellationRequested);
             Assert.False(_http1Connection.RequestAborted.IsCancellationRequested);
-        }
-
-        [Fact]
-        public void RequestAbortedTokenIsFullyUsableAfterCancellation()
-        {
-            var originalToken = _http1Connection.RequestAborted;
-            var originalRegistration = originalToken.Register(() => { });
-
-            _http1Connection.Abort(new ConnectionAbortedException());
-
-            Assert.True(originalToken.WaitHandle.WaitOne(TestConstants.DefaultTimeout));
-            Assert.True(_http1Connection.RequestAborted.WaitHandle.WaitOne(TestConstants.DefaultTimeout));
-
-#if NETCOREAPP2_2
-            Assert.Equal(originalToken, originalRegistration.Token);
-#elif NET461
-#else
-#error Target framework needs to be updated
-#endif
         }
 
         [Fact]
