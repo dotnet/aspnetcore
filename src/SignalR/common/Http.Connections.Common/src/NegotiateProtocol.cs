fd62c7d12e87a3a1c67142c6ff77c03b8b7c7fc6
BrennanConroy <brecon@microsoft.com>
2019-01-26 18:27:22 -0800

    update

--- src/SignalR/common/Http.Connections.Common/src/NegotiateProtocol.cs
+++ src/SignalR/common/Http.Connections.Common/src/NegotiateProtocol.cs
@@ -11,9 +11,6 @@ using Microsoft.AspNetCore.Internal;
 
 namespace Microsoft.AspNetCore.Http.Connections
 {
-    /// <summary>
-    /// Provides methods to handle parsing and writing the negotiate protocol.
-    /// </summary>
     public static class NegotiateProtocol
     {
         private const string ConnectionIdPropertyName = "connectionId";
@@ -35,11 +32,6 @@ namespace Microsoft.AspNetCore.Http.Connections
         private const string ProtocolVersionPropertyName = "ProtocolVersion";
         private static readonly byte[] ProtocolVersionPropertyNameBytes = Encoding.UTF8.GetBytes(ProtocolVersionPropertyName);
 
-        /// <summary>
-        /// Writes the <paramref name="response"/> as json into <paramref name="output"/>.
-        /// </summary>
-        /// <param name="response">The <see cref="NegotiationResponse"/> to write to json.</param>
-        /// <param name="output">The <see cref="IBufferWriter{T}"/> to write the <see cref="NegotiationResponse"/> to.</param>
         public static void WriteResponse(NegotiationResponse response, IBufferWriter<byte> output)
         {
             var writer = new Utf8JsonWriter(output, new JsonWriterState(new JsonWriterOptions() { SkipValidation = true }));
@@ -97,11 +89,6 @@ namespace Microsoft.AspNetCore.Http.Connections
             writer.Flush(isFinalBlock: true);
         }
 
-        /// <summary>
-        /// Parses the <see cref="NegotiationResponse"/> from the given <paramref name="content"/>.
-        /// </summary>
-        /// <param name="content">The data that contains the serialized <see cref="NegotiationResponse"/>.</param>
-        /// <returns>The deserialized <see cref="NegotiationResponse"/>.</returns>
         public static NegotiationResponse ParseResponse(ReadOnlySpan<byte> content)
         {
             try
@@ -206,13 +193,10 @@ namespace Microsoft.AspNetCore.Http.Connections
         }
 
         /// <summary>
-        /// Parses the <see cref="NegotiationResponse"/> from the given <paramref name="content"/>.
         /// <para>
         ///     This method is obsolete and will be removed in a future version.
         ///     The recommended alternative is <see cref="ParseResponse(ReadOnlySpan{byte})" />.
         /// </para>
-        /// <param name="content">The data that contains the serialized <see cref="NegotiationResponse"/>.</param>
-        /// <returns>The deserialized <see cref="NegotiationResponse"/>.</returns>
         /// </summary>
         [Obsolete("This method is obsolete and will be removed in a future version. The recommended alternative is ParseResponse(ReadOnlySpan{byte}).")]
         public static NegotiationResponse ParseResponse(Stream content)
--- src/SignalR/common/Http.Connections.Common/src/NegotiationResponse.cs
+++ src/SignalR/common/Http.Connections.Common/src/NegotiationResponse.cs
@@ -5,30 +5,12 @@ using System.Collections.Generic;
 
 namespace Microsoft.AspNetCore.Http.Connections
 {
-    /// <summary>
-    /// Stores information used by the client to start a real-time connection with the endpoint.
-    /// </summary>
     public class NegotiationResponse
     {
-        /// <summary>
-        /// If this has a value then the client should do another negotiate request with the <see cref="Url"/> and <see cref="AccessToken"/>.
-        /// </summary>
         public string Url { get; set; }
-        /// <summary>
-        /// Used with <see cref="Url"/> to negotiate with a different endpoint.
-        /// </summary>
         public string AccessToken { get; set; }
-        /// <summary>
-        /// The ID query string value to be used when connecting with the endpoint.
-        /// </summary>
         public string ConnectionId { get; set; }
-        /// <summary>
-        /// A list of <see cref="AvailableTransport"/> that the client can choose from to connect with.
-        /// </summary>
         public IList<AvailableTransport> AvailableTransports { get; set; }
-        /// <summary>
-        /// If this has a value then the negotiate failed and this value should have some details on why.
-        /// </summary>
         public string Error { get; set; }
     }
 }
