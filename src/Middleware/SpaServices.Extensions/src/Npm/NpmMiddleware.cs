e7a9c43b36eb8498f7fc54e766d955936f91b857
Alexey Zimarev <alex@zimarev.com>
2019-01-27 14:17:42 +0100

    Adding a base npm middleware and making it public

---
+++ src/Middleware/SpaServices.Extensions/src/Npm/NpmMiddleware.cs
@@ -0,0 +1,136 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.NodeServices.Npm;
+using Microsoft.AspNetCore.NodeServices.Util;
+using Microsoft.AspNetCore.SpaServices.Util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Http;
+using Microsoft.AspNetCore.SpaServices.Extensions.Util;
+
+namespace Microsoft.AspNetCore.SpaServices.Npm
+{
+    public static class NpmMiddleware
+    {
+        private const string LogCategoryName = "Microsoft.AspNetCore.SpaServices";
+
+        public static void Attach(ISpaBuilder spaBuilder,
+            string npmScriptName,
+            Func<int, string> npmParameters,
+            Func<int, IDictionary<string, string>> envVars, 
+            Regex outputMatch, Func<Match, int, Uri> listeningUri)
+        {
+            var sourcePath = spaBuilder.Options.SourcePath;
+            if (string.IsNullOrEmpty(sourcePath))
+            {
+                throw new ArgumentException("Cannot be null or empty", nameof(sourcePath));
+            }
+
+            if (string.IsNullOrEmpty(npmScriptName))
+            {
+                throw new ArgumentException("Cannot be null or empty", nameof(npmScriptName));
+            }
+
+            // Start Angular CLI and attach to middleware pipeline
+            var appBuilder = spaBuilder.ApplicationBuilder;
+            var logger = LoggerFinder.GetOrCreateLogger(appBuilder, LogCategoryName);
+            var npmServerPortTask = 
+                StartNpmServerAsync(sourcePath, npmScriptName, npmParameters, envVars, outputMatch, listeningUri, logger);
+
+            var targetUriTask = npmServerPortTask.ContinueWith(task => task.Result);
+
+            spaBuilder.UseProxyToSpaDevelopmentServer(() =>
+            {
+                // On each request, we create a separate startup task with its own timeout. That way, even if
+                // the first request times out, subsequent requests could still work.
+                var timeout = spaBuilder.Options.StartupTimeout;
+                return targetUriTask.WithTimeout(timeout,
+                    $"The npm process did not start listening for requests " +
+                    $"within the timeout period of {timeout.Seconds} seconds. " +
+                    $"Check the log output for error information.");
+            });
+        }
+
+        private static async Task<Uri> StartNpmServerAsync(
+            string sourcePath, string npmScriptName, 
+            Func<int, string> npmParameters, Func<int, IDictionary<string, string>> envVars,
+            Regex outputMatch, Func<Match, int, Uri> listeningUri,
+            ILogger logger)
+        {
+            var portNumber = TcpPortFinder.FindAvailablePort();
+            logger.LogInformation($"Starting {npmScriptName} on port {portNumber}...");
+
+            var npmScriptRunner = new NpmScriptRunner(
+                sourcePath, npmScriptName, npmParameters(portNumber), envVars(portNumber));
+            npmScriptRunner.AttachToLogger(logger);
+
+            Uri uri;
+            using (var stdErrReader = new EventedStreamStringReader(npmScriptRunner.StdErr))
+            {
+                try
+                {
+                    uri = listeningUri(await npmScriptRunner.StdOut.WaitForMatch(outputMatch), portNumber);
+                }
+                catch (EndOfStreamException ex)
+                {
+                    throw new InvalidOperationException(
+                        $"The NPM script '{npmScriptName}' exited without indicating that the " +
+                        $"server was listening for requests. The error output was: " +
+                        $"{stdErrReader.ReadAsString()}", ex);
+                }
+            }
+
+            // Even after the Angular CLI claims to be listening for requests, there's a short
+            // period where it will give an error if you make a request too quickly
+            await WaitForNpmServerToAcceptRequests(uri);
+
+            return uri;
+        }
+
+        private static async Task WaitForNpmServerToAcceptRequests(Uri cliServerUri)
+        {
+            // To determine when it's actually ready, try making HEAD requests to '/'. If it
+            // produces any HTTP response (even if it's 404) then it's ready. If it rejects the
+            // connection then it's not ready. We keep trying forever because this is dev-mode
+            // only, and only a single startup attempt will be made, and there's a further level
+            // of timeouts enforced on a per-request basis.
+            var timeoutMilliseconds = 1000;
+            using (var client = new HttpClient())
+            {
+                while (true)
+                {
+                    try
+                    {
+                        // If we get any HTTP response, the CLI server is ready
+                        await client.SendAsync(
+                            new HttpRequestMessage(HttpMethod.Head, cliServerUri),
+                            new CancellationTokenSource(timeoutMilliseconds).Token);
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        await Task.Delay(500);
+
+                        // Depending on the host's networking configuration, the requests can take a while
+                        // to go through, most likely due to the time spent resolving 'localhost'.
+                        // Each time we have a failure, allow a bit longer next time (up to a maximum).
+                        // This only influences the time until we regard the dev server as 'ready', so it
+                        // doesn't affect the runtime perf (even in dev mode) once the first connection is made.
+                        // Resolves https://github.com/aspnet/JavaScriptServices/issues/1611
+                        if (timeoutMilliseconds < 10000)
+                        {
+                            timeoutMilliseconds += 3000;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
