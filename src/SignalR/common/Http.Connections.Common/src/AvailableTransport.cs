28f41286982f40eb051e64728d294c695686b8d7
BrennanConroy <brecon@microsoft.com>
2019-01-26 17:55:38 -0800

    xml

--- src/SignalR/common/Http.Connections.Common/src/AvailableTransport.cs
+++ src/SignalR/common/Http.Connections.Common/src/AvailableTransport.cs
@@ -5,9 +5,19 @@ using System.Collections.Generic;
 
 namespace Microsoft.AspNetCore.Http.Connections
 {
+    /// <summary>
+    /// Stores the <see cref="Transport"/> and <see cref="TransferFormats"/> supported by the Transport.
+    /// Used in the <see cref="NegotiationResponse"/> to tell the client what is supported.
+    /// </summary>
     public class AvailableTransport
     {
+        /// <summary>
+        /// The name of the Transport.
+        /// </summary>
         public string Transport { get; set; }
+        /// <summary>
+        /// The formats supported by this <see cref="Transport"/>.
+        /// </summary>
         public IList<string> TransferFormats { get; set; }
     }
 }
