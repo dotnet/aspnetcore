9f71e602837b164e6f525ae33821d1d95ea29f6b
David Fowler <davidfowl@gmail.com>
2019-01-28 20:38:49 +0000

    Hoist method attributes from the request delegate as metadata. (#6911)
    - This should allow a more declarative approach to declaring endpoint metadata using the default methods.
    - Attributes are applied first and can be overridden imperatively

--- src/Http/Routing/test/UnitTests/Builder/MapEndpointEndpointDataSourceBuilderExtensionsTest.cs
+++ src/Http/Routing/test/UnitTests/Builder/MapEndpointEndpointDataSourceBuilderExtensionsTest.cs
@@ -2,11 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.AspNetCore.Routing.Matching;
 using Microsoft.AspNetCore.Routing.Patterns;
 using Xunit;
 
@@ -96,6 +94,60 @@ namespace Microsoft.AspNetCore.Builder
             Assert.Equal("Display name!", endpointBuilder1.DisplayName);
             Assert.Equal("/", endpointBuilder1.RoutePattern.RawText);
             Assert.Equal(metadata, Assert.Single(endpointBuilder1.Metadata));
+        }
+
+        [Fact]
+        public void MapEndpoint_AttributesCollectedAsMetadata()
+        {
+            // Arrange
+            var builder = new DefaultEndpointRouteBuilder();
+
+            // Act
+            var endpointBuilder = builder.Map(RoutePatternFactory.Parse("/"), "Display name!", Handle);
+
+            // Assert
+            var endpointBuilder1 = GetRouteEndpointBuilder(builder);
+            Assert.Equal("Display name!", endpointBuilder1.DisplayName);
+            Assert.Equal("/", endpointBuilder1.RoutePattern.RawText);
+            Assert.Equal(2, endpointBuilder1.Metadata.Count);
+            Assert.IsType<Attribute1>(endpointBuilder1.Metadata[0]);
+            Assert.IsType<Attribute2>(endpointBuilder1.Metadata[1]);
+        }
+
+        [Fact]
+        public void MapEndpoint_ExplicitMetadataAddedAfterAttributeMetadata()
+        {
+            // Arrange
+            var builder = new DefaultEndpointRouteBuilder();
+
+            // Act
+            var endpointBuilder = builder.Map(RoutePatternFactory.Parse("/"), "Display name!", Handle, new Metadata());
+
+            // Assert
+            var endpointBuilder1 = GetRouteEndpointBuilder(builder);
+            Assert.Equal("Display name!", endpointBuilder1.DisplayName);
+            Assert.Equal("/", endpointBuilder1.RoutePattern.RawText);
+            Assert.Equal(3, endpointBuilder1.Metadata.Count);
+            Assert.IsType<Attribute1>(endpointBuilder1.Metadata[0]);
+            Assert.IsType<Attribute2>(endpointBuilder1.Metadata[1]);
+            Assert.IsType<Metadata>(endpointBuilder1.Metadata[2]);
+        }
+
+        [Attribute1]
+        [Attribute2]
+        private static Task Handle(HttpContext context) => Task.CompletedTask;
+
+        private class Attribute1 : Attribute
+        {
+        }
+
+        private class Attribute2 : Attribute
+        {
+        }
+
+        private class Metadata
+        {
+
         }
     }
 }
