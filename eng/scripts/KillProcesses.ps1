35f4208691fb1323a464f7ac8635a5a983762e9c
Doug Bunting <dougbu@microsoft.com>
2019-02-03 18:59:01 -0800

    Kill processes after completing build and before publishing test results or artifacts - aspnet/AspNetCore-Internal#1735 - port aspnet/Extensions@e2bb8ac1a0 fix into this repo   - change `ci-official.yml` and `default-build.yml` instead of `azure-pipelines.yml`

---
+++ eng/scripts/KillProcesses.ps1
@@ -0,0 +1,16 @@
+#!/usr/bin/env pwsh -c
+$ErrorActionPreference = 'Continue'
+
+taskkill /T /F /IM dotnet.exe
+taskkill /T /F /IM testhost.exe
+taskkill /T /F /IM iisexpress.exe
+taskkill /T /F /IM iisexpresstray.exe
+taskkill /T /F /IM w3wp.exe
+taskkill /T /F /IM msbuild.exe
+taskkill /T /F /IM vbcscompiler.exe
+taskkill /T /F /IM git.exe
+taskkill /T /F /IM vctip.exe
+taskkill /T /F /IM chrome.exe
+iisreset /restart
+
+exit 0
