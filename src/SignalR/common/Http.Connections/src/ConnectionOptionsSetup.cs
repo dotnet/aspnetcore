713cabffb2be315070375e80553dea1a45d24dc8
MikaelMengistu <ethmengistu7@gmail.com>
2019-02-07 11:47:06 -0800

    usings

--- src/SignalR/common/Http.Connections/src/ConnectionOptionsSetup.cs
+++ src/SignalR/common/Http.Connections/src/ConnectionOptionsSetup.cs
@@ -1,10 +1,9 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.Extensions.Options;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.Extensions.Options;
+
 
 namespace Microsoft.AspNetCore.Http.Connections
 {
--- src/SignalR/common/Http.Connections/src/Internal/HttpConnectionManager.cs
+++ src/SignalR/common/Http.Connections/src/Internal/HttpConnectionManager.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Buffers.Text;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
