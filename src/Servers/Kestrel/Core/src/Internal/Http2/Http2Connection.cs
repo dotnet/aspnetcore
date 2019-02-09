64d5df9dbc6612d893736ca253eab80a1e9a98c1
David Fowler <davidfowl@gmail.com>
2019-02-08 18:34:50 -0800

    More efficient preface parsing - Slice before turning the buffer into a Span - Use SequenceEqual instead of a loop

--- src/Servers/Kestrel/Core/src/Internal/Http2/Http2Connection.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http2/Http2Connection.cs
@@ -357,29 +357,25 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2
             return false;
         }
 
-        private bool ParsePreface(ReadOnlySequence<byte> readableBuffer, out SequencePosition consumed, out SequencePosition examined)
+        private bool ParsePreface(in ReadOnlySequence<byte> buffer, out SequencePosition consumed, out SequencePosition examined)
         {
-            consumed = readableBuffer.Start;
-            examined = readableBuffer.End;
+            consumed = buffer.Start;
+            examined = buffer.End;
 
-            if (readableBuffer.Length < ClientPreface.Length)
+            if (buffer.Length < ClientPreface.Length)
             {
                 return false;
             }
 
-            var span = readableBuffer.IsSingleSegment
-                ? readableBuffer.First.Span
-                : readableBuffer.ToSpan();
+            var preface = buffer.Slice(0, ClientPreface.Length);
+            var span = preface.ToSpan();
 
-            for (var i = 0; i < ClientPreface.Length; i++)
+            if (!span.SequenceEqual(ClientPreface))
             {
-                if (ClientPreface[i] != span[i])
-                {
-                    throw new Http2ConnectionErrorException(CoreStrings.Http2ErrorInvalidPreface, Http2ErrorCode.PROTOCOL_ERROR);
-                }
+                throw new Http2ConnectionErrorException(CoreStrings.Http2ErrorInvalidPreface, Http2ErrorCode.PROTOCOL_ERROR);
             }
 
-            consumed = examined = readableBuffer.GetPosition(ClientPreface.Length);
+            consumed = examined = preface.End;
             return true;
         }
 
