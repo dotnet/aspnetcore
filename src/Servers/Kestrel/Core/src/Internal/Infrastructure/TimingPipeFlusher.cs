e5f31f264cbc4e21c501ae4e8b39d5e235348b20
Justin Kotalik <jukotali@microsoft.com>
2019-02-11 15:10:30 -0800

    Another spot

--- src/Servers/Kestrel/Core/src/Internal/Infrastructure/TimingPipeFlusher.cs
+++ src/Servers/Kestrel/Core/src/Internal/Infrastructure/TimingPipeFlusher.cs
@@ -72,7 +72,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Infrastructure
             {
                 if (_lastFlushTask == null || _lastFlushTask.IsCompleted)
                 {
-                    _lastFlushTask = flushValueTask.GetAsTask();
+                    _lastFlushTask = flushValueTask.AsTask();
                 }
 
                 return TimeFlushAsync(minRate, count, outputAborter, cancellationToken);
