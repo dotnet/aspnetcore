cb42740f17fbd354e04942d88b68700112275047
Pavel Krymets <pavel@krymets.com>
2019-02-15 10:40:27 -0800

    Less commands

--- src/Servers/IIS/tools/SetupTestEnvironment.ps1
+++ src/Servers/IIS/tools/SetupTestEnvironment.ps1
@@ -108,7 +108,7 @@ function Shutdown-Dumps()
     {
         if (Test-Path $cdb)
         {
-            & $cdb -z $dump.FullName -c ".netsyms no; .loadby sos coreclr; !sym noisy; .reload /f;.dumpcab -a $($dump.FullName).cab;q;"
+            & $cdb -z $dump.FullName -c ".loadby sos coreclr;.dumpcab -a $($dump.FullName).cab;q;"
             Remove-Item $dump.FullName
         }
     }
