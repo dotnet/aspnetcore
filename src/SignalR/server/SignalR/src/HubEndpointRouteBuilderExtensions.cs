b97eb501ddca899365ac293a0785efbd44f14e02
David Fowler <davidfowl@gmail.com>
2019-02-15 09:10:39 -0800

    PR feedback

--- src/SignalR/server/SignalR/src/HubEndpointRouteBuilderExtensions.cs
+++ src/SignalR/server/SignalR/src/HubEndpointRouteBuilderExtensions.cs
@@ -1,10 +1,13 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Authorization;
-using System.Linq;
 
 namespace Microsoft.AspNetCore.Routing
 {
