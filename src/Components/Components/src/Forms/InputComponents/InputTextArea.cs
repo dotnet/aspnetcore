12a1a45be9f147a914a53a643e9260bcca24b096
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-02-15 00:51:35 +0000

    Add InputTextArea

---
+++ src/Components/Components/src/Forms/InputComponents/InputTextArea.cs
@@ -0,0 +1,33 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Components.RenderTree;
+
+namespace Microsoft.AspNetCore.Components.Forms
+{
+    /* This is exactly equivalent to a .razor file containing:
+     *
+     *    @inherits InputBase<string>
+     *    <textarea bind="@CurrentValue" class="@CssClass"></textarea>
+     *
+     * The only reason it's not implemented as a .razor file is that we don't presently have the ability to compile those
+     * files within this project. Developers building their own input components should use Razor syntax.
+     */
+
+    /// <summary>
+    /// An input component for editing <see cref="string"/> values.
+    /// </summary>
+    public class InputTextArea : InputBase<string>
+    {
+        /// <inheritdoc />
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            builder.OpenElement(0, "textarea");
+            builder.AddAttribute(1, "class", CssClass);
+            builder.AddAttribute(2, "value", BindMethods.GetValue(CurrentValue));
+            builder.AddAttribute(3, "onchange", BindMethods.SetValueHandler(__value => CurrentValue = __value, CurrentValue));
+            builder.CloseElement();
+        }
+    }
+}
