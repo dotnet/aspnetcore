294a086e8cd78b31507d93b79625e209c57be219
Pavel Krymets <pavel@krymets.com>
2019-02-14 21:01:56 -0800

    Comands FTW

---
+++ eng/scripts/RunPowershell.cmd
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+ECHO %*
+SET _TAIL=%*
+CALL SET _TAIL=%%_TAIL:*%1=%%
+
+PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = ''; try { & '%~dp0%1' %_TAIL%; exit $LASTEXITCODE } catch { write-host $_; exit 1 }"
