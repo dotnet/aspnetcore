bddfc8762bef96f3f4c6f23f3b34a64be5fa2305
John Luo <johluo@microsoft.com>
2019-02-19 11:50:22 -0800

    Update

--- src/ProjectTemplates/Web.ProjectTemplates/content/Grpc-Application/Grpc-Application.Server/Startup.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/Grpc-Application/Grpc-Application.Server/Startup.cs
@@ -18,7 +18,7 @@ namespace Company.WebApplication1
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
