85ae18c723a27740a3e1056faca377fd999d613b
Nate McMaster <natemcmaster@users.noreply.github.com>
2019-02-20 09:53:53 -0800

    Make Visual Studio 2019 a prerequisite to building this repo (#7005)
    Changes:
    
    * Make Visual Studio 2019 a prerequisite for building this repo
    * Update .sln files
    * Update Windows SDK to 17134
    * Update developer docs
    * Disable ANCM tests
    * Update to .NET Core SDK 3.0 Preview 2
    * Use Microsoft.NET.Sdk.Razor as a package consistently accross the repo
    * React to changes in metadata from Microsoft.NETCore.App
    * React to changes in .NET Core SDK
    * Attempt to workaround CodeCheck.ps1 failure which doesn't repro locally or on different agents. Possibly due to differences in the version of the PowerShell task?
    * Remove dead YML file
    * Rename usages of win7-{x64,x86} to win-{x64,x86}
    * Update KoreBuild to 3.0.0-build-20190219.1

--- eng/scripts/CodeCheck.ps1
+++ eng/scripts/CodeCheck.ps1
@@ -33,13 +33,14 @@ function LogError {
 }
 
 try {
-    #
-    # Solutions
-    #
-
     if ($ci) {
+        # Install dotnet.exe
         & $repoRoot/build.ps1 -ci -norestore /t:InstallDotNet
     }
+
+    #
+    # Versions.props and Version.Details.xml
+    #
 
     Write-Host "Checking that Versions.props and Version.Details.xml match"
     [xml] $versionProps = Get-Content "$repoRoot/eng/Versions.props"
@@ -79,6 +80,10 @@ try {
             -filepath "$repoRoot\eng\Versions.props"
     }
 
+    #
+    # Solutions
+    #
+
     Write-Host "Checking that solutions are up to date"
 
     Get-ChildItem "$repoRoot/*.sln" -Recurse `
@@ -91,7 +96,7 @@ try {
         $slnDir = Split-Path -Parent $_
         $sln = $_
         & dotnet sln $_ list `
-            | ? { $_ -ne 'Project(s)' -and $_ -ne '----------' } `
+            | ? { $_ -like '*proj' } `
             | % {
                 $proj = Join-Path $slnDir $_
                 if (-not (Test-Path $proj)) {
@@ -121,9 +126,16 @@ try {
     }
 
     Write-Host "Run git diff to check for pending changes"
-    $changedFiles = git --no-pager diff --ignore-space-at-eol --name-only
+
+    # Redirect stderr to stdout because PowerShell does not consistently handle output to stderr
+    $changedFiles = & cmd /c 'git --no-pager diff --ignore-space-at-eol --name-only 2>&1'
+
     if ($changedFiles) {
         foreach ($file in $changedFiles) {
+            if (($file -like 'warning:*') -or ($file -like 'The file will have its original line endings*')) {
+                # git might emit warnings to stderr about CRLF vs LR, which can vary from machine to machine based on git's configuration
+                continue
+            }
             $filePath = Resolve-Path "${repoRoot}/${file}"
             LogError "Generated code is not up to date in $file." -filepath $filePath
             & git --no-pager diff --ignore-space-at-eol $filePath
