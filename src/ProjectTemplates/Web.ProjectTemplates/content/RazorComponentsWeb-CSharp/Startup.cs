c380e55de36d91f985aca3bf6dd04992c4a5a62c
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-02-21 11:31:29 -0800

    Last template touch-ups

--- src/ProjectTemplates/Web.ProjectTemplates/content/RazorComponentsWeb-CSharp/Startup.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/RazorComponentsWeb-CSharp/Startup.cs
@@ -21,8 +21,12 @@ namespace RazorComponentsWeb_CSharp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<WeatherForecastService>();
+            services.AddMvc()
+                .AddNewtonsoftJson();
+
             services.AddRazorComponents();
+
+            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
