5e43c786a879985715a071bb587c11a71d2513d7
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-02-23 00:26:24 -0800

    [MVC] Add components integration project to the build

---
+++ src/Mvc/Mvc.ViewFeatures.Prerendering/src/Class1.cs
@@ -0,0 +1,8 @@
+
+namespace Microsoft.AspNetCore.Mvc.ViewFeatures
+{
+    internal class Class1
+    {
+        
+    }
+}
---
+++ src/Mvc/Mvc.ViewFeatures.Prerendering/test/Class1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.ViewFeatures
+{
+    public class Class1Tests 
+    {
+        [Fact]
+        public void True()
+        {
+            Assert.True(true);
+        }
+    }
+}
