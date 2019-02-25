759a80cd240eb93c631f93fae12c7c345d24f1ed
natery2000 <natery2000@gmail.com>
2019-02-24 19:58:45 -0600

    Adding comments

--- src/Http/Http.Features/src/SameSiteMode.cs
+++ src/Http/Http.Features/src/SameSiteMode.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 namespace Microsoft.AspNetCore.Http
@@ -7,8 +7,11 @@ namespace Microsoft.AspNetCore.Http
     // This mirrors Microsoft.Net.Http.Headers.SameSiteMode
     public enum SameSiteMode
     {
+        /// <summary>The cookie will not be sent along with "same-site" requests, with "cross-site" top-level navigations</summary>
         None = 0,
+        /// <summary>The cookie will be sent with "same-site" requests, and with "cross-site" top-level navigations</summary>
         Lax,
+        /// <summary>The cookie will only be sent along with "same-site" requests</summary>
         Strict
     }
 }
