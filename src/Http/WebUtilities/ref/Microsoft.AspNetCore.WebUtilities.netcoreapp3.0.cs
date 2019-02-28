0d4c69edc60e6f4501059826848afefd9c6d814c
Justin Kotalik <jukotali@microsoft.com>
2019-02-28 11:16:41 -0800

    Final round of feedback

--- src/Http/WebUtilities/ref/Microsoft.AspNetCore.WebUtilities.netcoreapp3.0.cs
+++ src/Http/WebUtilities/ref/Microsoft.AspNetCore.WebUtilities.netcoreapp3.0.cs
@@ -1,15 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-namespace Microsoft.AspNetCore.Internal
-{
-    public partial class UrlDecoder
-    {
-        public UrlDecoder() { }
-        public static int DecodeInPlace(System.Span<byte> buffer, bool isFormEncoding) { throw null; }
-        public static int DecodeRequestLine(System.ReadOnlySpan<byte> source, System.Span<byte> destination, bool isFormEncoding) { throw null; }
-    }
-}
 namespace Microsoft.AspNetCore.WebUtilities
 {
     public static partial class Base64UrlTextEncoder
