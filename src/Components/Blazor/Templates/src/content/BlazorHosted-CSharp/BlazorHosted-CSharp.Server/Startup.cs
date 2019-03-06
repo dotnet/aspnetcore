dfe3eb7d2b04b2617b3606cfb85174a15ec4c7a2
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-03-06 05:50:30 -0800

    [Components][Fixes #8058] Update blazor template to use IWebHostEnvironment

--- src/Components/Blazor/Templates/src/content/BlazorHosted-CSharp/BlazorHosted-CSharp.Server/Startup.cs
+++ src/Components/Blazor/Templates/src/content/BlazorHosted-CSharp/BlazorHosted-CSharp.Server/Startup.cs
@@ -18,7 +18,7 @@ namespace BlazorHosted_CSharp.Server
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseResponseCompression();
 
