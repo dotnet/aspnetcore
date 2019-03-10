cb6e356df66feaf474091e32aad092a696fa65bf
Alessio Franceschelli <alessio@franceschelli.me>
2019-03-10 20:27:10 +0000

    Set comparer for the dictionary of headers

--- src/Middleware/HeaderPropagation/src/HeaderPropagationState.cs
+++ src/Middleware/HeaderPropagation/src/HeaderPropagationState.cs
@@ -1,21 +1,28 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using Microsoft.Extensions.Primitives;
 
 namespace Microsoft.AspNetCore.HeaderPropagation
 {
+    /// <summary>
+    /// Contains the headers values for the <see cref="HeaderPropagationMiddleware"/>.
+    /// </summary>
     public class HeaderPropagationState
     {
-        private static AsyncLocal<Dictionary<string, StringValues>> _headers { get; } = new AsyncLocal<Dictionary<string, StringValues>>();
+        private static AsyncLocal<Dictionary<string, StringValues>> _headers = new AsyncLocal<Dictionary<string, StringValues>>();
 
+        /// <summary>
+        /// Gets the headers values from the current request that can be propagated.
+        /// </summary>
         public Dictionary<string, StringValues> Headers
         {
             get
             {
-                return _headers.Value ?? (_headers.Value = new Dictionary<string, StringValues>());
+                return _headers.Value ?? (_headers.Value = new Dictionary<string, StringValues>(StringComparer.OrdinalIgnoreCase));
             }
         }
     }
