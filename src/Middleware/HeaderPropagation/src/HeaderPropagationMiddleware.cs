91a49709dae0066b1e8c9cbd7ef3dd6a7d657ceb
Alessio Franceschelli <alessio@franceschelli.me>
2019-03-10 13:22:58 +0000

    Refactored middleware logic

--- src/Middleware/HeaderPropagation/src/HeaderPropagationMiddleware.cs
+++ src/Middleware/HeaderPropagation/src/HeaderPropagationMiddleware.cs
@@ -27,34 +27,35 @@ namespace Microsoft.AspNetCore.HeaderPropagation
 
         public Task Invoke(HttpContext context)
         {
-            if (context != null)
+            foreach (var header in _options.Headers)
             {
-                foreach (var header in _options.Headers)
-                {
-                    if (!context.Request.Headers.TryGetValue(header.InputName, out var values)
-                        || StringValues.IsNullOrEmpty(values))
-                    {
-                        if (header.DefaultValuesGenerator != null)
-                        {
-                            values = header.DefaultValuesGenerator(context);
-                            if (StringValues.IsNullOrEmpty(values)) continue;
-                        }
-                        else if (!StringValues.IsNullOrEmpty(header.DefaultValues))
-                        {
-                            values = header.DefaultValues;
-                        }
-                        else
-                        {
-                            continue;
-                        }
-                    }
+                var values = GetValues(header, context);
 
+                if (!StringValues.IsNullOrEmpty(values))
+                {
                     var outputName = !string.IsNullOrEmpty(header.OutputName) ? header.OutputName : header.InputName;
                     _state.Headers.TryAdd(outputName, values);
                 }
             }
 
             return _next.Invoke(context);
+        }
+
+        private static StringValues GetValues(HeaderPropagationEntry header, HttpContext context)
+        {
+            if (context.Request.Headers.TryGetValue(header.InputName, out var values)
+                && !StringValues.IsNullOrEmpty(values))
+            {
+                return values;
+            }
+
+            if (header.DefaultValuesGenerator != null)
+            {
+                values = header.DefaultValuesGenerator(context);
+                if (!StringValues.IsNullOrEmpty(values)) return values;
+            }
+
+            return header.DefaultValues;
         }
     }
 }
