40d771a6721602ff4dc31c6bfcefeece88ccbda4
Stephen Halter <halter73@gmail.com>
2019-03-12 19:44:40 -0700

    Skip Kestrel's InMemory.FunctionalTests on macOS
    - until we figure out what's going on (I hope)

--- src/Servers/Kestrel/test/InMemory.FunctionalTests/Properties/AssemblyInfo.cs
+++ src/Servers/Kestrel/test/InMemory.FunctionalTests/Properties/AssemblyInfo.cs
@@ -1,8 +1,10 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.AspNetCore.Testing.xunit;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Testing;
 
 [assembly: ShortClassName]
 [assembly: LogLevel(LogLevel.Trace)]
+[assembly: OSSkipCondition(OperatingSystems.MacOSX, SkipReason = "https://github.com/aspnet/AspNetCore/issues/8164")]
