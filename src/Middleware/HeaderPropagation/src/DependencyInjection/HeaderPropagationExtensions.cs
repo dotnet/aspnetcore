5af6d22fcf01d853c9926928e34658f1b57e1b6a
Alessio Franceschelli <alessio@franceschelli.me>
2019-03-14 00:02:23 +0000

    Missing docs

--- src/Middleware/HeaderPropagation/src/DependencyInjection/HeaderPropagationExtensions.cs
+++ src/Middleware/HeaderPropagation/src/DependencyInjection/HeaderPropagationExtensions.cs
@@ -57,6 +57,11 @@ namespace Microsoft.AspNetCore.HeaderPropagation
             return services;
         }
 
+        /// <summary>
+        /// Adds a message handler for propagating headers collected by the <see cref="HeaderPropagationMiddleware"/> to a outgoing request.
+        /// </summary>
+        /// <param name="builder">The <see cref="IHttpClientBuilder"/> to add the message handler to.</param>
+        /// <returns>The <see cref="IHttpClientBuilder"/> so that additional calls can be chained.</returns>
         public static IHttpClientBuilder AddHeaderPropagation(this IHttpClientBuilder builder)
         {
             builder.Services.TryAddSingleton<HeaderPropagationValues>();
--- src/Middleware/HeaderPropagation/src/HeaderPropagationMessageHandler.cs
+++ src/Middleware/HeaderPropagation/src/HeaderPropagationMessageHandler.cs
@@ -10,12 +10,15 @@ using Microsoft.Extensions.Primitives;
 
 namespace Microsoft.AspNetCore.HeaderPropagation
 {
+    /// <summary>
+    /// A message handler for propagating headers collected by the <see cref="HeaderPropagationMiddleware"/> to a outgoing request.
+    /// </summary>
     public class HeaderPropagationMessageHandler : DelegatingHandler
     {
-        private readonly HeaderPropagationValues _state;
+        private readonly HeaderPropagationValues _values;
         private readonly HeaderPropagationOptions _options;
 
-        public HeaderPropagationMessageHandler(IOptions<HeaderPropagationOptions> options, HeaderPropagationValues state)
+        public HeaderPropagationMessageHandler(IOptions<HeaderPropagationOptions> options, HeaderPropagationValues values)
         {
             if (options == null)
             {
@@ -24,21 +27,20 @@ namespace Microsoft.AspNetCore.HeaderPropagation
 
             _options = options.Value;
 
-            _state = state ?? throw new ArgumentNullException(nameof(state));
+            _values = values ?? throw new ArgumentNullException(nameof(values));
         }
 
-        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
-            CancellationToken cancellationToken)
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             foreach ((var header, var entry) in _options.Headers)
             {
                 var outputName = !string.IsNullOrEmpty(entry.OutboundHeaderName) ? entry.OutboundHeaderName : header;
 
-                if (_state.Headers.TryGetValue(header, out var values) &&
+                if (_values.Headers.TryGetValue(header, out var values) &&
                     !StringValues.IsNullOrEmpty(values) &&
                     (entry.AlwaysAdd || !request.Headers.Contains(outputName)))
                 {
-                    request.Headers.TryAddWithoutValidation(outputName, (string[]) values);
+                    request.Headers.TryAddWithoutValidation(outputName, (string[])values);
                 }
             }
 
