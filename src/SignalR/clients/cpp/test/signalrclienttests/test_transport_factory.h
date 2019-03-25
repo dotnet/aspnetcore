e091fdee351ee24495e12e8619da29db115bd515
Brennan Conroy <brecon@microsoft.com>
2019-03-25 09:48:37 -0700

    fix rebase

--- src/SignalR/clients/cpp/test/signalrclienttests/test_transport_factory.h
+++ src/SignalR/clients/cpp/test/signalrclienttests/test_transport_factory.h
@@ -14,9 +14,7 @@ public:
     test_transport_factory(const std::shared_ptr<websocket_client>& websocket_client);
 
     std::shared_ptr<transport> create_transport(transport_type transport_type, const logger& logger,
-        const signalr_client_config& signalr_client_config,
-        std::function<void(const std::string&)> process_message_callback,
-        std::function<void(const std::exception&)> error_callback) override;
+        const signalr_client_config& signalr_client_config) override;
 
 private:
     std::shared_ptr<websocket_client> m_websocket_client;
