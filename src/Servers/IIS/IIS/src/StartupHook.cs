2054469e227ac91515b70cdb8e96b236a6efbe63
Justin Kotalik <jukotali@microsoft.com>
2019-03-25 08:58:08 -0700

    small nits

--- src/Servers/IIS/IIS/src/StartupHook.cs
+++ src/Servers/IIS/IIS/src/StartupHook.cs
@@ -6,66 +6,70 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Server.IIS;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Primitives;
 using Microsoft.Extensions.StackTrace.Sources;
 
 internal class StartupHook
 {
     public static void Initialize()
     {
-        Console.WriteLine("test");
+        // TODO make this unhandled exception
         AppDomain.CurrentDomain.FirstChanceException += (sender, eventArgs) =>
         {
             var builder = new StringBuilder();
             var exception = eventArgs.Exception;
+
             // Pinvoke for content root.
+            var iisConfigData = NativeMethods.HttpGetApplicationProperties();
+            var contentRoot = iisConfigData.pwzFullApplicationPath.TrimEnd(Path.DirectorySeparatorChar);
+
             var exceptionDetailProvider = new ExceptionDetailsProvider(
-               new PhysicalFileProvider(), // TODO get content root here
+               new PhysicalFileProvider(contentRoot),
                sourceCodeLineCount: 6);
 
             var details = exceptionDetailProvider.GetDetails(exception);
-            var detail = details.First();
 
             builder.Append("<h1>An error occurred while starting the application.</h1></br>");
-            builder.Append($"<div class=\"titleerror\">{HtmlEncoder.Default.Encode(exception.GetType().ToString())}: {HtmlEncodeAndReplaceLineBreaks(exception.Message)}");
-
-            var firstFrame = detail.StackFrames.FirstOrDefault();
-            var location = string.Empty;
-            if (firstFrame != null)
+            foreach (var detail in details)
             {
-                location = firstFrame.Function;
-            }
-
-            if (!string.IsNullOrEmpty(location) && firstFrame != null && !string.IsNullOrEmpty(firstFrame.File))
-            {
-                builder.Append($"<p class=\"location\">{HtmlEncoder.Default.Encode(location)} in <code title=\"{HtmlEncoder.Default.Encode(firstFrame.File)}\">{HtmlEncoder.Default.Encode(Path.GetFileName(firstFrame.File))}</code>, line {firstFrame.Line}</p>");
-            }
-            else if (!string.IsNullOrEmpty(location))
-            {
-                builder.Append($"<p class=\"location\">{HtmlEncoder.Default.Encode(location)}</p>");
-            }
-            else
-            {
-                builder.Append($"<p class=\"location\">@Resources.ErrorPageHtml_UnknownLocation</p>");
-            }
-
-            var reflectionTypeLoadException = detail.Error as ReflectionTypeLoadException;
-            if (reflectionTypeLoadException != null)
-            {
-                if (reflectionTypeLoadException.LoaderExceptions.Length > 0)
+                var firstFrame = detail.StackFrames.FirstOrDefault();
+                var location = string.Empty;
+                if (firstFrame != null)
                 {
-                    builder.Append("<h3>Loader Exceptions:</h3></br>");
-                    builder.Append("<ul>");
+                    location = firstFrame.Function;
+                }
 
-                    foreach (var ex in reflectionTypeLoadException.LoaderExceptions)
+                if (!string.IsNullOrEmpty(location) && firstFrame != null && !string.IsNullOrEmpty(firstFrame.File))
+                {
+                    builder.Append($"<p class=\"location\">{HtmlEncoder.Default.Encode(location)} in <code title=\"{HtmlEncoder.Default.Encode(firstFrame.File)}\">{HtmlEncoder.Default.Encode(Path.GetFileName(firstFrame.File))}</code>, line {firstFrame.Line}</p>");
+                }
+                else if (!string.IsNullOrEmpty(location))
+                {
+                    builder.Append($"<p class=\"location\">{HtmlEncoder.Default.Encode(location)}</p>");
+                }
+                else
+                {
+                    builder.Append($"<p class=\"location\">@Resources.ErrorPageHtml_UnknownLocation</p>");
+                }
+
+                var reflectionTypeLoadException = detail.Error as ReflectionTypeLoadException;
+                if (reflectionTypeLoadException != null)
+                {
+                    if (reflectionTypeLoadException.LoaderExceptions.Length > 0)
                     {
-                        builder.Append($"<li>{HtmlEncoder.Default.Encode(ex.Message)}</li>");
+                        builder.Append("<h3>Loader Exceptions:</h3></br>");
+                        builder.Append("<ul>");
+
+                        foreach (var ex in reflectionTypeLoadException.LoaderExceptions)
+                        {
+                            builder.Append($"<li>{HtmlEncoder.Default.Encode(ex.Message)}</li>");
+                        }
+                        builder.Append("</ul>");
                     }
-                    builder.Append("</ul>");
                 }
             }
 
@@ -111,7 +115,7 @@ internal class StartupHook
                             builder.Append("</ol>");
                         }
                         builder.Append($"<ol start=\"{frame.Line}\" class=\"highlight\">");
-                        foreach(var line in frame.ContextCode)
+                        foreach (var line in frame.ContextCode)
                         {
                             builder.Append($"<li><span>{HtmlEncoder.Default.Encode(line)}</span></li>");
                         }
@@ -145,12 +149,25 @@ internal class StartupHook
                 builder.Append($"</div>");
                 builder.Append($"</li>");
             }
+
             builder.Append("</ul>");
             builder.Append($"</div>");
 
-
             builder.Append("<footer>");
+
+            var runtimeDisplayName = HtmlEncoder.Default.Encode(RuntimeInformation.FrameworkDescription);
+            var runtimeArchitecture = HtmlEncoder.Default.Encode(RuntimeInformation.ProcessArchitecture.ToString());
+            var currentAssemblyVersion = HtmlEncoder.Default.Encode(RuntimeInformation.ProcessArchitecture.ToString());
+            var systemRuntimeAssembly = typeof(System.ComponentModel.DefaultValueAttribute).GetTypeInfo().Assembly;
+            var clrVersion = HtmlEncoder.Default.Encode(new AssemblyName(systemRuntimeAssembly.FullName).Version.ToString());
+            var operatingSystemDescription = HtmlEncoder.Default.Encode(RuntimeInformation.OSDescription);
+
+            builder.Append($"{runtimeDisplayName} {runtimeArchitecture} v{clrVersion} &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Microsoft.AspNetCore.Server.IIS version " +
+                $"{currentAssemblyVersion} &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; {operatingSystemDescription} &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; " +
+                $"<a href=\"{HtmlEncoder.Default.Encode("http://go.microsoft.com/fwlink/?LinkId=517394")}\">Need help?</a>");
+
             builder.Append("</footer>");
+
             NativeMethods.HttpSetStartupErrorPageContent(builder.ToString());
         };
     }
@@ -162,7 +179,6 @@ internal class StartupHook
             return string.Empty;
         }
 
-        // This is horrible, but I don't want to call linq
         var list = new List<string>();
         var arr = input.Split(new[] { "\r\n" }, StringSplitOptions.None);
         foreach (var a in arr)
@@ -172,68 +188,5 @@ internal class StartupHook
         }
 
         return string.Join("<br />" + Environment.NewLine, list);
-    }
-
-    public class PhysicalFileProvider : IFileProvider
-    {
-        public IDirectoryContents GetDirectoryContents(string subpath)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IFileInfo GetFileInfo(string contentRoot)
-        {
-            var fileInfo = new FileInfo(contentRoot);
-            return new PhysicalFileInfo(fileInfo);
-        }
-
-
-        public IChangeToken Watch(string filter)
-        {
-            throw new NotImplementedException();
-        }
-
-        private class PhysicalFileInfo : IFileInfo
-        {
-            private readonly FileInfo _info;
-
-            public PhysicalFileInfo(FileInfo info)
-            {
-                _info = info;
-            }
-
-            /// <inheritdoc />
-            public bool Exists => _info.Exists;
-
-            /// <inheritdoc />
-            public long Length => _info.Length;
-
-            /// <inheritdoc />
-            public string PhysicalPath => _info.FullName;
-
-            /// <inheritdoc />
-            public string Name => _info.Name;
-
-            /// <inheritdoc />
-            public DateTimeOffset LastModified => _info.LastWriteTimeUtc;
-
-            /// <summary>
-            /// Always false.
-            /// </summary>
-            public bool IsDirectory => false;
-            public Stream CreateReadStream()
-            {
-                // We are setting buffer size to 1 to prevent FileStream from allocating it's internal buffer
-                // 0 causes constructor to throw
-                var bufferSize = 1;
-                return new FileStream(
-                    PhysicalPath,
-                    FileMode.Open,
-                    FileAccess.Read,
-                    FileShare.ReadWrite,
-                    bufferSize,
-                    FileOptions.Asynchronous | FileOptions.SequentialScan);
-            }
-        }
     }
 }
