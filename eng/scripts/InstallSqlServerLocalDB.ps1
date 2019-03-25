7a5981131cd49df4fa9d9a1622136389da443ef6
Doug Bunting <dougbu@microsoft.com>
2019-03-25 12:40:41 -0700

    Drop back to SQL Server 2016 Express LocalDB

--- eng/scripts/InstallSqlServerLocalDB.ps1
+++ eng/scripts/InstallSqlServerLocalDB.ps1
@@ -1,11 +1,11 @@
 <#
 .SYNOPSIS
-    Installs SQL Server 2017 Express LocalDB on a machine.
+    Installs SQL Server 2016 Express LocalDB on a machine.
 .DESCRIPTION
     This script installs Microsoft SQL Server 2016 Express LocalDB on a machine.
 .LINK
-    https://docs.microsoft.com/en-us/sql/database-engine/configure-windows/sql-server-2016-express-localdb?view=sql-server-2017
-    https://docs.microsoft.com/en-us/sql/database-engine/install-windows/install-sql-server-from-the-command-prompt?view=sql-server-2017
+    https://docs.microsoft.com/en-us/sql/database-engine/configure-windows/sql-server-2016-express-localdb?view=sql-server-2016
+    https://docs.microsoft.com/en-us/sql/database-engine/install-windows/install-sql-server-from-the-command-prompt?view=sql-server-2016
 #>
 
 $ErrorActionPreference = 'Stop'
@@ -17,13 +17,13 @@ mkdir $intermedateDir -ErrorAction Ignore | Out-Null
 $bootstrapper = "$intermedateDir\SQLExpressInstaller.exe"
 $ProgressPreference = 'SilentlyContinue' # Workaround PowerShell/PowerShell#2138
 
-Write-Host "Installing SQL Server 2017 Express LocalDB" -f Magenta
+Write-Host "Installing SQL Server 2016 Express LocalDB" -f Magenta
 
 # Get the bootstrapper.
 Write-Host ""
-Write-Host "Downloading 'SQLServer2017-SSEI-Expr.exe' to '$bootstrapper'."
+Write-Host "Downloading 'SQLServer2016-SSEI-Expr.exe' to '$bootstrapper'."
 Invoke-WebRequest -OutFile $bootstrapper -Uri `
-  "https://download.microsoft.com/download/5/E/9/5E9B18CC-8FD5-467E-B5BF-BADE39C51F73/SQLServer2017-SSEI-Expr.exe"
+  "https://download.microsoft.com/download/3/7/6/3767D272-76A1-4F31-8849-260BD37924E4/SQLServer2016-SSEI-Expr.exe"
 
 # Download SqlLocalDB.msi.
 Write-Host ""
