f7e9389783040bf823c70d95bacf1312c8b200d2
Justin Kotalik <jukotali@microsoft.com>
2019-03-26 12:27:07 -0700

    missed file

--- src/Tools/dotnet-watch/test/NoDepsAppTests.cs
+++ src/Tools/dotnet-watch/test/NoDepsAppTests.cs
@@ -5,6 +5,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Testing;
 using Microsoft.AspNetCore.Testing.xunit;
 using Xunit;
 using Xunit.Abstractions;
