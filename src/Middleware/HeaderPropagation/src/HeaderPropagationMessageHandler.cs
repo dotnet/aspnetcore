adab2b907b57fe236f06db3989423b1c1dc37c28
Alessio Franceschelli <alessio@franceschelli.me>
2019-03-27 23:52:40 +0000

    Added docs for HeaderPropagationMessageHandler

--- src/Middleware/HeaderPropagation/src/HeaderPropagationMessageHandler.cs
+++ src/Middleware/HeaderPropagation/src/HeaderPropagationMessageHandler.cs
@@ -18,6 +18,12 @@ namespace Microsoft.AspNetCore.HeaderPropagation
         private readonly HeaderPropagationValues _values;
         private readonly HeaderPropagationOptions _options;
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="HeaderPropagationMessageHandler"/>.
+        /// </summary>
+        /// <param name="options">The options that define which headers are propagated.</param>
+        /// <param name="values">The values of the headers to be propagated populated by the
+        /// <see cref="HeaderPropagationMiddleware"/>.</param>
         public HeaderPropagationMessageHandler(IOptions<HeaderPropagationOptions> options, HeaderPropagationValues values)
         {
             if (options == null)
@@ -30,6 +36,17 @@ namespace Microsoft.AspNetCore.HeaderPropagation
             _values = values ?? throw new ArgumentNullException(nameof(values));
         }
 
+        /// <summary>
+        /// Sends an HTTP request to the inner handler to send to the server as an asynchronous operation, after adding
+        /// the propagated headers.
+        /// </summary>
+        /// <remarks>
+        /// If an header with the same name is already present in the request, even if empty, the corresponding
+        /// propagated header will not be added.
+        /// </remarks>
+        /// <param name="request">The HTTP request message to send to the server.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel operation.</param>
+        /// <returns>The task object representing the asynchronous operation.</returns>
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             foreach ((var headerName, var entry) in _options.Headers)
