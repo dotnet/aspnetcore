555691b600bda99b2dc85ba4ba8bddcf0fc3f545
David Fowler <davidfowl@gmail.com>
2019-03-29 09:00:05 -0700

    PR nits

--- src/SignalR/common/Http.Connections/src/NegotiateMetadata.cs
+++ src/SignalR/common/Http.Connections/src/NegotiateMetadata.cs
@@ -1,3 +1,6 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -5,7 +8,7 @@ using System.Text;
 namespace Microsoft.AspNetCore.Http.Connections
 {
     /// <summary>
-    /// Metadata to identify the /negotiate endpoint for HTTP dispatcher based
+    /// Metadata to identify the /negotiate endpoint for HTTP connections
     /// </summary>
     public class NegotiateMetadata
     {
