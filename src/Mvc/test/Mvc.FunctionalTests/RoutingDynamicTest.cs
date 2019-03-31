abe960bef12a50daf9ab36dbfdd4178045e4d64f
Ryan Nowak <rynowak@microsoft.com>
2019-03-31 11:59:46 -0700

    Add dynamic controller/page routes
    Adds infrastructure for a common IRouter-based pattern. In this pattern,
    an extender subclasses Route to post-process the route values before MVC
    action selection run. The new infrastructure duplicates this kind of
    experience but based on endpoint routing.
    
    The approach in this PR starts at the bottom... meaning that this is the
    most-focused and least-invasive way to implement a feature like this.
    Similar to fallback routing, this is a pattern built with matcher
    policies and metadata rather than a built-in feature of routing.
    
    It's valuable to point out that this approach uses IActionConstraint to
    disambiguate between actions. The other way we could go would be to make
    the *other* matcher policy implementations able to do this. This would
    mean that whenever you have a dynamic endpoint, you will not by using
    the DFA for features like HTTP methods. It also means that we need to go
    re-implement a bunch of infrastructure.
    
    This PR also adds the concept of an 'inert' endpoint - a non-Routable
    endpoint that's created when fallback/dynamic is in use. This seems like
    a cleaner design because we don't start *matching* RouteEndpoint
    instances for URLs that don't match. This resolves #8130

--- src/Mvc/test/Mvc.FunctionalTests/RoutingDynamicTest.cs
+++ src/Mvc/test/Mvc.FunctionalTests/RoutingDynamicTest.cs
@@ -10,23 +10,23 @@ using Xunit;
 
 namespace Microsoft.AspNetCore.Mvc.FunctionalTests
 {
-    public class RoutingFallbackTest : IClassFixture<MvcTestFixture<RoutingWebSite.StartupForFallback>>
+    public class RoutingDynamicTest : IClassFixture<MvcTestFixture<RoutingWebSite.StartupForDynamic>>
     {
-        public RoutingFallbackTest(MvcTestFixture<RoutingWebSite.StartupForFallback> fixture)
+        public RoutingDynamicTest(MvcTestFixture<RoutingWebSite.StartupForDynamic> fixture)
         {
             var factory = fixture.Factories.FirstOrDefault() ?? fixture.WithWebHostBuilder(ConfigureWebHostBuilder);
             Client = factory.CreateDefaultClient();
         }
 
-        private static void ConfigureWebHostBuilder(IWebHostBuilder builder) => builder.UseStartup<RoutingWebSite.StartupForFallback>();
+        private static void ConfigureWebHostBuilder(IWebHostBuilder builder) => builder.UseStartup<RoutingWebSite.StartupForDynamic>();
 
         public HttpClient Client { get; }
 
         [Fact]
-        public async Task Fallback_CanGet404ForMissingFile()
+        public async Task DynamicController_CanGet404ForMissingAction()
         {
             // Arrange
-            var url = "http://localhost/pranav.jpg";
+            var url = "http://localhost/dynamic/controller%3DFake,action%3DIndex";
             var request = new HttpRequestMessage(HttpMethod.Get, url);
 
             // Act
@@ -38,10 +38,25 @@ namespace Microsoft.AspNetCore.Mvc.FunctionalTests
         }
 
         [Fact]
-        public async Task Fallback_CanAccessKnownEndpoint()
+        public async Task DynamicPage_CanGet404ForMissingAction()
         {
             // Arrange
-            var url = "http://localhost/Edit/17";
+            var url = "http://localhost/dynamicpage/page%3D%2FFake";
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+            // Act
+            var response = await Client.SendAsync(request);
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task DynamicController_CanSelectControllerInArea()
+        {
+            // Arrange
+            var url = "http://localhost/dynamic/area%3Dadmin,controller%3Ddynamic,action%3Dindex";
             var request = new HttpRequestMessage(HttpMethod.Get, url);
 
             // Act
@@ -50,30 +65,14 @@ namespace Microsoft.AspNetCore.Mvc.FunctionalTests
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.Equal("Hello from Edit page", content.Trim());
+            Assert.Equal("Hello from dynamic controller: /link_generation/dynamic/index", content);
         }
 
         [Fact]
-        public async Task Fallback_CanFallbackToControllerInArea()
+        public async Task DynamicController_CanSelectControllerInArea_WithActionConstraints()
         {
             // Arrange
-            var url = "http://localhost/Admin/Foo";
-            var request = new HttpRequestMessage(HttpMethod.Get, url);
-
-            // Act
-            var response = await Client.SendAsync(request);
-            var content = await response.Content.ReadAsStringAsync();
-
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.Equal("Hello from fallback controller: /link_generation/Admin/Fallback/Index", content);
-        }
-
-        [Fact]
-        public async Task Fallback_CanFallbackToControllerInArea_WithActionConstraints()
-        {
-            // Arrange
-            var url = "http://localhost/Admin/Foo";
+            var url = "http://localhost/dynamic/area%3Dadmin,controller%3Ddynamic,action%3Dindex";
             var request = new HttpRequestMessage(HttpMethod.Post, url);
 
             // Act
@@ -82,30 +81,14 @@ namespace Microsoft.AspNetCore.Mvc.FunctionalTests
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.Equal("Hello from fallback controller POST: /link_generation/Admin/Fallback/Index", content);
+            Assert.Equal("Hello from dynamic controller POST: /link_generation/dynamic/index", content);
         }
 
         [Fact]
-        public async Task Fallback_CanFallbackToControllerInAreaPost()
+        public async Task DynamicPage_CanSelectPage()
         {
             // Arrange
-            var url = "http://localhost/Admin/Foo";
-            var request = new HttpRequestMessage(HttpMethod.Post, url);
-
-            // Act
-            var response = await Client.SendAsync(request);
-            var content = await response.Content.ReadAsStringAsync();
-
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.Equal("Hello from fallback controller POST: /link_generation/Admin/Fallback/Index", content);
-        }
-
-        [Fact]
-        public async Task Fallback_CanFallbackToPage()
-        {
-            // Arrange
-            var url = "http://localhost/Foo";
+            var url = "http://localhost/dynamicpage/page%3D%2FDynamicPage";
             var request = new HttpRequestMessage(HttpMethod.Get, url);
 
             // Act
@@ -114,7 +97,7 @@ namespace Microsoft.AspNetCore.Mvc.FunctionalTests
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.Equal("Hello from fallback page: /FallbackPage", content);
+            Assert.Equal("Hello from dynamic page: /DynamicPage", content);
         }
     }
 }
--- src/Mvc/test/WebSites/RoutingWebSite/Areas/Admin/DynamicController.cs
+++ src/Mvc/test/WebSites/RoutingWebSite/Areas/Admin/DynamicController.cs
@@ -6,17 +6,17 @@ using Microsoft.AspNetCore.Mvc;
 namespace RoutingWebSite.Areas.Admin
 {
     [Area("Admin")]
-    public class FallbackController : Controller
+    public class DynamicController : Controller
     {
         public ActionResult Index()
         {
-            return Content("Hello from fallback controller: " + Url.Action());
+            return Content("Hello from dynamic controller: " + Url.Action());
         }
 
         [HttpPost]
         public ActionResult Index(int x = 0)
         {
-            return Content("Hello from fallback controller POST: " + Url.Action());
+            return Content("Hello from dynamic controller POST: " + Url.Action());
         }
     }
 }
