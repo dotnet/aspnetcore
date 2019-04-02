fe67484ae8c0bb172c4569012fc7d11e0a9a64f3
Artak Mkrtchyan <artakm@corp.microsoft.com>
2019-04-02 14:27:03 -0700

    Remove unused `using`-s from Program and Startup classes

--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/Angular-CSharp/Program.cs
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/Angular-CSharp/Program.cs
@@ -1,12 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace Company.WebApplication1
 {
