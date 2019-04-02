ffca724e6571713eed070a46cf53fe533095ce38
David Fowler <davidfowl@gmail.com>
2019-04-01 22:22:55 -0700

    More doc comments and PR feedback

--- src/Middleware/Diagnostics.Abstractions/src/IDeveloperPageExceptionFilter.cs
+++ src/Middleware/Diagnostics.Abstractions/src/IDeveloperPageExceptionFilter.cs
@@ -1,3 +1,6 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,8 +16,8 @@ namespace Microsoft.AspNetCore.Diagnostics
         /// <summary>
         /// An exception handling method that is used to either format the exception or delegate to the next handler in the chain.
         /// </summary>
-        /// <param name="errorContext">The error context</param>
-        /// <param name="next">The next filter in the pipeline</param>
+        /// <param name="errorContext">The error context.</param>
+        /// <param name="next">The next filter in the pipeline.</param>
         /// <returns>A task the completes when the handler is done executing.</returns>
         Task HandleExceptionAsync(ErrorContext errorContext, Func<ErrorContext, Task> next);
     }
