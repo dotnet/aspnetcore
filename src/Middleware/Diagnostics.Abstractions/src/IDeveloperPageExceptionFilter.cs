8a1a164bba06a99ec0320ce2c6fd04652f944837
David Fowler <davidfowl@gmail.com>
2019-04-01 20:59:36 -0700

    Updated docs a bit

--- src/Middleware/Diagnostics.Abstractions/src/IDeveloperPageExceptionFilter.cs
+++ src/Middleware/Diagnostics.Abstractions/src/IDeveloperPageExceptionFilter.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 namespace Microsoft.AspNetCore.Diagnostics
 {
     /// <summary>
-    /// Provides an extensiblity point for changing the behavior of the DeveloperExceptionPageMiddleware />.
+    /// Provides an extensiblity point for changing the behavior of the DeveloperExceptionPageMiddleware.
     /// </summary>
     public interface IDeveloperPageExceptionFilter
     {
         /// <summary>
-        /// An exception handling method.
+        /// An exception handling method that is used to either format the exception or delegate to the next handler in the chain.
         /// </summary>
         /// <param name="errorContext">The error context</param>
         /// <param name="next">The next filter in the pipeline</param>
