6be7bea82d68bbf1c027152aeb3e84608389879c
Artak <34246760+mkArtakMSFT@users.noreply.github.com>
2019-04-02 19:14:11 -0700

    Update Startup.cs

--- src/ProjectTemplates/Web.ProjectTemplates/content/WebApi-CSharp/Startup.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/WebApi-CSharp/Startup.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;	
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 #if (RequiresHttps)
 using Microsoft.AspNetCore.HttpsPolicy;
