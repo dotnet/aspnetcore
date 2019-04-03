ac61394240ac8c401d56081a11c2170c95ff77d3
Artak <34246760+mkArtakMSFT@users.noreply.github.com>
2019-04-02 19:13:24 -0700

    Added missing usings

--- src/ProjectTemplates/Web.ProjectTemplates/content/WebApi-CSharp/Startup.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/WebApi-CSharp/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;	
+using Microsoft.AspNetCore.Hosting;
 #if (RequiresHttps)
 using Microsoft.AspNetCore.HttpsPolicy;
 #endif
