8f3c44f203d3acc81bbe048aada8fca09f8a294b
Artak Mkrtchyan <artakm@corp.microsoft.com>
2019-04-03 20:46:38 -0700

    Added back the important usings

--- src/ProjectTemplates/Web.ProjectTemplates/content/EmptyWeb-CSharp/Program.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/EmptyWeb-CSharp/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Company.WebApplication1
 {
