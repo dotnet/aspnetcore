c4eedb7552ab3a93728e02baea3ea41ac12b486c
Justin Kotalik <jukotali@microsoft.com>
2019-04-04 14:43:20 -0700

    Feedback:

--- src/Components/test/E2ETest/Tests/ComponentRenderingTest.cs
+++ src/Components/test/E2ETest/Tests/ComponentRenderingTest.cs
@@ -12,6 +12,7 @@ using BasicTestApp.HierarchicalImportsTest.Subdir;
 using Microsoft.AspNetCore.Components.E2ETest.Infrastructure;
 using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
 using Microsoft.AspNetCore.E2ETesting;
+using Microsoft.AspNetCore.Testing.xunit;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using Xunit;
