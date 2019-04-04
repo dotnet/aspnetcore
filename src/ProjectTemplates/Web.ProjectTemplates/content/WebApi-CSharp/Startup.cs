d27ce10629abb5546b4ed42074b26c2b18d33ce7
Artak Mkrtchyan <artakm@corp.microsoft.com>
2019-04-03 20:46:38 -0700

    Added back the important usings

--- src/ProjectTemplates/Web.ProjectTemplates/content/WebApi-CSharp/Startup.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/WebApi-CSharp/Startup.cs
@@ -20,6 +20,7 @@ using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Company.WebApplication1
 {
--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/Angular-CSharp/Program.cs
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/Angular-CSharp/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace Company.WebApplication1
 {
