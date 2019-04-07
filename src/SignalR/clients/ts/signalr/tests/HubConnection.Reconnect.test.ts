91e97fae5c451806c96ac67c19906f80a51d80a0
Stephen Halter <halter73@gmail.com>
2019-04-06 21:44:12 -0700

    reconnect "can be opted into"

--- src/SignalR/clients/ts/signalr/tests/HubConnection.Reconnect.test.ts
+++ src/SignalR/clients/ts/signalr/tests/HubConnection.Reconnect.test.ts
@@ -34,8 +34,93 @@ describe("auto reconnect", () => {
             await closePromise;
 
             expect(onreconnectingCalled).toBe(false);
-        },
-        "Connection disconnected with error 'Error: WebSocket closed with status code: 0 ().'.");
+        });
+    });
+
+    it("can be opted into", async () => {
+        await VerifyLogger.run(async (logger) => {
+            const reconnectedPromise = new PromiseSource();
+
+            let nextRetryDelayCalledPromise = new PromiseSource();
+            let continueRetryingPromise = new PromiseSource();
+
+            let lastRetryCount = -1;
+            let lastElapsedMs = -1;
+            let onreconnectingCount = 0;
+            let onreconnectedCount = 0;
+            let closeCount = 0;
+
+            const connection = new TestConnection();
+            const hubConnection = HubConnection.create(connection, logger, new JsonHubProtocol(), {
+                    nextRetryDelayInMilliseconds(previousRetryCount: number, elapsedMilliseconds: number) {
+                        lastRetryCount = previousRetryCount;
+                        lastElapsedMs = elapsedMilliseconds;
+                        nextRetryDelayCalledPromise.resolve();
+                        return 0;
+                    },
+                });
+
+            hubConnection.onreconnecting(() => {
+                onreconnectingCount++;
+            });
+
+            hubConnection.onreconnected(() => {
+                onreconnectedCount++;
+                reconnectedPromise.resolve();
+            });
+
+            hubConnection.onclose(() => {
+                closeCount++;
+            });
+
+            await hubConnection.start();
+
+            connection.start = () => {
+                const promise = continueRetryingPromise;
+                continueRetryingPromise = new PromiseSource();
+                return promise;
+            };
+
+            // Typically this would be called by the transport
+            connection.onclose!(new Error("Connection lost"));
+
+            await nextRetryDelayCalledPromise;
+            nextRetryDelayCalledPromise = new PromiseSource();
+
+            expect(lastRetryCount).toBe(0);
+            expect(lastElapsedMs).toBe(0);
+            expect(onreconnectingCount).toBe(1);
+            expect(onreconnectedCount).toBe(0);
+            expect(closeCount).toBe(0);
+
+            // Make sure the the Promise is "handled" immediately upon rejection or else this test fails.
+            continueRetryingPromise.catch(() => { });
+            continueRetryingPromise.reject(new Error("Reconnect attempt failed"));
+            await nextRetryDelayCalledPromise;
+
+            expect(lastRetryCount).toBe(1);
+            expect(lastElapsedMs).toBeGreaterThanOrEqual(0);
+            expect(onreconnectingCount).toBe(1);
+            expect(onreconnectedCount).toBe(0);
+            expect(closeCount).toBe(0);
+
+            continueRetryingPromise.resolve();
+            await reconnectedPromise;
+
+            expect(lastRetryCount).toBe(1);
+            expect(lastElapsedMs).toBeGreaterThanOrEqual(0);
+            expect(onreconnectingCount).toBe(1);
+            expect(onreconnectedCount).toBe(1);
+            expect(closeCount).toBe(0);
+
+            await hubConnection.stop();
+
+            expect(lastRetryCount).toBe(1);
+            expect(lastElapsedMs).toBeGreaterThanOrEqual(0);
+            expect(onreconnectingCount).toBe(1);
+            expect(onreconnectedCount).toBe(1);
+            expect(closeCount).toBe(1);
+        });
     });
 
     // it("can be opted into", async () => {
