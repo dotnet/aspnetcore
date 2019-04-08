de9756645dcb661cd56e07f2eee55e000400f265
Brennan Conroy <brecon@microsoft.com>
2019-04-08 13:49:33 -0700

    ref

--- src/SignalR/common/Protocols.Json/ref/Microsoft.AspNetCore.SignalR.Protocols.Json.netstandard2.0.cs
+++ src/SignalR/common/Protocols.Json/ref/Microsoft.AspNetCore.SignalR.Protocols.Json.netstandard2.0.cs
@@ -1,19 +1,11 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-namespace Microsoft.AspNetCore.SignalR
-{
-    public partial class JsonHubProtocolOptions
-    {
-        public JsonHubProtocolOptions() { }
-    }
-}
 namespace Microsoft.AspNetCore.SignalR.Protocol
 {
-    public partial class JsonHubProtocol : Microsoft.AspNetCore.SignalR.Protocol.IHubProtocol
+    public sealed partial class JsonHubProtocol : Microsoft.AspNetCore.SignalR.Protocol.IHubProtocol
     {
         public JsonHubProtocol() { }
-        public JsonHubProtocol(Microsoft.Extensions.Options.IOptions<Microsoft.AspNetCore.SignalR.JsonHubProtocolOptions> options) { }
         public int MinorVersion { get { throw null; } }
         public string Name { get { throw null; } }
         public Microsoft.AspNetCore.Connections.TransferFormat TransferFormat { get { throw null; } }
@@ -29,6 +21,5 @@ namespace Microsoft.Extensions.DependencyInjection
     public static partial class JsonProtocolDependencyInjectionExtensions
     {
         public static TBuilder AddJsonProtocol<TBuilder>(this TBuilder builder) where TBuilder : Microsoft.AspNetCore.SignalR.ISignalRBuilder { throw null; }
-        public static TBuilder AddJsonProtocol<TBuilder>(this TBuilder builder, System.Action<Microsoft.AspNetCore.SignalR.JsonHubProtocolOptions> configure) where TBuilder : Microsoft.AspNetCore.SignalR.ISignalRBuilder { throw null; }
     }
 }
