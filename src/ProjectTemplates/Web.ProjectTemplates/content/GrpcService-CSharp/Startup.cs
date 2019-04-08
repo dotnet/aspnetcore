67e08728ca2aac4e2b974ae268b46c4295b84057
John Luo <johluo@microsoft.com>
2019-04-07 22:42:38 -0700

    Remove client from gRPC templates (#9078)

--- src/ProjectTemplates/Web.ProjectTemplates/content/GrpcService-CSharp/Startup.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/GrpcService-CSharp/Startup.cs
@@ -30,6 +30,8 @@ namespace GrpcService_CSharp
 
             app.UseEndpoints(endpoints =>
             {
+                // Communication with gRPC endpoints must be made through a gRPC client.
+                // To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909
                 endpoints.MapGrpcService<GreeterService>();
             });
         }
