715da983a6fcebb6cfb0130ad63c565aa47e704b
Hao Kung <HaoK@users.noreply.github.com>
2019-04-15 12:09:32 -0700

    Add using

--- src/Identity/Specification.Tests/src/UserManagerSpecificationTests.cs
+++ src/Identity/Specification.Tests/src/UserManagerSpecificationTests.cs
@@ -9,6 +9,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Testing;
+using Microsoft.AspNetCore.Testing.xunit;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Xunit;
