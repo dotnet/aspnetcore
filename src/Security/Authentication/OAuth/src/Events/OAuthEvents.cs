fca8b606d705e4b7c4840a7fb9c89edddb111189
Menighin <joao.menighin@gmail.com>
2019-04-17 19:11:24 -0300

    Removing unused imports

--- src/Security/Authentication/OAuth/src/Events/OAuthEvents.cs
+++ src/Security/Authentication/OAuth/src/Events/OAuthEvents.cs
@@ -2,10 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.Authentication.OAuth
