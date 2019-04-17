2dc5694ecd1bd95e9091f6942657ebe1da6e0bb7
Ben Adams <thundercat@illyriad.co.uk>
2019-04-17 04:29:47 +0100

    Http1OutputProducer Reset sequentially (#9203)

--- src/Servers/Kestrel/Core/src/Internal/Http/Http1OutputProducer.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http/Http1OutputProducer.cs
@@ -481,10 +481,11 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
         {
             Debug.Assert(_currentSegmentOwner == null);
             Debug.Assert(_completedSegments == null || _completedSegments.Count == 0);
-            _autoChunk = false;
-            _startCalled = false;
-            _currentChunkMemoryUpdated = false;
+            // Cleared in sequential address ascending order 
             _currentMemoryPrefixBytes = 0;
+            _autoChunk = false;
+            _currentChunkMemoryUpdated = false;
+            _startCalled = false;
         }
 
         private ValueTask<FlushResult> WriteAsync(
