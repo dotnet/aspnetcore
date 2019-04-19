7b9da93e8a2bb56222c1edc4ff6a5b926f088552
Pranav K <prkrishn@hotmail.com>
2019-04-19 10:13:18 -0700

    Add a System.Text.Json based IJsonHelper
    Fixes https://github.com/aspnet/AspNetCore/issues/9564

---
+++ src/Mvc/Mvc.ViewFeatures/src/Rendering/SystemTextJsonHelper.cs
@@ -0,0 +1,29 @@
+﻿
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Html;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.AspNetCore.Mvc.Rendering
+{
+    internal class SystemTextJsonHelper : IJsonHelper
+    {
+        private readonly MvcOptions _mvcOptions;
+
+        public SystemTextJsonHelper(IOptions<MvcOptions> mvcOptions)
+        {
+            _mvcOptions = mvcOptions.Value;
+        }
+
+        /// <inheritdoc />
+        public IHtmlContent Serialize(object value)
+        {
+            // JsonSerializer always encodes
+            var json = JsonSerializer.ToString(value, value.GetType(), _mvcOptions.SerializerOptions);
+            return new HtmlString(json);
+        }
+    }
+}
