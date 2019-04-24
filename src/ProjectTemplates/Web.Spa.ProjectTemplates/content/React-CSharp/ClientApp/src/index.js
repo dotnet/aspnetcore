0e6c1b4b7fb26e5e9e1e7bdb5e3818a314ec2585
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-04-24 09:36:41 -0700

    Update index.js

--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/index.js
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/index.js
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 ////#if (IndividualLocalAuth)
-// Uncomment the lines below to register the generated service worker.
-// By default create-react-app includes a service worker to improve the
-// performance of the application by caching static assets. This service
-// worker, can however try and handle paths for the identity UI, so it is
-// disabled by default when Identity is being used.
-//
-//import registerServiceWorker from './registerServiceWorker';
+//// Uncomment the lines below to register the generated service worker.
+//// By default create-react-app includes a service worker to improve the
+//// performance of the application by caching static assets. This service
+//// worker, can however try and handle paths for the identity UI, so it is
+//// disabled by default when Identity is being used.
+////
+////import registerServiceWorker from './registerServiceWorker';
 ////#else
 import registerServiceWorker from './registerServiceWorker';
 ////#endif
@@ -26,13 +26,13 @@ ReactDOM.render(
   rootElement);
 
 ////#if (IndividualLocalAuth)
-// Uncomment the lines below to register the generated service worker.
-// By default create-react-app includes a service worker to improve the
-// performance of the application by caching static assets. This service
-// worker, can however try and handle paths for the identity UI, so it is
-// disabled by default when Identity is being used.
-//
-//registerServiceWorker();
+//// Uncomment the lines below to register the generated service worker.
+//// By default create-react-app includes a service worker to improve the
+//// performance of the application by caching static assets. This service
+//// worker, can however try and handle paths for the identity UI, so it is
+//// disabled by default when Identity is being used.
+////
+////registerServiceWorker();
 ////#else
 registerServiceWorker();
 ////#endif
