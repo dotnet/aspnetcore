31b26927fe19919bdb6cb055e6feefbd6b8de294
Ben Adams <thundercat@illyriad.co.uk>
2019-04-25 00:36:42 +0100

    Feedback (improved CPU cache usage)

--- src/Http/Http.Abstractions/src/Internal/PathStringHelper.cs
+++ src/Http/Http.Abstractions/src/Internal/PathStringHelper.cs
@@ -8,37 +8,11 @@ namespace Microsoft.AspNetCore.Http.Internal
 {
     internal static class PathStringHelper
     {
-        private static readonly bool[] ValidPathChars = {
-            false, false, false, false, false, false, false, false,     // 0x00 - 0x07
-            false, false, false, false, false, false, false, false,     // 0x08 - 0x0F
-            false, false, false, false, false, false, false, false,     // 0x10 - 0x17
-            false, false, false, false, false, false, false, false,     // 0x18 - 0x1F
-            false, true,  false, false, true,  false, true,  true,      // 0x20 - 0x27
-            true,  true,  true,  true,  true,  true,  true,  true,      // 0x28 - 0x2F
-            true,  true,  true,  true,  true,  true,  true,  true,      // 0x30 - 0x37
-            true,  true,  true,  true,  false, true,  false, false,     // 0x38 - 0x3F
-            true,  true,  true,  true,  true,  true,  true,  true,      // 0x40 - 0x47
-            true,  true,  true,  true,  true,  true,  true,  true,      // 0x48 - 0x4F
-            true,  true,  true,  true,  true,  true,  true,  true,      // 0x50 - 0x57
-            true,  true,  true,  false, false, false, false, true,      // 0x58 - 0x5F
-            false, true,  true,  true,  true,  true,  true,  true,      // 0x60 - 0x67
-            true,  true,  true,  true,  true,  true,  true,  true,      // 0x68 - 0x6F
-            true,  true,  true,  true,  true,  true,  true,  true,      // 0x70 - 0x77
-            true,  true,  true,  false, false, false, true,  false,     // 0x78 - 0x7F
-        };
-
-        // Map from an ASCII char to its hex value, e.g. arr['b'] == 11. 0xFF means it's not a hex digit.
-        // From coreclr/src/System.Private.CoreLib/shared/System/Number.Parsing.cs
-        // byte[] rather than ReadOnlySpan<byte> to avoid double .Length check https://github.com/dotnet/coreclr/issues/24209
-        private static readonly byte[] CharToHexLookup = new byte[]
-        {
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // 15
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // 31
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // 47
-            0x0,  0x1,  0x2,  0x3,  0x4,  0x5,  0x6,  0x7,  0x8,  0x9,  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // 63
-            0xFF, 0xA,  0xB,  0xC,  0xD,  0xE,  0xF,  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // 79
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // 95
-            0xFF, 0xa,  0xb,  0xc,  0xd,  0xe,  0xf // 102
+        private static readonly uint[] ValidPathChars = {
+            0b_0000_0000__0000_0000__0000_0000__0000_0000, // 0x00 - 0x1F
+            0b_0010_1111__1111_1111__1111_1111__1101_0010, // 0x20 - 0x3F
+            0b_1000_0111__1111_1111__1111_1111__1111_1111, // 0x40 - 0x5F
+            0b_0100_0111__1111_1111__1111_1111__1111_1110, // 0x60 - 0x7F
         };
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -47,7 +21,15 @@ namespace Microsoft.AspNetCore.Http.Internal
             // Use local array and uint .Length compare to elide the bounds check on array access
             var validChars = ValidPathChars;
             var i = (int)c;
-            return (uint)i < (uint)validChars.Length && validChars[i];
+
+            // Array is in chunks of 32 bits, so get offset by dividing by 32
+            var offset = i / 32;
+            // Significant bit position is the remainder of the above calc; i % 32 => i & 31
+            var significantBit = 1u << (i & 31);
+
+            // Check offset in bounds and check if significant bit set
+            return (uint)offset < (uint)validChars.Length &&
+                ((validChars[offset] & significantBit) == significantBit);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -67,17 +49,16 @@ namespace Microsoft.AspNetCore.Http.Internal
         {
             Debug.Assert(index < str.Length - 2);
 
-            var charToHex = CharToHexLookup;
-            var i1 = (int)str[index + 1];
-            var i2 = (int)str[index + 2];
-            if ((uint)i1 < (uint)charToHex.Length
-                && (uint)i2 < (uint)charToHex.Length)
-            {
-                var tmp = charToHex[i1] | charToHex[i2];
-                return tmp != 0xFF;
-            }
+            var c1 = str[index + 1];
+            var c2 = str[index + 2];
+            return IsHexadecimalChar(c1) && IsHexadecimalChar(c2);
+        }
 
-            return false;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool IsHexadecimalChar(char c)
+        {
+            // Between 0 - 9 or uppercased between A - F
+            return (c - '0') <= 9 || ((c & ~0x20) - 'A') <= ('F' - 'A');
         }
     }
 }
