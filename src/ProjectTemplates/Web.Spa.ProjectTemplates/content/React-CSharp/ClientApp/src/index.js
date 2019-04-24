315936057e539d1bf62ae7453bf58f8e9144cc2c
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-04-24 09:48:49 -0700

    Update comment

--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/index.js
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/index.js
@@ -4,12 +4,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 ////#if (IndividualLocalAuth)
-//// Uncomment the lines below to register the generated service worker.
-//// By default create-react-app includes a service worker to improve the
-//// performance of the application by caching static assets. This service
-//// worker can interfere with the Identity UI, so it is
-//// disabled by default when Identity is being used.
-////
 ////import registerServiceWorker from './registerServiceWorker';
 ////#else
 import registerServiceWorker from './registerServiceWorker';
@@ -26,7 +20,8 @@ ReactDOM.render(
   rootElement);
 
 ////#if (IndividualLocalAuth)
-//// Uncomment the lines below to register the generated service worker.
+//// Uncomment the line above that imports the registerServiceWorker function
+//// and the line below to register the generated service worker.
 //// By default create-react-app includes a service worker to improve the
 //// performance of the application by caching static assets. This service
 //// worker can interfere with the Identity UI, so it is
