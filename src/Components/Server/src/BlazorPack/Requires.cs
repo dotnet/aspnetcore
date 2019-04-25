8055ababaa42a5d39c6f953d5ba587de55f1f996
Pranav K <prkrishn@hotmail.com>
2019-04-25 10:48:31 -0700

    Update MessagePack commit to be inline with upstream changes

---
+++ src/Components/Server/src/BlazorPack/Requires.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+using System;
+using System.Buffers;
+
+namespace Nerdbank.Streams
+{
+    internal static class Requires
+    {
+        internal static void NotNull(object arg, string paramName)
+        {
+            if (arg == null)
+            {
+                throw new ArgumentNullException(nameof(paramName));
+            }
+        }
+
+        internal static void Argument(bool condition, string paramName, string message)
+        {
+            if (condition)
+            {
+                throw new ArgumentException(message, paramName);
+            }
+        }
+
+        internal static void Range(bool condition, string paramName)
+        {
+            if (condition)
+            {
+                throw new ArgumentOutOfRangeException(paramName);
+            }
+        }
+    }
+}
