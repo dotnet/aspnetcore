26685ca5c28bde39d53005775ad1b9d74a8dff6d
Hao Kung <HaoK@users.noreply.github.com>
2019-04-29 12:07:59 -0700

    Undo service extension class rename

--- src/Identity/Core/src/IdentityServiceCollectionExtensions.cs
+++ src/Identity/Core/src/IdentityServiceCollectionExtensions.cs
@@ -12,7 +12,7 @@ namespace Microsoft.Extensions.DependencyInjection
     /// <summary>
     /// Contains extension methods to <see cref="IServiceCollection"/> for configuring identity services.
     /// </summary>
-    public static class CoreIdentityServiceCollectionExtensions
+    public static class IdentityServiceCollectionExtensions
     {
         /// <summary>
         /// Adds the default identity system configuration for the specified User and Role types.
