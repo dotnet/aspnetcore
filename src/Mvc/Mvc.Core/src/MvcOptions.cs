21a822891953bd522bc2553537c56a2fdab8b2da
Ryan Nowak <nowakra@gmail.com>
2019-05-01 12:32:05 -0700

    Update src/Mvc/Mvc.Core/src/MvcOptions.cs
    Co-Authored-By: pranavkm <prkrishn@hotmail.com>

--- src/Mvc/Mvc.Core/src/MvcOptions.cs
+++ src/Mvc/Mvc.Core/src/MvcOptions.cs
@@ -103,7 +103,7 @@ namespace Microsoft.AspNetCore.Mvc
         public FormatterCollection<IInputFormatter> InputFormatters { get; }
 
         /// <summary>
-        /// Gets or sets the flag that determines if buffering is disabled for input formatters that
+        /// Gets or sets a value that determines if buffering is disabled for input formatters that
         /// synchronously read from the HTTP request body.
         /// </summary>
         public bool SuppressInputFormatterBuffering { get; set; }
