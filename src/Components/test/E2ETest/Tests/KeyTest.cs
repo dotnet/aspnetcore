a1b2327f8eb7e825e915f1b2a89d3a0a41405ed4
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-05-01 15:38:03 +0100

    In E2E test, verify correct preservation of components

--- src/Components/test/E2ETest/Tests/KeyTest.cs
+++ src/Components/test/E2ETest/Tests/KeyTest.cs
@@ -41,11 +41,11 @@ namespace Microsoft.AspNetCore.Components.E2ETest.Tests
                 },
                 after: new[]
                 {
-                    new Node("new1", "Inserted before"),
+                    new Node("new1", "Inserted before") { IsNew = true },
                     new Node("orig1", "A"),
-                    new Node("new2", "Inserted between"),
-                    new Node("orig1", "B"),
-                    new Node("new3", "Inserted after"),
+                    new Node("new2", "Inserted between") { IsNew = true },
+                    new Node("orig2", "B"),
+                    new Node("new3", "Inserted after") { IsNew = true },
                 });
         }
 
@@ -62,27 +62,33 @@ namespace Microsoft.AspNetCore.Components.E2ETest.Tests
 
             SetTextAreaValueFast(textbox, jsonBefore);
             updateButton.Click();
-            ValidateRenderedOutput(appElem, rootBefore);
+            ValidateRenderedOutput(appElem, rootBefore, validatePreservation: false);
 
             SetTextAreaValueFast(textbox, jsonAfter);
             updateButton.Click();
-            ValidateRenderedOutput(appElem, rootAfter);
+            ValidateRenderedOutput(appElem, rootAfter, validatePreservation: true);
         }
 
-        private static void ValidateRenderedOutput(IWebElement appElem, Node expectedRootNode)
+        private static void ValidateRenderedOutput(IWebElement appElem, Node expectedRootNode, bool validatePreservation)
         {
             var actualRootElem = appElem.FindElement(By.CssSelector(".render-output > .node"));
             var actualRootNode = ReadNodeFromDOM(actualRootElem);
-            AssertNodesEqual(expectedRootNode, actualRootNode);
+            AssertNodesEqual(expectedRootNode, actualRootNode, validatePreservation);
         }
 
-        private static void AssertNodesEqual(Node expectedRootNode, Node actualRootNode)
+        private static void AssertNodesEqual(Node expectedRootNode, Node actualRootNode, bool validatePreservation)
         {
             Assert.Equal(expectedRootNode.Label, actualRootNode.Label);
+
+            if (validatePreservation)
+            {
+                Assert.Equal(expectedRootNode.IsNew, actualRootNode.IsNew);
+            }
+
             Assert.Collection(
                 actualRootNode.Children,
                 expectedRootNode.Children.Select<Node, Action<Node>>(expectedChild =>
-                    (actualChild => AssertNodesEqual(expectedChild, actualChild))).ToArray());
+                    (actualChild => AssertNodesEqual(expectedChild, actualChild, validatePreservation))).ToArray());
         }
 
         private static Node ReadNodeFromDOM(IWebElement nodeElem)
@@ -90,7 +96,10 @@ namespace Microsoft.AspNetCore.Components.E2ETest.Tests
             var label = nodeElem.FindElement(By.ClassName("label")).Text;
             var childNodes = nodeElem
                 .FindElements(By.XPath("*[@class='children']/*[@class='node']"));
-            return new Node(key: null, label, childNodes.Select(ReadNodeFromDOM).ToArray());
+            return new Node(key: null, label, childNodes.Select(ReadNodeFromDOM).ToArray())
+            {
+                IsNew = nodeElem.FindElement(By.ClassName("is-new")).Text == "true"
+            };
         }
 
         private void SetTextAreaValueFast(IWebElement textAreaElementWithId, string value)
@@ -105,6 +114,7 @@ namespace Microsoft.AspNetCore.Components.E2ETest.Tests
             public string Key { get; }
             public string Label { get; }
             public Node[] Children { get; }
+            public bool IsNew { get; set; }
 
             public Node(string key, string label, params Node[] children)
             {
