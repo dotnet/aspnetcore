77424a6b067776e4c16fe4b997575b8602b2a1ae
Howard Richards <dev@conficient.com>
2019-05-05 14:19:41 +0100

    Amend server-side blazor template WeatherForecast TemperatureF to use formula - fixes #9955 (#9981)

--- src/ProjectTemplates/Web.ProjectTemplates/content/RazorComponentsWeb-CSharp/Data/WeatherForecast.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/RazorComponentsWeb-CSharp/Data/WeatherForecast.cs
@@ -8,7 +8,7 @@ namespace RazorComponentsWeb_CSharp.Data
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF { get; set; }
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
     }
