edd1ff5748bb7f7e51659b85789466c240f3c5dc
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2019-05-09 11:24:53 -0700

    update doc comments

--- src/Hosting/TestHost/src/TestServer.cs
+++ src/Hosting/TestHost/src/TestServer.cs
@@ -78,15 +78,12 @@ namespace Microsoft.AspNetCore.TestHost
         public IFeatureCollection Features { get; }
 
         /// <summary>
-        /// Gets or sets a value that controls whether synchronous IO is allowed for the <see cref="HttpContext.Request"/> and <see cref="HttpContext.Response"/>
+        /// Gets or sets a value that controls whether synchronous IO is allowed for the <see cref="HttpContext.Request"/> and <see cref="HttpContext.Response"/>. The default value is <see langword="false" />.
         /// </summary>
-        /// <remarks>
-        /// Defaults to false.
-        /// </remarks>
         public bool AllowSynchronousIO { get; set; }
 
         /// <summary>
-        /// Gets or sets a value that controls if <see cref="ExecutionContext"/> and <see cref="AsyncLocal{T}"/> values are preserved from the client to the server.
+        /// Gets or sets a value that controls if <see cref="ExecutionContext"/> and <see cref="AsyncLocal{T}"/> values are preserved from the client to the server. The default value is <see langword="false" />.
         /// </summary>
         public bool PreserveExecutionContext { get; set; }
 
