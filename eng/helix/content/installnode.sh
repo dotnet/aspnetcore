5e475997052184ac48265266dd7bfb1bfe02d26c
Hao Kung <HaoK@users.noreply.github.com>
2019-05-09 12:34:13 -0700

    Update installnode.sh

--- eng/helix/content/installnode.sh
+++ eng/helix/content/installnode.sh
@@ -2,12 +2,13 @@
 
 node_version=$1
 platformarch=$2
-output_dir="$HELIX_CORRELATION_PAYLOAD/node"
+output_dir="node"
 url="http://nodejs.org/dist/v$node_version/node-v$node_version-$platformarch.tar.gz"
 tmp="$(mktemp -d -t install-node.XXXXXX)"
 trap "rm -rf $tmp" EXIT
 cd "$tmp"
 curl -Lsfo $(basename $url) "$url"
+mkdir $output_dir
 tar --strip-components 1 -xzf "node-v$node_version-$platformarch.tar.gz" --no-same-owner --directory "$output_dir"
 
 export PATH="$PATH:$output_dir"
