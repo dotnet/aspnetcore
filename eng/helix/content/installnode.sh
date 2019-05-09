88ee824b91ac6ef210a6e1862e46ac5643769cdb
Hao Kung <HaoK@users.noreply.github.com>
2019-05-09 16:05:26 -0700

    Add prints

--- eng/helix/content/installnode.sh
+++ eng/helix/content/installnode.sh
@@ -8,7 +8,9 @@ tmp="$(mktemp -d -t install-node.XXXXXX)"
 trap "rm -rf $tmp" EXIT
 cd "$tmp"
 curl -Lsfo $(basename $url) "$url"
+echo "Installing node from $(basename $url) $url"
 mkdir $output_dir
+echo "Unpacking to $output_dir"
 tar --strip-components 1 -xzf "node-v$node_version-$platformarch.tar.gz" --no-same-owner --directory "$output_dir"
 
 export PATH="$PATH:$output_dir"
