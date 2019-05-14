cfa81b9d9987d5fa336a9b0cfdba69fb0572c60f
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-05-14 10:10:53 +0100

    AuthorizeView: allow 'Authorized' param as friendlier alias for 'ChildContent'

--- src/Components/Components/test/Auth/AuthorizeViewTest.cs
+++ src/Components/Components/test/Auth/AuthorizeViewTest.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Linq;
 using System.Security.Claims;
 using System.Security.Principal;
@@ -57,6 +58,23 @@ namespace Microsoft.AspNetCore.Components
         }
 
         [Fact]
+        public void RendersNothingIfAuthorizedButNoChildContentOrAuthorizedContentProvided()
+        {
+            // Arrange
+            var renderer = new TestRenderer();
+            var rootComponent = WrapInAuthorizeView();
+            rootComponent.AuthenticationState = TestAuthState.AuthenticatedAs("Nellie");
+
+            // Act
+            renderer.AssignRootComponentId(rootComponent);
+            rootComponent.TriggerRender();
+
+            // Assert
+            var diff = renderer.Batches.Single().GetComponentDiffs<AuthorizeView>().Single();
+            Assert.Empty(diff.Edits);
+        }
+
+        [Fact]
         public void RendersChildContentIfAuthorized()
         {
             // Arrange
@@ -81,15 +99,58 @@ namespace Microsoft.AspNetCore.Components
             });
         }
 
+        [Fact]
+        public void RendersAuthorizedContentIfAuthorized()
+        {
+            // Arrange
+            var renderer = new TestRenderer();
+            var rootComponent = WrapInAuthorizeView(
+                authorizedContent: context => builder =>
+                    builder.AddContent(0, $"You are authenticated as {context.User.Identity.Name}"));
+            rootComponent.AuthenticationState = TestAuthState.AuthenticatedAs("Nellie");
+
+            // Act
+            renderer.AssignRootComponentId(rootComponent);
+            rootComponent.TriggerRender();
+
+            // Assert
+            var diff = renderer.Batches.Single().GetComponentDiffs<AuthorizeView>().Single();
+            Assert.Collection(diff.Edits, edit =>
+            {
+                Assert.Equal(RenderTreeEditType.PrependFrame, edit.Type);
+                AssertFrame.Text(
+                    renderer.Batches.Single().ReferenceFrames[edit.ReferenceFrameIndex],
+                    "You are authenticated as Nellie");
+            });
+        }
+
+        [Fact]
+        public void ThrowsIfBothChildContentAndAuthorizedContentProvided()
+        {
+            // Arrange
+            var renderer = new TestRenderer();
+            var rootComponent = WrapInAuthorizeView(
+                authorizedContent: context => builder => { },
+                childContent: context => builder => { });
+
+            // Act/Assert
+            renderer.AssignRootComponentId(rootComponent);
+            var ex = Assert.Throws<InvalidOperationException>(() =>
+                rootComponent.TriggerRender());
+            Assert.Equal("When using AuthorizeView, do not specify both 'Authorized' and 'ChildContent'.", ex.Message);
+        }
+
         private static TestAuthStateProviderComponent WrapInAuthorizeView(
             RenderFragment<IAuthenticationState> childContent = null,
+            RenderFragment<IAuthenticationState> authorizedContent = null,
             RenderFragment notAuthorizedContent = null)
         {
             return new TestAuthStateProviderComponent(builder =>
             {
                 builder.OpenComponent<AuthorizeView>(0);
                 builder.AddAttribute(1, nameof(AuthorizeView.ChildContent), childContent);
-                builder.AddAttribute(2, nameof(AuthorizeView.NotAuthorized), notAuthorizedContent);
+                builder.AddAttribute(2, nameof(AuthorizeView.Authorized), authorizedContent);
+                builder.AddAttribute(3, nameof(AuthorizeView.NotAuthorized), notAuthorizedContent);
                 builder.CloseComponent();
             });
         }
