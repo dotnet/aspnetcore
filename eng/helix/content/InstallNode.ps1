6329f3ef8959e77b2d33ac4e7daa3c4bc15b6a55
Hao Kung <HaoK@users.noreply.github.com>
2019-05-15 14:48:16 -0700

    Add install node windows version

--- eng/helix/content/InstallNode.ps1
+++ eng/helix/content/InstallNode.ps1
@@ -9,6 +9,12 @@ $ProgressPreference = 'SilentlyContinue' # Workaround PowerShell/PowerShell#2138
 
 Set-StrictMode -Version 1
 
+if (Test-Path "%HELIX_CORRELATION_PAYLOAD%\node\bin\node.exe")
+{
+    Write-Host "Node.exe found at %HELIX_CORRELATION_PAYLOAD%\node\bin"
+    exit
+}
+
 $nodeFile="node-v$Version-win-x64"
 $url="http://nodejs.org/dist/v$Version/$nodeFile.zip"
 Write-Host "Starting download of NodeJs ${Version} from $url"
@@ -17,4 +23,5 @@ Write-Host "Done downloading NodeJS ${Version}"
 mkdir node -Force
 Expand-Archive "nodejs.zip" -destination node
 Write-Host "Expanded NodeJs"
-copy node/$nodeFile/node.exe .
+mkdir %HELIX_CORRELATION_PAYLOAD%\node\bin -Force
+copy node/$nodeFile/node.exe %HELIX_CORRELATION_PAYLOAD%\node\bin
