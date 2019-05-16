ec218ef7d07e7f4c09d0ba901db6d37dedd3ad5e
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-05-16 16:36:36 +0100

    Actual E2E tests for cascading authentication state

---
+++ src/Components/test/E2ETest/Tests/AuthTest.cs
@@ -0,0 +1,73 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using BasicTestApp;
+using Microsoft.AspNetCore.Components.E2ETest.Infrastructure;
+using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
+using Microsoft.AspNetCore.E2ETesting;
+using OpenQA.Selenium;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.AspNetCore.Components.E2ETest.Tests
+{
+    public class AuthTest : BasicTestAppTestBase
+    {
+        const string CascadingAuthenticationStateLink = "Cascading authentication state";
+
+        public AuthTest(
+            BrowserFixture browserFixture,
+            ToggleExecutionModeServerFixture<Program> serverFixture,
+            ITestOutputHelper output)
+            : base(browserFixture, serverFixture, output)
+        {
+            // Normally, the E2E tests use the Blazor dev server if they are testing
+            // client-side execution. But for the auth tests, we always have to run
+            // in "hosted on ASP.NET Core" mode, because we get the auth state from it.
+            serverFixture.UseAspNetHost(TestServer.Program.BuildWebHost);
+        }
+
+        [Fact]
+        public void CascadingAuthenticationState_Unauthenticated()
+        {
+            SignInAs(null);
+
+            var appElement = MountAndNavigateToAuthTest(CascadingAuthenticationStateLink);
+
+            Browser.Equal("False", () => appElement.FindElement(By.Id("identity-authenticated")).Text);
+            Browser.Equal(string.Empty, () => appElement.FindElement(By.Id("identity-name")).Text);
+            Browser.Equal("(none)", () => appElement.FindElement(By.Id("test-claim")).Text);
+        }
+
+        [Fact]
+        public void CascadingAuthenticationState_Authenticated()
+        {
+            SignInAs("someone cool");
+
+            var appElement = MountAndNavigateToAuthTest(CascadingAuthenticationStateLink);
+
+            Browser.Equal("True", () => appElement.FindElement(By.Id("identity-authenticated")).Text);
+            Browser.Equal("someone cool", () => appElement.FindElement(By.Id("identity-name")).Text);
+            Browser.Equal("Test claim value", () => appElement.FindElement(By.Id("test-claim")).Text);
+        }
+
+        IWebElement MountAndNavigateToAuthTest(string authLinkText)
+        {
+            Navigate(ServerPathBase);
+            var appElement = MountTestComponent<BasicTestApp.AuthTest.AuthRouter>();
+            WaitUntilExists(By.Id("auth-links"));
+            appElement.FindElement(By.LinkText(authLinkText)).Click();
+            return appElement;
+        }
+
+        void SignInAs(string usernameOrNull)
+        {
+            const string authenticationPageUrl = "/Authentication";
+            var baseRelativeUri = usernameOrNull == null
+                ? $"{authenticationPageUrl}?signout=true"
+                : $"{authenticationPageUrl}?username={usernameOrNull}";
+            Navigate(baseRelativeUri);
+            WaitUntilExists(By.CssSelector("h1#authentication"));
+        }
+    }
+}
