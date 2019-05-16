7fd8b15a27ee6f46c675f9f1b4566bfd381738b0
Nate McMaster <natemcmaster@users.noreply.github.com>
2019-05-16 08:48:53 -0700

    Detect java from registry

--- build.ps1
+++ build.ps1
@@ -303,6 +303,41 @@ $MSBuildArguments += "/p:_RunSign=$Sign"
 $MSBuildArguments += "/p:TargetArchitecture=$Architecture"
 $MSBuildArguments += "/p:TargetOsName=win"
 
+if ($BuildJava) {
+    $foundJdk = $false
+    $javac = Get-Command javac -ErrorAction Ignore
+    if ($env:JAVA_HOME) {
+        if (-not (Test-Path "${env:JAVA_HOME}\bin\javac.exe")) {
+            Write-Error "The env var JAVA_HOME was set, but ${env:JAVA_HOME}\bin\java.exe does not exist. Remove JAVA_HOME or update it to the correct location for the JDK. See https://www.bing.com/search?q=java_home for details."
+        }
+        $foundJdk = $true
+    }
+    elseif ($javac) {
+        $foundJdk = $true
+        $javaHome = Split-Path -Parent (Split-Path -Parent $javac.Path)
+        $env:JAVA_HOME = $javaHome
+        Write-Host -f Magenta "Detected JDK in $javaHome"
+    }
+    else {
+        try {
+            $jdkVersion = (Get-Item HKLM:\SOFTWARE\JavaSoft\JDK | Get-ItemProperty -name CurrentVersion).CurrentVersion
+            $javaHome = (Get-Item HKLM:\SOFTWARE\JavaSoft\JDK\$jdkVersion | Get-ItemProperty -Name JavaHome).JavaHome
+            if (Test-Path "${env:JAVA_HOME}\bin\java.exe") {
+                $env:JAVA_HOME = $javaHome
+                Write-Host -f Magenta "Detected JDK $jdkVersion in $env:JAVA_HOME"
+                $foundJdk = $true
+            }
+        }
+        catch {
+            Write-Verbose "Failed to detect Java: $_"
+        }
+    }
+
+    if (-not $foundJdk) {
+        Write-Error "Could not find java.exe. Install the JDK and put $JAVA_HOME\bin on the system PATH."
+    }
+}
+
 Import-Module -Force -Scope Local (Join-Path $korebuildPath 'KoreBuild.psd1')
 
 try {
