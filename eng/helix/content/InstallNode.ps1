cea48847dc7bd86093400ece0fb6a9fd3c404bb6
Hao Kung <HaoK@users.noreply.github.com>
2019-05-20 16:58:37 -0700

    Add alternative for win7/8 queues that don't have expand-archive

--- eng/helix/content/InstallNode.ps1
+++ eng/helix/content/InstallNode.ps1
@@ -26,7 +26,22 @@ $url="http://nodejs.org/dist/v$Version/$nodeFile.zip"
 Write-Host "Starting download of NodeJs ${Version} from $url"
 Invoke-WebRequest -UseBasicParsing -Uri "$url" -OutFile "nodejs.zip"
 Write-Host "Done downloading NodeJS ${Version}"
-Expand-Archive "nodejs.zip"
+
+if ((Get-Command "Expand-Archive" -ErrorAction SilentlyContinue)) 
+{ 
+    Expand-Archive "nodejs.zip"
+}
+else
+{
+    $shell = new-object -com shell.application
+    $zip = $shell.NameSpace("nodejs.zip")
+    foreach($item in $zip.items())
+    {
+        $shell.Namespace($output_dir).copyhere($item)
+    }
+    Start-Sleep -s 5
+}
+
 Write-Host "Expanded NodeJs"
 mkdir %HELIX_CORRELATION_PAYLOAD%\node\bin -Force
 copy nodejs/$nodeFile/node.exe %HELIX_CORRELATION_PAYLOAD%\node\bin
