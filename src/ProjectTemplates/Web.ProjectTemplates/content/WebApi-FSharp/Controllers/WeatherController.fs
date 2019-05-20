19bf518de102a17b02f8ce6384691812ab5fac27
Isaac Levin <isaac.r.levin@gmail.com>
2019-05-20 10:55:15 -0400

    #10333 Template Updates

---
+++ src/ProjectTemplates/Web.ProjectTemplates/content/WebApi-FSharp/Controllers/WeatherController.fs
@@ -0,0 +1,33 @@
+﻿namespace Company.WebApplication1.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+
+[<Route("api/SampleData/[controller]")>]
+[<ApiController>]
+type WeatherController () =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member this.Get(location:string, unit:TemperatureUnit) =
+        let rnd = System.Random()
+        let result = new WeatherResult (Location = location, Temperature = rnd.Next(-20, 55), TemperatureUnit = unit)
+        ActionResult<WeatherResult>(result)
+
+    [<HttpGet>]
+    member this.Get(location:string) =
+        let rnd = System.Random()
+        let result = new WeatherResult (Location = location, Temperature = rnd.Next(-20, 55), TemperatureUnit = TemperatureUnit.Celsius)
+        ActionResult<WeatherResult>(result)
+
+type TemperatureUnit =
+   | Celsius
+   | Fahrenheit
+
+type WeatherResult =
+    member val Temperature  "" with get, set
+    member val TemperatureUnit "" with get, set
+    member val Location  "" with get, set
