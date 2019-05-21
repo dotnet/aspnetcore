43c1f575f1a9b345899aeafe8c38858a41b40d0e
Hao Kung <HaoK@users.noreply.github.com>
2019-05-21 14:40:59 -0700

    Update eng/helix/content/installnode.sh
    Co-Authored-By: Nate McMaster <natemcmaster@users.noreply.github.com>

--- eng/helix/content/installnode.sh
+++ eng/helix/content/installnode.sh
@@ -6,7 +6,11 @@ if type -P "node" &>/dev/null; then
 fi
 
 node_version=$1
-platformarch=$2
+if [ "$uname" = "Darwin" ]; then
+   platformarch='darwin-x64'
+else
+   platformarch='linux-x64'
+fi
 output_dir=$HELIX_CORRELATION_PAYLOAD/node
 url="http://nodejs.org/dist/v$node_version/node-v$node_version-$platformarch.tar.gz"
 tmp="$(mktemp -d -t install-node.XXXXXX)"
