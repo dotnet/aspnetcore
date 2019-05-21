4a565abb6eebe32688ea130df756e84bf1f5cc19
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-05-21 10:25:27 +0100

    Add ServerSideAuth template example

---
+++ src/Components/AuthTemplates/ServerSideAuth/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ServerSideAuth
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            CreateWebHostBuilder(args).Build();
+    }
+}
