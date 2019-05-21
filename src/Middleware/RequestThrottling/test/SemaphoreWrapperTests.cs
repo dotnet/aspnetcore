ce3806ee10a2ecc923682a57631897f76841046e
Dylan Dmitri Gray <d.dylan.g@gmail.com>
2019-05-20 17:29:51 -0700

    smol cleanup

--- src/Middleware/RequestThrottling/test/SemaphoreWrapperTests.cs
+++ src/Middleware/RequestThrottling/test/SemaphoreWrapperTests.cs
@@ -1,12 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Xunit;
-using System.Threading;
 using System.Threading.Tasks;
-using System;
-using System.Runtime.CompilerServices;
-using Microsoft.AspNetCore.Testing;
+using Xunit;
 
 namespace Microsoft.AspNetCore.RequestThrottling.Tests
 {
