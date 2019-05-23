0cfa4e493cca13acc5a9290e6024d0bd5d8d6ef5
Justin Kotalik <jukotali@microsoft.com>
2019-05-23 15:45:39 -0700

    nit

--- src/Servers/IIS/IIS/test/Common.FunctionalTests/Inprocess/StdOutRedirectionTests.cs
+++ src/Servers/IIS/IIS/test/Common.FunctionalTests/Inprocess/StdOutRedirectionTests.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Server.IIS.FunctionalTests.Utilities;
 using Microsoft.AspNetCore.Server.IISIntegration.FunctionalTests;
+using Microsoft.AspNetCore.Server.IntegrationTesting;
 using Microsoft.AspNetCore.Server.IntegrationTesting.IIS;
 using Microsoft.AspNetCore.Testing.xunit;
 using Newtonsoft.Json;
