0930aa6918e672923558de07212c2600ccabda97
David Fowler <davidfowl@gmail.com>
2019-05-26 21:18:29 -0700

    Updated the ref

--- src/Servers/Kestrel/Transport.Abstractions/ref/Microsoft.AspNetCore.Server.Kestrel.Transport.Abstractions.netcoreapp3.0.cs
+++ src/Servers/Kestrel/Transport.Abstractions/ref/Microsoft.AspNetCore.Server.Kestrel.Transport.Abstractions.netcoreapp3.0.cs
@@ -1,12 +1,3 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Abstractions.Internal
-{
-    public static partial class KestrelMemoryPool
-    {
-        public static readonly int MinimumSegmentSize;
-        public static System.Buffers.MemoryPool<byte> Create() { throw null; }
-        public static System.Buffers.MemoryPool<byte> CreateSlabMemoryPool() { throw null; }
-    }
-}
