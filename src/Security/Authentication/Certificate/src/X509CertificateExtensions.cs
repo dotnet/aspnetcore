33ac70ddebb7ad1e278d676b060108734cadbb32
Hao Kung <HaoK@users.noreply.github.com>
2019-05-28 15:10:38 -0700

    Switch to span sequence equals

--- src/Security/Authentication/Certificate/src/X509CertificateExtensions.cs
+++ src/Security/Authentication/Certificate/src/X509CertificateExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 
@@ -17,6 +18,10 @@ namespace Microsoft.AspNetCore.Authentication.Certificate
         /// <param name="certificate">The <see cref="X509Certificate2"/>.</param>
         /// <returns>True if the certificate is self signed.</returns>
         public static bool IsSelfSigned(this X509Certificate2 certificate)
-            => certificate.SubjectName.RawData.SequenceEqual(certificate.IssuerName.RawData);
+        {
+            Span<byte> subject = certificate.SubjectName.RawData;
+            Span<byte> issuer = certificate.IssuerName.RawData;
+            return subject.SequenceEqual(issuer);
+        }
     }
 }
