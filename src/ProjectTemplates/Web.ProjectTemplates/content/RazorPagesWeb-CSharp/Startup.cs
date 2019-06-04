4298f63b2a4daf8dc14497a12295afd1143ef2f2
Isaac Levin <isaac.r.levin@gmail.com>
2019-06-04 11:03:09 -0400

    remove cookie consent from templates

--- src/ProjectTemplates/Web.ProjectTemplates/content/RazorPagesWeb-CSharp/Startup.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/RazorPagesWeb-CSharp/Startup.cs
@@ -52,11 +52,6 @@ namespace Company.WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-            });
 
 #if (IndividualLocalAuth)
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -151,8 +146,6 @@ namespace Company.WebApplication1
 
 #endif
             app.UseStaticFiles();
-
-            app.UseCookiePolicy();
 
             app.UseRouting();
 
