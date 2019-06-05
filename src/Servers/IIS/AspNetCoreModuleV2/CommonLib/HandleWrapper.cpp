5e28272a4cde3060e6e2b16d2809fa7f5264596a
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2019-06-05 10:29:06 -0700

    small note

--- src/Servers/IIS/AspNetCoreModuleV2/CommonLib/HandleWrapper.cpp
+++ src/Servers/IIS/AspNetCoreModuleV2/CommonLib/HandleWrapper.cpp
@@ -4,5 +4,6 @@
 #include "HandleWrapper.h"
 
 // Workaround for VC++ bug https://developercommunity.visualstudio.com/content/problem/33928/constexpr-failing-on-nullptr-v141-compiler-regress.html
+// REVIEW: This is fixed maybe? We could try removing it now that we're on v142. Is there test coverage?
 const HANDLE InvalidHandleTraits::DefaultHandle = INVALID_HANDLE_VALUE;
 const HANDLE FindFileHandleTraits::DefaultHandle = INVALID_HANDLE_VALUE;
