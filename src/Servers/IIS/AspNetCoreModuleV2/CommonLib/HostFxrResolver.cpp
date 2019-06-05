0032040a91e043b63b6a5a65eb09022397537a76
Justin Kotalik <jukotali@microsoft.com>
2019-06-04 17:25:19 -0700

    Update src/Servers/IIS/AspNetCoreModuleV2/CommonLib/HostFxrResolver.cpp
    Co-Authored-By: Andrew Stanton-Nurse <andrew@stanton-nurse.com>

--- src/Servers/IIS/AspNetCoreModuleV2/CommonLib/HostFxrResolver.cpp
+++ src/Servers/IIS/AspNetCoreModuleV2/CommonLib/HostFxrResolver.cpp
@@ -95,7 +95,7 @@ HostFxrResolver::GetHostFxrParameters(
             if (!is_regular_file(applicationDllPath))
             {
                 errorContext.subStatusCode = 38;
-                errorContext.errorReason = "Application Dll not found. Confirm the application dll is present and is not published as a single executable.";
+                errorContext.errorReason = "Application dll not found. Confirm the application dll is present. Single-file deployments are not supported in IIS.";
                 errorContext.generalErrorType = "ANCM Application Dll Not Found";
                 throw InvalidOperationException(
                     format(L"Application .dll was not found at %s. Confirm the application dll is present and is not published as a single executable.",
