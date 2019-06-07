e54805c741df3b024bf8a4c8b3eb252213aefd41
Hao Kung <HaoK@users.noreply.github.com>
2019-06-07 15:01:56 -0700

    Update InstallNode.ps1

--- eng/helix/content/InstallNode.ps1
+++ eng/helix/content/InstallNode.ps1
@@ -7,8 +7,6 @@
      The version of NodeJS to install.
  .PARAMETER InstallDir
      The directory to install NodeJS to.
- .PARAMETER Arch
-     The target architecture to install.
  .LINK 
      https://nodejs.org/en/
  #> 
@@ -18,9 +16,6 @@ param(
     
     [Parameter(Mandatory = $true)]
     $InstallDir
-    
-    [Parameter(Mandatory = $true)]
-    $Arch
 )
 
 $ErrorActionPreference = 'Stop'
@@ -40,7 +35,7 @@ if (Test-Path "$output_dir\node.exe")
     exit
 }
 
-$nodeFile="node-v$Version-win-$Arch"
+$nodeFile="node-v$Version-win-x64"
 $url="http://nodejs.org/dist/v$Version/$nodeFile.zip"
 Write-Host "Starting download of NodeJs ${Version} from $url"
 Invoke-WebRequest -UseBasicParsing -Uri "$url" -OutFile "nodejs.zip"
