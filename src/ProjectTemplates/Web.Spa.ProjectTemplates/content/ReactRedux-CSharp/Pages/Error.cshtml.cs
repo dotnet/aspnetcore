a90fc1d73a700ac4ea77862a187bca5b6cc97102
Isaac Levin <8878502+isaac2004@users.noreply.github.com>
2019-06-07 10:22:41 -0400

    Update Error.cshtml.cs

--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/Pages/Error.cshtml.cs
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/Pages/Error.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace Company.WebApplication1.Pages
 {
