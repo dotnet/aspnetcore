0cf8c0c43afddb8b5612ec121bcebbebb073bdd4
Isaac Levin <8878502+isaac2004@users.noreply.github.com>
2019-06-07 10:22:26 -0400

    Update SampleDataController.cs

--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/Controllers/SampleDataController.cs
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/Controllers/SampleDataController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Company.WebApplication1.Controllers
 {
