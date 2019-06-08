be3d683d232a9b2c3629a34e9b0055ebb2b10175
Hao Kung <HaoK@users.noreply.github.com>
2019-06-08 12:31:37 -0700

    Update SeleniumStandaloneServer.cs

--- src/Shared/E2ETesting/SeleniumStandaloneServer.cs
+++ src/Shared/E2ETesting/SeleniumStandaloneServer.cs
@@ -100,6 +100,10 @@ namespace Microsoft.AspNetCore.E2ETesting
                 psi.Arguments = $"/c npm {psi.Arguments}";
             }
 
+            // It's important that we get the folder value before we start the process to prevent
+            // untracked processes when the tracking folder is not correctly configure.
+            var trackingFolder = GetProcessTrackingFolder();
+            
             if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("helix")))
             {
                 // Just create a random tracking folder on helix
@@ -107,9 +111,6 @@ namespace Microsoft.AspNetCore.E2ETesting
                 Directory.CreateDirectory(trackingFolder);
             }
 
-            // It's important that we get the folder value before we start the process to prevent
-            // untracked processes when the tracking folder is not correctly configure.
-            var trackingFolder = GetProcessTrackingFolder();
             if (!Directory.Exists(trackingFolder))
             {
                 throw new InvalidOperationException($"Invalid tracking folder. Set the 'SeleniumProcessTrackingFolder' MSBuild property to a valid folder.");
