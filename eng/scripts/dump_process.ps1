c499f92368cf31bb67b443c707cc237df8cfc9e9
John Luo <johluo@microsoft.com>
2019-06-12 16:37:17 -0700

    Just overwrite it

--- eng/scripts/dump_process.ps1
+++ eng/scripts/dump_process.ps1
@@ -1,6 +1,6 @@
 Set-Location $args[0]
 
 while ($true) {
-    Get-Process > artifacts/log/processes/runningProcesses.$(get-date -f yyyy-MM-dd-HH-mm-ss).txt
+    Get-Process > artifacts/log/runningProcesses.txt
     Start-Sleep -Seconds 300
 }
