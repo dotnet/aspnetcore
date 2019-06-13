b5a48037752dc0b2ffd02aaf074b27840a7c52fa
John Luo <johluo@microsoft.com>
2019-06-12 18:53:52 -0700

    Can't test PlatformManifest since it's in the nupkg not the sharedfx layout

--- src/Framework/test/SharedFxTests.cs
+++ src/Framework/test/SharedFxTests.cs
@@ -138,57 +138,5 @@ namespace Microsoft.AspNetCore
             Assert.Equal(TestData.GetRepositoryCommit(), lines[0]);
             Assert.Equal(TestData.GetSharedFxVersion(), lines[1]);
         }
-
-        [Fact]
-        public void PlatformManifestListsAllFiles()
-        {
-            var platformManifestPath = Path.Combine(_sharedFxRoot, "data", "PlatformManifest.txt");
-            var expectedAssemblies = TestData.GetSharedFxDependencies()
-                .Split(';', StringSplitOptions.RemoveEmptyEntries)
-                .ToHashSet();
-
-            _output.WriteLine("==== file contents ====");
-            _output.WriteLine(File.ReadAllText(platformManifestPath));
-            _output.WriteLine("==== expected assemblies ====");
-            _output.WriteLine(string.Join('\n', expectedAssemblies.OrderBy(i => i)));
-
-            AssertEx.FileExists(platformManifestPath);
-
-            var manifestFileLines = File.ReadAllLines(platformManifestPath);
-
-            var actualAssemblies = manifestFileLines
-                .Where(s => !string.IsNullOrEmpty(s))
-                .Select(i =>
-                {
-                    var fileName = i.Split('|')[0];
-                    return fileName.EndsWith(".dll", StringComparison.Ordinal)
-                        ? fileName.Substring(0, fileName.Length - 4)
-                        : fileName;
-                })
-                .ToHashSet();
-
-            var missing = expectedAssemblies.Except(actualAssemblies);
-            var unexpected = actualAssemblies.Except(expectedAssemblies);
-
-            _output.WriteLine("==== missing assemblies from the manifest ====");
-            _output.WriteLine(string.Join('\n', missing));
-            _output.WriteLine("==== unexpected assemblies in the manifest ====");
-            _output.WriteLine(string.Join('\n', unexpected));
-
-            Assert.Empty(missing);
-            Assert.Empty(unexpected);
-
-            Assert.All(manifestFileLines, line =>
-            {
-                var parts = line.Split('|');
-                Assert.Equal(4, parts.Length);
-                Assert.Equal("Microsoft.AspNetCore.App", parts[1]);
-                if (parts[2].Length > 0)
-                {
-                    Assert.True(Version.TryParse(parts[2], out _), "Assembly version must be convertable to System.Version");
-                }
-                Assert.True(Version.TryParse(parts[3], out _), "File version must be convertable to System.Version");
-            });
-        }
     }
 }
