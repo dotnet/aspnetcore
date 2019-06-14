35c0613567628649b58eb75f87004acbe88e4c4c
John Luo <johluo@microsoft.com>
2019-06-14 10:55:47 -0700

    Remove logging to stop masking return code

--- build.cmd
+++ build.cmd
@@ -1,4 +1,3 @@
 @ECHO OFF
 SETLOCAL
 PowerShell -NoProfile -NoLogo -ExecutionPolicy Bypass -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = ''; try { & '%~dp0build.ps1' %*; exit $LASTEXITCODE } catch { write-host $_; exit 1 }"
-ECHO build.cmd completed
--- eng/scripts/cibuild.cmd
+++ eng/scripts/cibuild.cmd
@@ -1,4 +1,3 @@
 @ECHO OFF
 SET RepoRoot=%~dp0..\..
 %RepoRoot%\build.cmd -ci -all -pack -sign %*
-ECHO cibuild.cmd completed
