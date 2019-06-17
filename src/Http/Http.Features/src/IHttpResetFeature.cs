cf5987560ab2db74d82eb8bc8fb5c03018ae75d0
Chris R <Tratcher@outlook.com>
2019-06-17 14:32:37 -0700

    Refs 3w0JO$)($%*)GH*)$%Y*)$H*)H

--- src/Http/Http.Features/src/IHttpResetFeature.cs
+++ src/Http/Http.Features/src/IHttpResetFeature.cs
@@ -8,11 +8,10 @@ namespace Microsoft.AspNetCore.Http.Features
     /// </summary>
     public interface IHttpResetFeature
     {
-
         /// <summary>
         /// Send a reset message with the given error code. The request will be considered aborted.
         /// </summary>
-        /// <param name="errorCode"></param>
+        /// <param name="errorCode">The error code to send in the reset message.</param>
         void Reset(int errorCode);
     }
 }
