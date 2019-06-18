00035ba9e1e68a18ba584f65d7d097b1ab26b0eb
Hao Kung <HaoK@users.noreply.github.com>
2019-06-18 10:58:44 -0700

    Switch Startup filter to HostedService

--- src/Shared/WebEncoders/WebEncoders.cs
+++ src/Shared/WebEncoders/WebEncoders.cs
@@ -2,7 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+#if NETCOREAPP3_0
 using System.Buffers;
+#endif
 using System.Diagnostics;
 using System.Globalization;
 using Microsoft.Extensions.WebEncoders.Sources;
