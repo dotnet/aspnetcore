78e7ea3205b167ad5b76e4999b8627e5c622e72d
Justin Kotalik <jukotali@microsoft.com>
2019-06-18 12:51:11 -0700

    Get package correct

--- src/Installers/Windows/SharedFramework/GenerateNugetPack.ps1
+++ src/Installers/Windows/SharedFramework/GenerateNugetPack.ps1
@@ -2,36 +2,17 @@
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 param(
-    [Parameter(Mandatory=$true)][string]$ContentPath,
-    [Parameter(Mandatory=$true)][string]$NugetVersion,
+    [Parameter(Mandatory=$true)][string]$MsiPath,
+    [Parameter(Mandatory=$true)][string]$CabPath,
     [Parameter(Mandatory=$true)][string]$NuspecFile,
-    [Parameter(Mandatory=$true)][string]$NupkgFile,
-    [Parameter(Mandatory=$false)][string]$Architecture,
-    [Parameter(Mandatory=$false)][string]$CabPath
+    [Parameter(Mandatory=$true)][string]$OutputDirectory,
+    [Parameter(Mandatory=$true)][string]$Architecture,
+    [Parameter(Mandatory=$true)][string]$MajorVersion,
+    [Parameter(Mandatory=$true)][string]$MinorVersion
+
 )
 
-$RepoRoot = Convert-Path "$PSScriptRoot\..\..\..\..\..\.."
-$NuGetDir = Join-Path $RepoRoot "artifacts\Tools\nuget"
-$NuGetExe = Join-Path $NuGetDir "nuget.exe"
-$OutputDirectory = [System.IO.Path]::GetDirectoryName($NupkgFile)
-$ContentPath = [System.IO.Path]::GetFullPath($ContentPath)
-if ($CabPath) {
-    $CabPath = [System.IO.Path]::GetFullPath($CabPath)
-}
+$NuGetExe = "nuget.exe"
 
-if (-not (Test-Path $NuGetDir)) {
-    New-Item -ItemType Directory -Force -Path $NuGetDir | Out-Null
-}
-
-if (-not (Test-Path $NuGetExe)) {
-    # Using 3.5.0 to workaround https://github.com/NuGet/Home/issues/5016
-    Write-Output "Downloading nuget.exe to $NuGetExe"
-    wget https://dist.nuget.org/win-x86-commandline/v3.5.0/nuget.exe -OutFile $NuGetExe
-}
-
-if (Test-Path $NupkgFile) {
-    Remove-Item -Force $NupkgFile
-}
-
-& $NuGetExe pack $NuspecFile -Version $NugetVersion -OutputDirectory $OutputDirectory -NoDefaultExcludes -NoPackageAnalysis -Properties PAYLOAD_FILES=$ContentPath`;DOTNET_CAB_FILE=$CabPath`;ARCH=$Architecture
+& $NuGetExe pack $NuspecFile -OutputDirectory $OutputDirectory -NoDefaultExcludes -NoPackageAnalysis -Properties ASPNETCORE_RUNTIME_MSI=$MsiPath`;ASPNETCORE_CAB_FILE=$CabPath`;ARCH=$Architecture`;MAJOR_VERSION=$MajorVersion`;MINOR_VERSION=$MinorVersion
 Exit $LastExitCode
