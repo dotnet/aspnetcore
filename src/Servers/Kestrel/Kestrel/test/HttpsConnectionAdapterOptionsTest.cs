d6326694a5532cbdea9c736482420ed9c6aaea9d
MikaelMengistu <ethmengistu7@gmail.com>
2019-06-20 13:36:55 -0700

    unused using

--- src/Servers/Kestrel/Kestrel/test/HttpsConnectionAdapterOptionsTest.cs
+++ src/Servers/Kestrel/Kestrel/test/HttpsConnectionAdapterOptionsTest.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Net.Security;
 using System.Threading;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.AspNetCore.Server.Kestrel.Https;
