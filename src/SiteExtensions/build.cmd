228fa54b1ed3962887e3cdc019c5ac7b48165792
John Luo <johluo@microsoft.com>
2019-06-22 21:50:53 -0700

    Also build AspNetCoreRuntime.3.0.x64

--- src/SiteExtensions/build.cmd
+++ src/SiteExtensions/build.cmd
@@ -2,15 +2,16 @@
 SET RepoRoot=%~dp0..\..
 
 ECHO Building Microsoft.AspNetCore.Runtime.SiteExtension
-CALL %RepoRoot%\build.cmd -projects %~dp0Runtime\Microsoft.AspNetCore.Runtime.SiteExtension.pkgproj /bl:artifacts/log/se-runtime.binlog %*
+CALL %RepoRoot%\build.cmd -arch x64 -projects %~dp0Runtime\Microsoft.AspNetCore.Runtime.SiteExtension.pkgproj /bl:artifacts/log/se-runtime-x64.binlog %*
+CALL %RepoRoot%\build.cmd -arch x86 -projects %~dp0Runtime\Microsoft.AspNetCore.Runtime.SiteExtension.pkgproj /bl:artifacts/log/se-runtime-x86.binlog %*
 
 IF %ERRORLEVEL% NEQ 0 (
    EXIT /b %ErrorLevel%
 )
 
 ECHO Building LoggingBranch
-CALL %RepoRoot%\build.cmd -arch x64 -projects %~dp0LoggingBranch\LB.csproj /bl:artifacts/log/se-lb.binlog %*
-CALL %RepoRoot%\build.cmd -arch x86 -projects %~dp0LoggingBranch\LB.csproj /bl:artifacts/log/se-lb.binlog %*
+CALL %RepoRoot%\build.cmd -arch x64 -projects %~dp0LoggingBranch\LB.csproj /bl:artifacts/log/se-lb-x64.binlog %*
+CALL %RepoRoot%\build.cmd -arch x86 -projects %~dp0LoggingBranch\LB.csproj /bl:artifacts/log/se-lb-x86.binlog %*
 
 IF %ERRORLEVEL% NEQ 0 (
    EXIT /b %ErrorLevel%
