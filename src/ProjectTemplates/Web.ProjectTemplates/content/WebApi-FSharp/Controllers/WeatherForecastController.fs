09ec0d7338dd59a8f89e44f9da60a78cf54a2a7c
Pranav K <prkrishn@hotmail.com>
2019-06-24 14:17:59 -0700

    Once more

--- src/ProjectTemplates/Web.ProjectTemplates/content/WebApi-FSharp/Controllers/WeatherForecastController.fs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/WebApi-FSharp/Controllers/WeatherForecastController.fs
@@ -6,6 +6,7 @@ open System.Linq
 open System.Threading.Tasks
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Logging
+open Company.WebApplication1
 
 [<ApiController>]
 [<Route("[controller]")>]
@@ -15,7 +16,7 @@ type WeatherForecastController (logger : ILogger<WeatherForecastController>) =
     let summaries = [| "Freezing"; "Bracing"; "Chilly"; "Cool"; "Mild"; "Warm"; "Balmy"; "Hot"; "Sweltering"; "Scorching" |]
 
     [<HttpGet>]
-    member __.Get() =
+    member __.Get() : WeatherForecast[] =
         let rng = System.Random()
         [|
             for index in 0..4 ->
