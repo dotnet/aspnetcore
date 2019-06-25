e9211c4b1e94cf9fb4b252b8a5ac8e8fec463482
Pranav K <prkrishn@hotmail.com>
2019-06-25 09:24:10 -0700

    Update WeatherForecastController.cs

--- src/Components/Blazor/Templates/src/content/BlazorHosted-CSharp/BlazorHosted-CSharp.Server/Controllers/WeatherForecastController.cs
+++ src/Components/Blazor/Templates/src/content/BlazorHosted-CSharp/BlazorHosted-CSharp.Server/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BlazorHosted_CSharp.Server.Controller
+namespace BlazorHosted_CSharp.Server.Controllers
 {
     [ApiController]
     [Route("[controller]")]
