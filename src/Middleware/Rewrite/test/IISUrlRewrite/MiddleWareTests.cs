68718474cfcc62e2316d1934304e8a06969ba455
Justin Kotalik <jukotali@microsoft.com>
2019-06-24 22:59:32 -0700

    fix rewrite tests

--- src/Middleware/Rewrite/test/IISUrlRewrite/MiddleWareTests.cs
+++ src/Middleware/Rewrite/test/IISUrlRewrite/MiddleWareTests.cs
@@ -552,9 +552,9 @@ namespace Microsoft.AspNetCore.Rewrite.Tests.UrlRewrite
         }
 
         [Theory]
-        [InlineData(@"^http://localhost(/.*)", "http://localhost/foo/bar", UriMatchPart.Path)]
-        [InlineData(@"^http://localhost(/.*)", "http://www.test.com/foo/bar", UriMatchPart.Full)]
-        public async Task Invoke_GlobalRuleConditionMatchesAgainstFullUri_CodedRule(string conditionInputPattern, string expectedResult, UriMatchPart uriMatchPart)
+        [InlineData(@"^http://localhost(/.*)", "http://localhost/foo/bar", (int)UriMatchPart.Path)]
+        [InlineData(@"^http://localhost(/.*)", "http://www.test.com/foo/bar", (int)UriMatchPart.Full)]
+        public async Task Invoke_GlobalRuleConditionMatchesAgainstFullUri_CodedRule(string conditionInputPattern, string expectedResult, int uriMatchPart)
         {
             // arrange
             var inputParser = new InputParser();
@@ -570,7 +570,7 @@ namespace Microsoft.AspNetCore.Rewrite.Tests.UrlRewrite
                 inputParser,
                 "{REQUEST_URI}",
                 conditionInputPattern,
-                uriMatchPart,
+                (UriMatchPart)uriMatchPart,
                 ignoreCase: true,
                 negate: false);
             ruleBuilder.ConfigureConditionBehavior(LogicalGrouping.MatchAll, trackAllCaptures: true);
@@ -578,7 +578,7 @@ namespace Microsoft.AspNetCore.Rewrite.Tests.UrlRewrite
 
             var action = new RewriteAction(
                 RuleResult.SkipRemainingRules,
-                inputParser.ParseInputString(@"http://www.test.com{C:1}", uriMatchPart),
+                inputParser.ParseInputString(@"http://www.test.com{C:1}", (UriMatchPart)uriMatchPart),
                 queryStringAppend: false);
             ruleBuilder.AddUrlAction(action);
 
