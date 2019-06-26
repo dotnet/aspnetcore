776522b719a22d87d7eb078ec2851bd8ccc61fcf
Ryan Brandenburg <rybrande@microsoft.com>
2019-06-26 10:51:23 -0700

    Take new NSwag.ApiDescription.Client version

--- src/Tools/dotnet-openapi/test/OpenApiAddFileTests.cs
+++ src/Tools/dotnet-openapi/test/OpenApiAddFileTests.cs
@@ -1,7 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Threading;
