e2c18dbc5265e3e369ecc00cecd7f0e14615035f
David Fowler <davidfowl@gmail.com>
2019-06-26 09:23:56 -0700

    Replace the output pipe only

--- src/Servers/Kestrel/Core/src/Middleware/Internal/DuplexPipeStreamAdapter.cs
+++ src/Servers/Kestrel/Core/src/Middleware/Internal/DuplexPipeStreamAdapter.cs
@@ -4,9 +4,7 @@
 using System;
 using System.IO;
 using System.IO.Pipelines;
-using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Connections;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
@@ -17,9 +15,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
     /// <typeparam name="TStream"></typeparam>
     internal class DuplexPipeStreamAdapter<TStream> : DuplexPipeStream, IDuplexPipe where TStream : Stream
     {
-        private Task _inputTask;
         private Task _outputTask;
-        private int _minAllocBufferSize;
 
         public DuplexPipeStreamAdapter(IDuplexPipe duplexPipe, Func<Stream, TStream> createStream) :
             this(duplexPipe, new StreamPipeReaderOptions(leaveOpen: true), new StreamPipeWriterOptions(leaveOpen: true), createStream)
@@ -27,17 +23,9 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
         }
 
         public DuplexPipeStreamAdapter(IDuplexPipe duplexPipe, StreamPipeReaderOptions readerOptions, StreamPipeWriterOptions writerOptions, Func<Stream, TStream> createStream) :
-            base(duplexPipe.Input, duplexPipe.Output, throwOnCancelled: true)
+            base(duplexPipe.Input, duplexPipe.Output)
         {
             Stream = createStream(this);
-
-            var inputOptions = new PipeOptions(pool: readerOptions.Pool,
-                                               readerScheduler: PipeScheduler.Inline,
-                                               writerScheduler: PipeScheduler.Inline,
-                                               minimumSegmentSize: readerOptions.BufferSize,
-                                               pauseWriterThreshold: 1,
-                                               resumeWriterThreshold: 1,
-                                               useSynchronizationContext: false);
 
             var outputOptions = new PipeOptions(pool: writerOptions.Pool,
                                                 readerScheduler: PipeScheduler.Inline,
@@ -47,32 +35,18 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
                                                 minimumSegmentSize: writerOptions.MinimumBufferSize,
                                                 useSynchronizationContext: false);
 
-            Input = new Pipe(inputOptions);
+            Input = PipeReader.Create(Stream, readerOptions);
             Output = new Pipe(outputOptions);
-
-            _minAllocBufferSize = readerOptions.MinimumReadSize;
         }
 
         public ILogger Log { get; private set; }
 
         public TStream Stream { get; }
 
-        private Pipe Input { get; }
-
         private Pipe Output { get; }
 
-        PipeReader IDuplexPipe.Input
-        {
-            get
-            {
-                if (_inputTask == null)
-                {
-                    _inputTask = ReadInputAsync();
-                }
-                return Input.Reader;
-            }
-        }
-
+        public PipeReader Input { get; }
+        
         PipeWriter IDuplexPipe.Output
         {
             get
@@ -88,24 +62,14 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
 
         public override async ValueTask DisposeAsync()
         {
+            Input.Complete();
             Output.Writer.Complete();
-            Input.Reader.Complete();
-
 
             if (_outputTask != null)
             {
                 // Wait for the output task to complete, this ensures that we've copied
                 // the application data to the underlying stream
                 await _outputTask;
-            }
-
-            // Cancel the underlying stream so that the input task yields
-            CancelPendingRead();
-
-            if (_inputTask != null)
-            {
-                // The input task should yield now that we've cancelled it
-                await _inputTask;
             }
         }
 
@@ -153,48 +117,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
             finally
             {
                 Output.Reader.Complete();
-            }
-        }
-
-        private async Task ReadInputAsync()
-        {
-            Exception error = null;
-
-            try
-            {
-                while (true)
-                {
-                    var outputBuffer = Input.Writer.GetMemory(_minAllocBufferSize);
-                    var bytesRead = await Stream.ReadAsync(outputBuffer);
-                    Input.Writer.Advance(bytesRead);
-
-                    if (bytesRead == 0)
-                    {
-                        // FIN
-                        break;
-                    }
-
-                    var result = await Input.Writer.FlushAsync();
-
-                    if (result.IsCompleted)
-                    {
-                        break;
-                    }
-                }
-            }
-            catch (OperationCanceledException ex)
-            {
-                // Propagate the exception if it's ConnectionAbortedException
-                error = ex as ConnectionAbortedException;
-            }
-            catch (Exception ex)
-            {
-                // Don't rethrow the exception. It should be handled by the Pipeline consumer.
-                error = ex;
-            }
-            finally
-            {
-                Input.Writer.Complete(error);
             }
         }
     }
