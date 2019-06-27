e73272635cf9a8f8855df5b9e525e50d1561ef04
Stephen Halter <halter73@gmail.com>
2019-06-27 02:16:43 -0700

    missed one

--- src/SignalR/clients/csharp/Client/src/HttpConnectionFactory.cs
+++ src/SignalR/clients/csharp/Client/src/HttpConnectionFactory.cs
@@ -42,8 +42,7 @@ namespace Microsoft.AspNetCore.SignalR.Client
         /// <param name="endPoint">The <see cref="UriEndPoint"/> to connect to.</param>
         /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None" />.</param>
         /// <returns>
-        /// A <see cref="ValueTask{TResult}"/> that represents the asynchronous connect.
-        /// The <see cref="Task{TResult}.Result"/> property returns a <see cref="ConnectionContext"/> for the new connection.
+        /// A <see cref="ValueTask{TResult}" /> that represents the asynchronous connect, yielding the <see cref="ConnectionContext" /> for the new connection when completed.
         /// </returns>
         public async ValueTask<ConnectionContext> ConnectAsync(EndPoint endPoint, CancellationToken cancellationToken = default)
         {
