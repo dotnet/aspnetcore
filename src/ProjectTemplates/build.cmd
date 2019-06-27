be422512acd0d9aa2e182030ae520af05ace9627
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-06-27 04:13:29 -0700

    Enforce E2E test prerequisites when building individual solutions only

--- src/ProjectTemplates/build.cmd
+++ src/ProjectTemplates/build.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
 SET RepoRoot=%~dp0..\..
-%RepoRoot%\build.cmd -projects %~dp0**\*.*proj %*
+%RepoRoot%\build.cmd -projects %~dp0**\*.*proj -msbuildarguments "/p:EnforceE2ETestPrerequisites=true" %*
