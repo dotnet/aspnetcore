1c71752b1ff99bfd8dd70f54f127832a1645ae35
David Fowler <davidfowl@gmail.com>
2019-06-28 10:00:05 -0700

    Move connection id creation into the transport. (#11680)

--- src/Servers/Kestrel/shared/CorrelationIdGenerator.cs
+++ src/Servers/Kestrel/shared/CorrelationIdGenerator.cs
@@ -4,7 +4,7 @@
 using System;
 using System.Threading;
 
-namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Infrastructure
+namespace Microsoft.AspNetCore.Connections
 {
     internal static class CorrelationIdGenerator
     {
