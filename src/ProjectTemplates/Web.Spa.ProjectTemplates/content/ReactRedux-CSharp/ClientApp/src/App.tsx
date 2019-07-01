038768137f3a421b3ae593c47f704c32f7b21426
Ryan Brandenburg <rybrande@microsoft.com>
2019-07-01 14:52:14 -0700

    Accessibility for ReactRedux (#11571)
    Accessibility for ReactRedux

--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/ClientApp/src/App.tsx
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/ClientApp/src/App.tsx
@@ -5,9 +5,11 @@ import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
 
+import './custom.css'
+
 export default () => (
     <Layout>
-        <Route exact path='/' component={Home}/>
+        <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
     </Layout>
