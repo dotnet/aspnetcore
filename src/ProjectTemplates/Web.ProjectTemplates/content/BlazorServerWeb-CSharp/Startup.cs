ac3da8322dfeadbaee20feb237016da76e4c4611
Isaac Levin <isaac.r.levin@gmail.com>
2019-07-01 16:53:36 -0400

    Revert "added Error page to Server Blazor"
    This reverts commit 68039699fdd5123d62a0d5ad57a6fe4f3228a115.

--- src/ProjectTemplates/Web.ProjectTemplates/content/BlazorServerWeb-CSharp/Startup.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/BlazorServerWeb-CSharp/Startup.cs
@@ -144,7 +144,7 @@ namespace BlazorServerWeb_CSharp
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
 #if (RequiresHttps)
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
