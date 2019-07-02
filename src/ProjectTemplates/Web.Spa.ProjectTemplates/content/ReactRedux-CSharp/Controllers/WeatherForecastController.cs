e7e8d3088c4f47620ff89ebe204a9dd10837bd03
Ryan Brandenburg <rybrande@microsoft.com>
2019-07-02 10:34:19 -0700

    PR comments

--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/Controllers/WeatherForecastController.cs
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/Controllers/WeatherForecastController.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-#if (!NoAuth)
-using Microsoft.AspNetCore.Authorization;
-#endif
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
