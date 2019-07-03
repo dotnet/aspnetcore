d13edde00df36343f8d426d100caa21e29a2ca74
Ehsan Mirsaeedi <mirsaeedi@outlook.com>
2019-07-03 17:25:44 -0400

    test

---
+++ src/Features/JsonPatch/src/Internal/ErrorReporter - Copy.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNetCore.JsonPatch.Exceptions;
+
+namespace Microsoft.AspNetCore.JsonPatch.Internal
+{
+    internal static class ErrorReporter
+    {
+        public static readonly Action<JsonPatchError> Default = (error) =>
+        {
+            throw new JsonPatchException(error);
+        };
+    }
+}
