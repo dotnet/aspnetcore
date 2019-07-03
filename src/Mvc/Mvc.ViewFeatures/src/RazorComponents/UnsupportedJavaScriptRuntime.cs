68bda7c615e8aae8ad435a8a29a2e05a4bf21e0d
Pranav K <prkrishn@hotmail.com>
2019-07-03 15:46:04 -0700

    React to JSInterop changes

--- src/Mvc/Mvc.ViewFeatures/src/RazorComponents/UnsupportedJavaScriptRuntime.cs
+++ src/Mvc/Mvc.ViewFeatures/src/RazorComponents/UnsupportedJavaScriptRuntime.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.JSInterop;
 
@@ -10,6 +12,11 @@ namespace Microsoft.AspNetCore.Mvc.ViewFeatures
     internal class UnsupportedJavaScriptRuntime : IJSRuntime
     {
         public Task<T> InvokeAsync<T>(string identifier, params object[] args)
+        {
+            throw new InvalidOperationException("JavaScript interop calls cannot be issued during server-side prerendering, because the page has not yet loaded in the browser. Prerendered components must wrap any JavaScript interop calls in conditional logic to ensure those interop calls are not attempted during prerendering.");
+        }
+
+        public Task<TValue> InvokeAsync<TValue>(string identifier, IEnumerable<object> args, CancellationToken cancellationToken = default)
         {
             throw new InvalidOperationException("JavaScript interop calls cannot be issued during server-side prerendering, because the page has not yet loaded in the browser. Prerendered components must wrap any JavaScript interop calls in conditional logic to ensure those interop calls are not attempted during prerendering.");
         }
