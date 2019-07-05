e009ef5457ad939ade6bbf76ff307f7ae9a35bf0
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-07-05 11:37:49 +0100

    Use LogLevel.Debug

--- src/Components/Components/src/Rendering/Renderer.Log.cs
+++ src/Components/Components/src/Rendering/Renderer.Log.cs
@@ -10,21 +10,23 @@ namespace Microsoft.AspNetCore.Components.Rendering
     {
         internal static class Log
         {
+            private const LogLevel RendererLogLevel = LogLevel.Debug;
+
             private static readonly Action<ILogger, int, Type, int, Type, Exception> _initializingChildComponent =
-                LoggerMessage.Define<int, Type, int, Type>(LogLevel.Trace, new EventId(1, "InitializingChildComponent"), "Initializing component {componentId} ({componentType}) as child of {parentComponentId} ({parentComponentId})");
+                LoggerMessage.Define<int, Type, int, Type>(RendererLogLevel, new EventId(1, "InitializingChildComponent"), "Initializing component {componentId} ({componentType}) as child of {parentComponentId} ({parentComponentId})");
 
             private static readonly Action<ILogger, int, Type, Exception> _initializingRootComponent =
-                LoggerMessage.Define<int, Type>(LogLevel.Trace, new EventId(2, "InitializingRootComponent"), "Initializing root component {componentId} ({componentType})");
+                LoggerMessage.Define<int, Type>(RendererLogLevel, new EventId(2, "InitializingRootComponent"), "Initializing root component {componentId} ({componentType})");
 
             private static readonly Action<ILogger, int, Type, Exception> _renderingComponent =
-                LoggerMessage.Define<int, Type>(LogLevel.Trace, new EventId(3, "RenderingComponent"), "Rendering component {componentId} of type {componentType}");
+                LoggerMessage.Define<int, Type>(RendererLogLevel, new EventId(3, "RenderingComponent"), "Rendering component {componentId} of type {componentType}");
 
             private static readonly Action<ILogger, int, Type, Exception> _disposingComponent =
-                LoggerMessage.Define<int, Type>(LogLevel.Trace, new EventId(4, "DisposingComponent"), "Disposing component {componentId} of type {componentType}");
+                LoggerMessage.Define<int, Type>(RendererLogLevel, new EventId(4, "DisposingComponent"), "Disposing component {componentId} of type {componentType}");
 
             public static void InitializingComponent(ILogger logger, ComponentState componentState, ComponentState parentComponentState)
             {
-                if (logger.IsEnabled(LogLevel.Trace)) // This is almost always false, so skip the evaluations
+                if (logger.IsEnabled(RendererLogLevel)) // This is almost always false, so skip the evaluations
                 {
                     if (parentComponentState == null)
                     {
@@ -39,7 +41,7 @@ namespace Microsoft.AspNetCore.Components.Rendering
 
             public static void RenderingComponent(ILogger logger, ComponentState componentState)
             {
-                if (logger.IsEnabled(LogLevel.Trace)) // This is almost always false, so skip the evaluations
+                if (logger.IsEnabled(RendererLogLevel)) // This is almost always false, so skip the evaluations
                 {
                     _renderingComponent(logger, componentState.ComponentId, componentState.Component.GetType(), null);
                 }
@@ -47,7 +49,7 @@ namespace Microsoft.AspNetCore.Components.Rendering
 
             internal static void DisposingComponent(ILogger<Renderer> logger, ComponentState componentState)
             {
-                if (logger.IsEnabled(LogLevel.Trace)) // This is almost always false, so skip the evaluations
+                if (logger.IsEnabled(RendererLogLevel)) // This is almost always false, so skip the evaluations
                 {
                     _disposingComponent(logger, componentState.ComponentId, componentState.Component.GetType(), null);
                 }
