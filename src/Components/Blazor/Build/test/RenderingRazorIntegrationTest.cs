e464f9a3912dc58eb1d2d57f942f9dbb815b2a61
Ryan Nowak <rynowak@microsoft.com>
2019-07-04 18:26:03 -0700

    Remove obsolete test

--- src/Components/Blazor/Build/test/RenderingRazorIntegrationTest.cs
+++ src/Components/Blazor/Build/test/RenderingRazorIntegrationTest.cs
@@ -291,37 +291,6 @@ namespace Microsoft.AspNetCore.Blazor.Build.Test
         }
 
         [Fact]
-        public void SupportsAttributesWithEventHandlerValues()
-        {
-            // Arrange/Act
-            var component = CompileToComponent(
-                @"<elem attr=@MyHandleEvent />
-                @code {
-                    public bool HandlerWasCalled { get; set; } = false;
-
-                    void MyHandleEvent(Microsoft.AspNetCore.Components.UIEventArgs eventArgs)
-                    {
-                        HandlerWasCalled = true;
-                    }
-                }");
-            var handlerWasCalledProperty = component.GetType().GetProperty("HandlerWasCalled");
-
-            // Assert
-            Assert.False((bool)handlerWasCalledProperty.GetValue(component));
-            Assert.Collection(GetRenderTree(component),
-                frame => AssertFrame.Element(frame, "elem", 2, 0),
-                frame =>
-                {
-                    Assert.Equal(RenderTreeFrameType.Attribute, frame.FrameType);
-                    Assert.Equal(1, frame.Sequence);
-                    Assert.NotNull(frame.AttributeValue);
-
-                    ((Action<UIEventArgs>)frame.AttributeValue)(null);
-                    Assert.True((bool)handlerWasCalledProperty.GetValue(component));
-                });
-        }
-
-        [Fact]
         public void SupportsUsingStatements()
         {
             // Arrange/Act
