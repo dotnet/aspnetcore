6db69766851b83622dae26e4ea9792f482d1c7f8
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2019-07-09 11:55:41 -0700

    spelunk /usr/lib

--- spelunk.sh
+++ spelunk.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
 
+echo "*** PKGS ***"
 dpkg-query --show "*ssl*"
 
 for pkg in `dpkg-query --showformat="\\${binary:Package}\n" --show "*ssl*"`; do
     echo "*** $pkg ***"
     dpkg-query --listfiles $pkg
 done
+
+echo "*** LIBS ***"
+ls -l /usr/lib/x86_64-linux-gnu | grep ssl
