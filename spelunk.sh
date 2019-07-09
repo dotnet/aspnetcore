1049eb0c17529129b8ed41a91ddb8f76226b7391
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2019-07-09 11:49:35 -0700

    spelunk openssl versions

--- spelunk.sh
+++ spelunk.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-echo "Spelunking!"
+echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+
+dpkg-query --show "*ssl*"
+
+for pkg in `dpkg-query --showformat="\\${binary:Package}\n" --show "*ssl*"`; do
+    echo "*** $pkg ***"
+    dpkg-query --listfiles $pkg
+done
