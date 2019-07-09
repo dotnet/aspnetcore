bbf74000031e5d059426bdcf9921b8ac5e6bbb70
MikaelMengistu <ethmengistu7@gmail.com>
2019-07-09 16:41:10 -0700

    update log message

--- src/SignalR/clients/ts/signalr/tests/HttpConnection.test.ts
+++ src/SignalR/clients/ts/signalr/tests/HttpConnection.test.ts
@@ -213,12 +213,13 @@ describe("HttpConnection", () => {
             const connection = new HttpConnection("http://tempuri.org", options);
             await expect(connection.start(TransferFormat.Text))
                 .rejects
-                .toThrow("Unable to connect to the server with any of the available transports. WebSockets failed: null ServerSentEvents failed: Error: 'ServerSentEvents' is disabled by the client. LongPolling failed: Error: 'LongPolling' is disabled by the client.");
+                .toThrow("Unable to connect to the server with any of the available transports. WebSockets failed: The transport is not available. ServerSentEvents failed: Error: 'ServerSentEvents' is disabled by the client. LongPolling failed: Error: 'LongPolling' is disabled by the client.");
 
             expect(negotiateCount).toEqual(1);
         },
-        "Failed to start the transport 'WebSockets': null",
-        "Failed to start the connection: Error: Unable to connect to the server with any of the available transports. WebSockets failed: null ServerSentEvents failed: Error: 'ServerSentEvents' is disabled by the client. LongPolling failed: Error: 'LongPolling' is disabled by the client.");
+        "Failed to start the transport 'WebSockets': The transport is not available.",
+        "Failed to start the connection: Error: Unable to connect to the server with any of the available transports. WebSockets failed: The transport is not available. " +
+        "ServerSentEvents failed: Error: 'ServerSentEvents' is disabled by the client. LongPolling failed: Error: 'LongPolling' is disabled by the client.");
     });
 
     it("negotiate called again when transport fails to start and falls back", async () => {
