655a5836924a173cade41117ecb8b09da8194680
Chris R <Tratcher@outlook.com>
2019-07-10 15:21:27 -0700

    More tests

--- src/Servers/Kestrel/test/Interop.FunctionalTests/HttpClientHttp2InteropTests.cs
+++ src/Servers/Kestrel/test/Interop.FunctionalTests/HttpClientHttp2InteropTests.cs
@@ -12,9 +12,11 @@ using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.AspNetCore.Testing;
 using Microsoft.AspNetCore.Testing.xunit;
@@ -94,7 +96,6 @@ namespace Interop.FunctionalTests
                     webHostBuilder.ConfigureServices(AddTestLogging)
                     .Configure(app => app.Run(async context =>
                     {
-                        // await context.Response.StartAsync();
                         await context.Request.BodyReader.CopyToAsync(context.Response.BodyWriter);
                     }));
                 });
@@ -185,7 +186,6 @@ namespace Interop.FunctionalTests
                             allRequestsReceived.SetResult(0);
                         }
                         await allRequestsReceived.Task;
-                        await context.Response.StartAsync();
                         await context.Request.BodyReader.CopyToAsync(context.Response.BodyWriter);
                     }));
                 });
@@ -278,6 +278,291 @@ namespace Interop.FunctionalTests
                 }
 
                 Assert.True(totalRead == Repititions * Content.Length, "Too Short");
+            }
+        }
+
+        [ConditionalTheory]
+        [MemberData(nameof(SupportedSchemes))]
+        public async Task BidirectionalStreaming(string scheme)
+        {
+            var hostBuilder = new HostBuilder()
+                .ConfigureWebHost(webHostBuilder =>
+                {
+                    ConfigureKestrel(webHostBuilder, scheme);
+                    webHostBuilder.ConfigureServices(AddTestLogging)
+                    .Configure(app => app.Run(async context =>
+                    {
+                        var reader = context.Request.BodyReader;
+                        // Read Hello World and echo it back to the client, twice
+                        for (var i = 0; i < 2; i++)
+                        {
+                            var readResult = await reader.ReadAsync();
+                            while (!readResult.IsCompleted && readResult.Buffer.Length < "Hello World".Length)
+                            {
+                                reader.AdvanceTo(readResult.Buffer.Start, readResult.Buffer.End);
+                                readResult = await reader.ReadAsync();
+                            }
+
+                            var sequence = readResult.Buffer.Slice(0, "Hello World".Length);
+                            Assert.True(sequence.IsSingleSegment);
+                            await context.Response.BodyWriter.WriteAsync(sequence.First);
+                            reader.AdvanceTo(sequence.End);
+                        }
+
+                        var finalResult = await reader.ReadAsync();
+                        Assert.True(finalResult.IsCompleted && finalResult.Buffer.Length == 0);
+                    }));
+                });
+            using var host = await hostBuilder.StartAsync();
+
+            var url = $"{scheme}://127.0.0.1:{host.GetPort().ToString(CultureInfo.InvariantCulture)}/";
+
+            using var client = CreateClient();
+            client.DefaultRequestHeaders.ExpectContinue = true;
+
+            var streamingContent = new StreamingContent();
+            var request = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Version = HttpVersion.Version20,
+                Content = streamingContent,
+            };
+            var responseTask = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).DefaultTimeout();
+            // The server doesn't respond until we send the first set of data
+            await streamingContent.SendAsync("Hello World").DefaultTimeout();
+            var response = await responseTask;
+
+            Assert.Equal(HttpVersion.Version20, response.Version);
+            var stream = await response.Content.ReadAsStreamAsync();
+            var responseBuffer = new byte["Hello World".Length];
+
+
+            var read = await stream.ReadAsync(responseBuffer, 0, responseBuffer.Length).DefaultTimeout();
+            Assert.Equal(responseBuffer.Length, read);
+            Assert.Equal("Hello World", Encoding.UTF8.GetString(responseBuffer));
+
+            await streamingContent.SendAsync("Hello World").DefaultTimeout();
+            streamingContent.Complete();
+
+            read = await stream.ReadAsync(responseBuffer, 0, responseBuffer.Length).DefaultTimeout();
+            Assert.Equal(responseBuffer.Length, read);
+            Assert.Equal("Hello World", Encoding.UTF8.GetString(responseBuffer));
+
+            read = await stream.ReadAsync(responseBuffer, 0, responseBuffer.Length);
+            Assert.Equal(0, read);
+            await host.StopAsync();
+        }
+
+        [ConditionalTheory(Skip = "https://github.com/dotnet/corefx/issues/39404")]
+        [MemberData(nameof(SupportedSchemes))]
+        public async Task BidirectionalStreamingMoreClientData(string scheme)
+        {
+            var lastPacket = new TaskCompletionSource<string>(TaskCreationOptions.RunContinuationsAsynchronously);
+            var hostBuilder = new HostBuilder()
+                .ConfigureWebHost(webHostBuilder =>
+                {
+                    ConfigureKestrel(webHostBuilder, scheme);
+                    webHostBuilder.ConfigureServices(AddTestLogging)
+                    .Configure(app => app.Run(async context =>
+                    {
+                        var reader = context.Request.BodyReader;
+
+                        var readResult = await reader.ReadAsync();
+                        while (!readResult.IsCompleted && readResult.Buffer.Length < "Hello World".Length)
+                        {
+                            reader.AdvanceTo(readResult.Buffer.Start, readResult.Buffer.End);
+                            readResult = await reader.ReadAsync();
+                        }
+
+                        var sequence = readResult.Buffer.Slice(0, "Hello World".Length);
+                        Assert.True(sequence.IsSingleSegment);
+                        await context.Response.BodyWriter.WriteAsync(sequence.First);
+                        reader.AdvanceTo(sequence.End);
+                        await context.Features.Get<IHttpResponseCompletionFeature>().CompleteAsync();
+
+                        try
+                        {
+                            // The client sends one more packet after the server completes
+                            readResult = await reader.ReadAsync();
+                            while (!readResult.IsCompleted && readResult.Buffer.Length < "Hello World".Length)
+                            {
+                                reader.AdvanceTo(readResult.Buffer.Start, readResult.Buffer.End);
+                                readResult = await reader.ReadAsync();
+                            }
+
+                            Assert.True(readResult.Buffer.IsSingleSegment);
+                            var result = Encoding.UTF8.GetString(readResult.Buffer.FirstSpan);
+                            reader.AdvanceTo(readResult.Buffer.End);
+
+                            var finalResult = await reader.ReadAsync();
+                            Assert.True(finalResult.IsCompleted && finalResult.Buffer.Length == 0);
+                            lastPacket.SetResult(result);
+                        }
+                        catch (Exception ex)
+                        {
+                            lastPacket.SetException(ex);
+                        }
+                    }));
+                });
+            using var host = await hostBuilder.StartAsync();
+
+            var url = $"{scheme}://127.0.0.1:{host.GetPort().ToString(CultureInfo.InvariantCulture)}/";
+
+            using var client = CreateClient();
+            client.DefaultRequestHeaders.ExpectContinue = true;
+
+            var streamingContent = new StreamingContent();
+            var request = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Version = HttpVersion.Version20,
+                Content = streamingContent,
+            };
+            var responseTask = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).DefaultTimeout();
+            // The server doesn't respond until we send the first set of data
+            await streamingContent.SendAsync("Hello World").DefaultTimeout();
+            var response = await responseTask;
+
+            Assert.Equal(HttpVersion.Version20, response.Version);
+            var stream = await response.Content.ReadAsStreamAsync();
+            var responseBuffer = new byte["Hello World".Length];
+
+            var read = await stream.ReadAsync(responseBuffer, 0, responseBuffer.Length).DefaultTimeout();
+            Assert.Equal(responseBuffer.Length, read);
+            Assert.Equal("Hello World", Encoding.UTF8.GetString(responseBuffer));
+
+            read = await stream.ReadAsync(responseBuffer, 0, responseBuffer.Length).DefaultTimeout();
+            Assert.Equal(0, read);
+            stream.Dispose(); // https://github.com/dotnet/corefx/issues/39404 can be worked around by commenting out this Dispose
+
+            // Send one more message after the server has finished.
+            await streamingContent.SendAsync("Hello World").DefaultTimeout();
+            streamingContent.Complete();
+
+            var lastData = await lastPacket.Task.DefaultTimeout();
+            Assert.Equal("Hello World", lastData);
+
+            await host.StopAsync();
+        }
+
+        [ConditionalTheory(Skip = "https://github.com/dotnet/corefx/issues/39404")]
+        [MemberData(nameof(SupportedSchemes))]
+        public async Task ReverseEcho(string scheme)
+        {
+            var clientEcho = new TaskCompletionSource<string>(TaskCreationOptions.RunContinuationsAsynchronously);
+            var hostBuilder = new HostBuilder()
+                .ConfigureWebHost(webHostBuilder =>
+                {
+                    ConfigureKestrel(webHostBuilder, scheme);
+                    webHostBuilder.ConfigureServices(AddTestLogging)
+                    .Configure(app => app.Run(async context =>
+                    {
+                        // Prime it?
+                        // var readTask = context.Request.BodyReader.ReadAsync();
+                        context.Response.ContentType = "text/plain";
+                        await context.Response.WriteAsync("Hello World");
+                        await context.Features.Get<IHttpResponseCompletionFeature>().CompleteAsync();
+
+                        try
+                        {
+                            // var readResult = await readTask;
+                            // context.Request.BodyReader.AdvanceTo(readResult.Buffer.Start, readResult.Buffer.End);
+                            using var streamReader = new StreamReader(context.Request.Body);
+                            var read = await streamReader.ReadToEndAsync();
+                            clientEcho.SetResult(read);
+                        }
+                        catch (Exception ex)
+                        {
+                            clientEcho.SetException(ex);
+                        }
+                    }));
+                });
+            using var host = await hostBuilder.StartAsync();
+
+            var url = $"{scheme}://127.0.0.1:{host.GetPort().ToString(CultureInfo.InvariantCulture)}/";
+
+            using var client = CreateClient();
+            // client.DefaultRequestHeaders.ExpectContinue = true;
+
+            var streamingContent = new StreamingContent();
+            var request = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Version = HttpVersion.Version20,
+                Content = streamingContent,
+            };
+            using var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).DefaultTimeout();
+
+            Assert.Equal(HttpVersion.Version20, response.Version);
+
+            // Read Hello World and echo it back to the server.
+            /* https://github.com/dotnet/corefx/issues/39404
+            var read = await response.Content.ReadAsStringAsync().DefaultTimeout();
+            Assert.Equal("Hello World", read);
+            */
+            var stream = await response.Content.ReadAsStreamAsync();
+            var responseBuffer = new byte["Hello World".Length];
+
+            var read = await stream.ReadAsync(responseBuffer, 0, responseBuffer.Length).DefaultTimeout();
+            Assert.Equal(responseBuffer.Length, read);
+            Assert.Equal("Hello World", Encoding.UTF8.GetString(responseBuffer));
+
+            read = await stream.ReadAsync(responseBuffer, 0, responseBuffer.Length).DefaultTimeout();
+            Assert.Equal(0, read);
+            stream.Dispose(); // https://github.com/dotnet/corefx/issues/39404 can be worked around by commenting out this Dispose
+
+            await streamingContent.SendAsync("Hello World").DefaultTimeout();
+            streamingContent.Complete();
+
+            Assert.Equal("Hello World", await clientEcho.Task.DefaultTimeout());
+            await host.StopAsync();
+        }
+
+        private class StreamingContent : HttpContent
+        {
+            private TaskCompletionSource<int> _sendStarted = new TaskCompletionSource<int>(TaskCreationOptions.RunContinuationsAsynchronously);
+            private Func<string, Task> _sendContent;
+            private TaskCompletionSource<int> _sendComplete;
+
+            public StreamingContent()
+            {
+            }
+
+            public async Task SendAsync(string text)
+            {
+                await _sendStarted.Task;
+                await _sendContent(text);
+            }
+
+            public void Complete()
+            {
+                if (_sendComplete == null)
+                {
+                    throw new InvalidOperationException("Sending hasn't started yet.");
+                }
+                _sendComplete.TrySetResult(0);
+            }
+
+            protected override Task SerializeToStreamAsync(Stream stream, TransportContext context)
+            {
+                _sendComplete = new TaskCompletionSource<int>(TaskCreationOptions.RunContinuationsAsynchronously);
+                _sendContent = async text =>
+                {
+                    try
+                    {
+                        var bytes = Encoding.UTF8.GetBytes(text);
+                        await stream.WriteAsync(bytes);
+                    }
+                    catch (Exception ex)
+                    {
+                        _sendComplete.TrySetException(ex);
+                    }
+                };
+                _sendStarted.SetResult(0);
+                return _sendComplete.Task;
+            }
+
+            protected override bool TryComputeLength(out long length)
+            {
+                length = 0;
+                return false;
             }
         }
 
