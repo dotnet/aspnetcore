a3235afa19713e165d4db485fde9c5f3479885f9
MikaelMengistu <ethmengistu7@gmail.com>
2019-07-10 18:12:35 -0700

    clean up

--- src/SignalR/clients/java/signalr/src/test/java/com/microsoft/signalr/WebSocketTransportUrlFormatTest.java
+++ src/SignalR/clients/java/signalr/src/test/java/com/microsoft/signalr/WebSocketTransportUrlFormatTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-
 class WebSocketTransportUrlFormatTest {
     private static Stream<Arguments> protocols() {
         return Stream.of(
