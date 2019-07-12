7c73b91e0c296846de216cf25c3a62ec51421acc
John Luo <johluo@microsoft.com>
2019-07-12 13:22:29 -0700

    Build Web.JS before other projects

--- src/Components/build.cmd
+++ src/Components/build.cmd
@@ -1,3 +1,19 @@
 @ECHO OFF
 SET RepoRoot=%~dp0..\..
-%RepoRoot%\build.cmd -projects %~dp0**\*.*proj "/p:EnforceE2ETestPrerequisites=true" %*
+
+REM Building Web.JS first explicitly to workaround ordering issues between csproj and npmproj.
+ECHO Building Web.JS
+CALL %RepoRoot%\build.cmd -projects %~dp0Web.JS\Microsoft.AspNetCore.Components.Web.JS.npmproj "/p:EnforceE2ETestPrerequisites=true" %*
+
+IF %ERRORLEVEL% NEQ 0 (
+   EXIT /b %ErrorLevel%
+)
+
+ECHO Building Components
+CALL %RepoRoot%\build.cmd -projects %~dp0**\*.*proj "/p:EnforceE2ETestPrerequisites=true" %*
+
+IF %ERRORLEVEL% NEQ 0 (
+   EXIT /b %ErrorLevel%
+)
+
+ECHO Components successfully built!
