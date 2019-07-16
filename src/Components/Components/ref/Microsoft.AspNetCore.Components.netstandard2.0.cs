6f05f83a97e3b41dde7ba1c243b4857eda35ec3a
Pranav K <prkrishn@hotmail.com>
2019-07-16 09:36:15 -0700

    Use a JsonConverter with ElementRef

--- src/Components/Components/ref/Microsoft.AspNetCore.Components.netstandard2.0.cs
+++ src/Components/Components/ref/Microsoft.AspNetCore.Components.netstandard2.0.cs
@@ -154,8 +154,6 @@ namespace Microsoft.AspNetCore.Components
     public readonly partial struct ElementReference
     {
         private readonly object _dummy;
-        [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
-        public string __internalId { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } }
     }
     [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential)]
     public readonly partial struct EventCallback
