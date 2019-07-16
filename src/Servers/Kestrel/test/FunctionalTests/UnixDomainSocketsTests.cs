8f2fac74590f2f6fe43b9f0abb1721f07566c17c
Mehmet Can Kamar <mehmetcankamar@gmail.com>
2019-07-16 22:14:22 +0300

    functional tests for unixdomainsocket with url is completed

--- src/Servers/Kestrel/test/FunctionalTests/UnixDomainSocketsTests.cs
+++ src/Servers/Kestrel/test/FunctionalTests/UnixDomainSocketsTests.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.Connections.Features;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Testing;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Testing;
@@ -106,6 +107,99 @@ namespace Microsoft.AspNetCore.Server.Kestrel.FunctionalTests
                     await serverConnectionCompletedTcs.Task.DefaultTimeout();
 
                     await host.StopAsync().DefaultTimeout();
+                }
+            }
+            finally
+            {
+                Delete(path);
+            }
+        }
+
+#if LIBUV
+        [OSSkipCondition(OperatingSystems.Windows, SkipReason = "Libuv does not support unix domain sockets on Windows.")]
+#else
+        [OSSkipCondition(OperatingSystems.Windows, WindowsVersions.Win7, WindowsVersions.Win8, WindowsVersions.Win81, WindowsVersions.Win2008R2, SkipReason = "UnixDomainSocketEndPoint is not supported on older versions of Windows")]
+#endif
+        [ConditionalFact]
+        [CollectDump]
+        public async Task TestUnixDomainSocketWithUrl()
+        {
+            var path = Path.GetTempFileName();
+            var url = $"http://unix:/{path}";
+
+            Delete(path);
+
+            try
+            {
+                var hostBuilder = TransportSelector.GetWebHostBuilder()
+                    .UseUrls(url)
+                    .UseKestrel()
+                    .ConfigureServices(AddTestLogging)
+                    .Configure(app =>
+                    {
+                        app.Run(async context =>
+                        {
+                            var buffer = new byte[(int)context.Request.ContentLength];
+
+                            await context.Request.Body.ReadAsync(buffer);
+                            await context.Response.WriteAsync(Encoding.ASCII.GetString(buffer));
+                        });
+                    });
+
+
+                using (var host = hostBuilder.Build())
+                {
+                    await host.StartAsync();
+
+                    using (var socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified))
+                    {
+                        await socket.ConnectAsync(new UnixDomainSocketEndPoint(path));
+
+                        var data = $"Hello World {Path.GetRandomFileName()}";
+
+                        StringBuilder httpRequest = new StringBuilder();
+                        httpRequest.AppendLine($"POST / HTTP/1.1");
+                        httpRequest.AppendLine($"Host: localhost");
+                        httpRequest.AppendLine($"Content-Length: {data.Length}");
+                        httpRequest.AppendLine();
+                        httpRequest.AppendLine(data);
+
+
+                        // https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.networkstream.dataavailable?view=netcore-3.0
+                        using (var stream = new NetworkStream(socket))
+                        {
+                            var requestData = Encoding.UTF8.GetBytes(httpRequest.ToString());
+                            stream.Write(requestData, 0, requestData.Length);
+
+                            if (stream.CanRead)
+                            {
+
+                                byte[] myReadBuffer = new byte[1024];
+                                StringBuilder myCompleteMessage = new StringBuilder();
+                                int numberOfBytesRead = 0;
+
+                                // Incoming message may be larger than the buffer size.
+                                do
+                                {
+                                    numberOfBytesRead = stream.Read(myReadBuffer, 0, myReadBuffer.Length);
+
+                                    myCompleteMessage.AppendFormat("{0}", Encoding.ASCII.GetString(myReadBuffer, 0, numberOfBytesRead));
+
+                                }
+                                while (stream.DataAvailable);
+
+                                // https://github.com/dotnet/corefx/issues/5999
+                                // .NET Core HttpClient does not support unix sockets, it's difficult to parse raw response data. below is a little hacky way.
+                                Assert.True(myCompleteMessage.ToString().IndexOf(data) > -1);
+                            }
+                            else
+                            {
+                                Assert.True(false, "Network stream cannot be read");
+                            }
+                        }
+                    }
+
+                    await host.StopAsync();
                 }
             }
             finally
