9c19577d497b283076676c36ae9173b7b9fddb13
Stephen Halter <halter73@gmail.com>
2019-07-18 15:43:56 -0700

    Log client errors as Debug (#12262)

--- src/Servers/Kestrel/Transport.Libuv/src/Internal/LibuvTrace.cs
+++ src/Servers/Kestrel/Transport.Libuv/src/Internal/LibuvTrace.cs
@@ -27,7 +27,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.Internal
         // ConnectionWriteCallback: Reserved: 12
 
         private static readonly Action<ILogger, string, Exception> _connectionError =
-            LoggerMessage.Define<string>(LogLevel.Information, new EventId(14, nameof(ConnectionError)), @"Connection id ""{ConnectionId}"" communication error.");
+            LoggerMessage.Define<string>(LogLevel.Debug, new EventId(14, nameof(ConnectionError)), @"Connection id ""{ConnectionId}"" communication error.");
 
         private static readonly Action<ILogger, string, Exception> _connectionReset =
             LoggerMessage.Define<string>(LogLevel.Debug, new EventId(19, nameof(ConnectionReset)), @"Connection id ""{ConnectionId}"" reset.");
