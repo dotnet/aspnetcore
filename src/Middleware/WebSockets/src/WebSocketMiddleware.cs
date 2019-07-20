cf0c84901dfdce444cef393f90b0c13ec905bf4c
BrennanConroy <brecon@microsoft.com>
2019-07-19 22:45:39 -0700

    cleanup

--- src/Middleware/WebSockets/src/WebSocketMiddleware.cs
+++ src/Middleware/WebSockets/src/WebSocketMiddleware.cs
@@ -3,17 +3,14 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Net.WebSockets;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
 using Microsoft.Net.Http.Headers;
