9675240e969e109b9ddc93bc46fb85062b8ba51d
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2019-07-20 11:00:59 -0700

    Reduce truncation of information in the runningProcesses*.txt files - move from deprecated `Get-WmiObject` to `Get-CimInstance`

--- eng/scripts/dump_process.ps1
+++ eng/scripts/dump_process.ps1
@@ -3,7 +3,7 @@ Set-Location $args[0]
 $timestamp = $(get-date -f MM-dd-HH-mm)
 
 while ($true) {
-    Get-Process > artifacts/log/runningProcesses.$timestamp.txt
-    Get-WmiObject Win32_Process | select name, processid, commandline > artifacts/log/runningProcessesCommandLine.$timestamp.txt
+    Get-Process |Out-File -Width 300 artifacts/log/runningProcesses.$timestamp.txt
+    Get-CimInstance Win32_Process |select name, processid, commandline |Out-File -Width 800 artifacts/log/runningProcessesCommandLine.$timestamp.txt
     Start-Sleep -Seconds 300
 }
