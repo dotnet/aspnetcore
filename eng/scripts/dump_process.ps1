551231326b83300c6a761da79235edde4885706e
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2019-07-20 19:50:41 -0700

    Save previous process dump files - multiple files created today correspond to multiple '.\build.ps1' invocations

--- eng/scripts/dump_process.ps1
+++ eng/scripts/dump_process.ps1
@@ -3,10 +3,14 @@ $local:timestamp = $(get-date -f MM-dd-HH-mm)
 
 while ($true) {
     $local:file = "artifacts/log/runningProcesses.$timestamp.txt"
+    if (Test-Path $file) {Move-Item -Force $file "$file.bak"}
+
     $local:temp = Get-Process |Out-String -Width 300
     $temp.Split([Environment]::NewLine).TrimEnd() |? Length -gt 0 |Out-File -Width 300 $file
 
     $file = "artifacts/log/runningProcessesCommandLine.$timestamp.txt"
+    if (Test-Path $file) {Move-Item -Force $file "$file.bak"}
+
     $temp = Get-CimInstance Win32_Process |Format-Table -AutoSize Name, ProcessId, CommandLine |Out-String -Width 800
     $temp.Split([Environment]::NewLine).TrimEnd() |? Length -gt 0 |Out-File -Width 800 $file
 
