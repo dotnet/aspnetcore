a87d9dfec4271523001405e4539b6c4156304316
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2019-07-20 19:49:40 -0700

    Trim whitespace in process dump files

--- eng/scripts/dump_process.ps1
+++ eng/scripts/dump_process.ps1
@@ -2,8 +2,13 @@ Set-Location $args[0]
 $local:timestamp = $(get-date -f MM-dd-HH-mm)
 
 while ($true) {
-    Get-Process |Out-File -Width 300 artifacts/log/runningProcesses.$timestamp.txt
-    Get-CimInstance Win32_Process |select name, processid, commandline |Out-File -Width 800 artifacts/log/runningProcessesCommandLine.$timestamp.txt
+    $local:file = "artifacts/log/runningProcesses.$timestamp.txt"
+    $local:temp = Get-Process |Out-String -Width 300
+    $temp.Split([Environment]::NewLine).TrimEnd() |? Length -gt 0 |Out-File -Width 300 $file
+
+    $file = "artifacts/log/runningProcessesCommandLine.$timestamp.txt"
+    $temp = Get-CimInstance Win32_Process |Format-Table -AutoSize Name, ProcessId, CommandLine |Out-String -Width 800
+    $temp.Split([Environment]::NewLine).TrimEnd() |? Length -gt 0 |Out-File -Width 800 $file
 
     Start-Sleep -Seconds 120
 }
