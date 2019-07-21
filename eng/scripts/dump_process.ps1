30d262f04d9a9b143e161be9e59100e64420ed70
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2019-07-20 19:47:56 -0700

    Increase process dump frequency to once x 2 minutes (from 5 minutes)

--- eng/scripts/dump_process.ps1
+++ eng/scripts/dump_process.ps1
@@ -1,9 +1,9 @@
 Set-Location $args[0]
-
-$timestamp = $(get-date -f MM-dd-HH-mm)
+$local:timestamp = $(get-date -f MM-dd-HH-mm)
 
 while ($true) {
     Get-Process |Out-File -Width 300 artifacts/log/runningProcesses.$timestamp.txt
     Get-CimInstance Win32_Process |select name, processid, commandline |Out-File -Width 800 artifacts/log/runningProcessesCommandLine.$timestamp.txt
-    Start-Sleep -Seconds 300
+
+    Start-Sleep -Seconds 120
 }
