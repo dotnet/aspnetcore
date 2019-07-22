077df0e3ca6849df8173ca818693dbf1d99fa512
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-07-22 08:14:10 -0700

    Support more types of redirection during prerendering. Fixes #11591 (#12418)
    * E2E test to show current behavior
    
    * Actually support base-relative, root-relative, and absolute redirections during prerendering
    
    * Fix MVC functional test

--- src/Components/test/E2ETest/ServerExecutionTests/PrerenderingTest.cs
+++ src/Components/test/E2ETest/ServerExecutionTests/PrerenderingTest.cs
@@ -1,6 +1,10 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.E2ETest.Infrastructure;
 using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
 using Microsoft.AspNetCore.E2ETesting;
@@ -73,6 +77,24 @@ namespace Microsoft.AspNetCore.Components.E2ETest.ServerExecutionTests
             Browser.Equal(
                 _serverFixture.RootUri + url,
                 () => Browser.FindElement(By.TagName("strong")).Text);
+        }
+
+        [Theory]
+        [InlineData("base/relative", "prerendered/base/relative")]
+        [InlineData("/root/relative", "/root/relative")]
+        [InlineData("http://absolute/url", "http://absolute/url")]
+        public async Task CanRedirectDuringPrerendering(string destinationParam, string expectedRedirectionLocation)
+        {
+            var requestUri = new Uri(
+                _serverFixture.RootUri,
+                "prerendered/prerendered-redirection?destination=" + destinationParam);
+
+            var httpClient = new HttpClient(new HttpClientHandler { AllowAutoRedirect = false });
+            var response = await httpClient.GetAsync(requestUri);
+
+            var expectedUri = new Uri(_serverFixture.RootUri, expectedRedirectionLocation);
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+            Assert.Equal(expectedUri, response.Headers.Location);
         }
 
         private void BeginInteractivity()
