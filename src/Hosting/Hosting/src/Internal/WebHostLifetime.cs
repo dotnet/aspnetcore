a7f392a4d7d147009dcbf91f26787332b3c42d7c
Justin Kotalik <jukotali@microsoft.com>
2019-07-24 10:58:12 -0700

    Comment

--- src/Hosting/Hosting/src/Internal/WebHostLifetime.cs
+++ src/Hosting/Hosting/src/Internal/WebHostLifetime.cs
@@ -73,6 +73,9 @@ namespace Microsoft.AspNetCore.Hosting
                     _cts.Cancel();
                 }
             }
+            // When hosting with IIS in-process, we detach the Console handle on main thread exit.
+            // Console.WriteLine may throw here as we are logging to console on ProcessExit.
+            // We catch and ignore all exceptions here. Do not log to Console in thie exception handler.
             catch (Exception) {}
             // Wait on the given reset event
             _resetEvent.Wait();
