f066d9eceaf5a11bb1eeea9aac7c5668e07fd954
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-07-29 06:10:21 -0700

    [ApiAuthorization] Updates dependency versions * Updates to the latest version of Identity Server. * Updates to the latest version of oidc-client. * Removes unncessary code from the templates. * Updates EF migrations.

--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/Angular-CSharp/Data/SQLite/00000000000000_CreateIdentitySchema.cs
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/Angular-CSharp/Data/SQLite/00000000000000_CreateIdentitySchema.cs
@@ -230,9 +230,14 @@ namespace Company.WebApplication1.Data.Migrations
                 unique: true);
 
             migrationBuilder.CreateIndex(
-                name: "IX_PersistedGrants_SubjectId_ClientId_Type",
+                name: "IX_DeviceCodes_Expiration",
+                table: "DeviceCodes",
+                column: "Expiration");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersistedGrants_SubjectId_ClientId_Type_Expiration",
                 table: "PersistedGrants",
-                columns: new[] { "SubjectId", "ClientId", "Type" });
+                columns: new[] { "SubjectId", "ClientId", "Type", "Expiration" });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
