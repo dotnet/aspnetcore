c45e4e3fc63818034a06e21fb37c67b51e28663f
Justin Kotalik <jukotali@microsoft.com>
2019-07-30 14:41:23 -0700

    Can't remove throws

--- src/Servers/Kestrel/Core/src/Internal/Http2/Http2Connection.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http2/Http2Connection.cs
@@ -1323,6 +1323,16 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2
                     }
                 }
             }
+            catch (OperationCanceledException ex)
+            {
+                // Propagate the exception if it's ConnectionAbortedException		
+                error = ex as ConnectionAbortedException;
+            }
+            catch (Exception ex)
+            {
+                // Don't rethrow the exception. It should be handled by the Pipeline consumer.		
+                error = ex;
+            }
             finally
             {
                 await _context.Transport.Input.CompleteAsync();
