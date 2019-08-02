02d28e1c54e1264e713e8c3e0f77589c46f73ecf
Justin Kotalik <jkotalik12@gmail.com>
2019-08-02 08:02:59 -0700

    Only use read Pipe when running on Http2.  (#12737)

--- src/Servers/Kestrel/Core/src/Middleware/Internal/DuplexPipeStreamAdapter.cs
+++ src/Servers/Kestrel/Core/src/Middleware/Internal/DuplexPipeStreamAdapter.cs
@@ -5,7 +5,6 @@ using System;
 using System.IO;
 using System.IO.Pipelines;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Connections;
 
 namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
 {
@@ -15,11 +14,8 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
     /// <typeparam name="TStream"></typeparam>
     internal class DuplexPipeStreamAdapter<TStream> : DuplexPipeStream, IDuplexPipe where TStream : Stream
     {
-        private readonly Pipe _input;
-        private Task _inputTask;
         private bool _disposed;
         private readonly object _disposeLock = new object();
-        private readonly int _minAllocBufferSize;
 
         public DuplexPipeStreamAdapter(IDuplexPipe duplexPipe, Func<Stream, TStream> createStream) :
             this(duplexPipe, new StreamPipeReaderOptions(leaveOpen: true), new StreamPipeWriterOptions(leaveOpen: true), createStream)
@@ -27,38 +23,17 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
         }
 
         public DuplexPipeStreamAdapter(IDuplexPipe duplexPipe, StreamPipeReaderOptions readerOptions, StreamPipeWriterOptions writerOptions, Func<Stream, TStream> createStream) :
-            base(duplexPipe.Input, duplexPipe.Output, throwOnCancelled: true)
+            base(duplexPipe.Input, duplexPipe.Output)
         {
-            Stream = createStream(this);
-
-            var inputOptions = new PipeOptions(pool: readerOptions.Pool,
-                readerScheduler: PipeScheduler.ThreadPool,
-                writerScheduler: PipeScheduler.Inline,
-                pauseWriterThreshold: 1,
-                resumeWriterThreshold: 1,
-                minimumSegmentSize: readerOptions.Pool.GetMinimumSegmentSize(),
-                useSynchronizationContext: false);
-
-            _minAllocBufferSize = writerOptions.MinimumBufferSize;
-
-            _input = new Pipe(inputOptions);
-            Output = PipeWriter.Create(Stream, writerOptions);
+            var stream = createStream(this);
+            Stream = stream;
+            Input = PipeReader.Create(stream, readerOptions);
+            Output = PipeWriter.Create(stream, writerOptions);
         }
 
         public TStream Stream { get; }
 
-        public PipeReader Input
-        {
-            get
-            {
-                if (_inputTask == null)
-                {
-                    _inputTask = ReadInputAsync();
-                }
-
-                return _input.Reader;
-            }
-        }
+        public PipeReader Input { get; }
 
         public PipeWriter Output { get; }
 
@@ -73,64 +48,13 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
                 _disposed = true;
             }
 
-            _input.Reader.Complete();
-            Output.Complete();
-
-            CancelPendingRead();
-
-            if (_inputTask != null)
-            {
-                await _inputTask;
-            }
+            await Input.CompleteAsync();
+            await Output.CompleteAsync();
         }
 
         protected override void Dispose(bool disposing)
         {
             throw new NotSupportedException();
-        }
-
-        private async Task ReadInputAsync()
-        {
-            Exception error = null;
-            try
-            {
-                while (true)
-                {
-                    var outputBuffer = _input.Writer.GetMemory(_minAllocBufferSize);
-
-                    var bytesRead = await Stream.ReadAsync(outputBuffer);
-                    _input.Writer.Advance(bytesRead);
-
-                    if (bytesRead == 0)
-                    {
-                        // FIN
-                        break;
-                    }
-
-                    var result = await _input.Writer.FlushAsync();
-
-                    if (result.IsCompleted)
-                    {
-                        // flushResult should not be canceled.
-                        break;
-                    }
-                }
-
-            }
-            catch (OperationCanceledException ex)
-            {
-                // Propagate the exception if it's ConnectionAbortedException	
-                error = ex as ConnectionAbortedException;
-            }
-            catch (Exception ex)
-            {
-                // Don't rethrow the exception. It should be handled by the Pipeline consumer.	
-                error = ex;
-            }
-            finally
-            {
-                _input.Writer.Complete(error);
-            }
         }
     }
 }
