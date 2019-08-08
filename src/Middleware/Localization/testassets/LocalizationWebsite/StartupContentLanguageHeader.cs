a927d7ff3fc8242570a8516950cb062c466011ac
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2019-08-08 10:07:46 +0300

    Add missing namespaces

--- src/Middleware/Localization/testassets/LocalizationWebsite/StartupContentLanguageHeader.cs
+++ src/Middleware/Localization/testassets/LocalizationWebsite/StartupContentLanguageHeader.cs
@@ -1,8 +1,11 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
+using System.Globalization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Net.Http.Headers;
 
