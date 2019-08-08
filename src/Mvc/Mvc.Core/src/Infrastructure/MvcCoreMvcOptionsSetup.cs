d6d4bb27726b59c50df865a190bfc711be823a81
Pranav K <prkrishn@hotmail.com>
2019-08-08 12:28:54 -0700

    Ensure IFormFile binding for nested properties works (#12847)
    * Ensure IFormFile binding for nested properties works
    
    Fixes https://github.com/aspnet/AspNetCore/issues/9510

--- src/Mvc/Mvc.Core/src/Infrastructure/MvcCoreMvcOptionsSetup.cs
+++ src/Mvc/Mvc.Core/src/Infrastructure/MvcCoreMvcOptionsSetup.cs
@@ -96,6 +96,7 @@ namespace Microsoft.AspNetCore.Mvc
             options.ValueProviderFactories.Add(new RouteValueProviderFactory());
             options.ValueProviderFactories.Add(new QueryStringValueProviderFactory());
             options.ValueProviderFactories.Add(new JQueryFormValueProviderFactory());
+            options.ValueProviderFactories.Add(new FormFileValueProviderFactory());
 
             // Set up metadata providers
             ConfigureAdditionalModelMetadataDetailsProviders(options.ModelMetadataDetailsProviders);
