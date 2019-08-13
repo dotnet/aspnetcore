9bd027aa999ea08dfc2b82025002210b59b4ab34
Pranav K <prkrishn@hotmail.com>
2019-08-13 12:49:10 -0700

    Make Partial on PageBase and PageModel work correctly (#13013)
    * Make Partial on PageBase and PageModel work correctly
    
    Fixes https://github.com/aspnet/AspNetCore/issues/10438

--- src/Mvc/samples/MvcSandbox/Pages/PagesHome.cshtml.cs
+++ src/Mvc/samples/MvcSandbox/Pages/PagesHome.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MvcSandbox
 {
-    public class TestModel : PageModel
+    public class PagesHome : PageModel
     {
         public string Name { get; private set; } = "World";
 
