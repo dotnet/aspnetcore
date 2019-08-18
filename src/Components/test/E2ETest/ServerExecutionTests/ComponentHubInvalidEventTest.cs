be2a71855bb62c51846e20e624990da4783792ab
Ryan Nowak <nowakra@gmail.com>
2019-08-17 18:34:10 -0700

    Improve reliability of reliability tests
    Fixes: #13086
    
    - Fixed some wrong logging (wrong EventIds)
    - Added a base class for ignitor tests
    - Centralized code for capturing data in ignitor
    - Make `WaitForXyz` methods return the thing they found
    - Make `WaitForXyz` methods report logs on timeout
    - Fix synchronization problems with some tests
    - Split invalid event tests into their own class for code reuse
    
    The main thing here is that some of the JS interop reliability tests had
    causality/synchronization problems. They were either not waiting for a
    render batch, or they were not *triggering* a render batch.
    
    In addition to that, I did a general consolidation of the infrastructure
    for these tests to make sure that they are all using the same set of
    practices. I found and fixed cases where tests were using timeouts that
    were too small (inconsistency) or where they were using non-threadsafe
    data structures (these tests always involve concurrency).

---
+++ src/Components/test/E2ETest/ServerExecutionTests/ComponentHubInvalidEventTest.cs
@@ -0,0 +1,138 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.E2ETest;
+using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
+using Microsoft.AspNetCore.Components.RenderTree;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.AspNetCore.Components.E2ETests.ServerExecutionTests
+{
+    public class ComponentHubInvalidEventTest : IgnitorTest<AspNetSiteServerFixture>
+    {
+        public ComponentHubInvalidEventTest(AspNetSiteServerFixture serverFixture, ITestOutputHelper output)
+            : base(serverFixture, output)
+        {
+        }
+
+        protected override void InitializeFixture(AspNetSiteServerFixture serverFixture)
+        {
+            serverFixture.BuildWebHostMethod = TestServer.Program.BuildWebHost;
+        }
+
+        protected async override Task InitializeAsync()
+        {
+            var rootUri = ServerFixture.RootUri;
+            Assert.True(await Client.ConnectAsync(new Uri(rootUri, "/subdir")), "Couldn't connect to the app");
+            Assert.Single(Batches);
+
+            await Client.SelectAsync("test-selector-select", "BasicTestApp.CounterComponent");
+            Assert.Equal(2, Batches.Count);
+        }
+
+        [Fact]
+        public async Task DispatchingAnInvalidEventArgument_DoesNotProduceWarnings()
+        {
+            // Arrange
+            var expectedError = $"There was an unhandled exception on the current circuit, so this circuit will be terminated. For more details turn on " +
+                $"detailed exceptions in 'CircuitOptions.DetailedErrors'. Bad input data.";
+
+            var eventDescriptor = Serialize(new WebEventDescriptor()
+            {
+                BrowserRendererId = 0,
+                EventHandlerId = 3,
+                EventArgsType = "mouse",
+            });
+
+            // Act
+            await Client.ExpectCircuitError(() => Client.HubConnection.SendAsync(
+                "DispatchBrowserEvent",
+                eventDescriptor,
+                "{sadfadsf]"));
+
+            // Assert
+            var actualError = Assert.Single(Errors);
+            Assert.Equal(expectedError, actualError);
+            Assert.DoesNotContain(Logs, l => l.LogLevel > LogLevel.Information);
+            Assert.Contains(Logs, l => (l.LogLevel, l.Exception?.Message) == (LogLevel.Debug, "There was an error parsing the event arguments. EventId: '3'."));
+        }
+
+        [Fact]
+        public async Task DispatchingAnInvalidEvent_DoesNotTriggerWarnings()
+        {
+            // Arrange
+            var expectedError = $"There was an unhandled exception on the current circuit, so this circuit will be terminated. For more details turn on " +
+                $"detailed exceptions in 'CircuitOptions.DetailedErrors'. Failed to dispatch event.";
+
+            var eventDescriptor = Serialize(new WebEventDescriptor()
+            {
+                BrowserRendererId = 0,
+                EventHandlerId = 1990,
+                EventArgsType = "mouse",
+            });
+
+            var eventArgs = new MouseEventArgs
+            {
+                Type = "click",
+                Detail = 1,
+                ScreenX = 47,
+                ScreenY = 258,
+                ClientX = 47,
+                ClientY = 155,
+            };
+
+            // Act
+            await Client.ExpectCircuitError(() => Client.HubConnection.SendAsync(
+                "DispatchBrowserEvent",
+                eventDescriptor,
+                Serialize(eventArgs)));
+
+            // Assert
+            var actualError = Assert.Single(Errors);
+            Assert.Equal(expectedError, actualError);
+            Assert.DoesNotContain(Logs, l => l.LogLevel > LogLevel.Information);
+            Assert.Contains(Logs, l => (l.LogLevel, l.Message, l.Exception?.Message) ==
+                (LogLevel.Debug,
+                "There was an error dispatching the event '1990' to the application.",
+                "There is no event handler associated with this event. EventId: '1990'. (Parameter 'eventHandlerId')"));
+        }
+
+        [Fact]
+        public async Task DispatchingAnInvalidRenderAcknowledgement_DoesNotTriggerWarnings()
+        {
+            // Arrange
+            var expectedError = $"There was an unhandled exception on the current circuit, so this circuit will be terminated. For more details turn on " +
+                $"detailed exceptions in 'CircuitOptions.DetailedErrors'. Failed to complete render batch '1846'.";
+
+
+            Client.ConfirmRenderBatch = false;
+            await Client.ClickAsync("counter");
+
+            // Act
+            await Client.ExpectCircuitError(() => Client.HubConnection.SendAsync(
+                "OnRenderCompleted",
+                1846,
+                null));
+
+            // Assert
+            var actualError = Assert.Single(Errors);
+            Assert.Equal(expectedError, actualError);
+            Assert.DoesNotContain(Logs, l => l.LogLevel > LogLevel.Information);
+
+            var entry = Assert.Single(Logs, l => l.EventId.Name == "OnRenderCompletedFailed");
+            Assert.Equal(LogLevel.Debug, entry.LogLevel);
+            Assert.Matches("Failed to complete render batch '1846' in circuit host '.*'\\.", entry.Message);
+            Assert.Equal("Received an acknowledgement for batch with id '1846' when the last batch produced was '4'.", entry.Exception.Message);
+        }
+
+        private string Serialize<T>(T browserEventDescriptor) =>
+            JsonSerializer.Serialize(browserEventDescriptor, TestJsonSerializerOptionsProvider.Options);
+    }
+}
