638ee794fb8769435c899f9e51298633b3006588
Mikael Mengistu <mimengis@microsoft.com>
2019-08-21 16:14:01 -0700

    update

--- src/SignalR/server/SignalR/test/UserAgentHeaderTest.cs
+++ src/SignalR/server/SignalR/test/UserAgentHeaderTest.cs
@@ -1,6 +1,9 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.Http.Connections.Client;
 using Xunit;
 using Constants = Microsoft.AspNetCore.Http.Connections.Client.Internal.Constants;
@@ -12,14 +15,20 @@ namespace Microsoft.AspNetCore.Http.Connections.Tests
         [Fact]
         public void UserAgentHeaderIsAccurate()
         {
-            var userAgent = Constants.UserAgentHeader;
-            Assert.NotNull(userAgent);
-            Assert.StartsWith("Microsoft SignalR/", userAgent);
-
             var majorVersion = typeof(HttpConnection).Assembly.GetName().Version.Major;
             var minorVersion = typeof(HttpConnection).Assembly.GetName().Version.Minor;
+            var version = typeof(HttpConnection).Assembly.GetName().Version;
+            var os = RuntimeInformation.OSDescription;
+            var runtime = ".NET";
+            var runtimeVersion = RuntimeInformation.FrameworkDescription;
+            var assemblyVersion = typeof(Constants)
+                .Assembly
+                .GetCustomAttributes<AssemblyInformationalVersionAttribute>()
+                .FirstOrDefault();
+            var userAgent = Constants.UserAgentHeader;
+            var expectedUserAgent = $"Microsoft SignalR/{majorVersion}.{minorVersion} ({assemblyVersion.InformationalVersion}; {os}; {runtime}; {runtimeVersion})";
 
-            Assert.Contains($"{majorVersion}.{minorVersion}", userAgent);
+            Assert.Equal(expectedUserAgent, userAgent);
         }
     }
 }
