355744a05e44703a5d6e0c76f331f6634c6842da
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-09-11 07:47:16 -0700

    [Blazor] Move all test projects to Generic host

--- src/Components/Blazor/testassets/HostedInAspNet.Server/Program.cs
+++ src/Components/Blazor/testassets/HostedInAspNet.Server/Program.cs
@@ -1,9 +1,8 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace HostedInAspNet.Server
 {
@@ -14,12 +13,12 @@ namespace HostedInAspNet.Server
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseConfiguration(new ConfigurationBuilder()
-                    .AddCommandLine(args)
-                    .Build())
-                .UseStartup<Startup>()
-                .Build();
+        public static IHost BuildWebHost(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webHostBuilder =>
+            {
+                webHostBuilder.UseStartup<Startup>();
+            })
+            .Build();
     }
 }
