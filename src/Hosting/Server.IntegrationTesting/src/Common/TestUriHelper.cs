f6b3cc6d43f75c7fd2d5addac7dd4f059f75aec6
Justin Kotalik <jkotalik12@gmail.com>
2019-09-10 20:13:31 -0700

    Update TestUriHelper.cs

--- src/Hosting/Server.IntegrationTesting/src/Common/TestUriHelper.cs
+++ src/Hosting/Server.IntegrationTesting/src/Common/TestUriHelper.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Diagnostics;
+
 namespace Microsoft.AspNetCore.Server.IntegrationTesting.Common
 {
     public static class TestUriHelper
