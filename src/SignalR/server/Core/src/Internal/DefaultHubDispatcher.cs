a614daa72d9b1646713e0923216482ef3c21d95c
Kahbazi <A.Kahbazi@gmail.com>
2019-09-15 12:18:06 +0430

    Fix null reference exception

--- src/SignalR/server/Core/src/Internal/DefaultHubDispatcher.cs
+++ src/SignalR/server/Core/src/Internal/DefaultHubDispatcher.cs
@@ -265,7 +265,7 @@ namespace Microsoft.AspNetCore.SignalR.Internal
                         for (var parameterPointer = 0; parameterPointer < arguments.Length; parameterPointer++)
                         {
                             if (hubMethodInvocationMessage.Arguments.Length > hubInvocationArgumentPointer &&
-                                descriptor.OriginalParameterTypes[parameterPointer].IsAssignableFrom(hubMethodInvocationMessage.Arguments[hubInvocationArgumentPointer].GetType()))
+                                descriptor.OriginalParameterTypes[parameterPointer].IsAssignableFrom(hubMethodInvocationMessage.Arguments[hubInvocationArgumentPointer]?.GetType()))
                             {
                                 // The types match so it isn't a synthetic argument, just copy it into the arguments array
                                 arguments[parameterPointer] = hubMethodInvocationMessage.Arguments[hubInvocationArgumentPointer];
