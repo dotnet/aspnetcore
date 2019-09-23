1ffa904a2c4f51f4e7d229d01f5d33f8e91b973d
Saravana Kumar <saravananbscitm@gmail.com>
2019-09-23 12:51:00 +0530

    Get host name using request host.

--- src/Middleware/Rewrite/src/PatternSegments/HttpHostNameSegment.cs
+++ src/Middleware/Rewrite/src/PatternSegments/HttpHostNameSegment.cs
@@ -1,17 +1,13 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.Net.Http.Headers;
-
 namespace Microsoft.AspNetCore.Rewrite.PatternSegments
 {
     internal class HttpHostNameSegment : PatternSegment
     {
         public override string Evaluate(RewriteContext context, BackReferenceCollection ruleBackReferences, BackReferenceCollection conditionBackReferences)
         {
-            var host = context.HttpContext.Request.Headers[HeaderNames.Host].ToString();
-            var indexOfSeperator = host.IndexOf(':');
-            return indexOfSeperator == -1 ? host : host.Remove(indexOfSeperator);
+            return context.HttpContext.Request.Host.Host;;
         }
     }
 }
