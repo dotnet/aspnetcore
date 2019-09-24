5256b7683eb95d033115d342751393168584c8ad
Pranav K <prkrishn@hotmail.com>
2019-09-24 15:08:51 -0700

    Throw when UseAuthorization is incorrectly configured
    * Update AuthZ & Cors middlewares to only set endpoint routing metadata when
      executing in the context of endpoint routing
    * Add analyzers for incorrect UseAuth use
    
    Fixes https://github.com/aspnet/AspNetCore/issues/14049

--- src/Analyzers/Analyzers/test/TestFiles/StartupAnalyzerTest/UseAuthAfterUseEndpoints.cs
+++ src/Analyzers/Analyzers/test/TestFiles/StartupAnalyzerTest/UseAuthAfterUseEndpoints.cs
@@ -2,23 +2,16 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.AspNetCore.Analyzers.TestFiles.StartupAnalyzerTest
 {
-    public class MvcOptions_UseMvcAndConfiguredRoutes
+    public class UseAuthAfterUseEndpoints
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
-
         public void Configure(IApplicationBuilder app)
         {
-            /*MM*/app.UseMvc(routes =>
-            {
-                routes.MapRoute("Name", "Template");
-            });
+            app.UseRouting();
+            app.UseEndpoints(r => { });
+            /*MM*/app.UseAuthorization();
         }
     }
 }
