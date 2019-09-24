5d6db09b1ded5b5095b09017b76e9494736a255e
Ryan Nowak <nowakra@gmail.com>
2019-09-24 10:21:06 -0700

    React to change in logging testing

--- src/Servers/Kestrel/test/InMemory.FunctionalTests/Properties/AssemblyInfo.cs
+++ src/Servers/Kestrel/test/InMemory.FunctionalTests/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Testing;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Testing;
 
