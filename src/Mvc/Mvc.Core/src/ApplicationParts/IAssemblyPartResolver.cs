edbe96506f5d6e0bf4a8e5cfddffc9c101f5ca07
Andreas Müller <taori@users.noreply.github.com>
2019-09-25 07:27:14 +0200

    This fixes #aspnet/AspNetCore/issues/14356 and #aspnet/AspNetCore/issues/14359

---
+++ src/Mvc/Mvc.Core/src/ApplicationParts/IAssemblyPartResolver.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.AspNetCore.Mvc.ApplicationParts
+{
+    public interface IAssemblyPartResolver
+    {
+        /// <summary>
+        /// When implementing this interface you should make sure to recursively handle the dependencies which result from referencing a given part
+        /// </summary>
+        /// <param name="assemblyPart"></param>
+        /// <returns>All references which are dependencies of this application part.</returns>
+        IEnumerable<string> GetReferencePaths(AssemblyPart assemblyPart);
+    }
+}
---
+++ src/Mvc/Mvc.Razor.RuntimeCompilation/src/AssemblyPartResolvers.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+
+namespace Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation
+{
+    /// <summary>
+    /// This class is a workaround to enable compilation of plugin references which DependencyContext is unaware of
+    /// </summary>
+    internal class DependencyContextResolver : IAssemblyPartResolver
+    {
+        public IEnumerable<string> GetReferencePaths(AssemblyPart assemblyPart)
+        {
+            IEnumerable<string> references;
+            try
+            {
+                references = assemblyPart.GetReferencePaths();
+            }
+            catch (InvalidOperationException)
+            {
+                // DependencyContext was unable to resolve the dependencies because it is unaware of additional references.
+                yield break;
+            }
+
+            foreach (var reference in references)
+            {
+                yield return reference;
+            }
+        }
+    }
+
+    internal class CompileOptionsPartResolver : IAssemblyPartResolver
+    {
+        private readonly HashSet<string> _optionReferences;
+
+        internal CompileOptionsPartResolver(HashSet<string> optionReferences)
+        {
+            _optionReferences = optionReferences ?? throw new ArgumentNullException(nameof(optionReferences));
+        }
+
+        public IEnumerable<string> GetReferencePaths(AssemblyPart assemblyPart)
+        {
+            if (assemblyPart?.Assembly == null)
+                yield break;
+
+            if (_optionReferences.Contains(assemblyPart.Assembly.Location))
+                yield return assemblyPart.Assembly.Location;
+        }
+    }
+
+    internal class CompositeAssemblyPartResolver : IAssemblyPartResolver
+    {
+        private readonly IAssemblyPartResolver[] _resolvers;
+
+        internal CompositeAssemblyPartResolver(IAssemblyPartResolver[] resolvers)
+        {
+            _resolvers = resolvers;
+        }
+
+        public IEnumerable<string> GetReferencePaths(AssemblyPart assemblyPart)
+        {
+            return _resolvers.SelectMany(resolver => resolver.GetReferencePaths(assemblyPart)).ToHashSet();
+        }
+    }
+}
