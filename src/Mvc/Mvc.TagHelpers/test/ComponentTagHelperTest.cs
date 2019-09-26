80d019d72625b630618291c900b1ac56d3da3395
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-09-26 10:48:40 -0700

    Introduce ComponentTagHelper Fixes https://github.com/aspnet/AspNetCore/issues/13726

---
+++ src/Mvc/Mvc.TagHelpers/test/ComponentTagHelperTest.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.TagHelpers
+{
+    public class ComponentTagHelperTest
+    {
+        [Fact]
+        public async Task ProcessAsync_RendersComponent()
+        {
+            // Arrange
+            var tagHelper = new ComponentTagHelper
+            {
+                ViewContext = GetViewContext(),
+            };
+            var context = GetTagHelperContext();
+            var output = GetTagHelperOutput();
+
+            // Act
+            await tagHelper.ProcessAsync(context, output);
+
+            // Assert
+            var content = HtmlContentUtilities.HtmlContentToString(output.Content);
+            Assert.Equal("Hello world", content);
+            Assert.Null(output.TagName);
+        }
+
+        private static TagHelperContext GetTagHelperContext()
+        {
+            return new TagHelperContext(
+                "component",
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                Guid.NewGuid().ToString("N"));
+        }
+
+        private static TagHelperOutput GetTagHelperOutput()
+        {
+            return new TagHelperOutput(
+                "component",
+                new TagHelperAttributeList(),
+                (_, __) => Task.FromResult<TagHelperContent>(new DefaultTagHelperContent()));
+        }
+
+        private ViewContext GetViewContext()
+        {
+            var htmlContent = new HtmlContentBuilder().AppendHtml("Hello world");
+            var renderer = Mock.Of<IComponentRenderer>(c =>
+                c.RenderComponentAsync(It.IsAny<ViewContext>(), It.IsAny<Type>(), It.IsAny<RenderMode>(), It.IsAny<object>()) == Task.FromResult<IHtmlContent>(htmlContent));
+
+            var httpContext = new DefaultHttpContext
+            {
+                RequestServices = new ServiceCollection().AddSingleton<IComponentRenderer>(renderer).BuildServiceProvider(),
+            };
+
+            return new ViewContext
+            {
+                HttpContext = httpContext,
+            };
+        }
+    }
+}
---
+++ src/Mvc/Mvc.ViewFeatures/src/Rendering/HtmlHelperComponentExtensions.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.AspNetCore.Mvc.Rendering
+{
+    /// <summary>
+    /// Extensions for rendering components.
+    /// </summary>
+    public static class HtmlHelperComponentExtensions
+    {
+        /// <summary>
+        /// Renders the <typeparamref name="TComponent"/> <see cref="IComponent"/>.
+        /// </summary>
+        /// <param name="htmlHelper">The <see cref="IHtmlHelper"/>.</param>
+        /// <param name="renderMode">The <see cref="RenderMode"/> for the component.</param>
+        /// <returns>The HTML produced by the rendered <typeparamref name="TComponent"/>.</returns>
+        public static Task<IHtmlContent> RenderComponentAsync<TComponent>(this IHtmlHelper htmlHelper, RenderMode renderMode) where TComponent : IComponent
+            => RenderComponentAsync<TComponent>(htmlHelper, renderMode, parameters: null);
+
+        /// <summary>
+        /// Renders the <typeparamref name="TComponent"/> <see cref="IComponent"/>.
+        /// </summary>
+        /// <param name="htmlHelper">The <see cref="IHtmlHelper"/>.</param>
+        /// <param name="parameters">An <see cref="object"/> containing the parameters to pass
+        /// to the component.</param>
+        /// <param name="renderMode">The <see cref="RenderMode"/> for the component.</param>
+        /// <returns>The HTML produced by the rendered <typeparamref name="TComponent"/>.</returns>
+        public static Task<IHtmlContent> RenderComponentAsync<TComponent>(
+            this IHtmlHelper htmlHelper,
+            RenderMode renderMode,
+            object parameters) where TComponent : IComponent
+        {
+            if (htmlHelper is null)
+            {
+                throw new ArgumentNullException(nameof(htmlHelper));
+            }
+
+            var viewContext = htmlHelper.ViewContext;
+            var componentRenderer = viewContext.HttpContext.RequestServices.GetRequiredService<IComponentRenderer>();
+            return componentRenderer.RenderComponentAsync(viewContext, typeof(TComponent), renderMode, parameters);
+        }
+    }
+}
