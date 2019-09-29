9afb1ce6ae27c5120341484a6b0a7551905d5079
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-09-29 21:48:46 +0100

    CR: Clarify comment

--- src/Components/Components/src/Rendering/ParameterViewLifetime.cs
+++ src/Components/Components/src/Rendering/ParameterViewLifetime.cs
@@ -20,7 +20,8 @@ namespace Microsoft.AspNetCore.Components.Rendering
 
         public void AssertNotExpired()
         {
-            // _owner will be null if this instance is default(ParameterViewLifetime)
+            // If _owner is null, this instance is default(ParameterViewLifetime), which is
+            // the same as ParameterViewLifetime.Unbound. That means it never expires.
             if (_owner != null && _owner.ParameterViewValidityStamp != _stamp)
             {
                 throw new InvalidOperationException($"The {nameof(ParameterView)} instance can no longer be read because it has expired. {nameof(ParameterView)} can only be read synchronously and must not be stored for later use.");
