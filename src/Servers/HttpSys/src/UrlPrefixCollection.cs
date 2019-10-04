d76718d4b4010ff00ee0653e579d0363f1d8424a
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2019-10-04 11:57:52 -0700

    !fixup! bad merge
    - restore a missing `using`

--- src/Servers/HttpSys/src/UrlPrefixCollection.cs
+++ src/Servers/HttpSys/src/UrlPrefixCollection.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpSys.Internal;
 
 namespace Microsoft.AspNetCore.Server.HttpSys
 {
