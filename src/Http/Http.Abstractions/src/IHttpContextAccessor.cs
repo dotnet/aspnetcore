c6daf7155f33b0ab9f0a73c4d46d19c57006bf32
Jordy Sipkema <jordysipkema@gmail.com>
2019-10-06 00:10:01 +0200

    Apply suggestions from code review
    Co-Authored-By: Andrew Stanton-Nurse <andrew@stanton-nurse.com>

--- src/Http/Http.Abstractions/src/IHttpContextAccessor.cs
+++ src/Http/Http.Abstractions/src/IHttpContextAccessor.cs
@@ -4,12 +4,16 @@
 namespace Microsoft.AspNetCore.Http
 {
     /// <summary>
-    /// Accessor to the <see cref="HttpContext"/>
+    /// Provides access to the current <see cref="HttpContext"/>, if one is available.
     /// </summary>
+    /// <remarks>
+    /// This interface should be used with caution. It relies on <see cref="System.Threading.AsyncLocal{T}" /> which can have a negative performance impact on async calls.
+    /// It also creates a dependency on "ambient state" which can make testing more difficult.
+    /// </remarks>
     public interface IHttpContextAccessor
     {
         /// <summary>
-        /// Gets or sets the associated <see cref="HttpContext"/>.
+        /// Gets or sets the current <see cref="HttpContext"/>. Returns <see langword="null" /> if there is no active <see cref="HttpContext" />.
         /// </summary>
         HttpContext HttpContext { get; set; }
     }
