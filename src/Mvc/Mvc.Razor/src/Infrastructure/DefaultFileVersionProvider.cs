8d6ebd5b46e57421ffc1e92237c3578e4e1ad8e7
John Luo <johluo@microsoft.com>
2019-10-11 10:55:35 -0700

    Mvc

--- src/Mvc/Mvc.Razor/src/Infrastructure/DefaultFileVersionProvider.cs
+++ src/Mvc/Mvc.Razor/src/Infrastructure/DefaultFileVersionProvider.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
