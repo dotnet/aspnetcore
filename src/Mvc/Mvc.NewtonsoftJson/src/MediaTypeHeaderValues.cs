fbd3f2a51248aa079657edbea67cf8cf182c9d68
John Luo <johluo@microsoft.com>
2019-10-12 23:57:05 -0700

    Feedback

--- src/Mvc/Mvc.NewtonsoftJson/src/MediaTypeHeaderValues.cs
+++ src/Mvc/Mvc.NewtonsoftJson/src/MediaTypeHeaderValues.cs
@@ -3,7 +3,7 @@
 
 using Microsoft.Net.Http.Headers;
 
-namespace Microsoft.AspNetCore.Mvc.Formatters
+namespace Microsoft.AspNetCore.Mvc.NewtonsoftJson
 {
     internal static class MediaTypeHeaderValues
     {
@@ -13,16 +13,10 @@ namespace Microsoft.AspNetCore.Mvc.Formatters
         public static readonly MediaTypeHeaderValue TextJson
             = MediaTypeHeaderValue.Parse("text/json").CopyAsReadOnly();
 
+        public static readonly MediaTypeHeaderValue ApplicationJsonPatch
+            = MediaTypeHeaderValue.Parse("application/json-patch+json").CopyAsReadOnly();
+
         public static readonly MediaTypeHeaderValue ApplicationAnyJsonSyntax
             = MediaTypeHeaderValue.Parse("application/*+json").CopyAsReadOnly();
-
-        public static readonly MediaTypeHeaderValue ApplicationXml
-            = MediaTypeHeaderValue.Parse("application/xml").CopyAsReadOnly();
-
-        public static readonly MediaTypeHeaderValue TextXml
-            = MediaTypeHeaderValue.Parse("text/xml").CopyAsReadOnly();
-
-        public static readonly MediaTypeHeaderValue ApplicationAnyXmlSyntax
-            = MediaTypeHeaderValue.Parse("application/*+xml").CopyAsReadOnly();
     }
 }
