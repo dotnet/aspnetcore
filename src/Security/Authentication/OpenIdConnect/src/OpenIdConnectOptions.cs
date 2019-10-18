3b87562a6882a340b89fa991735cf35f6769b369
Steven Yeh <say25@cornell.edu>
2019-10-17 23:54:17 -0500

    Comment consistently with JwtBearerOptions

--- src/Security/Authentication/OpenIdConnect/src/OpenIdConnectOptions.cs
+++ src/Security/Authentication/OpenIdConnect/src/OpenIdConnectOptions.cs
@@ -334,7 +334,7 @@ namespace Microsoft.AspNetCore.Authentication.OpenIdConnect
         public TimeSpan AutomaticRefreshInterval { get; set; } = ConfigurationManager<OpenIdConnectConfiguration>.DefaultAutomaticRefreshInterval;
 
         /// <summary>
-        /// 30 seconds is the default time interval that must pass for <see cref="ConfigurationManager" /> to obtain a new configuration.
+        /// 30 seconds is the default, minimum time interval that must pass for <see cref="ConfigurationManager" /> to obtain a new configuration.
         /// </summary>
         public TimeSpan RefreshInterval { get; set; } = ConfigurationManager<OpenIdConnectConfiguration>.DefaultRefreshInterval;
     }
