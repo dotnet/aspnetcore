4bfae236fd56bb8da128ae99ad07e67591a62aba
Hao Kung <HaoK@users.noreply.github.com>
2019-10-23 14:31:23 -0700

    Update text

--- src/Security/Authorization/Core/src/DenyAnonymousAuthorizationRequirement.cs
+++ src/Security/Authorization/Core/src/DenyAnonymousAuthorizationRequirement.cs
@@ -32,7 +32,7 @@ namespace Microsoft.AspNetCore.Authorization.Infrastructure
 
         public override string ToString()
         {
-            return $"{nameof(DenyAnonymousAuthorizationRequirement)}:User authenticated";
+            return $"{nameof(DenyAnonymousAuthorizationRequirement)}:Requires an authenticated user.";
         }
     }
 }
