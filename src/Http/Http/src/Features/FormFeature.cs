f122a2fca2696778811f0e23a416f06ed5f52bfc
Gil Mishal <gil.mishal@slydeglobal.com>
2019-10-28 14:35:47 +0200

    updated formfeature to use new multipartPipeReader

--- src/Http/Http/src/Features/FormFeature.cs
+++ src/Http/Http/src/Features/FormFeature.cs
@@ -162,7 +162,7 @@ namespace Microsoft.AspNetCore.Http.Features
                     var formAccumulator = new KeyValueAccumulator();
 
                     var boundary = GetBoundary(contentType, _options.MultipartBoundaryLengthLimit);
-                    var multipartReader = new MultipartReader(boundary, _request.Body)
+                    var multipartReader = new MultipartPipeReader(boundary, _request.BodyReader)
                     {
                         HeadersCountLimit = _options.MultipartHeadersCountLimit,
                         HeadersLengthLimit = _options.MultipartHeadersLengthLimit,
