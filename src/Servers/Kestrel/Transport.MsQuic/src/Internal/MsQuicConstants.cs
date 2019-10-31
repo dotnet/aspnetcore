445153aeaab9cd7e0cf0ef0752190665d0926856
Justin Kotalik <jukotali@microsoft.com>
2019-10-31 10:37:40 -0700

    Feedback

--- src/Servers/Kestrel/Transport.MsQuic/src/Internal/MsQuicConstants.cs
+++ src/Servers/Kestrel/Transport.MsQuic/src/Internal/MsQuicConstants.cs
@@ -11,27 +11,28 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.MsQuic.Internal
         private const uint Success = 0;
 
         internal static Func<uint, string> ErrorTypeFromErrorCode = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? Windows.GetError : (Func<uint, string>)Linux.GetError;
-        
+
         internal static class Windows
         {
-            private const uint Pending = 0x703E5;
-            private const uint Continue = 0x704DE;
-            private const uint OutOfMemory = 0x8007000E;
-            private const uint InvalidParameter = 0x80070057;
-            private const uint InvalidState = 0x8007139F;
-            private const uint NotSupported = 0x80004002;
-            private const uint NotFound = 0x80070490;
-            private const uint BufferTooSmall = 0x8007007A;
-            private const uint HandshakeFailure = 0x80410000;
-            private const uint Aborted = 0x80004004;
-            private const uint AddressInUse = 0x80072740;
-            private const uint ConnectionTimeout = 0x800704CF;
-            private const uint ConnectionIdle = 0x800704D4;
-            private const uint InternalError = 0x80004005;
-            private const uint ServerBusy = 0x800704C9;
-            private const uint ProtocolError = 0x800704CD;
-            private const uint VerNegError = 0x80410001;
+            internal const uint Pending = 0x703E5;
+            internal const uint Continue = 0x704DE;
+            internal const uint OutOfMemory = 0x8007000E;
+            internal const uint InvalidParameter = 0x80070057;
+            internal const uint InvalidState = 0x8007139F;
+            internal const uint NotSupported = 0x80004002;
+            internal const uint NotFound = 0x80070490;
+            internal const uint BufferTooSmall = 0x8007007A;
+            internal const uint HandshakeFailure = 0x80410000;
+            internal const uint Aborted = 0x80004004;
+            internal const uint AddressInUse = 0x80072740;
+            internal const uint ConnectionTimeout = 0x800704CF;
+            internal const uint ConnectionIdle = 0x800704D4;
+            internal const uint InternalError = 0x80004005;
+            internal const uint ServerBusy = 0x800704C9;
+            internal const uint ProtocolError = 0x800704CD;
+            internal const uint VerNegError = 0x80410001;
 
+            // TODO return better error messages here.
             public static string GetError(uint status)
             {
                 switch (status)
@@ -79,23 +80,23 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.MsQuic.Internal
 
         internal static class Linux
         {
-            private const uint Pending = unchecked((uint)-2);
-            private const uint Continue = unchecked((uint)-1);
-            private const uint OutOfMemory = 12;
-            private const uint InvalidParameter = 22;
-            private const uint InvalidState = 200000002;
-            private const uint NotSupported = 95;
-            private const uint NotFound = 2;
-            private const uint BufferTooSmall = 75;
-            private const uint HandshakeFailure = 200000009;
-            private const uint Aborted = 200000008;
-            private const uint AddressInUse = 98;
-            private const uint ConnectionTimeout = 110;
-            private const uint ConnectionIdle = 200000011;
-            private const uint InternalError = 200000012;
-            private const uint ServerBusy = 200000007;
-            private const uint ProtocolError = 200000013;
-            private const uint VerNegError = 200000014;
+            internal const uint Pending = unchecked((uint)-2);
+            internal const uint Continue = unchecked((uint)-1);
+            internal const uint OutOfMemory = 12;
+            internal const uint InvalidParameter = 22;
+            internal const uint InvalidState = 200000002;
+            internal const uint NotSupported = 95;
+            internal const uint NotFound = 2;
+            internal const uint BufferTooSmall = 75;
+            internal const uint HandshakeFailure = 200000009;
+            internal const uint Aborted = 200000008;
+            internal const uint AddressInUse = 98;
+            internal const uint ConnectionTimeout = 110;
+            internal const uint ConnectionIdle = 200000011;
+            internal const uint InternalError = 200000012;
+            internal const uint ServerBusy = 200000007;
+            internal const uint ProtocolError = 200000013;
+            internal const uint VerNegError = 200000014;
 
 
             public static string GetError(uint status)
