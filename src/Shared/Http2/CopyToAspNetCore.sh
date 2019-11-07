4f9879fab205f47abcbdebe25f141e7f4ce573da
Günther Foidl <gue@korporal.at>
2019-11-07 14:19:19 +0100

    Use bash instead of sh

--- src/Shared/Http2/CopyToAspNetCore.sh
+++ src/Shared/Http2/CopyToAspNetCore.sh
@@ -1,17 +1,17 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-if [ -n "$1" ]; then
+if [[ -n "$1" ]]; then
     remote_repo="$1"
 else
     remote_repo="$ASPNETCORE_REPO"
 fi
 
-if [ -z "$remote_repo" ]; then
+if [[ -z "$remote_repo" ]]; then
     echo The 'ASPNETCORE_REPO' environment variable or command line parameter is not set, aborting.
     exit 1
 fi
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit 1
 
 echo "ASPNETCORE_REPO: $remote_repo"
 
