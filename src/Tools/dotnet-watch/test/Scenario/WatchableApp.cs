943c1b4530a5600a9ed92153d834797c484f9c2b
Brennan Conroy <brecon@microsoft.com>
2019-11-08 14:15:22 -0800

    Change how we resolve dotnet in tests

--- src/Tools/dotnet-watch/test/Scenario/WatchableApp.cs
+++ src/Tools/dotnet-watch/test/Scenario/WatchableApp.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Microsoft.Extensions.CommandLineUtils;
@@ -88,9 +89,12 @@ namespace Microsoft.DotNet.Watcher.Tools.FunctionalTests
             };
             args.AddRange(arguments);
 
+            var dotnetPath = typeof(WatchableApp).Assembly.GetCustomAttributes<AssemblyMetadataAttribute>()
+                    .Single(s => s.Key == "DotnetPath").Value;
+
             var spec = new ProcessSpec
             {
-                Executable = DotNetMuxer.MuxerPathOrDefault(),
+                Executable = dotnetPath,
                 Arguments = args,
                 WorkingDirectory = SourceDirectory,
                 EnvironmentVariables =
