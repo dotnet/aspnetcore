ea4024d047df4a5e2d3b149064fc7f92250a3e8a
Ryan Nowak <nowakra@gmail.com>
2019-11-11 16:41:59 -0800

    Use operations in our analyzers that need symbols
    Fixes: #16922
    
    This change updates our analyzers that need access to the symbols to use
    `IOperation` where possible. Using syntax analysis for this kind of
    analyzer has worse performance. These analyzers run on generated code,
    which can include EF migrations, the design of which amplifies these
    effects.
    
    On the path to this, I also added support for a few more cases that
    operations make easy. Since we're doing this anyway, I want to have
    confidence that we're checking everything (within reason). In some cases
    the diagnostic experience changed a bit (including more of the
    declaration/code) - I felt like all of these were OK changes. Given the
    kinds of error message reported by the analyzers "don't use that type"
    it seems like it's still a good enough experience without
    micro-optimizing all of the locations.

--- src/Components/Analyzers/test/TestFiles/ComponentInternalUsageDiagnosticsAnalyzerTest/UsersRendererTypesInMethodBody.cs
+++ src/Components/Analyzers/test/TestFiles/ComponentInternalUsageDiagnosticsAnalyzerTest/UsersRendererTypesInMethodBody.cs
@@ -3,12 +3,15 @@ using Microsoft.AspNetCore.Components.RenderTree;
 
 namespace Microsoft.AspNetCore.Components.Analyzers.Tests.TestFiles.ComponentInternalUsageDiagnosticsAnalyzerTest
 {
-    class UsesRenderTreeFrameTypeAsLocal
+    class UsersRendererTypesInMethodBody
     {
         private void Test()
         {
-            var test = RenderTreeFrameType./*MM*/Attribute;
+            var test = /*MMField*/RenderTreeFrameType.Attribute;
             GC.KeepAlive(test);
+
+            var frame = /*MMNewObject*/new RenderTreeFrame();
+            GC.KeepAlive(/*MMProperty*/frame.Component);
         }
 
     }
