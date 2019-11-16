bf6327be12eaeeac4ade906ec9d917e74844d0b0
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2019-11-15 18:49:30 -0800

    Remove recently-added `@(Compile)` and `@(Reference)` items - were added due to missing `[InternalsVisibleTo]` attributes in ref/ assemblies or as early workarounds   - plus, now transitive references **Just Work:tm:** - expose `ClosedGenericMatcher` in the usual (*.Manual.cs) way - also undo Microsoft.Extensions.ApiDescription.Server workaround

---
+++ src/Mvc/Mvc.Abstractions/ref/Microsoft.AspNetCore.Mvc.Abstractions.Manual.cs
@@ -0,0 +1,7 @@
+namespace Microsoft.Extensions.Internal
+{
+    internal static partial class ClosedGenericMatcher
+    {
+        public static System.Type ExtractGenericInterface(System.Type queryType, System.Type interfaceType) { throw null; }
+    }
+}
