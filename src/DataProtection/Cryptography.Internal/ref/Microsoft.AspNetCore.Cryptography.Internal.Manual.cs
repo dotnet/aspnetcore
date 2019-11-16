5135c0781dea7c9140e7e54738635857a11f3bb8
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2019-11-15 18:37:42 -0800

    Undo some manual ref/ project changes - now done automatically or centrally - remove manual `[TypeForwardedTo]` and `[InternalsVisibleTo]` attributes   - fully qualify a type now that `using` is gone - remove dupe `@(Compile)` items for *.Manual.cs files; included in the ref/ project files - remove redundant `$(AllowUnsafeBlocks)` and `$(NoWarn)` settings
    nits:
    - rename a *.Manual.cs file that's not TFM-specific
    - remove `private` members

--- src/DataProtection/Cryptography.Internal/ref/Microsoft.AspNetCore.Cryptography.Internal.Manual.cs
+++ src/DataProtection/Cryptography.Internal/ref/Microsoft.AspNetCore.Cryptography.Internal.Manual.cs
@@ -1,13 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: InternalsVisibleTo("Microsoft.AspNetCore.Cryptography.KeyDerivation, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f33a29044fa9d740c9b3213a93e57c84b472c84e0b8a0e1ae48e67a9f8f6de9d5f7f3d52ac23e48ac51801f1dc950abe901da34d2a9e3baadb141a17c77ef3c565dd5ee5054b91cf63bb3c6ab83f72ab3aafe93d0fc3c2348b764fafb0b1c0733de51459aeab46580384bf9d74c4e28164b7cde247f891ba07891c9d872ad2bb")]
-[assembly: InternalsVisibleTo("Microsoft.AspNetCore.DataProtection, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f33a29044fa9d740c9b3213a93e57c84b472c84e0b8a0e1ae48e67a9f8f6de9d5f7f3d52ac23e48ac51801f1dc950abe901da34d2a9e3baadb141a17c77ef3c565dd5ee5054b91cf63bb3c6ab83f72ab3aafe93d0fc3c2348b764fafb0b1c0733de51459aeab46580384bf9d74c4e28164b7cde247f891ba07891c9d872ad2bb")]
-
 namespace Microsoft.AspNetCore.Cryptography
 {
     internal static partial class Constants
@@ -178,7 +171,7 @@ namespace Microsoft.AspNetCore.Cryptography.Cng
     {
         public uint cbBuffer; // Length of buffer, in bytes
         public BCryptKeyDerivationBufferType BufferType; // Buffer type
-        public IntPtr pvBuffer; // Pointer to buffer
+        public System.IntPtr pvBuffer; // Pointer to buffer
     }
     [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential)]
     internal unsafe partial struct BCryptBufferDesc
