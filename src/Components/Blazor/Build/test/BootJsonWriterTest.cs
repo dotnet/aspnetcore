25ba9982861d3eaf2419ac5f3486eda8e12ea341
Pranav K <prkrishn@hotmail.com>
2019-11-19 07:49:34 -0800

    Fixup test

--- src/Components/Blazor/Build/test/BootJsonWriterTest.cs
+++ src/Components/Blazor/Build/test/BootJsonWriterTest.cs
@@ -13,7 +13,7 @@ namespace Microsoft.AspNetCore.Blazor.Build.Test
         public void ProducesJsonReferencingAssemblyAndDependencies()
         {
             // Arrange/Act
-            var assemblyReferences = new string[] { "System.Abc.dll", "MyApp.ClassLib.dll", };
+            var assemblyReferences = new string[] { "MyApp.EntryPoint.dll", "System.Abc.dll", "MyApp.ClassLib.dll", };
             var content = BootJsonWriter.GetBootJsonContent(
                 "MyApp.Entrypoint.dll",
                 assemblyReferences,
@@ -21,8 +21,8 @@ namespace Microsoft.AspNetCore.Blazor.Build.Test
 
             // Assert
             var parsedContent = JsonConvert.DeserializeObject<JObject>(content);
-            Assert.Equal("MyApp.Entrypoint.dll", parsedContent["main"].Value<string>());
-            Assert.Equal(assemblyReferences, parsedContent["assemblyReferences"].Values<string>());
+            Assert.Equal("MyApp.Entrypoint.dll", parsedContent["entryAssembly"].Value<string>());
+            Assert.Equal(assemblyReferences, parsedContent["assemblies"].Values<string>());
         }
     }
 }
