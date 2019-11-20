9d4dfd2c3dd9e8beb78b99a37bab6c298f1129c1
Hao Kung <HaoK@users.noreply.github.com>
2019-11-19 23:58:43 -0800

    Update installappruntime.sh

--- eng/helix/content/installappruntime.sh
+++ eng/helix/content/installappruntime.sh
@@ -7,6 +7,7 @@ appRuntimePath=$1
 output_dir=$2
 
 echo "Installing shared framework from $appRuntimePath"
-mkdir $output_dir
+cp $appRuntimePath sharedFx.gz 
+mkdir -p $output_dir
 echo "Unpacking to $output_dir"
-tar --strip-components 1 -xzf "$appRuntimePath" --no-same-owner --directory "$output_dir"
+tar -xzf sharedFx.gz --no-same-owner --directory "$output_dir"
