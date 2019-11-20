48a1c0227c270fc5ba56f4f508211066f6b61666
Hao Kung <HaoK@users.noreply.github.com>
2019-11-19 23:52:11 -0800

    Update InstallAppRuntime.ps1

--- eng/helix/content/InstallAppRuntime.ps1
+++ eng/helix/content/InstallAppRuntime.ps1
@@ -22,15 +22,17 @@ Set-StrictMode -Version 1
 
 Write-Host "Extracting to $InstallDir"
 
+$zipPackage = [io.path]::ChangeExtension($AppRuntimePath, ".zip")
+Rename-Item -Path $AppRuntimePath -NewName $zipPackage
 if (Get-Command -Name 'Microsoft.PowerShell.Archive\Expand-Archive' -ErrorAction Ignore) {
     # Use built-in commands where possible as they are cross-plat compatible
-    Microsoft.PowerShell.Archive\Expand-Archive -Path $AppRuntimePath -DestinationPath $InstallDir -Force
+    Microsoft.PowerShell.Archive\Expand-Archive -Path $zipPackage -DestinationPath $InstallDir -Force
 }
 else {
     Remove-Item $tempDir -Recurse -ErrorAction Ignore
     # Fallback to old approach for old installations of PowerShell
     Add-Type -AssemblyName System.IO.Compression.FileSystem
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($AppRuntimePath, $InstallDir)
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipPackage, $InstallDir)
 }
 
 Write-Host "Expanded App Runtime to $InstallDir"
