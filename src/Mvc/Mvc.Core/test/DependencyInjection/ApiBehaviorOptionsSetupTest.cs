24a503d27ee05a9e047f08ae21acc312fb3e37a2
David Driscoll <david.driscoll@gmail.com>
2019-11-22 00:23:05 -0500

    Set status code for the object result returned by ApiBehaviorOptionsSetup to match the problem details status code

--- src/Mvc/Mvc.Core/test/DependencyInjection/ApiBehaviorOptionsSetupTest.cs
+++ src/Mvc/Mvc.Core/test/DependencyInjection/ApiBehaviorOptionsSetupTest.cs
@@ -49,6 +49,27 @@ namespace Microsoft.Extensions.DependencyInjection
         }
 
         [Fact]
+        public void ProblemDetailsInvalidModelStateResponse_ReturnsObjectResultWithProblemDetails()
+        {
+            // Arrange
+            var actionContext = GetActionContext();
+            var factory = GetProblemDetailsFactory();
+
+            // Act
+            var result = ApiBehaviorOptionsSetup.ProblemDetailsInvalidModelStateResponse(factory, actionContext);
+
+            // Assert
+            var badRequest = Assert.IsType<ObjectResult>(result);
+            Assert.Equal(422, result.StatusCode);
+            Assert.Equal(new[] { "application/problem+json", "application/problem+xml" }, badRequest.ContentTypes.OrderBy(c => c));
+
+            var problemDetails = Assert.IsType<ValidationProblemDetails>(badRequest.Value);
+            Assert.Equal(422, problemDetails.Status);
+            Assert.Equal("One or more validation errors occurred.", problemDetails.Title);
+            Assert.Equal("https://tools.ietf.org/html/rfc7231#section-6.5.1", problemDetails.Type);
+        }
+
+        [Fact]
         public void ProblemDetailsInvalidModelStateResponse_UsesUserConfiguredLink()
         {
             // Arrange
@@ -117,6 +138,72 @@ namespace Microsoft.Extensions.DependencyInjection
             }
 
             return new DefaultProblemDetailsFactory(Options.Options.Create(options));
+        }
+
+        private static ProblemDetailsFactory GetCustomProblemDetailsFactory()
+        {
+            var options = new ApiBehaviorOptions();
+            var setup = new ApiBehaviorOptionsSetup();
+
+            return new CustomProblemDetailsFactory();
+        }
+
+        private class CustomProblemDetailsFactory : ProblemDetailsFactory
+        {
+            public override ProblemDetails CreateProblemDetails(
+                HttpContext httpContext,
+                int? statusCode = null,
+                string title = null,
+                string type = null,
+                string detail = null,
+                string instance = null)
+            {
+                statusCode ??= 500;
+
+                var problemDetails = new ProblemDetails
+                {
+                    Status = statusCode,
+                    Title = title,
+                    Type = type,
+                    Detail = detail,
+                    Instance = instance,
+                };
+
+                return problemDetails;
+            }
+
+            public override ValidationProblemDetails CreateValidationProblemDetails(
+                HttpContext httpContext,
+                ModelStateDictionary modelStateDictionary,
+                int? statusCode = null,
+                string title = null,
+                string type = null,
+                string detail = null,
+                string instance = null)
+            {
+                if (modelStateDictionary == null)
+                {
+                    throw new ArgumentNullException(nameof(modelStateDictionary));
+                }
+
+                statusCode ??= 422;
+
+                var problemDetails = new ValidationProblemDetails(modelStateDictionary)
+                {
+                    Status = statusCode,
+                    Type = type,
+                    Detail = detail,
+                    Instance = instance,
+                };
+
+                if (title != null)
+                {
+                    // For validation problem details, don't overwrite the default title with null.
+                    problemDetails.Title = title;
+                }
+
+                return problemDetails;
+            }
         }
 
         private static ActionContext GetActionContext()
