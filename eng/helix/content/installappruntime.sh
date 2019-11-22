de0fe18c614c54f5c9fb43ea989df3732b02e0d5
Hao Kung <HaoK@users.noreply.github.com>
2019-11-21 22:55:57 -0800

    Update installappruntime.sh

--- eng/helix/content/installappruntime.sh
+++ eng/helix/content/installappruntime.sh
@@ -5,9 +5,15 @@ set -e
 
 appRuntimePath=$1
 output_dir=$2
+tmpDir=./tmpRuntime
 
 echo "Installing shared framework from $appRuntimePath"
 cp $appRuntimePath sharedFx.zip
+
+mkdir -p $tmpDir
+echo "Unpacking to $tmpDir"
+unzip sharedFx.zip -d $tmpDir
 mkdir -p $output_dir
-echo "Unpacking to $output_dir"
-unzip sharedFx.zip -d $output_dir
+echo "Copying to $output_dir"
+cp $tmpDir/runtimes/win-x86/lib/netcoreapp5.0/* $output_dir
+cp $tmpDir/runtimes/win-x86/native/* $output_dir
