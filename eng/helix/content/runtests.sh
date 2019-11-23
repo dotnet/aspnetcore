4ce05b90809140d9c6b9bf854bd5351782173a5e
Hao Kung <HaoK@users.noreply.github.com>
2019-11-22 19:41:36 -0800

    Update runtests.sh

--- eng/helix/content/runtests.sh
+++ eng/helix/content/runtests.sh
@@ -87,7 +87,7 @@ ls -la $DOTNET_ROOT/shared/Microsoft.AspNetCore.App
 # Copy over any local shared fx if found
 if [ -d "Microsoft.AspNetCore.App" ]
 then
-    mv Microsoft.AppNetCore.App/* $DOTNET_ROOT/shared/Microsoft.AspNetCore.App/
+    cp Microsoft.AppNetCore.App/* $DOTNET_ROOT/shared/Microsoft.AspNetCore.App/$dotnet_runtime_version
 fi
 
 $DOTNET_ROOT/dotnet vstest $test_binary_path -lt >discovered.txt
