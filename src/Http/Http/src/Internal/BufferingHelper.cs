36b2720368789a802bc206a1ace9466855185417
Gil Mishal <gil.mishal@slydeglobal.com>
2019-12-08 20:11:19 +0200

    reverted changes to BufferingHelper

--- src/Http/Http/src/Internal/BufferingHelper.cs
+++ src/Http/Http/src/Internal/BufferingHelper.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.IO;
 using Microsoft.AspNetCore.Internal;
 using Microsoft.AspNetCore.WebUtilities;
 
