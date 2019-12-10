b8e0cb21a6386fe5cc0fee7fed7d27de209db128
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-12-10 12:10:20 +0000

    Make WebAssembly E2E tests all use async main

--- src/Components/test/testassets/BasicTestApp/Program.cs
+++ src/Components/test/testassets/BasicTestApp/Program.cs
@@ -2,14 +2,19 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Globalization;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Blazor.Hosting;
 
 namespace BasicTestApp
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            // Later on, the startup APIs will be inherently asynchronous
+            // Until then, use this artificial means to show async main is working
+            await Task.Yield();
+
             // We want the culture to be en-US so that the tests for bind can work consistently.
             CultureInfo.CurrentCulture = new CultureInfo("en-US");
 
