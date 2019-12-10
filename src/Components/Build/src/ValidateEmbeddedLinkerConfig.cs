2c4d7bcbaf6debc1d19a1bede0f1a7a092679a90
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-12-10 23:35:44 +0000

    Don't lock assemblies when validating contents

--- src/Components/Build/src/ValidateEmbeddedLinkerConfig.cs
+++ src/Components/Build/src/ValidateEmbeddedLinkerConfig.cs
@@ -2,11 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.IO;
-using System.Reflection;
+using System.Linq;
 using System.Text;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using Mono.Cecil;
 
 namespace Microsoft.AspNetCore.Components.Build
 {
@@ -17,17 +17,16 @@ namespace Microsoft.AspNetCore.Components.Build
 
         public override bool Execute()
         {
-            var assembly = Assembly.LoadFrom(AssemblyPath);
-            var expectedName = $"{assembly.GetName().Name}.xml";
-            var embeddedConfig = assembly.GetManifestResourceStream(expectedName);
+            var assemblyDefinition = AssemblyDefinition.ReadAssembly(AssemblyPath);
+            var expectedName = $"{assemblyDefinition.Name.Name}.xml";
+            var embeddedConfig = assemblyDefinition.MainModule.Resources.FirstOrDefault(r => r.Name.Equals(expectedName, StringComparison.Ordinal)) as EmbeddedResource;
             if (embeddedConfig == null)
             {
                Log.LogError($"Could not find linker config resource '{expectedName}' inside assembly at path '{AssemblyPath}'.\n\nTo fix this, run the build with /p:RegenerateLinkerConfig=true");
                return false;
             }
 
-            using var streamReader = new StreamReader(embeddedConfig);
-            var actualConfig = streamReader.ReadToEnd();
+            var actualConfig = Encoding.UTF8.GetString(embeddedConfig.GetResourceData());
             var expectedConfig = LinkerConfigGenerator.Generate(AssemblyPath);
 
             if (!actualConfig.Equals(expectedConfig, StringComparison.OrdinalIgnoreCase))
