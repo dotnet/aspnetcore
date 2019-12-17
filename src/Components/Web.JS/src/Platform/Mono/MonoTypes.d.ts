670886d6b30ccd945da02527ece5279c313a15b7
Pranav K <prkrishn@hotmail.com>
2019-12-17 14:59:19 -0800

    Use mono_bind_static_method for invoking JS methods

--- src/Components/Web.JS/src/Platform/Mono/MonoTypes.d.ts
+++ src/Components/Web.JS/src/Platform/Mono/MonoTypes.d.ts
@@ -10,7 +10,7 @@ declare namespace Module {
   function FS_createPath(parent, path, canRead, canWrite);
   function FS_createDataFile(parent, name, data, canRead, canWrite, canOwn);
 
-  function mono_call_assembly_entry_point(assemblyName: string, args: any[]): any;
+  function mono_bind_static_method(fqn: string): (args: any[]) => any;
 }
 
 // Emscripten declares these globals
