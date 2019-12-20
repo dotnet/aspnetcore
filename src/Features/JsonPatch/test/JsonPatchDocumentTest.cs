e5a4a85bda7d845617dad876b595e288417f09da
Whit Waldo <whit.waldo@innovian.net>
2019-12-20 09:47:44 -0600

    Added missing using in unit tests

--- src/Features/JsonPatch/test/JsonPatchDocumentTest.cs
+++ src/Features/JsonPatch/test/JsonPatchDocumentTest.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.AspNetCore.JsonPatch.Exceptions;
+using Microsoft.AspNetCore.JsonPatch.Operations;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Xunit;
