9cabe84359c2b96651b76dcf2d6ee616d5aae439
Whit Waldo <whit.waldo@innovian.net>
2019-12-20 06:00:42 -0600

    Typo correction

--- src/Features/JsonPatch/src/JsonPatchDocumentOfT.cs
+++ src/Features/JsonPatch/src/JsonPatchDocumentOfT.cs
@@ -821,7 +821,7 @@ namespace Microsoft.AspNetCore.JsonPatch
         /// <param name="objectToApplyTo">Object to apply the JsonPatchDocument to</param>
         /// <param name="modelState">The <see cref="ModelStateDictionary"/> to add errors.</param>
         /// <param name="prefix">The prefix to use when looking up values in <see cref="ModelStateDictionary"/>.</param>
-        public void ApplyTo<TModle>(
+        public void ApplyTo<TModel>(
             TModel objectToApplyTo,
             ModelStateDictionary modelState,
             string prefix)
