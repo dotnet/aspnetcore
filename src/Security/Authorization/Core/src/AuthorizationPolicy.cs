3cb22cf9ff3550f2d4287e3dada82cdfff81ed03
Kahbazi <A.Kahbazi@gmail.com>
2019-12-24 14:59:51 +0330

    Update AuthorizationPolicy.cs

--- src/Security/Authorization/Core/src/AuthorizationPolicy.cs
+++ src/Security/Authorization/Core/src/AuthorizationPolicy.cs
@@ -150,7 +150,7 @@ namespace Microsoft.AspNetCore.Authorization
                     }
 
                     var rolesSplit = authorizeDatum.Roles?.Split(',');
-                    if (rolesSplit?.Count > 0)
+                    if (rolesSplit?.Length > 0)
                     {
                         var trimmedRolesSplit = rolesSplit.Where(r => !string.IsNullOrWhiteSpace(r)).Select(r => r.Trim());
                         policyBuilder.RequireRole(trimmedRolesSplit);
@@ -158,7 +158,7 @@ namespace Microsoft.AspNetCore.Authorization
                     }
 
                     var authTypesSplit = authorizeDatum.AuthenticationSchemes?.Split(',');
-                    if (authTypesSplit?.Count > 0)
+                    if (authTypesSplit?.Length > 0)
                     {
                         foreach (var authType in authTypesSplit)
                         {
