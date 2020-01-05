d089b50d7b1357e3bc922c29286eaf4260e6e6ad
TeBeCo <tebeco@gmail.com>
2020-01-05 17:44:00 +0400

    Update MessagePackUtil to use MessagePackReader

--- src/SignalR/server/StackExchangeRedis/src/Internal/MessagePackUtil.cs
+++ src/SignalR/server/StackExchangeRedis/src/Internal/MessagePackUtil.cs
@@ -2,67 +2,36 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Buffers;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using MessagePack;
 
 namespace Microsoft.AspNetCore.SignalR.StackExchangeRedis.Internal
 {
+    //REVIEW : pretty sure this class can be deleted once the solution build since it's the same Api as MessagePackReader
     internal static class MessagePackUtil
     {
-        public static int ReadArrayHeader(ref ReadOnlyMemory<byte> data)
-        {
-            var arr = GetArray(data);
-            var val = MessagePackBinary.ReadArrayHeader(arr.Array, arr.Offset, out var readSize);
-            data = data.Slice(readSize);
-            return val;
-        }
+        public static int ReadArrayHeader(ref MessagePackReader reader) =>
+            reader.ReadArrayHeader();
 
-        public static int ReadMapHeader(ref ReadOnlyMemory<byte> data)
-        {
-            var arr = GetArray(data);
-            var val = MessagePackBinary.ReadMapHeader(arr.Array, arr.Offset, out var readSize);
-            data = data.Slice(readSize);
-            return val;
-        }
+        public static int ReadMapHeader(ref MessagePackReader reader) =>
+            reader.ReadMapHeader();
 
-        public static string ReadString(ref ReadOnlyMemory<byte> data)
-        {
-            var arr = GetArray(data);
-            var val = MessagePackBinary.ReadString(arr.Array, arr.Offset, out var readSize);
-            data = data.Slice(readSize);
-            return val;
-        }
+        public static string ReadString(ref MessagePackReader reader) =>
+            reader.ReadString();
 
-        public static byte[] ReadBytes(ref ReadOnlyMemory<byte> data)
-        {
-            var arr = GetArray(data);
-            var val = MessagePackBinary.ReadBytes(arr.Array, arr.Offset, out var readSize);
-            data = data.Slice(readSize);
-            return val;
-        }
+        public static byte[] ReadBytes(ref MessagePackReader reader) =>
+            // REVIEW : is that ToArray necessary ?
+            // REVIEW : Returning a "ReadOnlySequence<byte>?" have significant code change accross the repository, so for now i limited the impact here
+            // REVIEW : As my understanding is limited, it will allocate only if multiple sequence are not one next to another in memory ?
+            reader.ReadBytes()?.ToArray() ?? Array.Empty<byte>();
 
-        public static int ReadInt32(ref ReadOnlyMemory<byte> data)
-        {
-            var arr = GetArray(data);
-            var val = MessagePackBinary.ReadInt32(arr.Array, arr.Offset, out var readSize);
-            data = data.Slice(readSize);
-            return val;
-        }
+        public static int ReadInt32(ref MessagePackReader reader) =>
+            reader.ReadInt32();
 
-        public static byte ReadByte(ref ReadOnlyMemory<byte> data)
-        {
-            var arr = GetArray(data);
-            var val = MessagePackBinary.ReadByte(arr.Array, arr.Offset, out var readSize);
-            data = data.Slice(readSize);
-            return val;
-        }
+        public static byte ReadByte(ref MessagePackReader reader) =>
+            reader.ReadByte();
 
-        private static ArraySegment<byte> GetArray(ReadOnlyMemory<byte> data)
-        {
-            var isArray = MemoryMarshal.TryGetArray(data, out var array);
-            Debug.Assert(isArray);
-            return array;
-        }
     }
 }
