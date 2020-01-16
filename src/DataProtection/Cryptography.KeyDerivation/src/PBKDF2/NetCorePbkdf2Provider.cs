88579062c99b4bd799a83b6305955293aac75b92
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2020-01-15 18:05:15 -0800

    Revert "Multi-target all shared framework projects with netcoreapp3.1 (#17998)"
    This reverts commit 9522e1dd2e0c0a5b5bb2e9c4e37a31130a7940ff.

--- src/DataProtection/Cryptography.KeyDerivation/src/PBKDF2/NetCorePbkdf2Provider.cs
+++ src/DataProtection/Cryptography.KeyDerivation/src/PBKDF2/NetCorePbkdf2Provider.cs
@@ -1,7 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-#if NETCOREAPP
+#if NETCOREAPP2_0
 using System;
 using System.Diagnostics;
 using System.Security.Cryptography;
