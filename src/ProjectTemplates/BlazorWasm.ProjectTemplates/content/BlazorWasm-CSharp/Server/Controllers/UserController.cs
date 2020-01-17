718f47b0c5a8b3deb31d43f4e6d7d5ab7ee45dce
Javier Calvarro Nelson <jacalvar@microsoft.com>
2020-01-17 06:19:36 -0800

    Remove unnecessary class

--- src/ProjectTemplates/BlazorWasm.ProjectTemplates/content/BlazorWasm-CSharp/Server/Controllers/UserController.cs
+++ src/ProjectTemplates/BlazorWasm.ProjectTemplates/content/BlazorWasm-CSharp/Server/Controllers/UserController.cs
@@ -12,5 +12,47 @@ namespace BlazorWasm_CSharp.Server.Controllers
         [AllowAnonymous]
         public IActionResult GetCurrentUser() =>
             Ok(User.Identity.IsAuthenticated ? new UserInfo(User) : UserInfo.Anonymous);
+
+        private UserInfo CreateUserInfo(ClaimsPrincipal claimsPrincipal)
+        {
+            if (!claimsPrincipal.Identity.IsAuthenticated)
+            {
+                return UserInfo.Anonymous;
+            }
+
+            var userInfo = new UserInfo();
+            userInfo.IsAuthenticated = true;
+
+            if (identity is ClaimsIdentity claimsIdentity)
+            {
+                userInfo.NameClaimType = claimsIdentity.NameClaimType;
+                userInfo.RoleClaimType = claimsIdentity.RoleClaimType;
+            }
+            else
+            {
+                userInfo.NameClaimType = "name";
+                userInfo.RoleClaimType = "roles";
+            }
+
+            if (claimsPrincipal.Claims.Any())
+            {
+                var claims = new List<ClaimValue>();
+                var nameClaims = claimsPrincipal.FindAll(NameClaimType);
+                foreach (var claim in nameClaims)
+                {
+                    claims.Add(new ClaimValue(NameClaimType, claim.Value));
+                }
+
+                // Uncomment this code if you want to send additional claims to the client.
+                //foreach (var claim in claimsPrincipal.Claims.Except(nameClaims))
+                //{
+                //    claims.Add(new ClaimValue(claim.Type, claim.Value));
+                //}
+
+                userInfo.Claims = claims;
+            }
+
+            return userInfo;
+        }
     }
 }
--- src/ProjectTemplates/BlazorWasm.ProjectTemplates/content/BlazorWasm-CSharp/Shared/Authorization/UserInfo.cs
+++ src/ProjectTemplates/BlazorWasm.ProjectTemplates/content/BlazorWasm-CSharp/Shared/Authorization/UserInfo.cs
@@ -8,42 +8,6 @@ namespace BlazorWasm_CSharp.Shared.Authorization
     {
         public static readonly UserInfo Anonymous = new UserInfo();
 
-        public UserInfo() { }
-
-        public UserInfo(ClaimsPrincipal claimsPrincipal)
-        {
-            var identity = claimsPrincipal.Identity;
-            IsAuthenticated = identity.AuthenticationType != null;
-            if (identity is ClaimsIdentity claimsIdentity)
-            {
-                NameClaimType = claimsIdentity.NameClaimType;
-                RoleClaimType = claimsIdentity.RoleClaimType;
-            }
-            else
-            {
-                NameClaimType = "name";
-                RoleClaimType = "roles";
-            }
-
-            if (claimsPrincipal.Claims.Any())
-            {
-                var claims = new List<ClaimValue>();
-                var nameClaims = claimsPrincipal.FindAll(NameClaimType);
-                foreach (var claim in nameClaims)
-                {
-                    claims.Add(new ClaimValue(NameClaimType, claim.Value));
-                }
-
-                // Uncomment this code if you want to send additional claims to the client.
-                //foreach (var claim in claimsPrincipal.Claims.Except(nameClaims))
-                //{
-                //    claims.Add(new ClaimValue(claim.Type, claim.Value));
-                //}
-
-                Claims = claims;
-            }
-        }
-
         public bool IsAuthenticated { get; set; }
 
         public string NameClaimType { get; set; }
