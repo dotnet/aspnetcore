a857d644dccc309013ef48d53660a5152fa681c5
Javier Calvarro Nelson <jacalvar@microsoft.com>
2020-01-17 08:25:06 -0800

    Fix missing namespace

--- src/ProjectTemplates/BlazorWasm.ProjectTemplates/content/BlazorWasm-CSharp/Server/Controllers/UserController.cs
+++ src/ProjectTemplates/BlazorWasm.ProjectTemplates/content/BlazorWasm-CSharp/Server/Controllers/UserController.cs
@@ -1,6 +1,7 @@
-﻿using BlazorWasm_CSharp.Shared.Authorization;
+﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using BlazorWasm_CSharp.Shared.Authorization;
 
 namespace BlazorWasm_CSharp.Server.Controllers
 {
