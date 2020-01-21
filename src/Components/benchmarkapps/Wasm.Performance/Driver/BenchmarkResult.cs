ba08d60cb95c01e8de4ebeda3948217fcc6b24a8
Pranav K <prkrishn@hotmail.com>
2020-01-21 14:43:34 -0800

    Update names and descriptions for benchmarks (#18430)

--- src/Components/benchmarkapps/Wasm.Performance/Driver/BenchmarkResult.cs
+++ src/Components/benchmarkapps/Wasm.Performance/Driver/BenchmarkResult.cs
@@ -4,10 +4,21 @@ namespace Wasm.Performance.Driver
     {
         public string Name { get; set; }
 
+        public BenchmarkDescriptor Descriptor { get; set; }
+
+        public string ShortDescription { get; set; }
+
         public bool Success { get; set; }
 
         public int NumExecutions { get; set; }
 
         public double Duration { get; set; }
+
+        public class BenchmarkDescriptor
+        {
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+        }
     }
 }
