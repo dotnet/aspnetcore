9f2c3dfad048904ea4f805a64555d08c44d4faf2
Justin Kotalik <jkotalik12@gmail.com>
2020-01-31 15:42:43 -0800

    Update Http2StreamStack.cs

--- src/Servers/Kestrel/Core/src/Internal/Http2/Http2StreamStack.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http2/Http2StreamStack.cs
@@ -1,8 +1,12 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System;
 using System.Runtime.CompilerServices;
 
 namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2
 {
+    // See https://github.com/dotnet/runtime/blob/master/src/libraries/System.IO.Pipelines/src/System/IO/Pipelines/BufferSegmentStack.cs
     internal struct Http2StreamStack
     {
         private Http2StreamAsValueType[] _array;
