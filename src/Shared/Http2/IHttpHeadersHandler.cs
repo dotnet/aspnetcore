6e331a8c8f6410e8fef009c7f2afc7d318169f94
Justin Kotalik <jukotali@microsoft.com>
2020-02-11 11:57:44 -0800

    Update shared code

--- src/Shared/Http2/IHttpHeadersHandler.cs
+++ src/Shared/Http2/IHttpHeadersHandler.cs
@@ -2,13 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#if KESTREL
 using System;
 
-namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
-#else
 namespace System.Net.Http
-#endif
 {
 #if KESTREL
     public
