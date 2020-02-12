f34f4c0435f18ff922a6a91b449d0a75f9e0fbe8
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-02-12 14:44:01 +0000

    Write assets manifest in JSON form

---
+++ src/Components/Blazor/Build/src/Tasks/WriteAssetsManifestFile.cs
@@ -0,0 +1,66 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Microsoft.AspNetCore.Blazor.Build
+{
+    public class WriteAssetsManifestFile : Task
+    {
+        [Required]
+        public ITaskItem[] AssetsWithHashes { get; set; }
+
+        [Required]
+        public string OutputPath { get; set; }
+
+        public override bool Execute()
+        {
+            using var fileStream = File.Create(OutputPath);
+            WriteFile(fileStream);
+            return true;
+        }
+
+        internal void WriteFile(Stream stream)
+        {
+            var data = new AssetsManifestFile
+            {
+                assets = AssetsWithHashes.Select(item => new AssetsManifestFileEntry
+                {
+                    url = item.GetMetadata("AssetUrl"),
+                    hash = item.GetMetadata("FileHash"),
+                }).ToArray()
+            };
+
+            using var writer = JsonReaderWriterFactory.CreateJsonWriter(stream, Encoding.UTF8, ownsStream: false, indent: true);
+            new DataContractJsonSerializer(typeof(AssetsManifestFile)).WriteObject(writer, data);
+        }
+
+#pragma warning disable IDE1006 // Naming Styles
+        public class AssetsManifestFile
+        {
+            /// <summary>
+            /// Gets or sets the assets. Keys are URLs; values are base-64-formatted SHA256 content hashes.
+            /// </summary>
+            public AssetsManifestFileEntry[] assets { get; set; }
+        }
+
+        public class AssetsManifestFileEntry
+        {
+            /// <summary>
+            /// Gets or sets the asset URL. Normally this will be relative to the application's base href.
+            /// </summary>
+            public string url { get; set; }
+
+            /// <summary>
+            /// Gets or sets the file content hash. This should be the base-64-formatted SHA256 value.
+            /// </summary>
+            public string hash { get; set; }
+        }
+#pragma warning restore IDE1006 // Naming Styles
+    }
+}
