2152288956bc69015c6300657012736260e5caaa
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-02-12 23:45:43 +0000

    Update project template

--- src/ProjectTemplates/BlazorWasm.ProjectTemplates/content/BlazorWasm-CSharp/Client/wwwroot/service-worker.published.js
+++ src/ProjectTemplates/BlazorWasm.ProjectTemplates/content/BlazorWasm-CSharp/Client/wwwroot/service-worker.published.js
@@ -1,25 +1,12 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
-//
-// This service worker implements a cache-first policy. For offline support to work,
-// you must ensure that 'resourcesAvailableOffline' lists all the resources needed
-// by your application (except for .NET assemblies and dotnet.wasm).
-
-const resourcesAvailableOffline = [
-    'index.html',
-    'manifest.json',
-    'icon-512.png',
-    '_framework/blazor.boot.json',
-    '_framework/blazor.webassembly.js',
-    '_framework/wasm/dotnet.js',
-    'css/site.css',
-    'css/bootstrap/bootstrap.min.css',
-    'css/open-iconic/font/css/open-iconic-bootstrap.min.css',
-    'css/open-iconic/font/fonts/open-iconic.woff'
-];
 
 const cacheNamePrefix = 'offline-cache-';
-const cacheName = () => `${cacheNamePrefix}${serviceWorkerVersion}`;
+const cacheName = cacheNamePrefix + '{version}';
+const offlineResourcesInclude = [
+    /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/,
+    /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/
+];
 
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
@@ -27,8 +14,13 @@ self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
 async function onInstall(event) {
     console.info('Service worker: Install');
-    const cache = await caches.open(cacheName());
-    await cache.addAll(resourcesAvailableOffline);
+
+    // Fetch and cache all matching items from assets.json
+    const assetsManifest = await fetch('assets.json').then(response => response.json());
+    const assetsRequests = assetsManifest.assets
+        .filter(asset => offlineResourcesInclude.some(pattern => pattern.test(asset.url)))
+        .map(asset => new Request(asset.url, { integrity: asset.hash }));
+    await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
 }
 
 async function onActivate(event) {
@@ -36,8 +28,8 @@ async function onActivate(event) {
 
     // Delete unused caches
     const cacheKeys = await caches.keys();
-    return Promise.all(cacheKeys
-        .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName())
+    await Promise.all(cacheKeys
+        .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
         .map(key => caches.delete(key)));
 }
 
@@ -49,7 +41,7 @@ async function onFetch(event) {
         const shouldServeIndexHtml = event.request.mode === 'navigate';
 
         const request = shouldServeIndexHtml ? 'index.html' : event.request;
-        const cache = await caches.open(cacheName());
+        const cache = await caches.open(cacheName);
         cachedResponse = await cache.match(request);
     }
 
