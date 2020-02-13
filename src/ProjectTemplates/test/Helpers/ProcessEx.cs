1704910b843052cc789c8988ece6bf71094376ea
Hao Kung <HaoK@users.noreply.github.com>
2020-02-12 20:16:59 -0800

    Update ProcessEx.cs

--- src/ProjectTemplates/test/Helpers/ProcessEx.cs
+++ src/ProjectTemplates/test/Helpers/ProcessEx.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
