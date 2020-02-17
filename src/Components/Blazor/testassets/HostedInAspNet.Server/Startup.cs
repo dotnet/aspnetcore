3a0e839a9ac89afd610969d65e8b398dbb945d2a
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-02-17 15:22:24 +0000

    E2E tests for caching

--- src/Components/Blazor/testassets/HostedInAspNet.Server/Startup.cs
+++ src/Components/Blazor/testassets/HostedInAspNet.Server/Startup.cs
@@ -14,11 +14,19 @@ namespace HostedInAspNet.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<RequestLog>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, RequestLog requestLog)
         {
+            app.Use((context, next) =>
+            {
+                // This is used by E2E tests to verify that the correct resources were fetched
+                requestLog.AddRequest(context.Request);
+                return next();
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
