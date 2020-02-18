c28b82f17a85e76cab1f6cb168a541ad51ffac96
Hao Kung <HaoK@users.noreply.github.com>
2020-02-18 11:20:11 -0800

    Update IdentityUIPackageTest.cs

--- src/ProjectTemplates/test/IdentityUIPackageTest.cs
+++ src/ProjectTemplates/test/IdentityUIPackageTest.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Testing;
 using Templates.Test.Helpers;
 using Xunit;
 using Xunit.Abstractions;
