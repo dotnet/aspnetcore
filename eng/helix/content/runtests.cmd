647fa7d5ddf0b5ed70ebdfc24c2c68c2d400a13f
Hao Kung <HaoK@users.noreply.github.com>
2020-02-19 12:55:19 -0800

    Update runtests.cmd

--- eng/helix/content/runtests.cmd
+++ eng/helix/content/runtests.cmd
@@ -30,7 +30,7 @@ echo "Current Directory: %HELIX_WORKITEM_ROOT%"
 set HELIX=%helixQueue%
 set HELIX_DIR=%HELIX_WORKITEM_ROOT%
 set NUGET_FALLBACK_PACKAGES=%HELIX_DIR%
-set NUGET_RESTORE=%cd%\nugetRestore
+set NUGET_RESTORE=%HELIX_DIR%\nugetRestore
 echo "Setting HELIX_DIR: %HELIX_DIR%"
 echo Creating nuget restore directory: %NUGET_RESTORE%
 mkdir %NUGET_RESTORE%
