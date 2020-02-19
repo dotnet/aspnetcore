e22ac9c93f4e94e432cf07c267ef4c0edc4d09b6
Chris R <Tratcher@outlook.com>
2020-02-19 10:19:56 -0800

    Consume the internalized System.Net.Quic sources

---
+++ src/Shared/runtime/Quic/NetEventSource.Quic.cs
@@ -0,0 +1,13 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics.Tracing;
+
+namespace System.Net
+{
+    [EventSource(Name = "Microsoft-System-Net-Quic")]
+    internal sealed partial class NetEventSource : EventSource
+    {
+    }
+}
