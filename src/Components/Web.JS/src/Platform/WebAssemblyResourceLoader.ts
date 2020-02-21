8a7ff772fa0f7e3f4d7ddc761cc5ba55192fa62b
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-02-21 10:35:10 +0000

    Tolerate all the ways caching might be unavailable

--- src/Components/Web.JS/src/Platform/WebAssemblyResourceLoader.ts
+++ src/Components/Web.JS/src/Platform/WebAssemblyResourceLoader.ts
@@ -12,19 +12,13 @@ export class WebAssemblyResourceLoader {
       credentials: 'include',
       cache: networkFetchCacheMode
     });
+    const bootConfig: BootJsonData = await bootConfigResponse.json();
+    const cache = await getCacheToUseIfEnabled(bootConfig);
 
-    // Define a separate cache for each base href, so we're isolated from any other
-    // Blazor application running on the same origin. We need this so that we're free
-    // to purge from the cache anything we're not using and don't let it keep growing,
-    // since we don't want to be worst offenders for space usage.
-    const relativeBaseHref = document.baseURI.substring(document.location.origin.length);
-    const cacheName = `blazor-resources-${relativeBaseHref}`;
-    return new WebAssemblyResourceLoader(
-      await bootConfigResponse.json(),
-      await caches.open(cacheName));
+    return new WebAssemblyResourceLoader(bootConfig, cache);
   }
 
-  constructor (public readonly bootConfig: BootJsonData, private cache: Cache)
+  constructor (public readonly bootConfig: BootJsonData, private cacheIfUsed: Cache | null)
   {
   }
 
@@ -34,21 +28,15 @@ export class WebAssemblyResourceLoader {
   }
 
   loadResource(name: string, url: string, contentHash: string): LoadingResource {
-    // Setting 'cacheBootResources' to false bypasses the entire cache flow, including integrity checking.
-    // This gives developers an easy opt-out if they don't like anything about the default cache mechanism.
+    // Note that if cacheBootResources was explicitly disabled, we also bypass hash checking
+    // This is to give developers an easy opt-out from the entire caching/validation flow if
+    // there's anything they don't like about it.
 
-    // There's also a Chromium bug we need to work around here: the CacheStorage APIs say that when
-    // caches.open(name) returns a promise that succeeds, the value is meant to be a Cache instance.
-    // However, if the browser was launched with a --user-data-dir param that's "too long" in some sense,
-    // then even through the promise resolves as success, the value given is `undefined`.
-    // See https://stackoverflow.com/a/46626574. We're reporting this to Chromium and others, but in
-    // the meantime, if this.cache isn't set, just proceed without caching.
-    const useCache = this.bootConfig.cacheBootResources && this.cache;
+    const response = this.cacheIfUsed
+      ? this.loadResourceWithCaching(this.cacheIfUsed, name, url, contentHash)
+      : fetch(url, { cache: networkFetchCacheMode, integrity: this.bootConfig.cacheBootResources ? contentHash : undefined });
 
-    const response = useCache
-      ? this.loadResourceWithCaching(name, url, contentHash)
-      : fetch(url, { cache: networkFetchCacheMode });
-    return { name, url, response };
+      return { name, url, response };
   }
 
   logToConsole() {
@@ -57,8 +45,12 @@ export class WebAssemblyResourceLoader {
     const cacheResponseBytes = countTotalBytes(cacheLoadsEntries);
     const networkResponseBytes = countTotalBytes(networkLoadsEntries);
     const totalResponseBytes = cacheResponseBytes + networkResponseBytes;
-    const linkerDisabledWarning = this.bootConfig.linkerEnabled ? '%c' : '\n%cThis application was built with linking (tree shaking) disabled. Published applications will be significantly smaller.';
+    if (totalResponseBytes === 0) {
+      // We have no perf stats to display, likely because caching is not in use.
+      return;
+    }
 
+    const linkerDisabledWarning = this.bootConfig.linkerEnabled ? '%c' : '\n%cThis application was built with linking (tree shaking) disabled. Published applications will be significantly smaller.';
     console.groupCollapsed(`%cblazor%c Loaded ${toDataSizeString(totalResponseBytes)} resources${linkerDisabledWarning}`, 'background: purple; color: white; padding: 1px 3px; border-radius: 3px;', 'font-weight: bold;', 'font-weight: normal;');
 
     if (cacheLoadsEntries.length) {
@@ -79,17 +71,20 @@ export class WebAssemblyResourceLoader {
   async purgeUnusedCacheEntriesAsync() {
     // We want to keep the cache small because, even though the browser will evict entries if it
     // gets too big, we don't want to be considered problematic by the end user viewing storage stats
-    const cachedRequests = await this.cache.keys();
-    const deletionPromises = cachedRequests.map(async cachedRequest => {
-      if (!(cachedRequest.url in this.usedCacheKeys)) {
-        await this.cache.delete(cachedRequest);
-      }
-    });
+    const cache = this.cacheIfUsed;
+    if (cache) {
+      const cachedRequests = await cache.keys();
+      const deletionPromises = cachedRequests.map(async cachedRequest => {
+        if (!(cachedRequest.url in this.usedCacheKeys)) {
+          await cache.delete(cachedRequest);
+        }
+      });
 
-    return Promise.all(deletionPromises);
+      await Promise.all(deletionPromises);
+    }
   }
 
-  private async loadResourceWithCaching(name: string, url: string, contentHash: string) {
+  private async loadResourceWithCaching(cache: Cache, name: string, url: string, contentHash: string) {
     // Since we are going to cache the response, we require there to be a content hash for integrity
     // checking. We don't want to cache bad responses. There should always be a hash, because the build
     // process generates this data.
@@ -100,7 +95,7 @@ export class WebAssemblyResourceLoader {
     const cacheKey = toAbsoluteUri(`${url}.${contentHash}`);
     this.usedCacheKeys[cacheKey] = true;
 
-    const cachedResponse = await this.cache.match(cacheKey);
+    const cachedResponse = await cache.match(cacheKey);
     if (cachedResponse) {
       // It's in the cache.
       const responseBytes = parseInt(cachedResponse.headers.get('content-length') || '0');
@@ -109,12 +104,12 @@ export class WebAssemblyResourceLoader {
     } else {
       // It's not in the cache. Fetch from network.
       const networkResponse = await fetch(url, { cache: networkFetchCacheMode, integrity: contentHash });
-      this.addToCacheAsync(name, cacheKey, networkResponse); // Don't await - add to cache in background
+      this.addToCacheAsync(cache, name, cacheKey, networkResponse); // Don't await - add to cache in background
       return networkResponse;
     }
   }
 
-  private async addToCacheAsync(name: string, cacheKey: string, response: Response) {
+  private async addToCacheAsync(cache: Cache, name: string, cacheKey: string, response: Response) {
     // We have to clone in order to put this in the cache *and* not prevent other code from
     // reading the original response stream.
     const responseData = await response.clone().arrayBuffer();
@@ -129,12 +124,40 @@ export class WebAssemblyResourceLoader {
 
     // Add to cache as a custom response object so we can track extra data such as responseBytes
     // We can't rely on the server sending content-length (ASP.NET Core doesn't by default)
-    await this.cache.put(cacheKey, new Response(responseData, {
+    await cache.put(cacheKey, new Response(responseData, {
       headers: {
         'content-type': response.headers.get('content-type') || '',
         'content-length': (responseBytes || response.headers.get('content-length') || '').toString()
       }
     }));
+  }
+}
+
+async function getCacheToUseIfEnabled(bootConfig: BootJsonData): Promise<Cache | null> {
+  // caches will be undefined if we're running on an insecure origin (secure means https or localhost)
+  if (!bootConfig.cacheBootResources || typeof caches === 'undefined') {
+    return null;
+  }
+
+  // Define a separate cache for each base href, so we're isolated from any other
+  // Blazor application running on the same origin. We need this so that we're free
+  // to purge from the cache anything we're not using and don't let it keep growing,
+  // since we don't want to be worst offenders for space usage.
+  const relativeBaseHref = document.baseURI.substring(document.location.origin.length);
+  const cacheName = `blazor-resources-${relativeBaseHref}`;
+
+  try {
+    // There's a Chromium bug we need to be aware of here: the CacheStorage APIs say that when
+    // caches.open(name) returns a promise that succeeds, the value is meant to be a Cache instance.
+    // However, if the browser was launched with a --user-data-dir param that's "too long" in some sense,
+    // then even through the promise resolves as success, the value given is `undefined`.
+    // See https://stackoverflow.com/a/46626574 and https://bugs.chromium.org/p/chromium/issues/detail?id=1054541
+    // If we see this happening, return "null" to mean "proceed without caching".
+    return (await caches.open(cacheName)) || null;
+  } catch {
+    // There's no known scenario where we should get an exception here, but considering the
+    // Chromium bug above, let's tolerate it and treat as "proceed without caching".
+    return null;
   }
 }
 
