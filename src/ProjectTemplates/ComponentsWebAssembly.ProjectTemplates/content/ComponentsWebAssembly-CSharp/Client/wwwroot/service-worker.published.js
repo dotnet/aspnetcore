244d882c25729fb2913c7b2624d538c9c4653623
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-02-27 14:35:40 +0000

    Stop service worker interfering with Identity UI

--- src/ProjectTemplates/ComponentsWebAssembly.ProjectTemplates/content/ComponentsWebAssembly-CSharp/Client/wwwroot/service-worker.published.js
+++ src/ProjectTemplates/ComponentsWebAssembly.ProjectTemplates/content/ComponentsWebAssembly-CSharp/Client/wwwroot/service-worker.published.js
@@ -43,7 +43,13 @@ async function onFetch(event) {
     if (event.request.method === 'GET') {
         // For all navigation requests, try to serve index.html from cache
         // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
+//#if(IndividualLocalAuth && Hosted)
+        const shouldServeIndexHtml = event.request.mode === 'navigate'
+            && !event.request.url.includes('/connect/')
+            && !event.request.url.includes('/Identity/');
+//#else
         const shouldServeIndexHtml = event.request.mode === 'navigate';
+//#endif
 
         const request = shouldServeIndexHtml ? 'index.html' : event.request;
         const cache = await caches.open(cacheName);
