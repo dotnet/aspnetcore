d8812083d20f291e93a40e95bc533ff3651ab6cd
Tom Deseyn <tom.deseyn@gmail.com>
2020-02-27 18:40:57 +0100

    Update comment

--- src/Servers/Kestrel/Transport.Sockets/src/SocketTransportOptions.cs
+++ src/Servers/Kestrel/Transport.Sockets/src/SocketTransportOptions.cs
@@ -17,7 +17,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets
         public int IOQueueCount { get; set; } = Math.Min(Environment.ProcessorCount, 16);
 
         /// <summary>
-        /// Wait until there is data available to allocate a buffer. Set this to true to reduce memory used for idle connections.
+        /// Wait until there is data available to allocate a buffer. Setting this to false can increase throughput at the cost of increased memory usage.
         /// </summary>
         /// <remarks>
         /// Defaults to true.
