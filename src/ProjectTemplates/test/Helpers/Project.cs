3527312e29b9ea2b8c1c064e737ed1f0d9f05431
Hao Kung <HaoK@users.noreply.github.com>
2020-02-28 00:39:19 -0800

    Update Project.cs

--- src/ProjectTemplates/test/Helpers/Project.cs
+++ src/ProjectTemplates/test/Helpers/Project.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.CommandLineUtils;
