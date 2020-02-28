98654e58e03c128d036bd05bfb314887f8238ff5
Hao Kung <HaoK@users.noreply.github.com>
2020-02-28 08:13:15 -0800

    Update runtests.sh

--- eng/helix/content/runtests.sh
+++ eng/helix/content/runtests.sh
@@ -104,7 +104,7 @@ then
     dotnet nuget add source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet5/nuget/v3/index.json
     dotnet nuget list source
     
-    dotnet tool install dotnet-ef --version $efVersion
+    dotnet tool install dotnet-ef --global --version $efVersion
 fi
 
 if [ -e /proc/self/coredump_filter ]; then
