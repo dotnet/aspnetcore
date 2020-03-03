21068e0a45875714a9189489301b67c31c2ecdfe
Chris Ross <Tratcher@Outlook.com>
2020-03-03 09:57:24 -0800

    Update shared code license headers (#19518)

--- src/Shared/runtime/Http2/Hpack/HPackDecodingException.cs
+++ src/Shared/runtime/Http2/Hpack/HPackDecodingException.cs
@@ -1,6 +1,6 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0.
-// See THIRD-PARTY-NOTICES.TXT in the project root for license information.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 using System.Runtime.Serialization;
 
