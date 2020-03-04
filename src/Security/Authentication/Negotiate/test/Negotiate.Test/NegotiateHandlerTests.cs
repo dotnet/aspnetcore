eef063a1335ab3bb9d31bf70bad80a14a209eb59
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2020-03-04 08:59:12 -0800

    quarantine all negotiate tests

--- src/Security/Authentication/Negotiate/test/Negotiate.Test/NegotiateHandlerTests.cs
+++ src/Security/Authentication/Negotiate/test/Negotiate.Test/NegotiateHandlerTests.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Net.Http.Headers;
@@ -21,6 +22,7 @@ using Xunit.Sdk;
 
 namespace Microsoft.AspNetCore.Authentication.Negotiate
 {
+    [QuarantinedTest]
     public class NegotiateHandlerTests
     {
         [Fact]
