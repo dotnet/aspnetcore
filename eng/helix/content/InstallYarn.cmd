1a889f23b82a909d200f7be4f9a11fb7739e2cad
Hao Kung <HaoK@users.noreply.github.com>
2020-03-11 15:36:31 -0700

    Update InstallYarn.cmd

--- eng/helix/content/InstallYarn.cmd
+++ eng/helix/content/InstallYarn.cmd
@@ -1,9 +1,5 @@
 set PATH=%HELIX_WORKITEM_ROOT%;%PATH%;%HELIX_CORRELATION_PAYLOAD%\jdk\bin
-where npm
-echo Contents of %HELIX_WORKITEM_ROOT%\node_modules
-dir %HELIX_WORKITEM_ROOT%\node_modules
 call npm.cmd i yarn
 call yarn install
-call npm.cmd install npm@latest -g
 echo Contents of %HELIX_WORKITEM_ROOT%\node_modules
 dir %HELIX_WORKITEM_ROOT%\node_modules
