474270d4d9116de169fb8ae1a492276974b53ae5
Hao Kung <HaoK@users.noreply.github.com>
2020-03-13 11:02:29 -0700

    Manually restore npm/yarn

--- eng/helix/content/InstallYarn.cmd
+++ eng/helix/content/InstallYarn.cmd
@@ -1,13 +1,17 @@
 set PATH=%HELIX_WORKITEM_ROOT%;%PATH%;%HELIX_CORRELATION_PAYLOAD%\jdk\bin
 
-echo Contents of %HELIX_WORKITEM_ROOT%\node_modules
-dir %HELIX_WORKITEM_ROOT%\node_modules
-
 call npm.cmd i yarn
 echo Contents of %HELIX_WORKITEM_ROOT%\node_modules after npm i yarn
 dir %HELIX_WORKITEM_ROOT%\node_modules
 
-call yarn install --verbose
+xcopy /S /I /E %HELIX_WORKITEM_ROOT%\node_modules\npm npmBak
+xcopy /S /I /E %HELIX_WORKITEM_ROOT%\node_modules\yarn yarnBak
+
+call yarn install
+
+xcopy /S /I /E npmBak %HELIX_WORKITEM_ROOT%\node_modules\npm
+xcopy /S /I /E yarnBak %HELIX_WORKITEM_ROOT%\node_modules\yarn
 
 echo Contents of %HELIX_WORKITEM_ROOT%\node_modules after yarn install
 dir %HELIX_WORKITEM_ROOT%\node_modules
+
