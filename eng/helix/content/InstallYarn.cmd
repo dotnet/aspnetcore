3dcea8c842186e36d3ecab963f519795f231f6d4
Hao Kung <HaoK@users.noreply.github.com>
2020-03-13 20:21:38 -0700

    Remove spew

--- eng/helix/content/InstallYarn.cmd
+++ eng/helix/content/InstallYarn.cmd
@@ -1,17 +1,7 @@
 set PATH=%HELIX_WORKITEM_ROOT%;%PATH%;%HELIX_CORRELATION_PAYLOAD%\jdk\bin
-
 call npm.cmd i yarn
-echo Contents of %HELIX_WORKITEM_ROOT%\node_modules after npm i yarn
-dir %HELIX_WORKITEM_ROOT%\node_modules
-
-xcopy /S /I /E %HELIX_WORKITEM_ROOT%\node_modules\npm npmBak
-xcopy /S /I /E %HELIX_WORKITEM_ROOT%\node_modules\yarn yarnBak
-
+xcopy /S /I /E /Q %HELIX_WORKITEM_ROOT%\node_modules\npm npmBak
+xcopy /S /I /E /Q %HELIX_WORKITEM_ROOT%\node_modules\yarn yarnBak
 call yarn install
-
-xcopy /S /I /E npmBak %HELIX_WORKITEM_ROOT%\node_modules\npm
-xcopy /S /I /E yarnBak %HELIX_WORKITEM_ROOT%\node_modules\yarn
-
-echo Contents of %HELIX_WORKITEM_ROOT%\node_modules after yarn install
-dir %HELIX_WORKITEM_ROOT%\node_modules
-
+xcopy /S /I /E /Q npmBak %HELIX_WORKITEM_ROOT%\node_modules\npm
+xcopy /S /I /E /Q yarnBak %HELIX_WORKITEM_ROOT%\node_modules\yarn
