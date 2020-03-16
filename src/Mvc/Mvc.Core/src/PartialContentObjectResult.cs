02598de8227041228ff2332408da9b1ca9e65817
Ivan Borges <ivanborges@live.com>
2020-03-16 09:32:58 -0300

    Add objects result for a better restful webapi response treatment.

---
+++ src/Mvc/Mvc.Core/src/PartialContentObjectResult.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Microsoft.AspNetCore.Mvc
+{
+    /// <summary>
+    /// An <see cref="ObjectResult"/> that when executed will produce a <see cref="StatusCodes.Status206PartialContent"/> response.
+    /// </summary>
+    [DefaultStatusCode(DefaultStatusCode)]
+    public class PartialContentObjectResult : ObjectResult
+    {
+        private const int DefaultStatusCode = StatusCodes.Status206PartialContent;
+
+        /// <summary>
+        /// Creates a new <see cref="PartialContentObjectResult"/> instance.
+        /// </summary>
+        /// <param name="value">The value to format in the entity body.</param>
+        public PartialContentObjectResult([ActionResultObjectValue]object value)
+            : base(value)
+        {
+            StatusCode = DefaultStatusCode;
+        }
+    }
+}
---
+++ src/Mvc/Mvc.Core/src/PreconditionFailedObjectResult.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Microsoft.AspNetCore.Mvc
+{
+    /// <summary>
+    /// An <see cref="ObjectResult"/> that when executed will produce a <see cref="StatusCodes.Status422UnprocessableEntity"/> response.
+    /// </summary>
+    [DefaultStatusCode(DefaultStatusCode)]
+    public class PreconditionFailedObjectResult : ObjectResult
+    {
+        private const int DefaultStatusCode = StatusCodes.Status412PreconditionFailed;
+
+        /// <summary>
+        /// Creates a new <see cref="PreconditionFailedObjectResult"/> instance.
+        /// </summary>
+        /// <param name="value">The value to format in the entity body.</param>
+        public PreconditionFailedObjectResult(object value)
+            : base(value)
+        {
+            StatusCode = DefaultStatusCode;
+        }
+    }
+}
