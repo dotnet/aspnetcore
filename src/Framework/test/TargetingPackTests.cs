dc1a955db616452e89e6ba46e52fcd99524f5d36
Hao Kung <HaoK@users.noreply.github.com>
2020-03-18 20:41:21 -0700

    Look for targeting pack in right place

--- src/Framework/test/TargetingPackTests.cs
+++ src/Framework/test/TargetingPackTests.cs
@@ -26,6 +26,9 @@ namespace Microsoft.AspNetCore
         {
             _output = output;
             _expectedRid = TestData.GetSharedFxRuntimeIdentifier();
+            var root = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("helix")) 
+                ? TestData.GetTestDataValue("TargetingPackLayoutRoot") 
+                : Environment.GetEnvironmentVariable("DOTNET_ROOT");
             _targetingPackRoot = Path.Combine(TestData.GetTestDataValue("TargetingPackLayoutRoot"), "packs", "Microsoft.AspNetCore.App.Ref", TestData.GetTestDataValue("TargetingPackVersion"));
             _isTargetingPackBuilding = bool.Parse(TestData.GetTestDataValue("IsTargetingPackBuilding"));
         }
