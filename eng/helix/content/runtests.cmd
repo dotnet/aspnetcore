1f68e9d8d82ed8d3207d1fbeb308a31d1deba9c3
Hao Kung <HaoK@users.noreply.github.com>
2020-03-19 19:31:10 -0700

    Update runtests.cmd

--- eng/helix/content/runtests.cmd
+++ eng/helix/content/runtests.cmd
@@ -96,8 +96,9 @@ if %$quarantined%==true (
 echo "Copying TestResults\TestResults.xml to ."
 copy TestResults\TestResults.xml testResults.xml
 echo "Copying artifacts/logs to %HELIX_WORKITEM_UPLOAD_ROOT%/../"
-for /R artifacts/log %%f in (*.log) do copy %%f %HELIX_WORKITEM_UPLOAD_ROOT%/../
-for /R artifacts/log %%f in (*.log) do copy %%f %HELIX_WORKITEM_UPLOAD_ROOT%/
+echo "for /R artifacts/log %%f in (*.log) do copy %%f %HELIX_WORKITEM_UPLOAD_ROOT%/../"
+for /R artifacts/log %%f in (*.log) do echo "copy %%f %HELIX_WORKITEM_UPLOAD_ROOT%/../"
+for /R artifacts/log %%f in (*.log) do echo "copy %%f %HELIX_WORKITEM_UPLOAD_ROOT%/"
 
 exit /b %exit_code%
 
