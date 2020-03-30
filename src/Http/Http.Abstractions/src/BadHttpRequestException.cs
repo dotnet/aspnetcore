73245624282b288baa1677cfbe0ae37defec607e
Chris Oliver <cado82@gmail.com>
2020-03-31 00:34:48 +0100

    Add new BadHttpRequestException to Http Abstractions

---
+++ src/Http/Http.Abstractions/src/BadHttpRequestException.cs
@@ -0,0 +1,36 @@
+using System.IO;
+
+namespace Microsoft.AspNetCore.Http
+{
+    /// <summary>
+    /// Represents an HTTP request error
+    /// </summary>
+    public sealed class BadHttpRequestException : IOException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BadHttpRequestException"/> class.
+        /// </summary>
+        /// <param name="message">The message to associate with this exception.</param>
+        /// <param name="statusCode">The HTTP status code to associate with this exception.</param>
+        public BadHttpRequestException(string message, int statusCode)
+            : base(message)
+        {
+            StatusCode = statusCode;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BadHttpRequestException"/> class with the <see cref="StatusCode"/> set to 400 Bad Request.
+        /// </summary>
+        /// <param name="message">The message to associate with this exception</param>
+        public BadHttpRequestException(string message)
+            : base(message)
+        {
+            StatusCode = StatusCodes.Status400BadRequest;
+        }
+
+        /// <summary>
+        /// Gets the HTTP status code for this exception.
+        /// </summary>
+        public int StatusCode { get; }
+    }
+}
