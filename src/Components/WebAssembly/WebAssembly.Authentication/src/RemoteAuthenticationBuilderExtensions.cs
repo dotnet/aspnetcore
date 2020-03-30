ab29ab637f97ff40fba78181cf4386ee9dfe9b54
Javier Calvarro Nelson <jacalvar@microsoft.com>
2020-03-30 08:24:10 -0700

    Fix namespace

--- src/Components/WebAssembly/WebAssembly.Authentication/src/RemoteAuthenticationBuilderExtensions.cs
+++ src/Components/WebAssembly/WebAssembly.Authentication/src/RemoteAuthenticationBuilderExtensions.cs
@@ -1,10 +1,10 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
-namespace Microsoft.AspNetCore.Components.WebAssembly.Authentication
+namespace Microsoft.Extensions.DependencyInjection
 {
     /// <summary>
     /// Extensions for remote authentication services.
