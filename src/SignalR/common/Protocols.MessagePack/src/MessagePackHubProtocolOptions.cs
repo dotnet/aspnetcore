96095a7d0a11ac4dfcb9c4649a2c90edd5299595
TeBeCo <tebeco@gmail.com>
2020-04-02 20:51:56 +0200

    Update src/SignalR/common/Protocols.MessagePack/src/MessagePackHubProtocolOptions.cs
    Co-Authored-By: Andrew Stanton-Nurse <andrew@stanton-nurse.com>

--- src/SignalR/common/Protocols.MessagePack/src/MessagePackHubProtocolOptions.cs
+++ src/SignalR/common/Protocols.MessagePack/src/MessagePackHubProtocolOptions.cs
@@ -15,7 +15,7 @@ namespace Microsoft.AspNetCore.SignalR
         /// <para>Gets or sets the <see cref="MessagePackSerializerOptions"/> used internally by the <see cref="MessagePackSerializer" />.</para>
         /// <para>If you override the default value, we strongly recommend that you set <see cref="MessagePackSecurity" /> to <see cref="MessagePackSecurity.UntrustedData"/> by calling:</para>
         /// <code>customMessagePackSerializerOptions = customMessagePackSerializerOptions.WithSecurity(MessagePackSecurity.UntrustedData)</code>.
-        /// If you want to modify the default options you need to assign the options back to the <see cref="SerializerOptions" /> after modifications:
+        /// If you modify the default options you must also assign the updated options back to the <see cref="SerializerOptions" /> property:
         /// <code>options.SerializerOptions = options.SerializerOptions.WithResolver(new CustomResolver());</code>.
         /// </summary>
         public MessagePackSerializerOptions SerializerOptions
