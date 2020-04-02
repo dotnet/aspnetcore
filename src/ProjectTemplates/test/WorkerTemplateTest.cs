03b84fc3dae26334e26645d425d40747f2ea49c6
Hao Kung <HaoK@users.noreply.github.com>
2020-04-02 16:00:33 -0700

    Update WorkerTemplateTest.cs

--- src/ProjectTemplates/test/WorkerTemplateTest.cs
+++ src/ProjectTemplates/test/WorkerTemplateTest.cs
@@ -21,8 +21,8 @@ namespace Templates.Test
         public ProjectFactoryFixture ProjectFactory { get; }
         public ITestOutputHelper Output { get; }
 
-        [Fact]
-        [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/19716")]
+        [ConditionalFact]
+        [OSSkipCondition(OperatingSystems.MacOSX)]
         public async Task WorkerTemplateAsync()
         {
             Project = await ProjectFactory.GetOrCreateProject("worker", Output);
