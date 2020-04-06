ee1ba49be21716889dc967c3e0aea548a7beeb7c
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-04-06 18:44:10 +0100

    Update ws-proxy sources to match Mono commit c5149e31b4d7c

--- src/Components/WebAssembly/DebugProxy/src/MonoDebugProxy/ws-proxy/DevToolsProxy.cs
+++ src/Components/WebAssembly/DebugProxy/src/MonoDebugProxy/ws-proxy/DevToolsProxy.cs
@@ -11,103 +11,6 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 
 namespace WebAssembly.Net.Debugging {
-	internal struct SessionId {
-		public readonly string sessionId;
-
-		public SessionId (string sessionId)
-		{
-			this.sessionId = sessionId;
-		}
-
-		public override int GetHashCode ()
-			=> sessionId?.GetHashCode () ?? 0;
-
-		public override bool Equals (object obj)
-			=> (obj is SessionId) ? ((SessionId) obj).sessionId == sessionId : false;
-
-		public override string ToString ()
-			=> $"session-{sessionId}";
-	}
-
-	internal struct MessageId {
-		public readonly string sessionId;
-		public readonly int id;
-
-		public MessageId (string sessionId, int id)
-		{
-			this.sessionId = sessionId;
-			this.id = id;
-		}
-
-		public static implicit operator SessionId (MessageId id)
-			=> new SessionId (id.sessionId);
-
-		public override string ToString ()
-			=> $"msg-{sessionId}:::{id}";
-
-		public override int GetHashCode ()
-			=> (sessionId?.GetHashCode () ?? 0) ^ id.GetHashCode ();
-
-		public override bool Equals (object obj)
-			=> (obj is MessageId) ? ((MessageId) obj).sessionId == sessionId && ((MessageId) obj).id == id : false;
-	}
-
-	internal struct Result {
-		public JObject Value { get; private set; }
-		public JObject Error { get; private set; }
-
-		public bool IsOk => Value != null;
-		public bool IsErr => Error != null;
-
-		Result (JObject result, JObject error)
-		{
-			if (result != null && error != null)
-				throw new ArgumentException ($"Both {nameof(result)} and {nameof(error)} arguments cannot be non-null.");
-
-			bool resultHasError = String.Compare ((result? ["result"] as JObject)? ["subtype"]?. Value<string> (), "error") == 0;
-			if (result != null && resultHasError) {
-				this.Value = null;
-				this.Error = result;
-			} else {
-				this.Value = result;
-				this.Error = error;
-			}
-		}
-
-		public static Result FromJson (JObject obj)
-		{
-			//Log ("protocol", $"from result: {obj}");
-			return new Result (obj ["result"] as JObject, obj ["error"] as JObject);
-		}
-
-		public static Result Ok (JObject ok)
-			=> new Result (ok, null);
-
-		public static Result OkFromObject (object ok)
-			=> Ok (JObject.FromObject(ok));
-
-		public static Result Err (JObject err)
-			=> new Result (null, err);
-
-		public static Result Exception (Exception e)
-			=> new Result (null, JObject.FromObject (new { message = e.Message }));
-
-		public JObject ToJObject (MessageId target) {
-			if (IsOk) {
-				return JObject.FromObject (new {
-					target.id,
-					target.sessionId,
-					result = Value
-				});
-			} else {
-				return JObject.FromObject (new {
-					target.id,
-					target.sessionId,
-					error = Error
-				});
-			}
-		}
-	}
 
 	class DevToolsQueue {
 		Task current_send;
@@ -293,11 +196,12 @@ namespace WebAssembly.Net.Debugging {
 			int id = Interlocked.Increment (ref next_cmd_id);
 
 			var o = JObject.FromObject (new {
-				sessionId.sessionId,
 				id,
 				method,
 				@params = args
 			});
+			if (sessionId.sessionId != null)
+				o["sessionId"] = sessionId.sessionId;
 			var tcs = new TaskCompletionSource<Result> ();
 
 			var msgId = new MessageId (sessionId.sessionId, id);
@@ -317,10 +221,11 @@ namespace WebAssembly.Net.Debugging {
 		void SendEventInternal (SessionId sessionId, string method, JObject args, CancellationToken token)
 		{
 			var o = JObject.FromObject (new {
-				sessionId.sessionId,
 				method,
 				@params = args
 			});
+			if (sessionId.sessionId != null)
+				o["sessionId"] = sessionId.sessionId;
 
 			Send (this.ide, o, token);
 		}
