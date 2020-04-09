c05016233e9b9a7b6fed246bb5f1eda54ee851e8
Javier Calvarro Nelson <jacalvar@microsoft.com>
2020-04-09 08:52:08 -0700

    tmp

--- src/Components/WebAssembly/WebAssembly.Authentication/src/Services/ExpiredTokenException.cs
+++ src/Components/WebAssembly/WebAssembly.Authentication/src/Services/ExpiredTokenException.cs
@@ -5,14 +5,19 @@ using System;
 
 namespace Microsoft.AspNetCore.Components.WebAssembly.Authentication
 {
+    /// <summary>
+    /// An <see cref="Exception"/> that is thrown when an <see cref="AuthorizationMessageHandler"/> instance
+    /// is not able to provision an access token.
+    /// </summary>
     public class AccessTokenNotAvailableException : Exception
     {
-        private AccessTokenResult _tokenResult;
+        private readonly NavigationManager _navigation;
+        private readonly AccessTokenResult _tokenResult;
 
-        public AccessTokenNotAvailableException(AccessTokenResult tokenResult)
+        public AccessTokenNotAvailableException(AccessTokenResult tokenResult, NavigationManager navigation)
             : base(message: "Unable to provision an access token for the requested scopes.") =>
-            _tokenResult = tokenResult;
+            (_tokenResult, _navigation) = (tokenResult, navigation);
 
-        public void RedirectToLogin() => _tokenResult.TryGetToken(out _, redirect: true);
+        public void RedirectToLogin() => _navigation.NavigateTo(_tokenResult.RedirectUrl);
     }
 }
