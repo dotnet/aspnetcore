31add2b8e99434958e9e0f07e701ee5590b64809
Ben Adams <thundercat@illyriad.co.uk>
2020-04-25 02:36:02 +0100

    Make GetKnownHttpScheme "safe"

--- src/Servers/Kestrel/Core/src/Internal/Infrastructure/HttpUtilities.cs
+++ src/Servers/Kestrel/Core/src/Internal/Infrastructure/HttpUtilities.cs
@@ -430,20 +430,11 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Infrastructure
         /// <param name="knownScheme">A reference to the known scheme, if the input matches any</param>
         /// <returns>True when memory starts with known http or https schema</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static unsafe bool GetKnownHttpScheme(this Span<byte> span, out HttpScheme knownScheme)
+        public static bool GetKnownHttpScheme(this Span<byte> span, out HttpScheme knownScheme)
         {
-            fixed (byte* data = span)
+            if (span.Length >= sizeof(ulong))
             {
-                return GetKnownHttpScheme(data, span.Length, out knownScheme);
-            }
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static unsafe bool GetKnownHttpScheme(byte* location, int length, out HttpScheme knownScheme)
-        {
-            if (length >= sizeof(ulong))
-            {
-                var scheme = *(ulong*)location;
+                var scheme = BinaryPrimitives.ReadUInt64LittleEndian(span);
                 if ((scheme & _mask7Chars) == _httpSchemeLong)
                 {
                     knownScheme = HttpScheme.Http;
