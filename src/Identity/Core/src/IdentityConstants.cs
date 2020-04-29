21a2b463957e41a772c0fa366f6bdc3863779d1a
Hao Kung <HaoK@users.noreply.github.com>
2020-04-29 14:39:01 -0700

    Switch to const

--- src/Identity/Core/src/IdentityConstants.cs
+++ src/Identity/Core/src/IdentityConstants.cs
@@ -8,25 +8,24 @@ namespace Microsoft.AspNetCore.Identity
     /// </summary>
     public class IdentityConstants
     {
-        private static readonly string CookiePrefix = "Identity";
         /// <summary>
         /// The scheme used to identify application authentication cookies.
         /// </summary>
-        public static readonly string ApplicationScheme = CookiePrefix + ".Application";
+        public const string ApplicationScheme = "Identity.Application";
 
         /// <summary>
         /// The scheme used to identify external authentication cookies.
         /// </summary>
-        public static readonly string ExternalScheme = CookiePrefix + ".External";
+        public const string ExternalScheme = "Identity.External";
 
         /// <summary>
         /// The scheme used to identify Two Factor authentication cookies for saving the Remember Me state.
         /// </summary>
-        public static readonly string TwoFactorRememberMeScheme = CookiePrefix + ".TwoFactorRememberMe";
+        public const string TwoFactorRememberMeScheme = "Identity.TwoFactorRememberMe";
 
         /// <summary>
         /// The scheme used to identify Two Factor authentication cookies for round tripping user identities.
         /// </summary>
-        public static readonly string TwoFactorUserIdScheme = CookiePrefix + ".TwoFactorUserId";
+        public const string TwoFactorUserIdScheme = "Identity.TwoFactorUserId";
     }
 }
