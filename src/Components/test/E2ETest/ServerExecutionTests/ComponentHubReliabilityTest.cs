a9d702624a02ad4ebf593d9bf9c1c69f5702a6f5
Javier Calvarro Nelson <jacalvar@microsoft.com>
2020-05-06 19:12:06 +0200

    [Fixes #19666] [Components] Improve reliability of component quarantined tests (take 2) (#21499)
    * Tries to increase the reliability of the tests by:
      * Trying to ensure that the server is up and running before connecting.
      * Retrying a connection attempt multiple times.

--- src/Components/test/E2ETest/ServerExecutionTests/ComponentHubReliabilityTest.cs
+++ src/Components/test/E2ETest/ServerExecutionTests/ComponentHubReliabilityTest.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Net;
+using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
@@ -20,6 +22,32 @@ namespace Microsoft.AspNetCore.Components.E2ETest.ServerExecutionTests
         public ComponentHubReliabilityTest(BasicTestAppServerSiteFixture<ServerStartup> serverFixture, ITestOutputHelper output)
             : base(serverFixture, output)
         {
+        }
+
+        protected override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            var rootUri = ServerFixture.RootUri;
+            var baseUri = new Uri(rootUri, "/subdir");
+            var client = new HttpClient();
+            for (var i = 0; i < 10; i++)
+            {
+                try
+                {
+                    var response = await client.GetAsync(baseUri + "/_negotiate");
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        break;
+                    }
+                }
+                catch
+                {
+                    await Task.Delay(500);
+                    throw;
+                }
+            }
+
         }
 
         [Fact]
