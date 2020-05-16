71171a2d513ff053a90adf8d73b44b024690dac8
Phillip Carter <pcarter@fastmail.com>
2020-05-16 11:50:28 -0700

    Undo weird formatting

--- src/ProjectTemplates/Web.ProjectTemplates/content/WebApi-FSharp/WeatherForecast.fs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/WebApi-FSharp/WeatherForecast.fs
@@ -3,9 +3,9 @@ namespace Company.WebApplication1
 open System
 
 type WeatherForecast =
-  { Date: DateTime
-    TemperatureC: int
-    Summary: string }
+    { Date: DateTime
+      TemperatureC: int
+      Summary: string }
 
-  member this.TemperatureF =
-      32.0 + (float this.TemperatureC / 0.5556)
+    member this.TemperatureF =
+        32.0 + (float this.TemperatureC / 0.5556)
