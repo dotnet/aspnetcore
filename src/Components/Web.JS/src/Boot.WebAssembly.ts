9949ab46c361f6a57a3562dd85d8f7dd5374ec41
Pranav K <prkrishn@hotmail.com>
2020-05-18 13:21:31 -0700

    Fixup JS (#21922)
    * Fixup JS
    
    * Avoid monkey patching

--- src/Components/Web.JS/src/Boot.WebAssembly.ts
+++ src/Components/Web.JS/src/Boot.WebAssembly.ts
@@ -31,6 +31,11 @@ async function boot(options?: Partial<WebAssemblyStartOptions>): Promise<void> {
   };
 
   // Configure navigation via JS Interop
+  const getBaseUri = window['Blazor']._internal.navigationManager.getBaseURI;
+  const getLocationHref = window['Blazor']._internal.navigationManager.getLocationHref;
+  window['Blazor']._internal.navigationManager.getUnmarshalledBaseURI = () => BINDING.js_string_to_mono_string(getBaseUri());
+  window['Blazor']._internal.navigationManager.getUnmarshalledLocationHref = () => BINDING.js_string_to_mono_string(getLocationHref());
+
   window['Blazor']._internal.navigationManager.listenForNavigationEvents(async (uri: string, intercepted: boolean): Promise<void> => {
     await DotNet.invokeMethodAsync(
       'Microsoft.AspNetCore.Components.WebAssembly',
