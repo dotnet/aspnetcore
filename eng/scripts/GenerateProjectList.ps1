1d89ef8d56f14765174ed92de86f2ab0e360c6d3
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2020-05-20 21:23:49 -0700

    Adjust generation scripts to explicitly choose the MSBuild engine - ensure native assets are included in GenerateReferenceAssemblies.ps1 build - clean up the global state that tools.ps1 corrupts

--- eng/scripts/GenerateProjectList.ps1
+++ eng/scripts/GenerateProjectList.ps1
@@ -1,10 +1,18 @@
 param(
     [switch]$ci
 )
-$ErrorActionPreference = 'stop'
 
+$ErrorActionPreference = 'stop'
+$msbuildEngine = 'dotnet'
 $repoRoot = Resolve-Path "$PSScriptRoot/../.."
 
-& "$repoRoot\eng\common\msbuild.ps1" -ci:$ci "$repoRoot/eng/CodeGen.proj" `
+try {
+  & "$repoRoot\eng\common\msbuild.ps1" -ci:$ci "$repoRoot/eng/CodeGen.proj" `
     /t:GenerateProjectList `
     /bl:artifacts/log/genprojlist.binlog
+} finally {
+  Remove-Item variable:global:_BuildTool -ErrorAction Ignore
+  Remove-Item variable:global:_DotNetInstallDir -ErrorAction Ignore
+  Remove-Item variable:global:_ToolsetBuildProj -ErrorAction Ignore
+  Remove-Item variable:global:_MSBuildExe -ErrorAction Ignore
+}
