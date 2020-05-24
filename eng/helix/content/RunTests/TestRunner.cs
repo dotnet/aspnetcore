a806ae62c3847cfe6a83082cebda04e1a40c21ff
Hao Kung <HaoK@users.noreply.github.com>
2020-05-23 17:08:18 -0700

    Add helix-matrix.yml and windows arm 64 helix queue (#22002)

--- eng/helix/content/RunTests/TestRunner.cs
+++ eng/helix/content/RunTests/TestRunner.cs
@@ -309,6 +309,11 @@ namespace RunTests
             }
 
             var HELIX_WORKITEM_UPLOAD_ROOT = Environment.GetEnvironmentVariable("HELIX_WORKITEM_UPLOAD_ROOT");
+            if (string.IsNullOrEmpty(HELIX_WORKITEM_UPLOAD_ROOT))
+            {
+                Console.WriteLine("No HELIX_WORKITEM_UPLOAD_ROOT specified, skipping log copy");
+                return;
+            }
             Console.WriteLine($"Copying artifacts/log/ to {HELIX_WORKITEM_UPLOAD_ROOT}/");
             if (Directory.Exists("artifacts/log"))
             {
