9f0799ae84de645883ca7d8169ce606c09ce017e
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2020-05-31 17:20:03 -0700

    Remove extra `[SkipOnHelix]` attribute - only need the one compiled into Microsoft.AspNetCore.Testing - update the documentation to reflect this   - nit: address Markdown warnings that VS Code showed

--- src/Components/WebAssembly/Build/test/RuntimeDependenciesResolverTest.cs
+++ src/Components/WebAssembly/Build/test/RuntimeDependenciesResolverTest.cs
@@ -7,7 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using Microsoft.AspNetCore.Testing.xunit;
+using Microsoft.AspNetCore.Testing;
 using Xunit;
 
 namespace Microsoft.AspNetCore.Components.WebAssembly.Build
