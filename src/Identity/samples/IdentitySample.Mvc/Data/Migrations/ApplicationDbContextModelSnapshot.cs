a963bbebf97caf1848c0fe526b6e9b2f97cf5822
Smit Patel <smitpatel@users.noreply.github.com>
2020-06-05 19:35:36 -0700

    Add obsolete disable in older model snapshot
    Revert to using older API
    
    Resolves #22610

--- src/Identity/samples/IdentitySample.Mvc/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ src/Identity/samples/IdentitySample.Mvc/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -15,6 +15,7 @@ namespace IdentitySample.Data.Migrations
     {
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
+#pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rc3")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
@@ -35,7 +36,7 @@ namespace IdentitySample.Data.Migrations
                     b.HasKey("Id");
 
                     b.HasIndex("NormalizedName")
-                        .HasDatabaseName("RoleNameIndex");
+                        .HasName("RoleNameIndex");
 
                     b.ToTable("AspNetRoles");
                 });
@@ -166,11 +167,11 @@ namespace IdentitySample.Data.Migrations
                     b.HasKey("Id");
 
                     b.HasIndex("NormalizedEmail")
-                        .HasDatabaseName("EmailIndex");
+                        .HasName("EmailIndex");
 
                     b.HasIndex("NormalizedUserName")
                         .IsUnique()
-                        .HasDatabaseName("UserNameIndex");
+                        .HasName("UserNameIndex");
 
                     b.ToTable("AspNetUsers");
                 });
@@ -211,6 +212,7 @@ namespace IdentitySample.Data.Migrations
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade);
                 });
+#pragma warning restore 612, 618
         }
     }
 }
