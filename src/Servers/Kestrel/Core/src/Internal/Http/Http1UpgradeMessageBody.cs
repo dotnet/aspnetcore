0cc8e43fcfb3116f5107ed501c5277c9a4506b37
Huei Feng <34702552+hueifeng@users.noreply.github.com>
2020-06-11 06:45:29 +0800

    Update Http1UpgradeMessageBody.cs

--- src/Servers/Kestrel/Core/src/Internal/Http/Http1UpgradeMessageBody.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http/Http1UpgradeMessageBody.cs
@@ -52,6 +52,13 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
             _completed = true;
         }
 
+        public override Task CompleteAsync(Exception exception)
+        {
+            _context.ReportApplicationError(exception);
+            _completed = true;
+            return Task.CompletedTask;
+        }
+
         public override void CancelPendingRead()
         {
             _context.Input.CancelPendingRead();
