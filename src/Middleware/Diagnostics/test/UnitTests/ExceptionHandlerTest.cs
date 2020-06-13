e455f22e1460a4c71817c0a556e213d08c6a7caa
Scott Addie <scott.addie@microsoft.com>
2020-06-12 19:31:39 -0500

    Fix character encoding in string

--- src/Middleware/Diagnostics/test/UnitTests/ExceptionHandlerTest.cs
+++ src/Middleware/Diagnostics/test/UnitTests/ExceptionHandlerTest.cs
@@ -410,7 +410,7 @@ namespace Microsoft.AspNetCore.Diagnostics
             // Assert
             Assert.Equal("An error occurred when configuring the exception handler middleware. " +
                 "Either the 'ExceptionHandlingPath' or the 'ExceptionHandler' property must be set in 'UseExceptionHandler()'. " +
-                @"Alternatively, set one of the aforementioned properties in 'Startup.ConfigureServices' as follows: 'services.Configure&lt;ExceptionHandlerOptions&gt;(options =&gt; { ... });'.",
+                "Alternatively, set one of the aforementioned properties in 'Startup.ConfigureServices' as follows: 'services.Configure<ExceptionHandlerOptions>(options => { ... });'.",
                 exception.Message);
         }
     }
