f04992fab584fd0738ca20ec115b1b6b9dd185d7
Pranav K <prkrishn@hotmail.com>
2020-06-13 20:39:41 -0700

    Add nullable to DataProtection (#22591)
    * Add nullable to DataProtection
    
    Contributes to https://github.com/dotnet/aspnetcore/issues/5680

--- src/Shared/WebEncoders/WebEncoders.cs
+++ src/Shared/WebEncoders/WebEncoders.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 #nullable enable
-
 using System;
 #if NETCOREAPP
 using System.Buffers;
