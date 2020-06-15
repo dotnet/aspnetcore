ff539a118b3e15656f431bccc8035f3933d6c8e8
Hao Kung <HaoK@users.noreply.github.com>
2020-06-15 09:37:19 -0700

    Update runtests.cmd

--- eng/helix/content/runtests.cmd
+++ eng/helix/content/runtests.cmd
@@ -24,13 +24,8 @@ set DOTNET_CLI_HOME=%HELIX_CORRELATION_PAYLOAD%\home
 
 set PATH=%DOTNET_ROOT%;!PATH!;%HELIX_CORRELATION_PAYLOAD%\node\bin
 echo Set path to: %PATH%
-echo "Downloading dotnet-install.ps1"
-powershell.exe -NoProfile -ExecutionPolicy unrestricted -file GetDotNetInstall.ps1
-echo "Installing SDK"
-powershell.exe -NoProfile -ExecutionPolicy unrestricted -file dotnet-install.ps1 -Architecture %$arch% -Version %$sdkVersion% -InstallDir %DOTNET_ROOT%" -NoCdn true
-echo "Installing Runtime"
-echo powershell.exe -NoProfile -ExecutionPolicy unrestricted -file dotnet-install.ps1 -Architecture %$arch% -Runtime dotnet -Version %$runtimeVersion% -InstallDir %DOTNET_ROOT%" -NoCdn true
-powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1'))) -Architecture %$arch% -Runtime dotnet -Version %$runtimeVersion% -InstallDir %DOTNET_ROOT%"
+echo "Invoking GetDotNetInstall.sp1 %$arch% %$sdkVersion% %$runtimeVersion% %DOTNET_ROOT%"
+powershell.exe -NoProfile -ExecutionPolicy unrestricted -file GetDotNetInstall.ps1 %$arch% %$sdkVersion% %$runtimeVersion% %DOTNET_ROOT%
 
 set exit_code=0
 echo "Restore: dotnet restore RunTests\RunTests.csproj --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet5/nuget/v3/index.json --ignore-failed-sources..."
