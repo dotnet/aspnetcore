02d5efb906d749c648e8251e28d0729619d25852
Hao Kung <HaoK@users.noreply.github.com>
2020-06-24 13:26:13 -0700

    Update AuthorizationMiddleware.cs

--- src/Security/Authorization/Policy/src/AuthorizationMiddleware.cs
+++ src/Security/Authorization/Policy/src/AuthorizationMiddleware.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization.Policy;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Microsoft.AspNetCore.Authorization
 {
