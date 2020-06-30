5f0f9e491bdb1c7d57d149f517bbe3c85061fca0
Pranav K <prkrishn@hotmail.com>
2020-06-30 07:36:14 -0700

    Use linker extensibility to enable better trimming
    * Configure TrimmerDefaults=link if unspecified
    * Allow Microsoft.AspNetCore.* and Microsoft.Extensions.* packages to be trimmed.

--- src/Razor/test/testassets/blazorwasm/Program.cs
+++ src/Razor/test/testassets/blazorwasm/Program.cs
@@ -8,6 +8,7 @@ namespace standalone
         {
             GC.KeepAlive(typeof(System.Text.Json.JsonSerializer));
             GC.KeepAlive(typeof(RazorClassLibrary.Class1));
+            GC.KeepAlive(typeof(Microsoft.Extensions.Logging.ILogger));
 #if REFERENCE_classlibrarywithsatelliteassemblies
             GC.KeepAlive(typeof(classlibrarywithsatelliteassemblies.Class1));
 #endif
