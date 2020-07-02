4dc8baec427f1b5203eb68d2919c656e3cf2d64e
Noah Kim <noahbkim@gmail.com>
2020-07-02 17:25:50 -0400

    Separate TrySplitNamespaceAndType tests into class-specific file

--- src/Razor/Microsoft.AspNetCore.Razor.Language/test/DefaultRazorTagHelperBinderPhaseTest.cs
+++ src/Razor/Microsoft.AspNetCore.Razor.Language/test/DefaultRazorTagHelperBinderPhaseTest.cs
@@ -1342,26 +1342,6 @@ namespace Microsoft.AspNetCore.Razor.Language
             Assert.True(result);
         }
 
-        [Theory]
-        [InlineData("", false, "", "")]
-        [InlineData(".", true, "", "")]
-        [InlineData("Foo", true, "", "Foo")]
-        [InlineData("SomeProject.Foo", true, "SomeProject", "Foo")]
-        [InlineData("SomeProject.Foo<Bar>", true, "SomeProject", "Foo<Bar>")]
-        [InlineData("SomeProject.Foo<Bar.Baz>", true, "SomeProject", "Foo<Bar.Baz>")]
-        [InlineData("SomeProject.Foo<Bar.Baz>>", true, "", "SomeProject.Foo<Bar.Baz>>")]
-        [InlineData("SomeProject..Foo<Bar>", true, "SomeProject.", "Foo<Bar>")]
-        public void TrySplitNamespaceAndType_WorksAsExpected(string fullTypeName, bool expectedResult, string expectedNamespace, string expectedTypeName)
-        {
-            // Arrange & Act
-            var result = RazorCSharpUtilities.TrySplitNamespaceAndType(fullTypeName, out var @namespace, out var typeName);
-
-            // Assert
-            Assert.Equal(expectedResult, result);
-            Assert.Equal(expectedNamespace, DefaultRazorTagHelperBinderPhase.ComponentDirectiveVisitor.GetTextSpanContent(@namespace, fullTypeName));
-            Assert.Equal(expectedTypeName, DefaultRazorTagHelperBinderPhase.ComponentDirectiveVisitor.GetTextSpanContent(typeName, fullTypeName));
-        }
-
         private static RazorSourceDocument CreateComponentTestSourceDocument(string content, string filePath = null)
         {
             var sourceDocument = TestRazorSourceDocument.Create(content, filePath: filePath);
---
+++ src/Razor/Microsoft.AspNetCore.Razor.Language/test/RazorCSharpUtilitiesTest.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Xunit;
+
+namespace Microsoft.AspNetCore.Razor.Language
+{
+    public class RazorCSharpUtilitiesTest
+    {
+        [Theory]
+        [InlineData("", false, "", "")]
+        [InlineData(".", true, "", "")]
+        [InlineData("Foo", true, "", "Foo")]
+        [InlineData("SomeProject.Foo", true, "SomeProject", "Foo")]
+        [InlineData("SomeProject.Foo<Bar>", true, "SomeProject", "Foo<Bar>")]
+        [InlineData("SomeProject.Foo<Bar.Baz>", true, "SomeProject", "Foo<Bar.Baz>")]
+        [InlineData("SomeProject.Foo<Bar.Baz>>", true, "", "SomeProject.Foo<Bar.Baz>>")]
+        [InlineData("SomeProject..Foo<Bar>", true, "SomeProject.", "Foo<Bar>")]
+        public void TrySplitNamespaceAndType_WorksAsExpected(string fullTypeName, bool expectedResult, string expectedNamespace, string expectedTypeName)
+        {
+            // Arrange & Act
+            var result = RazorCSharpUtilities.TrySplitNamespaceAndType(fullTypeName, out var @namespace, out var typeName);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+            Assert.Equal(expectedNamespace, DefaultRazorTagHelperBinderPhase.ComponentDirectiveVisitor.GetTextSpanContent(@namespace, fullTypeName));
+            Assert.Equal(expectedTypeName, DefaultRazorTagHelperBinderPhase.ComponentDirectiveVisitor.GetTextSpanContent(typeName, fullTypeName));
+        }
+    }
+}
