f23e13af2445e9407716999cf560170f6cf9e904
Hao Kung <HaoK@users.noreply.github.com>
2020-07-06 13:09:59 -0700

    Set DOTNET_HOME to dotnet_root

--- eng/helix/content/runtests.cmd
+++ eng/helix/content/runtests.cmd
@@ -16,8 +16,8 @@ REM Batch only supports up to 9 arguments using the %# syntax, need to shift to 
 shift
 set $helixTimeout=%9
 
-set DOTNET_HOME=%HELIX_CORRELATION_PAYLOAD%\sdk
 set DOTNET_ROOT=%HELIX_CORRELATION_PAYLOAD%\dotnet
+set DOTNET_HOME=%DOTNET_ROOT%
 set DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 set DOTNET_MULTILEVEL_LOOKUP=0
 set DOTNET_CLI_HOME=%HELIX_CORRELATION_PAYLOAD%\home
