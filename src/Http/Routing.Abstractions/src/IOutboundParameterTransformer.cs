8750cd10386e89b46a1b5811170bd4bb21ebb081
Pranav K <prkrishn@hotmail.com>
2020-07-06 20:24:20 -0700

    Nullable followup for Routing (#23474)
    * Nullable followup for routing.
    
    * Update nullability based on usage
    * Use nullable enable in a few files
    
    * More nullable

--- src/Http/Routing.Abstractions/src/IOutboundParameterTransformer.cs
+++ src/Http/Routing.Abstractions/src/IOutboundParameterTransformer.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 namespace Microsoft.AspNetCore.Routing
@@ -14,6 +14,6 @@ namespace Microsoft.AspNetCore.Routing
         /// </summary>
         /// <param name="value">The route value to transform.</param>
         /// <returns>The transformed value.</returns>
-        string TransformOutbound(object value);
+        string? TransformOutbound(object? value);
     }
 }
