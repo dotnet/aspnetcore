0aeb2ef51e8e3eb732fc9868740442be47fe8973
Artak <34246760+mkArtakMSFT@users.noreply.github.com>
2020-07-06 22:21:02 -0700

    Update Startup.cs

--- src/Components/Samples/BlazorServerApp/Startup.cs
+++ src/Components/Samples/BlazorServerApp/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BlazorServerApp.Data;
 
+// testing codeowners
 namespace BlazorServerApp
 {
     public class Startup
