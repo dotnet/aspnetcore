46e604d446969562d4987e02d2ea5058b4b675f8
Ryan Brandenburg <rybrande@microsoft.com>
2020-07-08 12:56:53 -0700

    Include Assets in helix builds

--- src/ProjectTemplates/test/ByteOrderMarkTest.cs
+++ src/ProjectTemplates/test/ByteOrderMarkTest.cs
@@ -1,12 +1,10 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using Microsoft.AspNetCore.Testing;
 using Xunit;
 using Xunit.Abstractions;
 
