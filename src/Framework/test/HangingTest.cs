20ef2e2032bdc9901852f918869a567f265ecee2
Hao Kung <HaoK@users.noreply.github.com>
2020-07-10 12:06:23 -0700

    Update HangingTest.cs

--- src/Framework/test/HangingTest.cs
+++ src/Framework/test/HangingTest.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
-using ProcessUtilities;
 using Xunit;
 
 namespace XUnitTestProject3
