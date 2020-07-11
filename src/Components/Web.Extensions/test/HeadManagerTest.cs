0ad5577210e937e162d1246e91f1862f3f6613eb
Mackinnon Buck <mackinnon.buck@gmail.com>
2020-07-11 14:24:07 -0700

    Added more functional tests.

--- src/Components/Web.Extensions/test/HeadManagerTest.cs
+++ src/Components/Web.Extensions/test/HeadManagerTest.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Concurrent;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Test.Helpers;
 using Microsoft.JSInterop;
 using Moq;
 using Xunit;
@@ -114,6 +116,65 @@ namespace Microsoft.AspNetCore.Components.Web.Extensions
 
             // Verify that all changes were applied in the correct order
             Assert.All(elements, e => Assert.True(completedOrder.TryDequeue(out var key) && key.Equals(e.Key)));
+        }
+
+        [Theory]
+        [InlineData(null, null, null)]
+        [InlineData("name", "httpEquiv", null)]
+        [InlineData("name", null, "property")]
+        [InlineData(null, "httpEquiv", "property")]
+        [InlineData("name", "httpEquiv", "property")]
+        public void Meta_ThrowsWhenGivenInvalidParameters(string name, string httpEquiv, string property)
+        {
+            var serviceProvider = new TestServiceProvider();
+            serviceProvider.AddService(new HeadManager(Mock.Of<IJSRuntime>()));
+
+            var testRenderer = new TestRenderer(serviceProvider);
+            var rootComponent = new TestHostComponent(BuildMetaComponent(name, httpEquiv, property));
+            var componentId = testRenderer.AssignRootComponentId(rootComponent);
+
+            var ex = Assert.Throws<InvalidOperationException>(() => testRenderer.RenderRootComponent(componentId));
+            Assert.Contains("parameters must contain exactly one of", ex.Message);
+        }
+
+        [Theory]
+        [InlineData("name", null, null)]
+        [InlineData(null, "httpEquiv", null)]
+        [InlineData(null, null, "property")]
+        public void Meta_DoesNotThrowWhenGivenValidParameters(string name, string httpEquiv, string property)
+        {
+            var serviceProvider = new TestServiceProvider();
+            serviceProvider.AddService(new HeadManager(Mock.Of<IJSRuntime>()));
+
+            var testRenderer = new TestRenderer(serviceProvider);
+            var rootComponent = new TestHostComponent(BuildMetaComponent(name, httpEquiv, property));
+            var componentId = testRenderer.AssignRootComponentId(rootComponent);
+
+            testRenderer.RenderRootComponent(componentId);
+        }
+
+        private Action<RenderTreeBuilder> BuildMetaComponent(string name, string httpEquiv, string property)
+            => (builder) =>
+        {
+            builder.OpenComponent<Meta>(0);
+            builder.AddAttribute(1, nameof(Meta.Name), name);
+            builder.AddAttribute(2, nameof(Meta.HttpEquiv), httpEquiv);
+            builder.AddAttribute(3, nameof(Meta.Property), property);
+            builder.AddAttribute(4, nameof(Meta.Content), "test content");
+            builder.CloseComponent();
+        };
+
+        class TestHostComponent : AutoRenderComponent
+        {
+            private readonly Action<RenderTreeBuilder> _buildRenderTree;
+
+            public TestHostComponent(Action<RenderTreeBuilder> buildRenderTree)
+            {
+                _buildRenderTree = buildRenderTree;
+            }
+
+            protected override void BuildRenderTree(RenderTreeBuilder builder)
+                => _buildRenderTree(builder);
         }
 
         class TestHeadElement : HeadElementBase
