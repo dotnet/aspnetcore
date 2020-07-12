f3e753de8b7148d6ba8cbfb3942a460de362040d
Rory Murphy <ndmurph04@gmail.com>
2020-07-12 14:30:32 -0400

    Making ViewLookupCache settable by derived classes. This is necessary to allow derived classes to source the cache via dependency injection. Injecting the cache has the desirable feature of potentially permitting the RazorViewEngine to be injected as scoped rather than as a singleton, while still retaining the performance of caching the output of the heavier-weight initialization. Injecting as scoped is preferable to a singleton, as it allows for greater dynamism - e.g. in a multi-tenant application, there are instances where having different ViewLocationFormats can be useful.

--- src/Mvc/Mvc.Razor/src/RazorViewEngine.cs
+++ src/Mvc/Mvc.Razor/src/RazorViewEngine.cs
@@ -80,7 +80,7 @@ namespace Microsoft.AspNetCore.Mvc.Razor
         /// <summary>
         /// A cache for results of view lookups.
         /// </summary>
-        protected IMemoryCache ViewLookupCache { get; }
+        protected IMemoryCache ViewLookupCache { get; protected set; }
 
         /// <summary>
         /// Gets the case-normalized route value for the specified route <paramref name="key"/>.
