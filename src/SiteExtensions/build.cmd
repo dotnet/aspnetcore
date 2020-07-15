c71bc1527220c4d55356868e2039b87684d6959f
Will Godbe <wigodbe@microsoft.com>
2020-07-15 15:11:38 -0700

    Bring back siteExtensions logs

--- src/SiteExtensions/build.cmd
+++ src/SiteExtensions/build.cmd
@@ -2,7 +2,8 @@
 SET RepoRoot=%~dp0..\..
 
 ECHO Building Microsoft.AspNetCore.Runtime.SiteExtension
-CALL "%RepoRoot%\build.cmd" -projects "%~dp0Runtime\Microsoft.AspNetCore.Runtime.SiteExtension.pkgproj" %*
+CALL "%RepoRoot%\build.cmd" -projects "%~dp0Runtime\Microsoft.AspNetCore.Runtime.SiteExtension.pkgproj" ^
+    "/bl:%RepoRoot%/artifacts/log/SiteExtensions-Runtime-x86.binlog" %*
 IF %ERRORLEVEL% NEQ 0 (
    EXIT /b %ErrorLevel%
 )
@@ -11,7 +12,7 @@ ECHO Building LoggingBranch
 REM /p:DisableTransitiveFrameworkReferences=true is needed to prevent SDK from picking up transitive references to
 REM Microsoft.AspNetCore.App as framework references https://github.com/dotnet/sdk/pull/3221
 CALL "%RepoRoot%\build.cmd" -projects "%~dp0LoggingBranch\LB.csproj" ^
-    /p:DisableTransitiveFrameworkReferences=true %*
+    /p:DisableTransitiveFrameworkReferences=true "/bl:%RepoRoot%/artifacts/log/SiteExtensions-LoggingBranch-x64.binlog" %*
 IF %ERRORLEVEL% NEQ 0 (
    EXIT /b %ErrorLevel%
 )
