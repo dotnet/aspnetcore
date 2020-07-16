71c1ca2200edce0f67961bd5f5a25b59f652195c
Chris Sainty <chris.sainty@flagship-group.co.uk>
2020-07-16 20:22:38 +0100

    Updated null check in OnParametersSet

--- src/Components/Web/src/Forms/EditForm.cs
+++ src/Components/Web/src/Forms/EditForm.cs
@@ -88,7 +88,7 @@ namespace Microsoft.AspNetCore.Components.Forms
                 throw new InvalidOperationException($"{nameof(EditForm)} requires a {nameof(Model)} " +
                     $"parameter, or an {nameof(EditContext)} parameter, but not both.");
             }
-            else if (EditContext == null && Model == null)
+            else if (!_hasSetEditContextExplicitly && Model == null)
             {
                 throw new InvalidOperationException($"{nameof(EditForm)} requires either a {nameof(Model)} " +
                     $"parameter, or an {nameof(EditContext)} parameter, please provide one of these.");
