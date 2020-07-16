390eb4c2620bbbff681b926162b775331155feb1
zHaytam <haytam.zanid@gmail.com>
2020-07-16 19:03:42 +0100

    Add DisplayName

--- src/Components/Web/src/Forms/InputBase.cs
+++ src/Components/Web/src/Forms/InputBase.cs
@@ -51,6 +51,11 @@ namespace Microsoft.AspNetCore.Components.Forms
         [Parameter] public Expression<Func<TValue>>? ValueExpression { get; set; }
 
         /// <summary>
+        /// Gets or sets the display name for this field.
+        /// </summary>
+        [Parameter] public string? DisplayName { get; set; }
+
+        /// <summary>
         /// Gets the associated <see cref="Forms.EditContext"/>.
         /// </summary>
         protected EditContext EditContext { get; set; } = default!;
