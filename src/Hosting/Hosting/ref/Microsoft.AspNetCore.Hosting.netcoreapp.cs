cf77999a3b6bdb9d861ca7bf157b1264d67f3130
Kahbazi <A.Kahbazi@gmail.com>
2020-07-16 19:46:39 +0430

    Dispost CancellationTokenSource in WebHostExtensions (#23969)

--- src/Hosting/Hosting/ref/Microsoft.AspNetCore.Hosting.netcoreapp.cs
+++ src/Hosting/Hosting/ref/Microsoft.AspNetCore.Hosting.netcoreapp.cs
@@ -50,6 +50,7 @@ namespace Microsoft.AspNetCore.Hosting
         public static void Run(this Microsoft.AspNetCore.Hosting.IWebHost host) { }
         [System.Diagnostics.DebuggerStepThroughAttribute]
         public static System.Threading.Tasks.Task RunAsync(this Microsoft.AspNetCore.Hosting.IWebHost host, System.Threading.CancellationToken token = default(System.Threading.CancellationToken)) { throw null; }
+        [System.Diagnostics.DebuggerStepThroughAttribute]
         public static System.Threading.Tasks.Task StopAsync(this Microsoft.AspNetCore.Hosting.IWebHost host, System.TimeSpan timeout) { throw null; }
         public static void WaitForShutdown(this Microsoft.AspNetCore.Hosting.IWebHost host) { }
         [System.Diagnostics.DebuggerStepThroughAttribute]
