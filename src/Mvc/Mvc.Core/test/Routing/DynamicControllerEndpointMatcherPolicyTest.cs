f20a7a63ad8485c36ee7689323c1465549812f15
Javier Calvarro Nelson <jacalvar@microsoft.com>
2020-07-17 05:35:53 -0700

    Fix test

--- src/Mvc/Mvc.Core/test/Routing/DynamicControllerEndpointMatcherPolicyTest.cs
+++ src/Mvc/Mvc.Core/test/Routing/DynamicControllerEndpointMatcherPolicyTest.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -58,7 +58,7 @@ namespace Microsoft.AspNetCore.Mvc.Routing
                 _ => Task.CompletedTask,
                 new EndpointMetadataCollection(new object[]
                 {
-                    new DynamicControllerRouteValueTransformerMetadata(typeof(CustomTransformer)),
+                    new DynamicControllerRouteValueTransformerMetadata(typeof(CustomTransformer), null),
                 }),
                 "dynamic");
 
