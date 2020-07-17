9d4f47ee7e1b1366064ca73aede764ecbfc2db39
Zachary Becknell <zbecknell@users.noreply.github.com>
2020-07-17 09:20:29 -0400

    Add E2E tests for catch-all parameter

--- src/Components/test/E2ETest/Tests/RoutingTest.cs
+++ src/Components/test/E2ETest/Tests/RoutingTest.cs
@@ -110,6 +110,28 @@ namespace Microsoft.AspNetCore.Components.E2ETest.Tests
         }
 
         [Fact]
+        public void CanArriveAtPageWithCatchAllParameterWithEscapedSlashes()
+        {
+            SetUrlViaPushState("/WithCatchAllParameterWithEscapedSlashes/life/the/universe/and/everything = 42");
+
+            var app = Browser.MountTestComponent<TestRouter>();
+            var expected = $"The answer: life%2Fthe%2Funiverse%2Fand%2Feverything%20%3D%2042";
+
+            Assert.Equal(expected, app.FindElement(By.Id("test-info")).Text);
+        }
+
+        [Fact]
+        public void CanArriveAtPageWithCatchAllParameterWithoutEscapedSlashes()
+        {
+            SetUrlViaPushState("/WithCatchAllParameterWithoutEscapedSlashes/life/the/universe/and/everything = 42");
+
+            var app = Browser.MountTestComponent<TestRouter>();
+            var expected = $"The answer: life/the/universe/and/everything%20%3D%2042";
+
+            Assert.Equal(expected, app.FindElement(By.Id("test-info")).Text);
+        }
+
+        [Fact]
         public void CanArriveAtNonDefaultPage()
         {
             SetUrlViaPushState("/Other");
