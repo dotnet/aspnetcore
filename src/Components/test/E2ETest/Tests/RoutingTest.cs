a26b5b48bc0d33c8e5d22012f633ac8e741998cc
Zachary Becknell <zbecknell@users.noreply.github.com>
2020-07-17 13:35:35 -0400

    Fix typo causing test failure

--- src/Components/test/E2ETest/Tests/RoutingTest.cs
+++ src/Components/test/E2ETest/Tests/RoutingTest.cs
@@ -115,7 +115,7 @@ namespace Microsoft.AspNetCore.Components.E2ETest.Tests
             SetUrlViaPushState("/WithCatchAllParameter/life/the/universe/and/everything%20%3D%2042");
 
             var app = Browser.MountTestComponent<TestRouter>();
-            var expected = $"The answer: life/the/universe/and/everything = 42";
+            var expected = $"The answer: life/the/universe/and/everything = 42.";
 
             Assert.Equal(expected, app.FindElement(By.Id("test-info")).Text);
         }
