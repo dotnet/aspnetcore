eea4ec583041cdb187417739a6b46e6979f6f92c
SamProf <wsamprof@gmail.com>
2020-07-17 18:28:10 +0200

    Blazor - added directives `@public`, `@private`, `@internal`

---
+++ src/Razor/Microsoft.AspNetCore.Razor.Language/src/Extensions/AccessibilityModificatorDirectives.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Microsoft.AspNetCore.Razor.Language.Extensions
+{
+    internal static class AccessibilityModificatorDirectives
+    {
+        public static readonly DirectiveDescriptor InternalDirective = DirectiveDescriptor.CreateDirective(
+            "internal",
+            DirectiveKind.SingleLine,
+            builder =>
+            {
+                builder.Usage = DirectiveUsage.FileScopedMultipleOccurring;
+                builder.Description = "Add internal access modifier for component type";
+            });
+
+        public static readonly DirectiveDescriptor PublicDirective = DirectiveDescriptor.CreateDirective(
+            "public",
+            DirectiveKind.SingleLine,
+            builder =>
+            {
+                builder.Usage = DirectiveUsage.FileScopedMultipleOccurring;
+                builder.Description = "Add public access modifier for component type";
+            });
+
+        public static readonly DirectiveDescriptor PrivateDirective = DirectiveDescriptor.CreateDirective(
+            "private",
+            DirectiveKind.SingleLine,
+            builder =>
+            {
+                builder.Usage = DirectiveUsage.FileScopedMultipleOccurring;
+                builder.Description = "Add private access modifier for component type";
+            });
+
+        public static void Register(RazorProjectEngineBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.AddDirective(InternalDirective, FileKinds.Legacy, FileKinds.Component, FileKinds.ComponentImport);
+            builder.AddDirective(PrivateDirective, FileKinds.Legacy, FileKinds.Component, FileKinds.ComponentImport);
+            builder.AddDirective(PublicDirective, FileKinds.Legacy, FileKinds.Component, FileKinds.ComponentImport);
+            builder.Features.Add(new AccessibilityModificatorDirectivesPass());
+        }
+    }
+}
---
+++ src/Razor/Microsoft.AspNetCore.Razor.Language/src/Extensions/AccessibilityModificatorDirectivesPass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Razor.Language.Intermediate;
+
+namespace Microsoft.AspNetCore.Razor.Language.Extensions
+{
+    public class AccessibilityModificatorDirectivesPass: IntermediateNodePassBase, IRazorDirectiveClassifierPass
+    {
+        protected override void ExecuteCore(RazorCodeDocument codeDocument, DocumentIntermediateNode documentNode)
+        {
+            var @class = documentNode.FindPrimaryClass();
+            if (@class == null)
+            {
+                return;
+            }
+
+            string modificator = null;
+
+            // find accessibility modificator directives in documentNode
+            if (documentNode.FindDirectiveReferences(AccessibilityModificatorDirectives.PublicDirective).Count > 0)
+            {
+                modificator = "public";
+            }
+
+            if (documentNode.FindDirectiveReferences(AccessibilityModificatorDirectives.InternalDirective).Count > 0)
+            {
+                modificator = "internal";
+            }
+
+            if (documentNode.FindDirectiveReferences(AccessibilityModificatorDirectives.PrivateDirective).Count > 0)
+            {
+                modificator = "private";
+            }
+
+            // if of the accessibility modificator directives founded remove previous (usually public) and add new
+            if (modificator != null)
+            {
+                @class.Modifiers.Remove("public");
+                @class.Modifiers.Remove("private");
+                @class.Modifiers.Remove("internal");
+                @class.Modifiers.Insert(0, modificator);
+            }
+        }
+    }
+}
