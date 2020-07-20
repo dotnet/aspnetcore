15893a2cef3235bd87a5c3352b1896f931159aa9
Aristarkh Zagorodnikov <xm@drive.net>
2020-07-20 20:03:14 +0300

    Implement support for multiple LISTEN_FDS for systemd activation

--- src/Servers/Kestrel/Core/src/Systemd/KestrelServerOptionsSystemdExtensions.cs
+++ src/Servers/Kestrel/Core/src/Systemd/KestrelServerOptionsSystemdExtensions.cs
@@ -13,9 +13,10 @@ namespace Microsoft.AspNetCore.Hosting
         // SD_LISTEN_FDS_START https://www.freedesktop.org/software/systemd/man/sd_listen_fds.html
         private const ulong SdListenFdsStart = 3;
         private const string ListenPidEnvVar = "LISTEN_PID";
+        private const string ListenFdsEnvVar = "LISTEN_FDS";
 
         /// <summary>
-        /// Open file descriptor (SD_LISTEN_FDS_START) initialized by systemd socket-based activation logic if available.
+        /// Open file descriptors (starting from SD_LISTEN_FDS_START) initialized by systemd socket-based activation logic if available.
         /// </summary>
         /// <returns>
         /// The <see cref="KestrelServerOptions"/>.
@@ -26,7 +27,7 @@ namespace Microsoft.AspNetCore.Hosting
         }
 
         /// <summary>
-        /// Open file descriptor (SD_LISTEN_FDS_START) initialized by systemd socket-based activation logic if available.
+        /// Open file descriptors (starting from SD_LISTEN_FDS_START) initialized by systemd socket-based activation logic if available.
         /// Specify callback to configure endpoint-specific settings.
         /// </summary>
         /// <returns>
@@ -36,7 +37,17 @@ namespace Microsoft.AspNetCore.Hosting
         {
             if (string.Equals(Process.GetCurrentProcess().Id.ToString(CultureInfo.InvariantCulture), Environment.GetEnvironmentVariable(ListenPidEnvVar), StringComparison.Ordinal))
             {
-                options.ListenHandle(SdListenFdsStart, configure);
+                if (!byte.TryParse(Environment.GetEnvironmentVariable(ListenFdsEnvVar), NumberStyles.None, NumberFormatInfo.InvariantInfo, out var listenFds))
+                {
+                    listenFds = 1;
+                }
+
+                for (ulong handle = SdListenFdsStart, lastHandle = SdListenFdsStart + listenFds;
+                    handle <= lastHandle;
+                    ++handle)
+                {
+                    options.ListenHandle(handle, configure);
+                }
             }
 
             return options;
