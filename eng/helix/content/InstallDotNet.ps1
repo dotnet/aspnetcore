a086173a53ea8750fb31878c58a8e4d45d316c82
Hao Kung <HaoK@users.noreply.github.com>
2020-07-23 13:07:33 -0700

    Revert "Try using helix sdk support directly again (#23585)"
    This reverts commit 059fe39ae094a5d7076ad3583ef4bd70fa6aed0c.

--- eng/helix/content/InstallDotNet.ps1
+++ eng/helix/content/InstallDotNet.ps1
@@ -1,10 +1,12 @@
  <# 
  .SYNOPSIS 
-     Installs dotnet runtime using https://dot.net/v1/dotnet-install.ps1
+     Installs dotnet sdk and runtime using https://dot.net/v1/dotnet-install.ps1
  .DESCRIPTION
-     Installs dotnet runtime using https://dot.net/v1/dotnet-install.ps1
+     Installs dotnet sdk and runtime using https://dot.net/v1/dotnet-install.ps1
 .PARAMETER arch
     The architecture to install.
+.PARAMETER sdkVersion
+    The sdk version to install
 .PARAMETER runtimeVersion
     The runtime version to install
 .PARAMETER installDir
@@ -15,6 +17,9 @@ param(
     $arch,
 
     [Parameter(Mandatory = $true)]
+    $sdkVersion,
+    
+    [Parameter(Mandatory = $true)]
     $runtimeVersion,
     
     [Parameter(Mandatory = $true)]
@@ -23,6 +28,8 @@ param(
 
 & $PSScriptRoot\Download.ps1 "https://dot.net/v1/dotnet-install.ps1" $PSScriptRoot\dotnet-install.ps1
 Write-Host "Download of dotnet-install.ps1 complete..."
+Write-Host "Installing SDK...& $PSScriptRoot\dotnet-install.ps1 -Architecture $arch -Version $sdkVersion -InstallDir $installDir"
+Invoke-Expression "& $PSScriptRoot\dotnet-install.ps1 -Architecture $arch -Version $sdkVersion -InstallDir $installDir"
 Write-Host "Installing Runtime...& $PSScriptRoot\dotnet-install.ps1 -Architecture $arch -Runtime dotnet -Version $runtimeVersion -InstallDir $installDir"
 Invoke-Expression "& $PSScriptRoot\dotnet-install.ps1 -Architecture $arch -Runtime dotnet -Version $runtimeVersion -InstallDir $installDir"
 Write-Host "InstallDotNet.ps1 complete..." 
