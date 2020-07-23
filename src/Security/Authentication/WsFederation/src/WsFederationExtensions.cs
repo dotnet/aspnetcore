697b397b9b764b2b1aaa3488ecbed01183cdd5cd
Hao Kung <HaoK@users.noreply.github.com>
2020-07-23 15:00:01 -0700

    Revert AuthenticationAddXyz overload changes (#24253)

--- src/Security/Authentication/WsFederation/src/WsFederationExtensions.cs
+++ src/Security/Authentication/WsFederation/src/WsFederationExtensions.cs
@@ -32,16 +32,6 @@ namespace Microsoft.Extensions.DependencyInjection
             => builder.AddWsFederation(WsFederationDefaults.AuthenticationScheme, configureOptions);
 
         /// <summary>
-        /// Registers the <see cref="WsFederationHandler"/> using the default authentication scheme, display name, and the given options configuration.
-        /// </summary>
-        /// <typeparam name="TService">TService: A service resolved from the IServiceProvider for use when configuring this authentication provider. If you need multiple services then specify IServiceProvider and resolve them directly.</typeparam>
-        /// <param name="builder"></param>
-        /// <param name="configureOptions">A delegate that configures the <see cref="WsFederationOptions"/>.</param>
-        /// <returns></returns>
-        public static AuthenticationBuilder AddWsFederation<TService>(this AuthenticationBuilder builder, Action<WsFederationOptions, TService> configureOptions) where TService : class
-            => builder.AddWsFederation(WsFederationDefaults.AuthenticationScheme, configureOptions);
-
-        /// <summary>
         /// Registers the <see cref="WsFederationHandler"/> using the given authentication scheme, default display name, and the given options configuration.
         /// </summary>
         /// <param name="builder"></param>
@@ -49,17 +39,6 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <param name="configureOptions">A delegate that configures the <see cref="WsFederationOptions"/>.</param>
         /// <returns></returns>
         public static AuthenticationBuilder AddWsFederation(this AuthenticationBuilder builder, string authenticationScheme, Action<WsFederationOptions> configureOptions)
-            => builder.AddWsFederation(authenticationScheme, WsFederationDefaults.DisplayName, configureOptions);
-
-        /// <summary>
-        /// Registers the <see cref="WsFederationHandler"/> using the given authentication scheme, default display name, and the given options configuration.
-        /// </summary>
-        /// <typeparam name="TService">TService: A service resolved from the IServiceProvider for use when configuring this authentication provider. If you need multiple services then specify IServiceProvider and resolve them directly.</typeparam>
-        /// <param name="builder"></param>
-        /// <param name="authenticationScheme"></param>
-        /// <param name="configureOptions">A delegate that configures the <see cref="WsFederationOptions"/>.</param>
-        /// <returns></returns>
-        public static AuthenticationBuilder AddWsFederation<TService>(this AuthenticationBuilder builder, string authenticationScheme, Action<WsFederationOptions, TService> configureOptions) where TService : class
             => builder.AddWsFederation(authenticationScheme, WsFederationDefaults.DisplayName, configureOptions);
 
         /// <summary>
@@ -72,32 +51,8 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <returns></returns>
         public static AuthenticationBuilder AddWsFederation(this AuthenticationBuilder builder, string authenticationScheme, string displayName, Action<WsFederationOptions> configureOptions)
         {
-            Action<WsFederationOptions, IServiceProvider> configureOptionsWithServices;
-            if (configureOptions == null)
-            {
-                configureOptionsWithServices = null;
-            }
-            else
-            {
-                configureOptionsWithServices = (options, _) => configureOptions(options);
-            }
-
-            return builder.AddWsFederation(authenticationScheme, displayName, configureOptionsWithServices);
-        }
-
-        /// <summary>
-        /// Registers the <see cref="WsFederationHandler"/> using the given authentication scheme, display name, and options configuration.
-        /// </summary>
-        /// <typeparam name="TService">TService: A service resolved from the IServiceProvider for use when configuring this authentication provider. If you need multiple services then specify IServiceProvider and resolve them directly.</typeparam>
-        /// <param name="builder"></param>
-        /// <param name="authenticationScheme"></param>
-        /// <param name="displayName"></param>
-        /// <param name="configureOptions">A delegate that configures the <see cref="WsFederationOptions"/>.</param>
-        /// <returns></returns>
-        public static AuthenticationBuilder AddWsFederation<TService>(this AuthenticationBuilder builder, string authenticationScheme, string displayName, Action<WsFederationOptions, TService> configureOptions) where TService : class
-        {
             builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IPostConfigureOptions<WsFederationOptions>, WsFederationPostConfigureOptions>());
-            return builder.AddRemoteScheme<WsFederationOptions, WsFederationHandler, TService>(authenticationScheme, displayName, configureOptions);
+            return builder.AddRemoteScheme<WsFederationOptions, WsFederationHandler>(authenticationScheme, displayName, configureOptions);
         }
     }
 }
