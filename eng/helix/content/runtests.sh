05b72686f8c4fe5ac981919178dd20e94304556f
Hao Kung <HaoK@users.noreply.github.com>
2020-07-24 11:53:25 -0700

    Update runtests.sh

--- eng/helix/content/runtests.sh
+++ eng/helix/content/runtests.sh
@@ -70,14 +70,14 @@ fi
 # dontet-install.sh seems to affect the Linux filesystem and causes test flakiness unless we sync the filesystem before running tests
 sync
 
-$HELIX_CORRELATION_PAYLOAD/dotnet/dotnet --list-sdks
-$HELIX_CORRELATION_PAYLOAD/dotnet/dotnet --list-runtimes
+$HELIX_CORRELATION_PAYLOAD/dotnet --list-sdks
+$HELIX_CORRELATION_PAYLOAD/dotnet --list-runtimes
 
 exit_code=0
-echo "Restore: $HELIX_CORRELATION_PAYLOAD/dotnet/dotnet restore RunTests/RunTests.csproj --source https://api.nuget.org/v3/index.json --ignore-failed-sources..."
-$HELIX_CORRELATION_PAYLOAD/dotnet/dotnet restore RunTests/RunTests.csproj --source https://api.nuget.org/v3/index.json --ignore-failed-sources
-echo "Running tests: $HELIX_CORRELATION_PAYLOAD/dotnet/dotnet run --project RunTests/RunTests.csproj -- --target $1 --runtime $2 --queue $3 --arch $4 --quarantined $5 --ef $6 --aspnetruntime $7 --aspnetref $8 --helixTimeout $9..."
-$HELIX_CORRELATION_PAYLOAD/dotnet/dotnet run --project RunTests/RunTests.csproj -- --target $1 --runtime $2 --queue $3 --arch $4 --quarantined $5 --ef $6 --aspnetruntime $7 --aspnetref $8 --helixTimeout $9
+echo "Restore: $HELIX_CORRELATION_PAYLOAD/dotnet restore RunTests/RunTests.csproj --source https://api.nuget.org/v3/index.json --ignore-failed-sources..."
+$HELIX_CORRELATION_PAYLOAD/dotnet restore RunTests/RunTests.csproj --source https://api.nuget.org/v3/index.json --ignore-failed-sources
+echo "Running tests: $HELIX_CORRELATION_PAYLOAD/dotnet run --project RunTests/RunTests.csproj -- --target $1 --runtime $2 --queue $3 --arch $4 --quarantined $5 --ef $6 --aspnetruntime $7 --aspnetref $8 --helixTimeout $9..."
+$HELIX_CORRELATION_PAYLOAD/dotnet run --project RunTests/RunTests.csproj -- --target $1 --runtime $2 --queue $3 --arch $4 --quarantined $5 --ef $6 --aspnetruntime $7 --aspnetref $8 --helixTimeout $9
 exit_code=$?
 echo "Finished tests...exit_code=$exit_code"
 
