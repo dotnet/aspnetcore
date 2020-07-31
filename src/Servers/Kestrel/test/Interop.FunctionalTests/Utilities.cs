883e9b6b29a35c43f65a7bd0875b6160d64f5306
Hao Kung <HaoK@users.noreply.github.com>
2020-07-31 09:59:55 -0700

    Remove Debian 8 skip

--- src/Servers/Kestrel/test/Interop.FunctionalTests/Utilities.cs
+++ src/Servers/Kestrel/test/Interop.FunctionalTests/Utilities.cs
@@ -12,9 +12,7 @@ namespace Interop.FunctionalTests
             return // "Missing Windows ALPN support: https://en.wikipedia.org/wiki/Application-Layer_Protocol_Negotiation#Support" or missing compatible ciphers (Win8.1)
                 new MinimumOSVersionAttribute(OperatingSystems.Windows, WindowsVersions.Win10).IsMet
                 // "Missing SslStream ALPN support: https://github.com/dotnet/corefx/issues/30492"
-                && new OSSkipConditionAttribute(OperatingSystems.MacOSX).IsMet
-                // Debian 8 uses OpenSSL 1.0.1 which does not support ALPN
-                && new SkipOnHelixAttribute("https://github.com/dotnet/aspnetcore/issues/10428") { Queues = "Debian.8.Amd64;Debian.8.Amd64.Open" }.IsMet;
+                && new OSSkipConditionAttribute(OperatingSystems.MacOSX).IsMet;
         }
     }
 }
