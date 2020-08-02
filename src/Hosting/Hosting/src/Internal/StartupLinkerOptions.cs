c69d711065c445bf626c5867207e600c218b4c3a
David Fowler <davidfowl@gmail.com>
2020-08-02 15:04:43 -0700

    Fixed usings and copy right

--- src/Hosting/Hosting/src/Internal/StartupLinkerOptions.cs
+++ src/Hosting/Hosting/src/Internal/StartupLinkerOptions.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Collections.Generic;
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.Hosting.Internal
 {
