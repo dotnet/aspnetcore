4ef5e10e6d77d24fb251a8bf2f6d4b174cb31ed2
Mackinnon Buck <mackinnon.buck@gmail.com>
2020-08-03 17:02:12 -0700

    Virtualization support (#24179)

--- src/Components/Web.JS/src/GlobalExports.ts
+++ src/Components/Web.JS/src/GlobalExports.ts
@@ -1,6 +1,7 @@
 import { navigateTo, internalFunctions as navigationManagerInternalFunctions } from './Services/NavigationManager';
 import { attachRootComponentToElement } from './Rendering/Renderer';
 import { domFunctions } from './DomWrapper';
+import { Virtualize } from './Virtualize';
 
 // Make the following APIs available in global scope for invocation from JS
 window['Blazor'] = {
@@ -10,5 +11,6 @@ window['Blazor'] = {
     attachRootComponentToElement,
     navigationManager: navigationManagerInternalFunctions,
     domWrapper: domFunctions,
+    Virtualize,
   },
 };
