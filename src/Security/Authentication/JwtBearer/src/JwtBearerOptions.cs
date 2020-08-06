59fa552a7a8624b587648e9bf58619f624f68fd4
Hao Kung <HaoK@users.noreply.github.com>
2020-08-06 14:50:43 -0700

    Update JwtBearerOptions.cs

--- src/Security/Authentication/JwtBearer/src/JwtBearerOptions.cs
+++ src/Security/Authentication/JwtBearer/src/JwtBearerOptions.cs
@@ -16,6 +16,8 @@ namespace Microsoft.AspNetCore.Authentication.JwtBearer
     /// </summary>
     public class JwtBearerOptions : AuthenticationSchemeOptions
     {
+        private JwtSecurityTokenHandler _defaultHandler = new JwtSecurityTokenHandler();
+    
         /// <summary>
         /// Gets or sets if HTTPS is required for the metadata address or authority.
         /// The default is true. This should be disabled only in development environments.
@@ -90,7 +92,7 @@ namespace Microsoft.AspNetCore.Authentication.JwtBearer
         /// <summary>
         /// Gets the ordered list of <see cref="ISecurityTokenValidator"/> used to validate access tokens.
         /// </summary>
-        public IList<ISecurityTokenValidator> SecurityTokenValidators { get; } = new List<ISecurityTokenValidator> { new JwtSecurityTokenHandler() };
+        public IList<ISecurityTokenValidator> SecurityTokenValidators { get; } = new List<ISecurityTokenValidator> { _defaultHandler };
 
         /// <summary>
         /// Gets or sets the parameters used to validate identity tokens.
@@ -113,11 +115,16 @@ namespace Microsoft.AspNetCore.Authentication.JwtBearer
         public bool IncludeErrorDetails { get; set; } = true;
         
         /// <summary>
-        /// Gets or sets the <see cref="MapInboundClaims"/> property which is used when determining whether or not to map claim types that are extracted when validating a <see cref="System.IdentityModel.Tokens.Jwt.JwtSecurityToken"/>. 
+        /// Gets or sets the <see cref="MapInboundClaims"/> property on the default instance of <see cref="JwtSecurityTokenHandler"/> in SecurityTokenValidators, which is used when determining 
+        /// whether or not to map claim types that are extracted when validating a <see cref="JwtSecurityToken"/>. 
         /// <para>If this is set to true, the <see cref="Claim.Type"/> is set to the JSON claim 'name' after translating using this mapping. Otherwise, no mapping occurs.</para>
         /// <para>The default value is true.</para>
         /// </summary>
-        public bool MapInboundClaims { get; set; } = true;
+        public bool MapInboundClaims
+        {
+            get => _defaultHandler.MapInboundClaims;
+            set => _defaultHandler.MapInboundClaims = value;
+        }
 
         /// <summary>
         /// 1 day is the default time interval that afterwards, <see cref="ConfigurationManager" /> will obtain new configuration.
