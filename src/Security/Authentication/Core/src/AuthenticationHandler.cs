106f76cd87da2d90a3d2f6f309ab561a5391ef9e
David Fowler <davidfowl@gmail.com>
2020-08-10 10:24:26 -0700

    Removed using

--- src/Security/Authentication/Core/src/AuthenticationHandler.cs
+++ src/Security/Authentication/Core/src/AuthenticationHandler.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
