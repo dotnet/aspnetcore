d938dcd069b48b2ded5b9115be387ffc9a648631
BrennanConroy <brecon@microsoft.com>
2020-08-14 16:36:28 -0700

    Support more IAsyncEnumerable types in SignalR

--- src/SignalR/common/Shared/ReflectionHelper.cs
+++ src/SignalR/common/Shared/ReflectionHelper.cs
@@ -1,5 +1,5 @@
 // Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 #nullable disable
 
@@ -39,7 +39,10 @@ namespace Microsoft.AspNetCore.SignalR
         {
             if (type.IsGenericType)
             {
-                return type.GetGenericTypeDefinition() == typeof(IAsyncEnumerable<>);
+                if (type.GetGenericTypeDefinition() == typeof(IAsyncEnumerable<>))
+                {
+                    return true;
+                }
             }
 
             return type.GetInterfaces().Any(t =>
