02a83a569a0f1c8e83a8a7b6dff5cf3e618f4866
Will Godbe <wigodbe@microsoft.com>
2020-08-14 09:19:45 -0700

    Add TestUtils change that I didn't commit yesterday

--- src/SignalR/clients/java/signalr/src/test/java/com/microsoft/signalr/TestUtils.java
+++ src/SignalR/clients/java/signalr/src/test/java/com/microsoft/signalr/TestUtils.java
@@ -9,17 +9,26 @@ import java.nio.charset.StandardCharsets;
 class TestUtils {
 
     static HubConnection createHubConnection(String url) {
-        return createHubConnection(url, new MockTransport(true), true, new TestHttpClient());
+        return createHubConnection(url, new MockTransport(true), true, new TestHttpClient(), new JsonHubProtocol());
     }
 
     static HubConnection createHubConnection(String url, Transport transport) {
-        return createHubConnection(url, transport, true, new TestHttpClient());
+        return createHubConnection(url, transport, true, new TestHttpClient(), new JsonHubProtocol());
+    }
+    
+    static HubConnection createHubConnection(String url, HubProtocol protocol) {
+        return createHubConnection(url, new MockTransport(true), true, new TestHttpClient(), protocol);
+    }
+    
+    static HubConnection createHubConnection(String url, Transport transport, HubProtocol protocol) {
+        return createHubConnection(url, transport, true, new TestHttpClient(), protocol);
     }
 
-    static HubConnection createHubConnection(String url, Transport transport, boolean skipNegotiate, HttpClient client) {
+    static HubConnection createHubConnection(String url, Transport transport, boolean skipNegotiate, HttpClient client, HubProtocol protocol) {
         HttpHubConnectionBuilder builder = HubConnectionBuilder.create(url)
             .withTransportImplementation(transport)
             .withHttpClient(client)
+            .withProtocol(protocol)
             .shouldSkipNegotiate(skipNegotiate);
 
         return builder.build();
