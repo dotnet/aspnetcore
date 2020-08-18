cdb3cbd62037dcb492305c93501c4a6e76d8cb51
Justin Kotalik <jukotali@microsoft.com>
2020-08-18 09:34:43 -0700

    Remove \r\n for \n when writing to ostream (#24980)

--- src/Servers/IIS/IIS/test/Common.FunctionalTests/LogFileTests.cs
+++ src/Servers/IIS/IIS/test/Common.FunctionalTests/LogFileTests.cs
@@ -53,6 +53,8 @@ namespace Microsoft.AspNetCore.Server.IIS.FunctionalTests
             var contents = Helpers.ReadAllTextFromFile(Helpers.GetExpectedLogName(deploymentResult, _logFolderPath), Logger);
 
             Assert.Contains("TEST MESSAGE", contents);
+            Assert.DoesNotContain("\r\n\r\n", contents);
+            Assert.Contains("\r\n", contents);
         }
 
         // Move to separate file
