13db133d9bc8494be59149e0e9af64630a5329ba
Hao Kung <HaoK@users.noreply.github.com>
2020-08-21 12:58:25 -0700

    Create SkipOnHelixTest.cs

---
+++ src/Testing/test/SkipOnHelixTest.cs
@@ -0,0 +1,20 @@
+  
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNetCore.Testing;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Testing.Tests
+{
+    public class SkipOnHelixTests
+    {
+        [ConditionalFact]
+        [SkipOnHelix("This should be skipped", Queues = "Windows.10.Amd64.Open;Ubuntu.1604.Amd64.Open;OSX.1014.Amd64.Open")]
+        public void SkipOnHelix()
+        {
+            throw new Exception("Flaky!");
+        }
+    }
+}
