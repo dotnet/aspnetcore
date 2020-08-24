f185d50d4a4c5cb9b2368f18372c565396668855
Mackinnon Buck <mackinnon.buck@gmail.com>
2020-08-24 16:29:14 -0700

    Update InteropTest.cs

--- src/Components/test/E2ETest/Tests/InteropTest.cs
+++ src/Components/test/E2ETest/Tests/InteropTest.cs
@@ -133,7 +133,7 @@ namespace Microsoft.AspNetCore.Components.E2ETest.Tests
             var interopButton = Browser.FindElement(By.Id("btn-interop"));
             interopButton.Click();
 
-            var wait = new WebDriverWait(Browser, TimeSpan.FromSeconds(1000))
+            var wait = new WebDriverWait(Browser, TimeSpan.FromSeconds(10))
                 .Until(d => d.FindElement(By.Id("done-with-interop")));
 
             foreach (var expectedValue in expectedValues)
