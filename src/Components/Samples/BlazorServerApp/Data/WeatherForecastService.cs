022ecdbe52d0c1946cc62693f5dafa7ed42610b8
Mackinnon Buck <mackinnon.buck@gmail.com>
2020-08-25 16:55:55 -0700

    Update WeatherForecastService.cs

--- src/Components/Samples/BlazorServerApp/Data/WeatherForecastService.cs
+++ src/Components/Samples/BlazorServerApp/Data/WeatherForecastService.cs
@@ -14,7 +14,7 @@ namespace BlazorServerApp.Data
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
             var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 1000).Select(index => new WeatherForecast
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
