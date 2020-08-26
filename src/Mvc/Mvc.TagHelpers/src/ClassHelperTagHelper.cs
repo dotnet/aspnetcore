b51c666db85a3010d631e6cf69f4ac9bcd9331e6
John King <ABlueSky@outlook.com>
2020-08-26 21:12:15 +0800

    fix namespace

--- src/Mvc/Mvc.TagHelpers/src/ClassHelperTagHelper.cs
+++ src/Mvc/Mvc.TagHelpers/src/ClassHelperTagHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace Microsoft.AspNetCore.Mvc.TagHelpers
 {
--- src/Mvc/Mvc.TagHelpers/src/IfHelperTagHelper.cs
+++ src/Mvc/Mvc.TagHelpers/src/IfHelperTagHelper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace Microsoft.AspNetCore.Mvc.TagHelpers
 {
