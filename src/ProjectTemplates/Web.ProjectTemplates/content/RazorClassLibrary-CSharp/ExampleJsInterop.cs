3d68628e3d28b249934dced02430b05d4cbe78f5
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-09-04 12:18:04 +0100

    Update JS interop to use auto-loaded ES6 module

--- src/ProjectTemplates/Web.ProjectTemplates/content/RazorClassLibrary-CSharp/ExampleJsInterop.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/RazorClassLibrary-CSharp/ExampleJsInterop.cs
@@ -1,16 +1,36 @@
-using Microsoft.JSInterop;
+using System;
 using System.Threading.Tasks;
+using Microsoft.JSInterop;
 
 namespace Company.RazorClassLibrary1
 {
-    public class ExampleJsInterop
+    // This class provides an example of how JavaScript functionality can be wrapped
+    // in a .NET class for easy consumption. The associated JavaScript module is
+    // loaded on demand when the class is instantiated.
+    //
+    // This class can be registered as scoped DI service and then injected into Blazor
+    // components for use.
+
+    public class ExampleJsInterop : IAsyncDisposable
     {
-        public static ValueTask<string> Prompt(IJSRuntime jsRuntime, string message)
+        private readonly Task<IJSObjectReference> moduleTask;
+
+        public ExampleJsInterop(IJSRuntime jsRuntime)
         {
-            // Implemented in exampleJsInterop.js
-            return jsRuntime.InvokeAsync<string>(
-                "exampleJsFunctions.showPrompt",
-                message);
+            moduleTask = jsRuntime.InvokeAsync<IJSObjectReference>(
+                "import", "./_content/Company.RazorClassLibrary1/exampleJsInterop.js").AsTask();
+        }
+
+        public async ValueTask<string> Prompt(string message)
+        {
+            var module = await moduleTask;
+            return await module.InvokeAsync<string>("showPrompt", message);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            var module = await moduleTask;
+            await module.DisposeAsync();
         }
     }
 }
