40ca1eb84bff755696d0f7b506ac7ff6d6261b6d
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-09-04 16:11:29 +0100

    Update comment

--- src/ProjectTemplates/Web.ProjectTemplates/content/RazorClassLibrary-CSharp/ExampleJsInterop.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/RazorClassLibrary-CSharp/ExampleJsInterop.cs
@@ -6,7 +6,7 @@ namespace Company.RazorClassLibrary1
 {
     // This class provides an example of how JavaScript functionality can be wrapped
     // in a .NET class for easy consumption. The associated JavaScript module is
-    // loaded on demand when the class is instantiated.
+    // loaded on demand when first needed.
     //
     // This class can be registered as scoped DI service and then injected into Blazor
     // components for use.
