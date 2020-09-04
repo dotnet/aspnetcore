0dd4b3be86432097d3cb4bfebe35d880a06c6559
Mackinnon Buck <mackinnon.buck@gmail.com>
2020-09-04 11:41:14 -0700

    Update JSCallResultTypeHelper.cs and PublicAPI.Unshipped.txt

--- src/Shared/JSInterop/JSCallResultTypeHelper.cs
+++ src/Shared/JSInterop/JSCallResultTypeHelper.cs
@@ -1,15 +1,37 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Reflection;
+
 namespace Microsoft.JSInterop
 {
     internal static class JSCallResultTypeHelper
     {
+        // We avoid using Assembly.GetExecutingAssembly() because this is shared code.
+        private static readonly Assembly _currentAssembly = typeof(JSCallResultType).Assembly;
+
         public static JSCallResultType FromGeneric<TResult>()
-            => typeof(TResult) == typeof(IJSObjectReference)
-            || typeof(TResult) == typeof(IJSInProcessObjectReference)
-            || typeof(TResult) == typeof(IJSUnmarshalledObjectReference) ?
-                JSCallResultType.JSObjectReference :
-                JSCallResultType.Default;
+        {
+            var resultType = typeof(TResult);
+
+            if (resultType.Assembly == _currentAssembly)
+            {
+                if (resultType == typeof(IJSObjectReference)
+                    || resultType == typeof(IJSInProcessObjectReference)
+                    || resultType == typeof(IJSUnmarshalledObjectReference))
+                {
+                    return JSCallResultType.JSObjectReference;
+                }
+                else
+                {
+                    throw new ArgumentException($"JS interop cannot supply an instance of type '{resultType}'.");
+                }
+            }
+            else
+            {
+                return JSCallResultType.Default;
+            }
+        }
     }
 }
