f56f365eab4a32649085c251fc4ed25a59db5cfe
İlkay İlknur <ilkay@ilkayilknur.com>
2020-09-09 17:34:46 +0300

    Update src/Http/Headers/test/MediaTypeHeaderValueTest.cs
    test case fixes.
    
    Co-authored-by: Chris Ross <Tratcher@Outlook.com>

--- src/Http/Headers/test/MediaTypeHeaderValueTest.cs
+++ src/Http/Headers/test/MediaTypeHeaderValueTest.cs
@@ -744,6 +744,7 @@ namespace Microsoft.Net.Http.Headers
 
         [Theory]
         [InlineData("application/entity+json", "application/entity+txt")]
+        [InlineData("application/entity+json", "application/json")]
         [InlineData("application/entity+json", "application/entity.v2+json")]
         [InlineData("application/*+json", "application/entity+txt")]
         [InlineData("application/*+*", "application/json")]
