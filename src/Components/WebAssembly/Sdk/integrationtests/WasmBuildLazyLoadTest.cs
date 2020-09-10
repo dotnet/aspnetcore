df285031f7ab7a74e1df8a409ec0fab3d54eaef6
Juan Barahona <barahonajm@outlook.com>
2020-09-10 12:51:07 -0400

    removing uneeded imports

--- src/Components/WebAssembly/Sdk/integrationtests/WasmBuildLazyLoadTest.cs
+++ src/Components/WebAssembly/Sdk/integrationtests/WasmBuildLazyLoadTest.cs
@@ -1,8 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
