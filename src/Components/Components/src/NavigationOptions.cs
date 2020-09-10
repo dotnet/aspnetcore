127fd93f7d7a65eda536d5693b2dec32c24c4093
Mario van Zeist <m.vanzeist@home.nl>
2020-09-10 14:28:58 +0200

    Changes due to remarks

---
+++ src/Components/Components/src/NavigationOptions.cs
@@ -0,0 +1,22 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.AspNetCore.Components
+{
+    /// <summary>
+    /// Additional options for navigating to another URI
+    /// </summary>
+    public sealed class NavigationOptions
+    {
+        /// <summary>
+        ///If true, bypasses client-side routing and forces the browser to load the new page from the server, whether or not the URI would normally be handled by the client-side router.
+        /// </summary>
+        public bool ForceLoad { get; set; }
+        /// <summary>
+        /// If true, will replace the uri in the current browser history state, instead of pushing the new uri onto the browser history stack.
+        /// </summary>
+        public bool Replace { get; set; }
+    }
+}
