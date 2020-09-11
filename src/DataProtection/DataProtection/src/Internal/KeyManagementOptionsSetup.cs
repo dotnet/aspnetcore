d5b51e1ca8cb803f11c509f3549498da5b7816bd
Pranav K <prkrishn@hotmail.com>
2020-09-11 11:32:29 -0700

    Apply suggestions from code review

--- src/DataProtection/DataProtection/src/Internal/KeyManagementOptionsSetup.cs
+++ src/DataProtection/DataProtection/src/Internal/KeyManagementOptionsSetup.cs
@@ -1,8 +1,7 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
 using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
 using Microsoft.AspNetCore.DataProtection.KeyManagement;
