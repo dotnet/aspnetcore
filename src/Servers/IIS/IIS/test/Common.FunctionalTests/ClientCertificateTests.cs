d760657304a756fe740713477db7a1c7cdd8790f
Justin Kotalik <jukotali@microsoft.com>
2020-09-18 10:13:40 -0700

    Deflaking IIS tests (#26041)

--- src/Servers/IIS/IIS/test/Common.FunctionalTests/ClientCertificateTests.cs
+++ src/Servers/IIS/IIS/test/Common.FunctionalTests/ClientCertificateTests.cs
@@ -6,7 +6,6 @@ using System.Net.Http;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Server.IIS.FunctionalTests.Utilities;
-using Microsoft.AspNetCore.Server.IIS.FunctionalTests;
 using Microsoft.AspNetCore.Server.IntegrationTesting;
 using Microsoft.AspNetCore.Server.IntegrationTesting.Common;
 using Microsoft.AspNetCore.Server.IntegrationTesting.IIS;
