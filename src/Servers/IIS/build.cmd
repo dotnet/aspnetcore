91d0d32b0009df77b2941131e0e85b17f312a249
Justin Kotalik <jukotali@microsoft.com>
2020-09-22 15:04:05 -0700

    skip

--- src/Servers/IIS/build.cmd
+++ src/Servers/IIS/build.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
 SET RepoRoot=%~dp0..\..\..
-%RepoRoot%\build.cmd -projects %~dp0**\*.csproj %*
+%RepoRoot%\build.cmd -projects %~dp0**\*.csproj %* /p:SkipIISNewHandlerTests=true /p:SkipIISNewShimTests=true
