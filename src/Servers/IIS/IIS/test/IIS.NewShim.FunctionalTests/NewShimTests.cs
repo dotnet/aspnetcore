3ccc3eb7a01dc56036be146be662b39e6d49df5f
Justin Kotalik <jukotali@microsoft.com>
2020-09-22 21:45:25 -0700

    oops

--- src/Servers/IIS/IIS/test/IIS.NewShim.FunctionalTests/NewShimTests.cs
+++ src/Servers/IIS/IIS/test/IIS.NewShim.FunctionalTests/NewShimTests.cs
@@ -32,7 +32,7 @@ namespace Microsoft.AspNetCore.Server.IIS.FunctionalTests
                     return;
                 }
             }
-            throw new XunitException($"Could not find aspnetcorev2_inprocess.dll loaded in process {_fixture.DeploymentResult.HostProcess.ProcessName}");
+            throw new XunitException($"Could not find aspnetcorev2_inprocess.dll loaded in process {result.HostProcess.ProcessName}");
         }
     }
 }
