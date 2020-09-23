de1bf0abe47f51427a0693db32f206609d407910
Pranav K <prkrishn@hotmail.com>
2020-09-23 09:00:38 -0700

    Detect culture change in .NET (#26192)
    * Detect culture change in .NET
    
    With ICU sharding enabled, blazor wasm attempts to detect if the application culture was changed by the
    application code as part of Program.MainAsync and tell them they need to opt out of sharding.
    
    Prior to this change, Blazor compared the .NET culture string with a JS representation for language. With iOS 14,
    the two culture strings differ in casing which prevents the use of any Blazor WASM app the latest version installed.
    
    As part of this change, the comparison is performed entirely in .NET which avoids relying on the JS representation.
    
    * Fixups

--- src/Components/WebAssembly/WebAssembly/test/Hosting/WebAssemblyCultureProviderTest.cs
+++ src/Components/WebAssembly/WebAssembly/test/Hosting/WebAssemblyCultureProviderTest.cs
@@ -1,18 +1,20 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Globalization;
 using System.IO;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Services;
 using Microsoft.AspNetCore.Testing;
 using Moq;
 using Xunit;
-using static Microsoft.AspNetCore.Components.WebAssembly.Hosting.SatelliteResourcesLoader;
+using static Microsoft.AspNetCore.Components.WebAssembly.Hosting.WebAssemblyCultureProvider;
 
 namespace Microsoft.AspNetCore.Components.WebAssembly.Hosting
 {
-    public class SatelliteResourcesLoaderTest
+    public class WebAssemblyCultureProviderTest
     {
         [Theory]
         [InlineData("fr-FR", new[] { "fr-FR", "fr" })]
@@ -23,7 +25,7 @@ namespace Microsoft.AspNetCore.Components.WebAssembly.Hosting
             var culture = new CultureInfo(cultureName);
 
             // Act
-            var actual = SatelliteResourcesLoader.GetCultures(culture);
+            var actual = WebAssemblyCultureProvider.GetCultures(culture);
 
             // Assert
             Assert.Equal(expected, actual);
@@ -43,7 +45,7 @@ namespace Microsoft.AspNetCore.Components.WebAssembly.Hosting
                 .Returns(new object[] { File.ReadAllBytes(GetType().Assembly.Location) })
                 .Verifiable();
 
-            var loader = new SatelliteResourcesLoader(invoker.Object);
+            var loader = new WebAssemblyCultureProvider(invoker.Object, CultureInfo.CurrentCulture, CultureInfo.CurrentUICulture);
 
             // Act
             await loader.LoadCurrentCultureResourcesAsync();
@@ -62,13 +64,37 @@ namespace Microsoft.AspNetCore.Components.WebAssembly.Hosting
                 .Returns(Task.FromResult<object>(0))
                 .Verifiable();
 
-            var loader = new SatelliteResourcesLoader(invoker.Object);
+            var loader = new WebAssemblyCultureProvider(invoker.Object, CultureInfo.CurrentCulture, CultureInfo.CurrentUICulture);
 
             // Act
             await loader.LoadCurrentCultureResourcesAsync();
 
             // Assert
             invoker.Verify(i => i.InvokeUnmarshalled<object, object, object, object[]>(ReadSatelliteAssemblies, null, null, null), Times.Never());
+        }
+
+        [Fact]
+        public void ThrowIfCultureChangeIsUnsupported_ThrowsIfCulturesAreDifferentAndICUShardingIsUsed()
+        {
+            // Arrange
+            Environment.SetEnvironmentVariable("__BLAZOR_SHARDED_ICU", "1");
+            try
+            {
+                // WebAssembly is initialized with en-US
+                var cultureProvider = new WebAssemblyCultureProvider(WebAssemblyJSRuntimeInvoker.Instance, new CultureInfo("en-US"), new CultureInfo("en-US"));
+
+                // Culture is changed to fr-FR as part of the app
+                using var cultureReplacer = new CultureReplacer("fr-FR");
+
+                var ex = Assert.Throws<InvalidOperationException>(() => cultureProvider.ThrowIfCultureChangeIsUnsupported());
+                Assert.Equal("Blazor detected a change in the application's culture that is not supported with the current project configuration. " +
+                    "To change culture dynamically during startup, set <BlazorWebAssemblyLoadAllGlobalizationData>true</BlazorWebAssemblyLoadAllGlobalizationData> in the application's project file.",
+                    ex.Message);
+            }
+            finally
+            {
+                Environment.SetEnvironmentVariable("__BLAZOR_SHARDED_ICU", null);
+            }
         }
     }
 }
