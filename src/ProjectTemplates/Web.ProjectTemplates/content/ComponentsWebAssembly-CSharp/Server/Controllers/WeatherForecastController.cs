6b99c4c97a0d3127f959cc6fb2d9de315b3fbbcf
Jean-Marc Prieur <jmprieur@microsoft.com>
2020-10-02 19:26:35 +0200

    Improve the blazorwasm template

--- src/ProjectTemplates/Web.ProjectTemplates/content/ComponentsWebAssembly-CSharp/Server/Controllers/WeatherForecastController.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/ComponentsWebAssembly-CSharp/Server/Controllers/WeatherForecastController.cs
@@ -5,15 +5,6 @@ using System.Threading.Tasks;
 #if (!NoAuth)
 using Microsoft.AspNetCore.Authorization;
 #endif
-#if (GenerateApi)
-using Microsoft.Extensions.Configuration;
-using Microsoft.Identity.Web;
-using System.Net;
-using System.Net.Http;
-#endif
-#if (GenerateGraph)
-using Microsoft.Graph;
-#endif
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 #if (OrganizationalAuth || IndividualB2CAuth)
@@ -42,69 +33,6 @@ namespace ComponentsWebAssembly_CSharp.Server.Controllers
         static readonly string[] scopeRequiredByApi = new string[] { "api-scope" };
 #endif
 
-#if (GenerateApi)
-        private readonly IDownstreamWebApi _downstreamWebApi;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger,
-                              IDownstreamWebApi downstreamWebApi)
-        {
-             _logger = logger;
-            _downstreamWebApi = downstreamWebApi;
-        }
-
-        [HttpGet]
-        public async Task<IEnumerable<WeatherForecast>> Get()
-        {
-            HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
-
-            using var response = await _downstreamWebApi.CallWebApiForUserAsync("DownstreamApi").ConfigureAwait(false);
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
-            {
-                var apiResult = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                // Do something
-            }
-            else
-            {
-                var error = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                throw new HttpRequestException($"Invalid status code in the HttpResponseMessage: {response.StatusCode}: {error}");
-            }
-
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-
-#elseif (GenerateGraph)
-        private readonly GraphServiceClient _graphServiceClient;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger,
-                                         GraphServiceClient graphServiceClient)
-        {
-             _logger = logger;
-            _graphServiceClient = graphServiceClient;
-       }
-
-        [HttpGet]
-        public async Task<IEnumerable<WeatherForecast>> Get()
-        {
-            HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
-            var user = await _graphServiceClient.Me.Request().GetAsync();
-
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-#else
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
@@ -126,6 +54,5 @@ namespace ComponentsWebAssembly_CSharp.Server.Controllers
             })
             .ToArray();
         }
-#endif
     }
 }
