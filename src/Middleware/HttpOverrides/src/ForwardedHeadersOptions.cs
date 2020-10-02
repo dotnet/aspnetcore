0a2ca88f4837cbe34655d145d404587cfe70ea30
Pranav K <prkrishn@hotmail.com>
2020-10-02 09:49:20 -0700

    Update ForwardedHeadersOptions.cs

--- src/Middleware/HttpOverrides/src/ForwardedHeadersOptions.cs
+++ src/Middleware/HttpOverrides/src/ForwardedHeadersOptions.cs
@@ -13,21 +13,21 @@ namespace Microsoft.AspNetCore.Builder
     public class ForwardedHeadersOptions
     {
         /// <summary>
-        /// Use this header instead of <see cref="ForwardedHeadersDefaults.XForwardedForHeaderName"/>
+        /// Gets or sets the header used to retrieve the originating client IP. Defaults to the value specified by 
+        /// <see cref="ForwardedHeadersDefaults.XForwardedForHeaderName"/>.
         /// </summary>
-        /// <seealso cref="Microsoft.AspNetCore.HttpOverrides.ForwardedHeadersDefaults"/>
         public string ForwardedForHeaderName { get; set; } = ForwardedHeadersDefaults.XForwardedForHeaderName;
 
         /// <summary>
-        /// Use this header instead of <see cref="ForwardedHeadersDefaults.XForwardedHostHeaderName"/>
+        /// Gets or sets the header used to retrieve the original value of the Host header field.
+        /// Defaults to the value specified by <see cref="ForwardedHeadersDefaults.XForwardedHostHeaderName"/>
         /// </summary>
-        /// <seealso cref="ForwardedHeadersDefaults"/>
         public string ForwardedHostHeaderName { get; set; } = ForwardedHeadersDefaults.XForwardedHostHeaderName;
 
         /// <summary>
-        /// Use this header instead of <see cref="ForwardedHeadersDefaults.XForwardedProtoHeaderName"/>
+        /// Gets or sets the header used to retrieve the value for the originating scheme (HTTP/HTTPS).
+        /// Defaults to the value specified by <see cref="ForwardedHeadersDefaults.XForwardedProtoHeaderName"/>
         /// </summary>
-        /// <seealso cref="ForwardedHeadersDefaults"/>
         public string ForwardedProtoHeaderName { get; set; } = ForwardedHeadersDefaults.XForwardedProtoHeaderName;
 
         /// <summary>
