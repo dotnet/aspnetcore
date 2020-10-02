d2e50c3374ee2657927c932b35dfe907ad294177
Jean-Marc Prieur <jmprieur@microsoft.com>
2020-10-02 19:05:32 +0200

    Improving the blazorwasm template

---
+++ src/ProjectTemplates/Web.ProjectTemplates/content/ComponentsWebAssembly-CSharp/Server/Controllers/CallWebApiController.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.Resource;
+
+namespace AspNetCoreMicrosoftIdentityWebProjectTemplates.templates.ComponentsWebAssembly_CSharp.Server.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class CallWebApiController : Controller
+    {
+        // The web API will only accept tokens 1) for users, and 2) having the "access_as_user" scope for this API
+        static readonly string[] scopeRequiredByApi = new string[] { "access_as_user" };
+
+        private readonly IDownstreamWebApi _downstreamWebApi;
+        private readonly ILogger<CallWebApiController> _logger;
+
+        public CallWebApiController(ILogger<CallWebApiController> logger,
+                              IDownstreamWebApi downstreamWebApi)
+        {
+            _logger = logger;
+            _downstreamWebApi = downstreamWebApi;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
+
+            using var response = await _downstreamWebApi.CallWebApiForUserAsync("DownstreamApi").ConfigureAwait(false);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                // Do something
+            }
+            else
+            {
+                var error = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                throw new HttpRequestException($"Invalid status code in the HttpResponseMessage: {response.StatusCode}: {error}");
+            }
+        }
+    }
+}
---
+++ src/ProjectTemplates/Web.ProjectTemplates/content/ComponentsWebAssembly-CSharp/Server/Controllers/ShowProfileController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+using Microsoft.Identity.Web.Resource;
+
+namespace AspNetCoreMicrosoftIdentityWebProjectTemplates.templates.ComponentsWebAssembly_CSharp.Server.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class ShowProfileController : Controller
+    {
+        // The web API will only accept tokens 1) for users, and 2) having the "access_as_user" scope for this API
+        static readonly string[] scopeRequiredByApi = new string[] { "access_as_user" };
+
+        private readonly GraphServiceClient _graphServiceClient;
+
+        public ShowProfileController(GraphServiceClient graphServiceClient)
+        {
+            _graphServiceClient = graphServiceClient;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
+            var user = await _graphServiceClient.Me.Request().GetAsync();
+
+            return user.DisplayName;
+        }
+    }
+}
