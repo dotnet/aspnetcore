8118a25e7fcb94d5e98577445fae9b4ff8e83bb5
William Godbe <wigodbe@microsoft.com>
2020-10-07 22:28:49 -0700

    Add API ref docs for HTTPSys (#26649)
    * Add API ref docs for HTTPSys
    
    * Spacing
    
    * Feedback

--- src/Servers/HttpSys/src/IServerDelegationFeature.cs
+++ src/Servers/HttpSys/src/IServerDelegationFeature.cs
@@ -3,11 +3,16 @@
 
 namespace Microsoft.AspNetCore.Server.HttpSys
 {
+    /// <summary>
+    /// This exposes the creation of delegation rules on request queues owned by the server.
+    /// </summary>
     public interface IServerDelegationFeature
     {
         /// <summary>
         /// Create a delegation rule on request queue owned by the server.
         /// </summary>
+        /// <param name="queueName">The name of the Http.Sys request queue.</param>
+        /// <param name="urlPrefix">The URL of the Http.Sys Url Prefix.</param>
         /// <returns>
         /// Creates a <see cref="DelegationRule"/> that can used to delegate individual requests.
         /// </returns>
