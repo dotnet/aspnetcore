9ff5396363ffaec0a3060cb30a4b58fc6687be60
Pranav K <prkrishn@hotmail.com>
2020-10-12 13:54:32 -0700

    Add docs for remaining security items
    * Google / Facebook / Microsoft Account auth
    * Authorization, Authorization.Policy
    
    Contributes to https://github.com/dotnet/aspnetcore/issues/26397

--- src/Security/Authentication/Facebook/src/FacebookDefaults.cs
+++ src/Security/Authentication/Facebook/src/FacebookDefaults.cs
@@ -3,17 +3,37 @@
 
 namespace Microsoft.AspNetCore.Authentication.Facebook
 {
+    /// <summary>
+    /// Default values related to Facebook-based authentication handler.
+    /// </summary>
     public static class FacebookDefaults
     {
+        /// <summary>
+        /// The default scheme for Facebook authentication. Defaults to <c>Facebook</c>.
+        /// </summary>
         public const string AuthenticationScheme = "Facebook";
 
+        /// <summary>
+        /// The default display name for Facebook authentication. Defaults to <c>Facebook</c>.
+        /// </summary>
         public static readonly string DisplayName = "Facebook";
 
-        // https://developers.facebook.com/docs/facebook-login/manually-build-a-login-flow#login
+        /// <summary>
+        /// The default endpoint used to perform Facebook authentication.
+        /// </summary>
+        /// <remarks>
+        /// For more details about this endpoint, see https://developers.facebook.com/docs/facebook-login/manually-build-a-login-flow#login.
+        /// </remarks>
         public static readonly string AuthorizationEndpoint = "https://www.facebook.com/v8.0/dialog/oauth";
 
+        /// <summary>
+        /// The OAuth endpoint used to exchange access tokens.
+        /// </summary>
         public static readonly string TokenEndpoint = "https://graph.facebook.com/v8.0/oauth/access_token";
 
+        /// <summary>
+        /// The Facebook Graph API endpoint that is used to gather additional user information.
+        /// </summary>
         public static readonly string UserInformationEndpoint = "https://graph.facebook.com/v8.0/me";
     }
 }
--- src/Security/Authentication/MicrosoftAccount/src/MicrosoftChallengeProperties.cs
+++ src/Security/Authentication/MicrosoftAccount/src/MicrosoftChallengeProperties.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.OAuth;
 namespace Microsoft.AspNetCore.Authentication.MicrosoftAccount
 {    
     /// <summary>
+    /// <see cref="AuthenticationProperties"/> for Microsoft OAuth challenge request.
     /// See https://docs.microsoft.com/azure/active-directory/develop/v2-oauth2-auth-code-flow#request-an-authorization-code for reference
     /// </summary>
     public class MicrosoftChallengeProperties : OAuthChallengeProperties
@@ -28,19 +29,31 @@ namespace Microsoft.AspNetCore.Authentication.MicrosoftAccount
         /// </summary>
         public static readonly string PromptKey = "prompt";
 
+        /// <summary>
+        /// Initializes a new instance for <see cref="MicrosoftChallengeProperties"/>.
+        /// </summary>
         public MicrosoftChallengeProperties()
         { }
 
+        /// <summary>
+        /// Initializes a new instance for <see cref="MicrosoftChallengeProperties"/>.
+        /// </summary>
+        /// <inheritdoc />
         public MicrosoftChallengeProperties(IDictionary<string, string> items)
             : base(items)
         { }
 
+        /// <summary>
+        /// Initializes a new instance for <see cref="MicrosoftChallengeProperties"/>.
+        /// </summary>
+        /// <inheritdoc />
         public MicrosoftChallengeProperties(IDictionary<string, string> items, IDictionary<string, object> parameters)
             : base(items, parameters)
         { }
 
         /// <summary>
-        /// The "response_mode" parameter value being used for a challenge request.
+        /// Gets or sets the value for the <c>response_mode</c> parameter used for a challenge request. The response mode specifies the method
+        /// that should be used to send the resulting token back to the app. Can be one of the following: <c>query</c>, <c>fragment</c>, <c>form_post</c>.
         /// </summary>
         public string ResponseMode
         {
@@ -49,7 +62,11 @@ namespace Microsoft.AspNetCore.Authentication.MicrosoftAccount
         }
 
         /// <summary>
-        /// The "domain_hint" parameter value being used for a challenge request.
+        /// Gets or sets the value for the "domain_hint" parameter value being used for a challenge request.
+        /// <para>
+        /// If included, authentication will skip the email-based discovery process that user goes through on the sign-in page,
+        /// leading to a slightly more streamlined user experience 
+        /// </para>
         /// </summary>
         public string DomainHint
         {
@@ -58,7 +75,10 @@ namespace Microsoft.AspNetCore.Authentication.MicrosoftAccount
         }
 
         /// <summary>
-        /// The "login_hint" parameter value being used for a challenge request.
+        /// Gets or sets the value for the "login_hint" parameter value being used for a challenge request.
+        /// <para>
+        /// Can be used to pre-fill the username/email address field of the sign-in page for the user, if their username is known ahead of time.
+        /// </para>
         /// </summary>
         public string LoginHint
         {
@@ -67,7 +87,10 @@ namespace Microsoft.AspNetCore.Authentication.MicrosoftAccount
         }
 
         /// <summary>
-        /// The "prompt" parameter value being used for a challenge request.
+        /// Gets or sets the value for the "prompt" parameter value being used for a challenge request.
+        /// <para>
+        /// Indicates the type of user interaction that is required. The only valid values at this time are login, none, and consent.
+        /// </para>
         /// </summary>
         public string Prompt
         {
