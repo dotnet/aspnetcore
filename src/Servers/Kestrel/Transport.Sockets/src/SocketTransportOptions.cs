2abd355c773e0188092355ecf172caf38df6db8a
Javier Calvarro Nelson <jacalvar@microsoft.com>
2020-10-20 05:53:30 -0700

    [Kestrel] Add missing XML docs to public APIs

--- src/Servers/Kestrel/Transport.Sockets/src/SocketTransportOptions.cs
+++ src/Servers/Kestrel/Transport.Sockets/src/SocketTransportOptions.cs
@@ -6,6 +6,9 @@ using System.Buffers;
 
 namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets
 {
+    /// <summary>
+    /// Options for socket based transports.
+    /// </summary>
     public class SocketTransportOptions
     {
         /// <summary>
@@ -40,8 +43,14 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets
         /// </remarks>
         public int Backlog { get; set; } = 512;
 
+        /// <summary>
+        /// Gets or sets the maximum read buffer size.
+        /// </summary>
         public long? MaxReadBufferSize { get; set; } = 1024 * 1024;
 
+        /// <summary>
+        /// Gets or sets the maximum write buffer size.
+        /// </summary>
         public long? MaxWriteBufferSize { get; set; } = 64 * 1024;
 
         /// <summary>
