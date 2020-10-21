83acc300f32f00a20098639a13dbc870828f3721
Pranav K <prkrishn@hotmail.com>
2020-10-21 08:37:14 -0700

    Allow dumps to be captured for SDK integration tests
    SDK tests are currently not running on Helix. There is ongoing work to allow these to be run on Helix.
    In the meanwhile, we have a bunch of tests that are quarantined, succeeding for 30 days, but cannot be turned
    on because it's unclear why they hang.
    
    This PR changes the tests to use `dotnet test` for their runs and uses the cool new features to enforce test hangs
    and capture dumps.

--- src/Razor/Microsoft.NET.Sdk.Razor/integrationtests/MvcBuildIntegrationTestLegacy.cs
+++ src/Razor/Microsoft.NET.Sdk.Razor/integrationtests/MvcBuildIntegrationTestLegacy.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -31,7 +31,6 @@ namespace Microsoft.AspNetCore.Razor.Design.IntegrationTests
         }
 
         [Fact]
-        [QuarantinedTest]
         public virtual async Task Building_Project()
         {
             using (CreateTestProject())
