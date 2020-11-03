227195220922d18083b7df702010ddb46221cc99
Benjamin Day <benday@benday.com>
2020-11-03 14:01:47 -0500

    Added typescript fix for problem with specs created by "dotnet new angular --auth" (#18295)

--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/Angular-CSharp/ClientApp/src/api-authorization/login-menu/login-menu.component.spec.ts
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/Angular-CSharp/ClientApp/src/api-authorization/login-menu/login-menu.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { LoginMenuComponent } from './login-menu.component';
+import { AuthorizeService } from '../authorize.service';
+import { of } from 'rxjs';
 
 describe('LoginMenuComponent', () => {
   let component: LoginMenuComponent;
@@ -8,12 +10,20 @@ describe('LoginMenuComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule], 
       declarations: [ LoginMenuComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    let authService = TestBed.get(AuthorizeService);
+
+    spyOn(authService, 'ensureUserManagerInitialized').and.returnValue(
+      Promise.resolve());
+    spyOn(authService, 'getUserFromStorage').and.returnValue(
+      of(null));
+
     fixture = TestBed.createComponent(LoginMenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
