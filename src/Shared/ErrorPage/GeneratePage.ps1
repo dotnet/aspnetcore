58ffd238ed4b1f78e6484238d214784754c9b4e7
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2020-11-06 15:39:25 -0800

    Add option to `RazorPageGenerator` for `#line` preprocessor directives - avoid new CS1504 errors because compiler can't find the ErrorPage.cshtml file   - will soon override Arcade's `$(MicrosoftCodeAnalysisCSharpVersion)` - use new option in GeneratePage.ps1 script   - script also did not know `RazorSyntaxGenerator` had moved to this repo - reflect latest `RazorSyntaxGenerator` processing   - run GeneratePage.ps1   - update README.md
    nits:
    - quote all paths in GeneratePage.ps1 script
    - remove passive voice in README.md

--- src/Shared/ErrorPage/GeneratePage.ps1
+++ src/Shared/ErrorPage/GeneratePage.ps1
@@ -1,18 +1,15 @@
-param(
-    [Parameter(Mandatory = $true)][string]$ToolingRepoPath
-)
+$RepoRoot = Resolve-Path "$PSScriptRoot\..\..\.."
+$ToolPath = Join-Path "$RepoRoot" "artifacts\bin\RazorPageGenerator\Debug\net6.0\dotnet-razorpagegenerator.exe"
 
-$ToolPath = Join-Path $ToolingRepoPath "artifacts\bin\RazorPageGenerator\Debug\net6.0\dotnet-razorpagegenerator.exe"
-
-if (!(Test-Path $ToolPath)) {
+if (!(Test-Path "$ToolPath")) {
     throw "Unable to find razor page generator tool at $ToolPath"
 }
 
-& "$ToolPath" Microsoft.AspNetCore.Hosting.Views $PSScriptRoot
+& "$ToolPath" Microsoft.AspNetCore.Hosting.Views "$PSScriptRoot" Views/
 
-$TargetPath = Join-Path $PSScriptRoot "ErrorPage.Designer.cs"
-if (Test-Path $TargetPath) {
-    Remove-Item $TargetPath
+$TargetPath = Join-Path "$PSScriptRoot" "ErrorPage.Designer.cs"
+if (Test-Path "$TargetPath") {
+    Remove-Item "$TargetPath"
 }
 
-Move-Item "$PSScriptRoot\Views\ErrorPage.Designer.cs" $PSScriptRoot
+Move-Item "$PSScriptRoot\Views\ErrorPage.Designer.cs" "$PSScriptRoot"
