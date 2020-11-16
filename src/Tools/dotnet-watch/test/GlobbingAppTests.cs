2ec4ea19edda26c9a6dede404a76ceb91a39c55c
John Luo <johluo@microsoft.com>
2020-11-16 12:12:41 -0800

    Quarantine flaky tests (#27859)

--- src/Tools/dotnet-watch/test/GlobbingAppTests.cs
+++ src/Tools/dotnet-watch/test/GlobbingAppTests.cs
@@ -26,6 +26,7 @@ namespace Microsoft.DotNet.Watcher.Tools.FunctionalTests
         }
 
         [ConditionalTheory]
+        [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/27856")]
         [InlineData(true)]
         [InlineData(false)]
         [SkipOnHelix("https://github.com/dotnet/aspnetcore/issues/23360", Queues = "Debian.9.Arm64;Debian.9.Arm64.Open;(Debian.9.Arm64.Open)Ubuntu.1804.Armarch.Open@mcr.microsoft.com/dotnet-buildtools/prereqs:debian-9-helix-arm64v8-a12566d-20190807161036;(Debian.9.Arm64)Ubuntu.1804.Armarch@mcr.microsoft.com/dotnet-buildtools/prereqs:debian-9-helix-arm64v8-a12566d-20190807161036;(Fedora.28.Amd64.Open)Ubuntu.1604.Amd64.Open@mcr.microsoft.com/dotnet-buildtools/prereqs:fedora-28-helix-09ca40b-20190508143249")]
