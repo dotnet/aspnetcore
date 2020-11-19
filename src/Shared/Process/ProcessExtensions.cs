1b106da334d27d449d4374dcc27ad41f159dc5b1
Justin Kotalik <jukotali@microsoft.com>
2020-11-19 12:39:15 -0800

    missed file

--- src/Shared/Process/ProcessExtensions.cs
+++ src/Shared/Process/ProcessExtensions.cs
@@ -30,67 +30,7 @@ namespace Microsoft.Extensions.Internal
             }
             else
             {
-                var children = new HashSet<int>();
-                GetAllChildIdsUnix(pid, children, timeout);
-                foreach (var childId in children)
-                {
-                    KillProcessUnix(childId, timeout);
-                }
-                KillProcessUnix(pid, timeout);
-            }
-        }
-
-        private static void GetAllChildIdsUnix(int parentId, ISet<int> children, TimeSpan timeout)
-        {
-            try
-            {
-                RunProcessAndWaitForExit(
-                    "pgrep",
-                    $"-P {parentId}",
-                    timeout,
-                    out var stdout);
-
-                if (!string.IsNullOrEmpty(stdout))
-                {
-                    using (var reader = new StringReader(stdout))
-                    {
-                        while (true)
-                        {
-                            var text = reader.ReadLine();
-                            if (text == null)
-                            {
-                                return;
-                            }
-
-                            if (int.TryParse(text, out var id))
-                            {
-                                children.Add(id);
-                                // Recursively get the children
-                                GetAllChildIdsUnix(id, children, timeout);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Win32Exception ex) when (ex.Message.Contains("No such file or directory"))
-            {
-                // This probably means that pgrep isn't installed. Nothing to be done?
-            }
-        }
-
-        private static void KillProcessUnix(int processId, TimeSpan timeout)
-        {
-            try
-            {
-                RunProcessAndWaitForExit(
-                    "kill",
-                    $"-TERM {processId}",
-                    timeout,
-                    out var stdout);
-            }
-            catch (Win32Exception ex) when (ex.Message.Contains("No such file or directory"))
-            {
-                // This probably means that the process is already dead
+                process.Kill(entireProcessTree: true);
             }
         }
 
