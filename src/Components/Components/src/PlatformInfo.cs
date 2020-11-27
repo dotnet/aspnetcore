802238fe2e3b9d8b50946632da1c7050cd066f7f
Stefan Nikolei <stefan@nikolei.eu>
2020-11-27 22:22:54 +0100

    Fix PlatformInfo

--- src/Components/Components/src/PlatformInfo.cs
+++ src/Components/Components/src/PlatformInfo.cs
@@ -1,7 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Runtime.InteropServices;
+using System;
 
 namespace Microsoft.AspNetCore.Components
 {
@@ -11,7 +11,7 @@ namespace Microsoft.AspNetCore.Components
 
         static PlatformInfo()
         {
-            IsWebAssembly = OperatingSystem.IsBrowser());
+            IsWebAssembly = OperatingSystem.IsBrowser();
         }
     }
 }
