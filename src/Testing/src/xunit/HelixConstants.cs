30e90e29163f0f583eb047a88d2cf1b786d44aed
Chris Ross <Tratcher@Outlook.com>
2020-11-30 15:58:56 -0800

    Use short name for DebianArm64

--- src/Testing/src/xunit/HelixConstants.cs
+++ src/Testing/src/xunit/HelixConstants.cs
@@ -6,6 +6,6 @@ namespace Microsoft.AspNetCore.Testing
     public static class HelixConstants
     {
         public const string Windows10Arm64 = "Windows.10.Arm64v8.Open;";
-        public const string DebianArm64 = "(Debian.9.Arm64.Open)Ubuntu.1804.Armarch.Open@mcr.microsoft.com/dotnet-buildtools/prereqs:debian-9-helix-arm64v8-a12566d-20190807161036;";
+        public const string DebianArm64 = "Debian.9.Arm64.Open;";
     }
 }
