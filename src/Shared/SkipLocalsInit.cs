54e0d40e36cddfcd21c7caeda7ecd2ae5ff673ff
James Newton-King <james@newtonking.com>
2020-12-01 15:49:19 +1300

    Fix build

--- src/Shared/SkipLocalsInit.cs
+++ src/Shared/SkipLocalsInit.cs
@@ -4,6 +4,12 @@
 // Used to indicate to the compiler that the .locals init flag should not be set in method headers.
 #if NET6_0
 [module: System.Runtime.CompilerServices.SkipLocalsInit]
+#elif NETSTANDARD2_0
+// Not available
+#elif NETSTANDARD2_1
+// Not available
+#elif NET461
+// Not available
 #else
 #error Target frameworks need to be updated.
 #endif
