fce2856a819ec7fb47f4a654ab2bdec03f80903e
Pranav K <prkrishn@hotmail.com>
2020-12-27 17:32:06 -0800

    Add dcs for Microsoft.AspNetCore.Http

--- src/Http/Http.Features/src/IFormFeature.cs
+++ src/Http/Http.Features/src/IFormFeature.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.Http.Features
 {
+    /// <summary>
+    /// Allows reading the request body as a HTTP form.
+    /// </summary>
     public interface IFormFeature
     {
         /// <summary>
@@ -14,12 +17,22 @@ namespace Microsoft.AspNetCore.Http.Features
         bool HasFormContentType { get; }
 
         /// <summary>
-        /// The parsed form, if any.
+        /// The parsed form.
+        /// <para>
+        /// This API will return a non-null value if the
+        /// request body was read using <see cref="ReadFormAsync(CancellationToken)"/> or <see cref="ReadForm"/>, or
+        /// if a value was explicitly assigned.
+        /// </para>
         /// </summary>
         IFormCollection? Form { get; set; }
 
         /// <summary>
         /// Parses the request body as a form.
+        /// <para>
+        /// If the request body has not been previously read, this API performs a synchronous (blocking) read
+        /// on the HTTP input stream which may be unsupported or can adversely affect application performance.
+        /// Consider using <see cref="ReadFormAsync(CancellationToken)"/> instead.
+        /// </para>
         /// </summary>
         /// <returns>The <see cref="IFormCollection"/>.</returns>
         IFormCollection ReadForm();
