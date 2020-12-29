60a138c93d696797178e1a534f4ec2d388d834d1
Chris R <Tratcher@outlook.com>
2020-12-28 16:41:46 -0800

    Allow/ignore upgrades with bodies #17081

--- src/Servers/Kestrel/Core/src/Internal/Http/Http1MessageBody.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http/Http1MessageBody.cs
@@ -271,13 +271,13 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
                 keepAlive = (connectionOptions & ConnectionOptions.KeepAlive) == ConnectionOptions.KeepAlive;
             }
 
-            if (upgrade)
+            // Ignore upgrades if the request has a body. Technically it's possible to support, but we'd have to add a lot
+            // more logic to allow reading/draining the normal body before the connection could be fully upgraded.
+            // See https://tools.ietf.org/html/rfc7230#section-6.7, https://tools.ietf.org/html/rfc7540#section-3.2
+            if (upgrade
+                && headers.ContentLength.GetValueOrDefault() == 0
+                && headers.HeaderTransferEncoding.Count == 0)
             {
-                if (headers.HeaderTransferEncoding.Count > 0 || (headers.ContentLength.HasValue && headers.ContentLength.Value != 0))
-                {
-                    BadHttpRequestException.Throw(RequestRejectionReason.UpgradeRequestCannotHavePayload);
-                }
-
                 return new ForUpgrade(context);
             }
 
