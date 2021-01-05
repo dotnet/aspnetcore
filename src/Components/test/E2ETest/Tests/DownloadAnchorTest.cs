aecb335efa13a4ef14dd4c08464cf51d4fddabe1
Safia Abdalla <safia@microsoft.com>
2021-01-05 13:52:17 -0800

    Quarantine DownloadFileFromAnchor test

--- src/Components/test/E2ETest/Tests/DownloadAnchorTest.cs
+++ src/Components/test/E2ETest/Tests/DownloadAnchorTest.cs
@@ -29,6 +29,7 @@ namespace Microsoft.AspNetCore.Components.E2ETest.Tests
         }
 
         [Fact]
+        [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/29069")]
         public void DownloadFileFromAnchor()
         {
             // Arrange
