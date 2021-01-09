94560517f6189ddea59d3196338d950d26ac969c
Josua Jäger <mail@jaggerjo.com>
2021-01-09 22:33:45 +0100

    Add Xml Documentation for `IComponentRenderer`

--- src/Mvc/Mvc.ViewFeatures/src/RazorComponents/IComponentRenderer.cs
+++ src/Mvc/Mvc.ViewFeatures/src/RazorComponents/IComponentRenderer.cs
@@ -8,8 +8,20 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Microsoft.AspNetCore.Mvc.ViewFeatures
 {
+    /// <summary>
+    /// IComponentRenderer which can be used to render a component.
+    /// </summary>
     public interface IComponentRenderer
     {
+        /// <summary>
+        /// Renders a <paramref name="componentType"/> with <paramref name="parameters"/>
+        /// and returns <see cref="IHtmlContent"/>. 
+        /// </summary>
+        /// <param name="viewContext">The <see cref="ViewContext"/> in which the component is rendered</param>
+        /// <param name="componentType">The <see cref="Type"/> of the component to render</param>
+        /// <param name="renderMode">The <see cref="RenderMode"/></param>
+        /// <param name="parameters">The component parameters</param>
+        /// <returns></returns>
         Task<IHtmlContent> RenderComponentAsync(
             ViewContext viewContext,
             Type componentType,
