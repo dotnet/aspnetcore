138b439826cd1c49a6fdf910f2efc290397f83af
Spencer Stream <sprewstream@gmail.com>
2021-01-15 17:00:05 -0600

    Converting Home to function component

--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/App.js
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router';
 import Layout from './components/Layout';
-import { Home } from './components/Home';
+import Home from './components/Home';
 import FetchData from './components/FetchData';
 import Counter from './components/Counter';
 ////#if (IndividualLocalAuth)
