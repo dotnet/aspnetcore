08dd6cb06b9a64b8a4ab6422a9769a1434b99ed0
Spencer Stream <sprewstream@gmail.com>
2021-01-15 16:55:59 -0600

    Converting FetchData to function component

--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/App.js
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router';
 import Layout from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
+import FetchData from './components/FetchData';
 import Counter from './components/Counter';
 ////#if (IndividualLocalAuth)
 import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
