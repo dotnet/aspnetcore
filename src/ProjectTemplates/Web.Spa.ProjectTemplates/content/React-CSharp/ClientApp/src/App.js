5cb4b5bca798878b365eb1bf63d3221f8e975e8f
Spencer Stream <sprewstream@gmail.com>
2021-01-15 16:20:38 -0600

    Converting Layout to function component

--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/App.js
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
+import Layout from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import Counter from './components/Counter';
