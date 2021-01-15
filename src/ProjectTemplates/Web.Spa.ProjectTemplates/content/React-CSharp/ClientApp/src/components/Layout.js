1c584ddeeab7341a774e8c45a536076302b35723
Spencer Stream <sprewstream@gmail.com>
2021-01-15 17:08:00 -0600

    Converting NavMenu to function component

--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/components/Layout.js
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+import NavMenu from './NavMenu';
 
 const Layout = (props) => {
   return (
