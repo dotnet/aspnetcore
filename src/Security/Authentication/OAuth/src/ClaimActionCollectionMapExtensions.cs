b845acd12fb6c7b7a48f8511e871eddf06052df8
James Newton-King <james@newtonking.com>
2021-01-16 17:08:35 +1300

    Enable nullable on Authentication.MicrosoftAccount (#29253)

--- src/Security/Authentication/OAuth/src/ClaimActionCollectionMapExtensions.cs
+++ src/Security/Authentication/OAuth/src/ClaimActionCollectionMapExtensions.cs
@@ -92,7 +92,7 @@ namespace Microsoft.AspNetCore.Authentication
         /// <param name="collection">The <see cref="ClaimActionCollection"/>.</param>
         /// <param name="claimType">The value to use for Claim.Type when creating a Claim.</param>
         /// <param name="resolver">The Func that will be called to select value from the given json user data.</param>
-        public static void MapCustomJson(this ClaimActionCollection collection, string claimType, Func<JsonElement, string> resolver)
+        public static void MapCustomJson(this ClaimActionCollection collection, string claimType, Func<JsonElement, string?> resolver)
         {
             if (collection == null)
             {
@@ -110,7 +110,7 @@ namespace Microsoft.AspNetCore.Authentication
         /// <param name="claimType">The value to use for Claim.Type when creating a Claim.</param>
         /// <param name="valueType">The value to use for Claim.ValueType when creating a Claim.</param>
         /// <param name="resolver">The Func that will be called to select value from the given json user data.</param>
-        public static void MapCustomJson(this ClaimActionCollection collection, string claimType, string valueType, Func<JsonElement, string> resolver)
+        public static void MapCustomJson(this ClaimActionCollection collection, string claimType, string valueType, Func<JsonElement, string?> resolver)
         {
             if (collection == null)
             {
