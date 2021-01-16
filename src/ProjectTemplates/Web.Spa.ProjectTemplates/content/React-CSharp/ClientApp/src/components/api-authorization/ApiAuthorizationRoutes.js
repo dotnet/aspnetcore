77f9d451b100be4c5326c5dc919b36a2cc8c2a8a
Spencer Stream <sprewstream@gmail.com>
2021-01-15 18:42:21 -0600

    Converting Logout to function component

--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/components/api-authorization/ApiAuthorizationRoutes.js
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/components/api-authorization/ApiAuthorizationRoutes.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { Route } from 'react-router';
 import { Login } from './Login'
-import { Logout } from './Logout'
+import Logout from './Logout'
 import { ApplicationPaths, LoginActions, LogoutActions } from './ApiAuthorizationConstants';
 
 export default class ApiAuthorizationRoutes extends Component {
--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/components/api-authorization/Logout.js
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/components/api-authorization/Logout.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import { Component } from 'react';
+import React, { useEffect, useState } from 'react'
 import authService from './AuthorizeService';
 import { AuthenticationResultStatus } from './AuthorizeService';
 import { QueryParameterNames, LogoutActions, ApplicationPaths } from './ApiAuthorizationConstants';
@@ -7,122 +6,119 @@ import { QueryParameterNames, LogoutActions, ApplicationPaths } from './ApiAutho
 // The main responsibility of this component is to handle the user's logout process.
 // This is the starting point for the logout process, which is usually initiated when a
 // user clicks on the logout button on the LoginMenu component.
-export class Logout extends Component {
-    constructor(props) {
-        super(props);
+const Logout = (props) => {
+  const [message, setMessage] = useState(undefined);
+  const [isReady, setIsReady] = useState(false);
+  const [, setAuthenticated] = useState(false);
 
-        this.state = {
-            message: undefined,
-            isReady: false,
-            authenticated: false
-        };
+  const getReturnUrl = (state) => {
+    const params = new URLSearchParams(window.location.search);
+    const fromQuery = params.get(QueryParameterNames.ReturnUrl);
+    if (fromQuery && !fromQuery.startsWith(`${window.location.origin}/`)) {
+      // This is an extra check to prevent open redirects.
+      throw new Error("Invalid return url. The return url needs to have the same origin as the current page.")
     }
 
-    componentDidMount() {
-        const action = this.props.action;
-        switch (action) {
-            case LogoutActions.Logout:
-                if (!!window.history.state.state.local) {
-                    this.logout(this.getReturnUrl());
-                } else {
-                    // This prevents regular links to <app>/authentication/logout from triggering a logout
-                    this.setState({ isReady: true, message: "The logout was not initiated from within the page." });
-                }
-                break;
-            case LogoutActions.LogoutCallback:
-                this.processLogoutCallback();
-                break;
-            case LogoutActions.LoggedOut:
-                this.setState({ isReady: true, message: "You successfully logged out!" });
-                break;
-            default:
-                throw new Error(`Invalid action '${action}'`);
-        }
+    return (state && state.returnUrl) ||
+      fromQuery ||
+      `${window.location.origin}${ApplicationPaths.LoggedOut}`;
+  };
 
-        this.populateAuthenticationState();
+  const navigateToReturnUrl = (returnUrl) => {
+    return window.location.replace(returnUrl);
+  };
+
+  const logout = async (returnUrl) => {
+    const state = { returnUrl };
+    const isauthenticated = await authService.isAuthenticated();
+    if (isauthenticated) {
+      const result = await authService.signOut(state);
+      switch (result.status) {
+        case AuthenticationResultStatus.Redirect:
+          break;
+        case AuthenticationResultStatus.Success:
+          await navigateToReturnUrl(returnUrl);
+          break;
+        case AuthenticationResultStatus.Fail:
+          setMessage(result.message);
+          break;
+        default:
+          throw new Error("Invalid authentication result status.");
+      }
+    } else {
+      setMessage("You successfully logged out!");
     }
+  };
 
-    render() {
-        const { isReady, message } = this.state;
-        if (!isReady) {
-            return <div></div>
-        }
-        if (!!message) {
-            return (<div>{message}</div>);
+  const processLogoutCallback = async () => {
+    const url = window.location.href;
+    const result = await authService.completeSignOut(url);
+    switch (result.status) {
+      case AuthenticationResultStatus.Redirect:
+        // There should not be any redirects as the only time completeAuthentication finishes
+        // is when we are doing a redirect sign in flow.
+        throw new Error('Should not redirect.');
+      case AuthenticationResultStatus.Success:
+        await navigateToReturnUrl(getReturnUrl(result.state));
+        break;
+      case AuthenticationResultStatus.Fail:
+        setMessage(result.message);
+        break;
+      default:
+        throw new Error("Invalid authentication result status.");
+    }
+  };
+
+  const populateAuthenticationState = async () => {
+    const authenticated = await authService.isAuthenticated();
+    setIsReady(true);
+    setAuthenticated(authenticated);
+  };
+
+  useEffect(() => {
+    const action = props.action;
+    switch (action) {
+      case LogoutActions.Logout:
+        if (!!window.history.state.state.local) {
+          logout(getReturnUrl());
         } else {
-            const action = this.props.action;
-            switch (action) {
-                case LogoutActions.Logout:
-                    return (<div>Processing logout</div>);
-                case LogoutActions.LogoutCallback:
-                    return (<div>Processing logout callback</div>);
-                case LogoutActions.LoggedOut:
-                    return (<div>{message}</div>);
-                default:
-                    throw new Error(`Invalid action '${action}'`);
-            }
+          // This prevents regular links to <app>/authentication/logout from triggering a logout
+          setMessage("The logout was not initiated from within the page.");
+          setIsReady(true);
         }
+        break;
+      case LogoutActions.LogoutCallback:
+        processLogoutCallback();
+        break;
+      case LogoutActions.LoggedOut:
+        setMessage("You successfully logged out!");
+        setIsReady(true);
+        break;
+      default:
+        throw new Error(`Invalid action '${action}'`);
     }
 
-    async logout(returnUrl) {
-        const state = { returnUrl };
-        const isauthenticated = await authService.isAuthenticated();
-        if (isauthenticated) {
-            const result = await authService.signOut(state);
-            switch (result.status) {
-                case AuthenticationResultStatus.Redirect:
-                    break;
-                case AuthenticationResultStatus.Success:
-                    await this.navigateToReturnUrl(returnUrl);
-                    break;
-                case AuthenticationResultStatus.Fail:
-                    this.setState({ message: result.message });
-                    break;
-                default:
-                    throw new Error("Invalid authentication result status.");
-            }
-        } else {
-            this.setState({ message: "You successfully logged out!" });
-        }
-    }
+    populateAuthenticationState();
+  });
 
-    async processLogoutCallback() {
-        const url = window.location.href;
-        const result = await authService.completeSignOut(url);
-        switch (result.status) {
-            case AuthenticationResultStatus.Redirect:
-                // There should not be any redirects as the only time completeAuthentication finishes
-                // is when we are doing a redirect sign in flow.
-                throw new Error('Should not redirect.');
-            case AuthenticationResultStatus.Success:
-                await this.navigateToReturnUrl(this.getReturnUrl(result.state));
-                break;
-            case AuthenticationResultStatus.Fail:
-                this.setState({ message: result.message });
-                break;
-            default:
-                throw new Error("Invalid authentication result status.");
-        }
+  if (!isReady) {
+    return <div></div>
+  }
+  if (!!message) {
+    return (<div>{message}</div>);
+  } else {
+    const action = props.action;
+    switch (action) {
+      case LogoutActions.Logout:
+        return (<div>Processing logout</div>);
+      case LogoutActions.LogoutCallback:
+        return (<div>Processing logout callback</div>);
+      case LogoutActions.LoggedOut:
+        return (<div>{message}</div>);
+      default:
+        throw new Error(`Invalid action '${action}'`);
     }
+  }
+};
 
-    async populateAuthenticationState() {
-        const authenticated = await authService.isAuthenticated();
-        this.setState({ isReady: true, authenticated });
-    }
-
-    getReturnUrl(state) {
-        const params = new URLSearchParams(window.location.search);
-        const fromQuery = params.get(QueryParameterNames.ReturnUrl);
-        if (fromQuery && !fromQuery.startsWith(`${window.location.origin}/`)) {
-            // This is an extra check to prevent open redirects.
-            throw new Error("Invalid return url. The return url needs to have the same origin as the current page.")
-        }
-        return (state && state.returnUrl) ||
-            fromQuery ||
-            `${window.location.origin}${ApplicationPaths.LoggedOut}`;
-    }
-
-    navigateToReturnUrl(returnUrl) {
-        return window.location.replace(returnUrl);
-    }
-}
+export default Logout;
