90fe4f8a3180be4f4f3146624327e530c5edbb65
Hao Kung <HaoK@users.noreply.github.com>
2021-01-21 10:12:42 -0800

    Do deps copy in target instead

--- src/Mvc/Mvc.Testing.Tasks/src/GenerateMvcTestManifestTask.cs
+++ src/Mvc/Mvc.Testing.Tasks/src/GenerateMvcTestManifestTask.cs
@@ -24,11 +24,6 @@ namespace Microsoft.AspNetCore.Mvc.Testing.Tasks
         public string ManifestPath { get; set; }
 
         /// <summary>
-        /// The path to copy deps files to.
-        /// </summary>
-        public string PathToCopyDeps { get; set; }
-
-        /// <summary>
         /// A list of content root paths and assembly names to generate the
         /// manifest from.
         /// </summary>
@@ -44,23 +39,7 @@ namespace Microsoft.AspNetCore.Mvc.Testing.Tasks
             {
                 var contentRoot = project.GetMetadata("ContentRoot");
                 var assemblyName = project.GetMetadata("Identity");
-
-                // This is only set when publishing
-                if (string.IsNullOrEmpty(PathToCopyDeps))
-                {
-                    output[assemblyName] = contentRoot;
-                }
-                else
-                {
-                    // When publishing content root is always the BaseDirectory
-                    output[assemblyName] = "~";
-                    var depsFile = project.GetMetadata("DepsFile");
-                    Log.LogMessage("Looking for " + depsFile + ": "+ File.Exists(depsFile));
-                    if (File.Exists(depsFile))
-                    {
-                        File.Copy(depsFile, Path.Combine(PathToCopyDeps, Path.GetFileName(depsFile)));
-                    }
-                }
+                output[assemblyName] = contentRoot;
             }
 
             var serializer = new DataContractJsonSerializer(typeof(Dictionary<string, string>), new DataContractJsonSerializerSettings
