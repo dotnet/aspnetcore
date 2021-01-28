975a8a9f50365242ea4c7f88f36715712380d3b2
Mikel Blanchard <mblanchard@macrosssoftware.com>
2021-01-28 15:26:33 -0800

    Added an IgnoreSection overload with a required parameter.

--- src/Mvc/Mvc.Razor/src/RazorPage.cs
+++ src/Mvc/Mvc.Razor/src/RazorPage.cs
@@ -215,6 +215,18 @@ namespace Microsoft.AspNetCore.Mvc.Razor
         /// </summary>
         /// <param name="sectionName">The section to ignore.</param>
         public void IgnoreSection(string sectionName)
+            => IgnoreSectionCore(sectionName, true);
+
+        /// <summary>
+        /// In layout pages, ignores rendering the content of the section named <paramref name="sectionName"/>.
+        /// </summary>
+        /// <param name="sectionName">The section to ignore.</param>
+        /// <param name="required">Indicates the <paramref name="sectionName"/> section must be registered
+        /// (using <c>@section</c>) in the page.</param>
+        public void IgnoreSection(string sectionName, bool required)
+            => IgnoreSectionCore(sectionName, required);
+
+        private void IgnoreSectionCore(string sectionName, bool required)
         {
             if (sectionName == null)
             {
@@ -223,11 +235,15 @@ namespace Microsoft.AspNetCore.Mvc.Razor
 
             if (!PreviousSectionWriters.ContainsKey(sectionName))
             {
-                // If the section is not defined, throw an error.
-                throw new InvalidOperationException(Resources.FormatSectionNotDefined(
-                    ViewContext.ExecutingFilePath,
-                    sectionName,
-                    ViewContext.View.Path));
+                if (required)
+                {
+                    // If the section is not defined, throw an error.
+                    throw new InvalidOperationException(Resources.FormatSectionNotDefined(
+                        ViewContext.ExecutingFilePath,
+                        sectionName,
+                        ViewContext.View.Path));
+                }
+                return;
             }
 
             if (_ignoredSections == null)
