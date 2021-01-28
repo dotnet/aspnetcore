33c3822051be2f85e621aa872357c36113944737
Tanay Parikh <TanayParikh@users.noreply.github.com>
2021-01-28 14:40:16 -0800

    Quarantine E2E Test DownloadFileFromAnchor
    Extension of tests quarantined in #29652 / #29698.
    
    Failure: https://dev.azure.com/dnceng/public/_build/results?buildId=970420&view=ms.vss-test-web.build-test-results-tab&runId=30490800&resultId=100005&paneView=debug

--- src/Components/test/E2ETest/Tests/DownloadAnchorTest.cs
+++ src/Components/test/E2ETest/Tests/DownloadAnchorTest.cs
@@ -29,6 +29,7 @@ namespace Microsoft.AspNetCore.Components.E2ETest.Tests
         }
 
         [Fact]
+        [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/29739")]
         public void DownloadFileFromAnchor()
         {
             // Arrange
