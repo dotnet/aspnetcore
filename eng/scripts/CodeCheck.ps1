32994e97b755866ac66563dead61f63077b253ec
John Luo <johluo@microsoft.com>
2021-02-08 01:37:06 -0800

    Why doesn't git command work?

--- eng/scripts/CodeCheck.ps1
+++ eng/scripts/CodeCheck.ps1
@@ -193,6 +193,9 @@ try {
 
     Write-Host "Checking for changes to API baseline files"
 
+    git --version
+    git log -n 5
+
     # Retrieve the set of changed files compared to main
     git rev-parse HEAD
     $commitSha = git rev-parse HEAD
