e20561182330ebab8f96519b134e720af6fe7973
Brennan <brecon@microsoft.com>
2021-02-11 09:19:36 -0800

    [release/3.1] Remove old unused Docker files - in part, this is a `cherry-pick` of c5c8f4385ee8 - also cleans up the old SystemdActivation files

--- src/Servers/Kestrel/samples/Http2SampleApp/scripts/build-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-dotnet publish --framework netcoreapp2.2 "$DIR/../Http2SampleApp.csproj"
-
-docker build -t kestrel-http2-sample "$DIR/.."
