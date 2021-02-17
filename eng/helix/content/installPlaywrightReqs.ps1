b7d90c3fb72ff7fffffadf981b3f4494fe1ce773
Hao Kung <HaoK@users.noreply.github.com>
2021-02-17 13:33:19 -0800

    Update installPlaywrightReqs.ps1

--- eng/helix/content/installPlaywrightReqs.ps1
+++ eng/helix/content/installPlaywrightReqs.ps1
@@ -1,6 +1,6 @@
 Write-Host "Enable-WindowsOptionalFeature -Online -FeatureName ServerMediaFoundation  (For Playwright)"
 try {
-    Enable-WindowsOptionalFeature -Online -FeatureName ServerMediaFoundation 
+    Enable-WindowsOptionalFeature -Online -FeatureName ServerMediaFoundation -Force -Confirm:$false
 } catch {
     Write-Host "Enable-WindowsOptionalFeature -Online -FeatureName ServerMediaFoundation threw an exception: $PSItem.Exception.Message"
 }
