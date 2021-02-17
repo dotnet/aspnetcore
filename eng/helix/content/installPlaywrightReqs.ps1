954b230feeb6cc1d0a21c9e15027b329ee4d5867
Hao Kung <HaoK@users.noreply.github.com>
2021-02-17 13:37:05 -0800

    Update installPlaywrightReqs.ps1

--- eng/helix/content/installPlaywrightReqs.ps1
+++ eng/helix/content/installPlaywrightReqs.ps1
@@ -1,3 +1,5 @@
+Write-Host "Set-ExecutionPolicy Bypass -Scope Process"
+Set-ExecutionPolicy Bypass -Scope Process
 Write-Host "Enable-WindowsOptionalFeature -Online -FeatureName ServerMediaFoundation  (For Playwright)"
 try {
     Enable-WindowsOptionalFeature -Online -FeatureName ServerMediaFoundation -Force -Confirm:$false
