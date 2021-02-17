bd5864c8ce176024b69b0169edfaef4ee6e127c8
Hao Kung <HaoK@users.noreply.github.com>
2021-02-17 15:56:42 -0800

    Only try to install server media foundation if needed

--- eng/helix/content/installPlaywrightReqs.ps1
+++ eng/helix/content/installPlaywrightReqs.ps1
@@ -1,8 +1,12 @@
 Write-Host "Set-ExecutionPolicy Bypass -Scope Process"
 Set-ExecutionPolicy Bypass -Scope Process
-Write-Host "Enable-WindowsOptionalFeature -Online -FeatureName ServerMediaFoundation  (For Playwright)"
-try {
-    Enable-WindowsOptionalFeature -Online -FeatureName ServerMediaFoundation -Force -Confirm:$false
-} catch {
-    Write-Host "Enable-WindowsOptionalFeature -Online -FeatureName ServerMediaFoundation threw an exception: $PSItem.Exception.Message"
+if ((Get-WindowsOptionalFeature -FeatureName ServerMediaFoundation -Online).State -eq "Enabled") {
+    Write-Host "ServerMediaFoundation feature already enabled."
+} else {
+    Write-Host "Enable-WindowsOptionalFeature -Online -FeatureName ServerMediaFoundation -Confirm:$false (For Playwright)"
+    try {
+        Enable-WindowsOptionalFeature -Online -FeatureName ServerMediaFoundation -Confirm:$false
+    } catch {
+        Write-Host "Enable-WindowsOptionalFeature -Online -FeatureName ServerMediaFoundation -Confirm:$false threw an exception: $PSItem"
+    }
 }
