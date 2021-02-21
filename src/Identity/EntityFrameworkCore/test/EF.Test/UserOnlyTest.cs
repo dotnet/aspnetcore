14763986c791c9d50db64458d3a3353a970b8694
Kevin Pilch <me@pilchie.com>
2021-02-21 13:08:57 -0800

    Fix credscan issues in Identity, except where they affect checked in certs

--- src/Identity/EntityFrameworkCore/test/EF.Test/UserOnlyTest.cs
+++ src/Identity/EntityFrameworkCore/test/EF.Test/UserOnlyTest.cs
@@ -57,7 +57,7 @@ namespace Microsoft.AspNetCore.Identity.EntityFrameworkCore.Test
             Assert.NotNull(userManager);
 
             const string userName = "admin";
-            const string password = "1qaz@WSX";
+            const string password = "[PLACEHOLDER]-1a";
             var user = new IdentityUser { UserName = userName };
             IdentityResultAssert.IsSuccess(await userManager.CreateAsync(user, password));
             IdentityResultAssert.IsSuccess(await userManager.DeleteAsync(user));
