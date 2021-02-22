d06b5205111cfc854b46076b257285d698b12d01
John Luo <johluo@microsoft.com>
2021-02-22 11:23:31 -0800

    Update script for internal

--- eng/scripts/CodeCheck.ps1
+++ eng/scripts/CodeCheck.ps1
@@ -192,12 +192,18 @@ try {
     }
 
     $targetBranch = $env:SYSTEM_PULLREQUEST_TARGETBRANCH
+    if (-not ($targetBranch.StartsWith('refs/heads/'))) {
+        $targetBranch = refs/heads$targetBranch
+    }
     $sourceBranch = $env:SYSTEM_PULLREQUEST_SOURCEBRANCH
+    if (-not ($sourceBranch.StartsWith('refs/heads/'))) {
+        $sourceBranch = refs/heads$sourceBranch
+    }
 
     # Retrieve the set of changed files compared to main
     Write-Host "Checking for changes to API baseline files $targetBranch...$sourceBranch"
 
-    $changedFilesFromTarget = git --no-pager diff origin/$targetBranch...origin/$sourceBranch --ignore-space-change --name-only --diff-filter=ar
+    $changedFilesFromTarget = git --no-pager diff $targetBranch...$sourceBranch --ignore-space-change --name-only --diff-filter=ar
     $changedAPIBaselines = [System.Collections.Generic.List[string]]::new()
 
     if ($changedFilesFromTarget) {
