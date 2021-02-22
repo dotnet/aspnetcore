fc1d63f8af6eac343bbbf170dbb6de9276208bde
BrennanConroy <brecon@microsoft.com>
2021-02-22 14:50:13 -0800

    fixup kestrel microbenchmarks

--- src/Servers/Kestrel/perf/Microbenchmarks/Http2FrameWriterBenchmark.cs
+++ src/Servers/Kestrel/perf/Microbenchmarks/Http2FrameWriterBenchmark.cs
@@ -4,10 +4,12 @@
 using System.Buffers;
 using System.IO.Pipelines;
 using BenchmarkDotNet.Attributes;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
 using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2;
 using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2.FlowControl;
 using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Infrastructure;
+using Microsoft.AspNetCore.Testing;
 using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Microsoft.AspNetCore.Server.Kestrel.Microbenchmarks
@@ -27,6 +29,12 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Microbenchmarks
             var options = new PipeOptions(_memoryPool, readerScheduler: PipeScheduler.Inline, writerScheduler: PipeScheduler.Inline, useSynchronizationContext: false);
             _pipe = new Pipe(options);
 
+            var serviceContext = TestContextFactory.CreateServiceContext(
+                serverOptions: new KestrelServerOptions(),
+                httpParser: new HttpParser<Http1ParsingHandler>(),
+                dateHeaderValueManager: new DateHeaderValueManager(),
+                log: new MockTrace());
+
             _frameWriter = new Http2FrameWriter(
                 new NullPipeWriter(),
                 connectionContext: null,
@@ -36,7 +44,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Microbenchmarks
                 minResponseDataRate: null,
                 "TestConnectionId",
                 _memoryPool,
-                new Core.Internal.ServiceContext());
+                serviceContext);
 
             _responseHeaders = new HttpResponseHeaders();
             _responseHeaders.HeaderContentType = "application/json";
--- src/Servers/Kestrel/perf/Microbenchmarks/ResponseHeaderCollectionBenchmark.cs
+++ src/Servers/Kestrel/perf/Microbenchmarks/ResponseHeaderCollectionBenchmark.cs
@@ -179,7 +179,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Microbenchmarks
             var serviceContext = TestContextFactory.CreateServiceContext(
                 serverOptions: new KestrelServerOptions(),
                 httpParser: new HttpParser<Http1ParsingHandler>(),
-                dateHeaderValueManager: new DateHeaderValueManager(),
+                dateHeaderValueManager: _dateHeaderValueManager,
                 log: new MockTrace());
 
             var connectionContext = TestContextFactory.CreateHttpConnectionContext(
