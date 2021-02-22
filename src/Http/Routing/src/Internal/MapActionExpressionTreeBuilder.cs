cc79d3efd4380b685fa2ea3791c5dd0ecf2e6492
Kahbazi <akahbazi@gmail.com>
2021-02-23 01:26:32 +0330

    Fix Task of IResult for MapAction (#30355)

--- src/Http/Routing/src/Internal/MapActionExpressionTreeBuilder.cs
+++ src/Http/Routing/src/Internal/MapActionExpressionTreeBuilder.cs
@@ -427,7 +427,7 @@ namespace Microsoft.AspNetCore.Routing.Internal
             return ExecuteAwaited(task, httpContext);
         }
 
-        private static async ValueTask ExecuteTaskResult<T>(Task<T> task, HttpContext httpContext) where T : IResult
+        private static async Task ExecuteTaskResult<T>(Task<T> task, HttpContext httpContext) where T : IResult
         {
             await (await task).ExecuteAsync(httpContext);
         }
