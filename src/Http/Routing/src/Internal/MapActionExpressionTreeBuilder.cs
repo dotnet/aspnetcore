1735db41b3478a6331444f4ea34109b7ba485368
Kahbazi <akahbazi@gmail.com>
2021-02-27 00:18:46 +0330

    Support optional input for MapAction (#30434)
    * Support optional input for MapAction
    
    * Revert passing HttpContext.RequestAborted
    
    * Fix tests

--- src/Http/Routing/src/Internal/MapActionExpressionTreeBuilder.cs
+++ src/Http/Routing/src/Internal/MapActionExpressionTreeBuilder.cs
@@ -24,7 +24,8 @@ namespace Microsoft.AspNetCore.Routing.Internal
         private static readonly MethodInfo ChangeTypeMethodInfo = GetMethodInfo<Func<object, Type, object>>((value, type) => Convert.ChangeType(value, type, CultureInfo.InvariantCulture));
         private static readonly MethodInfo ExecuteTaskOfTMethodInfo = typeof(MapActionExpressionTreeBuilder).GetMethod(nameof(ExecuteTask), BindingFlags.NonPublic | BindingFlags.Static)!;
         private static readonly MethodInfo ExecuteTaskOfStringMethodInfo = typeof(MapActionExpressionTreeBuilder).GetMethod(nameof(ExecuteTaskOfString), BindingFlags.NonPublic | BindingFlags.Static)!;
-        private static readonly MethodInfo ExecuteValueTaskOfTMethodInfo = typeof(MapActionExpressionTreeBuilder).GetMethod(nameof(ExecuteValueTask), BindingFlags.NonPublic | BindingFlags.Static)!;
+        private static readonly MethodInfo ExecuteValueTaskOfTMethodInfo = typeof(MapActionExpressionTreeBuilder).GetMethod(nameof(ExecuteValueTaskOfT), BindingFlags.NonPublic | BindingFlags.Static)!;
+        private static readonly MethodInfo ExecuteValueTaskMethodInfo = typeof(MapActionExpressionTreeBuilder).GetMethod(nameof(ExecuteValueTask), BindingFlags.NonPublic | BindingFlags.Static)!;
         private static readonly MethodInfo ExecuteValueTaskOfStringMethodInfo = typeof(MapActionExpressionTreeBuilder).GetMethod(nameof(ExecuteValueTaskOfString), BindingFlags.NonPublic | BindingFlags.Static)!;
         private static readonly MethodInfo ExecuteTaskResultOfTMethodInfo = typeof(MapActionExpressionTreeBuilder).GetMethod(nameof(ExecuteTaskResult), BindingFlags.NonPublic | BindingFlags.Static)!;
         private static readonly MethodInfo ExecuteValueResultTaskOfTMethodInfo = typeof(MapActionExpressionTreeBuilder).GetMethod(nameof(ExecuteValueTaskResult), BindingFlags.NonPublic | BindingFlags.Static)!;
@@ -71,28 +72,31 @@ namespace Microsoft.AspNetCore.Routing.Internal
             // This argument represents the deserialized body returned from IHttpRequestReader
             // when the method has a FromBody attribute declared
 
-            var args = new List<Expression>();
+            var methodParameters = method.GetParameters();
+            var args = new List<Expression>(methodParameters.Length);
 
-            foreach (var parameter in method.GetParameters())
+            foreach (var parameter in methodParameters)
             {
                 Expression paramterExpression = Expression.Default(parameter.ParameterType);
 
-                if (parameter.GetCustomAttributes().OfType<IFromRouteMetadata>().FirstOrDefault() is { } routeAttribute)
+                var parameterCustomAttributes = parameter.GetCustomAttributes();
+
+                if (parameterCustomAttributes.OfType<IFromRouteMetadata>().FirstOrDefault() is { } routeAttribute)
                 {
                     var routeValuesProperty = Expression.Property(HttpRequestExpr, nameof(HttpRequest.RouteValues));
                     paramterExpression = BindParamenter(routeValuesProperty, parameter, routeAttribute.Name);
                 }
-                else if (parameter.GetCustomAttributes().OfType<IFromQueryMetadata>().FirstOrDefault() is { } queryAttribute)
+                else if (parameterCustomAttributes.OfType<IFromQueryMetadata>().FirstOrDefault() is { } queryAttribute)
                 {
                     var queryProperty = Expression.Property(HttpRequestExpr, nameof(HttpRequest.Query));
                     paramterExpression = BindParamenter(queryProperty, parameter, queryAttribute.Name);
                 }
-                else if (parameter.GetCustomAttributes().OfType<IFromHeaderMetadata>().FirstOrDefault() is { } headerAttribute)
+                else if (parameterCustomAttributes.OfType<IFromHeaderMetadata>().FirstOrDefault() is { } headerAttribute)
                 {
                     var headersProperty = Expression.Property(HttpRequestExpr, nameof(HttpRequest.Headers));
                     paramterExpression = BindParamenter(headersProperty, parameter, headerAttribute.Name);
                 }
-                else if (parameter.GetCustomAttributes().OfType<IFromBodyMetadata>().FirstOrDefault() is { } bodyAttribute)
+                else if (parameterCustomAttributes.OfType<IFromBodyMetadata>().FirstOrDefault() is { } bodyAttribute)
                 {
                     if (consumeBodyDirectly)
                     {
@@ -109,7 +113,7 @@ namespace Microsoft.AspNetCore.Routing.Internal
                     bodyType = parameter.ParameterType;
                     paramterExpression = Expression.Convert(DeserializedBodyArg, bodyType);
                 }
-                else if (parameter.GetCustomAttributes().OfType<IFromFormMetadata>().FirstOrDefault() is { } formAttribute)
+                else if (parameterCustomAttributes.OfType<IFromFormMetadata>().FirstOrDefault() is { } formAttribute)
                 {
                     if (consumeBodyDirectly)
                     {
@@ -125,27 +129,24 @@ namespace Microsoft.AspNetCore.Routing.Internal
                 {
                     paramterExpression = Expression.Call(GetRequiredServiceMethodInfo.MakeGenericMethod(parameter.ParameterType), RequestServicesExpr);
                 }
-                else
+                else if (parameter.ParameterType == typeof(IFormCollection))
                 {
-                    if (parameter.ParameterType == typeof(IFormCollection))
+                    if (consumeBodyDirectly)
                     {
-                        if (consumeBodyDirectly)
-                        {
-                            ThrowCannotReadBodyDirectlyAndAsForm();
-                        }
+                        ThrowCannotReadBodyDirectlyAndAsForm();
+                    }
 
-                        consumeBodyAsForm = true;
+                    consumeBodyAsForm = true;
 
-                        paramterExpression = Expression.Property(HttpRequestExpr, nameof(HttpRequest.Form));
-                    }
-                    else if (parameter.ParameterType == typeof(HttpContext))
-                    {
-                        paramterExpression = HttpContextParameter;
-                    }
-                    else if (parameter.ParameterType == typeof(CancellationToken))
-                    {
-                        paramterExpression = RequestAbortedExpr;
-                    }
+                    paramterExpression = Expression.Property(HttpRequestExpr, nameof(HttpRequest.Form));
+                }
+                else if (parameter.ParameterType == typeof(HttpContext))
+                {
+                    paramterExpression = HttpContextParameter;
+                }
+                else if (parameter.ParameterType == typeof(CancellationToken))
+                {
+                    paramterExpression = RequestAbortedExpr;
                 }
 
                 args.Add(paramterExpression);
@@ -181,6 +182,12 @@ namespace Microsoft.AspNetCore.Routing.Internal
                 if (method.ReturnType == typeof(Task))
                 {
                     body = methodCall;
+                }
+                else if (method.ReturnType == typeof(ValueTask))
+                {
+                    body = Expression.Call(
+                                        ExecuteValueTaskMethodInfo,
+                                        methodCall);
                 }
                 else if (method.ReturnType.IsGenericType &&
                          method.ReturnType.GetGenericTypeDefinition() == typeof(Task<>))
@@ -263,7 +270,7 @@ namespace Microsoft.AspNetCore.Routing.Internal
                 var box = Expression.TypeAs(methodCall, typeof(object));
                 body = Expression.Call(JsonResultWriteResponseAsync, HttpResponseExpr, box, Expression.Constant(CancellationToken.None));
             }
-            else 
+            else
             {
                 body = Expression.Call(JsonResultWriteResponseAsync, HttpResponseExpr, methodCall, Expression.Constant(CancellationToken.None));
             }
@@ -398,10 +405,20 @@ namespace Microsoft.AspNetCore.Routing.Internal
                 expr = Expression.Convert(expr, parameter.ParameterType);
             }
 
+            Expression defaultExpression;
+            if (parameter.HasDefaultValue)
+            {
+                defaultExpression = Expression.Constant(parameter.DefaultValue);
+            }
+            else
+            {
+                defaultExpression = Expression.Default(parameter.ParameterType);
+            }
+
             // property[key] == null ? default : (ParameterType){Type}.Parse(property[key]);
             expr = Expression.Condition(
                 Expression.Equal(valueArg, Expression.Constant(null)),
-                Expression.Default(parameter.ParameterType),
+                defaultExpression,
                 expr);
 
             return expr;
@@ -449,7 +466,22 @@ namespace Microsoft.AspNetCore.Routing.Internal
             return ExecuteAwaited(task, httpContext);
         }
 
-        private static Task ExecuteValueTask<T>(ValueTask<T> task, HttpContext httpContext)
+        private static Task ExecuteValueTask(ValueTask task)
+        {
+            static async Task ExecuteAwaited(ValueTask task)
+            {
+                await task;
+            }
+
+            if (task.IsCompletedSuccessfully)
+            {
+                task.GetAwaiter().GetResult();
+            }
+
+            return ExecuteAwaited(task);
+        }
+
+        private static Task ExecuteValueTaskOfT<T>(ValueTask<T> task, HttpContext httpContext)
         {
             static async Task ExecuteAwaited(ValueTask<T> task, HttpContext httpContext)
             {
