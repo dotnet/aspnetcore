46e25b9cfee17aaa91ac6436d13730a8158a1ecd
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2021-03-01 16:04:44 +0000

    Make WPF sample able to run

---
+++ src/Components/WebView/Samples/BlazorWpfApp/App.xaml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Windows;
+
+namespace BlazorWpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+            {
+                MessageBox.Show(error.ExceptionObject.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            };
+        }
+    }
+}
---
+++ src/Components/WebView/Samples/BlazorWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var serviceCollection = new ServiceCollection();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+
+            InitializeComponent();
+        }
+    }
+}
