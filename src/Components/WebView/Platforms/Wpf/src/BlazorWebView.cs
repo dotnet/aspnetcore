794a05249b3a045b6483c6befac2768bb2ab982d
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2021-03-03 12:18:09 +0000

    Let the WPF developer specify the host page

--- src/Components/WebView/Platforms/Wpf/src/BlazorWebView.cs
+++ src/Components/WebView/Platforms/Wpf/src/BlazorWebView.cs
@@ -15,11 +15,11 @@ namespace Microsoft.AspNetCore.Components.WebView.Wpf
     public sealed class BlazorWebView : Control, IDisposable
     {
         #region Dependency property definitions
-        //public static readonly DependencyProperty HostPageProperty = DependencyProperty.Register(
-        //    name: nameof(HostPage),
-        //    propertyType: typeof(string),
-        //    ownerType: typeof(BlazorWebView),
-        //    typeMetadata: new PropertyMetadata(OnHostPagePropertyChanged));
+        public static readonly DependencyProperty HostPageProperty = DependencyProperty.Register(
+            name: nameof(HostPage),
+            propertyType: typeof(string),
+            ownerType: typeof(BlazorWebView),
+            typeMetadata: new PropertyMetadata(OnHostPagePropertyChanged));
 
         public static readonly DependencyProperty RootComponentsProperty = DependencyProperty.Register(
             name: nameof(RootComponents),
@@ -48,51 +48,50 @@ namespace Microsoft.AspNetCore.Components.WebView.Wpf
             };
         }
 
-        //public string HostPage
-        //{
-        //    get { return (string)GetValue(HostPageProperty); }
-        //    set { SetValue(HostPageProperty, value); }
-        //}
+        public string HostPage
+        {
+            get => (string)GetValue(HostPageProperty);
+            set => SetValue(HostPageProperty, value);
+        }
 
         public ObservableCollection<RootComponent> RootComponents =>
             (ObservableCollection<RootComponent>)GetValue(RootComponentsProperty);
 
         public IServiceProvider Services
         {
-            get { return (IServiceProvider)GetValue(ServicesProperty); }
-            set { SetValue(ServicesProperty, value); }
+            get => (IServiceProvider)GetValue(ServicesProperty);
+            set => SetValue(ServicesProperty, value);
         }
 
         private static void OnServicesPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((BlazorWebView)d).OnServicesPropertyChanged(e);
 
         private void OnServicesPropertyChanged(DependencyPropertyChangedEventArgs e) => StartWebViewCoreIfPossible();
 
-        //private static void OnHostPagePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((BlazorWebView)d).OnHostPagePropertyChanged(e);
+        private static void OnHostPagePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((BlazorWebView)d).OnHostPagePropertyChanged(e);
 
-        //private void OnHostPagePropertyChanged(DependencyPropertyChangedEventArgs e) => StartWebViewCoreIfPossible();
+        private void OnHostPagePropertyChanged(DependencyPropertyChangedEventArgs e) => StartWebViewCoreIfPossible();
 
         private bool RequiredStartupPropertiesSet =>
             _webview != null &&
-            //HostPage != null &&
+            HostPage != null &&
             Services != null;
 
         public override void OnApplyTemplate()
         {
             // Called when the control is created after its child control (the WebView2) is created from the Template property
-
             base.OnApplyTemplate();
 
-            // TODO: Can this be called more than once? We need the following only to happen once.
-            _webview = (WebView2Control)GetTemplateChild(webViewTemplateChildName);
-
-            StartWebViewCoreIfPossible();
+            if (_webview == null)
+            {
+                _webview = (WebView2Control)GetTemplateChild(webViewTemplateChildName);
+                StartWebViewCoreIfPossible();
+            }
         }
 
         protected override void OnInitialized(EventArgs e)
         {
             // Called when BeginInit/EndInit are used, such as when creating the control from XAML
             base.OnInitialized(e);
-
             StartWebViewCoreIfPossible();
         }
 
@@ -103,12 +102,11 @@ namespace Microsoft.AspNetCore.Components.WebView.Wpf
                 return;
             }
 
-            // TODO: Make content root configurable. Allow the developer to set the host page page.
-            var appDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            var wwwroot = Path.Combine(appDir, "wwwroot");
-            var fileProvider = new PhysicalFileProvider(Directory.Exists(wwwroot) ? wwwroot : appDir);
+            // We assume the host page is always in the root of the content directory, because it's
+            // unclear there's any other use case. We can add more options later if so.
+            var contentRootDir = Path.GetDirectoryName(Path.GetFullPath(HostPage));
+            var fileProvider = new PhysicalFileProvider(contentRootDir);
 
-            // TODO: Can this get called multiple times, such as from OnApplyTemplate or a property change? Do we need to handle this more efficiently?
             _webviewManager = new WebView2WebViewManager(new WpfWeb2ViewWrapper(_webview), Services, WpfDispatcher.Instance, fileProvider);
             foreach (var rootComponent in RootComponents)
             {
