2d47404972468e8e822d49a00a4ebbc49f6b57b3
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2021-03-03 10:49:44 +0000

    Prevent multi-initialization

--- src/Components/WebView/Platforms/Wpf/src/WpfWeb2ViewWrapper.cs
+++ src/Components/WebView/Platforms/Wpf/src/WpfWeb2ViewWrapper.cs
@@ -9,15 +9,11 @@ namespace Microsoft.AspNetCore.Components.WebView.Wpf
     internal class WpfWeb2ViewWrapper : IWebView2Wrapper
     {
         private readonly WebView2Control _webView2;
+        private bool _hasInitialized;
 
         public WpfWeb2ViewWrapper(WebView2Control webView2)
         {
-            if (webView2 is null)
-            {
-                throw new ArgumentNullException(nameof(webView2));
-            }
-
-            _webView2 = webView2;
+            _webView2 = webView2 ?? throw new ArgumentNullException(nameof(webView2));
         }
 
 
@@ -31,6 +27,13 @@ namespace Microsoft.AspNetCore.Components.WebView.Wpf
 
         public Task EnsureCoreWebView2Async(CoreWebView2Environment environment = null)
         {
+            if (_hasInitialized)
+            {
+                // We don't want people to think they can set more than one environment
+                throw new InvalidOperationException($"{nameof(EnsureCoreWebView2Async)} may only be called once per control.");
+            }
+
+            _hasInitialized = true;
             return _webView2.EnsureCoreWebView2Async(environment);
         }
     }
