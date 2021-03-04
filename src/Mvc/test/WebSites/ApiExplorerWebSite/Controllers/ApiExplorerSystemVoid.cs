ef3b406a46d0485e407b4b971ac0469829d4930b
Safia Abdalla <safia@microsoft.com>
2021-03-04 11:09:13 -0800

    Respect user set Type for client errors via ProducesResponseType (#30509)
    * Respect user set Type for client errors via ProducesResponseType
    
    * Address feedback from PR review
    
    * React to name feedback
    
    * Maintain existing behavior where IsDefaultResponse=true

---
+++ src/Mvc/test/WebSites/ApiExplorerWebSite/Controllers/ApiExplorerSystemVoid.cs
@@ -0,0 +1,20 @@
+
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiExplorerWebSite
+{
+    [Route("ApiExplorerVoid/[action]")]
+    [ApiController]
+    public class ApiExplorerVoidController : Controller
+    {
+        [ProducesResponseType(typeof(void), 401)]
+        public IActionResult ActionWithVoidType() => Ok();
+
+        [ProducesResponseType(401)]
+        public IActionResult ActionWithNoExplicitType() => Ok();
+
+    }
+}
