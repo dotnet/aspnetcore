96d27a344ceb2ffa005cafe1b017c7845f7bc906
Eilon Lipton <Eilon@users.noreply.github.com>
2021-03-04 11:02:51 -0800

    Add license headers

--- src/Components/WebView/WebView/src/ComponentsWebViewServiceCollectionExtensions.cs
+++ src/Components/WebView/WebView/src/ComponentsWebViewServiceCollectionExtensions.cs
@@ -1,3 +1,6 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Components.WebView.Services;
--- src/Components/WebView/WebView/test/Infrastructure/TextNode.cs
+++ src/Components/WebView/WebView/test/Infrastructure/TextNode.cs
@@ -1,4 +1,7 @@
-﻿namespace Microsoft.AspNetCore.Components.WebView.Document
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.AspNetCore.Components.WebView.Document
 {
     internal class TextNode : TestNode
     {
