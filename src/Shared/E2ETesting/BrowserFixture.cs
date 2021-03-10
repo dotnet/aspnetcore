8d56d2a4d87ce6fd0a608f4223c63a01177a314a
Safia Abdalla <safia@microsoft.com>
2021-03-10 12:41:36 -0800

    Add additional check for downloaded file check

--- src/Shared/E2ETesting/BrowserFixture.cs
+++ src/Shared/E2ETesting/BrowserFixture.cs
@@ -33,6 +33,8 @@ namespace Microsoft.AspNetCore.E2ETesting
 
         public IMessageSink DiagnosticsMessageSink { get; }
 
+        public string UserProfileDir { get; private set; }
+
         public static void EnforceSupportedConfigurations()
         {
             // Do not change the current platform support without explicit approval.
@@ -168,6 +170,7 @@ namespace Microsoft.AspNetCore.E2ETesting
             {
                 Directory.CreateDirectory(userProfileDirectory);
                 opts.AddArgument($"--user-data-dir={userProfileDirectory}");
+                opts.AddUserProfilePreference("download.default_directory", Path.Combine(userProfileDirectory, "Downloads"));
             }
 
             var instance = await SeleniumStandaloneServer.GetInstanceAsync(output);
