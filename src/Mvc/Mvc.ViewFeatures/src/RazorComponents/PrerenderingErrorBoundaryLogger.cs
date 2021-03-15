4514899f083d4d4eca5a931f6edfe3f89963e30a
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2021-03-15 13:03:19 +0000

    Add PrerenderingErrorBoundaryLogger

---
+++ src/Mvc/Mvc.ViewFeatures/src/RazorComponents/PrerenderingErrorBoundaryLogger.cs
@@ -0,0 +1,30 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Microsoft.AspNetCore.Mvc.ViewFeatures
+{
+    internal class PrerenderingErrorBoundaryLogger : IErrorBoundaryLogger
+    {
+        private static readonly Action<ILogger, string, Exception> _exceptionCaughtByErrorBoundary = LoggerMessage.Define<string>(
+            LogLevel.Warning,
+            100,
+            "Unhandled exception rendering component: {Message}");
+
+        private readonly ILogger _logger;
+
+        public PrerenderingErrorBoundaryLogger(ILogger<ErrorBoundary> logger)
+        {
+            _logger = logger;
+        }
+
+        public ValueTask LogErrorAsync(Exception exception, bool clientOnly)
+        {
+            _exceptionCaughtByErrorBoundary(_logger, exception.Message, exception);
+            return ValueTask.CompletedTask;
+        }
+    }
+}
