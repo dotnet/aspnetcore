72112886440a5084aec84941cbaf3517a6843b60
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2021-03-18 14:48:30 +0000

    E2E test apps for WinForms and WPF BlazorWebView (#31021)

--- src/Components/WebView/Platforms/WindowsForms/testassets/WinFormsTestApp/Form1.cs
+++ src/Components/WebView/Platforms/WindowsForms/testassets/WinFormsTestApp/Form1.cs
@@ -1,35 +1,25 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
 using System.Windows.Forms;
+using System.Net.Http;
 using Microsoft.AspNetCore.Components.WebView.WindowsForms;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace BlazorWinFormsApp
+namespace WinFormsTestApp
 {
     public partial class Form1 : Form
     {
-        private readonly AppState _appState = new();
-
         public Form1()
         {
             var serviceCollection = new ServiceCollection();
             serviceCollection.AddBlazorWebView();
-            serviceCollection.AddSingleton<AppState>(_appState);
+            serviceCollection.AddSingleton<HttpClient>();
             InitializeComponent();
 
-            blazorWebView1.HostPage = @"wwwroot\index.html";
+            blazorWebView1.HostPage = @"wwwroot\webviewhost.html";
             blazorWebView1.Services = serviceCollection.BuildServiceProvider();
-            blazorWebView1.RootComponents.Add<Main>("#app");
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show(
-                owner: this,
-                text: $"Current counter value is: {_appState.Counter}",
-                caption: "Counter");
+            blazorWebView1.RootComponents.Add<BasicTestApp.Index>("root");
         }
     }
 }
