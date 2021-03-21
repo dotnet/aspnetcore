08fc87ab6b1da8104dbaf1f0b7b891170151141a
David Fowler <davidfowl@gmail.com>
2021-03-21 10:41:02 -0700

    Fix some low hanging allocation fruit - Use singleton pipe options - Use Task.Yield instead of AwaitableThreadPool on the server side - Change WebSocketMiddleware handshake helpers to remove enumerator allocations.

--- src/SignalR/common/Http.Connections/src/Internal/HttpConnectionDispatcher.cs
+++ src/SignalR/common/Http.Connections/src/Internal/HttpConnectionDispatcher.cs
@@ -718,8 +718,9 @@ namespace Microsoft.AspNetCore.Http.Connections.Internal
 
         private HttpConnectionContext CreateConnection(HttpConnectionDispatcherOptions options, int clientProtocolVersion = 0)
         {
-            var transportPipeOptions = new PipeOptions(pauseWriterThreshold: options.TransportMaxBufferSize, resumeWriterThreshold: options.TransportMaxBufferSize / 2, readerScheduler: PipeScheduler.ThreadPool, useSynchronizationContext: false);
-            var appPipeOptions = new PipeOptions(pauseWriterThreshold: options.ApplicationMaxBufferSize, resumeWriterThreshold: options.ApplicationMaxBufferSize / 2, readerScheduler: PipeScheduler.ThreadPool, useSynchronizationContext: false);
+            var transportPipeOptions = options.TransportPipeOptions!;
+            var appPipeOptions = options.AppPipeOptions!;
+
             return _manager.CreateConnection(transportPipeOptions, appPipeOptions, clientProtocolVersion);
         }
 
