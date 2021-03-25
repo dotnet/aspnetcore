dd4d78310fc640b04b0fd7e30ca970f41fc304fc
Hao Kung <HaoK@users.noreply.github.com>
2021-03-25 09:07:36 -0700

    Revert "Revert "BlazorTemplate test improvements (#30907)" (#31223)"
    This reverts commit 333134cf80af0b48b3245f4642cd6ff4d1569d43.

---
+++ src/ProjectTemplates/BlazorTemplates.Tests/PriorityOrderer.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Templates.Test
+{
+    public class PriorityOrderer : ITestCaseOrderer
+    {
+        public IEnumerable<TTestCase> OrderTestCases<TTestCase>(
+            IEnumerable<TTestCase> testCases) where TTestCase : ITestCase
+        {
+            string assemblyName = typeof(TestPriorityAttribute).AssemblyQualifiedName!;
+            var sortedMethods = new SortedDictionary<int, List<TTestCase>>();
+            foreach (TTestCase testCase in testCases)
+            {
+                int priority = testCase.TestMethod.Method
+                    .GetCustomAttributes(assemblyName)
+                    .FirstOrDefault()
+                    ?.GetNamedArgument<int>(nameof(TestPriorityAttribute.Priority)) ?? 0;
+
+                GetOrCreate(sortedMethods, priority).Add(testCase);
+            }
+
+            foreach (TTestCase testCase in
+                sortedMethods.Keys.SelectMany(
+                    priority => sortedMethods[priority].OrderBy(
+                        testCase => testCase.TestMethod.Method.Name)))
+            {
+                yield return testCase;
+            }
+        }
+
+        private static TValue GetOrCreate<TKey, TValue>(
+            IDictionary<TKey, TValue> dictionary, TKey key)
+            where TKey : struct
+            where TValue : new() =>
+            dictionary.TryGetValue(key, out TValue result)
+                ? result
+                : (dictionary[key] = new TValue());
+    }
+}
