2857014789c1b711399dd2b48b4cfbfe89581bad
Andrzej <andrzej.pindor@gmail.com>
2021-03-29 17:16:01 +0200

    Move logging to separate class to prevent allocation per generic type (#31345)

--- src/Servers/HttpSys/src/RequestProcessing/RequestContextOfT.cs
+++ src/Servers/HttpSys/src/RequestProcessing/RequestContextOfT.cs
@@ -51,7 +51,7 @@ namespace Microsoft.AspNetCore.Server.HttpSys
                 }
                 catch (Exception ex)
                 {
-                    Log.RequestProcessError(Logger, ex);
+                    RequestContextLog.RequestProcessError(Logger, ex);
                     if (context != null)
                     {
                         application.DisposeContext(context, ex);
@@ -85,14 +85,14 @@ namespace Microsoft.AspNetCore.Server.HttpSys
                 {
                     if (messagePump.DecrementOutstandingRequest() == 0 && messagePump.Stopping)
                     {
-                        Log.RequestsDrained(Logger);
+                        RequestContextLog.RequestsDrained(Logger);
                         messagePump.SetShutdownSignal();
                     }
                 }
             }
             catch (Exception ex)
             {
-                Log.RequestError(Logger, ex);
+                RequestContextLog.RequestError(Logger, ex);
                 Abort();
             }
         }
