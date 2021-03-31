563680ce62ec8ad4c1552fa6ff7f2d12c658c2d6
Youssef Victor <youssefvictor00@gmail.com>
2021-03-31 21:38:23 +0200

    Fix file header in src/Localization

--- src/Localization/Localization/test/Microsoft.Extensions.Localization.RootNamespace.Tests/AssemblyInfo.cs
+++ src/Localization/Localization/test/Microsoft.Extensions.Localization.RootNamespace.Tests/AssemblyInfo.cs
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 using System.Reflection;
 using Microsoft.Extensions.Localization;
 
--- src/Localization/Localization/test/Microsoft.Extensions.Localization.Tests/Model.cs
+++ src/Localization/Localization/test/Microsoft.Extensions.Localization.Tests/Model.cs
@@ -1,4 +1,6 @@
-// This namespace for test resources with alternative RootNamespace
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 namespace MyNamespace
 {
     public class Model
