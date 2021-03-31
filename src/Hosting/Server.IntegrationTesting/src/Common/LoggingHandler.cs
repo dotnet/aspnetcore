2981f9aa9291cd767224e4533ebd3d23799ea227
Youssef Victor <youssefvictor00@gmail.com>
2021-03-31 20:51:47 +0200

    Fix file header in src/Hosting

--- src/Hosting/Server.IntegrationTesting/src/Common/LoggingHandler.cs
+++ src/Hosting/Server.IntegrationTesting/src/Common/LoggingHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
