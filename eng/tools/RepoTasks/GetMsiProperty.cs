e838cebbb0d0394c19faf381cadf4b5067697161
Youssef Victor <youssefvictor00@gmail.com>
2021-03-31 19:17:37 +0200

    Fix file header in tools.slnf - eng/tools folder

--- eng/tools/RepoTasks/GetMsiProperty.cs
+++ eng/tools/RepoTasks/GetMsiProperty.cs
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 #if BUILD_MSI_TASKS
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
