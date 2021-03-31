a3e55c655cb0bdb0ae509abbf1097209a028fbdf
Youssef Victor <youssefvictor00@gmail.com>
2021-03-31 19:43:03 +0200

    Fix file header in HttpAbstractions.slnf - src/Http folder

--- src/Http/Routing/test/UnitTests/GlobalSuppressions.cs
+++ src/Http/Routing/test/UnitTests/GlobalSuppressions.cs
@@ -1,7 +1,5 @@
-// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage(
     "Build",
