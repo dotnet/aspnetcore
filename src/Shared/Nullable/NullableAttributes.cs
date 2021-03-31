95250c0271a0c1b828f5f25d680c13bc9c1f85b7
Youssef Victor <youssefvictor00@gmail.com>
2021-03-31 19:49:08 +0200

    Fix file header in HttpAbstractions.slnf - src/Shared/Nullable folder

--- src/Shared/Nullable/NullableAttributes.cs
+++ src/Shared/Nullable/NullableAttributes.cs
@@ -1,7 +1,5 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-// From https://github.com/dotnet/runtime/blob/ef72b95937703e485fdbbb75f3251fedfd1a0ef9/src/libraries/System.Private.CoreLib/src/System/Diagnostics/CodeAnalysis/NullableAttributes.cs
 
 namespace System.Diagnostics.CodeAnalysis
 {
