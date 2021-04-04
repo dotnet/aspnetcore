3d27a4485f036b15d13170827202a130d9918add
martincostello <martin@martincostello.com>
2021-04-04 19:28:27 +0100

    Make methods static
    Make methods that do not access instance state static.

--- src/Features/JsonPatch/src/Adapters/ObjectAdapter.cs
+++ src/Features/JsonPatch/src/Adapters/ObjectAdapter.cs
@@ -329,7 +329,7 @@ namespace Microsoft.AspNetCore.JsonPatch.Adapters
             }
         }
 
-        private JsonPatchError CreateOperationFailedError(object target, string path, Operation operation, string errorMessage)
+        private static JsonPatchError CreateOperationFailedError(object target, string path, Operation operation, string errorMessage)
         {
             return new JsonPatchError(
                 target,
@@ -337,7 +337,7 @@ namespace Microsoft.AspNetCore.JsonPatch.Adapters
                 errorMessage ?? Resources.FormatCannotPerformOperation(operation.op, path));
         }
 
-        private JsonPatchError CreatePathNotFoundError(object target, string path, Operation operation, string errorMessage)
+        private static JsonPatchError CreatePathNotFoundError(object target, string path, Operation operation, string errorMessage)
         {
             return new JsonPatchError(
                 target,
