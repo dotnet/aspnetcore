2b493c45b9d25c60c5fa1bc1399f37c14c146718
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2021-04-05 18:47:33 +0100

    Ability to deactivate DataAnnotationsValidation dynamically. Fixes #31027 (#31413)

--- src/Components/Forms/src/EditContextDataAnnotationsExtensions.cs
+++ src/Components/Forms/src/EditContextDataAnnotationsExtensions.cs
@@ -16,98 +16,118 @@ namespace Microsoft.AspNetCore.Components.Forms
     /// </summary>
     public static class EditContextDataAnnotationsExtensions
     {
-        private static ConcurrentDictionary<(Type ModelType, string FieldName), PropertyInfo?> _propertyInfoCache
-            = new ConcurrentDictionary<(Type, string), PropertyInfo?>();
-
         /// <summary>
         /// Adds DataAnnotations validation support to the <see cref="EditContext"/>.
         /// </summary>
         /// <param name="editContext">The <see cref="EditContext"/>.</param>
+        [Obsolete("Use " + nameof(EnableDataAnnotationsValidation) + " instead.")]
         public static EditContext AddDataAnnotationsValidation(this EditContext editContext)
         {
-            if (editContext == null)
-            {
-                throw new ArgumentNullException(nameof(editContext));
-            }
-
-            var messages = new ValidationMessageStore(editContext);
-
-            // Perform object-level validation on request
-            editContext.OnValidationRequested +=
-                (sender, eventArgs) => ValidateModel((EditContext)sender!, messages);
-
-            // Perform per-field validation on each field edit
-            editContext.OnFieldChanged +=
-                (sender, eventArgs) => ValidateField(editContext, messages, eventArgs.FieldIdentifier);
-
+            EnableDataAnnotationsValidation(editContext);
             return editContext;
         }
 
-        private static void ValidateModel(EditContext editContext, ValidationMessageStore messages)
+        /// <summary>
+        /// Enables DataAnnotations validation support for the <see cref="EditContext"/>.
+        /// </summary>
+        /// <param name="editContext">The <see cref="EditContext"/>.</param>
+        /// <returns>A disposable object whose disposal will remove DataAnnotations validation support from the <see cref="EditContext"/>.</returns>
+        public static IDisposable EnableDataAnnotationsValidation(this EditContext editContext)
         {
-            var validationContext = new ValidationContext(editContext.Model);
-            var validationResults = new List<ValidationResult>();
-            Validator.TryValidateObject(editContext.Model, validationContext, validationResults, true);
-
-            // Transfer results to the ValidationMessageStore
-            messages.Clear();
-            foreach (var validationResult in validationResults)
-            {
-                if (validationResult == null)
-                {
-                    continue;
-                }
-
-                if (!validationResult.MemberNames.Any())
-                {
-                    messages.Add(new FieldIdentifier(editContext.Model, fieldName: string.Empty), validationResult.ErrorMessage!);
-                    continue;
-                }
-
-                foreach (var memberName in validationResult.MemberNames)
-                {
-                    messages.Add(editContext.Field(memberName), validationResult.ErrorMessage!);
-                }
-            }
-
-            editContext.NotifyValidationStateChanged();
+            return new DataAnnotationsEventSubscriptions(editContext);
         }
 
-        private static void ValidateField(EditContext editContext, ValidationMessageStore messages, in FieldIdentifier fieldIdentifier)
+        private sealed class DataAnnotationsEventSubscriptions : IDisposable
         {
-            if (TryGetValidatableProperty(fieldIdentifier, out var propertyInfo))
+            private static readonly ConcurrentDictionary<(Type ModelType, string FieldName), PropertyInfo?> _propertyInfoCache = new();
+
+            private readonly EditContext _editContext;
+            private readonly ValidationMessageStore _messages;
+
+            public DataAnnotationsEventSubscriptions(EditContext editContext)
             {
-                var propertyValue = propertyInfo.GetValue(fieldIdentifier.Model);
-                var validationContext = new ValidationContext(fieldIdentifier.Model)
+                _editContext = editContext ?? throw new ArgumentNullException(nameof(editContext));
+                _messages = new ValidationMessageStore(_editContext);
+
+                _editContext.OnFieldChanged += OnFieldChanged;
+                _editContext.OnValidationRequested += OnValidationRequested;
+            }
+
+            private void OnFieldChanged(object? sender, FieldChangedEventArgs eventArgs)
+            {
+                var fieldIdentifier = eventArgs.FieldIdentifier;
+                if (TryGetValidatableProperty(fieldIdentifier, out var propertyInfo))
                 {
-                    MemberName = propertyInfo.Name
-                };
-                var results = new List<ValidationResult>();
+                    var propertyValue = propertyInfo.GetValue(fieldIdentifier.Model);
+                    var validationContext = new ValidationContext(fieldIdentifier.Model)
+                    {
+                        MemberName = propertyInfo.Name
+                    };
+                    var results = new List<ValidationResult>();
 
-                Validator.TryValidateProperty(propertyValue, validationContext, results);
-                messages.Clear(fieldIdentifier);
-                messages.Add(fieldIdentifier, results.Select(result => result.ErrorMessage!));
+                    Validator.TryValidateProperty(propertyValue, validationContext, results);
+                    _messages.Clear(fieldIdentifier);
+                    _messages.Add(fieldIdentifier, results.Select(result => result.ErrorMessage!));
 
-                // We have to notify even if there were no messages before and are still no messages now,
-                // because the "state" that changed might be the completion of some async validation task
-                editContext.NotifyValidationStateChanged();
+                    // We have to notify even if there were no messages before and are still no messages now,
+                    // because the "state" that changed might be the completion of some async validation task
+                    _editContext.NotifyValidationStateChanged();
+                }
             }
-        }
 
-        private static bool TryGetValidatableProperty(in FieldIdentifier fieldIdentifier, [NotNullWhen(true)] out PropertyInfo? propertyInfo)
-        {
-            var cacheKey = (ModelType: fieldIdentifier.Model.GetType(), fieldIdentifier.FieldName);
-            if (!_propertyInfoCache.TryGetValue(cacheKey, out propertyInfo))
+            private void OnValidationRequested(object? sender, ValidationRequestedEventArgs e)
             {
-                // DataAnnotations only validates public properties, so that's all we'll look for
-                // If we can't find it, cache 'null' so we don't have to try again next time
-                propertyInfo = cacheKey.ModelType.GetProperty(cacheKey.FieldName);
+                var validationContext = new ValidationContext(_editContext.Model);
+                var validationResults = new List<ValidationResult>();
+                Validator.TryValidateObject(_editContext.Model, validationContext, validationResults, true);
 
-                // No need to lock, because it doesn't matter if we write the same value twice
-                _propertyInfoCache[cacheKey] = propertyInfo;
+                // Transfer results to the ValidationMessageStore
+                _messages.Clear();
+                foreach (var validationResult in validationResults)
+                {
+                    if (validationResult == null)
+                    {
+                        continue;
+                    }
+
+                    if (!validationResult.MemberNames.Any())
+                    {
+                        _messages.Add(new FieldIdentifier(_editContext.Model, fieldName: string.Empty), validationResult.ErrorMessage!);
+                        continue;
+                    }
+
+                    foreach (var memberName in validationResult.MemberNames)
+                    {
+                        _messages.Add(_editContext.Field(memberName), validationResult.ErrorMessage!);
+                    }
+                }
+
+                _editContext.NotifyValidationStateChanged();
             }
 
-            return propertyInfo != null;
+            public void Dispose()
+            {
+                _messages.Clear();
+                _editContext.OnFieldChanged -= OnFieldChanged;
+                _editContext.OnValidationRequested -= OnValidationRequested;
+                _editContext.NotifyValidationStateChanged();
+            }
+
+            private static bool TryGetValidatableProperty(in FieldIdentifier fieldIdentifier, [NotNullWhen(true)] out PropertyInfo? propertyInfo)
+            {
+                var cacheKey = (ModelType: fieldIdentifier.Model.GetType(), fieldIdentifier.FieldName);
+                if (!_propertyInfoCache.TryGetValue(cacheKey, out propertyInfo))
+                {
+                    // DataAnnotations only validates public properties, so that's all we'll look for
+                    // If we can't find it, cache 'null' so we don't have to try again next time
+                    propertyInfo = cacheKey.ModelType.GetProperty(cacheKey.FieldName);
+
+                    // No need to lock, because it doesn't matter if we write the same value twice
+                    _propertyInfoCache[cacheKey] = propertyInfo;
+                }
+
+                return propertyInfo != null;
+            }
         }
     }
 }
