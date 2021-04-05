a566260eceb8b461b1b20a482924c9e78f1de7c9
David Fowler <davidfowl@gmail.com>
2021-04-05 07:52:08 -0700

    Re-use cancellation tokens in the https middleware - These tokens are usually short lived and exist for the purposes of cancelling the handshake.

---
+++ src/Servers/Kestrel/Core/src/Internal/CancellationTokenSourcePool.cs
@@ -0,0 +1,62 @@
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
+{
+    internal class CancellationTokenSourcePool
+    {
+        private const int MaxQueueSize = 1024;
+
+        private readonly ConcurrentQueue<PooledCancellationTokenSource> _queue = new();
+        private int _count;
+
+        public PooledCancellationTokenSource Rent()
+        {
+            if (_queue.TryDequeue(out var cts))
+            {
+                Interlocked.Decrement(ref _count);
+                return cts;
+            }
+            return new PooledCancellationTokenSource(this);
+        }
+
+        private bool Return(PooledCancellationTokenSource cts)
+        {
+            // This counting isn't accurate, but it's good enough for what we need to avoid using _queue.Count which could be expensive
+            if (!cts.TryReset() || Interlocked.Increment(ref _count) > MaxQueueSize)
+            {
+                Interlocked.Decrement(ref _count);
+                return false;
+            }
+
+            _queue.Enqueue(cts);
+            return true;
+        }
+
+        /// <summary>
+        /// A <see cref="CancellationTokenSource"/> with a back pointer to the pool it came from.
+        /// Dispose will return it to the pool.
+        /// </summary>
+        public class PooledCancellationTokenSource : CancellationTokenSource
+        {
+            private readonly CancellationTokenSourcePool _pool;
+
+            public PooledCancellationTokenSource(CancellationTokenSourcePool pool)
+            {
+                _pool = pool;
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                if (disposing)
+                {
+                    // If we failed to return to the pool then dispose
+                    if (!_pool.Return(this))
+                    {
+                        base.Dispose(disposing);
+                    }
+                }
+            }
+        }
+    }
+}
