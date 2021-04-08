08531c454b4cca656dedec05afb04b8d010f5b08
Justin Kotalik <jukotali@microsoft.com>
2021-04-08 11:01:08 -0700

    Adding logging

---
+++ src/Middleware/HttpLogging2/src/HttpLoggingBuilderExtensions.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.HttpsPolicy;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    /// <summary>
+    /// Extension methods for the HttpsRedirection middleware.
+    /// </summary>
+    public static class HttpsPolicyBuilderExtensions
+    {
+        /// <summary>
+        /// Adds middleware for redirecting HTTP Requests to HTTPS.
+        /// </summary>
+        /// <param name="app">The <see cref="IApplicationBuilder"/> instance this method extends.</param>
+        /// <returns>The <see cref="IApplicationBuilder"/> for HttpsRedirection.</returns>
+        public static IApplicationBuilder UseHttpsRedirection(this IApplicationBuilder app)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            var serverAddressFeature = app.ServerFeatures.Get<IServerAddressesFeature>();
+            if (serverAddressFeature != null)
+            {
+                app.UseMiddleware<HttpsRedirectionMiddleware>(serverAddressFeature);
+            }
+            else
+            {
+                app.UseMiddleware<HttpsRedirectionMiddleware>();
+            }
+            return app;
+        }
+    }
+}
