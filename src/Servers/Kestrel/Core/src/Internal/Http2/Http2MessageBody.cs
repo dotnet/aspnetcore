a99aea8331456959bdfdd09e72f4caf373d2f885
William Godbe <wigodbe@microsoft.com>
2021-04-12 15:07:48 -0700

    Update Http2MessageBody.cs

--- src/Servers/Kestrel/Core/src/Internal/Http2/Http2MessageBody.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http2/Http2MessageBody.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Diagnostics;
 using System.IO.Pipelines;
 using System.Threading;
 using System.Threading.Tasks;
