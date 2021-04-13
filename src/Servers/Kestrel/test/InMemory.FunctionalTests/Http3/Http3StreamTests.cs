7893fbd7a1d58b27f40c0eec37a1c6522c327b2a
Will Godbe <wigodbe@microsoft.com>
2021-04-13 12:56:50 -0700

    Quarantine failling test

--- src/Servers/Kestrel/test/InMemory.FunctionalTests/Http3/Http3StreamTests.cs
+++ src/Servers/Kestrel/test/InMemory.FunctionalTests/Http3/Http3StreamTests.cs
@@ -628,6 +628,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Tests
         }
 
         [Fact]
+        [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/31777")]
         public async Task ContentLength_Received_NoDataFrames_Reset()
         {
             var headers = new[]
