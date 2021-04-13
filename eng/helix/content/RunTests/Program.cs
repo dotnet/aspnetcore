703c1485b473b88d48cd0683394346c5d0653671
Hao Kung <HaoK@users.noreply.github.com>
2021-04-12 22:06:12 -0700

    Update Program.cs

--- eng/helix/content/RunTests/Program.cs
+++ eng/helix/content/RunTests/Program.cs
@@ -17,11 +17,7 @@ namespace RunTests
                 var keepGoing = runner.SetupEnvironment();
                 if (keepGoing)
                 {
-                    keepGoing = await runner.InstallDotnetDump();
-                }
-                if (keepGoing)
-                {
-                    keepGoing = await runner.InstallAspNetAppIfNeededAsync();
+                    keepGoing = await runner.InstallDotnetToolsAsync();
                 }
 #if INSTALLPLAYWRIGHT
                 if (keepGoing)
