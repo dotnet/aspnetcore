ff492e5ba0e0de96274cf682704325ae973a2c71
Juho Hanhimäki <juho.hanhimaki@arnon.fi>
2021-04-15 09:45:44 +0300

    Improved test name

--- src/Components/Authorization/test/AuthorizeViewTest.cs
+++ src/Components/Authorization/test/AuthorizeViewTest.cs
@@ -293,7 +293,7 @@ namespace Microsoft.AspNetCore.Components.Authorization
         }
 
         [Fact]
-        public void RendersAuthorizingWhenAuthenticationIsCompletedBeforehandAndAuthorizationIsAsync()
+        public void RendersAuthorizingUntilAuthorizationCompletedAsync()
         {
             // Covers https://github.com/dotnet/aspnetcore/pull/31794
             // Arrange
