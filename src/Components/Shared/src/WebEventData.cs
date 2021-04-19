105b951ea8197f3763108d45f0e61f2f4c7283b8
Pranav K <prkrishn@hotmail.com>
2021-04-19 14:31:44 -0700

    Preserve JSON deserialized properties from being trimmed away
    Fixes https://github.com/dotnet/aspnetcore/issues/31950

--- src/Components/Shared/src/WebEventData.cs
+++ src/Components/Shared/src/WebEventData.cs
@@ -79,6 +79,8 @@ namespace Microsoft.AspNetCore.Components.Web
             }
         }
 
+        [DynamicDependency(JsonSerialized, typeof(DataTransfer))]
+        [DynamicDependency(JsonSerialized, typeof(DataTransferItem))]
         private static bool TryDeserializeStandardWebEventArgs(string eventName, string eventArgsJson, [NotNullWhen(true)] out EventArgs? eventArgs)
         {
             // For back-compatibility, we recognize the built-in list of web event names and hard-code
