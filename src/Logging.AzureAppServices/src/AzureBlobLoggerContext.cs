723b98fe9c720d373ee84de2655478774a699a82
EP2018 <cs@yogigrantz.com>
2021-04-19 18:26:19 -0700

    Added AzureBlobLoggerContext
    Added AzureBlobLoggerContext as per discussion in https://github.com/dotnet/aspnetcore/pull/27383#issuecomment-818007583

---
+++ src/Logging.AzureAppServices/src/AzureBlobLoggerContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.Extensions.Logging.AzureAppServices
+{
+    public readonly struct AzureBlobLoggerContext
+    {
+        public AzureBlobLoggerContext(string appName, string identifier, DateTimeOfset timestamp);
+        public string AppName { get; }
+        public string Identifier { get; }
+        public DateTimeOffset Timestamp { get; }
+    }
+}
