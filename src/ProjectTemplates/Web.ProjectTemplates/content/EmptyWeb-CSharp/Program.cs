ddaf769b17eba6725e564ed90280c8693ae1b40a
Stephen Halter <halter73@gmail.com>
2021-04-20 15:05:47 -0700

    Add UseDeveloperExceptionPage() back

--- src/ProjectTemplates/Web.ProjectTemplates/content/EmptyWeb-CSharp/Program.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/EmptyWeb-CSharp/Program.cs
@@ -1,7 +1,13 @@
 using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
 
 await using var app = WebApplication.Create(args);
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.MapGet("/", (Func<string>)(() => "Hello World!"));
 
