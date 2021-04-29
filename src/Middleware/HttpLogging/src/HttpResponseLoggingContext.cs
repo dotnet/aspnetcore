be1b3a23be810703bf220daca3a01d21ba9956b6
Justin Kotalik <jukotali@microsoft.com>
2021-04-29 11:07:27 -0700

    headers

--- src/Middleware/HttpLogging/src/HttpResponseLoggingContext.cs
+++ src/Middleware/HttpLogging/src/HttpResponseLoggingContext.cs
@@ -1,8 +1,7 @@
-using System;
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
 namespace Microsoft.AspNetCore.HttpLogging
