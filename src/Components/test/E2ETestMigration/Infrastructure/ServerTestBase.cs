cb26b17073220e93fe803675ff46296e76a750dd
Hao Kung <HaoK@users.noreply.github.com>
2021-05-05 08:57:30 -0700

    Make tests work, add bind test example

---
+++ src/Components/test/E2ETestMigration/Infrastructure/ServerTestBase.cs
@@ -0,0 +1,50 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.BrowserTesting;
+using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
+using Microsoft.AspNetCore.Testing;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.AspNetCore.Components.E2ETest.Infrastructure
+{
+    public abstract class ServerTestBase<TServerFixture>
+        : PlaywrightTestBase,
+        IClassFixture<TServerFixture>
+        where TServerFixture: ServerFixture
+    {
+        public string ServerPathBase => "/subdir";
+
+        protected readonly TServerFixture _serverFixture;
+
+        public ServerTestBase(
+            TServerFixture serverFixture,
+            ITestOutputHelper output)
+            : base(output)
+        {
+            _serverFixture = serverFixture;
+        }
+
+        public void Navigate(string relativeUrl, bool noReload = false)
+        {
+            //Browser.Navigate(_serverFixture.RootUri, relativeUrl, noReload);
+        }
+
+        protected override async Task InitializeCoreAsync(TestContext context)
+        {
+            await base.InitializeCoreAsync(context);
+
+            if (BrowserManager.IsAvailable(BrowserKind.Chromium))
+            {
+                await using var browser = await BrowserManager.GetBrowserInstance(BrowserKind.Chromium, BrowserContextInfo);
+                var page = await browser.NewPageAsync();
+                // Clear logs - we check these during tests in some cases.
+                // Make sure each test starts clean.
+                await page.EvaluateAsync("console.clear()");
+                await page.CloseAsync();
+            }
+        }
+    }
+}
