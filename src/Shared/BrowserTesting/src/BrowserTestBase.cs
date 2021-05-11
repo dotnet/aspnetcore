6c171b6e6be310a0c2b9021734d906a26e46d350
Hao Kung <HaoK@users.noreply.github.com>
2021-05-11 10:45:27 -0700

    Move playwright stuff to BrowserTestBase

--- src/Shared/BrowserTesting/src/BrowserTestBase.cs
+++ src/Shared/BrowserTesting/src/BrowserTestBase.cs
@@ -5,40 +5,35 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.BrowserTesting;
 using Microsoft.AspNetCore.Testing;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Testing;
 using PlaywrightSharp;
 using Xunit;
 using Xunit.Abstractions;
-using Xunit.Sdk;
 
-namespace Microsoft.AspNetCore.Components.E2ETest.Infrastructure
+namespace Microsoft.AspNetCore.BrowserTesting
 {
-    public class PlaywrightTestBase : LoggedTest, IAsyncLifetime
+    public class BrowserTestBase : LoggedTest, IAsyncLifetime
     {
         private static readonly bool _isCIEnvironment =
             !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("ContinuousIntegrationBuild"));
 
-        public PlaywrightTestBase(ITestOutputHelper output) : base(output) { }
+        public BrowserTestBase(ITestOutputHelper output) : base(output) { }
 
-        protected override async Task InitializeCoreAsync(TestContext context)
+        protected async override Task InitializeCoreAsync(TestContext context)
         {
             BrowserManager = await BrowserManager.CreateAsync(CreateConfiguration(), LoggerFactory);
             BrowserContextInfo = new ContextInformation(LoggerFactory);
-            _output = new TestOutputLogger(Logger);
+            _output = new BrowserTestOutputLogger(Logger);
         }
 
         public Task InitializeAsync() => Task.CompletedTask;
 
         private static IConfiguration CreateConfiguration()
         {
-            var basePath = Path.GetDirectoryName(typeof(PlaywrightTestBase).Assembly.Location);
+            var basePath = Path.GetDirectoryName(typeof(BrowserTestBase).Assembly.Location);
             var os = Environment.OSVersion.Platform switch
             {
                 PlatformID.Win32NT => "win",
@@ -74,7 +69,7 @@ namespace Microsoft.AspNetCore.Components.E2ETest.Infrastructure
             {
                 if (_output == null)
                 {
-                    _output = new TestOutputLogger(Logger);
+                    _output = new BrowserTestOutputLogger(Logger);
                 }
                 return _output;
             }
@@ -82,22 +77,5 @@ namespace Microsoft.AspNetCore.Components.E2ETest.Infrastructure
 
         public ContextInformation BrowserContextInfo { get; protected set; }
         public BrowserManager BrowserManager { get; private set; }
-
-        protected async Task MountTestComponentAsync<TComponent>(IPage page)
-        {
-            var componentType = typeof(TComponent);
-            var componentTypeName = componentType.Assembly == typeof(BasicTestApp.Program).Assembly ?
-                componentType.FullName :
-                componentType.AssemblyQualifiedName;
-            var testSelector = await page.WaitForSelectorAsync("#test-selector > select");
-
-            Output.WriteLine("Selecting test: " + componentTypeName);
-
-            var option = $"#test-selector > select > option[value='{componentTypeName}']";
-            var selected = await page.SelectOptionAsync("#test-selector > select", componentTypeName);
-            Assert.True(selected.Length == 1);
-            Assert.Equal(componentTypeName, selected.First());
-        }
-
     }
 }
