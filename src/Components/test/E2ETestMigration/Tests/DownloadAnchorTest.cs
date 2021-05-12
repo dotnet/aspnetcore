ff9617ac0b7eee154c803df12384585bd1ea999a
Safia Abdalla <safia@microsoft.com>
2021-05-12 08:57:27 -0700

    Migrate DownloadFileFromAnchor test to Playwright (#32499)
    * Migrate DownloadFileFromAnchor test to Playwright
    * Unquarantine migrated test

---
+++ src/Components/test/E2ETestMigration/Tests/DownloadAnchorTest.cs
@@ -0,0 +1,66 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using BasicTestApp;
+using BasicTestApp.RouterTest;
+using Microsoft.AspNetCore.BrowserTesting;
+using Microsoft.AspNetCore.Components.E2ETest.Infrastructure;
+using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
+using Microsoft.AspNetCore.Testing;
+using Moq;
+using PlaywrightSharp;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.AspNetCore.Components.E2ETest.Tests
+{
+    public class DownloadAnchorTest
+        : ServerTestBase<ToggleExecutionModeServerFixture<Program>>
+    {
+        public DownloadAnchorTest(
+            ToggleExecutionModeServerFixture<Program> serverFixture,
+            ITestOutputHelper output)
+            : base(serverFixture, output)
+        {            
+        }
+
+        private IPage _page;
+        private IBrowserContext _browser;
+
+        protected override async Task InitializeCoreAsync(TestContext context)
+        {
+            await base.InitializeCoreAsync(context);
+
+            _browser = await BrowserManager.GetBrowserInstance(BrowserKind.Chromium, BrowserContextInfo);
+            var page = await _browser.NewPageAsync();
+            var url = _serverFixture.RootUri + "subdir";
+            var response = await page.GoToAsync(url);
+
+            await MountTestComponentAsync<TestRouter>(page);
+            _page = page;
+        }
+
+        [Fact]
+        public async Task DownloadFileFromAnchor()
+        {
+            // Arrange
+            var initialUrl = _page.Url;
+            var downloadTask = _page.WaitForEventAsync(PageEvent.Download);
+
+            // Act
+            await Task.WhenAll(
+                downloadTask,
+                _page.ClickAsync("a[download]"));
+
+            // Assert URL should still be same as before click
+            Assert.Equal(initialUrl, _page.Url);
+
+            // Assert that the resource was downloaded            
+            var download = downloadTask.Result.Download;
+            Assert.Equal($"{_serverFixture.RootUri}subdir/images/blazor_logo_1000x.png", download.Url);
+            Assert.Equal("blazor_logo_1000x.png", download.SuggestedFilename);
+        }
+    }
+}
