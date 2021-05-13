da53f0fc51bde62f7a0c894609fdda6595e3f5c4
Safia Abdalla <safia@safia.rocks>
2021-05-13 15:02:27 -0700

    Fix obsolete types and update SDK again

--- eng/tools/RepoTasks/Uuid.cs
+++ eng/tools/RepoTasks/Uuid.cs
@@ -42,7 +42,7 @@ namespace RepoTasks
             Buffer.BlockCopy(nameBytes, 0, hashBuffer, 16, nameBytes.Length);
             byte[] hash;
 
-            using (SHA256 sha256 = new SHA256Managed())
+            using (SHA256 sha256 = SHA256.Create())
             {
                 hash = sha256.ComputeHash(hashBuffer);
             }
