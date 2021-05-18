aef05c813dc58171fbe89b6f118c8e9191e584f5
Tanay Parikh <TanayParikh@users.noreply.github.com>
2021-05-18 12:30:07 -0700

    Add using

--- src/Components/test/E2ETest/Tests/SignalRClientTest.cs
+++ src/Components/test/E2ETest/Tests/SignalRClientTest.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using BasicTestApp;
 using Microsoft.AspNetCore.Components.E2ETest.Infrastructure;
 using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
