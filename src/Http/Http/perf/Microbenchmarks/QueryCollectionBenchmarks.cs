446855954299e67b3bb6974aae34896926fbf01c
Günther Foidl <gue@korporal.at>
2021-05-18 20:57:12 +0200

    Updated benchmarks

--- src/Http/Http/perf/Microbenchmarks/QueryCollectionBenchmarks.cs
+++ src/Http/Http/perf/Microbenchmarks/QueryCollectionBenchmarks.cs
@@ -2,44 +2,73 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Primitives;
 using static Microsoft.AspNetCore.Http.Features.QueryFeature;
 
 namespace Microsoft.AspNetCore.Http
 {
+    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+    [CategoriesColumn]
     public class QueryCollectionBenchmarks
     {
         private string _queryString;
         private string _singleValue;
+        private string _singleValueWithPlus;
 
         [IterationSetup]
         public void Setup()
         {
             _queryString = "?key1=value1&key2=value2&key3=value3&key4=&key5=";
             _singleValue = "?key1=value1";
+            _singleValueWithPlus = "?key1=value1+value2+value3";
         }
 
-        [Benchmark]
+        [Benchmark(Description = "ParseNew")]
+        [BenchmarkCategory("QueryString")]
         public void ParseNew()
         {
-            var dict = QueryFeature.ParseNullableQueryInternal(_queryString);
+            _ = QueryFeature.ParseNullableQueryInternal(_queryString);
         }
 
-        [Benchmark]
+        [Benchmark(Description = "ParseNew")]
+        [BenchmarkCategory("Single")]
         public void ParseNewSingle()
         {
-            var dict = QueryFeature.ParseNullableQueryInternal(_singleValue);
+            _ = QueryFeature.ParseNullableQueryInternal(_singleValue);
         }
 
-        [Benchmark]
-        public void Parse()
+        [Benchmark(Description = "ParseNew")]
+        [BenchmarkCategory("SingleWithPlus")]
+        public void ParseNewSingleWithPlus()
         {
-            var dict = QueryHelpers.ParseNullableQuery(_queryString);
+            _ = QueryFeature.ParseNullableQueryInternal(_singleValueWithPlus);
+        }
+
+        [Benchmark(Description = "QueryHelpersParse")]
+        [BenchmarkCategory("QueryString")]
+        public void QueryHelpersParse()
+        {
+            _ = QueryHelpers.ParseNullableQuery(_queryString);
+        }
+
+        [Benchmark(Description = "QueryHelpersParse")]
+        [BenchmarkCategory("Single")]
+        public void QueryHelpersParseSingle()
+        {
+            _ = QueryHelpers.ParseNullableQuery(_singleValue);
+        }
+
+        [Benchmark(Description = "QueryHelpersParse")]
+        [BenchmarkCategory("SingleWithPlus")]
+        public void QueryHelpersParseSingleWithPlus()
+        {
+            _ = QueryHelpers.ParseNullableQuery(_singleValueWithPlus);
         }
 
         [Benchmark]
+        [BenchmarkCategory("Constructor")]
         public void Constructor()
         {
             var dict = new KvpAccumulator();
