225a264ce92e54ce8125952b62d0cc2cce59546a
Pranav K <prkrishn@hotmail.com>
2021-05-20 12:47:28 -0700

    Reduce some allocations in Razor
    Nothing particularly dramatic, just some low hanging fruits that remove string allocations
    in TagHelperMatchingConvention. VS ships with a copy of System.Memory, so we're not the first to
    introduce this dependency over there.
    
    Some before and after benchmarks because there was a convenient scenario set up for this:
    
    // * Summary *
    
    BenchmarkDotNet=v0.12.1.1466-nightly, OS=Windows 10.0.19043
    Intel Core i7-8700 CPU 3.20GHz (Coffee Lake), 1 CPU, 12 logical and 6 physical cores
    .NET SDK=6.0.100-preview.5.21230.2
      [Host]     : .NET Core 2.1.27 (CoreCLR 4.6.29916.01, CoreFX 4.6.29916.03), X64 RyuJIT
      Job-GISAGT : .NET Core 2.1.27 (CoreCLR 4.6.29916.01, CoreFX 4.6.29916.03), X64 RyuJIT
    
    Server=True  Toolchain=.NET Core 2.1  RunStrategy=Throughput
    
    Before:
    |                                  Method |        Mean |     Error |    StdDev |     Op/s |  Gen 0 |  Gen 1 |  Gen 2 | Allocated |
    |---------------------------------------- |------------:|----------:|----------:|---------:|-------:|-------:|-------:|----------:|
    |      'TagHelper Design Time Processing' | 2,313.36 us | 25.477 us | 21.275 us |    432.3 | 7.8125 | 3.9063 | 3.9063 |    986 KB |
    | 'TagHelper Component Directive Parsing' |    93.52 us |  1.408 us |  1.100 us | 10,693.5 |      - |      - |      - |      3 KB |
    
    Ater:
    |                                  Method |        Mean |     Error |    StdDev |     Op/s |  Gen 0 |  Gen 1 |  Gen 2 | Allocated |
    |---------------------------------------- |------------:|----------:|----------:|---------:|-------:|-------:|-------:|----------:|
    |      'TagHelper Design Time Processing' | 2,205.42 us | 28.353 us | 26.522 us |    453.4 | 7.8125 | 3.9063 | 3.9063 | 963 KB |
    | 'TagHelper Component Directive Parsing' |    85.89 us |  0.724 us |  0.565 us | 11,643.1 |      - |      - |      - | 288 B |
    
    Also some minor cleanup of Linq operations and null-ref checks in internal code

--- src/Razor/Microsoft.AspNetCore.Razor.Language/src/Legacy/LegacySyntaxNodeExtensions.cs
+++ src/Razor/Microsoft.AspNetCore.Razor.Language/src/Legacy/LegacySyntaxNodeExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -48,11 +48,6 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
 
         public static SpanContext GetSpanContext(this SyntaxNode node)
         {
-            if (node == null)
-            {
-                throw new ArgumentNullException(nameof(node));
-            }
-
             var context = node.GetAnnotationValue(SyntaxConstants.SpanContextKind);
 
             return context is SpanContext ? (SpanContext)context : null;
