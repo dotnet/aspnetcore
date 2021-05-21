6401e3595174f7c9c63f1a5fd8da7c6c361ddfc6
Hao Kung <HaoK@users.noreply.github.com>
2021-05-21 16:36:36 -0700

    Fix compilation

--- src/ProjectTemplates/BlazorTemplates.Tests/PlaywrightFixture.cs
+++ src/ProjectTemplates/BlazorTemplates.Tests/PlaywrightFixture.cs
@@ -19,19 +19,18 @@ namespace ProjectTemplates.Tests.Infrastructure
     public class PlaywrightFixture<TTestAssemblyType> : IAsyncLifetime
     {
         private static readonly bool _isCIEnvironment =
-            !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("ContinuousIntegrationBuild"));
+            !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("ContinuousIntegrationBuild")) ||
+            !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("Helix"));
 
         private readonly IMessageSink _diagnosticsMessageSink;
-        private readonly IConfiguration _configuration;
+        private static BrowserManagerConfiguration _config = new BrowserManagerConfiguration(CreateConfiguration(typeof(TTestAssemblyType).Assembly));
 
         public PlaywrightFixture(IMessageSink diagnosticsMessageSink)
         {
             _diagnosticsMessageSink = diagnosticsMessageSink;
-
-            _configuration = CreateConfiguration(typeof(TTestAssemblyType).Assembly);
         }
 
-        private IConfiguration CreateConfiguration(Assembly assembly)
+        private static IConfiguration CreateConfiguration(Assembly assembly)
         {
             var basePath = Path.GetDirectoryName(assembly.Location);
             var os = Environment.OSVersion.Platform switch
@@ -65,7 +64,7 @@ namespace ProjectTemplates.Tests.Infrastructure
             var sink = new TestSink();
             sink.MessageLogged += LogBrowserManagerMessage;
             var factory = new TestLoggerFactory(sink, enabled: true);
-            BrowserManager = await BrowserManager.CreateAsync(_configuration, factory);
+            BrowserManager = await BrowserManager.CreateAsync(_config, factory);
         }
 
         private void LogBrowserManagerMessage(WriteContext context)
