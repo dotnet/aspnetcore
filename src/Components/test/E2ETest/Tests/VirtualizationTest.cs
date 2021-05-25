e1176e0f958d3c1e2a8852156874c9bf856f6d6a
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2021-05-25 14:36:04 +0100

    Update src/Components/test/E2ETest/Tests/VirtualizationTest.cs

--- src/Components/test/E2ETest/Tests/VirtualizationTest.cs
+++ src/Components/test/E2ETest/Tests/VirtualizationTest.cs
@@ -169,6 +169,7 @@ namespace Microsoft.AspNetCore.Components.E2ETest.Tests
         }
 
         [Fact]
+        [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/25929")]
         public void CancelsOutdatedRefreshes_Async()
         {
             Browser.MountTestComponent<VirtualizationComponent>();
