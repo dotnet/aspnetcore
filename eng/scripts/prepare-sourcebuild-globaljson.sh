07c000605ce033da677ea0b1319e09e84e842605
= <johluo@microsoft.com>
2021-06-02 10:57:20 -0700

    Modify source build global json script

--- eng/scripts/prepare-sourcebuild-globaljson.sh
+++ eng/scripts/prepare-sourcebuild-globaljson.sh
@@ -2,7 +2,21 @@
 
 set -euo pipefail
 
-jq \
-    '.tools.runtimes = {.tools.runtimes | .dotnet = ."dotnet/x64" | del(."dotnet/x64") | del(."dotnet/x86")}' \
-    global.json > global.json.swap
+while IFS= read -r line
+do
+    # Remove dotnet/x64 runtimes
+    if [[ "$line" == *"dotnet/x86"* ]]
+    then
+        while IFS= read -r removeLine
+        do
+            if [[ "$removeLine" == *"]"* ]]
+            then
+                break
+            fi
+        done
+    else
+        # Change dotnet/x64 to dotnet
+        echo "${line/dotnet\/x64/dotnet}"
+    fi
+done < global.json > global.json.swap
 mv global.json.swap global.json
