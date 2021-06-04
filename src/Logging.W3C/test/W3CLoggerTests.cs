00d3ea5f3cb23cc633f03b8f12dc9aa6495a8229
Will Godbe <wigodbe@microsoft.com>
2021-06-04 13:27:07 -0700

    one more test

--- src/Logging.W3C/test/W3CLoggerTests.cs
+++ src/Logging.W3C/test/W3CLoggerTests.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
 using Xunit;
 
 namespace Microsoft.Extensions.Logging.W3C.Tests
@@ -35,6 +37,35 @@ namespace Microsoft.Extensions.Logging.W3C.Tests
         }
 
         [Fact]
+        public void DoesNotLogOnDifferentCategory()
+        {
+            using var provider = new TestW3CLoggerProvider(TempPath);
+            var logger = provider.CreateLogger("Microsoft.AspNetCore.Bogus");
+            var state = new List<KeyValuePair<string, object>>();
+            state.Add(new KeyValuePair<string, object>(nameof(DateTime), _timestampOne));
+
+            logger.Log(LogLevel.Information, new EventId(7, "W3CLog"), state, null, (st, ex) => null);
+            Assert.Null(provider.GetLogFileFullName("Microsoft.AspNetCore.Bogus"));
+        }
+
+        [Fact]
+        public void DoesNotLogOnDifferentEventId()
+        {
+            string fileName;
+            using (var provider = new TestW3CLoggerProvider(TempPath))
+            {
+                var logger = provider.CreateLogger("Microsoft.AspNetCore.W3CLogging");
+                var state = new List<KeyValuePair<string, object>>();
+                state.Add(new KeyValuePair<string, object>(nameof(DateTime), _timestampOne));
+
+                logger.Log(LogLevel.Information, new EventId(2, "Bogus"), state, null, (st, ex) => null);
+                fileName = provider.GetLogFileFullName("Microsoft.AspNetCore.W3CLogging");
+            }
+            var lines = File.ReadAllLines(fileName);
+            Assert.Empty(lines);
+        }
+
+        [Fact]
         public void WritesToTextFile()
         {
             string fileName;
@@ -60,6 +91,34 @@ namespace Microsoft.Extensions.Logging.W3C.Tests
             Assert.StartsWith("2021-01-02 03:04:05 ", lines[3]);
             // Assert that the log's time-taken is within 10 seconds of DateTime.Now minus our arbitary start time (01/02/21 at 3:04:05)
             Assert.True(now.Subtract(_timestampOne).TotalSeconds - Convert.ToDouble(lines[3].Substring(20), CultureInfo.InvariantCulture) < 10);
+        }
+
+        [Fact]
+        public void HandlesNullValues()
+        {
+            string fileName;
+            using (var provider = new TestW3CLoggerProvider(TempPath, W3CLoggingFields.UriQuery | W3CLoggingFields.Host | W3CLoggingFields.ProtocolStatus))
+            {
+                var logger = provider.CreateLogger("Microsoft.AspNetCore.W3CLogging");
+                var state = new List<KeyValuePair<string, object>>();
+                state.Add(new KeyValuePair<string, object>(nameof(HttpRequest.QueryString), null));
+                state.Add(new KeyValuePair<string, object>(nameof(HeaderNames.Host), null));
+                state.Add(new KeyValuePair<string, object>(nameof(HttpResponse.StatusCode), null));
+
+                logger.Log(LogLevel.Information, new EventId(7, "W3CLog"), state, null, (st, ex) => null);
+                fileName = provider.GetLogFileFullName("Microsoft.AspNetCore.W3CLogging");
+            }
+
+            var lines = File.ReadAllLines(fileName);
+            Assert.Equal("#Version: 1.0", lines[0]);
+            Assert.StartsWith("#Start-Date: ", lines[1]);
+            var startDate = DateTime.Parse(lines[1].Substring(13), CultureInfo.InvariantCulture);
+            // Assert that the log was written in the last 10 seconds
+            var now = DateTime.Now;
+            Assert.True(now.Subtract(startDate).TotalSeconds < 10);
+
+            Assert.Equal("#Fields: cs-uri-query sc-status cs-host", lines[2]);
+            Assert.Equal("- - -", lines[3]);
         }
     }
 }
