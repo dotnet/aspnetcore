681138f77bf9e86e5a78b28257b755f2dfc5565f
Stephen Halter <halter73@gmail.com>
2021-06-08 16:19:25 -0700

    AddMethodInfoApiExplorerServices

--- src/Mvc/Mvc.ApiExplorer/src/DependencyInjection/EndpointMethodInfoApiExplorerServiceCollectionExtensions.cs
+++ src/Mvc/Mvc.ApiExplorer/src/DependencyInjection/EndpointMethodInfoApiExplorerServiceCollectionExtensions.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Microsoft.AspNetCore.Mvc.ApiExplorer.DependencyInjection
 {
-    class EndpointMethodInfoApiExplorerServiceCollectionExtensions
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class EndpointMethodInfoApiExplorerServiceCollectionExtensions
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddMethodInfoApiExplorerServices(this IServiceCollection services)
+        {
+            // Try to add default services in case MVC services aren't added.
+            services.TryAddSingleton<IActionDescriptorCollectionProvider, DefaultActionDescriptorCollectionProvider>();
+            services.TryAddSingleton<IApiDescriptionGroupCollectionProvider, ApiDescriptionGroupCollectionProvider>();
+
+            services.TryAddEnumerable(
+                ServiceDescriptor.Transient<IApiDescriptionProvider, EndpointMethodInfoApiDescriptionProvider>());
+        }
     }
 }
