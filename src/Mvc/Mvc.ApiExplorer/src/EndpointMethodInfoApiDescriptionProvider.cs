804fd15843d7a2a84b0bf7354244e1dedf42beca
Stephen Halter <halter73@gmail.com>
2021-06-08 13:30:48 -0700

    WIP

--- src/Mvc/Mvc.ApiExplorer/src/EndpointMethodInfoApiDescriptionProvider.cs
+++ src/Mvc/Mvc.ApiExplorer/src/EndpointMethodInfoApiDescriptionProvider.cs
@@ -1,11 +1,14 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Collections.Generic;
+using System;
 using System.Reflection;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Routing.Patterns;
+using Microsoft.Extensions.Internal;
 
 namespace Microsoft.AspNetCore.Mvc.ApiExplorer
 {
@@ -49,7 +52,7 @@ namespace Microsoft.AspNetCore.Mvc.ApiExplorer
                 RelativePath = pattern.ToString(),
                 ActionDescriptor = new ActionDescriptor
                 {
-                    RouteValues = new Dictionary<string, string?>
+                    RouteValues =
                     {
                         // Swagger uses this to group endpoints together.
                         // For now, put all endpoints configured with Map(Delegate) together.
@@ -59,10 +62,59 @@ namespace Microsoft.AspNetCore.Mvc.ApiExplorer
                 },
             };
 
-            //var returnType = actionMethodInfo.ReturnType.
-            //if (actionMethodInfo.)
+            var responseType = actionMethodInfo.ReturnType;
+
+            if (AwaitableInfo.IsTypeAwaitable(responseType, out var awaitableInfo))
+            {
+                responseType = awaitableInfo.ResultType;
+            }
+
+            if (CreateApiResponseType(responseType) is { } apiResponseType)
+            {
+                apiDescription.SupportedResponseTypes.Add(apiResponseType);
+            }
 
             return apiDescription;
+        }
+
+        private static ApiResponseType? CreateApiResponseType(Type responseType)
+        {
+            if (typeof(IResult).IsAssignableFrom(responseType))
+            {
+                // Can't determine anything about IResults yet. IResult<T> could help here.
+                // REVIEW: Is there any value in returning an ApiResponseType with StatusCode = 200 and that's it?
+                return null;
+            }
+
+            if (responseType == typeof(void))
+            {
+                return new ApiResponseType
+                {
+                    ModelMetadata = new EndpointMethodInfoModelMetadata(ModelMetadataIdentity.ForType(typeof(void))),
+                    StatusCode = 200,
+                };
+            }
+
+            if (responseType == typeof(string))
+            {
+                // This uses HttpResponse.WriteAsync(string) method which doesn't set a content type. It could be anything,
+                // but I think "text/plain" is a reasonable assumption.
+
+                return new ApiResponseType
+                {
+                    ApiResponseFormats = { new ApiResponseFormat { MediaType = "text/plain" } },
+                    ModelMetadata = new EndpointMethodInfoModelMetadata(ModelMetadataIdentity.ForType(typeof(string))),
+                    StatusCode = 200,
+                };
+            }
+
+            // Everything else is written using HttpResponse.WriteAsJsonAsync<TValue>(T).
+            return new ApiResponseType
+            {
+                ApiResponseFormats = { new ApiResponseFormat { MediaType = "application/json" } },
+                ModelMetadata = new EndpointMethodInfoModelMetadata(ModelMetadataIdentity.ForType(responseType)),
+                StatusCode = 200,
+            };
         }
     }
 }
