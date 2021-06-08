01e31bff21b64f02f0f935ceb2ad373c65cbd566
Stephen Halter <halter73@gmail.com>
2021-06-08 13:30:48 -0700

    WIP

---
+++ src/Mvc/Mvc.ApiExplorer/src/DependencyInjection/EndpointMethodInfoApiExplorerServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Mvc.ApiExplorer.DependencyInjection
+{
+    class EndpointMethodInfoApiExplorerServiceCollectionExtensions
+    {
+    }
+}
