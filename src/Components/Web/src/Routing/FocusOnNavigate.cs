e97c7be79735e692085cdc1d26455f75343f0ef2
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2021-06-11 13:15:13 +0100

    Add FocusOnNavigate component

---
+++ src/Components/Web/src/Routing/FocusOnNavigate.cs
@@ -0,0 +1,65 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace Microsoft.AspNetCore.Components.Routing
+{
+    /// <summary>
+    /// After navigating from one page to another, sets focus to an element
+    /// matching a CSS selector. This can be used to build an accessible
+    /// navigation system compatible with screen readers.
+    /// </summary>
+    public class FocusOnNavigate : ComponentBase
+    {
+        private Type? _lastNavigatedPageType;
+        private bool _focusAfterRender;
+
+        [Inject] private IJSRuntime JSRuntime { get; set; } = default!;
+
+        /// <summary>
+        /// Gets or sets the route data. This can be obtained from an enclosing
+        /// <see cref="Router"/> component.
+        /// </summary>
+        [Parameter] public RouteData? RouteData { get; set; } // TODO: [EditorRequired]
+
+        /// <summary>
+        /// Gets or sets a CSS selector describing the element to be focused after
+        /// navigation between pages.
+        /// </summary>
+        [Parameter] public string? Selector { get; set; } // TODO: [EditorRequired]
+
+        /// <inheritdoc />
+        protected override void OnParametersSet()
+        {
+            if (RouteData is null)
+            {
+                throw new InvalidOperationException($"{nameof(FocusOnNavigate)} requires a non-null value for the parameter '{nameof(RouteData)}'.");
+            }
+
+            if (string.IsNullOrWhiteSpace(Selector))
+            {
+                throw new InvalidOperationException($"{nameof(FocusOnNavigate)} requires a nonempty value for the parameter '{nameof(Selector)}'.");
+            }
+
+            // We focus whenever the page type changes, including to or from 'null'
+            if (RouteData!.PageType != _lastNavigatedPageType)
+            {
+                _lastNavigatedPageType = RouteData!.PageType;
+                _focusAfterRender = true;
+            }
+        }
+
+        /// <inheritdoc />
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (_focusAfterRender)
+            {
+                _focusAfterRender = false;
+                await JSRuntime.InvokeVoidAsync(DomWrapperInterop.FocusBySelector, Selector);
+            }
+        }
+    }
+}
