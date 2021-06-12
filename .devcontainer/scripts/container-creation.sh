a2ab1002d34c806cccfe8efefdf66ff1f7244d0c
Safia Abdalla <safia@microsoft.com>
2021-06-11 18:36:06 -0700

    Use only-build-repo-tasks to install SDK

--- .devcontainer/scripts/container-creation.sh
+++ .devcontainer/scripts/container-creation.sh
@@ -5,5 +5,5 @@ set -e
 # GitHub doesn't pull in submodules by default when mounting
 # the repo directory on the container so we do this post-create
 git submodule update --init --recursive
-# Run restore to install the SDK before the app starts
-./restore.sh
+# Install SDK and tool dependencies before container starts
+./eng/build.sh --only-build-repo-tasks
