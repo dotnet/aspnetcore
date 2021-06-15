3199df3807e864ebc11e498bdbd5e2d040b1a5d5
Stephen Halter <halter73@gmail.com>
2021-06-15 12:59:28 -0700

    Add request/response format tests

--- src/Mvc/Mvc.ApiExplorer/test/EndpointMetadataApiDescriptionProviderTest.cs
+++ src/Mvc/Mvc.ApiExplorer/test/EndpointMetadataApiDescriptionProviderTest.cs
@@ -18,32 +18,24 @@ namespace Microsoft.AspNetCore.Mvc.ApiExplorer
         [Fact]
         public void ApiDescription_MultipleCreatedForMultipleHttpMethods()
         {
-            Action action = () => { };
-            var httpMethods = new string[] { "FOO", "BAR" };
+            var apiDescriptions = GetApiDescriptions(() => { }, "/", new string[] { "FOO", "BAR" });
 
-            var apiDescriptions = GetApiDescriptions(action.Method, httpMethods);
-
-            Assert.Equal(httpMethods.Length, apiDescriptions.Count);
+            Assert.Equal(2, apiDescriptions.Count);
         }
 
         [Fact]
         public void ApiDescription_NotCreatedIfNoHttpMethods()
         {
-            Action action = () => { };
-            var emptyHttpMethods = new string[0];
-
-            var apiDescriptions = GetApiDescriptions(action.Method, emptyHttpMethods);
+            var apiDescriptions = GetApiDescriptions(() => { }, "/", Array.Empty<string>());
 
             Assert.Empty(apiDescriptions);
         }
 
-
         [Fact]
         public void ApiDescription_UsesDeclaringTypeAsControllerName()
         {
-            var apiDescriptions = GetApiDescriptions(((Action)TestAction).Method);
+            var apiDescription = GetApiDescription(TestAction);
 
-            var apiDescription = Assert.Single(apiDescriptions);
             var declaringTypeName = typeof(EndpointMetadataApiDescriptionProviderTest).Name;
             Assert.Equal(declaringTypeName, apiDescription.ActionDescriptor.RouteValues["controller"]);
         }
@@ -51,23 +43,104 @@ namespace Microsoft.AspNetCore.Mvc.ApiExplorer
         [Fact]
         public void ApiDescription_UsesMapAsControllerNameIfNoDeclaringType()
         {
-            Action action = () => { };
+            var apiDescription = GetApiDescription(() => { });
 
-            var apiDescriptions = GetApiDescriptions(action.Method);
-
-            var apiDescription = Assert.Single(apiDescriptions);
             Assert.Equal("Map", apiDescription.ActionDescriptor.RouteValues["controller"]);
         }
 
-        private IList<ApiDescription> GetApiDescriptions(
-            MethodInfo methodInfo,
-            IEnumerable<string> httpMethods = null,
-            string pattern = null)
+        [Fact]
+        public void ApiDescription_AddsJsonRequestFormatWhenFromBodyInferred()
         {
+            static void AssertApiDescriptionHasJsonRequestFormat(ApiDescription apiDescription)
+            {
+                var requestFormat = Assert.Single(apiDescription.SupportedRequestFormats);
+                Assert.Equal("application/json", requestFormat.MediaType);
+                Assert.Null(requestFormat.Formatter);
+            }
+
+            AssertApiDescriptionHasJsonRequestFormat(GetApiDescription(
+                (InferredJsonType fromBody) => { }));
+
+            AssertApiDescriptionHasJsonRequestFormat(GetApiDescription((
+                [FromBody] int fromBody) => { }));
+        }
+
+        [Fact]
+        public void ApiDescription_UsesMetadadataInsteadOfDefaultJsonRequestFormat()
+        {
+            static void AssertApiDescriptionHasCustomRequestFormat(ApiDescription apiDescription)
+            {
+                var requestFormat = Assert.Single(apiDescription.SupportedRequestFormats);
+                Assert.Equal("application/custom", requestFormat.MediaType);
+                Assert.Null(requestFormat.Formatter);
+            }
+
+            AssertApiDescriptionHasCustomRequestFormat(GetApiDescription(
+                [Consumes("application/custom")]
+                (InferredJsonType fromBody) => { }));
+
+            AssertApiDescriptionHasCustomRequestFormat(GetApiDescription(
+                [Consumes("application/custom")]
+                ([FromBody] int fromBody) => { }));
+        }
+
+        [Fact]
+        public void ApiDescription_AddsJsonResponseFormatWhenFromBodyInferred()
+        {
+            var apiDescription = GetApiDescription(() => new InferredJsonType());
+
+            var responseType = Assert.Single(apiDescription.SupportedResponseTypes);
+            Assert.Equal(typeof(InferredJsonType), responseType.Type);
+            Assert.Equal(typeof(InferredJsonType), responseType.ModelMetadata.ModelType);
+
+            var responseFormat = Assert.Single(responseType.ApiResponseFormats);
+            Assert.Equal("application/json", responseFormat.MediaType);
+            Assert.Null(responseFormat.Formatter);
+        }
+
+        [Fact]
+        public void ApiDescription_AddsTextResponseFormatWhenFromBodyInferred()
+        {
+            var apiDescription = GetApiDescription(() => "foo");
+
+            var responseType = Assert.Single(apiDescription.SupportedResponseTypes);
+            Assert.Equal(typeof(string), responseType.Type);
+            Assert.Equal(typeof(string), responseType.ModelMetadata.ModelType);
+
+            var responseFormat = Assert.Single(responseType.ApiResponseFormats);
+            Assert.Equal("text/plain", responseFormat.MediaType);
+            Assert.Null(responseFormat.Formatter);
+        }
+
+        [Fact]
+        public void ApiDescription_AddsNoResponseFormatWhenItCannotBeInferredAndTheresNoMetadata()
+        {
+            static void AssertApiDescriptionIsVoid(ApiDescription apiDescription)
+            {
+                var responseType = Assert.Single(apiDescription.SupportedResponseTypes);
+                Assert.Equal(typeof(void), responseType.Type);
+                Assert.Equal(typeof(void), responseType.ModelMetadata.ModelType);
+
+                Assert.Empty(responseType.ApiResponseFormats);
+            }
+
+            AssertApiDescriptionIsVoid(GetApiDescription(() => { }));
+            AssertApiDescriptionIsVoid(GetApiDescription(() => Task.CompletedTask));
+            AssertApiDescriptionIsVoid(GetApiDescription(() => new ValueTask()));
+        }
+
+        private IList<ApiDescription> GetApiDescriptions(
+            Delegate action,
+            string pattern = null,
+            IEnumerable<string> httpMethods = null)
+        {
+            var methodInfo = action.Method;
+            var attributes = methodInfo.GetCustomAttributes();
             var context = new ApiDescriptionProviderContext(Array.Empty<ActionDescriptor>());
 
             var httpMethodMetadata = new HttpMethodMetadata(httpMethods ?? new[] { "GET" });
-            var endpointMetadata = new EndpointMetadataCollection(methodInfo, httpMethodMetadata);
+            var metadataItems = new List<object>(attributes) { methodInfo, httpMethodMetadata };
+            var endpointMetadata = new EndpointMetadataCollection(metadataItems.ToArray());
             var routePattern = RoutePatternFactory.Pattern(pattern ?? "/");
 
             var endpoint = new RouteEndpoint(httpContext => Task.CompletedTask, routePattern, 0, endpointMetadata, null);
@@ -81,7 +154,18 @@ namespace Microsoft.AspNetCore.Mvc.ApiExplorer
             return context.Results;
         }
 
+        private ApiDescription GetApiDescription(Delegate action, string pattern = null) =>
+            Assert.Single(GetApiDescriptions(action, pattern));
+
         private static void TestAction()
+        {
+        }
+
+        private class InferredJsonType
+        {
+        }
+
+        private interface IInferredServiceType
         {
         }
     }
