055186b659a6747a8a9fca4865eecbfb3de39dfd
Stephen Halter <halter73@gmail.com>
2021-06-16 12:02:35 -0700

    Rename AddEndpointsApiExplorer and fix namespace

--- src/Mvc/Mvc.ApiExplorer/src/DependencyInjection/EndpointMethodInfoApiExplorerServiceCollectionExtensions.cs
+++ src/Mvc/Mvc.ApiExplorer/src/DependencyInjection/EndpointMethodInfoApiExplorerServiceCollectionExtensions.cs
@@ -1,22 +1,23 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
-namespace Microsoft.AspNetCore.Mvc.ApiExplorer.DependencyInjection
+namespace Microsoft.Extensions.DependencyInjection
 {
     /// <summary>
-    /// 
+    /// Extensions for configuring ApiExplorer using <see cref="Endpoint.Metadata"/>.
     /// </summary>
     public static class EndpointMetadataApiExplorerServiceCollectionExtensions
     {
         /// <summary>
-        /// 
+        /// Configures ApiExplorer using <see cref="Endpoint.Metadata"/>.
         /// </summary>
-        /// <param name="services"></param>
-        public static void AddEndpointMetadataApiExplorer(this IServiceCollection services)
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        public static void AddEndpointsApiExplorer(this IServiceCollection services)
         {
             // Try to add default services in case MVC services aren't added.
             services.TryAddSingleton<IActionDescriptorCollectionProvider, DefaultActionDescriptorCollectionProvider>();
