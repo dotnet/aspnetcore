468ef7adea22335c9ce1eaa4515a6d3b48a12021
proff <proff@proffmail.ru>
2021-06-16 16:43:49 +0300

    adds DynamicComponent.Instance property (#33505)

--- src/Components/Components/test/DynamicComponentTest.cs
+++ src/Components/Components/test/DynamicComponentTest.cs
@@ -61,8 +61,8 @@ namespace Microsoft.AspNetCore.Components
 
             // Assert
             var batch = renderer.Batches.Single();
-            AssertFrame.Component<TestComponent>(batch.ReferenceFrames[0], 1, 0);
-            AssertFrame.Text(batch.ReferenceFrames[1], "Hello from TestComponent with IntProp=0", 0);
+            AssertFrame.Component<TestComponent>(batch.ReferenceFrames[0], 2, 0);
+            AssertFrame.Text(batch.ReferenceFrames[2], "Hello from TestComponent with IntProp=0", 0);
         }
 
         [Fact]
@@ -94,13 +94,39 @@ namespace Microsoft.AspNetCore.Components
             var batch = renderer.Batches.Single();
 
             // It renders a reference to the child component with its parameters
-            AssertFrame.Component<TestComponent>(batch.ReferenceFrames[0], 3, 0);
+            AssertFrame.Component<TestComponent>(batch.ReferenceFrames[0], 4, 0);
             AssertFrame.Attribute(batch.ReferenceFrames[1], nameof(TestComponent.IntProp), 123, 1);
             AssertFrame.Attribute(batch.ReferenceFrames[2], nameof(TestComponent.ChildContent), 1);
 
             // The child component itself is rendered
-            AssertFrame.Text(batch.ReferenceFrames[3], "Hello from TestComponent with IntProp=123", 0);
-            AssertFrame.Text(batch.ReferenceFrames[4], "This is some child content", 0);
+            AssertFrame.Text(batch.ReferenceFrames[4], "Hello from TestComponent with IntProp=123", 0);
+            AssertFrame.Text(batch.ReferenceFrames[5], "This is some child content", 0);
+        }
+
+        [Fact]
+        public void CanAccessToChildComponentInstance()
+        {
+            // Arrange
+            var instance = new DynamicComponent();
+            var renderer = new TestRenderer();
+            var childParameters = new Dictionary<string, object>
+            {
+                { nameof(TestComponent.IntProp), 123 },
+            };
+            var parameters = ParameterView.FromDictionary(new Dictionary<string, object>
+            {
+                { nameof(DynamicComponent.Type), typeof(TestComponent) },
+                { nameof(DynamicComponent.Parameters), childParameters },
+            });
+
+            // Act
+            renderer.RenderRootComponent(
+                renderer.AssignRootComponentId(instance),
+                parameters);
+
+            // Assert
+            Assert.IsType<TestComponent>(instance.Instance);
+            Assert.Equal(123, ((TestComponent)instance.Instance).IntProp);
         }
 
         private class TestComponent : AutoRenderComponent
