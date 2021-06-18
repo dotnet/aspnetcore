8060141321101926df5c25835425a7a06dd2a0e5
Brennan <brecon@microsoft.com>
2021-06-18 12:24:50 -0700

    Add IHttpUpgradeFeature to TestServer for SignalR WebSocket support (#33595)
    * Add IHttpUpgradeFeature to TestServer for SignalR WebSocket support
    * fb
    * more test

---
+++ src/SignalR/clients/csharp/Client/test/UnitTests/TestServerTests.cs
@@ -0,0 +1,121 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SignalR.Tests;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace Microsoft.AspNetCore.SignalR.Client.Tests
+{
+    public class TestServerTests : VerifiableLoggedTest
+    {
+        [Fact]
+        public async Task WebSocketsWorks()
+        {
+            using (StartVerifiableLog())
+            {
+                var builder = new WebHostBuilder().ConfigureServices(s =>
+                {
+                    s.AddLogging();
+                    s.AddSingleton(LoggerFactory);
+                    s.AddSignalR();
+                }).Configure(app =>
+                {
+                    app.UseRouting();
+                    app.UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapHub<EchoHub>("/echo");
+                    });
+                });
+                var server = new TestServer(builder);
+
+                var webSocketFactoryCalled = false;
+                var connectionBuilder = new HubConnectionBuilder()
+                    .WithUrl(server.BaseAddress + "echo", options =>
+                    {
+                        options.Transports = Http.Connections.HttpTransportType.WebSockets;
+                        options.HttpMessageHandlerFactory = _ =>
+                        {
+                            return server.CreateHandler();
+                        };
+                        options.WebSocketFactory = async (context, token) =>
+                        {
+                            webSocketFactoryCalled = true;
+                            var wsClient = server.CreateWebSocketClient();
+                            return await wsClient.ConnectAsync(context.Uri, default);
+                        };
+                    });
+                connectionBuilder.Services.AddLogging();
+                connectionBuilder.Services.AddSingleton(LoggerFactory);
+                var connection = connectionBuilder.Build();
+
+                var originalMessage = "message";
+                connection.On<string>("Echo", (receivedMessage) =>
+                {
+                    Assert.Equal(originalMessage, receivedMessage);
+                });
+
+                await connection.StartAsync();
+                await connection.InvokeAsync("Echo", originalMessage);
+                Assert.True(webSocketFactoryCalled);
+            }
+        }
+
+        [Fact]
+        public async Task LongPollingWorks()
+        {
+            using (StartVerifiableLog())
+            {
+                var builder = new WebHostBuilder().ConfigureServices(s =>
+                {
+                    s.AddLogging();
+                    s.AddSingleton(LoggerFactory);
+                    s.AddSignalR();
+                }).Configure(app =>
+                {
+                    app.UseRouting();
+                    app.UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapHub<EchoHub>("/echo");
+                    });
+                });
+                var server = new TestServer(builder);
+
+                var connectionBuilder = new HubConnectionBuilder()
+                    .WithUrl(server.BaseAddress + "echo", options =>
+                    {
+                        options.Transports = Http.Connections.HttpTransportType.LongPolling;
+                        options.HttpMessageHandlerFactory = _ =>
+                        {
+                            return server.CreateHandler();
+                        };
+                    });
+                connectionBuilder.Services.AddLogging();
+                connectionBuilder.Services.AddSingleton(LoggerFactory);
+                var connection = connectionBuilder.Build();
+
+                var originalMessage = "message";
+                connection.On<string>("Echo", (receivedMessage) =>
+                {
+                    Assert.Equal(originalMessage, receivedMessage);
+                });
+
+                await connection.StartAsync();
+                await connection.InvokeAsync("Echo", originalMessage);
+            }
+        }
+    }
+
+    class EchoHub : Hub
+    {
+        public Task Echo(string message)
+        {
+            return Clients.All.SendAsync("Echo", message);
+        }
+    }
+}
