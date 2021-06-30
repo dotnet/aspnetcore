f0a88660a0e8a37818b996b03e8ce8a03768a039
Tanay Parikh <taparik@microsoft.com>
2021-06-30 16:39:20 -0700

    Refactor Shared Streaming Interop Code TS

--- src/Components/Web.JS/src/Platform/Circuits/CircuitStreamingInterop.ts
+++ src/Components/Web.JS/src/Platform/Circuits/CircuitStreamingInterop.ts
@@ -1,4 +1,5 @@
 import { HubConnection } from '@microsoft/signalr';
+import { getNextChunk } from '../../StreamingInterop';
 
 export function sendJSDataStream(connection: HubConnection, data: ArrayBufferView | Blob, streamId: string, chunkSize: number) {
     // Run the rest in the background, without delaying the completion of the call to sendJSDataStream
@@ -50,23 +51,3 @@ export function sendJSDataStream(connection: HubConnection, data: ArrayBufferVie
         }
     }, 0);
 };
-
-async function getNextChunk(data: ArrayBufferView | Blob, position: number, nextChunkSize: number): Promise<Uint8Array> {
-    if (data instanceof Blob) {
-        return await getChunkFromBlob(data, position, nextChunkSize);
-    } else {
-        return getChunkFromArrayBufferView(data, position, nextChunkSize);
-    }
-}
-
-async function getChunkFromBlob(data: Blob, position: number, nextChunkSize: number): Promise<Uint8Array> {
-    const chunkBlob = data.slice(position, Math.min(position + nextChunkSize, data.size));
-    const arrayBuffer = await chunkBlob.arrayBuffer();
-    const nextChunkData = new Uint8Array(arrayBuffer);
-    return nextChunkData;
-}
-
-function getChunkFromArrayBufferView(data: ArrayBufferView, position: number, nextChunkSize: number) {
-    const nextChunkData = new Uint8Array(data.buffer, data.byteOffset + position, nextChunkSize);
-    return nextChunkData;
-}
