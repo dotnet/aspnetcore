09338ccdbdc757ad8ccbaf22400809d08b1de317
DamianEdwards <damian@damianedwards.com>
2021-07-02 14:00:58 -0700

    Updated WebApi template:
    - uses minimal hosting APIs
    - uses top-level statements

--- src/ProjectTemplates/Web.ProjectTemplates/content/WebApi-CSharp/Program.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/WebApi-CSharp/Program.cs
@@ -1,26 +1,81 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+#if (OrganizationalAuth || IndividualB2CAuth)
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+#endif
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+#if (GenerateGraph)
+using Microsoft.Graph;
+#endif
+#if (EnableOpenAPI)
+using Microsoft.OpenApi.Models;
+#endif
 
-namespace Company.WebApplication1
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+#if (OrganizationalAuth)
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+#if (GenerateApiOrGraph)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"))
+        .EnableTokenAcquisitionToCallDownstreamApi()
+#if (GenerateApi)
+            .AddDownstreamWebApi("DownstreamApi", builder.Configuration.GetSection("DownstreamApi"))
+#endif
+#if (GenerateGraph)
+            .AddMicrosoftGraph(builder.Configuration.GetSection("DownstreamApi"))
+#endif
+            .AddInMemoryTokenCaches();
+#else
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+#endif
+#elif (IndividualB2CAuth)
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+#if (GenerateApi)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAdB2C"))
+        .EnableTokenAcquisitionToCallDownstreamApi()
+            .AddDownstreamWebApi("DownstreamApi", builder.Configuration.GetSection("DownstreamApi"))
+            .AddInMemoryTokenCaches();
+#else
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAdB2C"));
+#endif
+#endif
+
+builder.Services.AddControllers();
+#if (EnableOpenAPI)
+builder.Services.AddSwaggerGen(c =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    c.SwaggerDoc("v1", new() { Title = "Company.WebApplication1", Version = "v1" });
+});
+#endif
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    #if (EnableOpenAPI)
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Company.WebApplication1 v1"));
+    #endif
 }
+#if (RequiresHttps)
+
+app.UseHttpsRedirection();
+#endif
+
+#if (OrganizationalAuth || IndividualAuth)
+app.UseAuthentication();
+#endif
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
