2ee5f056b51b82974cfb0c54d15885460bed23a7
James Newton-King <james@newtonking.com>
2021-07-06 17:08:55 +1200

    Update

--- src/Servers/Kestrel/Core/src/Internal/Infrastructure/TransportManager.cs
+++ src/Servers/Kestrel/Core/src/Internal/Infrastructure/TransportManager.cs
@@ -8,7 +8,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Connections;
