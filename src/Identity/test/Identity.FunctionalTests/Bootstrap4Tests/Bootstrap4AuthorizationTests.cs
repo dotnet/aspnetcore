2e2e5fdec2a4c38c87ec63af360909c65873e42e
Hao Kung <HaoK@users.noreply.github.com>
2021-07-07 00:08:48 -0700

    Update Bootstrap4AuthorizationTests.cs

--- src/Identity/test/Identity.FunctionalTests/Bootstrap4Tests/Bootstrap4AuthorizationTests.cs
+++ src/Identity/test/Identity.FunctionalTests/Bootstrap4Tests/Bootstrap4AuthorizationTests.cs
@@ -4,7 +4,7 @@
 using Identity.DefaultUI.WebSite;
 using Identity.DefaultUI.WebSite.Data;
 
-namespace Microsoft.AspNetCore.Identity.FunctionalTests.IdentityUserTests
+namespace Microsoft.AspNetCore.Identity.FunctionalTests.Bootstrap4Tests
 {
     public class Bootstrap4AuthorizationTests : AuthorizationTests<ApplicationUserStartup, ApplicationDbContext>
     {
