e7be6611e92075e1188d653aaa187a8c9b9860e0
Hao Kung <HaoK@users.noreply.github.com>
2021-07-07 00:08:01 -0700

    Update Bootstrap4ManagementTests.cs

--- src/Identity/test/Identity.FunctionalTests/Bootstrap4Tests/Bootstrap4ManagementTests.cs
+++ src/Identity/test/Identity.FunctionalTests/Bootstrap4Tests/Bootstrap4ManagementTests.cs
@@ -4,7 +4,7 @@
 using Identity.DefaultUI.WebSite;
 using Identity.DefaultUI.WebSite.Data;
 
-namespace Microsoft.AspNetCore.Identity.FunctionalTests.Bootstrap3Tests
+namespace Microsoft.AspNetCore.Identity.FunctionalTests.Bootstrap4Tests
 {
     public class Bootstrap4ManagementTests : ManagementTests<ApplicationUserStartup, ApplicationDbContext>
     {
