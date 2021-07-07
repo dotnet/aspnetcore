aed0a07df957b418db4a06ed54a76f304f58fc51
Brennan <brecon@microsoft.com>
2021-07-07 09:26:35 -0700

    Revert "Add base interface for IHubContext (#33443)"
    This reverts commit 65276b0476c4a004e2dff8c205230a59deb6c627.

--- src/SignalR/server/Core/src/IHubContext.cs
+++ src/SignalR/server/Core/src/IHubContext.cs
@@ -6,7 +6,7 @@ namespace Microsoft.AspNetCore.SignalR
     /// <summary>
     /// A context abstraction for a hub.
     /// </summary>
-    public interface IHubContext
+    public interface IHubContext<THub> where THub : Hub
     {
         /// <summary>
         /// Gets a <see cref="IHubClients"/> that can be used to invoke methods on clients connected to the hub.
@@ -17,12 +17,5 @@ namespace Microsoft.AspNetCore.SignalR
         /// Gets a <see cref="IGroupManager"/> that can be used to add and remove connections to named groups.
         /// </summary>
         IGroupManager Groups { get; }
-    }
-
-    /// <summary>
-    /// A context abstraction for a hub.
-    /// </summary>
-    public interface IHubContext<out THub> : IHubContext where THub : Hub
-    {
     }
 }
