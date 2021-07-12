57f686eafdc3922376e9cf3fb1eeff4adfd54548
Mohamed Seada <mohamed.seada.1994@gmail.com>
2021-07-12 17:59:28 +0200

    Fix method documented return value (#34264)

--- src/Identity/Core/src/SignInManager.cs
+++ src/Identity/Core/src/SignInManager.cs
@@ -266,7 +266,7 @@ namespace Microsoft.AspNetCore.Identity
         /// </summary>
         /// <param name="principal">The principal whose stamp should be validated.</param>
         /// <returns>The task object representing the asynchronous operation. The task will contain the <typeparamref name="TUser"/>
-        /// if the stamp matches the persisted value, otherwise it will return false.</returns>
+        /// if the stamp matches the persisted value, otherwise it will return null.</returns>
         public virtual async Task<TUser> ValidateSecurityStampAsync(ClaimsPrincipal principal)
         {
             if (principal == null)
@@ -289,7 +289,7 @@ namespace Microsoft.AspNetCore.Identity
         /// </summary>
         /// <param name="principal">The principal whose stamp should be validated.</param>
         /// <returns>The task object representing the asynchronous operation. The task will contain the <typeparamref name="TUser"/>
-        /// if the stamp matches the persisted value, otherwise it will return false.</returns>
+        /// if the stamp matches the persisted value, otherwise it will return null.</returns>
         public virtual async Task<TUser> ValidateTwoFactorSecurityStampAsync(ClaimsPrincipal principal)
         {
             if (principal == null || principal.Identity?.Name == null)
