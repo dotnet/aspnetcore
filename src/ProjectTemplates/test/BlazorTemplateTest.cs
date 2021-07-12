dcae2a696a7b76051f69ad7b0ef4541d607d3b3f
Hao Kung <HaoK@users.noreply.github.com>
2021-07-12 16:53:44 -0700

    Run blazor create/build/publish as part of template tests (#34275)

--- src/ProjectTemplates/test/BlazorTemplateTest.cs
+++ src/ProjectTemplates/test/BlazorTemplateTest.cs
@@ -7,7 +7,6 @@ using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.BrowserTesting;
 using Microsoft.AspNetCore.Testing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -18,18 +17,14 @@ using Xunit.Abstractions;
 
 namespace Templates.Test
 {
-    public abstract class BlazorTemplateTest : LoggedTest, IAsyncLifetime
+    public abstract class BlazorTemplateTest : LoggedTest
     {
-        public const int BUILDCREATEPUBLISH_PRIORITY = -1000;
-
         public BlazorTemplateTest(ProjectFactoryFixture projectFactory)
         {
             ProjectFactory = projectFactory;
         }
 
         public ProjectFactoryFixture ProjectFactory { get; set; }
-        public ContextInformation BrowserContextInfo { get; protected set; }
-        public BrowserManager BrowserManager { get; private set; }
 
         private ITestOutputHelper _output;
         public ITestOutputHelper Output
@@ -45,48 +40,6 @@ namespace Templates.Test
         }
 
         public abstract string ProjectType { get; }
-        private static readonly bool _isCIEnvironment =
-            !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("ContinuousIntegrationBuild"));
-
-        protected override async Task InitializeCoreAsync(TestContext context)
-        {
-            BrowserManager = await BrowserManager.CreateAsync(CreateConfiguration(), LoggerFactory);
-            BrowserContextInfo = new ContextInformation(LoggerFactory);
-            _output = new TestOutputLogger(Logger);
-        }
-
-        public Task InitializeAsync() => Task.CompletedTask;
-
-        public Task DisposeAsync() => BrowserManager.DisposeAsync();
-
-        private static IConfiguration CreateConfiguration()
-        {
-            var basePath = Path.GetDirectoryName(typeof(BlazorTemplateTest).Assembly.Location);
-            var os = Environment.OSVersion.Platform switch
-            {
-                PlatformID.Win32NT => "win",
-                PlatformID.Unix => "linux",
-                PlatformID.MacOSX => "osx",
-                _ => null
-            };
-
-            var builder = new ConfigurationBuilder()
-                .AddJsonFile(Path.Combine(basePath, "playwrightSettings.json"))
-                .AddJsonFile(Path.Combine(basePath, $"playwrightSettings.{os}.json"), optional: true);
-
-            if (_isCIEnvironment)
-            {
-                builder.AddJsonFile(Path.Combine(basePath, "playwrightSettings.ci.json"), optional: true)
-                    .AddJsonFile(Path.Combine(basePath, $"playwrightSettings.ci.{os}.json"), optional: true);
-            }
-
-            if (Debugger.IsAttached)
-            {
-                builder.AddJsonFile(Path.Combine(basePath, "playwrightSettings.debug.json"), optional: true);
-            }
-
-            return builder.Build();
-        }
 
         protected async Task<Project> CreateBuildPublishAsync(string projectName, string auth = null, string[] args = null, string targetFramework = null, bool serverProject = false, bool onlyCreate = false)
         {
@@ -134,22 +87,6 @@ namespace Templates.Test
             };
 
             return subProject;
-        }
-
-        public static bool TryValidateBrowserRequired(BrowserKind browserKind, bool isRequired, out string error)
-        {
-            error = !isRequired ? null : $"Browser '{browserKind}' is required but not configured on '{RuntimeInformation.OSDescription}'";
-            return isRequired;
-        }
-
-        protected void EnsureBrowserAvailable(BrowserKind browserKind)
-        {
-            Assert.False(
-                TryValidateBrowserRequired(
-                    browserKind,
-                    isRequired: !BrowserManager.IsExplicitlyDisabled(browserKind),
-                    out var errorMessage),
-                errorMessage);
         }
     }
 }
