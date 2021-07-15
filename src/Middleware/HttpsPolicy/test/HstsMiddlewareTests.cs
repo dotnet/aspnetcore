eb8870c557b79c39b2852441a0b8bdb6d54b6c64
local <local@localhost>
2021-07-15 22:21:45 +0200

    Add HstsMiddleware unit tests

--- src/Middleware/HttpsPolicy/test/HstsMiddlewareTests.cs
+++ src/Middleware/HttpsPolicy/test/HstsMiddlewareTests.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Testing;
+using Microsoft.Extensions.Options;
 using Microsoft.Net.Http.Headers;
 using Xunit;
 
@@ -22,6 +23,144 @@ namespace Microsoft.AspNetCore.HttpsPolicy.Tests
 {
     public class HstsMiddlewareTests
     {
+#region Unit tests
+        [Fact]
+        public void HstsMiddleware_ArgumentNextIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                _ = new HstsMiddleware(next: null, options: new OptionsWrapper<HstsOptions>(new HstsOptions()));
+            });
+        }
+
+        [Fact]
+        public void HstsMiddleware_ArgumentOptionsIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                _ = new HstsMiddleware(innerHttpContext => Task.CompletedTask, options: null);
+            });
+        }
+
+        [Fact]
+        public async Task Invoke_SetsHstsHeader()
+        {
+            var middleware = new HstsMiddleware(innerHttpContext => Task.CompletedTask, new OptionsWrapper<HstsOptions>(new HstsOptions()));
+
+            var context = new DefaultHttpContext();
+            context.Request.Host = new HostString("example.com");
+            context.Request.Scheme = "https";
+
+            await using (context.Response.Body = new MemoryStream())
+            {
+                await middleware.Invoke(context);
+
+                context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+                using var streamReader = new StreamReader(context.Response.Body);
+                _ = await streamReader.ReadToEndAsync();
+            }
+
+            Assert.Equal("max-age=2592000", context.Response.Headers[HeaderNames.StrictTransportSecurity].FirstOrDefault());
+        }
+
+        [Theory]
+        [InlineData(0, false, false, "max-age=0")]
+        [InlineData(-1, false, false, "max-age=-1")]
+        [InlineData(0, true, false, "max-age=0; includeSubDomains")]
+        [InlineData(50000, false, true, "max-age=50000; preload")]
+        [InlineData(0, true, true, "max-age=0; includeSubDomains; preload")]
+        [InlineData(50000, true, true, "max-age=50000; includeSubDomains; preload")]
+        public async Task Invoke_SetsHeaderCorrectly(int maxAge, bool includeSubDomains, bool preload, string expected)
+        {
+            var hstsOptions = new HstsOptions
+            {
+                Preload = preload,
+                IncludeSubDomains = includeSubDomains,
+                MaxAge = TimeSpan.FromSeconds(maxAge)
+            };
+
+            var middleware = new HstsMiddleware(innerHttpContext => Task.CompletedTask, new OptionsWrapper<HstsOptions>(hstsOptions));
+
+            var context = new DefaultHttpContext();
+            context.Request.Host = new HostString("example.com");
+            context.Request.Scheme = "https";
+
+            await using (context.Response.Body = new MemoryStream())
+            {
+                await middleware.Invoke(context);
+
+                context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+                using var streamReader = new StreamReader(context.Response.Body);
+                _ = await streamReader.ReadToEndAsync();
+            }
+
+            Assert.Equal(expected, context.Response.Headers[HeaderNames.StrictTransportSecurity].FirstOrDefault());
+        }
+
+        [Theory]
+        [InlineData(0, false, false)]
+        [InlineData(-1, false, false)]
+        [InlineData(0, true, false)]
+        [InlineData(50000, false, true)]
+        [InlineData(0, true, true)]
+        [InlineData(50000, true, true)]
+        public async Task Invoke_SchemeIsInsecure_DoesNotSetHstsHeader(int maxAge, bool includeSubDomains, bool preload)
+        {
+            var hstsOptions = new HstsOptions
+            {
+                Preload = preload,
+                IncludeSubDomains = includeSubDomains,
+                MaxAge = TimeSpan.FromSeconds(maxAge)
+            };
+
+            var middleware = new HstsMiddleware(innerHttpContext => Task.CompletedTask, new OptionsWrapper<HstsOptions>(hstsOptions));
+
+            var context = new DefaultHttpContext();
+            context.Request.Host = new HostString("example.com");
+            context.Request.Scheme = "http";
+
+            await using (context.Response.Body = new MemoryStream())
+            {
+                await middleware.Invoke(context);
+
+                context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+                using var streamReader = new StreamReader(context.Response.Body);
+                _ = await streamReader.ReadToEndAsync();
+            }
+
+            Assert.Null(context.Response.Headers[HeaderNames.StrictTransportSecurity].FirstOrDefault());
+        }
+
+        [Theory]
+        [InlineData("localhost")]
+        [InlineData("127.0.0.1")]
+        [InlineData("[::1]")]
+        public async Task Invoke_HostIsExcluded_DoesNotSetHstsHeader(string hostName)
+        {
+            var middleware = new HstsMiddleware(innerHttpContext => Task.CompletedTask, new OptionsWrapper<HstsOptions>(new HstsOptions()));
+
+            var context = new DefaultHttpContext();
+            context.Request.Host = new HostString(hostName);
+            context.Request.Scheme = "https";
+
+            await using (context.Response.Body = new MemoryStream())
+            {
+                await middleware.Invoke(context);
+
+                context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+                using var streamReader = new StreamReader(context.Response.Body);
+                _ = await streamReader.ReadToEndAsync();
+            }
+
+            Assert.Null(context.Response.Headers[HeaderNames.StrictTransportSecurity].FirstOrDefault());
+        }
+#endregion // Unit tests
+
+#region Integration tests
         [Fact]
         public async Task SetOptionsWithDefault_SetsMaxAgeToCorrectValue()
         {
@@ -403,5 +542,6 @@ namespace Microsoft.AspNetCore.HttpsPolicy.Tests
             Assert.Equal(LogLevel.Trace, message.LogLevel);
             Assert.Equal("Adding HSTS header to response.", message.State.ToString());
         }
+#endregion // Integration tests
     }
 }
