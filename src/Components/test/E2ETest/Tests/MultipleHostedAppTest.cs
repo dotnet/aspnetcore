ef165c602596b869b8e262eb1ff109b3cda0ec94
Pranav K <prkrishn@hotmail.com>
2021-07-15 14:02:57 -0700

    Quarantine tests Microsoft.AspNetCore.Components.E2ETest.Tests.MultipleHostedAppTest

--- src/Components/test/E2ETest/Tests/MultipleHostedAppTest.cs
+++ src/Components/test/E2ETest/Tests/MultipleHostedAppTest.cs
@@ -4,6 +4,7 @@
 using Microsoft.AspNetCore.Components.E2ETest.Infrastructure;
 using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
 using Microsoft.AspNetCore.E2ETesting;
+using Microsoft.AspNetCore.Testing;
 using OpenQA.Selenium;
 using Xunit;
 using Xunit.Abstractions;
@@ -30,6 +31,7 @@ namespace Microsoft.AspNetCore.Components.E2ETest.Tests
         }
 
         [Fact]
+        [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/34395")]
         public void CanLoadBlazorAppFromSubPath()
         {
             Navigate("/app/");
@@ -39,12 +41,14 @@ namespace Microsoft.AspNetCore.Components.E2ETest.Tests
         }
 
         [Fact]
+        [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/34395")]
         public void HasTitle()
         {
             Assert.Equal("Sample Blazor app", Browser.Title);
         }
 
         [Fact]
+        [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/34395")]
         public void ServesStaticAssetsFromClientAppWebRoot()
         {
             var javascriptExecutor = (IJavaScriptExecutor)Browser;
