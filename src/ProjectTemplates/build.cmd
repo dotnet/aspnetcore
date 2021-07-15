9669bdb6f40b3d7b0b8a31695b5d6bc6d10631d9
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2021-07-14 21:49:40 -0700

    Apply suggestions from code review

--- src/ProjectTemplates/build.cmd
+++ src/ProjectTemplates/build.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
 SET RepoRoot=%~dp0..\..
 %RepoRoot%\eng\build.cmd -projects %~dp0*\*.*proj "/p:EnforceE2ETestPrerequisites=true" %*
-%RepoRoot%\eng\build.cmd -projects %~dp0\..\submodules\spa-templates\src\*.*proj "/p:EnforceE2ETestPrerequisites=true" %*
+%RepoRoot%\eng\build.cmd -projects %~dp0\..\submodules\spa-templates\src\*.*proj "/p:EnforceE2ETestPrerequisites=true" -noBuildRepoTasks -noBuildNative %*
