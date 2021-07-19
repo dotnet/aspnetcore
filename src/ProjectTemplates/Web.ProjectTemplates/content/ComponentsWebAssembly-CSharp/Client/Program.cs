fce3d154e29cbe22a98eb73c335c694e8ebd0822
Mackinnon Buck <mackinnon.buck@gmail.com>
2021-07-19 11:56:19 -0700

    Updated Blazor project templates to use <PageTitle>.

--- src/ProjectTemplates/Web.ProjectTemplates/content/ComponentsWebAssembly-CSharp/Client/Program.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/ComponentsWebAssembly-CSharp/Client/Program.cs
@@ -10,6 +10,7 @@ using ComponentsWebAssembly_CSharp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 #if (!Hosted || NoAuth)
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
