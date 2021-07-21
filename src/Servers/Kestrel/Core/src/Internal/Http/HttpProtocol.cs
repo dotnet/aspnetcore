29c7fe687b248b1ab88a732d683679df960785fc
James Newton-King <james@newtonking.com>
2021-07-21 13:34:06 +1200

    HTTP/3: Change alt-svc protocol to h3 and add upgrade test (#34469)

--- src/Servers/Kestrel/Core/src/Internal/Http/HttpProtocol.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http/HttpProtocol.cs
@@ -1194,17 +1194,17 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
                 }
             }
 
-            // TODO allow customization of this.
-            // Chrome is using h3-29 for protocol ID as of 12/2020. This is likely to be the alt-svc
-            // value until HTTP/3 is finalized.
-            // More info: https://blog.chromium.org/2020/10/chrome-is-deploying-http3-and-ietf-quic.html
             if (ServerOptions.EnableAltSvc && _httpVersion < Http.HttpVersion.Http3)
             {
+                // TODO: Perf. Avoid allocating enumerator and property's LINQ.
+                // https://github.com/dotnet/aspnetcore/issues/34468
                 foreach (var option in ServerOptions.ListenOptions)
                 {
                     if ((option.Protocols & HttpProtocols.Http3) == HttpProtocols.Http3)
                     {
-                        responseHeaders.HeaderAltSvc = $"h3-29=\":{option.IPEndPoint!.Port}\"; ma=84600";
+                        // TODO: Perf. Create string once instead of per-request.
+                        // https://github.com/dotnet/aspnetcore/issues/34468
+                        responseHeaders.HeaderAltSvc = $"h3=\":{option.IPEndPoint!.Port}\"; ma=84600";
                         break;
                     }
                 }
