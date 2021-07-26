17f6edac1e7de0ecc40587cfd83adab3221f0c50
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2021-07-26 13:56:39 +0100

    Fix build

--- src/Components/Server/src/Circuits/CircuitJSComponentInterop.cs
+++ src/Components/Server/src/Circuits/CircuitJSComponentInterop.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.ComponentModel;
 using System.Text.Json;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.Web.Infrastructure;
