df3530fb59282694da37581ff33777f41c5bd6cd
Safia Abdalla <safia@safia.rocks>
2021-07-27 10:52:55 -0500

    Support setting endpoints on group names to resolve #34541

---
+++ src/Http/Routing/src/EndpointGroupNameMetadata.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.AspNetCore.Routing
+{
+    /// <summary>
+    /// Specifies an endpoint group name in <see cref="Endpoint.Metadata"/>.
+    /// </summary>
+    public class EndpointGroupNameMetadata : IEndpointGroupNameMetadata
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="EndpointGroupNameMetadata"/> with the provided endpoint name.
+        /// </summary>
+        /// <param name="endpointGroupName">The endpoint name.</param>
+        public EndpointGroupNameMetadata(string endpointGroupName)
+        {
+            if (endpointGroupName == null)
+            {
+                throw new ArgumentNullException(nameof(endpointGroupName));
+            }
+
+            EndpointGroupName = endpointGroupName;
+        }
+
+        /// <summary>
+        /// Gets the endpoint name.
+        /// </summary>
+        public string EndpointGroupName { get; }
+    }
+}
