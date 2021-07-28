252901ff5d02bbeda9b30242ed82e29909f1551f
Tanay Parikh <TanayParikh@users.noreply.github.com>
2021-07-28 19:47:38 +0000

    PR Feedback

--- src/SignalR/clients/ts/signalr/src/Errors.ts
+++ src/SignalR/clients/ts/signalr/src/Errors.ts
@@ -69,6 +69,7 @@ export class AbortError extends Error {
 }
 
 /** Error thrown when the selected transport is unsupported by the browser. */
+/** @private */
 export class UnsupportedTransportError extends Error {
     // @ts-ignore: Intentionally unused.
     // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -98,6 +99,7 @@ export class UnsupportedTransportError extends Error {
 }
 
 /** Error thrown when the selected transport is disabled by the browser. */
+/** @private */
 export class DisabledTransportError extends Error {
     // @ts-ignore: Intentionally unused.
     // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -127,6 +129,7 @@ export class DisabledTransportError extends Error {
 }
 
 /** Error thrown when the selected transport cannot be started. */
+/** @private */
 export class FailedToStartTransportError extends Error {
     // @ts-ignore: Intentionally unused.
     // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -156,6 +159,7 @@ export class FailedToStartTransportError extends Error {
 }
 
 /** Error thrown when multiple errors have occured. */
+/** @private */
 export class AggregateErrors extends Error {
     // @ts-ignore: Intentionally unused.
     // eslint-disable-next-line @typescript-eslint/naming-convention
