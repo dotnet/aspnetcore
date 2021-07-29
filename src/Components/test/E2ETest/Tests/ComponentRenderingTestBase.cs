0a47f7f3e66072b74392312227247bfcb060485e
John Luo <johluo@microsoft.com>
2021-07-29 14:47:27 -0700

    Skip failing tests

--- src/Components/test/E2ETest/Tests/ComponentRenderingTestBase.cs
+++ src/Components/test/E2ETest/Tests/ComponentRenderingTestBase.cs
@@ -702,7 +702,7 @@ namespace Microsoft.AspNetCore.Components.E2ETest.Tests
             });
         }
 
-        [Fact]
+        [Fact(Skip = "https://github.com/dotnet/aspnetcore/issues/34857")]
         public void CanHandleClearedChild()
         {
             var appElement = Browser.MountTestComponent<ContentEditable>();
