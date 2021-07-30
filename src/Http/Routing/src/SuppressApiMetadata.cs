4b67fc38f8668dee457871a5027803cf53376a9d
Safia Abdalla <safia@safia.rocks>
2021-07-29 19:20:56 -0500

    Add support for SuppressApi to close #34068

---
+++ src/Http/Routing/src/SuppressApiMetadata.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.AspNetCore.Routing
+{
+    /// <summary>
+    /// Specifies an endpoint name in <see cref="Endpoint.Metadata"/>.
+    /// </summary>
+    /// <remarks>
+    /// Endpoint names must be unique within an application, and can be used to unambiguously
+    /// identify a desired endpoint for URI generation using <see cref="LinkGenerator"/>.
+    /// </remarks>
+    public sealed class SuppressApiMetadata : ISuppressApiMetadata
+    {
+        /// <summary>
+        /// Gets the endpoint name.
+        /// </summary>
+        public bool SuppressApi => true;
+    }
+}
