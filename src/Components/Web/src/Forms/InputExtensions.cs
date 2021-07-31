bfcb5dd43de94375f56725ad852cb88afe66da73
Mackinnon Buck <mackinnon.buck@gmail.com>
2021-07-30 17:22:57 -0700

    Bool binding support for InputRadioGroup and InputSelect.

--- src/Components/Web/src/Forms/InputExtensions.cs
+++ src/Components/Web/src/Forms/InputExtensions.cs
@@ -16,23 +16,61 @@ namespace Microsoft.AspNetCore.Components.Forms
         {
             try
             {
-                if (BindConverter.TryConvertTo<TValue>(value, CultureInfo.CurrentCulture, out var parsedValue))
+                // We special-case bool values because BindConverter reserves bool conversion for conditional attributes.
+                if (typeof(TValue) == typeof(bool))
+                {
+                    if (TryConvertToBool(value, out result))
+                    {
+                        validationErrorMessage = null;
+                        return true;
+                    }
+                }
+                else if (typeof(TValue) == typeof(bool?))
+                {
+                    if (TryConvertToNullableBool(value, out result))
+                    {
+                        validationErrorMessage = null;
+                        return true;
+                    }
+                }
+                else if (BindConverter.TryConvertTo<TValue>(value, CultureInfo.CurrentCulture, out var parsedValue))
                 {
                     result = parsedValue;
                     validationErrorMessage = null;
                     return true;
                 }
-                else
-                {
-                    result = default;
-                    validationErrorMessage = $"The {input.DisplayName ?? input.FieldIdentifier.FieldName} field is not valid.";
-                    return false;
-                }
+
+                result = default;
+                validationErrorMessage = $"The {input.DisplayName ?? input.FieldIdentifier.FieldName} field is not valid.";
+                return false;
             }
             catch (InvalidOperationException ex)
             {
                 throw new InvalidOperationException($"{input.GetType()} does not support the type '{typeof(TValue)}'.", ex);
             }
+        }
+
+        private static bool TryConvertToBool<TValue>(string? value, out TValue result)
+        {
+            if (bool.TryParse(value, out var @bool))
+            {
+                result = (TValue)(object)@bool;
+                return true;
+            }
+
+            result = default!;
+            return false;
+        }
+
+        private static bool TryConvertToNullableBool<TValue>(string? value, out TValue result)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                result = default!;
+                return true;
+            }
+
+            return TryConvertToBool(value, out result);
         }
     }
 }
--- src/Components/Web/src/Forms/InputSelect.cs
+++ src/Components/Web/src/Forms/InputSelect.cs
@@ -64,6 +64,22 @@ namespace Microsoft.AspNetCore.Components.Forms
         protected override bool TryParseValueFromString(string? value, [MaybeNullWhen(false)] out TValue result, [NotNullWhen(false)] out string? validationErrorMessage)
             => this.TryParseSelectableValueFromString(value, out result, out validationErrorMessage);
 
+        /// <inheritdoc />
+        protected override string? FormatValueAsString(TValue? value)
+        {
+            // We special-case bool values because BindConverter reserves bool conversion for conditional attributes.
+            if (typeof(TValue) == typeof(bool))
+            {
+                return (bool)(object)value! ? "true" : "false";
+            }
+            else if (typeof(TValue) == typeof(bool?))
+            {
+                return value is not null && (bool)(object)(value) ? "true" : "false";
+            }
+
+            return base.FormatValueAsString(value);
+        }
+
         private void SetCurrentValueAsStringArray(string?[]? value)
         {
             CurrentValue = BindConverter.TryConvertTo<TValue>(value, CultureInfo.CurrentCulture, out var result)
