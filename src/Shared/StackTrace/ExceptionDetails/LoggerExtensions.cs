90c08fe18cd455d15add9b469b41fa288b26eb96
Pranav K <prkrishn@hotmail.com>
2021-07-31 12:26:03 -0700

    Convert some LoggerMessage.Define to LoggerMessageAttribute (#34905)

--- src/Shared/StackTrace/ExceptionDetails/LoggerExtensions.cs
+++ src/Shared/StackTrace/ExceptionDetails/LoggerExtensions.cs
@@ -1,26 +1,13 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.Extensions.StackTrace.Sources
 {
-    internal static class LoggerExtensions
+    internal static partial class LoggerExtensions
     {
-        private static readonly Action<ILogger, Exception> _failedToReadStackFrameInfo;
-
-        static LoggerExtensions()
-        {
-            _failedToReadStackFrameInfo = LoggerMessage.Define(
-                logLevel: LogLevel.Debug,
-                eventId: new EventId(0, "FailedToReadStackTraceInfo"),
-                formatString: "Failed to read stack trace information for exception.");
-        }
-
-        public static void FailedToReadStackTraceInfo(this ILogger logger, Exception exception)
-        {
-            _failedToReadStackFrameInfo(logger, exception);
-        }
+        [LoggerMessage(0, LogLevel.Debug, "Failed to read stack trace information for exception.", EventName = "FailedToReadStackTraceInfo")]
+        public static partial void FailedToReadStackTraceInfo(this ILogger logger, Exception exception);
     }
 }
