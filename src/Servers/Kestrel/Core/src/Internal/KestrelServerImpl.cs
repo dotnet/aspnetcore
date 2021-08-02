zhongxiangggan/zhongxiangggan

HTTP/3: Refactor alt-svc upgrade

src/Servers/Kestrel/Core/src/Internal/KestrelServerImpl.cs
@@ -171,7 +171,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core
                             throw new InvalidOperationException($"Cannot start HTTP/1.x or HTTP/2 server if no {nameof(IConnectionListenerFactory)} is registered.");
                         }
 
-                        options.UseHttpServer(ServiceContext, application, options.Protocols);
+                        options.UseHttpServer(ServiceContext, application, options.Protocols, options.EnableAltSvc);
                         var connectionDelegate = options.Build();
 
                         // Add the connection limit middleware
@@ -187,7 +187,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core
                             throw new InvalidOperationException($"Cannot start HTTP/3 server if no {nameof(IMultiplexedConnectionListenerFactory)} is registered.");
                         }
 
-                        options.UseHttp3Server(ServiceContext, application);
+                        options.UseHttp3Server(ServiceContext, application, options.Protocols, options.EnableAltSvc);
                         var multiplexedConnectionDelegate = ((IMultiplexedConnectionBuilder)options).Build();
 
                         // Add the connection limit middleware

src/Servers/Kestrel/Core/src/ListenOptions.cs
@@ -85,6 +85,19 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core
         public HttpProtocols Protocols { get; set; } = DefaultHttpProtocols;
 
         /// <summary>
+        /// Gets or sets a value that controls whether to the "Alt-Svc" header is included with the response.
+        /// The "Alt-Svc" header is used by clients to upgrade HTTP/1.1 and HTTP/2 connections to HTTP/3.
+        /// <para>
+        /// The "Alt-Svc" header is only included with a response if <see cref="Protocols"/> has either
+        /// HTTP/1.1 or HTTP/2 enabled, and HTTP/3 is enabled.
+        /// </para>
+        /// </summary>
+        /// <remarks>
+        /// Defaults to true.
+        /// </remarks>
+        public bool EnableAltSvc { get; set; } = true;
+
+        /// <summary>
         /// Gets the application <see cref="IServiceProvider"/>.
         /// </summary>
         public IServiceProvider ApplicationServices => KestrelServerOptions?.ApplicationServices!; // TODO - Always available?

src/Servers/Kestrel/Transport.Libuv/test/LibuvTransportTests.cs
@@ -187,7 +187,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.Tests
                 return context.Response.WriteAsync("Hello World");
             });
 
-            listenOptions.UseHttpServer(serviceContext, testApplication, Core.HttpProtocols.Http1);
+            listenOptions.UseHttpServer(serviceContext, testApplication, Core.HttpProtocols.Http1, enableAltSvc: false);
 
             var transportContext = new TestLibuvTransportContext
             {

dotnet/aspnetcore

src/Servers/Kestrel/Core/src/Internal/KestrelServerImpl.cs
@@ -171,7 +171,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core
                             throw new InvalidOperationException($"Cannot start HTTP/1.x or HTTP/2 server if no {nameof(IConnectionListenerFactory)} is registered.");
                         }
 
-                        options.UseHttpServer(ServiceContext, application, options.Protocols);
+                        options.UseHttpServer(ServiceContext, application, options.Protocols, options.AddAltSvcHeader);
                         var connectionDelegate = options.Build();
 
                         // Add the connection limit middleware
@@ -187,7 +187,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core
                             throw new InvalidOperationException($"Cannot start HTTP/3 server if no {nameof(IMultiplexedConnectionListenerFactory)} is registered.");
                         }
 
-                        options.UseHttp3Server(ServiceContext, application);
+                        options.UseHttp3Server(ServiceContext, application, options.Protocols, options.AddAltSvcHeader);
                         var multiplexedConnectionDelegate = ((IMultiplexedConnectionBuilder)options).Build();
 
                         // Add the connection limit middleware

src/Servers/Kestrel/Core/src/ListenOptions.cs
@@ -85,6 +85,19 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core
         public HttpProtocols Protocols { get; set; } = DefaultHttpProtocols;
 
         /// <summary>
+        /// Gets or sets a value that controls whether to the "Alt-Svc" header is included with response headers.
+        /// The "Alt-Svc" header is used by clients to upgrade HTTP/1.1 and HTTP/2 connections to HTTP/3.
+        /// <para>
+        /// The "Alt-Svc" header is only included with a response if <see cref="Protocols"/> has either
+        /// HTTP/1.1 or HTTP/2 enabled, and HTTP/3 is enabled.
+        /// </para>
+        /// </summary>
+        /// <remarks>
+        /// Defaults to true.
+        /// </remarks>
+        public bool AddAltSvcHeader { get; set; } = true;
+
+        /// <summary>
         /// Gets the application <see cref="IServiceProvider"/>.
         /// </summary>
         public IServiceProvider ApplicationServices => KestrelServerOptions?.ApplicationServices!; // TODO - Always available?

src/Servers/Kestrel/Transport.Libuv/test/LibuvTransportTests.cs
@@ -187,7 +187,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.Tests
                 return context.Response.WriteAsync("Hello World");
             });
 
-            listenOptions.UseHttpServer(serviceContext, testApplication, Core.HttpProtocols.Http1);
+            listenOptions.UseHttpServer(serviceContext, testApplication, Core.HttpProtocols.Http1, addAltSvcHeader: false);
 
             var transportContext = new TestLibuvTransportContext
             {