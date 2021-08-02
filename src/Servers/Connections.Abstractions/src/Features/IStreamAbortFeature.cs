6cf9e32b35159269b902e24dfacfa60ddf79b762
James Newton-King <james@newtonking.com>
2021-08-03 09:23:33 +1200

    Review feedback

--- src/Servers/Connections.Abstractions/src/Features/IStreamAbortFeature.cs
+++ src/Servers/Connections.Abstractions/src/Features/IStreamAbortFeature.cs
@@ -12,14 +12,14 @@ namespace Microsoft.AspNetCore.Connections.Features
         /// Abort the read side of the connection stream.
         /// </summary>
         /// <param name="errorCode">The error code to send with the abort.</param>
-        /// <param name="abortReason">An optional <see cref="ConnectionAbortedException"/> describing the reason to abort the read side of the connection stream.</param>
+        /// <param name="abortReason">A <see cref="ConnectionAbortedException"/> describing the reason to abort the read side of the connection stream.</param>
         void AbortRead(long errorCode, ConnectionAbortedException abortReason);
 
         /// <summary>
         /// Abort the write side of the connection stream.
         /// </summary>
         /// <param name="errorCode">The error code to send with the abort.</param>
-        /// <param name="abortReason">An optional <see cref="ConnectionAbortedException"/> describing the reason to abort the write side of the connection stream.</param>
+        /// <param name="abortReason">A <see cref="ConnectionAbortedException"/> describing the reason to abort the write side of the connection stream.</param>
         void AbortWrite(long errorCode, ConnectionAbortedException abortReason);
     }
 }
