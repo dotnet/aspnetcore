4893067ca6a44fb4f6de2ac930671380d2e64f62
Tanay Parikh <taparik@microsoft.com>
2021-08-03 13:06:17 -0400

    ++PublicAPIs

--- src/Components/Web/src/Forms/InputLargeTextArea/InputLargeTextArea.cs
+++ src/Components/Web/src/Forms/InputLargeTextArea/InputLargeTextArea.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Diagnostics.CodeAnalysis;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.JSInterop;
 
