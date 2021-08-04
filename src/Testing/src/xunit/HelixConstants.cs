31703ad6a3b8f2b03f5f358efbbd5c37289fc869
Tanay Parikh <TanayParikh@users.noreply.github.com>
2021-08-03 22:53:11 -0400

    Update HelixConstants.cs

--- src/Testing/src/xunit/HelixConstants.cs
+++ src/Testing/src/xunit/HelixConstants.cs
@@ -6,6 +6,7 @@ namespace Microsoft.AspNetCore.Testing
     public static class HelixConstants
     {
         public const string Windows10Arm64 = "Windows.10.Arm64v8.Open;";
+        public const string Windows10Amd64Server20H2 = "Windows.10.Amd64.Server20H2.Open;"
         public const string DebianArm64 = "Debian.9.Arm64.Open;";
     }
 }
