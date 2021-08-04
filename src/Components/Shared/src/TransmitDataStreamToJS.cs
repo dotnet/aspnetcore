a3b90f1840e6c16ccd6904fde069445557099f11
Tanay Parikh <taparik@microsoft.com>
2021-08-04 12:54:46 -0400

    Cleanup usings

--- src/Components/Shared/src/TransmitDataStreamToJS.cs
+++ src/Components/Shared/src/TransmitDataStreamToJS.cs
@@ -1,10 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Buffers;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.JSInterop;
 
 namespace Microsoft.AspNetCore.Components
--- src/JSInterop/Microsoft.JSInterop/src/JSRuntime.cs
+++ src/JSInterop/Microsoft.JSInterop/src/JSRuntime.cs
@@ -1,14 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
 using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.JSInterop.Infrastructure;
 using static Microsoft.AspNetCore.Internal.LinkerFlags;
 
