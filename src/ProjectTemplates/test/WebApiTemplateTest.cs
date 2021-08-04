01d13fd249a5c6c2e35f3ba1d5f7921b2c187e20
Tanay Parikh <TanayParikh@users.noreply.github.com>
2021-08-03 22:58:22 -0400

    Update WebApiTemplateTest.cs

--- src/ProjectTemplates/test/WebApiTemplateTest.cs
+++ src/ProjectTemplates/test/WebApiTemplateTest.cs
@@ -45,7 +45,7 @@ namespace Templates.Test
         public Task WebApiTemplateFSharp() => WebApiTemplateCore(languageOverride: "F#");
 
         [ConditionalFact]
-        [SkipOnHelix("Cert failure, https://github.com/dotnet/aspnetcore/issues/28090", Queues = "All.OSX;" + HelixConstants.Windows10Arm64 + HelixConstants.DebianArm64)]
+        [SkipOnHelix("Cert failure, https://github.com/dotnet/aspnetcore/issues/28090", Queues = "All.OSX;" + HelixConstants.Windows10Arm64 + HelixConstants.DebianArm64 + HelixConstants.Windows10Amd64Server20H)]
         public Task WebApiTemplateCSharp() => WebApiTemplateCore(languageOverride: null);
 
         [ConditionalFact]
