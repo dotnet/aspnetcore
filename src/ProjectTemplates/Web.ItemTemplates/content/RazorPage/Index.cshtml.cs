e5e3924609f34d1cfb98160b72bb8a48757e692a
Phil Henning <phenning@microsoft.com>
2021-08-09 13:53:38 -0400

    Revert to block namespace

--- src/ProjectTemplates/Web.ItemTemplates/content/RazorPage/Index.cshtml.cs
+++ src/ProjectTemplates/Web.ItemTemplates/content/RazorPage/Index.cshtml.cs
@@ -5,15 +5,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyApp.Namespace;
-
-#if NameIsPage
-public class IndexModel : Microsoft.AspNetCore.Mvc.RazorPages.PageModel
-#else
-public class IndexModel : PageModel
-#endif
+namespace MyApp.Namespace
 {
-    public void OnGet()
+    #if NameIsPage
+    public class IndexModel : Microsoft.AspNetCore.Mvc.RazorPages.PageModel
+    #else
+    public class IndexModel : PageModel
+    #endif
     {
+        public void OnGet()
+        {
+        }
     }
 }
