ee3c49d3bfc1d2123701e20c8521dd3d96f7a636
Hao Kung <HaoK@users.noreply.github.com>
2021-08-10 14:08:33 -0700

    Revert

--- src/Servers/IIS/IIS/test/Common.FunctionalTests/FrebTests.cs
+++ src/Servers/IIS/IIS/test/Common.FunctionalTests/FrebTests.cs
@@ -49,6 +49,7 @@ namespace Microsoft.AspNetCore.Server.IIS.FunctionalTests
         }
 
         [ConditionalFact]
+        [MaximumOSVersion(OperatingSystems.Windows, WindowsVersions.Win10_20H2, SkipReason = "Shutdown hangs https://github.com/dotnet/aspnetcore/issues/25107")]
         [RequiresIIS(IISCapability.FailedRequestTracingModule)]
         public async Task CheckCommonFrebEvents()
         {
