5e6f3a426acca2673a51241e796783dc79f8f0b8
Hao Kung <HaoK@users.noreply.github.com>
2021-08-10 17:26:53 -0700

    Differentiate namespaces in IIS functionals (#35207)

--- src/Servers/IIS/IIS/test/Common.FunctionalTests/MaxRequestBodySizeTests.cs
+++ src/Servers/IIS/IIS/test/Common.FunctionalTests/MaxRequestBodySizeTests.cs
@@ -10,7 +10,20 @@ using Microsoft.AspNetCore.Server.IntegrationTesting.IIS;
 using Microsoft.AspNetCore.Testing;
 using Xunit;
 
+#if !IIS_FUNCTIONALS
+using Microsoft.AspNetCore.Server.IIS.FunctionalTests;
+
+#if IISEXPRESS_FUNCTIONALS
+namespace Microsoft.AspNetCore.Server.IIS.IISExpress.FunctionalTests
+#elif NEWHANDLER_FUNCTIONALS
+namespace Microsoft.AspNetCore.Server.IIS.NewHandler.FunctionalTests
+#elif NEWSHIM_FUNCTIONALS
+namespace Microsoft.AspNetCore.Server.IIS.NewShim.FunctionalTests
+#endif
+
+#else
 namespace Microsoft.AspNetCore.Server.IIS.FunctionalTests
+#endif
 {
     [Collection(PublishedSitesCollection.Name)]
     public class MaxRequestBodySizeTests : IISFunctionalTestBase
