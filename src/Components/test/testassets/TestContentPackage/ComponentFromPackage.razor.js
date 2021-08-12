6db97d33c7eebd3757fe11a290a0efe738d286e7
Javier Calvarro Nelson <jacalvar@microsoft.com>
2021-08-12 03:36:38 -0700

    Add additional E2E test for file modules

--- src/Components/test/testassets/TestContentPackage/ComponentFromPackage.razor.js
+++ src/Components/test/testassets/TestContentPackage/ComponentFromPackage.razor.js
@@ -1,3 +1,5 @@
-export function showPrompt(message) {
-    window.TestContentPackage.showPrompt(message);
+export function displayMessage(message) {
+    const element = document.createElement("p");
+    element.innerText = message;
+    document.querySelector('.js-module-message').appendChild(element);
 }
