ff51fd7105a9003841215f1f3b0b8fc9e2998a67
Hao Kung <HaoK@users.noreply.github.com>
2021-08-12 21:01:59 -0700

    Add ISocketConnectionContextFactory (#34769)

--- src/Servers/Kestrel/Transport.Sockets/src/Internal/SocketConnection.cs
+++ src/Servers/Kestrel/Transport.Sockets/src/Internal/SocketConnection.cs
@@ -1,13 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Buffers;
 using System.Diagnostics;
 using System.IO.Pipelines;
 using System.Net.Sockets;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Connections;
 using Microsoft.Extensions.Logging;
 
--- src/Servers/Kestrel/test/Sockets.BindTests/SocketTransportFactoryTests.cs
+++ src/Servers/Kestrel/test/Sockets.BindTests/SocketTransportFactoryTests.cs
@@ -17,7 +17,7 @@ namespace Sockets.BindTests
         [Fact]
         public async Task ThrowsNotImplementedExceptionWhenBindingToUriEndPoint()
         {
-            var socketTransportFactory = new SocketTransportFactory(Options.Create(new SocketTransportOptions()), Mock.Of<ILoggerFactory>());
+            var socketTransportFactory = new SocketTransportFactory(Options.Create(new SocketTransportOptions()), new LoggerFactory());
             await Assert.ThrowsAsync<NotImplementedException>(async () => await socketTransportFactory.BindAsync(new UriEndPoint(new Uri("http://127.0.0.1:5554"))));
         }
     }
