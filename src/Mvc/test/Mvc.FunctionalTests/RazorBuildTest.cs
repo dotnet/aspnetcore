4076badb85701330ea8531329e8896535a3551bd
Pranav K <prkrishn@hotmail.com>
2021-08-13 15:07:36 -0700

    Allow DefaultPageLoader.LoadAsync to work with CompiledPageActionDescriptor (#35325)
    With a fallback page, DefaultPageLoader might observe a mix of PageActionDescriptor
    and CompiledPageActionDescriptor instances. Currently encountering the latter results in an exception.
    
    Fixes https://github.com/dotnet/aspnetcore/issues/35060

--- src/Mvc/test/Mvc.FunctionalTests/RazorBuildTest.cs
+++ src/Mvc/test/Mvc.FunctionalTests/RazorBuildTest.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
@@ -48,6 +48,19 @@ namespace Microsoft.AspNetCore.Mvc.FunctionalTests
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             Assert.Equal("Hello from runtime-compiled rzc page!", responseBody.Trim());
+        }
+
+        [Fact]
+        public async Task RuntimeCompilation_WithFallbackPage_Works()
+        {
+            // Regression test for https://github.com/dotnet/aspnetcore/issues/35060
+            // Act
+            var response = await Client.GetAsync("Fallback");
+
+            // Assert
+            await response.AssertStatusCodeAsync(HttpStatusCode.OK);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            Assert.Equal("Hello from fallback page!", responseBody.Trim());
         }
 
         [Fact]
