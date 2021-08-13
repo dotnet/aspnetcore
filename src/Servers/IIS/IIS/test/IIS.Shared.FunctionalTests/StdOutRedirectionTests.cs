47732fbeb2b516145e9fead94da32ea51c3d2c38
William Godbe <wigodbe@microsoft.com>
2021-08-13 10:45:36 -0700

    Update src/Servers/IIS/IIS/test/IIS.Shared.FunctionalTests/StdOutRedirectionTests.cs
    Co-authored-by: Brennan <brecon@microsoft.com>

--- src/Servers/IIS/IIS/test/IIS.Shared.FunctionalTests/StdOutRedirectionTests.cs
+++ src/Servers/IIS/IIS/test/IIS.Shared.FunctionalTests/StdOutRedirectionTests.cs
@@ -56,7 +56,7 @@ namespace Microsoft.AspNetCore.Server.IIS.FunctionalTests
             StopServer();
 
             var contents = Helpers.ReadAllTextFromFile(Helpers.GetExpectedLogName(deploymentResult, LogFolderPath), Logger);
-            var expectedString = "TThe framework 'Microsoft.NETCore.App', version '2.9.9' (x64) was not found.";
+            var expectedString = "The framework 'Microsoft.NETCore.App', version '2.9.9' (x64) was not found.";
             EventLogHelpers.VerifyEventLogEvent(deploymentResult, expectedString, Logger);
             Assert.Contains(expectedString, contents);
         }
