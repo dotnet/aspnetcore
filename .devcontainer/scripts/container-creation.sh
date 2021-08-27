d74e7b9a8b9931e7eae70f3fb1acea710e2d5194
Tanay Parikh <TanayParikh@users.noreply.github.com>
2021-08-27 16:50:35 -0700

    Add `activate.sh` to devcontainer startup script

--- .devcontainer/scripts/container-creation.sh
+++ .devcontainer/scripts/container-creation.sh
@@ -6,9 +6,12 @@ set -e
 # the repo directory on the container so we do this post-create
 git submodule update --init --recursive
 # Install SDK and tool dependencies before container starts
-# Also run the full restore on the repo so that go-to definition
-# and other language features will be available in C# files
+# Also run the full restore and activate scripts on the repo so 
+# that go-to definition and other language features will be 
+# available in C# files
 ./restore.sh
+. ./activate.sh
+
 # The container creation script is executed in a new Bash instance
 # so we exit at the end to avoid the creation process lingering.
 exit
