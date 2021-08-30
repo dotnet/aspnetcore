3263a84c53ffb408e7a406242e628c972374ce97
Javier Calvarro Nelson <jacalvar@microsoft.com>
2021-08-30 08:03:33 -0700

    Update and fix test project

--- src/Components/WebAssembly/testassets/Wasm.Authentication.Server/Data/Migrations/20210830141745_UpdateSchemaFor60.Designer.cs
+++ src/Components/WebAssembly/testassets/Wasm.Authentication.Server/Data/Migrations/20210830141745_UpdateSchemaFor60.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Wasm.Authentication.Server.Data;
 
@@ -10,9 +11,10 @@ using Wasm.Authentication.Server.Data;
 namespace Wasm.Authentication.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210830141745_UpdateSchemaFor60")]
+    partial class UpdateSchemaFor60
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.0-alpha.1.21427.1");
---
+++ src/Components/WebAssembly/testassets/Wasm.Authentication.Server/Data/Migrations/20210830141745_UpdateSchemaFor60.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wasm.Authentication.Server.Data.Migrations
+{
+    public partial class UpdateSchemaFor60 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Version",
+                table: "Keys",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsX509Certificate",
+                table: "Keys",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "DataProtected",
+                table: "Keys",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersistedGrants_ConsumedTime",
+                table: "PersistedGrants",
+                column: "ConsumedTime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PersistedGrants_ConsumedTime",
+                table: "PersistedGrants");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Version",
+                table: "Keys",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsX509Certificate",
+                table: "Keys",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "DataProtected",
+                table: "Keys",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER");
+        }
+    }
+}
