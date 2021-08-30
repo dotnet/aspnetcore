1170d1909ad0d0bf12e534f65c9356cca3debf1c
Tanay Parikh <TanayParikh@users.noreply.github.com>
2021-08-30 16:20:21 -0700

    Update src/JSInterop/Microsoft.JSInterop/src/Infrastructure/IJSVoidResult.cs

--- src/JSInterop/Microsoft.JSInterop/src/Infrastructure/IJSVoidResult.cs
+++ src/JSInterop/Microsoft.JSInterop/src/Infrastructure/IJSVoidResult.cs
@@ -8,7 +8,6 @@ namespace Microsoft.JSInterop.Infrastructure
     /// Represents a void result from a JavaScript call.
     /// This property is public to support cross-assembly accessibility for WebAssembly and should not be used by user code.
     /// </summary>
-
     [EditorBrowsable(EditorBrowsableState.Never)]
     public interface IJSVoidResult
     {
