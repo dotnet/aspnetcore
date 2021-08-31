3fa23307b5bf5335004ffb91c76611e57f3b5cf0
Tanay Parikh <TanayParikh@users.noreply.github.com>
2021-08-31 12:51:54 -0700

    Stop Serializing Return Values for `InvokeVoidAsync` (#35807) (#35979)
    * Stop Serializing `InvokeVoidAsync` Return Value
    
    * Fix E2E Test
    
    * Add JSObjectReferenceTest
    
    * Remove `IJSVoidResult` from the public API
    
    * release .js files
    
    * Revert "Remove `IJSVoidResult` from the public API"
    
    This reverts commit e7548722677d81673e967fd8bc86d1e56479af3f.
    
    * Add monocrash to gitignore
    
    * Fix Tests
    
    * Update src/JSInterop/Microsoft.JSInterop/src/PublicAPI.Unshipped.txt
    
    * PR Feedback @pranavkm

--- src/Shared/JSInterop/JSCallResultTypeHelper.cs
+++ src/Shared/JSInterop/JSCallResultTypeHelper.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Reflection;
+using Microsoft.JSInterop.Infrastructure;
 
 namespace Microsoft.JSInterop
 {
@@ -12,21 +13,25 @@ namespace Microsoft.JSInterop
 
         public static JSCallResultType FromGeneric<TResult>()
         {
-            if (typeof(TResult).Assembly == _currentAssembly
-                && (typeof(TResult) == typeof(IJSObjectReference)
-                || typeof(TResult) == typeof(IJSInProcessObjectReference)
-                || typeof(TResult) == typeof(IJSUnmarshalledObjectReference)))
+            if (typeof(TResult).Assembly == _currentAssembly)
             {
-                return JSCallResultType.JSObjectReference;
+                if (typeof(TResult) == typeof(IJSObjectReference) ||
+                    typeof(TResult) == typeof(IJSInProcessObjectReference) ||
+                    typeof(TResult) == typeof(IJSUnmarshalledObjectReference))
+                {
+                    return JSCallResultType.JSObjectReference;
+                }
+                else if (typeof(TResult) == typeof(IJSStreamReference))
+                {
+                    return JSCallResultType.JSStreamReference;
+                }
+                else if (typeof(TResult) == typeof(IJSVoidResult))
+                {
+                    return JSCallResultType.JSVoidResult;
+                }
             }
-            else if (typeof(TResult).Assembly == _currentAssembly && typeof(TResult) == typeof(IJSStreamReference))
-            {
-                return JSCallResultType.JSStreamReference;
-            }
-            else
-            {
-                return JSCallResultType.Default;
-            }
+
+            return JSCallResultType.Default;
         }
     }
 }
