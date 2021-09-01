b3a7cc6aad8fec2730a215b9ecda8785e0e2ed35
Tanay Parikh <TanayParikh@users.noreply.github.com>
2021-08-31 18:34:21 -0700

    Add .NET Dev Certs to DevContainer By Default

--- .devcontainer/scripts/container-creation.sh
+++ .devcontainer/scripts/container-creation.sh
@@ -5,10 +5,15 @@ set -e
 # GitHub doesn't pull in submodules by default when mounting
 # the repo directory on the container so we do this post-create
 git submodule update --init --recursive
+
 # Install SDK and tool dependencies before container starts
 # Also run the full restore on the repo so that go-to definition
 # and other language features will be available in C# files
 ./restore.sh
+
+# Add .NET Dev Certs to environment to facilitate debugging.
+dotnet dev-certs https
+
 # The container creation script is executed in a new Bash instance
 # so we exit at the end to avoid the creation process lingering.
 exit
