1c65ffac3e93d48710c82ce36fa796cbebeb50fc
Tanay Parikh <TanayParikh@users.noreply.github.com>
2021-09-01 09:34:27 -0700

    Update .devcontainer/scripts/container-creation.sh

--- .devcontainer/scripts/container-creation.sh
+++ .devcontainer/scripts/container-creation.sh
@@ -12,6 +12,8 @@ git submodule update --init --recursive
 ./restore.sh
 
 # Add .NET Dev Certs to environment to facilitate debugging.
+# Do **NOT** do this in a public base image as all images inheriting
+# from the base image would inherit these dev certs as well. 
 dotnet dev-certs https
 
 # The container creation script is executed in a new Bash instance
