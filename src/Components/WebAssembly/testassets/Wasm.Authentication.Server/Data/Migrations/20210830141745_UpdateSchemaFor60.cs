9e245a2b5988902d25fa79805d75eab4070591af
Javier Calvarro Nelson <jacalvar@microsoft.com>
2021-09-02 02:50:50 -0700

    Add license header

--- src/Components/WebAssembly/testassets/Wasm.Authentication.Server/Data/Migrations/20210830141745_UpdateSchemaFor60.cs
+++ src/Components/WebAssembly/testassets/Wasm.Authentication.Server/Data/Migrations/20210830141745_UpdateSchemaFor60.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
