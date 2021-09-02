dda0bf83607bf557c0817ac33f937ad257701303
Kahbazi <A.Kahbazi@gmail.com>
2021-09-02 22:19:31 +0430

    Store and reuse health status for health checks

--- src/Middleware/HealthChecks/src/HealthCheckResponseWriters.cs
+++ src/Middleware/HealthChecks/src/HealthCheckResponseWriters.cs
@@ -1,7 +1,7 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
@@ -9,10 +9,26 @@ namespace Microsoft.AspNetCore.Diagnostics.HealthChecks
 {
     internal static class HealthCheckResponseWriters
     {
+        private static readonly byte[] DegradedBytes = Encoding.UTF8.GetBytes(HealthStatus.Degraded.ToString());
+        private static readonly byte[] HealthyBytes = Encoding.UTF8.GetBytes(HealthStatus.Healthy.ToString());
+        private static readonly byte[] UnhealthyBytes = Encoding.UTF8.GetBytes(HealthStatus.Unhealthy.ToString());
+
         public static Task WriteMinimalPlaintext(HttpContext httpContext, HealthReport result)
         {
             httpContext.Response.ContentType = "text/plain";
-            return httpContext.Response.WriteAsync(result.Status.ToString());
+            return result.Status switch
+            {
+                HealthStatus.Degraded => Write(httpContext, DegradedBytes),
+                HealthStatus.Healthy => Write(httpContext, HealthyBytes),
+                HealthStatus.Unhealthy => Write(httpContext, UnhealthyBytes),
+                _ => httpContext.Response.WriteAsync(result.Status.ToString())
+            };
+
+            async static Task Write(HttpContext context, byte[] bytes)
+            {
+                await context.Response.Body.WriteAsync(bytes.AsMemory());
+                await context.Response.Body.FlushAsync();
+            }
         }
     }
 }
