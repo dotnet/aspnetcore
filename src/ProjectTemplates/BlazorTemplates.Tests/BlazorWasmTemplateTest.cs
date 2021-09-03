281976afff70943f8424bcea57b0810b8e7db51d
Javier Calvarro Nelson <jacalvar@microsoft.com>
2021-09-03 13:10:17 -0700

    Fix tests

--- src/ProjectTemplates/BlazorTemplates.Tests/BlazorWasmTemplateTest.cs
+++ src/ProjectTemplates/BlazorTemplates.Tests/BlazorWasmTemplateTest.cs
@@ -420,8 +420,7 @@ namespace Templates.Test
         private async Task TestBasicNavigation(string appName, IPage page, bool usesAuth = false, bool skipFetchData = false)
         {
             await page.WaitForSelectorAsync("nav");
-
-            Assert.Equal("Index", (await page.GetTitleAsync()).Trim());
+            Assert.Equal(usesAuth ? appName.Trim() : "Index", (await page.GetTitleAsync()).Trim());
 
             // Initially displays the home page
             await page.WaitForSelectorAsync("h1 >> text=Hello, world!");
