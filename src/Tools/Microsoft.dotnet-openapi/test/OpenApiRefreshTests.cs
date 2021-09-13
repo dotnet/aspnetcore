da3f65a6cc608c7a9342392954541e4287f2bfe5
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2021-09-12 17:00:52 -0700

    [release/3.1] Handle shortened JSON file in `dotnet openapi`
    * backport of #36362
    * handle shortened JSON file in `dotnet openapi`
      - #35767
      - an existing JSON file must be truncated
    * extend `dotnet openapi refresh` tests
      - include regression test for #35767
    * !fixup! Get test class compiling in this branch
      - `AssertNoErrors(...)` doesn't exist here

--- src/Tools/Microsoft.dotnet-openapi/test/OpenApiRefreshTests.cs
+++ src/Tools/Microsoft.dotnet-openapi/test/OpenApiRefreshTests.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.IO;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.DotNet.OpenApi.Tests;
 using Xunit;
@@ -20,29 +19,90 @@ namespace Microsoft.DotNet.OpenApi.Refresh.Tests
         {
             CreateBasicProject(withOpenApi: false);
 
+            // Add <OpenApiReference/> to the project. Ignore initial filename.json content.
             var app = GetApplication();
             var run = app.Execute(new[] { "add", "url", FakeOpenApiUrl });
 
-            Assert.True(string.IsNullOrEmpty(_error.ToString()), $"Threw error: {_error.ToString()}");
+            Assert.True(string.IsNullOrEmpty(_error.ToString()), $"Threw error: {_error}");
             Assert.Equal(0, run);
 
+            // File will grow after the refresh.
             var expectedJsonPath = Path.Combine(_tempDir.Root, "filename.json");
-            var json = await File.ReadAllTextAsync(expectedJsonPath);
-            json += "trash";
-            await File.WriteAllTextAsync(expectedJsonPath, json);
+            await File.WriteAllTextAsync(expectedJsonPath, "trash");
 
             var firstWriteTime = File.GetLastWriteTime(expectedJsonPath);
 
-            Thread.Sleep(TimeSpan.FromSeconds(1));
+            await Task.Delay(TimeSpan.FromSeconds(1));
 
             app = GetApplication();
             run = app.Execute(new[] { "refresh", FakeOpenApiUrl });
 
-            Assert.True(string.IsNullOrEmpty(_error.ToString()), $"Threw error: {_error.ToString()}");
+            Assert.True(string.IsNullOrEmpty(_error.ToString()), $"Threw error: {_error}");
             Assert.Equal(0, run);
 
             var secondWriteTime = File.GetLastWriteTime(expectedJsonPath);
             Assert.True(firstWriteTime < secondWriteTime, $"File wasn't updated! {firstWriteTime} {secondWriteTime}");
+            Assert.Equal(Content, await File.ReadAllTextAsync(expectedJsonPath), ignoreLineEndingDifferences: true);
+        }
+
+        // Regression test for #35767 scenario.
+        [Fact]
+        public async Task OpenApi_Refresh_MuchShorterFile()
+        {
+            CreateBasicProject(withOpenApi: false);
+
+            // Add <OpenApiReference/> to the project. Ignore initial filename.json content.
+            var app = GetApplication();
+            var run = app.Execute(new[] { "add", "url", FakeOpenApiUrl });
+
+            Assert.True(string.IsNullOrEmpty(_error.ToString()), $"Threw error: {_error}");
+            Assert.Equal(0, run);
+
+            // File will shrink after the refresh.
+            var expectedJsonPath = Path.Combine(_tempDir.Root, "filename.json");
+            await File.WriteAllTextAsync(expectedJsonPath, PackageUrlContent);
+
+            var firstWriteTime = File.GetLastWriteTime(expectedJsonPath);
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            app = GetApplication();
+            run = app.Execute(new[] { "refresh", FakeOpenApiUrl });
+
+            Assert.True(string.IsNullOrEmpty(_error.ToString()), $"Threw error: {_error}");
+            Assert.Equal(0, run);
+
+            var secondWriteTime = File.GetLastWriteTime(expectedJsonPath);
+            Assert.True(firstWriteTime < secondWriteTime, $"File wasn't updated! {firstWriteTime} {secondWriteTime}");
+            Assert.Equal(Content, await File.ReadAllTextAsync(expectedJsonPath), ignoreLineEndingDifferences: true);
+        }
+
+        [Fact]
+        public async Task OpenApi_Refresh_UnchangedFile()
+        {
+            CreateBasicProject(withOpenApi: false);
+
+            // Add <OpenApiReference/> to the project and write the filename.json file.
+            var app = GetApplication();
+            var run = app.Execute(new[] { "add", "url", FakeOpenApiUrl });
+
+            Assert.True(string.IsNullOrEmpty(_error.ToString()), $"Threw error: {_error}");
+            Assert.Equal(0, run);
+
+            var expectedJsonPath = Path.Combine(_tempDir.Root, "filename.json");
+            var firstWriteTime = File.GetLastWriteTime(expectedJsonPath);
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            app = GetApplication();
+            run = app.Execute(new[] { "refresh", FakeOpenApiUrl });
+
+            Assert.True(string.IsNullOrEmpty(_error.ToString()), $"Threw error: {_error}");
+            Assert.Equal(0, run);
+
+            var secondWriteTime = File.GetLastWriteTime(expectedJsonPath);
+            Assert.Equal(firstWriteTime, secondWriteTime);
+            Assert.Equal(Content, await File.ReadAllTextAsync(expectedJsonPath));
         }
     }
 }
