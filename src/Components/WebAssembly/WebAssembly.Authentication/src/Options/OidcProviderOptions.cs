8d1e55e6118efdc958b31d94e0c784dd65bf4eba
Jonathan <vanillajonathan@users.noreply.github.com>
2021-09-14 13:58:01 +0200

    Improve documentation

--- src/Components/WebAssembly/WebAssembly.Authentication/src/Options/OidcProviderOptions.cs
+++ src/Components/WebAssembly/WebAssembly.Authentication/src/Options/OidcProviderOptions.cs
@@ -7,17 +7,17 @@ using System.Text.Json.Serialization;
 namespace Microsoft.AspNetCore.Components.WebAssembly.Authentication
 {
     /// <summary>
-    /// Represents options to pass down to configure the oidc-client.js library used when using a standard OIDC flow.
+    /// Represents options to pass down to configure the oidc-client.js library used when using a standard OpenID Connect (OIDC) flow.
     /// </summary>
     public class OidcProviderOptions
     {
         /// <summary>
-        /// Gets or sets the authority of the OIDC identity provider.
+        /// Gets or sets the authority of the OpenID Connect (OIDC) identity provider.
         /// </summary>
         public string Authority { get; set; }
 
         /// <summary>
-        /// Gets or sets the metadata url of the oidc provider.
+        /// Gets or sets the metadata URL of the OpenID Connect (OIDC) provider.
         /// </summary>
         public string MetadataUrl { get; set; }
 
@@ -33,14 +33,14 @@ namespace Microsoft.AspNetCore.Components.WebAssembly.Authentication
         public IList<string> DefaultScopes { get; } = new List<string> { "openid", "profile" };
 
         /// <summary>
-        /// Gets or sets the redirect uri for the application. The application will be redirected here after the user has completed the sign in
+        /// Gets or sets the redirect URI for the application. The application will be redirected here after the user has completed the sign in
         /// process from the identity provider.
         /// </summary>
         [JsonPropertyName("redirect_uri")]
         public string RedirectUri { get; set; }
 
         /// <summary>
-        /// Gets or sets the post logout redirect uri for the application. The application will be redirected here after the user has completed the sign out
+        /// Gets or sets the post logout redirect URI for the application. The application will be redirected here after the user has completed the sign out
         /// process from the identity provider.
         /// </summary>
         [JsonPropertyName("post_logout_redirect_uri")]
