1ac4ccbd56ba35e419ee17313c1320bec5de9f04
Hao Kung <HaoK@users.noreply.github.com>
2021-09-29 09:17:47 -0700

    Fix error in startup test (#37076)

--- src/Servers/IIS/IIS/test/Common.FunctionalTests/StartupTests.cs
+++ src/Servers/IIS/IIS/test/Common.FunctionalTests/StartupTests.cs
@@ -445,18 +445,11 @@ namespace Microsoft.AspNetCore.Server.IIS.FunctionalTests
 
             File.Delete(Path.Combine(deploymentResult.ContentRoot, "aspnetcorev2_inprocess.dll"));
 
-            if (DeployerSelector.HasNewShim && DeployerSelector.HasNewHandler)
+            if (DeployerSelector.HasNewShim)
             {
                 await AssertSiteFailsToStartWithInProcessStaticContent(deploymentResult, "500.33");
 
                 EventLogHelpers.VerifyEventLogEvent(deploymentResult, EventLogHelpers.InProcessFailedToFindRequestHandler(deploymentResult), Logger);
-            }
-            else if (DeployerSelector.HasNewShim)
-            {
-                // Forwards compat tests fail earlier due to a error with the M.AspNetCore.Server.IIS package.
-                await AssertSiteFailsToStartWithInProcessStaticContent(deploymentResult, "500.31");
-
-                EventLogHelpers.VerifyEventLogEvent(deploymentResult, EventLogHelpers.InProcessFailedToFindNativeDependencies(deploymentResult), Logger);
             }
             else
             {
