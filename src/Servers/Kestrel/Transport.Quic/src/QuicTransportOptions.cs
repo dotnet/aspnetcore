37f9ce14dc4b2d0c73085c060c66287e2884f745
Chris R <Tratcher@outlook.com>
2021-10-01 14:02:17 -0700

    Remove RequiresPreviewFeatures

--- src/Servers/Kestrel/Transport.Quic/src/QuicTransportOptions.cs
+++ src/Servers/Kestrel/Transport.Quic/src/QuicTransportOptions.cs
@@ -12,7 +12,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Quic
     /// <summary>
     /// Options for Quic based connections.
     /// </summary>
-    [RequiresPreviewFeatures("Kestrel HTTP/3 support for .NET 6 is in preview.", Url = "https://aka.ms/aspnet/kestrel/http3reqs")]
     public class QuicTransportOptions
     {
         /// <summary>
