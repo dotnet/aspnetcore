5d7ab313e040c5f89cacaccd3b55fa8800f47172
Hao Kung <HaoK@users.noreply.github.com>
2021-10-08 12:54:56 -0700

    Add task using

--- src/Servers/IIS/IIS/test/testassets/InProcessWebSite/Program.cs
+++ src/Servers/IIS/IIS/test/testassets/InProcessWebSite/Program.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Server;
