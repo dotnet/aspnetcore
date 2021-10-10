09068cf2ed014d5edc0358f1fb613404a38ffdb1
martincostello <martin@martincostello.com>
2021-10-10 09:32:59 +0100

    Fix IDE0059 suggestion
    Fix IDE0059 suggestion by using _ for the variable name.

--- src/Servers/Kestrel/Core/src/Internal/Infrastructure/HttpCharacters.cs
+++ src/Servers/Kestrel/Core/src/Internal/Infrastructure/HttpCharacters.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Runtime.CompilerServices;
 
 namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Infrastructure
@@ -18,7 +17,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Infrastructure
         internal static void Initialize()
         {
             // Access _alphaNumeric to initialize static fields
-            var initialize = _alphaNumeric;
+            var _ = _alphaNumeric;
         }
 
         private static bool[] InitializeAlphaNumeric()
