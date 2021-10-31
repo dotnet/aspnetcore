4b5c27ee50440d8ac1807ced00b5f34c4bbf3f6e
Artak <34246760+mkArtakMSFT@users.noreply.github.com>
2021-10-31 14:28:04 -0700

    Update ErrorViewModel.cs

--- src/ProjectTemplates/Web.ProjectTemplates/content/StarterWeb-CSharp/Models/ErrorViewModel.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/StarterWeb-CSharp/Models/ErrorViewModel.cs
@@ -7,5 +7,7 @@ namespace Company.WebApplication1.Models
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        
+        public string SomeTestProperty => "This is just a test";
     }
 }
