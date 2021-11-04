3fb735e82f7970c0d01a315355eea2c999bf5b1f
Stephen Halter <halter73@gmail.com>
2021-11-04 13:30:29 -0700

    Improve exception messages in EndpointMiddleware

--- src/Http/Routing/test/UnitTests/EndpointMiddlewareTest.cs
+++ src/Http/Routing/test/UnitTests/EndpointMiddlewareTest.cs
@@ -1,16 +1,12 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
 using Moq;
-using Xunit;
 
 namespace Microsoft.AspNetCore.Routing
 {
@@ -101,8 +97,8 @@ namespace Microsoft.AspNetCore.Routing
             // Arrange
             var expected = "Endpoint Test contains authorization metadata, but a middleware was not found that supports authorization." +
                 Environment.NewLine +
-                "Configure your application startup by adding app.UseAuthorization() inside the call to Configure(..) in the application startup code. " +
-                "The call to app.UseAuthorization() must appear between app.UseRouting() and app.UseEndpoints(...).";
+                "Configure your application startup by adding app.UseAuthorization() in the application startup code. " +
+                "If there are calls to app.UseRouting() and app.UseEndpoints(...), the call to app.UseAuthorization() must go between them.";
             var httpContext = new DefaultHttpContext
             {
                 RequestServices = new ServiceProvider()
@@ -198,8 +194,8 @@ namespace Microsoft.AspNetCore.Routing
             // Arrange
             var expected = "Endpoint Test contains CORS metadata, but a middleware was not found that supports CORS." +
                 Environment.NewLine +
-                "Configure your application startup by adding app.UseCors() inside the call to Configure(..) in the application startup code. " +
-                "The call to app.UseCors() must appear between app.UseRouting() and app.UseEndpoints(...).";
+                "Configure your application startup by adding app.UseCors() in the application startup code. " +
+                "If there are calls to app.UseRouting() and app.UseEndpoints(...), the call to app.UseCors() must go between them.";
             var httpContext = new DefaultHttpContext
             {
                 RequestServices = new ServiceProvider()
