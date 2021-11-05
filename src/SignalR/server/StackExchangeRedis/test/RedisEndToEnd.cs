e63ad982661120a1744f857c3ff9e0dee3039606
Safia Abdalla <safia@microsoft.com>
2021-11-04 17:05:38 -0700

    Unquarantine RedisEndToEnd tests (#38095)
    * Unquarantine CanSendAndReceiveUserMessagesFromMultipleConnectionsWithSameUser
    
    * Unquarantine more passing RedisEndToEnd tests

--- src/SignalR/server/StackExchangeRedis/test/RedisEndToEnd.cs
+++ src/SignalR/server/StackExchangeRedis/test/RedisEndToEnd.cs
@@ -60,7 +60,6 @@ namespace Microsoft.AspNetCore.SignalR.StackExchangeRedis.Tests
         [ConditionalTheory]
         [SkipIfDockerNotPresent]
         [MemberData(nameof(TransportTypesAndProtocolTypes))]
-        [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/33851")]
         public async Task HubConnectionCanSendAndReceiveGroupMessages(HttpTransportType transportType, string protocolName)
         {
             using (StartVerifiableLog())
@@ -93,7 +92,6 @@ namespace Microsoft.AspNetCore.SignalR.StackExchangeRedis.Tests
         [ConditionalTheory]
         [SkipIfDockerNotPresent]
         [MemberData(nameof(TransportTypesAndProtocolTypes))]
-        [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/37080")]
         public async Task CanSendAndReceiveUserMessagesFromMultipleConnectionsWithSameUser(HttpTransportType transportType, string protocolName)
         {
             using (StartVerifiableLog())
@@ -123,7 +121,6 @@ namespace Microsoft.AspNetCore.SignalR.StackExchangeRedis.Tests
         [ConditionalTheory]
         [SkipIfDockerNotPresent]
         [MemberData(nameof(TransportTypesAndProtocolTypes))]
-        [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/33851")]
         public async Task CanSendAndReceiveUserMessagesWhenOneConnectionWithUserDisconnects(HttpTransportType transportType, string protocolName)
         {
             // Regression test:
