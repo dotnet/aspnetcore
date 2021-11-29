0d1dd355b59a0b0933803fa49f2eacd50a25ac91
Tanay Parikh <taparik@microsoft.com>
2021-11-29 11:32:23 -0800

    Use DOTNET_ROLL_FORWARD with `dotnet` `playwright` tool

--- eng/helix/content/RunTests/TestRunner.cs
+++ eng/helix/content/RunTests/TestRunner.cs
@@ -111,18 +111,18 @@ namespace RunTests
             {
                 Console.WriteLine($"Installing playwright npm package");
 
-                await ProcessUtil.RunAsync($"npm",
-                    $"install playwright",
+                await ProcessUtil.RunAsync($"{Options.DotnetRoot}/dotnet",
+                    $"tool install Microsoft.Playwright.CLI --tool-path {Options.HELIX_WORKITEM_ROOT}",
                     environmentVariables: EnvironmentVariables,
                     outputDataReceived: Console.WriteLine,
                     errorDataReceived: Console.Error.WriteLine,
                     throwOnError: false,
                     cancellationToken: new CancellationTokenSource(TimeSpan.FromMinutes(2)).Token);
 
-                Console.WriteLine($"Installing playwright Browsers");
+                Console.WriteLine($"Installing Playwright Browsers: {Environment.GetEnvironmentVariable("PLAYWRIGHT_BROWSERS_PATH")}");
 
-                await ProcessUtil.RunAsync($"playwright",
-                    $"install",
+                await ProcessUtil.RunAsync($"{Options.DotnetRoot}/dotnet",
+                    "playwright install",
                     environmentVariables: EnvironmentVariables,
                     outputDataReceived: Console.WriteLine,
                     errorDataReceived: Console.Error.WriteLine,
