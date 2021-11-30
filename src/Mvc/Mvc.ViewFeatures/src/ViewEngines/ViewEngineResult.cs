ae4559ecae652b0d83e372d04eb03f6b6e21bc17
Pranav K <prkrishn@hotmail.com>
2021-11-30 11:10:33 -0800

    Update src/Mvc/Mvc.ViewFeatures/src/ViewEngines/ViewEngineResult.cs

--- src/Mvc/Mvc.ViewFeatures/src/ViewEngines/ViewEngineResult.cs
+++ src/Mvc/Mvc.ViewFeatures/src/ViewEngines/ViewEngineResult.cs
@@ -12,7 +12,7 @@ using Microsoft.AspNetCore.Mvc.ViewFeatures;
 namespace Microsoft.AspNetCore.Mvc.ViewEngines;
 
 /// <summary>
-/// Represents the result of a <see cref="IViewEngine" >view engine</see >.
+/// Represents the result of a <see cref="IViewEngine" />.
 /// </summary>
 public class ViewEngineResult
 {
