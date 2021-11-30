4d87cc897de94d1d8f051d3adfe730e9b636f34f
Christopher Yeleighton <ne01026@shark.2a.pl>
2021-11-30 20:45:12 +0100

    an <see cref="IViewEngine"

--- src/Mvc/Mvc.ViewFeatures/src/ViewEngines/ViewEngineResult.cs
+++ src/Mvc/Mvc.ViewFeatures/src/ViewEngines/ViewEngineResult.cs
@@ -12,7 +12,7 @@ using Microsoft.AspNetCore.Mvc.ViewFeatures;
 namespace Microsoft.AspNetCore.Mvc.ViewEngines;
 
 /// <summary>
-/// Represents the result of a <see cref="IViewEngine" />.
+/// Represents the result of an <see cref="IViewEngine" />.
 /// </summary>
 public class ViewEngineResult
 {
