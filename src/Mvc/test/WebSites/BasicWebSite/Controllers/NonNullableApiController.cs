20f69813b9985f568849f7a431ea092dc943e5dc
Bruno Oliveira <brolivei@microsoft.com>
2021-12-03 17:20:09 -0800

    Adding end-to-end test

---
+++ src/Mvc/test/WebSites/BasicWebSite/Controllers/NonNullableApiController.cs
@@ -0,0 +1,23 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicWebSite.Controllers;
+
+#nullable enable
+
+[ApiController]
+[Route("api/NonNullable")]
+public class NonNullableApiController : ControllerBase
+{
+    // GET: api/<controller>
+    [HttpGet]
+    public ActionResult<string> Get(string language = "pt-br")
+    {
+        return language;
+    }
+}
+
+#nullable restore
