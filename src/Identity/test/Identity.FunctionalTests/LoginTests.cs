0d0ddd8284e8e54ed34981bf383ca11b61d25a2e
Will Godbe <wigodbe@microsoft.com>
2021-12-07 10:12:54 -0800

    Fixup

--- src/Identity/test/Identity.FunctionalTests/LoginTests.cs
+++ src/Identity/test/Identity.FunctionalTests/LoginTests.cs
@@ -5,6 +5,7 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Identity.DefaultUI.WebSite;
+using Microsoft.AspNetCore.Testing.xunit;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
--- src/Tools/dotnet-watch/test/FileWatcherTests.cs
+++ src/Tools/dotnet-watch/test/FileWatcherTests.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
+using Microsoft.AspNetCore.Testing.xunit;
 using Microsoft.DotNet.Watcher.Internal;
 using Xunit;
 using Xunit.Abstractions;
