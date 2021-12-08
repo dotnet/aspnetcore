a06ffd7c0d3327cbf7061e87bd1ed59c0b7a8c8b
Christopher Yeleighton <ne01026@shark.2a.pl>
2021-12-08 20:06:00 +0100

    language-agnostic hyperlink
    Note: I really prefer it to be en-us because the translations are hardly intelligible, so the first thing I do is to switch to the original, which is only one link away but the time and bandwidth is wasted.
    
    Co-authored-by: Chris Ross <Tratcher@Outlook.com>

--- src/Mvc/Mvc.ViewFeatures/src/Rendering/IHtmlHelperOfT.cs
+++ src/Mvc/Mvc.ViewFeatures/src/Rendering/IHtmlHelperOfT.cs
@@ -71,7 +71,7 @@ public interface IHtmlHelper<TModel> : IHtmlHelper
     /// </para>
     /// <para>
     /// Custom templates are found under a <c>DisplayTemplates</c> folder within the
-    /// <see href="https://docs.microsoft.com/en-us/aspnet/core/tutorials/razor-pages/razor-pages-start#pages-folder-2" >Pages</see > folder.
+    /// <see href="https://docs.microsoft.com/aspnet/core/tutorials/razor-pages/razor-pages-start#pages-folder-2" >Pages</see > folder.
     /// The folder name is case-sensitive on
     /// case-sensitive file systems.
     /// </para>
