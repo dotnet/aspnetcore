383a918668246031333d6e791436471205fff70e
Hao Kung <HaoK@users.noreply.github.com>
2021-12-13 09:59:43 -0800

    Add arm64 to tests and assets

--- src/Servers/IIS/tools/GenerateNativeAssets.ps1
+++ src/Servers/IIS/tools/GenerateNativeAssets.ps1
@@ -6,6 +6,10 @@ $platforms = @(
         VCPlatform = "x64";
     },
     @{
+        Platform = "ARM64";
+        VCPlatform = "ARM64";
+    },
+    @{
         Platform = "x86";
         VCPlatform = "Win32";
     }
@@ -125,9 +129,10 @@ $content = @"
 <Project>
     <PropertyGroup>
         <PackNativeAssets Condition="'`$(BuildIisNativeProjects)' == 'true'">true</PackNativeAssets>
-        <NativePlatform Condition="'`$(Platform)' == 'AnyCPU'">x64</NativePlatform>
+        <NativePlatform Condition="'`$(Platform)' == 'AnyCPU'">arm64</NativePlatform>
+        <NativePlatform Condition="'`$(Platform)' == 'arm64'">arm64</NativePlatform>
         <NativePlatform Condition="'`$(NativePlatform)' == ''">`$(Platform)</NativePlatform>
-        <NativeVCPlatform Condition="'`$(NativePlatform)' == 'x86'">Win32</NativeVCPlatform>
+        <NativeVCPlatform Condition="'`$(NativePlatform)' == 'x86'">Win64</NativeVCPlatform>
         <NativeVCPlatform Condition="'`$(NativeVCPlatform)' == ''">`$(NativePlatform)</NativeVCPlatform>
     </PropertyGroup>
     <ItemGroup>
