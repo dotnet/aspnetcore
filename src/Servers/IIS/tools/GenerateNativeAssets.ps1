193dd372a5ca6266db01183872fab729f966be70
Hao Kung <HaoK@users.noreply.github.com>
2021-12-16 13:41:40 -0800

    Revert "Installer arm64 stuff"
    This reverts commit 93154f143a0f06bc3bc4b83aa16463a331e6ceb2.

--- src/Servers/IIS/tools/GenerateNativeAssets.ps1
+++ src/Servers/IIS/tools/GenerateNativeAssets.ps1
@@ -130,6 +130,7 @@ $content = @"
     <PropertyGroup>
         <PackNativeAssets Condition="'`$(BuildIisNativeProjects)' == 'true'">true</PackNativeAssets>
         <NativePlatform Condition="'`$(Platform)' == 'AnyCPU'">x64</NativePlatform>
+        <NativePlatform Condition="'`$(Platform)' == 'arm64'">arm64</NativePlatform>
         <NativePlatform Condition="'`$(NativePlatform)' == ''">`$(Platform)</NativePlatform>
         <NativeVCPlatform Condition="'`$(NativePlatform)' == 'x86'">Win32</NativeVCPlatform>
         <NativeVCPlatform Condition="'`$(NativeVCPlatform)' == ''">`$(NativePlatform)</NativeVCPlatform>
