f1d2f19f01d67522908f9507a2770ce46014a97e
Tanay Parikh <TanayParikh@users.noreply.github.com>
2021-12-23 04:29:28 +0000

    Add headers to TS files

--- src/Components/Web.JS/src/Platform/Mono/MonoDebugger.ts
+++ src/Components/Web.JS/src/Platform/Mono/MonoDebugger.ts
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 import { WebAssemblyResourceLoader } from '../WebAssemblyResourceLoader';
 
 const currentBrowserIsChrome = (window as any).chrome
