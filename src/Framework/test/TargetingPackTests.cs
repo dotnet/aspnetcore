b8d524c6169d65206788ccd2eccf2512b1abcdc9
Shreyas Jejurkar <shreyasjejurkar123@live.com>
2021-12-24 07:18:18 +0000

    Framework

--- src/Framework/test/TargetingPackTests.cs
+++ src/Framework/test/TargetingPackTests.cs
@@ -1,18 +1,13 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Reflection;
 using System.Reflection.Metadata;
 using System.Reflection.PortableExecutable;
 using System.Runtime.CompilerServices;
 using System.Xml.Linq;
 using NuGet.Versioning;
-using Xunit;
 using Xunit.Abstractions;
 
 namespace Microsoft.AspNetCore;
