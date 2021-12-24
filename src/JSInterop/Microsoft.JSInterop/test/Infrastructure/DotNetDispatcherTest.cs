0d9683b608c7851d61f72f92d96f5b220474e348
Shreyas Jejurkar <shreyasjejurkar123@live.com>
2021-12-24 07:20:13 +0000

    JSInterop

--- src/JSInterop/Microsoft.JSInterop/test/Infrastructure/DotNetDispatcherTest.cs
+++ src/JSInterop/Microsoft.JSInterop/test/Infrastructure/DotNetDispatcherTest.cs
@@ -2,11 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 #nullable disable
-using System;
-using System.Linq;
 using System.Text.Json;
-using System.Threading.Tasks;
-using Xunit;
 
 namespace Microsoft.JSInterop.Infrastructure;
 
