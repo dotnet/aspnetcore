9777b5240d1de097796c8f2b29b3e03f8d284f20
Bruno Oliveira <brolivei@microsoft.com>
2022-01-03 15:11:14 -0800

    Adding unit test

--- src/Mvc/Mvc.Core/test/ModelBinding/Validation/DefaultComplexObjectValidationStrategyTest.cs
+++ src/Mvc/Mvc.Core/test/ModelBinding/Validation/DefaultComplexObjectValidationStrategyTest.cs
@@ -4,6 +4,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+
+using Moq;
+
 using Xunit;
 
 namespace Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
@@ -45,6 +49,46 @@ public class DefaultComplexObjectValidationStrategyTest
             entry =>
             {
                 Assert.Equal("prefix.Name", entry.Key);
+                Assert.Equal("Joey", entry.Model);
+                Assert.Same(metadata.Properties["Name"], entry.Metadata);
+            });
+    }
+
+    [Fact]
+    public void GetChildren_ReturnsExpectedElements_WithValidationModelName()
+    {
+        // Arrange
+        var model = new Person()
+        {
+            Age = 23,
+            Id = 1,
+            Name = "Joey",
+        };
+
+        var metadata = TestModelMetadataProvider.CreateDefaultProvider(new List<IMetadataDetailsProvider> { new TestValidationModelNameProvider() }).GetMetadataForType(typeof(Person));
+        var strategy = DefaultComplexObjectValidationStrategy.Instance;
+
+        // Act
+        var enumerator = strategy.GetChildren(metadata, "prefix", model);
+
+        // Assert
+        Assert.Collection(
+            BufferEntries(enumerator).OrderBy(e => e.Key),
+            entry =>
+            {
+                Assert.Equal("prefix.AGE", entry.Key);
+                Assert.Equal(23, entry.Model);
+                Assert.Same(metadata.Properties["Age"], entry.Metadata);
+            },
+            entry =>
+            {
+                Assert.Equal("prefix.ID", entry.Key);
+                Assert.Equal(1, entry.Model);
+                Assert.Same(metadata.Properties["Id"], entry.Metadata);
+            },
+            entry =>
+            {
+                Assert.Equal("prefix.NAME", entry.Key);
                 Assert.Equal("Joey", entry.Model);
                 Assert.Same(metadata.Properties["Name"], entry.Metadata);
             });
@@ -153,5 +197,10 @@ public class DefaultComplexObjectValidationStrategyTest
         public int Age => 23;
 
         public string Name => _string.Substring(3, 5);
+    }
+
+    private class TestValidationModelNameProvider : IValidationMetadataProvider
+    {
+        public void CreateValidationMetadata(ValidationMetadataProviderContext context) => context.ValidationMetadata.ValidationModelName = context.Key.Name?.ToUpperInvariant();
     }
 }
