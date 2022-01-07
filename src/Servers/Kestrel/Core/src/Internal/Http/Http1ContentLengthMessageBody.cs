8536686cd89c3e2d6609aaff4a585cbb16aea6b0
Sébastien Ros <sebastienros@gmail.com>
2022-01-07 11:36:13 -0800

    Fix spacing after merge

--- src/Servers/Kestrel/Core/src/Internal/Http/Http1ContentLengthMessageBody.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http/Http1ContentLengthMessageBody.cs
@@ -214,23 +214,23 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
 
             _isReading = false;
 
-        // The current body is read, though there might be more bytes to read on the stream with pipelining
+            // The current body is read, though there might be more bytes to read on the stream with pipelining
             if (_readCompleted)
             {
-            if (!_finalAdvanceCalled && consumed.Equals(_readResult.Buffer.End))
+                if (!_finalAdvanceCalled && consumed.Equals(_readResult.Buffer.End))
                 {
-                // Don't reference the old buffer as it will be released by the pipe afer calling AdvancedTo
-                _readResult = new ReadResult(new ReadOnlySequence<byte>(), isCanceled: false, isCompleted: true);
+                    // Don't reference the old buffer as it will be released by the pipe afer calling AdvancedTo
+                    _readResult = new ReadResult(new ReadOnlySequence<byte>(), isCanceled: false, isCompleted: true);
 
                     _context.Input.AdvanceTo(consumed);
                     _finalAdvanceCalled = true;
                     _context.OnTrailersComplete();
                 }
-            else
-            {
-                // If the old stored _readResult was canceled, it's already been observed. Do not store a canceled read result permanently.
-                _readResult = new ReadResult(_readResult.Buffer.Slice(consumed, _readResult.Buffer.End), isCanceled: false, isCompleted: true);
-            }
+                else
+                {
+                    // If the old stored _readResult was canceled, it's already been observed. Do not store a canceled read result permanently.
+                    _readResult = new ReadResult(_readResult.Buffer.Slice(consumed, _readResult.Buffer.End), isCanceled: false, isCompleted: true);
+                }
 
                 return;
             }
