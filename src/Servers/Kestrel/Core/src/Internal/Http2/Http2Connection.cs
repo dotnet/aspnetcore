4604e9bce59d21b7267d75c3381ee6243760b1ab
James Newton-King <james@newtonking.com>
2022-01-09 21:42:45 +1300

    Fix build

--- src/Servers/Kestrel/Core/src/Internal/Http2/Http2Connection.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http2/Http2Connection.cs
@@ -12,7 +12,6 @@ using System.Text;
 using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.Connections.Features;
 using Microsoft.AspNetCore.Hosting.Server;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Internal;
 using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
