addab21ac2d15bd5230a60b0573e2557d520fc15
Sean Reeser <v-seanreeser@microsoft.com>
2022-01-13 19:58:38 +0000

    Merged PR 20647: Fix more build errors
    Merging in release/5.0 changes and fix from 6.0

--- src/Framework/test/TargetingPackTests.cs
+++ src/Framework/test/TargetingPackTests.cs
@@ -109,22 +109,17 @@ namespace Microsoft.AspNetCore
 
             Assert.All(dlls, path =>
             {
-                // Skip netstandard2.0 System.IO.Pipelines assembly. References have old versions.
-                var filename = Path.GetFileName(path);
-                if (!string.Equals("System.IO.Pipelines.dll", filename, StringComparison.OrdinalIgnoreCase))
-                {
-                    using var fileStream = File.OpenRead(path);
-                    using var peReader = new PEReader(fileStream, PEStreamOptions.Default);
-                    var reader = peReader.GetMetadataReader(MetadataReaderOptions.Default);
+            using var fileStream = File.OpenRead(path);
+            using var peReader = new PEReader(fileStream, PEStreamOptions.Default);
+            var reader = peReader.GetMetadataReader(MetadataReaderOptions.Default);
 
-                    Assert.All(reader.AssemblyReferences, handle =>
-                    {
-                        var reference = reader.GetAssemblyReference(handle);
-                        var result = 0 == reference.Version.Revision;
+            Assert.All(reader.AssemblyReferences, handle =>
+            {
+                var reference = reader.GetAssemblyReference(handle);
+                var result = (0 == reference.Version.Revision && 0 == reference.Version.Build);
 
-                        Assert.True(result, $"In {filename}, {reference.GetAssemblyName()} has unexpected version {reference.Version}.");
-                    });
-                }
+                Assert.True(result, $"In {Path.GetFileName(path)}, {reference.GetAssemblyName()} has unexpected version {reference.Version}.");
+            });
             });
         }
 
