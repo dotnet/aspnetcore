ae431b44e618e0d322519f2819651723b70934f1
Christoph Fink <fink.christoph@gmail.com>
2022-01-17 07:34:38 +0100

    Change optional parameter to overload.

--- src/Components/Web/src/Forms/InputFile/BrowserFileExtensions.cs
+++ src/Components/Web/src/Forms/InputFile/BrowserFileExtensions.cs
@@ -10,7 +10,7 @@ namespace Microsoft.AspNetCore.Components.Forms
     /// Contains helper methods for <see cref="IBrowserFile"/>.
     /// </summary>
     public static class BrowserFileExtensions
-    {
+    {        
         /// <summary>
         /// Attempts to convert the current image file to a new one of the specified file type and maximum file dimensions.
         /// <para>
@@ -26,9 +26,31 @@ namespace Microsoft.AspNetCore.Components.Forms
         /// <param name="format">The new image format.</param>
         /// <param name="maxWidth">The maximum image width.</param>
         /// <param name="maxHeight">The maximum image height</param>
+        /// <returns>A <see cref="ValueTask"/> representing the completion of the operation.</returns>
+        public static ValueTask<IBrowserFile> RequestImageFileAsync(this IBrowserFile browserFile, string format, int maxWidth, int maxHeight)
+        {
+            return browserFile.RequestImageFileAsync(format, maxWidth, maxHeight, null);
+        }
+        
+        /// <summary>
+        /// Attempts to convert the current image file to a new one of the specified file type and maximum file dimensions.
+        /// <para>
+        /// Caution: there is no guarantee that the file will be converted, or will even be a valid image file at all, either
+        /// before or after conversion. The conversion is requested within the browser before it is transferred to .NET
+        /// code, so the resulting data should be treated as untrusted.
+        /// </para>
+        /// </summary>
+        /// <remarks>
+        /// The image will be scaled to fit the specified dimensions while preserving the original aspect ratio.
+        /// The quality parameter is not supported on all browsers, especially mobile browsers.
+        /// </remarks>
+        /// <param name="browserFile">The <see cref="IBrowserFile"/> to convert to a new image file.</param>
+        /// <param name="format">The new image format.</param>
+        /// <param name="maxWidth">The maximum image width.</param>
+        /// <param name="maxHeight">The maximum image height</param>
         /// <param name="quality">The quality of the image</param>
         /// <returns>A <see cref="ValueTask"/> representing the completion of the operation.</returns>
-        public static ValueTask<IBrowserFile> RequestImageFileAsync(this IBrowserFile browserFile, string format, int maxWidth, int maxHeight, double? quality = null)
+        public static ValueTask<IBrowserFile> RequestImageFileAsync(this IBrowserFile browserFile, string format, int maxWidth, int maxHeight, double? quality)
         {
             if (browserFile is BrowserFile browserFileInternal)
             {
