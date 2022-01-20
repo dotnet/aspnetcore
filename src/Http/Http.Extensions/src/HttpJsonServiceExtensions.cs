eeca3a5e26a8ce4539212d715d1a35383d19e2c1
Safia Abdalla <safia@safia.rocks>
2022-01-20 21:55:46 +0000

    Fix up crefs

--- src/Http/Http.Extensions/src/HttpJsonServiceExtensions.cs
+++ src/Http/Http.Extensions/src/HttpJsonServiceExtensions.cs
@@ -10,20 +10,18 @@ namespace Microsoft.Extensions.DependencyInjection;
 /// </summary>
 public static class RouteHandlerJsonServiceExtensions
 {
-#pragma warning disable CS0419 // Ambiguous reference in cref attribute
     /// <summary>
     /// Configures options used for reading and writing JSON by route handlers.
     /// </summary>
     /// <remarks>
     /// The options configured here will only affect JSON returned and processed
-    /// from route handlers, not controllers, when using <see cref="Microsoft.AspNetCore.Http.HttpRequestJsonExtensions.ReadFromJsonAsync" />
-    /// and <see cref="Microsoft.AspNetCore.Http.HttpResponseJsonExtensions.WriteAsJsonAsync" />.
+    /// from route handlers, not controllers, when using <see cref="O:Microsoft.AspNetCore.Http.HttpRequestJsonExtensions.ReadFromJsonAsync" />
+    /// and <see cref="O:Microsoft.AspNetCore.Http.HttpResponseJsonExtensions.WriteAsJsonAsync" />.
     /// </remarks>
     /// <param name="services">The <see cref="IServiceCollection" /> to configure options on.</param>
     /// <param name="configureOptions">The <see cref="Action{JsonOptions}"/> to configure the
     /// <see cref="JsonOptions"/>.</param>
     /// <returns>The modified <see cref="IServiceCollection"/>.</returns>
-#pragma warning restore CS0419 // Ambiguous reference in cref attribute
     public static IServiceCollection ConfigureRouteHandlerJsonOptions(this IServiceCollection services, Action<JsonOptions> configureOptions)
     {
         services.Configure<JsonOptions>(configureOptions);
