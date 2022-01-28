3e298f31989e9268b870cf6698f8768302e3ab78
Bruno Oliveira <brunolins16@users.noreply.github.com>
2022-01-28 14:48:41 -0800

    Update src/Mvc/Mvc.Core/test/ModelBinding/TestModelBinderProviderContext.cs
    Co-authored-by: Pranav K <prkrishn@hotmail.com>

--- src/Mvc/Mvc.Core/test/ModelBinding/TestModelBinderProviderContext.cs
+++ src/Mvc/Mvc.Core/test/ModelBinding/TestModelBinderProviderContext.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
