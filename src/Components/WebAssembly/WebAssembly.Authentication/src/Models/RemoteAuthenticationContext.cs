ed423dc7bd5ba8034ea4918ee88ba54a01be2d08
Pranav K <prkrishn@hotmail.com>
2022-01-28 12:03:38 -0800

    Annotate WebAssembly.Authentication (#39838)
    * Annotate WebAssembly.Authentication
    
    Fixes https://github.com/dotnet/aspnetcore/issues/30751
    Fixes https://github.com/dotnet/aspnetcore/issues/35162

--- src/Components/WebAssembly/WebAssembly.Authentication/src/Models/RemoteAuthenticationContext.cs
+++ src/Components/WebAssembly/WebAssembly.Authentication/src/Models/RemoteAuthenticationContext.cs
@@ -1,13 +1,16 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
+using static Microsoft.AspNetCore.Internal.LinkerFlags;
+
 namespace Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 /// <summary>
 /// Represents the context during authentication operations.
 /// </summary>
 /// <typeparam name="TRemoteAuthenticationState"></typeparam>
-public class RemoteAuthenticationContext<TRemoteAuthenticationState> where TRemoteAuthenticationState : RemoteAuthenticationState
+public class RemoteAuthenticationContext<[DynamicallyAccessedMembers(JsonSerialized)] TRemoteAuthenticationState> where TRemoteAuthenticationState : RemoteAuthenticationState
 {
     /// <summary>
     /// Gets or sets the url for the current authentication operation.
