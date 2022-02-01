6817667856ee8f07755a7459ee8b4e6bd93d0234
Hao Kung <HaoK@users.noreply.github.com>
2022-02-01 10:08:03 -0800

    Undo more diffs

--- src/Servers/IIS/AspNetCoreModuleV2/CommonLib/StringHelpers.cpp
+++ src/Servers/IIS/AspNetCoreModuleV2/CommonLib/StringHelpers.cpp
@@ -4,17 +4,6 @@
 #include "StringHelpers.h"
 #include "exceptions.h"
 
-//bool ends_with(const std::wstring &source, const std::wstring &suffix, bool ignoreCase)
-//{
-//    if (source.length() < suffix.length())
-//    {
-//        return false;
-//    }
-//
-//    const auto offset = source.length() - suffix.length();
-//    return CSTR_EQUAL == CompareStringOrdinal(source.c_str() + offset, static_cast<int>(suffix.length()), suffix.c_str(), static_cast<int>(suffix.length()), ignoreCase);
-//}
-
 bool equals_ignore_case(const std::wstring& s1, const std::wstring& s2)
 {
     return compare_ignore_case(s1, s2) == 0;
