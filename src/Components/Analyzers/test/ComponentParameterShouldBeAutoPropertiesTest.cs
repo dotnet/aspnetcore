5ac197c6e9d3b8a12992e7cad04b0ba4851f5030
Pranav K <prkrishn@hotmail.com>
2022-02-02 17:39:11 -0800

    Update ComponentParameterShouldBeAutoPropertiesTest.cs

--- src/Components/Analyzers/test/ComponentParameterShouldBeAutoPropertiesTest.cs
+++ src/Components/Analyzers/test/ComponentParameterShouldBeAutoPropertiesTest.cs
@@ -5,149 +5,148 @@ using Microsoft.CodeAnalysis.Diagnostics;
 using TestHelper;
 using Xunit;
 
-namespace Microsoft.AspNetCore.Components.Analyzers.Tests
+namespace Microsoft.AspNetCore.Components.Analyzers.Tests;
+
+public class ComponentParameterShouldBeAutoPropertiesTest : DiagnosticVerifier
 {
-    public class ComponentParameterShouldBeAutoPropertiesTest : DiagnosticVerifier
+    [Fact]
+    public void IsAutoProperty_NoDiagnostic()
     {
-        [Fact]
-        public void IsAutoProperty_NoDiagnostic()
-        {
-            var source = @"
+        var source = @"
 using Microsoft.AspNetCore.Components
 
 public class C
 {
-    [Parameter]
-    public string MyProp { get; set; }
+[Parameter]
+public string MyProp { get; set; }
 
-    [Parameter]
-    public string MyProp2 { set; get; }
+[Parameter]
+public string MyProp2 { set; get; }
 }
 " + ComponentsTestDeclarations.Source;
-            VerifyCSharpDiagnostic(source);
-        }
+        VerifyCSharpDiagnostic(source);
+    }
 
-        [Fact]
-        public void HaveSameSemanticAsAutoProperty_NoDiagnostic()
-        {
-            var source = @"
+    [Fact]
+    public void HaveSameSemanticAsAutoProperty_NoDiagnostic()
+    {
+        var source = @"
 using Microsoft.AspNetCore.Components
 
 public class C
 {
-    private string _myProp;
-    private string _myProp2;
-    private string _myProp3;
+private string _myProp;
+private string _myProp2;
+private string _myProp3;
 
-    [Parameter]
-    public string MyProp
+[Parameter]
+public string MyProp
+{
+    get => _myProp;
+    set => _myProp = value;
+}
+
+[Parameter]
+public string MyProp2
+{
+    set => _myProp2 = value;
+    get => _myProp2;
+}
+
+[Parameter]
+public string MyProp3
+{
+    get
     {
-        get => _myProp;
-        set => _myProp = value;
+        return _myProp3;
     }
-
-    [Parameter]
-    public string MyProp2
+    set
     {
-        set => _myProp2 = value;
-        get => _myProp2;
-    }
-
-    [Parameter]
-    public string MyProp3
-    {
-        get
-        {
-            return _myProp3;
-        }
-        set
-        {
-            _myProp3 = value;
-        }
+        _myProp3 = value;
     }
 }
+}
 " + ComponentsTestDeclarations.Source;
-            VerifyCSharpDiagnostic(source);
-        }
+        VerifyCSharpDiagnostic(source);
+    }
 
-        [Fact]
-        public void HaveLogicInSetter_Diagnostic()
-        {
-            var source = @"
+    [Fact]
+    public void HaveLogicInSetter_Diagnostic()
+    {
+        var source = @"
 using Microsoft.AspNetCore.Components
 
 public class C
 {
-    private string _myProp;
+private string _myProp;
 
-    [Parameter]
-    public string MyProp
+[Parameter]
+public string MyProp
+{
+    get
     {
-        get
-        {
-            return _myProp;
-        }
-        set
-        {
-            DoSomething();
-            _myProp = value;
-        }
+        return _myProp;
     }
+    set
+    {
+        DoSomething();
+        _myProp = value;
+    }
+}
 
-    private void DoSomething() { }
+private void DoSomething() { }
 }
 " + ComponentsTestDeclarations.Source;
-            VerifyCSharpDiagnostic(source, new DiagnosticResult
+        VerifyCSharpDiagnostic(source, new DiagnosticResult
+        {
+            Id = DiagnosticDescriptors.ComponentParametersShouldBeAutoProperies.Id,
+            Message = "Component parameter 'C.MyProp' should be auto property",
+            Locations = new[]
             {
-                Id = DiagnosticDescriptors.ComponentParametersShouldBeAutoProperies.Id,
-                Message = "Component parameter 'C.MyProp' should be auto property",
-                Locations = new[]
-                {
-                    new DiagnosticResultLocation("Test0.cs", 9, 19),
-                },
-                Severity = CodeAnalysis.DiagnosticSeverity.Warning,
-            });
-        }
+                new DiagnosticResultLocation("Test0.cs", 9, 19),
+            },
+            Severity = CodeAnalysis.DiagnosticSeverity.Warning,
+        });
+    }
 
-        [Fact]
-        public void HaveLogicInGetter_Diagnostic()
-        {
-            var source = @"
+    [Fact]
+    public void HaveLogicInGetter_Diagnostic()
+    {
+        var source = @"
 using Microsoft.AspNetCore.Components
 
 public class C
 {
-    private string _myProp;
+private string _myProp;
 
-    [Parameter]
-    public string MyProp
+[Parameter]
+public string MyProp
+{
+    get
     {
-        get
-        {
-            DoSomething();
-            return _myProp;
-        }
-        set
-        {
-            _myProp = value;
-        }
+        DoSomething();
+        return _myProp;
     }
+    set
+    {
+        _myProp = value;
+    }
+}
 
-    private void DoSomething() { }
+private void DoSomething() { }
 }
 " + ComponentsTestDeclarations.Source;
-            VerifyCSharpDiagnostic(source, new DiagnosticResult
+        VerifyCSharpDiagnostic(source, new DiagnosticResult
+        {
+            Id = DiagnosticDescriptors.ComponentParametersShouldBeAutoProperies.Id,
+            Message = "Component parameter 'C.MyProp' should be auto property",
+            Locations = new[]
             {
-                Id = DiagnosticDescriptors.ComponentParametersShouldBeAutoProperies.Id,
-                Message = "Component parameter 'C.MyProp' should be auto property",
-                Locations = new[]
-                {
-                    new DiagnosticResultLocation("Test0.cs", 9, 19),
-                },
-                Severity = CodeAnalysis.DiagnosticSeverity.Warning,
-            });
-        }
-
-        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new ComponentParameterAnalyzer();
+                new DiagnosticResultLocation("Test0.cs", 9, 19),
+            },
+            Severity = CodeAnalysis.DiagnosticSeverity.Warning,
+        });
     }
+
+    protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new ComponentParameterAnalyzer();
 }
