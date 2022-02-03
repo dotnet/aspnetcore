50f5af536beb5d25503bf8e9beea7971b57ca331
Mikel Blanchard <mblanchard@macrosssoftware.com>
2022-02-03 12:31:39 -0800

    MVC: Save an allocation in PageRequestDelegateFactory & ControllerRequestDelegateFactory (#39942)
    * Save an allocation and some sets in PageRequestDelegateFactory.
    
    * Applied optimization to ControllerRequestDelegateFactory flow.

--- src/Mvc/Mvc.Core/src/ControllerContext.cs
+++ src/Mvc/Mvc.Core/src/ControllerContext.cs
@@ -1,9 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Core;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Routing;
 
 namespace Microsoft.AspNetCore.Mvc;
 
@@ -37,6 +39,20 @@ public class ControllerContext : ActionContext
                 typeof(ControllerActionDescriptor)),
                 nameof(context));
         }
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="ControllerContext"/>.
+    /// </summary>
+    /// <param name="httpContext">The <see cref="HttpContext"/> for the current request.</param>
+    /// <param name="routeData">The <see cref="RouteData"/> for the current request.</param>
+    /// <param name="actionDescriptor">The <see cref="ControllerActionDescriptor"/> for the selected action.</param>
+    internal ControllerContext(
+        HttpContext httpContext,
+        RouteData routeData,
+        ControllerActionDescriptor actionDescriptor)
+        : base(httpContext, routeData, actionDescriptor)
+    {
     }
 
     /// <summary>
