436ab16cb7900cd8572d3a00dd3b88f4c7b73652
Robert Haken <haken@havit.cz>
2022-02-15 09:06:48 +0100

    Input components two-way binding - nullable reference types annotations removal

--- src/Components/test/testassets/BasicTestApp/FormsTest/DerivedInputDateComponent.cs
+++ src/Components/test/testassets/BasicTestApp/FormsTest/DerivedInputDateComponent.cs
@@ -1,12 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using Microsoft.AspNetCore.Components;
-using System.Globalization;
 using Microsoft.AspNetCore.Components.Forms;
-using Newtonsoft.Json.Linq;
 
 namespace BasicTestApp.FormsTest;
 public class DerivedInputDateComponent : InputDate<DateTime?>
@@ -14,7 +9,7 @@ public class DerivedInputDateComponent : InputDate<DateTime?>
     // Supports InputsTwoWayBindingComponent test
     // Repro for https://github.com/dotnet/aspnetcore/issues/40097
 
-    protected override bool TryParseValueFromString(string? value, [MaybeNullWhen(false)] out DateTime? result, [NotNullWhen(false)] out string? validationErrorMessage)
+    protected override bool TryParseValueFromString(string value, out DateTime? result, out string validationErrorMessage)
     {
         if (value == "0001-01-01")
         {
