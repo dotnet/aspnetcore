1d43fef08546dddd43b978133f50fc7b2378d11d
David Acker <davidacker3@gmail.com>
2022-02-19 13:07:31 -0500

    Fix extensions namespaces

--- src/Middleware/RequestDecompression/src/RequestDecompressionBuilderExtensions.cs
+++ src/Middleware/RequestDecompression/src/RequestDecompressionBuilderExtensions.cs
@@ -1,9 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.RequestDecompression;
 
-namespace Microsoft.AspNetCore.RequestDecompression;
+namespace Microsoft.AspNetCore.Builder;
 
 /// <summary>
 /// Extension methods for the request decompression middleware.
--- src/Middleware/RequestDecompression/src/RequestDecompressionServiceExtensions.cs
+++ src/Middleware/RequestDecompression/src/RequestDecompressionServiceExtensions.cs
@@ -1,10 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Microsoft.AspNetCore.RequestDecompression;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
-namespace Microsoft.AspNetCore.RequestDecompression;
+namespace Microsoft.AspNetCore.Builder;
 
 /// <summary>
 /// Extension methods for the RequestDecompression middleware.
