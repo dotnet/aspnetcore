522a04014867a3488208a9c2135bf76fe81a3fb0
Pranav K <prkrishn@hotmail.com>
2022-02-23 14:16:05 -0800

    Enable CA1810 as a warning Contributes to https://github.com/dotnet/aspnetcore/issues/24055

--- src/Servers/Kestrel/shared/KnownHeaders.cs
+++ src/Servers/Kestrel/shared/KnownHeaders.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#pragma warning disable CA1810 // Initialize all static fields inline. This is a code generator.
+
 using System.Diagnostics;
 using System.Globalization;
 using System.Net.Http.HPack;
