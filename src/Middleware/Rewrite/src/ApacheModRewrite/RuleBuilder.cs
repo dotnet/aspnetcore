e31dcf49f09aebd3b88ec8826586dfa33bfe3f9f
Shreyas Jejurkar <shreyasjejurkar123@live.com>
2022-03-08 20:07:28 +0530

    Update RuleBuilder.cs

--- src/Middleware/Rewrite/src/ApacheModRewrite/RuleBuilder.cs
+++ src/Middleware/Rewrite/src/ApacheModRewrite/RuleBuilder.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Http;
