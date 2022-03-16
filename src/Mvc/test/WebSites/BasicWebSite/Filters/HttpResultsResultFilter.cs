6a26ae13dbdf1a9367fbda5cfc126277532cebe4
Bruno Lins de Oliveira <brolivei@microsoft.com>
2022-03-15 17:07:54 -0700

    adding test filter

---
+++ src/Mvc/test/WebSites/BasicWebSite/Filters/HttpResultsResultFilter.cs
@@ -0,0 +1,24 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BasicWebSite;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class HttpResultsFilter : Attribute, IResultFilter
+{
+    public void OnResultExecuted(ResultExecutedContext context)
+    {
+    }
+
+    public void OnResultExecuting(ResultExecutingContext context)
+    {
+        if (context is { Result: HttpResultsActionResult { Result: IResult result } })
+        {
+            context.Result = new StatusCodeResult(StatusCodes.Status200OK);
+            context.HttpContext.Response.Headers["X-HttpResultType"] = result.GetType().Name;
+        }
+    }
+}
