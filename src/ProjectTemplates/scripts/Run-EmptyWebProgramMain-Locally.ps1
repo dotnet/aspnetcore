ed1e6ffe450888099c5e40d2d80da5e582b5acb6
Damian Edwards <damian@damianedwards.com>
2022-03-25 10:51:24 -0700

    Add UseProgramMain open to EmptyWeb template

--- src/ProjectTemplates/scripts/Run-EmptyWebProgramMain-Locally.ps1
+++ src/ProjectTemplates/scripts/Run-EmptyWebProgramMain-Locally.ps1
@@ -9,4 +9,4 @@ $ErrorActionPreference = 'Stop'
 
 . $PSScriptRoot\Test-Template.ps1
 
-Test-Template "reactredux" "reactredux" "Microsoft.DotNet.Web.Spa.ProjectTemplates.7.0.7.0.0-dev.nupkg" $false
+Test-Template "web" "web --use-program-main" "Microsoft.DotNet.Web.ProjectTemplates.7.0.7.0.0-dev.nupkg" $false
