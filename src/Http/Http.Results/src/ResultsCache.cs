ef3e2637a81379626bd910032d4f0bdcd1dee646
Bruno Lins de Oliveira <brolivei@microsoft.com>
2022-03-29 13:04:05 -0700

    Caching known results

---
+++ src/Http/Http.Results/src/ResultsCache.cs
@@ -0,0 +1,15 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.AspNetCore.Http;
+
+internal static partial class ResultsCache
+{
+    public static NotFoundObjectHttpResult NotFound { get; } = new();
+    public static UnauthorizedHttpResult Unauthorized { get; } = new();
+    public static BadRequestObjectHttpResult BadRequest { get; } = new();
+    public static ConflictObjectHttpResult Conflict { get; } = new();
+    public static NoContentHttpResult NoContent { get; } = new();
+    public static OkObjectHttpResult Ok { get; } = new();
+    public static UnprocessableEntityObjectHttpResult UnprocessableEntity { get; } = new();
+}
