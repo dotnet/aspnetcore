63cd2d4aa320a5db269a1e4c1f82b94cc55f8186
William Godbe <wigodbe@microsoft.com>
2022-03-29 14:54:14 -0700

    Fix W3CLogger tests (#40922)
    * Fix W3CLogger tests
    
    * Feedback
    
    * Update W3CLoggerTests.cs
    
    * Update W3CLoggingMiddlewareTests.cs

--- src/Middleware/HttpLogging/test/W3CLoggingMiddlewareTests.cs
+++ src/Middleware/HttpLogging/test/W3CLoggingMiddlewareTests.cs
@@ -95,7 +95,9 @@ public class W3CLoggingMiddlewareTests
         Assert.StartsWith("#Start-Date: ", lines[1]);
         var startDate = DateTime.Parse(lines[1].Substring(13), CultureInfo.InvariantCulture);
         // Assert that the log was written in the last 10 seconds
-        Assert.True(now.Subtract(startDate).TotalSeconds < 10);
+        // W3CLogger writes start-time to second precision, so delta could be as low as -0.999...
+        var delta = startDate.Subtract(now).TotalSeconds;
+        Assert.InRange(delta, -1, 10);
 
         Assert.Equal("#Fields: date time c-ip s-computername s-ip s-port cs-method cs-uri-stem cs-uri-query sc-status time-taken cs-version cs-host cs(User-Agent) cs(Referer)", lines[2]);
         Assert.DoesNotContain(lines[3], "Snickerdoodle");
@@ -129,7 +131,9 @@ public class W3CLoggingMiddlewareTests
         Assert.StartsWith("#Start-Date: ", lines[1]);
         var startDate = DateTime.Parse(lines[1].Substring(13), CultureInfo.InvariantCulture);
         // Assert that the log was written in the last 10 seconds
-        Assert.True(now.Subtract(startDate).TotalSeconds < 10);
+        // W3CLogger writes start-time to second precision, so delta could be as low as -0.999...
+        var delta = startDate.Subtract(now).TotalSeconds;
+        Assert.InRange(delta, -1, 10);
 
         Assert.Equal("#Fields: time-taken", lines[2]);
         double num;
