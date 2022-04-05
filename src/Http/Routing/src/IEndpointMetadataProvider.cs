0b516ba009675a0839a760818c8085c2fbf76fec
Damian Edwards <damian@damianedwards.com>
2022-04-04 19:23:19 -0700

    Doc comment fixup

--- src/Http/Routing/src/IEndpointMetadataProvider.cs
+++ src/Http/Routing/src/IEndpointMetadataProvider.cs
@@ -4,13 +4,13 @@
 namespace Microsoft.AspNetCore.Http;
 
 /// <summary>
-/// Indicates that a type provides a static method that returns <see cref="Endpoint"/> metadata when declared as a parameter type,
-/// <see cref="Attribute"/> type, or the returned type of an <see cref="Endpoint"/> route handler delegate.
+/// Indicates that a type provides a static method that provides <see cref="Endpoint"/> metadata when declared as a parameter type or the
+/// returned type of an <see cref="Endpoint"/> route handler delegate.
 /// </summary>
 public interface IEndpointMetadataProvider
 {
     /// <summary>
-    /// Supplies objects to apply as metadata to the related <see cref="Endpoint"/>.
+    /// Populates metadata for the related <see cref="Endpoint"/>.
     /// </summary>
     /// <param name="context">The <see cref="EndpointMetadataContext"/>.</param>
     static abstract void PopulateMetadata(EndpointMetadataContext context);
