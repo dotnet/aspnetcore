108c22182e1ada7052e969add7d2d128f786c37b
Will Godbe <wigodbe@microsoft.com>
2022-04-08 01:40:24 +0000

    Merged PR 21240: [6.0] MSRC 70023 - ASP.Net FormFeature.cs - DenialOfService
    # ASP.Net FormFeature.cs - DenialOfService
    
    When parsing multi-part form data with FormFeature.cs, we do not honor ValueCountLimit when the content disposition is of an unknown type. Therefore an attacker could send multi-part form data where very part has invalid content disposition, and make us read indefinitely.
    
    ## Description
    
    When parsing multi-part form data with FormFeature.cs, we do not honor ValueCountLimit when the content disposition is of an unknown type. Therefore an attacker could send multi-part form data where very part has invalid content disposition, and make us read indefinitely.
    
    ## Customer Impact
    
    Prevents a potential Denial-of-service attack.
    
    ## Regression?
    
    - [ ] Yes
    - [x] No
    
    ## Risk
    
    - [ ] High
    - [x] Medium
    - [ ] Low
    
    We could have missed another potential version of this vulnerability
    
    ## Verification
    
    - [x] Manual (required)
    - [x] Automated
    
    Added a test, plus confirmed with a local repro that the pre-existing slowdown goes away after the change.
    
    ## Packaging changes reviewed?
    
    - [ ] Yes
    - [ ] No
    - [x] N/A
    
    ----
    
    ## When servicing release/2.1
    
    - [ ] Make necessary changes in eng/PatchConfig.props

--- src/Http/Http/test/Features/FormFeatureTests.cs
+++ src/Http/Http/test/Features/FormFeatureTests.cs
@@ -167,6 +167,12 @@ InvalidContentDispositionValue +
 "\r\n" +
 "Foo\r\n";
 
+        private const string MultipartFormFileNonFormOrFileContentDispositionValue = "--WebKitFormBoundary5pDRpGheQXaM8k3T\r\n" +
+"Content-Disposition:x" +
+"\r\n" +
+"\r\n" +
+"Foo\r\n";
+
         private const string MultipartFormWithField =
             MultipartFormField +
             MultipartFormEnd;
@@ -466,6 +472,30 @@ InvalidContentDispositionValue +
 
             var exception = await Assert.ThrowsAsync<InvalidDataException>(() => context.Request.ReadFormAsync());
             Assert.Equal("Form value count limit 2 exceeded.", exception.Message);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task ReadFormAsync_NonFormOrFieldContentDisposition_ValueCountLimitExceeded_Throw(bool bufferRequest)
+        {
+            var formContent = new List<byte>();
+            formContent.AddRange(Encoding.UTF8.GetBytes(MultipartFormFileNonFormOrFileContentDispositionValue));
+            formContent.AddRange(Encoding.UTF8.GetBytes(MultipartFormFileNonFormOrFileContentDispositionValue));
+            formContent.AddRange(Encoding.UTF8.GetBytes(MultipartFormFileNonFormOrFileContentDispositionValue));
+            formContent.AddRange(Encoding.UTF8.GetBytes(MultipartFormEnd));
+
+            var context = new DefaultHttpContext();
+            var responseFeature = new FakeResponseFeature();
+            context.Features.Set<IHttpResponseFeature>(responseFeature);
+            context.Request.ContentType = MultipartContentType;
+            context.Request.Body = new NonSeekableReadStream(formContent.ToArray());
+
+            IFormFeature formFeature = new FormFeature(context.Request, new FormOptions() { BufferBody = bufferRequest, ValueCountLimit = 2 });
+            context.Features.Set<IFormFeature>(formFeature);
+
+            var exception = await Assert.ThrowsAsync<InvalidDataException>(() => context.Request.ReadFormAsync());
+            Assert.Equal("Unrecognized Content-Disposition. Form value count limit 2 exceeded.", exception.Message);
         }
 
         [Theory]
