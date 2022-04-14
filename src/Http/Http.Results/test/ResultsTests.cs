9c81ba7e9249f2e1b1676abf5e978777487c5705
Damian Edwards <damian@damianedwards.com>
2022-04-14 16:23:50 -0700

    More tests

--- src/Http/Http.Results/test/ResultsTests.cs
+++ src/Http/Http.Results/test/ResultsTests.cs
@@ -1,8 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Collections.ObjectModel;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Net.Http.Headers;
 
@@ -121,27 +124,178 @@ public class ResultsTests
         Assert.Equal(StatusCodes.Status400BadRequest, result.StatusCode);
     }
 
-    [Fact]
-    public void Bytes_WithRouteNameAndRouteValues_ResultHasCorrectValues()
+    [Theory]
+    [MemberData(nameof(Bytes_ResultHasCorrectValues_Data))]
+    public void Bytes_ResultHasCorrectValues(string contentType, string fileDownloadName, bool enableRangeProcessing, DateTimeOffset lastModified, EntityTagHeaderValue entityTag)
     {
         // Arrange
         var contents = new byte[0];
-        var contentType = "text/plain";
-        var fileDownloadName = "testfile";
-        var enableRangeProcessing = true;
-        var lastModified = new DateTimeOffset(2022, 1, 1, 0, 0, 1, TimeSpan.FromHours(-8));
-        var entityTag = EntityTagHeaderValue.Any;
 
         // Act
         var result = Results.Bytes(contents, contentType, fileDownloadName, enableRangeProcessing, lastModified, entityTag) as FileContentHttpResult;
 
         // Assert
         Assert.Equal(contents, result.FileContents);
-        Assert.Equal(contentType, result.ContentType);
+        Assert.Equal(contentType ?? "application/octet-stream", result.ContentType);
         Assert.Equal(fileDownloadName, result.FileDownloadName);
         Assert.Equal(enableRangeProcessing, result.EnableRangeProcessing);
         Assert.Equal(lastModified, result.LastModified);
         Assert.Equal(entityTag, result.EntityTag);
+    }
+
+    public static IEnumerable<object[]> Bytes_ResultHasCorrectValues_Data => new List<object[]>
+    {
+        new object[] { "text/plain", "testfile", true, new DateTimeOffset(2022, 1, 1, 0, 0, 1, TimeSpan.FromHours(-8)), EntityTagHeaderValue.Any },
+        new object[] { default(string), default(string), default(bool), default(DateTimeOffset?), default(EntityTagHeaderValue) },
+    };
+
+    [Theory]
+    [MemberData(nameof(Challenge_ResultHasCorrectValues_Data))]
+    public void Challenge_ResultHasCorrectValues(AuthenticationProperties properties, IList<string> authenticationSchemes)
+    {
+        // Act
+        var result = Results.Challenge(properties, authenticationSchemes) as ChallengeHttpResult;
+
+        // Assert
+        Assert.Equal(properties, result.Properties);
+        Assert.Equal(authenticationSchemes ?? new ReadOnlyCollection<string>(new List<string>()), result.AuthenticationSchemes);
+    }
+
+    public static IEnumerable<object[]> Challenge_ResultHasCorrectValues_Data => new List<object[]>
+    {
+        new object[] { new AuthenticationProperties(), new List<string> { "TestScheme" } },
+        new object[] { new AuthenticationProperties(), default(IList<string>) },
+        new object[] { default(AuthenticationProperties), new List<string> { "TestScheme" } },
+        new object[] { default(AuthenticationProperties), default(IList<string>) },
+    };
+
+    [Fact]
+    public void Conflict_WithValue_ResultHasCorrectValues()
+    {
+        // Arrange
+        var value = new { };
+
+        // Act
+        var result = Results.Conflict(value) as Conflict<object>;
+
+        // Assert
+        Assert.Equal(StatusCodes.Status409Conflict, result.StatusCode);
+        Assert.Equal(value, result.Value);
+    }
+
+    [Fact]
+    public void Conflict_WithNoArgs_ResultHasCorrectValues()
+    {
+        // Act
+        var result = Results.Conflict() as Conflict;
+
+        // Assert
+        Assert.Equal(StatusCodes.Status409Conflict, result.StatusCode);
+    }
+
+    [Fact]
+    public void Content_WithContentAndMediaType_ResultHasCorrectValues()
+    {
+        // Arrange
+        var content = "test content";
+        var mediaType = MediaTypeHeaderValue.Parse("text/plain");
+
+        // Act
+        var result = Results.Content(content, mediaType) as ContentHttpResult;
+
+        // Assert
+        Assert.Null(result.StatusCode);
+        Assert.Equal(content, result.ResponseContent);
+        Assert.Equal(mediaType.ToString(), result.ContentType);
+    }
+
+    [Fact]
+    public void Content_WithContentAndContentTypeAndEncoding_ResultHasCorrectValues()
+    {
+        // Arrange
+        var content = "test content";
+        var contentType = "text/plain";
+        var encoding = Encoding.UTF8;
+
+        // Act
+        var result = Results.Content(content, contentType, null) as ContentHttpResult;
+
+        // Assert
+        Assert.Null(result.StatusCode);
+        Assert.Equal(content, result.ResponseContent);
+        Assert.Equal(contentType, result.ContentType);
+    }
+
+    [Fact]
+    public void Created_WithStringUriAndValue_ResultHasCorrectValues()
+    {
+        // Arrange
+        var uri = "https://example.com/entity";
+        var value = new { };
+
+        // Act
+        var result = Results.Created(uri, value) as Created<object>;
+
+        // Assert
+        Assert.Equal(StatusCodes.Status201Created, result.StatusCode);
+        Assert.Equal(uri, result.Location);
+        Assert.Equal(value, result.Value);
+    }
+
+    [Fact]
+    public void Created_WithStringUri_ResultHasCorrectValues()
+    {
+        // Arrange
+        var uri = "https://example.com/entity";
+
+        // Act
+        var result = Results.Created(uri, null) as Created;
+
+        // Assert
+        Assert.Equal(StatusCodes.Status201Created, result.StatusCode);
+        Assert.Equal(uri, result.Location);
+    }
+
+    [Fact]
+    public void Created_WithUriAndValue_ResultHasCorrectValues()
+    {
+        // Arrange
+        var uri = new Uri("https://example.com/entity");
+        var value = new { };
+
+        // Act
+        var result = Results.Created(uri, value) as Created<object>;
+
+        // Assert
+        Assert.Equal(StatusCodes.Status201Created, result.StatusCode);
+        Assert.Equal(uri.ToString(), result.Location);
+        Assert.Equal(value, result.Value);
+    }
+
+    [Fact]
+    public void Created_WithUri_ResultHasCorrectValues()
+    {
+        // Arrange
+        var uri = new Uri("https://example.com/entity");
+
+        // Act
+        var result = Results.Created(uri, null) as Created;
+
+        // Assert
+        Assert.Equal(StatusCodes.Status201Created, result.StatusCode);
+        Assert.Equal(uri.ToString(), result.Location);
+    }
+
+    [Fact]
+    public void Created_WithNullStringUri_ThrowsArgNullException()
+    {
+        Assert.Throws<ArgumentNullException>("uri", () => Results.Created(default(string), null));
+    }
+
+    [Fact]
+    public void Created_WithNullUri_ThrowsArgNullException()
+    {
+        Assert.Throws<ArgumentNullException>("uri", () => Results.Created(default(Uri), null));
     }
 
     [Theory]
