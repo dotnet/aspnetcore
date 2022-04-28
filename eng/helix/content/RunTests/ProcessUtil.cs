f55023415adacaf88ae9059a4b375d79bc2f5cec
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2022-04-28 11:40:50 -0700

    Correct minor problems in `RunTests` source

--- eng/helix/content/RunTests/ProcessUtil.cs
+++ eng/helix/content/RunTests/ProcessUtil.cs
@@ -1,6 +1,5 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 using System;
 using System.Collections.Generic;
@@ -94,7 +93,6 @@ namespace RunTests
                 },
                 EnableRaisingEvents = true
             };
-
 
             if (workingDirectory != null)
             {
