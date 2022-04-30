0be2c29d9c09da46e95047afb41514fb58137c26
Tanay Parikh <TanayParikh@users.noreply.github.com>
2022-04-29 18:31:05 -0700

    Helix Restore Issue Mitigation (#41311)
    * RunTests dotnet run --no-build
    
    * Remove duplicate run
    
    * Add to Build.props
    
    * Treat CA2007 as a warning in RunTests
    
    * NoWarn CA2007
    
    * Ignore NU5104 for System.CommandLine prerelease package
    
    * PR Feedback
    
    * PR Feedback
    
    * Generate .config/dotnet-tools.json
      - add generation to GenerateFiles.csproj
      - include all required versions in eng/Versions.props
        - nit: bump tool versions slightly
        - `dotnet-dump` move from `5.0.0-*` to `6.0.322601` is largest version bump
      - have `git` ignore generated file
        - nit: put `*.svclog` together w/ other extension exclusions
    
    * Add tool packages to Helix correlation payloads
    
    * Do not restore tools from network
      - get tool packages from Helix correlation root
      - to do this, save and restore NuGet.config file
      - this removes `--version` from `dotnet-dump` and `dotnet-ef` installations
        - will only have a single package for each tool in the correlation payload
    
    * Do not pass `dotnet-ef` version to `RunTests`
      - mostly cleanup; no longer needed
    
    * Remove DotNetToolsInstaller.csproj
      - mostly cleanup
      - `dotnet-ef` tool now restored by Arcade's Tools.proj much earlier in our build because
        most configured tool packages are needed in `RunTests` on Helix agents
    
    * Add `--playwright` option in `Runtests`
      - remove `INSTALLPLAYWRIGHT` define and `$env:INSTALLPLAYWRIGHT`
      - always reference Microsoft.Playwright in `RunTests`
      - nit: `InstallPlaywrightAsync()` wasn't `async`; fix it and rename to `InstallPlaywright()`
    
    * Use Arcade SDK in `RunTests` and `Publish` project
      - match most other projects in this repo
        - remove empty Directory.Build.props and .targets files preventing Arcade imports
      - exclude project build if `$(SkipTestBuild)` (though not a test project)
    
    * Correct minor problems in `RunTests` source
    
    * Use file-scoped namespaces in `RunTests` source
    
    * Use VS code fixes in `RunTests`
    
    * Use published `RunTests` output on Helix agents
    
    * Move `RunTests` under eng/tools/
      - don't need the project on Helix agents
      - restore Directory.Build.* files removed when switching to Arcade
    
    * Add `RunTests` to tools.slnf
    
    * Rename `RunTests` project to `HelixTestRunner`
      - less confusing given `RunTests` target and runtests.sh et cetera
    
    * !fixup! Define `$(ArtifactsBinDir)` in helix.proj
    
    * !fixup! Use `$(Configuration)` in helix.proj
    
    * !fixup! Fix typo in property function
    
    * !fixup! Fix `dotnet` syntax in runtests.sh
    
    * !fixup! Correct `%(HelixCorrelationPayload.Destination)`
    
    Co-authored-by: Doug Bunting <6431421+dougbu@users.noreply.github.com>

--- eng/helix/content/runtests.cmd
+++ eng/helix/content/runtests.cmd
@@ -7,14 +7,12 @@ set $aspRuntimeVersion=%2
 set $queue=%3
 set $arch=%4
 set $quarantined=%5
-set $ef=%6
-set $helixTimeout=%7
-set $installPlaywright=%8
+set $helixTimeout=%6
+set $installPlaywright=%7
 REM Batch only supports up to 9 arguments using the %# syntax, need to shift to get more
 
 set DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 set DOTNET_MULTILEVEL_LOOKUP=0
-set InstallPlaywright=%$installPlaywright%
 set PLAYWRIGHT_BROWSERS_PATH=%CD%\ms-playwright
 
 set "PATH=%HELIX_WORKITEM_ROOT%;%PATH%;%HELIX_WORKITEM_ROOT%\node\bin"
@@ -23,17 +21,8 @@ echo.
 
 set exit_code=0
 
-echo "Restore: dotnet restore RunTests\RunTests.csproj --ignore-failed-sources"
-dotnet restore RunTests\RunTests.csproj --ignore-failed-sources
-
-if not errorlevel 0 (
-    set exit_code=%errorlevel%
-    echo "Restore runtests failed: exit_code=%exit_code%"
-    EXIT /b %exit_code%
-)
-
-echo "Running tests: dotnet run --no-restore --project RunTests\RunTests.csproj -- --target %$target% --runtime %$aspRuntimeVersion% --queue %$queue% --arch %$arch% --quarantined %$quarantined% --ef %$ef% --helixTimeout %$helixTimeout%"
-dotnet run --no-restore --project RunTests\RunTests.csproj -- --target %$target% --runtime %$aspRuntimeVersion% --queue %$queue% --arch %$arch% --quarantined %$quarantined% --ef %$ef% --helixTimeout %$helixTimeout%
+echo "Running tests: $HELIX_CORRELATION_PAYLOAD/HelixTestRunner/HelixTestRunner.exe --target %$target% --runtime %$aspRuntimeVersion% --queue %$queue% --arch %$arch% --quarantined %$quarantined% --helixTimeout %$helixTimeout% --playwright $installPlaywright"
+$HELIX_CORRELATION_PAYLOAD/HelixTestRunner/HelixTestRunner.exe --target %$target% --runtime %$aspRuntimeVersion% --queue %$queue% --arch %$arch% --quarantined %$quarantined% --helixTimeout %$helixTimeout% --playwright $installPlaywright
 if not errorlevel 0 (
     set exit_code=%errorlevel%
 )
