5172a85f5c43bfe3a2388c10b47c3a247c7aa8b7
Brennan <brecon@microsoft.com>
2022-05-18 09:02:52 -0700

    Quarantine StreamPool_MultipleStreamsConcurrent_StreamsReturnedToPool
    https://github.com/dotnet/aspnetcore/issues/39477

--- src/Servers/Kestrel/test/InMemory.FunctionalTests/Http2/Http2ConnectionTests.cs
+++ src/Servers/Kestrel/test/InMemory.FunctionalTests/Http2/Http2ConnectionTests.cs
@@ -427,6 +427,7 @@ public class Http2ConnectionTests : Http2TestBase
     }
 
     [Fact]
+    [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/39477")]
     public async Task StreamPool_MultipleStreamsConcurrent_StreamsReturnedToPool()
     {
         await InitializeConnectionAsync(_echoApplication);
