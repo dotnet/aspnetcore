e6d9afd8fafca41828c245ad1139b45f0b516ae0
Daniel Genkin <t-dagenkin@microsoft.com>
2022-05-19 15:55:12 -0700

    Fixed the handshake/method not supported issue

--- src/Servers/Kestrel/Core/src/Internal/Http3/Http3Connection.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http3/Http3Connection.cs
@@ -56,9 +56,11 @@ internal sealed class Http3Connection : IHttp3StreamLifetimeHandler, IRequestPro
 
         _serverSettings.HeaderTableSize = (uint)httpLimits.Http3.HeaderTableSize;
         _serverSettings.MaxRequestHeaderFieldSectionSize = (uint)httpLimits.MaxRequestHeadersTotalSize;
+        _serverSettings.EnableWebTransport = 1; // indicate that webtranport is supported on the server
+        _serverSettings.H3Datagram = 1; // indicate that webtranport is supported on the server
     }
 
-    private void UpdateHighestOpenedRequestStreamId(long streamId)
+private void UpdateHighestOpenedRequestStreamId(long streamId)
     {
         // Only one thread will update the highest stream ID value at a time.
         // Additional thread safty not required.
@@ -655,6 +657,10 @@ internal sealed class Http3Connection : IHttp3StreamLifetimeHandler, IRequestPro
                 _clientSettings.MaxRequestHeaderFieldSectionSize = (uint)value;
                 break;
             case Http3SettingType.QPackBlockedStreams:
+                break;
+            case Http3SettingType.EnableWebTransport:
+                break;
+            case Http3SettingType.H3Datagram:
                 break;
             default:
                 throw new InvalidOperationException("Unexpected setting: " + type);
--- src/Servers/Kestrel/Core/src/Internal/Http3/Http3PeerSettings.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http3/Http3PeerSettings.cs
@@ -8,16 +8,20 @@ internal sealed class Http3PeerSettings
     // Note these are protocol defaults, not Kestrel defaults.
     public const uint DefaultHeaderTableSize = 0;
     public const uint DefaultMaxRequestHeaderFieldSize = uint.MaxValue;
+    public const uint DefaultEnableWebTransport = 0;
+    public const uint DefaultH3Datagram = 0;
 
     public uint HeaderTableSize { get; internal set; } = DefaultHeaderTableSize;
     public uint MaxRequestHeaderFieldSectionSize { get; internal set; } = DefaultMaxRequestHeaderFieldSize;
+    public uint EnableWebTransport { get; internal set; } = DefaultEnableWebTransport;
+    public uint H3Datagram { get; internal set; } = DefaultH3Datagram;
 
     // Gets the settings that are different from the protocol defaults (as opposed to the server defaults).
     internal List<Http3PeerSetting> GetNonProtocolDefaults()
     {
-        // By default, there is only one setting that is sent from server to client.
+        // By default, there are only two settings that is sent from server to client.
         // Set capacity to that value.
-        var list = new List<Http3PeerSetting>(1);
+        var list = new List<Http3PeerSetting>(3);
 
         if (HeaderTableSize != DefaultHeaderTableSize)
         {
@@ -28,6 +32,12 @@ internal sealed class Http3PeerSettings
         {
             list.Add(new Http3PeerSetting(Http3SettingType.MaxFieldSectionSize, MaxRequestHeaderFieldSectionSize));
         }
+
+        // indicate that Kestrel supports WebTransport (TODO move into WebTransport project and inline)
+        list.Add(new Http3PeerSetting(Http3SettingType.EnableWebTransport, EnableWebTransport));
+
+        // indicate that Kestrel supports Http3 datgrams (TODO move into WebTransport project and inline)
+        list.Add(new Http3PeerSetting(Http3SettingType.H3Datagram, H3Datagram));
 
         return list;
     }
