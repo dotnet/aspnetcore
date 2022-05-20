9ce55b01d752cb4dd9d8a3d1addd5301e95dbdaa
Stephen Halter <halter73@gmail.com>
2022-05-20 15:28:08 -0700

    Remove outdated comments HostingApplicationDiagnostics

--- src/Hosting/Hosting/src/Internal/HostingApplicationDiagnostics.cs
+++ src/Hosting/Hosting/src/Internal/HostingApplicationDiagnostics.cs
@@ -326,12 +326,8 @@ internal sealed class HostingApplicationDiagnostics
         Activity? activity = null;
         if (_activitySource.HasListeners())
         {
-            if (ActivityContext.TryParse(requestId, traceState, true, out ActivityContext context))
+            if (ActivityContext.TryParse(requestId, traceState, isRemote: true, out ActivityContext context))
             {
-                // The requestId used W3C ID format. Unfortunately the ActivitySource.CreateActivity overload that
-                // takes a string ID sets ActivityContext.IsRemote = false when it parses the string internally.
-                // We work around that by using the ActivityContext ID overload and setting ActivityContext.IsRemote
-                // to true after parsing it.
                 activity = _activitySource.CreateActivity(ActivityName, ActivityKind.Server, context);
             }
             else
