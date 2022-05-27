f51ca4a5751d42a001f8f48b6ce9616ca2c26237
Daniel Genkin <t-dagenkin@microsoft.com>
2022-05-27 16:35:57 -0700

    Move Webtransport related parts into a new port and undid changed to 5001

--- src/Servers/Kestrel/samples/Http3SampleApp/Program.cs
+++ src/Servers/Kestrel/samples/Http3SampleApp/Program.cs
@@ -24,63 +24,20 @@ public class Program
             })
             .ConfigureWebHost(webHost =>
             {
+                var cert = CertificateLoader.LoadFromStoreCert("localhost", StoreName.My.ToString(), StoreLocation.CurrentUser, false);
+
                 webHost.UseKestrel()
                 .ConfigureKestrel((context, options) =>
                 {
-                    // CERTIFICATE STUFF THAT SHOULD BE MERGED WITH KESTREL EVENTUALLY///////////
-                    X509Certificate2 cert = null;
-                    var store = new X509Store("testing2", StoreLocation.CurrentUser);
-                    store.Open(OpenFlags.ReadWrite);
-                    if (store.Certificates.Count > 0)
-                    {
-                        cert = store.Certificates[^1];
-
-                        // rotate key after it expires
-                        if (DateTime.Parse(cert.GetExpirationDateString(), null) < DateTimeOffset.UtcNow)
-                        {
-                            cert = null;
-                        }
-                    }
-                    if (cert == null)
-                    {
-                        // generate a new cert
-                        var now = DateTimeOffset.UtcNow;
-                        SubjectAlternativeNameBuilder sanBuilder = new();
-                        sanBuilder.AddDnsName("localhost");
-                        using var ec = ECDsa.Create(ECCurve.NamedCurves.nistP256);
-                        CertificateRequest req = new("CN=localhost", ec, HashAlgorithmName.SHA256);
-                        // Adds purpose
-                        req.CertificateExtensions.Add(new X509EnhancedKeyUsageExtension(new OidCollection
-                        {
-                            new("1.3.6.1.5.5.7.3.1") // serverAuth
-                        }, false));
-                        // Adds usage
-                        req.CertificateExtensions.Add(new X509KeyUsageExtension(X509KeyUsageFlags.DigitalSignature, false));
-                        // Adds subject alternate names
-                        req.CertificateExtensions.Add(sanBuilder.Build());
-                        // Sign
-                        using var crt = req.CreateSelfSigned(now, now.AddDays(14)); // 14 days is the max duration of a certificate for this
-                        cert = new(crt.Export(X509ContentType.Pfx));
-
-                        // Save
-                        store.Add(cert);
-                    }
-                    store.Close();
-
-                    var hash = SHA256.HashData(cert.RawData);
-                    var certStr = Convert.ToBase64String(hash);
-                    Console.WriteLine($"\n\n\n\n\nCertificate: {certStr}\n\n\n\n");
-                    ///////////////////////////////////////////////////////////////////////////
-
                     options.ListenAnyIP(5000, listenOptions =>
                     {
                         listenOptions.UseConnectionLogging();
                         listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
                     });
 
-                    options.Listen(IPAddress.Any, 5001, listenOptions =>// USE THIS PORT FOR WEBTRANSPORT
+                    options.Listen(IPAddress.Any, 5001, listenOptions =>
                     {
-                        listenOptions.UseHttps(cert);
+                        listenOptions.UseHttps();
                         listenOptions.UseConnectionLogging();
                         listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
                     });
@@ -146,6 +103,14 @@ public class Program
                         listenOptions.UseConnectionLogging();
                         listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
                     });
+
+                    // Port configured for WebTransport
+                    options.Listen(IPAddress.Any, 5007, listenOptions =>
+                    {
+                        listenOptions.UseHttps(GenerateManualCertificate());
+                        listenOptions.UseConnectionLogging();
+                        listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
+                    });
                 })
                 .UseStartup<Startup>();
             });
@@ -156,5 +121,55 @@ public class Program
         using var httpEventSource = new HttpEventSourceListener(host.Services.GetRequiredService<ILoggerFactory>());
 
         host.Run();
+    }
+
+    // Adapted from: https://github.com/wegylexy/webtransport
+    // We will need to eventually merge this with existing Kestrel certificate generation
+    // tracked in issue #41762
+    private static X509Certificate2 GenerateManualCertificate()
+    {
+        X509Certificate2 cert = null;
+        var store = new X509Store("KestrelWebTransportCertificates", StoreLocation.CurrentUser);
+        store.Open(OpenFlags.ReadWrite);
+        if (store.Certificates.Count > 0)
+        {
+            cert = store.Certificates[^1];
+
+            // rotate key after it expires
+            if (DateTime.Parse(cert.GetExpirationDateString(), null) < DateTimeOffset.UtcNow)
+            {
+                cert = null;
+            }
+        }
+        if (cert == null)
+        {
+            // generate a new cert
+            var now = DateTimeOffset.UtcNow;
+            SubjectAlternativeNameBuilder sanBuilder = new();
+            sanBuilder.AddDnsName("localhost");
+            using var ec = ECDsa.Create(ECCurve.NamedCurves.nistP256);
+            CertificateRequest req = new("CN=localhost", ec, HashAlgorithmName.SHA256);
+            // Adds purpose
+            req.CertificateExtensions.Add(new X509EnhancedKeyUsageExtension(new OidCollection
+            {
+                new("1.3.6.1.5.5.7.3.1") // serverAuth
+            }, false));
+            // Adds usage
+            req.CertificateExtensions.Add(new X509KeyUsageExtension(X509KeyUsageFlags.DigitalSignature, false));
+            // Adds subject alternate names
+            req.CertificateExtensions.Add(sanBuilder.Build());
+            // Sign
+            using var crt = req.CreateSelfSigned(now, now.AddDays(14)); // 14 days is the max duration of a certificate for this
+            cert = new(crt.Export(X509ContentType.Pfx));
+
+            // Save
+            store.Add(cert);
+        }
+        store.Close();
+
+        var hash = SHA256.HashData(cert.RawData);
+        var certStr = Convert.ToBase64String(hash);
+        Console.WriteLine($"\n\n\n\n\nCertificate: {certStr}\n\n\n\n"); // <-- you will need to put this output into the JS API call to allo wthe connection
+        return cert;
     }
 }
