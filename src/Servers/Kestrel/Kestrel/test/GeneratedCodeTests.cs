fc1099934c666a34dfb2d075e5341385bbbb38ea
Chris R <Tratcher@outlook.com>
2022-05-27 14:14:41 -0700

    Remove Http2Connection generated code

--- src/Servers/Kestrel/Kestrel/test/GeneratedCodeTests.cs
+++ src/Servers/Kestrel/Kestrel/test/GeneratedCodeTests.cs
@@ -24,14 +24,12 @@ public class GeneratedCodeTests
         var httpHeadersGeneratedPath = Path.Combine(AppContext.BaseDirectory, "shared", "GeneratedContent", "HttpHeaders.Generated.cs");
         var httpProtocolGeneratedPath = Path.Combine(AppContext.BaseDirectory, "shared", "GeneratedContent", "HttpProtocol.Generated.cs");
         var httpUtilitiesGeneratedPath = Path.Combine(AppContext.BaseDirectory, "shared", "GeneratedContent", "HttpUtilities.Generated.cs");
-        var http2ConnectionGeneratedPath = Path.Combine(AppContext.BaseDirectory, "shared", "GeneratedContent", "Http2Connection.Generated.cs");
         var transportMultiplexedConnectionGeneratedPath = Path.Combine(AppContext.BaseDirectory, "shared", "GeneratedContent", "TransportMultiplexedConnection.Generated.cs");
         var transportConnectionGeneratedPath = Path.Combine(AppContext.BaseDirectory, "shared", "GeneratedContent", "TransportConnection.Generated.cs");
 
         var testHttpHeadersGeneratedPath = Path.GetTempFileName();
         var testHttpProtocolGeneratedPath = Path.GetTempFileName();
         var testHttpUtilitiesGeneratedPath = Path.GetTempFileName();
-        var testHttp2ConnectionGeneratedPath = Path.GetTempFileName();
         var testTransportMultiplexedConnectionGeneratedPath = Path.GetTempFileName();
         var testTransportConnectionGeneratedPath = Path.GetTempFileName();
 
@@ -40,28 +38,24 @@ public class GeneratedCodeTests
             var currentHttpHeadersGenerated = File.ReadAllText(httpHeadersGeneratedPath);
             var currentHttpProtocolGenerated = File.ReadAllText(httpProtocolGeneratedPath);
             var currentHttpUtilitiesGenerated = File.ReadAllText(httpUtilitiesGeneratedPath);
-            var currentHttp2ConnectionGenerated = File.ReadAllText(http2ConnectionGeneratedPath);
             var currentTransportConnectionBaseGenerated = File.ReadAllText(transportMultiplexedConnectionGeneratedPath);
             var currentTransportConnectionGenerated = File.ReadAllText(transportConnectionGeneratedPath);
 
             CodeGenerator.Program.Run(testHttpHeadersGeneratedPath,
                 testHttpProtocolGeneratedPath,
                 testHttpUtilitiesGeneratedPath,
-                testHttp2ConnectionGeneratedPath,
                 testTransportMultiplexedConnectionGeneratedPath,
                 testTransportConnectionGeneratedPath);
 
             var testHttpHeadersGenerated = File.ReadAllText(testHttpHeadersGeneratedPath);
             var testHttpProtocolGenerated = File.ReadAllText(testHttpProtocolGeneratedPath);
             var testHttpUtilitiesGenerated = File.ReadAllText(testHttpUtilitiesGeneratedPath);
-            var testHttp2ConnectionGenerated = File.ReadAllText(testHttp2ConnectionGeneratedPath);
             var testTransportMultiplxedConnectionGenerated = File.ReadAllText(testTransportMultiplexedConnectionGeneratedPath);
             var testTransportConnectionGenerated = File.ReadAllText(testTransportConnectionGeneratedPath);
 
             AssertFileContentEqual(currentHttpHeadersGenerated, testHttpHeadersGenerated, "HTTP headers");
             AssertFileContentEqual(currentHttpProtocolGenerated, testHttpProtocolGenerated, "HTTP protocol");
             AssertFileContentEqual(currentHttpUtilitiesGenerated, testHttpUtilitiesGenerated, "HTTP utilities");
-            AssertFileContentEqual(currentHttp2ConnectionGenerated, testHttp2ConnectionGenerated, "HTTP2 connection");
             AssertFileContentEqual(currentTransportConnectionBaseGenerated, testTransportMultiplxedConnectionGenerated, "TransportConnectionBase");
             AssertFileContentEqual(currentTransportConnectionGenerated, testTransportConnectionGenerated, "TransportConnection");
         }
@@ -70,7 +64,6 @@ public class GeneratedCodeTests
             File.Delete(testHttpHeadersGeneratedPath);
             File.Delete(testHttpProtocolGeneratedPath);
             File.Delete(testHttpUtilitiesGeneratedPath);
-            File.Delete(testHttp2ConnectionGeneratedPath);
             File.Delete(testTransportMultiplexedConnectionGeneratedPath);
             File.Delete(testTransportConnectionGeneratedPath);
         }
