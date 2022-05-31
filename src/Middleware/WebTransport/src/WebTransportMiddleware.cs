586ccc8c895862b65645c4b0f979db1eecd29626
Daniel Genkin <t-dagenkin@microsoft.com>
2022-05-31 13:56:18 -0700

    Added a new WebTransportMiddleware project

---
+++ src/Middleware/WebTransport/src/WebTransportMiddleware.cs
@@ -0,0 +1,20 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.AspNetCore.WebTransport;
+
+/// <summary>
+/// Main class responsible for accepting and managing WebTransport
+/// sessions
+/// </summary>
+public class WebTransportMiddleware
+{
+    /// <summary>
+    /// Not implemented yet. It will be soon though
+    /// </summary>
+    /// <exception cref="NotImplementedException"></exception>
+    public WebTransportMiddleware()
+    {
+        throw new NotImplementedException();
+    }
+}
