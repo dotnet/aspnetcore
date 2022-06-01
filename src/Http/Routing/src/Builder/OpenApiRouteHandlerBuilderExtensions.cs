3652f213a746b059e3caa893bd470b0043eb1394
Stephen Halter <halter73@gmail.com>
2022-05-31 17:08:44 -0700

    Remove overloads that were not api-approved

--- src/Http/Routing/src/Builder/OpenApiRouteHandlerBuilderExtensions.cs
+++ src/Http/Routing/src/Builder/OpenApiRouteHandlerBuilderExtensions.cs
@@ -38,38 +38,6 @@ public static class OpenApiRouteHandlerBuilderExtensions
     /// Adds an <see cref="IProducesResponseTypeMetadata"/> to <see cref="EndpointBuilder.Metadata"/> for all endpoints
     /// produced by <paramref name="builder"/>.
     /// </summary>
-    /// <param name="builder">The <see cref="IEndpointConventionBuilder"/>.</param>
-    /// <param name="responseType">The type of the response. Defaults to null.</param>
-    /// <param name="statusCode">The response status code.</param>
-    /// <param name="contentType">The response content type. Defaults to "application/json" if responseType is not null, otherwise defaults to null.</param>
-    /// <param name="additionalContentTypes">Additional response content types the endpoint produces for the supplied status code.</param>
-    /// <returns>A <see cref="RouteHandlerBuilder"/> that can be used to further customize the endpoint.</returns>
-#pragma warning disable RS0026
-    public static TBuilder Produces<TBuilder>(
-#pragma warning restore RS0026
-        this TBuilder builder,
-        Type? responseType = null,
-        int statusCode = StatusCodes.Status200OK,
-        string? contentType = null,
-        params string[] additionalContentTypes) where TBuilder : IEndpointConventionBuilder
-    {
-        if (responseType is Type && string.IsNullOrEmpty(contentType))
-        {
-            contentType = "application/json";
-        }
-
-        if (contentType is null)
-        {
-            return builder.WithMetadata(new ProducesResponseTypeMetadata(responseType ?? typeof(void), statusCode));
-        }
-
-        return builder.WithMetadata(new ProducesResponseTypeMetadata(responseType ?? typeof(void), statusCode, contentType, additionalContentTypes));
-    }
-
-    /// <summary>
-    /// Adds an <see cref="IProducesResponseTypeMetadata"/> to <see cref="EndpointBuilder.Metadata"/> for all endpoints
-    /// produced by <paramref name="builder"/>.
-    /// </summary>
     /// <typeparam name="TResponse">The type of the response.</typeparam>
     /// <param name="builder">The <see cref="RouteHandlerBuilder"/>.</param>
     /// <param name="statusCode">The response status code. Defaults to <see cref="StatusCodes.Status200OK"/>.</param>
@@ -84,7 +52,7 @@ public static class OpenApiRouteHandlerBuilderExtensions
         string? contentType = null,
         params string[] additionalContentTypes)
     {
-        return Produces(builder, typeof(TResponse), statusCode, contentType, additionalContentTypes);
+        return Produces(builder, statusCode, typeof(TResponse), contentType, additionalContentTypes);
     }
 
     /// <summary>
@@ -106,30 +74,17 @@ public static class OpenApiRouteHandlerBuilderExtensions
         string? contentType = null,
         params string[] additionalContentTypes)
     {
-        return Produces(builder, responseType, statusCode, contentType, additionalContentTypes);
-    }
-
-    /// <summary>
-    /// Adds an <see cref="IProducesResponseTypeMetadata"/> with a <see cref="ProblemDetails"/> type
-    /// to <see cref="EndpointBuilder.Metadata"/> for all endpoints produced by <paramref name="builder"/>.
-    /// </summary>
-    /// <param name="builder">The <see cref="IEndpointConventionBuilder"/>.</param>
-    /// <param name="statusCode">The response status code.</param>
-    /// <param name="contentType">The response content type. Defaults to "application/problem+json".</param>
-    /// <returns>A <see cref="RouteHandlerBuilder"/> that can be used to further customize the endpoint.</returns>
-#pragma warning disable RS0026
-    public static TBuilder ProducesProblem<TBuilder>(
-#pragma warning restore RS0026
-        this TBuilder builder,
-        int statusCode,
-        string? contentType = null) where TBuilder : IEndpointConventionBuilder
-    {
-        if (string.IsNullOrEmpty(contentType))
+        if (responseType is Type && string.IsNullOrEmpty(contentType))
         {
-            contentType = "application/problem+json";
+            contentType = "application/json";
         }
 
-        return Produces(builder, typeof(ProblemDetails), statusCode, contentType);
+        if (contentType is null)
+        {
+            return builder.WithMetadata(new ProducesResponseTypeMetadata(responseType ?? typeof(void), statusCode));
+        }
+
+        return builder.WithMetadata(new ProducesResponseTypeMetadata(responseType ?? typeof(void), statusCode, contentType, additionalContentTypes));
     }
 
     /// <summary>
@@ -141,29 +96,13 @@ public static class OpenApiRouteHandlerBuilderExtensions
     /// <param name="contentType">The response content type. Defaults to "application/problem+json".</param>
     /// <returns>A <see cref="RouteHandlerBuilder"/> that can be used to further customize the endpoint.</returns>
     public static RouteHandlerBuilder ProducesProblem(this RouteHandlerBuilder builder, int statusCode, string? contentType = null)
-        => ProducesProblem<RouteHandlerBuilder>(builder, statusCode, contentType);
-
-    /// <summary>
-    /// Adds an <see cref="IProducesResponseTypeMetadata"/> with a <see cref="HttpValidationProblemDetails"/> type
-    /// to <see cref="EndpointBuilder.Metadata"/> for all endpoints produced by <paramref name="builder"/>.
-    /// </summary>
-    /// <param name="builder">The <see cref="IEndpointConventionBuilder"/>.</param>
-    /// <param name="statusCode">The response status code. Defaults to <see cref="StatusCodes.Status400BadRequest"/>.</param>
-    /// <param name="contentType">The response content type. Defaults to "application/problem+json".</param>
-    /// <returns>A <see cref="RouteHandlerBuilder"/> that can be used to further customize the endpoint.</returns>
-#pragma warning disable RS0026
-    public static TBuilder ProducesValidationProblem<TBuilder>(
-#pragma warning restore RS0026
-        this TBuilder builder,
-        int statusCode = StatusCodes.Status400BadRequest,
-        string? contentType = null) where TBuilder : IEndpointConventionBuilder
     {
         if (string.IsNullOrEmpty(contentType))
         {
             contentType = "application/problem+json";
         }
 
-        return Produces(builder, typeof(HttpValidationProblemDetails), statusCode, contentType);
+        return Produces(builder, statusCode, typeof(ProblemDetails), contentType);
     }
 
     /// <summary>
@@ -179,7 +118,12 @@ public static class OpenApiRouteHandlerBuilderExtensions
         int statusCode = StatusCodes.Status400BadRequest,
         string? contentType = null)
     {
-        return ProducesValidationProblem<RouteHandlerBuilder>(builder, statusCode, contentType);
+        if (string.IsNullOrEmpty(contentType))
+        {
+            contentType = "application/problem+json";
+        }
+
+        return Produces(builder, statusCode, typeof(HttpValidationProblemDetails), contentType);
     }
 
     /// <summary>
@@ -216,37 +160,52 @@ public static class OpenApiRouteHandlerBuilderExtensions
     /// Adds <see cref="IAcceptsMetadata"/> to <see cref="EndpointBuilder.Metadata"/> for all endpoints
     /// produced by <paramref name="builder"/>.
     /// </summary>
-    /// <param name="builder">The <see cref="IEndpointConventionBuilder"/>.</param>
-    /// <param name="requestType">The type of the request body.</param>
-    /// <param name="isOptional">Sets a value that determines if the request body is optional.</param>
+    /// <typeparam name="TRequest">The type of the request body.</typeparam>
+    /// <param name="builder">The <see cref="RouteHandlerBuilder"/>.</param>
     /// <param name="contentType">The request content type that the endpoint accepts.</param>
     /// <param name="additionalContentTypes">The list of additional request content types that the endpoint accepts.</param>
     /// <returns>A <see cref="RouteHandlerBuilder"/> that can be used to further customize the endpoint.</returns>
-    public static TBuilder Accepts<TBuilder>(
-        this TBuilder builder,
-        Type requestType,
-        bool isOptional,
+    public static RouteHandlerBuilder Accepts<TRequest>(
+        this RouteHandlerBuilder builder,
         string contentType,
-        params string[] additionalContentTypes) where TBuilder : IEndpointConventionBuilder
+        params string[] additionalContentTypes) where TRequest : notnull
     {
-        var contentTypes = GetAllContentTypes(contentType, additionalContentTypes);
-        return builder.WithMetadata(new AcceptsMetadata(requestType, isOptional, contentTypes));
+        return Accepts(builder, typeof(TRequest), contentType, additionalContentTypes);
     }
 
     /// <summary>
     /// Adds <see cref="IAcceptsMetadata"/> to <see cref="EndpointBuilder.Metadata"/> for all endpoints
     /// produced by <paramref name="builder"/>.
     /// </summary>
-    /// <param name="builder">The <see cref="IEndpointConventionBuilder"/>.</param>
+    /// <typeparam name="TRequest">The type of the request body.</typeparam>
+    /// <param name="builder">The <see cref="RouteHandlerBuilder"/>.</param>
+    /// <param name="isOptional">Sets a value that determines if the request body is optional.</param>
+    /// <param name="contentType">The request content type that the endpoint accepts.</param>
+    /// <param name="additionalContentTypes">The list of additional request content types that the endpoint accepts.</param>
+    /// <returns>A <see cref="RouteHandlerBuilder"/> that can be used to further customize the endpoint.</returns>
+    public static RouteHandlerBuilder Accepts<TRequest>(
+        this RouteHandlerBuilder builder,
+        bool isOptional,
+        string contentType,
+        params string[] additionalContentTypes) where TRequest : notnull
+    {
+        return Accepts(builder, typeof(TRequest), isOptional, contentType, additionalContentTypes);
+    }
+
+    /// <summary>
+    /// Adds <see cref="IAcceptsMetadata"/> to <see cref="EndpointBuilder.Metadata"/> for all endpoints
+    /// produced by <paramref name="builder"/>.
+    /// </summary>
+    /// <param name="builder">The <see cref="RouteHandlerBuilder"/>.</param>
     /// <param name="requestType">The type of the request body.</param>
     /// <param name="contentType">The request content type that the endpoint accepts.</param>
     /// <param name="additionalContentTypes">The list of additional request content types that the endpoint accepts.</param>
     /// <returns>A <see cref="RouteHandlerBuilder"/> that can be used to further customize the endpoint.</returns>
-    public static TBuilder Accepts<TBuilder>(
-        this TBuilder builder,
+    public static RouteHandlerBuilder Accepts(
+        this RouteHandlerBuilder builder,
         Type requestType,
         string contentType,
-        params string[] additionalContentTypes) where TBuilder : IEndpointConventionBuilder
+        params string[] additionalContentTypes)
     {
         return Accepts(builder, requestType, isOptional: false, contentType, additionalContentTypes);
     }
@@ -255,60 +214,6 @@ public static class OpenApiRouteHandlerBuilderExtensions
     /// Adds <see cref="IAcceptsMetadata"/> to <see cref="EndpointBuilder.Metadata"/> for all endpoints
     /// produced by <paramref name="builder"/>.
     /// </summary>
-    /// <typeparam name="TRequest">The type of the request body.</typeparam>
-    /// <param name="builder">The <see cref="RouteHandlerBuilder"/>.</param>
-    /// <param name="contentType">The request content type that the endpoint accepts.</param>
-    /// <param name="additionalContentTypes">The list of additional request content types that the endpoint accepts.</param>
-    /// <returns>A <see cref="RouteHandlerBuilder"/> that can be used to further customize the endpoint.</returns>
-    public static RouteHandlerBuilder Accepts<TRequest>(
-        this RouteHandlerBuilder builder,
-        string contentType,
-        params string[] additionalContentTypes) where TRequest : notnull
-    {
-        return Accepts<RouteHandlerBuilder>(builder, typeof(TRequest), contentType, additionalContentTypes);
-    }
-
-    /// <summary>
-    /// Adds <see cref="IAcceptsMetadata"/> to <see cref="EndpointBuilder.Metadata"/> for all endpoints
-    /// produced by <paramref name="builder"/>.
-    /// </summary>
-    /// <typeparam name="TRequest">The type of the request body.</typeparam>
-    /// <param name="builder">The <see cref="RouteHandlerBuilder"/>.</param>
-    /// <param name="isOptional">Sets a value that determines if the request body is optional.</param>
-    /// <param name="contentType">The request content type that the endpoint accepts.</param>
-    /// <param name="additionalContentTypes">The list of additional request content types that the endpoint accepts.</param>
-    /// <returns>A <see cref="RouteHandlerBuilder"/> that can be used to further customize the endpoint.</returns>
-    public static RouteHandlerBuilder Accepts<TRequest>(
-        this RouteHandlerBuilder builder,
-        bool isOptional,
-        string contentType,
-        params string[] additionalContentTypes) where TRequest : notnull
-    {
-        return Accepts<RouteHandlerBuilder>(builder, typeof(TRequest), isOptional, contentType, additionalContentTypes);
-    }
-
-    /// <summary>
-    /// Adds <see cref="IAcceptsMetadata"/> to <see cref="EndpointBuilder.Metadata"/> for all endpoints
-    /// produced by <paramref name="builder"/>.
-    /// </summary>
-    /// <param name="builder">The <see cref="RouteHandlerBuilder"/>.</param>
-    /// <param name="requestType">The type of the request body.</param>
-    /// <param name="contentType">The request content type that the endpoint accepts.</param>
-    /// <param name="additionalContentTypes">The list of additional request content types that the endpoint accepts.</param>
-    /// <returns>A <see cref="RouteHandlerBuilder"/> that can be used to further customize the endpoint.</returns>
-    public static RouteHandlerBuilder Accepts(
-        this RouteHandlerBuilder builder,
-        Type requestType,
-        string contentType,
-        params string[] additionalContentTypes)
-    {
-        return Accepts<RouteHandlerBuilder>(builder, requestType, contentType, additionalContentTypes);
-    }
-
-    /// <summary>
-    /// Adds <see cref="IAcceptsMetadata"/> to <see cref="EndpointBuilder.Metadata"/> for all endpoints
-    /// produced by <paramref name="builder"/>.
-    /// </summary>
     /// <param name="builder">The <see cref="RouteHandlerBuilder"/>.</param>
     /// <param name="requestType">The type of the request body.</param>
     /// <param name="isOptional">Sets a value that determines if the request body is optional.</param>
@@ -322,7 +227,8 @@ public static class OpenApiRouteHandlerBuilderExtensions
         string contentType,
         params string[] additionalContentTypes)
     {
-        return Accepts<RouteHandlerBuilder>(builder, requestType, isOptional, contentType, additionalContentTypes);
+        var contentTypes = GetAllContentTypes(contentType, additionalContentTypes);
+        return builder.WithMetadata(new AcceptsMetadata(requestType, isOptional, contentTypes));
     }
 
     /// <summary>
--- src/Http/Routing/test/UnitTests/Builder/OpenApiRouteHandlerBuilderExtensionsTest.cs
+++ src/Http/Routing/test/UnitTests/Builder/OpenApiRouteHandlerBuilderExtensionsTest.cs
@@ -1,11 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Metadata;
@@ -28,98 +23,6 @@ public class OpenApiRouteHandlerBuilderExtensionsTest
     }
 
     [Fact]
-    public void Produces_AddsProducesResponseTypeMetadataWithJsonContentType()
-    {
-        static void GenericProducesType(IEndpointConventionBuilder builder) =>
-            builder.Produces(typeof(TestEndointConventionBuilder));
-        static void SpecificProducesType(RouteHandlerBuilder builder) =>
-            builder.Produces<TestEndointConventionBuilder>();
-
-        static void AssertMetadata(EndpointBuilder builder)
-        {
-            var metadata = Assert.IsType<ProducesResponseTypeMetadata>(Assert.Single(builder.Metadata));
-
-            Assert.Equal(typeof(TestEndointConventionBuilder), metadata.Type);
-            Assert.Equal(StatusCodes.Status200OK, metadata.StatusCode);
-            Assert.Equal("application/json", Assert.Single(metadata.ContentTypes));
-        }
-
-        RunWithBothBuilders(GenericProducesType, SpecificProducesType, AssertMetadata);
-    }
-
-    [Fact]
-    public void Produces_AddsProducesResponseTypeMetadataWithVoidType()
-    {
-        static void GenericProducesNotFound(IEndpointConventionBuilder builder) =>
-            builder.Produces(statusCode: StatusCodes.Status404NotFound);
-        static void SpecificProducesNotFound(RouteHandlerBuilder builder) =>
-            builder.Produces(StatusCodes.Status404NotFound);
-
-        static void AssertMetadata(EndpointBuilder builder)
-        {
-            var metadata = Assert.IsType<ProducesResponseTypeMetadata>(Assert.Single(builder.Metadata));
-
-            Assert.Equal(typeof(void), metadata.Type);
-            Assert.Equal(StatusCodes.Status404NotFound, metadata.StatusCode);
-            Assert.Empty(metadata.ContentTypes);
-        }
-
-        RunWithBothBuilders(GenericProducesNotFound, SpecificProducesNotFound, AssertMetadata);
-    }
-
-    [Fact]
-    public void Produces_WithNoArgs_AddsProducesResponseTypeMetadata()
-    {
-        var builder = new TestEndointConventionBuilder();
-        builder.Produces();
-
-        var metadata = Assert.IsType<ProducesResponseTypeMetadata>(Assert.Single(builder.Metadata));
-        Assert.Equal(typeof(void), metadata.Type);
-        Assert.Equal(StatusCodes.Status200OK, metadata.StatusCode);
-        Assert.Empty(metadata.ContentTypes);
-    }
-
-    [Fact]
-    public void ProdcesProblem_AddsProducesResponseTypeMetadataWithProblemDetailsType()
-    {
-        static void GenericProducesProblem(IEndpointConventionBuilder builder) =>
-            builder.ProducesProblem(StatusCodes.Status400BadRequest);
-        static void SpecificProducesProblem(RouteHandlerBuilder builder) =>
-            builder.ProducesProblem(StatusCodes.Status400BadRequest);
-
-        static void AssertMetadata(EndpointBuilder builder)
-        {
-            var metadata = Assert.IsType<ProducesResponseTypeMetadata>(Assert.Single(builder.Metadata));
-
-            Assert.Equal(typeof(ProblemDetails), metadata.Type);
-            Assert.Equal(StatusCodes.Status400BadRequest, metadata.StatusCode);
-            Assert.Equal("application/problem+json", Assert.Single(metadata.ContentTypes));
-        }
-
-        RunWithBothBuilders(GenericProducesProblem, SpecificProducesProblem, AssertMetadata);
-    }
-
-    [Fact]
-    public void ProdcesValidiationProblem_AddsProducesResponseTypeMetadataWithHttpValidationProblemDetailsType()
-    {
-        static void GenericProducesValidationProblem(IEndpointConventionBuilder builder) =>
-            builder.ProducesValidationProblem();
-        static void SpecificProducesValidationProblem(RouteHandlerBuilder builder) =>
-            builder.ProducesValidationProblem();
-
-        static void AssertMetadata(EndpointBuilder builder)
-        {
-            var metadata = Assert.IsType<ProducesResponseTypeMetadata>(Assert.Single(builder.Metadata));
-
-            Assert.Equal(typeof(HttpValidationProblemDetails), metadata.Type);
-            Assert.Equal(StatusCodes.Status400BadRequest, metadata.StatusCode);
-            Assert.Equal("application/problem+json", Assert.Single(metadata.ContentTypes));
-        }
-
-        RunWithBothBuilders(GenericProducesValidationProblem, SpecificProducesValidationProblem, AssertMetadata);
-    }
-
-    [Fact]
     public void WithTags_AddsTagsAttribute()
     {
         static void GenericWithTags(IEndpointConventionBuilder builder) => builder.WithTags("a", "b", "c");
@@ -138,23 +41,74 @@ public class OpenApiRouteHandlerBuilderExtensionsTest
     }
 
     [Fact]
+    public void Produces_AddsProducesResponseTypeMetadataWithJsonContentType()
+    {
+        var testBuilder = new TestEndointConventionBuilder();
+        var builder = new RouteHandlerBuilder(new[] { testBuilder });
+
+        builder.Produces<TestEndointConventionBuilder>();
+
+        var metadata = Assert.IsType<ProducesResponseTypeMetadata>(Assert.Single(testBuilder.Metadata));
+        Assert.Equal(typeof(TestEndointConventionBuilder), metadata.Type);
+        Assert.Equal(StatusCodes.Status200OK, metadata.StatusCode);
+        Assert.Equal("application/json", Assert.Single(metadata.ContentTypes));
+    }
+
+    [Fact]
+    public void Produces_AddsProducesResponseTypeMetadataWithVoidType()
+    {
+        var testBuilder = new TestEndointConventionBuilder();
+        var builder = new RouteHandlerBuilder(new[] { testBuilder });
+
+        builder.Produces(StatusCodes.Status404NotFound);
+
+        var metadata = Assert.IsType<ProducesResponseTypeMetadata>(Assert.Single(testBuilder.Metadata));
+        Assert.Equal(typeof(void), metadata.Type);
+        Assert.Equal(StatusCodes.Status404NotFound, metadata.StatusCode);
+        Assert.Empty(metadata.ContentTypes);
+    }
+
+    [Fact]
+    public void ProdcesProblem_AddsProducesResponseTypeMetadataWithProblemDetailsType()
+    {
+        var testBuilder = new TestEndointConventionBuilder();
+        var builder = new RouteHandlerBuilder(new[] { testBuilder });
+
+        builder.ProducesProblem(StatusCodes.Status400BadRequest);
+
+        var metadata = Assert.IsType<ProducesResponseTypeMetadata>(Assert.Single(testBuilder.Metadata));
+        Assert.Equal(typeof(ProblemDetails), metadata.Type);
+        Assert.Equal(StatusCodes.Status400BadRequest, metadata.StatusCode);
+        Assert.Equal("application/problem+json", Assert.Single(metadata.ContentTypes));
+    }
+
+    [Fact]
+    public void ProdcesValidiationProblem_AddsProducesResponseTypeMetadataWithHttpValidationProblemDetailsType()
+    {
+        var testBuilder = new TestEndointConventionBuilder();
+        var builder = new RouteHandlerBuilder(new[] { testBuilder });
+
+        builder.ProducesValidationProblem();
+
+        var metadata = Assert.IsType<ProducesResponseTypeMetadata>(Assert.Single(testBuilder.Metadata));
+        Assert.Equal(typeof(HttpValidationProblemDetails), metadata.Type);
+        Assert.Equal(StatusCodes.Status400BadRequest, metadata.StatusCode);
+        Assert.Equal("application/problem+json", Assert.Single(metadata.ContentTypes));
+    }
+
+    [Fact]
     public void Accepts_AddsAcceptsMetadataWithSpecifiedType()
     {
-        static void GenericAccepts(IEndpointConventionBuilder builder) =>
-            builder.Accepts(typeof(TestEndointConventionBuilder), "text/plain");
-        static void SpecificAccepts(RouteHandlerBuilder builder) =>
-            builder.Accepts<TestEndointConventionBuilder>("text/plain");
+        var testBuilder = new TestEndointConventionBuilder();
+        var builder = new RouteHandlerBuilder(new[] { testBuilder });
 
-        static void AssertMetadata(EndpointBuilder builder)
-        {
-            var metadata = Assert.IsType<AcceptsMetadata>(Assert.Single(builder.Metadata));
+        builder.Accepts<TestEndointConventionBuilder>("text/plain");
 
-            Assert.Equal(typeof(TestEndointConventionBuilder), metadata.RequestType);
-            Assert.Equal("text/plain", Assert.Single(metadata.ContentTypes));
-            Assert.False(metadata.IsOptional);
-        }
+        var metadata = Assert.IsType<AcceptsMetadata>(Assert.Single(testBuilder.Metadata));
 
-        RunWithBothBuilders(GenericAccepts, SpecificAccepts, AssertMetadata);
+        Assert.Equal(typeof(TestEndointConventionBuilder), metadata.RequestType);
+        Assert.Equal("text/plain", Assert.Single(metadata.ContentTypes));
+        Assert.False(metadata.IsOptional);
     }
 
     [Fact]
