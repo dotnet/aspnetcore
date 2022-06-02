ce7eca5646c082a6b82c93d6f5663ca44db94175
Daniel Genkin <t-dagenkin@microsoft.com>
2022-06-02 14:54:36 -0700

    Fixed some issues that caused CI to fail

--- src/Middleware/WebTransport/samples/EchoApp/Program.cs
+++ src/Middleware/WebTransport/samples/EchoApp/Program.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace EchoApp;
+namespace WebTransport.EchoApp;
 
 public class Program
 {
--- src/Middleware/WebTransport/samples/EchoApp/Startup.cs
+++ src/Middleware/WebTransport/samples/EchoApp/Startup.cs
@@ -1,10 +1,10 @@
-//// Licensed to the .NET Foundation under one or more agreements.
-//// The .NET Foundation licenses this file to you under the MIT license.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
 //using System.Net.WebTransport;
 using System.Text;
 
-namespace EchoApp;
+namespace WebTransport.EchoApp;
 
 public class Startup
 {
