e07c7c2666d81e69f243d8c5fd78b314ecb96ad8
Mackinnon Buck <mackinnon.buck@gmail.com>
2022-06-06 14:13:30 -0700

    Updated spa template JS dependencies

--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/App.js
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/React-CSharp/ClientApp/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/ClientApp/src/App.tsx
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/ClientApp/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Route } from 'react-router';
+import { Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
