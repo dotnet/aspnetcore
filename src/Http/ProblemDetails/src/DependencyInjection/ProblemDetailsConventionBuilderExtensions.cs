961830269a3a9a066a93cfe708c5602ef96fb997
Bruno Oliveira <brolivei@microsoft.com>
2022-06-07 10:35:08 -0700

    Using metadata

--- src/Http/ProblemDetails/src/DependencyInjection/ProblemDetailsConventionBuilderExtensions.cs
+++ src/Http/ProblemDetails/src/DependencyInjection/ProblemDetailsConventionBuilderExtensions.cs
@@ -2,11 +2,21 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 
-namespace Microsoft.AspNetCore.Http.ProblemDetails.DependencyInjection;
+namespace Microsoft.Extensions.DependencyInjection;
 
+/// <summary>
+/// 
+/// </summary>
 public static class ProblemDetailsConventionBuilderExtensions
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TBuilder"></typeparam>
+    /// <param name="builder"></param>
+    /// <returns></returns>
     public static TBuilder WithProblemDetails<TBuilder>(this TBuilder builder) where TBuilder : IEndpointConventionBuilder
-    => builder.WithMetadata(new TagsAttribute(tags));
+    => builder.WithMetadata(new ProblemDetailsResponseMetadata());
 }
