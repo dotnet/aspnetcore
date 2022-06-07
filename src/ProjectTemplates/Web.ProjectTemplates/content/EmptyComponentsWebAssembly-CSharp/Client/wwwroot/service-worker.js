d9767a1b8a85b715615aa11e502fa55ddb6769e6
Cera Samson <cerasamson@gmail.com>
2022-06-07 10:42:28 -0700

    Delete Empty WASM Template files

--- src/ProjectTemplates/Web.ProjectTemplates/content/EmptyComponentsWebAssembly-CSharp/Client/wwwroot/service-worker.js
+++
@@ -1,4 +0,0 @@
-// In development, always fetch from the network and do not enable offline support.
-// This is because caching would make development more difficult (changes would not
-// be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
--- src/ProjectTemplates/Web.ProjectTemplates/content/EmptyComponentsWebAssembly-CSharp/Server/Models/ApplicationUser.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace EmptyComponentsWebAssembly_CSharp.Server.Models;
-
-public class ApplicationUser : IdentityUser
-{
-}
