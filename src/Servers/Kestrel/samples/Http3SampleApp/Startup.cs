a68d9cf0119e7a1f02933e2f1e72d49b5bc78ba1
Daniel Genkin <t-dagenkin@microsoft.com>
2022-06-07 10:19:54 -0700

    Finish off the webtransport handshake and get ready for accepting streams and such

--- src/Servers/Kestrel/samples/Http3SampleApp/Startup.cs
+++ src/Servers/Kestrel/samples/Http3SampleApp/Startup.cs
@@ -18,9 +18,15 @@ public class Startup
         {
             var memory = new Memory<byte>(new byte[4096]);
             var length = await context.Request.Body.ReadAsync(memory);
-            context.Response.Headers["test"] = "foo";
-            // for testing
-            await context.Response.WriteAsync($"Hello World! {context.Request.Protocol} {context.Connection.ClientCertificate?.Subject}");
+
+            AppContext.TryGetSwitch("Microsoft.AspNetCore.Server.Kestrel.Experimental.WebTransportAndH3Datagrams", out var isWebTransport);
+            if (isWebTransport)
+            {
+                context.Response.Headers.Append("sec-webtransport-http3-draft", "draft02");
+                await context.Response.Body.FlushAsync();
+
+                await Task.Delay(TimeSpan.FromMinutes(5));
+            }
         });
     }
 }
