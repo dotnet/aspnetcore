2e100cfcda0c96baa1e3178a57cc185276c74556
Cera Samson <cerasamson@gmail.com>
2022-06-10 14:51:42 -0700

    Added EmptyBlazorServerTest; fixed dismiss formatting

---
+++ src/ProjectTemplates/test/EmptyBlazorServerTemplateTest.cs
@@ -0,0 +1,28 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Testing;
+using Templates.Test.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Templates.Test;
+
+public class EmptyBlazorServerTemplateTest : BlazorTemplateTest
+{
+    public EmptyBlazorServerTemplateTest(ProjectFactoryFixture projectFactory)
+        : base(projectFactory)
+    {
+    }
+
+    public override string ProjectType { get; } = "blazorserver-empty";
+
+    [Fact]
+    public Task EmptyBlazorServerTemplateWorks() => CreateBuildPublishAsync();
+}
