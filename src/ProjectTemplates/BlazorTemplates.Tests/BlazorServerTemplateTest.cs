a7226615ff5fb15e37599f4fc7a3886d46ce436f
Tanay Parikh <TanayParikh@users.noreply.github.com>
2022-06-17 21:03:34 +0000

    Initial Unskip

--- src/ProjectTemplates/BlazorTemplates.Tests/BlazorServerTemplateTest.cs
+++ src/ProjectTemplates/BlazorTemplates.Tests/BlazorServerTemplateTest.cs
@@ -25,7 +25,7 @@ public class BlazorServerTemplateTest : BlazorTemplateTest
 
     public override string ProjectType { get; } = "blazorserver";
 
-    [Theory(Skip = "https://github.com/dotnet/aspnetcore/issues/30761")]
+    [Theory]
     [InlineData(BrowserKind.Chromium)]
     public async Task BlazorServerTemplateWorks_NoAuth(BrowserKind browserKind)
     {
@@ -80,7 +80,7 @@ public class BlazorServerTemplateTest : BlazorTemplateTest
     public static IEnumerable<object[]> BlazorServerTemplateWorks_IndividualAuthData =>
             BrowserManager.WithBrowsers(new[] { BrowserKind.Chromium }, true, false);
 
-    [Theory(Skip = "https://github.com/dotnet/aspnetcore/issues/30882")]
+    [Theory]
     [MemberData(nameof(BlazorServerTemplateWorks_IndividualAuthData))]
     [SkipOnHelix("https://github.com/dotnet/aspnetcore/issues/30825", Queues = "All.OSX")]
     public async Task BlazorServerTemplateWorks_IndividualAuth(BrowserKind browserKind)
@@ -180,7 +180,7 @@ public class BlazorServerTemplateTest : BlazorTemplateTest
         Assert.Equal(5, await page.Locator("p+table>tbody>tr").CountAsync());
     }
 
-    [Theory(Skip = "https://github.com/dotnet/aspnetcore/issues/30882")]
+    [Theory]
     [InlineData("IndividualB2C", null)]
     [InlineData("IndividualB2C", new [] { "--called-api-url \"https://graph.microsoft.com\"", "--called-api-scopes user.readwrite" })]
     [InlineData("SingleOrg", null)]
