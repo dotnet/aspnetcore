25132ba4ef3715085fc7be4745507e083e696de6
Cera Samson <t-samsoncera@microsoft.com>
2022-06-20 18:47:25 -0700

    Update src/ProjectTemplates/Web.ProjectTemplates/content/EmptyComponentsWebAssembly-CSharp/Server/Program.cs
    Co-authored-by: Tanay Parikh <TanayParikh@users.noreply.github.com>

--- src/ProjectTemplates/Web.ProjectTemplates/content/EmptyComponentsWebAssembly-CSharp/Server/Program.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/EmptyComponentsWebAssembly-CSharp/Server/Program.cs
@@ -12,15 +12,13 @@ if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
 }
+#if (HasHttpsProfile)
 else
 {
-#if (HasHttpsProfile)
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-#endif
 }
 
-#if (HasHttpsProfile)
 app.UseHttpsRedirection();
 #endif
 app.UseBlazorFrameworkFiles();
