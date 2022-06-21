a382a0fe0a578c47d5831412a818ff632bdc4f98
Chris Ross <chrross@microsoft.com>
2022-06-20 17:12:43 -0700

    Make H2 WebSockets ignore MaxRequestBodySize and MinRequestBodyDataRate #42101 (#42269)

--- src/Servers/Kestrel/Core/src/Internal/Http/HttpProtocol.FeatureCollection.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http/HttpProtocol.FeatureCollection.cs
@@ -149,7 +149,7 @@ internal partial class HttpProtocol
 
     bool IHttpUpgradeFeature.IsUpgradableRequest => IsUpgradableRequest;
 
-    bool IHttpExtendedConnectFeature.IsExtendedConnect => IsConnectRequest;
+    bool IHttpExtendedConnectFeature.IsExtendedConnect => IsExtendedConnectRequest;
 
     string? IHttpExtendedConnectFeature.Protocol => ConnectProtocol;
 
@@ -195,7 +195,7 @@ internal partial class HttpProtocol
         set => AllowSynchronousIO = value;
     }
 
-    bool IHttpMaxRequestBodySizeFeature.IsReadOnly => HasStartedConsumingRequestBody || IsUpgraded;
+    bool IHttpMaxRequestBodySizeFeature.IsReadOnly => HasStartedConsumingRequestBody || IsUpgraded || IsExtendedConnectRequest;
 
     long? IHttpMaxRequestBodySizeFeature.MaxRequestBodySize
     {
@@ -290,12 +290,12 @@ internal partial class HttpProtocol
 
     async ValueTask<Stream> IHttpExtendedConnectFeature.AcceptAsync()
     {
-        if (!IsConnectRequest)
+        if (!IsExtendedConnectRequest)
         {
             throw new InvalidOperationException(CoreStrings.CannotAcceptNonConnectRequest);
         }
 
-        if (IsUpgraded)
+        if (IsExtendedConnectAccepted)
         {
             throw new InvalidOperationException(CoreStrings.AcceptCannotBeCalledMultipleTimes);
         }
@@ -305,7 +305,7 @@ internal partial class HttpProtocol
             throw new InvalidOperationException(CoreStrings.ConnectStatusMustBe200);
         }
 
-        IsUpgraded = true;
+        IsExtendedConnectAccepted = true;
 
         await FlushAsync();
 
