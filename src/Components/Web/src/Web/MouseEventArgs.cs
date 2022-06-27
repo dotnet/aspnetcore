0076041b0282e64e9de70726e0621ec85e09b3ab
Cera Samson <cerasamson@gmail.com>
2022-06-27 12:17:51 -0700

    Revert MouseEventArgs.cs

--- src/Components/Web/src/Web/MouseEventArgs.cs
+++ src/Components/Web/src/Web/MouseEventArgs.cs
@@ -98,14 +98,4 @@ public class MouseEventArgs : EventArgs
     /// Gets or sets the type of the event.
     /// </summary>
     public string Type { get; set; } = default!;
-
-    /// <summary>
-    /// The X coordinate of the mouse pointer relative to the position of the last mousemove event.
-    /// </summary>
-    public double MovementX { get; set; }
-
-    /// <summary>
-    /// The Y coordinate of the mouse pointer relative to the position of the last mousemove event.
-    /// </summary>
-    public double MovementY { get; set; }
 }
