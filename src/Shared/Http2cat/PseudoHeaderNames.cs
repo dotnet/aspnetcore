851b217b9d780ce8426951c8c21247d127a15c15
Daniel Genkin <t-dagenkin@microsoft.com>
2022-07-01 15:51:42 -0700

    Fixed some CI issues

--- src/Shared/Http2cat/PseudoHeaderNames.cs
+++ src/Shared/Http2cat/PseudoHeaderNames.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace Microsoft.Net.Http.Headers;
+namespace Microsoft.AspNetCore.Http2Cat;
 
 [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1802:Use literals where appropriate", Justification = "So referenceEquals works")]
 internal static class PseudoHeaderNames
