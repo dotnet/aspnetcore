4223766337d69bb863719971dc253af1dfe999cf
James Newton-King <james@newtonking.com>
2022-07-01 10:31:28 +0800

    Annotate write method

--- src/Hosting/Hosting/src/Internal/HostingApplicationDiagnostics.cs
+++ src/Hosting/Hosting/src/Internal/HostingApplicationDiagnostics.cs
@@ -225,7 +225,7 @@ internal sealed class HostingApplicationDiagnostics
 
     [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026",
         Justification = "The values being passed into Write have the commonly used properties being preserved with DynamicDependency.")]
-    private static void WriteDiagnosticEvent<TValue>(
+    private static void WriteDiagnosticEvent<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties)] TValue>(
         DiagnosticSource diagnosticSource, string name, TValue value)
     {
         diagnosticSource.Write(name, value);
