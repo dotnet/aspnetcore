9358a0f9a2d40d53023fd349a343cb180e8397b3
James Newton-King <james@newtonking.com>
2022-07-08 19:58:52 +0800

    Comment

--- src/Framework/AspNetCoreAnalyzers/src/Analyzers/RouteEmbeddedLanguage/Infrastructure/VersionChecker.cs
+++ src/Framework/AspNetCoreAnalyzers/src/Analyzers/RouteEmbeddedLanguage/Infrastructure/VersionChecker.cs
@@ -9,6 +9,9 @@ namespace Microsoft.AspNetCore.Analyzers.RouteEmbeddedLanguage.Infrastructure;
 
 internal static class VersionChecker
 {
+    // Route parser requires features made public from a specific version of Roslyn.
+    // Check the loaded version is greater than the min version.
+    // If this check isn't made then errors will be thrown because of missing APIs and assemblies.
     private static readonly Version MinVersion = new Version(4, 300, 22, 32601);
 
     private static bool? _isSupported;
