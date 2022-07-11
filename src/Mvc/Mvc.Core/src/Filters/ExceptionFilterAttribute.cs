df829674da3115d3ed52603125b69e7d60765d93
Amirghk <90721706+Amirghk@users.noreply.github.com>
2022-07-11 13:42:28 +0430

    Found a typo
    Changed an (an) to (a)

--- src/Mvc/Mvc.Core/src/Filters/ExceptionFilterAttribute.cs
+++ src/Mvc/Mvc.Core/src/Filters/ExceptionFilterAttribute.cs
@@ -4,7 +4,7 @@
 namespace Microsoft.AspNetCore.Mvc.Filters;
 
 /// <summary>
-/// An abstract filter that runs asynchronously after an action has thrown an <see cref="Exception"/>. Subclasses
+/// An abstract filter that runs asynchronously after an action has thrown a <see cref="Exception"/>. Subclasses
 /// must override <see cref="OnException"/> or <see cref="OnExceptionAsync"/> but not both.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
