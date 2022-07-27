6df6573d746f4a3b12cea51af9a15bfab9e0743a
James Newton-King <james@newtonking.com>
2022-07-27 14:17:05 +0800

    Revert "Route tooling (#42597)" (#42943)

--- src/Framework/AspNetCoreAnalyzers/test/TestDiagnosticAnalyzer.cs
+++ src/Framework/AspNetCoreAnalyzers/test/TestDiagnosticAnalyzer.cs
@@ -3,21 +3,12 @@
 
 using System.Reflection;
 using Microsoft.AspNetCore.Analyzer.Testing;
-using Microsoft.AspNetCore.Analyzers.RouteEmbeddedLanguage;
-using Microsoft.AspNetCore.Analyzers.RouteEmbeddedLanguage.Infrastructure;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Classification;
-using Microsoft.CodeAnalysis.Completion;
 using Microsoft.CodeAnalysis.Diagnostics;
-using Microsoft.CodeAnalysis.ExternalAccess.AspNetCore.EmbeddedLanguages;
-using Microsoft.CodeAnalysis.Host.Mef;
-using Microsoft.CodeAnalysis.Testing;
-using Microsoft.CodeAnalysis.Text;
-using Microsoft.VisualStudio.Composition;
 
 namespace Microsoft.AspNetCore.Analyzers;
 
-internal class TestDiagnosticAnalyzerRunner : DiagnosticAnalyzerRunner
+public class TestDiagnosticAnalyzerRunner : DiagnosticAnalyzerRunner
 {
     public TestDiagnosticAnalyzerRunner(DiagnosticAnalyzer analyzer)
     {
@@ -26,87 +17,11 @@ internal class TestDiagnosticAnalyzerRunner : DiagnosticAnalyzerRunner
 
     public DiagnosticAnalyzer Analyzer { get; }
 
-    public async Task<ClassifiedSpan[]> GetClassificationSpansAsync(TextSpan textSpan, params string[] sources)
-    {
-        var project = CreateProjectWithReferencesInBinDir(GetType().Assembly, sources);
-        var doc = project.Solution.GetDocument(project.Documents.First().Id);
-
-        var result = await Classifier.GetClassifiedSpansAsync(doc, textSpan, CancellationToken.None);
-
-        return result.ToArray();
-    }
-
-    public async Task<CompletionResult> GetCompletionsAndServiceAsync(int caretPosition, params string[] sources)
-    {
-        var project = CreateProjectWithReferencesInBinDir(GetType().Assembly, sources);
-        var doc = project.Solution.GetDocument(project.Documents.First().Id);
-
-        var completionService = CompletionService.GetService(doc);
-        var result = await completionService.GetCompletionsAsync(doc, caretPosition, CompletionTrigger.Invoke);
-
-        return new(doc, completionService, result);
-    }
-
-    public async Task<AspNetCoreBraceMatchingResult?> GetBraceMatchesAsync(int caretPosition, params string[] sources)
-    {
-        var project = CreateProjectWithReferencesInBinDir(GetType().Assembly, sources);
-        var doc = project.Solution.GetDocument(project.Documents.First().Id);
-
-        var (_, token, model) = await RouteStringSyntaxDetectorDocument.TryGetStringSyntaxTokenAtPositionAsync(doc, caretPosition, CancellationToken.None);
-        var braceMatcher = new RoutePatternBraceMatcher();
-
-        return braceMatcher.FindBraces(model, token, caretPosition, CancellationToken.None);
-    }
-
-    public async Task<List<AspNetCoreHighlightSpan>> GetHighlightingAsync(int caretPosition, params string[] sources)
-    {
-        var project = CreateProjectWithReferencesInBinDir(GetType().Assembly, sources);
-        var doc = project.Solution.GetDocument(project.Documents.First().Id);
-
-        var (_, token, model) = await RouteStringSyntaxDetectorDocument.TryGetStringSyntaxTokenAtPositionAsync(doc, caretPosition, CancellationToken.None);
-        var highlighter = new RoutePatternHighlighter();
-
-        var highlights = highlighter.GetDocumentHighlights(model, token, caretPosition, CancellationToken.None);
-        return highlights.SelectMany(h => h.HighlightSpans).ToList();
-    }
-
     public Task<Diagnostic[]> GetDiagnosticsAsync(params string[] sources)
     {
         var project = CreateProjectWithReferencesInBinDir(GetType().Assembly, sources);
 
         return GetDiagnosticsAsync(project);
-    }
-
-    private static readonly Lazy<IExportProviderFactory> ExportProviderFactory;
-
-    static TestDiagnosticAnalyzerRunner()
-    {
-        ExportProviderFactory = new Lazy<IExportProviderFactory>(
-            () =>
-            {
-#pragma warning disable VSTHRD011 // Use AsyncLazy<T>
-#pragma warning disable VSTHRD002 // Avoid problematic synchronous waits
-                var assemblies = MefHostServices.DefaultAssemblies.ToList();
-                assemblies.Add(RoutePatternClassifier.TestAccessor.ExternalAccessAssembly);
-
-                var discovery = new AttributedPartDiscovery(Resolver.DefaultInstance, isNonPublicSupported: true);
-                var parts = Task.Run(() => discovery.CreatePartsAsync(assemblies)).GetAwaiter().GetResult();
-                var catalog = ComposableCatalog.Create(Resolver.DefaultInstance).AddParts(parts); //.WithDocumentTextDifferencingService();
-
-                var configuration = CompositionConfiguration.Create(catalog);
-                var runtimeComposition = RuntimeComposition.CreateRuntimeComposition(configuration);
-                return runtimeComposition.CreateExportProviderFactory();
-#pragma warning restore VSTHRD002 // Avoid problematic synchronous waits
-#pragma warning restore VSTHRD011 // Use AsyncLazy<T>
-            },
-            LazyThreadSafetyMode.ExecutionAndPublication);
-    }
-
-    private static AdhocWorkspace CreateWorkspace()
-    {
-        var exportProvider = ExportProviderFactory.Value.CreateExportProvider();
-        var host = MefHostServices.Create(exportProvider.AsCompositionContext());
-        return new AdhocWorkspace(host);
     }
 
     public static Project CreateProjectWithReferencesInBinDir(Assembly testAssembly, params string[] source)
@@ -115,9 +30,7 @@ internal class TestDiagnosticAnalyzerRunner : DiagnosticAnalyzerRunner
         // However these binaries are always present in the bin output. As a "temporary" workaround, we'll add
         // every dll file that's present in the test's build output as a metadatareference.
 
-        Func<Workspace> createWorkspace = CreateWorkspace;
-
-        var project = DiagnosticProject.Create(testAssembly, source, createWorkspace, typeof(RoutePatternClassifier));
+        var project = DiagnosticProject.Create(testAssembly, source);
         foreach (var assembly in Directory.EnumerateFiles(AppContext.BaseDirectory, "*.dll"))
         {
             if (!project.MetadataReferences.Any(c => string.Equals(Path.GetFileNameWithoutExtension(c.Display), Path.GetFileNameWithoutExtension(assembly), StringComparison.OrdinalIgnoreCase)))
@@ -139,5 +52,3 @@ internal class TestDiagnosticAnalyzerRunner : DiagnosticAnalyzerRunner
         return options.WithOutputKind(OutputKind.ConsoleApplication);
     }
 }
-
-public record CompletionResult(Document Document, CompletionService Service, CompletionList Completions);
