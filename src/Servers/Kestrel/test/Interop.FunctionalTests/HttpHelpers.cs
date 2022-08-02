fa5507453f2f6287f0fa9519b104b8da5c3a64d1
James Newton-King <james@newtonking.com>
2022-08-02 12:04:26 +0800

    PR feedback

--- src/Servers/Kestrel/test/Interop.FunctionalTests/HttpHelpers.cs
+++ src/Servers/Kestrel/test/Interop.FunctionalTests/HttpHelpers.cs
@@ -14,7 +14,6 @@ using Microsoft.AspNetCore.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Serilog.Core;
 
 namespace Interop.FunctionalTests;
 
