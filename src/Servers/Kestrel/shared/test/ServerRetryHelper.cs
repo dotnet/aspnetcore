64834c279182614b0a00eb895e111e0c4f881b0d
James Newton-King <james@newtonking.com>
2022-08-02 11:44:32 +0800

    Comment

--- src/Servers/Kestrel/shared/test/ServerRetryHelper.cs
+++ src/Servers/Kestrel/shared/test/ServerRetryHelper.cs
@@ -1,13 +1,15 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.AspNetCore.Testing;
 
 public static class ServerRetryHelper
 {
+    /// <summary>
+    /// Retry a func. Useful when a test needs an explicit port and you want to avoid port conflicts.
+    /// </summary>
     public static async Task BindPortsWithRetry(Func<int, Task> retryFunc, ILogger logger)
     {
         var retryCount = 0;
