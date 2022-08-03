2cf906641dc0bcc03b5abba8ff178e4e513fc67e
Stephen Halter <halter73@gmail.com>
2022-08-03 12:57:45 -0700

    Add MethodInfo to all endpoint metadata in RouteEndpointDataSource

--- src/Http/Routing/src/RouteEndpointDataSource.cs
+++ src/Http/Routing/src/RouteEndpointDataSource.cs
@@ -141,7 +141,11 @@ internal sealed class RouteEndpointDataSource : EndpointDataSource
             displayName = $"Fallback {displayName}";
         }
 
-        RequestDelegate? factoryCreatedRequestDelegate = null;
+        // If we're not a route handler, we started with a fully realized (although unfiltered) RequestDelegate, so we can just redirect to that
+        // while running any conventions. We'll put the original back if it remains unfiltered right before building the endpoint.
+        RequestDelegate? factoryCreatedRequestDelegate = isRouteHandler ? null : (RequestDelegate)entry.RouteHandler;
+
+        // Let existing conventions capture and call into builder.RequestDelegate as long as they do so after it has been created.
         RequestDelegate redirectRequestDelegate = context =>
         {
             if (factoryCreatedRequestDelegate is null)
@@ -152,25 +156,15 @@ internal sealed class RouteEndpointDataSource : EndpointDataSource
             return factoryCreatedRequestDelegate(context);
         };
 
+        // Add MethodInfo and HttpMethodMetadata (if any) as first metadata items as they are intrinsic to the route much like
+        // the pattern or default display name. This gives visibility to conventions like WithOpenApi() to intrinsic route details
+        // (namely the MethodInfo) even when applied early as group conventions.
         RouteEndpointBuilder builder = new(redirectRequestDelegate, pattern, order)
         {
             DisplayName = displayName,
             ApplicationServices = _applicationServices,
+            Metadata = { handler.Method },
         };
-
-        if (isRouteHandler)
-        {
-            // Add MethodInfo and HttpMethodMetadata (if any) as first metadata items as they are intrinsic to the route much like
-            // the pattern or default display name. This gives visibility to conventions like WithOpenApi() to intrinsic route details
-            // (namely the MethodInfo) even when applied early as group conventions.
-            builder.Metadata.Add(handler.Method);
-        }
-        else
-        {
-            // If we're not a route handler, we started with a fully realized (although unfiltered) RequestDelegate, so we can just redirect to that
-            // while running any conventions. We'll put the original back if it remains unfiltered right before building the endpoint.
-            factoryCreatedRequestDelegate = (RequestDelegate)entry.RouteHandler;
-        }
 
         if (entry.HttpMethods is not null)
         {
