6a8884e4d07be89c6f75aec2e1ca003c8df6af60
Cera Samson <t-samsoncera@microsoft.com>
2022-08-03 12:26:01 -0700

    Update src/Components/Web/src/IFileDownloader.cs
    Co-authored-by: Tanay Parikh <TanayParikh@users.noreply.github.com>

--- src/Components/Web/src/IFileDownloader.cs
+++ src/Components/Web/src/IFileDownloader.cs
@@ -9,10 +9,10 @@ namespace Microsoft.AspNetCore.Components;
 public interface IFileDownloader
 {
     /// <summary>
-    /// Takes in a byte[] representing file data and converts it into a MemoryStream.
+    /// Takes in a byte[] representing file data and triggers the file download on the client.
     /// </summary>
-    /// <param name="fileName">A <see cref="string"/> that contains the specified file name.</param>
-    /// <param name="data"> The <see cref="byte"/>[] data that will be converted into a <see cref="MemoryStream"/>.</param>
+    /// <param name="fileName">A <see cref="string"/> that represents the default name of the file that will be downloaded.</param>
+    /// <param name="data"> The <see cref="byte"/>[] data that will be downloaded by the client.</param>
     Task SaveAs(string fileName, byte[] data);
 
     /// <summary>
