7233f2fda2feebd040d65b60e9509e753fb80c15
Cera Samson <cerasamson@gmail.com>
2022-08-09 14:10:24 -0700

    (WIP) add FileSystem API implementation

--- src/Components/Web.JS/src/FileDownloader.ts
+++ src/Components/Web.JS/src/FileDownloader.ts
@@ -2,13 +2,42 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 export async function downloadFile(data: any, fileName: string) {
-    const arrayBuffer = await data.arrayBuffer();
-    const blob = new Blob([arrayBuffer]);
-    const url = URL.createObjectURL(blob);
-    const anchorElement = document.createElement('a');
-    anchorElement.href = url;
-    anchorElement.download = fileName;
-    anchorElement.click();
-    anchorElement.remove();
-    URL.revokeObjectURL(url);
+    // For Chromium browsers, show "Save As" dialog and then stream the data into the file without buffering it all in membey
+    if (typeof (window as any).showSaveFilePicker() === 'function')
+    {
+        // Show the "Save As" dialog
+        let fileWriter;
+        try {
+            const fileHandle = await (window as any).showSaveFilePicker();
+            // Create a FileSystemWritableFileStream to write to.
+            fileWriter = await fileHandle.createWritable();
+        } catch {
+            // User pressed cancel, so abort the whole thing
+            return;
+        }
+
+        const reader = data.getReader();
+        while (true) {
+            const readResult = await reader.read();
+            if (readResult.done) {
+                break;
+            }
+            // Write the contents of the file to the stream.
+            await fileWriter.write(readResult.value);
+        }
+        // Close the file and write the contents to disk
+        await fileWriter.close();
+    }
+    else {
+        // The following option works for all browsers
+        const arrayBuffer = await data.arrayBuffer();
+        const blob = new Blob([arrayBuffer]);
+        const url = URL.createObjectURL(blob);
+        const anchorElement = document.createElement('a');
+        anchorElement.href = url;
+        anchorElement.download = fileName;
+        anchorElement.click();
+        anchorElement.remove();
+        URL.revokeObjectURL(url);
+    }
 }
