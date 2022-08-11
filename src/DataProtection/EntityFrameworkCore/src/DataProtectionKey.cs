0c9eb331abd2551f92d06dc848da3c646041fa0c
James Newton-King <james@newtonking.com>
2022-08-11 08:37:36 +0800

    Dynamic include DataProtectionKey.Id (#43204)

--- src/DataProtection/EntityFrameworkCore/src/DataProtectionKey.cs
+++ src/DataProtection/EntityFrameworkCore/src/DataProtectionKey.cs
@@ -1,11 +1,17 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
 
 /// <summary>
 /// Code first model used by <see cref="EntityFrameworkCoreXmlRepository{TContext}"/>.
 /// </summary>
+// DataProtectionKey.Id is not used anywhere. Add DynamicallyAccessedMembers to prevent it from being trimmed.
+// Note that in the future EF may annotate itself to include properties automatically, and the annotation here could be removed.
+// Fixes https://github.com/dotnet/aspnetcore/issues/43187
+[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties)]
 public class DataProtectionKey
 {
     /// <summary>
