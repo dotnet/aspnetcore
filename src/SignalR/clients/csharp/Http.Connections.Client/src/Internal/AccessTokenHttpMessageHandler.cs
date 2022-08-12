7194f1cbf02c0f5bf3b4bb20d19d8cc6922b7391
Brennan <brecon@microsoft.com>
2022-08-11 18:56:20 -0700

    [SignalR] [.NET Client] Retry HTTP request on auth failure (#42671)

--- src/SignalR/clients/csharp/Http.Connections.Client/src/Internal/AccessTokenHttpMessageHandler.cs
+++ src/SignalR/clients/csharp/Http.Connections.Client/src/Internal/AccessTokenHttpMessageHandler.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
@@ -11,6 +12,7 @@ namespace Microsoft.AspNetCore.Http.Connections.Client.Internal;
 internal sealed class AccessTokenHttpMessageHandler : DelegatingHandler
 {
     private readonly HttpConnection _httpConnection;
+    private string? _accessToken;
 
     public AccessTokenHttpMessageHandler(HttpMessageHandler inner, HttpConnection httpConnection) : base(inner)
     {
@@ -19,13 +21,41 @@ internal sealed class AccessTokenHttpMessageHandler : DelegatingHandler
 
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        var accessToken = await _httpConnection.GetAccessTokenAsync().ConfigureAwait(false);
-
-        if (!string.IsNullOrEmpty(accessToken))
+        var isNegotiate = false;
+        if (string.IsNullOrEmpty(_accessToken) ||
+            // Negotiate redirects likely will have a new access token so let's always grab a (potentially) new access token on negotiate
+#if NET5_0_OR_GREATER
+            request.Options.TryGetValue(new HttpRequestOptionsKey<bool>("IsNegotiate"), out var value) && value == true
+#else
+            request.Properties.TryGetValue("IsNegotiate", out var value) && value is true
+#endif
+            )
         {
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            isNegotiate = true;
+            _accessToken = await _httpConnection.GetAccessTokenAsync().ConfigureAwait(false);
         }
 
-        return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        if (!string.IsNullOrEmpty(_accessToken))
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
+        }
+
+        var result = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        // retry once with a new token on auth failure
+        if (!isNegotiate && result.StatusCode is HttpStatusCode.Unauthorized)
+        {
+            HttpConnection.Log.RetryAccessToken(_httpConnection._logger, result.StatusCode);
+            result.Dispose();
+            _accessToken = await _httpConnection.GetAccessTokenAsync().ConfigureAwait(false);
+
+            if (!string.IsNullOrEmpty(_accessToken))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
+            }
+            // Retrying the request relies on any HttpContent being non-disposable.
+            // Currently this is true, the only HttpContent we send is type ReadOnlySequenceContent which is used by SSE and LongPolling for sending an already buffered byte[]
+            result = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        }
+        return result;
     }
 }
