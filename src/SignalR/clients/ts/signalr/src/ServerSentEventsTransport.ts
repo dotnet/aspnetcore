6ca05392386844740cb268bd197b6dc257a502ef
Brennan Conroy <brecon@microsoft.com>
2022-08-16 10:16:50 -0700

    fb

--- src/SignalR/clients/ts/signalr/src/ServerSentEventsTransport.ts
+++ src/SignalR/clients/ts/signalr/src/ServerSentEventsTransport.ts
@@ -11,7 +11,7 @@ import { IHttpConnectionOptions } from "./IHttpConnectionOptions";
 /** @private */
 export class ServerSentEventsTransport implements ITransport {
     private readonly _httpClient: HttpClient;
-    private readonly _accessTokenFactory: (() => string | Promise<string>) | undefined;
+    private readonly _accessToken: string | undefined;
     private readonly _logger: ILogger;
     private readonly _options: IHttpConnectionOptions;
     private _eventSource?: EventSource;
@@ -20,10 +20,10 @@ export class ServerSentEventsTransport implements ITransport {
     public onreceive: ((data: string | ArrayBuffer) => void) | null;
     public onclose: ((error?: Error) => void) | null;
 
-    constructor(httpClient: HttpClient, accessTokenFactory: (() => string | Promise<string>) | undefined, logger: ILogger,
+    constructor(httpClient: HttpClient, accessToken: string | undefined, logger: ILogger,
                 options: IHttpConnectionOptions) {
         this._httpClient = httpClient;
-        this._accessTokenFactory = accessTokenFactory;
+        this._accessToken = accessToken;
         this._logger = logger;
         this._options = options;
 
@@ -41,11 +41,8 @@ export class ServerSentEventsTransport implements ITransport {
         // set url before accessTokenFactory because this._url is only for send and we set the auth header instead of the query string for send
         this._url = url;
 
-        if (this._accessTokenFactory) {
-            const token = await this._accessTokenFactory();
-            if (token) {
-                url += (url.indexOf("?") < 0 ? "?" : "&") + `access_token=${encodeURIComponent(token)}`;
-            }
+        if (this._accessToken) {
+            url += (url.indexOf("?") < 0 ? "?" : "&") + `access_token=${encodeURIComponent(this._accessToken)}`;
         }
 
         return new Promise<void>((resolve, reject) => {
