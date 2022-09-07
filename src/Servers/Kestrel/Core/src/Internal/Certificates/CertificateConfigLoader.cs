6e6f78c84c8a4de84c467bc80482e0e6c625581d
Hao Kung <haokung+github@gmail.com>
2022-09-07 08:22:01 -0700

    Remove using

--- src/Servers/Kestrel/Core/src/Internal/Certificates/CertificateConfigLoader.cs
+++ src/Servers/Kestrel/Core/src/Internal/Certificates/CertificateConfigLoader.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Globalization;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Server.Kestrel.Https;
