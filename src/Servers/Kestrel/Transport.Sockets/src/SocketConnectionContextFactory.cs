df4b4179e8d8d62240dc371f9c5b44838fcac6f8
David Fowler <davidfowl@gmail.com>
2022-09-12 15:37:35 -0700

    Fix double dispatch on linux - Fixing a regression introduced into linux sockets workloads when removing double dispatching on windows. We only want to dispatch by default if we're running on the old windows IO threadpool.

--- src/Servers/Kestrel/Transport.Sockets/src/SocketConnectionContextFactory.cs
+++ src/Servers/Kestrel/Transport.Sockets/src/SocketConnectionContextFactory.cs
@@ -48,6 +48,14 @@ public sealed class SocketConnectionContextFactory : IDisposable
         var maxWriteBufferSize = _options.MaxWriteBufferSize ?? 0;
         var applicationScheduler = options.UnsafePreferInlineScheduling ? PipeScheduler.Inline : PipeScheduler.ThreadPool;
 
+        // Socket callbacks run on the threads polling for IO if we're using the old thread windows thread pool
+        var dispatchSocketCallbacks = OperatingSystem.IsWindows() &&
+                                      (Environment.GetEnvironmentVariable("DOTNET_ThreadPool_UsePortableThreadPoolForIO") == "0" ||
+                                      Environment.GetEnvironmentVariable("COMPlus_ThreadPool_UsePortableThreadPoolForIO") == "0");
+
+        PipeScheduler SelectSocketsScheduler(PipeScheduler dispatchingScheduler) =>
+            dispatchSocketCallbacks ? dispatchingScheduler : PipeScheduler.Inline;
+
         if (_settingsCount > 0)
         {
             _settings = new QueueSettings[_settingsCount];
@@ -56,13 +64,14 @@ public sealed class SocketConnectionContextFactory : IDisposable
             {
                 var memoryPool = _options.MemoryPoolFactory();
                 var transportScheduler = options.UnsafePreferInlineScheduling ? PipeScheduler.Inline : new IOQueue();
+                var socketsScheduler = SelectSocketsScheduler(transportScheduler);
 
                 _settings[i] = new QueueSettings()
                 {
                     Scheduler = transportScheduler,
                     InputOptions = new PipeOptions(memoryPool, applicationScheduler, transportScheduler, maxReadBufferSize, maxReadBufferSize / 2, useSynchronizationContext: false),
                     OutputOptions = new PipeOptions(memoryPool, transportScheduler, applicationScheduler, maxWriteBufferSize, maxWriteBufferSize / 2, useSynchronizationContext: false),
-                    SocketSenderPool = new SocketSenderPool(transportScheduler),
+                    SocketSenderPool = new SocketSenderPool(socketsScheduler),
                     MemoryPool = memoryPool,
                 };
             }
@@ -71,6 +80,8 @@ public sealed class SocketConnectionContextFactory : IDisposable
         {
             var memoryPool = _options.MemoryPoolFactory();
             var transportScheduler = options.UnsafePreferInlineScheduling ? PipeScheduler.Inline : PipeScheduler.ThreadPool;
+            var socketsScheduler = SelectSocketsScheduler(transportScheduler);
+
             _settings = new QueueSettings[]
             {
                 new QueueSettings()
@@ -78,7 +89,7 @@ public sealed class SocketConnectionContextFactory : IDisposable
                     Scheduler = transportScheduler,
                     InputOptions = new PipeOptions(memoryPool, applicationScheduler, transportScheduler, maxReadBufferSize, maxReadBufferSize / 2, useSynchronizationContext: false),
                     OutputOptions = new PipeOptions(memoryPool, transportScheduler, applicationScheduler, maxWriteBufferSize, maxWriteBufferSize / 2, useSynchronizationContext: false),
-                    SocketSenderPool = new SocketSenderPool(PipeScheduler.Inline),
+                    SocketSenderPool = new SocketSenderPool(socketsScheduler),
                     MemoryPool = memoryPool,
                 }
             };
@@ -97,7 +108,7 @@ public sealed class SocketConnectionContextFactory : IDisposable
 
         var connection = new SocketConnection(socket,
             setting.MemoryPool,
-            PipeScheduler.Inline,
+            setting.SocketSenderPool.Scheduler,
             _logger,
             setting.SocketSenderPool,
             setting.InputOptions,
