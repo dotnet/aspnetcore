fe31070e6649d558abed4dc358428982cd8e82f2
FatTiger <owner@wangshenjie.com>
2022-09-23 22:37:07 +0800

    fix dotnet#43523 HttpSys Content-Length and Transfer-Encoding conflicts

--- src/Servers/HttpSys/src/RequestProcessing/Request.cs
+++ src/Servers/HttpSys/src/RequestProcessing/Request.cs
@@ -134,9 +134,29 @@ internal sealed partial class Request
             {
                 // Note Http.Sys adds the Transfer-Encoding: chunked header to HTTP/2 requests with bodies for back compat.
                 var transferEncoding = Headers[HeaderNames.TransferEncoding].ToString();
-                if (string.Equals("chunked", transferEncoding.Trim(), StringComparison.OrdinalIgnoreCase))
+                if (transferEncoding != null && string.Equals("chunked", transferEncoding.Split(',')[0].Trim(), StringComparison.OrdinalIgnoreCase))
                 {
                     _contentBoundaryType = BoundaryType.Chunked;
+
+                    // https://datatracker.ietf.org/doc/html/rfc7230#section-3.3.2
+                    // A sender MUST NOT send a Content-Length header field in any message
+                    // that contains a Transfer-Encoding header field.
+                    // https://datatracker.ietf.org/doc/html/rfc7230#section-3.3.3
+                    // If a message is received with both a Transfer-Encoding and a
+                    // Content-Length header field, the Transfer-Encoding overrides the
+                    // Content-Length.  Such a message might indicate an attempt to
+                    // perform request smuggling (Section 9.5) or response splitting
+                    // (Section 9.4) and ought to be handled as an error.  A sender MUST
+                    // remove the received Content-Length field prior to forwarding such
+                    // a message downstream.
+                    // We should remove the Content-Length request header in this case, for compatibility
+                    // reasons, include X-Content-Length so that the original Content-Length is still available.
+                    if (!StringValues.IsNullOrEmpty(Headers.ContentLength))
+                    {
+                        IHeaderDictionary headerDictionary = Headers;
+                        headerDictionary.Add("X-Content-Length", headerDictionary[HeaderNames.ContentLength]);
+                        Headers.ContentLength = StringValues.Empty;
+                    }
                 }
                 else
                 {
