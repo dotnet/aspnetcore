7209d49c2223eee57727fe6ea6a5a167e930ab5f
Mackinnon Buck <mackinnon.buck@gmail.com>
2022-10-04 13:03:43 -0700

    [release/6.0] Backport 'Make title prerendering not interfere with svg titles' (#44343)
    * Make title prerendering not interfere with svg titles. Fixes #36380. (#37009)
    
    * Update blazor.*.js
    
    Co-authored-by: Steve Sanderson <SteveSandersonMS@users.noreply.github.com>

--- src/Components/test/E2ETest/ServerExecutionTests/HeadModificationPrerenderingTest.cs
+++ src/Components/test/E2ETest/ServerExecutionTests/HeadModificationPrerenderingTest.cs
@@ -36,6 +36,10 @@ namespace Microsoft.AspNetCore.Components.E2ETest.ServerExecutionTests
             Browser.Equal("Title 1", () => Browser.Title);
             Browser.Exists(By.Id("meta-description"));
 
+            // Check that unrelated <title> elements were left alone
+            Browser.Equal("This element is used to test that PageTitle prerendering doesn't interfere with non-head title elements.",
+                () => Browser.Exists(By.CssSelector("#svg-for-title-prerendering-test title")).Text);
+
             var titleText1 = Browser.FindElement(By.Id("title-text-1"));
             titleText1.Clear();
             titleText1.SendKeys("Updated title 1\n");
