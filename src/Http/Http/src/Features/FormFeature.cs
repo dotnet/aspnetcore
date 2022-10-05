8c59e48ce33024f03d7d1d453b7367e99eea2e0f
Sébastien Ros <sebastienros@gmail.com>
2022-10-05 15:00:00 -0700

    Update FormFeature.cs

--- src/Http/Http/src/Features/FormFeature.cs
+++ src/Http/Http/src/Features/FormFeature.cs
@@ -101,11 +101,10 @@ public class FormFeature : IFormFeature
 
         if (!HasFormContentType)
         {
-            throw new InvalidOperationException("Form Collection properties can only be accessed from a POST request");
+            throw new InvalidOperationException("This request does not have a Content-Type header. Forms are available from requests with bodies like POSTs and a form Content-Type of either application/x-www-form-urlencoded or multipart/form-data.");
         }
 
-        // TODO: Issue #456 Avoid Sync-over-Async https://devblogs.microsoft.com/pfxteam/should-i-expose-synchronous-wrappers-for-asynchronous-methods/
-        // TODO: How do we prevent thread exhaustion?
+        // c.f., https://aka.ms/aspnet/forms-async
         return ReadFormAsync().GetAwaiter().GetResult();
     }
 
