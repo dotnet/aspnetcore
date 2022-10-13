e6190b77b6912c3d3f8e99386c6250529b1fd434
David Acker <davidacker3@gmail.com>
2022-10-13 19:11:00 -0400

    Fix nullable reference type

--- src/Framework/AspNetCoreAnalyzers/test/Verifiers/CSharpCodeFixVerifier.cs
+++ src/Framework/AspNetCoreAnalyzers/test/Verifiers/CSharpCodeFixVerifier.cs
@@ -52,7 +52,7 @@ public static partial class CSharpCodeFixVerifier<TAnalyzer, TCodeFix>
         => await VerifyCodeFixAsync(source, new[] { expected }, fixedSource);
 
     /// <inheritdoc cref="CodeFixVerifier{TAnalyzer, TCodeFix, TTest, TVerifier}.VerifyCodeFixAsync(string, DiagnosticResult[], string)"/>
-    public static async Task VerifyCodeFixAsync(string source, DiagnosticResult[] expected, string fixedSource, int? expectedIterations = null, string usageSource = null, string? codeActionEquivalenceKey = null)
+    public static async Task VerifyCodeFixAsync(string source, DiagnosticResult[] expected, string fixedSource, int? expectedIterations = null, string usageSource = null, string codeActionEquivalenceKey = null)
     {
         var test = new CSharpCodeFixTest<TAnalyzer, TCodeFix, XUnitVerifier>
         {
