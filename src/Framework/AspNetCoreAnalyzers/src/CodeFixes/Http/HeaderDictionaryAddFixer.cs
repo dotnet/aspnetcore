dd3612712e27d27f997a8caec40be7f9c34b5e37
David Acker <davidacker3@gmail.com>
2022-10-13 18:37:59 -0400

    Add code fix to Framework/AspNetCoreAnalyzers

---
+++ src/Framework/AspNetCoreAnalyzers/src/CodeFixes/Http/HeaderDictionaryAddFixer.cs
@@ -0,0 +1,143 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Microsoft.AspNetCore.Analyzers.Http.Fixers;
+
+[ExportCodeFixProvider(LanguageNames.CSharp), Shared]
+public class HeaderDictionaryAddFixer : CodeFixProvider
+{
+    public override ImmutableArray<string> FixableDiagnosticIds { get; } = ImmutableArray.Create(DiagnosticDescriptors.DoNotUseIHeaderDictionaryAdd.Id);
+
+    public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+    public sealed override Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        foreach (var diagnostic in context.Diagnostics)
+        {
+            var appendTitle = "Use IHeaderDictionary.Append";
+            context.RegisterCodeFix(
+                CodeAction.Create(appendTitle,
+                    cancellationToken => ReplaceAddWithAppendAsync(diagnostic, context.Document, cancellationToken),
+                    equivalenceKey: appendTitle),
+                diagnostic);
+
+            var indexerTitle = "Use the indexer";
+            context.RegisterCodeFix(
+                CodeAction.Create(indexerTitle,
+                    cancellationToken => ReplaceAddWithIndexerAsync(diagnostic, context.Document, cancellationToken),
+                    equivalenceKey: indexerTitle),
+                diagnostic);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private static async Task<Document> ReplaceAddWithAppendAsync(Diagnostic diagnostic, Document document, CancellationToken cancellationToken)
+    {
+        var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+        if (root is not CompilationUnitSyntax compilationUnitSyntax)
+        {
+            return document;
+        }
+
+        var invocation = compilationUnitSyntax.FindNode(diagnostic.Location.SourceSpan);
+
+        if (invocation is InvocationExpressionSyntax { Expression: MemberAccessExpressionSyntax { Name.Identifier: { } identifierToken } })
+        {
+            compilationUnitSyntax = compilationUnitSyntax.ReplaceToken(identifierToken, SyntaxFactory.Identifier("Append"));
+
+            // IHeaderDictionary.Append is defined as an extension method on Microsoft.AspNetCore.Http.HeaderDictionaryExtensions.
+            // We'll need to add the required using directive, when not already present.
+            compilationUnitSyntax = AddRequiredUsingDirectiveForAppend(compilationUnitSyntax);
+
+            return document.WithSyntaxRoot(compilationUnitSyntax);
+        }
+
+        return document;
+    }
+
+    private static CompilationUnitSyntax AddRequiredUsingDirectiveForAppend(CompilationUnitSyntax compilationUnitSyntax)
+    {
+        var usingDirectives = compilationUnitSyntax.Usings;
+
+        var includesRequiredUsingDirective = false;
+        var insertionIndex = 0;
+
+        for (var i = 0; i < usingDirectives.Count; i++)
+        {
+            var result = string.Compare(
+                "Microsoft.AspNetCore.Http",
+                usingDirectives[i].Name.ToString(),
+                StringComparison.Ordinal);
+
+            if (result == 0)
+            {
+                includesRequiredUsingDirective = true;
+                break;
+            }
+
+            if (result < 0)
+            {
+                insertionIndex = i;
+            }
+        }
+
+        if (includesRequiredUsingDirective)
+        {
+            return compilationUnitSyntax;
+        }
+
+        var requiredUsingDirective =
+            SyntaxFactory.UsingDirective(
+                SyntaxFactory.QualifiedName(
+                    SyntaxFactory.QualifiedName(
+                        SyntaxFactory.IdentifierName("Microsoft"),
+                        SyntaxFactory.IdentifierName("AspNetCore")),
+                    SyntaxFactory.IdentifierName("Http")));
+
+        return compilationUnitSyntax.WithUsings(
+            usingDirectives.Insert(insertionIndex, requiredUsingDirective));
+    }
+
+    private static async Task<Document> ReplaceAddWithIndexerAsync(Diagnostic diagnostic, Document document, CancellationToken cancellationToken)
+    {
+        var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+        if (root == null)
+        {
+            return document;
+        }
+
+        var invocation = root.FindNode(diagnostic.Location.SourceSpan);
+
+        if (invocation is InvocationExpressionSyntax
+            {
+                Expression: MemberAccessExpressionSyntax memberAccessExpression,
+                ArgumentList.Arguments: { Count: 2 } arguments
+            })
+        {
+            var assignmentExpression =
+                SyntaxFactory.AssignmentExpression(
+                    SyntaxKind.SimpleAssignmentExpression,
+                    SyntaxFactory.ElementAccessExpression(
+                        memberAccessExpression.Expression,
+                        SyntaxFactory.BracketedArgumentList(
+                            SyntaxFactory.SeparatedList(new[] { arguments[0] }))),
+                    arguments[1].Expression);
+
+            return document.WithSyntaxRoot(root.ReplaceNode(invocation, assignmentExpression));
+        }
+
+        return document;
+    }
+}
---
+++ src/Framework/AspNetCoreAnalyzers/test/Http/HeaderDictionaryAddFixerTests.cs
@@ -0,0 +1,229 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.CodeAnalysis.Testing;
+using VerifyCS = Microsoft.AspNetCore.Analyzers.Verifiers.CSharpCodeFixVerifier<
+    Microsoft.AspNetCore.Analyzers.Http.HeaderDictionaryAddAnalyzer,
+    Microsoft.AspNetCore.Analyzers.Http.Fixers.HeaderDictionaryAddFixer>;
+
+namespace Microsoft.AspNetCore.Analyzers.Http;
+
+public class HeaderDictionaryAddFixerTests
+{
+    private const string AppendCodeActionEquivalenceKey = "Use IHeaderDictionary.Append";
+    private const string IndexerCodeActionEquivalenceKey = "Use the indexer";
+
+    [Fact]
+    public async Task IHeaderDictionary_WithAdd_FixedWithAppend()
+    {
+        // Arrange & Act & Assert
+        await VerifyCS.VerifyCodeFixAsync(@"
+using Microsoft.AspNetCore.Http;
+namespace HeaderDictionaryAddFixerTests;
+public class Program
+{
+    public static void Main()
+    {
+        var context = new DefaultHttpContext();
+        {|#0:context.Request.Headers.Add(""Accept"", ""text/html"")|};
+    }
+}",
+        new[]
+        {
+            new DiagnosticResult(DiagnosticDescriptors.DoNotUseIHeaderDictionaryAdd)
+                .WithLocation(0)
+                .WithMessage(Resources.Analyzer_HeaderDictionaryAdd_Message)
+        },
+        @"
+using Microsoft.AspNetCore.Http;
+namespace HeaderDictionaryAddFixerTests;
+public class Program
+{
+    public static void Main()
+    {
+        var context = new DefaultHttpContext();
+        context.Request.Headers.Append(""Accept"", ""text/html"");
+    }
+}",
+        codeActionEquivalenceKey: AppendCodeActionEquivalenceKey);
+    }
+
+    public static IEnumerable<object[]> FixedWithAppendAddsUsingDirectiveTestData()
+    {
+        yield return new[]
+        {
+            @"
+using Microsoft.AspNetCore.Mvc;
+namespace HeaderDictionaryAddFixerTests;
+public class TestController : ControllerBase
+{
+    public IActionResult Endpoint()
+    {
+        {|#0:Response.Headers.Add(""Content-Type"", ""text/html"")|};
+        return Ok();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+    }
+}",
+            @"
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace HeaderDictionaryAddFixerTests;
+public class TestController : ControllerBase
+{
+    public IActionResult Endpoint()
+    {
+        Response.Headers.Append(""Content-Type"", ""text/html"");
+        return Ok();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+    }
+}"
+        };
+
+        yield return new[]
+        {
+            @"
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+namespace HeaderDictionaryAddFixerTests;
+public class TestController : ControllerBase
+{
+    public IActionResult Endpoint()
+    {
+        {|#0:Response.Headers.Add(""Content-Type"", ""text/html"")|};
+        return Ok(new List<string>());
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+    }
+}",
+            @"
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace HeaderDictionaryAddFixerTests;
+public class TestController : ControllerBase
+{
+    public IActionResult Endpoint()
+    {
+        Response.Headers.Append(""Content-Type"", ""text/html"");
+        return Ok(new List<string>());
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+    }
+}"
+        };
+
+        yield return new[]
+        {
+            @"
+namespace HeaderDictionaryAddFixerTests;
+public class TestController : Microsoft.AspNetCore.Mvc.ControllerBase
+{
+    public Microsoft.AspNetCore.Mvc.IActionResult Endpoint()
+    {
+        {|#0:Response.Headers.Add(""Content-Type"", ""text/html"")|};
+        return Ok();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+    }
+}",
+            @"
+using Microsoft.AspNetCore.Http;
+
+namespace HeaderDictionaryAddFixerTests;
+public class TestController : Microsoft.AspNetCore.Mvc.ControllerBase
+{
+    public Microsoft.AspNetCore.Mvc.IActionResult Endpoint()
+    {
+        Response.Headers.Append(""Content-Type"", ""text/html"");
+        return Ok();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+    }
+}"
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(FixedWithAppendAddsUsingDirectiveTestData))]
+    public async Task IHeaderDictionary_WithAdd_FixedWithAppend_AddsUsingDirective(string source, string fixedSource)
+    {
+        // Arrange & Act & Assert
+        await VerifyCS.VerifyCodeFixAsync(
+            source.TrimStart(),
+            new[]
+            {
+                new DiagnosticResult(DiagnosticDescriptors.DoNotUseIHeaderDictionaryAdd)
+                    .WithLocation(0)
+                    .WithMessage(Resources.Analyzer_HeaderDictionaryAdd_Message)
+            },
+            fixedSource.TrimStart(),
+            codeActionEquivalenceKey: AppendCodeActionEquivalenceKey);
+    }
+
+    [Fact]
+    public async Task IHeaderDictionary_WithAdd_FixedWithIndexer()
+    {
+        // Arrange & Act & Assert
+        await VerifyCS.VerifyCodeFixAsync(@"
+using Microsoft.AspNetCore.Http;
+namespace HeaderDictionaryAddFixerTests;
+public class Program
+{
+    public static void Main()
+    {
+        var context = new DefaultHttpContext();
+        {|#0:context.Request.Headers.Add(""Accept"", ""text/html"")|};
+    }
+}",
+        new[]
+        {
+            new DiagnosticResult(DiagnosticDescriptors.DoNotUseIHeaderDictionaryAdd)
+                .WithLocation(0)
+                .WithMessage(Resources.Analyzer_HeaderDictionaryAdd_Message)
+        },
+        @"
+using Microsoft.AspNetCore.Http;
+namespace HeaderDictionaryAddFixerTests;
+public class Program
+{
+    public static void Main()
+    {
+        var context = new DefaultHttpContext();
+        context.Request.Headers[""Accept""] = ""text/html"";
+    }
+}",
+        codeActionEquivalenceKey: IndexerCodeActionEquivalenceKey);
+    }
+}
