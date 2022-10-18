2d5042079edb57ef88663da1c973c616774f2f51
Aaron Robinson <arobins@microsoft.com>
2022-10-18 13:51:42 -0700

    Remove unused using.

--- src/Servers/HttpSys/src/Helpers.cs
+++ src/Servers/HttpSys/src/Helpers.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Globalization;
-using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Microsoft.AspNetCore.Server.HttpSys;
