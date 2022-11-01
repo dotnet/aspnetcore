455735ee18431af20915ae01bc5e4e2962b260a6
Bruno Oliveira <brolivei@microsoft.com>
2022-11-01 13:36:37 -0700

    clean up

--- src/Http/Http.Extensions/src/RequestDelegateFactory.cs
+++ src/Http/Http.Extensions/src/RequestDelegateFactory.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Data.Common;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
@@ -9,7 +8,6 @@ using System.IO.Pipelines;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
 using System.Security.Claims;
 using System.Text;
