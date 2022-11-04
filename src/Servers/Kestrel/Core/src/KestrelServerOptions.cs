d417c3a9bd655808cb04e25b92ae095398c171c5
Aditya Mandaleeka <adityamandaleeka@users.noreply.github.com>
2022-11-03 20:31:00 -0700

    Make Kestrel prefer on-disk cert for macOS bind. (#44835)

--- src/Servers/Kestrel/Core/src/KestrelServerOptions.cs
+++ src/Servers/Kestrel/Core/src/KestrelServerOptions.cs
@@ -287,7 +287,7 @@ public class KestrelServerOptions
             var logger = ApplicationServices!.GetRequiredService<ILogger<KestrelServer>>();
             try
             {
-                DefaultCertificate = CertificateManager.Instance.ListCertificates(StoreName.My, StoreLocation.CurrentUser, isValid: true)
+                DefaultCertificate = CertificateManager.Instance.ListCertificates(StoreName.My, StoreLocation.CurrentUser, isValid: true, requireExportable: false)
                     .FirstOrDefault();
 
                 if (DefaultCertificate != null)
