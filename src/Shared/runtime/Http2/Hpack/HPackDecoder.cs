8350b948d04cd04dcd2f0628c8e04ba17b902d9b
Chris Ross <Tratcher@Outlook.com>
2022-11-03 20:10:49 -0700

    [7.0] Limit the hpack buffer resize (#44644)
    * Limit the hpack buffer resize #44643
    * More resizes

--- src/Shared/runtime/Http2/Hpack/HPackDecoder.cs
+++ src/Shared/runtime/Http2/Hpack/HPackDecoder.cs
@@ -577,7 +577,7 @@ namespace System.Net.Http.HPack
                     throw new HPackDecodingException(SR.Format(SR.net_http_headers_exceeded_length, _maxHeadersLength));
                 }
 
-                _stringOctets = new byte[Math.Max(length, _stringOctets.Length * 2)];
+                _stringOctets = new byte[Math.Max(length, Math.Min(_stringOctets.Length * 2, _maxHeadersLength))];
             }
 
             _stringLength = length;
@@ -625,7 +625,7 @@ namespace System.Net.Http.HPack
         {
             if (dst.Length < _stringLength)
             {
-                dst = new byte[Math.Max(_stringLength, dst.Length * 2)];
+                dst = new byte[Math.Max(_stringLength, Math.Min(dst.Length * 2, _maxHeadersLength))];
             }
         }
 
