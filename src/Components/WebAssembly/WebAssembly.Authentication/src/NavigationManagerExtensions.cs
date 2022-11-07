a251cb3edc63a8789da24b801a9fcd3e3bb1cc16
Daniel Roth <daroth@microsoft.com>
2022-11-07 13:36:34 -0800

    Fix typo in NavigateToLogin doc comments (#43933)

--- src/Components/WebAssembly/WebAssembly.Authentication/src/NavigationManagerExtensions.cs
+++ src/Components/WebAssembly/WebAssembly.Authentication/src/NavigationManagerExtensions.cs
@@ -26,7 +26,7 @@ public static class NavigationManagerExtensions
     /// Initiates a logout operation by navigating to the log out endpoint.
     /// </summary>
     /// <remarks>
-    /// The navigation includes stated that is added to the browser history entry to
+    /// The navigation includes state that is added to the browser history entry to
     /// prevent logout operations performed from different contexts.
     /// </remarks>
     /// <param name="manager">The <see cref="NavigationManager"/>.</param>
@@ -48,7 +48,7 @@ public static class NavigationManagerExtensions
     /// Initiates a logout operation by navigating to the log out endpoint.
     /// </summary>
     /// <remarks>
-    /// The navigation includes stated that is added to the browser history entry to
+    /// The navigation includes state that is added to the browser history entry to
     /// prevent logout operations performed from different contexts.
     /// </remarks>
     /// <param name="manager">The <see cref="NavigationManager"/>.</param>
@@ -66,7 +66,7 @@ public static class NavigationManagerExtensions
     /// Initiates a logout operation by navigating to the log out endpoint.
     /// </summary>
     /// <remarks>
-    /// The navigation includes stated that is added to the browser history entry to
+    /// The navigation includes state that is added to the browser history entry to
     /// prevent logout operations performed from different contexts.
     /// </remarks>
     /// <param name="manager">The <see cref="NavigationManager"/>.</param>
