05b3d88935782d219ac5cf92cc920e900ca15087
David Fowler <davidfowl@gmail.com>
2022-11-06 16:07:43 -0800

    Added routing so we can have both endpoints

--- src/Servers/Kestrel/samples/PlaintextApp/Startup.cs
+++ src/Servers/Kestrel/samples/PlaintextApp/Startup.cs
@@ -1,25 +1,47 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-var builder = WebApplication.CreateBuilder();
-
-var app = builder.Build();
-
 var payload = "Hello, World!"u8.ToArray();
 
-app.Run(context =>
-{
-    return Task.RunAsGreenThread(() =>
-    {
-        var response = context.Response;
+var host = new HostBuilder()
+     .ConfigureWebHost(webHost =>
+     {
+         webHost.UseKestrel(o => o.ListenLocalhost(5000));
 
-        response.StatusCode = 200;
-        response.ContentType = "text/plain";
-        response.ContentLength = payload.Length;
+         webHost.ConfigureServices(services => services.AddRouting());
+         webHost.Configure(app =>
+         {
+             app.UseRouting();
+             app.UseEndpoints(endpoints =>
+             {
+                 endpoints.MapGet("/", context =>
+                 {
+                     var response = context.Response;
 
-        // This is async IO under the covers!
-        response.Body.Write(payload);
-    });
-});
+                     response.StatusCode = 200;
+                     response.ContentType = "text/plain";
+                     response.ContentLength = payload.Length;
 
-app.Run();
+                     return response.Body.WriteAsync(payload).AsTask();
+                 });
+
+                 endpoints.MapGet("/green", context =>
+                 {
+                     return Task.RunAsGreenThread(() =>
+                     {
+                         var response = context.Response;
+
+                         response.StatusCode = 200;
+                         response.ContentType = "text/plain";
+                         response.ContentLength = payload.Length;
+
+                         // This is async IO under the covers!
+                         response.Body.Write(payload);
+                     });
+                 });
+             });
+         });
+     })
+     .Build();
+
+host.Run();
