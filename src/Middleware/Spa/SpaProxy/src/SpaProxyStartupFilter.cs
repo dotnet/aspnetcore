b91c3d3e16023fcbe302ba96cf4fbaad90892e84
Surayya Huseyn Zada <shuseynzada@microsoft.com>
2022-11-21 19:44:50 +0100

    Starting SPA proxy right when pipeline is build

--- src/Middleware/Spa/SpaProxy/src/SpaProxyStartupFilter.cs
+++ src/Middleware/Spa/SpaProxy/src/SpaProxyStartupFilter.cs
@@ -3,13 +3,32 @@
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace Microsoft.AspNetCore.SpaProxy;
 
 internal sealed class SpaProxyStartupFilter : IStartupFilter
 {
+    private readonly SpaProxyLaunchManager _spaProxyLaunchManager;
+    private readonly IHostApplicationLifetime _hostLifetime;
+
+    public SpaProxyStartupFilter(
+        SpaProxyLaunchManager spaProxyLaunchManager,
+        IHostApplicationLifetime hostLifetime)
+    {
+        _spaProxyLaunchManager = spaProxyLaunchManager ?? throw new ArgumentNullException(nameof(spaProxyLaunchManager));
+        _hostLifetime = hostLifetime ?? throw new ArgumentNullException(nameof(hostLifetime));
+    }
+
     public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
     {
+        Task.Run(async () =>
+        {
+            if (!await _spaProxyLaunchManager.IsSpaProxyRunning(new CancellationToken()))
+            {
+                _spaProxyLaunchManager.StartInBackground(_hostLifetime.ApplicationStopping);
+            }
+        });
         return builder =>
         {
             builder.UseMiddleware<SpaProxyMiddleware>();
