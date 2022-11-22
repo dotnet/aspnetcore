b76a82ee648fb8900dd3400f87c783a792dd9f08
Marco Minerva <marco.minerva@gmail.com>
2022-11-22 10:06:39 +0100

    Fix response description #45192

--- src/OpenApi/src/OpenApiGenerator.cs
+++ src/OpenApi/src/OpenApiGenerator.cs
@@ -31,6 +31,76 @@ internal sealed class OpenApiGenerator
 
     internal static readonly ParameterBindingMethodCache ParameterBindingMethodCache = new();
 
+    private static readonly Dictionary<int, string> Phrases = new()
+    {
+        { 100, "Continue" },
+        { 101, "Switching Protocols" },
+        { 102, "Processing" },
+
+        { 200, "OK" },
+        { 201, "Created" },
+        { 202, "Accepted" },
+        { 203, "Non-Authoritative Information" },
+        { 204, "No Content" },
+        { 205, "Reset Content" },
+        { 206, "Partial Content" },
+        { 207, "Multi-Status" },
+        { 208, "Already Reported" },
+        { 226, "IM Used" },
+
+        { 300, "Multiple Choices" },
+        { 301, "Moved Permanently" },
+        { 302, "Found" },
+        { 303, "See Other" },
+        { 304, "Not Modified" },
+        { 305, "Use Proxy" },
+        { 306, "Switch Proxy" },
+        { 307, "Temporary Redirect" },
+        { 308, "Permanent Redirect" },
+
+        { 400, "Bad Request" },
+        { 401, "Unauthorized" },
+        { 402, "Payment Required" },
+        { 403, "Forbidden" },
+        { 404, "Not Found" },
+        { 405, "Method Not Allowed" },
+        { 406, "Not Acceptable" },
+        { 407, "Proxy Authentication Required" },
+        { 408, "Request Timeout" },
+        { 409, "Conflict" },
+        { 410, "Gone" },
+        { 411, "Length Required" },
+        { 412, "Precondition Failed" },
+        { 413, "Payload Too Large" },
+        { 414, "URI Too Long" },
+        { 415, "Unsupported Media Type" },
+        { 416, "Range Not Satisfiable" },
+        { 417, "Expectation Failed" },
+        { 418, "I'm a teapot" },
+        { 419, "Authentication Timeout" },
+        { 421, "Misdirected Request" },
+        { 422, "Unprocessable Entity" },
+        { 423, "Locked" },
+        { 424, "Failed Dependency" },
+        { 426, "Upgrade Required" },
+        { 428, "Precondition Required" },
+        { 429, "Too Many Requests" },
+        { 431, "Request Header Fields Too Large" },
+        { 451, "Unavailable For Legal Reasons" },
+
+        { 500, "Internal Server Error" },
+        { 501, "Not Implemented" },
+        { 502, "Bad Gateway" },
+        { 503, "Service Unavailable" },
+        { 504, "Gateway Timeout" },
+        { 505, "HTTP Version Not Supported" },
+        { 506, "Variant Also Negotiates" },
+        { 507, "Insufficient Storage" },
+        { 508, "Loop Detected" },
+        { 510, "Not Extended" },
+        { 511, "Network Authentication Required" },
+    };
+
     /// <summary>
     /// Creates an <see cref="OpenApiGenerator" /> instance given an <see cref="IHostEnvironment" />
     /// and an <see cref="IServiceProviderIsService" /> instance.
@@ -192,7 +262,7 @@ internal sealed class OpenApiGenerator
 
         foreach (var annotation in eligibileAnnotations)
         {
-            var statusCode = annotation.Key.ToString(CultureInfo.InvariantCulture);
+            var statusCode = annotation.Key;
 
             // TODO: Use the discarded response Type for schema generation
             var (_, contentTypes) = annotation.Value;
@@ -203,7 +273,7 @@ internal sealed class OpenApiGenerator
                 responseContent[contentType] = new OpenApiMediaType();
             }
 
-            responses[statusCode] = new OpenApiResponse
+            responses[statusCode.ToString(CultureInfo.InvariantCulture)] = new OpenApiResponse
             {
                 Content = responseContent,
                 Description = GetResponseDescription(statusCode)
@@ -212,23 +282,8 @@ internal sealed class OpenApiGenerator
         return responses;
     }
 
-    private static string GetResponseDescription(string statusCode)
-    {
-        if (statusCode.Length != 3)
-        {
-            return string.Empty;
-        }
-        var first = statusCode[0];
-        return first switch
-        {
-            '1' => "Information",
-            '2' => "Success",
-            '3' => "Redirection",
-            '4' => "Client error",
-            '5' => "Server error",
-            _ => string.Empty,
-        };
-    }
+    private static string GetResponseDescription(int statusCode)
+        => Phrases.TryGetValue(statusCode, out var phrase) ? phrase : string.Empty;
 
     private static void GenerateDefaultContent(MediaTypeCollection discoveredContentTypeAnnotation, Type? discoveredTypeAnnotation)
     {
