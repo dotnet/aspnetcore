16c66cc7bfdf3d18b9a43195c3d6148553ce91a5
Günther Foidl <gue@korporal.at>
2022-12-06 22:30:04 +0100

    Added comment for _invalidFieldChars

--- src/Shared/ServerInfrastructure/HttpCharacters.cs
+++ src/Shared/ServerInfrastructure/HttpCharacters.cs
@@ -33,6 +33,7 @@ internal static class HttpCharacters
     // HTAB, [VCHAR, SP]
     private static readonly IndexOfAnyValues<char> _allowedFieldChars = IndexOfAnyValues.Create("\t !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" + AlphaNumeric);
 
+    // Values are [0x00, 0x1F] without 0x09 (HTAB) and with 0x7F.
     private static readonly IndexOfAnyValues<char> _invalidFieldChars = IndexOfAnyValues.Create(
         "\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\u0008\u000A\u000B\u000C\u000D\u000E\u000F\u0010" +
         "\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001A\u001B\u001C\u001D\u001E\u001F\u007F");
