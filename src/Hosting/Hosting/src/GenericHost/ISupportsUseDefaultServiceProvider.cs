414c0d0e70ac8c0a7ce714c47aab967e7e8a6278
Andrew Casey <andrew.casey@microsoft.com>
2022-12-13 12:38:58 -0800

    DefaultServiceProvider fallout

--- src/Hosting/Hosting/src/GenericHost/ISupportsUseDefaultServiceProvider.cs
+++ src/Hosting/Hosting/src/GenericHost/ISupportsUseDefaultServiceProvider.cs
@@ -1,11 +1,13 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.AspNetCore.Hosting;
 
 internal interface ISupportsUseDefaultServiceProvider
 {
+    [RequiresDynamicCode("The default service provider requires dynamic code and is not AOT safe")]
     IWebHostBuilder UseDefaultServiceProvider(Action<WebHostBuilderContext, ServiceProviderOptions> configure);
 }
