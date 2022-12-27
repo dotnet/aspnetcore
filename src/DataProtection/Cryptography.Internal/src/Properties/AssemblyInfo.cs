b99885f57f91a2d8212fb55a37d2bae37dbe5f0f
Simon Lamon <simonlamon93@hotmail.com>
2022-12-27 20:39:48 +0100

    Remove using statements

--- src/DataProtection/Cryptography.Internal/src/Properties/AssemblyInfo.cs
+++ src/DataProtection/Cryptography.Internal/src/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // we only ever p/invoke into DLLs known to be in the System32 folder
