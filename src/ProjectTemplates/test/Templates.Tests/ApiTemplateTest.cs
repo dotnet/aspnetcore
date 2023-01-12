761e4763755719a1740701cbdd7971dd87941473
Damian Edwards <damian@damianedwards.com>
2023-01-12 13:34:20 -0800

    Update ApiTemplateTest.cs

--- src/ProjectTemplates/test/Templates.Tests/ApiTemplateTest.cs
+++ src/ProjectTemplates/test/Templates.Tests/ApiTemplateTest.cs
@@ -1,11 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Testing;
 using Templates.Test.Helpers;
 using Xunit.Abstractions;
@@ -108,5 +103,6 @@ public class ApiTemplateTest : LoggedTest
         await aspNetProcess.AssertOk("/albums");
         await aspNetProcess.AssertOk("/albums/1");
         await aspNetProcess.AssertNotFound("/albums/100");
+        await aspNetProcess.AssertNotFound("/");
     }
 }
