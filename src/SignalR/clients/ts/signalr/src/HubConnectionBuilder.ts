66e5d5070bfd71516c41b55c6cd0bc5520a2349d
Surayya Huseyn Zada <114938397+surayya-MS@users.noreply.github.com>
2023-01-17 19:49:57 +0100

    Update src/SignalR/clients/ts/signalr/src/HubConnectionBuilder.ts
    Co-authored-by: Brennan <brecon@microsoft.com>

--- src/SignalR/clients/ts/signalr/src/HubConnectionBuilder.ts
+++ src/SignalR/clients/ts/signalr/src/HubConnectionBuilder.ts
@@ -198,7 +198,7 @@ export class HubConnectionBuilder {
         return this;
     }
 
-    /** Configures keepAliveIntervalInMilliseconds for the {@link @microsoft/signalr.HubConnection}.
+    /** Configures {@link @microsoft/signalr.HubConnection.keepAliveIntervalInMilliseconds} for the {@link @microsoft/signalr.HubConnection}.
      *
      * @returns The {@link @microsoft/signalr.HubConnectionBuilder} instance, for chaining.
      */
