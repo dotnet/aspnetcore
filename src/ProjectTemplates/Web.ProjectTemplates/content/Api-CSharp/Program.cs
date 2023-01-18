743349d6395358897cea9fc4bf4835bb3bc16c21
Eric Erhardt <eric.erhardt@microsoft.com>
2023-01-18 17:06:32 -0600

    Use the new slim hosting API in the api template.

--- src/ProjectTemplates/Web.ProjectTemplates/content/Api-CSharp/Program.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/Api-CSharp/Program.cs
@@ -3,7 +3,7 @@ using System.Text.Json.Serialization;
 #endif
 using Company.ApiApplication1;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateSlimBuilder(args);
 builder.Logging.AddConsole();
 
 #if (NativeAot)
