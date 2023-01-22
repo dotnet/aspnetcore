38cf0ff5aca5a8629cb5732e0f51b8fe2db5d0f5
James Newton-King <james@newtonking.com>
2023-01-22 14:07:50 +0800

    Fix route syntax highlighting parameter color

--- src/Framework/AspNetCoreAnalyzers/src/Analyzers/RouteEmbeddedLanguage/RoutePatternClassifier.cs
+++ src/Framework/AspNetCoreAnalyzers/src/Analyzers/RouteEmbeddedLanguage/RoutePatternClassifier.cs
@@ -94,7 +94,10 @@ internal sealed class RoutePatternClassifier : IAspNetCoreEmbeddedLanguageClassi
 
         public void Visit(RoutePatternNameParameterPartNode node)
         {
-            AddClassification(node.ParameterNameToken, ClassificationTypeNames.ParameterName);
+            // Parameter name should look like a regular parameter.
+            // ClassificationTypeNames.ParameterName isn't working so temporarily reuse color from JSON syntax.
+            // TODO: Fix with https://github.com/dotnet/aspnetcore/issues/46207
+            AddClassification(node.ParameterNameToken, "json - object");
         }
 
         public void Visit(RoutePatternPolicyParameterPartNode node)
