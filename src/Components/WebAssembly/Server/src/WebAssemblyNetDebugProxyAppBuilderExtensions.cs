1a1ab568867f537c1c3c7b36b2ba315a567d8b0d
Thays Grazia <thaystg@gmail.com>
2023-01-23 15:08:02 -0300

    [blazor][wasm][debug]Press alt-shift-d and open firefox debug tab attached to the blazor app (#46132)
    * Press alt-shift-d and open firefox debug tab attached to the blazor app
    
    * remove debugger.launch.
    
    * removing unrelated changes
    
    * Removing unnecessary changes on chrome debugging.
    
    * addressing @mkArtakMSFT comments
    
    * Addressing @mkArtakMSFT comments.
    
    * Addressing Steve comments, adding a console.warning message and remove the beautiful message, removed the Newtonsoft from the send message, todo: remove the Newtonsoft from receive message.
    
    * Completely removing newtonsoft usage as asked by steve.
    
    * Change warning message.

--- src/Components/WebAssembly/Server/src/WebAssemblyNetDebugProxyAppBuilderExtensions.cs
+++ src/Components/WebAssembly/Server/src/WebAssemblyNetDebugProxyAppBuilderExtensions.cs
@@ -31,8 +31,12 @@ public static class WebAssemblyNetDebugProxyAppBuilderExtensions
                     browserUrl = new Uri(browserParam);
                     devToolsHost = $"http://{browserUrl.Host}:{browserUrl.Port}";
                 }
-
-                var debugProxyBaseUrl = await DebugProxyLauncher.EnsureLaunchedAndGetUrl(context.RequestServices, devToolsHost);
+                var isFirefox = string.IsNullOrEmpty(queryParams.Get("isFirefox")) ? false : true;
+                if (isFirefox)
+                {
+                    devToolsHost = "localhost:6000";
+                }
+                var debugProxyBaseUrl = await DebugProxyLauncher.EnsureLaunchedAndGetUrl(context.RequestServices, devToolsHost, isFirefox);
                 var requestPath = context.Request.Path.ToString();
                 if (requestPath == string.Empty)
                 {
@@ -43,7 +47,14 @@ public static class WebAssemblyNetDebugProxyAppBuilderExtensions
                 {
                     case "/":
                         var targetPickerUi = new TargetPickerUi(debugProxyBaseUrl, devToolsHost);
-                        await targetPickerUi.Display(context);
+                        if (isFirefox)
+                        {
+                            await targetPickerUi.DisplayFirefox(context);
+                        }
+                        else
+                        {
+                            await targetPickerUi.Display(context);
+                        }
                         break;
                     case "/ws-proxy":
                         context.Response.Redirect($"{debugProxyBaseUrl}{browserUrl!.PathAndQuery}");
