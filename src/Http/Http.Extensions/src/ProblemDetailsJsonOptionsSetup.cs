941f92ae6250c0bc644f80449df858f8bd2d5582
Bruno Oliveira <brunolins16@users.noreply.github.com>
2023-01-23 22:14:03 +0000

    using AddContext

--- src/Http/Http.Extensions/src/ProblemDetailsJsonOptionsSetup.cs
+++ src/Http/Http.Extensions/src/ProblemDetailsJsonOptionsSetup.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Text.Json.Serialization.Metadata;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.Extensions.Options;
 
@@ -13,13 +12,8 @@ internal sealed class ProblemDetailsJsonOptionsSetup : IPostConfigureOptions<Jso
     {
         if (options.SerializerOptions.TypeInfoResolver is not null)
         {
-            if (options.SerializerOptions.IsReadOnly)
-            {
-                options.SerializerOptions = new(options.SerializerOptions);
-            }
-
             // Combine the current resolver with our internal problem details context
-            options.SerializerOptions.TypeInfoResolver = JsonTypeInfoResolver.Combine(options.SerializerOptions.TypeInfoResolver!, ProblemDetailsJsonContext.Default);
+            options.SerializerOptions.AddContext<ProblemDetailsJsonContext>();
         }
     }
 }
