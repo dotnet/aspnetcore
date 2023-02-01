9df633ec160ba6d83d5e6b8f041cf6c448660398
Bruno Oliveira <brolivei@microsoft.com>
2023-02-01 09:56:01 -0800

    Fixing notnull annotation

--- src/Shared/Json/JsonSerializerExtensions.cs
+++ src/Shared/Json/JsonSerializerExtensions.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Text.Json.Serialization.Metadata;
@@ -12,7 +13,7 @@ internal static class JsonSerializerExtensions
     public static bool HasKnownPolymorphism(this JsonTypeInfo jsonTypeInfo)
      => jsonTypeInfo.Type.IsSealed || jsonTypeInfo.Type.IsValueType || jsonTypeInfo.PolymorphismOptions is not null;
 
-    public static bool IsValid(this JsonTypeInfo jsonTypeInfo, Type? runtimeType)
+    public static bool IsValid(this JsonTypeInfo jsonTypeInfo, [NotNullWhen(false)] Type? runtimeType)
      => runtimeType is null || jsonTypeInfo.Type == runtimeType || jsonTypeInfo.HasKnownPolymorphism();
 
     public static JsonTypeInfo GetReadOnlyTypeInfo(this JsonSerializerOptions options, Type type)
