ec74e2e2a8642947570c5185b03bd0838a88f35b
Mitch Denny <midenn@microsoft.com>
2023-02-13 17:12:25 +1100

    Remove rogue namespaces.

--- src/Http/Http.Extensions/test/RequestDelegateGenerator/RequestDelegateGeneratorTests.cs
+++ src/Http/Http.Extensions/test/RequestDelegateGenerator/RequestDelegateGeneratorTests.cs
@@ -1,10 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Security.Cryptography.Pkcs;
 using Microsoft.AspNetCore.Http.Generators.StaticRouteHandlerModel;
-using Microsoft.Diagnostics.Runtime.ICorDebug;
-using Newtonsoft.Json.Linq;
 
 namespace Microsoft.AspNetCore.Http.Generators.Tests;
 
