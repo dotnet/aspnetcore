0e7d9c5f0a74f73369cc25e1980d221931f80239
Surayya Huseyn Zada <shuseynzada@microsoft.com>
2023-02-21 18:05:51 +0100

    E2E tests

--- src/Components/test/E2ETest/Tests/SectionsTest.cs
+++ src/Components/test/E2ETest/Tests/SectionsTest.cs
@@ -25,40 +25,155 @@ public class SectionsTest : ServerTestBase<ToggleExecutionModeServerFixture<Prog
     protected override void InitializeAsyncCore()
     {
         Navigate(ServerPathBase, noReload: _serverFixture.ExecutionMode == ExecutionMode.Client);
-        _appElement = Browser.MountTestComponent<BasicTestApp.SectionsTest.ParentComponent>();
+        _appElement = Browser.MountTestComponent<BasicTestApp.SectionsTest.ParentComponentWithTwoChildren>();
     }
 
     [Fact]
-    public void SectionOutletInParentComponentRendersSectionContentOfChildComponent()
+    public void NoExistingSectionContents_SectionOutletsRenderNothing()
     {
-        //Nothing is chosen yet
+        // At the beginning no SectionContents are rendered
         Browser.DoesNotExist(By.Id("counter"));
-        var options = _appElement.FindElement(By.Id("child-component"));
+        Browser.DoesNotExist(By.Id("text"));
+    }
 
-        // Choose Counter
-        options.FindElement(By.Name("counter")).Click();
+    [Fact]
+    public void RenderOneSectionContent_MatchingSectionOutletRendersContentSuccessfully()
+    {
+        _appElement.FindElement(By.Id("counter_render_section_content")).Click();
+
         var counter = Browser.Exists(By.Id("counter"));
-
         Assert.Equal("0", counter.Text);
-        var incrememntButton = _appElement.FindElement(By.Id("increment_button"));
 
-        incrememntButton.Click();
+        _appElement.FindElement(By.Id("increment_button")).Click();
+
         Assert.Equal("1", counter.Text);
     }
 
     [Fact]
-    public void SectionOutletInParentComponentRendersSectionContentOfAnotherChildComponent()
+    public void RenderTwoSectionContentsWithSameName_LastRenderedOverridesSectionOutletContent()
     {
-        var options = _appElement.FindElement(By.Id("child-component"));
+        _appElement.FindElement(By.Id("counter_render_section_content")).Click();
 
-        // Choose Counter
-        options.FindElement(By.Name("counter")).Click();
         Browser.Exists(By.Id("counter"));
 
-        // Choose Simple Component
-        options.FindElement(By.Name("simple-component")).Click();
-        var simpleComponentText = Browser.Exists(By.Id("text"));
-        Assert.Equal("Hello!", simpleComponentText.Text);
+        _appElement.FindElement(By.Id("text_render_section_content")).Click();
+
+        Browser.Exists(By.Id("text"));
         Browser.DoesNotExist(By.Id("counter"));
+    }
+
+    [Fact]
+    public void SecondSectionContentGetsDisposed_SectionOutletRendersFirstSectionContent()
+    {
+        //Render Counter and TextComponent SectionContents with same Name
+        // TextComponent's SectionContent overrides Counter SectionContent
+        _appElement.FindElement(By.Id("counter_render_section_content")).Click();
+        _appElement.FindElement(By.Id("text_render_section_content")).Click();
+
+        _appElement.FindElement(By.Id("text_dispose_section_content")).Click();
+
+        Browser.Exists(By.Id("counter"));
+    }
+
+    [Fact]
+    public void BothSectionContentsGetDisposed_SectionOutletsRenderNothing()
+    {
+        _appElement.FindElement(By.Id("counter_render_section_content")).Click();
+        _appElement.FindElement(By.Id("text_render_section_content")).Click();
+
+        _appElement.FindElement(By.Id("counter_dispose_section_content")).Click();
+        _appElement.FindElement(By.Id("text_dispose_section_content")).Click();
+
+        Browser.DoesNotExist(By.Id("counter"));
+        Browser.DoesNotExist(By.Id("text"));
+    }
+
+    [Fact]
+    public void SectionContentNameChanges_MatchingSectionOutletRendersContent()
+    {
+        // Render Counter and TextComponent SectionContents with same Name
+        // TextComponent's SectionContent overrides Counter SectionContent
+        _appElement.FindElement(By.Id("counter_render_section_content")).Click();
+        _appElement.FindElement(By.Id("text_render_section_content")).Click();
+
+        _appElement.FindElement(By.Id("counter_change_section_content_name")).Click();
+
+        Browser.Exists(By.Id("counter"));
+    }
+
+    [Fact]
+    public void SectionContentNameChangesToNonExisting_NoMatchingSectionOutletResultingNoRendering()
+    {
+        // Render Counter and TextComponent SectionContents with same Name
+        // TextComponent's SectionContent overrides Counter SectionContent
+        _appElement.FindElement(By.Id("counter_render_section_content")).Click();
+        _appElement.FindElement(By.Id("text_render_section_content")).Click();
+
+        _appElement.FindElement(By.Id("counter_change_section_content_name_nonexisting")).Click();
+
+        Browser.DoesNotExist(By.Id("counter"));
+    }
+
+    [Fact]
+    public void SectionOutletGetsDisposed_NoContentsRendered()
+    {
+        // Render Counter and TextComponent SectionContents with same Name      
+        _appElement.FindElement(By.Id("counter_render_section_content")).Click();
+        _appElement.FindElement(By.Id("text_render_section_content")).Click();
+
+        // TextComponent's SectionContent overrides Counter SectionContent
+        Browser.Exists(By.Id("text"));
+
+        _appElement.FindElement(By.Id("section_outlet_dispose")).Click();
+
+        Browser.DoesNotExist(By.Id("counter"));
+        Browser.DoesNotExist(By.Id("text"));
+    }
+
+    [Fact]
+    public void DefaultSectionContent_DoesNotOverrideAnotherSectionContent()
+    {
+        _appElement.FindElement(By.Id("text_section_content_make_default")).Click();
+
+        _appElement.FindElement(By.Id("counter_render_section_content")).Click();
+        _appElement.FindElement(By.Id("text_render_section_content")).Click();
+
+        // TextComponent's SectionContent IsDefaultContent=true does not override Counter's SectionContent
+        Browser.DoesNotExist(By.Id("text"));
+        Browser.Exists(By.Id("counter"));
+    }
+
+    [Fact]
+    public void BothDefaultSectionContents_()
+    {
+        //TODO
+    }
+
+    [Fact]
+    public void DefaultSectionContent_RendersWhenAnotherSectionContentGetsDisposed()
+    {
+        _appElement.FindElement(By.Id("text_section_content_make_default")).Click();
+
+        _appElement.FindElement(By.Id("counter_render_section_content")).Click();
+        _appElement.FindElement(By.Id("text_render_section_content")).Click();
+
+        _appElement.FindElement(By.Id("counter_dispose_section_content")).Click();
+
+        Browser.DoesNotExist(By.Id("counter"));
+        Browser.Exists(By.Id("text"));
+    }
+
+    // TODO support changing IsDefaultContent
+    [Fact]
+    public void IsDefaultContentChanges_DoesNotOverrideAnotherSectionContent()
+    {
+        _appElement.FindElement(By.Id("counter_render_section_content")).Click();
+        _appElement.FindElement(By.Id("text_render_section_content")).Click();
+
+        _appElement.FindElement(By.Id("text_section_content_make_default")).Click();
+
+        // TextComponent's SectionContent IsDefaultContent=true does not override Counter's SectionContent
+        Browser.DoesNotExist(By.Id("text"));
+        Browser.Exists(By.Id("counter"));
     }
 }
