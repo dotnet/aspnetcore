bc7fc50c500d010c970a3f875defeb70cd8eab0f
Eric Erhardt <eric.erhardt@microsoft.com>
2023-02-22 16:44:14 -0600

    PR feedback

--- src/Http/Routing/src/Constraints/RegexRouteConstraint.cs
+++ src/Http/Routing/src/Constraints/RegexRouteConstraint.cs
@@ -41,7 +41,8 @@ public class RegexRouteConstraint : IRouteConstraint, IParameterLiteralNodeMatch
         ArgumentNullException.ThrowIfNull(regexPattern);
 
         // Create regex instance lazily to avoid compiling regexes at app startup. Delay creation until Constraint is first evaluated.
-        // This is not thread safe. No side effect but multiple instances of a regex instance could be created from a burst of requests.
+        // This is not thread-safe. No side effect, but multiple instances of a regex instance could be created from a burst of requests.
+        // The regex instance is created by a delegate here to allow the regex engine to be trimmed when this constructor is trimmed.
         _regexFactory = () => new Regex(
             regexPattern,
             RegexOptions.CultureInvariant | RegexOptions.Compiled | RegexOptions.IgnoreCase,
