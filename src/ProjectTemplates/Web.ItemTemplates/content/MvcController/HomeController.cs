ecc10557650a2f0a54b1edc8398a4faae81d5da1
Deep Choudhery <54324771+deepchoudhery@users.noreply.github.com>
2023-02-24 13:55:39 -0800

    Add 'dotnet new' web item templates (razor view, api controller, mvc controller) (#46714)
    * add dotnet new templates (razor view, api controller, mvc controller) (#1)
    
    * added razor view
    
    * fix identity for razor view item template
    
    * added api controller
    
    * fixed api, added mvc controller
    
    * pr comment fixes
    
    * PR comment fixes 2
    
    * fixing postActions.description
    
    * changed encoding for razor view Index.cshtml

---
+++ src/ProjectTemplates/Web.ItemTemplates/content/MvcController/HomeController.cs
@@ -0,0 +1,88 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    #if NameIsController
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+    #else
+    public class HomeController : Controller
+    #endif
+    {
+        // GET: HomeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #if(actions)
+        // GET: HomeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HomeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HomeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endif
+    }
+}
