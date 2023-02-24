8c830d1580a15b342d01ac2f35f6f44afc404d10
Bruno Oliveira <brolivei@microsoft.com>
2023-02-24 15:58:04 -0800

    Removing extra usings

--- src/Http/Http.Extensions/src/HttpJsonServiceExtensions.cs
+++ src/Http/Http.Extensions/src/HttpJsonServiceExtensions.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.AspNetCore.Http.Json;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Options;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
