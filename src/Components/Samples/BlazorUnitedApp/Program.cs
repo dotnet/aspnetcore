7a2e8217c7eb349956a51f76b0df5851b30977b0
jacalvar <jacalvar@microsoft.com>
2023-03-03 15:14:25 +0100

    Update sample project to match United idioms

--- src/Components/Samples/BlazorUnitedApp/Program.cs
+++ src/Components/Samples/BlazorUnitedApp/Program.cs
@@ -6,6 +6,8 @@ using BlazorUnitedApp.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddRazorComponents();
+
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
@@ -23,5 +25,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.MapRazorComponents();
 
 app.Run();
