d14207d70b0788f2ac2866daef7efd41fa7a353a
jacalvar <jacalvar@microsoft.com>
2023-03-09 20:58:19 +0100

    Endpoint discovery

--- src/Components/Endpoints/src/Builder/RazorComponentsEndpointRouteBuilderExtensions.cs
+++ src/Components/Endpoints/src/Builder/RazorComponentsEndpointRouteBuilderExtensions.cs
@@ -2,7 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Linq;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Endpoints;
+using Microsoft.AspNetCore.Components.Infrastructure;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -18,24 +20,27 @@ public static class RazorComponentsEndpointRouteBuilderExtensions
     /// </summary>
     /// <param name="endpoints"></param>
     /// <returns></returns>
-    public static IEndpointConventionBuilder MapRazorComponents(this IEndpointRouteBuilder endpoints)
+    public static RazorComponentEndpointConventionBuilder MapRazorComponents<TRootComponent>(this IEndpointRouteBuilder endpoints)
+        where TRootComponent : IRazorComponentApplication<TRootComponent>
     {
         ArgumentNullException.ThrowIfNull(endpoints);
 
         EnsureRazorComponentServices(endpoints);
 
-        return GetOrCreateDataSource(endpoints).DefaultBuilder;
+        return GetOrCreateDataSource<TRootComponent>(endpoints).DefaultBuilder;
     }
 
-    private static RazorComponentEndpointDataSource GetOrCreateDataSource(IEndpointRouteBuilder endpoints)
+    private static RazorComponentEndpointDataSource<TRootComponent> GetOrCreateDataSource<TRootComponent>(IEndpointRouteBuilder endpoints)
+        where TRootComponent : IRazorComponentApplication<TRootComponent>
     {
-        var dataSource = endpoints.DataSources.OfType<RazorComponentEndpointDataSource>().FirstOrDefault();
+        var dataSource = endpoints.DataSources.OfType<RazorComponentEndpointDataSource<TRootComponent>>().FirstOrDefault();
         if (dataSource == null)
         {
             // Very likely this needs to become a factory and we might need to have multiple endpoint data
             // sources, once we figure out the exact scenarios for
             // https://github.com/dotnet/aspnetcore/issues/46992
-            dataSource = endpoints.ServiceProvider.GetRequiredService<RazorComponentEndpointDataSource>();
+            var factory = endpoints.ServiceProvider.GetRequiredService<RazorComponentEndpointDataSourceFactory>();
+            dataSource = factory.CreateDataSource<TRootComponent>();
             endpoints.DataSources.Add(dataSource);
         }
 
---
+++ src/Components/Endpoints/src/Discovery/PageCollection.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Linq;
+
+namespace Microsoft.AspNetCore.Components;
+
+internal class PageCollection
+{
+    public PageCollection(IEnumerable<PageDefinition> pages)
+    {
+        Pages = pages.ToList();
+    }
+
+    internal static PageCollection Empty { get; } = new PageCollection(Enumerable.Empty<PageDefinition>());
+
+    internal List<PageDefinition> Pages { get; }
+}
