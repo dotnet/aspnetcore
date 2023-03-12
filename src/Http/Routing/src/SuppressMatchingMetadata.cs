3cf3bb12c556fcd35d1cd7f398b619e3e920c7b0
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2023-03-12 08:38:10 +0000

    Override the ToString method in the SuppressMatchingMetadata  class.

--- src/Http/Routing/src/SuppressMatchingMetadata.cs
+++ src/Http/Routing/src/SuppressMatchingMetadata.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 namespace Microsoft.AspNetCore.Routing;
@@ -13,4 +13,10 @@ public sealed class SuppressMatchingMetadata : ISuppressMatchingMetadata
     /// Gets a value indicating whether the associated endpoint should be used for URL matching.
     /// </summary>
     public bool SuppressMatching => true;
+
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+        return SuppressMatching.ToString();
+    }
 }
