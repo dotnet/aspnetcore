0e71e8e37663afa4481f0a0d6c0f41b0945a1ef4
Aditya Mandaleeka <adityam@microsoft.com>
2023-03-15 19:10:19 -0700

    Doc comments.

--- src/Http/Http/src/Timeouts/IHttpRequestTimeoutFeature.cs
+++ src/Http/Http/src/Timeouts/IHttpRequestTimeoutFeature.cs
@@ -4,17 +4,18 @@
 namespace Microsoft.AspNetCore.Http.Timeouts;
 
 /// <summary>
-/// 
+/// Used to control timeouts on the current request.
 /// </summary>
 public interface IHttpRequestTimeoutFeature
 {
     /// <summary>
-    /// 
+    /// A <see cref="CancellationToken" /> that will trigger when the request times out.
     /// </summary>
     CancellationToken RequestTimeoutToken { get; }
 
     /// <summary>
-    /// 
+    /// Disables the request timeout if it hasn't already expired. This does not
+    /// trigger the <see cref="RequestTimeoutToken"/>.
     /// </summary>
     void DisableTimeout();
 }
