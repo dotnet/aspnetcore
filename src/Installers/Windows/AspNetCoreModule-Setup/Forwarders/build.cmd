6906d0b4464e32f2a73b8a42642f50022c4d7115
Lex Li <support@lextm.com>
2023-03-19 04:10:45 -0700

    Revised ARM64 installation folder contents (dotnet#47115).

---
+++ src/Installers/Windows/AspNetCoreModule-Setup/Forwarders/build.cmd
@@ -0,0 +1,18 @@
+SET objDir=%1
+SET binDir=%2
+
+cl /c /Fo%objDir%\aspnetcorev2_arm64.obj empty.cpp
+cl /c /arm64EC /Fo%objDir%\aspnetcorev2_x64.obj empty.cpp
+
+link /lib /machine:arm64 /def:aspnetcorev2_arm64.def /out:%objDir%\aspnetcorev2_arm64.lib
+link /lib /machine:x64 /def:aspnetcorev2_x64.def /out:%objDir%\aspnetcorev2_x64.lib
+
+link /dll /noentry /machine:arm64x /defArm64Native:aspnetcorev2_arm64.def /def:aspnetcorev2_x64.def %objDir%\aspnetcorev2_arm64.obj %objDir%\aspnetcorev2_x64.obj /out:%binDir%\aspnetcorev2.dll %objDir%\aspnetcorev2_arm64.lib %objDir%\aspnetcorev2_x64.lib
+
+cl /c /Fo%objDir%\aspnetcorev2_outofprocess_arm64.obj empty.cpp
+cl /c /arm64EC /Fo%objDir%\aspnetcorev2_outofprocess_x64.obj empty.cpp
+
+link /lib /machine:arm64 /def:aspnetcorev2_outofprocess_arm64.def /out:%objDir%\aspnetcorev2_outofprocess_arm64.lib
+link /lib /machine:x64 /def:aspnetcorev2_outofprocess_x64.def /out:%objDir%\aspnetcorev2_outofprocess_x64.lib
+
+link /dll /noentry /machine:arm64x /defArm64Native:aspnetcorev2_outofprocess_arm64.def /def:aspnetcorev2_outofprocess_x64.def %objDir%\aspnetcorev2_outofprocess_arm64.obj %objDir%\aspnetcorev2_outofprocess_x64.obj /out:%binDir%\aspnetcorev2_outofprocess.dll %objDir%\aspnetcorev2_outofprocess_arm64.lib %objDir%\aspnetcorev2_outofprocess_x64.lib
