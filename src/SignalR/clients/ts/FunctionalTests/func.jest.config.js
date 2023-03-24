b589624bc4b2be6d5949e0f64480fb80bc05edaf
Brennan Conroy <brecon@microsoft.com>
2023-03-24 11:04:34 -0700

    Update Webpack in SignalR

--- src/SignalR/clients/ts/FunctionalTests/func.jest.config.js
+++ src/SignalR/clients/ts/FunctionalTests/func.jest.config.js
@@ -6,25 +6,26 @@ module.exports = {
         // We reference the ESM output from tests and don't want to run them through jest as it won't understand the syntax
         ".*/node_modules/(?!@microsoft)"
     ],
-    globals: {
-        "ts-jest": {
-            "tsconfig": "./tsconfig.jest.json",
-            "babelConfig": true,
-
-            // Needed in order to properly process the JS files
-            // We run 'tsc --noEmit' to get TS diagnostics before the test instead
-            "diagnostics": false,
-        }
-    },
     reporters: [
         "default",
         ["../common/node_modules/jest-junit/index.js", { "outputDirectory": "../../../../../artifacts/log/", "outputName": `${process.platform}` + ".node.functional.junit.xml" }]
     ],
     transform: {
-        "^.+\\.(jsx?|tsx?)$": "../common/node_modules/ts-jest"
+        "^.+\\.(jsx?|tsx?)$": [
+            "../common/node_modules/ts-jest",
+            {
+                "tsconfig": "./tsconfig.jest.json",
+                "babelConfig": true,
+
+                // Needed in order to properly process the JS files
+                // We run 'tsc --noEmit' to get TS diagnostics before the test instead
+                "diagnostics": false,
+            }
+        ]
     },
     testEnvironment: "node",
     testRegex: "(Tests)\\.(jsx?|tsx?)$",
+    testRunner: "../common/node_modules/jest-jasmine2",
     moduleNameMapper: {
         "^ts-jest$": "<rootDir>/../common/node_modules/ts-jest",
         "^@microsoft/signalr$": "<rootDir>/../signalr/dist/cjs/index.js"
