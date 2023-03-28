83ab2363a4817bb67c1b1dba708d908634700224
deepchoudhery <decho@microsoft.com>
2023-03-28 12:31:33 -0700

    change 'ValuesController' to 'ValueController'

--- src/ProjectTemplates/Web.ItemTemplates/content/ApiController/ValueController.cs
+++ src/ProjectTemplates/Web.ItemTemplates/content/ApiController/ValueController.cs
@@ -6,39 +6,39 @@ namespace MyApp.Namespace
     [Route("api/[controller]")]
     [ApiController]
     #if NameIsController
-    public class ValuesController : Microsoft.AspNetCore.Mvc.ControllerBase
+    public class ValueController : Microsoft.AspNetCore.Mvc.ControllerBase
     #else
-    public class ValuesController : ControllerBase
+    public class ValueController : ControllerBase
     #endif
     {
         #if(actions)
-        // GET: api/<ValuesController>
+        // GET: api/<ValueController>
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/<ValuesController>/5
+        // GET api/<ValueController>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/<ValuesController>
+        // POST api/<ValueController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/<ValuesController>/5
+        // PUT api/<ValueController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/<ValuesController>/5
+        // DELETE api/<ValueController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
