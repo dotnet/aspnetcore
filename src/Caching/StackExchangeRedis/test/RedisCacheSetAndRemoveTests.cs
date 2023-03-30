ab0c6d504a3eca1e0afd8d025c9e584d51b07c74
Marc Gravell <marc.gravell@gmail.com>
2023-03-30 14:35:18 +0100

    include tests via string-based API

--- src/Caching/StackExchangeRedis/test/RedisCacheSetAndRemoveTests.cs
+++ src/Caching/StackExchangeRedis/test/RedisCacheSetAndRemoveTests.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Runtime.CompilerServices;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Distributed;
 using Xunit;
@@ -99,7 +100,7 @@ public class RedisCacheSetAndRemoveTests
     }
 
     [Fact(Skip = SkipReason)]
-    public void GetSetEmptyNonNullBuffer()
+    public void SetGetEmptyNonNullBuffer()
     {
         var cache = RedisTestConfig.CreateCacheInstance(GetType().Name);
         var key = Me();
@@ -113,18 +114,66 @@ public class RedisCacheSetAndRemoveTests
     }
 
     [Fact(Skip = SkipReason)]
-    public async Task GetSetEmptyNonNullBufferAsync()
+    public async Task SetGetEmptyNonNullBufferAsync()
     {
         var cache = RedisTestConfig.CreateCacheInstance(GetType().Name);
         var key = Me();
         await cache.RemoveAsync(key); // known state
         Assert.Null(await cache.GetAsync(key)); // expect null
 
-        cache.Set(key, Array.Empty<byte>());
+        await cache.SetAsync(key, Array.Empty<byte>());
         var arr = await cache.GetAsync(key);
         Assert.NotNull(arr);
         Assert.Empty(arr);
     }
+
+    [Theory(Skip = SkipReason)]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("abc")]
+    public void SetGetNonNullString(string payload)
+    {
+        var cache = RedisTestConfig.CreateCacheInstance(GetType().Name);
+        var key = Me();
+        cache.Remove(key); // known state
+        Assert.Null(cache.Get(key)); // expect null
+        cache.SetString(key, payload);
+
+        // check raw bytes
+        var raw = cache.Get(key);
+        Assert.Equal(Hex(payload), Hex(raw));
+
+        // check via string API
+        var value = cache.GetString(key);
+        Assert.NotNull(value);
+        Assert.Equal(payload, value);
+    }
+
+    [Theory(Skip = SkipReason)]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("abc")]
+    [InlineData("abc def ghi jkl mno pqr stu vwx yz!")]
+    public async Task SetGetNonNullStringAsync(string payload)
+    {
+        var cache = RedisTestConfig.CreateCacheInstance(GetType().Name);
+        var key = Me();
+        await cache.RemoveAsync(key); // known state
+        Assert.Null(await cache.GetAsync(key)); // expect null
+        await cache.SetStringAsync(key, payload);
+
+        // check raw bytes
+        var raw = await cache.GetAsync(key);
+        Assert.Equal(Hex(payload), Hex(raw));
+
+        // check via string API
+        var value = await cache.GetStringAsync(key);
+        Assert.NotNull(value);
+        Assert.Equal(payload, value);
+    }
+
+    static string Hex(byte[] value) => BitConverter.ToString(value);
+    static string Hex(string value) => Hex(Encoding.UTF8.GetBytes(value));
 
     private static string Me([CallerMemberName] string caller = "") => caller;
 }
