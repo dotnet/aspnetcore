ce3d4583675f92fe670e6409893da290b2df421a
Andrew Casey <andrew.casey@microsoft.com>
2023-04-03 13:49:33 -0700

    Sanitize 0x7F in Content-Disposition filename
    We were using the ASCII boundaries, but it looks like we should have been using the ISO-8859 boundaries.
    
    Fixes #47027

--- src/Http/Headers/src/ContentDispositionHeaderValue.cs
+++ src/Http/Headers/src/ContentDispositionHeaderValue.cs
@@ -509,7 +509,7 @@ public class ContentDispositionHeaderValue
             for (int i = 0; i < result.Length; i++)
             {
                 var c = result[i];
-                if ((int)c > 0x7f || (int)c < 0x20)
+                if ((int)c >= 0x7f || (int)c < 0x20)
                 {
                     c = '_'; // Replace out-of-range characters
                 }
@@ -530,12 +530,12 @@ public class ContentDispositionHeaderValue
             && value.EndsWith("\"", StringComparison.Ordinal);
     }
 
-    // tspecials are required to be in a quoted string.  Only non-ascii needs to be encoded.
+    // tspecials are required to be in a quoted string.  Only non-ascii and control characters need to be encoded.
     private static bool RequiresEncoding(StringSegment input)
     {
         Contract.Assert(input != null);
 
-        return input.AsSpan().IndexOfAnyExceptInRange((char)0x20, (char)0x7f) >= 0;
+        return input.AsSpan().IndexOfAnyExceptInRange((char)0x20, (char)0x7e) >= 0;
     }
 
     // Encode using MIME encoding
