2d93d36676065455fd81994a672c681cbe8b6ba2
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2023-04-05 18:28:02 +0100

    Add pathbase support to Photino WebView so we can run the router E2E tests there

--- src/Components/WebView/WebView/src/Services/WebViewScrollToLocationHash.cs
+++ src/Components/WebView/WebView/src/Services/WebViewScrollToLocationHash.cs
@@ -2,10 +2,46 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.JSInterop;
 
 namespace Microsoft.AspNetCore.Components.WebView.Services;
 
 internal sealed class WebViewScrollToLocationHash : IScrollToLocationHash
 {
-    public Task RefreshScrollPositionForHash(string locationAbsolute) => Task.CompletedTask;
+    private IJSRuntime _jsRuntime;
+
+    public void AttachJSRuntime(IJSRuntime jsRuntime)
+    {
+        if (HasAttachedJSRuntime)
+        {
+            throw new InvalidOperationException("JSRuntime has already been initialized.");
+        }
+
+        _jsRuntime = jsRuntime;
+    }
+
+    public bool HasAttachedJSRuntime => _jsRuntime != null;
+
+    public async Task RefreshScrollPositionForHash(string locationAbsolute)
+    {
+        //JSRuntime = (WebViewJSRuntime)ServiceProvider.GetRequiredService<IJSRuntime>();
+        if (!HasAttachedJSRuntime)
+        {
+            // We should generally never get here in the ordinary case. Router will only call this API once pre-rendering is complete.
+            // This would guard any unusual usage of this API.
+            throw new InvalidOperationException("Navigation commands can not be issued at this time. This is because the component is being " +
+                "prerendered and the page has not yet loaded in the browser or because the circuit is currently disconnected. " +
+                "Components must wrap any navigation calls in conditional logic to ensure those navigation calls are not " +
+                "attempted during prerendering or while the client is disconnected.");
+        }
+
+        var hashIndex = locationAbsolute.IndexOf("#", StringComparison.CurrentCultureIgnoreCase);
+
+        if (hashIndex > -1 && locationAbsolute.Length > hashIndex + 1)
+        {
+            var elementId = locationAbsolute[(hashIndex + 1)..];
+
+            await _jsRuntime.InvokeVoidAsync("Blazor._internal.navigationManager.scrollToElement", elementId).AsTask();
+        }
+    }
 }
