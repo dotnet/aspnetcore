3d22cba5e3971609f708d468db73fbe7458c9929
Tratcher <Tratcher@users.noreply.github.com>
2023-04-11 08:04:40 +0000

    Sync shared code from runtime

--- src/Shared/runtime/Http3/QPack/QPackEncodingException.cs
+++ src/Shared/runtime/Http3/QPack/QPackEncodingException.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.ComponentModel;
 using System.Runtime.Serialization;
 
 namespace System.Net.Http.QPack
@@ -17,6 +18,9 @@ namespace System.Net.Http.QPack
         {
         }
 
+#if NET8_0_OR_GREATER
+        [Obsolete(Obsoletions.LegacyFormatterImplMessage, DiagnosticId = Obsoletions.LegacyFormatterImplDiagId, UrlFormat = Obsoletions.SharedUrlFormat)]
+#endif
         private QPackEncodingException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
