bac4bf5c3ad866d1f888222cb082c03301b2d2a3
jacalvar <jacalvar@microsoft.com>
2023-04-12 16:26:51 +0200

    Add DebuggerDisplay to component state

--- src/Components/Components/src/Rendering/ComponentState.cs
+++ src/Components/Components/src/Rendering/ComponentState.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Components.RenderTree;
 
@@ -11,6 +12,7 @@ namespace Microsoft.AspNetCore.Components.Rendering;
 /// within the context of a <see cref="Renderer"/>. This is an internal implementation
 /// detail of <see cref="Renderer"/>.
 /// </summary>
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
 public class ComponentState : IDisposable
 {
     private readonly Renderer _renderer;
@@ -296,5 +298,10 @@ public class ComponentState : IDisposable
         {
             return Task.FromException(e);
         }
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return $"{ComponentId} - {Component.GetType().Name} - Disposed: {_componentWasDisposed}";
     }
 }
