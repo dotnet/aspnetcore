0c64eb5043c3961d0a49e0398e99fff59054ca73
Brennan <brecon@microsoft.com>
2023-04-19 14:43:18 -0700

    Fix RequestBody logging when zero byte read is used

--- src/Middleware/HttpLogging/src/RequestBufferingStream.cs
+++ src/Middleware/HttpLogging/src/RequestBufferingStream.cs
@@ -27,6 +27,12 @@ internal sealed class RequestBufferingStream : BufferingStream
     {
         var res = await _innerStream.ReadAsync(destination, cancellationToken);
 
+        // Zero-byte reads can occur when using PipeReader, we don't want to accidentally complete the RequestBody logging in this case
+        if (destination.IsEmpty)
+        {
+            return res;
+        }
+
         WriteToBuffer(destination.Slice(0, res).Span);
 
         return res;
@@ -35,6 +41,12 @@ internal sealed class RequestBufferingStream : BufferingStream
     public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
     {
         var res = await _innerStream.ReadAsync(buffer.AsMemory(offset, count), cancellationToken);
+
+        // Zero-byte reads can occur when using PipeReader, we don't want to accidentally complete the RequestBody logging in this case
+        if (count == 0)
+        {
+            return res;
+        }
 
         WriteToBuffer(buffer.AsSpan(offset, res));
 
