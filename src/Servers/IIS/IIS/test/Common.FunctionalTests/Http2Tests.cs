ef17e9b1b7bbc2ea1ce6aaabe6547b29694b331b
Mitch Denny <midenn@microsoft.com>
2023-04-21 00:01:11 +1000

    Disable some more functional tests.

--- src/Servers/IIS/IIS/test/Common.FunctionalTests/Http2Tests.cs
+++ src/Servers/IIS/IIS/test/Common.FunctionalTests/Http2Tests.cs
@@ -38,6 +38,7 @@ namespace Microsoft.AspNetCore.Server.IIS.FunctionalTests;
 /// with newer functionality.
 /// </summary>
 [Collection(IISHttpsTestSiteCollection.Name)]
+[SkipOnHelix("Unsupported queue", Queues = "Windows.Amd64.VS2022.Pre.Open;")]
 public class Http2Tests
 {
     public Http2Tests(IISTestSiteFixture fixture)
