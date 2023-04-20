234ad8ec6e841abf1e248ef1db7e67b16e0f6036
Sven Boemer <sbomer@gmail.com>
2023-04-20 21:36:56 +0000

    Fix image name

--- src/Testing/src/xunit/HelixConstants.cs
+++ src/Testing/src/xunit/HelixConstants.cs
@@ -8,5 +8,5 @@ public static class HelixConstants
     public const string Windows10Arm64 = "Windows.10.Arm64v8.Open;";
     public const string DebianAmd64 = "Debian.11.Amd64.Open;";
     public const string DebianArm64 = "Debian.11.Arm64.Open;";
-    public const string AlmaLinuxAmd64 = "(AlmaLinux.8.Amd64.Open)Ubuntu.1804.Amd64.Open@svencontainers.azurecr.io/ubuntu-22.04-arm64v8/almalinux;";
+    public const string AlmaLinuxAmd64 = "(AlmaLinux.8.Amd64.Open)Ubuntu.1804.Amd64.Open@svencontainers.azurecr.io/almalinux;";
 }
