67b8687ad0b37de6f92750bdf360aa350617f478
jacalvar <jacalvar@microsoft.com>
2023-04-20 22:52:45 +0200

    Move ModelBindingContext to Microsoft.AspNetCore.Components namespace

--- src/Components/Components/test/CascadingModelBinderTest.cs
+++ src/Components/Components/test/CascadingModelBinderTest.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Diagnostics.CodeAnalysis;
-using Microsoft.AspNetCore.Components.Binding;
 using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.AspNetCore.Components.Test.Helpers;
 using Microsoft.Extensions.DependencyInjection;
