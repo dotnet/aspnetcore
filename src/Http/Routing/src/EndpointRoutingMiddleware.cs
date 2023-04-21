1c63bf6beb2115c26060746d89c3e62fc801be03
James Newton-King <james@newtonking.com>
2023-04-21 08:42:18 +0800

    Write log message for matched fallback routes (#47798)
    Co-authored-by: Chris Ross <Tratcher@Outlook.com>

--- src/Http/Routing/src/EndpointRoutingMiddleware.cs
+++ src/Http/Routing/src/EndpointRoutingMiddleware.cs
@@ -109,6 +109,12 @@ internal sealed partial class EndpointRoutingMiddleware
 
             Log.MatchSuccess(_logger, endpoint);
 
+            if (_logger.IsEnabled(LogLevel.Debug)
+                && endpoint.Metadata.GetMetadata<FallbackMetadata>() is not null)
+            {
+                Log.FallbackMatch(_logger, endpoint);
+            }
+
             var shortCircuitMetadata = endpoint.Metadata.GetMetadata<ShortCircuitMetadata>();
             if (shortCircuitMetadata is not null)
             {
@@ -283,5 +289,8 @@ internal sealed partial class EndpointRoutingMiddleware
 
         [LoggerMessage(6, LogLevel.Information, "The endpoint '{EndpointName}' is being short circuited without running additional middleware or producing a response.", EventName = "ShortCircuitedEndpoint")]
         public static partial void ShortCircuitedEndpoint(ILogger logger, Endpoint endpointName);
+
+        [LoggerMessage(7, LogLevel.Debug, "Matched endpoint '{EndpointName}' is a fallback endpoint.", EventName = "FallbackMatch", SkipEnabledCheck = true)]
+        public static partial void FallbackMatch(ILogger logger, Endpoint endpointName);
     }
 }
