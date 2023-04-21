446e31df3e0f04a772e76f85c12d9e0534aaa655
Surayya Huseyn Zada <shuseynzada@microsoft.com>
2023-04-21 15:43:27 +0200

    remove unnecessary RoutingStateProvider

--- src/Components/test/testassets/Components.TestServer/RazorComponentEndpointsStartup.cs
+++ src/Components/test/testassets/Components.TestServer/RazorComponentEndpointsStartup.cs
@@ -20,7 +20,6 @@ public class RazorComponentEndpointsStartup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddScoped<RoutingStateProvider>();
         services.AddRazorComponents();
     }
 
