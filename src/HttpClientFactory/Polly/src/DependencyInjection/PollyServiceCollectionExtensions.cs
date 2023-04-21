f4c0777fdb73a9623655fb9c47f49d1978e8a6a3
Iustin Micu <iustin.micu@888holdings.com>
2023-04-21 10:34:01 +0300

    Added TryAddPolicyRegistry extension methods for IPolicyRegistry

--- src/HttpClientFactory/Polly/src/DependencyInjection/PollyServiceCollectionExtensions.cs
+++ src/HttpClientFactory/Polly/src/DependencyInjection/PollyServiceCollectionExtensions.cs
@@ -105,4 +105,101 @@ public static class PollyServiceCollectionExtensions
 
         return services;
     }
+
+    /// <summary>
+    /// Registers an empty <see cref="PolicyRegistry"/> in the service collection with service types
+    /// <see cref="IPolicyRegistry{String}"/>, <see cref="IReadOnlyPolicyRegistry{String}"/>, and
+    /// <see cref="IConcurrentPolicyRegistry{String}"/> if the service type hasn't already been registered
+    /// and returns the newly created registry.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+    /// <returns>The newly created <see cref="IPolicyRegistry{String}"/>.</returns>
+    public static IPolicyRegistry<string> TryAddPolicyRegistry(this IServiceCollection services)
+    {
+        if (services == null)
+        {
+            throw new ArgumentNullException(nameof(services));
+        }
+
+        // Create an empty registry, register and return it as an instance. This is the best way to get a
+        // single instance registered using all the interfaces.
+        var registry = new PolicyRegistry();
+
+        services.TryAddSingleton<IConcurrentPolicyRegistry<string>>(registry);
+        services.TryAddSingleton<IPolicyRegistry<string>>(registry);
+        services.TryAddSingleton<IReadOnlyPolicyRegistry<string>>(registry);
+
+        return registry;
+    }
+
+    /// <summary>
+    /// Registers the provided <see cref="IPolicyRegistry{String}"/> in the service collection with service types
+    /// <see cref="IPolicyRegistry{String}"/>, <see cref="IReadOnlyPolicyRegistry{String}"/>, and
+    /// <see cref="IConcurrentPolicyRegistry{String}"/> if the service type hasn't already been registered
+    /// and returns the provided registry.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+    /// <param name="registry">The <see cref="IPolicyRegistry{String}"/>.</param>
+    /// <returns>The provided <see cref="IPolicyRegistry{String}"/>.</returns>
+    public static IPolicyRegistry<string> TryAddPolicyRegistry(this IServiceCollection services, IPolicyRegistry<string> registry)
+    {
+        if (services == null)
+        {
+            throw new ArgumentNullException(nameof(services));
+        }
+
+        if (registry == null)
+        {
+            throw new ArgumentNullException(nameof(registry));
+        }
+
+        services.TryAddSingleton<IPolicyRegistry<string>>(registry);
+        services.TryAddSingleton<IReadOnlyPolicyRegistry<string>>(registry);
+
+        if (registry is IConcurrentPolicyRegistry<string> concurrentRegistry)
+        {
+            services.TryAddSingleton<IConcurrentPolicyRegistry<string>>(concurrentRegistry);
+        }
+
+        return registry;
+    }
+
+    /// <summary>
+    /// Registers an empty <see cref="PolicyRegistry"/> in the service collection with service types
+    /// <see cref="IPolicyRegistry{String}"/>, <see cref="IReadOnlyPolicyRegistry{String}"/>, and
+    /// <see cref="IConcurrentPolicyRegistry{String}"/> if the service type hasn't already been registered
+    /// and uses the specified delegate to configure it.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+    /// <param name="configureRegistry">A delegate that is used to configure an <see cref="IPolicyRegistry{String}"/>.</param>
+    /// <returns>The provided <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection TryAddPolicyRegistry(this IServiceCollection services, Action<IServiceProvider, IPolicyRegistry<string>> configureRegistry)
+    {
+        if (services == null)
+        {
+            throw new ArgumentNullException(nameof(services));
+        }
+
+        if (configureRegistry == null)
+        {
+            throw new ArgumentNullException(nameof(configureRegistry));
+        }
+
+        // Create an empty registry, configure it and register it as an instance.
+        // This is the best way to get a single instance registered using all the interfaces.
+        services.TryAddSingleton(serviceProvider =>
+        {
+            var registry = new PolicyRegistry();
+
+            configureRegistry(serviceProvider, registry);
+
+            return registry;
+        });
+
+        services.TryAddSingleton<IConcurrentPolicyRegistry<string>>(serviceProvider => serviceProvider.GetRequiredService<PolicyRegistry>());
+        services.TryAddSingleton<IPolicyRegistry<string>>(serviceProvider => serviceProvider.GetRequiredService<PolicyRegistry>());
+        services.TryAddSingleton<IReadOnlyPolicyRegistry<string>>(serviceProvider => serviceProvider.GetRequiredService<PolicyRegistry>());
+
+        return services;
+    }
 }
