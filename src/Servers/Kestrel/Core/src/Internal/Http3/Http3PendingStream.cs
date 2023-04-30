e2ade1e44cb52ac4a305295be7bc1ad9e773b1a0
Keith Dahlby <dahlbyk@gmail.com>
2023-04-30 00:21:36 -0500

    fixup! Avoid System.Exception in Kestrel
    Co-authored-by: Chris Ross <Tratcher@Outlook.com>

--- src/Servers/Kestrel/Core/src/Internal/Http3/Http3PendingStream.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http3/Http3PendingStream.cs
@@ -51,7 +51,7 @@ internal sealed class Http3PendingStream
 
                 if (result.IsCanceled)
                 {
-                    throw new IOException("The read operation was canceled.");
+                    throw new OperationCanceledException("The read operation was canceled.");
                 }
 
                 var readableBuffer = result.Buffer;
