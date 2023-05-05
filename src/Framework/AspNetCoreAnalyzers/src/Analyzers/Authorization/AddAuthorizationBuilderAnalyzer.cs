f3af8dc59b379661d82b07163579978348532a9f
David Acker <davidacker3@gmail.com>
2023-05-05 19:10:42 -0400

    Refactor pattern checks into separate methods

--- src/Framework/AspNetCoreAnalyzers/src/Analyzers/Authorization/AddAuthorizationBuilderAnalyzer.cs
+++ src/Framework/AspNetCoreAnalyzers/src/Analyzers/Authorization/AddAuthorizationBuilderAnalyzer.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Microsoft.AspNetCore.App.Analyzers.Infrastructure;
 using Microsoft.CodeAnalysis;
@@ -66,20 +67,74 @@ public sealed class AddAuthorizationBuilderAnalyzer : DiagnosticAnalyzer
 
     private static bool IsCompatibleWithAuthorizationBuilder(IInvocationOperation invocation, AuthorizationOptionsTypes authorizationOptionsTypes)
     {
-        if (invocation is { Arguments: { Length: 2 } invocationArguments }
-            && invocationArguments[1] is IArgumentOperation { ChildOperations: { Count: 1 } argumentOperations }
-            && argumentOperations.First() is IDelegateCreationOperation { ChildOperations: { Count: 1 } delegateCreationOperations }
-            && delegateCreationOperations.First() is IAnonymousFunctionOperation { ChildOperations: { Count: 1 } anonymousFunctionOperations }
-            && anonymousFunctionOperations.First() is IBlockOperation blockOperation)
+        if (TryGetConfigureArgumentOperation(invocation, out var configureArgumentOperation)
+            && TryGetConfigureDelegateCreationOperation(configureArgumentOperation, out var configureDelegateCreationOperation)
+            && TryGetConfigureAnonymousFunctionOperation(configureDelegateCreationOperation, out var configureAnonymousFunctionOperation)
+            && TryGetConfigureBlockOperation(configureAnonymousFunctionOperation, out var configureBlockOperation))
         {
             // Ensure that the child operations of the configuration action passed to AddAuthorization are all related to AuthorizationOptions.
-            var allOperationsInvolveAuthorizationOptions = blockOperation.ChildOperations
+            var allOperationsInvolveAuthorizationOptions = configureBlockOperation.ChildOperations
                 .Where(operation => operation is not IReturnOperation { IsImplicit: true })
                 .All(operation => DoesOperationInvolveAuthorizationOptions(operation, authorizationOptionsTypes));
 
             return allOperationsInvolveAuthorizationOptions
                 // Ensure that the configuration action passed to AddAuthorization does not use any AuthorizationOptions-specific APIs.
-                && IsConfigureActionCompatibleWithAuthorizationBuilder(blockOperation, authorizationOptionsTypes);
+                && IsConfigureActionCompatibleWithAuthorizationBuilder(configureBlockOperation, authorizationOptionsTypes);
+        }
+
+        return false;
+    }
+
+    private static bool TryGetConfigureArgumentOperation(IInvocationOperation invocation, [NotNullWhen(true)] out IArgumentOperation? configureArgumentOperation)
+    {
+        configureArgumentOperation = null;
+
+        if (invocation is { Arguments: { Length: 2 } invocationArguments })
+        {
+            configureArgumentOperation = invocationArguments[1];
+            return true;
+        }
+
+        return false;
+    }
+
+    private static bool TryGetConfigureDelegateCreationOperation(IArgumentOperation configureArgumentOperation, [NotNullWhen(true)] out IDelegateCreationOperation? configureDelegateCreationOperation)
+    {
+        configureDelegateCreationOperation = null;
+
+        if (configureArgumentOperation is { ChildOperations: { Count: 1 } argumentChildOperations }
+            && argumentChildOperations.First() is IDelegateCreationOperation delegateCreationOperation)
+        {
+            configureDelegateCreationOperation = delegateCreationOperation;
+            return true;
+        }
+
+        return false;
+    }
+
+    private static bool TryGetConfigureAnonymousFunctionOperation(IDelegateCreationOperation configureDelegateCreationOperation, [NotNullWhen(true)] out IAnonymousFunctionOperation? configureAnonymousFunctionOperation)
+    {
+        configureAnonymousFunctionOperation = null;
+
+        if (configureDelegateCreationOperation is { ChildOperations: { Count: 1 } delegateCreationChildOperations }
+            && delegateCreationChildOperations.First() is IAnonymousFunctionOperation anonymousFunctionOperation)
+        {
+            configureAnonymousFunctionOperation = anonymousFunctionOperation;
+            return true;
+        }
+
+        return false;
+    }
+
+    private static bool TryGetConfigureBlockOperation(IAnonymousFunctionOperation configureAnonymousFunctionOperation, [NotNullWhen(true)] out IBlockOperation? configureBlockOperation)
+    {
+        configureBlockOperation = null;
+
+        if (configureAnonymousFunctionOperation is { ChildOperations: { Count: 1 } anonymousFunctionChildOperations }
+            && anonymousFunctionChildOperations.First() is IBlockOperation blockOperation)
+        {
+            configureBlockOperation = blockOperation;
+            return true;
         }
 
         return false;
