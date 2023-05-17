edf3ee6925c7de18e73789a146796a8fa77377fa
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2023-05-17 15:23:59 +0100

    Clean up

--- src/Components/Web.JS/src/Rendering/DomMerging/EditDistance.ts
+++ src/Components/Web.JS/src/Rendering/DomMerging/EditDistance.ts
@@ -1,9 +1,12 @@
-type EqualityCallback<T> = (a: T, b: T) => boolean;
+export function compareArrays<T>(before: T[], after: T[], equals: (a: T, b: T) => boolean): Operation[] {
+  const operations = computeOperations(before, after, equals);
+  return toEditScript(operations);
+}
 
-export function compareArrays<T>(before: T[], after: T[], equalityCallback: EqualityCallback<T>): Operation[] {
+function computeOperations<T>(before: T[], after: T[], equals: (a: T, b: T) => boolean): Operation[][] {
   // Initialize matrices
-  const matrix: number[][] = [];
-  const chosenOps: Operation[][] = [];
+  const costs: number[][] = [];
+  const operations: Operation[][] = [];
   const beforeLength = before.length;
   const afterLength = after.length;
   if (before.length === 0 && afterLength === 0) {
@@ -11,50 +14,52 @@ export function compareArrays<T>(before: T[], after: T[], equalityCallback: Equa
   }
 
   for (let beforeIndex = 0; beforeIndex <= beforeLength; beforeIndex++) {
-    (matrix[beforeIndex] = Array(afterLength + 1))[0] = beforeIndex;
-    chosenOps[beforeIndex] = Array(afterLength + 1);
+    (costs[beforeIndex] = Array(afterLength + 1))[0] = beforeIndex;
+    operations[beforeIndex] = Array(afterLength + 1);
   }
-  const rowZero = matrix[0];
+  const rowZero = costs[0];
   for (let afterIndex = 1; afterIndex <= afterLength; afterIndex++) {
     rowZero[afterIndex] = afterIndex;
   }
 
   for (let beforeIndex = 1; beforeIndex <= before.length; beforeIndex++) {
     for (let afterIndex = 1; afterIndex <= after.length; afterIndex++) {
-      const beforeItem = before[beforeIndex - 1];
-      const afterItem = after[afterIndex - 1];
-      const isEqual = equalityCallback(beforeItem, afterItem);
-      const costAsDelete = matrix[beforeIndex - 1][afterIndex] + 1;
-      const costAsInsert = matrix[beforeIndex][afterIndex - 1] + 1;
-      const costAsRetain = isEqual ? matrix[beforeIndex - 1][afterIndex - 1] : Number.MAX_VALUE;
+      const isEqual = equals(before[beforeIndex - 1], after[afterIndex - 1]);
+      const costAsDelete = costs[beforeIndex - 1][afterIndex] + 1;
+      const costAsInsert = costs[beforeIndex][afterIndex - 1] + 1;
+      const costAsRetain = isEqual ? costs[beforeIndex - 1][afterIndex - 1] : Number.MAX_VALUE;
 
       if (costAsRetain < costAsInsert && costAsRetain < costAsDelete) {
-        matrix[beforeIndex][afterIndex] = costAsRetain;
-        chosenOps[beforeIndex][afterIndex] = Operation.Retain;
+        costs[beforeIndex][afterIndex] = costAsRetain;
+        operations[beforeIndex][afterIndex] = Operation.Retain;
       } else if (costAsInsert < costAsDelete) {
-        matrix[beforeIndex][afterIndex] = costAsInsert;
-        chosenOps[beforeIndex][afterIndex] = Operation.Insert;
+        costs[beforeIndex][afterIndex] = costAsInsert;
+        operations[beforeIndex][afterIndex] = Operation.Insert;
       } else {
-        matrix[beforeIndex][afterIndex] = costAsDelete;
-        chosenOps[beforeIndex][afterIndex] = Operation.Delete;
+        costs[beforeIndex][afterIndex] = costAsDelete;
+        operations[beforeIndex][afterIndex] = Operation.Delete;
       }
     }
   }
 
-  const operations: Operation[] = [];
-  let beforeIndex = before.length;
-  let afterIndex = after.length;
+  return operations;
+}
 
+function toEditScript(operations: Operation[][]) {
+  // Start in the bottom-right corner, and work backwards
+  const result: Operation[] = [];
+  let beforeIndex = operations.length - 1;
+  let afterIndex = operations[beforeIndex]?.length - 1;
   while (beforeIndex > 0 || afterIndex > 0) {
-    const thisOp = beforeIndex === 0
+    const operation = beforeIndex === 0
       ? Operation.Insert
       : afterIndex === 0
         ? Operation.Delete
-        : chosenOps[beforeIndex][afterIndex];
+        : operations[beforeIndex][afterIndex];
 
-    operations.unshift(thisOp);
+    result.unshift(operation);
 
-    switch (thisOp) {
+    switch (operation) {
       case Operation.Retain:
         beforeIndex--;
         afterIndex--;
@@ -68,11 +73,11 @@ export function compareArrays<T>(before: T[], after: T[], equalityCallback: Equa
     }
   }
 
-  return operations;
+  return result;
 }
 
 export enum Operation {
-  Retain = 0,
-  Insert = 1,
-  Delete = 2,
+  Retain = 1,
+  Insert = 2,
+  Delete = 3,
 }
