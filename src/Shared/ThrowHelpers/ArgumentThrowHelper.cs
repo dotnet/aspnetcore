7bafcea726666d5230435e85f1b1f280ffe55ec7
Safia Abdalla <safia@safia.rocks>
2023-05-18 15:13:14 -0700

    Address feedback from review

--- src/Shared/ThrowHelpers/ArgumentThrowHelper.cs
+++ src/Shared/ThrowHelpers/ArgumentThrowHelper.cs
@@ -11,17 +11,13 @@ namespace Microsoft.AspNetCore.Shared;
 
 internal static partial class ArgumentThrowHelper
 {
-#if !NET7_0_OR_GREATER || NETSTANDARD ||NETFRAMEWORK
-    private const string EmptyString = "";
-#endif
-
     /// <summary>Throws an <see cref="ArgumentNullException"/> if <paramref name="argument"/> is null or an <see cref="ArgumentException"/> if it is empty.</summary>
     /// <param name="argument">The reference type argument to validate as neither null nor empty.</param>
     /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
     public static void ThrowIfNullOrEmpty([NotNull] string? argument, [CallerArgumentExpression("argument")] string? paramName = null)
     {
-#if !NET7_0_OR_GREATER || NETSTANDARD ||NETFRAMEWORK
-        if (argument is null or EmptyString)
+#if !NET7_0_OR_GREATER || NETSTANDARD || NETFRAMEWORK
+        if (argument is null or string.Empty)
         {
             ArgumentNullThrowHelper.ThrowIfNull(argument);
             Throw(paramName);
@@ -31,7 +27,7 @@ internal static partial class ArgumentThrowHelper
 #endif
     }
 
-#if !NET7_0_OR_GREATER || NETSTANDARD ||NETFRAMEWORK
+#if !NET7_0_OR_GREATER || NETSTANDARD || NETFRAMEWORK
     [DoesNotReturn]
     internal static void Throw(string? paramName) =>
         throw new ArgumentException("The value cannot be an empty string.", paramName);
