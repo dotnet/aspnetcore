c85160858e2e809a920e88c9e48b47e4e1e76871
Andrew Casey <andrew.casey@microsoft.com>
2023-05-18 14:07:09 -0700

    Use negative exit codes in error scenarios

--- src/DefaultBuilder/test/Microsoft.AspNetCore.TrimmingTests/SlimBuilderDoesNotDependOnX509Test.cs
+++ src/DefaultBuilder/test/Microsoft.AspNetCore.TrimmingTests/SlimBuilderDoesNotDependOnX509Test.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateSlimBuilder(args);
 var app = builder.Build();
 
 if (X509Utilities.HasCertificateType) {
-    return 1;
+    return -1;
 }
 
 return 100; // Success
