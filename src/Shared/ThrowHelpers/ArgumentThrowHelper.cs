ff9d6bcb0599a1f894a065daf888533305be754b
Safia Abdalla <safia@safia.rocks>
2023-05-18 15:53:41 -0700

    Fix nullability attributes in shared helpers

--- src/Shared/ThrowHelpers/ArgumentThrowHelper.cs
+++ src/Shared/ThrowHelpers/ArgumentThrowHelper.cs
@@ -14,10 +14,14 @@ internal static partial class ArgumentThrowHelper
     /// <summary>Throws an <see cref="ArgumentNullException"/> if <paramref name="argument"/> is null or an <see cref="ArgumentException"/> if it is empty.</summary>
     /// <param name="argument">The reference type argument to validate as neither null nor empty.</param>
     /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
-    public static void ThrowIfNullOrEmpty([NotNull] string? argument, [CallerArgumentExpression("argument")] string? paramName = null)
+    public static void ThrowIfNullOrEmpty(
+#if INTERNAL_NULLABLE_ATTRIBUTES || NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
+        [NotNull]
+#endif
+        string? argument, [CallerArgumentExpression("argument")] string? paramName = null)
     {
 #if !NET7_0_OR_GREATER || NETSTANDARD || NETFRAMEWORK
-        if (argument is null or string.Empty)
+        if (string.IsNullOrEmpty(argument))
         {
             ArgumentNullThrowHelper.ThrowIfNull(argument);
             Throw(paramName);
@@ -28,7 +32,9 @@ internal static partial class ArgumentThrowHelper
     }
 
 #if !NET7_0_OR_GREATER || NETSTANDARD || NETFRAMEWORK
+#if INTERNAL_NULLABLE_ATTRIBUTES || NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
     [DoesNotReturn]
+#endif
     internal static void Throw(string? paramName) =>
         throw new ArgumentException("The value cannot be an empty string.", paramName);
 #endif
