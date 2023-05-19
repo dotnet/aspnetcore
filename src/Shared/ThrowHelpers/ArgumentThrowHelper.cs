6f2af0276901d238a0cc34518f215f7d66df834e
Safia Abdalla <safia@safia.rocks>
2023-05-18 17:12:47 -0700

    Update check again

--- src/Shared/ThrowHelpers/ArgumentThrowHelper.cs
+++ src/Shared/ThrowHelpers/ArgumentThrowHelper.cs
@@ -11,10 +11,6 @@ namespace Microsoft.AspNetCore.Shared;
 
 internal static partial class ArgumentThrowHelper
 {
-#if !NET7_0_OR_GREATER || NETSTANDARD || NETFRAMEWORK
-    private const string EmptyString = "";
-#endif
-
     /// <summary>Throws an <see cref="ArgumentNullException"/> if <paramref name="argument"/> is null or an <see cref="ArgumentException"/> if it is empty.</summary>
     /// <param name="argument">The reference type argument to validate as neither null nor empty.</param>
     /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
@@ -25,7 +21,7 @@ internal static partial class ArgumentThrowHelper
         string? argument, [CallerArgumentExpression("argument")] string? paramName = null)
     {
 #if !NET7_0_OR_GREATER || NETSTANDARD || NETFRAMEWORK
-        if (argument is null or EmptyString)
+        if (argument is null || argument == string.Empty)
         {
             ArgumentNullThrowHelper.ThrowIfNull(argument);
             Throw(paramName);
