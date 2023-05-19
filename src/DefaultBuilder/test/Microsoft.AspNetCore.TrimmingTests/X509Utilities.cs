14f973e5cbf70fb793f66f5d6045dd2a988a0058
Andrew Casey <andrew.casey@microsoft.com>
2023-05-19 14:14:55 -0700

    Revert "Simplify type lookup"
    This reverts commit d118f30153302e03a309c18da2a64cdd433a2826.

--- src/DefaultBuilder/test/Microsoft.AspNetCore.TrimmingTests/X509Utilities.cs
+++ src/DefaultBuilder/test/Microsoft.AspNetCore.TrimmingTests/X509Utilities.cs
@@ -14,9 +14,10 @@ public static class X509Utilities
     public static bool HasCertificateType =>
         GetType("System.Security.Cryptography", "System.Security.Cryptography.X509Certificates.X509Certificate") is not null;
 
-    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2057:UnrecognizedReflectionPattern",
+    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026:RequiresUnreferencedCode",
         Justification = "Returning null when the type is unreferenced is desirable")]
     private static Type? GetType(string assemblyName, string typeName) {
-        return Type.GetType($"{typeName}, {assemblyName}");
+        var assembly = AppDomain.CurrentDomain.GetAssemblies().SingleOrDefault(a => a.GetName().Name == assemblyName);
+        return assembly?.GetType(typeName);
     }
 }
