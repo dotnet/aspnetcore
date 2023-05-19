a9634441f47a8c321bcf20b25e9f0d36d32a5c5e
Safia Abdalla <safia@safia.rocks>
2023-05-18 17:01:28 -0700

    Use EmptyString again for checks

--- src/Shared/ThrowHelpers/ArgumentThrowHelper.cs
+++ src/Shared/ThrowHelpers/ArgumentThrowHelper.cs
@@ -11,6 +11,10 @@ namespace Microsoft.AspNetCore.Shared;
 
 internal static partial class ArgumentThrowHelper
 {
+#if !NET7_0_OR_GREATER || NETSTANDARD || NETFRAMEWORK
+    private const string EmptyString = "";
+#endif
+
     /// <summary>Throws an <see cref="ArgumentNullException"/> if <paramref name="argument"/> is null or an <see cref="ArgumentException"/> if it is empty.</summary>
     /// <param name="argument">The reference type argument to validate as neither null nor empty.</param>
     /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
@@ -21,7 +25,7 @@ internal static partial class ArgumentThrowHelper
         string? argument, [CallerArgumentExpression("argument")] string? paramName = null)
     {
 #if !NET7_0_OR_GREATER || NETSTANDARD || NETFRAMEWORK
-        if (argument is null or argument == string.Empty)
+        if (argument is null or EmptyString)
         {
             ArgumentNullThrowHelper.ThrowIfNull(argument);
             Throw(paramName);
