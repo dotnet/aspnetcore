05e77a0725d3de38d2e205e6900f0e8f18c0f8e3
wtgodbe <wigodbe@microsoft.com>
2023-05-22 16:12:11 -0700

    Headers

--- src/Installers/Debian/tools/scripts/manpage_generator.py
+++ src/Installers/Debian/tools/scripts/manpage_generator.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #
 # Copyright (c) .NET Foundation and contributors. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
