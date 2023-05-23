ce0339061f17b83dbe8da0af389b2dc88e2dfe3a
Sebastien Ros <sebastienros@gmail.com>
2023-05-23 16:58:33 -0700

    Update unit tests

--- src/ObjectPool/test/DependencyInjectionExtensionsTest.cs
+++ src/ObjectPool/test/DependencyInjectionExtensionsTest.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.ObjectPool;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.ObjectPool.TestResources;
 using Xunit;
+using System.Linq;
 
 namespace Microsoft.Extensions.ObjectPool;
 
@@ -21,10 +22,11 @@ public class DependencyInjectionExtensionsTest
             .Configure<PoolOptions>(typeof(TestClass).FullName, options => options.Capacity = 2048)
             .Configure<PoolOptions>(typeof(TestDependency).FullName, options => options.Capacity = 4096)
             ;
-        using var provider = services.BuildServiceProvider();
+        using var provider = services.BuildServiceProvider(validateScopes: true);
 
         var sut = provider.GetRequiredService<IOptionsMonitor<PoolOptions>>();
 
+        Assert.Equal(1024, sut.Get(typeof(object).FullName!).Capacity);
         Assert.Equal(2048, sut.Get(typeof(TestClass).FullName!).Capacity);
         Assert.Equal(4096, sut.Get(typeof(TestDependency).FullName!).Capacity);
     }
@@ -34,8 +36,8 @@ public class DependencyInjectionExtensionsTest
     {
         var services = new ServiceCollection().AddPool<TestDependency>();
 
-        var sut = services.BuildServiceProvider().GetService<ObjectPool<TestDependency>>();
-        using var provider = services.BuildServiceProvider();
+        var sut = services.BuildServiceProvider(validateScopes: true).GetService<ObjectPool<TestDependency>>();
+        using var provider = services.BuildServiceProvider(validateScopes: true);
         var optionsMonitor = provider.GetRequiredService<IOptionsMonitor<PoolOptions>>();
 
         Assert.NotNull(sut);
@@ -62,7 +64,7 @@ public class DependencyInjectionExtensionsTest
             .AddSingleton<TestDependency>()
             .AddPool<ITestClass, TestClass>();
 
-        using var provider = services.BuildServiceProvider();
+        using var provider = services.BuildServiceProvider(validateScopes: true);
         var sut = provider.GetService<ObjectPool<ITestClass>>();
         var optionsMonitor = provider.GetRequiredService<IOptionsMonitor<PoolOptions>>();
 
@@ -78,7 +80,7 @@ public class DependencyInjectionExtensionsTest
             .AddSingleton<TestDependency>()
             .AddPool<ITestClass, TestClass>(options => options.Capacity = 64);
 
-        using var provider = services.BuildServiceProvider();
+        using var provider = services.BuildServiceProvider(validateScopes: true);
         var sut = provider.GetService<ObjectPool<ITestClass>>();
         var optionsMonitor = provider.GetRequiredService<IOptionsMonitor<PoolOptions>>();
 
@@ -94,35 +96,62 @@ public class DependencyInjectionExtensionsTest
             .AddSingleton<TestDependency>()
             .AddPool<ITestClass, TestClass>();
 
-        using var provider = services.BuildServiceProvider();
+        using var provider = services.BuildServiceProvider(validateScopes: true);
         var sut = provider.GetRequiredService<ObjectPool<ITestClass>>();
 
         var pooled = sut.Get();
         sut.Return(pooled);
 
         Assert.Equal(1, pooled.ResetCalled);
+        Assert.Equal(0, pooled.DisposedCalled);
     }
 
     [Fact]
-    public void ResolvePooledInstanceDirectly()
+    public void AddPool_CapacityReached_CreatesNew()
     {
+        var capacity = 64;
+
         var services = new ServiceCollection()
             .AddSingleton<TestDependency>()
-            .AddPool<ITestClass, TestClass>()
-            .AddScoped(provider => provider.GetRequiredService<ObjectPool<ITestClass>>().Get())
-            ;
+            .AddPool<ITestClass, TestClass>(options => options.Capacity = capacity);
 
-        using var provider = services.BuildServiceProvider();
+        using var provider = services.BuildServiceProvider(validateScopes: true);
+        var sut = provider.GetRequiredService<ObjectPool<ITestClass>>();
 
-        ITestClass resolved;
+        var instances = new ITestClass[capacity];
 
-        using (var scope = provider.CreateScope())
+        for (var i = 0; i< capacity; i++)
         {
-            resolved = scope.ServiceProvider.GetRequiredService<ITestClass>();
+            instances[i] = sut.Get();
         }
 
-        Assert.NotNull(resolved);
-        Assert.Equal(1, resolved.DisposedCalled);
+        for (var i = 0; i < capacity; i++)
+        {
+            sut.Return(instances[i]);
+        }
+
+        for (var i = 0; i < capacity; i++)
+        {
+            var pooled = sut.Get();
+            Assert.Contains(pooled, instances);
+        }
+
+        var newPooled = sut.Get();
+        Assert.DoesNotContain(newPooled, instances);
+    }
+
+    [Fact]
+    public void AddPool_DependsOnScoped_Invalid()
+    {
+        // Pooled classes are resolved using the root service provider,
+        // hence they should not be able to have scoped dependencies
+
+        var services = new ServiceCollection()
+            .AddScoped<TestDependency>()
+            .AddPool<ITestClass, TestClass>();
+
+        using var provider = services.BuildServiceProvider(validateScopes: true);
+        Assert.Throws<InvalidOperationException>(() => provider.GetRequiredService<ObjectPool<ITestClass>>().Get());
     }
 
     [Fact]
@@ -135,7 +164,7 @@ public class DependencyInjectionExtensionsTest
             .AddScoped<ITestClass>(provider => provider.GetRequiredService<Pooled<TestClass>>().Object)
             ;
 
-        using var provider = services.BuildServiceProvider();
+        using var provider = services.BuildServiceProvider(validateScopes: true);
 
         ITestClass resolved1, resolved2;
 
@@ -152,13 +181,70 @@ public class DependencyInjectionExtensionsTest
         Assert.NotNull(resolved2);
         Assert.Same(resolved1, resolved2);
 
+        Assert.Equal(1, resolved1.DisposedCalled); // Pooled instances are disposed by the scope when it is disposed
+        Assert.Equal(1, resolved1.ResetCalled);
+    }
+
+    [Fact]
+    public void PooledHelperReturnsTransientInstances_DifferentScopes()
+    {
+        // Resolving the same class from two different scopes should return two
+        // distinct instances.
+
+        var services = new ServiceCollection()
+            .AddSingleton<TestDependency>()
+            .AddPool<TestClass>()
+            .AddTransient<Pooled<TestClass>>()
+            .AddTransient<ITestClass>(provider => provider.GetRequiredService<Pooled<TestClass>>().Object)
+            ;
+
+        using var provider = services.BuildServiceProvider(validateScopes: true);
+
+        ITestClass resolved1, resolved2, resolved3, resolved4;
+
+        var scope1 = provider.CreateScope();
+        resolved1 = scope1.ServiceProvider.GetRequiredService<ITestClass>();
+        resolved2 = scope1.ServiceProvider.GetRequiredService<ITestClass>();
+
+        var scope2 = provider.CreateScope();
+        resolved3 = scope2.ServiceProvider.GetRequiredService<ITestClass>();
+        resolved4 = scope2.ServiceProvider.GetRequiredService<ITestClass>();
+
+        scope1.Dispose();
+        scope2.Dispose();
+
+        Assert.NotNull(resolved1);
+        Assert.NotNull(resolved2);
+        Assert.NotNull(resolved3);
+        Assert.NotNull(resolved4);
+
+        // Not the same instances as they are transient from a DI perspective so
+        // should each call .Object on a new Pooled<TestClass> instance
+
+        Assert.NotSame(resolved1, resolved2);
+        Assert.NotSame(resolved3, resolved4);
+
+        // All pooled objects should be returned and disposed once
+
         Assert.Equal(1, resolved1.DisposedCalled);
         Assert.Equal(1, resolved1.ResetCalled);
+
+        Assert.Equal(1, resolved2.DisposedCalled);
+        Assert.Equal(1, resolved2.ResetCalled);
+
+        Assert.Equal(1, resolved3.DisposedCalled);
+        Assert.Equal(1, resolved3.ResetCalled);
+
+        Assert.Equal(1, resolved4.DisposedCalled);
+        Assert.Equal(1, resolved4.ResetCalled);
     }
 
     [Fact]
     public void PooledHelperReturnsScopedInstances_DifferentScopes()
     {
+        // Resolving the same class from two different scopes should return two
+        // distinct instances.
+
         var services = new ServiceCollection()
             .AddSingleton<TestDependency>()
             .AddPool<TestClass>()
@@ -166,27 +252,39 @@ public class DependencyInjectionExtensionsTest
             .AddScoped<ITestClass>(provider => provider.GetRequiredService<Pooled<TestClass>>().Object)
             ;
 
-        using var provider = services.BuildServiceProvider();
+        using var provider = services.BuildServiceProvider(validateScopes: true);
 
-        ITestClass resolved1, resolved2;
+        ITestClass resolved1, resolved2, resolved3, resolved4;
 
-        using (var scope = provider.CreateScope())
-        {
-            resolved1 = scope.ServiceProvider.GetRequiredService<ITestClass>();
-        }
+        var scope1 = provider.CreateScope();
+        resolved1 = scope1.ServiceProvider.GetRequiredService<ITestClass>();
+        resolved2 = scope1.ServiceProvider.GetRequiredService<ITestClass>();
 
-        // The object should be returned and recalled in the next scope
+        var scope2 = provider.CreateScope();
+        resolved3 = scope2.ServiceProvider.GetRequiredService<ITestClass>();
+        resolved4 = scope2.ServiceProvider.GetRequiredService<ITestClass>();
 
-        using (var scope = provider.CreateScope())
-        {
-            resolved2 = scope.ServiceProvider.GetRequiredService<ITestClass>();
-        }
+        scope1.Dispose();
+        scope2.Dispose();
 
         Assert.NotNull(resolved1);
         Assert.NotNull(resolved2);
-        Assert.Same(resolved1, resolved2);
+        Assert.NotNull(resolved3);
+        Assert.NotNull(resolved4);
 
-        Assert.Equal(2, resolved1.DisposedCalled);
-        Assert.Equal(2, resolved1.ResetCalled);
+        // Same instances when resolved from the same scope
+        // Different scopes get different pooled instances
+
+        Assert.Same(resolved1, resolved2);
+        Assert.Same(resolved3, resolved4);
+        Assert.NotSame(resolved1, resolved3);
+
+        // All pooled objects should be returned and disposed once
+
+        Assert.Equal(1, resolved1.DisposedCalled);
+        Assert.Equal(1, resolved1.ResetCalled);
+
+        Assert.Equal(1, resolved3.DisposedCalled);
+        Assert.Equal(1, resolved3.ResetCalled);
     }
 }
