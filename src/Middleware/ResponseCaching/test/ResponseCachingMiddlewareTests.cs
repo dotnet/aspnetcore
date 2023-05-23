72d7d41db1e4b9fd1a7e844d28cb8f13ea967f40
Chris Ross <Tratcher@Outlook.com>
2023-05-23 09:30:00 -0700

    Update ResponseCachingMiddlewareTests.cs
    Fix rounding

--- src/Middleware/ResponseCaching/test/ResponseCachingMiddlewareTests.cs
+++ src/Middleware/ResponseCaching/test/ResponseCachingMiddlewareTests.cs
@@ -446,7 +446,7 @@ public class ResponseCachingMiddlewareTests
     {
         var timeProvider = new MockTimeProvider();
         var now = timeProvider.GetUtcNow();
-        now = new DateTimeOffset(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second + 1, now.Offset); // Round up to seconds.
+        now = new DateTimeOffset(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second, now.Offset).AddSeconds(1); // Round up to seconds.
         timeProvider.AdvanceTo(now);
         var sink = new TestSink();
         var middleware = TestUtils.CreateTestMiddleware(testSink: sink, options: new ResponseCachingOptions
