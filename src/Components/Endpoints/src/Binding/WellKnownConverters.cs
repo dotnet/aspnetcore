0de6ad60dd401b8a8a4b90ade8c86a3a9ca6c0a3
jacalvar <jacalvar@microsoft.com>
2023-05-25 20:02:07 +0200

    Fix warning

--- src/Components/Endpoints/src/Binding/WellKnownConverters.cs
+++ src/Components/Endpoints/src/Binding/WellKnownConverters.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 namespace Microsoft.AspNetCore.Components.Endpoints.Binding;
@@ -7,7 +7,9 @@ internal static class WellKnownConverters
 {
     public static readonly IReadOnlyDictionary<Type, FormDataConverter> Converters;
 
+#pragma warning disable CA1810 // Initialize reference type static fields inline
     static WellKnownConverters()
+#pragma warning restore CA1810 // Initialize reference type static fields inline
     {
         var converters = new Dictionary<Type, FormDataConverter>
         {
