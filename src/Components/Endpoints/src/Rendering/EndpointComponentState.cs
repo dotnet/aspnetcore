c694a57b291c5a2bd718a1653b2d4b0be087e935
Surayya Huseyn Zada <shuseynzada@microsoft.com>
2023-05-30 18:35:12 +0200

    remove using

--- src/Components/Endpoints/src/Rendering/EndpointComponentState.cs
+++ src/Components/Endpoints/src/Rendering/EndpointComponentState.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Concurrent;
-using System.ComponentModel;
 using System.Reflection;
 using System.Reflection.Metadata;
 using Microsoft.AspNetCore.Components.Endpoints;
