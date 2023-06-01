cc182c5edc928c7c8f9c502abb3715902daae913
jacalvar <jacalvar@microsoft.com>
2023-06-01 15:43:19 +0200

    Fix build

--- src/Components/Endpoints/test/Binding/FormDataMapperTests.cs
+++ src/Components/Endpoints/test/Binding/FormDataMapperTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Buffers;
+using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Immutable;
 using System.Collections.ObjectModel;
