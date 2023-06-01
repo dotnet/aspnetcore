d07b7f3fd8cb2e7d87fcdc162650c671f24aff71
jacalvar <jacalvar@microsoft.com>
2023-06-01 19:23:43 +0200

    Proper dictionary processing

--- src/Components/Endpoints/src/Binding/FormDataReader.cs
+++ src/Components/Endpoints/src/Binding/FormDataReader.cs
@@ -12,6 +12,8 @@ internal struct FormDataReader
     private readonly IReadOnlyDictionary<string, StringValues> _formCollection;
     private string _prefix;
 
+    private IReadOnlyDictionary<string, HashSet<string>>? _formDictionaryKeysByPrefix;
+
     public FormDataReader(IReadOnlyDictionary<string, StringValues> formCollection, CultureInfo culture)
     {
         _formCollection = formCollection;
@@ -23,7 +25,58 @@ internal struct FormDataReader
 
     internal IEnumerable<string> GetKeys()
     {
+        if (_formDictionaryKeysByPrefix == null)
+        {
+            _formDictionaryKeysByPrefix = ProcessFormKeys();
+        }
+
+        if (_formDictionaryKeysByPrefix.TryGetValue(_prefix, out var foundKeys))
+        {
+            return foundKeys;
+        }
+
         return _formCollection.Keys;
+    }
+
+    private IReadOnlyDictionary<string, HashSet<string>> ProcessFormKeys()
+    {
+        var keys = _formCollection.Keys;
+        var result = new Dictionary<string, HashSet<string>>();
+        // We need to iterate over all the keys in the dictionary and process each key to split it into segments where
+        // the prefixes are string separated by . and the keys are enclosed in []. For example if the key is
+        // Customer.Orders[<<OrderId>>]BillingInfo.FirstName, then we need to split it into Customer.Orders,
+        // [<<OrderId>>] and BillingInfo.FirstName. We then, need to group all the keys by the prefix. So, for the
+        // above example, we will have an entry for the prefix Customer.Orders that will include [<<OrderId>>] as the
+        // key.
+
+        foreach (var key in keys)
+        {
+            var startIndex = key.IndexOf('[');
+            while (startIndex > 0)
+            {
+                var endIndex = key.IndexOf(']');
+                if (endIndex == -1)
+                {
+                    // Ignore malformed keys
+                    break;
+                }
+
+                var prefix = key[..startIndex];
+                var keyValue = key[startIndex..(endIndex + 1)];
+                if (result.TryGetValue(prefix, out var foundKeys))
+                {
+                    foundKeys.Add(keyValue);
+                }
+                else
+                {
+                    result.Add(prefix, new HashSet<string> { keyValue });
+                }
+
+                startIndex = key.IndexOf('[', endIndex + 1);
+            }
+        }
+
+        return result;
     }
 
     internal void PopPrefix(string key)
