0b040cb76dd31f0280b71cab15663ccb0a33dca3
James Newton-King <james@newtonking.com>
2023-06-06 17:02:01 +0800

    Update

--- src/Http/Routing/src/RoutingMetrics.cs
+++ src/Http/Routing/src/RoutingMetrics.cs
@@ -1,9 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics;
 using System.Diagnostics.Metrics;
-using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Diagnostics.Metrics;
 
 namespace Microsoft.AspNetCore.Routing;
