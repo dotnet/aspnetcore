404044d496d615bdad5d0fb853fbbdca2ec3775b
James Newton-King <james@newtonking.com>
2023-06-08 22:50:45 +0800

    Fix build

--- src/Hosting/Hosting/src/Internal/HostingRequestUnhandledLog.cs
+++ src/Hosting/Hosting/src/Internal/HostingRequestUnhandledLog.cs
@@ -2,9 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections;
-using System.Globalization;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
 
 namespace Microsoft.AspNetCore.Hosting;
 
