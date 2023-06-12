5017e8243518636848de50a360b9a35a76c6b9e6
Surayya Huseyn Zada <shuseynzada@microsoft.com>
2023-06-12 13:04:20 +0200

    added more e2e tests for sections with error boundary

--- src/Components/test/E2ETest/Tests/SectionsWithErrorBoundaryTest.cs
+++ src/Components/test/E2ETest/Tests/SectionsWithErrorBoundaryTest.cs
@@ -29,20 +29,77 @@ public class SectionsWithErrorBoundaryTest : ServerTestBase<ToggleExecutionModeS
     }
 
     [Fact]
-    public void ErrorBoundaryForSectionOutletContentIsDeterminedByMatchingSectionContent()
+    public void RenderSectionContent_ErrorBoundaryForSectionOutletContentIsDeterminedByMatchingSectionContent()
     {
-        Browser.Equal("First Section", () => Browser.Exists(By.TagName("h1")).Text);
+        // Doesn't matter if SectionOutlet is rendered before or after SectionContent
+        Browser.FindElement(By.Id("render-section-outlet")).Click();
+        Browser.FindElement(By.Id("render-second-section-content")).Click();
+
+        Browser.FindElement(By.Id("error-button")).Click();
+
+        Browser.Equal("Sorry!", () => Browser.Exists(By.TagName("p")).Text);
+    }
+
+    [Fact]
+    public void RenderTwoSectionContentsWithSameId_ErrorBoundaryForSectionOutletIsDeterminedByLastRenderedSectionContent()
+    {
+        // show that after second sc error thrown then first is now rendered and is functional
+        Browser.FindElement(By.Id("render-second-section-content")).Click();
+        Browser.FindElement(By.Id("render-first-section-content")).Click();
+        Browser.FindElement(By.Id("render-section-outlet")).Click();
 
         Browser.FindElement(By.Id("error-button")).Click();
 
         Browser.Exists(By.ClassName("blazor-error-boundary"));
+    }
 
-        // Switch to the second SectionContent
+    [Fact]
+    public void SecondSectionContentIdChanged_ErrorBoundaryForSectionOutletIsDeterminedByFirstSectionContent()
+    {
+        Browser.FindElement(By.Id("render-second-section-content")).Click();
+        Browser.FindElement(By.Id("render-first-section-content")).Click();
+        Browser.FindElement(By.Id("render-section-outlet")).Click();
 
-        Browser.FindElement(By.Id("change-section-content")).Click();
+        Browser.FindElement(By.Id("error-button")).Click();
 
-        Browser.Equal("Second Section", () => Browser.Exists(By.TagName("h1")).Text);
+        Browser.Exists(By.ClassName("blazor-error-boundary"));
+    }
 
+    [Fact]
+    public void SecondSectionContentDisposed_ErrorBoundaryForSectionOutletIsDeterminedByFirstSectionContent()
+    {
+        Browser.FindElement(By.Id("render-first-section-content")).Click();
+        Browser.FindElement(By.Id("render-second-section-content")).Click();
+        Browser.FindElement(By.Id("render-section-outlet")).Click();
+
+        Browser.FindElement(By.Id("dispose-second-section-content")).Click();
+        Browser.FindElement(By.Id("error-button")).Click();
+
+        Browser.Exists(By.ClassName("blazor-error-boundary"));
+    }
+
+    [Fact]
+    public void FirstSectionContentDisposedThenRenderSecondSectionContent_ErrorBoundaryForSectionOutletIsDeterminedBySecondSectionContent()
+    {
+        Browser.FindElement(By.Id("render-section-outlet")).Click();
+        Browser.FindElement(By.Id("render-first-section-content")).Click();
+
+        Browser.FindElement(By.Id("dispose-first-section-content")).Click();
+        Browser.FindElement(By.Id("render-second-section-content")).Click();
+        Browser.FindElement(By.Id("error-button")).Click();
+
+        Browser.Equal("Sorry!", () => Browser.Exists(By.TagName("p")).Text);
+    }
+
+    [Fact]
+    public void SectionOutletIdChanged_ErrorBoundaryForSectionOutletIsDeterminedByMatchingSectionContent()
+    {
+        Browser.FindElement(By.Id("render-section-outlet")).Click();
+        Browser.FindElement(By.Id("render-first-section-content")).Click();
+        Browser.FindElement(By.Id("change-second-section-content-id")).Click();
+        Browser.FindElement(By.Id("render-second-section-content")).Click();
+
+        Browser.FindElement(By.Id("change-section-outlet-id")).Click();
         Browser.FindElement(By.Id("error-button")).Click();
 
         Browser.Equal("Sorry!", () => Browser.Exists(By.TagName("p")).Text);
