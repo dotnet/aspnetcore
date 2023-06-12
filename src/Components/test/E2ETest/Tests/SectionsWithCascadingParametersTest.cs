18a4759c545e1b84544838cb4586f06f434daef5
Surayya Huseyn Zada <shuseynzada@microsoft.com>
2023-06-12 13:04:00 +0200

    added more e2e tests for sections with cascading parameter

--- src/Components/test/E2ETest/Tests/SectionsWithCascadingParametersTest.cs
+++ src/Components/test/E2ETest/Tests/SectionsWithCascadingParametersTest.cs
@@ -29,11 +29,70 @@ public class SectionsWithCascadingParametersTest : ServerTestBase<ToggleExecutio
     }
 
     [Fact]
-    public void CascadingParameterForSectionOutletContentIsDeterminedByMatchingSectionContent()
+    public void RenderSectionContent_CascadingParameterForSectionOutletIsDeterminedByMatchingSectionContent()
     {
-        Browser.Equal("First Section with additional text for first section", () => Browser.Exists(By.TagName("p")).Text);
-
+        // Doesn't matter if SectionOutlet is rendered before or after SectionContent
+        Browser.FindElement(By.Id("render-section-outlet")).Click();
         Browser.FindElement(By.Id("render-second-section-content")).Click();
+
+        Browser.Equal("Second Section with additional text for second section", () => Browser.Exists(By.TagName("p")).Text);
+    }
+
+    [Fact]
+    public void RenderTwoSectionContentsWithSameId_CascadingParameterForSectionOutletIsDeterminedByLastRenderedSectionContent()
+    {
+        Browser.FindElement(By.Id("render-second-section-content")).Click();
+        Browser.FindElement(By.Id("render-first-section-content")).Click();
+        Browser.FindElement(By.Id("render-section-outlet")).Click();
+
+        Browser.Equal("First Section with additional text for first section", () => Browser.Exists(By.TagName("p")).Text);
+    }
+
+    [Fact]
+    public void SecondSectionContentIdChanged_CascadingParameterForSectionOutletIsDeterminedByFirstSectionContent()
+    {
+        Browser.FindElement(By.Id("render-section-outlet")).Click();
+        Browser.FindElement(By.Id("render-first-section-content")).Click();
+        Browser.FindElement(By.Id("render-second-section-content")).Click();
+
+        Browser.FindElement(By.Id("change-second-section-content-id")).Click();
+
+        Browser.Equal("First Section with additional text for first section", () => Browser.Exists(By.TagName("p")).Text);
+    }
+
+    [Fact]
+    public void SecondSectionContentDisposed_CascadingParameterForSectionOutletIsDeterminedByFirstSectionContent()
+    {
+        Browser.FindElement(By.Id("render-first-section-content")).Click();
+        Browser.FindElement(By.Id("render-second-section-content")).Click();
+        Browser.FindElement(By.Id("render-section-outlet")).Click();
+
+        Browser.FindElement(By.Id("dispose-second-section-content")).Click();
+
+        Browser.Equal("First Section with additional text for first section", () => Browser.Exists(By.TagName("p")).Text);
+    }
+
+    [Fact]
+    public void FirstSectionContentDisposedThenRenderSecondSectionContent_CascadingParameterForSectionOutletIsDeterminedBySecondSectionContent()
+    {
+        Browser.FindElement(By.Id("render-section-outlet")).Click();
+        Browser.FindElement(By.Id("render-first-section-content")).Click();
+
+        Browser.FindElement(By.Id("dispose-first-section-content")).Click();
+        Browser.FindElement(By.Id("render-second-section-content")).Click();
+
+        Browser.Equal("Second Section with additional text for second section", () => Browser.Exists(By.TagName("p")).Text);
+    }
+
+    [Fact]
+    public void SectionOutletIdChanged_CascadingParameterForSectionOutletIsDeterminedByMatchingSectionContent()
+    {
+        Browser.FindElement(By.Id("render-section-outlet")).Click();
+        Browser.FindElement(By.Id("render-first-section-content")).Click();
+        Browser.FindElement(By.Id("change-second-section-content-id")).Click();
+        Browser.FindElement(By.Id("render-second-section-content")).Click();
+
+        Browser.FindElement(By.Id("change-section-outlet-id")).Click();
 
         Browser.Equal("Second Section with additional text for second section", () => Browser.Exists(By.TagName("p")).Text);
     }
