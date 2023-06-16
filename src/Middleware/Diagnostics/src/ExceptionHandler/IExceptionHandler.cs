3b71a49195b0ee9c2f9ca39eb8ef0109c03c036c
Divyesh Bhandari <79130336+divyeshio@users.noreply.github.com>
2023-06-16 22:57:49 +0530

    Update IExceptionHandler.cs

--- src/Middleware/Diagnostics/src/ExceptionHandler/IExceptionHandler.cs
+++ src/Middleware/Diagnostics/src/ExceptionHandler/IExceptionHandler.cs
@@ -12,11 +12,16 @@ namespace Microsoft.AspNetCore.Diagnostics;
 public interface IExceptionHandler
 {
     /// <summary>
-    /// 
+    /// Tries to handle the specified exception asynchronously within the ASP.NET Core pipeline.
+    /// Implementations of this method can provide custom exception handling logic for different scenarios. 
     /// </summary>
     /// <param name="httpContext"></param>
     /// <param name="exception"></param>
     /// <param name="cancellationToken"></param>
-    /// <returns></returns>
+    /// <returns>
+    /// A <see cref="ValueTask{TResult}"/> representing the asynchronous handling operation indicating 
+    /// <see langword="true"/> if the exception was
+    /// handled successfully; otherwise <see langword="false"/>.
+    /// </returns>
     ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken);
 }
