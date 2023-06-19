a1da413be0650e3bce837039c4655038dfb386de
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2023-06-19 16:09:25 +0100

    Initial E2E test

---
+++ src/Components/test/E2ETest/ServerRenderingTests/EnhancedNavigationTest.cs
@@ -0,0 +1,38 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
+using Microsoft.AspNetCore.Components.E2ETest.Infrastructure;
+using Microsoft.AspNetCore.E2ETesting;
+using TestServer;
+using Xunit.Abstractions;
+using Components.TestServer.RazorComponents;
+using OpenQA.Selenium;
+
+namespace Microsoft.AspNetCore.Components.E2ETests.ServerRenderingTests;
+
+public class EnhancedNavigationTest : ServerTestBase<BasicTestAppServerSiteFixture<RazorComponentEndpointsStartup<App>>>
+{
+    public EnhancedNavigationTest(
+        BrowserFixture browserFixture,
+        BasicTestAppServerSiteFixture<RazorComponentEndpointsStartup<App>> serverFixture,
+        ITestOutputHelper output)
+        : base(browserFixture, serverFixture, output)
+    {
+    }
+
+    [Fact]
+    public void CanNavigateToAnotherPageWhilePreservingCommonDOMElements()
+    {
+        Navigate(ServerPathBase);
+
+        var h1Elem = Browser.Exists(By.TagName("h1"));
+        Browser.Equal("Hello", () => h1Elem.Text);
+        
+        Browser.Exists(By.TagName("nav")).FindElement(By.LinkText("Streaming")).Click();
+
+        // Important: we're checking the *same* <h1> element as earlier, showing that we got to the
+        // destination, and it's done so without a page load, and it preserved the element
+        Browser.Equal("Streaming Rendering", () => h1Elem.Text);
+    }
+}
