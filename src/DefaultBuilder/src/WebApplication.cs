56760c2f38e4b9d388f5c9f9f1c4871786bde831
James Newton-King <james@newtonking.com>
2023-06-21 11:47:54 +0800

    Fix build

--- src/DefaultBuilder/src/WebApplication.cs
+++ src/DefaultBuilder/src/WebApplication.cs
@@ -3,7 +3,6 @@
 
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Hosting.Server.Features;
