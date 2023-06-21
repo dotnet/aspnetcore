14a4f4591b2e6ab1cee8a3aa4e5a929c5fb8e67a
James Newton-King <james@newtonking.com>
2023-06-21 17:00:05 +0800

    Improve WebApplication debugging (#48827)

--- src/Hosting/Hosting/src/Internal/ApplicationLifetime.cs
+++ src/Hosting/Hosting/src/Internal/ApplicationLifetime.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -9,6 +10,9 @@ namespace Microsoft.AspNetCore.Hosting;
 /// <summary>
 /// Allows consumers to perform cleanup during a graceful shutdown.
 /// </summary>
+[DebuggerDisplay("ApplicationStarted = {ApplicationStarted.IsCancellationRequested}, " +
+    "ApplicationStopping = {ApplicationStopping.IsCancellationRequested}, " +
+    "ApplicationStopped = {ApplicationStopped.IsCancellationRequested}")]
 #pragma warning disable CS0618 // Type or member is obsolete
 internal sealed class ApplicationLifetime : IApplicationLifetime, Extensions.Hosting.IApplicationLifetime, IHostApplicationLifetime
 #pragma warning restore CS0618 // Type or member is obsolete
