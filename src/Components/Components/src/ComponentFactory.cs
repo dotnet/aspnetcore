5cd57889cc2b696578b6dbf2937de99cfe2eb999
David Acker <davidacker3@gmail.com>
2023-06-20 21:15:30 -0400

    Convert ComponentTypeInfoCacheEntry to class

--- src/Components/Components/src/ComponentFactory.cs
+++ src/Components/Components/src/ComponentFactory.cs
@@ -120,7 +120,17 @@ internal sealed class ComponentFactory
     }
 
     // Tracks information about a specific component type that ComponentFactory uses
-    private record class ComponentTypeInfoCacheEntry(
-        IComponentRenderMode? ComponentTypeRenderMode,
-        Action<IServiceProvider, IComponent> PerformPropertyInjection);
+    private sealed class ComponentTypeInfoCacheEntry
+    {
+        public IComponentRenderMode? ComponentTypeRenderMode { get; }
+        public Action<IServiceProvider, IComponent> PerformPropertyInjection { get; }
+
+        public ComponentTypeInfoCacheEntry(
+            IComponentRenderMode? componentTypeRenderMode,
+            Action<IServiceProvider, IComponent> performPropertyInjection)
+        {
+            ComponentTypeRenderMode = componentTypeRenderMode;
+            PerformPropertyInjection = performPropertyInjection;
+        }
+    }
 }
