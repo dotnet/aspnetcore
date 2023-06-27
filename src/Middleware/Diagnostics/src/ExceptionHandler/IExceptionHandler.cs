d92a95fc57543008c581779824a08408a3de3756
James Newton-King <james@newtonking.com>
2023-06-27 08:31:42 +0800

    Update IExceptionHandler.cs (#49040)

--- src/Middleware/Diagnostics/src/ExceptionHandler/IExceptionHandler.cs
+++ src/Middleware/Diagnostics/src/ExceptionHandler/IExceptionHandler.cs
@@ -13,15 +13,15 @@ public interface IExceptionHandler
 {
     /// <summary>
     /// Tries to handle the specified exception asynchronously within the ASP.NET Core pipeline.
-    /// Implementations of this method can provide custom exception handling logic for different scenarios. 
+    /// Implementations of this method can provide custom exception-handling logic for different scenarios. 
     /// </summary>
-    /// <param name="httpContext"></param>
-    /// <param name="exception"></param>
-    /// <param name="cancellationToken"></param>
+    /// <param name="httpContext">The <see cref="HttpContext"/> for the request.</param>
+    /// <param name="exception">The unhandled exception.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
     /// <returns>
-    /// A <see cref="ValueTask{TResult}"/> representing the asynchronous handling operation indicating 
-    /// <see langword="true"/> if the exception was
-    /// handled successfully; otherwise <see langword="false"/>.
+    /// A task that represents the asynchronous read operation. The value of its <see cref="P:System.Threading.Tasks.ValueTask`1.Result" />
+    /// property contains the result of the handling operation.
+    /// <see langword="true"/> if the exception was handled successfully; otherwise <see langword="false"/>.
     /// </returns>
     ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken);
 }
