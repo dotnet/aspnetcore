29e9d27f0785d0a7fd1537fea83cd6b95768464c
Nikita Balabaev <xakep139@users.noreply.github.com>
2023-07-03 13:57:43 +0200

    Fix Http MinimalSample

--- src/Http/samples/MinimalSample/Program.cs
+++ src/Http/samples/MinimalSample/Program.cs
@@ -1,8 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics;
 using System.Reflection;
+using System.Runtime;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Http.Metadata;
 using Microsoft.AspNetCore.Mvc;
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Logger.LogInformation($"Current process ID: {Process.GetCurrentProcess().Id}");
+app.Logger.LogInformation($"Current process ID: {Environment.ProcessId}");
 
 string Plaintext() => "Hello, World!";
 app.MapGet("/plaintext", Plaintext);
