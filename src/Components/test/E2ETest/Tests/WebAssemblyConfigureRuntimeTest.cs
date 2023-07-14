7c77b5d8e1edd73d36977d00bf6a3391c48ff51d
Marek Fišera <mara@neptuo.com>
2023-07-14 22:20:10 +0200

    e2e checking configureRuntime

---
+++ src/Components/test/E2ETest/Tests/WebAssemblyConfigureRuntimeTest.cs
@@ -0,0 +1,48 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.AspNetCore.Components.E2ETest.Infrastructure;
+using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
+using Microsoft.AspNetCore.E2ETesting;
+using OpenQA.Selenium;
+using Xunit.Abstractions;
+
+namespace Microsoft.AspNetCore.Components.E2ETest.Tests;
+
+public class WebAssemblyConfigureRuntimeTest
+    : ServerTestBase<BlazorWasmTestAppFixture<StandaloneApp.Program>>, IDisposable
+{
+    public WebAssemblyConfigureRuntimeTest(
+        BrowserFixture browserFixture,
+        BlazorWasmTestAppFixture<StandaloneApp.Program> serverFixture,
+        ITestOutputHelper output)
+        : base(browserFixture, serverFixture, output)
+    {
+    }
+
+    protected override void InitializeAsyncCore()
+    {
+        Navigate("/configure-runtime", noReload: true);
+        WaitUntilLoaded();
+    }
+
+    [Fact]
+    public void ConfigureRuntimeWorks()
+    {
+        var element = Browser.Exists(By.Id("environment"));
+        Browser.Equal("true", () => element.Text);
+    }
+
+    private void WaitUntilLoaded()
+    {
+        var app = Browser.Exists(By.TagName("app"));
+        Browser.NotEqual("Loading...", () => app.Text);
+    }
+
+    public void Dispose()
+    {
+        // Make the tests run faster by navigating back to the home page when we are done
+        // If we don't, then the next test will reload the whole page before it starts
+        Browser.Exists(By.LinkText("Home")).Click();
+    }
+}
