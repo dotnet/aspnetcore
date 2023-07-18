ad0b7ec73a324905fbe0097e4160b4316e5fbf4b
jacalvar <jacalvar@microsoft.com>
2023-07-18 18:44:18 +0200

    Remove benchmarks

--- src/Mvc/perf/Microbenchmarks/Microsoft.AspNetCore.Mvc/RuntimePerformanceBenchmarkBase.cs
+++ src/Mvc/perf/Microbenchmarks/Microsoft.AspNetCore.Mvc/RuntimePerformanceBenchmarkBase.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Diagnostics;
@@ -24,7 +24,7 @@ using Microsoft.Extensions.Options;
 
 namespace Microsoft.AspNetCore.Mvc.Microbenchmarks;
 
-public abstract class RuntimePerformanceBenchmarkBase
+public class RuntimePerformanceBenchmarkBase
 {
     private sealed class NullLoggerFactory : ILoggerFactory, ILogger
     {
