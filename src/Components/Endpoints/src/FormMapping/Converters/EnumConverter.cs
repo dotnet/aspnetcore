7d74d1e40a6f5df1ab543b33f907b92411248b2e
jacalvar <jacalvar@microsoft.com>
2023-07-18 14:41:15 +0200

    fix build

--- src/Components/Endpoints/src/FormMapping/Converters/EnumConverter.cs
+++ src/Components/Endpoints/src/FormMapping/Converters/EnumConverter.cs
@@ -1,12 +1,15 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
 namespace Microsoft.AspNetCore.Components.Endpoints.FormMapping;
 
 internal class EnumConverter<TEnum> : FormDataConverter<TEnum> where TEnum : struct, Enum
 {
+    [RequiresDynamicCode(FormMappingHelpers.RequiresDynamicCodeMessage)]
+    [RequiresUnreferencedCode(FormMappingHelpers.RequiresUnreferencedCodeMessage)]
     internal override bool TryRead(ref FormDataReader reader, Type type, FormDataMapperOptions options, out TEnum result, out bool found)
     {
         found = reader.TryGetValue(out var value);
