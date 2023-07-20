f6d88adf6dd553e995ef8c82429931255dcdca00
Preston Jennings <preston.jennings@jitterbit.com>
2023-07-20 10:42:33 -0700

    Generate packages.
    refs VINYL-14964

--- pack.sh
+++ pack.sh
@@ -9,5 +9,5 @@ cat pack-list.txt | while read line || [[ -n $line ]];
 do
     echo $line
     fullPath=$(pwd)/$(find . -iname "$line")
-    ./eng/build.sh --configuration $configuration --projects $fullPath
+    ./eng/build.sh --pack --configuration $configuration --projects $fullPath
 done
