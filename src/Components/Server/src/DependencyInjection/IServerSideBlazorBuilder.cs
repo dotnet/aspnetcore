5eb13cf3ab48b7cfd4c1d37d10dc5476c04b6d5a
jacalvar <jacalvar@microsoft.com>
2023-08-03 14:27:31 +0200

    Undo API change

--- src/Components/Server/src/DependencyInjection/IServerSideBlazorBuilder.cs
+++ src/Components/Server/src/DependencyInjection/IServerSideBlazorBuilder.cs
@@ -1,19 +1,17 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.AspNetCore.Components.Endpoints;
-
 namespace Microsoft.Extensions.DependencyInjection;
 
 /// <summary>
 /// A builder that can be used to configure Server-Side Blazor.
 /// </summary>
-public interface IServerSideBlazorBuilder : IRazorComponentsBuilder
+public interface IServerSideBlazorBuilder
 {
     /// <summary>
     /// Gets the <see cref="IServiceCollection"/>.
     /// </summary>
-    public new IServiceCollection Services { get; }
+    new IServiceCollection Services { get; }
 
     IServiceCollection IRazorComponentsBuilder.Services => Services;
 }
