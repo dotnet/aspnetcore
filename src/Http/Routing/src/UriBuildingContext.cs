4751f964131890daae5a1610df2fa94fc8522c9f
James Newton-King <james@newtonking.com>
2023-08-07 09:39:57 +0800

    Clean up

--- src/Http/Routing/src/UriBuildingContext.cs
+++ src/Http/Routing/src/UriBuildingContext.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Diagnostics;
-using System.Globalization;
 using System.Text;
 using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Http;
