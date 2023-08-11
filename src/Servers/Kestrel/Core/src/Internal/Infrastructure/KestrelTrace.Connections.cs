425cb241ab06d3999e1fbc9fa8a861e4d0a62e3c
Andrew Casey <andrew.casey@microsoft.com>
2023-08-11 13:22:50 -0700

    Update comments to reflect that KestrelTrace has split into separate loggers

--- src/Servers/Kestrel/Core/src/Internal/Infrastructure/KestrelTrace.Connections.cs
+++ src/Servers/Kestrel/Core/src/Internal/Infrastructure/KestrelTrace.Connections.cs
@@ -97,6 +97,6 @@ internal sealed partial class KestrelTrace : ILogger
         [LoggerMessage(39, LogLevel.Debug, @"Connection id ""{ConnectionId}"" accepted.", EventName = "ConnectionAccepted")]
         public static partial void ConnectionAccepted(ILogger logger, string connectionId);
 
-        // Highest shared ID is 66. New consecutive IDs start at 67. Please update this comment in every file in which it appears.
+        // IDs prior to 64 are reserved for back compat (the various KestrelTrace loggers used to share a single sequence)
     }
 }
