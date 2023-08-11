2e6e1d5462d6d1182a44b3fece0d2be4f5a5ef44
Safia Abdalla <safia@microsoft.com>
2023-08-11 10:31:23 -0700

    Remove MediaTypeHeaderValue

--- src/Shared/HttpExtensions.cs
+++ src/Shared/HttpExtensions.cs
@@ -1,15 +1,30 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 using Microsoft.AspNetCore.Http;
-using Microsoft.Net.Http.Headers;
 
 internal static class HttpExtensions
 {
+    internal static string UrlEncodedFormContentType = "application/x-www-form-urlencoded";
+    internal static string MultipartFormContentType = "multipart/form-data";
+
     internal static bool IsValidHttpMethodForForm(string method) =>
         HttpMethods.IsPost(method) || HttpMethods.IsPut(method) || HttpMethods.IsPatch(method);
 
-    internal static bool IsValidContentTypeForForm(string? contentType) =>
-        MediaTypeHeaderValue.TryParse(contentType, out var mediaType) &&
-        (mediaType.MediaType.Equals("application/x-www-form-urlencoded", StringComparison.OrdinalIgnoreCase) ||
-        mediaType.MediaType.Equals("multipart/form-data", StringComparison.OrdinalIgnoreCase));
+    internal static bool IsValidContentTypeForForm(string? contentType)
+    {
+        if (contentType == null)
+        {
+            return false;
+        }
+
+        // Abort early if this doesn't look like it could be a form-related content-type
+
+        if (contentType.Length < 19)
+        {
+            return false;
+        }
+
+        return contentType.Equals(UrlEncodedFormContentType, StringComparison.OrdinalIgnoreCase) ||
+            contentType.StartsWith(MultipartFormContentType, StringComparison.OrdinalIgnoreCase);
+    }
 }
