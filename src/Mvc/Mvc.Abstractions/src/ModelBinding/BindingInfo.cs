af49ecd682a9240b3b3a1a1106a329db9563525d
Benjamin Petit <bpetit@microsoft.com>
2023-08-17 22:01:18 +0200

    Remove useless using directive

--- src/Mvc/Mvc.Abstractions/src/ModelBinding/BindingInfo.cs
+++ src/Mvc/Mvc.Abstractions/src/ModelBinding/BindingInfo.cs
@@ -4,7 +4,6 @@
 using System.Linq;
 using System.Reflection;
 using Microsoft.AspNetCore.Http.Metadata;
-using System.Reflection.Metadata;
 using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
 
