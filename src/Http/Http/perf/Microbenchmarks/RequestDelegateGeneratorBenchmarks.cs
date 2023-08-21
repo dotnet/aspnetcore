cf3e80709ffeff97ba4ed16030c6967bb2137b32
Safia Abdalla <safia@safia.rocks>
2023-08-21 12:17:55 -0700

    Add benchmarks for RDG

---
+++ src/Http/Http/perf/Microbenchmarks/RequestDelegateGeneratorBenchmarks.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+using BenchmarkDotNet.Attributes;
+using Microsoft.AspNetCore.Http.Generators.Tests;
+
+namespace Microsoft.AspNetCore.Http.Microbenchmarks;
+
+public class RequestDelegateGeneratorBenchmarks : RequestDelegateCreationTestBase
+{
+    protected override bool IsGeneratorEnabled => true;
+
+    [Params(10, 100, 1000, 10000)]
+    public int EndpointCount { get; set; }
+
+    [Benchmark]
+    public async Task CreateRequestDelegate()
+    {
+        var source = "";
+        for (var i = 0; i < EndpointCount; i++)
+        {
+            source += $"""app.MapGet("/route{i}", (int? id) => "Hello World!");""";
+        }
+        await RunGeneratorAsync(source);
+    }
+}
