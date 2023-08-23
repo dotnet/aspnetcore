e252d592c7585305d9f6bdbb43a48831346cdaa7
Safia Abdalla <safia@safia.rocks>
2023-08-23 16:24:44 -0700

    Add usings

--- src/Http/Http/perf/Microbenchmarks/RequestDelegateGeneratorBenchmarks.cs
+++ src/Http/Http/perf/Microbenchmarks/RequestDelegateGeneratorBenchmarks.cs
@@ -1,7 +1,11 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+using System.Text;
 using BenchmarkDotNet.Attributes;
 using Microsoft.AspNetCore.Http.Generators.Tests;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.AspNetCore.Http.Microbenchmarks;
 
