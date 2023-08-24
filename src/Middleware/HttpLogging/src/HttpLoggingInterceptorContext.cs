3e3a8caec39bb06154f4f028693eb9930dffc7b1
Chris R <Tratcher@outlook.com>
2023-08-24 13:26:18 -0700

    Usings

--- src/Middleware/HttpLogging/src/HttpLoggingInterceptorContext.cs
+++ src/Middleware/HttpLogging/src/HttpLoggingInterceptorContext.cs
@@ -1,9 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Numerics;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 
 namespace Microsoft.AspNetCore.HttpLogging;
 
