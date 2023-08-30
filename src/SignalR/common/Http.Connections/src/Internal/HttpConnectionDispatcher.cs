dedfd0938eb890ba076f24e0f292321279a01ee3
Brennan <brecon@microsoft.com>
2023-08-30 09:36:26 -0700

    using

--- src/SignalR/common/Http.Connections/src/Internal/HttpConnectionDispatcher.cs
+++ src/SignalR/common/Http.Connections/src/Internal/HttpConnectionDispatcher.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Buffers;
-using System.Diagnostics;
 using System.Security.Claims;
 using System.Security.Principal;
 using Microsoft.AspNetCore.Authentication;
