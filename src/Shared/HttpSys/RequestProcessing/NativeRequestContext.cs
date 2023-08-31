0136c49d0f1d76133d9098ba2e6a07feda3139c4
Nolan Glore <nglore@microsoft.com>
2023-08-30 21:31:35 -0700

    Fixup pRequestInfo after GCs

--- src/Shared/HttpSys/RequestProcessing/NativeRequestContext.cs
+++ src/Shared/HttpSys/RequestProcessing/NativeRequestContext.cs
@@ -553,9 +553,12 @@ namespace Microsoft.AspNetCore.HttpSys.Internal
 
             var info = new Dictionary<int, ReadOnlyMemory<byte>>(count);
 
+            long fixup = (byte*)nativeRequest - (byte*)baseAddress;
+            var pRequestInfo = (HttpApiTypes.HTTP_REQUEST_INFO*)((byte*)nativeRequest->pRequestInfo + fixup);
+
             for (var i = 0; i < count; i++)
             {
-                var requestInfo = nativeRequest->pRequestInfo[i];
+                var requestInfo = pRequestInfo[i];
                 var offset = (long)requestInfo.pInfo - (long)baseAddress;
                 info.Add(
                     (int)requestInfo.InfoType,
