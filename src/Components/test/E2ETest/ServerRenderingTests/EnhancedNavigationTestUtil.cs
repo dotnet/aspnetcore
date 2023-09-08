3902de4a4255e94491aca881682e1285c7e53b7b
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2023-09-08 12:16:02 +0100

    Remove unused method

--- src/Components/test/E2ETest/ServerRenderingTests/EnhancedNavigationTestUtil.cs
+++ src/Components/test/E2ETest/ServerRenderingTests/EnhancedNavigationTestUtil.cs
@@ -32,9 +32,4 @@ public static class EnhancedNavigationTestUtil
 
     public static long GetScrollY(this IWebDriver browser)
         => Convert.ToInt64(((IJavaScriptExecutor)browser).ExecuteScript("return window.scrollY"), CultureInfo.CurrentCulture);
-
-    public static void SetScrollY(this IWebDriver browser, long value)
-    {
-        ((IJavaScriptExecutor)browser).ExecuteScript($"window.scrollTo(0, {value})");
-    }
 }
