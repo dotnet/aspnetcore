99349024ae89fa187a0128e0839167e7787448c2
James Newton-King <james@newtonking.com>
2023-09-12 20:20:02 +0800

    Clean up route tooling for components (#50651)

--- src/Framework/AspNetCoreAnalyzers/src/Analyzers/Infrastructure/RoutePattern/RoutePatternOptions.cs
+++ src/Framework/AspNetCoreAnalyzers/src/Analyzers/Infrastructure/RoutePattern/RoutePatternOptions.cs
@@ -7,30 +7,12 @@ internal sealed class RoutePatternOptions
 {
     private RoutePatternOptions() { }
     public bool SupportTokenReplacement { get; private set; }
-    public bool SupportComplexSegments { get; private set; }
-    public bool SupportDefaultValues { get; private set; }
-    public bool SupportTwoAsteriskCatchAll { get; private set; }
-    public char[]? AdditionalInvalidParameterCharacters { get; private set; }
 
-    public static readonly RoutePatternOptions DefaultRoute = new RoutePatternOptions
-    {
-        SupportComplexSegments = true,
-        SupportDefaultValues = true,
-        SupportTwoAsteriskCatchAll = true,
-    };
+    public static readonly RoutePatternOptions DefaultRoute = new RoutePatternOptions();
+    public static readonly RoutePatternOptions ComponentsRoute = new RoutePatternOptions();
 
     public static readonly RoutePatternOptions MvcAttributeRoute = new RoutePatternOptions
     {
-        SupportComplexSegments = true,
-        SupportDefaultValues = true,
-        SupportTwoAsteriskCatchAll = true,
         SupportTokenReplacement = true,
-    };
-
-    public static readonly RoutePatternOptions ComponentsRoute = new RoutePatternOptions
-    {
-        SupportComplexSegments = true,
-        SupportDefaultValues = true,
-        SupportTwoAsteriskCatchAll = true,
     };
 }
