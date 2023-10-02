51b3e9b1e6472d2a5f12f19a530c4dc8bc821a68
Stephen Halter <halter73@gmail.com>
2023-10-02 12:43:16 -0700

    Make AddComponentRenderMode param nullable

--- src/Components/Components/src/Rendering/RenderTreeBuilder.cs
+++ src/Components/Components/src/Rendering/RenderTreeBuilder.cs
@@ -682,10 +682,8 @@ public sealed class RenderTreeBuilder : IDisposable
     /// Adds a frame indicating the render mode on the enclosing component frame.
     /// </summary>
     /// <param name="renderMode">The <see cref="IComponentRenderMode"/>.</param>
-    public void AddComponentRenderMode(IComponentRenderMode renderMode)
+    public void AddComponentRenderMode(IComponentRenderMode? renderMode)
     {
-        ArgumentNullException.ThrowIfNull(renderMode);
-
         // Note that a ComponentRenderMode frame is technically a child of the Component frame to which it applies,
         // hence the terminology of "adding" it rather than "setting" it. For performance reasons, the diffing system
         // will only look for ComponentRenderMode frames:
