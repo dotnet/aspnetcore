39362bc4b4537349776d4337639a86e48dd1b83b
Nobuyuki Akama <nakama@microsoft.com>
2023-10-16 09:05:49 +0900

    Add non-prerendering modes

--- src/Components/Web/src/RenderMode/RenderMode.cs
+++ src/Components/Web/src/RenderMode/RenderMode.cs
@@ -24,4 +24,22 @@ public static class RenderMode
     /// Gets an <see cref="IComponentRenderMode"/> that means the render mode will be determined automatically based on a policy.
     /// </summary>
     public static InteractiveAutoRenderMode InteractiveAuto { get; } = new();
+
+    /// <summary>
+    /// Gets an <see cref="IComponentRenderMode"/> that represents rendering interactively on the server via Blazor Server hosting
+    /// without server-side prerendering.
+    /// </summary>
+    public static InteractiveServerRenderMode InteractiveServerWithoutPrerendering { get; } = new(prerender: false);
+
+    /// <summary>
+    /// Gets an <see cref="IComponentRenderMode"/> that represents rendering interactively on the client via Blazor WebAssembly hosting
+    /// without server-side prerendering.
+    /// </summary>
+    public static InteractiveWebAssemblyRenderMode InteractiveWebAssemblyWithoutPrerendering { get; } = new(prerender: false);
+
+    /// <summary>
+    /// Gets an <see cref="IComponentRenderMode"/> that means the render mode will be determined automatically based on a policy without server-side prerendering.
+    /// </summary>
+    public static InteractiveAutoRenderMode InteractiveAutoWithoutPrerendering { get; } = new(prerender: false);
+
 }
