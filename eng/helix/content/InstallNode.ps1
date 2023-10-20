031a06350180386f71f9e1c72b49bd139eabdff6
jacalvar <jacalvar@microsoft.com>
2023-10-20 16:10:43 +0200

    Add retries to the node install script

--- eng/helix/content/InstallNode.ps1
+++ eng/helix/content/InstallNode.ps1
@@ -1,4 +1,4 @@
- <#
+<#
  .SYNOPSIS
      Installs Node.js from https://nodejs.org/dist on a machine
  .DESCRIPTION
@@ -19,47 +19,59 @@ $InstallDir = $PSScriptRoot + '\nodejs' # Always install to workitem root / node
 
 Set-StrictMode -Version 1
 
-if (Get-Command "node.exe" -ErrorAction SilentlyContinue)
-{
+if (Get-Command "node.exe" -ErrorAction SilentlyContinue) {
     Write-Host "Found node.exe in PATH"
     exit
 }
 
-if (Test-Path "$InstallDir\node.exe")
-{
+if (Test-Path "$InstallDir\node.exe") {
     Write-Host "Node.exe found at $InstallDir\node.exe"
     exit
 }
 
-$nodeFile="node-v$Version-win-x64"
-$url="https://nodejs.org/dist/v$Version/$nodeFile.zip"
-Write-Host "Starting download of Node.js ${Version} from $url"
-& $PSScriptRoot\Download.ps1 $url nodejs.zip
-Write-Host "Done downloading NodeJS ${Version}"
+$maxAttempts = 5;
+$current = 0;
+do {
+    $failed = DownloadAnInstall
+    $current++;
+} while ($current -lt $maxAttempts -and $failed -eq $true);
 
-$tempPath = [System.IO.Path]::GetTempPath()
-$tempDir = Join-Path $tempPath nodejs
-New-Item -Path "$tempDir" -ItemType "directory" -Force
-Write-Host "Extracting to $tempDir"
+function DownloadAnInstall () {
+    $tempPath = [System.IO.Path]::GetTempPath()
+    $tempDir = Join-Path $tempPath nodejs
+    New-Item -Path "$tempDir" -ItemType "directory" -Force
+    try {
+        $nodeFile = "node-v$Version-win-x64"
+        $url = "https://nodejs.org/dist/v$Version/$nodeFile.zip"
+        Write-Host "Starting download of Node.js ${Version} from $url"
+        & $PSScriptRoot\Download.ps1 $url nodejs.zip
+        Write-Host "Done downloading NodeJS ${Version}"
 
-if (Get-Command -Name 'Microsoft.PowerShell.Archive\Expand-Archive' -ErrorAction Ignore) {
-    # Use built-in commands where possible as they are cross-plat compatible
-    Microsoft.PowerShell.Archive\Expand-Archive -Path "nodejs.zip" -DestinationPath $tempDir -Force
-}
-else {
-    Remove-Item $tempDir -Recurse -ErrorAction Ignore
-    # Fallback to old approach for old installations of PowerShell
-    Add-Type -AssemblyName System.IO.Compression.FileSystem
-    [System.IO.Compression.ZipFile]::ExtractToDirectory("nodejs.zip", $tempDir)
-}
+        Write-Host "Extracting to $tempDir"
 
-Write-Host "Expanded Node.js to $tempDir, moving $tempDir\$nodeFile to $InstallDir subdir"
-move $tempDir\$nodeFile $InstallDir
-if (Test-Path "$InstallDir\node.exe")
-{
-    Write-Host "Node.exe copied to $InstallDir"
-}
-else
-{
-    Write-Host "Node.exe not copied to $InstallDir"
+        if (Get-Command -Name 'Microsoft.PowerShell.Archive\Expand-Archive' -ErrorAction Ignore) {
+            # Use built-in commands where possible as they are cross-plat compatible
+            Microsoft.PowerShell.Archive\Expand-Archive -Path "nodejs.zip" -DestinationPath $tempDir -Force
+        }
+        else {
+            Remove-Item $tempDir -Recurse -ErrorAction Ignore
+            # Fallback to old approach for old installations of PowerShell
+            Add-Type -AssemblyName System.IO.Compression.FileSystem
+            [System.IO.Compression.ZipFile]::ExtractToDirectory("nodejs.zip", $tempDir)
+        }
+
+        Write-Host "Expanded Node.js to $tempDir, moving $tempDir\$nodeFile to $InstallDir subdir"
+        Move-Item $tempDir\$nodeFile $InstallDir
+        if (Test-Path "$InstallDir\node.exe") {
+            Write-Host "Node.exe copied to $InstallDir"
+        }
+        else {
+            Write-Host "Node.exe not copied to $InstallDir"
+        }
+        return $true;
+    }
+    catch {
+        Remove-Item nodejs.zip -Force
+        return $false;
+    }
 }
