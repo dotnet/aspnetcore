1ba6694a37e3dae6245df960926474e0ebcf5db2
Robert Keller <robertkeller04@gmail.com>
2023-10-23 10:00:59 +0200

    Update HubConnection.ts to provide a easier to read order of SignalR Message parameters
    Changed order of SignalR Messages to increase readability in WebSocket debugging:
    - The change ensures that the target is the first parameter of the signalR Message
    - This makes it easier debugging running applications
    - It also follows the way the SignalR Server sends Messages
    - SignalR .Net Servers always use the ordering where target is the first parameter in the message

--- src/SignalR/clients/ts/signalr/src/HubConnection.ts
+++ src/SignalR/clients/ts/signalr/src/HubConnection.ts
@@ -994,15 +994,15 @@ export class HubConnection {
         if (nonblocking) {
             if (streamIds.length !== 0) {
                 return {
+                    target: methodName,
                     arguments: args,
                     streamIds,
-                    target: methodName,
                     type: MessageType.Invocation,
                 };
             } else {
                 return {
-                    arguments: args,
                     target: methodName,
+                    arguments: args,
                     type: MessageType.Invocation,
                 };
             }
@@ -1012,17 +1012,17 @@ export class HubConnection {
 
             if (streamIds.length !== 0) {
                 return {
+                    target: methodName,
                     arguments: args,
                     invocationId: invocationId.toString(),
                     streamIds,
-                    target: methodName,
                     type: MessageType.Invocation,
                 };
             } else {
                 return {
+                    target: methodName,
                     arguments: args,
                     invocationId: invocationId.toString(),
-                    target: methodName,
                     type: MessageType.Invocation,
                 };
             }
@@ -1096,17 +1096,17 @@ export class HubConnection {
 
         if (streamIds.length !== 0) {
             return {
+                target: methodName,
                 arguments: args,
                 invocationId: invocationId.toString(),
                 streamIds,
-                target: methodName,
                 type: MessageType.StreamInvocation,
             };
         } else {
             return {
+                target: methodName,
                 arguments: args,
                 invocationId: invocationId.toString(),
-                target: methodName,
                 type: MessageType.StreamInvocation,
             };
         }
