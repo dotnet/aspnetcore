786cff067bf63086b54f902950063ce153856934
Andrew Casey <andrew.casey@microsoft.com>
2023-11-07 09:06:43 -0800

    Suppress IL2072 with pragmas
    ...rather than WarningSuppressions.xml now that it's reported by a roslyn analyzer.  Analogous to 533a66cfce8103d0b35d3769e1e67234a900bc85 for IL2111.

--- src/Components/Components/src/ComponentFactory.cs
+++ src/Components/Components/src/ComponentFactory.cs
@@ -89,7 +89,11 @@ internal sealed class ComponentFactory
 
     private static void PerformPropertyInjection(IServiceProvider serviceProvider, IComponent instance)
     {
+        // Suppressed with "pragma warning disable" instead of WarningSuppressions.xml so ILLink Roslyn Anayzer doesn't report the warning.
+        #pragma warning disable IL2072 // 'componentType' argument does not satisfy 'DynamicallyAccessedMemberTypes.All' in call to 'Microsoft.AspNetCore.Components.ComponentFactory.GetComponentTypeInfo(Type)'.
         var componentTypeInfo = GetComponentTypeInfo(instance.GetType());
+        #pragma warning restore IL2072 // 'componentType' argument does not satisfy 'DynamicallyAccessedMemberTypes.All' in call to 'Microsoft.AspNetCore.Components.ComponentFactory.GetComponentTypeInfo(Type)'.
+
         componentTypeInfo.PerformPropertyInjection(serviceProvider, instance);
     }
 
