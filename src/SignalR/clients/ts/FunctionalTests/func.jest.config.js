4a1372f12b9baff3a8a0b361a1e8ebdea4cc67b3
jacalvar <jacalvar@microsoft.com>
2023-11-10 21:16:25 +0100

    All tests work

--- src/SignalR/clients/ts/FunctionalTests/func.jest.config.js
+++ src/SignalR/clients/ts/FunctionalTests/func.jest.config.js
@@ -2,26 +2,20 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 module.exports = {
+        transformIgnorePatterns: [
+        // We reference the ESM output from tests and don't want to run them through jest as it won't understand the syntax
+        ".*/node_modules/(?!@microsoft)"
+    ],
     reporters: [
         "default",
         ["jest-junit", { "outputDirectory": "../../../../artifacts/log/", "outputName": `${process.platform}` + ".signalr.junit.xml" }]
     ],
-    transform: {
-        "^.+\\.tsx?$": [
-            "ts-jest",
-            {
-                "tsconfig": "./tsconfig.jest.json",
-                "babelConfig": true,
-                "diagnostics": true
-            }
-        ]
-    },
     testEnvironment: "node",
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+    testRegex: "(Tests)\\.(jsx?|tsx?)$",
     testRunner: "jest-jasmine2",
     moduleNameMapper: {
         "^ts-jest$": "ts-jest",
-        "^@microsoft/signalr$": "<rootDir>/signalr/dist/cjs/index.js"
+        "^@microsoft/signalr$": "<rootDir>../node_modules/@microsoft/signalr/dist/cjs/index.js"
     },
     moduleFileExtensions: [
         "ts",
