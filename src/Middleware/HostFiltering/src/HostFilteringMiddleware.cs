3444786e6a8bc8d1748841ed8517adbfb6e93e7c
Kamil Charytoniuk <charytech@gmail.com>
2023-11-13 14:26:06 +0100

    Fixed HostFilteringMiddleware. Added HostFilteringMiddleware.MiddlewareConfigurationManager

--- src/Middleware/HostFiltering/src/HostFilteringMiddleware.cs
+++ src/Middleware/HostFiltering/src/HostFilteringMiddleware.cs
@@ -1,7 +1,7 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -20,18 +20,16 @@ public class HostFilteringMiddleware
     // Matches Http.Sys.
     private static readonly byte[] DefaultResponse = Encoding.ASCII.GetBytes(
         "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\"\"http://www.w3.org/TR/html4/strict.dtd\">\r\n"
-        + "<HTML><HEAD><TITLE>Bad Request</TITLE>\r\n"
-        + "<META HTTP-EQUIV=\"Content-Type\" Content=\"text/html; charset=us-ascii\"></ HEAD >\r\n"
-        + "<BODY><h2>Bad Request - Invalid Hostname</h2>\r\n"
-        + "<hr><p>HTTP Error 400. The request hostname is invalid.</p>\r\n"
-        + "</BODY></HTML>");
+            + "<HTML><HEAD><TITLE>Bad Request</TITLE>\r\n"
+            + "<META HTTP-EQUIV=\"Content-Type\" Content=\"text/html; charset=us-ascii\"></ HEAD >\r\n"
+            + "<BODY><h2>Bad Request - Invalid Hostname</h2>\r\n"
+            + "<hr><p>HTTP Error 400. The request hostname is invalid.</p>\r\n"
+            + "</BODY></HTML>"
+    );
 
     private readonly RequestDelegate _next;
     private readonly ILogger<HostFilteringMiddleware> _logger;
-    private readonly IOptionsMonitor<HostFilteringOptions> _optionsMonitor;
-    private HostFilteringOptions _options;
-    private IList<StringSegment>? _allowedHosts;
-    private bool? _allowAnyNonEmptyHost;
+    private readonly MiddlewareConfigurationManager _middlewareConfigurationManager;
 
     /// <summary>
     /// A middleware used to filter requests by their Host header.
@@ -39,20 +37,15 @@ public class HostFilteringMiddleware
     /// <param name="next"></param>
     /// <param name="logger"></param>
     /// <param name="optionsMonitor"></param>
-    public HostFilteringMiddleware(RequestDelegate next, ILogger<HostFilteringMiddleware> logger,
-        IOptionsMonitor<HostFilteringOptions> optionsMonitor)
+    public HostFilteringMiddleware(
+        RequestDelegate next,
+        ILogger<HostFilteringMiddleware> logger,
+        IOptionsMonitor<HostFilteringOptions> optionsMonitor
+    )
     {
         _next = next ?? throw new ArgumentNullException(nameof(next));
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        _optionsMonitor = optionsMonitor ?? throw new ArgumentNullException(nameof(optionsMonitor));
-        _options = _optionsMonitor.CurrentValue;
-        _optionsMonitor.OnChange(options =>
-        {
-            // Clear the cached settings so the next EnsureConfigured will re-evaluate.
-            _options = options;
-            _allowedHosts = new List<StringSegment>();
-            _allowAnyNonEmptyHost = null;
-        });
+        _middlewareConfigurationManager = new MiddlewareConfigurationManager(optionsMonitor ?? throw new ArgumentNullException(nameof(optionsMonitor)), logger);
     }
 
     /// <summary>
@@ -62,33 +55,21 @@ public class HostFilteringMiddleware
     /// <returns></returns>
     public Task Invoke(HttpContext context)
     {
-        var allowedHosts = EnsureConfigured();
+        var middlewareConfiguration = _middlewareConfigurationManager.GetLatestMiddlewareConfiguration();
 
-        if (!CheckHost(context, allowedHosts))
+        if (!CheckHost(context, middlewareConfiguration))
         {
-            return HostValidationFailed(context);
+            return HostValidationFailed(context, middlewareConfiguration);
         }
 
         return _next(context);
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private IList<StringSegment> EnsureConfigured()
-    {
-        if (_allowAnyNonEmptyHost == true || _allowedHosts?.Count > 0)
-        {
-            Debug.Assert(_allowedHosts != null);
-            return _allowedHosts;
-        }
-
-        return Configure();
-    }
-
     [MethodImpl(MethodImplOptions.NoInlining)]
-    private Task HostValidationFailed(HttpContext context)
+    private static Task HostValidationFailed(HttpContext context, MiddlewareConfiguration middlewareConfiguration)
     {
         context.Response.StatusCode = 400;
-        if (_options.IncludeFailureMessage)
+        if (middlewareConfiguration.IncludeFailureMessage)
         {
             context.Response.ContentLength = DefaultResponse.Length;
             context.Response.ContentType = "text/html";
@@ -97,84 +78,34 @@ public class HostFilteringMiddleware
         return Task.CompletedTask;
     }
 
-    private IList<StringSegment> Configure()
-    {
-        var allowedHosts = new List<StringSegment>();
-        if (_options.AllowedHosts?.Count > 0 && !TryProcessHosts(_options.AllowedHosts, allowedHosts))
-        {
-            _logger.WildcardDetected();
-            _allowedHosts = allowedHosts;
-            _allowAnyNonEmptyHost = true;
-            return _allowedHosts;
-        }
-
-        if (allowedHosts.Count == 0)
-        {
-            throw new InvalidOperationException("No allowed hosts were configured.");
-        }
-
-        if (_logger.IsEnabled(LogLevel.Debug))
-        {
-            _logger.AllowedHosts(string.Join("; ", allowedHosts));
-        }
-
-        _allowedHosts = allowedHosts;
-        return _allowedHosts;
-    }
-
-    // returns false if any wildcards were found
-    private static bool TryProcessHosts(IEnumerable<string> incoming, IList<StringSegment> results)
-    {
-        foreach (var entry in incoming)
-        {
-            // Punycode. Http.Sys requires you to register Unicode hosts, but the headers contain punycode.
-            var host = new HostString(entry).ToUriComponent();
-
-            if (IsTopLevelWildcard(host))
-            {
-                // Disable filtering
-                return false;
-            }
-
-            if (!results.Contains(host, StringSegmentComparer.OrdinalIgnoreCase))
-            {
-                results.Add(host);
-            }
-        }
-
-        return true;
-    }
-
-    private static bool IsTopLevelWildcard(string host)
-    {
-        return (string.Equals("*", host, StringComparison.Ordinal) // HttpSys wildcard
-                       || string.Equals("[::]", host, StringComparison.Ordinal) // Kestrel wildcard, IPv6 Any
-                       || string.Equals("0.0.0.0", host, StringComparison.Ordinal)); // IPv4 Any
-    }
-
     // This does not duplicate format validations that are expected to be performed by the host.
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private bool CheckHost(HttpContext context, IList<StringSegment> allowedHosts)
+    private bool CheckHost(HttpContext context, MiddlewareConfiguration middlewareConfiguration)
     {
         var host = context.Request.Headers.Host.ToString();
 
         if (host.Length == 0)
         {
-            return IsEmptyHostAllowed(context);
+            return IsEmptyHostAllowed(context, middlewareConfiguration);
         }
 
-        if (_allowAnyNonEmptyHost == true)
+        if (middlewareConfiguration.AllowAnyNonEmptyHost)
         {
             _logger.AllHostsAllowed();
             return true;
         }
 
-        return CheckHostInAllowList(allowedHosts, host);
+        return CheckHostInAllowList(middlewareConfiguration.AllowedHosts, host);
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    private bool CheckHostInAllowList(IList<StringSegment> allowedHosts, string host)
+    private bool CheckHostInAllowList(IList<StringSegment>? allowedHosts, string host)
     {
+        if (allowedHosts is null)
+        {
+            return false;
+        }
+
         if (HostString.MatchesAny(new StringSegment(host), allowedHosts))
         {
             _logger.AllowedHostMatched(host);
@@ -186,16 +117,119 @@ public class HostFilteringMiddleware
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    private bool IsEmptyHostAllowed(HttpContext context)
+    private bool IsEmptyHostAllowed(HttpContext context, MiddlewareConfiguration middlewareConfiguration)
     {
         // Http/1.0 does not require the host header.
         // Http/1.1 requires the header but the value may be empty.
-        if (!_options.AllowEmptyHosts)
+        if (!middlewareConfiguration.AllowEmptyHosts)
         {
             _logger.RequestRejectedMissingHost(context.Request.Protocol);
             return false;
         }
         _logger.RequestAllowedMissingHost(context.Request.Protocol);
         return true;
+    }
+
+    internal sealed record MiddlewareConfiguration(bool AllowAnyNonEmptyHost, bool AllowEmptyHosts, bool IncludeFailureMessage, ReadOnlyCollection<StringSegment>? AllowedHosts = default);
+
+    internal sealed class MiddlewareConfigurationManager
+    {
+        private readonly object _syncObject = new();
+        private event Func<MiddlewareHostFilteringOptions>? _changeRequest;
+        private MiddlewareConfiguration _middlewareConfiguration;
+        private readonly ILogger<HostFilteringMiddleware> _logger;
+
+        internal MiddlewareConfigurationManager(IOptionsMonitor<HostFilteringOptions> _optionsMonitor, ILogger<HostFilteringMiddleware> logger)
+        {
+            _logger = logger;
+            _middlewareConfiguration = ConfigureMiddleware(new(_optionsMonitor.CurrentValue));
+            _optionsMonitor.OnChange(options =>
+            {
+                lock (_syncObject)
+                {
+                    var middlewareOptions = new MiddlewareHostFilteringOptions(options);
+                    _changeRequest = () => middlewareOptions;
+                }
+            });
+        }
+        internal MiddlewareConfiguration GetLatestMiddlewareConfiguration()
+        {
+            if (_changeRequest is not null)
+            {
+                MiddlewareHostFilteringOptions options;
+                lock (_syncObject)
+                {
+                    options = _changeRequest();
+                    _changeRequest = null;
+                }
+                _middlewareConfiguration = ConfigureMiddleware(options);
+                return _middlewareConfiguration;
+            }
+            if (_middlewareConfiguration.AllowAnyNonEmptyHost == true || _middlewareConfiguration.AllowedHosts?.Count > 0)
+            {
+                return _middlewareConfiguration;
+            }
+            if (_middlewareConfiguration.AllowedHosts is null || _middlewareConfiguration.AllowedHosts?.Count == 0)
+            {
+                throw new InvalidOperationException("No allowed hosts were configured.");
+            }
+            return _middlewareConfiguration;
+        }
+
+        private MiddlewareConfiguration ConfigureMiddleware(MiddlewareHostFilteringOptions options)
+        {
+            var allowedHosts = new List<StringSegment>();
+            if (
+                options.AllowedHosts?.Count > 0
+                && !TryProcessHosts(options.AllowedHosts, allowedHosts)
+            )
+            {
+                _logger.WildcardDetected();
+                return new(true, options.AllowEmptyHosts, options.IncludeFailureMessage);
+            }
+
+            if (_logger.IsEnabled(LogLevel.Debug))
+            {
+                _logger.AllowedHosts(string.Join("; ", allowedHosts));
+            }
+            return new(false, options.AllowEmptyHosts, options.IncludeFailureMessage, allowedHosts.AsReadOnly());
+        }
+
+        private static bool TryProcessHosts(IEnumerable<string> incoming, List<StringSegment> results)
+        {
+            foreach (var entry in incoming)
+            {
+                // Punycode. Http.Sys requires you to register Unicode hosts, but the headers contain punycode.
+                var host = new HostString(entry).ToUriComponent();
+
+                if (IsTopLevelWildcard(host))
+                {
+                    // Disable filtering
+                    return false;
+                }
+
+                if (!results.Contains(host, StringSegmentComparer.OrdinalIgnoreCase))
+                {
+                    results.Add(host);
+                }
+            }
+            return true;
+        }
+
+        private static bool IsTopLevelWildcard(string host)
+        {
+            return (
+                string.Equals("*", host, StringComparison.Ordinal) // HttpSys wildcard
+                || string.Equals("[::]", host, StringComparison.Ordinal) // Kestrel wildcard, IPv6 Any
+                || string.Equals("0.0.0.0", host, StringComparison.Ordinal)
+            ); // IPv4 Any
+        }
+
+        private sealed record MiddlewareHostFilteringOptions(ReadOnlyCollection<string> AllowedHosts, bool AllowEmptyHosts, bool IncludeFailureMessage)
+        {
+            public MiddlewareHostFilteringOptions(HostFilteringOptions options) : this(options.AllowedHosts.AsReadOnly(), options.AllowEmptyHosts, options.IncludeFailureMessage)
+            {
+            }
+        };
     }
 }
--- src/Middleware/HostFiltering/test/HostFilteringMiddlewareTests.cs
+++ src/Middleware/HostFiltering/test/HostFilteringMiddlewareTests.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
 using Microsoft.Net.Http.Headers;
@@ -263,6 +264,113 @@ public class HostFilteringMiddlewareTests
 
         response = await server.CreateRequest("/").GetAsync();
         Assert.Equal(200, (int)response.StatusCode);
+    }
+
+    [Theory]
+    [InlineData("*", true, true)]
+    [InlineData("[::]", true, true)]
+    [InlineData("localHost;*", true, true)]
+    [InlineData("localHost;foo.example.com.bar:443", false, true)]
+    [InlineData("localHost;foo.example.com.bar:443", true, false)]
+    public void MiddlewareConfigurationManagerSupportsDynamicOptionsReloadChangeRequestedDifferentNewObjectReturned(string allowedHost, bool allowEmptyHosts, bool includeFailureMessage)
+    {
+        var options = new HostFilteringOptions()
+        {
+            AllowedHosts = new List<string>() { "*" },
+            AllowEmptyHosts = true,
+            IncludeFailureMessage = true
+        };
+
+        var optionsMonitor = new OptionsWrapperMonitor<HostFilteringOptions>(options);
+
+        var sut = new HostFilteringMiddleware.MiddlewareConfigurationManager(optionsMonitor, new NullLogger<HostFilteringMiddleware>());
+
+        var configurationBeforeChange = sut.GetLatestMiddlewareConfiguration();
+
+        Assert.NotNull(configurationBeforeChange);
+        Assert.Equal(configurationBeforeChange.AllowAnyNonEmptyHost, configurationBeforeChange.AllowedHosts is null);
+        Assert.Equal(options.AllowEmptyHosts, configurationBeforeChange.AllowEmptyHosts);
+        Assert.Equal(options.IncludeFailureMessage, configurationBeforeChange.IncludeFailureMessage);
+        if (configurationBeforeChange.AllowAnyNonEmptyHost)
+        {
+            Assert.True(configurationBeforeChange.AllowedHosts is null);
+        }
+        else
+        {
+            Assert.True(options.AllowedHosts.All(x => configurationBeforeChange.AllowedHosts.Contains(x)));
+        }
+
+        var newOption = new HostFilteringOptions
+        {
+            AllowedHosts = allowedHost.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries),
+            AllowEmptyHosts = allowEmptyHosts,
+            IncludeFailureMessage = includeFailureMessage
+        };
+
+        optionsMonitor.InvokeChanged(newOption);
+
+        var configurationAfterChange = sut.GetLatestMiddlewareConfiguration();
+
+        Assert.NotNull(configurationAfterChange);
+        Assert.Equal(newOption.AllowEmptyHosts, configurationAfterChange.AllowEmptyHosts);
+        Assert.Equal(newOption.IncludeFailureMessage, configurationAfterChange.IncludeFailureMessage);
+        if (configurationAfterChange.AllowAnyNonEmptyHost)
+        {
+            Assert.True(configurationAfterChange.AllowedHosts is null);
+        }
+        else
+        {
+            Assert.True(newOption.AllowedHosts.All(x => configurationAfterChange.AllowedHosts.Contains(x)));
+        }
+
+        Assert.False(ReferenceEquals(configurationBeforeChange, configurationAfterChange));
+    }
+
+    [Fact]
+    public void MiddlewareConfigurationManagerSupportsDynamicOptionsReloadChangeNotRequestedTheSameObjectReturned()
+    {
+        var options = new HostFilteringOptions()
+        {
+            AllowedHosts = new List<string>() { "localhost;foo.example.com.bar:443" },
+            AllowEmptyHosts = true,
+            IncludeFailureMessage = true
+        };
+
+        var optionsMonitor = new OptionsWrapperMonitor<HostFilteringOptions>(options);
+
+        var sut = new HostFilteringMiddleware.MiddlewareConfigurationManager(optionsMonitor, new NullLogger<HostFilteringMiddleware>());
+
+        var result1 = sut.GetLatestMiddlewareConfiguration();
+
+        var result2 = sut.GetLatestMiddlewareConfiguration();
+
+        Assert.Equal(result1, result2);
+        Assert.True(ReferenceEquals(result1, result2));
+    }
+
+    internal class OptionsWrapperMonitor<T> : IOptionsMonitor<T>
+    {
+        private event Action<T, string> _listener;
+
+        public OptionsWrapperMonitor(T currentValue)
+        {
+            CurrentValue = currentValue;
+        }
+
+        public IDisposable OnChange(Action<T, string> listener)
+        {
+            _listener = listener;
+            return null;
+        }
+
+        public T Get(string name) => CurrentValue;
+
+        public T CurrentValue { get; }
+
+        internal void InvokeChanged(T obj)
+        {
+            _listener.Invoke(obj, null);
+        }
     }
 
     private class ReloadableMemorySource : IConfigurationSource
