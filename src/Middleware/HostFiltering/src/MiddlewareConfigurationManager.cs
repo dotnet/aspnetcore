82cf9194534c02a66fb88a66b98e6b0dfd33d965
Kamil Charytoniuk <charytech@gmail.com>
2023-11-23 14:04:34 +0100

    PR comments

--- src/Middleware/HostFiltering/src/MiddlewareConfigurationManager.cs
+++ src/Middleware/HostFiltering/src/MiddlewareConfigurationManager.cs
@@ -14,47 +14,27 @@ internal sealed record MiddlewareConfiguration(bool AllowAnyNonEmptyHost, bool A
 
 internal sealed class MiddlewareConfigurationManager
 {
-    private readonly object _syncObject = new();
-    private event Func<HostFilteringOptions>? _getChangeRequestObject;
-    private MiddlewareConfiguration? _middlewareConfiguration;
+    private MiddlewareConfiguration _middlewareConfiguration;
     private readonly ILogger<HostFilteringMiddleware> _logger;
 
     internal MiddlewareConfigurationManager(IOptionsMonitor<HostFilteringOptions> _optionsMonitor, ILogger<HostFilteringMiddleware> logger)
     {
         _logger = logger;
-        // Configuration will evaluate during first request
-        _getChangeRequestObject = () => _optionsMonitor.CurrentValue;
+        _middlewareConfiguration = ConfigureMiddleware(_optionsMonitor.CurrentValue);
         _optionsMonitor.OnChange(options =>
         {
-            lock (_syncObject)
-            {
-                _getChangeRequestObject = () => options;
-                if (_logger.IsEnabled(LogLevel.Debug))
-                {
-                    _logger.OptionsChanged(GetLogMessageForHostFilteringOptions(options));
-                }
-            }
+            _middlewareConfiguration = ConfigureMiddleware(options);
         });
     }
     internal MiddlewareConfiguration GetLatestMiddlewareConfiguration()
     {
-        if (_getChangeRequestObject is not null)
-        {
-            HostFilteringOptions options;
-            lock (_syncObject)
-            {
-                options = _getChangeRequestObject();
-                _getChangeRequestObject = null;
-            }
-            _middlewareConfiguration = ConfigureMiddleware(options);
-        }
-
-        if (_middlewareConfiguration?.AllowAnyNonEmptyHost != true && (_middlewareConfiguration?.AllowedHosts is null || _middlewareConfiguration.AllowedHosts?.Count == 0))
+        var config = _middlewareConfiguration;
+        if (!config.AllowAnyNonEmptyHost && (config.AllowedHosts is null || config.AllowedHosts.Count == 0))
         {
             throw new InvalidOperationException("No allowed hosts were configured.");
         }
 
-        return _middlewareConfiguration;
+        return config;
     }
 
     private MiddlewareConfiguration ConfigureMiddleware(HostFilteringOptions options)
@@ -66,10 +46,7 @@ internal sealed class MiddlewareConfigurationManager
             _logger.MiddlewareConfigurationStarted(GetLogMessageForHostFilteringOptions(options));
         }
 
-        if (
-            options.AllowedHosts?.Count > 0
-            && !TryProcessHosts(options.AllowedHosts, allowedHosts)
-        )
+        if (options.AllowedHosts?.Count > 0 && !TryProcessHosts(options.AllowedHosts, allowedHosts))
         {
             _logger.WildcardDetected();
             return new(true, options.AllowEmptyHosts, options.IncludeFailureMessage);
@@ -107,12 +84,11 @@ internal sealed class MiddlewareConfigurationManager
 
     private static bool IsTopLevelWildcard(string host)
     {
-        return (
-            string.Equals("*", host, StringComparison.Ordinal) // HttpSys wildcard
+        return string.Equals("*", host, StringComparison.Ordinal) // HttpSys wildcard
             || string.Equals("[::]", host, StringComparison.Ordinal) // Kestrel wildcard, IPv6 Any
-            || string.Equals("0.0.0.0", host, StringComparison.Ordinal)
-        ); // IPv4 Any
+            || string.Equals("0.0.0.0", host, StringComparison.Ordinal);// IPv4 Any
     }
+
     private static string GetLogMessageForHostFilteringOptions(HostFilteringOptions hostFilteringOptions)
     {
         return $"{{AllowedHosts = {string.Join("; ", hostFilteringOptions.AllowedHosts)}, AllowEmptyHosts = {hostFilteringOptions.AllowEmptyHosts}, IncludeFailureMessage = {hostFilteringOptions.IncludeFailureMessage}}}";
