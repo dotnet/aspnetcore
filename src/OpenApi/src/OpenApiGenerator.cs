444aa9ec575ef807770de4eff9aa508edd00f37c
Brian Rourke Boll <brianrourkeboll@users.noreply.github.com>
2023-11-27 16:48:33 -0500

    Add support for F# `Async<'T>` as an awaitable type in minimal APIs (#46898)
    * Remove unneeded unboxing/boxing
    
    * Add doc comment
    
    * Remove unused using
    
    * Convert F# Async<> to Task<> in request delegates
    
    * Use `CoercedAwaitableInfo` to convert `FSharp.Control.FSharpAsync<T>`
      values to `System.Threading.Tasks.Task<TResult>` when building request
      delegates and populate endpoint metadata accordingly.
    
    * Add tests for F# async request delegate support
    
    * F# async not supported by source generator
    
    * Add a comment to the request delegate source generator tests that
      explicitly calls out that F# async is not currently supported by the
      source generator.
    
    * Actually remove extra convert call
    
    * Shrink diff
    
    * Use CoercedAwaitableInfo in OpenApi gen
    
    * Use CoercedAwaitableInfo in EndpointMetadataApiDescriptionProvider
    
    * Add FSharpAsync tests for EndpointMetadataApiDescriptionProvider
    
    * Add runtime creation test for FSharpAsync
    
    * Convert unit awaitables to void awaitables
    
    * FSharpAsync<unit> → Task
    
    * Task<unit> → Task
    
    * ValueTask<unit> → ValueTask
    
    * Add tests for unit → void awaitable conversions
    
    * Make consistent
    
    * Don't need
    
    * Handle boxed IResult
    
    * Simplify
    
    * Add runtime tests back
    
    * Use affirmative form
    
    ---------
    
    Co-authored-by: Safia Abdalla <safia@microsoft.com>

--- src/OpenApi/src/OpenApiGenerator.cs
+++ src/OpenApi/src/OpenApiGenerator.cs
@@ -98,9 +98,9 @@ internal sealed class OpenApiGenerator
     {
         var responses = new OpenApiResponses();
         var responseType = method.ReturnType;
-        if (AwaitableInfo.IsTypeAwaitable(responseType, out var awaitableInfo))
+        if (CoercedAwaitableInfo.IsTypeAwaitable(responseType, out var coercedAwaitableInfo))
         {
-            responseType = awaitableInfo.ResultType;
+            responseType = coercedAwaitableInfo.AwaitableInfo.ResultType;
         }
 
         if (typeof(IResult).IsAssignableFrom(responseType))
