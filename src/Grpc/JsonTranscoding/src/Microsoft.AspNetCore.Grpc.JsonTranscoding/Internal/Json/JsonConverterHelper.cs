2db1f18cbf97b6fcf5b58ebf508ae37a22e40228
James Newton-King <james@newtonking.com>
2023-11-29 12:41:27 +0800

    Better fix

--- src/Grpc/JsonTranscoding/src/Microsoft.AspNetCore.Grpc.JsonTranscoding/Internal/Json/JsonConverterHelper.cs
+++ src/Grpc/JsonTranscoding/src/Microsoft.AspNetCore.Grpc.JsonTranscoding/Internal/Json/JsonConverterHelper.cs
@@ -146,7 +146,7 @@ internal static class JsonConverterHelper
         var existingValue = (IDictionary)fieldDescriptor.Accessor.GetValue(message);
         foreach (DictionaryEntry item in newValues)
         {
-            existingValue[item.Key] = item.Value ?? Value.ForNull();
+            existingValue[item.Key] = item.Value;
         }
     }
 
