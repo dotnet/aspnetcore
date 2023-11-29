4e1f2af679e4d09d131348584bbde80c01f1a247
Andrew Casey <amcasey@users.noreply.github.com>
2023-11-29 12:20:23 -0800

    Quarantine BidirectionalStream_ServerReadsDataAndCompletes_GracefullyClosed

--- src/Servers/Kestrel/Transport.NamedPipes/test/NamedPipeConnectionTests.cs
+++ src/Servers/Kestrel/Transport.NamedPipes/test/NamedPipeConnectionTests.cs
@@ -14,7 +14,7 @@ public class NamedPipeConnectionTests : TestApplicationErrorLoggerLoggedTest
 {
     private static readonly byte[] TestData = Encoding.UTF8.GetBytes("Hello world");
 
-    [Repeat(100)]
+    [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/52462")]
     [ConditionalFact]
     public async Task BidirectionalStream_ServerReadsDataAndCompletes_GracefullyClosed()
     {
