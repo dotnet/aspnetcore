000e79b0756f68ba75df718028d83620b3c8586c
Andrew Casey <amcasey@users.noreply.github.com>
2023-11-29 14:12:22 -0800

    Quarantine ServerReset_BeforeRequestBody_ClientBodyThrows

--- src/Servers/Kestrel/test/Interop.FunctionalTests/HttpClientHttp2InteropTests.cs
+++ src/Servers/Kestrel/test/Interop.FunctionalTests/HttpClientHttp2InteropTests.cs
@@ -689,7 +689,7 @@ public class HttpClientHttp2InteropTests : LoggedTest
         await host.StopAsync().DefaultTimeout();
     }
 
-    [Repeat(100)]
+    [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/46717")]
     [Theory]
     [MemberData(nameof(SupportedSchemes))]
     public async Task ServerReset_BeforeRequestBody_ClientBodyThrows(string scheme)
