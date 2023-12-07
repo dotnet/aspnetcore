78d835756b75f8e56bc05e06b9a02869294694a9
Andrew Casey <andrew.casey@microsoft.com>
2023-12-07 14:13:32 -0800

    Make IDataProtectionKeyContext settable
    Not requiring it is a [footgun](https://github.com/dotnet/aspnetcore/issues/12333#issuecomment-1845881131)

--- src/DataProtection/EntityFrameworkCore/src/IDataProtectionKeyContext.cs
+++ src/DataProtection/EntityFrameworkCore/src/IDataProtectionKeyContext.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.EntityFrameworkCore;
@@ -13,5 +13,5 @@ public interface IDataProtectionKeyContext
     /// <summary>
     /// A collection of <see cref="DataProtectionKey"/>
     /// </summary>
-    DbSet<DataProtectionKey> DataProtectionKeys { get; }
+    DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
 }
