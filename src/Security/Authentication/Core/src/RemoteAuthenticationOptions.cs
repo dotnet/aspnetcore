e0e1d582a993feddf346e7224c7fbd712376d91f
joegoldman2 <147369450+joegoldman2@users.noreply.github.com>
2023-12-11 18:33:33 +0200

    Fix XML documentation of CorrelationCookie and NonceCookie (#52715)

--- src/Security/Authentication/Core/src/RemoteAuthenticationOptions.cs
+++ src/Security/Authentication/Core/src/RemoteAuthenticationOptions.cs
@@ -146,7 +146,7 @@ public class RemoteAuthenticationOptions : AuthenticationSchemeOptions
     /// <item><description><see cref="CookieBuilder.SameSite"/> defaults to <see cref="SameSiteMode.None"/>.</description></item>
     /// <item><description><see cref="CookieBuilder.HttpOnly"/> defaults to <c>true</c>.</description></item>
     /// <item><description><see cref="CookieBuilder.IsEssential"/> defaults to <c>true</c>.</description></item>
-    /// <item><description><see cref="CookieBuilder.SecurePolicy"/> defaults to <see cref="CookieSecurePolicy.SameAsRequest"/>.</description></item>
+    /// <item><description><see cref="CookieBuilder.SecurePolicy"/> defaults to <see cref="CookieSecurePolicy.Always"/>.</description></item>
     /// </list>
     /// </remarks>
     public CookieBuilder CorrelationCookie
