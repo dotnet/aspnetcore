4c5ec382f232df1f59fbdbd57a9875c58e57092b
Stephen Halter <halter73@gmail.com>
2024-01-09 16:52:11 -0800

    SymmetricSecurityKey needs 32 bytes
    - This could also be worked around with an AppContext switch: "Switch.Microsoft.IdentityModel.UnsafeRelaxHmacKeySizeValidation"
    - See https://github.com/AzureAD/azure-activedirectory-identitymodel-extensions-for-dotnet/wiki/IDX10720 for more info

--- src/SignalR/common/Http.Connections/test/HttpConnectionDispatcherTests.cs
+++ src/SignalR/common/Http.Connections/test/HttpConnectionDispatcherTests.cs
@@ -13,6 +13,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.WebSockets;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Security.Principal;
 using System.Text;
 using System.Threading;
@@ -2762,7 +2763,7 @@ namespace Microsoft.AspNetCore.Http.Connections.Tests
         [InlineData(HttpTransportType.WebSockets)]
         public async Task AuthenticationExpirationSetOnAuthenticatedConnectionWithJWT(HttpTransportType transportType)
         {
-            SymmetricSecurityKey SecurityKey = new SymmetricSecurityKey(Guid.NewGuid().ToByteArray());
+            SymmetricSecurityKey SecurityKey = new SymmetricSecurityKey(SHA256.HashData(Guid.NewGuid().ToByteArray()));
             JwtSecurityTokenHandler JwtTokenHandler = new JwtSecurityTokenHandler();
 
             using var host = CreateHost(services =>
@@ -2924,7 +2925,7 @@ namespace Microsoft.AspNetCore.Http.Connections.Tests
         [InlineData(HttpTransportType.WebSockets)]
         public async Task AuthenticationExpirationUsesCorrectScheme(HttpTransportType transportType)
         {
-            var SecurityKey = new SymmetricSecurityKey(Guid.NewGuid().ToByteArray());
+            var SecurityKey = new SymmetricSecurityKey(SHA256.HashData(Guid.NewGuid().ToByteArray()));
             var JwtTokenHandler = new JwtSecurityTokenHandler();
 
             using var host = CreateHost(services =>
