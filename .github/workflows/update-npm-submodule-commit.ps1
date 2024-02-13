42aba32f39e55a7651860a48cd592def65643564
jacalvar <jacalvar@microsoft.com>
2024-02-13 19:46:36 +0100

    Path fixes

--- .github/workflows/update-npm-submodule-commit.ps1
+++ .github/workflows/update-npm-submodule-commit.ps1
@@ -3,7 +3,9 @@ param(
     [string]$CommitSha
 )
 
+$repoRoot = Resolve-Path -Path "$PSScriptRoot/../..";
+$submodulePath = Join-Path -Path $repoRoot -ChildPath "src/submodules/Node-Externals"
 # Fetch and checkout the commit sha in the submodule in src/submodules/Node-Externals
-Set-Location src/submodules/Node-Externals
+Set-Location $submodulePath
 git fetch origin
 git checkout $CommitSha
