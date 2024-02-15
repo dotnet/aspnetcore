27a1e4a18664dbacea532320e4f16dd69b7bd415
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-02-15 16:12:25 +0000

    Begin adding ThreadingApp.Server

---
+++ src/Components/WebAssembly/testassets/ThreadingApp.Server/Program.cs
@@ -0,0 +1,27 @@
+using ThreadingApp.Server.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
