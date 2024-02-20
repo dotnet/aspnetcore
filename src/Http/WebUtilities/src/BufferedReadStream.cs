f7ad5091d6fbc34f989e1eabc18936d098295f69
gurustron <gurustronpublic@gmail.com>
2024-02-20 21:52:53 +0300

    Fix the ObjectDisposedException.ThrowIf usage

--- src/Http/WebUtilities/src/BufferedReadStream.cs
+++ src/Http/WebUtilities/src/BufferedReadStream.cs
@@ -418,6 +418,6 @@ public class BufferedReadStream : Stream
 
     private void CheckDisposed()
     {
-        ObjectDisposedException.ThrowIf(_disposed, nameof(BufferedReadStream));
+        ObjectDisposedException.ThrowIf(_disposed, this);
     }
 }
