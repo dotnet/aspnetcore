9dc43ee05cba5cad16f7ba56cb88711e7096bf84
Andrew J Said <andrewjsaid@gmail.com>
2024-02-20 15:08:47 +0000

    [Routing] Speed up ILEmitTrie jump table using Binary Search Tree (#52928)
    * Uses binary search for 4 or more entries in a given table.
    * Matches input on length first.
    * Uses binary search when performing vectorized comparisons.
    * Uses binary search on single character comparisons and optimizes the code when all characters are letters.

--- src/Http/Routing/test/UnitTests/Matching/ILEmitTrieJumpTableTest.cs
+++ src/Http/Routing/test/UnitTests/Matching/ILEmitTrieJumpTableTest.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Moq;
@@ -51,7 +51,7 @@ public abstract class ILEmitTreeJumpTableTestBase : MultipleEntryJumpTableTest
     [InlineData("\u007F", "he\u007Flo-world", 2, 1)]
     [InlineData("\uFFFF", "he\uFFFFlo-world", 2, 1)]
 
-    // non-ASCII character in first section vectorized comparions
+    // non-ASCII character in first section vectorized comparisons
     [InlineData("hel\u007F", "hel\u007Fo-world", 0, 4)]
     [InlineData("hel\uFFFF", "hel\uFFFFo-world", 0, 4)]
     [InlineData("el\u007Fo", "hel\u007Fo-world", 1, 4)]
@@ -69,7 +69,7 @@ public abstract class ILEmitTreeJumpTableTestBase : MultipleEntryJumpTableTest
     [InlineData("llo-\u007F", "hello-\u007Forld", 2, 5)]
     [InlineData("llo-\uFFFF", "hello-\uFFFFForld", 2, 5)]
 
-    // non-ASCII character in first section vectorized comparions
+    // non-ASCII character in first section vectorized comparisons
     [InlineData("hello-w\u007F", "hello-w\u007Forld", 0, 8)]
     [InlineData("hello-w\uFFFF", "hello-w\uFFFForld", 0, 8)]
     [InlineData("ello-w\u007Fo", "hello-w\u007Forld", 1, 8)]
@@ -97,7 +97,7 @@ public abstract class ILEmitTreeJumpTableTestBase : MultipleEntryJumpTableTest
     }
 
     // Tests for difference in casing with ASCII casing rules. Verifies our case
-    // manipulation algorthm is correct.
+    // manipulation algorithm is correct.
     //
     // We convert from upper case to lower
     // 'A' and 'a' are 32 bits apart at the low end
@@ -112,7 +112,7 @@ public abstract class ILEmitTreeJumpTableTestBase : MultipleEntryJumpTableTest
     [InlineData("A", "healo-world", 2, 1)]
     [InlineData("Z", "hezlo-world", 2, 1)]
 
-    // character in first section vectorized comparions
+    // character in first section vectorized comparisons
     [InlineData("helA", "helao-world", 0, 4)]
     [InlineData("helZ", "helzo-world", 0, 4)]
     [InlineData("elAo", "helao-world", 1, 4)]
@@ -130,7 +130,7 @@ public abstract class ILEmitTreeJumpTableTestBase : MultipleEntryJumpTableTest
     [InlineData("llo-A", "hello-aorld", 2, 5)]
     [InlineData("llo-Z", "hello-zForld", 2, 5)]
 
-    // character in first section vectorized comparions
+    // character in first section vectorized comparisons
     [InlineData("hello-wA", "hello-waorld", 0, 8)]
     [InlineData("hello-wZ", "hello-wzorld", 0, 8)]
     [InlineData("ello-wAo", "hello-waorld", 1, 8)]
@@ -158,7 +158,7 @@ public abstract class ILEmitTreeJumpTableTestBase : MultipleEntryJumpTableTest
     }
 
     // Tests for difference in casing with ASCII casing rules. Verifies our case
-    // manipulation algorthm is correct.
+    // manipulation algorithm is correct.
     //
     // We convert from upper case to lower
     // '@' and '`' are 32 bits apart at the low end
@@ -176,7 +176,7 @@ public abstract class ILEmitTreeJumpTableTestBase : MultipleEntryJumpTableTest
     [InlineData("@", "he`lo-world", 2, 1)]
     [InlineData("[", "he{lo-world", 2, 1)]
 
-    // character in first section vectorized comparions
+    // character in first section vectorized comparisons
     [InlineData("hel@", "hel`o-world", 0, 4)]
     [InlineData("hel[", "hel{o-world", 0, 4)]
     [InlineData("el@o", "hel`o-world", 1, 4)]
@@ -194,7 +194,7 @@ public abstract class ILEmitTreeJumpTableTestBase : MultipleEntryJumpTableTest
     [InlineData("llo-@", "hello-`orld", 2, 5)]
     [InlineData("llo-[", "hello-{Forld", 2, 5)]
 
-    // character in first section vectorized comparions
+    // character in first section vectorized comparisons
     [InlineData("hello-w@", "hello-w`orld", 0, 8)]
     [InlineData("hello-w[", "hello-w{orld", 0, 8)]
     [InlineData("ello-w@o", "hello-w`orld", 1, 8)]
@@ -219,5 +219,31 @@ public abstract class ILEmitTreeJumpTableTestBase : MultipleEntryJumpTableTest
 
         // Assert
         Assert.Equal(0, result);
+    }
+
+    // Tests for correct branching in binary search
+    [Theory]
+    [InlineData("hello0 world", "hello1 world", "hello2 world", "hello1 world2")]
+    [InlineData("1111", "2222", "3333", "4444", "5555", "6666", "7777", "8888")] // vectorized
+    [InlineData("1", "7777777", "22", "88888888", "55555", "666666", "333", "4444")]
+    [InlineData("1", "4", "8", "3", "5", "2", "6", "7")] // non-vectorized
+    [InlineData("1", "a", "4", "B", "8", "C", "3", "d", "5", "e", "2", "F", "6", "g", "7")] // mixed letters and numbers
+    [InlineData("@", "1", "a", "4", "B", "8", "C", "3", "d", "5", "e", "2", "F", "6", "g", "7", "`")] // @ and ` are 0x20 apart which means binary search must be disabled
+    public void GetDestination_Found_WithBinarySearch(params string[] segments)
+    {
+        // Arrange
+        var entries = segments.Select((e, i) => (e, i + 1)).ToArray();
+        var table = CreateTable(0, -1, entries);
+
+        foreach (var (segment, destination) in entries)
+        {
+            var pathSegment = new PathSegment(0, segment.Length);
+
+            // Act
+            var result = table.GetDestination(segment, pathSegment);
+
+            // Assert
+            Assert.Equal(destination, result);
+        }
     }
 }
