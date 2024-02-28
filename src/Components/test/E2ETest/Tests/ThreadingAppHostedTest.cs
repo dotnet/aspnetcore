d6708cfeb3ecb0668144c99df67ffc8a6b9c8324
Mackinnon Buck <mackinnon.buck@gmail.com>
2024-02-28 12:17:16 -0800

    Remove unnecessary using

--- src/Components/test/E2ETest/Tests/ThreadingAppHostedTest.cs
+++ src/Components/test/E2ETest/Tests/ThreadingAppHostedTest.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Collections.ObjectModel;
 using Microsoft.AspNetCore.Components.E2ETest.Infrastructure;
 using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
 using Microsoft.AspNetCore.E2ETesting;
