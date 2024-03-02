c7c76d7d3bb30e29e8386d1a3e6dae9e307fd0de
Eric Erhardt <eric.erhardt@microsoft.com>
2024-03-01 20:05:13 -0600

    Update dependencies from https://github.com/dotnet/runtime build 20240301.1 (#54297)
    * Update dependencies from https://github.com/dotnet/runtime build 20240301.1
    
    Microsoft.Bcl.AsyncInterfaces , Microsoft.Bcl.TimeProvider , Microsoft.Extensions.Caching.Abstractions , Microsoft.Extensions.Caching.Memory , Microsoft.Extensions.Configuration , Microsoft.Extensions.Configuration.Abstractions , Microsoft.Extensions.Configuration.Binder , Microsoft.Extensions.Configuration.CommandLine , Microsoft.Extensions.Configuration.EnvironmentVariables , Microsoft.Extensions.Configuration.FileExtensions , Microsoft.Extensions.Configuration.Ini , Microsoft.Extensions.Configuration.Json , Microsoft.Extensions.Configuration.UserSecrets , Microsoft.Extensions.Configuration.Xml , Microsoft.Extensions.DependencyInjection , Microsoft.Extensions.DependencyInjection.Abstractions , Microsoft.Extensions.DependencyModel , Microsoft.Extensions.Diagnostics , Microsoft.Extensions.Diagnostics.Abstractions , Microsoft.Extensions.FileProviders.Abstractions , Microsoft.Extensions.FileProviders.Composite , Microsoft.Extensions.FileProviders.Physical , Microsoft.Extensions.FileSystemGlobbing , Microsoft.Extensions.HostFactoryResolver.Sources , Microsoft.Extensions.Hosting , Microsoft.Extensions.Hosting.Abstractions , Microsoft.Extensions.Http , Microsoft.Extensions.Logging , Microsoft.Extensions.Logging.Abstractions , Microsoft.Extensions.Logging.Configuration , Microsoft.Extensions.Logging.Console , Microsoft.Extensions.Logging.Debug , Microsoft.Extensions.Logging.EventLog , Microsoft.Extensions.Logging.EventSource , Microsoft.Extensions.Logging.TraceSource , Microsoft.Extensions.Options , Microsoft.Extensions.Options.ConfigurationExtensions , Microsoft.Extensions.Options.DataAnnotations , Microsoft.Extensions.Primitives , Microsoft.Internal.Runtime.AspNetCore.Transport , Microsoft.NET.Runtime.MonoAOTCompiler.Task , Microsoft.NET.Runtime.WebAssembly.Sdk , Microsoft.NETCore.App.Ref , Microsoft.NETCore.App.Runtime.AOT.win-x64.Cross.browser-wasm , Microsoft.NETCore.App.Runtime.win-x64 , Microsoft.NETCore.BrowserDebugHost.Transport , Microsoft.NETCore.Platforms , System.Collections.Immutable , System.Composition , System.Configuration.ConfigurationManager , System.Diagnostics.DiagnosticSource , System.Diagnostics.EventLog , System.Diagnostics.PerformanceCounter , System.DirectoryServices.Protocols , System.IO.Hashing , System.IO.Pipelines , System.Net.Http.Json , System.Net.Http.WinHttpHandler , System.Reflection.Metadata , System.Resources.Extensions , System.Runtime.Caching , System.Security.Cryptography.Pkcs , System.Security.Cryptography.Xml , System.Security.Permissions , System.ServiceProcess.ServiceController , System.Text.Encodings.Web , System.Text.Json , System.Threading.AccessControl , System.Threading.Channels , System.Threading.RateLimiting , Microsoft.SourceBuild.Intermediate.runtime.linux-x64
     From Version 9.0.0-preview.2.24114.1 -> To Version 9.0.0-preview.3.24151.1
    
    * Fix ThreadingHostedAppTest failures
    
    * Remove unnecessary using
    
    * Fix ThreadingAppTest failures
    
    * Rollback to 9.0.0-preview.3.24129.5 to isolate the break
    
    * Rollback to 9.0.0-preview.3.24128.10 to try to get a green build.
    
    ---------
    
    Co-authored-by: dotnet-maestro[bot] <dotnet-maestro[bot]@users.noreply.github.com>
    Co-authored-by: Mackinnon Buck <mackinnon.buck@gmail.com>

--- src/Components/test/E2ETest/Tests/ThreadingAppTest.cs
+++ src/Components/test/E2ETest/Tests/ThreadingAppTest.cs
@@ -46,22 +46,22 @@ public class ThreadingAppTest
         var mainHeaderSelector = By.TagName("h1");
 
         // Verify we start at home, with the home link highlighted
-        Assert.Equal("Hello, world!", Browser.Exists(mainHeaderSelector).Text);
-        Assert.Collection(Browser.FindElements(activeNavLinksSelector),
+        Browser.Equal("Hello, world!", () => Browser.Exists(mainHeaderSelector).Text);
+        Browser.Collection(() => Browser.FindElements(activeNavLinksSelector),
             item => Assert.Equal("Home", item.Text.Trim()));
 
         // Click on the "counter" link
         Browser.Exists(By.LinkText("Counter")).Click();
 
         // Verify we're now on the counter page, with that nav link (only) highlighted
-        Assert.Equal("Counter", Browser.Exists(mainHeaderSelector).Text);
-        Assert.Collection(Browser.FindElements(activeNavLinksSelector),
+        Browser.Equal("Counter", () => Browser.Exists(mainHeaderSelector).Text);
+        Browser.Collection(() => Browser.FindElements(activeNavLinksSelector),
             item => Assert.Equal("Counter", item.Text.Trim()));
 
         // Verify we can navigate back to home too
         Browser.Exists(By.LinkText("Home")).Click();
-        Assert.Equal("Hello, world!", Browser.Exists(mainHeaderSelector).Text);
-        Assert.Collection(Browser.FindElements(activeNavLinksSelector),
+        Browser.Equal("Hello, world!", () => Browser.Exists(mainHeaderSelector).Text);
+        Browser.Collection(() => Browser.FindElements(activeNavLinksSelector),
             item => Assert.Equal("Home", item.Text.Trim()));
     }
 
@@ -88,7 +88,7 @@ public class ThreadingAppTest
     {
         // Navigate to "Fetch data"
         Browser.Exists(By.LinkText("Fetch data")).Click();
-        Assert.Equal("Weather forecast", Browser.Exists(By.TagName("h1")).Text);
+        Browser.Equal("Weather forecast", () => Browser.Exists(By.TagName("h1")).Text);
 
         // Wait until loaded
         var tableSelector = By.CssSelector("table.table");
