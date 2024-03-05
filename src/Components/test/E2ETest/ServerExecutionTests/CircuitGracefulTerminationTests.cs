8f9a9c8f704b46f48f925cdd5e5fbfc2ff698f21
Mackinnon Buck <mackinnon.buck@gmail.com>
2024-03-05 15:37:41 -0800

    Remove unnecessary using

--- src/Components/test/E2ETest/ServerExecutionTests/CircuitGracefulTerminationTests.cs
+++ src/Components/test/E2ETest/ServerExecutionTests/CircuitGracefulTerminationTests.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
 using Microsoft.AspNetCore.E2ETesting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging.Testing;
-using Microsoft.AspNetCore.InternalTesting;
 using OpenQA.Selenium;
 using TestServer;
 using Xunit.Abstractions;
