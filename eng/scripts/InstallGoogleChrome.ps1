03113610c02643eb9858aed6fca82f6d69a0c0e0
Matt Mitchell (.NET) <mmitche@microsoft.com>
2024-03-12 14:09:08 -0700

    Change tempdir

--- eng/scripts/InstallGoogleChrome.ps1
+++ eng/scripts/InstallGoogleChrome.ps1
@@ -1,4 +1,6 @@
-$InstallerPath = "$env:Temp\chrome_installer.exe";
+$tempDir = "$repoRoot\obj"
+mkdir $tempDir -ea Ignore | out-null
+$InstallerPath = "$tempDir\chrome_installer.exe";
 & $PSScriptRoot\Download.ps1 "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" $InstallerPath
 Start-Process -FilePath $InstallerPath -Args "/silent /install" -Verb RunAs -Wait;
 Remove-Item $InstallerPath
