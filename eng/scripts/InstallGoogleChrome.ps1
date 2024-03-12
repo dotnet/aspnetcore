732cae4e92bcc3bf9ba3944a40e3ef26e4c5d4dc
Matt Mitchell (.NET) <mmitche@microsoft.com>
2024-03-12 15:23:39 -0700

    Install chrome only on windows

--- eng/scripts/InstallGoogleChrome.ps1
+++ eng/scripts/InstallGoogleChrome.ps1
@@ -1,6 +1,4 @@
-$tempDir = "$repoRoot\obj"
-mkdir $tempDir -ea Ignore | out-null
-$InstallerPath = "$tempDir\chrome_installer.exe";
+$InstallerPath = "$env:Temp\chrome_installer.exe";
 & $PSScriptRoot\Download.ps1 "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" $InstallerPath
 Start-Process -FilePath $InstallerPath -Args "/silent /install" -Verb RunAs -Wait;
 Remove-Item $InstallerPath
