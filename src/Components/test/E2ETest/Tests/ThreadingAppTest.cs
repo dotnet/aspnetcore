45d655694c7d3dca8531d1b8cf59b41cab8ec027
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-03-28 11:28:54 +0000

    Don't try to interact with DOM after threaded app shutdown

--- src/Components/test/E2ETest/Tests/ThreadingAppTest.cs
+++ src/Components/test/E2ETest/Tests/ThreadingAppTest.cs
@@ -11,7 +11,7 @@ using Xunit.Abstractions;
 namespace Microsoft.AspNetCore.Components.E2ETest.Tests;
 
 public class ThreadingAppTest
-    : ServerTestBase<BlazorWasmTestAppFixture<ThreadingApp.Program>>, IDisposable
+    : ServerTestBase<BlazorWasmTestAppFixture<ThreadingApp.Program>>
 {
     public ThreadingAppTest(
         BrowserFixture browserFixture,
@@ -126,12 +126,5 @@ public class ThreadingAppTest
     {
         var app = Browser.Exists(By.TagName("app"));
         Browser.NotEqual("Loading...", () => app.Text);
-    }
-
-    public void Dispose()
-    {
-        // Make the tests run faster by navigating back to the home page when we are done
-        // If we don't, then the next test will reload the whole page before it starts
-        Browser.Exists(By.LinkText("Home")).Click();
     }
 }
