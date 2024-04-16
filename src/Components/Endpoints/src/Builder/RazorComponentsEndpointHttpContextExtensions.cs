905e99a5ccb20715f13f688c11581af67710cc84
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-04-16 11:03:16 +0100

    Remove GetPageComponentType extension

--- src/Components/Endpoints/src/Builder/RazorComponentsEndpointHttpContextExtensions.cs
+++ src/Components/Endpoints/src/Builder/RazorComponentsEndpointHttpContextExtensions.cs
@@ -20,15 +20,6 @@ public static class RazorComponentsEndpointHttpContextExtensions
     private static readonly ConcurrentDictionary<Type, bool> AllowsInteractiveRoutingCache = new();
 
     /// <summary>
-    /// If the current endpoint is a Razor component, returns the component type.
-    /// Otherwise, returns <see langword="null" />.
-    /// </summary>
-    /// <param name="context">The <see cref="HttpContext"/>.</param>
-    /// <returns>The page component type if the current endpoint is a Razor component; otherwise <see langword="null" />.</returns>
-    public static Type? GetPageComponentType(this HttpContext? context)
-        => context?.GetEndpoint()?.Metadata.GetMetadata<ComponentTypeMetadata>()?.Type;
-
-    /// <summary>
     /// Determines whether the current endpoint is a Razor component that can be reached through
     /// interactive routing. This is true for all page components except if they declare the
     /// attribute <see cref="AllowInteractiveRoutingAttribute"/> with value <see langword="false"/>.
@@ -37,10 +28,12 @@ public static class RazorComponentsEndpointHttpContextExtensions
     /// <returns>True if the current endpoint is a Razor component that does not declare <see cref="AllowInteractiveRoutingAttribute"/> with value <see langword="false"/>.</returns>
     public static bool AllowsInteractiveRouting(this HttpContext? context)
     {
-        return GetPageComponentType(context) is { } type
+        var pageType = context?.GetEndpoint()?.Metadata.GetMetadata<ComponentTypeMetadata>()?.Type;
+
+        return pageType is not null
             && AllowsInteractiveRoutingCache.GetOrAdd(
-                type,
-                type.GetCustomAttribute<AllowInteractiveRoutingAttribute>() is not { Allow: false });
+                pageType,
+                pageType.GetCustomAttribute<AllowInteractiveRoutingAttribute>() is not { Allow: false });
     }
 
     internal static class MetadataUpdateHandler
