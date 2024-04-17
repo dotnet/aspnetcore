96134fc1f20b661b104713f5870a56d6fb053b12
Piotr Wosiek <piotr.m.wosiek@gmail.com>
2024-04-17 21:06:44 +0200

    Add missing TimeProvider registration (#53946)

--- src/Identity/Core/src/IdentityServiceCollectionExtensions.cs
+++ src/Identity/Core/src/IdentityServiceCollectionExtensions.cs
@@ -171,12 +171,14 @@ public static class IdentityServiceCollectionExtensions
 
     private sealed class PostConfigureSecurityStampValidatorOptions : IPostConfigureOptions<SecurityStampValidatorOptions>
     {
-        public PostConfigureSecurityStampValidatorOptions(TimeProvider timeProvider)
+        public PostConfigureSecurityStampValidatorOptions(TimeProvider? timeProvider = null)
         {
+            // We could assign this to "timeProvider ?? TimeProvider.System", but
+            // SecurityStampValidator already has system clock fallback logic.
             TimeProvider = timeProvider;
         }
 
-        private TimeProvider TimeProvider { get; }
+        private TimeProvider? TimeProvider { get; }
 
         public void PostConfigure(string? name, SecurityStampValidatorOptions options)
         {
