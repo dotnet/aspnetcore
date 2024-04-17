ffa0e0a5264c2347726e5ded03ae3737477ea777
Marc Gravell <marc.gravell@gmail.com>
2024-04-17 15:51:24 +0100

    hybrid cache baseline perf

--- src/Caching/perf/Microsoft.Extensions.Caching.Benchmarks/Program.cs
+++ src/Caching/perf/Microsoft.Extensions.Caching.Benchmarks/Program.cs
@@ -4,42 +4,55 @@ using Microsoft.Extensions.Caching.Benchmarks;
 
 #if DEBUG
 // validation
-using var obj = new DistributedCacheBenchmarks { PayloadSize = 11512, Sliding = true };
-Console.WriteLine($"Expected: {obj.PayloadSize}*{DistributedCacheBenchmarks.OperationsPerInvoke} = {obj.PayloadSize * DistributedCacheBenchmarks.OperationsPerInvoke}");
-Console.WriteLine();
+using (var hc = new HybridCacheBenchmarks())
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine((await hc.HitDistributedCache()).Name);
+        Console.WriteLine((await hc.HitHybridCache()).Name);
+        Console.WriteLine((await hc.HitHybridCacheImmutable()).Name);
+    }
+}
 
-obj.Backend = DistributedCacheBenchmarks.BackendType.Redis;
-obj.GlobalSetup();
-Console.WriteLine(obj.GetSingleRandom());
-Console.WriteLine(obj.GetSingleFixed());
-Console.WriteLine(obj.GetSingleRandomBuffer());
-Console.WriteLine(obj.GetSingleFixedBuffer());
-Console.WriteLine(obj.GetConcurrentRandom());
-Console.WriteLine(obj.GetConcurrentFixed());
-Console.WriteLine(await obj.GetSingleRandomAsync());
-Console.WriteLine(await obj.GetSingleFixedAsync());
-Console.WriteLine(await obj.GetSingleRandomBufferAsync());
-Console.WriteLine(await obj.GetSingleFixedBufferAsync());
-Console.WriteLine(await obj.GetConcurrentRandomAsync());
-Console.WriteLine(await obj.GetConcurrentFixedAsync());
-Console.WriteLine();
+/*
+using (var obj = new DistributedCacheBenchmarks { PayloadSize = 11512, Sliding = true })
+{
+    Console.WriteLine($"Expected: {obj.PayloadSize}*{DistributedCacheBenchmarks.OperationsPerInvoke} = {obj.PayloadSize * DistributedCacheBenchmarks.OperationsPerInvoke}");
+    Console.WriteLine();
 
-obj.Backend = DistributedCacheBenchmarks.BackendType.SqlServer;
-obj.GlobalSetup();
-Console.WriteLine(obj.GetSingleRandom());
-Console.WriteLine(obj.GetSingleFixed());
-Console.WriteLine(obj.GetSingleRandomBuffer());
-Console.WriteLine(obj.GetSingleFixedBuffer());
-Console.WriteLine(obj.GetConcurrentRandom());
-Console.WriteLine(obj.GetConcurrentFixed());
-Console.WriteLine(await obj.GetSingleRandomAsync());
-Console.WriteLine(await obj.GetSingleFixedAsync());
-Console.WriteLine(await obj.GetSingleRandomBufferAsync());
-Console.WriteLine(await obj.GetSingleFixedBufferAsync());
-Console.WriteLine(await obj.GetConcurrentRandomAsync());
-Console.WriteLine(await obj.GetConcurrentFixedAsync());
-Console.WriteLine();
+    obj.Backend = DistributedCacheBenchmarks.BackendType.Redis;
+    obj.GlobalSetup();
+    Console.WriteLine(obj.GetSingleRandom());
+    Console.WriteLine(obj.GetSingleFixed());
+    Console.WriteLine(obj.GetSingleRandomBuffer());
+    Console.WriteLine(obj.GetSingleFixedBuffer());
+    Console.WriteLine(obj.GetConcurrentRandom());
+    Console.WriteLine(obj.GetConcurrentFixed());
+    Console.WriteLine(await obj.GetSingleRandomAsync());
+    Console.WriteLine(await obj.GetSingleFixedAsync());
+    Console.WriteLine(await obj.GetSingleRandomBufferAsync());
+    Console.WriteLine(await obj.GetSingleFixedBufferAsync());
+    Console.WriteLine(await obj.GetConcurrentRandomAsync());
+    Console.WriteLine(await obj.GetConcurrentFixedAsync());
+    Console.WriteLine();
 
+    obj.Backend = DistributedCacheBenchmarks.BackendType.SqlServer;
+    obj.GlobalSetup();
+    Console.WriteLine(obj.GetSingleRandom());
+    Console.WriteLine(obj.GetSingleFixed());
+    Console.WriteLine(obj.GetSingleRandomBuffer());
+    Console.WriteLine(obj.GetSingleFixedBuffer());
+    Console.WriteLine(obj.GetConcurrentRandom());
+    Console.WriteLine(obj.GetConcurrentFixed());
+    Console.WriteLine(await obj.GetSingleRandomAsync());
+    Console.WriteLine(await obj.GetSingleFixedAsync());
+    Console.WriteLine(await obj.GetSingleRandomBufferAsync());
+    Console.WriteLine(await obj.GetSingleFixedBufferAsync());
+    Console.WriteLine(await obj.GetConcurrentRandomAsync());
+    Console.WriteLine(await obj.GetConcurrentFixedAsync());
+    Console.WriteLine();
+}
+*/
 #else
-BenchmarkRunner.Run(typeof(DistributedCacheBenchmarks).Assembly, args: args);
+BenchmarkSwitcher.FromAssembly(typeof(DistributedCacheBenchmarks).Assembly).Run(args: args);
 #endif
