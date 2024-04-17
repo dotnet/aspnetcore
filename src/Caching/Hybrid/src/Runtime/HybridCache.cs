59bc62a70229090107777df48deddfd23a6c18a9
Marc Gravell <marc.gravell@gmail.com>
2024-04-17 11:44:32 +0100

    nit comment

--- src/Caching/Hybrid/src/Runtime/HybridCache.cs
+++ src/Caching/Hybrid/src/Runtime/HybridCache.cs
@@ -94,7 +94,7 @@ public abstract class HybridCache
     }
 
     /// <summary>
-    /// Asynchronously removes the value associated with the specified tags.
+    /// Asynchronously removes all values associated with the specified tags.
     /// </summary>
     /// <remarks>Implementors should treat <c>null</c> as empty</remarks>
     public virtual ValueTask RemoveTagsAsync(IEnumerable<string> tags, CancellationToken token = default)
@@ -118,7 +118,7 @@ public abstract class HybridCache
     }
 
     /// <summary>
-    /// Asynchronously removes the value associated with the specified tag.
+    /// Asynchronously removes all values associated with the specified tag.
     /// </summary>
     public abstract ValueTask RemoveTagAsync(string tag, CancellationToken token = default);
 }
