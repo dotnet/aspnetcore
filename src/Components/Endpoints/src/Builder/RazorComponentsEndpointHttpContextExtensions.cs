c0f01a731bf0c5d0136311b01853aedc9851d805
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-04-19 11:02:45 +0100

    Revert "Simplify"
    This reverts commit a488faadf5f2fa52deed771be9a066da299e9e19.

--- src/Components/Endpoints/src/Builder/RazorComponentsEndpointHttpContextExtensions.cs
+++ src/Components/Endpoints/src/Builder/RazorComponentsEndpointHttpContextExtensions.cs
@@ -1,8 +1,14 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Collections.Concurrent;
+using System.Reflection;
+using System.Reflection.Metadata;
 using Microsoft.AspNetCore.Components.Endpoints;
+using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Http;
+
+[assembly: MetadataUpdateHandler(typeof(RazorComponentsEndpointHttpContextExtensions.MetadataUpdateHandler))]
 
 namespace Microsoft.AspNetCore.Components.Routing;
 
@@ -11,6 +17,8 @@ namespace Microsoft.AspNetCore.Components.Routing;
 /// </summary>
 public static class RazorComponentsEndpointHttpContextExtensions
 {
+    private static readonly ConcurrentDictionary<Type, bool> AllowsInteractiveRoutingCache = new();
+
     /// <summary>
     /// Determines whether the current endpoint is a Razor component that can be reached through
     /// interactive routing. This is true for all page components except if they declare the
@@ -20,8 +28,20 @@ public static class RazorComponentsEndpointHttpContextExtensions
     /// <returns>True if the current endpoint is a Razor component that does not declare <see cref="AllowInteractiveRoutingAttribute"/> with value <see langword="false"/>.</returns>
     public static bool AllowsInteractiveRouting(this HttpContext? context)
     {
-        var metadata = context?.GetEndpoint()?.Metadata;
-        return metadata?.GetMetadata<ComponentTypeMetadata>() is not null
-            && metadata.GetMetadata<AllowInteractiveRoutingAttribute>() is not { Allow: false };
+        var pageType = context?.GetEndpoint()?.Metadata.GetMetadata<ComponentTypeMetadata>()?.Type;
+
+        return pageType is not null
+            && AllowsInteractiveRoutingCache.GetOrAdd(
+                pageType,
+                pageType.GetCustomAttribute<AllowInteractiveRoutingAttribute>() is not { Allow: false });
+    }
+
+    internal static class MetadataUpdateHandler
+    {
+        /// <summary>
+        /// Invoked as part of <see cref="MetadataUpdateHandlerAttribute" /> contract for hot reload.
+        /// </summary>
+        public static void ClearCache(Type[]? _)
+            => AllowsInteractiveRoutingCache.Clear();
     }
 }
