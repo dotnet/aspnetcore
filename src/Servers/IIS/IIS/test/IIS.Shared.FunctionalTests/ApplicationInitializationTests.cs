fd2b6cc6b45a6953d0da18ffdb1c83bbdbeaaabc
Brennan <brecon@microsoft.com>
2024-04-22 13:23:58 -0700

    Apply suggestions from code review

--- src/Servers/IIS/IIS/test/IIS.Shared.FunctionalTests/ApplicationInitializationTests.cs
+++ src/Servers/IIS/IIS/test/IIS.Shared.FunctionalTests/ApplicationInitializationTests.cs
@@ -102,7 +102,6 @@ public class ApplicationInitializationTests : IISFunctionalTestBase
             await Helpers.Retry(async () => await File.ReadAllTextAsync(Path.Combine(result.ContentRoot, "Started.txt")), TimeoutExtensions.DefaultTimeoutValue);
             StopServer();
             EventLogHelpers.VerifyEventLogEvent(result, EventLogHelpers.Started(result), Logger);
-            EventLogHelpers.VerifyEventLogEvent(result, EventLogHelpers.ShutdownMessage(result), Logger);
         }
     }
 
