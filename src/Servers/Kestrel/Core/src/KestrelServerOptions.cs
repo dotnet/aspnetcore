277ecba97dc56850f5c6492a27cf9b83ab9649ba
jacalvar <jacalvar@microsoft.com>
2024-04-24 17:14:35 +0200

    More break fixes

--- src/Servers/Kestrel/Core/src/KestrelServerOptions.cs
+++ src/Servers/Kestrel/Core/src/KestrelServerOptions.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Diagnostics;
-using System.Linq;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
