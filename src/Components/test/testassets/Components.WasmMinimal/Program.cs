21884d3227b3d3baec918c786f3e18500dd019fb
Stephen Halter <halter73@gmail.com>
2024-05-13 17:22:33 -0700

    Build in AuthenticationStateProviders from project templates

--- src/Components/test/testassets/Components.WasmMinimal/Program.cs
+++ src/Components/test/testassets/Components.WasmMinimal/Program.cs
@@ -1,10 +1,29 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Security.Claims;
 using Components.TestServer.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddSingleton<AsyncOperationService>();
+builder.Services.AddCascadingAuthenticationState();
+
+var additionalClaim = DefaultWebAssemblyJSRuntime.Instance.Invoke<string>("getQueryParam", "additionalClaim");
+
+builder.Services.AddAuthenticationStateDeserialization(options =>
+{
+    var originalCallback = options.DeserializationCallback;
+    options.DeserializationCallback = async authenticationStateData =>
+    {
+        var authenticationState = await originalCallback(authenticationStateData);
+        if (!string.IsNullOrEmpty(additionalClaim))
+        {
+            authenticationState.User.Identities.First().AddClaim(new Claim("additional-claim", additionalClaim));
+        }
+        return authenticationState;
+    };
+});
 
 await builder.Build().RunAsync();
