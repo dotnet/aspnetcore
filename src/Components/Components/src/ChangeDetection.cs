3fcb59e204a074f71c1e3eccd20081a36a9ae9ca
MattyLeslie <mattlesliee@gmail.com>
2024-05-28 12:29:17 +0200

    Making _immutableNonObjectTypesCache readonly

--- src/Components/Components/src/ChangeDetection.cs
+++ src/Components/Components/src/ChangeDetection.cs
@@ -7,7 +7,7 @@ namespace Microsoft.AspNetCore.Components;
 
 internal sealed class ChangeDetection
 {
-    private static ConcurrentDictionary<Type, bool> _immutableNonObjectTypesCache = new();
+    private static readonly ConcurrentDictionary<Type, bool> _immutableNonObjectTypesCache = new();
     public static bool MayHaveChanged<T1, T2>(T1 oldValue, T2 newValue)
     {
         var oldIsNotNull = oldValue != null;
