b62f4a8c338cf58bfc3330b66a03fdfe154bbe0d
Mike Kistler <mikekistler@microsoft.com>
2024-06-12 09:56:54 -0700

    simpler fix

--- src/Http/Authentication.Abstractions/src/IAuthenticationHandler.cs
+++ src/Http/Authentication.Abstractions/src/IAuthenticationHandler.cs
@@ -14,23 +14,23 @@ public interface IAuthenticationHandler
     /// Initialize the authentication handler. The handler should initialize anything it needs from the request and scheme as part of this method.
     /// </summary>
     /// <param name="scheme">The <see cref="AuthenticationScheme"/> scheme.</param>
-    /// <param name="context">The <see cref="HttpContext"/> context.</param>
+    /// <param name="context">The <see cref="HttpContext"/> context for the current request.</param>
     Task InitializeAsync(AuthenticationScheme scheme, HttpContext context);
 
     /// <summary>
-    /// Authenticate the current request using the <see cref="HttpContext"/> passed to the <see cref="InitializeAsync"/> method.
+    /// Authenticate the current request.
     /// </summary>
     /// <returns>The <see cref="AuthenticateResult"/> result.</returns>
     Task<AuthenticateResult> AuthenticateAsync();
 
     /// <summary>
-    /// Challenge the current request using the <see cref="HttpContext"/> passed to the <see cref="InitializeAsync"/> method.
+    /// Challenge the current request.
     /// </summary>
     /// <param name="properties">The <see cref="AuthenticationProperties"/> that contains the extra meta-data arriving with the authentication.</param>
     Task ChallengeAsync(AuthenticationProperties? properties);
 
     /// <summary>
-    /// Forbid the current request using the <see cref="HttpContext"/> passed to the <see cref="InitializeAsync"/> method.
+    /// Forbid the current request.
     /// </summary>
     /// <param name="properties">The <see cref="AuthenticationProperties"/> that contains the extra meta-data arriving with the authentication.</param>
     Task ForbidAsync(AuthenticationProperties? properties);
