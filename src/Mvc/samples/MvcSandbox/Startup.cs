28edc30ce045f93f1d56d7c33831979cc91d4f41
jacalvar <jacalvar@microsoft.com>
2024-06-13 23:12:31 +0200

    Undo MVC samples

--- src/Mvc/samples/MvcSandbox/Startup.cs
+++ src/Mvc/samples/MvcSandbox/Startup.cs
@@ -16,23 +16,20 @@ public class Startup
     public void Configure(IApplicationBuilder app)
     {
         app.UseDeveloperExceptionPage();
+        app.UseStaticFiles();
 
         app.UseRouting();
 
         static void ConfigureEndpoints(IEndpointRouteBuilder endpoints)
         {
-            endpoints.MapStaticAssets();
             endpoints.MapGet("/MapGet", () => "MapGet");
 
-            endpoints.MapControllers()
-                .WithResourceCollection();
-
+            endpoints.MapControllers();
             endpoints.MapControllerRoute(
                 Guid.NewGuid().ToString(),
                 "{controller=Home}/{action=Index}/{id?}");
 
-            endpoints.MapRazorPages()
-                .WithResourceCollection();
+            endpoints.MapRazorPages();
         }
 
         app.UseEndpoints(builder =>
