c26607b79bfac4a01caa2e1ac9f6ecca914cb55d
James Newton-King <james@newtonking.com>
2024-06-28 16:11:52 +0800

    Update src/Shared/Metrics/MetricsConstants.cs

--- src/Shared/Metrics/MetricsConstants.cs
+++ src/Shared/Metrics/MetricsConstants.cs
@@ -8,6 +8,6 @@ internal static class MetricsConstants
     // Follows boundaries from http.server.request.duration/http.client.request.duration
     public static readonly IReadOnlyList<double> ShortSecondsBucketBoundaries = [0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10];
 
-    // Not based on a standard. See https://github.com/open-telemetry/semantic-conventions/issues/336
+    // Not based on a standard. Larger bucket sizes for longer lasting operations, e.g. HTTP connection duration. See https://github.com/open-telemetry/semantic-conventions/issues/336
     public static readonly IReadOnlyList<double> LongSecondsBucketBoundaries = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 30, 60, 120, 300];
 }
