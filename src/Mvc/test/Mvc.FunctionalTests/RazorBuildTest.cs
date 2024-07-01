549d9e8c0c621b3cf2f83f11899fa248da34a45c
Brennan <brecon@microsoft.com>
2024-07-01 10:44:12 -0700

    flaky

--- src/Mvc/test/Mvc.FunctionalTests/RazorBuildTest.cs
+++ src/Mvc/test/Mvc.FunctionalTests/RazorBuildTest.cs
@@ -100,6 +100,7 @@ public class RazorBuildTest : LoggedTest
     }
 
     [Fact]
+    [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/56553")]
     [LogLevel(LogLevel.Trace)]
     public async Task RazorViews_AreUpdatedOnChange()
     {
@@ -138,6 +139,7 @@ public class RazorBuildTest : LoggedTest
     }
 
     [Fact]
+    [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/56553")]
     [LogLevel(LogLevel.Trace)]
     public async Task RazorPages_AreUpdatedOnChange()
     {
