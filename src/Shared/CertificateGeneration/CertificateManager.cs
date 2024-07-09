ce982c294370e280048e4ef1e2195ebede67b5be
Andrew Casey <amcasey@users.noreply.github.com>
2024-07-09 13:00:15 -0700

    Search for trusted certificates consistently on Windows
    1. Don't use thumbprint so we don't get flagged for using SHA-1
    2. Make TrustCertificateCore and RemoveCertificateFromTrustedRoots consistent

--- src/Shared/CertificateGeneration/CertificateManager.cs
+++ src/Shared/CertificateGeneration/CertificateManager.cs
@@ -812,6 +812,51 @@ internal abstract class CertificateManager
     internal static string GetDescription(X509Certificate2 c) =>
         $"{c.Thumbprint} - {c.Subject} - Valid from {c.NotBefore:u} to {c.NotAfter:u} - IsHttpsDevelopmentCertificate: {IsHttpsDevelopmentCertificate(c).ToString().ToLowerInvariant()} - IsExportable: {Instance.IsExportable(c).ToString().ToLowerInvariant()}";
 
+    /// <remarks>
+    /// <see cref="X509Certificate.Equals(X509Certificate?)"/> is not adequate for security purposes.
+    /// </remarks>
+    internal static bool AreCertificatesEqual(X509Certificate2 cert1, X509Certificate2 cert2)
+    {
+        return cert1.RawDataMemory.Span.SequenceEqual(cert2.RawDataMemory.Span);
+    }
+
+    /// <summary>
+    /// Given a certificate, presumably from the <see cref="StoreName.My"/> store, try to find the
+    /// corresponding certificate in the <see cref="StoreName.Root"/> store."/>
+    /// </summary>
+    /// <param name="store">An open <see cref="StoreName.Root"/> <see cref="X509Store"/>.</param>
+    /// <param name="certificate">A certificate to search for.</param>
+    /// <param name="rootCertificate">The certificate, if any, corresponding to <paramref name="certificate"/> in <paramref name="store"/>.</param>
+    /// <returns>True if a corresponding certificate was found.</returns>
+    /// <remarks><see cref="ListCertificates"/> has richer filtering and a lot of debugging output that's unhelpful here.</remarks>
+    internal static bool TryFindCertificateInRootStore(X509Store store, X509Certificate2 certificate, [NotNullWhen(true)] out X509Certificate2? rootCertificate)
+    {
+        Debug.Assert(store.Name == "Root");
+
+        // We're only interested in the public part of the certificate.
+        using var publicCertificate = X509CertificateLoader.LoadCertificate(certificate.Export(X509ContentType.Cert));
+
+        // We specifically don't search by thumbprint to avoid being flagged for using a SHA-1 hash.
+        var certificatesWithSubjectName = store.Certificates.Find(X509FindType.FindBySubjectName, publicCertificate.SubjectName, validOnly: false);
+        if (certificatesWithSubjectName.Count > 0)
+        {
+            var certsToDispose = new List<X509Certificate2>();
+            foreach (var candidate in certificatesWithSubjectName.OfType<X509Certificate2>())
+            {
+                if (AreCertificatesEqual(candidate, certificate))
+                {
+                    DisposeCertificates(certsToDispose);
+                    rootCertificate = candidate;
+                    return true;
+                }
+                certsToDispose.Add(candidate);
+            }
+        }
+
+        rootCertificate = null;
+        return false;
+    }
+
     [EventSource(Name = "Dotnet-dev-certs")]
     public sealed class CertificateManagerEventSource : EventSource
     {
