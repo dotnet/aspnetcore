bd50a68ecfa4b39b0275805c2c82490e9a71eb18
Martin Costello <martin@martincostello.com>
2024-07-15 23:09:45 +0100

    Amortise Content-Type arrays (#56525)
    - Amortise arrays used for `TypedResults`' metadata.
    - Use `ProducesResponseTypeMetadata.CreateUnvalidated` to bypass `Content-Type` validation for static values.
    - Add and use constant for `application/octet-stream`.

--- src/Http/Http.Results/src/HttpResultsHelper.cs
+++ src/Http/Http.Results/src/HttpResultsHelper.cs
@@ -17,7 +17,13 @@ namespace Microsoft.AspNetCore.Http;
 
 internal static partial class HttpResultsHelper
 {
+    internal const string BinaryContentType = "application/octet-stream";
     internal const string DefaultContentType = "text/plain; charset=utf-8";
+    internal const string ProblemDetailsContentType = "application/problem+json";
+
+    internal static IEnumerable<string> ApplicationJsonContentTypes { get; } = ["application/json"];
+    internal static IEnumerable<string> ProblemDetailsContentTypes { get; } = [ProblemDetailsContentType];
+
     private static readonly Encoding DefaultEncoding = Encoding.UTF8;
 
     [UnconditionalSuppressMessage("Trimming", "IL2026:RequiresUnreferencedCode",
