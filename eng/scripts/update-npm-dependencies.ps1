44ab8a64ecd5ac5d6cb6183ef6080ad5517d0b0f
Javier Calvarro Nelson <jacalvar@microsoft.com>
2024-07-16 18:05:23 +0200

    [Infrastructure] Updated npm package cache 2024-07-15 (#56802)
    * Clear npm cache on each update
    
    * Set Node-Externals to main to avoid merge conflicts
    
    * Add a warning about merging Node-Externals PR before the main PR can get merged
    
    * Updated npm package cache 2024-07-15

--- eng/scripts/update-npm-dependencies.ps1
+++ eng/scripts/update-npm-dependencies.ps1
@@ -3,7 +3,8 @@
 
 param (
     [switch]$WhatIf,
-    [switch]$SkipPullRequestCreation
+    [switch]$SkipPullRequestCreation,
+    [switch]$SkipClearCache
 )
 
 $ErrorActionPreference = "Stop"
@@ -18,6 +19,12 @@ if (-not $WhatIf) {
 Write-Host "Removing package-lock.json"
 if (-not $WhatIf) {
     Remove-Item .\package-lock.json
+}
+
+if (-not $SkipClearCache -and -not $WhatIf) {
+    Write-Host "Clearing the npm cache"
+    Remove-Item -Recurse -Force "$PWD/src/submodules/Node-Externals/cache"
+    New-Item -ItemType Directory -Path "$PWD/src/submodules/Node-Externals/cache"
 }
 
 try {
@@ -97,7 +104,7 @@ else {
     git branch --set-upstream-to=origin/main
     git push origin $branchName`:$branchName --force;
     gh repo set-default dotnet/Node-Externals
-    gh pr create --base main --head "infrastructure/update-npm-package-cache-$(Get-Date -Format 'yyyy-MM-dd')" --title "[Infrastructure] Updated npm package cache $(Get-Date -Format 'yyyy-MM-dd')" --body ""
+    gh pr create --base main --head "infrastructure/update-npm-package-cache-$(Get-Date -Format 'yyyy-MM-dd')" --title "[Infrastructure] Updated npm package cache $(Get-Date -Format 'yyyy-MM-dd')" --body "Do not merge this PR until the one in aspnetcore passes all checks."
 }
 
 ## Navigate to the root of the repository
