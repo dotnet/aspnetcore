ec418f09488cc56ff59d3805feb28e326db17007
ladeak <ladeak87@windowslive.com>
2024-07-18 19:02:43 +0200

    Remove using statement

--- src/Servers/Kestrel/Core/src/Internal/Http/PathNormalizer.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http/PathNormalizer.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Diagnostics;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using Microsoft.AspNetCore.Internal;
