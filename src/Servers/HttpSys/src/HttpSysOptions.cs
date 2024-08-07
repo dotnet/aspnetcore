902b6c9a1255701e2f1cd87442f0a0401c01aaf7
Aditya Mandaleeka <adityam@microsoft.com>
2024-08-06 18:24:20 -0700

    [6.0] Optionally respect HTTP/1.0 keep-Alive for HTTP.sys.

--- src/Servers/HttpSys/src/HttpSysOptions.cs
+++ src/Servers/HttpSys/src/HttpSysOptions.cs
@@ -30,6 +30,11 @@ namespace Microsoft.AspNetCore.Server.HttpSys
         private long? _maxRequestBodySize = DefaultMaxRequestBodySize;
         private string? _requestQueueName;
 
+        private const string RespectHttp10KeepAliveSwitch = "Microsoft.AspNetCore.Server.HttpSys.RespectHttp10KeepAlive";
+
+        // Internal for testing
+        internal bool RespectHttp10KeepAlive = AppContext.TryGetSwitch(RespectHttp10KeepAliveSwitch, out var enabled) && enabled;
+
         /// <summary>
         /// Initializes a new <see cref="HttpSysOptions"/>.
         /// </summary>
