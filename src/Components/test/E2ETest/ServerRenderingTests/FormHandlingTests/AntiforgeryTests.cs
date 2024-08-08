31085a24586f37410096b1c23d574f8bbc8409fa
Javier Calvarro Nelson <jacalvar@microsoft.com>
2024-08-08 20:15:35 +0200

    Fix antiforgery not being available after first render

---
+++ src/Components/test/E2ETest/ServerRenderingTests/FormHandlingTests/AntiforgeryTests.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Components.TestServer.RazorComponents;
+using Microsoft.AspNetCore.Components.E2ETest.Infrastructure;
+using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
+using Microsoft.AspNetCore.E2ETesting;
+using OpenQA.Selenium;
+using TestServer;
+using Xunit.Abstractions;
+
+namespace Microsoft.AspNetCore.Components.E2ETests.ServerRenderingTests.FormHandlingTests;
+
+public class AntiforgeryTests : ServerTestBase<BasicTestAppServerSiteFixture<RazorComponentEndpointsStartup<App>>>
+{
+    public AntiforgeryTests(
+        BrowserFixture browserFixture,
+        BasicTestAppServerSiteFixture<RazorComponentEndpointsStartup<App>> serverFixture,
+        ITestOutputHelper output)
+        : base(browserFixture, serverFixture, output)
+    {
+    }
+
+    public override Task InitializeAsync()
+        => InitializeAsync(BrowserFixture.StreamingContext);
+
+    [Theory]
+    [InlineData("server")]
+    [InlineData("webassembly")]
+    public void CanUseAntiforgeryAfterInitialRender(string target)
+    {
+        Navigate($"{ServerPathBase}/{target}-antiforgery-form");
+
+        Browser.Exists(By.Id("interactive"));
+
+        Browser.Click(By.Id("render-form"));
+
+        var input = Browser.Exists(By.Id("name"));
+        input.SendKeys("Test");
+        var submit = Browser.Exists(By.Id("submit"));
+        submit.Click();
+
+        var result = Browser.Exists(By.Id("result"));
+        Browser.Equal("Test", () => result.Text);
+    }
+}
