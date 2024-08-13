3d13fae15873da334bbff2371b6892346a931093
Safia Abdalla <safia@microsoft.com>
2024-08-12 18:42:06 -0700

    Localize transformers list and throw from type-based transformers (#57296)
    * Localize transformers list and throw from type-based transformers
    
    * Use array and fix typo

--- src/OpenApi/src/Services/Schemas/OpenApiSchemaService.cs
+++ src/OpenApi/src/Services/Schemas/OpenApiSchemaService.cs
@@ -125,7 +125,7 @@ internal sealed class OpenApiSchemaService(
         }
     };
 
-    internal async Task<OpenApiSchema> GetOrCreateSchemaAsync(Type type, IServiceProvider scopedServiceProvider, List<IOpenApiSchemaTransformer> schemaTransformers, ApiParameterDescription? parameterDescription = null, bool captureSchemaByRef = false, CancellationToken cancellationToken = default)
+    internal async Task<OpenApiSchema> GetOrCreateSchemaAsync(Type type, IServiceProvider scopedServiceProvider, IOpenApiSchemaTransformer[] schemaTransformers, ApiParameterDescription? parameterDescription = null, bool captureSchemaByRef = false, CancellationToken cancellationToken = default)
     {
         var key = parameterDescription?.ParameterDescriptor is IParameterInfoParameterDescriptor parameterInfoDescription
             && parameterDescription.ModelMetadata.PropertyName is null
@@ -143,9 +143,9 @@ internal sealed class OpenApiSchemaService(
         return schema;
     }
 
-    internal async Task ApplySchemaTransformersAsync(OpenApiSchema schema, Type type, IServiceProvider scopedServiceProvider, List<IOpenApiSchemaTransformer> schemaTransformers, ApiParameterDescription? parameterDescription = null, CancellationToken cancellationToken = default)
+    internal async Task ApplySchemaTransformersAsync(OpenApiSchema schema, Type type, IServiceProvider scopedServiceProvider, IOpenApiSchemaTransformer[] schemaTransformers, ApiParameterDescription? parameterDescription = null, CancellationToken cancellationToken = default)
     {
-        if (schemaTransformers.Count == 0)
+        if (schemaTransformers.Length == 0)
         {
             return;
         }
@@ -158,7 +158,7 @@ internal sealed class OpenApiSchemaService(
             ParameterDescription = parameterDescription,
             ApplicationServices = scopedServiceProvider
         };
-        for (var i = 0; i < schemaTransformers.Count; i++)
+        for (var i = 0; i < schemaTransformers.Length; i++)
         {
             // Reset context object to base state before running each transformer.
             var transformer = schemaTransformers[i];
