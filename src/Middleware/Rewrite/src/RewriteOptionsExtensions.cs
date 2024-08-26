3fe3e44cd4059499f7ff08ef1455d93259c96f34
Chad Tolkien <chad@sodadigital.com.au>
2024-08-26 10:13:05 +1000

    * Restore the comment for permanent redirect * Align 302 coments to match.

--- src/Middleware/Rewrite/src/RewriteOptionsExtensions.cs
+++ src/Middleware/Rewrite/src/RewriteOptionsExtensions.cs
@@ -50,7 +50,8 @@ public static class RewriteOptionsExtensions
     }
 
     /// <summary>
-    /// Redirect the request if the regex matches the HttpContext's PathString, with returning a 302 - Found status code.
+    /// Redirect the request if the regex matches the HttpContext's PathString, with returning a 302
+    /// status code for found.
     /// </summary>
     /// <param name="options">The <see cref="RewriteOptions"/>.</param>
     /// <param name="regex">The regex string to compare with.</param>
@@ -76,7 +77,7 @@ public static class RewriteOptionsExtensions
     }
 
     /// <summary>
-    /// Redirect a request to https if the incoming request is http, with returning a 301 - Moved Permanently status code.
+    /// Redirect a request to https if the incoming request is http, with returning a 301
     /// status code for permanently redirected.
     /// </summary>
     /// <param name="options">The <see cref="RewriteOptions"/>.</param>
