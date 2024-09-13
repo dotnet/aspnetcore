63e59de50b571de3079e6fa9817ed4b7478bfe74
Safia Abdalla <safia@microsoft.com>
2024-09-13 16:43:11 +0000

    Fix linker warnings

--- src/SignalR/server/Core/src/Internal/DynamicClientProxy.cs
+++ src/SignalR/server/Core/src/Internal/DynamicClientProxy.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
 using System.Dynamic;
 
 namespace Microsoft.AspNetCore.SignalR.Internal;
@@ -9,6 +10,7 @@ internal sealed class DynamicClientProxy : DynamicObject
 {
     private readonly IClientProxy _clientProxy;
 
+    [RequiresDynamicCodeAttribute("This constructor requires dynamic code generation to construct a call site.")]
     public DynamicClientProxy(IClientProxy clientProxy)
     {
         _clientProxy = clientProxy;
