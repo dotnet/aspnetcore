6f7b7719da79034224fc8a392d63ef4a74557478
Safia Abdalla <safia@microsoft.com>
2024-09-19 22:41:41 +0000

    Fix using for testing packages

--- src/Http/Http.Extensions/test/RequestDelegateFactoryTests.EndpointFilters.cs
+++ src/Http/Http.Extensions/test/RequestDelegateFactoryTests.EndpointFilters.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.InternalTesting;
+using Microsoft.AspNetCore.Testing;
 
 namespace Microsoft.AspNetCore.Routing.Internal;
 
