87cf9b959961f43c5635e8d8014ae7278f3b8f3b
wtgodbe <wigodbe@microsoft.com>
2024-10-01 10:26:46 -0700

    Update to net10.0

--- src/ProjectTemplates/scripts/Run-WebApiAot-Locally.ps1
+++ src/ProjectTemplates/scripts/Run-WebApiAot-Locally.ps1
@@ -9,4 +9,4 @@ $ErrorActionPreference = 'Stop'
 
 . $PSScriptRoot\Test-Template.ps1
 
-Test-Template "webapiaot" "webapiaot" "Microsoft.DotNet.Web.ProjectTemplates.9.0.9.0.0-dev.nupkg" $false
+Test-Template "webapiaot" "webapiaot" "Microsoft.DotNet.Web.ProjectTemplates.10.0.10.0.0-dev.nupkg" $false
