f79137753b128124c9eb838730ca1ab53d17e304
tmat <tomas.matousek@microsoft.com>
2024-10-09 19:13:04 -0700

    Hot Reload agent improvements
    Refactor agent code so that it can be shared with dotnet-watch implementation via a shared project.
    
    Add new API applyHotReloadDeltas to apply updates.

---
+++ src/Components/WebAssembly/WebAssembly/src/HotReload/AgentMessageSeverity.cs
@@ -0,0 +1,11 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.Extensions.HotReload;
+
+internal enum AgentMessageSeverity : byte
+{
+    Verbose = 0,
+    Warning = 1,
+    Error = 2,
+}
---
+++ src/Components/WebAssembly/WebAssembly/src/HotReload/ResponseLoggingLevel.cs
@@ -0,0 +1,10 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.Extensions.HotReload;
+
+internal enum ResponseLoggingLevel : byte
+{
+    WarningsAndErrors = 0,
+    Verbose = 1,
+}
