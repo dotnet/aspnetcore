49dfa82540c8b0e845e6eb70fa9fdad3134a89ed
wtgodbe <wigodbe@microsoft.com>
2024-10-14 09:58:00 -0700

    Update branding to 2.3.0

--- src/DefaultBuilder/src/WebHost.cs
+++ src/DefaultBuilder/src/WebHost.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HostFiltering;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
