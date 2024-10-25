192bd6275b8a22e37c30e5ada6d1ae179611e44d
Chris/0 <cosborn@cimpress.com>
2024-10-25 16:47:49 -0400

    Fully Qualify Type Names When Resolving in RDG
    These two places correspond to resolution of a service from DI when
    attributed with `FromServicesAttribute` or with
    `FromKeyedServicesAttribute`. When the type name of the service is not
    fully qualified, the generator can generate code which fails to compile.
    
    For example, given a service `ExampleService` in namespace `Http`, the
    generator can generate code to resolve an instance of such a service
    like this:
    
    ```c#
    var e_local = httpContext.RequestServices.GetRequiredKeyedService<Http.ExampleService>("example");
    ```
    
    …but `Microsoft.AspNetCore.Http` is a nearer match for the reference to
    "Http", so the compiler will look for a type
    `Microsoft.AspNetCore.Http.ExampleService` (which doesn't exist – at
    least, not at time of writing), and compilation will fail.
    
    Fixes: #58633

--- src/Http/Http.Extensions/gen/StaticRouteHandlerModel/Emitters/EndpointParameterEmitter.cs
+++ src/Http/Http.Extensions/gen/StaticRouteHandlerModel/Emitters/EndpointParameterEmitter.cs
@@ -389,8 +389,8 @@ internal static class EndpointParameterEmitter
         // Unlike other scenarios, this will result in an exception being thrown
         // at runtime.
         var assigningCode = endpointParameter.IsOptional ?
-            $"httpContext.RequestServices.GetService<{endpointParameter.Type}>();" :
-            $"httpContext.RequestServices.GetRequiredService<{endpointParameter.Type}>()";
+            $"httpContext.RequestServices.GetService<{endpointParameter.Type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat)}>();" :
+            $"httpContext.RequestServices.GetRequiredService<{endpointParameter.Type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat)}>()";
         codeWriter.WriteLine($"var {endpointParameter.EmitHandlerArgument()} = {assigningCode};");
     }
 
@@ -404,8 +404,8 @@ internal static class EndpointParameterEmitter
         codeWriter.EndBlock();
 
         var assigningCode = endpointParameter.IsOptional ?
-            $"httpContext.RequestServices.GetKeyedService<{endpointParameter.Type}>({endpointParameter.KeyedServiceKey});" :
-            $"httpContext.RequestServices.GetRequiredKeyedService<{endpointParameter.Type}>({endpointParameter.KeyedServiceKey})";
+            $"httpContext.RequestServices.GetKeyedService<{endpointParameter.Type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat)}>({endpointParameter.KeyedServiceKey});" :
+            $"httpContext.RequestServices.GetRequiredKeyedService<{endpointParameter.Type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat)}>({endpointParameter.KeyedServiceKey})";
         codeWriter.WriteLine($"var {endpointParameter.EmitHandlerArgument()} = {assigningCode};");
     }
 
