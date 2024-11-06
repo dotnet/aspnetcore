d87f55a57a18f66be26a4752e5cbdcb253b0fa7b
William Godbe <wigodbe@microsoft.com>
2024-11-05 21:05:00 -0800

    [release/2.3] Always build modules (#58809)
    * Always build modules
    
    * Don't build templates
    
    * No Templating

--- src/Templating/src/Microsoft.DotNet.Web.ProjectTemplates/content/StarterWeb-FSharp/Models/ErrorViewModel.fs
+++
@@ -1,8 +0,0 @@
-namespace Company.WebApplication1
-
-open System
-
-type ErrorViewModel private () =
-    member val RequestId : string = null with get, set
-
-    member val ShowRequestId : bool = true with get, set
