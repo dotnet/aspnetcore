8cd24a21e4ae718b6cbd321a3a472bfdf546ff73
Dmitrii Korolev <dmkorolev@microsoft.com>
2024-11-12 21:11:15 +0100

    init the owin sample

--- src/Http/samples/MinimalSampleOwin/Program.cs
+++ src/Http/samples/MinimalSampleOwin/Program.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Reflection;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Http.Metadata;
@@ -11,11 +10,34 @@ using Microsoft.AspNetCore.Owin;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-hostBuilder.Build().Run();
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+string Plaintext() => "Hello, World!";
+app.MapGet("/plaintext", Plaintext);
 
-Func<IDictionary<string, object>, Task> ConfigureOwinPipeline(Func<IDictionary<string, object>, Task> next) => env =>
+app.MapGet("/", () => $"""
+    Operating System: {Environment.OSVersion}
+    .NET version: {Environment.Version}
+    Username: {Environment.UserName}
+    Date and Time: {DateTime.Now}
+    """);
+
+app.UseOwin(pipeline =>
 {
-    return next(env);
-};
+    pipeline(next =>
+    {
+        return async environment =>
+        {
+            // if you want to get OWIN environment properties
+            //if (environment is OwinEnvironment owin)
+            //{
+            //    foreach (var prop in owin)
+            //    {
+            //        app.Logger.LogInformation($"{prop.Key} - {prop.Value}");
+            //    }
+            //}
+
+            await next(environment);
+        };
+    });
+});
+
+app.Run();
