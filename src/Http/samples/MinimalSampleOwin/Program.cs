32da5f8f839b2cb72af63a46cd07dca39ac294b7
Dmitrii Korolev <dmkorolev@microsoft.com>
2024-11-14 14:19:09 +0100

    suppress NativeAOT warnings for Owin sample

--- src/Http/samples/MinimalSampleOwin/Program.cs
+++ src/Http/samples/MinimalSampleOwin/Program.cs
@@ -10,8 +10,13 @@ using Microsoft.AspNetCore.Owin;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Logger.LogInformation($"Current process ID: {Environment.ProcessId}");
+
+#pragma warning disable IL3050 // Calling members annotated with 'RequiresDynamicCodeAttribute' may break functionality when AOT compiling.
+#pragma warning disable IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
 string Plaintext() => "Hello, World!";
 app.MapGet("/plaintext", Plaintext);
+
 
 app.MapGet("/", () => $"""
     Operating System: {Environment.OSVersion}
@@ -19,6 +24,8 @@ app.MapGet("/", () => $"""
     Username: {Environment.UserName}
     Date and Time: {DateTime.Now}
     """);
+#pragma warning restore IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
+#pragma warning restore IL3050 // Calling members annotated with 'RequiresDynamicCodeAttribute' may break functionality when AOT compiling.
 
 app.UseOwin(pipeline =>
 {
