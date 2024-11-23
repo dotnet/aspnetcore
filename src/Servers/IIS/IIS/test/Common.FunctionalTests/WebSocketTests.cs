d086eec9cd593375a1c190d671469d6dc36c9458
Safia Abdalla <safia@microsoft.com>
2024-11-22 19:50:04 -0800

    Skip WebSocketOutOfProcessTests on Windows.Amd64.Server2022.Open (#59112)
    * Skip WebSocketOutOfProcessTests on Windows.Amd64.Server2022.Open
    
    * Use if-def from base class
    
    * Fix in-proc and remove attributes from abstract class

--- src/Servers/IIS/IIS/test/Common.FunctionalTests/WebSocketTests.cs
+++ src/Servers/IIS/IIS/test/Common.FunctionalTests/WebSocketTests.cs
@@ -27,13 +27,6 @@ namespace Microsoft.AspNetCore.Server.IIS.NewShim.FunctionalTests;
 namespace Microsoft.AspNetCore.Server.IIS.FunctionalTests;
 #endif
 
-[MinimumOSVersion(OperatingSystems.Windows, WindowsVersions.Win8, SkipReason = "No WebSocket supported on Win7")]
-#if IISEXPRESS_FUNCTIONALS
-[SkipOnHelix("Unsupported queue", Queues = "Windows.Amd64.VS2022.Pre.Open")]
-#else
-// These queues do not have websockets enabled currently for full IIS
-[SkipOnHelix("Unsupported queue", Queues = "Windows.Amd64.VS2022.Pre.Open;Windows.Amd64.Server2022.Open")]
-#endif
 public abstract class WebSocketsTests : FunctionalTestsBase
 {
     public IISTestSiteFixture Fixture { get; }
