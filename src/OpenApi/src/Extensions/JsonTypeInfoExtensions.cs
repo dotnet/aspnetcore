a9883cb22bc40f7329ea5164a729133dc83f1e84
Sander ten Brinke <s.tenbrinke2@gmail.com>
2024-11-27 15:31:33 +0000

    Fix typo

--- src/OpenApi/src/Extensions/JsonTypeInfoExtensions.cs
+++ src/OpenApi/src/Extensions/JsonTypeInfoExtensions.cs
@@ -67,7 +67,7 @@ internal static class JsonTypeInfoExtensions
         }
 
         // Although arrays are enumerable types they are not encoded correctly
-        // with JsonTypeInfoKind.Enumerable so we handle the Enumerble type
+        // with JsonTypeInfoKind.Enumerable so we handle the Enumerable type
         // case here.
         if (jsonTypeInfo is JsonTypeInfo { Kind: JsonTypeInfoKind.Enumerable } || type.IsArray)
         {
