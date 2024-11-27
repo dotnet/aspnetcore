9cda598696cdc151a140ba0991ee9a005ea00cb4
Sander ten Brinke <s.tenbrinke2@gmail.com>
2024-11-27 15:45:58 +0000

    Fix bug

--- src/OpenApi/src/Extensions/JsonTypeInfoExtensions.cs
+++ src/OpenApi/src/Extensions/JsonTypeInfoExtensions.cs
@@ -104,7 +104,9 @@ internal static class JsonTypeInfoExtensions
         // Generic types become a concatenation of the generic type name and the type arguments
         if (type.IsGenericType)
         {
-            var genericTypeName = type.Name[..type.Name.LastIndexOf('`')];
+            // We need to handle the case where the generic type is a nested type, so we check if the name contains a backtick already.
+            // Example: https://github.com/dotnet/aspnetcore/issues/59092
+            var genericTypeName = type.Name.Contains('`') ? type.Name[..type.Name.LastIndexOf('`')] : type.Name;
             var genericArguments = type.GetGenericArguments();
             var argumentNames = string.Join("And", genericArguments.Select(arg => arg.GetSchemaReferenceId(options)));
             return $"{genericTypeName}Of{argumentNames}";
