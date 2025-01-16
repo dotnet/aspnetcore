a8129ada1de6240c3d78a2fbf46163154997baec
James Newton-King <james@newtonking.com>
2025-01-16 11:03:14 +0800

    Improve logging

--- src/Hosting/Hosting/test/Internal/AsyncEnumerableExtensions.cs
+++ src/Hosting/Hosting/test/Internal/AsyncEnumerableExtensions.cs
@@ -2,12 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Numerics;
+using Microsoft.Extensions.Logging;
 
 namespace System.Collections.Generic;
 
 internal static class AsyncEnumerableExtensions
 {
-    public static async Task WaitForValueAsync<T>(this IAsyncEnumerator<T> values, T expectedValue) where T : INumber<T>
+    public static async Task WaitForValueAsync<T>(this IAsyncEnumerator<T> values, T expectedValue, string operationName, ILogger logger) where T : INumber<T>
     {
         T value = T.Zero;
         try
@@ -17,8 +18,11 @@ internal static class AsyncEnumerableExtensions
                 value = values.Current;
                 if (value == expectedValue)
                 {
+                    logger.LogDebug("Operation {OperationName} completed with value {Value}.", operationName, value);
                     return;
                 }
+
+                logger.LogDebug("Operation {OperationName} expected {ExpectedValue} but got {Value}.", operationName, expectedValue, value);
             }
 
             throw new InvalidOperationException("Data ended without match.");
@@ -26,28 +30,6 @@ internal static class AsyncEnumerableExtensions
         catch (Exception ex)
         {
             throw new InvalidOperationException($"Results ended with final value of {value}. Expected value of {expectedValue}.", ex);
-        }
-    }
-
-    public static async Task WaitForSumValueAsync<T>(this IAsyncEnumerator<T> values, T expectedValue) where T: INumber<T>
-    {
-        T value = T.Zero;
-        try
-        {
-            while (await values.MoveNextAsync())
-            {
-                value += values.Current;
-                if (value == expectedValue)
-                {
-                    return;
-                }
-            }
-
-            throw new InvalidOperationException("Data ended without match.");
-        }
-        catch (Exception ex)
-        {
-            throw new InvalidOperationException($"Results ended with final sum value of {value}. Expected sum value of {expectedValue}.", ex);
         }
     }
 }
