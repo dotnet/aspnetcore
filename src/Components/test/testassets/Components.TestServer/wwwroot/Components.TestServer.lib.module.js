3e37137a3776feac11eb41b3ff88948ed5709aa7
Marek Fisera <marekfisera@microsoft.com>
2025-01-16 17:17:36 +0100

    Test case

---
+++ src/Components/test/testassets/Components.TestServer/wwwroot/Components.TestServer.lib.module.js
@@ -0,0 +1,7 @@
+export function beforeWebAssemblyStart() {
+    appendElement('server--classic-and-modern-before-web-assembly-start', 'Server project Classic and modern "beforeWebAssemblyStart"');
+}
+
+export function afterWebAssemblyStarted() {
+    appendElement('server--classic-and-modern-after-web-assembly-started', 'Server project Classic and modern "afterWebAssemblyStarted"');
+}
