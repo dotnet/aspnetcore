1be78aec6e8def5393cb5de1a40a450ded128857
Marek Fisera <marekfisera@microsoft.com>
2025-01-20 15:31:29 +0100

    Add appendElement function

--- src/Components/test/testassets/Components.TestServer/wwwroot/Components.TestServer.lib.module.js
+++ src/Components/test/testassets/Components.TestServer/wwwroot/Components.TestServer.lib.module.js
@@ -5,3 +5,15 @@ export function beforeWebAssemblyStart() {
 export function afterWebAssemblyStarted() {
     appendElement('server--classic-and-modern-after-web-assembly-started', 'Server project Classic and modern "afterWebAssemblyStarted"');
 }
+
+// Duplicated in BasicTestApp\wwwroot\JSInitializers\ClassicAndModern\BasicTestApp.lib.module.js
+function appendElement(id, text) {
+    var content = document.getElementById('initializers-content');
+    if (!content) {
+        return;
+    }
+    var element = document.createElement('p');
+    element.id = id;
+    element.innerText = text;
+    content.appendChild(element);
+}
