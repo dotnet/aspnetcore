86a0a54e447a451a7676e4ae8ab9178a9ca18773
Javier Calvarro Nelson <jacalvar@microsoft.com>
2025-03-03 16:47:43 +0100

    Undo sample changes

--- src/Components/Samples/BlazorUnitedApp/Program.cs
+++ src/Components/Samples/BlazorUnitedApp/Program.cs
@@ -22,10 +22,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-else
-{
-    app.UseWebAssemblyDebugging();
-}
 
 app.UseHttpsRedirection();
 
