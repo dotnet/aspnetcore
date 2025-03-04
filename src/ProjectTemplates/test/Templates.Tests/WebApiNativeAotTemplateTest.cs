983ec24f060462f7a4b733793854b713e4bcaa38
Sander ten Brinke <s.tenbrinke2@gmail.com>
2025-03-04 16:52:58 +0100

    Fix assertions where we expected OpenAPI to be enabled for published apps, which is not the case

--- src/ProjectTemplates/test/Templates.Tests/WebApiNativeAotTemplateTest.cs
+++ src/ProjectTemplates/test/Templates.Tests/WebApiNativeAotTemplateTest.cs
@@ -34,30 +34,51 @@ public class WebApiNativeAotTemplateTest : LoggedTest
     [SkipOnHelix("https://github.com/dotnet/aspnetcore/issues/47478", Queues = HelixConstants.NativeAotNotSupportedHelixQueues)]
     public async Task WebApiNativeAotTemplateCSharp()
     {
-        await WebApiNativeAotTemplateCore(languageOverride: null, additionalEndpointsThatShould200Ok: new[] { "/openapi/v1.json" });
+        await WebApiNativeAotTemplateCore(languageOverride: null, additionalEndpointsThatShould200OkForBuiltProjects: new[] { "/openapi/v1.json" });
     }
 
     [ConditionalFact]
     [SkipOnHelix("https://github.com/dotnet/aspnetcore/issues/47478", Queues = HelixConstants.NativeAotNotSupportedHelixQueues)]
     public async Task WebApiNativeAotTemplateProgramMainCSharp()
     {
-        await WebApiNativeAotTemplateCore(languageOverride: null, args: new[] { ArgConstants.UseProgramMain }, additionalEndpointsThatShould200Ok: new[] { "/openapi/v1.json" });
+        await WebApiNativeAotTemplateCore(languageOverride: null, args: new[] { ArgConstants.UseProgramMain }, additionalEndpointsThatShould200OkForBuiltProjects: new[] { "/openapi/v1.json" });
+    }
+
+    [ConditionalTheory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public async Task WebApiNativeAotTemplateCSharp_OpenApiDisabledInProductionEnvironment()
+    {
+        var args = useProgramMain
+            ? new[] { ArgConstants.UseProgramMain }
+            : new[] { };
+
+        await WebApiNativeAotTemplateCore(languageOverride: null, args: args, additionalEndpointsThatShould404NotFoundForPublishedProjects: new[] { "/openapi/v1.json" });
     }
 
     [ConditionalTheory]
     [InlineData(false)]
     [InlineData(true)]
     [SkipOnHelix("https://github.com/dotnet/aspnetcore/issues/47478", Queues = HelixConstants.NativeAotNotSupportedHelixQueues)]
-    public async Task WebApiTemplateCSharp_WithoutOpenAPI(bool useProgramMain)
+    public async Task WebApiNativeAotTemplateCSharp_WithoutOpenAPI(bool useProgramMain)
     {
         var args = useProgramMain
             ? new[] { ArgConstants.UseProgramMain, ArgConstants.NoOpenApi }
             : new[] { ArgConstants.NoOpenApi };
 
-        await WebApiNativeAotTemplateCore(languageOverride: null, args: args, additionalEndpointsThatShould404NotFound: new[] { "/openapi/v1.json" });
+        await WebApiNativeAotTemplateCore(
+            languageOverride: null,
+            args: args,
+            additionalEndpointsThatShould404NotFoundForBuiltProjects: new[] { "/openapi/v1.json" };
     }
 
-    private async Task WebApiNativeAotTemplateCore(string languageOverride, string[] args = null, string[] additionalEndpointsThatShould200Ok = null, string[] additionalEndpointsThatShould404NotFound = null)
+    private async Task WebApiNativeAotTemplateCore(
+        string languageOverride,
+        string[] args = null,
+        string[] additionalEndpointsThatShould200OkForBuiltProjects = null,
+        string[] additionalEndpointsThatShould200OkForPublishedProjects = null,
+        string[] additionalEndpointsThatShould404NotFoundForBuiltProjects = null,
+        string[] additionalEndpointsThatShould404NotFoundForPublishedProjects = null)
     {
         var project = await ProjectFactory.CreateProject(Output);
         project.SetCurrentRuntimeIdentifier();
@@ -90,7 +111,7 @@ public class WebApiNativeAotTemplateTest : LoggedTest
             Assert.False(
                aspNetProcess.Process.HasExited,
                ErrorMessages.GetFailedProcessMessageOrEmpty("Run built project", project, aspNetProcess.Process));
-            await AssertEndpoints(aspNetProcess, additionalEndpointsThatShould200Ok, additionalEndpointsThatShould404NotFound);
+            await AssertEndpoints(aspNetProcess, additionalEndpointsThatShould200OkForBuiltProjects, additionalEndpointsThatShould404NotFoundForBuiltProjects);
         }
 
         using (var aspNetProcess = project.StartPublishedProjectAsync(noHttps: true, usePublishedAppHost: true))
@@ -99,7 +120,7 @@ public class WebApiNativeAotTemplateTest : LoggedTest
                 aspNetProcess.Process.HasExited,
                 ErrorMessages.GetFailedProcessMessageOrEmpty("Run published project", project, aspNetProcess.Process));
 
-            await AssertEndpoints(aspNetProcess, additionalEndpointsThatShould200Ok, additionalEndpointsThatShould404NotFound);
+            await AssertEndpoints(aspNetProcess, additionalEndpointsThatShould200OkForPublishedProjects, additionalEndpointsThatShould404NotFoundForPublishedProjects);
         }
     }
 
