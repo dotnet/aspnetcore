d6f407aed73e59f63c29aaff9ade8618dcfdf971
Artak Mkrtchyan <artakm@microsoft.com>
2025-03-06 09:39:23 -0800

    Added a test to validate generic HttpClient setup works.

--- src/Mvc/test/Mvc.FunctionalTests/RealServerBackedIntegrationTests.cs
+++ src/Mvc/test/Mvc.FunctionalTests/RealServerBackedIntegrationTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Net;
+using System.Net.Http;
 using System.Net.Http.Headers;
 
 namespace Microsoft.AspNetCore.Mvc.FunctionalTests;
@@ -36,5 +37,21 @@ public class RealServerBackedIntegrationTests : IClassFixture<KestrelBasedWapFac
         Assert.Contains("second", responseContent);
         Assert.Contains("wall", responseContent);
         Assert.Contains("floor", responseContent);
+    }
+
+    [Fact]
+    public async Task ServerReachableViaGenericHttpClient()
+    {
+        // Arrange
+        var baseAddress = new Uri("http://localhost:5000");
+
+        // Act
+        using var factoryClient = Factory.CreateClient();
+        using var client = new HttpClient() { BaseAddress = factoryClient.BaseAddress };
+
+        using var response = await client.GetAsync("/");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
 }
