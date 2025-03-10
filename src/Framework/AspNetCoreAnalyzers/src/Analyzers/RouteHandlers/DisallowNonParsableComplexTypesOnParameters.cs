b3854fd897ba8072ce2e6e1a8523f3f87cf48cdd
Ben Hopkins <ben@hopkins.tech>
2025-03-10 21:58:28 +0000

    Added comments around the additional check

--- src/Framework/AspNetCoreAnalyzers/src/Analyzers/RouteHandlers/DisallowNonParsableComplexTypesOnParameters.cs
+++ src/Framework/AspNetCoreAnalyzers/src/Analyzers/RouteHandlers/DisallowNonParsableComplexTypesOnParameters.cs
@@ -127,7 +127,8 @@ public partial class RouteHandlerAnalyzer : DiagnosticAnalyzer
                 parameterTypeSymbol = namedTypeSymbol;
             }
 
-            // If it is nullable, unwrap it.
+            // If it is nullable and we have type arguments, unwrap it.
+            // The length check aims to alleviate AD0001 warnings when referencing methods in external class libraries that contain parameters
             if (parameterTypeSymbol!.ConstructedFrom.SpecialType == SpecialType.System_Nullable_T &&
                 parameterTypeSymbol.TypeArguments.Length > 0)
             {
