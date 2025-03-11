e6eb15181b94a6a431b98a9cfb338150bb4fe153
Varorbc <Varorbc@gmail.com>
2025-03-12 05:48:01 +0800

    use primary constructors (#59712)
    Co-authored-by: Safia Abdalla <safia@microsoft.com>

--- src/ProjectTemplates/Web.ProjectTemplates/content/WebApiAot-CSharp/Program.Main.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/WebApiAot-CSharp/Program.Main.cs
@@ -28,13 +28,14 @@ public class Program
         }
 #endif
 
-        var sampleTodos = new Todo[] {
+        Todo[] sampleTodos =
+        [
             new(1, "Walk the dog"),
             new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
             new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
             new(4, "Clean the bathroom"),
             new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-        };
+        ];
 
         var todosApi = app.MapGroup("/todos");
 #if (EnableOpenAPI)
