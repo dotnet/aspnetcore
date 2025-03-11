ba6ad54bba45f7427f7bd1d7be41f46658806460
wtgodbe <wigodbe@microsoft.com>
2025-03-11 14:04:50 -0700

    Delete SignalR ts client

--- src/SignalR/clients/ts/signalr/webpack.config.js
+++
@@ -1,5 +0,0 @@
-// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
-const baseConfig = require("../webpack.config.base");
-module.exports = baseConfig(__dirname, "signalr");
