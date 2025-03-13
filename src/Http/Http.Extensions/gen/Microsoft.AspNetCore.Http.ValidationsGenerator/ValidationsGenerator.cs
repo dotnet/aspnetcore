f470910b2fbf562eaaffc13491af551d6a6114af
Safia Abdalla <safia@microsoft.com>
2025-03-12 20:38:50 -0700

    Support input validation for minimal APIs via generic resolver model (#60724)
    * Add generic implementation for validations source generator
    
    * Make validate methods async and fix AoT suppressions
    
    * Update emitted code
    
    * Remove deadcode
    
    * Clean up API shapes a bit
    
    * Fix up RequiredAttribute handling
    
    * Add ValidatableContext and simplify API signature
    
    * Support ValidationOptions and multiple resolvers
    
    * Make ValidatableContext a setup entry
    
    * Move registration of filter to route handlers
    
    * Fix async for tests and IValidatableObject
    
    * Add doc comments
    
    * Add more tests
    
    * Enable PublicAPI analyzers and update public API
    
    * Add MaxDepth handling
    
    * Docs tweaks and package generator in shared framework
    
    * Clean up tests
    
    * Update for trimming
    
    * Harden parameter resolution check
    
    * Switch to runtime-based resolution for ParameterInfo validations
    
    * Prune out uneeded types
    
    * Fix up ValidatableParameterInfo signature
    
    * Make Validate methods virtual and support CustomValidationAttribute
    
    * Fix up emitted code and use explicit namespaces
    
    * Fix up suppression for ValidationContext trimming
    
    * Actually use attribute-based suppression
    
    * Fix up suppression for trimming warnings
    
    * Benchmarks, more tests, some tweaks
    
    * More tests and add DisableValidationFilter
    
    * Update API and add sample app
    
    * Tweak more APIs
    
    * Tweaks after API review
    
    * Fix nullability handling in generator and add sample Http file
    
    * Harden nullability and index checks in generator
    
    * Don't generate TypeInfo for invalidatable types
    
    * No-op code gen under more cases
    
    * Address feedback
    
    * Address feedback
    
    * Scrub out InterceptsLocationAttribute lines
    
    * Exempt more types, add no-op tests
    
    * Use List<Type> directly for method

--- src/Http/Http.Extensions/gen/Microsoft.AspNetCore.Http.ValidationsGenerator/ValidationsGenerator.cs
+++ src/Http/Http.Extensions/gen/Microsoft.AspNetCore.Http.ValidationsGenerator/ValidationsGenerator.cs
@@ -1,14 +1,52 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Linq;
 using Microsoft.CodeAnalysis;
 
 namespace Microsoft.AspNetCore.Http.ValidationsGenerator;
 
+[Generator]
 public sealed partial class ValidationsGenerator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        return;
+        // Resolve the symbols that will be required when making comparisons
+        // in future steps.
+        var requiredSymbols = context.CompilationProvider.Select(ExtractRequiredSymbols);
+
+        // Find the builder.Services.AddValidation() call in the application.
+        var addValidation = context.SyntaxProvider.CreateSyntaxProvider(
+            predicate: FindAddValidation,
+            transform: TransformAddValidation
+        );
+        // Extract types that have been marked with [ValidatableType].
+        var validatableTypesWithAttribute = context.SyntaxProvider.ForAttributeWithMetadataName(
+            "Microsoft.AspNetCore.Http.Validation.ValidatableTypeAttribute",
+            predicate: ShouldTransformSymbolWithAttribute,
+            transform: TransformValidatableTypeWithAttribute
+        );
+        // Extract all minimal API endpoints in the application.
+        var endpoints = context.SyntaxProvider
+            .CreateSyntaxProvider(
+                predicate: FindEndpoints,
+                transform: TransformEndpoints)
+            .Where(endpoint => endpoint is not null);
+        // Extract validatable types from all endpoints.
+        var validatableTypesFromEndpoints = endpoints
+            .Combine(requiredSymbols)
+            .Select(ExtractValidatableEndpoint);
+        // Join all validatable types encountered in the type graph.
+        var validatableTypes = validatableTypesWithAttribute
+            .Concat(validatableTypesFromEndpoints)
+            .Distinct(ValidatableTypeComparer.Instance)
+            .Collect();
+
+        var emitInputs = addValidation
+            .Combine(validatableTypes);
+
+        // Emit the IValidatableInfo resolver injection and
+        // ValidatableTypeInfo for all validatable types.
+        context.RegisterSourceOutput(emitInputs, Emit);
     }
 }
