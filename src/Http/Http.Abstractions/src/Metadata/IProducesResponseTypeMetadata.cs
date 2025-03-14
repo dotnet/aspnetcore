3ca5e527549f7cffd6364b9c040f283718c2452b
Sander ten Brinke <s.tenbrinke2@gmail.com>
2025-03-14 18:01:32 +0100

    Apply suggestions from code review
    Co-authored-by: Safia Abdalla <safia@safia.rocks>

--- src/Http/Http.Abstractions/src/Metadata/IProducesResponseTypeMetadata.cs
+++ src/Http/Http.Abstractions/src/Metadata/IProducesResponseTypeMetadata.cs
@@ -21,7 +21,7 @@ public interface IProducesResponseTypeMetadata
     /// <summary>
     /// Gets the description of the response.
     /// </summary>
-    string? Description { get; } = null;
+    string? Description => null;
 
     /// <summary>
     /// Gets the content types supported by the metadata.
