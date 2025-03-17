bc2f372447c249133ca270dcd6647edc0ca10b88
Safia Abdalla <safia@safia.rocks>
2025-03-17 12:42:01 -0700

    Use trim-safe ValiationContext constructor and update RuntimeParameterInfoResolver

--- src/Http/Http.Abstractions/src/Validation/RuntimeValidatableParameterInfoResolver.cs
+++ src/Http/Http.Abstractions/src/Validation/RuntimeValidatableParameterInfoResolver.cs
@@ -27,6 +27,15 @@ internal sealed class RuntimeValidatableParameterInfoResolver : IValidatableInfo
         var validationAttributes = parameterInfo
             .GetCustomAttributes<ValidationAttribute>()
             .ToArray();
+
+        // If there are no validation attributes and this type is not a complex type
+        // we don't need to validate it. Complex types without attributes are still
+        // validatable because we want to run the validations on the properties.
+        if (validationAttributes.Length == 0 && !IsClass(parameterInfo.ParameterType))
+        {
+            validatableInfo = null;
+            return false;
+        }
         validatableInfo = new RuntimeValidatableParameterInfo(
             parameterType: parameterInfo.ParameterType,
             name: parameterInfo.Name,
@@ -47,7 +56,7 @@ internal sealed class RuntimeValidatableParameterInfoResolver : IValidatableInfo
         return parameterInfo.Name!;
     }
 
-    private sealed class RuntimeValidatableParameterInfo(
+    internal sealed class RuntimeValidatableParameterInfo(
         Type parameterType,
         string name,
         string displayName,
@@ -57,5 +66,32 @@ internal sealed class RuntimeValidatableParameterInfoResolver : IValidatableInfo
         protected override ValidationAttribute[] GetValidationAttributes() => _validationAttributes;
 
         private readonly ValidationAttribute[] _validationAttributes = validationAttributes;
+    }
+
+    private static bool IsClass(Type type)
+    {
+        // Skip primitives, enums, and common built-in types that don't need validation
+        // if they don't have attributes
+        if (type.IsPrimitive ||
+            type.IsEnum ||
+            type == typeof(string) ||
+            type == typeof(decimal) ||
+            type == typeof(DateTime) ||
+            type == typeof(DateTimeOffset) ||
+            type == typeof(TimeOnly) ||
+            type == typeof(DateOnly) ||
+            type == typeof(TimeSpan) ||
+            type == typeof(Guid))
+        {
+            return false;
+        }
+
+        // Check if the underlying type in a nullable is valid
+        if (Nullable.GetUnderlyingType(type) is { } nullableType)
+        {
+            return IsClass(nullableType);
+        }
+
+        return type.IsClass;
     }
 }
