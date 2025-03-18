18b3b4ac8dfc4498edc6bf26c7bde9a35ef840f8
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2025-03-18 16:48:44 +0100

    Add `NotFound` method in `NavigationManager` for interactive and static rendering (#60752)
    * Interactive NotFound event + SSR status code.
    
    * Remove circular DI, pass renderer on initialization instead.
    
    * Interactive NotFound tests.
    
    * Server test.
    
    * @javiercn's fixes
    
    * Fix failing test that was missing initialization.

--- src/Components/test/E2ETest/ServerRenderingTests/NoInteractivityTest.cs
+++ src/Components/test/E2ETest/ServerRenderingTests/NoInteractivityTest.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Net.Http;
 using Components.TestServer.RazorComponents;
 using Microsoft.AspNetCore.Components.E2ETest.Infrastructure;
 using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
