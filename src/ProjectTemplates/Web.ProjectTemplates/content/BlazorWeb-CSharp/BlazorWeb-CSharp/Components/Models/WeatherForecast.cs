44e7d8e6dd02c5f1711a45b4d2794562654e3bbb
Ilona Tomkowicz <itomkowicz@microsoft.com>
2025-03-25 10:51:51 +0100

    Apply smallest possible changes to templates.

--- src/ProjectTemplates/Web.ProjectTemplates/content/BlazorWeb-CSharp/BlazorWeb-CSharp/Components/Models/WeatherForecast.cs
+++
@@ -1,8 +0,0 @@
-public class WeatherForecast
-{
-    public int Id { get; set; }
-    public DateOnly Date { get; set; }
-    public int TemperatureC { get; set; }
-    public string? Summary { get; set; }
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
