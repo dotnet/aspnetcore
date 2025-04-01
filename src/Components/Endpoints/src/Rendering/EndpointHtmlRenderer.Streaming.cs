e845ad72ce5098d1191c8d7b31290706caf8c84a
Marek Fišera <mara@neptuo.com>
2025-04-01 14:08:03 +0200

    Fix style rules

--- src/Components/Endpoints/src/Rendering/EndpointHtmlRenderer.Streaming.cs
+++ src/Components/Endpoints/src/Rendering/EndpointHtmlRenderer.Streaming.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Diagnostics;
-using System.Linq;
-using System.Reflection.PortableExecutable;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.Encodings.Web;
