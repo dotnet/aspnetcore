836e2597c04c51811acdc04ebb2c756ac9b94ba0
Marek Fišera <mara@neptuo.com>
2025-04-03 15:16:53 +0200

    Feedback

--- src/Components/Endpoints/src/Builder/ResourcePreloadCollection.cs
+++ src/Components/Endpoints/src/Builder/ResourcePreloadCollection.cs
@@ -13,80 +13,86 @@ internal class ResourcePreloadCollection
 
     public ResourcePreloadCollection(ResourceAssetCollection assets)
     {
-        if (assets != null)
+        var headerBuilder = new StringBuilder();
+        var headers = new Dictionary<string, List<(int Order, string Value)>>();
+        foreach (var asset in assets)
         {
-            var headers = new List<(string? Group, int Order, string Value)>();
-            foreach (var asset in assets)
+            if (asset.Properties == null)
             {
-                if (asset.Properties == null)
-                {
-                    continue;
-                }
+                continue;
+            }
 
-                // Use preloadgroup=webassembly to identify assets that should to be preloaded
-                string? group = null;
-                foreach (var property in asset.Properties)
+            // Use preloadgroup property to identify assets that should be preloaded
+            string? group = null;
+            foreach (var property in asset.Properties)
+            {
+                if (property.Name.Equals("preloadgroup", StringComparison.OrdinalIgnoreCase))
                 {
-                    if (property.Name.Equals("preloadgroup", StringComparison.OrdinalIgnoreCase))
-                    {
-                        group = property.Value ?? string.Empty;
-                        break;
-                    }
-                }
-
-                if (group == null)
-                {
-                    continue;
-                }
-
-                var header = new StringBuilder();
-                header.Append('<');
-                header.Append(asset.Url);
-                header.Append('>');
-
-                int order = 0;
-                foreach (var property in asset.Properties)
-                {
-                    if (property.Name.Equals("preloadrel", StringComparison.OrdinalIgnoreCase))
-                    {
-                        header.Append("; rel=").Append(property.Value);
-                    }
-                    else if (property.Name.Equals("preloadas", StringComparison.OrdinalIgnoreCase))
-                    {
-                        header.Append("; as=").Append(property.Value);
-                    }
-                    else if (property.Name.Equals("preloadpriority", StringComparison.OrdinalIgnoreCase))
-                    {
-                        header.Append("; fetchpriority=").Append(property.Value);
-                    }
-                    else if (property.Name.Equals("preloadcrossorigin", StringComparison.OrdinalIgnoreCase))
-                    {
-                        header.Append("; crossorigin=").Append(property.Value);
-                    }
-                    else if (property.Name.Equals("integrity", StringComparison.OrdinalIgnoreCase))
-                    {
-                        header.Append("; integrity=\"").Append(property.Value).Append('"');
-                    }
-                    else if (property.Name.Equals("preloadorder", StringComparison.OrdinalIgnoreCase))
-                    {
-                        if (!int.TryParse(property.Value, out order))
-                        {
-                            order = 0;
-                        }
-                    }
-                }
-
-                if (header != null)
-                {
-                    headers.Add((group, order, header.ToString()));
+                    group = property.Value ?? string.Empty;
+                    break;
                 }
             }
 
-            foreach (var group in headers.GroupBy(h => h.Group))
+            if (group == null)
             {
-                _storage[group.Key ?? string.Empty] = group.OrderBy(h => h.Order).Select(h => h.Value).ToArray();
+                continue;
+            }
+
+            var header = CreateHeader(headerBuilder, asset.Url, asset.Properties);
+            if (!headers.TryGetValue(group, out var groupHeaders))
+            {
+                groupHeaders = headers[group] = new List<(int Order, string Value)>();
+            }
+
+            groupHeaders.Add(header);
+        }
+
+        foreach (var group in headers)
+        {
+            _storage[group.Key ?? string.Empty] = group.Value.OrderBy(h => h.Order).Select(h => h.Value).ToArray();
+        }
+    }
+
+    private static (int order, string header) CreateHeader(StringBuilder headerBuilder, string url, IEnumerable<ResourceAssetProperty> properties)
+    {
+        headerBuilder.Clear();
+        headerBuilder.Append('<');
+        headerBuilder.Append(url);
+        headerBuilder.Append('>');
+
+        int order = 0;
+        foreach (var property in properties)
+        {
+            if (property.Name.Equals("preloadrel", StringComparison.OrdinalIgnoreCase))
+            {
+                headerBuilder.Append("; rel=").Append(property.Value);
+            }
+            else if (property.Name.Equals("preloadas", StringComparison.OrdinalIgnoreCase))
+            {
+                headerBuilder.Append("; as=").Append(property.Value);
+            }
+            else if (property.Name.Equals("preloadpriority", StringComparison.OrdinalIgnoreCase))
+            {
+                headerBuilder.Append("; fetchpriority=").Append(property.Value);
+            }
+            else if (property.Name.Equals("preloadcrossorigin", StringComparison.OrdinalIgnoreCase))
+            {
+                headerBuilder.Append("; crossorigin=").Append(property.Value);
+            }
+            else if (property.Name.Equals("integrity", StringComparison.OrdinalIgnoreCase))
+            {
+                headerBuilder.Append("; integrity=\"").Append(property.Value).Append('"');
+            }
+            else if (property.Name.Equals("preloadorder", StringComparison.OrdinalIgnoreCase))
+            {
+                if (!int.TryParse(property.Value, out order))
+                {
+                    order = 0;
+                }
             }
         }
+
+        return (order, headerBuilder.ToString());
     }
 
     public bool TryGetLinkHeaders(string group, out StringValues linkHeaders)
