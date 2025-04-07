f33bc0304e0d0744286eab8ea9b0f45acec6fd73
Günther Foidl <gue@korporal.at>
2025-04-07 10:56:55 +0200

    Removed bound checks in SharedUrlHelper.IsLocalUrl and use JIT unrolling for checks

--- src/Shared/ResultsHelpers/SharedUrlHelper.cs
+++ src/Shared/ResultsHelpers/SharedUrlHelper.cs
@@ -37,40 +37,40 @@ internal static class SharedUrlHelper
             return false;
         }
 
+        var urlSpan = url.AsSpan();
+
         // Allows "/" or "/foo" but not "//" or "/\".
-        if (url[0] == '/')
+        if (urlSpan.StartsWith('/'))
         {
             // url is exactly "/"
-            if (url.Length == 1)
+            if (urlSpan.Length < 2)
             {
                 return true;
             }
 
             // url doesn't start with "//" or "/\"
-            if (url[1] != '/' && url[1] != '\\')
+            if (urlSpan[1] is not '/' and '\\')
             {
-                return !HasControlCharacter(url.AsSpan(1));
+                return !HasControlCharacter(urlSpan.Slice(1));
             }
 
             return false;
         }
 
         // Allows "~/" or "~/foo" but not "~//" or "~/\".
-        if (url[0] == '~' && url.Length > 1 && url[1] == '/')
+        if (urlSpan.StartsWith("~/"))
         {
             // url is exactly "~/"
-            if (url.Length == 2)
+            if (urlSpan.Length < 3)
             {
                 return true;
             }
 
             // url doesn't start with "~//" or "~/\"
-            if (url[2] != '/' && url[2] != '\\')
+            if (urlSpan[2] is not '/' and '\\')
             {
-                return !HasControlCharacter(url.AsSpan(2));
+                return !HasControlCharacter(urlSpan.Slice(2));
             }
-
-            return false;
         }
 
         return false;
