c522c61ee8e087305a52ad198f392bc7253b2c24
Safia Abdalla <safia@safia.rocks>
2025-04-17 14:42:02 -0700

    Update to Microsoft.OpenApi v2.0.0-preview.17

--- src/OpenApi/test/Microsoft.AspNetCore.OpenApi.SourceGenerators.Tests/OperationTests.MinimalApis.cs
+++ src/OpenApi/test/Microsoft.AspNetCore.OpenApi.SourceGenerators.Tests/OperationTests.MinimalApis.cs
@@ -1,8 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Net.Http;
 using System.Text.Json.Nodes;
-using Microsoft.OpenApi.Models;
 
 namespace Microsoft.AspNetCore.OpenApi.SourceGenerators.Tests;
 
@@ -231,68 +231,68 @@ public class Example : Task<int>
         await SnapshotTestHelper.Verify(source, generator, out var compilation);
         await SnapshotTestHelper.VerifyOpenApi(compilation, document =>
         {
-            var path = document.Paths["/1"].Operations[OperationType.Get];
+            var path = document.Paths["/1"].Operations[HttpMethod.Get];
             Assert.Equal("A summary of the action.", path.Summary);
             Assert.Equal("A description of the action.", path.Description);
 
-            var path2 = document.Paths["/2"].Operations[OperationType.Get];
+            var path2 = document.Paths["/2"].Operations[HttpMethod.Get];
             Assert.Equal("The name of the person.", path2.Parameters[0].Description);
             Assert.Equal("Returns the greeting.", path2.Responses["200"].Description);
 
-            var path3 = document.Paths["/3"].Operations[OperationType.Get];
+            var path3 = document.Paths["/3"].Operations[HttpMethod.Get];
             Assert.Equal("The name of the person.", path3.Parameters[0].Description);
             var example = Assert.IsAssignableFrom<JsonNode>(path3.Parameters[0].Example);
             Assert.Equal("\"Testy McTester\"", example.ToJsonString());
 
-            var path4 = document.Paths["/4"].Operations[OperationType.Get];
+            var path4 = document.Paths["/4"].Operations[HttpMethod.Get];
             var response = path4.Responses["404"];
             Assert.Equal("Indicates that the value was not found.", response.Description);
 
-            var path5 = document.Paths["/5"].Operations[OperationType.Get];
+            var path5 = document.Paths["/5"].Operations[HttpMethod.Get];
             Assert.Equal("Indicates that the value was not found.", path5.Responses["404"].Description);
             Assert.Equal("Indicates that the value is even.", path5.Responses["200"].Description);
             Assert.Equal("Indicates that the value is less than 50.", path5.Responses["201"].Description);
 
-            var path6 = document.Paths["/6"].Operations[OperationType.Post];
+            var path6 = document.Paths["/6"].Operations[HttpMethod.Post];
             Assert.Equal("Creates a new user.", path6.Summary);
             Assert.Contains("Sample request:", path6.Description);
             var userParam = path6.RequestBody.Content["application/json"];
             var userExample = Assert.IsAssignableFrom<JsonNode>(userParam.Example);
             Assert.Equal("johndoe", userExample["username"].GetValue<string>());
 
-            var path7 = document.Paths["/7"].Operations[OperationType.Put];
+            var path7 = document.Paths["/7"].Operations[HttpMethod.Put];
             var idParam = path7.Parameters.First(p => p.Name == "id");
             Assert.True(idParam.Deprecated);
             Assert.Equal("Legacy ID parameter - use uuid instead.", idParam.Description);
 
-            var path8 = document.Paths["/8"].Operations[OperationType.Get];
+            var path8 = document.Paths["/8"].Operations[HttpMethod.Get];
             Assert.Equal("A summary of Get8.", path8.Summary);
 
-            var path9 = document.Paths["/9"].Operations[OperationType.Get];
+            var path9 = document.Paths["/9"].Operations[HttpMethod.Get];
             Assert.Equal("A summary of Get9.", path9.Summary);
 
-            var path10 = document.Paths["/10"].Operations[OperationType.Get];
+            var path10 = document.Paths["/10"].Operations[HttpMethod.Get];
             Assert.Equal("A summary of Get10.", path10.Summary);
 
-            var path11 = document.Paths["/11"].Operations[OperationType.Get];
+            var path11 = document.Paths["/11"].Operations[HttpMethod.Get];
             Assert.Equal("A summary of Get11.", path11.Summary);
 
-            var path12 = document.Paths["/12"].Operations[OperationType.Get];
+            var path12 = document.Paths["/12"].Operations[HttpMethod.Get];
             Assert.Equal("A summary of Get12.", path12.Summary);
 
-            var path13 = document.Paths["/13"].Operations[OperationType.Get];
+            var path13 = document.Paths["/13"].Operations[HttpMethod.Get];
             Assert.Equal("A summary of Get13.", path13.Summary);
 
-            var path14 = document.Paths["/14"].Operations[OperationType.Get];
+            var path14 = document.Paths["/14"].Operations[HttpMethod.Get];
             Assert.Equal("A summary of Get14.", path14.Summary);
 
-            var path15 = document.Paths["/15"].Operations[OperationType.Get];
+            var path15 = document.Paths["/15"].Operations[HttpMethod.Get];
             Assert.Equal("A summary of Get15.", path15.Summary);
 
-            var path16 = document.Paths["/16"].Operations[OperationType.Post];
+            var path16 = document.Paths["/16"].Operations[HttpMethod.Post];
             Assert.Equal("A summary of Post16.", path16.Summary);
 
-            var path17 = document.Paths["/17"].Operations[OperationType.Get];
+            var path17 = document.Paths["/17"].Operations[HttpMethod.Get];
             Assert.Equal("A summary of Get17.", path17.Summary);
         });
     }
