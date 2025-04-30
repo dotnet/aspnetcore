aae7b2f3e2b43f955eed7c1271aa79ef08d808bc
Dmitrii Korolev <dmkorolev@microsoft.com>
2025-04-30 18:20:24 +0200

    added comment

--- src/Servers/Kestrel/Core/src/HttpsConnectionAdapterOptions.cs
+++ src/Servers/Kestrel/Core/src/HttpsConnectionAdapterOptions.cs
@@ -100,6 +100,8 @@ public class HttpsConnectionAdapterOptions
     /// <summary>
     /// A callback to be invoked to get the TLS client hello bytes.
     /// Null by default.
+    /// If you want to store the bytes from the <see cref="System.Buffers.ReadOnlySequence{T}"/>, 
+    /// copy them into a buffer that you control rather than keeping a reference to the <see cref="ReadOnlySequence{T}"/> or <see cref="ReadOnlyMemory{T}"/> instances.
     /// </summary>
     public Action<ConnectionContext, ReadOnlySequence<byte>>? TlsClientHelloBytesCallback { get; set; }
 
