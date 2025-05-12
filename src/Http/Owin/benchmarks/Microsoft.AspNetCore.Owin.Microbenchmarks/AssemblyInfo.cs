f5f9ac739c4ea359d3392895d0af17d3730ac9fc
Dmitrii Korolev <dmkorolev@microsoft.com>
2025-05-12 12:43:48 +0200

    fix build & bench setup

--- src/Http/Owin/benchmarks/Microsoft.AspNetCore.Owin.Microbenchmarks/AssemblyInfo.cs
+++ src/Http/Owin/benchmarks/Microsoft.AspNetCore.Owin.Microbenchmarks/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using BenchmarkDotNet.Running;
-
-BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run();
+[assembly: BenchmarkDotNet.Attributes.AspNetCoreBenchmark]
