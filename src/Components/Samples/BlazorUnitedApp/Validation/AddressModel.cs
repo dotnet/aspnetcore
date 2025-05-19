10e7eb45bc9ab3d17a644551dd4d9a28fe159e05
Ondřej Roztočil <roztocil@outlook.com>
2025-05-19 16:35:58 +0200

    Improve demo

--- src/Components/Samples/BlazorUnitedApp/Validation/AddressModel.cs
+++ src/Components/Samples/BlazorUnitedApp/Validation/AddressModel.cs
@@ -7,9 +7,6 @@ public class AddressModel
     [Required(ErrorMessage = "Street is required.")]
     public string? Street { get; set; }
 
-    [Required(ErrorMessage = "City is required.")]
-    public string? City { get; set; }
-
     [Required(ErrorMessage = "Zip Code is required.")]
     [StringLength(10, MinimumLength = 5, ErrorMessage = "Zip Code must be between 5 and 10 characters.")]
     public string? ZipCode { get; set; }
