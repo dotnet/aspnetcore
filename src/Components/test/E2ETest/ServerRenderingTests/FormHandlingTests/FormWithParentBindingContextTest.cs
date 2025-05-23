138f00bcf24b52e9a3bdc88f42c2bbcacec57fe5
Ilona Tomkowicz <itomkowicz@microsoft.com>
2025-05-23 09:28:07 +0200

    Backport blocking the failing test.

--- src/Components/test/E2ETest/ServerRenderingTests/FormHandlingTests/FormWithParentBindingContextTest.cs
+++ src/Components/test/E2ETest/ServerRenderingTests/FormHandlingTests/FormWithParentBindingContextTest.cs
@@ -1287,7 +1287,7 @@ public class FormWithParentBindingContextTest : ServerTestBase<BasicTestAppServe
     }
 
     [Theory]
-    [InlineData(true)]
+    // [InlineData(true)] QuarantinedTest: https://github.com/dotnet/aspnetcore/issues/61882
     [InlineData(false)]
     public void CanUseFormWithMethodGet(bool suppressEnhancedNavigation)
     {
