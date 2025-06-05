18cd941505eff53886554b7453fca8584ebd8e67
Javier Calvarro Nelson <jacalvar@microsoft.com>
2025-06-05 17:52:48 +0200

    Undo sample changes

--- src/Components/Samples/BlazorUnitedApp/Data/WeatherForecast.cs
+++ src/Components/Samples/BlazorUnitedApp/Data/WeatherForecast.cs
@@ -1,11 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics;
-
 namespace BlazorUnitedApp.Data;
 
-[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
 public class WeatherForecast
 {
     public DateOnly Date { get; set; }
@@ -15,7 +12,4 @@ public class WeatherForecast
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
-
-    private string GetDebuggerDisplay() =>
-        $"{Date:yyyy-MM-dd}: {TemperatureC}°C ({TemperatureF}°F) - {Summary ?? "No summary"}";
 }
--- src/Components/Samples/BlazorUnitedApp/Data/WeatherForecastService.cs
+++ src/Components/Samples/BlazorUnitedApp/Data/WeatherForecastService.cs
@@ -10,14 +10,13 @@ public class WeatherForecastService
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    public async Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
+    public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
     {
-        await Task.Delay(1000);
-        return [.. Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = startDate.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })];
+        }).ToArray());
     }
 }
