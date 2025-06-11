32fd18991c4d09308e02fa861c0a2ff060fc3375
pavelsavara <pavel.savara@gmail.com>
2025-06-11 16:20:11 +0200

    ci

--- src/Components/Endpoints/src/RazorComponentEndpointInvoker.cs
+++ src/Components/Endpoints/src/RazorComponentEndpointInvoker.cs
@@ -3,7 +3,6 @@
 
 using System.Buffers;
 using System.Diagnostics;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Antiforgery;
