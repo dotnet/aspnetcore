971b7abcf0f42fa8e9658268d7f168adc679125d
Ilona Tomkowicz <itomkowicz@microsoft.com>
2025-06-16 09:19:37 +0200

    Update templates.

--- src/ProjectTemplates/Web.ProjectTemplates/content/BlazorWeb-CSharp/BlazorWeb-CSharp/Program.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/BlazorWeb-CSharp/BlazorWeb-CSharp/Program.cs
@@ -14,6 +14,8 @@ using BlazorWeb_CSharp.Components.Account;
 using BlazorWeb_CSharp.Data;
 #endif
 
+System.AppContext.SetSwitch("Microsoft.AspNetCore.Components.Endpoints.NavigationManager.DisableThrowNavigationException", true);
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
