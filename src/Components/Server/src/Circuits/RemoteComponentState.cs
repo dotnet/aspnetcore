2be1c4bb3b734c0872d8a68575febbf532d705e5
Javier Calvarro Nelson <jacalvar@microsoft.com>
2025-06-17 19:27:37 +0200

    Cleanup and tests

--- src/Components/Server/src/Circuits/RemoteComponentState.cs
+++ src/Components/Server/src/Circuits/RemoteComponentState.cs
@@ -1,10 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.ComponentModel;
 using Microsoft.AspNetCore.Components.Rendering;
-using Microsoft.AspNetCore.Components.RenderTree;
-using static Microsoft.AspNetCore.Components.Server.Circuits.RemoteRenderer;
 
 namespace Microsoft.AspNetCore.Components.Server.Circuits;
 
