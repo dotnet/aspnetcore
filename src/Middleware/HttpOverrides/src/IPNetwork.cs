8258d9012bebe0795c95f8f295b234a20afa4459
Weihan Li <weihanli@outlook.com>
2025-06-27 10:25:21 +0800

    Update IMicrosoft.AspNetCore.HttpOverrides.PNetwork implementation

--- src/Middleware/HttpOverrides/src/IPNetwork.cs
+++ src/Middleware/HttpOverrides/src/IPNetwork.cs
@@ -12,101 +12,35 @@ namespace Microsoft.AspNetCore.HttpOverrides;
 /// </summary>
 public class IPNetwork
 {
+    private readonly System.Net.IPNetwork _network;
+    
     /// <summary>
     /// Create a new <see cref="IPNetwork"/> with the specified <see cref="IPAddress"/> and prefix length.
     /// </summary>
     /// <param name="prefix">The <see cref="IPAddress"/>.</param>
     /// <param name="prefixLength">The prefix length.</param>
     /// <exception cref="ArgumentOutOfRangeException"><paramref name="prefixLength"/> is out of range.</exception>
-    public IPNetwork(IPAddress prefix, int prefixLength) : this(prefix, prefixLength, true)
+    public IPNetwork(IPAddress prefix, int prefixLength)
     {
-    }
-
-    private IPNetwork(IPAddress prefix, int prefixLength, bool checkPrefixLengthRange)
-    {
-        if (checkPrefixLengthRange &&
-            !IsValidPrefixLengthRange(prefix, prefixLength))
-        {
-            throw new ArgumentOutOfRangeException(nameof(prefixLength), "The prefix length was out of range.");
-        }
-
-        Prefix = prefix;
-        PrefixLength = prefixLength;
-        PrefixBytes = Prefix.GetAddressBytes();
-        Mask = CreateMask();
+        _network = new(prefix, prefixLength);
     }
 
     /// <summary>
     /// Get the <see cref="IPAddress"/> that represents the prefix for the network.
     /// </summary>
-    public IPAddress Prefix { get; }
-
-    private byte[] PrefixBytes { get; }
+    public IPAddress Prefix => _network.BaseAddress;
 
     /// <summary>
     /// The CIDR notation of the subnet mask
     /// </summary>
-    public int PrefixLength { get; }
-
-    private byte[] Mask { get; }
+    public int PrefixLength => _network.PrefixLength;
 
     /// <summary>
     /// Determine whether a given The <see cref="IPAddress"/> is part of the IP network.
     /// </summary>
     /// <param name="address">The <see cref="IPAddress"/>.</param>
     /// <returns><see langword="true"/> if the <see cref="IPAddress"/> is part of the IP network. Otherwise, <see langword="false"/>.</returns>
-    public bool Contains(IPAddress address)
-    {
-        if (Prefix.AddressFamily != address.AddressFamily)
-        {
-            return false;
-        }
-
-        var addressBytes = address.GetAddressBytes();
-        for (int i = 0; i < PrefixBytes.Length && Mask[i] != 0; i++)
-        {
-            if ((PrefixBytes[i] & Mask[i]) != (addressBytes[i] & Mask[i]))
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private byte[] CreateMask()
-    {
-        var mask = new byte[PrefixBytes.Length];
-        int remainingBits = PrefixLength;
-        int i = 0;
-        while (remainingBits >= 8)
-        {
-            mask[i] = 0xFF;
-            i++;
-            remainingBits -= 8;
-        }
-        if (remainingBits > 0)
-        {
-            mask[i] = (byte)(0xFF << (8 - remainingBits));
-        }
-
-        return mask;
-    }
-
-    private static bool IsValidPrefixLengthRange(IPAddress prefix, int prefixLength)
-    {
-        if (prefixLength < 0)
-        {
-            return false;
-        }
-
-        return prefix.AddressFamily switch
-        {
-            AddressFamily.InterNetwork => prefixLength <= 32,
-            AddressFamily.InterNetworkV6 => prefixLength <= 128,
-            _ => true
-        };
-    }
+    public bool Contains(IPAddress address) => _network.Contains(address);
 
     /// <summary>
     /// Converts the specified <see cref="ReadOnlySpan{T}"/> of <see langword="char"/> representation of
@@ -121,17 +55,7 @@ public class IPNetwork
     /// <inheritdoc cref="TryParseComponents(ReadOnlySpan{char}, out IPAddress?, out int)"/>
     public static IPNetwork Parse(ReadOnlySpan<char> networkSpan)
     {
-        if (!TryParseComponents(networkSpan, out var prefix, out var prefixLength))
-        {
-            throw new FormatException("An invalid IP address or prefix length was specified.");
-        }
-
-        if (!IsValidPrefixLengthRange(prefix, prefixLength))
-        {
-            throw new ArgumentOutOfRangeException(nameof(networkSpan), "The prefix length was out of range.");
-        }
-
-        return new IPNetwork(prefix, prefixLength, false);
+        return System.Net.IPNetwork.Parse(networkSpan);
     }
 
     /// <summary>
@@ -152,55 +76,20 @@ public class IPNetwork
     /// <inheritdoc cref="TryParseComponents(ReadOnlySpan{char}, out IPAddress?, out int)"/>
     public static bool TryParse(ReadOnlySpan<char> networkSpan, [NotNullWhen(true)] out IPNetwork? network)
     {
-        network = null;
-
-        if (!TryParseComponents(networkSpan, out var prefix, out var prefixLength))
+        if (System.Net.IPNetwork.TryParse(networkSpan, out var ipNetwork))
         {
-            return false;
+            network = ipNetwork;
+            return true;
         }
 
-        if (!IsValidPrefixLengthRange(prefix, prefixLength))
-        {
-            return false;
-        }
-
-        network = new IPNetwork(prefix, prefixLength, false);
-        return true;
+        return false;
     }
 
-    /// <remarks>
-    /// <para>
-    /// The specified representation must be expressed using CIDR (Classless Inter-Domain Routing) notation, or 'slash notation',
-    /// which contains an IPv4 or IPv6 address and the subnet mask prefix length, separated by a forward slash.
-    /// </para>
-    /// <example>
-    /// e.g. <c>"192.168.0.1/31"</c> for IPv4, <c>"2001:db8:3c4d::1/127"</c> for IPv6
-    /// </example>
-    /// </remarks>
-    private static bool TryParseComponents(
-        ReadOnlySpan<char> networkSpan,
-        [NotNullWhen(true)] out IPAddress? prefix,
-        out int prefixLength)
+    /// <summary>
+    /// Convert <see cref="System.Net.IPNetwork" /> to <see cref="Microsoft.AspNetCore.HttpOverrides.IPNetwork" /> implicitly
+    /// </sumary>
+    public static implicit operator IPNetwork(System.Net.IPNetwork ipNetwork)
     {
-        prefix = null;
-        prefixLength = default;
-
-        var forwardSlashIndex = networkSpan.IndexOf('/');
-        if (forwardSlashIndex < 0)
-        {
-            return false;
-        }
-
-        if (!IPAddress.TryParse(networkSpan.Slice(0, forwardSlashIndex), out prefix))
-        {
-            return false;
-        }
-
-        if (!int.TryParse(networkSpan.Slice(forwardSlashIndex + 1), out prefixLength))
-        {
-            return false;
-        }
-
-        return true;
+        return new IPNetwork(ipNetwork.BaseAddress, ipNetwork.PrefixLength);
     }
 }
