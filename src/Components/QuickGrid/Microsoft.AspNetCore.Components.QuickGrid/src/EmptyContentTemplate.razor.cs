504d4530e7560293af6ec5089ffdfff1bbe1a087
Adam Shortland <ashortland@equinix.com>
2025-07-08 13:23:28 -0400

    Adds documentation to EmptyContentTemplate.razor.cs Makes ChildContent nullable Adds relevant parts of EmptyContentTemplate to PublicAPI.Shipped.txt

--- src/Components/QuickGrid/Microsoft.AspNetCore.Components.QuickGrid/src/EmptyContentTemplate.razor.cs
+++ src/Components/QuickGrid/Microsoft.AspNetCore.Components.QuickGrid/src/EmptyContentTemplate.razor.cs
@@ -5,9 +5,16 @@ using Microsoft.AspNetCore.Components.QuickGrid.Infrastructure;
 
 namespace Microsoft.AspNetCore.Components.QuickGrid;
 
+/// <summary>
+/// A component that is displayed when the <see cref="QuickGrid{TGridItem}"/> has no data available.
+/// </summary>
+/// <typeparam name="TGridItem">The type of data represented by each row in the grid.</typeparam>
 public partial class EmptyContentTemplate<TGridItem>
 {
     [CascadingParameter] internal InternalGridContext<TGridItem> InternalGridContext { get; set; } = default!;
 
-    [Parameter] public RenderFragment ChildContent { get; set; }
+    /// <summary>
+    /// Defines the content to be rendered by <see cref="QuickGrid{TGridItem}"/> when no data is available.
+    /// </summary>
+    [Parameter] public RenderFragment? ChildContent { get; set; }
 }
