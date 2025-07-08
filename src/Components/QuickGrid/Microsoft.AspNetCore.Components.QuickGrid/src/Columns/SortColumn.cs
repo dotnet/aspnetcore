bed1adfed4e87c6563734249e754b19a6854fc2b
Adam Shortland <ashortland@equinix.com>
2025-07-08 13:30:32 -0400

    Adds license header to SortColumn

--- src/Components/QuickGrid/Microsoft.AspNetCore.Components.QuickGrid/src/Columns/SortColumn.cs
+++ src/Components/QuickGrid/Microsoft.AspNetCore.Components.QuickGrid/src/Columns/SortColumn.cs
@@ -1,4 +1,7 @@
-﻿namespace Microsoft.AspNetCore.Components.QuickGrid;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.AspNetCore.Components.QuickGrid;
 
 /// <summary>
 /// Provides information about the column that has sorting applied.
