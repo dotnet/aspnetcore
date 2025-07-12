bc997eba7765765cddfbc190ae61e3f8664b2316
Sander ten Brinke <s.tenbrinke2@gmail.com>
2025-07-12 11:54:21 +0200

    Add better type check to fix issue with description not being set

--- src/Mvc/Mvc.ApiExplorer/src/EndpointMetadataApiDescriptionProvider.cs
+++ src/Mvc/Mvc.ApiExplorer/src/EndpointMetadataApiDescriptionProvider.cs
@@ -322,6 +322,7 @@ internal sealed class EndpointMetadataApiDescriptionProvider : IApiDescriptionPr
         Type returnType,
         EndpointMetadataCollection endpointMetadata)
     {
+        System.Diagnostics.Debugger.Break();
         var responseType = returnType;
 
         // We support attributes (which implement the IApiResponseMetadataProvider) interface
@@ -405,13 +406,26 @@ internal sealed class EndpointMetadataApiDescriptionProvider : IApiDescriptionPr
             foreach (var metadata in responseMetadataTypes)
             {
                 if (metadata.StatusCode == apiResponseType.StatusCode &&
-                    metadata.Type == apiResponseType.Type &&
+                    TypesAreCompatible(apiResponseType?.Type, metadata?.Type) &&
                     metadata.Description is not null)
                 {
                     matchingDescription = metadata.Description;
                 }
             }
             return matchingDescription;
+        }
+
+        static bool TypesAreCompatible(Type? apiResponseType, Type? metadaType)
+        {
+            // We need to a special check for cases where the inferred type is different than the one specified in attributes.
+            // For example, an endpoint that defines [ProducesResponseType<IEnumerable<WeatherForecast>>],
+            // but the endpoint returns weatherForecasts.ToList(). Because List<> is a different type than IEnumerable<>, it would incorrectly return false.
+            // Currently, we do a "simple" biderectional check to see if the types are assignable to each other.
+            // This isn't very thorough, but it works for most cases.
+            // For more information, check the related bug: https://github.com/dotnet/aspnetcore/issues/60518
+            return apiResponseType == metadaType ||
+                metadaType?.IsAssignableFrom(apiResponseType) == true ||
+                apiResponseType?.IsAssignableFrom(metadaType) == true;
         }
     }
 
