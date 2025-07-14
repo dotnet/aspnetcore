01e43d143cc4a99c45aa9df77508601fa1ee42d0
Ilona Tomkowicz <itomkowicz@microsoft.com>
2025-07-14 10:26:58 +0200

    Fix.

--- src/ProjectTemplates/Web.ProjectTemplates/content/BlazorWeb-CSharp/BlazorWebCSharp.1/Program.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/BlazorWeb-CSharp/BlazorWebCSharp.1/Program.cs
@@ -94,7 +94,7 @@ else
 if (!app.Environment.IsDevelopment())
 #endif
 {
-    app.UseExceptionHandler("/Error", createScopeForStatusCodePages: true);
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
 #if (HasHttpsProfile)
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
