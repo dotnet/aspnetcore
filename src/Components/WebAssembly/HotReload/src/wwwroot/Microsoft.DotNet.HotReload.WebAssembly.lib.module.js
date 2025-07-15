d563672c3f1f6fb6c342f1bd013561d61cfbd70f
Marek Fišera <mara@neptuo.com>
2025-07-15 17:40:12 +0200

    Switch between built-in and referenced hotreload

--- src/Components/WebAssembly/HotReload/src/wwwroot/Microsoft.DotNet.HotReload.WebAssembly.lib.module.js
+++ src/Components/WebAssembly/HotReload/src/wwwroot/Microsoft.DotNet.HotReload.WebAssembly.lib.module.js
@@ -1,4 +1,22 @@
+export async function onRuntimeConfigLoaded(config) {
+    const value = "false";
+    debugger;
+    config.environmentVariables["__BLAZOR_WEBASSEMBLY_LEGACY_HOTRELOAD"] = value;
+}
+
 export async function onRuntimeReady({ getAssemblyExports }) {
     const exports = await getAssemblyExports("Microsoft.DotNet.HotReload.WebAssembly");
     await exports.Microsoft.DotNet.HotReload.WebAssembly.Interop.InitializeAsync(document.baseURI);
+
+    if (!window.Blazor) {
+        window.Blazor = {};
+    }
+
+    window.Blazor._internal.applyHotReloadDeltas = (deltas, loggingLevel) => {
+        return DotNet.invokeMethod('Microsoft.DotNet.HotReload.WebAssembly', 'ApplyHotReloadDeltas', deltas, loggingLevel) ?? [];
+    };
+
+    window.Blazor._internal.getApplyUpdateCapabilities = () => {
+        return exports.Microsoft.DotNet.HotReload.WebAssembly.Interop.GetApplyUpdateCapabilities() ?? '';
+    };
 }
