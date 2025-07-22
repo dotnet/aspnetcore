d6a0582b7036531ad5021a9a96aaf46369fed835
Mackinnon Buck <mackinnon.buck@gmail.com>
2025-07-22 11:01:39 -0400

    [release/10.0-preview7] Passkey design follow-ups (#62841)
    * Passkey design follow-ups (#62530)
    
    * Use `GetService()` instead of `GetRequiredService()`
    
    * Remove usings

---
+++ src/Identity/Extensions.Stores/src/IdentityPasskeyData.cs
@@ -0,0 +1,71 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Microsoft.AspNetCore.Identity;
+
+/// <summary>
+/// Represents data associated with a passkey.
+/// </summary>
+public class IdentityPasskeyData
+{
+    /// <summary>
+    /// Gets or sets the public key associated with this passkey.
+    /// </summary>
+    public virtual byte[] PublicKey { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the friendly name for this passkey.
+    /// </summary>
+    public virtual string? Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the time this passkey was created.
+    /// </summary>
+    public virtual DateTimeOffset CreatedAt { get; set; }
+
+    /// <summary>
+    /// Gets or sets the signature counter for this passkey.
+    /// </summary>
+    public virtual uint SignCount { get; set; }
+
+    /// <summary>
+    /// Gets or sets the transports supported by this passkey.
+    /// </summary>
+    /// <remarks>
+    /// See <see href="https://www.w3.org/TR/webauthn-3/#enumdef-authenticatortransport"/>.
+    /// </remarks>
+    public virtual string[]? Transports { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether the passkey has a verified user.
+    /// </summary>
+    public virtual bool IsUserVerified { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether the passkey is eligible for backup.
+    /// </summary>
+    public virtual bool IsBackupEligible { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether the passkey is currently backed up.
+    /// </summary>
+    public virtual bool IsBackedUp { get; set; }
+
+    /// <summary>
+    /// Gets or sets the attestation object associated with this passkey.
+    /// </summary>
+    /// <remarks>
+    /// See <see href="https://www.w3.org/TR/webauthn-3/#attestation-object"/>.
+    /// </remarks>
+    public virtual byte[] AttestationObject { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the collected client data JSON associated with this passkey.
+    /// </summary>
+    /// <remarks>
+    /// See <see href="https://www.w3.org/TR/webauthn-3/#dictdef-collectedclientdata"/>.
+    /// </remarks>
+    public virtual byte[] ClientDataJson { get; set; } = default!;
+}
--- src/Identity/test/Identity.Test/UserManagerTest.cs
+++ src/Identity/test/Identity.Test/UserManagerTest.cs
@@ -785,18 +785,18 @@ public class UserManagerTest
     }
 
     [Fact]
-    public async Task SetPasskeyAsyncCallsStore()
+    public async Task AddOrUpdatePasskeyAsyncCallsStore()
     {
         // Setup
         var store = new Mock<IUserPasskeyStore<PocoUser>>();
         var user = new PocoUser { UserName = "Foo" };
-        var passkey = new UserPasskeyInfo(null, null, null, default, 0, null, false, false, false, null, null);
-        store.Setup(s => s.SetPasskeyAsync(user, passkey, CancellationToken.None)).Returns(Task.CompletedTask).Verifiable();
+        var passkey = new UserPasskeyInfo(null, null, default, 0, null, false, false, false, null, null);
+        store.Setup(s => s.AddOrUpdatePasskeyAsync(user, passkey, CancellationToken.None)).Returns(Task.CompletedTask).Verifiable();
         store.Setup(s => s.UpdateAsync(user, CancellationToken.None)).ReturnsAsync(IdentityResult.Success).Verifiable();
         var userManager = MockHelpers.TestUserManager<PocoUser>(store.Object);
 
         // Act
-        var result = await userManager.SetPasskeyAsync(user, passkey);
+        var result = await userManager.AddOrUpdatePasskeyAsync(user, passkey);
 
         // Assert
         Assert.True(result.Succeeded);
@@ -809,7 +809,7 @@ public class UserManagerTest
         // Setup
         var store = new Mock<IUserPasskeyStore<PocoUser>>();
         var user = new PocoUser { UserName = "Foo" };
-        var passkey = new UserPasskeyInfo(null, null, null, default, 0, null, false, false, false, null, null);
+        var passkey = new UserPasskeyInfo(null, null, default, 0, null, false, false, false, null, null);
         var passkeys = (IList<UserPasskeyInfo>)[passkey];
         store.Setup(s => s.GetPasskeysAsync(user, CancellationToken.None)).Returns(Task.FromResult(passkeys)).Verifiable();
         var userManager = MockHelpers.TestUserManager<PocoUser>(store.Object);
@@ -1371,7 +1371,7 @@ public class UserManagerTest
         Assert.Throws<ArgumentNullException>("provider", () => manager.RegisterTokenProvider("whatever", null));
         await Assert.ThrowsAsync<ArgumentNullException>("roles", async () => await manager.AddToRolesAsync(new PocoUser(), null));
         await Assert.ThrowsAsync<ArgumentNullException>("roles", async () => await manager.RemoveFromRolesAsync(new PocoUser(), null));
-        await Assert.ThrowsAsync<ArgumentNullException>("passkey", async () => await manager.SetPasskeyAsync(new PocoUser(), null));
+        await Assert.ThrowsAsync<ArgumentNullException>("passkey", async () => await manager.AddOrUpdatePasskeyAsync(new PocoUser(), null));
         await Assert.ThrowsAsync<ArgumentNullException>("credentialId", async () => await manager.GetPasskeyAsync(new PocoUser(), null));
         await Assert.ThrowsAsync<ArgumentNullException>("credentialId", async () => await manager.FindByPasskeyIdAsync(null));
         await Assert.ThrowsAsync<ArgumentNullException>("credentialId", async () => await manager.RemovePasskeyAsync(new PocoUser(), null));
@@ -1477,7 +1477,7 @@ public class UserManagerTest
         await Assert.ThrowsAsync<ArgumentNullException>("user",
             async () => await manager.IsLockedOutAsync(null));
         await Assert.ThrowsAsync<ArgumentNullException>("user",
-            async () => await manager.SetPasskeyAsync(null, null));
+            async () => await manager.AddOrUpdatePasskeyAsync(null, null));
         await Assert.ThrowsAsync<ArgumentNullException>("user",
             async () => await manager.GetPasskeysAsync(null));
         await Assert.ThrowsAsync<ArgumentNullException>("user",
@@ -1523,7 +1523,7 @@ public class UserManagerTest
         await Assert.ThrowsAsync<ObjectDisposedException>(() => manager.GenerateEmailConfirmationTokenAsync(null));
         await Assert.ThrowsAsync<ObjectDisposedException>(() => manager.IsEmailConfirmedAsync(null));
         await Assert.ThrowsAsync<ObjectDisposedException>(() => manager.ConfirmEmailAsync(null, null));
-        await Assert.ThrowsAsync<ObjectDisposedException>(() => manager.SetPasskeyAsync(null, null));
+        await Assert.ThrowsAsync<ObjectDisposedException>(() => manager.AddOrUpdatePasskeyAsync(null, null));
         await Assert.ThrowsAsync<ObjectDisposedException>(() => manager.GetPasskeysAsync(null));
         await Assert.ThrowsAsync<ObjectDisposedException>(() => manager.GetPasskeyAsync(null, null));
         await Assert.ThrowsAsync<ObjectDisposedException>(() => manager.FindByPasskeyIdAsync(null));
@@ -1813,7 +1813,7 @@ public class UserManagerTest
             return Task.FromResult(0);
         }
 
-        public Task SetPasskeyAsync(PocoUser user, UserPasskeyInfo passkey, CancellationToken cancellationToken)
+        public Task AddOrUpdatePasskeyAsync(PocoUser user, UserPasskeyInfo passkey, CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
         }
@@ -2110,7 +2110,7 @@ public class UserManagerTest
             throw new NotImplementedException();
         }
 
-        public Task SetPasskeyAsync(PocoUser user, UserPasskeyInfo passkey, CancellationToken cancellationToken)
+        public Task AddOrUpdatePasskeyAsync(PocoUser user, UserPasskeyInfo passkey, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
