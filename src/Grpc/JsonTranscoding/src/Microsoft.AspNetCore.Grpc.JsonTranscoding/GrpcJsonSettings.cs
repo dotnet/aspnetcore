a7ee15fc631c1147dbc1ab4cf60bf91c50b12616
James Newton-King <james@newtonking.com>
2025-07-23 13:54:53 +0800

    Add gRPC JSON transcoding option for stripping enum prefix

--- src/Grpc/JsonTranscoding/src/Microsoft.AspNetCore.Grpc.JsonTranscoding/GrpcJsonSettings.cs
+++ src/Grpc/JsonTranscoding/src/Microsoft.AspNetCore.Grpc.JsonTranscoding/GrpcJsonSettings.cs
@@ -48,4 +48,42 @@ public sealed class GrpcJsonSettings
     /// </para>
     /// </remarks>
     public bool PropertyNameCaseInsensitive { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the enum type name prefix should be removed when reading and writing enum values.
+    /// The default value is <see langword="false"/>.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// In Protocol Buffers, enum value names are globally scoped, so they are often prefixed with the enum type name
+    /// to avoid name collisions. For example, the <c>Status</c> enum might define values like <c>STATUS_UNKNOWN</c>
+    /// and <c>STATUS_OK</c>.
+    /// </para>
+    /// <code>
+    /// enum Status {
+    ///   STATUS_UNKNOWN = 0;
+    ///   STATUS_OK = 1;
+    /// }
+    /// </code>
+    /// <para>
+    /// When <see cref="RemoveEnumPrefix"/> is set to <see langword="true"/>:
+    /// </para>
+    /// <list type="bullet">
+    /// <item>
+    /// <description>The <c>STATUS</c> prefix is removed from enum values. The enum values above will be read and written as <c>UNKNOWN</c> and <c>OK</c> instead of <c>STATUS_UNKNOWN</c> and <c>STATUS_OK</c>.</description>
+    /// </item>
+    /// <item>
+    /// <description>Original prefixed values are used as a fallback when reading JSON. For example, <c>STATUS_OK</c> and <c>OK</c> map to the <c>STATUS_OK</c> enum value.</description>
+    /// </item>
+    /// </list>
+    /// <para>
+    /// The Protobuf JSON specification requires enum values in JSON to match enum fields exactly.
+    /// Enabling this option may reduce interoperability, as removing enum prefix might not be supported
+    /// by other JSON transcoding implementations.
+    /// </para>
+    /// <para>
+    /// For more information, see <see href="https://protobuf.dev/programming-guides/json/"/>.
+    /// </para>
+    /// </remarks>
+    public bool RemoveEnumPrefix { get; set; }
 }
