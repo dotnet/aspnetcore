7e3cc358ce00f025b1fbea16d2f779fe3c834846
Brennan <brecon@microsoft.com>
2025-08-14 12:41:57 -0700

    Apply suggestions from code review
    Co-authored-by: Safia Abdalla <safia@microsoft.com>

--- src/DefaultBuilder/test/Microsoft.AspNetCore.Tests/WebHostTests.cs
+++ src/DefaultBuilder/test/Microsoft.AspNetCore.Tests/WebHostTests.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#pragma warning disable ASPDEPR008 // Type or member is obsolete
 #pragma warning disable ASPDEPR008 // IWebHost is obsolete
 
 using System.Collections.Concurrent;
