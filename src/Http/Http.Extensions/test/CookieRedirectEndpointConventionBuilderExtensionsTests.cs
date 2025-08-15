ff36a6117ba3259ee03c1ac63d0071b27a16fd9b
Stephen Halter <halter73@gmail.com>
2025-08-15 14:44:32 -0700

    Apply suggestion from @halter73

--- src/Http/Http.Extensions/test/CookieRedirectEndpointConventionBuilderExtensionsTests.cs
+++ src/Http/Http.Extensions/test/CookieRedirectEndpointConventionBuilderExtensionsTests.cs
@@ -60,36 +60,6 @@ public class CookieRedirectEndpointConventionBuilderExtensionsTests
         Assert.Same(builder, result);
     }
 
-    [Fact]
-    public void DisableCookieRedirect_WithMultipleCalls_AddsSingleMetadata()
-    {
-        // Arrange
-        var builder = new TestEndpointConventionBuilder();
-
-        // Act
-        builder.DisableCookieRedirect();
-        builder.DisableCookieRedirect();
-
-        // Assert
-        Assert.Equal(2, builder.Metadata.Count);
-        Assert.All(builder.Metadata, metadata => Assert.IsAssignableFrom<IDisableCookieRedirectMetadata>(metadata));
-    }
-
-    [Fact]
-    public void AllowCookieRedirect_WithMultipleCalls_AddsSingleMetadata()
-    {
-        // Arrange
-        var builder = new TestEndpointConventionBuilder();
-
-        // Act
-        builder.AllowCookieRedirect();
-        builder.AllowCookieRedirect();
-
-        // Assert
-        Assert.Equal(2, builder.Metadata.Count);
-        Assert.All(builder.Metadata, metadata => Assert.IsAssignableFrom<IAllowCookieRedirectMetadata>(metadata));
-    }
-
     private sealed class TestEndpointConventionBuilder : EndpointBuilder, IEndpointConventionBuilder
     {
         public void Add(Action<EndpointBuilder> convention)
