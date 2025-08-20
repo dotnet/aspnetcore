5ef86a167f8a831afc37d98eaa3cbb4784170337
Ilona Tomkowicz <itomkowicz@microsoft.com>
2025-08-20 16:04:22 +0200

    Wait for the base page to be loaded to make sure session storage is available.

--- src/Components/test/E2ETest/Infrastructure/ServerTestBase.cs
+++ src/Components/test/E2ETest/Infrastructure/ServerTestBase.cs
@@ -4,6 +4,7 @@
 using Microsoft.AspNetCore.Components.E2ETest.Infrastructure.ServerFixtures;
 using Microsoft.AspNetCore.E2ETesting;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using Xunit.Abstractions;
 
 namespace Microsoft.AspNetCore.Components.E2ETest.Infrastructure;
@@ -31,10 +32,48 @@ public abstract class ServerTestBase<TServerFixture>
         Browser.Navigate(_serverFixture.RootUri, relativeUrl);
     }
 
-    protected override void InitializeAsyncCore()
+    protected override void InitializeAsyncCore(bool supportEnhancedNavigationSuppression = false)
     {
+        if (supportEnhancedNavigationSuppression)
+        {
+            GrantTestId();
+        }
+
         // Clear logs - we check these during tests in some cases.
         // Make sure each test starts clean.
         ((IJavaScriptExecutor)Browser).ExecuteScript("console.clear()");
+
+        InitializeAsyncCore();
+    }
+
+    protected override void InitializeAsyncCore()
+    {
+    }
+
+    private void GrantTestId()
+    {
+        // We have to be on any page to ensure session storage is available
+        // Since PageLoadStrategy can be set to None, we need to manually wait for page load
+        Navigate(ServerPathBase);
+
+        // Wait for the page to be completely loaded (equivalent to PageLoadStrategy.Normal)
+        var wait = new OpenQA.Selenium.Support.UI.WebDriverWait(Browser, TimeSpan.FromSeconds(10));
+        wait.Until(driver =>
+        {
+            try
+            {
+                // Check if document is ready and load event has fired
+                var loadComplete = ((IJavaScriptExecutor)driver).ExecuteScript(
+                    "return document.readyState === 'complete' && performance.timing.loadEventEnd > 0");
+                return (bool)loadComplete;
+            }
+            catch
+            {
+                return false;
+            }
+        });
+
+        var testId = Guid.NewGuid().ToString("N")[..8];
+        ((IJavaScriptExecutor)Browser).ExecuteScript($"sessionStorage.setItem('test-id', '{testId}')");
     }
 }
