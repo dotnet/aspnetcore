d27777c83164adf21df1ca60bc836d3e82bbf493
Sjoerd van der Meer <sjoerd.vandermeer@xebia.com>
2025-08-26 17:19:21 +0200

    Move Schema Annotations handling to a schema transformer

--- src/OpenApi/src/Services/Schemas/OpenApiSchemaService.cs
+++ src/OpenApi/src/Services/Schemas/OpenApiSchemaService.cs
@@ -101,35 +101,12 @@ internal sealed class OpenApiSchemaService(
             {
                 schema.ApplyNullabilityContextInfo(jsonPropertyInfo);
             }
-            if (context.TypeInfo.Type.GetCustomAttributes(inherit: false).OfType<DescriptionAttribute>().LastOrDefault() is { } typeDescriptionAttribute)
-            {
-                schema[OpenApiSchemaKeywords.DescriptionKeyword] = typeDescriptionAttribute.Description;
-            }
             if (context.PropertyInfo is { AttributeProvider: { } attributeProvider })
             {
                 var propertyAttributes = attributeProvider.GetCustomAttributes(inherit: false);
                 if (propertyAttributes.OfType<ValidationAttribute>() is { } validationAttributes)
                 {
                     schema.ApplyValidationAttributes(validationAttributes);
-                }
-                if (propertyAttributes.OfType<DefaultValueAttribute>().LastOrDefault() is { } defaultValueAttribute)
-                {
-                    schema.ApplyDefaultValue(defaultValueAttribute.Value, context.TypeInfo);
-                }
-                var isInlinedSchema = schema[OpenApiConstants.SchemaId] is null;
-                if (isInlinedSchema)
-                {
-                    if (propertyAttributes.OfType<DescriptionAttribute>().LastOrDefault() is { } descriptionAttribute)
-                    {
-                        schema[OpenApiSchemaKeywords.DescriptionKeyword] = descriptionAttribute.Description;
-                    }
-                }
-                else
-                {
-                    if (propertyAttributes.OfType<DescriptionAttribute>().LastOrDefault() is { } descriptionAttribute)
-                    {
-                        schema[OpenApiConstants.RefDescriptionAnnotation] = descriptionAttribute.Description;
-                    }
                 }
             }
             schema.PruneNullTypeForComponentizedTypes();
