f91659b78d5e2f51c46a24f4512bf7f1c61b6c6d
Sjoerd van der Meer <sjoerd.vandermeer@xebia.com>
2025-08-26 22:56:36 +0200

    Fix tests and lint errors

--- src/OpenApi/src/Services/Schemas/OpenApiSchemaService.cs
+++ src/OpenApi/src/Services/Schemas/OpenApiSchemaService.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Concurrent;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.IO.Pipelines;
