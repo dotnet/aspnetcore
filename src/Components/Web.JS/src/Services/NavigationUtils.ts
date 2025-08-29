33920bbc6cf5ec3dd7c8c465691be018c312df9d
Daria Tiurina <dariatiurina@microsoft.com>
2025-08-29 12:25:11 +0200

    Feedback

--- src/Components/Web.JS/src/Services/NavigationUtils.ts
+++ src/Components/Web.JS/src/Services/NavigationUtils.ts
@@ -47,15 +47,11 @@ export function isWithinBaseUriSpace(href: string) {
   && (nextChar === '' || nextChar === '/' || nextChar === '?' || nextChar === '#');
 }
 
-export function isSameUrlWithDifferentHash(oldUrl: string, newUrl: string): boolean {
-  try {
-    const a = new URL(oldUrl);
-    const b = new URL(newUrl);
-    return a.origin === b.origin && a.pathname === b.pathname
-      && a.search === b.search && b.hash !== '';
-  } catch {
-    return false;
-  }
+export function isSamePageWithHash(oldUrl: string, newUrl: string): boolean {
+  const a = new URL(oldUrl);
+  const b = new URL(newUrl);
+  return a.origin === b.origin && a.pathname === b.pathname
+    && a.search === b.search && b.hash !== '';
 }
 
 export function isForSamePath(url1: string, url2: string) {
