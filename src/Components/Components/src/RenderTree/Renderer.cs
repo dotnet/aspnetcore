3ac6731ade1759093f80f2011b28c3758d312f6c
Javier Calvarro Nelson <jacalvar@microsoft.com>
2025-09-02 13:25:21 +0200

    Add component type errors when a component fails to render

--- src/Components/Components/src/RenderTree/Renderer.cs
+++ src/Components/Components/src/RenderTree/Renderer.cs
@@ -805,7 +805,7 @@ public abstract partial class Renderer : IDisposable, IAsyncDisposable
         _isBatchInProgress = true;
         var updateDisplayTask = Task.CompletedTask;
         var batchStartTimestamp = ComponentMetrics != null && ComponentMetrics.IsBatchEnabled ? Stopwatch.GetTimestamp() : 0;
-
+        RenderQueueEntry nextToRender = default;
         try
         {
             if (_batchBuilder.ComponentRenderQueue.Count == 0)
@@ -826,7 +826,7 @@ public abstract partial class Renderer : IDisposable, IAsyncDisposable
             // Process render queue until empty
             while (_batchBuilder.ComponentRenderQueue.Count > 0)
             {
-                var nextToRender = _batchBuilder.ComponentRenderQueue.Dequeue();
+                nextToRender = _batchBuilder.ComponentRenderQueue.Dequeue();
                 RenderInExistingBatch(nextToRender);
             }
 
@@ -847,6 +847,11 @@ public abstract partial class Renderer : IDisposable, IAsyncDisposable
             if (ComponentMetrics != null && ComponentMetrics.IsBatchEnabled)
             {
                 ComponentMetrics.FailBatchSync(e, batchStartTimestamp);
+            }
+            if (nextToRender.ComponentState?.Component != null)
+            {
+                var componentType = nextToRender.ComponentState.Component.GetType();
+                Log.ErrorRenderingComponent(_logger, componentType, e);
             }
 
             // Ensure we catch errors while running the render functions of the components.
