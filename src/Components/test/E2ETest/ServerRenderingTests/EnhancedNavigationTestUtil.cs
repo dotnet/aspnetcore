047bd950000225d6e38c8ad305c1b91bac096163
Ilona Tomkowicz <itomkowicz@microsoft.com>
2025-09-02 16:53:02 +0200

    ID is not necessary if we're cleaning and not creating it for every test.

--- src/Components/test/E2ETest/ServerRenderingTests/EnhancedNavigationTestUtil.cs
+++ src/Components/test/E2ETest/ServerRenderingTests/EnhancedNavigationTestUtil.cs
@@ -34,15 +34,9 @@ public static class EnhancedNavigationTestUtil
                 throw new InvalidOperationException("Session storage not found. Ensure that the browser is on the correct origin by navigating to a page or by setting skipNavigation to false.", ex);
             }
 
-            var testId = ((IJavaScriptExecutor)browser).ExecuteScript($"return sessionStorage.getItem('test-id')");
-            if (testId is null || string.IsNullOrEmpty(testId as string))
-            {
-                testId = GenerateTestId(browser);
-            }
+            ((IJavaScriptExecutor)browser).ExecuteScript("sessionStorage.setItem('suppress-enhanced-navigation', 'true')");
 
-            ((IJavaScriptExecutor)browser).ExecuteScript($"sessionStorage.setItem('suppress-enhanced-navigation-{testId}', 'true')");
-
-            var suppressEnhancedNavigation = ((IJavaScriptExecutor)browser).ExecuteScript($"return sessionStorage.getItem('suppress-enhanced-navigation-{testId}');");
+            var suppressEnhancedNavigation = ((IJavaScriptExecutor)browser).ExecuteScript("return sessionStorage.getItem('suppress-enhanced-navigation');");
             Assert.True(suppressEnhancedNavigation is not null && (string)suppressEnhancedNavigation == "true",
                 "Expected 'suppress-enhanced-navigation' to be set in sessionStorage.");
             _isSuppressed = true;
@@ -75,15 +69,7 @@ public static class EnhancedNavigationTestUtil
                 }
                 NavigateToOrigin(fixture);
             }
-
-            var testId = ((IJavaScriptExecutor)browser).ExecuteScript($"return sessionStorage.getItem('test-id')");
-            if (testId is null || string.IsNullOrEmpty(testId as string))
-            {
-                return;
-            }
-
-            ((IJavaScriptExecutor)browser).ExecuteScript($"sessionStorage.removeItem('test-id')");
-            ((IJavaScriptExecutor)browser).ExecuteScript($"sessionStorage.removeItem('suppress-enhanced-navigation-{testId}')");
+            ((IJavaScriptExecutor)browser).ExecuteScript($"sessionStorage.removeItem('suppress-enhanced-navigation')");
         }
         catch (WebDriverException ex) when (ex.Message.Contains("invalid session id"))
         {
@@ -104,13 +90,6 @@ public static class EnhancedNavigationTestUtil
         // Navigate to the test origin to ensure the browser is on the correct state to access sessionStorage
         fixture.Navigate($"{fixture.ServerPathBase}/");
         fixture.Browser.Exists(By.Id("session-storage-anchor"));
-    }
-
-    private static string GenerateTestId(IWebDriver browser)
-    {
-        var testId = Guid.NewGuid().ToString("N")[..8];
-        ((IJavaScriptExecutor)browser).ExecuteScript($"sessionStorage.setItem('test-id', '{testId}')");
-        return testId;
     }
 
     public static long GetScrollY(this IWebDriver browser)
