773460572b6ffac6c5e4b7c1c9afbcb5a7e9353e
Martin Costello <martin@martincostello.com>
2025-09-04 06:21:52 +0100

    [OpenAPI] Use invariant culture for TextWriter (#62239)
    Ensure OpenAPI documents are written to a culture-invariant `TextWriter` implementation.
    
    Resolves #60628.

--- src/OpenApi/test/Microsoft.AspNetCore.OpenApi.Tests/Integration/OpenApiDocumentIntegrationTests.cs
+++ src/OpenApi/test/Microsoft.AspNetCore.OpenApi.Tests/Integration/OpenApiDocumentIntegrationTests.cs
@@ -1,12 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Globalization;
 using Microsoft.AspNetCore.InternalTesting;
-using Microsoft.AspNetCore.OpenApi;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
-using Microsoft.OpenApi.Writers;
 
 [UsesVerify]
 public sealed class OpenApiDocumentIntegrationTests(SampleAppFixture fixture) : IClassFixture<SampleAppFixture>
@@ -20,21 +15,12 @@ public sealed class OpenApiDocumentIntegrationTests(SampleAppFixture fixture) : 
     [InlineData("schemas-by-ref")]
     public async Task VerifyOpenApiDocument(string documentName)
     {
-        var documentService = fixture.Services.GetRequiredKeyedService<OpenApiDocumentService>(documentName);
-        var scopedServiceProvider = fixture.Services.CreateScope();
-        var document = await documentService.GetOpenApiDocumentAsync(scopedServiceProvider.ServiceProvider);
-        await Verifier.Verify(GetOpenApiJson(document))
+        using var client = fixture.CreateClient();
+        var json = await client.GetStringAsync($"/openapi/{documentName}.json");
+        await Verify(json)
             .UseDirectory(SkipOnHelixAttribute.OnHelix()
                 ? Path.Combine(Environment.GetEnvironmentVariable("HELIX_WORKITEM_ROOT"), "Integration", "snapshots")
                 : "snapshots")
             .UseParameters(documentName);
-    }
-
-    private static string GetOpenApiJson(OpenApiDocument document)
-    {
-        using var textWriter = new StringWriter(CultureInfo.InvariantCulture);
-        var jsonWriter = new OpenApiJsonWriter(textWriter);
-        document.SerializeAsV3(jsonWriter);
-        return textWriter.ToString();
     }
 }
