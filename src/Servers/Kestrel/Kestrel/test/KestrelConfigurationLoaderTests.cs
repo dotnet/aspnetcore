5988967720238bcfd7eb5cce147638c025ce7d1d
William Godbe <wigodbe@microsoft.com>
2025-09-18 12:10:39 -0700

    Add OS skip condition for MacOSX in tests

--- src/Servers/Kestrel/Kestrel/test/KestrelConfigurationLoaderTests.cs
+++ src/Servers/Kestrel/Kestrel/test/KestrelConfigurationLoaderTests.cs
@@ -776,6 +776,7 @@ public class KestrelConfigurationLoaderTests
 
     [Theory]
     [MemberData(nameof(GetPemCertificateTestData))]
+    [OSSkipCondition(OperatingSystems.MacOSX)]
     public void ConfigureEndpoint_CanLoadPemCertificates(string algorithmType, string keyPassword, string extension)
     {
         var serverOptions = CreateServerOptions();
