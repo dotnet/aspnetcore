bc55f71547b5511313dbc06352c0f0f6bedc7752
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2025-10-15 15:56:48 +0200

    Add using directive for Microsoft.AspNetCore.InternalTesting

--- src/Identity/Specification.Tests/src/IdentitySpecificationTestBase.cs
+++ src/Identity/Specification.Tests/src/IdentitySpecificationTestBase.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Security.Claims;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.InternalTesting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Xunit;
