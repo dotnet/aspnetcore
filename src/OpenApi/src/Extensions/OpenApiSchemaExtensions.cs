5cf96f70d84ae737bcecfb41207802599c047547
Sjoerd van der Meer <sjoerd.vandermeer@xebia.com>
2025-10-20 23:11:40 +0200

    Cleanup some funky logic

--- src/OpenApi/src/Extensions/OpenApiSchemaExtensions.cs
+++ src/OpenApi/src/Extensions/OpenApiSchemaExtensions.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Text.Json.Nodes;
+
 namespace Microsoft.AspNetCore.OpenApi;
 
 internal static class OpenApiSchemaExtensions
@@ -16,6 +18,44 @@ internal static class OpenApiSchemaExtensions
                 _nullSchema,
                 originalSchema
             ]
+        };
+    }
+
+    public static bool IsComponentizedSchema(this OpenApiSchema schema)
+        => schema.IsComponentizedSchema(out _);
+
+    public static bool IsComponentizedSchema(this OpenApiSchema schema, out string schemaId)
+    {
+        if(schema.Metadata is not null
+            && schema.Metadata.TryGetValue(OpenApiConstants.SchemaId, out var schemaIdAsObject)
+            && schemaIdAsObject is string schemaIdString)
+        {
+            schemaId = schemaIdString;
+            return true;
+        }
+        schemaId = string.Empty;
+        return false;
+    }
+
+    public static OpenApiSchemaReference CreateReference(this OpenApiSchema schema, OpenApiDocument document)
+    {
+        if (!schema.IsComponentizedSchema(out var schemaId))
+        {
+            throw new InvalidOperationException("Schema is not a componentized schema.");
+        }
+
+        object? description = null;
+        object? example = null;
+        object? defaultAnnotation = null;
+        schema.Metadata?.TryGetValue(OpenApiConstants.RefDescriptionAnnotation, out description);
+        schema.Metadata?.TryGetValue(OpenApiConstants.RefExampleAnnotation, out example);
+        schema.Metadata?.TryGetValue(OpenApiConstants.RefDefaultAnnotation, out defaultAnnotation);
+
+        return new OpenApiSchemaReference(schemaId, document)
+        {
+            Description = description as string,
+            Examples = example is JsonNode exampleJson ? [exampleJson] : null,
+            Default = defaultAnnotation as JsonNode,
         };
     }
 }
