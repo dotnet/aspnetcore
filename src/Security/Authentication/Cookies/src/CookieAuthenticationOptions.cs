01ff0062047716ddae350bb9330602ab692d014c
Mackinnon Buck <mackinnon.buck@gmail.com>
2025-10-21 12:28:05 -0700

    Update XML docs

--- src/Security/Authentication/Cookies/src/CookieAuthenticationOptions.cs
+++ src/Security/Authentication/Cookies/src/CookieAuthenticationOptions.cs
@@ -128,7 +128,7 @@ public class CookieAuthenticationOptions : AuthenticationSchemeOptions
     /// even if it is passed to the server after the browser should have purged it.
     /// </para>
     /// <para>
-    /// This property should be used instead of <see cref="CookieOptions.Expires"/>.
+    /// This property should be used instead of <see cref="CookieOptions.Expires"/>, which is ignored.
     /// </para>
     /// </summary>
     public TimeSpan ExpireTimeSpan { get; set; }
