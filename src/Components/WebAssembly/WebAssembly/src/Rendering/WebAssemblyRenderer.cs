3ea48d7385ce773941bfa010b499e1cae704e532
Ilona Tomkowicz <itomkowicz@microsoft.com>
2025-11-21 18:26:42 +0100

    Redunadnt because wasm rnderer already has OnUpdateRootComponents handle

--- src/Components/WebAssembly/WebAssembly/src/Rendering/WebAssemblyRenderer.cs
+++ src/Components/WebAssembly/WebAssembly/src/Rendering/WebAssemblyRenderer.cs
@@ -46,7 +46,6 @@ internal sealed partial class WebAssemblyRenderer : WebRenderer
 
         ElementReferenceContext = DefaultWebAssemblyJSRuntime.Instance.ElementReferenceContext;
         DefaultWebAssemblyJSRuntime.Instance.OnUpdateRootComponents += OnUpdateRootComponents;
-        GetOrCreateWebRootComponentManager();
     }
 
     [UnconditionalSuppressMessage("Trimming", "IL2072", Justification = "These are root components which belong to the user and are in assemblies that don't get trimmed.")]
