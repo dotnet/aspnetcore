effad8811f09edc07ac16580e78edc8b6edd233a
Kristian Hellang <kristian@hellang.com>
2025-11-26 11:19:05 +0100

    Extract helper method to get model names

--- src/OpenApi/test/Microsoft.AspNetCore.OpenApi.SourceGenerators.Tests/snapshots/OperationTests.SupportsXmlCommentsOnOperationsFromMinimalApis#OpenApiXmlCommentSupport.generated.verified.cs
+++ src/OpenApi/test/Microsoft.AspNetCore.OpenApi.SourceGenerators.Tests/snapshots/OperationTests.SupportsXmlCommentsOnOperationsFromMinimalApis#OpenApiXmlCommentSupport.generated.verified.cs
@@ -543,13 +543,7 @@ namespace Microsoft.AspNetCore.OpenApi.Generated
                 return null;
             }
 
-            var modelNames = parameterInfo
-                .GetCustomAttributes(inherit: false)
-                .OfType<IModelNameProvider>()
-                .Select(p => p.Name)
-                .Append(parameterInfo.Name)
-                .Where(n => !string.IsNullOrEmpty(n))
-                .ToHashSet();
+            var modelNames = GetModelNames(parameterInfo);
 
             foreach (var parameter in parameters)
             {
@@ -567,6 +561,17 @@ namespace Microsoft.AspNetCore.OpenApi.Generated
             }
 
             return null;
+        }
+
+        private static IReadOnlySet<string> GetModelNames(ParameterInfo parameterInfo)
+        {
+            return parameterInfo
+                .GetCustomAttributes(inherit: false)
+                .OfType<IModelNameProvider>()
+                .Select(p => p.Name!)
+                .Append(parameterInfo.Name!)
+                .Where(n => !string.IsNullOrEmpty(n))
+                .ToHashSet();
         }
 
         private static OpenApiParameter UnwrapOpenApiParameter(IOpenApiParameter sourceParameter)
