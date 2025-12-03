a8098f9f61aeb6a38c597523b4447bc1ba300a8b
Ilona Tomkowicz <itomkowicz@microsoft.com>
2025-12-03 17:58:44 +0100

    Feedback: use same caching approach as `FieldIdentifier` uses.

--- src/Components/Web/src/Forms/DisplayName.cs
+++ src/Components/Web/src/Forms/DisplayName.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Collections.Concurrent;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq.Expressions;
@@ -14,9 +15,11 @@ namespace Microsoft.AspNetCore.Components.Forms;
 /// or <see cref="DisplayNameAttribute"/> if present, or falling back to the property name.
 /// </summary>
 /// <typeparam name="TValue">The type of the field.</typeparam>
-public class DisplayName<TValue> : ComponentBase
+public class DisplayName<TValue> : IComponent
 {
-  private Expression<Func<TValue>>? _previousFieldAccessor;
+    private static readonly ConcurrentDictionary<MemberInfo, string> _displayNameCache = new();
+
+    private RenderHandle _renderHandle;
     private string? _displayName;
 
     /// <summary>
@@ -25,56 +28,84 @@ public class DisplayName<TValue> : ComponentBase
     [Parameter, EditorRequired]
     public Expression<Func<TValue>>? For { get; set; }
 
-    /// <inheritdoc />
-    protected override void OnParametersSet()
+    static DisplayName()
     {
+        if (HotReloadManager.Default.MetadataUpdateSupported)
+        {
+            HotReloadManager.Default.OnDeltaApplied += ClearCache;
+        }
+    }
+
+    /// <inheritdoc />
+    public void Attach(RenderHandle renderHandle)
+    {
+        _renderHandle = renderHandle;
+    }
+
+    /// <inheritdoc />
+    public Task SetParametersAsync(ParameterView parameters)
+    {
+        parameters.SetParameterProperties(this);
+
         if (For is null)
         {
             throw new InvalidOperationException($"{GetType()} requires a value for the " +
                 $"{nameof(For)} parameter.");
         }
 
-        if (For != _previousFieldAccessor)
+        var newDisplayName = GetDisplayName(For);
+
+        // Only re-render if the display name string has actually changed
+        if (newDisplayName != _displayName)
         {
-            _displayName = GetDisplayName(For);
-            _previousFieldAccessor = For;
+            _displayName = newDisplayName;
+            _renderHandle.Render(BuildRenderTree);
         }
+
+        return Task.CompletedTask;
     }
 
-    /// <inheritdoc />
-    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    private void BuildRenderTree(RenderTreeBuilder builder)
     {
         builder.AddContent(0, _displayName);
     }
 
     private static string GetDisplayName(Expression<Func<TValue>> expression)
     {
-        if (expression.Body is MemberExpression memberExpression)
+        if (expression.Body is not MemberExpression memberExpression)
         {
-            var member = memberExpression.Member;
+            throw new ArgumentException(
+                $"The provided expression contains a {expression.Body.GetType().Name} which is not supported. " +
+                $"{nameof(DisplayName<TValue>)} only supports simple member accessors (fields, properties) of an object.",
+                nameof(expression));
+        }
 
-            var displayAttribute = member.GetCustomAttribute<DisplayAttribute>();
-            if (displayAttribute != null)
+        var member = memberExpression.Member;
+
+        return _displayNameCache.GetOrAdd(member, static m =>
+        {
+            var displayAttribute = m.GetCustomAttribute<DisplayAttribute>();
+            if (displayAttribute is not null)
             {
                 var name = displayAttribute.GetName();
-                if (name != null)
+                if (name is not null)
                 {
                     return name;
                 }
             }
 
-            var displayNameAttribute = member.GetCustomAttribute<DisplayNameAttribute>();
+            var displayNameAttribute = m.GetCustomAttribute<DisplayNameAttribute>();
             if (displayNameAttribute?.DisplayName is not null)
             {
                 return displayNameAttribute.DisplayName;
             }
 
-            return member.Name;
-        }
+            return m.Name;
+        });
+    }
 
-        throw new ArgumentException(
-            $"The provided expression contains a {expression.Body.GetType().Name} which is not supported. " +
-            $"{nameof(DisplayName<TValue>)} only supports simple member accessors (fields, properties) of an object.",
-            nameof(expression));
+    private static void ClearCache()
+    {
+        _displayNameCache.Clear();
     }
 }
