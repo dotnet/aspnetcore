ba210fdd9cab5c2d4497fef76155fae56149b67f
Ilona Tomkowicz <itomkowicz@microsoft.com>
2025-12-03 15:50:59 +0100

    API proposal.

---
+++ src/Components/Web/src/Forms/DisplayNameLabel.cs
@@ -0,0 +1,79 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Reflection;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Microsoft.AspNetCore.Components.Forms;
+
+/// <summary>
+/// Displays the display name for a specified field, reading from <see cref="DisplayAttribute"/>
+/// or <see cref="DisplayNameAttribute"/> if present, or falling back to the property name.
+/// </summary>
+/// <typeparam name="TValue">The type of the field.</typeparam>
+public class DisplayNameLabel<TValue> : ComponentBase
+{
+    private Expression<Func<TValue>>? _previousFieldAccessor;
+    private string? _displayName;
+
+    /// <summary>
+    /// Gets or sets a collection of additional attributes that will be applied to the created element.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
+
+    /// <summary>
+    /// Specifies the field for which the display name should be shown.
+    /// </summary>
+    [Parameter, EditorRequired]
+    public Expression<Func<TValue>>? For { get; set; }
+
+    /// <inheritdoc />
+    protected override void OnParametersSet()
+    {
+        if (For == null)
+        {
+            throw new InvalidOperationException($"{GetType()} requires a value for the " +
+                $"{nameof(For)} parameter.");
+        }
+
+        if (For != _previousFieldAccessor)
+        {
+            _displayName = GetDisplayName(For);
+            _previousFieldAccessor = For;
+        }
+    }
+
+    /// <inheritdoc />
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.AddContent(0, _displayName);
+    }
+
+    private static string GetDisplayName(Expression<Func<TValue>> expression)
+    {
+        if (expression.Body is MemberExpression memberExpression)
+        {
+            var member = memberExpression.Member;
+
+            var displayAttribute = member.GetCustomAttribute<DisplayAttribute>();
+            if (displayAttribute?.Name != null)
+            {
+                return displayAttribute.Name;
+            }
+
+            var displayNameAttribute = member.GetCustomAttribute<DisplayNameAttribute>();
+            if (displayNameAttribute?.DisplayName != null)
+            {
+                return displayNameAttribute.DisplayName;
+            }
+
+            return member.Name;
+        }
+
+        return string.Empty;
+    }
+}
