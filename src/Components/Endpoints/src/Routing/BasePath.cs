1b5c78561efa06efc1824b896759b75048ba9c9e
Ilona Tomkowicz <itomkowicz@microsoft.com>
2025-12-03 09:15:34 +0100

    Fix namespace.

--- src/Components/Endpoints/src/Routing/BasePath.cs
+++ src/Components/Endpoints/src/Routing/BasePath.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Microsoft.AspNetCore.Components.Rendering;
+
 namespace Microsoft.AspNetCore.Components.Endpoints;
 
 /// <summary>
@@ -24,7 +26,7 @@ public sealed class BasePath : IComponent
         return Task.CompletedTask;
     }
 
-    private void Render(Rendering.RenderTreeBuilder builder)
+    private void Render(RenderTreeBuilder builder)
     {
         builder.OpenElement(0, "base");
         builder.AddAttribute(1, "href", ComputeHref());
