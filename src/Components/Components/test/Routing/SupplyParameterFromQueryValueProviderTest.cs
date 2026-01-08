9549ba61ccaf6acf4baf4018f2559063924d676c
claudiogodoy99 <cavalcantigodoy@gmail.com>
2026-01-08 12:17:09 -0300

    Test(Components): change namespace SupplyParameterFromQueryValueProviderTest

--- src/Components/Components/test/Routing/SupplyParameterFromQueryValueProviderTest.cs
+++ src/Components/Components/test/Routing/SupplyParameterFromQueryValueProviderTest.cs
@@ -1,11 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-
 using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.AspNetCore.Components.Test.Helpers;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Microsoft.AspNetCore.Components.Test.Rendering;
+namespace Microsoft.AspNetCore.Components.Routing;
 
 public class SupplyParameterFromQueryValueProviderTest
 {
