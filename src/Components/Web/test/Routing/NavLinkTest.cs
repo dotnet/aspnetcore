20f9cefd425a0b2862be180cb26e41bb4c5e817f
Ilona Tomkowicz <itomkowicz@microsoft.com>
2026-01-14 14:51:42 +0100

    Feedback: consolidate the tests.

--- src/Components/Web/test/Routing/NavLinkTest.cs
+++ src/Components/Web/test/Routing/NavLinkTest.cs
@@ -3,7 +3,6 @@
 
 #nullable enable
 
-using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.AspNetCore.Components.RenderTree;
 using Microsoft.AspNetCore.Components.Test.Helpers;
 
@@ -11,219 +10,66 @@ namespace Microsoft.AspNetCore.Components.Routing;
 
 public class NavLinkTest
 {
-    [Fact]
-    public async Task NavLink_WithRelativeToCurrentUri_ResolvesHrefRelativeToCurrentPath()
+    [Theory]
+    [InlineData("https://example.com/", "https://example.com/sub-site/page-a", "details", "https://example.com/sub-site/details")]
+    [InlineData("https://example.com/", "https://example.com/a/b/c/page", "sibling", "https://example.com/a/b/c/sibling")]
+    [InlineData("https://example.com/", "https://example.com/page", "other", "https://example.com/other")]
+    [InlineData("https://example.com/", "https://example.com/folder/page?query=value#hash", "other", "https://example.com/folder/other")]
+    [InlineData("https://example.com/org/project/app/", "https://example.com/org/project/app/admin/users", "roles", "https://example.com/org/project/app/admin/roles")]
+    public async Task NavLink_WithRelativeToCurrentUri_ResolvesHrefCorrectly(
+        string baseUri, string currentUri, string href, string expectedHref)
     {
-        var testNavigationManager = new TestNavigationManager();
-        testNavigationManager.Initialize("https://example.com/", "https://example.com/sub-site/page-a");
+        var renderedHref = await RenderNavLinkAndGetAttributeAsync(
+            baseUri, currentUri, href, relativeToCurrentUri: true, attributeName: "href");
 
-        var renderer = new TestRenderer();
-        var component = new NavLink();
-        
-        var componentId = renderer.AssignRootComponentId(component);
-        SetNavigationManager(component, testNavigationManager);
-        
-        var parameters = ParameterView.FromDictionary(new Dictionary<string, object?>
-        {
-            [nameof(NavLink.RelativeToCurrentUri)] = true,
-            [nameof(NavLink.AdditionalAttributes)] = new Dictionary<string, object>
-            {
-                ["href"] = "details"
-            }
-        });
-        
-        await renderer.RenderRootComponentAsync(componentId, parameters);
-
-        var batch = renderer.Batches.Single();
-        var hrefFrame = batch.ReferenceFrames.First(f => f.AttributeName == "href");
-        Assert.Equal("https://example.com/sub-site/details", hrefFrame.AttributeValue);
-    }
-
-    [Fact]
-    public async Task NavLink_WithRelativeToCurrentUri_HandlesNestedPaths()
-    {
-        var testNavigationManager = new TestNavigationManager();
-        testNavigationManager.Initialize("https://example.com/", "https://example.com/a/b/c/page");
-
-        var renderer = new TestRenderer();
-        var component = new NavLink();
-        
-        var componentId = renderer.AssignRootComponentId(component);
-        SetNavigationManager(component, testNavigationManager);
-        
-        var parameters = ParameterView.FromDictionary(new Dictionary<string, object?>
-        {
-            [nameof(NavLink.RelativeToCurrentUri)] = true,
-            [nameof(NavLink.AdditionalAttributes)] = new Dictionary<string, object>
-            {
-                ["href"] = "sibling"
-            }
-        });
-        
-        await renderer.RenderRootComponentAsync(componentId, parameters);
-
-        var batch = renderer.Batches.Single();
-        var hrefFrame = batch.ReferenceFrames.First(f => f.AttributeName == "href");
-        Assert.Equal("https://example.com/a/b/c/sibling", hrefFrame.AttributeValue);
-    }
-
-    [Fact]
-    public async Task NavLink_WithRelativeToCurrentUri_HandlesQueryAndFragment()
-    {
-        var testNavigationManager = new TestNavigationManager();
-        testNavigationManager.Initialize("https://example.com/", "https://example.com/folder/page?query=value#hash");
-
-        var renderer = new TestRenderer();
-        var component = new NavLink();
-        
-        var componentId = renderer.AssignRootComponentId(component);
-        SetNavigationManager(component, testNavigationManager);
-        
-        var parameters = ParameterView.FromDictionary(new Dictionary<string, object?>
-        {
-            [nameof(NavLink.RelativeToCurrentUri)] = true,
-            [nameof(NavLink.AdditionalAttributes)] = new Dictionary<string, object>
-            {
-                ["href"] = "other"
-            }
-        });
-        
-        await renderer.RenderRootComponentAsync(componentId, parameters);
-
-        var batch = renderer.Batches.Single();
-        var hrefFrame = batch.ReferenceFrames.First(f => f.AttributeName == "href");
-        Assert.Equal("https://example.com/folder/other", hrefFrame.AttributeValue);
+        Assert.Equal(expectedHref, renderedHref);
     }
 
     [Fact]
     public async Task NavLink_WithRelativeToCurrentUriFalse_DoesNotResolve()
     {
-        var testNavigationManager = new TestNavigationManager();
-        testNavigationManager.Initialize("https://example.com/", "https://example.com/folder/page");
+        var renderedHref = await RenderNavLinkAndGetAttributeAsync(
+            "https://example.com/", "https://example.com/folder/page",
+            href: "relative", relativeToCurrentUri: false, attributeName: "href");
 
-        var renderer = new TestRenderer();
-        var component = new NavLink();
-        
-        var componentId = renderer.AssignRootComponentId(component);
-        SetNavigationManager(component, testNavigationManager);
-        
-        var parameters = ParameterView.FromDictionary(new Dictionary<string, object?>
-        {
-            [nameof(NavLink.RelativeToCurrentUri)] = false,
-            [nameof(NavLink.AdditionalAttributes)] = new Dictionary<string, object>
-            {
-                ["href"] = "relative"
-            }
-        });
-        
-        await renderer.RenderRootComponentAsync(componentId, parameters);
-
-        var batch = renderer.Batches.Single();
-        var hrefFrame = batch.ReferenceFrames.First(f => f.AttributeName == "href");
-        // Should keep the original href as-is (from AdditionalAttributes)
-        Assert.Equal("relative", hrefFrame.AttributeValue);
-    }
-
-    [Fact]
-    public async Task NavLink_WithRelativeToCurrentUri_AtRootLevel()
-    {
-        var testNavigationManager = new TestNavigationManager();
-        testNavigationManager.Initialize("https://example.com/", "https://example.com/page");
-
-        var renderer = new TestRenderer();
-        var component = new NavLink();
-        
-        var componentId = renderer.AssignRootComponentId(component);
-        SetNavigationManager(component, testNavigationManager);
-        
-        var parameters = ParameterView.FromDictionary(new Dictionary<string, object?>
-        {
-            [nameof(NavLink.RelativeToCurrentUri)] = true,
-            [nameof(NavLink.AdditionalAttributes)] = new Dictionary<string, object>
-            {
-                ["href"] = "other"
-            }
-        });
-        
-        await renderer.RenderRootComponentAsync(componentId, parameters);
-
-        var batch = renderer.Batches.Single();
-        var hrefFrame = batch.ReferenceFrames.First(f => f.AttributeName == "href");
-        Assert.Equal("https://example.com/other", hrefFrame.AttributeValue);
+        Assert.Equal("relative", renderedHref);
     }
 
     [Fact]
     public async Task NavLink_WithRelativeToCurrentUri_PreservesActiveClassLogic()
     {
-        var testNavigationManager = new TestNavigationManager();
-        testNavigationManager.Initialize("https://example.com/", "https://example.com/sub-site/details");
+        var classValue = await RenderNavLinkAndGetAttributeAsync(
+            "https://example.com/", "https://example.com/sub-site/details",
+            href: "details", relativeToCurrentUri: true, attributeName: "class");
 
-        var renderer = new TestRenderer();
-        var component = new NavLink();
-        
-        var componentId = renderer.AssignRootComponentId(component);
-        SetNavigationManager(component, testNavigationManager);
-        
-        var parameters = ParameterView.FromDictionary(new Dictionary<string, object?>
-        {
-            [nameof(NavLink.RelativeToCurrentUri)] = true,
-            [nameof(NavLink.AdditionalAttributes)] = new Dictionary<string, object>
-            {
-                ["href"] = "details"
-            }
-        });
-        
-        // Initially on details page - link should be active
-        await renderer.RenderRootComponentAsync(componentId, parameters);
-        var batch = renderer.Batches.Single();
-        var classFrame = batch.ReferenceFrames.FirstOrDefault(f => f.AttributeName == "class");
-        Assert.Equal("active", classFrame.AttributeValue);
+        Assert.Equal("active", classValue);
     }
 
-    [Fact]
-    public async Task NavLink_WithRelativeToCurrentUri_WorksWithDeeplyNestedBaseUri()
+    private async Task<object?> RenderNavLinkAndGetAttributeAsync(
+        string baseUri, string currentUri, string href, bool relativeToCurrentUri, string attributeName)
     {
-        // App hosted at https://example.com/org/project/app/
-        var testNavigationManager = new TestNavigationManager();
-        testNavigationManager.Initialize("https://example.com/org/project/app/", "https://example.com/org/project/app/admin/users");
+        var navigationManager = new TestNavigationManager();
+        navigationManager.Initialize(baseUri, currentUri);
 
         var renderer = new TestRenderer();
-        var component = new NavLink();
-        
+        var component = new NavLink { NavigationManager = navigationManager };
         var componentId = renderer.AssignRootComponentId(component);
-        SetNavigationManager(component, testNavigationManager);
-        
+
         var parameters = ParameterView.FromDictionary(new Dictionary<string, object?>
         {
-            [nameof(NavLink.RelativeToCurrentUri)] = true,
-            [nameof(NavLink.AdditionalAttributes)] = new Dictionary<string, object>
-            {
-                ["href"] = "roles"
-            }
+            [nameof(NavLink.RelativeToCurrentUri)] = relativeToCurrentUri,
+            [nameof(NavLink.AdditionalAttributes)] = new Dictionary<string, object> { ["href"] = href }
         });
-        
+
         await renderer.RenderRootComponentAsync(componentId, parameters);
 
         var batch = renderer.Batches.Single();
-        var hrefFrame = batch.ReferenceFrames.First(f => f.AttributeName == "href");
-        Assert.Equal("https://example.com/org/project/app/admin/roles", hrefFrame.AttributeValue);
-    }
-
-    private void SetNavigationManager(NavLink component, NavigationManager navigationManager)
-    {
-        component.NavigationManager = navigationManager;
+        return batch.ReferenceFrames.FirstOrDefault(f => f.AttributeName == attributeName).AttributeValue;
     }
 
     private class TestNavigationManager : NavigationManager
     {
-        public TestNavigationManager()
-        {
-        }
-
-        public new void Initialize(string baseUri, string uri)
-        {
-            base.Initialize(baseUri, uri);
-        }
+        public new void Initialize(string baseUri, string uri) => base.Initialize(baseUri, uri);
 
         protected override void NavigateToCore(string uri, NavigationOptions options)
         {
