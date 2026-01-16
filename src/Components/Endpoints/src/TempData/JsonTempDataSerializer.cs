c95e54c3fa9c63b8dcf972235647ed122e2a67c4
Daria Tiurina <dariatiurina@microsoft.com>
2026-01-16 17:32:09 +0100

    fix

--- src/Components/Endpoints/src/TempData/JsonTempDataSerializer.cs
+++ src/Components/Endpoints/src/TempData/JsonTempDataSerializer.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Globalization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -144,7 +145,7 @@ internal class JsonTempDataSerializer : ITempDataSerializer
         foreach (var kvp in data)
         {
             normalizedData[kvp.Key] = kvp.Value is Enum enumValue
-                ? Convert.ToInt32(enumValue)
+                ? Convert.ToInt32(enumValue, CultureInfo.InvariantCulture)
                 : kvp.Value;
         }
         return JsonSerializer.SerializeToUtf8Bytes<IDictionary<string, object?>>(normalizedData, JsonTempDataSerializerContext.Default.Options);
