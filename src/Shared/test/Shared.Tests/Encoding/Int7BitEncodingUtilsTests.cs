434e79b71ecdbc31bb75371c2ec7d87662b739b9
Korolev Dmitry <dmkorolev@microsoft.com>
2026-01-17 12:52:44 +0100

    fix build in shared

--- src/Shared/test/Shared.Tests/Encoding/Int7BitEncodingUtilsTests.cs
+++ src/Shared/test/Shared.Tests/Encoding/Int7BitEncodingUtilsTests.cs
@@ -1,10 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Text;
-using Microsoft.AspNetCore.Shared;
-
-namespace Microsoft.AspNetCore.Shared.Tests.Encoding;
+namespace Microsoft.AspNetCore.Shared.Tests;
 
 public class Int7BitEncodingUtilsTests
 {
