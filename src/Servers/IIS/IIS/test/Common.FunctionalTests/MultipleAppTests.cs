93beb8d52c7069b17625b1adf08b9a20ab73cc4e
Brennan Conroy <brecon@microsoft.com>
2026-02-01 23:48:40 -0800

    wip

--- src/Servers/IIS/IIS/test/Common.FunctionalTests/MultipleAppTests.cs
+++ src/Servers/IIS/IIS/test/Common.FunctionalTests/MultipleAppTests.cs
@@ -1,19 +1,13 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Server.IIS.FunctionalTests;
 using Microsoft.AspNetCore.Server.IIS.FunctionalTests.Utilities;
 using Microsoft.AspNetCore.Server.IntegrationTesting;
 using Microsoft.AspNetCore.InternalTesting;
-using Xunit;
 using System.Globalization;
-using System.Diagnostics;
-using System.IO;
 
 #if !IIS_FUNCTIONALS
 using Microsoft.AspNetCore.Server.IIS.FunctionalTests;
