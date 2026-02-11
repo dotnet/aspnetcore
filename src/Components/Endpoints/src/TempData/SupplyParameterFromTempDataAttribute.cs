f755d97470f1711cc64522eb00ea8797c0ae5f48
Daria Tiurina <dariatiurina@microsoft.com>
2026-02-11 21:11:43 +0100

    Fixed namespace

--- src/Components/Endpoints/src/TempData/SupplyParameterFromTempDataAttribute.cs
+++ src/Components/Endpoints/src/TempData/SupplyParameterFromTempDataAttribute.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace Microsoft.AspNetCore.Components;
+namespace Microsoft.AspNetCore.Components.Endpoints;
 
 /// <summary>
 /// Indicates that the value of the associated property should be supplied from
