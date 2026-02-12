256686a40298e1458d5b359a1d354a803d595c26
Ondřej Roztočil <roztocil@outlook.com>
2026-02-12 19:00:46 +0100

    Add proof-of-concept implementation

---
+++ src/Validation/src/Localization/IAttributeArgumentProvider.cs
@@ -0,0 +1,11 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Microsoft.Extensions.Validation.Localization;
+
+public interface IAttributeArgumentProvider
+{
+    public object?[] GetFormatArgs(ValidationAttribute attribute, string displayName);
+}
