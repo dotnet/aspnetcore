327b1f4525d05a5af4fe20f34c9a67f5014cc7c7
Ondřej Roztočil <roztocil@outlook.com>
2026-02-16 14:48:20 +0100

    Add missing comments, clean up

--- src/Validation/src/Localization/IAttributeArgumentProvider.cs
+++ src/Validation/src/Localization/IAttributeArgumentProvider.cs
@@ -5,7 +5,16 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Microsoft.Extensions.Validation.Localization;
 
+/// <summary>
+/// Provides the format arguments used when formatting localized validation error messages.
+/// </summary>
 public interface IAttributeArgumentProvider
 {
+    /// <summary>
+    /// Returns the format arguments for the specified <paramref name="attribute"/>.
+    /// </summary>
+    /// <param name="attribute">The validation attribute whose error message is being formatted.</param>
+    /// <param name="displayName">The resolved display name of the member being validated.</param>
+    /// <returns>An array of arguments to pass to <see cref="string.Format(string, object?[])"/>.</returns>
     public object?[] GetFormatArgs(ValidationAttribute attribute, string displayName);
 }
