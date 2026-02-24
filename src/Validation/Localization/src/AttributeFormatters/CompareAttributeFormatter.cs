d03e84509393c92835381257fefe329dcc4f44c1
Ondřej Roztočil <roztocil@outlook.com>
2026-02-24 01:18:37 +0100

    Move files in the Localization package

--- src/Validation/Localization/src/AttributeFormatters/CompareAttributeFormatter.cs
+++ src/Validation/Localization/src/AttributeFormatters/CompareAttributeFormatter.cs
@@ -4,7 +4,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 
-namespace Microsoft.Extensions.Validation.Localization.Attributes;
+namespace Microsoft.Extensions.Validation.Localization.AttributeFormatters;
 
 /// <summary>
 /// Formats error messages for <see cref="CompareAttribute"/> using the other property name.
