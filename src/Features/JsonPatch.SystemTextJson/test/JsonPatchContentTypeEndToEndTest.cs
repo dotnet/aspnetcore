f67034f408c96891ae48efc0de675a0c0d601ce1
Stephen Halter <halter73@gmail.com>
2026-03-04 12:37:34 -0800

    Add back `using System.Threading.Tasks;`

--- src/Features/JsonPatch.SystemTextJson/test/JsonPatchContentTypeEndToEndTest.cs
+++ src/Features/JsonPatch.SystemTextJson/test/JsonPatchContentTypeEndToEndTest.cs
@@ -4,6 +4,7 @@
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.InternalTesting;
