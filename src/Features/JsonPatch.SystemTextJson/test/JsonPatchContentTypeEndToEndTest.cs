ff373c26524167f930ebbcb3723856add1747644
Stephen Halter <halter73@gmail.com>
2026-03-04 11:52:51 -0800

    Simplify tests to not need to find solution root

--- src/Features/JsonPatch.SystemTextJson/test/JsonPatchContentTypeEndToEndTest.cs
+++ src/Features/JsonPatch.SystemTextJson/test/JsonPatchContentTypeEndToEndTest.cs
@@ -1,19 +1,15 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.InternalTesting;
-using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Xunit;
 
 namespace Microsoft.AspNetCore.JsonPatch.SystemTextJson;
@@ -29,28 +25,12 @@ public class JsonPatchContentTypeEndToEndTest : LoggedTest
     [InlineData("/typed", "text/plain", false)]
     public async Task PatchContentTypes_AreHandledAsExpected(string route, string contentType, bool shouldBeAccepted)
     {
-        using var factory = new JsonPatchWebApplicationFactory(LoggerFactory);
-        using var client = factory.CreateClient();
-
-        using var content = new StringContent("[]", Encoding.UTF8, contentType);
-
-        using var response = await client.PatchAsync(route, content);
-
-        var expectedStatusCode = shouldBeAccepted ? HttpStatusCode.OK : HttpStatusCode.UnsupportedMediaType;
-        Assert.Equal(expectedStatusCode, response.StatusCode);
-    }
-
-    private sealed class JsonPatchWebApplicationFactory(ILoggerFactory loggerFactory) : WebApplicationFactory<JsonPatchContentTypeEndToEndTest>
-    {
-        protected override void ConfigureWebHost(IWebHostBuilder builder) => builder.UseContentRoot(Directory.GetCurrentDirectory());
-
-        protected override IHostBuilder CreateHostBuilder()
-        {
-            return new HostBuilder().ConfigureWebHost(webHostBuilder =>
+        using var host = new HostBuilder()
+            .ConfigureWebHost(webHostBuilder =>
             {
                 webHostBuilder
                     .UseTestServer()
-                    .ConfigureServices(services => services.AddRouting().AddSingleton(loggerFactory))
+                    .ConfigureServices(services => services.AddRouting().AddSingleton(LoggerFactory))
                     .Configure(app =>
                     {
                         app.UseRouting();
@@ -60,7 +40,19 @@ public class JsonPatchContentTypeEndToEndTest : LoggedTest
                             endpoints.MapPatch("/typed", (JsonPatchDocument<SimpleObject> patch) => { });
                         });
                     });
-            });
-        }
+            })
+            .Build();
+
+        await host.StartAsync();
+
+        using var client = host.GetTestClient();
+        using var content = new StringContent("[]", Encoding.UTF8, contentType);
+
+        using var response = await client.PatchAsync(route, content);
+
+        var expectedStatusCode = shouldBeAccepted ? HttpStatusCode.OK : HttpStatusCode.UnsupportedMediaType;
+        Assert.Equal(expectedStatusCode, response.StatusCode);
+
+        await host.StopAsync();
     }
 }
