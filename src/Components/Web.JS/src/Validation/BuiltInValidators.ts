39e68fb74cbf4f72a141a0ca146ef525783a3f25
Ondřej Roztočil <roztocil@outlook.com>
2026-04-10 16:38:34 +0200

    Implement url validator

--- src/Components/Web.JS/src/Validation/BuiltInValidators.ts
+++ src/Components/Web.JS/src/Validation/BuiltInValidators.ts
@@ -11,7 +11,7 @@ export function registerBuiltInValidators(registry: ValidatorRegistry): void {
   registry.set('range', rangeValidator);
   registry.set('regex', regexValidator);
   registry.set('email', emailValidator);
-  // TODO: url
+  registry.set('url', urlValidator);
   // TODO: phone
   // TODO: creditcard
   // TODO: equalto
@@ -118,4 +118,18 @@ const emailValidator: Validator = (context: ValidationContext): ValidationResult
   }
 
   return emailPattern.test(value);
+};
+
+// URL pattern from jQuery validation, based on Diego Perini's regex.
+// Allows http, https, ftp, and protocol-relative URLs.
+// Source: https://gist.github.com/dperini/729294
+const urlPattern = /^(?:(?:(?:https?|ftp):)?\/\/)(?:(?:[^\]\[?\/<~#`!@$^&*()+=}|:";',>{ ]|%[0-9A-Fa-f]{2})+(?::(?:[^\]\[?\/<~#`!@$^&*()+=}|:";',>{ ]|%[0-9A-Fa-f]{2})*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+
+const urlValidator: Validator = (context: ValidationContext): ValidationResult => {
+  const { value } = context;
+  if (!value) {
+    return true;
+  }
+
+  return urlPattern.test(value);
 };
