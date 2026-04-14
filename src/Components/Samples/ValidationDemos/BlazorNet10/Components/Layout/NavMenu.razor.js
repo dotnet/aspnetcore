af7b0c18327c797955bdd66ee328374292c06af1
Ondřej Roztočil <roztocil@outlook.com>
2026-04-14 13:24:59 +0200

    Update BlazorNet10 demo

--- src/Components/Samples/ValidationDemos/BlazorNet10/Components/Layout/NavMenu.razor.js
+++
@@ -1,9 +0,0 @@
-// Handle navigation menu toggle
-const navScrollable = document.getElementById("nav-scrollable");
-const navToggler = document.querySelector(".navbar-toggler");
-
-if (navScrollable && navToggler) {
-    navScrollable.addEventListener("click", function() {
-        navToggler.click();
-    });
-}
--- src/Components/Samples/ValidationDemos/BlazorNet10/Program.cs
+++ src/Components/Samples/ValidationDemos/BlazorNet10/Program.cs
@@ -1,25 +1,20 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 using BlazorNet10.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddLocalization();
+builder.Services.AddValidation();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
