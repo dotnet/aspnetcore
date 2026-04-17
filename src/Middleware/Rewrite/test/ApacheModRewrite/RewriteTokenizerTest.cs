2af7b8759085cab30f687b4175083dae68577ca2
süleyman mercan <slymanmrcan@gmail.com>
2026-04-17 16:46:31 +0300

    Fix ArgumentException for regex shorthand classes in Apache mod_rewrite tokenizer
    Replace Regex.Unescape() with custom UnescapeApacheString() that only
    handles Apache-specific escape sequences (quotes, spaces, backslashes)
    while preserving regex patterns like \d, \w, \s intact.
    
    Fixes #18555

--- src/Middleware/Rewrite/test/ApacheModRewrite/RewriteTokenizerTest.cs
+++ src/Middleware/Rewrite/test/ApacheModRewrite/RewriteTokenizerTest.cs
@@ -88,4 +88,87 @@ public class RewriteTokenizerTest
         var ex = Assert.Throws<FormatException>(() => Tokenizer.Tokenize("\""));
         Assert.Equal("Mismatched number of quotes: \"", ex.Message);
     }
+
+    [Fact]
+    public void Tokenize_RegexDigitShorthand_DoesNotThrow()
+    {
+        var testString = @"RewriteRule ^/(\d)$ /?num=\$1";
+
+        var tokens = Tokenizer.Tokenize(testString);
+
+        var expected = new List<string>();
+        expected.Add("RewriteRule");
+        expected.Add(@"^/(\d)$");
+        expected.Add(@"/?num=\$1");
+        Assert.Equal(expected, tokens);
+    }
+
+    [Fact]
+    public void Tokenize_RegexWordShorthand_DoesNotThrow()
+    {
+        var testString = @"RewriteRule ^/(\w+)$ /?word=\$1";
+
+        var tokens = Tokenizer.Tokenize(testString);
+
+        var expected = new List<string>();
+        expected.Add("RewriteRule");
+        expected.Add(@"^/(\w+)$");
+        expected.Add(@"/?word=\$1");
+        Assert.Equal(expected, tokens);
+    }
+
+    [Fact]
+    public void Tokenize_RegexWhitespaceShorthand_DoesNotThrow()
+    {
+        var testString = @"RewriteRule ^/(\s+)$ /?s=\$1";
+
+        var tokens = Tokenizer.Tokenize(testString);
+
+        var expected = new List<string>();
+        expected.Add("RewriteRule");
+        expected.Add(@"^/(\s+)$");
+        expected.Add(@"/?s=\$1");
+        Assert.Equal(expected, tokens);
+    }
+
+    [Fact]
+    public void Tokenize_EscapedSpaceStillWorks_AfterFix()
+    {
+        var testString = @"RewriteRule ^/foo\ bar$ /result";
+
+        var tokens = Tokenizer.Tokenize(testString);
+
+        var expected = new List<string>();
+        expected.Add("RewriteRule");
+        expected.Add("^/foo bar$");
+        expected.Add("/result");
+        Assert.Equal(expected, tokens);
+    }
+
+    [Fact]
+    public void Tokenize_RegexShorthandAndEscapedSpace_BothWork()
+    {
+        var testString = @"RewriteRule ^/(\d)\ test$ /result";
+
+        var tokens = Tokenizer.Tokenize(testString);
+
+        var expected = new List<string>();
+        expected.Add("RewriteRule");
+        expected.Add(@"^/(\d) test$");
+        expected.Add("/result");
+        Assert.Equal(expected, tokens);
+    }
+    [Fact]
+    public void Tokenize_RegexShorthandInsideQuotes_DoesNotThrow()
+    {
+        var testString = "RewriteRule \"^/(\\d)$\" /result";
+
+        var tokens = Tokenizer.Tokenize(testString);
+
+        var expected = new List<string>();
+        expected.Add("RewriteRule");
+        expected.Add(@"^/(\d)$");
+        expected.Add("/result");
+        Assert.Equal(expected, tokens);
+    }
 }
