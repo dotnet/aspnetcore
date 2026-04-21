6dc811f64138a98a0966252a7aab30c830b33491
Daria Tiurina <dariatiurina@microsoft.com>
2026-04-21 15:41:02 +0200

    Change to usage of the SupplyParameterFromTempData

--- src/ProjectTemplates/Web.ProjectTemplates/content/BlazorWeb-CSharp/BlazorWebCSharp.1/Components/Account/IdentityRedirectManager.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/BlazorWeb-CSharp/BlazorWebCSharp.1/Components/Account/IdentityRedirectManager.cs
@@ -1,7 +1,4 @@
-using System.Security.Claims;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Identity;
-using BlazorWebCSharp._1.Data;
 
 namespace BlazorWebCSharp._1.Components.Account;
 
@@ -29,19 +26,7 @@ internal sealed class IdentityRedirectManager(NavigationManager navigationManage
         RedirectTo(newUri);
     }
 
-    public void RedirectToWithStatus(string uri, string message, ITempData tempData)
-    {
-        tempData[StatusMessageKey] = message;
-        RedirectTo(uri);
-    }
-
     private string CurrentPath => navigationManager.ToAbsoluteUri(navigationManager.Uri).GetLeftPart(UriPartial.Path);
 
     public void RedirectToCurrentPage() => RedirectTo(CurrentPath);
-
-    public void RedirectToCurrentPageWithStatus(string message, ITempData tempData)
-        => RedirectToWithStatus(CurrentPath, message, tempData);
-
-    public void RedirectToInvalidUser(UserManager<ApplicationUser> userManager, ClaimsPrincipal user, ITempData tempData)
-        => RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(user)}'.", tempData);
 }
