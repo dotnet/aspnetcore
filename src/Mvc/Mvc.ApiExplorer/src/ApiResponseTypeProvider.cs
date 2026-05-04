3ba070be4ee00aa1cdbd3410df39f3115535d56c
Korolev Dmitry <dmkorolev@microsoft.com>
2026-05-04 13:59:06 +0200

    add Debug.Assert to apiresponsetypeprovider scope processing

--- src/Mvc/Mvc.ApiExplorer/src/ApiResponseTypeProvider.cs
+++ src/Mvc/Mvc.ApiExplorer/src/ApiResponseTypeProvider.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Metadata;
@@ -270,7 +271,9 @@ internal sealed class ApiResponseTypeProvider
                     if (statusCodeScopes.TryGetValue(statusCode, out var existingScope))
                     {
                         // attributeScope > existingScope: cannot happend due to desc order processing
+                        Debug.Assert(attributeScope <= existingScope);
                         // attributeScope < existingScope: skip, higher scope already claimed this status code
+
                         if (attributeScope == existingScope)
                         {
                             // Same scope, same key: merge content types
