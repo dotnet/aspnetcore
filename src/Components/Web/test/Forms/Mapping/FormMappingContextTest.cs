f93a337a2facdc76d945ae95f79066bfe8fff283
sheiksyedm <sheiksyedm@users.noreply.github.com>
2026-05-21 22:24:54 +0530

    Add test for form binding error handler using correct field key
    Verifies that GetFormPostValue passes the actual field name (not the
    root parameter name) to the error handler when a form has a restricted
    form name. Without the fix, AttachParentValue throws because the error
    key does not start with the parent prefix.

--- src/Components/Web/test/Forms/Mapping/FormMappingContextTest.cs
+++ src/Components/Web/test/Forms/Mapping/FormMappingContextTest.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Forms.Mapping;
 
 namespace Microsoft.AspNetCore.Components;
 
@@ -19,5 +20,56 @@ public class FormMappingContextTest
     {
         var context = new FormMappingContext("name");
         Assert.Equal("name", context.MappingScopeName);
+    }
+
+    [Fact]
+    public void GetFormPostValue_ErrorHandler_UsesCorrectKeyForChildObjectErrors()
+    {
+        var mappingContext = new FormMappingContext("");
+        var formName = "myForm";
+        var attribute = new SupplyParameterFromFormAttribute { FormName = formName };
+        var parameterInfo = new CascadingParameterInfo(attribute, "Model", typeof(TestModelWithChild));
+
+        var parentKey = "Model.SubModels[0]";
+        var childErrorKey = "Model.SubModels[0].SubModelNumber";
+
+        var mapper = new ErrorTriggeringFormValueMapper(childErrorKey, parentKey);
+
+        var result = SupplyParameterFromFormValueProvider.GetFormPostValue(mapper, mappingContext, parameterInfo);
+
+        var error = mappingContext.GetErrors(formName, childErrorKey);
+        Assert.NotNull(error);
+        Assert.Equal("container", error.Container);
+    }
+
+    private class ErrorTriggeringFormValueMapper : IFormValueMapper
+    {
+        private readonly string _errorKey;
+        private readonly string _parentKey;
+
+        public ErrorTriggeringFormValueMapper(string errorKey, string parentKey)
+        {
+            _errorKey = errorKey;
+            _parentKey = parentKey;
+        }
+
+        public bool CanMap(Type valueType, string mappingScopeName, string formName) => true;
+
+        public void Map(FormValueMappingContext context)
+        {
+            context.OnError!(_errorKey, $"The value '' is not valid for '{_errorKey}'.", "");
+            context.MapErrorToContainer!(_parentKey, "container");
+        }
+    }
+
+    private class TestModelWithChild
+    {
+        public string Name { get; set; }
+        public TestSubModel[] SubModels { get; set; }
+    }
+
+    private class TestSubModel
+    {
+        public int SubModelNumber { get; set; }
     }
 }
