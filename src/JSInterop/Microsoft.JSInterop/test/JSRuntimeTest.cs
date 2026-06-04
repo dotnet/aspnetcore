22162f22e7ec1278a745f74242230561a74f52c6
GOVINSAGA <73192061+GOVINSAGA@users.noreply.github.com>
2026-06-04 14:48:31 +0530

    Throw JSException on DefaultAsyncTimeout in JSInterop

--- src/JSInterop/Microsoft.JSInterop/test/JSRuntimeTest.cs
+++ src/JSInterop/Microsoft.JSInterop/test/JSRuntimeTest.cs
@@ -47,7 +47,8 @@ public class JSRuntimeTest
         var task = runtime.InvokeAsync<object>("test identifier 1", "arg1", 123, true);
 
         // Assert
-        await Assert.ThrowsAsync<TaskCanceledException>(async () => await task);
+        var ex = await Assert.ThrowsAsync<JSException>(async () => await task);
+        Assert.Equal("The JS interop call 'test identifier 1' timed out after 1000ms. The timeout is controlled by DefaultAsyncTimeout.", ex.Message);
     }
 
     [Fact]
