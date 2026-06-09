fbcaa5c9fc783518ac11e81c2a4ba8d8abbef905
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2026-06-09 18:10:49 +0530

    Added the test case

--- src/Components/QuickGrid/Microsoft.AspNetCore.Components.QuickGrid/test/QuickGridFooterTemplateTest.cs
+++ src/Components/QuickGrid/Microsoft.AspNetCore.Components.QuickGrid/test/QuickGridFooterTemplateTest.cs
@@ -97,6 +97,62 @@ public class QuickGridFooterTemplateTest
         Assert.False(component.ShowFooter);
     }
 
+    [Fact]
+    public void ColumnFooterTemplate_NotSet_DoesNotRenderColumnFooterRow()
+    {
+        var serviceProvider = BuildServiceProvider();
+        var renderer = new TestRenderer(serviceProvider);
+
+        var component = new GridWithNoColumnFooters();
+        var componentId = renderer.AssignRootComponentId(component);
+        renderer.RenderRootComponent(componentId);
+
+        Assert.False(component.HasColumnFooters);
+    }
+
+    [Fact]
+    public void ColumnFooterTemplate_Set_RendersColumnFooterRow()
+    {
+        var serviceProvider = BuildServiceProvider();
+        var renderer = new TestRenderer(serviceProvider);
+
+        var component = new GridWithColumnFooters();
+        var componentId = renderer.AssignRootComponentId(component);
+        renderer.RenderRootComponent(componentId);
+
+        Assert.True(component.HasColumnFooters);
+        Assert.Equal(2, component.ColumnCount);
+    }
+
+    [Fact]
+    public void ColumnFooterTemplate_PartialColumns_RendersFooterRowForAllColumns()
+    {
+        var serviceProvider = BuildServiceProvider();
+        var renderer = new TestRenderer(serviceProvider);
+
+        var component = new GridWithPartialColumnFooters();
+        var componentId = renderer.AssignRootComponentId(component);
+        renderer.RenderRootComponent(componentId);
+
+        Assert.True(component.HasColumnFooters);
+        Assert.Equal(2, component.TotalColumns);
+        Assert.Equal(1, component.ColumnsWithFooter);
+    }
+
+    [Fact]
+    public void ColumnFooterTemplate_AndGridFooterTemplate_BothRender()
+    {
+        var serviceProvider = BuildServiceProvider();
+        var renderer = new TestRenderer(serviceProvider);
+
+        var component = new GridWithBothFooterTypes();
+        var componentId = renderer.AssignRootComponentId(component);
+        renderer.RenderRootComponent(componentId);
+
+        Assert.True(component.HasGridFooter);
+        Assert.True(component.HasColumnFooters);
+    }
+
     // --- Helper components ---
 
     private class TestItem
@@ -229,6 +285,110 @@ public class QuickGridFooterTemplateTest
                 }));
             }
             builder.CloseComponent();
+        }
+    }
+
+    private sealed class GridWithNoColumnFooters : ComponentBase
+    {
+        public bool HasColumnFooters { get; private set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenComponent<QuickGrid<TestItem>>(0);
+            builder.AddAttribute(1, "Items", _testItems);
+            builder.AddAttribute(2, "ChildContent", (RenderFragment)(b =>
+            {
+                b.OpenComponent<PropertyColumn<TestItem, int>>(0);
+                b.AddAttribute(1, "Property", (System.Linq.Expressions.Expression<Func<TestItem, int>>)(p => p.Id));
+                b.CloseComponent();
+            }));
+            builder.CloseComponent();
+
+            HasColumnFooters = false;
+        }
+    }
+
+    private sealed class GridWithColumnFooters : ComponentBase
+    {
+        public bool HasColumnFooters { get; private set; }
+        public int ColumnCount { get; private set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenComponent<QuickGrid<TestItem>>(0);
+            builder.AddAttribute(1, "Items", _testItems);
+            builder.AddAttribute(2, "ChildContent", (RenderFragment)(b =>
+            {
+                b.OpenComponent<PropertyColumn<TestItem, int>>(0);
+                b.AddAttribute(1, "Property", (System.Linq.Expressions.Expression<Func<TestItem, int>>)(p => p.Id));
+                b.AddAttribute(2, "FooterTemplate", (RenderFragment)(fb => fb.AddContent(0, "Total IDs")));
+                b.CloseComponent();
+
+                b.OpenComponent<PropertyColumn<TestItem, string>>(3);
+                b.AddAttribute(4, "Property", (System.Linq.Expressions.Expression<Func<TestItem, string>>)(p => p.Name));
+                b.AddAttribute(5, "FooterTemplate", (RenderFragment)(fb => fb.AddContent(0, "Total Names")));
+                b.CloseComponent();
+            }));
+            builder.CloseComponent();
+
+            HasColumnFooters = true;
+            ColumnCount = 2;
+        }
+    }
+
+    private sealed class GridWithPartialColumnFooters : ComponentBase
+    {
+        public bool HasColumnFooters { get; private set; }
+        public int TotalColumns { get; private set; }
+        public int ColumnsWithFooter { get; private set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenComponent<QuickGrid<TestItem>>(0);
+            builder.AddAttribute(1, "Items", _testItems);
+            builder.AddAttribute(2, "ChildContent", (RenderFragment)(b =>
+            {
+                b.OpenComponent<PropertyColumn<TestItem, int>>(0);
+                b.AddAttribute(1, "Property", (System.Linq.Expressions.Expression<Func<TestItem, int>>)(p => p.Id));
+                b.CloseComponent();
+
+                b.OpenComponent<PropertyColumn<TestItem, string>>(2);
+                b.AddAttribute(3, "Property", (System.Linq.Expressions.Expression<Func<TestItem, string>>)(p => p.Name));
+                b.AddAttribute(4, "FooterTemplate", (RenderFragment)(fb => fb.AddContent(0, $"Count: {_testItems.Count()}")));
+                b.CloseComponent();
+            }));
+            builder.CloseComponent();
+
+            HasColumnFooters = true;
+            TotalColumns = 2;
+            ColumnsWithFooter = 1;
+        }
+    }
+
+    private sealed class GridWithBothFooterTypes : ComponentBase
+    {
+        public bool HasGridFooter { get; private set; }
+        public bool HasColumnFooters { get; private set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenComponent<QuickGrid<TestItem>>(0);
+            builder.AddAttribute(1, "Items", _testItems);
+            builder.AddAttribute(2, "ChildContent", (RenderFragment)(b =>
+            {
+                b.OpenComponent<PropertyColumn<TestItem, int>>(0);
+                b.AddAttribute(1, "Property", (System.Linq.Expressions.Expression<Func<TestItem, int>>)(p => p.Id));
+                b.AddAttribute(2, "FooterTemplate", (RenderFragment)(fb => fb.AddContent(0, "Column footer")));
+                b.CloseComponent();
+            }));
+            builder.AddAttribute(3, "FooterTemplate", (RenderFragment)(b =>
+            {
+                b.AddContent(0, $"Grand total: {_testItems.Count()}");
+            }));
+            builder.CloseComponent();
+
+            HasGridFooter = true;
+            HasColumnFooters = true;
         }
     }
 }
