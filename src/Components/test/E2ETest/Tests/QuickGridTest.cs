c862aa48639499d348718be278a4e6625699e3d9
Yuvan111 <yuvanshankar.arunagiri@syncfusion.com>
2026-06-11 12:08:26 +0530

    Correction in RowDetailsTemplate test case for Quick Grid component

--- src/Components/test/E2ETest/Tests/QuickGridTest.cs
+++ src/Components/test/E2ETest/Tests/QuickGridTest.cs
@@ -66,7 +66,7 @@ public class QuickGridTest : ServerTestBase<ToggleExecutionModeServerFixture<Pro
             if (firstName == "Julie")
             {
                 isJulieRowFound = true;
-                Assert.Equal("row-clickable highlight", row.GetDomAttribute("class"));
+                Assert.Equal("row-clickable highlight ", row.GetDomAttribute("class"));
             }
             else
             {
