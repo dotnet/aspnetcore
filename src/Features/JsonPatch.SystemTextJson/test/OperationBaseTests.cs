f5ce08fe90bbd167b6b3470c73c82e296bdcd8a3
bbratic <bojana.bratic14@gmail.com>
2026-06-15 22:20:50 +0200

    Restore original OperationBaseTests structure

--- src/Features/JsonPatch.SystemTextJson/test/OperationBaseTests.cs
+++ src/Features/JsonPatch.SystemTextJson/test/OperationBaseTests.cs
@@ -1,38 +1,40 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Xunit;
+
 namespace Microsoft.AspNetCore.JsonPatch.SystemTextJson.Operations;
 
 public class OperationBaseTests
 {
-	[Xunit.Theory]
-	[Xunit.InlineData("ADd", OperationType.Add)]
-	[Xunit.InlineData("Copy", OperationType.Copy)]
-	[Xunit.InlineData("mOVE", OperationType.Move)]
-	[Xunit.InlineData("REMOVE", OperationType.Remove)]
-	[Xunit.InlineData("replace", OperationType.Replace)]
-	[Xunit.InlineData("TeSt", OperationType.Test)]
-	public void SetValidOperationType(string op, OperationType operationType)
-	{
-		// Arrange
-		var operationBase = new OperationBase();
-		operationBase.op = op;
+    [Theory]
+    [InlineData("ADd", OperationType.Add)]
+    [InlineData("Copy", OperationType.Copy)]
+    [InlineData("mOVE", OperationType.Move)]
+    [InlineData("REMOVE", OperationType.Remove)]
+    [InlineData("replace", OperationType.Replace)]
+    [InlineData("TeSt", OperationType.Test)]
+    public void SetValidOperationType(string op, OperationType operationType)
+    {
+        // Arrange
+        var operationBase = new OperationBase();
+        operationBase.op = op;
 
-		// Act & Assert
-		Xunit.Assert.Equal(operationType, operationBase.OperationType);
-	}
+        // Act & Assert
+        Assert.Equal(operationType, operationBase.OperationType);
+    }
 
-	[Xunit.Theory]
-	[Xunit.InlineData("invalid", OperationType.Invalid)]
-	[Xunit.InlineData("coppy", OperationType.Invalid)]
-	[Xunit.InlineData("notvalid", OperationType.Invalid)]
-	public void InvalidOperationType_SetsOperationTypeInvalid(string op, OperationType operationType)
-	{
-		// Arrange
-		var operationBase = new OperationBase();
-		operationBase.op = op;
+    [Theory]
+    [InlineData("invalid", OperationType.Invalid)]
+    [InlineData("coppy", OperationType.Invalid)]
+    [InlineData("notvalid", OperationType.Invalid)]
+    public void InvalidOperationType_SetsOperationTypeInvalid(string op, OperationType operationType)
+    {
+        // Arrange
+        var operationBase = new OperationBase();
+        operationBase.op = op;
 
-		// Act & Assert
-		Xunit.Assert.Equal(operationType, operationBase.OperationType);
-	}
+        // Act & Assert
+        Assert.Equal(operationType, operationBase.OperationType);
+    }
 }
