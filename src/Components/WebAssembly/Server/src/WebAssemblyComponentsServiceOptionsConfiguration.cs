43a5fa7748afc111cf599057962a0a86509fd322
Daria Tiurina <dariatiurina@microsoft.com>
2026-06-22 18:10:50 +0200

    Fix culture API

---
+++ src/Components/WebAssembly/Server/src/WebAssemblyComponentsServiceOptionsConfiguration.cs
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.AspNetCore.Components.WebAssembly.Server;
+
+internal sealed class WebAssemblyComponentsServiceOptionsConfiguration(IConfiguration configuration) : IPostConfigureOptions<WebAssemblyComponentsServiceOptions>
+{
+    public void PostConfigure(string? name, WebAssemblyComponentsServiceOptions options)
+    {
+        var value = configuration["Components:UseCultureFromServer"];
+        if (!string.IsNullOrEmpty(value) && bool.TryParse(value, out var useCultureFromServer))
+        {
+            options.UseCultureFromServer = useCultureFromServer;
+        }
+    }
+}
