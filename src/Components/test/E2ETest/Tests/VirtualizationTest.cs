b3a81307d406167538e96b1e4db5e9a444a8248d
Ilona Tomkowicz <itomkowicz@microsoft.com>
2026-07-17 17:59:45 +0200

    Block failure from CI.

--- src/Components/test/E2ETest/Tests/VirtualizationTest.cs
+++ src/Components/test/E2ETest/Tests/VirtualizationTest.cs
@@ -2645,7 +2645,8 @@ public class VirtualizationTest : ServerTestBase<ToggleExecutionModeServerFixtur
     [InlineData("2", false)]
     [InlineData("0", true)]
     [InlineData("1", true)]
-    [InlineData("2", true)]
+    // Disabled pending fix https://github.com/dotnet/aspnetcore/issues/67865 (End mode converges to bottom on mount before the "start near top" precondition; scrollTop 54776 ≈ scrollHeight 55076. Flake on slow Linux Mono CI leg, build 1514924):
+    // [InlineData("2", true)]
     public void QuickGrid_AnchorMode_EndKeyJumpsToBottom(string anchorMode, bool useItemsProvider)
     {
         MountQuickGridAnchorModeComponent(anchorMode, useItemsProvider);
