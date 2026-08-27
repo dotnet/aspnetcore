87f0ec51090c2d93c9eb59a2384ef5447c487fdc
Daria Tiurina <dariatiurina@microsoft.com>
2026-08-27 15:17:45 +0200

    Feedback

--- src/Components/Samples/BlazorWebAppGlobal.Client/Layout/NavMenu.razor.js
+++ src/Components/Samples/BlazorWebAppGlobal.Client/Layout/NavMenu.razor.js
@@ -1,17 +1,19 @@
 // Handle navigation menu toggle
-customElements.define('nav-menu', class extends HTMLElement {
-    #closeMenu = event => {
-        const toggler = this.querySelector('.navbar-toggler');
-        if (toggler?.checked && event.target.closest('.nav-scrollable')) {
-            toggler.checked = false;
+if (!customElements.get('nav-menu')) {
+    customElements.define('nav-menu', class extends HTMLElement {
+        #closeMenu = event => {
+            const toggler = this.querySelector('.navbar-toggler');
+            if (toggler?.checked && event.target.closest('.nav-scrollable')) {
+                toggler.checked = false;
+            }
+        };
+
+        connectedCallback() {
+            this.addEventListener('click', this.#closeMenu);
         }
-    };
 
-    connectedCallback() {
-        this.addEventListener('click', this.#closeMenu);
-    }
-
-    disconnectedCallback() {
-        this.removeEventListener('click', this.#closeMenu);
-    }
-});
+        disconnectedCallback() {
+            this.removeEventListener('click', this.#closeMenu);
+        }
+    });
+}
