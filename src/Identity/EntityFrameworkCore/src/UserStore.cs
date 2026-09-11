48451ac33c6f2c6e494cc7daca5fc85ed6de88a8
Javier Calvarro Nelson <6995051+javiercn@users.noreply.github.com>
2026-09-11 17:23:05 +0200

    Use ordinal equality for external login keys

--- src/Identity/EntityFrameworkCore/src/UserStore.cs
+++ src/Identity/EntityFrameworkCore/src/UserStore.cs
@@ -335,9 +335,11 @@ public class UserStore<TUser, TRole, TContext, [DynamicallyAccessedMembers(Dynam
     /// <param name="providerKey">The key provided by the <paramref name="loginProvider"/> to identify a user.</param>
     /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to propagate notifications that the operation should be canceled.</param>
     /// <returns>The user login if it exists.</returns>
-    protected override Task<TUserLogin?> FindUserLoginAsync(TKey userId, string loginProvider, string providerKey, CancellationToken cancellationToken)
+    protected override async Task<TUserLogin?> FindUserLoginAsync(TKey userId, string loginProvider, string providerKey, CancellationToken cancellationToken)
     {
-        return UserLogins.SingleOrDefaultAsync(userLogin => userLogin.UserId.Equals(userId) && userLogin.LoginProvider == loginProvider && userLogin.ProviderKey == providerKey, cancellationToken);
+        var userLogin = await UserLogins.SingleOrDefaultAsync(userLogin => userLogin.UserId.Equals(userId) && userLogin.LoginProvider == loginProvider && userLogin.ProviderKey == providerKey, cancellationToken);
+
+        return IsMatchingUserLogin(userLogin, loginProvider, providerKey) ? userLogin : null;
     }
 
     /// <summary>
@@ -347,10 +349,17 @@ public class UserStore<TUser, TRole, TContext, [DynamicallyAccessedMembers(Dynam
     /// <param name="providerKey">The key provided by the <paramref name="loginProvider"/> to identify a user.</param>
     /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to propagate notifications that the operation should be canceled.</param>
     /// <returns>The user login if it exists.</returns>
-    protected override Task<TUserLogin?> FindUserLoginAsync(string loginProvider, string providerKey, CancellationToken cancellationToken)
+    protected override async Task<TUserLogin?> FindUserLoginAsync(string loginProvider, string providerKey, CancellationToken cancellationToken)
     {
-        return UserLogins.SingleOrDefaultAsync(userLogin => userLogin.LoginProvider == loginProvider && userLogin.ProviderKey == providerKey, cancellationToken);
+        var userLogin = await UserLogins.SingleOrDefaultAsync(userLogin => userLogin.LoginProvider == loginProvider && userLogin.ProviderKey == providerKey, cancellationToken);
+
+        return IsMatchingUserLogin(userLogin, loginProvider, providerKey) ? userLogin : null;
     }
+
+    private static bool IsMatchingUserLogin(TUserLogin? userLogin, string loginProvider, string providerKey)
+        => userLogin is not null &&
+            string.Equals(userLogin.LoginProvider, loginProvider, StringComparison.Ordinal) &&
+            string.Equals(userLogin.ProviderKey, providerKey, StringComparison.Ordinal);
 
     /// <summary>
     /// Adds the given <paramref name="normalizedRoleName"/> to the specified <paramref name="user"/>.
