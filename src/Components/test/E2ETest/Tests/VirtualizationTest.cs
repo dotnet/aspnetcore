ca331e27ec082dd601d415223cc315d8e347e65a
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2026-09-14 14:20:41 +0200

    Unquarantine passing AnchorMode_Start_LargePrependAtTop_StillShowsNewItems. (#69271)

--- src/Components/test/E2ETest/Tests/VirtualizationTest.cs
+++ src/Components/test/E2ETest/Tests/VirtualizationTest.cs
@@ -4288,7 +4288,6 @@ public class VirtualizationTest : ServerTestBase<ToggleExecutionModeServerFixtur
     [InlineData(true, false)]
     [InlineData(false, true)]
     [InlineData(true, true)]
-    [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/68728")]
     public void AnchorMode_Start_LargePrependAtTop_StillShowsNewItems(bool variableHeight, bool useItemsProvider)
     {
         MountAnchorModeComponent("1", variableHeight, useItemsProvider, delay: useItemsProvider);
