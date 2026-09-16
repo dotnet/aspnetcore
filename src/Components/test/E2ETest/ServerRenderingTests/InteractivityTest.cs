cb2b2e7f09897bee47ee544d56072a6420786f54
Daria Tiurina <dariatiurina@microsoft.com>
2026-09-16 11:49:57 +0200

    Fix tests that use AppContextSwitch (#67492)

--- src/Components/test/E2ETest/ServerRenderingTests/InteractivityTest.cs
+++ src/Components/test/E2ETest/ServerRenderingTests/InteractivityTest.cs
@@ -1592,7 +1592,7 @@ public class InteractivityTest : ServerTestBase<BasicTestAppServerSiteFixture<Ra
     [InlineData(false)]
     public void NavigatesWithInteractivityByRequestRedirection(bool controlFlowByException)
     {
-        AppContext.SetSwitch("Microsoft.AspNetCore.Components.Endpoints.NavigationManager.DisableThrowNavigationException", isEnabled: !controlFlowByException);
+        TestFeatureSwitches.SetDisableThrowNavigationException(!controlFlowByException);
         Navigate($"{ServerPathBase}/routing/ssr-navigate-to");
         Browser.Equal("Click submit to navigate to home", () => Browser.Exists(By.Id("test-info")).Text);
         Browser.Click(By.Id("redirectButton"));
