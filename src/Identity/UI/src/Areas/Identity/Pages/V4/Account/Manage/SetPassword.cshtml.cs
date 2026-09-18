69ec73c90e7f4e7871d0d0f814b77004b96c7b2d
Roland Vı́zner <t-rvzner@microsoft.com>
2026-09-18 14:27:41 +0200

    Make SetPassword reauthentication state internal

--- src/Identity/UI/src/Areas/Identity/Pages/V4/Account/Manage/SetPassword.cshtml.cs
+++ src/Identity/UI/src/Areas/Identity/Pages/V4/Account/Manage/SetPassword.cshtml.cs
@@ -34,7 +34,7 @@ public abstract class SetPasswordModel : PageModel
     ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
     ///     directly from your code. This API may change or be removed in future releases.
     /// </summary>
-    public bool IsReauthenticated { get; set; }
+    internal bool IsReauthenticated { get; set; }
 
     /// <summary>
     ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
