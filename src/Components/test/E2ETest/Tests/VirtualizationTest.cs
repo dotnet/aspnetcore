7caf394369a81bf309313b25049a925dc25de11d
irfana jaffer sadhik <irfana.jaffersadhik@syncfusion.com>
2026-09-18 12:54:41 +0530

    Alternate fix

--- src/Components/test/E2ETest/Tests/VirtualizationTest.cs
+++ src/Components/test/E2ETest/Tests/VirtualizationTest.cs
@@ -5828,6 +5828,7 @@ public class VirtualizationTest : ServerTestBase<ToggleExecutionModeServerFixtur
         MountAnchorModeForScrollToItem(useProvider: true, delay: true);
         var container = Browser.Exists(By.Id("scroll-container"));
         var js = (IJavaScriptExecutor)Browser;
+        WaitForRenderToSettle(container, js);
 
         container.SendKeys(Keys.End);
         Browser.True(() => container.FindElements(By.CssSelector(".item[data-index='999']")).Count > 0);
